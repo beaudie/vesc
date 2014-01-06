60ed3ac4e6e6fdb57a9120b0d9adac678cd74162
Jamie Madill <jmadill@chromium.org>
2014-01-06 14:45:54 -0500

    Clamp stencil reference value to D3D11 8-bit limitations.
    D3D11 can only mask 8 bits of a stencil reference value, so the upper bits are ignored.
    GL, however, expects the reference value to be able to scale to 32-bits.
    
    We will expect to fail certain edge cases where the reference value, and mask, both use
    particular 9+ bit values.
    
    Change-Id: I8c7451270d9e40310f4154955d38e51bbf7de4df

--- src/libGLESv2/renderer/d3d11/Renderer11.cpp
+++ src/libGLESv2/renderer/d3d11/Renderer11.cpp
@@ -788,7 +788,7 @@ void Renderer11::setDepthStencilState(const gl::DepthStencilState &depthStencilS
                 "setting the default depth stencil state.");
         }
 
-        mDeviceContext->OMSetDepthStencilState(dxDepthStencilState, static_cast<UINT>(stencilRef));
+        mDeviceContext->OMSetDepthStencilState(dxDepthStencilState, static_cast<UINT>(std::min(stencilRef, 0xFF)));
 
         mCurDepthStencilState = depthStencilState;
         mCurStencilRef = stencilRef;
