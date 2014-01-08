e2b3df2f55560dd281bcd2c8dcf2b48418b99448
Geoff Lang <geofflang@chromium.org>
2014-01-08 13:41:47 -0500

    Add more D3D11 buffer usages.
    Works around performance regressions when index and vertex buffer bind flags are used on the same buffer.
    
    Change-Id: I28bc0d3147c6bd70cec507f20e41d97ec4cc45a5

--- src/libGLESv2/renderer/d3d11/Renderer11.cpp
+++ src/libGLESv2/renderer/d3d11/Renderer11.cpp
@@ -660,7 +660,7 @@ bool Renderer11::setUniformBuffers(const gl::Buffer *vertexUniformBuffers[], con
         if (uniformBuffer)
         {
             BufferStorage11 *bufferStorage = BufferStorage11::makeBufferStorage11(uniformBuffer->getStorage());
-            ID3D11Buffer *constantBuffer = bufferStorage->getBuffer(true);
+            ID3D11Buffer *constantBuffer = bufferStorage->getBuffer(BUFFER_USAGE_UNIFORM);
 
             if (!constantBuffer)
             {
@@ -682,7 +682,7 @@ bool Renderer11::setUniformBuffers(const gl::Buffer *vertexUniformBuffers[], con
         if (uniformBuffer)
         {
             BufferStorage11 *bufferStorage = BufferStorage11::makeBufferStorage11(uniformBuffer->getStorage());
-            ID3D11Buffer *constantBuffer = bufferStorage->getBuffer(true);
+            ID3D11Buffer *constantBuffer = bufferStorage->getBuffer(BUFFER_USAGE_UNIFORM);
 
             if (!constantBuffer)
             {
@@ -1132,7 +1132,7 @@ GLenum Renderer11::applyIndexBuffer(const GLvoid *indices, gl::Buffer *elementAr
                 BufferStorage11 *storage = BufferStorage11::makeBufferStorage11(indexInfo->storage);
                 IndexBuffer11* indexBuffer = IndexBuffer11::makeIndexBuffer11(indexInfo->indexBuffer);
 
-                mDeviceContext->IASetIndexBuffer(storage->getBuffer(false), indexBuffer->getIndexFormat(), indexInfo->startOffset);
+                mDeviceContext->IASetIndexBuffer(storage->getBuffer(BUFFER_USAGE_INDEX), indexBuffer->getIndexFormat(), indexInfo->startOffset);
 
                 mAppliedIBSerial = 0;
                 mAppliedStorageIBSerial = storage->getSerial();
