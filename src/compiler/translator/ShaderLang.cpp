3c7f6b38551b6a9783e603ae24c1bcefb852bfaa
Vsevolod Buzinov <vbuzinov@nvidia.com>
2014-01-23 16:48:48 +0200

    Implement gl_TexCoord support for use with NVPR
    GL_CHROMIUM_NV_path_rendering extension makes use of gl_TexCoord
    in the fragment shaders it generates.
    
    Make gl_TexCoord available if it is requested with
    ResourceInfo::CHROMIUM_NV_path_rendering flag. Provide gl_MaxTextureCoords value
    with ResourceInfo::MaxTextureCoords variable.
    
    The variable gl_TexCoord will be read-only in fragment shader and
    writeable in vertex shader.
    
    Change-Id: I0691dbbec6d7713e79f89f4e62a565cbeb99be58

--- src/compiler/translator/ShaderLang.cpp
+++ src/compiler/translator/ShaderLang.cpp
@@ -76,6 +76,7 @@ void ShInitBuiltInResources(ShBuiltInResources* resources)
     resources->MaxTextureImageUnits = 8;
     resources->MaxFragmentUniformVectors = 16;
     resources->MaxDrawBuffers = 1;
+    resources->MaxTextureCoords = 0;
 
     // Extensions.
     resources->OES_standard_derivatives = 0;
@@ -83,6 +84,7 @@ void ShInitBuiltInResources(ShBuiltInResources* resources)
     resources->ARB_texture_rectangle = 0;
     resources->EXT_draw_buffers = 0;
     resources->EXT_frag_depth = 0;
+    resources->CHROMIUM_NV_path_rendering = 0;
 
     // Disable highp precision in fragment shader by default.
     resources->FragmentPrecisionHigh = 0;
