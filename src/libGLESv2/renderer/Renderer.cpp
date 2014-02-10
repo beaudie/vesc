eec06feeb2a41d7e39e2f49f2fb1d2afc9e412b2
Geoff Lang <geofflang@chromium.org>
2014-02-10 13:00:44 -0500

    Add defines to enable or disable specific renderers.
    Change-Id: I88c168205b142701ec843aa394c82c3a5857ed0d

--- src/libGLESv2/renderer/Renderer.cpp
+++ src/libGLESv2/renderer/Renderer.cpp
@@ -11,77 +11,76 @@
 #include "libGLESv2/main.h"
 #include "libGLESv2/Program.h"
 #include "libGLESv2/renderer/Renderer.h"
-#include "libGLESv2/renderer/d3d9/Renderer9.h"
-#include "libGLESv2/renderer/d3d11/Renderer11.h"
 #include "common/utilities.h"
 #include "third_party/trace_event/trace_event.h"
 
-#if !defined(ANGLE_ENABLE_D3D11)
+#if defined (ANGLE_ENABLE_D3D9)
+#include "libGLESv2/renderer/d3d9/Renderer9.h"
+#endif // ANGLE_ENABLE_D3D9
+
+#if defined (ANGLE_ENABLE_D3D11)
+#include "libGLESv2/renderer/d3d11/Renderer11.h"
+#endif // ANGLE_ENABLE_D3D11
+
+#if !defined(ANGLE_DEFAULT_D3D11)
 // Enables use of the Direct3D 11 API for a default display, when available
-#define ANGLE_ENABLE_D3D11 1
+#define ANGLE_DEFAULT_D3D11 1
 #endif
 
 namespace rx
 {
 
-Renderer::Renderer(egl::Display *display) : mDisplay(display)
-{
-    mCurrentClientVersion = 2;
-}
+    Renderer::Renderer(egl::Display *display) : mDisplay(display)
+    {
+        mCurrentClientVersion = 2;
+    }
 
 }
 
 extern "C"
 {
 
-rx::Renderer *glCreateRenderer(egl::Display *display, HDC hDc, EGLNativeDisplayType displayId)
-{
-    rx::Renderer *renderer = NULL;
-    EGLint status = EGL_BAD_ALLOC;
-    
-    if (ANGLE_ENABLE_D3D11 ||
-        displayId == EGL_D3D11_ELSE_D3D9_DISPLAY_ANGLE ||
-        displayId == EGL_D3D11_ONLY_DISPLAY_ANGLE)
+    rx::Renderer *glCreateRenderer(egl::Display *display, HDC hDc, EGLNativeDisplayType displayId)
     {
-        renderer = new rx::Renderer11(display, hDc);
-    
-        if (renderer)
+#if defined(ANGLE_ENABLE_D3D11)
+        if (ANGLE_DEFAULT_D3D11 ||
+            displayId == EGL_D3D11_ELSE_D3D9_DISPLAY_ANGLE ||
+            displayId == EGL_D3D11_ONLY_DISPLAY_ANGLE)
         {
-            status = renderer->initialize();
+            rx::Renderer11 *renderer = new rx::Renderer11(display, hDc);
+            if (renderer->initialize() == EGL_SUCCESS)
+            {
+                return renderer;
+            }
+            else
+            {
+                // Failed to create a D3D11 renderer, try D3D9
+                SafeDelete(renderer);
+            }
         }
+#endif
 
-        if (status == EGL_SUCCESS)
+#if defined(ANGLE_ENABLE_D3D9)
+        if (displayId != EGL_D3D11_ONLY_DISPLAY_ANGLE)
         {
-            return renderer;
+            rx::Renderer9 *renderer = new rx::Renderer9(display, hDc, false);
+            if (renderer->initialize() == EGL_SUCCESS)
+            {
+                return renderer;
+            }
+            else
+            {
+                SafeDelete(renderer);
+            }
         }
-        else if (displayId == EGL_D3D11_ONLY_DISPLAY_ANGLE)
-        {
-            return NULL;
-        }
+#endif
 
-        // Failed to create a D3D11 renderer, try creating a D3D9 renderer
+        return NULL;
+    }
+
+    void glDestroyRenderer(rx::Renderer *renderer)
+    {
         delete renderer;
     }
-
-    bool softwareDevice = (displayId == EGL_SOFTWARE_DISPLAY_ANGLE);
-    renderer = new rx::Renderer9(display, hDc, softwareDevice);
-    
-    if (renderer)
-    {
-        status = renderer->initialize();
-    }
-
-    if (status == EGL_SUCCESS)
-    {
-        return renderer;
-    }
-
-    return NULL;
-}
-
-void glDestroyRenderer(rx::Renderer *renderer)
-{
-    delete renderer;
-}
 
 }
