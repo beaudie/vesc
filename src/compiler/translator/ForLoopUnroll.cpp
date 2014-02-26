f6679ec2b771f42868f8f2c042334c6e578ed61b
Zhenyao Mo <zmo@chromium.org>
2014-02-26 15:45:34 -0800

    Remove tabs
    Change-Id: I044c2143038b10480222895db63743b5b98ff995

--- src/compiler/translator/ForLoopUnroll.cpp
+++ src/compiler/translator/ForLoopUnroll.cpp
@@ -36,7 +36,7 @@ bool ForLoopUnrollMarker::visitBinary(Visit, TIntermBinary *node)
                         break;
                       default:
                         UNREACHABLE();
-		    }
+                    }
                 }
             }
         }
