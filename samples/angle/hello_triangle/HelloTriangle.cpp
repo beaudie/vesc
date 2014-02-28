6cfc8d13a0af6105752d7c3c6cc54ecaca2c8f88
Geoff Lang <geofflang@chromium.org>
2014-02-28 13:11:18 -0500

    Port the Hello_Triangle sample to the new sample framework.
    BUG=angle:521
    
    Change-Id: I887257193af3130447c4691bb72303cc978b96fe

---
+++ samples/angle/hello_triangle/HelloTriangle.cpp
@@ -0,0 +1,81 @@
+#include "SampleApplication.h"
+#include "shaderutil.h"
+
+class HelloTriangleSample : public SampleApplication
+{
+  public:
+    HelloTriangleSample::HelloTriangleSample()
+        : SampleApplication("HelloTriangle", 1280, 720)
+    {
+    }
+
+    virtual bool initialize()
+    {
+        const std::string vs = SHADER_SOURCE
+        (
+            attribute vec4 vPosition;
+            void main()
+            {
+                gl_Position = vPosition;
+            }
+        );
+        
+        const std::string fs = SHADER_SOURCE
+        (
+            precision mediump float;
+            void main()
+            {
+                gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+            }
+        );
+
+        mProgram = CompileProgram(vs, fs);
+        if (!mProgram)
+        {
+            return false;
+        }
+
+        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+        return true;
+    }
+
+    virtual void destroy()
+    {
+        glDeleteProgram(mProgram);
+    }
+
+    virtual void draw()
+    {
+        GLfloat vertices[] =
+        {
+             0.0f,  0.5f, 0.0f,
+            -0.5f, -0.5f, 0.0f,
+             0.5f, -0.5f, 0.0f,
+        };
+
+        // Set the viewport
+        glViewport(0, 0, getWindow()->getWidth(), getWindow()->getHeight());
+
+        // Clear the color buffer
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Use the program object
+        glUseProgram(mProgram);
+
+        // Load the vertex data
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, vertices);
+        glEnableVertexAttribArray(0);
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+    }
+
+  private:
+    GLuint mProgram;
+};
+
+int main(int argc, char **argv)
+{
+    HelloTriangleSample app;
+    return app.run();
+}
