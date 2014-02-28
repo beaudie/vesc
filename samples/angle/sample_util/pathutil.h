62f71e837a3f501b7d39cacb72796d208f122d76
Geoff Lang <geofflang@chromium.org>
2014-02-28 13:05:51 -0500

    Add an ANGLE sample framework.
    BUG=angle:521
    
    Change-Id: I4e586e2d9ba807ce57c063509fd5c21b880fda8f

---
+++ samples/angle/sample_util/pathutil.h
@@ -0,0 +1,15 @@
+//
+// Copyright (c) 2014 The ANGLE Project Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+
+#ifndef SAMPLE_UTIL_PATH_UTIL_H
+#define SAMPLE_UTIL_PATH_UTIL_H
+
+#include <string>
+
+std::string getExecutablePath();
+std::string getExecutableDir();
+
+#endif // SAMPLE_UTIL_PATH_UTIL_H
