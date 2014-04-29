b9817c417f01affc857ea2eeda1d911b609dd2f3
Jamie Madill <jmadill@chromium.org>
2014-04-29 16:17:22 -0400

    Store current pack operation as a queued command.
    The pack pixels operation is split in two halves: a GPU
    copy then a CPU readback. We defer the CPU readback until
    we can't wait any longer. That is, we do the readback when
    the user maps the buffer, or there's another readback or
    internal operation.
    
    This offers the benefits of doing as much GPU work as
    asynchronously as possible, and only doing the readback
    work on related API calls (map or subsequent pack calls).
    
    BUG=angle:511
    
    Change-Id: I95a01da2b0e842438b180e8cdbb382c9a46ae210

--- src/libGLESv2/renderer/d3d11/BufferStorage11.cpp
+++ src/libGLESv2/renderer/d3d11/BufferStorage11.cpp
@@ -142,11 +142,14 @@ class BufferStorage11::PackStorage11 : public BufferStorage11::TypedBufferStorag
     void packPixels(ID3D11Texture2D *srcTexure, UINT srcSubresource, const PackPixelsParams &params);
 
   private:
+
+    void flushQueuedPackCommand();
+
     ID3D11Texture2D *mStagingTexture;
     DXGI_FORMAT mTextureFormat;
     gl::Extents mTextureSize;
     unsigned char *mMemoryBuffer;
-    PackPixelsParams mPackParams;
+    PackPixelsParams *mQueuedPackCommand;
     bool mDataModified;
 };
 
@@ -635,6 +638,7 @@ BufferStorage11::PackStorage11::PackStorage11(Renderer11 *renderer)
       mStagingTexture(NULL),
       mTextureFormat(DXGI_FORMAT_UNKNOWN),
       mMemoryBuffer(NULL),
+      mQueuedPackCommand(NULL),
       mDataModified(false)
 {
 }
@@ -643,6 +647,7 @@ BufferStorage11::PackStorage11::~PackStorage11()
 {
     SafeRelease(mStagingTexture);
     SafeDeleteArray(mMemoryBuffer);
+    SafeDelete(mQueuedPackCommand);
 }
 
 bool BufferStorage11::PackStorage11::copyFromStorage(TypedBufferStorage11 *source, size_t sourceOffset,
@@ -678,10 +683,8 @@ void *BufferStorage11::PackStorage11::map(GLbitfield access)
     //  and if D3D packs the staging texture memory identically to how we would fill
     //  the pack buffer according to the current pack state.
 
-    ASSERT(mMemoryBuffer);
-    mRenderer->packPixels(mStagingTexture, mPackParams, mMemoryBuffer);
+    flushQueuedPackCommand();
     mDataModified = (mDataModified || (access & GL_MAP_WRITE_BIT) != 0);
-
     return mMemoryBuffer;
 }
 
@@ -692,7 +695,8 @@ void BufferStorage11::PackStorage11::unmap()
 
 void BufferStorage11::PackStorage11::packPixels(ID3D11Texture2D *srcTexure, UINT srcSubresource, const PackPixelsParams &params)
 {
-    mPackParams = params;
+    flushQueuedPackCommand();
+    mQueuedPackCommand = new PackPixelsParams(params);
 
     D3D11_TEXTURE2D_DESC textureDesc;
     srcTexure->GetDesc(&textureDesc);
@@ -751,6 +755,17 @@ void BufferStorage11::PackStorage11::packPixels(ID3D11Texture2D *srcTexure, UINT
 
     // Asynchronous copy
     immediateContext->CopySubresourceRegion(mStagingTexture, 0, 0, 0, 0, srcTexure, srcSubresource, &srcBox);
+}
+
+void BufferStorage11::PackStorage11::flushQueuedPackCommand()
+{
+    ASSERT(mMemoryBuffer);
+
+    if (mQueuedPackCommand)
+    {
+        mRenderer->packPixels(mStagingTexture, *mQueuedPackCommand, mMemoryBuffer);
+        SafeDelete(mQueuedPackCommand);
+    }
 }
 
 }
