google/angle

commit ed14b79dc8e9ec4f4a831bb5213ab67ef01af196

Shaders should fail compile if a boolean's precision is specified
Tested and no regression on WebGL conformance 1.0.3

BUG=angle:628
BUG=368874
TEST=http://www.khronos.org/registry/webgl/sdk/tests/conformance/glsl/misc/boolean_precision.html

Change-Id: Ie74616c3ab846eea19f4bd4a041fc0f00d55f151
Reviewed-on: https://chromium-review.googlesource.com/198884
Reviewed-by: Jamie Madill <jmadill@chromium.org>
Reviewed-by: Nicolas Capens <nicolascapens@chromium.org>
Tested-by: Zhenyao Mo <zmo@chromium.org>

src/compiler/translator/glslang.y
@@ -1254,6 +1254,11 @@ type_specifier
     | precision_qualifier type_specifier_no_prec {
         $$ = $2;
         $$.precision = $1;
+
+        if (!SupportsPrecision($2.type) {
+            context->error(@1, "illegal type for precision qualifier", getBasicString($2.type));
+            context->recover();
+        }
     }
     ;
 

src/compiler/translator/glslang_tab.cpp
@@ -3937,6 +3937,11 @@ yyreduce:
     {
         (yyval.interm.type) = (yyvsp[(2) - (2)].interm.type);
         (yyval.interm.type).precision = (yyvsp[(1) - (2)].interm.precision);
+
+        if (!SupportsPrecision((yyvsp[(2) - (2)].interm.type).type)) {
+            context->error((yylsp[(1) - (1)]), "illegal type for precision qualifier", getBasicString((yyvsp[(2) - (2)].interm.type).type));
+            context->recover();
+        }
     }
     break;
 

src/compiler/translator/BaseTypes.h
@@ -289,6 +289,11 @@ inline bool IsShadowSampler(TBasicType type)
     return false;
 }
 
+inline bool SupportsPrecision(TBasicType type)
+{
+    return type == EbtFloat || type == EbtInt || type == EbtUInt || IsSampler(type);
+}
+
 //
 // Qualifiers and built-ins.  These are mainly used to see what can be read
 // or written, and by the machine dependent translator to know which registers

src/compiler/translator/SymbolTable.h
@@ -377,7 +377,7 @@ public:
     void dump(TInfoSink &infoSink) const;
 
     bool setDefaultPrecision(const TPublicType& type, TPrecision prec) {
-        if (!supportsPrecision(type.type))
+        if (!SupportsPrecision(type.type))
             return false;
         if (type.isAggregate())
             return false; // Not allowed to set for aggregate types
@@ -389,7 +389,7 @@ public:
     // Searches down the precisionStack for a precision qualifier for the specified TBasicType
     TPrecision getDefaultPrecision( TBasicType type){
 
-        if (!supportsPrecision(type))
+        if (!SupportsPrecision(type))
             return EbpUndefined;
 
         // unsigned integers use the same precision as signed
@@ -412,11 +412,6 @@ public:
 
 private:
     ESymbolLevel currentLevel() const { return static_cast<ESymbolLevel>(table.size() - 1); }
-
-    bool supportsPrecision(TBasicType type) {
-      // Only supports precision for int, float, and sampler types.
-      return type == EbtFloat || type == EbtInt || type == EbtUInt || IsSampler(type);
-    }
 
     std::vector<TSymbolTableLevel*> table;
     typedef TMap<TBasicType, TPrecision> PrecisionStackLevel;

beaudie/vesc

commit 4c7cf90a650dce5cc7f0723acb5c9ba646d772c5

Fix missing bracket in translator source.
Broken in ed14b79dc8e9ec4f4a831bb5213ab67ef01af196.

BUG=angle:628

Change-Id: Ia6b92dd13362bb51f0aa17a14278039d568f5434

src/compiler/translator/glslang.y
@@ -1255,7 +1255,7 @@ type_specifier
         $$ = $2;
         $$.precision = $1;
 
-        if (!SupportsPrecision($2.type) {
+        if (!SupportsPrecision($2.type)) {
             context->error(@1, "illegal type for precision qualifier", getBasicString($2.type));
             context->recover();
         }

src/compiler/translator/glslang_tab.cpp
@@ -807,19 +807,19 @@ static const yytype_uint16 yyrline[] =
     1064,  1068,  1072,  1079,  1083,  1087,  1094,  1098,  1102,  1123,
     1132,  1138,  1141,  1147,  1153,  1160,  1169,  1178,  1186,  1189,
     1196,  1200,  1207,  1210,  1214,  1218,  1227,  1236,  1244,  1254,
-    1261,  1264,  1267,  1273,  1280,  1283,  1289,  1292,  1295,  1301,
-    1304,  1319,  1323,  1327,  1331,  1335,  1339,  1344,  1349,  1354,
-    1359,  1364,  1369,  1374,  1379,  1384,  1389,  1394,  1399,  1404,
-    1409,  1414,  1419,  1424,  1429,  1434,  1439,  1444,  1448,  1452,
-    1456,  1460,  1464,  1468,  1472,  1476,  1480,  1484,  1488,  1492,
-    1496,  1500,  1504,  1512,  1520,  1524,  1537,  1537,  1540,  1540,
-    1546,  1549,  1565,  1568,  1577,  1581,  1587,  1594,  1609,  1613,
-    1617,  1618,  1624,  1625,  1626,  1627,  1628,  1632,  1633,  1633,
-    1633,  1643,  1644,  1648,  1648,  1649,  1649,  1654,  1657,  1667,
-    1670,  1676,  1677,  1681,  1689,  1693,  1703,  1708,  1725,  1725,
-    1730,  1730,  1737,  1737,  1745,  1748,  1754,  1757,  1763,  1767,
-    1774,  1781,  1788,  1795,  1806,  1815,  1819,  1826,  1829,  1835,
-    1835
+    1266,  1269,  1272,  1278,  1285,  1288,  1294,  1297,  1300,  1306,
+    1309,  1324,  1328,  1332,  1336,  1340,  1344,  1349,  1354,  1359,
+    1364,  1369,  1374,  1379,  1384,  1389,  1394,  1399,  1404,  1409,
+    1414,  1419,  1424,  1429,  1434,  1439,  1444,  1449,  1453,  1457,
+    1461,  1465,  1469,  1473,  1477,  1481,  1485,  1489,  1493,  1497,
+    1501,  1505,  1509,  1517,  1525,  1529,  1542,  1542,  1545,  1545,
+    1551,  1554,  1570,  1573,  1582,  1586,  1592,  1599,  1614,  1618,
+    1622,  1623,  1629,  1630,  1631,  1632,  1633,  1637,  1638,  1638,
+    1638,  1648,  1649,  1653,  1653,  1654,  1654,  1659,  1662,  1672,
+    1675,  1681,  1682,  1686,  1694,  1698,  1708,  1713,  1730,  1730,
+    1735,  1735,  1742,  1742,  1750,  1753,  1759,  1762,  1768,  1772,
+    1779,  1786,  1793,  1800,  1811,  1820,  1824,  1831,  1834,  1840,
+    1840
 };
 #endif
 
@@ -3939,7 +3939,7 @@ yyreduce:
         (yyval.interm.type).precision = (yyvsp[(1) - (2)].interm.precision);
 
         if (!SupportsPrecision((yyvsp[(2) - (2)].interm.type).type)) {
-            context->error((yylsp[(1) - (1)]), "illegal type for precision qualifier", getBasicString((yyvsp[(2) - (2)].interm.type).type));
+            context->error((yylsp[(1) - (2)]), "illegal type for precision qualifier", getBasicString((yyvsp[(2) - (2)].interm.type).type));
             context->recover();
         }
     }