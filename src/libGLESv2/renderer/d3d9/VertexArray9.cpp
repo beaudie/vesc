5d0d3313308a7920829f7b69bb7fa29847ff9849
Brandon Jones <bajones@chromium.org>
2014-06-06 17:19:38 -0700

    Refactoring VertexArrays
    BUG=angle:676
    Change-Id: If17b05ab667d79adcaacfbd1811ed92c0ce47fff

---
+++ src/libGLESv2/renderer/d3d9/VertexArray9.cpp
@@ -0,0 +1,42 @@
+#include "precompiled.h"
+//
+// Copyright 2014 The ANGLE Project Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+
+// VertexArray9.cpp: Defines the rx::VertexArray9 class which implements rx::VertexArrayImpl.
+
+#include "libGLESv2/renderer/d3d9/VertexArray9.h"
+#include "libGLESv2/renderer/d3d9/Renderer9.h"
+
+namespace rx
+{
+
+VertexArray9::VertexArray9(rx::Renderer9 *renderer)
+    : VertexArrayImpl(),
+      mRenderer(renderer)
+{
+}
+
+VertexArray9::~VertexArray9()
+{
+}
+
+void VertexArray9::setElementArrayBuffer(const gl::Buffer *buffer)
+{
+}
+
+void VertexArray9::setAttribute(size_t idx, const gl::VertexAttribute &attr)
+{
+}
+
+void VertexArray9::setAttributeDivisor(size_t idx, GLuint divisor)
+{
+}
+
+void VertexArray9::enableAttribute(size_t idx, bool enabledState)
+{
+}
+
+}
