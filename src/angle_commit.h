aa86e149272e1ebd824b5c58c6f2e067538bf87f
Brett Wilson <brettw@chromium.org>
2014-06-09 10:12:27 -0400

    Use commit_id.py on Windows, and handle missing git.
    This allows us to delete the Windows batch file.
    
    Changes the commit_id script to take the working directory so that it
    can be called from a different working directory than the angle
    repository is in.
    
    Renames the generated commit header to angle_commit.h. This is being
    written to the shared generated code directory for the entire build,
    and "commit.h" is insufficiently unique.
    
    BUG=angle:669
    
    Change-Id: I35e80411a7e8ba1e02ce3f6a4fc54ed4dbc918f3

---
+++ src/angle_commit.h
@@ -0,0 +1,7 @@
+// This is a default commit hash header, for use when git is not available
+
+#define ANGLE_COMMIT_HASH "unknown hash"
+#define ANGLE_COMMIT_HASH_SIZE 12
+#define ANGLE_COMMIT_DATE "unknown date"
+
+#define ANGLE_DISABLE_PROGRAM_BINARY_LOAD
