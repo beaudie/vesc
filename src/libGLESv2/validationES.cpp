f123942c3e487c555774cb87a8741c834786ffa8
Jamie Madill <jmadill@chromium.org>
2014-06-18 13:11:29 -0400

    Move more draw call validation to the API.
    The GL expects us to reject invalid draw calls before we start
    doing any work, so we can prevent internal unnecessary state
    changes.
    
    BUG=angle:571
    
    Change-Id: Ic71218b3c2d5dc310280d3738bb1387753a10e03

--- src/libGLESv2/validationES.cpp
+++ src/libGLESv2/validationES.cpp
@@ -19,6 +19,7 @@
 #include "libGLESv2/Query.h"
 #include "libGLESv2/ProgramBinary.h"
 #include "libGLESv2/TransformFeedback.h"
+#include "libGLESv2/VertexArray.h"
 
 #include "common/mathutil.h"
 #include "common/utilities.h"
@@ -1312,6 +1313,17 @@ static bool ValidateDrawBase(const gl::Context *context, GLsizei count)
         return gl::error(GL_INVALID_OPERATION, false);
     }
 
+    if (!context->getCurrentProgram())
+    {
+        return gl::error(GL_INVALID_OPERATION, false);
+    }
+
+    gl::ProgramBinary *programBinary = context->getCurrentProgramBinary();
+    if (!programBinary->validateSamplers(NULL))
+    {
+        return gl::error(GL_INVALID_OPERATION, false);
+    }
+
     // No-op if zero count
     return (count > 0);
 }
@@ -1384,6 +1396,12 @@ bool ValidateDrawElements(const gl::Context *context, GLenum mode, GLsizei count
 
     // Check for mapped buffers
     if (context->hasMappedBuffer(GL_ELEMENT_ARRAY_BUFFER))
+    {
+        return gl::error(GL_INVALID_OPERATION, false);
+    }
+
+    gl::VertexArray *vao = context->getCurrentVertexArray();
+    if (!indices && !vao->getElementArrayBuffer())
     {
         return gl::error(GL_INVALID_OPERATION, false);
     }
