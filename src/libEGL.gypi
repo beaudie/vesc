a5584adb77917abfe0f1350e4e415a32e9fb7671
Geoff Lang <geofflang@chromium.org>
2014-07-11 09:52:58 -0400

    Create an Error object to represent GL errors.
    Change-Id: I1a54e6f308b5b0f310c725a7771af737a77ecd42

--- src/libEGL.gypi
+++ src/libEGL.gypi
@@ -25,6 +25,7 @@
             '../include/angle_gl.h',
             'common/RefCountObject.cpp',
             'common/RefCountObject.h',
+            'common/angleutils.cpp',
             'common/angleutils.h',
             'common/blocklayout.cpp',
             'common/blocklayout.h',
---
+++ src/libGLESv2/Error.cpp
@@ -0,0 +1,32 @@
+//
+// Copyright (c) 2014 The ANGLE Project Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+
+#include "libGLESv2/Error.h"
+
+#include "common/angleutils.h"
+
+#include <cstdarg>
+
+namespace gl
+{
+
+Error::Error(GLenum errorCode)
+    : mCode(errorCode),
+      mMessage()
+{
+}
+
+Error::Error(GLenum errorCode, const std::string &msg, ...)
+    : mCode(errorCode),
+      mMessage()
+{
+    va_list vararg;
+    va_start(vararg, msg);
+    mMessage = Format(msg, vararg);
+    va_end(vararg);
+}
+
+}
