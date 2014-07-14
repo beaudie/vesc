bed271a23c4adf6ea580b33e8b06743809124af3
Jamie Madill <jmadill@chromium.org>
2014-07-14 11:27:55 -0400

    Remove sh::InterfaceBlock member info.
    We can compute the interface block member info entirely on the
    API side. This will allow us to get rid of some un-necessary
    code in the compiler.
    
    BUG=angle:466
    
    Change-Id: I664ffc82de5f2723156e51f4e9ffc07e4de162aa

--- src/common/shadervars.h
+++ src/common/shadervars.h
@@ -127,28 +127,6 @@ struct Varying : public ShaderVariable
     std::string structName;
 };
 
-struct BlockMemberInfo
-{
-    BlockMemberInfo(int offset, int arrayStride, int matrixStride, bool isRowMajorMatrix)
-        : offset(offset),
-          arrayStride(arrayStride),
-          matrixStride(matrixStride),
-          isRowMajorMatrix(isRowMajorMatrix)
-    {}
-
-    static BlockMemberInfo getDefaultBlockInfo()
-    {
-        return BlockMemberInfo(-1, -1, -1, false);
-    }
-
-    int offset;
-    int arrayStride;
-    int matrixStride;
-    bool isRowMajorMatrix;
-};
-
-typedef std::vector<BlockMemberInfo> BlockMemberInfoArray;
-
 struct InterfaceBlock
 {
     InterfaceBlock()
@@ -173,7 +151,6 @@ struct InterfaceBlock
     bool isRowMajorLayout;
     bool staticUse;
     std::vector<InterfaceBlockField> fields;
-    std::vector<BlockMemberInfo> blockInfo;
 };
 
 }
