7e127871bf86c9714cfafb77cdb4147686f784e2
Jamie Madill <jmadill@chromium.org>
2014-08-21 10:04:08 -0400

    Add a Resize method to OSWindow.
    BUG=angle:730
    
    Change-Id: I64106f05eb4188305eb34bbabe7d1cde037e1948

--- util/win32/Win32Window.cpp
+++ util/win32/Win32Window.cpp
@@ -474,3 +474,22 @@ OSWindow *CreateOSWindow()
 {
     return new Win32Window();
 }
+
+bool Win32Window::resize(int width, int height)
+{
+    mWidth = width;
+    mHeight = height;
+
+    RECT windowRect;
+    if (!GetWindowRect(mNativeWindow, &windowRect))
+    {
+        return false;
+    }
+
+    if (!MoveWindow(mNativeWindow, windowRect.left, windowRect.top, width, height, FALSE))
+    {
+        return false;
+    }
+
+    return true;
+}
