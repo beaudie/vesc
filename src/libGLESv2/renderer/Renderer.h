f95c8b6079109012742c69d229cf03bdddacc63d
Brandon Jones <bajones@chromium.org>
2014-08-25 17:02:59 -0700

    Starting refactor of ProgramBinary
    This is gonna take a while...
    
    BUG=angle:731
    Change-Id: Ief72c3361b6429f3f6e0bc2d2ea0810d523ff178

--- src/libGLESv2/renderer/Renderer.h
+++ src/libGLESv2/renderer/Renderer.h
@@ -56,6 +56,7 @@ class VertexArrayImpl;
 class BufferStorage;
 struct TranslatedIndexData;
 class ShaderImpl;
+class ProgramBinaryImpl;
 class ShaderExecutable;
 class SwapChain;
 class RenderTarget;
@@ -196,6 +197,7 @@ class Renderer
 
     // Shader creation
     virtual ShaderImpl *createShader(GLenum type) = 0;
+    virtual ProgramBinaryImpl *createProgramBinary() = 0;
 
     // Shader operations
     virtual void releaseShaderCompiler() = 0;
