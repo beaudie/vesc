0bf02a1e873da6c519a23135d31a5bc00e09ce0c
Cooper Partin <coopp@microsoft.com>
2014-09-23 10:25:02 -0700

    Added SurfaceHost to enable additional hosts and abstract access for EGLNativeWindowType.
    Change-Id: I590b52925c6b9127d08eaf0e5ba2a0bde30505c3

--- src/libGLESv2/renderer/d3d/d3d11/Renderer11.h
+++ src/libGLESv2/renderer/d3d/d3d11/Renderer11.h
@@ -57,7 +57,7 @@ class Renderer11 : public Renderer
 
     virtual void sync(bool block);
 
-    virtual SwapChain *createSwapChain(HWND window, HANDLE shareHandle, GLenum backBufferFormat, GLenum depthBufferFormat);
+    virtual SwapChain *createSwapChain(rx::SurfaceHost host, HANDLE shareHandle, GLenum backBufferFormat, GLenum depthBufferFormat);
 
     virtual gl::Error generateSwizzle(gl::Texture *texture);
     virtual void setSamplerState(gl::SamplerType type, int index, const gl::SamplerState &sampler);
