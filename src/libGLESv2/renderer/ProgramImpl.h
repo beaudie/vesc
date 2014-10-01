2dfeed57a543e797f4c9792ed9d5d953f9169230
Brandon Jones <bajones@chromium.org>
2014-10-01 12:49:30 -0700

    Moving Uniform handling into ProgramD3D.
    Moves the last references to HLSLBlockEncoder out of ProgramBinary. Only one reference to ShaderD3D remains.
    
    BUG=angle:731
    Change-Id: Ie23e24e09fbed4c8c5fab3bab814b7092a383c7f

--- src/libGLESv2/renderer/ProgramImpl.h
+++ src/libGLESv2/renderer/ProgramImpl.h
@@ -13,7 +13,10 @@
 #include "libGLESv2/BinaryStream.h"
 #include "libGLESv2/Constants.h"
 #include "libGLESv2/ProgramBinary.h"
+#include "libGLESv2/Shader.h"
 #include "libGLESv2/renderer/Renderer.h"
+
+#include <map>
 
 namespace rx
 {
@@ -30,6 +33,17 @@ public:
     virtual GLenum getTransformFeedbackBufferMode() const = 0;
     virtual std::vector<gl::LinkedVarying> &getTransformFeedbackLinkedVaryings() = 0;
     virtual sh::Attribute *getShaderAttributes() = 0;
+    virtual const std::vector<gl::LinkedUniform*> &getUniforms() const = 0;
+    virtual const std::vector<gl::VariableLocation> &getUniformIndices() const = 0;
+    virtual const std::vector<gl::UniformBlock*> &getUniformBlocks() const = 0;
+    virtual gl::LinkedUniform *getUniformByLocation(GLint location) const = 0;
+    virtual GLuint getUniformBlockIndex(std::string name) const = 0;
+
+    virtual GLint getSamplerMapping(gl::SamplerType type, unsigned int samplerIndex, const gl::Caps &caps) const = 0;
+    virtual GLenum getSamplerTextureType(gl::SamplerType type, unsigned int samplerIndex) const = 0;
+    virtual GLint getUsedSamplerRange(gl::SamplerType type) const = 0;
+    virtual void updateSamplerMapping() = 0;
+    virtual bool validateSamplers(gl::InfoLog *infoLog, const gl::Caps &caps) = 0;
 
     virtual GLenum getBinaryFormat() = 0;
     virtual bool load(gl::InfoLog &infoLog, gl::BinaryInputStream *stream) = 0;
@@ -43,14 +57,41 @@ public:
                       int *registers, std::vector<gl::LinkedVarying> *linkedVaryings,
                       std::map<int, gl::VariableLocation> *outputVariables, const gl::Caps &caps) = 0;
 
-    virtual void initializeUniformStorage(const std::vector<gl::LinkedUniform*> &uniforms) = 0;
+    virtual bool linkUniforms(gl::InfoLog &infoLog, const gl::Shader &vertexShader, const gl::Shader &fragmentShader,
+                              const gl::Caps &caps) = 0;
+    virtual bool linkUniformBlocks(gl::InfoLog &infoLog, const gl::Shader &vertexShader, const gl::Shader &fragmentShader,
+                              const gl::Caps &caps) = 0;
 
     virtual gl::Error applyUniforms(const std::vector<gl::LinkedUniform*> &uniforms) = 0;
-    virtual gl::Error applyUniformBuffers(const std::vector<gl::UniformBlock*> uniformBlocks, const std::vector<gl::Buffer*> boundBuffers,
-                                     const gl::Caps &caps) = 0;
+    virtual gl::Error applyUniformBuffers(const std::vector<gl::Buffer*> boundBuffers, const gl::Caps &caps) = 0;
     virtual bool assignUniformBlockRegister(gl::InfoLog &infoLog, gl::UniformBlock *uniformBlock, GLenum shader,
                                             unsigned int registerIndex, const gl::Caps &caps) = 0;
-    virtual unsigned int getReservedUniformVectors(GLenum shader) = 0;
+    
+    virtual void setUniform1fv(GLint location, GLsizei count, const GLfloat *v) = 0;
+    virtual void setUniform2fv(GLint location, GLsizei count, const GLfloat *v) = 0;
+    virtual void setUniform3fv(GLint location, GLsizei count, const GLfloat *v) = 0;
+    virtual void setUniform4fv(GLint location, GLsizei count, const GLfloat *v) = 0;
+    virtual void setUniform1iv(GLint location, GLsizei count, const GLint *v) = 0;
+    virtual void setUniform2iv(GLint location, GLsizei count, const GLint *v) = 0;
+    virtual void setUniform3iv(GLint location, GLsizei count, const GLint *v) = 0;
+    virtual void setUniform4iv(GLint location, GLsizei count, const GLint *v) = 0;
+    virtual void setUniform1uiv(GLint location, GLsizei count, const GLuint *v) = 0;
+    virtual void setUniform2uiv(GLint location, GLsizei count, const GLuint *v) = 0;
+    virtual void setUniform3uiv(GLint location, GLsizei count, const GLuint *v) = 0;
+    virtual void setUniform4uiv(GLint location, GLsizei count, const GLuint *v) = 0;
+    virtual void setUniformMatrix2fv(GLint location, GLsizei count, GLboolean transpose, const GLfloat *value) = 0;
+    virtual void setUniformMatrix3fv(GLint location, GLsizei count, GLboolean transpose, const GLfloat *value) = 0;
+    virtual void setUniformMatrix4fv(GLint location, GLsizei count, GLboolean transpose, const GLfloat *value) = 0;
+    virtual void setUniformMatrix2x3fv(GLint location, GLsizei count, GLboolean transpose, const GLfloat *value) = 0;
+    virtual void setUniformMatrix3x2fv(GLint location, GLsizei count, GLboolean transpose, const GLfloat *value) = 0;
+    virtual void setUniformMatrix2x4fv(GLint location, GLsizei count, GLboolean transpose, const GLfloat *value) = 0;
+    virtual void setUniformMatrix4x2fv(GLint location, GLsizei count, GLboolean transpose, const GLfloat *value) = 0;
+    virtual void setUniformMatrix3x4fv(GLint location, GLsizei count, GLboolean transpose, const GLfloat *value) = 0;
+    virtual void setUniformMatrix4x3fv(GLint location, GLsizei count, GLboolean transpose, const GLfloat *value) = 0;
+
+    virtual void getUniformfv(GLint location, GLfloat *params) = 0;
+    virtual void getUniformiv(GLint location, GLint *params) = 0;
+    virtual void getUniformuiv(GLint location, GLuint *params) = 0;
 
     virtual void reset() = 0;
 };
