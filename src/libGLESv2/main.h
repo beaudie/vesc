a3bc5ef8929a9d22dbcacba81fa3cabeff932fa9
Geoff Lang <geofflang@chromium.org>
2014-10-03 15:51:38 -0400

    Remove the gl::error function.
    BUG=angle:520
    
    Change-Id: Iefd4345aa347041143d5452cc2fcf48c071d494b

--- src/libGLESv2/main.h
+++ src/libGLESv2/main.h
@@ -36,16 +36,6 @@ Context *getContext();
 Context *getNonLostContext();
 egl::Display *getDisplay();
 
-void error(GLenum errorCode);
-
-template<class T>
-const T &error(GLenum errorCode, const T &returnValue)
-{
-    error(errorCode);
-
-    return returnValue;
-}
-
 }
 
 namespace rx
