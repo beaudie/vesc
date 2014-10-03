0505d4191a96814ea1762d8ccc1939155191249d
Jamie Madill <jmadill@chromium.org>
2014-10-03 15:12:50 -0400

    Use JSON as startup parameters to BufferSubData test.
    BUG=angle:744
    
    Change-Id: I60287f3c612011f505f1bc20632efc4dd17001d3

--- tests/perf_tests/BufferSubData.cpp
+++ tests/perf_tests/BufferSubData.cpp
@@ -148,7 +148,7 @@ std::string BufferSubDataParams::suffix() const
     }
 
     strstr << vertexComponentCount;
-    strstr << "_every" << updatesEveryNFrames;
+    strstr << "_every" << updateRate;
 
     return strstr.str();
 }
@@ -281,7 +281,7 @@ void BufferSubDataBenchmark::drawBenchmark()
 {
     for (unsigned int it = 0; it < mParams.iterations; it++)
     {
-        if (mParams.updateSize > 0 && ((mNumFrames % mParams.updatesEveryNFrames) == 0))
+        if (mParams.updateSize > 0 && ((mNumFrames % mParams.updateRate) == 0))
         {
             glBufferSubData(GL_ARRAY_BUFFER, 0, mParams.updateSize, mUpdateData);
         }
