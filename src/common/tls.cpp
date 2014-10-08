faac2f8137c5b3ebe46e85aec930ca984a1391a0
Cooper Partin <coopp@microsoft.com>
2014-10-08 10:41:56 -0700

    Added IInspectable EGLNativeWindowType and ICoreWindow support
    Change-Id: I9ad82b7819bcca1c05e7aa60dc2baec4a7bc403c

--- src/common/tls.cpp
+++ src/common/tls.cpp
@@ -10,6 +10,21 @@
 
 #include <assert.h>
 
+#ifdef ANGLE_ENABLE_WINDOWS_STORE
+#include <vector>
+#include <set>
+#include <map>
+#include <mutex>
+
+#include <wrl/client.h>
+#include <wrl/async.h>
+#include <Windows.System.Threading.h>
+
+using namespace std;
+using namespace Windows::Foundation;
+using namespace ABI::Windows::System::Threading;
+#endif
+
 TLSIndex CreateTLSIndex()
 {
     TLSIndex index;
@@ -72,3 +87,164 @@ void *GetTLSValue(TLSIndex index)
     return pthread_getspecific(index);
 #endif
 }
+
+#ifdef ANGLE_ENABLE_WINDOWS_STORE
+// Thread local storage.
+typedef vector<void*> ThreadLocalData;
+
+static __declspec(thread) ThreadLocalData* currentThreadData = nullptr;
+static set<ThreadLocalData*> allThreadData;
+static DWORD nextTlsIndex = 0;
+static vector<DWORD> freeTlsIndices;
+static mutex tlsAllocationLock;
+
+void Sleep(unsigned long dwMilliseconds)
+{
+    static HANDLE singletonEvent = nullptr;
+
+    HANDLE sleepEvent = singletonEvent;
+
+    // Demand create the event.
+    if (!sleepEvent)
+    {
+        sleepEvent = CreateEventEx(nullptr, nullptr, CREATE_EVENT_MANUAL_RESET, EVENT_ALL_ACCESS);
+
+        if (!sleepEvent)
+            return;
+
+        HANDLE previousEvent = InterlockedCompareExchangePointerRelease(&singletonEvent, sleepEvent, nullptr);
+
+        if (previousEvent)
+        {
+            // Back out if multiple threads try to demand create at the same time.
+            CloseHandle(sleepEvent);
+            sleepEvent = previousEvent;
+        }
+    }
+
+    // Emulate sleep by waiting with timeout on an event that is never signalled.
+    WaitForSingleObjectEx(sleepEvent, dwMilliseconds, false);
+}
+
+unsigned long TlsAlloc()
+{
+    // Can we reuse a previously freed TLS slot?
+    if (!freeTlsIndices.empty())
+    {
+        DWORD result = freeTlsIndices.back();
+        freeTlsIndices.pop_back();
+        return result;
+    }
+
+    // Allocate a new TLS slot.
+    return nextTlsIndex++;
+}
+
+int TlsFree(unsigned long dwTlsIndex)
+{
+    assert(dwTlsIndex < nextTlsIndex);
+    assert(find(freeTlsIndices.begin(), freeTlsIndices.end(), dwTlsIndex) == freeTlsIndices.end());
+
+    // Store this slot for reuse by TlsAlloc.
+    try
+    {
+        freeTlsIndices.push_back(dwTlsIndex);
+    }
+    catch (...)
+    {
+        return false;
+    }
+
+    // Zero the value for all threads that might be using this now freed slot.
+    for each (auto threadData in allThreadData)
+    {
+        if (threadData->size() > dwTlsIndex)
+        {
+            threadData->at(dwTlsIndex) = nullptr;
+        }
+    }
+
+    return true;
+}
+
+void* TlsGetValue(unsigned long dwTlsIndex)
+{
+    ThreadLocalData* threadData = currentThreadData;
+
+    if (threadData && threadData->size() > dwTlsIndex)
+    {
+        // Return the value of an allocated TLS slot.
+        return threadData->at(dwTlsIndex);
+    }
+    else
+    {
+        // Default value for unallocated slots.
+        return nullptr;
+    }
+}
+
+int TlsSetValue(unsigned long dwTlsIndex, void* lpTlsValue)
+{
+    ThreadLocalData* threadData = currentThreadData;
+
+    if (!threadData)
+    {
+        // First time allocation of TLS data for this thread.
+        try
+        {
+            threadData = new ThreadLocalData(dwTlsIndex + 1, nullptr);
+
+            //lock_guard<mutex> lock(tlsAllocationLock);
+
+            allThreadData.insert(threadData);
+
+            currentThreadData = threadData;
+        }
+        catch (...)
+        {
+            if (threadData)
+                delete threadData;
+
+            return false;
+        }
+    }
+    else if (threadData->size() <= dwTlsIndex)
+    {
+        // This thread already has a TLS data block, but it must be expanded to fit the specified slot.
+        try
+        {
+            //lock_guard<mutex> lock(tlsAllocationLock);
+
+            threadData->resize(dwTlsIndex + 1, nullptr);
+        }
+        catch (...)
+        {
+            return false;
+        }
+    }
+
+    // Store the new value for this slot.
+    threadData->at(dwTlsIndex) = lpTlsValue;
+
+    return true;
+}
+
+// Called at thread exit to clean up TLS allocations.
+void TlsShutdown()
+{
+    ThreadLocalData* threadData = currentThreadData;
+
+    if (threadData)
+    {
+        {
+            lock_guard<mutex> lock(tlsAllocationLock);
+
+            allThreadData.erase(threadData);
+        }
+
+        currentThreadData = nullptr;
+
+        delete threadData;
+    }
+}
+#endif
--- src/common/tls.h
+++ src/common/tls.h
@@ -12,6 +12,24 @@
 #include "common/platform.h"
 
 #ifdef ANGLE_PLATFORM_WINDOWS
+
+// TLS does not exist for Windows Store and needs to be emulated
+#   ifdef ANGLE_ENABLE_WINDOWS_STORE
+#       define TLS_OUT_OF_INDEXES -1
+#       ifndef CREATE_SUSPENDED
+#           define CREATE_SUSPENDED 0x00000004
+#       endif
+
+        void Sleep(_In_ unsigned long dwMilliseconds);
+        unsigned long TlsAlloc();
+        int TlsFree(_In_ unsigned long dwTlsIndex);
+        void* TlsGetValue(_In_ unsigned long dwTlsIndex);
+        int TlsSetValue(_In_ unsigned long dwTlsIndex, _In_opt_ void* lpTlsValue);
+        void TlsShutdown();
+        inline void * LocalAlloc(unsigned int uFlags, size_t size) { return malloc(size); };
+        inline void LocalFree(void* index) { free((void*)index); };
+
+#   endif
     typedef DWORD TLSIndex;
 #   define TLS_INVALID_INDEX (TLS_OUT_OF_INDEXES)
 #elif defined(ANGLE_PLATFORM_POSIX)
