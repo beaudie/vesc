3cbfafcd69d4c6b7c01613dc6a91a131ae13c3d4
Nicolas Capens <capn@chromium.org>
2014-10-10 10:11:54 -0400

    Use attributes to avoid unrolling and flatten conditionals.
    BUG=395048
    
    Change-Id: I91ea8ac3a822ab1f1ac24907d42326e3c6c9d9af

--- src/compiler/translator/OutputHLSL.cpp
+++ src/compiler/translator/OutputHLSL.cpp
@@ -2292,7 +2292,7 @@ bool OutputHLSL::visitSelection(Visit visit, TIntermSelection *node)
     {
         mUnfoldShortCircuit->traverse(node->getCondition());
 
-        out << "if (";
+        out << "[flatten] if (";
 
         node->getCondition()->traverse(this);
 
@@ -2372,14 +2372,14 @@ bool OutputHLSL::visitLoop(Visit visit, TIntermLoop *node)
 
     if (node->getType() == ELoopDoWhile)
     {
-        out << "{do\n";
+        out << "{[fastopt] do\n";
 
         outputLineDirective(node->getLine().first_line);
         out << "{\n";
     }
     else
     {
-        out << "{for(";
+        out << "{[fastopt] for(";
 
         if (node->getInit())
         {
@@ -2686,7 +2686,7 @@ bool OutputHLSL::handleExcessiveLoop(TIntermLoop *node)
 
                 // for(int index = initial; index < clampedLimit; index += increment)
 
-                out << "for(";
+                out << "[fastopt] for(";
                 index->traverse(this);
                 out << " = ";
                 out << initial;
