07ab58e5a6b445d536b692cd47724865d7fa4b46
Geoff Lang <geofflang@chromium.org>
2014-11-04 17:05:00 -0500

    Split the RenderTarget classes based on if they are backing a SwapChain.
    bug=angle:824
    
    Change-Id: I89e475d4065102dbaa7fa1f1bfd02c7207def75c

--- src/libGLESv2/renderer/d3d/d3d9/RenderTarget9.h
+++ src/libGLESv2/renderer/d3d/d3d9/RenderTarget9.h
@@ -16,25 +16,79 @@ namespace rx
 {
 class Renderer;
 class Renderer9;
+class SwapChain9;
 
 class RenderTarget9 : public RenderTarget
 {
   public:
-    RenderTarget9(IDirect3DSurface9 *surface, GLenum internalFormat, GLsizei width, GLsizei height, GLsizei depth,
-                  GLsizei samples);
+    RenderTarget9();
     virtual ~RenderTarget9();
 
     static RenderTarget9 *makeRenderTarget9(RenderTarget *renderTarget);
 
-    virtual void invalidate(GLint x, GLint y, GLsizei width, GLsizei height);
-
-    IDirect3DSurface9 *getSurface();
+    virtual IDirect3DSurface9 *getSurface() = 0;
 
   private:
     DISALLOW_COPY_AND_ASSIGN(RenderTarget9);
+};
+
+class SurfaceRenderTarget9 : public RenderTarget9
+{
+  public:
+    SurfaceRenderTarget9(IDirect3DSurface9 *surface, GLenum internalFormat, GLsizei width, GLsizei height, GLsizei depth,
+                         GLsizei samples);
+
+    virtual ~SurfaceRenderTarget9();
+
+    virtual GLsizei getWidth() const { return mWidth; }
+    virtual GLsizei getHeight() const { return mHeight; }
+    virtual GLsizei getDepth() const { return mDepth; }
+    virtual GLenum getInternalFormat() const { return mInternalFormat; }
+    virtual GLenum getActualFormat() const { return mActualFormat; }
+    virtual GLsizei getSamples() const { return mSamples; }
+
+    virtual void invalidate(GLint x, GLint y, GLsizei width, GLsizei height);
+
+    virtual IDirect3DSurface9 *getSurface();
+
+  private:
+    DISALLOW_COPY_AND_ASSIGN(SurfaceRenderTarget9);
+
+    GLsizei mWidth;
+    GLsizei mHeight;
+    GLsizei mDepth;
+    GLenum mInternalFormat;
+    GLenum mActualFormat;
+    GLsizei mSamples;
 
     IDirect3DSurface9 *mRenderTarget;
 };
+
+class SwapChainRenderTarget9 : public RenderTarget9
+{
+  public:
+    SwapChainRenderTarget9(SwapChain9 *swapChain, bool depth);
+
+    virtual ~SwapChainRenderTarget9();
+
+    virtual GLsizei getWidth() const;
+    virtual GLsizei getHeight() const;
+    virtual GLsizei getDepth() const;
+    virtual GLenum getInternalFormat() const;
+    virtual GLenum getActualFormat() const;
+    virtual GLsizei getSamples() const;
+
+    virtual void invalidate(GLint x, GLint y, GLsizei width, GLsizei height);
+
+    virtual IDirect3DSurface9 *getSurface();
+
+  private:
+    DISALLOW_COPY_AND_ASSIGN(SwapChainRenderTarget9);
+
+    SwapChain9 *mSwapChain;
+    bool mDepth;
+};
+
 
 }
 
