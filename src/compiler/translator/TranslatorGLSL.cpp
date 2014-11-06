a72ef49809a15a22dcb2bef113f6224fdfb57b60
Olli Etuaho <oetuaho@nvidia.com>
2014-11-06 17:25:48 +0200

    Add mediump and lowp precision emulation support for GLSL output
    This implements the rounding as specified in WEBGL_debug_shader_precision
    extension proposal for desktop GLSL output. Most of the changes are done
    to OutputGLSLBase, which inserts calls to the rounding routines webgl_frm
    and webgl_frl in the appropriate places, and TranslatorGLSL, which writes
    the rounding routines themselves to the shader.
    
    Compound assignments which are subject to emulation are transformed from
    "x op= y" to "webgl_compound_op_frm(x, y)". This is done by the new class
    EmulateCompoundAssignment.
    
    Comprehensive tests for the added code generation are included.
    
    BUG=angle:787
    
    Change-Id: I0d0ad9327888f803a32e79b64b08763c654c913b

--- src/compiler/translator/TranslatorGLSL.cpp
+++ src/compiler/translator/TranslatorGLSL.cpp
@@ -6,8 +6,135 @@
 
 #include "compiler/translator/TranslatorGLSL.h"
 
+#include "compiler/translator/EmulateCompoundAssignment.h"
 #include "compiler/translator/OutputGLSL.h"
 #include "compiler/translator/VersionGLSL.h"
+
+
+namespace {
+
+static void writeVectorPrecisionEmulationHelpers(TInfoSinkBase& sink, unsigned int size) {
+    sink <<
+    "vec" << size << " webgl_frm(in vec" << size << " v) {\n"
+    "    v = clamp(v, -65504.0, 65504.0);\n"
+    "    vec" << size << " exponent = floor(log2(abs(v) + 1e-30)) - 10.0;\n"
+    "    bvec" << size << " isNonZero = greaterThanEqual(exponent, vec" << size << "(-25.0));\n"
+    "    v = v * exp2(-exponent);\n"
+    "    v = sign(v) * floor(abs(v));\n"
+    "    return v * exp2(exponent) * vec" << size << "(isNonZero);\n"
+    "}\n"
+
+    "vec" << size << " webgl_frl(in vec" << size << " v) {\n"
+    "    v = clamp(v, -2.0, 2.0);\n"
+    "    v = v * 256.0;\n"
+    "    v = sign(v) * floor(abs(v));\n"
+    "    return v * 0.00390625;\n"
+    "}\n";
+}
+
+static void writeMatrixPrecisionEmulationHelpers(TInfoSinkBase& sink, const char *functionName) {
+    sink <<
+    "mat2 " << functionName << "(in mat2 m) {\n"
+    "    mat2 rounded;\n"
+    "    rounded[0] = " << functionName << "(m[0]);\n"
+    "    rounded[1] = " << functionName << "(m[1]);\n"
+    "    return rounded;\n"
+    "}\n"
+
+    "mat3 " << functionName << "(in mat3 m) {\n"
+    "    mat3 rounded;\n"
+    "    rounded[0] = " << functionName << "(m[0]);\n"
+    "    rounded[1] = " << functionName << "(m[1]);\n"
+    "    rounded[2] = " << functionName << "(m[2]);\n"
+    "    return rounded;\n"
+    "}\n"
+
+    "mat4 " << functionName << "(in mat4 m) {\n"
+    "    mat4 rounded;\n"
+    "    rounded[0] = " << functionName << "(m[0]);\n"
+    "    rounded[1] = " << functionName << "(m[1]);\n"
+    "    rounded[2] = " << functionName << "(m[2]);\n"
+    "    rounded[3] = " << functionName << "(m[3]);\n"
+    "    return rounded;\n"
+    "}\n";
+}
+
+static void writePrecisionEmulationHelpers(TInfoSinkBase& sink) {
+    // Write the webgl_frm functions that round floating point numbers to
+    // half precision, and webgl_frl functions that round them to minimum lowp
+    // precision.
+
+    // Unoptimized version of webgl_frm for single floats:
+    //
+    // int webgl_maxNormalExponent(in int exponentBits) {
+    //     int possibleExponents = int(exp2(float(exponentBits)));
+    //     int exponentBias = possibleExponents / 2 - 1;
+    //     int allExponentBitsOne = possibleExponents - 1;
+    //     return (allExponentBitsOne - 1) - exponentBias;
+    // }
+    //
+    // float webgl_frm(in float x) {
+    //     int mantissaBits = 10;
+    //     int exponentBits = 5;
+    //     float possibleMantissas = exp2(float(mantissaBits));
+    //     float mantissaMax = 2.0 - 1.0 / possibleMantissas;
+    //     int maxNE = webgl_maxNormalExponent(exponentBits);
+    //     float max = exp2(float(maxNE)) * mantissaMax;
+    //     if (x > max) {
+    //         return max;
+    //     }
+    //     if (x < -max) {
+    //         return -max;
+    //     }
+    //     float exponent = floor(log2(abs(x)));
+    //     if (abs(x) == 0.0 || exponent < -float(maxNE)) {
+    //         return 0.0 * sign(x)
+    //     }
+    //     x = x * exp2(-(exponent - float(mantissaBits)));
+    //     x = sign(x) * floor(abs(x));
+    //     return x * exp2(exponent - float(mantissaBits));
+    // }
+
+    // All numbers with a magnitude less than 2^-15 are subnormal, and are
+    // flushed to zero.
+
+    // Note the constant numbers below:
+    // a) 65504 is the maximum possible mantissa (1.1111111111 in binary) times
+    //    2^15, the maximum normal exponent.
+    // b) 10.0 is the number of mantissa bits.
+    // c) -25.0 is the minimum normal half-float exponent -15.0 minus the number
+    //    of mantissa bits.
+    // d) + 1e-30 is to make sure the argument of log2() won't be zero. It can
+    //    only affect the result of log2 on x where abs(x) < 1e-22. Since these
+    //    numbers will be flushed to zero either way (2^-15 is the smallest
+    //    normal positive number), this does not introduce any error.
+
+    sink <<
+    "float webgl_frm(in float x) {\n"
+    "    x = clamp(x, -65504.0, 65504.0);\n"
+    "    float exponent = floor(log2(abs(x) + 1e-30)) - 10.0;\n"
+    "    bool isNonZero = (exponent >= -25.0);\n"
+    "    x = x * exp2(-exponent);\n"
+    "    x = sign(x) * floor(abs(x));\n"
+    "    return x * exp2(exponent) * float(isNonZero);\n"
+    "}\n"
+
+    "float webgl_frl(in float x) {\n"
+    "    x = clamp(x, -2.0, 2.0);\n"
+    "    x = x * 256.0;\n"
+    "    x = sign(x) * floor(abs(x));\n"
+    "    return x * 0.00390625;\n"
+    "}\n";
+
+    writeVectorPrecisionEmulationHelpers(sink, 2);
+    writeVectorPrecisionEmulationHelpers(sink, 3);
+    writeVectorPrecisionEmulationHelpers(sink, 4);
+    writeMatrixPrecisionEmulationHelpers(sink, "webgl_frm");
+    writeMatrixPrecisionEmulationHelpers(sink, "webgl_frl");
+}
+
+} // namespace anonymous
+
 
 TranslatorGLSL::TranslatorGLSL(sh::GLenum type, ShShaderSpec spec)
     : TCompiler(type, spec, SH_GLSL_OUTPUT) {
@@ -24,6 +151,16 @@ void TranslatorGLSL::translate(TIntermNode* root) {
     // Write extension behaviour as needed
     writeExtensionBehavior();
 
+    bool precisionEmulation = getResources().WEBGL_debug_shader_precision && getPragma().debugShaderPrecision;
+
+    if (precisionEmulation)
+    {
+        writePrecisionEmulationHelpers(sink);
+        EmulateCompoundAssignment emulateCompoundAssignment;
+        root->traverse(&emulateCompoundAssignment);
+        emulateCompoundAssignment.writeEmulation(sink);
+    }
+
     // Write emulated built-in functions if needed.
     getBuiltInFunctionEmulator().OutputEmulatedFunctionDefinition(
         sink, false);
@@ -32,7 +169,7 @@ void TranslatorGLSL::translate(TIntermNode* root) {
     getArrayBoundsClamper().OutputClampingFunctionDefinition(sink);
 
     // Write translated shader.
-    TOutputGLSL outputGLSL(sink, getArrayIndexClampingStrategy(), getHashFunction(), getNameMap(), getSymbolTable(), getShaderVersion());
+    TOutputGLSL outputGLSL(sink, getArrayIndexClampingStrategy(), getHashFunction(), getNameMap(), getSymbolTable(), getShaderVersion(), precisionEmulation);
     root->traverse(&outputGLSL);
 }
 
