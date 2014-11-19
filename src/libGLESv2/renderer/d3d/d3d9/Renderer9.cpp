3324600ef06cf3df98802def839c512b5d7d5aac
Geoff Lang <geofflang@chromium.org>
2014-11-19 11:48:40 -0500

    Remove includes of libGLESv2/main.h.
    It was mostly being included for gl::error or the debug.h include.
    
    BUG=angle:733
    
    Change-Id: If40d511c5422ac9265fbd809f647a91bcc802d41

--- src/libGLESv2/renderer/d3d/d3d9/Renderer9.cpp
+++ src/libGLESv2/renderer/d3d/d3d9/Renderer9.cpp
@@ -27,7 +27,6 @@
 #include "libGLESv2/renderer/d3d/TextureD3D.h"
 #include "libGLESv2/renderer/d3d/TransformFeedbackD3D.h"
 #include "libGLESv2/renderer/d3d/RenderbufferD3D.h"
-#include "libGLESv2/main.h"
 #include "libGLESv2/Buffer.h"
 #include "libGLESv2/Texture.h"
 #include "libGLESv2/Framebuffer.h"
