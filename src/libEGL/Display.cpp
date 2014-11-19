385d945b56d7cc9fba84d41f6a45b8598f5bd2cc
Geoff Lang <geofflang@chromium.org>
2014-11-19 12:38:25 -0500

    Remove includes of libEGL/main.h.
    It was mostly being included for querying the current state and EGL includes.
    
    BUG=angle:733
    
    Change-Id: Ia985dbc9de5739f6ea7fa5bb6eb2e187cc1d60f3

--- src/libEGL/Display.cpp
+++ src/libEGL/Display.cpp
@@ -22,8 +22,10 @@
 #include "libGLESv2/Context.h"
 #include "libGLESv2/renderer/SwapChain.h"
 
-#include "libEGL/main.h"
 #include "libEGL/Surface.h"
+
+#include <EGL/egl.h>
+#include <EGL/eglext.h>
 
 namespace egl
 {
