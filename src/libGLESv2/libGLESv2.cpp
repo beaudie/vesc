c1f58168de7071fbebc4356b55b6b7ba230237b5
Geoff Lang <geofflang@chromium.org>
2014-12-01 12:48:41 -0500

    Move ReadPixels to the Framebuffer object and Impl.
    BUG=angle:841
    
    Change-Id: I71deac9e755b5dfa010596cd1f8a213c24d895bf

--- src/libGLESv2/libGLESv2.cpp
+++ src/libGLESv2/libGLESv2.cpp
@@ -3749,7 +3749,12 @@ void GL_APIENTRY glReadnPixelsEXT(GLint x, GLint y, GLsizei width, GLsizei heigh
             return;
         }
 
-        gl::Error error = context->readPixels(x, y, width, height, format, type, &bufSize, data);
+        gl::Framebuffer *framebufferObject = context->getState().getReadFramebuffer();
+        ASSERT(framebufferObject);
+
+        gl::Rectangle area(x, y, width, height);
+
+        gl::Error error = framebufferObject->readPixels(context->getState(), area, format, type, data);
         if (error.isError())
         {
             context->recordError(error);
@@ -3780,7 +3785,12 @@ void GL_APIENTRY glReadPixels(GLint x, GLint y, GLsizei width, GLsizei height,
             return;
         }
 
-        gl::Error error = context->readPixels(x, y, width, height, format, type, NULL, pixels);
+        gl::Framebuffer *framebufferObject = context->getState().getReadFramebuffer();
+        ASSERT(framebufferObject);
+
+        gl::Rectangle area(x, y, width, height);
+
+        gl::Error error = framebufferObject->readPixels(context->getState(), area, format, type, pixels);
         if (error.isError())
         {
             context->recordError(error);
