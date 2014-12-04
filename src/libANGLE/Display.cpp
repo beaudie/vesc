c5449e3977a700455ef71b54986fa2ef567ddbc7
Jamie Madill <jmadill@chromium.org>
2014-12-04 11:31:47 -0500

    Move native display check to the DisplayImpl.
    This fixes another Linux compile error in the EGL layer.
    
    BUG=angle:773
    
    Change-Id: Iba643a72fb7b0d5994fe69e46184256e07aa3aad

--- src/libANGLE/Display.cpp
+++ src/libANGLE/Display.cpp
@@ -135,6 +135,15 @@ Display *Display::getDisplay(EGLNativeDisplayType displayId, const AttributeMap 
     else
     {
         display = new Display(displayId);
+
+        // Validate the native display
+        if (!display->isValidNativeDisplay(displayId))
+        {
+            // Still returns success
+            SafeDelete(display);
+            return NULL;
+        }
+
         displays->insert(std::make_pair(displayId, display));
     }
 
@@ -676,6 +685,16 @@ const char *Display::getExtensionString(egl::Display *display)
 bool Display::isValidNativeWindow(EGLNativeWindowType window) const
 {
     return mImplementation->isValidNativeWindow(window);
+}
+
+bool Display::isValidNativeDisplay(EGLNativeDisplayType display) const
+{
+    // TODO(jmadill): handle this properly
+#if defined(ANGLE_PLATFORM_WINDOWS) && !defined(ANGLE_ENABLE_WINDOWS_STORE)
+    return (WindowFromDC(display) != NULL);
+#else
+    return true;
+#endif
 }
 
 bool Display::supportsPlatformD3D()
