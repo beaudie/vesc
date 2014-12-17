9fd46a3724ad4bfac022d6c81e68653a94f43d6f
Geoff Lang <geofflang@chromium.org>
2014-12-17 15:28:23 -0500

    Remove all uses of "actual" formats.
    Change-Id: I7cd2d1a56772fdf18bcf926456399322d13e7a4f

--- src/libANGLE/validationES.cpp
+++ src/libANGLE/validationES.cpp
@@ -524,14 +524,14 @@ bool ValidateBlitFramebufferParameters(gl::Context *context, GLint srcX0, GLint 
 
         if (readColorBuffer && drawColorBuffer)
         {
-            GLenum readInternalFormat = readColorBuffer->getActualFormat();
+            GLenum readInternalFormat = readColorBuffer->getInternalFormat();
             const InternalFormat &readFormatInfo = GetInternalFormatInfo(readInternalFormat);
 
             for (unsigned int i = 0; i < gl::IMPLEMENTATION_MAX_DRAW_BUFFERS; i++)
             {
                 if (drawFramebuffer->isEnabledColorAttachment(i))
                 {
-                    GLenum drawInternalFormat = drawFramebuffer->getColorbuffer(i)->getActualFormat();
+                    GLenum drawInternalFormat = drawFramebuffer->getColorbuffer(i)->getInternalFormat();
                     const InternalFormat &drawFormatInfo = GetInternalFormatInfo(drawInternalFormat);
 
                     // The GL ES 3.0.2 spec (pg 193) states that:
@@ -631,7 +631,7 @@ bool ValidateBlitFramebufferParameters(gl::Context *context, GLint srcX0, GLint 
 
             if (readBuffer && drawBuffer)
             {
-                if (readBuffer->getActualFormat() != drawBuffer->getActualFormat())
+                if (readBuffer->getInternalFormat() != drawBuffer->getInternalFormat())
                 {
                     context->recordError(Error(GL_INVALID_OPERATION));
                     return false;
@@ -915,7 +915,7 @@ bool ValidateReadPixelsParameters(gl::Context *context, GLint x, GLint y, GLsize
 
     GLenum currentFormat = framebuffer->getImplementationColorReadFormat();
     GLenum currentType = framebuffer->getImplementationColorReadType();
-    GLenum currentInternalFormat = readBuffer->getActualFormat();
+    GLenum currentInternalFormat = readBuffer->getInternalFormat();
     GLuint clientVersion = context->getClientVersion();
 
     bool validReadFormat = (clientVersion < 3) ? ValidES2ReadFormatType(context, format, type) :
