60d524e7bcd7af5bdd6a0e502e5487d0dcc71f1c
Geoff Lang <geofflang@chromium.org>
2014-12-19 17:03:25 -0500

    Merge all gl::Texture* classes.
    Change-Id: I11d94c1432f8fc70a1edaaf8228bbc43c3c8fff3

--- src/libANGLE/validationES.cpp
+++ src/libANGLE/validationES.cpp
@@ -1262,7 +1262,7 @@ bool ValidateCopyTexImageParametersBase(gl::Context* context, GLenum target, GLi
         return false;
     }
 
-    gl::Texture *texture = context->getTargetTexture(IsCubemapTextureTarget(target) ? GL_TEXTURE_CUBE_MAP : target);
+    gl::Texture *texture = context->getTargetTexture(IsCubeMapTextureTarget(target) ? GL_TEXTURE_CUBE_MAP : target);
     if (!texture)
     {
         context->recordError(Error(GL_INVALID_OPERATION));
@@ -1301,7 +1301,7 @@ bool ValidateCopyTexImageParametersBase(gl::Context* context, GLenum target, GLi
     }
     else
     {
-        if (IsCubemapTextureTarget(target) && width != height)
+        if (IsCubeMapTextureTarget(target) && width != height)
         {
             context->recordError(Error(GL_INVALID_VALUE));
             return false;
