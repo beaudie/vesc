52a4a1a3ec7ed2ea1c4a3d9f9d90df62f1e01f80
Jamie Madill <jmadill@chromium.org>
2015-01-16 10:58:24 -0500

    Allow 32-bit float formats in ES2 CopyTexSubImage.
    These corner case formats can come about from using floating
    point textures and trying to copy between them. There currently
    isn't a good alternative in WebGL 1 for copies.
    
    BUG=angle:850
    
    Change-Id: I3dc29e42b5ec7dcf071185bc09c6b3e9e3cb3207

--- src/libANGLE/validationES2.cpp
+++ src/libANGLE/validationES2.cpp
@@ -448,7 +448,11 @@ bool ValidateES2CopyTexImageParameters(Context* context, GLenum target, GLint le
                   colorbufferFormat != GL_RGB8_OES &&
                   colorbufferFormat != GL_RGBA4 &&
                   colorbufferFormat != GL_RGB5_A1 &&
-                  colorbufferFormat != GL_RGBA8_OES)
+                  colorbufferFormat != GL_RGBA8_OES &&
+                  colorbufferFormat != GL_R32F &&
+                  colorbufferFormat != GL_RG32F &&
+                  colorbufferFormat != GL_RGB32F &&
+                  colorbufferFormat != GL_RGBA32F)
               {
                   context->recordError(Error(GL_INVALID_OPERATION));
                   return false;
@@ -460,7 +464,10 @@ bool ValidateES2CopyTexImageParameters(Context* context, GLenum target, GLint le
                   colorbufferFormat != GL_RGB8_OES &&
                   colorbufferFormat != GL_RGBA4 &&
                   colorbufferFormat != GL_RGB5_A1 &&
-                  colorbufferFormat != GL_RGBA8_OES)
+                  colorbufferFormat != GL_RGBA8_OES &&
+                  colorbufferFormat != GL_RG32F &&
+                  colorbufferFormat != GL_RGB32F &&
+                  colorbufferFormat != GL_RGBA32F)
               {
                   context->recordError(Error(GL_INVALID_OPERATION));
                   return false;
@@ -471,7 +478,9 @@ bool ValidateES2CopyTexImageParameters(Context* context, GLenum target, GLint le
                 colorbufferFormat != GL_RGB8_OES &&
                 colorbufferFormat != GL_RGBA4 &&
                 colorbufferFormat != GL_RGB5_A1 &&
-                colorbufferFormat != GL_RGBA8_OES)
+                colorbufferFormat != GL_RGBA8_OES &&
+                colorbufferFormat != GL_RGB32F &&
+                colorbufferFormat != GL_RGBA32F)
             {
                 context->recordError(Error(GL_INVALID_OPERATION));
                 return false;
@@ -481,7 +490,8 @@ bool ValidateES2CopyTexImageParameters(Context* context, GLenum target, GLint le
           case GL_RGBA:
             if (colorbufferFormat != GL_RGBA4 &&
                 colorbufferFormat != GL_RGB5_A1 &&
-                colorbufferFormat != GL_RGBA8_OES)
+                colorbufferFormat != GL_RGBA8_OES &&
+                colorbufferFormat != GL_RGBA32F)
             {
                 context->recordError(Error(GL_INVALID_OPERATION));
                 return false;
