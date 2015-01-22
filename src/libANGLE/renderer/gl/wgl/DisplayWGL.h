52211dd19f797ccc1b874f603602e07538a500fc
Geoff Lang <geofflang@chromium.org>
2015-01-22 13:33:56 -0500

    Add a basic WGL implementation.
    Change-Id: I5202086990b4f4fbf455fd73c3e29e5e3e1f6160

--- src/libANGLE/renderer/gl/wgl/DisplayWGL.h
+++ src/libANGLE/renderer/gl/wgl/DisplayWGL.h
@@ -10,6 +10,7 @@
 #define LIBANGLE_RENDERER_GL_WGL_DISPLAYWGL_H_
 
 #include "libANGLE/renderer/gl/DisplayGL.h"
+#include "libANGLE/renderer/gl/wgl/wglext.h"
 
 namespace rx
 {
@@ -47,6 +48,29 @@ class DisplayWGL : public DisplayGL
 
     void generateExtensions(egl::DisplayExtensions *outExtensions) const override;
     void generateCaps(egl::Caps *outCaps) const override;
+
+    bool isWGLExtensionSupported(HDC deviceContext, const std::string &name) const;
+
+    HMODULE mOpenGLModule;
+    GLuint mGLVersionMajor;
+    GLuint mGLVersionMinor;
+
+    PFNWGLGETEXTENSIONSSTRINGEXTPROC mGetExtensionStringEXT;
+    PFNWGLGETEXTENSIONSSTRINGARBPROC mGetExtensionStringARB;
+
+    PFNWGLCREATECONTEXTATTRIBSARBPROC mCreateContextAttribsARB;
+
+    PFNWGLGETPIXELFORMATATTRIBIVARBPROC mGetPixelFormatAttribivARB;
+
+    PFNWGLSWAPINTERVALEXTPROC mSwapIntervalEXT;
+
+    ATOM mWindowClass;
+    HWND mWindow;
+    HDC mDeviceContext;
+    int mPixelFormat;
+    HGLRC mWGLContext;
+
+    egl::Display *mDisplay;
 };
 
 }
