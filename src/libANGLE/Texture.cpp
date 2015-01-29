2215fa8b85cdd362625c425e8494073ce5cf7fa8
Geoff Lang <geofflang@chromium.org>
2015-01-29 13:25:14 +0000

    Update texture size information on egl[Bind|Release]TexImage.
    BUG=450257
    
    Change-Id: I7609131d78f255bf9f79b42f39b02303a5c14b60

--- src/libANGLE/Texture.cpp
+++ src/libANGLE/Texture.cpp
@@ -10,6 +10,7 @@
 #include "libANGLE/Data.h"
 #include "libANGLE/formatutils.h"
 
+#include "libANGLE/Config.h"
 #include "libANGLE/Surface.h"
 
 #include "common/mathutil.h"
@@ -390,6 +391,15 @@ void Texture::setImageDesc(const ImageIndex &index, const ImageDesc &desc)
     mImageDescs[index] = desc;
 }
 
+void Texture::clearImageDesc(const ImageIdentifier& index)
+{
+    ImageDescMap::iterator iter = mImageDescs.find(index);
+    if (iter != mImageDescs.end())
+    {
+        mImageDescs.erase(iter);
+    }
+}
+
 void Texture::clearImageDescs()
 {
     mImageDescs.clear();
@@ -397,9 +407,18 @@ void Texture::clearImageDescs()
 
 void Texture::bindTexImage(egl::Surface *surface)
 {
+    ASSERT(surface);
+
     releaseTexImage();
     mTexture->bindTexImage(surface);
     mBoundSurface = surface;
+
+    // Set the image info to the size and format of the surface
+    ASSERT(mTarget == GL_TEXTURE_2D);
+
+    Extents size(surface->getWidth(), surface->getHeight(), 1);
+    ImageDesc desc(size, surface->getConfig()->mRenderTargetFormat);
+    setImageDesc(ImageIdentifier(mTarget, 0), desc);
 }
 
 void Texture::releaseTexImage()
@@ -408,6 +427,10 @@ void Texture::releaseTexImage()
     {
         mBoundSurface = NULL;
         mTexture->releaseTexImage();
+
+        // Erase the image info for level 0
+        ASSERT(mTarget == GL_TEXTURE_2D);
+        clearImageDesc(ImageIdentifier(mTarget, 0));
     }
 }
 
