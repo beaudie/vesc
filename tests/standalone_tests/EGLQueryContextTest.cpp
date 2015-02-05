7f35a8c1a919f904ff4107eb34dd8ae820aaf518
Régis Fénéon <regis.feneon@gmail.com>
2015-02-05 12:57:44 +0100

    Implement eglQueryContext
    Change-Id: I5e709f772d6222cdb7b5d7c060830a75eac9f815

---
+++ tests/standalone_tests/EGLQueryContextTest.cpp
@@ -0,0 +1,118 @@
+#include "gtest/gtest.h"
+
+#include <EGL/egl.h>
+
+#define CLIENT_VERSION 2
+
+class EGLQueryContextTest : public testing::Test
+{
+  public:
+    virtual void SetUp() {
+      mDisplay = eglGetDisplay(EGL_DEFAULT_DISPLAY);
+      EXPECT_TRUE(mDisplay != EGL_NO_DISPLAY);
+      EXPECT_TRUE(eglInitialize(mDisplay, NULL, NULL) != EGL_FALSE);
+
+      EGLint ncfg;
+      EGLint cfgattrs[] = {
+          EGL_RED_SIZE, 8,
+          EGL_GREEN_SIZE, 8,
+          EGL_BLUE_SIZE, 8,
+          EGL_RENDERABLE_TYPE, EGL_OPENGL_ES2_BIT,
+          EGL_SURFACE_TYPE, EGL_PBUFFER_BIT,
+          EGL_NONE };
+      EXPECT_TRUE(eglChooseConfig(mDisplay, cfgattrs, &mConfig, 1, &ncfg) != EGL_FALSE);
+      EXPECT_TRUE(ncfg == 1);
+
+      EGLint ctxattrs[] = {
+          EGL_CONTEXT_CLIENT_VERSION, CLIENT_VERSION,
+          EGL_NONE };
+      mContext = eglCreateContext(mDisplay, mConfig, NULL, ctxattrs);
+      EXPECT_TRUE(mContext != EGL_NO_CONTEXT);
+
+      EGLint surfattrs[] = {
+          EGL_WIDTH, 16,
+          EGL_HEIGHT, 16,
+          EGL_NONE };
+      mSurface = eglCreatePbufferSurface(mDisplay, mConfig, surfattrs);
+      EXPECT_TRUE(mSurface != EGL_NO_SURFACE);
+    }
+
+    virtual void TearDown() {
+        eglMakeCurrent(mDisplay, EGL_NO_SURFACE, EGL_NO_SURFACE, EGL_NO_CONTEXT);
+        eglDestroyContext(mDisplay, mContext);
+        eglDestroySurface(mDisplay, mSurface);
+        eglTerminate(mDisplay);
+    }
+
+    EGLDisplay mDisplay;
+    EGLConfig mConfig;
+    EGLContext mContext;
+    EGLSurface mSurface;
+};
+
+TEST_F(EGLQueryContextTest, GetConfigID)
+{
+    EGLint configId, contextConfigId;
+    EXPECT_TRUE(eglGetConfigAttrib(mDisplay, mConfig, EGL_CONFIG_ID, &configId) != EGL_FALSE);
+    EXPECT_TRUE(eglQueryContext(mDisplay, mContext, EGL_CONFIG_ID, &contextConfigId) != EGL_FALSE);
+    EXPECT_TRUE(configId == contextConfigId);
+}
+
+TEST_F(EGLQueryContextTest, GetClientType)
+{
+    EGLint clientType;
+    EXPECT_TRUE(eglQueryContext(mDisplay, mContext, EGL_CONTEXT_CLIENT_TYPE, &clientType) != EGL_FALSE);
+    EXPECT_TRUE(clientType == EGL_OPENGL_ES_API);
+}
+
+TEST_F(EGLQueryContextTest, GetClientVersion)
+{
+    EGLint clientVersion;
+    EXPECT_TRUE(eglQueryContext(mDisplay, mContext, EGL_CONTEXT_CLIENT_VERSION, &clientVersion) != EGL_FALSE);
+    EXPECT_TRUE(clientVersion == CLIENT_VERSION);
+}
+
+TEST_F(EGLQueryContextTest, GetRenderBufferNoSurface)
+{
+    EGLint renderBuffer;
+    EXPECT_TRUE(eglQueryContext(mDisplay, mContext, EGL_RENDER_BUFFER, &renderBuffer) != EGL_FALSE);
+    EXPECT_TRUE(renderBuffer == EGL_NONE);
+}
+
+TEST_F(EGLQueryContextTest, GetRenderBufferBoundSurface)
+{
+    EGLint renderBuffer, contextRenderBuffer;
+    EXPECT_TRUE(eglQuerySurface(mDisplay, mSurface, EGL_RENDER_BUFFER, &renderBuffer) != EGL_FALSE);
+    EXPECT_TRUE(eglMakeCurrent(mDisplay, mSurface, mSurface, mContext) != EGL_FALSE);
+    EXPECT_TRUE(eglQueryContext(mDisplay, mContext, EGL_RENDER_BUFFER, &contextRenderBuffer) != EGL_FALSE);
+    EXPECT_TRUE(renderBuffer == contextRenderBuffer);
+}
+
+TEST_F(EGLQueryContextTest, BadDisplay)
+{
+    EGLint val;
+    EXPECT_TRUE(eglQueryContext(EGL_NO_DISPLAY, mContext, EGL_CONTEXT_CLIENT_TYPE, &val) == EGL_FALSE);
+    EXPECT_TRUE(eglGetError() == EGL_BAD_DISPLAY);
+}
+
+TEST_F(EGLQueryContextTest, NotInitialized)
+{
+    EGLint val;
+    eglTerminate(mDisplay);
+    EXPECT_TRUE(eglQueryContext(mDisplay, mContext, EGL_CONTEXT_CLIENT_TYPE, &val) == EGL_FALSE);
+    EXPECT_TRUE(eglGetError() == EGL_NOT_INITIALIZED);
+}
+
+TEST_F(EGLQueryContextTest, BadContext)
+{
+    EGLint val;
+    EXPECT_TRUE(eglQueryContext(mDisplay, EGL_NO_CONTEXT, EGL_CONTEXT_CLIENT_TYPE, &val) == EGL_FALSE);
+    EXPECT_TRUE(eglGetError() == EGL_BAD_CONTEXT);
+}
+
+TEST_F(EGLQueryContextTest, BadAttribute)
+{
+    EGLint val;
+    EXPECT_TRUE(eglQueryContext(mDisplay, mContext, EGL_HEIGHT, &val) == EGL_FALSE);
+    EXPECT_TRUE(eglGetError() == EGL_BAD_ATTRIBUTE);
+}
