05c805f20b86c4174689b0a0133ff1a98a2c8794
Geoff Lang <geofflang@chromium.org>
2015-02-19 16:23:19 -0500

    Load all core WGL functions.
    BUG=angle:879
    
    Change-Id: If204156e69480cd7e1e7c43db8496dd150ac9e15

--- src/libANGLE/renderer/gl/wgl/FunctionsWGL.cpp
+++ src/libANGLE/renderer/gl/wgl/FunctionsWGL.cpp
@@ -46,11 +46,30 @@ static void GetWGLExtensionProcAddress(HMODULE glModule, PFNWGLGETPROCADDRESSPRO
 }
 
 FunctionsWGL::FunctionsWGL()
-    : createContext(nullptr),
+    : copyContext(nullptr),
+      createContext(nullptr),
+      createLayerContext(nullptr),
       deleteContext(nullptr),
+      getCurrentContext(nullptr),
+      getCurrentDC(nullptr),
+      getProcAddress(nullptr),
       makeCurrent(nullptr),
+      shareLists(nullptr),
+      useFontBitmapsA(nullptr),
+      useFontBitmapsW(nullptr),
+      swapBuffers(nullptr),
+      useFontOutlinesA(nullptr),
+      useFontOutlinesW(nullptr),
+      describeLayerPlane(nullptr),
+      setLayerPaletteEntries(nullptr),
+      getLayerPaletteEntries(nullptr),
+      realizeLayerPalette(nullptr),
+      swapLayerBuffers(nullptr),
+      swapMultipleBuffers(nullptr),
       createContextAttribsARB(nullptr),
       getPixelFormatAttribivARB(nullptr),
+      getExtensionStringEXT(nullptr),
+      getExtensionStringARB(nullptr),
       swapIntervalEXT(nullptr)
 {
 }
@@ -62,16 +81,29 @@ void FunctionsWGL::intialize(HMODULE glModule, HDC context)
     GetWGLProcAddress(glModule, nullptr, "wglGetProcAddress", &getProcAddressWGL);
 
     // Load the core wgl functions
-    GetWGLProcAddress(glModule, getProcAddressWGL, "wglCreateContext", &createContext);
-    GetWGLProcAddress(glModule, getProcAddressWGL, "wglDeleteContext", &deleteContext);
-    GetWGLProcAddress(glModule, getProcAddressWGL, "wglMakeCurrent", &makeCurrent);
+    GetWGLProcAddress(glModule, getProcAddress, "wglCopyContext", &copyContext);
+    GetWGLProcAddress(glModule, getProcAddress, "wglCreateContext", &createContext);
+    GetWGLProcAddress(glModule, getProcAddress, "wglCreateLayerContext", &createLayerContext);
+    GetWGLProcAddress(glModule, getProcAddress, "wglDeleteContext", &deleteContext);
+    GetWGLProcAddress(glModule, getProcAddress, "wglGetCurrentContext", &getCurrentContext);
+    GetWGLProcAddress(glModule, getProcAddress, "wglGetCurrentDC", &getCurrentDC);
+    GetWGLProcAddress(glModule, getProcAddress, "wglMakeCurrent", &makeCurrent);
+    GetWGLProcAddress(glModule, getProcAddress, "wglShareLists", &shareLists);
+    GetWGLProcAddress(glModule, getProcAddress, "wglUseFontBitmapsA", &useFontBitmapsA);
+    GetWGLProcAddress(glModule, getProcAddress, "wglUseFontBitmapsW", &useFontBitmapsW);
+    swapBuffers = SwapBuffers; // SwapBuffers is statically linked from GDI
+    GetWGLProcAddress(glModule, getProcAddress, "wglUseFontOutlinesA", &useFontOutlinesA);
+    GetWGLProcAddress(glModule, getProcAddress, "wglUseFontOutlinesW", &useFontOutlinesW);
+    GetWGLProcAddress(glModule, getProcAddress, "wglDescribeLayerPlane", &describeLayerPlane);
+    GetWGLProcAddress(glModule, getProcAddress, "wglSetLayerPaletteEntries", &setLayerPaletteEntries);
+    GetWGLProcAddress(glModule, getProcAddress, "wglGetLayerPaletteEntries", &getLayerPaletteEntries);
+    GetWGLProcAddress(glModule, getProcAddress, "wglRealizeLayerPalette", &realizeLayerPalette);
+    GetWGLProcAddress(glModule, getProcAddress, "wglSwapLayerBuffers", &swapLayerBuffers);
+    GetWGLProcAddress(glModule, getProcAddress, "wglSwapMultipleBuffers", &swapMultipleBuffers);
 
     // Load extension string getter functions
-    PFNWGLGETEXTENSIONSSTRINGEXTPROC getExtensionStringEXT = nullptr;
-    GetWGLProcAddress(glModule, getProcAddressWGL, "wglGetExtensionsStringEXT", &getExtensionStringEXT);
-
-    PFNWGLGETEXTENSIONSSTRINGARBPROC getExtensionStringARB = nullptr;
-    GetWGLProcAddress(glModule, getProcAddressWGL, "wglGetExtensionsStringARB", &getExtensionStringARB);
+    GetWGLProcAddress(glModule, getProcAddress, "wglGetExtensionsStringEXT", &getExtensionStringEXT);
+    GetWGLProcAddress(glModule, getProcAddress, "wglGetExtensionsStringARB", &getExtensionStringARB);
 
     std::string extensions = "";
     if (getExtensionStringEXT)
