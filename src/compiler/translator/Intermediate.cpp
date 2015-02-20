72ebf29b2577534c617c4a1d35bf472b6735c4d2
Olli Etuaho <oetuaho@nvidia.com>
2015-02-20 11:34:03 +0200

    Add stubs for switch and case output
    BUG=angle:921
    
    Change-Id: I58bd645a8d53ef5bad9b680e54c8948d50932fca

--- src/compiler/translator/Intermediate.cpp
+++ src/compiler/translator/Intermediate.cpp
@@ -461,17 +461,19 @@ TIntermTyped *TIntermediate::addSelection(
 TIntermSwitch *TIntermediate::addSwitch(
     TIntermTyped *init, TIntermAggregate *statementList, const TSourceLoc &line)
 {
-    mInfoSink.info.message(EPrefixInternalError, line,
-        "Switch statements are disabled for now");
-    return NULL;
+    TIntermSwitch *node = new TIntermSwitch(init, statementList);
+    node->setLine(line);
+
+    return node;
 }
 
 TIntermCase *TIntermediate::addCase(
     TIntermTyped *condition, const TSourceLoc &line)
 {
-    mInfoSink.info.message(EPrefixInternalError, line,
-        "Case labels and default labels are disabled for now");
-    return NULL;
+    TIntermCase *node = new TIntermCase(condition);
+    node->setLine(line);
+
+    return node;
 }
 
 //
