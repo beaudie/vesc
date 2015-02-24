d0f9d9f7d73b09891fe0defb25495841d7353d99
Geoff Lang <geofflang@chromium.org>
2015-02-24 11:47:20 -0500

    Implement basic functionality in BufferGL.
    BUG=angle:881
    
    Change-Id: I8d761741d204d26cea1c87eec725bc8ebaaaa584

--- src/libANGLE/renderer/gl/BufferGL.cpp
+++ src/libANGLE/renderer/gl/BufferGL.cpp
@@ -9,45 +9,75 @@
 #include "libANGLE/renderer/gl/BufferGL.h"
 
 #include "common/debug.h"
+#include "libANGLE/angletypes.h"
+#include "libANGLE/renderer/gl/FunctionsGL.h"
+#include "libANGLE/renderer/gl/StateManagerGL.h"
 
 namespace rx
 {
 
-BufferGL::BufferGL()
-    : BufferImpl()
-{}
+BufferGL::BufferGL(const FunctionsGL *functions, StateManagerGL *stateManager)
+    : BufferImpl(),
+      mFunctions(functions),
+      mStateManager(stateManager),
+      mBufferID(0)
+{
+    ASSERT(mFunctions);
+    ASSERT(mStateManager);
+
+    mFunctions->genBuffers(1, &mBufferID);
+}
 
 BufferGL::~BufferGL()
-{}
+{
+    if (mBufferID)
+    {
+        mFunctions->deleteBuffers(1, &mBufferID);
+        mBufferID = 0;
+    }
+}
 
 gl::Error BufferGL::setData(const void* data, size_t size, GLenum usage)
 {
-    UNIMPLEMENTED();
-    return gl::Error(GL_INVALID_OPERATION);
+    mStateManager->setBuffer(GL_ARRAY_BUFFER, mBufferID);
+    mFunctions->bufferData(GL_ARRAY_BUFFER, size, data, usage);
+    return gl::Error(GL_NO_ERROR);
 }
 
 gl::Error BufferGL::setSubData(const void* data, size_t size, size_t offset)
 {
-    UNIMPLEMENTED();
-    return gl::Error(GL_INVALID_OPERATION);
+    mStateManager->setBuffer(GL_ARRAY_BUFFER, mBufferID);
+    mFunctions->bufferSubData(GL_ARRAY_BUFFER, offset, size, data);
+    return gl::Error(GL_NO_ERROR);
 }
 
 gl::Error BufferGL::copySubData(BufferImpl* source, GLintptr sourceOffset, GLintptr destOffset, GLsizeiptr size)
 {
-    UNIMPLEMENTED();
-    return gl::Error(GL_INVALID_OPERATION);
+    BufferGL *sourceGL = GetAs<BufferGL>(source);
+
+    mStateManager->setBuffer(GL_ARRAY_BUFFER, mBufferID);
+    mStateManager->setBuffer(GL_ELEMENT_ARRAY_BUFFER, sourceGL->getBufferID());
+
+    mFunctions->copyBufferSubData(GL_ELEMENT_ARRAY_BUFFER, GL_ARRAY_BUFFER, sourceOffset, destOffset, size);
+
+    return gl::Error(GL_NO_ERROR);
 }
 
 gl::Error BufferGL::map(size_t offset, size_t length, GLbitfield access, GLvoid **mapPtr)
 {
-    UNIMPLEMENTED();
-    return gl::Error(GL_INVALID_OPERATION);
+    // TODO: look into splitting this into two functions, glMapBuffer is available in 1.5, but
+    // glMapBufferRange requires 3.0
+
+    mStateManager->setBuffer(GL_ARRAY_BUFFER, mBufferID);
+    *mapPtr = mFunctions->mapBufferRange(GL_ARRAY_BUFFER, offset, length, access);
+    return gl::Error(GL_NO_ERROR);
 }
 
 gl::Error BufferGL::unmap()
 {
-    UNIMPLEMENTED();
-    return gl::Error(GL_INVALID_OPERATION);
+    mStateManager->setBuffer(GL_ARRAY_BUFFER, mBufferID);
+    mFunctions->unmapBuffer(GL_ARRAY_BUFFER);
+    return gl::Error(GL_NO_ERROR);
 }
 
 void BufferGL::markTransformFeedbackUsage()
@@ -59,6 +89,11 @@ gl::Error BufferGL::getData(const uint8_t **outData)
 {
     UNIMPLEMENTED();
     return gl::Error(GL_INVALID_OPERATION);
+}
+
+GLuint BufferGL::getBufferID() const
+{
+    return mBufferID;
 }
 
 }
