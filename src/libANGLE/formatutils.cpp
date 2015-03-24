97cedaa20b5abeee5c13fd25161a2362acbe4736
Olli Etuaho <oetuaho@nvidia.com>
2015-03-24 17:28:18 +0200

    Micro-optimize math in IndexDataManager
    Use bitwise operations instead of division, which is expensive on multiple CPU
    architectures.
    
    BUG=angleproject:956
    TEST=angle_end2end_tests
    
    Change-Id: I57ab540d447c03dae5a96bafb4975fc37e310261

--- src/libANGLE/formatutils.cpp
+++ src/libANGLE/formatutils.cpp
@@ -132,6 +132,7 @@ FormatMap BuildFormatMap()
 
 Type::Type()
     : bytes(0),
+      bytesShift(0),
       specialInterpretation(false)
 {
 }
@@ -144,6 +145,13 @@ static inline void InsertTypeInfo(TypeInfoMap *map, GLenum type, GLuint bytes, b
 {
     Type info;
     info.bytes = bytes;
+    GLuint i = 0;
+    while ((1u << i) < bytes)
+    {
+        ++i;
+    }
+    info.bytesShift = i;
+    ASSERT((1u << info.bytesShift) == bytes);
     info.specialInterpretation = specialInterpretation;
 
     map->insert(std::make_pair(type, info));
