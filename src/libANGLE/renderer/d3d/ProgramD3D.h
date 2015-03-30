be4427bbdc7b67d55b3d69b2489ad78c2e7b38da
Geoff Lang <geofflang@chromium.org>
2015-03-30 19:51:55 +0000

    Revert "Cache std::vectors in ProgramD3D to avoid allocations during draw calls."
    Caused a warning on 32 bit builds.
    
    This reverts commit 7a26a1ad025101e156d2fbc9887a6c94acf00721.
    
    Change-Id: Idf241d7b5670ec9e3ccc399a3e7c8270be72568c

--- src/libANGLE/renderer/d3d/ProgramD3D.h
+++ src/libANGLE/renderer/d3d/ProgramD3D.h
@@ -225,12 +225,6 @@ class ProgramD3D : public ProgramImpl
     GLuint mUsedPixelSamplerRange;
     bool mDirtySamplerMapping;
 
-    // Cache for validateSamplers
-    std::vector<GLenum> mTextureUnitTypesCache;
-
-    // Cache for getPixelExecutableForFramebuffer
-    std::vector<GLenum> mPixelShaderOutputFormatCache;
-
     int mShaderVersion;
 
     int mAttributesByLayout[gl::MAX_VERTEX_ATTRIBS];
