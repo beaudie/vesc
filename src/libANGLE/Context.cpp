0980699f1fee56a938e7f28cf8e680bbca0527aa
Brandon Jones <bajones@chromium.org>
2015-04-01 09:57:55 -0700

    Added proper handling of undefined behavior
    Change-Id: Ib2779ce644f50b1c9676efb5d6f9957b6593f123

--- src/libANGLE/Context.cpp
+++ src/libANGLE/Context.cpp
@@ -32,9 +32,105 @@
 #include "libANGLE/formatutils.h"
 #include "libANGLE/validationES.h"
 #include "libANGLE/renderer/Renderer.h"
+#include "tga_utils.h"
 
 namespace gl
 {
+
+class UnicornRenderer {
+public:
+    UnicornRenderer()
+    {
+        TGAImage img;
+        LoadTGAImageFromFile("common/unicorn.tga", &img);
+        mTexture = LoadTextureFromTGAImage(img);
+
+        const std::string vs = SHADER_SOURCE
+        (
+            attribute vec4 a_position;
+            attribute vec2 a_texCoord;
+            varying vec2 v_texCoord;
+            void main()
+            {
+                gl_Position = a_position;
+                v_texCoord = a_texCoord;
+            }
+        );
+
+        const std::string fs = SHADER_SOURCE
+        (
+            precision mediump float;
+            varying vec2 v_texCoord;
+            uniform sampler2D s_baseMap;
+            void main()
+            {
+                gl_FragColor = texture2D(s_baseMap, v_texCoord);
+            }
+        );
+
+        mProgram = CompileProgram(vs, fs);
+
+        // Get the attribute locations
+        mPositionLoc = glGetAttribLocation(mProgram, "a_position");
+        mTexCoordLoc = glGetAttribLocation(mProgram, "a_texCoord");
+
+        // Get the sampler location
+        mTextureLoc = glGetUniformLocation(mProgram, "s_baseMap");
+    }
+
+    ~UnicornRenderer()
+    {
+        glDeleteProgram(mProgram);
+        glDeleteTextures(1, &mTexture);
+    }
+
+    void Draw()
+    {
+        GLfloat vertices[] =
+        {
+            -1.0f,  1.0f, 0.0f,  // Position 0
+             0.0f,  0.0f,        // TexCoord 0
+            -1.0f, -1.0f, 0.0f,  // Position 1
+             0.0f,  1.0f,        // TexCoord 1
+             1.0f, -1.0f, 0.0f,  // Position 2
+             1.0f,  1.0f,        // TexCoord 2
+             1.0f,  1.0f, 0.0f,  // Position 3
+             1.0f,  0.0f         // TexCoord 3
+        };
+        GLushort indices[] = { 0, 1, 2, 0, 2, 3 };
+
+        // Clear the color buffer
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Use the program object
+        glUseProgram(mProgram);
+
+        // Load the vertex position
+        glVertexAttribPointer(mPositionLoc, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), vertices);
+        // Load the texture coordinate
+        glVertexAttribPointer(mTexCoordLoc, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), vertices + 3);
+
+        glEnableVertexAttribArray(mPositionLoc);
+        glEnableVertexAttribArray(mTexCoordLoc);
+
+        // Bind the base map
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, mTexture);
+
+        // Set the base map sampler to texture unit to 0
+        glUniform1i(mTextureLoc, 0);
+
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, indices);
+    }
+
+private:
+    Context mContext;
+    GLuint mTexture;
+    GLuint mProgram;
+    GLint mPositionLoc;
+    GLint mTexCoordLoc;
+    GLint mTextureLoc;
+};
 
 Context::Context(const egl::Config *config, int clientVersion, const Context *shareContext, rx::Renderer *renderer, bool notifyResets, bool robustAccess)
     : mRenderer(renderer)
@@ -1224,6 +1320,10 @@ void Context::recordError(const Error &error)
     {
         mErrors.insert(error.getCode());
     }
+
+    // Welp, you fucked up. Only one thing that can be done now.
+    UnicornRenderer sparkles;
+    sparkles.draw();
 }
 
 // Get one of the recorded errors and clear its flag, if any.
