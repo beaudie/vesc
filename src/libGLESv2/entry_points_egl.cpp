0685ea39e2cbfd96a09d85b6fd72a90dcd4c0151
Jamie Madill <jmadill@chromium.org>
2015-04-10 11:45:36 -0400

    Release Surface when calling makeCurrent with null.
    Refactorings to egl::Surface to enable ref-counting were causing
    a situation where we could have two Window surfaces alive at the
    same time. This would confuse the window procude logic in
    SurfaceD3D. Releasing the surface fixes this issue and conforms
    closely to the wording on the spec on when Surfaces should be
    deleted. Also add a test for message loops and surfaces.
    
    BUG=475085
    BUG=angleproject:963
    
    Change-Id: Icdee3a7db97c9b54d779dabf1e1f82a89fefc546

--- src/libGLESv2/entry_points_egl.cpp
+++ src/libGLESv2/entry_points_egl.cpp
@@ -564,14 +564,18 @@ EGLBoolean EGLAPIENTRY MakeCurrent(EGLDisplay dpy, EGLSurface draw, EGLSurface r
         UNIMPLEMENTED();   // FIXME
     }
 
+    gl::Context *previousContext = GetGlobalContext();
+
     SetGlobalDisplay(display);
     SetGlobalDrawSurface(drawSurface);
     SetGlobalReadSurface(readSurface);
     SetGlobalContext(context);
 
-    if (context != nullptr && display != nullptr && drawSurface != nullptr)
+    display->makeCurrent(drawSurface, readSurface, context);
+
+    if (previousContext != nullptr)
     {
-        display->makeCurrent(drawSurface, readSurface, context);
+        previousContext->releaseSurface();
     }
 
     SetGlobalError(Error(EGL_SUCCESS));
