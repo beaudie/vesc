3678ba79d993793d64fb7fdd5909f5556075e2cb
Geoff Lang <geofflang@chromium.org>
2015-04-10 16:29:46 -0400

    EGLimage.
    BUG=angleproject:970
    
    Change-Id: Icd97206c2e98b17726f017f18507bde5f372568c

--- src/libANGLE/RefCountObject.h
+++ src/libANGLE/RefCountObject.h
@@ -44,6 +44,12 @@ public:
     {
     }
 
+    explicit BindingPointer(ObjectType *newObject)
+        : BindingPointer()
+    {
+        set(newObject);
+    }
+
     BindingPointer(const BindingPointer<ObjectType> &other)
         : mObject(nullptr)
     {
@@ -57,8 +63,7 @@ public:
 
     virtual ~BindingPointer()
     {
-        // Objects have to be released before the resource manager is destroyed, so they must be explicitly cleaned up.
-        ASSERT(mObject == nullptr);
+        set(nullptr);
     }
 
     virtual void set(ObjectType *newObject)
