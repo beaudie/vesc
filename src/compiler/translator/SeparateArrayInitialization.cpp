7e657b99adb64d0516d4302a3350e1f6b165f952
Olli Etuaho <oetuaho@nvidia.com>
2015-04-16 14:26:10 +0300

    Support array initialization in HLSL output
    Do this by separating each array initialization into a declaration and an
    assignment. Array assignment is already supported in HLSL output by replacing
    it with a function call.
    
    The functionality is tested by the struct array constructor tests in dEQP.
    
    BUG=angleproject:941
    TEST=dEQP-GLES3.functional.shaders.arrays.constructor.*
    
    Change-Id: Ida84fc343b767bea8b2d04e91c60cb8197d39039

---
+++ src/compiler/translator/SeparateArrayInitialization.cpp
@@ -0,0 +1,51 @@
+//
+// Copyright (c) 2002-2015 The ANGLE Project Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+
+#include "compiler/translator/SeparateArrayInitialization.h"
+
+SeparateArrayInitialization::SeparateArrayInitialization()
+    : TIntermTraverser(true, false, false)
+{
+}
+
+bool SeparateArrayInitialization::visitAggregate(Visit, TIntermAggregate *node)
+{
+    if (node->getOp() == EOpDeclaration)
+    {
+        TIntermSequence *sequence = node->getSequence();
+        ASSERT(sequence->size() == 1);
+        TIntermBinary *initNode = sequence->back()->getAsBinaryNode();
+        if (initNode != nullptr && initNode->getOp() == EOpInitialize)
+        {
+            TIntermTyped *initializer = initNode->getRight();
+            if (initializer->isArray())
+            {
+                TIntermTyped *symbol = initNode->getLeft();
+                TIntermAggregate *parentAgg = getParentNode()->getAsAggregate();
+                ASSERT(parentAgg != nullptr);
+
+                TIntermSequence replacements;
+
+                TIntermAggregate *replacementDeclaration = new TIntermAggregate;
+                replacementDeclaration->setOp(EOpDeclaration);
+                replacementDeclaration->getSequence()->push_back(symbol);
+                replacementDeclaration->setLine(symbol->getLine());
+                replacements.push_back(replacementDeclaration);
+
+                TIntermBinary *replacementAssignment = new TIntermBinary(EOpAssign);
+                replacementAssignment->setLeft(symbol);
+                replacementAssignment->setRight(initializer);
+                replacementAssignment->setType(initializer->getType());
+                replacementAssignment->setLine(symbol->getLine());
+                replacements.push_back(replacementAssignment);
+
+                mMultiReplacements.push_back(NodeReplaceWithMultipleEntry(parentAgg, node, replacements));
+            }
+        }
+        return false;
+    }
+    return true;
+}
