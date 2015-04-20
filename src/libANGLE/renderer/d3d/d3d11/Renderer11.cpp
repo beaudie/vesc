e380b363216635c9a23cc9906f6ba3290afa4b80
Jamie Madill <jmadill@chromium.org>
2015-04-20 08:18:58 -0400

    Make Framebuffer::Data members private.
    This makes "Data" a proper class, and enforces access control when
    used in FramebufferImpl. This gives a cleaner refactor when we
    switch the internals of the class to use value types to store
    attachments instead of pointer types.
    
    BUG=angleproject:963
    
    Change-Id: If825095458eaf9367f616f0bb54084025efb9882

--- src/libANGLE/renderer/d3d/d3d11/Renderer11.cpp
+++ src/libANGLE/renderer/d3d/d3d11/Renderer11.cpp
@@ -1270,7 +1270,7 @@ gl::Error Renderer11::applyRenderTarget(const gl::Framebuffer *framebuffer)
 
     for (size_t colorAttachment = 0; colorAttachment < colorbuffers.size(); ++colorAttachment)
     {
-        gl::FramebufferAttachment *colorbuffer = colorbuffers[colorAttachment];
+        const gl::FramebufferAttachment *colorbuffer = colorbuffers[colorAttachment];
 
         if (colorbuffer)
         {
@@ -1319,7 +1319,7 @@ gl::Error Renderer11::applyRenderTarget(const gl::Framebuffer *framebuffer)
 
     // Get the depth stencil buffers
     ID3D11DepthStencilView* framebufferDSV = NULL;
-    gl::FramebufferAttachment *depthStencil = framebuffer->getDepthOrStencilbuffer();
+    const gl::FramebufferAttachment *depthStencil = framebuffer->getDepthOrStencilbuffer();
     if (depthStencil)
     {
         RenderTarget11 *depthStencilRenderTarget = NULL;
@@ -2415,7 +2415,7 @@ std::string Renderer11::getShaderModelSuffix() const
 gl::Error Renderer11::copyImage2D(const gl::Framebuffer *framebuffer, const gl::Rectangle &sourceRect, GLenum destFormat,
                                   const gl::Offset &destOffset, TextureStorage *storage, GLint level)
 {
-    gl::FramebufferAttachment *colorbuffer = framebuffer->getReadColorbuffer();
+    const gl::FramebufferAttachment *colorbuffer = framebuffer->getReadColorbuffer();
     ASSERT(colorbuffer);
 
     RenderTarget11 *sourceRenderTarget = NULL;
@@ -2466,7 +2466,7 @@ gl::Error Renderer11::copyImage2D(const gl::Framebuffer *framebuffer, const gl::
 gl::Error Renderer11::copyImageCube(const gl::Framebuffer *framebuffer, const gl::Rectangle &sourceRect, GLenum destFormat,
                                     const gl::Offset &destOffset, TextureStorage *storage, GLenum target, GLint level)
 {
-    gl::FramebufferAttachment *colorbuffer = framebuffer->getReadColorbuffer();
+    const gl::FramebufferAttachment *colorbuffer = framebuffer->getReadColorbuffer();
     ASSERT(colorbuffer);
 
     RenderTarget11 *sourceRenderTarget = NULL;
@@ -2517,7 +2517,7 @@ gl::Error Renderer11::copyImageCube(const gl::Framebuffer *framebuffer, const gl
 gl::Error Renderer11::copyImage3D(const gl::Framebuffer *framebuffer, const gl::Rectangle &sourceRect, GLenum destFormat,
                                   const gl::Offset &destOffset, TextureStorage *storage, GLint level)
 {
-    gl::FramebufferAttachment *colorbuffer = framebuffer->getReadColorbuffer();
+    const gl::FramebufferAttachment *colorbuffer = framebuffer->getReadColorbuffer();
     ASSERT(colorbuffer);
 
     RenderTarget11 *sourceRenderTarget = NULL;
@@ -2568,7 +2568,7 @@ gl::Error Renderer11::copyImage3D(const gl::Framebuffer *framebuffer, const gl::
 gl::Error Renderer11::copyImage2DArray(const gl::Framebuffer *framebuffer, const gl::Rectangle &sourceRect, GLenum destFormat,
                                        const gl::Offset &destOffset, TextureStorage *storage, GLint level)
 {
-    gl::FramebufferAttachment *colorbuffer = framebuffer->getReadColorbuffer();
+    const gl::FramebufferAttachment *colorbuffer = framebuffer->getReadColorbuffer();
     ASSERT(colorbuffer);
 
     RenderTarget11 *sourceRenderTarget = NULL;
