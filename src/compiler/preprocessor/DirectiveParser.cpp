7d4f4ed9c6c8082bd22a4914b12026bca72b466e
Geoff Lang <geofflang@chromium.org>
2015-04-27 14:48:59 -0400

    Track if a non-preprocessor token has been seen and validate #extension with it.
    Fixes:
    dEQP-GLES2.functional.shaders.preprocessor.extensions.after_non_preprocessing_tokens_vertex
    dEQP-GLES2.functional.shaders.preprocessor.extensions.after_non_preprocessing_tokens_fragment
    
    BUG=angleproject:989
    
    Change-Id: Ie0aba80b2fde0160e83fc95f545b62954af2e5fc

--- src/compiler/preprocessor/DirectiveParser.cpp
+++ src/compiler/preprocessor/DirectiveParser.cpp
@@ -210,6 +210,7 @@ DirectiveParser::DirectiveParser(Tokenizer *tokenizer,
                                  Diagnostics *diagnostics,
                                  DirectiveHandler *directiveHandler)
     : mPastFirstStatement(false),
+      mSeenNonPreprocessorToken(false),
       mTokenizer(tokenizer),
       mMacroSet(macroSet),
       mDiagnostics(diagnostics),
@@ -227,6 +228,10 @@ void DirectiveParser::lex(Token *token)
         {
             parseDirective(token);
             mPastFirstStatement = true;
+        }
+        else if (!isEOD(token))
+        {
+            mSeenNonPreprocessorToken = true;
         }
 
         if (token->type == Token::LAST)
@@ -701,6 +706,12 @@ void DirectiveParser::parseExtension(Token *token)
         mTokenizer->lex(token);
     }
     if (valid && (state != EXT_BEHAVIOR + 1))
+    {
+        mDiagnostics->report(Diagnostics::PP_INVALID_EXTENSION_DIRECTIVE,
+                             token->location, token->text);
+        valid = false;
+    }
+    if (valid && mSeenNonPreprocessorToken)
     {
         mDiagnostics->report(Diagnostics::PP_INVALID_EXTENSION_DIRECTIVE,
                              token->location, token->text);
