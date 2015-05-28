f25177944ea7085578d948b109aafab298ef85e4
Jamie Madill <jmadill@chromium.org>
2015-05-28 13:26:02 -0400

    Add a perf test for eglInitialize.
    Also shutdown performance, since it's not easy to only test one.
    
    BUG=angleproject:
    
    Change-Id: I9d06426788ed336031271f8876589b111f1294b7

---
+++ src/tests/perf_tests/EGLInitializePerf.cpp
@@ -0,0 +1,95 @@
+//
+// Copyright 2015 The ANGLE Project Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+// EGLInitializePerfTest:
+//   Performance test for device creation.
+//
+
+#include "ANGLEPerfTest.h"
+#include "test_utils/angle_test_configs.h"
+#include "test_utils/angle_test_instantiate.h"
+
+using namespace testing;
+
+namespace
+{
+
+class EGLInitializePerfTest : public ANGLEPerfTest,
+                              public WithParamInterface<angle::PlatformParameters>
+{
+  public:
+    EGLInitializePerfTest();
+    ~EGLInitializePerfTest();
+
+    void step(float dt, double totalTime) override;
+
+  private:
+    std::vector<EGLint> mDisplayAttributes;
+    OSWindow *mOSWindow;
+    EGLDisplay mDisplay;
+};
+
+EGLInitializePerfTest::EGLInitializePerfTest()
+    : ANGLEPerfTest("EGLInitialize", "_run"),
+      mOSWindow(nullptr),
+      mDisplay(EGL_NO_DISPLAY)
+{
+    auto platform = GetParam().mEGLPlatformParameters;
+
+    mDisplayAttributes.push_back(EGL_PLATFORM_ANGLE_TYPE_ANGLE);
+    mDisplayAttributes.push_back(platform.renderer);
+    mDisplayAttributes.push_back(EGL_PLATFORM_ANGLE_MAX_VERSION_MAJOR_ANGLE);
+    mDisplayAttributes.push_back(platform.majorVersion);
+    mDisplayAttributes.push_back(EGL_PLATFORM_ANGLE_MAX_VERSION_MINOR_ANGLE);
+    mDisplayAttributes.push_back(platform.minorVersion);
+
+    if (platform.renderer == EGL_PLATFORM_ANGLE_TYPE_D3D9_ANGLE ||
+        platform.renderer == EGL_PLATFORM_ANGLE_TYPE_D3D11_ANGLE)
+    {
+        mDisplayAttributes.push_back(EGL_PLATFORM_ANGLE_DEVICE_TYPE_ANGLE);
+        mDisplayAttributes.push_back(platform.deviceType);
+    }
+    mDisplayAttributes.push_back(EGL_NONE);
+
+    mOSWindow = CreateOSWindow();
+    mOSWindow->initialize("EGLInitialize Test", 64, 64);
+
+    auto eglGetPlatformDisplayEXT = reinterpret_cast<PFNEGLGETPLATFORMDISPLAYEXTPROC>(eglGetProcAddress("eglGetPlatformDisplayEXT"));
+    if (eglGetPlatformDisplayEXT == nullptr)
+    {
+        std::cerr << "Error getting platform display!" << std::endl;
+        return;
+    }
+
+    mDisplay = eglGetPlatformDisplayEXT(EGL_PLATFORM_ANGLE_ANGLE, mOSWindow->getNativeDisplay(), &mDisplayAttributes[0]);
+}
+
+EGLInitializePerfTest::~EGLInitializePerfTest()
+{
+    SafeDelete(mOSWindow);
+}
+
+void EGLInitializePerfTest::step(float dt, double totalTime)
+{
+    ASSERT_TRUE(mDisplay != EGL_NO_DISPLAY);
+
+    EGLint majorVersion, minorVersion;
+    ASSERT_TRUE(eglInitialize(mDisplay, &majorVersion, &minorVersion) == EGL_TRUE);
+    ASSERT_TRUE(eglTerminate(mDisplay) == EGL_TRUE);
+
+    if (mTimer->getElapsedTime() >= 5.0)
+    {
+        mRunning = false;
+    }
+}
+
+TEST_P(EGLInitializePerfTest, Run)
+{
+    run();
+}
+
+ANGLE_INSTANTIATE_TEST(EGLInitializePerfTest, angle::ES2_D3D11());
+
+}
