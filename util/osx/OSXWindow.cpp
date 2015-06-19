4ce87ebd511e3eb2415ffa63318ec77d96cbb0c0
Corentin Wallez <cwallez@chromium.org>
2015-06-19 15:42:09 -0700

    Compile end2end tests on MacOS
    This includes an implementation of OSX timer and path_utils.
    
    BUG=angleproject:891
    
    Change-Id: Id5440a8321bcfd4a61f3fe35b4a3fc0c651f564f

---
+++ util/osx/OSXWindow.cpp
@@ -0,0 +1,14 @@
+//
+// Copyright (c) 2015 The ANGLE Project Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+
+// OSXWindow.cpp: Implementation of OSWindow for OSX
+
+#include "osx/OSXWindow.h"
+
+OSWindow *CreateOSWindow()
+{
+    return nullptr;
+}
