768620950b90a0018df78349ebfb29d5dd2237a7
Arun Patole <apatole@nvidia.com>
2015-06-30 17:48:25 +0530

    Constant fold float pack/unpack functions
    This change adds constant folding support for following floating
    point pack and unpack functions:
        - packSnorm2x16, unpackSnorm2x16, packUnorm2x16, unpackUnorm2x16,
          packHalf2x16, and unpackHalf2x16.
    
    BUG=angleproject:913
    TEST=angle_unittests(new: MathUtilTest.packAndUnpack*), dEQP Tests
    dEQP-GLES3.functional.shaders.constant_expressions.builtin_functions.float_pack_unpack.*
    (all 10 tests started passing with this change)
    
    Change-Id: I2b69faebee4127e5e13900b3a9485b7145950277

---
+++ src/common/mathutil_unittest.cpp
@@ -0,0 +1,82 @@
+//
+// Copyright 2015 The ANGLE Project Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+// mathutil_unittests:
+//   Unit tests for the utils defined in mathutil.h
+//
+
+#include "mathutil.h"
+
+#include <gtest/gtest.h>
+
+using namespace gl;
+
+namespace
+{
+
+// Test the correctness of packSnorm2x16 and unpackSnorm2x16 functions.
+// For floats f1 and f2, unpackSNorm2x16(packSNorm2x16(f1, f2)) should be same as f1 and f2.
+TEST(MathUtilTest, packAndUnpackSnorm2x16Test)
+{
+    const float input[2][8] =
+    {
+        0.0f, 1.0f, -1.0f, -1.0f, 0.875f, 0.00392f, -0.000675f, -0.6937f,
+        0.0f, 1.0f, 1.0f, -1.0f, 0.75f, -0.99215f, 0.004954f, -0.02146f
+    };
+    const float floatFaultTolerance = 0.0001f;
+    float outputVal1, outputVal2;
+
+    for (size_t i = 0; i < 8; i++)
+    {
+        unpackSNorm2x16(packSNorm2x16(input[0][i], input[1][i]), &outputVal1, &outputVal2);
+        EXPECT_NEAR(input[0][i], outputVal1, floatFaultTolerance);
+        EXPECT_NEAR(input[1][i], outputVal2, floatFaultTolerance);
+    }
+}
+
+// Test the correctness of packUnorm2x16 and unpackUnorm2x16 functions.
+// For floats f1 and f2, unpackUNorm2x16(packUNorm2x16(f1, f2)) should be same as f1 and f2.
+TEST(MathUtilTest, packAndUnpackUnorm2x16Test)
+{
+    const float input[2][8] =
+    {
+        0.0f, 1.0f, -1.0f, -1.0f, 0.875f, 0.00392f, -0.000675f, -0.6937f,
+        0.0f, 1.0f, 1.0f, -1.0f, 0.75f, -0.99215f, 0.004954f, -0.02146f
+    };
+    const float floatFaultTolerance = 0.0001f;
+    float outputVal1, outputVal2;
+
+    for (size_t i = 0; i < 8; i++)
+    {
+        unpackUNorm2x16(packUNorm2x16(input[0][i], input[1][i]), &outputVal1, &outputVal2);
+        float expected = input[0][i] < 0.0f ? 0.0f : input[0][i];
+        EXPECT_NEAR(expected, outputVal1, floatFaultTolerance);
+        expected = input[1][i] < 0.0f ? 0.0f : input[1][i];
+        EXPECT_NEAR(expected, outputVal2, floatFaultTolerance);
+    }
+}
+
+// Test the correctness of packHalf2x16 and unpackHalf2x16 functions.
+// For floats f1 and f2, unpackHalf2x16(packHalf2x16(f1, f2)) should be same as f1 and f2.
+TEST(MathUtilTest, packAndUnpackHalf2x16Test)
+{
+    const float input[2][8] =
+    {
+        0.0f, 1.0f, -1.0f, -1.0f, 0.875f, 0.00392f, -0.000675f, -0.6937f,
+        0.0f, 1.0f, 1.0f, -1.0f, 0.75f, -0.99215f, 0.004954f, -0.02146f
+    };
+    const float floatFaultTolerance = 0.0005f;
+    float outputVal1, outputVal2;
+
+    for (size_t i = 0; i < 8; i++)
+    {
+        unpackHalf2x16(packHalf2x16(input[0][i], input[1][i]), &outputVal1, &outputVal2);
+        EXPECT_NEAR(input[0][i], outputVal1, floatFaultTolerance);
+        EXPECT_NEAR(input[1][i], outputVal2, floatFaultTolerance);
+    }
+}
+
+}
+
