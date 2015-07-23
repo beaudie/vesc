072d16442449512f38b5b71fe9f791289563cff9
Geoff Lang <geofflang@chromium.org>
2015-07-23 14:54:47 -0400

    Fix typo when syncing GL_UNPACK_SKIP_ROWS and GL_PACK_SKIP_ROWS.
    Change-Id: I090e21ebd7781ce3372b8842421e7da3e4bedbac

--- src/libANGLE/renderer/gl/StateManagerGL.cpp
+++ src/libANGLE/renderer/gl/StateManagerGL.cpp
@@ -270,7 +270,7 @@ void StateManagerGL::setPixelUnpackState(GLint alignment, GLint rowLength, GLint
 
     if (mUnpackSkipRows != skipRows)
     {
-        mUnpackSkipRows = rowLength;
+        mUnpackSkipRows = skipRows;
         mFunctions->pixelStorei(GL_UNPACK_SKIP_ROWS, mUnpackSkipRows);
     }
 
@@ -309,7 +309,7 @@ void StateManagerGL::setPixelPackState(GLint alignment, GLint rowLength, GLint s
 
     if (mPackSkipRows != skipRows)
     {
-        mPackSkipRows = rowLength;
+        mPackSkipRows = skipRows;
         mFunctions->pixelStorei(GL_PACK_SKIP_ROWS, mPackSkipRows);
     }
 
