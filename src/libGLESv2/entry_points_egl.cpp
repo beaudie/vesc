6b01148ebf03ca44cc32fe68bf6cb66092a174e6
Geoff Lang <geofflang@chromium.org>
2015-08-07 16:49:07 -0400

    Add stubs for EGL_EXT_swap_buffers_with_damage.
    BUG=512090
    
    Change-Id: I9413d6f5c13b9ea59ab9c923dc6c5d157f344166

--- src/libGLESv2/entry_points_egl.cpp
+++ src/libGLESv2/entry_points_egl.cpp
@@ -1808,6 +1808,9 @@ __eglMustCastToProperFunctionPointerType EGLAPIENTRY GetProcAddress(const char *
         INSERT_PROC_ADDRESS(egl, CreateStreamProducerD3DTextureNV12ANGLE);
         INSERT_PROC_ADDRESS(egl, StreamPostD3DTextureNV12ANGLE);
 
+        // EGL_EXT_swap_buffers_with_damage
+        INSERT_PROC_ADDRESS(egl, SwapBuffersWithDamageEXT);
+
 #undef INSERT_PROC_ADDRESS
         return map;
     };
