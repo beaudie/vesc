e3b6e2b88019b39a4a5fcb77d3dafe4133768db7
Corentin Wallez <cwallez@chromium.org>
2015-08-18 11:29:46 -0400

    ProgramGL: Mark all columns of matrix attributes as active
    The active attributes are used to determine which of the vertex
    attributes to enable. Matrices take up several attributes depending on
    the number of column they have; this patch marks all the columns of
    active matrix attributes as active.
    
    TEST=dEQP-GLES2.functional.shaders.linkage.varying_type_mat*
    
    BUG=angleproject:1127
    
    Change-Id: I82f548f329f872c4e66aa0e4e71c6061a55b9987

--- src/libANGLE/renderer/gl/ProgramGL.cpp
+++ src/libANGLE/renderer/gl/ProgramGL.cpp
@@ -198,7 +198,11 @@ LinkResult ProgramGL::link(const gl::Data &data, gl::InfoLog &infoLog,
         // TODO: determine attribute precision
         setShaderAttribute(static_cast<size_t>(i), attributeType, GL_NONE, attributeName, attributeSize, location);
 
-        mActiveAttributesMask.set(location);
+        size_t attributeUsage = gl::IsMatrixType(attributeType) ? gl::VariableColumnCount(attributeType) : 1;
+        for (size_t offset = 0; offset < attributeUsage; offset++)
+        {
+            mActiveAttributesMask.set(location + offset);
+        }
     }
 
     return LinkResult(true, gl::Error(GL_NO_ERROR));
