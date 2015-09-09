e97eda370da4ffd2bbccd6e93aa09ee5e6b9bea0
Geoff Lang <geofflang@chromium.org>
2015-09-09 15:33:31 -0400

    Explicitly enable framebuffer SRGB blending in StateManagerGL.
    In DesktopGL, SRGB blending must be enabled or linear blending will be
    used.
    
    reland: Work around issues on AMD drivers where SRGB blending would be
    used on clears of linear attachments.
    
    Passes all dEQP-GLES3.functional.fragment_ops.blend.fbo_srgb.* tests (1106
    new passing tests).
    
    BUG=angleproject:883
    BUG=angleproject:885
    
    Change-Id: I03fbb0910ce414552ba841099a177446d0ace7b6

--- src/libANGLE/renderer/gl/StateManagerGL.cpp
+++ src/libANGLE/renderer/gl/StateManagerGL.cpp
@@ -90,6 +90,7 @@ StateManagerGL::StateManagerGL(const FunctionsGL *functions, const gl::Caps &ren
       mClearColor(0.0f, 0.0f, 0.0f, 0.0f),
       mClearDepth(1.0f),
       mClearStencil(0),
+      mFramebufferSRGBEnabled(false),
       mTextureCubemapSeamlessEnabled(false),
       mLocalDirtyBits()
 {
@@ -504,6 +505,7 @@ gl::Error StateManagerGL::setGenericDrawState(const gl::Data &data)
     const gl::Framebuffer *framebuffer = state.getDrawFramebuffer();
     const FramebufferGL *framebufferGL = GetImplAs<FramebufferGL>(framebuffer);
     bindFramebuffer(GL_DRAW_FRAMEBUFFER, framebufferGL->getFramebufferID());
+    framebufferGL->syncDrawState();
 
     // Seamless cubemaps are required for ES3 and higher contexts.
     setTextureCubemapSeamlessEnabled(data.clientVersion >= 3);
@@ -1222,6 +1224,22 @@ void StateManagerGL::syncState(const gl::State &state, const gl::State::DirtyBit
         }
 
         mLocalDirtyBits.reset();
+    }
+}
+
+void StateManagerGL::setFramebufferSRGBEnabled(bool enabled)
+{
+    if (mFramebufferSRGBEnabled != enabled)
+    {
+        mFramebufferSRGBEnabled = enabled;
+        if (mFramebufferSRGBEnabled)
+        {
+            mFunctions->enable(GL_FRAMEBUFFER_SRGB);
+        }
+        else
+        {
+            mFunctions->disable(GL_FRAMEBUFFER_SRGB);
+        }
     }
 }
 
