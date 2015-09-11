647913d5d4539602a66f5ee1d6784052b5ac35c6
Dian Xiang <dianx@google.com>
2015-09-11 10:50:04 -0700

    Adding checks in DrawBuffers to return INVALID_ENUM
    When a enum is given that is not a BACK, NONE, or COLOR_ATTACHMENTi where
    i ranges from 0 to MAX_COLOR_ATTACHMENT, an INVALID_ENUM should be
    returned rather than an INVALID_OPERATION.
    
    BUG=angleproject:1148
    
    Change-Id: I3663f897face14f6ba46a15fb982efda6f4f4b05

--- src/libANGLE/validationES2.cpp
+++ src/libANGLE/validationES2.cpp
@@ -897,4 +897,63 @@ bool ValidateDiscardFramebufferEXT(Context *context, GLenum target, GLsizei numA
     return ValidateDiscardFramebufferBase(context, target, numAttachments, attachments, defaultFramebuffer);
 }
 
+bool ValidateDrawBuffers(Context *context, GLsizei n, const GLenum *bufs)
+{
+    // INVALID_VALUE is generated if n is negative or greater than value of MAX_DRAW_BUFFERS
+    if (n < 0 || static_cast<GLuint>(n) > context->getCaps().maxDrawBuffers)
+    {
+        context->recordError(
+            Error(GL_INVALID_VALUE, "n must be non-negative and no greater than MAX_DRAW_BUFFERS"));
+        return false;
+    }
+
+    // INVALID_OPERATION is generated if GL is bound to the default framebuffer
+    // and n is not 1 or bufs is bound to value other than BACK and NONE
+    ASSERT(context->getState().getDrawFramebuffer());
+    if (context->getState().getDrawFramebuffer()->id() == 0)
+    {
+        if (n != 1)
+        {
+            context->recordError(Error(GL_INVALID_OPERATION,
+                                       "n must be 1 when GL is bound to the default framebuffer"));
+            return false;
+        }
+
+        if (bufs[0] != GL_NONE && bufs[0] != GL_BACK)
+        {
+            context->recordError(Error(
+                GL_INVALID_OPERATION,
+                "Only NONE or BACK are valid values when drawing to the default framebuffer"));
+            return false;
+        }
+    }
+    else
+    {
+        // INVALID_ENUM-value in bufs is not NONE, BACK, or GL_COLOR_ATTACHMENTi
+        // INVALID_OPERATION-GL is bound to buffer and ith argument is not COLOR_ATTACHMENTi or NONE
+        GLuint maxColorAttachment =
+            GL_COLOR_ATTACHMENT0_EXT + context->getCaps().maxColorAttachments;
+
+        for (int colorAttachment = 0; colorAttachment < n; colorAttachment++)
+        {
+            const GLenum attachment = GL_COLOR_ATTACHMENT0_EXT + colorAttachment;
+
+            if (bufs[colorAttachment] != GL_NONE && bufs[colorAttachment] != GL_BACK &&
+                (bufs[colorAttachment] < GL_COLOR_ATTACHMENT0_EXT ||
+                 bufs[colorAttachment] >= maxColorAttachment))
+            {
+                context->recordError(Error(GL_INVALID_ENUM, "Invalid buffer value"));
+                return false;
+            }
+            else if (bufs[colorAttachment] != GL_NONE && bufs[colorAttachment] != attachment)
+            {
+                context->recordError(Error(GL_INVALID_OPERATION,
+                                           "Ith value does not match COLOR_ATTACHMENTi or NONE"));
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
 }
