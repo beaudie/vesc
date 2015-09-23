bf54bfac847c1545212e7c353f97e24bfa4d490a
Jamie Madill <jmadill@chromium.org>
2015-09-23 09:50:38 -0400

    Remove rx::ShaderSh and move the shared code to the GL.
    The GL layer can interact with the translator directly, to query all
    the active shader variables and call ShCompile.
    
    BUG=angleproject:1159
    
    Change-Id: I334a9bef28f93cf85dd8cac0fb8542ac567cc3ec

--- src/libANGLE/renderer/gl/RendererGL.cpp
+++ src/libANGLE/renderer/gl/RendererGL.cpp
@@ -242,9 +242,9 @@ CompilerImpl *RendererGL::createCompiler()
     return new CompilerGL(mFunctions);
 }
 
-ShaderImpl *RendererGL::createShader(gl::Shader::Data *data)
+ShaderImpl *RendererGL::createShader(const gl::Shader::Data &data)
 {
-    return new ShaderGL(data, *this);
+    return new ShaderGL(data, mFunctions);
 }
 
 ProgramImpl *RendererGL::createProgram(const gl::Program::Data &data)
