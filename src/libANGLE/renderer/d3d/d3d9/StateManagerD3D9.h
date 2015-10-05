546f9ea04c76ef61fbecc2e6fec8803ef4aeef2a
Dian Xiang <dianx@google.com>
2015-10-05 16:48:06 -0700

    Using dirty bits notification for blend state and depth stencil state
    Dirty bit notifications are used in GL for state tracking, but not D3D.
    Before, D3D would use memcmp to check a change in state for every call.
    This showed up as a hot spot in the perf test runs. Hence, switching D3D
    over to the dirty bit system similar to GL backend will help.
    
    Change-Id: I482edc852f1dcc888af3038ff3a61916496a02bc

---
+++ src/libANGLE/renderer/d3d/d3d9/StateManagerD3D9.h
@@ -0,0 +1,94 @@
+//
+// Copyright (c) 2015 The ANGLE Project Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+
+// StateManagerD3D11.h: Defines a class for caching D3D11 state
+
+#ifndef LIBANGLE_RENDERER_D3D9_STATEMANAGERD3D9_H_
+#define LIBANGLE_RENDERER_D3D9_STATEMANAGERD3D9_H_
+
+#include "libANGLE/renderer/d3d/StateManagerD3D.h"
+
+namespace rx
+{
+
+class StateManagerD3D9 final : public StateManagerD3D
+{
+  public:
+    explicit StateManagerD3D9(IDirect3DDevice9 *device, D3DADAPTER_IDENTIFIER9 &adapterIdentifier);
+
+    virtual ~StateManagerD3D9();
+
+    virtual gl::Error setBlendState(const gl::Framebuffer *framebuffer,
+                                    const gl::BlendState &blendState,
+                                    const gl::ColorF &blendColor,
+                                    unsigned int sampleMask,
+                                    const gl::State::DirtyBits &dirtyBits);
+
+    virtual gl::Error setDepthStencilState(const gl::DepthStencilState &depthStencilState,
+                                           int stencilRef,
+                                           int stencilBackRef,
+                                           bool frontFaceCCW,
+                                           const gl::State::DirtyBits &dirtyBits);
+
+    virtual gl::Error setRasterizerState(const gl::RasterizerState &rasterizerState,
+                                         const gl::State::DirtyBits &dirtyBits);
+
+    virtual void setCurStencilSize(unsigned int size);
+
+  private:
+    static const D3DRENDERSTATETYPE D3DRS_CCW_STENCILREF       = D3DRS_STENCILREF;
+    static const D3DRENDERSTATETYPE D3DRS_CCW_STENCILMASK      = D3DRS_STENCILMASK;
+    static const D3DRENDERSTATETYPE D3DRS_CCW_STENCILWRITEMASK = D3DRS_STENCILWRITEMASK;
+
+    VendorID getVendorId() const;
+
+    // Blend state setting functions
+    void setDepthMask(bool depthMask);
+    void setDepthTestAndFunc(bool depthTest, GLenum depthFunc);
+    void setStencilTestEnabled(bool stencilTest);
+    void setStencilFuncsFront(GLenum stencilFunc,
+                              GLuint stencilMask,
+                              int stencilRef,
+                              bool frontFaceCCW);
+    void setStencilFuncsBack(GLenum stencilBackFunc,
+                             GLuint stencilBackMask,
+                             int stencilBackRef,
+                             bool frontFaceCCW);
+
+    void setStencilWriteMaskFront(GLuint stencilWritemask, bool frontFaceCCW);
+    void setStencilWriteMaskBack(GLuint stencilBackWritemask, bool frontFaceCCW);
+    void setStencilOpsFront(GLenum stencilFail,
+                            GLenum stencilPassDepthFail,
+                            GLenum stencilPassDepthPass,
+                            bool frontFaceCCW);
+    void setStencilOpsBack(GLenum stencilBackFail,
+                           GLenum stencilBackPassDepthFail,
+                           GLenum stencilBackPassDepthPass,
+                           bool frontFaceCCW);
+
+    // Depth stencil state setting functions
+    void setBlendEnableFuncsEquations(const gl::BlendState &blendState,
+                                      const gl::ColorF &blendColor);
+    void setBlendColor(const gl::ColorF &blendColor, const gl::BlendState &blendState);
+    void setBlendEnabled(bool blendEnabled);
+    void setSampleAlphaToCoverageEnabled(bool sampleAlphaToCoverage);
+    void setDitherEnabled(bool ditherEnabled);
+    void setBlendColorMask(const gl::BlendState &blendState, const gl::Framebuffer *framebuffer);
+    void setSampleMask(unsigned int sampleMask);
+
+    // Rasterizer state setting functions
+    void setRasterizerMode(bool cullFace, GLenum cullMode, GLenum frontFace);
+    void setRasterizerPolygonOffset(bool polygonOffsetFill,
+                                    GLfloat polygonOffsetFactor,
+                                    GLfloat polygonOffsetUnits);
+
+    IDirect3DDevice9 *mDevice;
+    D3DADAPTER_IDENTIFIER9 &mAdapterIdentifier;
+
+    unsigned int mMaxStencil;
+};
+}
+#endif
