40031539dd75be160fdb57fb10715b05c1e9297c
Jacek Caban <cjacek@gmail.com>
2015-10-05 16:03:10 +0200

    Fixed compilation on mingw.
    Change-Id: I5734f789f25fbc6b9b90f2043459a3a5f72008c4

--- include/platform/Platform.h
+++ include/platform/Platform.h
@@ -134,6 +134,8 @@ class Platform
 
 }
 
+extern "C" {
+
 typedef void(*ANGLEPlatformInitializeFunc)(angle::Platform*);
 ANGLE_EXPORT void ANGLEPlatformInitialize(angle::Platform*);
 
@@ -142,5 +144,7 @@ ANGLE_EXPORT void ANGLEPlatformShutdown();
 
 typedef angle::Platform *(*ANGLEPlatformCurrentFunc)();
 ANGLE_EXPORT angle::Platform *ANGLEPlatformCurrent();
+
+}
 
 #endif // ANGLE_PLATFORM_H
