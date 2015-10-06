0a7abd40bf1fab3311198bbcdd532250e27d3ff7
Geoff Lang <geofflang@chromium.org>
2015-10-06 10:25:14 -0400

    asdf
    Change-Id: I87674ee5fd6a643c751ba6efd94e29e987d0738a

--- src/libANGLE/renderer/d3d/d3d11/Renderer11.h
+++ src/libANGLE/renderer/d3d/d3d11/Renderer11.h
@@ -307,7 +307,7 @@ class Renderer11 : public RendererD3D
 
     HMODULE mD3d11Module;
     HMODULE mDxgiModule;
-    std::vector<D3D_FEATURE_LEVEL> mAvailableFeatureLevels;
+    std::vect    or<D3D_FEATURE_LEVEL> mAvailableFeatureLevels;
     D3D_DRIVER_TYPE mDriverType;
 
     HLSLCompiler mCompiler;
