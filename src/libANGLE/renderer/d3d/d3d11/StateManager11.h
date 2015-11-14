3710019ebc4238d65a3a1fa8cc38db1ad15168bf
Dian Xiang <dianx@google.com>
2015-11-13 16:39:53 -0800

    Using dirty bit notification for depth stencil state
    BUG=angleproject:1161
    
    This is a continuation of the dirty bit refactor
    
    Change-Id: If7d523e6200fef631051cab0bde47fdec8385c17

--- src/libANGLE/renderer/d3d/d3d11/StateManager11.h
+++ src/libANGLE/renderer/d3d/d3d11/StateManager11.h
@@ -33,18 +33,32 @@ class StateManager11 final : angle::NonCopyable
                             const gl::ColorF &blendColor,
                             unsigned int sampleMask);
 
+    gl::Error setDepthStencilState(const gl::DepthStencilState &depthStencilState,
+                                   int stencilRef,
+                                   int stencilBackRef);
+
     void forceSetBlendState() { mForceSetBlendState = true; }
-    void setBlendStateIsDirty() { mBlendStateIsDirty = true; }
+    void forceSetDepthStencilState() { mForceSetDepthStencilState = true; }
+
+    void updateStencilSizeIfChanged(bool depthStencilInitialized, unsigned int stencilSize);
 
   private:
     // Blend State
-    bool mBlendStateIsDirty;
+    bool mForceSetBlendState;
     // TODO(dianx) temporary representation of a dirty bit. once we move enough states in,
     // try experimenting with dirty bit instead of a bool
-    bool mForceSetBlendState;
+    bool mBlendStateIsDirty;
     gl::BlendState mCurBlendState;
     gl::ColorF mCurBlendColor;
     unsigned int mCurSampleMask;
+
+    // Currently applied depth stencil state
+    bool mForceSetDepthStencilState;
+    bool mDepthStencilStateIsDirty;
+    gl::DepthStencilState mCurDepthStencilState;
+    int mCurStencilRef;
+    int mCurStencilBackRef;
+    unsigned int mCurStencilSize;
 
     Renderer11 *mRenderer11;
 };
