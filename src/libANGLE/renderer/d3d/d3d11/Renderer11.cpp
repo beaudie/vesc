147adb90b3fecf3b84e19346c0659a91560fdca0
Dian Xiang <dianx@google.com>
2015-11-16 19:19:34 -0800

    Using dirty bit notification for rasterizer state
    BUG=angleproject:1161
    
    This is a continuation of the dirty bit refactor
    
    Change-Id: I78ab046aaeb7de6dd288433c86e6524dcb2d6a66

--- src/libANGLE/renderer/d3d/d3d11/Renderer11.cpp
+++ src/libANGLE/renderer/d3d/d3d11/Renderer11.cpp
@@ -11,7 +11,6 @@
 #include <EGL/eglext.h>
 #include <sstream>
 
-#include "common/BitSetIterator.h"
 #include "common/tls.h"
 #include "common/utilities.h"
 #include "libANGLE/Buffer.h"
@@ -471,7 +470,7 @@ void Renderer11::SRVCache::clear()
 Renderer11::Renderer11(egl::Display *display)
     : RendererD3D(display),
       mStateCache(this),
-      mStateManager(this),
+      mStateManager(),
       mLastHistogramUpdateTime(ANGLEPlatformCurrent()->monotonicallyIncreasingTime()),
       mDebug(nullptr)
 {
@@ -833,6 +832,8 @@ void Renderer11::initializeDevice()
 
     ASSERT(!mPixelTransfer);
     mPixelTransfer = new PixelTransfer11(this);
+
+    mStateManager.initialize(mDeviceContext, &mStateCache);
 
     const gl::Caps &rendererCaps = getRendererCaps();
 
@@ -1360,23 +1361,7 @@ gl::Error Renderer11::setUniformBuffers(const gl::Data &data,
 
 gl::Error Renderer11::setRasterizerState(const gl::RasterizerState &rasterState)
 {
-    if (mForceSetRasterState || memcmp(&rasterState, &mCurRasterState, sizeof(gl::RasterizerState)) != 0)
-    {
-        ID3D11RasterizerState *dxRasterState = NULL;
-        gl::Error error = mStateCache.getRasterizerState(rasterState, mScissorEnabled, &dxRasterState);
-        if (error.isError())
-        {
-            return error;
-        }
-
-        mDeviceContext->RSSetState(dxRasterState);
-
-        mCurRasterState = rasterState;
-    }
-
-    mForceSetRasterState = false;
-
-    return gl::Error(GL_NO_ERROR);
+    return mStateManager.setRasterizerState(rasterState);
 }
 
 void Renderer11::syncState(const gl::State &state, const gl::State::DirtyBits &bitmask)
@@ -1416,11 +1401,12 @@ void Renderer11::setScissorRectangle(const gl::Rectangle &scissor, bool enabled)
 
         if (enabled != mScissorEnabled)
         {
-            mForceSetRasterState = true;
+            mStateManager.forceSetRasterState();
         }
 
         mCurScissor = scissor;
         mScissorEnabled = enabled;
+        mStateManager.setCurScissorEnabled(mScissorEnabled);
     }
 
     mForceSetScissor = false;
@@ -1690,7 +1676,7 @@ gl::Error Renderer11::applyRenderTarget(const gl::Framebuffer *framebuffer)
 
         if (!mDepthStencilInitialized)
         {
-            mForceSetRasterState = true;
+            mStateManager.forceSetRasterState();
         }
 
         for (size_t rtIndex = 0; rtIndex < ArraySize(framebufferRTVs); rtIndex++)
@@ -2472,7 +2458,7 @@ void Renderer11::markAllStateDirty()
 
     mStateManager.forceSetBlendState();
     mStateManager.forceSetDepthStencilState();
-    mForceSetRasterState = true;
+    mStateManager.forceSetRasterState();
     mForceSetScissor = true;
     mForceSetViewport = true;
 
