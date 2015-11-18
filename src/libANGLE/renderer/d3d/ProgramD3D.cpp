b7bec0ff68c94c23a00acd9bd696a271b1b55c50
Olli Etuaho <oetuaho@nvidia.com>
2015-11-18 13:45:19 +0200

    D3D: Run applyTextures before applyShaders
    applyShaders will update uniform data by uploading the constant buffer.
    In the future uniform data will contain the base level of textures so
    that shaders can use it to adjust results of texture functions. The most
    natural place to set the base level in uniform data is applyTextures, so
    it needs to be run before applyShaders.
    
    updateSamplerMapping needs to be called before applyTextures, but
    genericDraw* functions already do that. Remove redundant
    updateSamplerMapping call from ProgramD3D::applyUniforms.
    
    BUG=angleproject:596
    BUG=angleproject:1092
    TEST=angle_end2end_tests
    
    Change-Id: I65bb3d47dafef719854a5a70d8a828980dc13096

--- src/libANGLE/renderer/d3d/ProgramD3D.cpp
+++ src/libANGLE/renderer/d3d/ProgramD3D.cpp
@@ -1409,7 +1409,7 @@ void ProgramD3D::initializeUniformStorage()
 
 gl::Error ProgramD3D::applyUniforms(GLenum drawMode)
 {
-    updateSamplerMapping();
+    ASSERT(!mDirtySamplerMapping);
 
     gl::Error error = mRenderer->applyUniforms(*this, drawMode, mD3DUniforms);
     if (error.isError())
