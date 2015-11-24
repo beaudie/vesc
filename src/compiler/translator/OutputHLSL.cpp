585e5b7e4cae28462da445e3697b8d6edff877c8
Jamie Madill <jmadill@chromium.org>
2015-11-24 13:00:12 -0500

    Note that we need to figure out wrap modes for integer texures.
    Change-Id: Ic7e89a111728dfb18821534996bf5b9a5ad172b6

--- src/compiler/translator/OutputHLSL.cpp
+++ src/compiler/translator/OutputHLSL.cpp
@@ -1130,6 +1130,7 @@ void OutputHLSL::header(const BuiltInFunctionEmulator *builtInFunctionEmulator)
                 // Convert from normalized floating-point to integer
                 if (textureFunction->method != TextureFunction::FETCH)
                 {
+                    // TODO(jmadill): Figure out how to handle wrap modes here.
                     addressx = "int(floor(width * frac((";
                     addressy = "int(floor(height * frac((";
 
