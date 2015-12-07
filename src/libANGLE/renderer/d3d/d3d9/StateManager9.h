2857580a839af836c8dee14e94ceb67b53a241d0
Dian Xiang <dianx@google.com>
2015-12-07 13:15:10 -0800

    Using dirty bits notification for D3D9 blend state
    BUG=angleproject:1249
    
    Dirty bit notifications are used in GL and D3D11 for state tracking. This
    is a continuation of D3D dirty bit refactor for D3D9
    
    Change-Id: Ifaa1826a93cf36c83d68150107b164d1d269c2b0

---
+++ src/libANGLE/renderer/d3d/d3d9/StateManager9.h
@@ -0,0 +1,77 @@
+//
+// Copyright (c) 2015 The ANGLE Project Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+
+// StateManager9.h: Defines a class for caching D3D9 state
+
+#include "libANGLE/angletypes.h"
+#include "libANGLE/Data.h"
+#include "libANGLE/State.h"
+
+namespace rx
+{
+
+class StateManager9 final : angle::NonCopyable
+{
+  public:
+    StateManager9();
+    ~StateManager9();
+
+    void initialize(IDirect3DDevice9 *device, D3DADAPTER_IDENTIFIER9 *mAdapterIdentifier);
+
+    void syncState(const gl::State &state, const gl::State::DirtyBits &dirtyBits);
+
+    gl::Error setBlendState(const gl::Framebuffer *framebuffer,
+                            const gl::BlendState &blendState,
+                            const gl::ColorF &blendColor,
+                            unsigned int sampleMask);
+
+    void forceSetBlendState();
+    void resetDirtyBits() { mDirtyBits.reset(); }
+    VendorID getVendorId() const;
+
+  private:
+    void setBlendEnabled(bool enabled);
+    void setBlendColor(const gl::BlendState &blendState, const gl::ColorF &blendColor);
+
+    void setBlendFuncsEquations(const gl::BlendState &blendState);
+
+    void setColorMask(const gl::Framebuffer *framebuffer,
+                      bool red,
+                      bool blue,
+                      bool green,
+                      bool alpha);
+
+    void setSampleAlphaToCoverage(bool enabled);
+
+    void setDither(bool dither);
+
+    void setSampleMask(unsigned int sampleMask);
+
+    enum DirtyBitType
+    {
+        DIRTY_BIT_BLEND_ENABLED,
+        DIRTY_BIT_BLEND_COLOR,
+        DIRTY_BIT_BLEND_FUNCS_EQUATIONS,
+        DIRTY_BIT_SAMPLE_ALPHA_TO_COVERAGE,
+        DIRTY_BIT_COLOR_MASK,
+        DIRTY_BIT_DITHER,
+        DIRTY_BIT_SAMPLE_MASK,
+
+        DIRTY_BIT_MAX
+    };
+
+    typedef std::bitset<DIRTY_BIT_MAX> DirtyBits;
+
+    // Currently applied blend state
+    gl::BlendState mCurBlendState;
+    gl::ColorF mCurBlendColor;
+    unsigned int mCurSampleMask;
+
+    IDirect3DDevice9 *mDevice;
+    D3DADAPTER_IDENTIFIER9 *mAdapterIdentifier;
+    DirtyBits mDirtyBits;
+};
+}
