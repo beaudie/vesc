37281a18c8a7e01f2ed9bf32a00d174ea06e857b
Dian Xiang <dianx@google.com>
2015-12-07 20:15:28 -0800

    Using dirty bits notification for D3D9 depth stencil state
    BUG=angleproject:1249
    
    This is a continuation of the D3D dirty bit refactor for D3D9 for
    performance enhancements
    
    Change-Id: I8690d47999b73483c47f4994dc46cd97f4ced63d

--- src/libANGLE/renderer/d3d/d3d9/StateManager9.h
+++ src/libANGLE/renderer/d3d/d3d9/StateManager9.h
@@ -23,30 +23,30 @@ class StateManager9 final : angle::NonCopyable
 
     void syncState(const gl::State &state, const gl::State::DirtyBits &dirtyBits);
 
-    gl::Error setBlendAndRasterizerState(const gl::State &glState, unsigned int sampleMask);
+    gl::Error setBlendDepthRasterStates(const gl::State &glState, unsigned int sampleMask);
+
     void forceSetBlendState();
     void forceSetRasterState();
+    void forceSetDepthStencilState();
 
     void updateDepthSizeIfChanged(bool depthStencilInitialized, unsigned int depthSize);
+    void updateStencilSizeIfChanged(bool depthStencilInitialized, unsigned int stencilSize);
+
     void resetDirtyBits() { mDirtyBits.reset(); }
     VendorID getVendorId() const;
 
   private:
+    // Blend state functions
     void setBlendEnabled(bool enabled);
     void setBlendColor(const gl::BlendState &blendState, const gl::ColorF &blendColor);
-
     void setBlendFuncsEquations(const gl::BlendState &blendState);
-
     void setColorMask(const gl::Framebuffer *framebuffer,
                       bool red,
                       bool blue,
                       bool green,
                       bool alpha);
-
     void setSampleAlphaToCoverage(bool enabled);
-
     void setDither(bool dither);
-
     void setSampleMask(unsigned int sampleMask);
 
     // Current raster state functions
@@ -55,8 +55,34 @@ class StateManager9 final : angle::NonCopyable
                               GLfloat polygonOffsetFactor,
                               GLfloat polygonOffsetUnits);
 
+    // Depth stencil state functions
+    void setStencilOpsFront(GLenum stencilFail,
+                            GLenum stencilPassDepthFail,
+                            GLenum stencilPassDepthPass,
+                            bool frontFaceCCW);
+    void setStencilOpsBack(GLenum stencilBackFail,
+                           GLenum stencilBackPassDepthFail,
+                           GLenum stencilBackPassDepthPass,
+                           bool frontFaceCCW);
+    void setStencilBackWriteMask(GLuint stencilBackWriteMask, bool frontFaceCCW);
+    void setDepthFunc(bool depthTest, GLenum depthFunc);
+    void setStencilTestEnabled(bool enabled);
+    void setDepthMask(bool depthMask);
+    void setStencilFuncsFront(GLenum stencilFunc,
+                              GLuint stencilMask,
+                              GLint stencilRef,
+                              bool frontFaceCCW,
+                              unsigned int maxStencil);
+    void setStencilFuncsBack(GLenum stencilBackFunc,
+                             GLuint stencilBackMask,
+                             GLint stencilBackRef,
+                             bool frontFaceCCW,
+                             unsigned int maxStencil);
+    void setStencilWriteMask(GLuint stencilWriteMask, bool frontFaceCCW);
+
     enum DirtyBitType
     {
+        // Blend dirty bits
         DIRTY_BIT_BLEND_ENABLED,
         DIRTY_BIT_BLEND_COLOR,
         DIRTY_BIT_BLEND_FUNCS_EQUATIONS,
@@ -65,8 +91,20 @@ class StateManager9 final : angle::NonCopyable
         DIRTY_BIT_DITHER,
         DIRTY_BIT_SAMPLE_MASK,
 
+        // Rasterizer dirty bits
         DIRTY_BIT_RASTER_CULL_MODE,
         DIRTY_BIT_RASTER_POLYGON_OFFSET,
+
+        // Depth stencil dirty bits
+        DIRTY_BIT_STENCIL_DEPTH_MASK,
+        DIRTY_BIT_STENCIL_DEPTH_FUNC,
+        DIRTY_BIT_STENCIL_TEST_ENABLED,
+        DIRTY_BIT_STENCIL_FUNCS_FRONT,
+        DIRTY_BIT_STENCIL_FUNCS_BACK,
+        DIRTY_BIT_STENCIL_WRITEMASK_FRONT,
+        DIRTY_BIT_STENCIL_WRITEMASK_BACK,
+        DIRTY_BIT_STENCIL_OPS_FRONT,
+        DIRTY_BIT_STENCIL_OPS_BACK,
 
         DIRTY_BIT_MAX
     };
@@ -78,8 +116,21 @@ class StateManager9 final : angle::NonCopyable
     gl::ColorF mCurBlendColor;
     unsigned int mCurSampleMask;
 
+    // Currently applied raster state
     gl::RasterizerState mCurRasterState;
     unsigned int mCurDepthSize;
+
+    // Currently applied depth stencil state
+    gl::DepthStencilState mCurDepthStencilState;
+    int mCurStencilRef;
+    int mCurStencilBackRef;
+    bool mCurFrontFaceCCW;
+    unsigned int mCurStencilSize;
+
+    // FIXME: Unsupported by D3D9
+    static const D3DRENDERSTATETYPE D3DRS_CCW_STENCILREF       = D3DRS_STENCILREF;
+    static const D3DRENDERSTATETYPE D3DRS_CCW_STENCILMASK      = D3DRS_STENCILMASK;
+    static const D3DRENDERSTATETYPE D3DRS_CCW_STENCILWRITEMASK = D3DRS_STENCILWRITEMASK;
 
     IDirect3DDevice9 *mDevice;
     D3DADAPTER_IDENTIFIER9 *mAdapterIdentifier;
