1c86b1fffaefef015dcdf0c0cf5890a47e6122ed
Austin Kinross <aukinros@microsoft.com>
2015-12-18 14:01:46 -0800

    Implement EGL_experimental_present_path_angle
    This allows ANGLE to render directly onto a D3D swapchain in the correct
    orientation when using the D3D11 renderer.
    
    The trick is to add an extra uniform to each shader which takes either the
    value +1.0 or -1.0. When rendering to a texture, ANGLE sets this value to -1.0.
    When rendering to the default framebuffer, ANGLE sets this value to +1.0.
    ANGLE multiplies vertex positions by this value in the VS to invert rendering
    when appropriate. It also corrects other state (e.g. viewport/scissor rect)
    and shader built-in values (e.g. gl_FragCoord).
    
    This saves a substantial amount of GPU time and lowers power consumption. For
    example, the old method (where ANGLE renders all content onto an offscreen
    texture, and then copies/inverts this onto the swapchain at eglSwapBuffers()
    time) uses about 20% of the GPU each frame on a Lumia 630.
    
    Verification:
    + dEQP GL ES2 tests pass when "present path fast" is enabled
    + all but one ANGLE_end2end_tests pass (it renders a GL_POINT one pixel off the
      expected location)
    
    BUG=angleproject:1219
    
    Change-Id: Ib6eeea46bafa6ebce4adada0ae9db3a433b8fc4c

--- src/libGLESv2/entry_points_egl_ext.cpp
+++ src/libGLESv2/entry_points_egl_ext.cpp
@@ -163,6 +163,7 @@ EGLDisplay EGLAPIENTRY GetPlatformDisplayEXT(EGLenum platform, void *native_disp
         bool minorVersionSpecified   = false;
         bool enableAutoTrimSpecified = false;
         bool deviceTypeSpecified     = false;
+        bool presentPathFastEnabled  = false;
 
         if (attrib_list)
         {
@@ -228,6 +229,21 @@ EGLDisplay EGLAPIENTRY GetPlatformDisplayEXT(EGLenum platform, void *native_disp
                     enableAutoTrimSpecified = true;
                     break;
 
+                    case EGL_EXPERIMENTAL_PRESENT_PATH_ANGLE:
+                        switch (curAttrib[1])
+                        {
+                            case EGL_EXPERIMENTAL_PRESENT_PATH_FAST_ANGLE:
+                            case EGL_EXPERIMENTAL_PRESENT_PATH_COPY_ANGLE:
+                                break;
+                            default:
+                                SetGlobalError(
+                                    Error(EGL_BAD_ATTRIBUTE,
+                                          "Invalid value for EGL_EXPERIMENTAL_PRESENT_PATH_ANGLE"));
+                                return EGL_NO_DISPLAY;
+                        }
+                        presentPathFastEnabled = (curAttrib[1] == EGL_TRUE);
+                        break;
+
                     case EGL_PLATFORM_ANGLE_DEVICE_TYPE_ANGLE:
                         switch (curAttrib[1])
                         {
@@ -278,6 +294,15 @@ EGLDisplay EGLAPIENTRY GetPlatformDisplayEXT(EGLenum platform, void *native_disp
             SetGlobalError(
                 Error(EGL_BAD_ATTRIBUTE,
                       "EGL_PLATFORM_ANGLE_ENABLE_AUTOMATIC_TRIM_ANGLE requires a device type of "
+                      "EGL_PLATFORM_ANGLE_TYPE_D3D11_ANGLE."));
+            return EGL_NO_DISPLAY;
+        }
+
+        if (presentPathFastEnabled && platformType != EGL_PLATFORM_ANGLE_TYPE_D3D11_ANGLE)
+        {
+            SetGlobalError(
+                Error(EGL_BAD_ATTRIBUTE,
+                      "EGL_EXPERIMENTAL_PRESENT_PATH_FAST_ANGLE requires a device type of "
                       "EGL_PLATFORM_ANGLE_TYPE_D3D11_ANGLE."));
             return EGL_NO_DISPLAY;
         }
