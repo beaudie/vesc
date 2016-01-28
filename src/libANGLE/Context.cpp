5dd1c464de221f4f5dc121b6fb5fb583bf289534
Ian Ewell <ewell@google.com>
2016-01-28 17:36:55 -0500

    Finish basic timer query support in GL backend
    Support is mostly complete with the webGL test for the extension passing
    in Canary with some flakiness in the timestamp test which I could not
    duplicate inside ANGLE. I suspect the bug is within Chromium and will look
    into it later. Since the extension is mostly done, it is now enabled by
    default so new builds of Canary should have support for that extension in
    WebGL in OpenGL with experimental WebGL 2.0 enabled.
    
    Change-Id: If018b7e3ae84aff7e40c73ff8e672a86689ae6c4

--- src/libANGLE/Context.cpp
+++ src/libANGLE/Context.cpp
@@ -1081,6 +1081,11 @@ void Context::getIntegerv(GLenum pname, GLint *params)
           *params = mExtensions.maxLabelLength;
           break;
 
+      // GL_EXT_disjoint_timer_query
+      case GL_GPU_DISJOINT_EXT:
+          *params = mRenderer->getGPUDisjoint();
+          break;
+
       default:
         mState.getIntegerv(getData(), pname, params);
         break;
@@ -1108,6 +1113,11 @@ void Context::getInteger64v(GLenum pname, GLint64 *params)
       case GL_MAX_SERVER_WAIT_TIMEOUT:
         *params = mCaps.maxServerWaitTimeout;
         break;
+
+      // GL_EXT_disjoint_timer_query
+      case GL_TIMESTAMP_EXT:
+          *params = mRenderer->getTimestamp();
+          break;
       default:
         UNREACHABLE();
         break;
@@ -1328,6 +1338,22 @@ bool Context::getQueryParameterInfo(GLenum pname, GLenum *type, unsigned int *nu
         *type = GL_FLOAT;
         *numParams = 1;
         return true;
+      case GL_TIMESTAMP_EXT:
+          if (!mExtensions.disjointTimerQuery)
+          {
+              return false;
+          }
+          *type      = GL_INT_64_ANGLEX;
+          *numParams = 1;
+          return true;
+      case GL_GPU_DISJOINT_EXT:
+          if (!mExtensions.disjointTimerQuery)
+          {
+              return false;
+          }
+          *type      = GL_INT;
+          *numParams = 1;
+          return true;
     }
 
     if (mExtensions.debug)
