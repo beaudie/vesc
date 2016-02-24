db94ae4d6ce4acdd54305f94e876a4084635b54b
Olli Etuaho <oetuaho@nvidia.com>
2016-02-24 14:45:06 +0200

    Remove getDXGIFormat() function from RenderTarget11
    This query would return either the RTV format, the DSV format or the
    texture format depending on the render target. This made the code hard
    to understand.
    
    getDXGIFormat() calls are replaced by querying the ANGLE format, and
    explicitly choosing which associated DXGI format to query info for
    (RTV format or DSV format).
    
    This refactoring makes changing some format associations for integer
    texture formats easier in the future.
    
    BUG=angleproject:1244
    TEST=angle_end2end_tests,
         dEQP-GLES3.functional.fbo.blit.* (no regressions)
    
    Change-Id: Ibe3c03fc6b7768af1a131d4df3909a1e20a71228

--- src/libANGLE/renderer/d3d/d3d11/Renderer11.cpp
+++ src/libANGLE/renderer/d3d/d3d11/Renderer11.cpp
@@ -3947,7 +3947,16 @@ gl::Error Renderer11::blitRenderbufferRect(const gl::Rectangle &readRectIn,
 
     const auto &destFormatInfo = gl::GetInternalFormatInfo(drawRenderTarget->getInternalFormat());
     const auto &srcFormatInfo  = gl::GetInternalFormatInfo(readRenderTarget->getInternalFormat());
-    const auto &dxgiFormatInfo = d3d11::GetDXGIFormatInfo(drawRenderTarget11->getDXGIFormat());
+    DXGI_FORMAT drawDXGIFormat = DXGI_FORMAT_UNKNOWN;
+    if (colorBlit)
+    {
+        drawDXGIFormat = d3d11::GetANGLEFormatSet(drawRenderTarget11->getANGLEFormat()).rtvFormat;
+    }
+    else
+    {
+        drawDXGIFormat = d3d11::GetANGLEFormatSet(drawRenderTarget11->getANGLEFormat()).dsvFormat;
+    }
+    const auto &dxgiFormatInfo = d3d11::GetDXGIFormatInfo(drawDXGIFormat);
 
     // Some blits require masking off emulated texture channels. eg: from RGBA8 to RGB8, we
     // emulate RGB8 with RGBA8, so we need to mask off the alpha channel when we copy.
@@ -3985,7 +3994,7 @@ gl::Error Renderer11::blitRenderbufferRect(const gl::Rectangle &readRectIn,
 
     gl::Error result(GL_NO_ERROR);
 
-    if (readRenderTarget11->getDXGIFormat() == drawRenderTarget11->getDXGIFormat() &&
+    if (readRenderTarget11->getANGLEFormat() == drawRenderTarget11->getANGLEFormat() &&
         !stretchRequired && !outOfBounds && !flipRequired && !partialDSBlit &&
         !colorMaskingNeeded && (!(depthBlit || stencilBlit) || wholeBufferCopy))
     {
