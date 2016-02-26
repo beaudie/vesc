524a1ff2d6da1497c933a5f31d0a68f17c7e615c
Olli Etuaho <oetuaho@nvidia.com>
2016-02-26 14:05:12 +0200

    D3D11: Check packPixels memcpy support based on ANGLEFormat
    Instead of querying DXGI format info to determine whether framebuffer
    data can be copied to packPixels target memory with memcpy, compare
    the internal format information from the ANGLE format to the packing
    format. This makes it possible to change some of the DXGI formats
    used for integer textures without breaking packPixels.
    
    This makes it possible to entirely remove the DXGI format to GL
    internal format mapping from the DXGI format info table.
    
    BUG=angleproject:1244
    TEST=angle_end2end_tests,
         dEQP-GLES3.functional.fbo.* (no regressions),
         dEQP-GLES3.functional.pbo.* (no regressions),
    
    Change-Id: Id5ffd581349e7afe39872d69cb05381f3e78d352

--- src/libANGLE/renderer/d3d/d3d11/Renderer11.cpp
+++ src/libANGLE/renderer/d3d/d3d11/Renderer11.cpp
@@ -3761,8 +3761,9 @@ gl::Error Renderer11::packPixels(const TextureHelper11 &textureHelper,
         inputPitch = static_cast<int>(mapping.RowPitch);
     }
 
-    const d3d11::DXGIFormat &dxgiFormatInfo    = d3d11::GetDXGIFormatInfo(textureHelper.getFormat());
-    const gl::InternalFormat &sourceFormatInfo = gl::GetInternalFormatInfo(dxgiFormatInfo.internalFormat);
+    const auto &angleFormatInfo = d3d11::GetANGLEFormatSet(params.angleFormat);
+    const gl::InternalFormat &sourceFormatInfo =
+        gl::GetInternalFormatInfo(angleFormatInfo.glInternalFormat);
     if (sourceFormatInfo.format == params.format && sourceFormatInfo.type == params.type)
     {
         uint8_t *dest = pixelsOut + params.offset;
@@ -3773,6 +3774,8 @@ gl::Error Renderer11::packPixels(const TextureHelper11 &textureHelper,
     }
     else
     {
+        const d3d11::DXGIFormat &dxgiFormatInfo =
+            d3d11::GetDXGIFormatInfo(textureHelper.getFormat());
         ColorCopyFunction fastCopyFunc =
             dxgiFormatInfo.getFastCopyFunction(params.format, params.type);
         GLenum sizedDestInternalFormat = gl::GetSizedInternalFormat(params.format, params.type);
@@ -3794,7 +3797,6 @@ gl::Error Renderer11::packPixels(const TextureHelper11 &textureHelper,
         }
         else
         {
-            const auto &angleFormatInfo           = d3d11::GetANGLEFormatSet(params.angleFormat);
             ColorReadFunction colorReadFunction   = angleFormatInfo.colorReadFunction;
             ColorWriteFunction colorWriteFunction = GetColorWriteFunction(params.format, params.type);
 
