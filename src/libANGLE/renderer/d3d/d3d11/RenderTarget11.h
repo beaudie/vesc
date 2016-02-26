cbf78943836360e83a6124187cc34027a0135f59
Olli Etuaho <oetuaho@nvidia.com>
2016-02-26 14:37:57 +0200

    D3D11: Use blit SRV format for blits
    blitSRVFormat stores the format that is used with ANGLE's internal
    blit shaders. By default, it is the same as the normal SRV format.
    For integer textures with a red channel, the RTV format is used
    instead. This makes it possible to change the storage format and the
    SRV format for the integer textures without affecting the blit format.
    
    The blitSRVFormat is used when doing blits in Blit11::copyTexture().
    
    An exception is made for depth/stencil renderbuffer blit - in these cases
    it is okay to assume that the regular SRV format works for blitting.
    
    In the future the regular SRV format for integer textures will be changed
    to be different from their blit SRV format.
    
    TEST=angle_end2end_tests
         dEQP-GLES3.functional.fbo.blit.* (no regression)
         dEQP-GLES3.functional.texture.swizzle.* (no regression)
    BUG=angleproject:1244
    
    Change-Id: Ie0e790e58ec054b64ef5983a09dbfc7754f269ca

--- src/libANGLE/renderer/d3d/d3d11/RenderTarget11.h
+++ src/libANGLE/renderer/d3d/d3d11/RenderTarget11.h
@@ -30,6 +30,7 @@ class RenderTarget11 : public RenderTargetD3D
     virtual ID3D11RenderTargetView *getRenderTargetView() const = 0;
     virtual ID3D11DepthStencilView *getDepthStencilView() const = 0;
     virtual ID3D11ShaderResourceView *getShaderResourceView() const = 0;
+    virtual ID3D11ShaderResourceView *getBlitShaderResourceView() const = 0;
 
     virtual unsigned int getSubresourceIndex() const = 0;
 
@@ -52,6 +53,7 @@ class TextureRenderTarget11 : public RenderTarget11
     TextureRenderTarget11(ID3D11RenderTargetView *rtv,
                           ID3D11Resource *resource,
                           ID3D11ShaderResourceView *srv,
+                          ID3D11ShaderResourceView *blitSRV,
                           GLenum internalFormat,
                           d3d11::ANGLEFormat angleFormat,
                           GLsizei width,
@@ -79,6 +81,7 @@ class TextureRenderTarget11 : public RenderTarget11
     ID3D11RenderTargetView *getRenderTargetView() const override;
     ID3D11DepthStencilView *getDepthStencilView() const override;
     ID3D11ShaderResourceView *getShaderResourceView() const override;
+    ID3D11ShaderResourceView *getBlitShaderResourceView() const override;
 
     unsigned int getSubresourceIndex() const override;
 
@@ -94,6 +97,10 @@ class TextureRenderTarget11 : public RenderTarget11
     ID3D11RenderTargetView *mRenderTarget;
     ID3D11DepthStencilView *mDepthStencil;
     ID3D11ShaderResourceView *mShaderResource;
+
+    // Shader resource view to use with internal blit shaders. Not set for depth/stencil render
+    // targets.
+    ID3D11ShaderResourceView *mBlitShaderResource;
 };
 
 class SurfaceRenderTarget11 : public RenderTarget11
@@ -112,6 +119,7 @@ class SurfaceRenderTarget11 : public RenderTarget11
     ID3D11RenderTargetView *getRenderTargetView() const override;
     ID3D11DepthStencilView *getDepthStencilView() const override;
     ID3D11ShaderResourceView *getShaderResourceView() const override;
+    ID3D11ShaderResourceView *getBlitShaderResourceView() const override;
 
     unsigned int getSubresourceIndex() const override;
 
