deb525dead60a4a743c551d417f076db5f6ccc78
Ian Ewell <ewell@google.com>
2016-03-02 15:26:39 -0500

    Add support for EGL_KHR_stream.
    EGL_KHR_stream is now implemented. Since the extension does not come with
    any producers or consumers, it does not have much functionality and the
    implementation is therefore very simple (validation layers and a new
    object to store some attributes). This however add the groundwork to add
    the appropriate consumer and producer extensions to stream D3D NV12
    textures directly into ANGLE which will significantly improve video
    performance on Chromium on D3D-based platforms.
    
    BUG=angleproject:1332
    
    Change-Id: Ie240c73869f5098d1215cc5e27aa5decd06c3ed1

---
+++ src/libANGLE/Stream.cpp
@@ -0,0 +1,80 @@
+//
+// Copyright (c) 2016 The ANGLE Project Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+
+// Stream.cpp: Implements the egl::Stream class, representing the stream
+// where frames are streamed in. Implements EGLStreanKHR.
+
+#include "libANGLE/Stream.h"
+
+#include <platform/Platform.h>
+#include <EGL/eglext.h>
+
+#include "common/debug.h"
+#include "common/mathutil.h"
+#include "common/platform.h"
+#include "common/utilities.h"
+#include "libANGLE/Context.h"
+
+namespace egl
+{
+
+Stream::Stream(const AttributeMap &attribs)
+    : mState(EGL_STREAM_STATE_CREATED_KHR), mProducerFrame(0), mConsumerFrame(0)
+{
+    mConsumerLatency = attribs.get(EGL_CONSUMER_LATENCY_USEC_KHR, 0);
+}
+
+Stream::~Stream()
+{
+}
+
+void Stream::streamAttribute(EGLint attribute, EGLint value)
+{
+    switch (attribute)
+    {
+        case EGL_CONSUMER_LATENCY_USEC_KHR:
+            mConsumerLatency = value;
+            break;
+        default:
+            UNREACHABLE();
+            break;
+    }
+}
+
+EGLint Stream::queryStream(EGLint attribute)
+{
+    switch (attribute)
+    {
+        case EGL_STREAM_STATE_KHR:
+            return mState;
+        case EGL_CONSUMER_LATENCY_USEC_KHR:
+            return mConsumerLatency;
+        default:
+            UNREACHABLE();
+            return 0;
+    }
+}
+
+EGLuint64KHR Stream::queryStreamu64(EGLint attribute)
+{
+    switch (attribute)
+    {
+        case EGL_PRODUCER_FRAME_KHR:
+            return mProducerFrame;
+        case EGL_CONSUMER_FRAME_KHR:
+            return mConsumerFrame;
+        default:
+            UNREACHABLE();
+            return 0;
+    }
+}
+
+EGLenum Stream::getState() const
+{
+    return mState;
+}
+
+} // namespace egl
--- src/libANGLE/validationEGL.cpp
+++ src/libANGLE/validationEGL.cpp
@@ -14,6 +14,7 @@
 #include "libANGLE/Device.h"
 #include "libANGLE/Display.h"
 #include "libANGLE/Image.h"
+#include "libANGLE/Stream.h"
 #include "libANGLE/Surface.h"
 
 #include <EGL/eglext.h>
@@ -1050,6 +1051,182 @@ Error ValidateReleaseDeviceANGLE(Device *device)
     if (owningDisplay != nullptr)
     {
         return Error(EGL_BAD_DEVICE_EXT, "Device must have been created using eglCreateDevice");
+    }
+
+    return Error(EGL_SUCCESS);
+}
+
+Error ValidateCreateStreamKHR(const Display *display, const AttributeMap &attributes)
+{
+    Error error = ValidateDisplay(display);
+    if (error.isError())
+    {
+        return error;
+    }
+
+    const DisplayExtensions &displayExtensions = display->getExtensions();
+    if (!displayExtensions.stream)
+    {
+        return Error(EGL_BAD_ALLOC, "Stream extension not active");
+    }
+
+    for (const auto &attributeIter : attributes)
+    {
+        EGLint attribute = attributeIter.first;
+        EGLint value     = attributeIter.second;
+
+        switch (attribute)
+        {
+            case EGL_STREAM_STATE_KHR:
+            case EGL_PRODUCER_FRAME_KHR:
+            case EGL_CONSUMER_FRAME_KHR:
+                return Error(EGL_BAD_ACCESS, "Attempt to initialize readonly parameter");
+            case EGL_CONSUMER_LATENCY_USEC_KHR:
+                // Technically not in spec but a latency < 0 makes no sense so we check it
+                if (value < 0)
+                {
+                    return Error(EGL_BAD_PARAMETER, "Latency must be positive");
+                }
+                break;
+            default:
+                return Error(EGL_BAD_ATTRIBUTE, "Invalid stream attribute");
+        }
+    }
+
+    return Error(EGL_SUCCESS);
+}
+
+Error ValidateDestroyStreamKHR(const Display *display, const Stream *stream)
+{
+    Error error = ValidateDisplay(display);
+    if (error.isError())
+    {
+        return error;
+    }
+
+    const DisplayExtensions &displayExtensions = display->getExtensions();
+    if (!displayExtensions.stream)
+    {
+        return Error(EGL_BAD_ACCESS, "Stream extension not active");
+    }
+
+    if (stream == EGL_NO_STREAM_KHR || !display->isValidStream(stream))
+    {
+        return Error(EGL_BAD_STREAM_KHR, "Invalid stream");
+    }
+
+    return Error(EGL_SUCCESS);
+}
+
+Error ValidateStreamAttribKHR(const Display *display,
+                              const Stream *stream,
+                              EGLint attribute,
+                              EGLint value)
+{
+    Error error = ValidateDisplay(display);
+    if (error.isError())
+    {
+        return error;
+    }
+
+    const DisplayExtensions &displayExtensions = display->getExtensions();
+    if (!displayExtensions.stream)
+    {
+        return Error(EGL_BAD_ACCESS, "Stream extension not active");
+    }
+
+    if (stream == EGL_NO_STREAM_KHR || !display->isValidStream(stream))
+    {
+        return Error(EGL_BAD_STREAM_KHR, "Invalid stream");
+    }
+
+    if (stream->getState() == EGL_STREAM_STATE_DISCONNECTED_KHR)
+    {
+        return Error(EGL_BAD_STATE_KHR, "Bad stream state");
+    }
+
+    switch (attribute)
+    {
+        case EGL_STREAM_STATE_KHR:
+        case EGL_PRODUCER_FRAME_KHR:
+        case EGL_CONSUMER_FRAME_KHR:
+            return Error(EGL_BAD_ACCESS, "Attribute is read only");
+        case EGL_CONSUMER_LATENCY_USEC_KHR:
+            if (value < 0)
+            {
+                return Error(EGL_BAD_PARAMETER, "Stream consumer latency must be positive");
+            }
+            break;
+        default:
+            return Error(EGL_BAD_ATTRIBUTE, "Invalid attribute");
+    }
+
+    return Error(EGL_SUCCESS);
+}
+
+Error ValidateQueryStreamKHR(const Display *display,
+                             const Stream *stream,
+                             EGLenum attribute,
+                             EGLint *value)
+{
+    Error error = ValidateDisplay(display);
+    if (error.isError())
+    {
+        return error;
+    }
+
+    const DisplayExtensions &displayExtensions = display->getExtensions();
+    if (!displayExtensions.stream)
+    {
+        return Error(EGL_BAD_ACCESS, "Stream extension not active");
+    }
+
+    if (stream == EGL_NO_STREAM_KHR || !display->isValidStream(stream))
+    {
+        return Error(EGL_BAD_STREAM_KHR, "Invalid stream");
+    }
+
+    switch (attribute)
+    {
+        case EGL_STREAM_STATE_KHR:
+        case EGL_CONSUMER_LATENCY_USEC_KHR:
+            break;
+        default:
+            return Error(EGL_BAD_ATTRIBUTE, "Invalid attribute");
+    }
+
+    return Error(EGL_SUCCESS);
+}
+
+Error ValidateQueryStreamu64KHR(const Display *display,
+                                const Stream *stream,
+                                EGLenum attribute,
+                                EGLuint64KHR *value)
+{
+    Error error = ValidateDisplay(display);
+    if (error.isError())
+    {
+        return error;
+    }
+
+    const DisplayExtensions &displayExtensions = display->getExtensions();
+    if (!displayExtensions.stream)
+    {
+        return Error(EGL_BAD_ACCESS, "Stream extension not active");
+    }
+
+    if (stream == EGL_NO_STREAM_KHR || !display->isValidStream(stream))
+    {
+        return Error(EGL_BAD_STREAM_KHR, "Invalid stream");
+    }
+
+    switch (attribute)
+    {
+        case EGL_CONSUMER_FRAME_KHR:
+        case EGL_PRODUCER_FRAME_KHR:
+            break;
+        default:
+            return Error(EGL_BAD_ATTRIBUTE, "Invalid attribute");
     }
 
     return Error(EGL_SUCCESS);
