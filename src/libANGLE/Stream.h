04fe4b3c2f3d5ff661d6b8eca7f8e64201893b63
Ian Ewell <ewell@google.com>
2016-03-02 15:26:39 -0500

    Add support for EGL_KHR_stream.
    EGL_KHR_stream is now implemented. Since the extension does not come with
    any producers or consumers, it does not have much functionality and the
    implementation is therefore very simple (validation layers and a new
    object to store some attributes). This however add the groundwork to add
    the appropriate consumer and producer extensions to stream D3D NV12
    textures directly into ANGLE which will significantly improve video
    performance on Chromium on D3D-based platforms.
    
    BUG=angleproject:1332
    
    Change-Id: Ie240c73869f5098d1215cc5e27aa5decd06c3ed1

---
+++ src/libANGLE/Stream.h
@@ -0,0 +1,57 @@
+//
+// Copyright (c) 2016 The ANGLE Project Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+
+// Stream.h: Defines the egl::Stream class, representing the stream
+// where frames are streamed in. Implements EGLStreanKHR.
+
+#ifndef LIBANGLE_STREAM_H_
+#define LIBANGLE_STREAM_H_
+
+#include <EGL/egl.h>
+#include <EGL/eglext.h>
+
+#include <set>
+#include <vector>
+
+#include "libANGLE/Error.h"
+#include "libANGLE/Caps.h"
+#include "libANGLE/Config.h"
+#include "libANGLE/AttributeMap.h"
+#include "libANGLE/renderer/Renderer.h"
+
+namespace gl
+{
+class Context;
+}
+
+namespace egl
+{
+class Device;
+class Image;
+class Surface;
+class Display;
+
+class Stream final : angle::NonCopyable
+{
+  public:
+    Stream(const AttributeMap &attribs);
+    ~Stream();
+
+    Error streamAttribute(EGLint attribute, EGLint value);
+
+    EGLint queryStream(EGLint attribute);
+    EGLuint64KHR queryStreamu64(EGLint attribute);
+
+  private:
+    // EGL defined attributes
+    EGLint mState;
+    EGLuint64KHR mProducerFrame;
+    EGLuint64KHR mConsumerFrame;
+    EGLint mConsumerLatency;
+};
+}
+
+#endif  // LIBANGLE_STREAM_H_
