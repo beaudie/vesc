35c2f8c7bfca78da260e9cff5d14fbdeb2541005
Ian Ewell <ewell@google.com>
2016-03-10 13:47:21 -0500

    Add initial support for various stream extensions.
    Add entry points and validation for various egl stream extensions
    including EGL_KHR_stream_consumer_gltexture and
    EGL_NV_stream_consumer_gltexture_yuv and NV_EGL_stream_consumer_external.
    The extensions functionality is not yet implemented and the extension
    strings are thus not exposed yet.
    
    BUG=angleproject:1332
    Change-Id: I115d872557db38d8dd94cc367038668406719109

--- src/libANGLE/Stream.cpp
+++ src/libANGLE/Stream.cpp
@@ -27,9 +27,9 @@ Stream::Stream(rx::StreamImpl *impl, const AttributeMap &attribs)
       mState(EGL_STREAM_STATE_CREATED_KHR),
       mProducerFrame(0),
       mConsumerFrame(0),
-      mConsumerLatency(attribs.get(EGL_CONSUMER_LATENCY_USEC_KHR, 0))
+      mConsumerLatency(attribs.get(EGL_CONSUMER_LATENCY_USEC_KHR, 0)),
+      mConsumerAcquireTimeout(attribs.get(EGL_CONSUMER_ACQUIRE_TIMEOUT_USEC_KHR, 0))
 {
-
 }
 
 Stream::~Stream()
@@ -60,6 +60,16 @@ EGLuint64KHR Stream::getConsumerFrame() const
 EGLenum Stream::getState() const
 {
     return mState;
+}
+
+void Stream::setConsumerAcquireTimeout(EGLint timeout)
+{
+    mConsumerAcquireTimeout = timeout;
+}
+
+EGLint Stream::getConsumerAcquireTimeout() const
+{
+    return mConsumerAcquireTimeout;
 }
 
 }  // namespace egl
