8e30cbb32a4173c9e95624d88f4d29d5e8287035
Corentin Wallez <cwallez@chromium.org>
2016-03-29 21:58:33 -0400

    Suppress end2end failures for the new Linux Intel bot.
    Since this is a new bot, we should make it green ASAP so we can catch
    any new regressions.
    
    BUG=angleproject:1346
    
    Change-Id: I2c1288b8115a9d7980d5a3f37753b35cbb837486

--- src/tests/gl_tests/StateChangeTest.cpp
+++ src/tests/gl_tests/StateChangeTest.cpp
@@ -290,6 +290,15 @@ class StateChangeRenderTest : public StateChangeTest
 // Test that re-creating a currently attached texture works as expected.
 TEST_P(StateChangeRenderTest, RecreateTexture)
 {
+#if defined(ANGLE_PLATFORM_LINUX)
+    if (IsIntel())
+    {
+        // TODO(cwallez): Fix on Linux Intel drivers (http://anglebug.com/1346)
+        std::cout << "Test disabled on Linux Intel OpenGL." << std::endl;
+        return;
+    }
+#endif
+
     glBindFramebuffer(GL_FRAMEBUFFER, mFramebuffer);
 
     glBindTexture(GL_TEXTURE_2D, mTextures[0]);
