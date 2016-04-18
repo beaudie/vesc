73e1d80299cb0722fb7150e076def6b7dcdade1f
Geoff Lang <geofflang@chromium.org>
2016-04-18 15:39:32 -0400

    Implement CHROMIUM_copy_texture and CHROMIUM_copy_compressed_texture.
    BUG=angleproject:1356
    
    Change-Id: I70246762411dbeeb3e291e317854139a68d80070

--- src/libANGLE/renderer/d3d/d3d11/renderer11_utils.cpp
+++ src/libANGLE/renderer/d3d/d3d11/renderer11_utils.cpp
@@ -1234,6 +1234,8 @@ void GenerateCaps(ID3D11Device *device, ID3D11DeviceContext *deviceContext, cons
     extensions->vertexArrayObject        = true;
     extensions->noError                  = true;
     extensions->lossyETCDecode           = true;
+    extensions->copyTexture              = true;
+    extensions->copyCompressedTexture    = true;
 
     // D3D11 Feature Level 10_0+ uses SV_IsFrontFace in HLSL to emulate gl_FrontFacing.
     // D3D11 Feature Level 9_3 doesn't support SV_IsFrontFace, and has no equivalent, so can't support gl_FrontFacing.
