6e2f935abf34b740f39dc60b497dd2fb145e513f
Geoff Lang <geofflang@chromium.org>
2016-04-20 11:13:22 -0700

    Implement CHROMIUM_copy_compressed_texture for D3D11.
    BUG=angleproject:1356
    
    Change-Id: Id563997d2921cf558c52a781ae66d8bde58d1f2f

--- src/libANGLE/Context.h
+++ src/libANGLE/Context.h
@@ -481,6 +481,7 @@ class Context final : public ValidationContext
                                 GLboolean unpackFlipY,
                                 GLboolean unpackPremultiplyAlpha,
                                 GLboolean unpackUnmultiplyAlpha);
+    void compressedCopyTextureCHROMIUM(GLuint sourceId, GLuint destId);
 
     void generateMipmap(GLenum target);
 
