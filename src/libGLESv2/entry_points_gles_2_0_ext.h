2f092316127cf4f828adedc68fd3438fa3522daa
Geoff Lang <geofflang@chromium.org>
2016-04-20 10:42:34 -0700

    Implement CHROMIUM_copy_texture for D3D11.
    BUG=angleproject:1356
    
    Change-Id: I70246762411dbeeb3e291e317854139a68d80070

--- src/libGLESv2/entry_points_gles_2_0_ext.h
+++ src/libGLESv2/entry_points_gles_2_0_ext.h
@@ -179,6 +179,27 @@ ANGLE_EXPORT void GL_APIENTRY StencilThenCoverStrokePathCHROMIUM(GLuint path,
                                                                  GLint reference,
                                                                  GLuint mask,
                                                                  GLenum coverMode);
+
+// GL_CHROMIUM_copy_texture
+ANGLE_EXPORT void GL_APIENTRY CopyTextureCHROMIUM(GLuint sourceId,
+                                                  GLuint destId,
+                                                  GLint internalFormat,
+                                                  GLenum destType,
+                                                  GLboolean unpackFlipY,
+                                                  GLboolean unpackPremultiplyAlpha,
+                                                  GLboolean unpackUnmultiplyAlpha);
+
+ANGLE_EXPORT void GL_APIENTRY CopySubTextureCHROMIUM(GLuint sourceId,
+                                                     GLuint destId,
+                                                     GLint xoffset,
+                                                     GLint yoffset,
+                                                     GLint x,
+                                                     GLint y,
+                                                     GLsizei width,
+                                                     GLsizei height,
+                                                     GLboolean unpackFlipY,
+                                                     GLboolean unpackPremultiplyAlpha,
+                                                     GLboolean unpackUnmultiplyAlpha);
 }
 
 #endif // LIBGLESV2_ENTRYPOINTGLES20EXT_H_
