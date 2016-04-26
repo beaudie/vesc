60fcd86879ae5c45f55e9fbf0c3a690c1ca057e6
Jamie Madill <jmadill@chromium.org>
2016-04-26 13:41:34 -0400

    Rename gl::Program::Data to gl::ProgramState.
    BUG=angleproject:1363
    
    Change-Id: Iccbc085e29bbcb6b53a22c89d486ebfc4e898c32

--- src/libANGLE/renderer/d3d/d3d9/Renderer9.h
+++ src/libANGLE/renderer/d3d/d3d9/Renderer9.h
@@ -190,7 +190,7 @@ class Renderer9 : public RendererD3D
 
     // Shader creation
     ShaderImpl *createShader(const gl::Shader::Data &data) override;
-    ProgramImpl *createProgram(const gl::Program::Data &data) override;
+    ProgramImpl *createProgram(const gl::ProgramState &data) override;
 
     // Shader operations
     gl::Error loadExecutable(const void *function,
