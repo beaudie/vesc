d87ce8927f2bb3020f5a4c2af4ebecce74514e25
Olli Etuaho <oetuaho@nvidia.com>
2016-05-13 16:15:35 +0300

    Fix GenerateMipmap when base level or max level are set
    According to GLES 3.0.4 section 3.8.10, GenerateMipmap should generate
    levels based on the base level, and generate them at most up to the
    max level. Levels outside the base/max level range should be unchanged
    by GenerateMipmap.
    
    The Texture class is fixed so that the image descs are set only for
    the changed mipmap range when GenerateMipmap is called.
    
    The D3D backend is fixed so that mipmap generation is correctly
    started from the base level instead of level 0, and making sure that
    mipmaps are generated only up to the max level. Generating mipmaps for
    array textures is also fixed for cases where the base level depth >=
    max(width, height) * 2.
    
    The GL backend is fixed to sync texture state before GenerateMipmap is
    called, so that base level and max level are set correctly in the
    driver.
    
    The GenerateMipmap entry point is refactored so that it has a separate
    validation function and a context function which does the work.
    Validation for out-of-range base levels is added.
    
    New tests are added to verify the functionality. One corner case in
    the tests fails on NVIDIA GL drivers likely due to a driver bug -
    similar rules for GenerateMipmap are found from newer GLES specs and
    also OpenGL specs (checked versions 3.3 and 4.4).
    
    BUG=angleproject:596
    TEST=angle_end2end_tests
    
    Change-Id: Ifc7b4126281967fc4f6dc4f9452e5b01e39f83d7

--- src/libANGLE/Context.cpp
+++ src/libANGLE/Context.cpp
@@ -2628,6 +2628,12 @@ void Context::compressedTexSubImage3D(GLenum target,
                                                imageSize, reinterpret_cast<const uint8_t *>(data)));
 }
 
+void Context::generateMipmap(GLenum target)
+{
+    Texture *texture = getTargetTexture(target);
+    handleError(texture->generateMipmaps());
+}
+
 void Context::getBufferPointerv(GLenum target, GLenum /*pname*/, void **params)
 {
     Buffer *buffer = getState().getTargetBuffer(target);
