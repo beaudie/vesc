cd66d40840fcf67c136c0a9f6ce3e4e5af46f9d3
Jamie Madill <jmadill@chromium.org>
2016-05-17 13:43:17 -0400

    Make Platform errors trigger test failures.
    This can be useful in the Vulkan back-end to make validation layer
    errors cause test cases to fail.
    
    BUG=angleproject:1319
    
    Change-Id: I523f3c874e892a2646600e4c5c554319ed8d770c

--- src/tests/test_utils/ANGLETest.h
+++ src/tests/test_utils/ANGLETest.h
@@ -18,6 +18,7 @@
 #include "angle_test_configs.h"
 #include "common/angleutils.h"
 #include "shader_utils.h"
+#include "system_utils.h"
 #include "Vector.h"
 
 #define EXPECT_GL_ERROR(err) EXPECT_EQ(static_cast<GLenum>(err), glGetError())
@@ -164,6 +165,7 @@ class ANGLETest : public ::testing::TestWithParam<angle::PlatformParameters>
     EGLint getPlatformRenderer() const;
 
     void ignoreD3D11SDKLayersWarnings();
+    void ignoreANGLEPlatformMessages();
 
   private:
     bool createEGLContext();
@@ -186,8 +188,12 @@ class ANGLETest : public ::testing::TestWithParam<angle::PlatformParameters>
 class ANGLETestEnvironment : public testing::Environment
 {
   public:
-    virtual void SetUp();
-    virtual void TearDown();
+    void SetUp() override;
+    void TearDown() override;
+
+  private:
+    // For loading and freeing platform
+    std::unique_ptr<angle::Library> mGLESLibrary;
 };
 
 bool IsIntel();
