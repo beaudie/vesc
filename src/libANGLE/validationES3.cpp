56435de29d5f5b7307e813b92a5068649e0737bf
Jamie Madill <jmadill@chromium.org>
2016-06-02 13:04:11 -0400

    Pass depth to computeBlockSize.
    This was very wrong for 3D textures.
    
    BUG=angleproject:1384
    
    Change-Id: I7f042449e30e1e909778c0524d1ce99d20ddfd65

--- src/libANGLE/validationES3.cpp
+++ src/libANGLE/validationES3.cpp
@@ -490,7 +490,7 @@ bool ValidateES3TexImageParametersBase(Context *context,
         }
 
         const gl::InternalFormat &formatInfo = gl::GetInternalFormatInfo(sizedFormat);
-        auto copyBytesOrErr                  = formatInfo.computeBlockSize(type, width, height);
+        auto copyBytesOrErr = formatInfo.computeBlockSize(type, gl::Extents(width, height, depth));
         if (copyBytesOrErr.isError())
         {
             context->handleError(copyBytesOrErr.getError());
@@ -1557,7 +1557,8 @@ bool ValidateCompressedTexImage3D(Context *context,
     }
 
     const InternalFormat &formatInfo = GetInternalFormatInfo(internalformat);
-    auto blockSizeOrErr              = formatInfo.computeBlockSize(GL_UNSIGNED_BYTE, width, height);
+    auto blockSizeOrErr =
+        formatInfo.computeBlockSize(GL_UNSIGNED_BYTE, gl::Extents(width, height, depth));
     if (blockSizeOrErr.isError())
     {
         context->handleError(blockSizeOrErr.getError());
@@ -1905,7 +1906,8 @@ bool ValidateCompressedTexSubImage3D(Context *context,
     }
 
     const InternalFormat &formatInfo = GetInternalFormatInfo(format);
-    auto blockSizeOrErr              = formatInfo.computeBlockSize(GL_UNSIGNED_BYTE, width, height);
+    auto blockSizeOrErr =
+        formatInfo.computeBlockSize(GL_UNSIGNED_BYTE, gl::Extents(width, height, depth));
     if (blockSizeOrErr.isError())
     {
         context->handleError(blockSizeOrErr.getError());
