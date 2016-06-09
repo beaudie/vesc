da60956d46c8d1a450da716171ae9fc68d5630a4
Jamie Madill <jmadill@chromium.org>
2016-06-09 07:09:26 -0700

    Make Framebuffer::checkStatus non-const.
    The goal of this refactor is two-fold: it forces the validation layer
    to call through to the ValidationContext to mutate the Framebuffer,
    to keep the layering intact as best as possible. It also will allow
    us to update a state cache in the Framebuffer (without using bogus
    'mutable') when we support dirty bits updates to the framebuffer
    completeness, coming in a subsequent CL.
    
    BUG=angleproject:1388
    
    Change-Id: Ia9befba0c915c087f2fe0557d91060256f6d4950

--- src/libANGLE/renderer/d3d/d3d11/Renderer11.cpp
+++ src/libANGLE/renderer/d3d/d3d11/Renderer11.cpp
@@ -1499,7 +1499,8 @@ gl::Error Renderer11::updateState(const gl::ContextState &data, GLenum drawMode)
     // Applies the render target surface, depth stencil surface, viewport rectangle and
     // scissor rectangle to the renderer
     const gl::Framebuffer *framebufferObject = glState.getDrawFramebuffer();
-    ASSERT(framebufferObject && framebufferObject->checkStatus(data) == GL_FRAMEBUFFER_COMPLETE);
+    ASSERT(framebufferObject &&
+           framebufferObject->getCachedStatus(data) == GL_FRAMEBUFFER_COMPLETE);
     ANGLE_TRY(applyRenderTarget(framebufferObject));
 
     // Set the present path state
@@ -1516,7 +1517,7 @@ gl::Error Renderer11::updateState(const gl::ContextState &data, GLenum drawMode)
     mStateManager.setScissorRectangle(glState.getScissor(), glState.isScissorTestEnabled());
 
     // Applying rasterizer state to D3D11 device
-    int samples                    = framebufferObject->getSamples(data);
+    int samples                    = framebufferObject->getCachedSamples(data);
     gl::RasterizerState rasterizer = glState.getRasterizerState();
     rasterizer.pointDrawMode       = (drawMode == GL_POINTS);
     rasterizer.multiSample         = (samples != 0);
