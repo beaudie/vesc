3b405ff004e1e36adbeec7ecd146b2e47a7b3dad
Martin Radev <mradev@nvidia.com>
2016-07-22 15:27:42 +0300

    Change grammar to support features from es31
    The grammar has been changed so that ES31 grammar is followed more
    closely. The ES31 grammar is not fully supported, only functionality
    related to qualifier enumeration is added.
    
    The ParseContext is changed so that type qualifiers can be now joined
    together (i.e. like layout qualifiers). This will allow enumeration of
    multiple storage qualifiers (i.e. uniform readonly coherent) which is
    essential for support of ES31 features.
    
    Some of the error checks had to be moved closer to the root of the
    parse tree since some of the information about the expression might be
    missing.
    
    Unfortunately, as there is no explicit ordering imposed by the
    grammar, additional checks for proper order of qualifiers had to be
    added. I also included unit tests which test against malformed
    shaders.
    
    BUG=angleproject:1442
    
    TEST=angle_end2end_tests
    TEST=angle_unittests
    TEST=dEQP-GLES3.functional.shaders.*precision*
    TEST=dEQP-GLES3.functional.shaders.*function*
    TEST=dEQP-GLES2.functional.shaders.*
    
    Change-Id: Ib3653a1ed1bfced099a6b2cbf35a7cd480c9100d

--- src/compiler/translator/util.h
+++ src/compiler/translator/util.h
@@ -65,6 +65,7 @@ class GetVariableTraverser : angle::NonCopyable
     const TSymbolTable &mSymbolTable;
 };
 
+bool IsBuiltinOutputVariable(TQualifier qualifier);
 }
 
 #endif // COMPILER_TRANSLATOR_UTIL_H_
