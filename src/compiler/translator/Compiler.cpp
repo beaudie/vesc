9f1bc35e5337937f0da0bf59eb2ae492d1435387
Qiankun Miao <qiankun.miao@intel.com>
2016-08-05 17:58:40 +0800

    Invariant qualifier should not leak across shaders
    Change-Id: I2a35899bffd28767b8bc638f415857636d4ad8ef

--- src/compiler/translator/Compiler.cpp
+++ src/compiler/translator/Compiler.cpp
@@ -206,6 +206,7 @@ TIntermNode *TCompiler::compileTreeImpl(const char *const shaderStrings[],
                                         const int compileOptions)
 {
     clearResults();
+    symbolTable.clearInvariantVaryings();
 
     ASSERT(numStrings > 0);
     ASSERT(GetGlobalPoolAllocator());
