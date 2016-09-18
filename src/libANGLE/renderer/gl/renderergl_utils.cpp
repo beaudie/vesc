55ccc294302a6915fc410b2cb30be4cc35afdae2
Qin Jiajia <jiajia.qin@intel.com>
2016-09-18 13:55:13 +0800

    Add workaround for unused std140 and shared uniform blocks on MacOS
    On some Mac drivers with shader version 4.1, they will
    treat unused std140 and shared uniform blocks' members as inactive. However,
    WebGL2.0 based on OpenGL ES3.0.4 requires all members of a named uniform block
    declared with a shared or std140 layout qualifier to be considered active.
    The uniform block itself is also considered active.
    
    This workaround is to reference all members of unused std140 and shared uniform blocks
    at the beginning of the vertex/fragment shader's main().
    
    BUG=chromium:618464
    TEST=UniformBufferTest.ActiveUniformBlockNumber
    
    Change-Id: I1d2c5e3e8da04786ac6a37fd26f7bb9c14cd76ed

--- src/libANGLE/renderer/gl/renderergl_utils.cpp
+++ src/libANGLE/renderer/gl/renderergl_utils.cpp
@@ -891,6 +891,7 @@ void GenerateWorkarounds(const FunctionsGL *functions, WorkaroundsGL *workaround
 
 #if defined(ANGLE_PLATFORM_APPLE)
     workarounds->doWhileGLSLCausesGPUHang = true;
+    workarounds->useUnusedBlocksWithStandardOrSharedLayout = true;
 #endif
 
     workarounds->finishDoesNotCauseQueriesToBeAvailable =
