f314139aa3ce74a0483645a019e9b845fed938b8
Stanislav Chiknavaryan <stanisc@chromium.org>
2016-09-27 13:28:25 -0700

    Implementation of eglGetSyncValuesCHROMIUM extension.
    This change adds implementation of eglGetSyncValuesCHROMIUM extension
    on D3D11 with Direct Composition. This should work on Windows 8.1 and
    above.
    
    The implementation is based on IDXGISwapChain::GetFrameStatistics.
    
    Extension documentation:
    https://chromium.googlesource.com/chromium/src/gpu/+/master/GLES2/extensions/CHROMIUM/EGL_CHROMIUM_get_sync_values.txt
    
    BUG=angleproject:1402
    
    Change-Id: I306434dd8d85d618b14edfa38fc2a22e50fddacc

--- src/libANGLE/renderer/gl/SurfaceGL.h
+++ src/libANGLE/renderer/gl/SurfaceGL.h
@@ -29,6 +29,7 @@ class SurfaceGL : public SurfaceImpl
     }
 
     FramebufferImpl *createDefaultFramebuffer(const gl::FramebufferState &data) override;
+    egl::Error getSyncValues(EGLuint64KHR *ust, EGLuint64KHR *msc, EGLuint64KHR *sbc) override;
 
     virtual egl::Error makeCurrent() = 0;
 
