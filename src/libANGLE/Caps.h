e699d5d3a5c5ddd39552eb26eff6a4958d84bde1
Jamie Madill <jmadill@chromium.org>
2016-10-26 17:47:28 -0400

    Vulkan: Add display creation, test and extension.
    With this CL we have the ability to create Vulkan test configs and run
    basic tests, although the only thing that works is creating a Vulkan
    Renderer using the extension.
    
    BUG=angleproject:1319
    
    Change-Id: I8ad17bba01241334be7da16e68fea38762ca6a20

--- src/libANGLE/Caps.h
+++ src/libANGLE/Caps.h
@@ -654,6 +654,9 @@ struct ClientExtensions
     // EGL_ANGLE_platform_angle_null
     bool platformANGLENULL;
 
+    // EGL_ANGLE_platform_angle_vulkan
+    bool platformANGLEVulkan;
+
     // EGL_ANGLE_device_creation
     bool deviceCreation;
 
@@ -670,6 +673,6 @@ struct ClientExtensions
     bool clientGetAllProcAddresses;
 };
 
-}
+}  // namespace egl
 
 #endif // LIBANGLE_CAPS_H_
