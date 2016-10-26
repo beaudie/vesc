cceef2a6f7c48d412095c2180128de8d0190e4d0
Jamie Madill <jmadill@chromium.org>
2016-10-26 12:44:02 -0400

    D3D11: Make several format tables constexpr.
    This should guarantee the best memory access patterns.
    
    BUG=angleproject:1389
    
    Change-Id: I5e2c8c530a07798494afd3ea36b6164d7564c02c

--- src/libANGLE/renderer/d3d/d3d11/texture_format_table.h
+++ src/libANGLE/renderer/d3d/d3d11/texture_format_table.h
@@ -32,19 +32,21 @@ namespace d3d11
 // DSVs given a GL internal format.
 struct Format final : angle::NonCopyable
 {
-    Format();
-    Format(GLenum internalFormat,
-           angle::Format::ID formatID,
-           DXGI_FORMAT texFormat,
-           DXGI_FORMAT srvFormat,
-           DXGI_FORMAT rtvFormat,
-           DXGI_FORMAT dsvFormat,
-           DXGI_FORMAT blitSRVFormat,
-           GLenum swizzleFormat,
-           InitializeTextureDataFunction internalFormatInitializer,
-           const Renderer11DeviceCaps &deviceCaps);
+    constexpr Format();
+    constexpr Format(GLenum internalFormat,
+                     angle::Format::ID formatID,
+                     DXGI_FORMAT texFormat,
+                     DXGI_FORMAT srvFormat,
+                     DXGI_FORMAT rtvFormat,
+                     DXGI_FORMAT dsvFormat,
+                     DXGI_FORMAT blitSRVFormat,
+                     GLenum swizzleFormat,
+                     InitializeTextureDataFunction internalFormatInitializer);
 
     static const Format &Get(GLenum internalFormat, const Renderer11DeviceCaps &deviceCaps);
+
+    const Format &getSwizzleFormat(const Renderer11DeviceCaps &deviceCaps) const;
+    LoadFunctionMap getLoadFunctions() const;
 
     GLenum internalFormat;
     const angle::Format &format;
@@ -56,12 +58,44 @@ struct Format final : angle::NonCopyable
 
     DXGI_FORMAT blitSRVFormat;
 
-    const Format &swizzle;
+    GLenum swizzleFormat;
 
     InitializeTextureDataFunction dataInitializerFunction;
-
-    LoadFunctionMap loadFunctions;
 };
+
+constexpr Format::Format()
+    : internalFormat(GL_NONE),
+      format(angle::Format::Get(angle::Format::ID::NONE)),
+      texFormat(DXGI_FORMAT_UNKNOWN),
+      srvFormat(DXGI_FORMAT_UNKNOWN),
+      rtvFormat(DXGI_FORMAT_UNKNOWN),
+      dsvFormat(DXGI_FORMAT_UNKNOWN),
+      blitSRVFormat(DXGI_FORMAT_UNKNOWN),
+      swizzleFormat(GL_NONE),
+      dataInitializerFunction(nullptr)
+{
+}
+
+constexpr Format::Format(GLenum internalFormat,
+                         angle::Format::ID formatID,
+                         DXGI_FORMAT texFormat,
+                         DXGI_FORMAT srvFormat,
+                         DXGI_FORMAT rtvFormat,
+                         DXGI_FORMAT dsvFormat,
+                         DXGI_FORMAT blitSRVFormat,
+                         GLenum swizzleFormat,
+                         InitializeTextureDataFunction internalFormatInitializer)
+    : internalFormat(internalFormat),
+      format(angle::Format::Get(formatID)),
+      texFormat(texFormat),
+      srvFormat(srvFormat),
+      rtvFormat(rtvFormat),
+      dsvFormat(dsvFormat),
+      blitSRVFormat(blitSRVFormat),
+      swizzleFormat(swizzleFormat),
+      dataInitializerFunction(internalFormatInitializer)
+{
+}
 
 }  // namespace d3d11
 
