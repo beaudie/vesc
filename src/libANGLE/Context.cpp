22847d3b116682e941381a2ab8cb08e9588a7ebf
Geoff Lang <geofflang@chromium.org>
2016-11-01 10:08:02 -0400

    Properly check for debug and robust access context creation flags.
    These flags can come from both attributes and bitfield flags.
    
    Also move the robust access check to an UNIMPLEMENTED in Context, dEQP ES 3.1
    tests attempt to use this flag and it's convenient to be able test by disabling
    UNIMPLEMENTED assertions.
    
    BUG=angleproject:1442
    
    Change-Id: I1641fdb3afee308235351ebd60be680765b16b3b

--- src/libANGLE/Context.cpp
+++ src/libANGLE/Context.cpp
@@ -167,12 +167,15 @@ GLenum GetResetStrategy(const egl::AttributeMap &attribs)
 
 bool GetRobustAccess(const egl::AttributeMap &attribs)
 {
-    return (attribs.get(EGL_CONTEXT_OPENGL_ROBUST_ACCESS_EXT, EGL_FALSE) == EGL_TRUE);
+    return (attribs.get(EGL_CONTEXT_OPENGL_ROBUST_ACCESS_EXT, EGL_FALSE) == EGL_TRUE) ||
+           ((attribs.get(EGL_CONTEXT_FLAGS_KHR, 0) & EGL_CONTEXT_OPENGL_ROBUST_ACCESS_BIT_KHR) !=
+            0);
 }
 
 bool GetDebug(const egl::AttributeMap &attribs)
 {
-    return (attribs.get(EGL_CONTEXT_OPENGL_DEBUG, EGL_FALSE) == EGL_TRUE);
+    return (attribs.get(EGL_CONTEXT_OPENGL_DEBUG, EGL_FALSE) == EGL_TRUE) ||
+           ((attribs.get(EGL_CONTEXT_FLAGS_KHR, 0) & EGL_CONTEXT_OPENGL_DEBUG_BIT_KHR) != 0);
 }
 
 bool GetNoError(const egl::AttributeMap &attribs)
@@ -254,7 +257,10 @@ Context::Context(rx::EGLImplFactory *implFactory,
       mCurrentSurface(nullptr),
       mResourceManager(nullptr)
 {
-    ASSERT(!mRobustAccess);  // Unimplemented
+    if (mRobustAccess)
+    {
+        UNIMPLEMENTED();
+    }
 
     initCaps(GetWebGLContext(attribs));
     initWorkarounds();
