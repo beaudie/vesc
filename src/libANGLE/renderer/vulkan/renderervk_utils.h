380a9898e25effa9088c8b805c6b4a180c12b9ae
Jamie Madill <jmadill@chromium.org>
2016-11-03 16:45:33 -0400

    Vulkan: Configure a simple RenderPass for drawing.
    BUG=angleproject:1319
    
    Change-Id: I0668527a338a2f10ca74b43f2a8bd82534e732c0

--- src/libANGLE/renderer/vulkan/renderervk_utils.h
+++ src/libANGLE/renderer/vulkan/renderervk_utils.h
@@ -39,6 +39,7 @@ enum class TextureDimension
 namespace vk
 {
 class Framebuffer;
+class RenderPass;
 
 class Error final
 {
@@ -107,6 +108,12 @@ class CommandBuffer final : public WrappedObject<VkCommandBuffer>
                            VkImageLayout oldLayout,
                            VkImageLayout newLayout);
 
+    void beginRenderPass(const RenderPass &renderPass,
+                         const Framebuffer &framebuffer,
+                         const gl::Rectangle &renderArea,
+                         const std::vector<VkClearValue> &clearValues);
+    void endRenderPass();
+
     void imageBarrier(VkPipelineStageFlags srcStageMask,
                       VkPipelineStageFlags destStageMask,
                       const VkImageMemoryBarrier &barrier);
@@ -162,6 +169,18 @@ class DeviceMemory final : public WrappedObject<VkDeviceMemory>
     Error allocate(const VkMemoryAllocateInfo &allocInfo);
     Error map(VkDeviceSize offset, VkDeviceSize size, VkMemoryMapFlags flags, uint8_t **mapPointer);
     void unmap();
+};
+
+class RenderPass final : public WrappedObject<VkRenderPass>
+{
+  public:
+    RenderPass();
+    RenderPass(VkDevice device);
+    RenderPass(RenderPass &&other);
+    ~RenderPass();
+    RenderPass &operator=(RenderPass &&other);
+
+    Error init(const VkRenderPassCreateInfo &createInfo);
 };
 
 class StagingImage final : angle::NonCopyable
