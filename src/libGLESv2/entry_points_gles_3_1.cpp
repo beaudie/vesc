0a17e4fde6f3fab078221df01195d0b0b46e8fca
JiangYizhou <yizhou.jiang@intel.com>
2016-11-16 09:57:22 +0800

    Implement new ES3.1 framebuffer parameters.
    Change-Id: I425e73a6b798fc7c73841ab98d7c8aabc381133d

--- src/libGLESv2/entry_points_gles_3_1.cpp
+++ src/libGLESv2/entry_points_gles_3_1.cpp
@@ -13,6 +13,8 @@
 #include "libANGLE/Error.h"
 
 #include "libANGLE/validationES31.h"
+#include "libANGLE/queryconversions.h"
+#include "libANGLE/queryutils.h"
 
 #include "common/debug.h"
 
@@ -82,11 +84,13 @@ void GL_APIENTRY FramebufferParameteri(GLenum target, GLenum pname, GLint param)
     Context *context = GetValidGlobalContext();
     if (context)
     {
-        if (!context->skipValidation())
+        if (!context->skipValidation() &&
+            !ValidationFramebufferParameteri(context, target, pname, param))
         {
-            context->handleError(Error(GL_INVALID_OPERATION, "Entry point not implemented"));
+            return;
         }
-        UNIMPLEMENTED();
+        Framebuffer *framebuffer = context->getGLState().getTargetFramebuffer(target);
+        SetFramebufferParameteri(framebuffer, pname, param);
     }
 }
 
@@ -97,11 +101,13 @@ void GL_APIENTRY GetFramebufferParameteriv(GLenum target, GLenum pname, GLint *p
     Context *context = GetValidGlobalContext();
     if (context)
     {
-        if (!context->skipValidation())
+        if (!context->skipValidation() &&
+            ValidationGetFramebufferParameteri(context, target, pname, params))
         {
-            context->handleError(Error(GL_INVALID_OPERATION, "Entry point not implemented"));
+            return;
         }
-        UNIMPLEMENTED();
+        const Framebuffer *framebuffer = context->getGLState().getTargetFramebuffer(target);
+        QueryFramebufferParameteriv(framebuffer, pname, params);
     }
 }
 
