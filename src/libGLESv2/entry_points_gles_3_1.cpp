64e7b97ca89e70846c515f88f22d303f1a1ff399
Geoff Lang <geofflang@chromium.org>
2016-12-02 10:20:43 -0500

    Change dEQP ES 3.1 expectations from FAIL to SKIP.
    UNIMPLEMENTED debug spam was causing the tests time time out.  We can mark them
    as FAIL again once the dEQP test setup/tear down code doesn't emmit so many
    messages.
    
    Implement a couple validation cases for ES 3.1 to greatly reduce the spam.
    
    BUG=angleproject:1647
    BUG=angleproject:1442
    
    Change-Id: Ie7b4ac8737a2df1c0ada6ad53154ddf2f37d9c3c

--- src/libGLESv2/entry_points_gles_3_1.cpp
+++ src/libGLESv2/entry_points_gles_3_1.cpp
@@ -260,7 +260,11 @@ void GL_APIENTRY BindProgramPipeline(GLuint pipeline)
     Context *context = GetValidGlobalContext();
     if (context)
     {
-        UNIMPLEMENTED();
+        if (pipeline != 0)
+        {
+            // Binding non-zero pipelines is not implemented yet.
+            UNIMPLEMENTED();
+        }
     }
 }
 
@@ -945,7 +949,11 @@ void GL_APIENTRY BindImageTexture(GLuint unit,
     Context *context = GetValidGlobalContext();
     if (context)
     {
-        UNIMPLEMENTED();
+        if (texture != 0)
+        {
+            // Binding non-zero image textures is not implemented yet.
+            UNIMPLEMENTED();
+        }
     }
 }
 
