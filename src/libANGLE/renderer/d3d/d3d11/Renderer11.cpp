a8672f704e8fe3c9e8f8de5fca96223e456607f4
Jiawei-Shao <jiawei.shao@intel.com>
2016-12-09 16:38:02 +0800

    ES31: Refactor VertexArray for Vertex Attrib Binding
    OpenGL ES3.1 feature Vertex Attrib Binding requires vertex arrays should
    be split into two arrays:
    1. an array of vertex buffer binding points, each of which specifies:
      - a bound buffer object,
      - a starting offset for vertex attribute data in that buffer object,
      - a stride used by all attributes using that binding point,
      - a frequency divisor used by all attributes using that binding point.
    2. an array of generic vertex attribute format information records, each
    of which specifies:
      - a reference to one of the new buffer binding points above,
      - a component count and format, and a normalization flag for the
        attribute data,
      - the offset of the attribute data relative to the base offset of each
        vertex found at the associated binding point.
    
    Current ANGLE implementation simply uses a struct to represent a vertex
    attribute object, which does not meet the requirements above.
    
    This patch aims to be the the basis of the implementation of all ES3.1
    Vertex Attrib Binding APIs by refactoring the struct VertexAttribute and
    the class VertexArray to fit the new data layout and ensuring all current
    functionality is retained.
    
    BUG=angleproject:1593
    
    TEST=angle_unittests, angle_end2end_tests, gpu_unittests
    
    Change-Id: Ieb41f1bf503f815fd0476d2ea045dcb863465254

--- src/libANGLE/renderer/d3d/d3d11/Renderer11.cpp
+++ src/libANGLE/renderer/d3d/d3d11/Renderer11.cpp
@@ -4414,14 +4414,15 @@ gl::ErrorOrResult<unsigned int> Renderer11::getVertexSpaceRequired(
     }
 
     unsigned int elementCount = 0;
-    if (instances == 0 || attrib.divisor == 0)
+    const auto &binding       = *attrib.binding;
+    if (instances == 0 || binding.divisor == 0)
     {
         elementCount = count;
     }
     else
     {
         // Round up to divisor, if possible
-        elementCount = UnsignedCeilDivide(static_cast<unsigned int>(instances), attrib.divisor);
+        elementCount = UnsignedCeilDivide(static_cast<unsigned int>(instances), binding.divisor);
     }
 
     gl::VertexFormatType formatType      = gl::GetVertexFormatType(attrib);
