5c70b56affce43596507f4bf896993b009f3e53e
Frank Henigman <fjhenigman@chromium.org>
2017-01-05 15:48:26 -0500

    vulkan hello triangle
    Change-Id: I3bc0cdefe02b02c046b0e85822019a0f1762235e

--- src/libANGLE/renderer/vulkan/BufferVk.cpp
+++ src/libANGLE/renderer/vulkan/BufferVk.cpp
@@ -116,7 +116,7 @@ gl::Error BufferVk::map(ContextImpl *context, GLenum access, GLvoid **mapPtr)
     ASSERT(mBuffer.getMemory().getHandle() != VK_NULL_HANDLE);
 
     ANGLE_TRY(
-        mBuffer.getMemory().map(0, mState.getSize(), 0, reinterpret_cast<uint8_t **>(mapPtr)));
+        mBuffer.getMemory().map(0, mRequiredSize, 0, reinterpret_cast<uint8_t **>(mapPtr)));
 
     return gl::NoError();
 }
