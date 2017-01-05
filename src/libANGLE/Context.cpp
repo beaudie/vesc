faf1cbb7057f685112b9a5c3983773f4453f11d3
Bryan Bernhart <bryan.bernhart@intel.com>
2017-01-05 13:09:31 -0800

    Apply max draw buffer limit for WebGL shaders.
    For WebGL, using native caps incorrectly sets gl_MaxDrawBuffers.
    This change always applys the limit for compatible shaders.
    
    BUG=angleproject:1523
    
    Change-Id: Ie956f4b9fdd10bdf9276ae08eaeb49e65690185b

--- src/libANGLE/Context.cpp
+++ src/libANGLE/Context.cpp
@@ -2315,11 +2315,14 @@ void Context::initExtensionStrings()
     }
     mExtensionString = mergeExtensionStrings(mExtensionStrings);
 
+    const gl::Extensions &nativeExtensions = mImplementation->getNativeExtensions();
+
     mRequestableExtensionStrings.clear();
     for (const auto &extensionInfo : GetExtensionInfoMap())
     {
         if (extensionInfo.second.Requestable &&
-            !(mExtensions.*(extensionInfo.second.ExtensionsMember)))
+            !(mExtensions.*(extensionInfo.second.ExtensionsMember)) &&
+            nativeExtensions.*(extensionInfo.second.ExtensionsMember))
         {
             mRequestableExtensionStrings.push_back(MakeStaticString(extensionInfo.first));
         }
@@ -2392,6 +2395,10 @@ void Context::requestExtension(const char *name)
     mExtensions.*(extension.ExtensionsMember) = true;
     updateCaps();
     initExtensionStrings();
+
+    // Re-create the compiler with the requested extensions enabled.
+    SafeDelete(mCompiler);
+    mCompiler = new Compiler(mImplementation.get(), mState);
 }
 
 size_t Context::getRequestableExtensionStringCount() const
