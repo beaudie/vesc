956935ec03c9b9989366ebae0d53870e855a6dcb
Jamie Madill <jmadill@chromium.org>
2017-01-06 15:24:46 -0500

    Add destroy handler to SurfaceImpl.
    This allows the Vulkan back-end to avoid storing a reference to
    the VkDevice. This will extend to all the Vulkan object handle wrapper
    types.
    
    BUG=angleproject:1684
    
    Change-Id: I3a98e94bc171ca27f225ce57996c3fdf9581e6e1

--- src/libANGLE/Display.cpp
+++ src/libANGLE/Display.cpp
@@ -764,7 +764,7 @@ Error Display::makeCurrent(egl::Surface *drawSurface, egl::Surface *readSurface,
     if (context != nullptr && drawSurface != nullptr)
     {
         ASSERT(readSurface == drawSurface);
-        context->makeCurrent(drawSurface);
+        context->makeCurrent(this, drawSurface);
     }
 
     return egl::Error(EGL_SUCCESS);
@@ -806,7 +806,7 @@ void Display::destroySurface(Surface *surface)
     }
 
     mState.surfaceSet.erase(surface);
-    surface->onDestroy();
+    surface->onDestroy(this);
 }
 
 void Display::destroyImage(egl::Image *image)
@@ -825,6 +825,7 @@ void Display::destroyStream(egl::Stream *stream)
 
 void Display::destroyContext(gl::Context *context)
 {
+    context->destroy(this);
     mContextSet.erase(context);
     SafeDelete(context);
 }
