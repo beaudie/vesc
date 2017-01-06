75ec3d55149cbae90879fba2761d727bee9a63f2
Jamie Madill <jmadill@chromium.org>
2017-01-06 15:24:44 -0500

    Vulkan: Implement shader compilation.
    This hooks up the Vulkan GLSL, decorated with locations, to glslang,
    and then pipes the SPIRV back to the Program implementation for later
    use when making pipelines to run draw calls.
    
    The program compilation tests work now, but don't really test
    anything other than not generating Vulkan validation layer errors
    during compilation and shader object generation.
    
    BUG=angleproject:1576
    
    Change-Id: I625e42219f4b4d1433dd3109b94e1a2f666ba4bd

--- src/libANGLE/renderer/d3d/ProgramD3D.cpp
+++ src/libANGLE/renderer/d3d/ProgramD3D.cpp
@@ -17,6 +17,7 @@
 #include "libANGLE/VaryingPacking.h"
 #include "libANGLE/VertexArray.h"
 #include "libANGLE/features.h"
+#include "libANGLE/renderer/ContextImpl.h"
 #include "libANGLE/renderer/d3d/DynamicHLSL.h"
 #include "libANGLE/renderer/d3d/FramebufferD3D.h"
 #include "libANGLE/renderer/d3d/RendererD3D.h"
@@ -1334,10 +1335,12 @@ LinkResult ProgramD3D::compileProgramExecutables(const gl::ContextState &context
             (!usesGeometryShader(GL_POINTS) || pointGS));
 }
 
-LinkResult ProgramD3D::link(const gl::ContextState &data,
+LinkResult ProgramD3D::link(ContextImpl *contextImpl,
                             const gl::VaryingPacking &packing,
                             gl::InfoLog &infoLog)
 {
+    const auto &data = contextImpl->getContextState();
+
     reset();
 
     const gl::Shader *vertexShader   = mState.getAttachedVertexShader();
