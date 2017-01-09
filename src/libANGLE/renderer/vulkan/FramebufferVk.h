46e3eead3f90b173a588f5259febd49d21cc596a
JiangYizhou <yizhou.jiang@intel.com>
2017-01-09 10:22:53 +0800

    ES31: Implement getMultisamplefv for D3D part.
    Implement getMultisamplefv for d3d part.
    As d3d function GetSamplePosition belongs to hlsl function, create a
    hlsl pipeline to retrive sample positions.
    
    BUG=angleproject:1590
    TEST=dEQP-GLES31.functional.texture.multisample.samples_*.sample_position
    
    Change-Id: I6e6006ed1c4e22fe006522e9ffd3297247bee75e

--- src/libANGLE/renderer/vulkan/FramebufferVk.h
+++ src/libANGLE/renderer/vulkan/FramebufferVk.h
@@ -81,7 +81,9 @@ class FramebufferVk : public FramebufferImpl, public ResourceVk
     void syncState(const gl::Context *context,
                    const gl::Framebuffer::DirtyBits &dirtyBits) override;
 
-    gl::Error getSamplePosition(size_t index, GLfloat *xy) const override;
+    gl::Error getSamplePosition(const gl::Context *context,
+                                size_t index,
+                                GLfloat *xy) const override;
 
     gl::Error beginRenderPass(const gl::Context *context,
                               VkDevice device,
