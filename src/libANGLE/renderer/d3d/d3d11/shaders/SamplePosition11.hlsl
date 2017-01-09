0fd74b1314a86ca4187132383fe097776b87a687
JiangYizhou <yizhou.jiang@intel.com>
2017-01-09 10:22:53 +0800

    ES31: Implement getMultisamplefv for D3D part.
    Implement getMultisamplefv for d3d part.
    As d3d function GetSamplePosition belongs to hlsl function, create a
    hlsl pipeline to retrive sample positions.
    
    BUG=angleproject:1590
    TEST=dEQP-GLES31.functional.texture.multisample.samples_*.sample_position
    
    Change-Id: I6e6006ed1c4e22fe006522e9ffd3297247bee75e

---
+++ src/libANGLE/renderer/d3d/d3d11/shaders/SamplePosition11.hlsl
@@ -0,0 +1,52 @@
+Texture2DMS<float4> TextureMSF  : register(t0);
+Texture2DMS<uint4>  TextureMSUI : register(t0);
+Texture2DMS<int4>   TextureMSI  : register(t0);
+
+struct SamplePosition
+{
+    float x, y;
+};
+RWStructuredBuffer<SamplePosition>  SamplePositionsBuffer : register(u1);
+
+void VS_SamplePosition(in float2  inPosition :    POSITION, in float2  inTexCoord : TEXCOORD0,
+    out float4 outPosition : SV_POSITION, out float2 outTexCoord : TEXCOORD0)
+{
+    outPosition = float4(inPosition, 0.0f, 1.0f);
+    outTexCoord = inTexCoord;
+}
+
+void PS_SamplePosition_2F(in float4 inPosition : SV_POSITION, in float2 inTexCoord : TEXCOORD0)
+{
+    uint width, height, sampleCount;
+    TextureMSF.GetDimensions(width, height, sampleCount);
+    for (uint index = 0; index < sampleCount; index++)
+    {
+        float2 location = TextureMSF.GetSamplePosition(index);
+		SamplePositionsBuffer[index].x = location.x;
+		SamplePositionsBuffer[index].y = location.y;
+    }
+}
+
+void PS_SamplePosition_2UI(in float4 inPosition : SV_POSITION, in float2 inTexCoord : TEXCOORD0)
+{
+    uint width, height, sampleCount;
+    TextureMSUI.GetDimensions(width, height, sampleCount);
+    for (uint index = 0; index < sampleCount; index++)
+    {
+        float2 location = TextureMSUI.GetSamplePosition(index);
+		SamplePositionsBuffer[index].x = location.x;
+		SamplePositionsBuffer[index].y = location.y;
+    }
+}
+
+void PS_SamplePosition_2I(in float4 inPosition : SV_POSITION, in float2 inTexCoord : TEXCOORD0)
+{
+    uint width, height, sampleCount;
+    TextureMSUI.GetDimensions(width, height, sampleCount);
+    for (uint index = 0; index < sampleCount; index++)
+    {
+        float2 location = TextureMSUI.GetSamplePosition(index);
+		SamplePositionsBuffer[index].x = location.x;
+		SamplePositionsBuffer[index].y = location.y;
+    }
+}
