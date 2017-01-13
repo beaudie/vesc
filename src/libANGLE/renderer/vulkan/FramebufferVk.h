03bb447282dc73698bf4875e801c6d7b9021392b
Jamie Madill <jmadill@chromium.org>
2017-01-13 17:29:49 -0500

    Vulkan: Configure a simple RenderPass for drawing.
    BUG=angleproject:1319
    
    Change-Id: I1779638b449b4021db85c5503b9ce93645f843d1

--- src/libANGLE/renderer/vulkan/FramebufferVk.h
+++ src/libANGLE/renderer/vulkan/FramebufferVk.h
@@ -75,9 +75,22 @@ class FramebufferVk : public FramebufferImpl
 
     void syncState(const gl::Framebuffer::DirtyBits &dirtyBits) override;
 
+    gl::Error beginRenderPass(VkDevice device,
+                              vk::CommandBuffer *commandBuffer,
+                              const gl::State &glState);
+
+    gl::ErrorOrResult<vk::RenderPass *> getRenderPass(VkDevice device);
+
   private:
     FramebufferVk(const gl::FramebufferState &state);
     FramebufferVk(const gl::FramebufferState &state, WindowSurfaceVk *backbuffer);
+
+    gl::ErrorOrResult<vk::Framebuffer *> getFramebuffer(VkDevice device);
+
+    WindowSurfaceVk *mBackbuffer;
+
+    vk::RenderPass mRenderPass;
+    vk::Framebuffer mFramebuffer;
 };
 
 }  // namespace rx
