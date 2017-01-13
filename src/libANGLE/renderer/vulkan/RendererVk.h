27a9e53b0c6d3729d9c9f1987b5b9b5eba0167b8
Jamie Madill <jmadill@chromium.org>
2017-01-13 17:29:59 -0500

    Vulkan: Enable command queueing.
    BUG=angleproject:####
    
    Change-Id: I4fcfd90ad0665d127bf01a10214a604f3407d9e4

--- src/libANGLE/renderer/vulkan/RendererVk.h
+++ src/libANGLE/renderer/vulkan/RendererVk.h
@@ -70,6 +70,12 @@ class RendererVk : angle::NonCopyable
 
     uint32_t getCurrentCommandSerial() const;
 
+    template <typename T>
+    void enqueueGarbage(uint32_t serial, T &&object)
+    {
+        mGarbage.emplace_back(std::unique_ptr<vk::GarbageObject<T>>(new vk::GarbageObject<T>(serial, std::move(object))));
+    }
+
   private:
     void ensureCapsInitialized() const;
     void generateCaps(gl::Caps *outCaps,
@@ -103,6 +109,7 @@ class RendererVk : angle::NonCopyable
     uint32_t mCurrentCommandSerial;
     uint32_t mLastCompletedCommandSerial;
     std::vector<vk::FenceAndCommandBuffer> mInFlightCommands;
+    std::vector<std::unique_ptr<vk::IGarbageObject>> mGarbage;
 };
 
 }  // namespace rx
