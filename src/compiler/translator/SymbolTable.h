58b333325e4371c2c005cd3567c48b5aa99cb686
Olli Etuaho <oetuaho@nvidia.com>
2017-01-22 14:51:23 -0800

    Add symbol table function to get TFunction for a built-in op
    Built-in parameter qualifiers are stored in the symbol table. Some
    traversers need the qualifier information to determine whether a node
    is an l-value. Add an utility function that maps a TIntermAggregate
    node to a symbol table entry, so that the traversers can get to this
    information in a convenient way.
    
    This will be necessary for adding more built-ins that have out
    parameters from ESSL 3.10.
    
    BUG=angleproject:1730
    TEST=angle_unittests
    
    Change-Id: I4bc622d70b2326a04cc858ff1258c22320c590dc

--- src/compiler/translator/SymbolTable.h
+++ src/compiler/translator/SymbolTable.h
@@ -41,6 +41,8 @@
 namespace sh
 {
 
+class TIntermAggregate;
+
 // Symbol base class. (Can build functions or variables out of these...)
 class TSymbol : angle::NonCopyable
 {
@@ -423,6 +425,8 @@ class TSymbolTable : angle::NonCopyable
     TSymbol *findGlobal(const TString &name) const;
 
     TSymbol *findBuiltIn(const TString &name, int shaderVersion) const;
+
+    TFunction *findBuiltInOp(TIntermAggregate *callNode, int shaderVersion) const;
 
     TSymbolTableLevel *getOuterLevel()
     {
