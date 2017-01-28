f5fb80d65c41e3edb00ed853461d582a366ff26f
Yuly Novikov <ynovikov@chromium.org>
2017-01-27 22:45:18 -0500

    Direct logging to Platform when available
    All logging should be done via ERR() and WARN(),
    which call angle::Platform's logError and logWarning,
    if there is current Platform which supports logging.
    Otherwise, ERR() is directed to std::cerr.
    
    Misc fixes to keep tests passing.
    
    BUG=angleproject:1660, angleproject:1644
    
    Change-Id: I2bca33a021537185d0c236a3083789af3236b5f3

--- src/libANGLE/Display.cpp
+++ src/libANGLE/Display.cpp
@@ -70,29 +70,9 @@ class DefaultPlatform : public angle::Platform
 public:
     DefaultPlatform() {}
     ~DefaultPlatform() override {}
-
-    void logError(const char *errorMessage) override;
-    void logWarning(const char *warningMessage) override;
-    void logInfo(const char *infoMessage) override;
 };
 
 std::unique_ptr<DefaultPlatform> g_defaultPlatform = nullptr;
-
-void DefaultPlatform::logError(const char *errorMessage)
-{
-    ERR() << errorMessage;
-}
-
-void DefaultPlatform::logWarning(const char *warningMessage)
-{
-    WARN() << warningMessage;
-}
-
-void DefaultPlatform::logInfo(const char *infoMessage)
-{
-    // Uncomment this if you want Vulkan spam.
-    // WARN() << infoMessage;
-}
 
 }  // namespace angle
 
@@ -411,6 +391,8 @@ Display::~Display()
 
     SafeDelete(mDevice);
     SafeDelete(mImplementation);
+
+    gl::UninitializeDebugAnnotations();
 }
 
 void Display::setAttributes(rx::DisplayImpl *impl, const AttributeMap &attribMap)
@@ -429,6 +411,8 @@ Error Display::initialize()
     // Re-initialize default platform if it's needed
     InitDefaultPlatformImpl();
 
+    gl::InitializeDebugAnnotations(&mAnnotator);
+
     SCOPED_ANGLE_HISTOGRAM_TIMER("GPU.ANGLE.DisplayInitializeMS");
     TRACE_EVENT0("gpu.angle", "egl::Display::initialize");
 
@@ -443,10 +427,7 @@ Error Display::initialize()
     if (error.isError())
     {
         // Log extended error message here
-        std::stringstream errorStream;
-        errorStream << "ANGLE Display::initialize error " << error.getID() << ": "
-                    << error.getMessage();
-        ANGLEPlatformCurrent()->logError(errorStream.str().c_str());
+        ERR() << "ANGLE Display::initialize error " << error.getID() << ": " << error.getMessage();
         return error;
     }
 
