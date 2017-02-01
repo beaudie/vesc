9c3b4d593c51aa68fc2d3afa1acc2e0a4dd9255c
Olli Etuaho <oetuaho@nvidia.com>
2017-02-01 15:37:48 +0000

    Add ESSL 3.10 ldexp/frexp builtins
    Change-Id: I1330e69978b0cf53efbc3416150194764414e96c

--- src/common/mathutil_unittest.cpp
+++ src/common/mathutil_unittest.cpp
@@ -310,4 +310,12 @@ TEST(MathUtilTest, FindMSB)
     EXPECT_EQ(31, gl::FindMSB(0x80000000u));
 }
 
+// Test Ldexp, which combines mantissa and exponent into a floating-point number.
+TEST(MathUtilTest, Ldexp)
+{
+    EXPECT_EQ(1.25f, Ldexp(0.625f, 2));
+    EXPECT_EQ(std::numeric_limits<float>::infinity(), Ldexp(0.625f, 129));
+    EXPECT_EQ(0.0f, Ldexp(1.0f, -129));
+}
+
 }  // anonymous namespace
