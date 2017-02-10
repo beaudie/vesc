c150f54d752f57e2b90ca761c86ee96369e3f5a9
Geoff Lang <geofflang@chromium.org>
2017-02-10 14:56:55 -0500

    Add artificial limits to the total memory allocated by the NULL backend.
    Fuzzer tests were capable of allocating very large chunks of memory by
    calling glBufferData with a null pointer.  This sets a limit before the
    NULL backend starts returning out of memory GL errors.
    
    BUG=602737
    
    Change-Id: Ic53ebcf999f951b96c1df82e4db57e949d03c908

--- src/libANGLE/renderer/null/ContextNULL.cpp
+++ src/libANGLE/renderer/null/ContextNULL.cpp
@@ -31,11 +31,45 @@
 namespace rx
 {
 
-ContextNULL::ContextNULL(const gl::ContextState &state) : ContextImpl(state)
+AllocationTrackerNULL::AllocationTrackerNULL(size_t maxTotalAllocationSize)
+    : mAllocatedBytes(0), mMaxBytes(maxTotalAllocationSize)
 {
+}
+
+AllocationTrackerNULL::~AllocationTrackerNULL()
+{
+    // ASSERT that all objects with the NULL renderer clean up after themselves
+    ASSERT(mAllocatedBytes == 0);
+}
+
+bool AllocationTrackerNULL::updateMemoryAllocation(size_t oldSize, size_t newSize)
+{
+    ASSERT(mAllocatedBytes >= oldSize);
+
+    size_t releasedSize = mAllocatedBytes - oldSize;
+    size_t finalSize    = releasedSize + newSize;
+    if (finalSize < releasedSize || finalSize > mMaxBytes)
+    {
+        // Overflow or allocation would be too large
+        return false;
+    }
+
+    mAllocatedBytes = finalSize;
+    return true;
+}
+
+ContextNULL::ContextNULL(const gl::ContextState &state, AllocationTrackerNULL *allocationTracker)
+    : ContextImpl(state), mAllocationTracker(allocationTracker)
+{
+    ASSERT(mAllocationTracker != nullptr);
+
     const gl::Version maxClientVersion(3, 1);
     mCaps        = GenerateMinimumCaps(maxClientVersion);
+
     mExtensions  = gl::Extensions();
+    mExtensions.copyTexture           = true;
+    mExtensions.copyCompressedTexture = true;
+
     mTextureCaps = GenerateMinimumTextureCapsMap(maxClientVersion, mExtensions);
 }
 
@@ -286,7 +320,7 @@ RenderbufferImpl *ContextNULL::createRenderbuffer()
 
 BufferImpl *ContextNULL::createBuffer(const gl::BufferState &state)
 {
-    return new BufferNULL(state);
+    return new BufferNULL(state, mAllocationTracker);
 }
 
 VertexArrayImpl *ContextNULL::createVertexArray(const gl::VertexArrayState &data)
