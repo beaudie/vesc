c325f12d1c296fb7fffbbd20834b515e747a6087
Olli Etuaho <oetuaho@nvidia.com>
2017-02-19 18:05:10 +0000

    Implement location layout qualifier for uniforms
    This is a complete implementation of the uniform location layout
    qualifier. Uniform location set in the shader is plumbed to shader
    linking, which does several link-time checks for conflicts and
    recursively applies the location to struct members.
    
    Validate that location is consistent as specified in the table in
    section 9.2.1 of the ESSL 3.10.4 spec. The location set in the shader
    must also be consistent with the one set via the
    CHROMIUM_bind_uniform_location API, if any.
    
    Location conflicts must be checked even if the uniforms are not
    statically used. Because of this unused uniforms are now recorded in
    the program state object.
    
    Location is validated against the maximum number of uniform locations
    at compile time as specified in section 4.4.3 of the ESSL 3.10.4 spec.
    
    All dEQP uniform location tests don't yet pass due to unrelated bugs.
    
    BUG=angleproject:1442
    TEST=angle_end2end_tests, dEQP-GLES31.functional.uniform_location.*
    
    Change-Id: I1f968e971f521fbc804b01e1a7c2b4d14f24d20f

--- src/compiler/translator/Types.cpp
+++ src/compiler/translator/Types.cpp
@@ -445,6 +445,28 @@ size_t TType::getObjectSize() const
     return totalSize;
 }
 
+int TType::getLocationCount() const
+{
+    int count = 1;
+
+    if (getBasicType() == EbtStruct)
+        count = structure->locationCount();
+
+    if (isArray())
+    {
+        if (count == 0)
+            return 0;
+
+        size_t currentArraySize = getArraySize();
+        if (currentArraySize > std::numeric_limits<int>::max() / count)
+            count = std::numeric_limits<int>::max();
+        else
+            count *= static_cast<int>(currentArraySize);
+    }
+
+    return count;
+}
+
 TStructure::TStructure(const TString *name, TFieldList *fields)
     : TFieldListCollection(name, fields),
       mDeepestNesting(0),
@@ -583,15 +605,33 @@ TString TFieldListCollection::buildMangledName(const TString &mangledNamePrefix)
 size_t TFieldListCollection::calculateObjectSize() const
 {
     size_t size = 0;
-    for (size_t i = 0; i < mFields->size(); ++i)
+    for (const TField *field : *mFields)
     {
-        size_t fieldSize = (*mFields)[i]->type()->getObjectSize();
+        size_t fieldSize = field->type()->getObjectSize();
         if (fieldSize > INT_MAX - size)
             size = INT_MAX;
         else
             size += fieldSize;
     }
     return size;
+}
+
+int TFieldListCollection::locationCount() const
+{
+    int count = 0;
+    for (const TField *field : *mFields)
+    {
+        int fieldCount = field->type()->getLocationCount();
+        if (fieldCount > std::numeric_limits<int>::max() - count)
+        {
+            count = std::numeric_limits<int>::max();
+        }
+        else
+        {
+            count += fieldCount;
+        }
+    }
+    return count;
 }
 
 int TStructure::calculateDeepestNesting() const
