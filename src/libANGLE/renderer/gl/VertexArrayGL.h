fa61ae4cc3b40023fb6ca9d939d80195bff45834
Shao <jiawei.shao@intel.com>
2017-02-20 21:25:59 +0800

    [WIP] ES31: Implement Vertex Attrib Binding on D3D11 backends
    This patch intends to implement Vertex Attrib Binding on D3D11 backends:
    (1) Add entry points and validation code on following APIs:
       - VertexAttribFormat
       - VertexAttribIFormat
       - VertexAttribBinding
       - BindVertexBuffer
       - VertexBindingDivisor
    (2) Implement all APIs mentioned in (1) on D3D 11 backends.
    (3) Add queries on following parameters:
       - VERTEX_ATTRIB_BINDING
       - VERTEX_ATTRIB_RELATIVE_OFFSET
       - VERTEX_BINDING_DIVISOR
       - VERTEX_BINDING_OFFSET
       - VERTEX_BINDING_STRIDE
       - VERTEX_BINDING_BUFFER
       - MAX_VERTEX_ATTRIB_RELATIVE_OFFSET
       - MAX_VERTEX_ATTRIB_BINDINGS
    
    BUG=angleproject:1593
    TEST=angle_end2end_tests
    TEST=angle_unittests
    TEST=dEQP-GLES31.functional.vertex_attribute_binding.*
    TEST=dEQP-GLES31.functional.state_query.integer.max_vertex_attrib_relative_offset_*
    TEST=dEQP-GLES31.functional.state_query.integer.max_vertex_attrib_bindings_*
    TEST=dEQP-GLES31.functional.state_query.integer.max_vertex_attrib_stride_*
    TEST=dEQP-GLES31.functional.state_query.vertex_attribute_binding.*
    TEST=dEQP-GLES31.functional.debug.negative_coverage.log.vertex_array.vertex_attrib_pointer
    TEST=dEQP-GLES31.functional.debug.negative_coverage.get_error.vertex_array.vertex_attrib_format
    TEST=dEQP-GLES31.functional.debug.negative_coverage.get_error.vertex_array.vertex_attrib_i_format
    
    Change-Id: I4b477a82df6aad89b89b088580a06d66963e6666

--- src/libANGLE/renderer/gl/VertexArrayGL.h
+++ src/libANGLE/renderer/gl/VertexArrayGL.h
@@ -14,90 +14,90 @@
 namespace rx
 {
 
-class FunctionsGL;
-class StateManagerGL;
+	class FunctionsGL;
+	class StateManagerGL;
 
-class VertexArrayGL : public VertexArrayImpl
-{
-  public:
-    VertexArrayGL(const gl::VertexArrayState &data,
-                  const FunctionsGL *functions,
-                  StateManagerGL *stateManager);
-    ~VertexArrayGL() override;
+	class VertexArrayGL : public VertexArrayImpl
+	{
+	public:
+		VertexArrayGL(const gl::VertexArrayState &data,
+			const FunctionsGL *functions,
+			StateManagerGL *stateManager);
+		~VertexArrayGL() override;
 
-    gl::Error syncDrawArraysState(const gl::AttributesMask &activeAttributesMask,
-                                  GLint first,
-                                  GLsizei count,
-                                  GLsizei instanceCount) const;
-    gl::Error syncDrawElementsState(const gl::AttributesMask &activeAttributesMask,
-                                    GLsizei count,
-                                    GLenum type,
-                                    const GLvoid *indices,
-                                    GLsizei instanceCount,
-                                    bool primitiveRestartEnabled,
-                                    const GLvoid **outIndices) const;
-    gl::Error syncElementArrayState() const;
+		gl::Error syncDrawArraysState(const gl::AttributesMask &activeAttributesMask,
+			GLint first,
+			GLsizei count,
+			GLsizei instanceCount) const;
+		gl::Error syncDrawElementsState(const gl::AttributesMask &activeAttributesMask,
+			GLsizei count,
+			GLenum type,
+			const GLvoid *indices,
+			GLsizei instanceCount,
+			bool primitiveRestartEnabled,
+			const GLvoid **outIndices) const;
+		gl::Error syncElementArrayState() const;
 
-    GLuint getVertexArrayID() const;
-    GLuint getAppliedElementArrayBufferID() const;
+		GLuint getVertexArrayID() const;
+		GLuint getAppliedElementArrayBufferID() const;
 
-    void syncState(const gl::VertexArray::DirtyBits &dirtyBits) override;
+		void syncState(const gl::VertexArray::DirtyBits &dirtyBits) override;
 
-  private:
-    gl::Error syncDrawState(const gl::AttributesMask &activeAttributesMask,
-                            GLint first,
-                            GLsizei count,
-                            GLenum type,
-                            const GLvoid *indices,
-                            GLsizei instanceCount,
-                            bool primitiveRestartEnabled,
-                            const GLvoid **outIndices) const;
+	private:
+		gl::Error syncDrawState(const gl::AttributesMask &activeAttributesMask,
+			GLint first,
+			GLsizei count,
+			GLenum type,
+			const GLvoid *indices,
+			GLsizei instanceCount,
+			bool primitiveRestartEnabled,
+			const GLvoid **outIndices) const;
 
-    // Apply index data, only sets outIndexRange if attributesNeedStreaming is true
-    gl::Error syncIndexData(GLsizei count,
-                            GLenum type,
-                            const GLvoid *indices,
-                            bool primitiveRestartEnabled,
-                            bool attributesNeedStreaming,
-                            gl::IndexRange *outIndexRange,
-                            const GLvoid **outIndices) const;
+		// Apply index data, only sets outIndexRange if attributesNeedStreaming is true
+		gl::Error syncIndexData(GLsizei count,
+			GLenum type,
+			const GLvoid *indices,
+			bool primitiveRestartEnabled,
+			bool attributesNeedStreaming,
+			gl::IndexRange *outIndexRange,
+			const GLvoid **outIndices) const;
 
-    // Returns the amount of space needed to stream all attributes that need streaming
-    // and the data size of the largest attribute
-    void computeStreamingAttributeSizes(const gl::AttributesMask &activeAttributesMask,
-                                        GLsizei instanceCount,
-                                        const gl::IndexRange &indexRange,
-                                        size_t *outStreamingDataSize,
-                                        size_t *outMaxAttributeDataSize) const;
+		// Returns the amount of space needed to stream all attributes that need streaming
+		// and the data size of the largest attribute
+		void computeStreamingAttributeSizes(const gl::AttributesMask &activeAttributesMask,
+			GLsizei instanceCount,
+			const gl::IndexRange &indexRange,
+			size_t *outStreamingDataSize,
+			size_t *outMaxAttributeDataSize) const;
 
-    // Stream attributes that have client data
-    gl::Error streamAttributes(const gl::AttributesMask &activeAttributesMask,
-                               GLsizei instanceCount,
-                               const gl::IndexRange &indexRange) const;
+		// Stream attributes that have client data
+		gl::Error streamAttributes(const gl::AttributesMask &activeAttributesMask,
+			GLsizei instanceCount,
+			const gl::IndexRange &indexRange) const;
 
-    void updateNeedsStreaming(size_t attribIndex);
-    void updateAttribEnabled(size_t attribIndex);
-    void updateAttribPointer(size_t attribIndex);
-    void updateAttribDivisor(size_t attribIndex);
+		void updateNeedsStreaming(size_t attribIndex);
+		void updateAttribEnabled(size_t attribIndex);
+		void updateAttribPointer(size_t attribIndex);
+		void updateAttribDivisor(size_t attribIndex);
 
-    const FunctionsGL *mFunctions;
-    StateManagerGL *mStateManager;
+		const FunctionsGL *mFunctions;
+		StateManagerGL *mStateManager;
 
-    GLuint mVertexArrayID;
+		GLuint mVertexArrayID;
 
-    mutable BindingPointer<gl::Buffer> mAppliedElementArrayBuffer;
+		mutable BindingPointer<gl::Buffer> mAppliedElementArrayBuffer;
 
-    mutable std::vector<gl::VertexAttribute> mAppliedAttributes;
-    mutable std::vector<gl::VertexBinding> mAppliedBindings;
+		mutable std::vector<gl::VertexAttribute> mAppliedAttributes;
+		mutable std::vector<gl::VertexBinding> mAppliedBindings;
 
-    mutable size_t mStreamingElementArrayBufferSize;
-    mutable GLuint mStreamingElementArrayBuffer;
+		mutable size_t mStreamingElementArrayBufferSize;
+		mutable GLuint mStreamingElementArrayBuffer;
 
-    mutable size_t mStreamingArrayBufferSize;
-    mutable GLuint mStreamingArrayBuffer;
+		mutable size_t mStreamingArrayBufferSize;
+		mutable GLuint mStreamingArrayBuffer;
 
-    gl::AttributesMask mAttributesNeedStreaming;
-};
+		gl::AttributesMask mAttributesNeedStreaming;
+	};
 
 }
 
