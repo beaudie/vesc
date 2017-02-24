9129bbc3f9ae92c1d7c309abb6981e355b45c563
Olli Etuaho <oetuaho@nvidia.com>
2017-02-24 12:36:50 +0000

    Unify and simplify shader variable collection
    Instead of setting variable information in both CollectVariables and
    the GetVariableTraverser helper class it uses, keep all of this
    functionality in CollectVariables. A single helper function handles
    setting variable information that doesn't depend on variable type, and
    the rest is done in "record" functions that are implemented for each
    variable type.
    
    This removes templates from the code, making it leaner and easier to
    understand, and will help with implementing future features like
    adding binding and location layout qualifiers for uniforms.
    
    BUG=angleproject:1442
    TEST=angle_unittests, angle_end2end_tests,
         dEQP-GLES2.functional.shaders.*
    
    Change-Id: I79148b7b3fa9cb46634a22bdcc9ce0c04f970384

--- src/tests/compiler_tests/CollectVariables_test.cpp
+++ src/tests/compiler_tests/CollectVariables_test.cpp
@@ -740,3 +740,58 @@ TEST_F(CollectHashedVertexVariablesTest, StructUniform)
     EXPECT_EQ("webgl_5", field.mappedName);
     EXPECT_TRUE(field.fields.empty());
 }
+
+TEST_F(CollectFragmentVariablesTest, MultiDeclaration)
+{
+    const std::string &shaderString =
+        "#version 300 es\n"
+        "precision mediump float;\n"
+        "out vec4 out_fragColor;\n"
+        "uniform float uA, uB;\n"
+        "void main() {\n"
+        "   out_fragColor = vec4(uA, uA, uA, uB);\n"
+        "}\n";
+
+    compile(shaderString);
+
+    const auto &uniforms = mTranslator->getUniforms();
+    ASSERT_EQ(2u, uniforms.size());
+
+    const Uniform &uniform = uniforms[0];
+    EXPECT_EQ(0u, uniform.arraySize);
+    EXPECT_GLENUM_EQ(GL_MEDIUM_FLOAT, uniform.precision);
+    EXPECT_TRUE(uniform.staticUse);
+    EXPECT_GLENUM_EQ(GL_FLOAT, uniform.type);
+    EXPECT_EQ("uA", uniform.name);
+
+    const Uniform &uniformB = uniforms[1];
+    EXPECT_EQ(0u, uniformB.arraySize);
+    EXPECT_GLENUM_EQ(GL_MEDIUM_FLOAT, uniformB.precision);
+    EXPECT_TRUE(uniformB.staticUse);
+    EXPECT_GLENUM_EQ(GL_FLOAT, uniformB.type);
+    EXPECT_EQ("uB", uniformB.name);
+}
+
+TEST_F(CollectFragmentVariablesTest, EmptyDeclarator)
+{
+    const std::string &shaderString =
+        "#version 300 es\n"
+        "precision mediump float;\n"
+        "out vec4 out_fragColor;\n"
+        "uniform float /* empty declarator */, uB;\n"
+        "void main() {\n"
+        "   out_fragColor = vec4(uB, uB, uB, uB);\n"
+        "}\n";
+
+    compile(shaderString);
+
+    const auto &uniforms = mTranslator->getUniforms();
+    ASSERT_EQ(1u, uniforms.size());
+
+    const Uniform &uniformB = uniforms[0];
+    EXPECT_EQ(0u, uniformB.arraySize);
+    EXPECT_GLENUM_EQ(GL_MEDIUM_FLOAT, uniformB.precision);
+    EXPECT_TRUE(uniformB.staticUse);
+    EXPECT_GLENUM_EQ(GL_FLOAT, uniformB.type);
+    EXPECT_EQ("uB", uniformB.name);
+}
