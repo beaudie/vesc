7c724a6afcc159bb91ce2a7f83a75a6a0d67821e
Yunchao He <yunchao.he@intel.com>
2017-03-14 15:34:03 +0800

    ES31: Add PROGRAM_SEPARABLE to ProgramParameter and GetProgram
    BUG=angleproject:1939
    TEST=angle_end2end_tests
    
    Change-Id: I97ad11360f7c015947a2c0cc7d4a47f994726834

--- src/libANGLE/renderer/gl/ProgramGL.cpp
+++ src/libANGLE/renderer/gl/ProgramGL.cpp
@@ -111,6 +111,16 @@ void ProgramGL::setBinaryRetrievableHint(bool retrievable)
     }
 }
 
+void ProgramGL::setSeparable(bool separable)
+{
+    // glProgramParameteri isn't always available on ES backends.
+    if (mFunctions->programParameteri)
+    {
+        mFunctions->programParameteri(mProgramID, GL_PROGRAM_SEPARABLE,
+                                      separable ? GL_TRUE : GL_FALSE);
+    }
+}
+
 LinkResult ProgramGL::link(ContextImpl *contextImpl,
                            const gl::VaryingPacking &packing,
                            gl::InfoLog &infoLog)
