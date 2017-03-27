4101096f19a6ca67da4307c5a8a8a64be3c96ab2
Jiajia Qin <jiajia.qin@intel.com>
2017-03-27 14:12:59 +0800

    ES31: Add GL_SHADER_STORAGE_BUFFER_BINDING binding point
    The affected APIs are below:
    getIntegeri_v getInteger64i_v BindBuffer BindBufferBase BindBufferRange
    
    BUG=angleproject:1951
    TEST=dEQP-GLES31.functional.state_query.integer.shader_storage_buffer_binding_*
         dEQP-GLES31.functional.state_query.indexed.shader_storage_buffer_*
    
    Change-Id: Ief7186b2ebe305f14e620c31841bc244f84429a5

--- src/libANGLE/validationES3.cpp
+++ src/libANGLE/validationES3.cpp
@@ -1335,6 +1335,20 @@ static bool ValidateBindBufferCommon(Context *context,
                     Error(GL_INVALID_ENUM, "ATOMIC_COUNTER_BUFFER is not supported in GLES3."));
                 return false;
             }
+            if (index >= caps.maxShaderStorageBufferBindings)
+            {
+                context->handleError(Error(GL_INVALID_VALUE,
+                                           "index is greater than or equal to the number of "
+                                           "SHADER_STORAGE_BUFFER indexed binding points."));
+                return false;
+            }
+            if (buffer != 0 && (offset % caps.shaderStorageBufferOffsetAlignment) != 0)
+            {
+                context->handleError(Error(
+                    GL_INVALID_VALUE,
+                    "offset must be multiple of value of SHADER_STORAGE_BUFFER_OFFSET_ALIGNMENT."));
+                return false;
+            }
             break;
         }
         default:
@@ -1993,6 +2007,24 @@ bool ValidateIndexedStateQuery(ValidationContext *context,
                 context->handleError(
                     Error(GL_INVALID_VALUE,
                           "index is outside the valid range for GL_ATOMIC_COUNTER_BUFFER_BINDING"));
+                return false;
+            }
+            break;
+        case GL_SHADER_STORAGE_BUFFER_START:
+        case GL_SHADER_STORAGE_BUFFER_SIZE:
+        case GL_SHADER_STORAGE_BUFFER_BINDING:
+            if (context->getClientVersion() < ES_3_1)
+            {
+                context->handleError(
+                    Error(GL_INVALID_ENUM,
+                          "Shader storage buffers are not supported in this version of GL"));
+                return false;
+            }
+            if (index >= caps.maxShaderStorageBufferBindings)
+            {
+                context->handleError(
+                    Error(GL_INVALID_VALUE,
+                          "index is outside the valid range for GL_SHADER_STORAGE_BUFFER_BINDING"));
                 return false;
             }
             break;
