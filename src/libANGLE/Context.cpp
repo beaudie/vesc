15c92db07c4d9b37557368c6b2b3b402e985ea55
Xinghua Cao <xinghua.cao@intel.com>
2017-03-29 15:36:04 +0800

    ES31: Implement glDispatchCompute for OpenGL backend
    This patch refers to https://chromium-review.googlesource.com/c/380639/
    
    BUG=angleproject:1955
    TESTCASE=angle_end2end_tests
    
    Change-Id: Iafd7a6ba2d71c0b332d9267a1260d9dbd9800c02

--- src/libANGLE/Context.cpp
+++ src/libANGLE/Context.cpp
@@ -3957,4 +3957,10 @@ Error Context::getScratchBuffer(size_t requestedSize, angle::MemoryBuffer **scra
     return gl::NoError();
 }
 
+void Context::dispatchCompute(GLuint numGroupsX, GLuint numGroupsY, GLuint numGroupsZ)
+{
+    mImplementation->dispatchCompute(numGroupsX, numGroupsY, numGroupsZ);
+    return;
+}
+
 }  // namespace gl
