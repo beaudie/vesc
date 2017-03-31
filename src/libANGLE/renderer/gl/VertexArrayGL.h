ad94c9715a04439fb57d3e5706d7b1279ae3754a
Shao <jiawei.shao@intel.com>
2017-03-31 15:13:21 +0800

    ES31: clean up legacy issues in VertexArrayGL
    This patch intends to solve some legacy issues in VertexArrayGL
    before the implementation of ES3.1 feature Vertex Attrib Binding.
    1. Split the code of client memory pointers into an individual
    function.
    2. Remove redundant comparisons in updateAttribPointer.
    3. Substitude some common pieces of code by one function call.
    
    BUG=angleproject:1593
    
    Change-Id: I8f68534bb9291a601b9b77954d7281e5171c2b55

--- src/libANGLE/renderer/gl/VertexArrayGL.h
+++ src/libANGLE/renderer/gl/VertexArrayGL.h
@@ -75,10 +75,20 @@ class VertexArrayGL : public VertexArrayImpl
                                GLsizei instanceCount,
                                const gl::IndexRange &indexRange) const;
 
+    bool isVertexAttribPointerValid(size_t attribIndex) const;
+
     void updateNeedsStreaming(size_t attribIndex);
     void updateAttribEnabled(size_t attribIndex);
     void updateAttribPointer(size_t attribIndex);
     void updateAttribDivisor(size_t attribIndex);
+
+    void callVertexAttribPointer(GLuint attribIndex,
+                                 GLuint size,
+                                 GLenum type,
+                                 GLboolean normalized,
+                                 GLsizei stride,
+                                 GLintptr offset,
+                                 GLboolean pureInteger) const;
 
     const FunctionsGL *mFunctions;
     StateManagerGL *mStateManager;
