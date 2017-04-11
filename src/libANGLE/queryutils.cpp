af5935a8afcd0e46f6cb921c11db409ea5cca83b
jchen10 <jie.a.chen@intel.com>
2017-04-11 13:59:04 +0800

    ES31: Add glGetProgramResourceLocation API
    Add API entry and validation checks(GLES 3.1 section 7.3).
    Add the first 2 interfaces(PROGRAM_INPUT and PROGRAM_OUTPUT) implementation.
    
    BUG=angleproject:1920
    TEST=angle_end2end_tests:ProgramInterfaceTestES31.*
    
    Change-Id: I5128cda43b0d9176c910b036cdc76bf37757670e

--- src/libANGLE/queryutils.cpp
+++ src/libANGLE/queryutils.cpp
@@ -1027,7 +1027,7 @@ GLuint QueryProgramResourceIndex(const Program *program,
         case GL_PROGRAM_OUTPUT:
             return program->getOutputResourceIndex(name);
 
-        // TODO(Jie): more interfaces.
+        // TODO(jie.a.chen@intel.com): more interfaces.
         case GL_UNIFORM:
         case GL_UNIFORM_BLOCK:
         case GL_TRANSFORM_FEEDBACK_VARYING:
@@ -1059,7 +1059,7 @@ void QueryProgramResourceName(const Program *program,
             program->getOutputResourceName(index, bufSize, length, name);
             break;
 
-        // TODO(Jie): more interfaces.
+        // TODO(jie.a.chen@intel.com): more interfaces.
         case GL_UNIFORM:
         case GL_UNIFORM_BLOCK:
         case GL_TRANSFORM_FEEDBACK_VARYING:
@@ -1070,6 +1070,33 @@ void QueryProgramResourceName(const Program *program,
 
         default:
             UNREACHABLE();
+    }
+}
+
+GLint QueryProgramResourceLocation(const Program *program,
+                                   GLenum programInterface,
+                                   const GLchar *name)
+{
+    switch (programInterface)
+    {
+        case GL_PROGRAM_INPUT:
+            return program->getAttributeLocation(name);
+
+        case GL_PROGRAM_OUTPUT:
+            return program->getFragDataLocation(name);
+
+        // TODO(jie.a.chen@intel.com): more interfaces.
+        case GL_UNIFORM:
+        case GL_UNIFORM_BLOCK:
+        case GL_TRANSFORM_FEEDBACK_VARYING:
+        case GL_BUFFER_VARIABLE:
+        case GL_SHADER_STORAGE_BLOCK:
+            UNIMPLEMENTED();
+            return -1;
+
+        default:
+            UNREACHABLE();
+            return -1;
     }
 }
 
