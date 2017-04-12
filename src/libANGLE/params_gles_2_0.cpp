00886f846547f848a0c6413ed3f7b749d7a9898c
Jamie Madill <jmadill@chromium.org>
2017-04-12 08:44:18 -0400

    Use parameter cache to skip passing IndexRange.
    For DrawElements et al., we can use a clever design to store
    entry point parameters and compute index ranges lazily. This allows
    us to compute the index range outside of the validation layer.
    
    Fixing this will let us implement a few things, such as the no error
    extension. It will also allow auto-generation of the entry points,
    since we won't have to have special cases for certain entry
    points. It will also help fix the syncState layering problem. Now the
    cached parameter helper (which is owned by the Context) can make the
    impl layer calls, instead of the validation layer calling the impl
    directly.
    
    BUG=angleproject:747
    
    Change-Id: I871e99e1334cf6e61ef8da62fde3ced094903f8a

---
+++ src/libANGLE/params_gles_2_0.cpp
@@ -0,0 +1,87 @@
+//
+// Copyright 2017 The ANGLE Project Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+// params_gles_2_0:
+//   Parameter wrapper structs for OpenGL ES 2.0. These helpers cache re-used values
+//   in entry point routines.
+
+#include "libANGLE/params_gles_2_0.h"
+
+#include "common/utilities.h"
+#include "libANGLE/Context.h"
+#include "libANGLE/VertexArray.h"
+
+namespace gl
+{
+
+DrawElementsParams::DrawElementsParams(Context *context,
+                                       GLenum mode,
+                                       GLsizei count,
+                                       GLenum type,
+                                       const GLvoid *indices)
+    : ParamsBase(context), mMode(mode), mCount(count), mType(type), mIndices(indices)
+{
+    mTypeInfo = &DrawElementsParams::TypeInfo;
+}
+
+const Optional<IndexRange> &DrawElementsParams::getIndexRange() const
+{
+    if (mIndexRange.valid())
+    {
+        return mIndexRange;
+    }
+
+    const State &state = mContext->getGLState();
+
+    const gl::VertexArray *vao     = state.getVertexArray();
+    gl::Buffer *elementArrayBuffer = vao->getElementArrayBuffer().get();
+
+    if (elementArrayBuffer)
+    {
+        uintptr_t offset = reinterpret_cast<uintptr_t>(mIndices);
+        IndexRange indexRange;
+        Error error =
+            elementArrayBuffer->getIndexRange(mType, static_cast<size_t>(offset), mCount,
+                                              state.isPrimitiveRestartEnabled(), &indexRange);
+        if (error.isError())
+        {
+            mContext->handleError(error);
+            return mIndexRange;
+        }
+
+        mIndexRange = indexRange;
+    }
+    else
+    {
+        mIndexRange = ComputeIndexRange(mType, mIndices, mCount, state.isPrimitiveRestartEnabled());
+    }
+
+    return mIndexRange;
+}
+
+DrawElementsInstancedParams::DrawElementsInstancedParams(Context *context,
+                                                         GLenum mode,
+                                                         GLsizei count,
+                                                         GLenum type,
+                                                         const GLvoid *indices,
+                                                         GLsizei instanceCount)
+    : DrawElementsParams(context, mode, count, type, indices), mInstanceCount(instanceCount)
+{
+    mTypeInfo = &DrawElementsInstancedParams::TypeInfo;
+}
+
+DrawRangeElementsParams::DrawRangeElementsParams(Context *context,
+                                                 GLenum mode,
+                                                 GLuint start,
+                                                 GLuint end,
+                                                 GLsizei count,
+                                                 GLenum type,
+                                                 const GLvoid *indices)
+    : DrawElementsParams(context, mode, count, type, indices), mStart(start), mEnd(end)
+{
+    mTypeInfo = &DrawRangeElementsParams::TypeInfo;
+}
+
+}  // namespace gl
