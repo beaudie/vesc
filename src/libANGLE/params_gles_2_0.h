8061e44ae328757ede72632db8903194b6de70c5
Jamie Madill <jmadill@chromium.org>
2017-04-12 08:44:18 -0400

    Use parameter cache to skip passing IndexRange.
    For DrawElements et al., we can use a clever design to store
    entry point parameters and compute index ranges lazily. This allows
    us to compute the index range outside of the validation layer.
    
    Fixing this will let us implement a few things, such as the no error
    extension. It will also allow auto-generation of the entry points,
    since we won't have to have special cases for certain entry
    points. It will also help fix the syncState layering problem. Now the
    cached parameter helper (which is owned by the Context) can make the
    impl layer calls, instead of the validation layer calling the impl
    directly.
    
    We use a memory buffer in Context to save the parameters in a generic
    way without reallocation on call. We also check type safety by storing
    a type info pointer which can handle inheritance between type classes.
    Optional variables for the cache determine when to re-compute values.
    
    BUG=angleproject:747
    
    Change-Id: I871e99e1334cf6e61ef8da62fde3ced094903f8a

---
+++ src/libANGLE/params_gles_2_0.h
@@ -0,0 +1,165 @@
+//
+// Copyright 2017 The ANGLE Project Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+// params_gles_2_0:
+//   Parameter wrapper structs for OpenGL ES 2.0. These helpers cache re-used values
+//   in entry point routines.
+
+#ifndef LIBANGLE_PARAMS_GLES_2_0_H_
+#define LIBANGLE_PARAMS_GLES_2_0_H_
+
+#include "angle_gl.h"
+#include "common/Optional.h"
+#include "common/angleutils.h"
+#include "common/mathutil.h"
+
+namespace gl
+{
+class Context;
+
+enum class EntryPoint
+{
+    Invalid,
+    DrawArrays,
+    DrawElements,
+    DrawElementsInstanced,
+    DrawElementsInstancedANGLE,
+    DrawRangeElements,
+};
+
+template <EntryPoint EP>
+struct EntryPointParam;
+
+template <EntryPoint EP>
+using EntryPointParamType = typename EntryPointParam<EP>::Type;
+
+class ParamTypeInfo
+{
+  public:
+    constexpr ParamTypeInfo(EntryPoint selfType, const ParamTypeInfo *parentType)
+        : mSelfType(selfType), mParentTypeInfo(parentType)
+    {
+    }
+
+    constexpr bool hasDynamicType(EntryPoint dynamicType) const
+    {
+        return mSelfType == dynamicType ||
+               (mParentTypeInfo && mParentTypeInfo->hasDynamicType(dynamicType));
+    }
+
+  private:
+    EntryPoint mSelfType;
+    const ParamTypeInfo *mParentTypeInfo;
+};
+
+class ParamsBase : angle::NonCopyable
+{
+  public:
+    ParamsBase(Context *context, ...);
+
+    static constexpr ParamTypeInfo TypeInfo = {EntryPoint::Invalid, nullptr};
+
+    constexpr bool hasDynamicType(EntryPoint dynamicType) const
+    {
+        return mTypeInfo->hasDynamicType(dynamicType);
+    }
+
+  protected:
+    const ParamTypeInfo *mTypeInfo;
+    Context *mContext;
+};
+
+class DrawElementsParams : public ParamsBase
+{
+  public:
+    DrawElementsParams(Context *context,
+                       GLenum mode,
+                       GLsizei count,
+                       GLenum type,
+                       const GLvoid *indices);
+
+    const Optional<IndexRange> &getIndexRange() const;
+
+    static constexpr ParamTypeInfo TypeInfo = {EntryPoint::DrawElements, nullptr};
+
+  protected:
+    GLenum mMode;
+    GLsizei mCount;
+    GLenum mType;
+    const GLvoid *mIndices;
+
+    // Cached stuff.
+    mutable Optional<IndexRange> mIndexRange;
+};
+
+class DrawElementsInstancedParams : public DrawElementsParams
+{
+  public:
+    DrawElementsInstancedParams(Context *context,
+                                GLenum mode,
+                                GLsizei count,
+                                GLenum type,
+                                const GLvoid *indices,
+                                GLsizei instanceCount);
+
+    static constexpr ParamTypeInfo TypeInfo = {EntryPoint::DrawElementsInstanced,
+                                               &DrawElementsParams::TypeInfo};
+
+  protected:
+    GLsizei mInstanceCount;
+};
+
+class DrawRangeElementsParams : public DrawElementsParams
+{
+  public:
+    DrawRangeElementsParams(Context *context,
+                            GLenum mode,
+                            GLuint start,
+                            GLuint end,
+                            GLsizei count,
+                            GLenum type,
+                            const GLvoid *indices);
+
+    static constexpr ParamTypeInfo TypeInfo = {EntryPoint::DrawRangeElements,
+                                               &DrawElementsParams::TypeInfo};
+
+  protected:
+    GLuint mStart;
+    GLuint mEnd;
+};
+
+template <>
+struct EntryPointParam<EntryPoint::DrawElements>
+{
+    using Type = DrawElementsParams;
+};
+
+template <>
+struct EntryPointParam<EntryPoint::DrawElementsInstanced>
+{
+    using Type = DrawElementsInstancedParams;
+};
+
+template <>
+struct EntryPointParam<EntryPoint::DrawElementsInstancedANGLE>
+{
+    using Type = DrawElementsInstancedParams;
+};
+
+template <>
+struct EntryPointParam<EntryPoint::DrawRangeElements>
+{
+    using Type = DrawRangeElementsParams;
+};
+
+template <EntryPoint EP>
+struct EntryPointParam
+{
+    using Type = ParamsBase;
+};
+
+}  // namespace gl
+
+#endif  // LIBANGLE_PARAMS_GLES_2_0_H_
