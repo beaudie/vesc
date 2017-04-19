1fa97bef3c22ff7e36d3b8d68bfe945b3b208db6
jchen10 <jie.a.chen@intel.com>
2017-04-19 09:10:56 +0800

    ES31: Implement binding layout for uniform blocks
    The binding point of uniform blocks can be specified in shaders with
    this CL. See spec ESSL 3.10, section 4.4.4, page 58 for more info.
    
    dEQP-GLES31.functional.ubo.* still can't completely pass as
    the missing of arrays-of-arrays feature.
    
    Fix the resource indices speculation errors in a few test cases.
    
    TEST=angle_end2end_tests:UniformBufferTest
    BUG=angleproject:1442
    BUG=angleproject:1920
    
    Change-Id: If95d468fc109834a132b9b817730d3fdc3a615da

--- src/compiler/translator/ParseContext.cpp
+++ src/compiler/translator/ParseContext.cpp
@@ -131,6 +131,7 @@ TParseContext::TParseContext(TSymbolTable &symt,
       mMaxImageUnits(resources.MaxImageUnits),
       mMaxCombinedTextureImageUnits(resources.MaxCombinedTextureImageUnits),
       mMaxUniformLocations(resources.MaxUniformLocations),
+      mMaxUniformBufferBindings(resources.MaxUniformBufferBindings),
       mDeclaringFunction(false)
 {
     mComputeShaderLocalSize.fill(-1);
@@ -1410,6 +1411,15 @@ void TParseContext::checkSamplerBindingIsValid(const TSourceLoc &location,
     if (binding >= 0 && binding + arraySize > mMaxCombinedTextureImageUnits)
     {
         error(location, "sampler binding greater than maximum texture units", "binding");
+    }
+}
+
+void TParseContext::checkBlockBindingIsValid(const TSourceLoc &location, int binding, int arraySize)
+{
+    if (binding + arraySize > mMaxUniformBufferBindings)
+    {
+        error(location, "interface block binding greater than MAX_UNIFORM_BUFFER_BINDINGS",
+              "binding");
     }
 }
 
@@ -2845,8 +2855,22 @@ TIntermDeclaration *TParseContext::addInterfaceBlock(
 
     checkMemoryQualifierIsNotSpecified(typeQualifier.memoryQualifier, typeQualifier.line);
 
-    // TODO(oetuaho): Remove this and support binding for blocks.
-    checkBindingIsNotSpecified(typeQualifier.line, typeQualifier.layoutQualifier.binding);
+    // add array index
+    unsigned int arraySize = 0;
+    if (arrayIndex != nullptr)
+    {
+        arraySize = checkIsValidArraySize(arrayIndexLine, arrayIndex);
+    }
+
+    if (mShaderVersion < 310)
+    {
+        checkBindingIsNotSpecified(typeQualifier.line, typeQualifier.layoutQualifier.binding);
+    }
+    else
+    {
+        checkBlockBindingIsValid(typeQualifier.line, typeQualifier.layoutQualifier.binding,
+                                 arraySize);
+    }
 
     checkYuvIsNotSpecified(typeQualifier.line, typeQualifier.layoutQualifier.yuv);
 
@@ -2912,6 +2936,7 @@ TIntermDeclaration *TParseContext::addInterfaceBlock(
         // check layout qualifiers
         TLayoutQualifier fieldLayoutQualifier = fieldType->getLayoutQualifier();
         checkLocationIsNotSpecified(field->line(), fieldLayoutQualifier);
+        checkBindingIsNotSpecified(field->line(), fieldLayoutQualifier.binding);
 
         if (fieldLayoutQualifier.blockStorage != EbsUnspecified)
         {
@@ -2931,13 +2956,6 @@ TIntermDeclaration *TParseContext::addInterfaceBlock(
         }
 
         fieldType->setLayoutQualifier(fieldLayoutQualifier);
-    }
-
-    // add array index
-    unsigned int arraySize = 0;
-    if (arrayIndex != nullptr)
-    {
-        arraySize = checkIsValidArraySize(arrayIndexLine, arrayIndex);
     }
 
     TInterfaceBlock *interfaceBlock =
