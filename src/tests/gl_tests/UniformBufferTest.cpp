625360959d46471ee8b7b54f5ab93d47e5f20199
jchen10 <jie.a.chen@intel.com>
2017-04-19 09:10:56 +0800

    ES31: Implement binding layout for uniform blocks
    The binding point of uniform blocks can be specified in shaders with
    this CL. See spec ESSL 3.10, section 4.4.4, page 58 for more info.
    
    dEQP-GLES31.functional.ubo.* still can't completely pass as
    the missing of arrays-of-arrays feature. Neither can
    dEQP-GLES31.functional.layout_binding.ubo.* due to the incomplete
    implementation of program interface APIs.
    
    TEST=angle_end2end_tests:UniformBufferTest
    BUG=angleproject:1442
    
    Change-Id: If95d468fc109834a132b9b817730d3fdc3a615da

--- src/tests/gl_tests/UniformBufferTest.cpp
+++ src/tests/gl_tests/UniformBufferTest.cpp
@@ -394,6 +394,7 @@ TEST_P(UniformBufferTest, ActiveUniformNames)
     glGetProgramiv(program, GL_ACTIVE_UNIFORM_MAX_LENGTH, &maxLength);
     std::vector<GLchar> strUniformNameBuffer(maxLength + 1, 0);
     const GLchar *uniformNames[1];
+
     uniformNames[0] = "blockName1.f1";
     glGetUniformIndices(program, 1, uniformNames, &index);
     EXPECT_NE(GL_INVALID_INDEX, index);
@@ -636,6 +637,162 @@ TEST_P(UniformBufferTest, VeryLargeReadback)
     glUnmapBuffer(GL_UNIFORM_BUFFER);
 }
 
+class UniformBufferTest31 : public ANGLETest
+{
+  protected:
+    UniformBufferTest31()
+    {
+        setWindowWidth(128);
+        setWindowHeight(128);
+        setConfigRedBits(8);
+        setConfigGreenBits(8);
+        setConfigBlueBits(8);
+        setConfigAlphaBits(8);
+    }
+};
+
+TEST_P(UniformBufferTest31, MaxUniformBufferBindingsExceeded)
+{
+    GLint maxUniformBufferBindings;
+    glGetIntegerv(GL_MAX_UNIFORM_BUFFER_BINDINGS, &maxUniformBufferBindings);
+    std::string source =
+        "#version 310 es\n"
+        "in vec4 position;\n"
+        "layout(binding = ";
+    std::stringstream ss;
+    ss << maxUniformBufferBindings;
+    source = source + ss.str() +
+             ") uniform uni {\n"
+             "    vec4 color;\n"
+             "};\n"
+             "void main()\n"
+             "{\n"
+             "    gl_Position = position;\n"
+             "}";
+    GLuint shader = CompileShader(GL_VERTEX_SHADER, source);
+    EXPECT_EQ(0u, shader);
+}
+
+// Test uniform block bindings specified in layout shader text work properly.
+TEST_P(UniformBufferTest31, UniformBufferBindings)
+{
+    const std::string &vertexShaderSource =
+        "#version 310 es\n"
+        "in vec4 position;\n"
+        "void main()\n"
+        "{\n"
+        "    gl_Position = position;\n"
+        "}";
+    const std::string &fragmentShaderSource =
+        "#version 310 es\n"
+        "precision highp float;\n"
+        "layout(binding = 2) uniform uni {\n"
+        "    vec4 color;\n"
+        "};\n"
+        "out vec4 fragColor;\n"
+        "void main()\n"
+        "{"
+        "    fragColor = color;\n"
+        "}";
+
+    ANGLE_GL_PROGRAM(program, vertexShaderSource, fragmentShaderSource);
+    GLuint uniformBufferIndex = glGetUniformBlockIndex(program, "uni");
+    ASSERT_NE(GL_INVALID_INDEX, uniformBufferIndex);
+    GLBuffer uniformBuffer;
+
+    int px = getWindowWidth() / 2;
+    int py = getWindowHeight() / 2;
+
+    ASSERT_GL_NO_ERROR();
+
+    // Let's create a buffer which contains one vec4.
+    GLuint vec4Size = 4 * sizeof(float);
+    std::vector<char> v(vec4Size);
+    float *first = reinterpret_cast<float *>(v.data());
+
+    first[0] = 10.f / 255.f;
+    first[1] = 20.f / 255.f;
+    first[2] = 30.f / 255.f;
+    first[3] = 40.f / 255.f;
+
+    glBindBuffer(GL_UNIFORM_BUFFER, uniformBuffer.get());
+    glBufferData(GL_UNIFORM_BUFFER, vec4Size, v.data(), GL_STATIC_DRAW);
+
+    EXPECT_GL_NO_ERROR();
+
+    glBindBufferBase(GL_UNIFORM_BUFFER, 2, uniformBuffer.get());
+    drawQuad(program, "position", 0.5f);
+    EXPECT_GL_NO_ERROR();
+    EXPECT_PIXEL_EQ(px, py, 10, 20, 30, 40);
+
+    // Clear the framebuffer
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    EXPECT_PIXEL_EQ(px, py, 0, 0, 0, 0);
+
+    // Try to bind the buffer to another binding point
+    glUniformBlockBinding(program, uniformBufferIndex, 5);
+    glBindBufferBase(GL_UNIFORM_BUFFER, 5, uniformBuffer.get());
+    drawQuad(program, "position", 0.5f);
+    EXPECT_GL_NO_ERROR();
+    EXPECT_PIXEL_EQ(px, py, 10, 20, 30, 40);
+}
+
+// Test uniform blocks used as instanced array take next binding point for each subsequent element.
+TEST_P(UniformBufferTest31, ConsecutiveBindingsForBlockArray)
+{
+    const std::string &vertexShaderSource =
+        "#version 310 es\n"
+        "in vec4 position;\n"
+        "void main()\n"
+        "{\n"
+        "    gl_Position = position;\n"
+        "}";
+    const std::string &fragmentShaderSource =
+        "#version 310 es\n"
+        "precision highp float;\n"
+        "layout(binding = 2) uniform uni {\n"
+        "    vec4 color;\n"
+        "} blocks[2];\n"
+        "out vec4 fragColor;\n"
+        "void main()\n"
+        "{\n"
+        "    fragColor = blocks[0].color + blocks[1].color;\n"
+        "}";
+
+    ANGLE_GL_PROGRAM(program, vertexShaderSource, fragmentShaderSource);
+    std::array<GLBuffer, 2> uniformBuffers;
+
+    int px = getWindowWidth() / 2;
+    int py = getWindowHeight() / 2;
+
+    ASSERT_GL_NO_ERROR();
+
+    // Let's create a buffer which contains one vec4.
+    GLuint vec4Size = 4 * sizeof(float);
+    std::vector<char> v(vec4Size);
+    float *first = reinterpret_cast<float *>(v.data());
+
+    first[0] = 10.f / 255.f;
+    first[1] = 20.f / 255.f;
+    first[2] = 30.f / 255.f;
+    first[3] = 40.f / 255.f;
+
+    glBindBuffer(GL_UNIFORM_BUFFER, uniformBuffers[0].get());
+    glBufferData(GL_UNIFORM_BUFFER, vec4Size, v.data(), GL_STATIC_DRAW);
+    EXPECT_GL_NO_ERROR();
+    glBindBufferBase(GL_UNIFORM_BUFFER, 2, uniformBuffers[0].get());
+    ASSERT_GL_NO_ERROR();
+    glBindBuffer(GL_UNIFORM_BUFFER, uniformBuffers[1].get());
+    glBufferData(GL_UNIFORM_BUFFER, vec4Size, v.data(), GL_STATIC_DRAW);
+    EXPECT_GL_NO_ERROR();
+    glBindBufferBase(GL_UNIFORM_BUFFER, 3, uniformBuffers[1].get());
+
+    drawQuad(program, "position", 0.5f);
+    EXPECT_GL_NO_ERROR();
+    EXPECT_PIXEL_EQ(px, py, 20, 40, 60, 80);
+}
+
 // Use this to select which configurations (e.g. which renderer, which GLES major version) these tests should be run against.
 ANGLE_INSTANTIATE_TEST(UniformBufferTest,
                        ES3_D3D11(),
@@ -643,5 +800,6 @@ ANGLE_INSTANTIATE_TEST(UniformBufferTest,
                        ES3_D3D11_FL11_1_REFERENCE(),
                        ES3_OPENGL(),
                        ES3_OPENGLES());
+ANGLE_INSTANTIATE_TEST(UniformBufferTest31, ES31_D3D11(), ES31_OPENGL(), ES31_OPENGLES());
 
 } // namespace
