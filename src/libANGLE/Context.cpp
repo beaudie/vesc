38bc2a43445b4f3af000763545c773a5e30f26c2
Jamie Madill <jmadill@chromium.org>
2017-04-19 14:54:04 -0400

    GLES2: Clean up entry points for auto-gen.
    This cleans up a few things:
     * refactors a few remaining missed EPs
     * removes unnecessary includes
     * rename one Context entry point
     * reformat the entry points file
     * moves some ES2 EP validation to validationES2
    
    The last item makes for a significant diff, but this is a refactor
    change only, with no functionality change.
    
    BUG=angleproject:747
    
    Change-Id: I7860cc4b6260b6c22faa5f2885297333c0cdb4ed

--- src/libANGLE/Context.cpp
+++ src/libANGLE/Context.cpp
@@ -4242,7 +4242,7 @@ void Context::getProgramiv(GLuint program, GLenum pname, GLint *params)
     QueryProgramiv(programObject, pname, params);
 }
 
-void Context::getInfoLog(GLuint program, GLsizei bufsize, GLsizei *length, GLchar *infolog)
+void Context::getProgramInfoLog(GLuint program, GLsizei bufsize, GLsizei *length, GLchar *infolog)
 {
     Program *programObject = getProgram(program);
     ASSERT(programObject);
