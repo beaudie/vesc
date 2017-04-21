7da9b3d059677dfcb764ad2854ad6d43a758d249
Shao <jiawei.shao@intel.com>
2017-04-21 15:36:54 +0800

    ES31: Add Geometry Shader compilation on GLSL compiler
    This patch intends to implement the compilation of Geometry Shader
    based on OpenGL ES 3.1 extension EXT_geometry_shader in ANGLE GLSL
    compiler.
    
    1. Add support to the shader type GL_GEOMETRY_SHADER_EXT.
    2. Implement compilation of Geometry Shader in ANGLE GLSL compiler:
    (1) Add support to OpenGL ES 3.1 extension "EXT_geometry_shader".
    (2) Add all built-in constants, variables and functions required in
        the Geometry Shader.
    (3) Add validations of the input and output primitive declarations
        in the layouts of the Geometry Shader.
    (4) Only accept arrays as the inputs of the Geometry Shader.
    (5) Allow unsized arrays as the inputs of the Geometry Shader and
        assign size to them if a legal input primitive or an array input
        with legal size is previously defined.
    (6) Split the original 'varying' to 'inVarying' and 'outVarying' to
        store the input and output variables of the Geometry Shader.
    (7) Allow the interface block 'gl_in' as a built-in input varying.
    (8) Add support to "centroid" qualifier and interpolation qualifiers
        on the input and output of the Geometry Shader.
    3. Add unit tests to cover all the new features added in this patch.
    
    BUG=angleproject:1941
    TEST=angle_unittests
    
    Change-Id: Ib349acddc4d3d8f85152cf1b420428368bd9bece

--- src/compiler/translator/Compiler.h
+++ src/compiler/translator/Compiler.h
@@ -110,8 +110,9 @@ class TCompiler : public TShHandleBase
     const std::vector<sh::Attribute> &getAttributes() const { return attributes; }
     const std::vector<sh::OutputVariable> &getOutputVariables() const { return outputVariables; }
     const std::vector<sh::Uniform> &getUniforms() const { return uniforms; }
-    const std::vector<sh::Varying> &getVaryings() const { return varyings; }
     const std::vector<sh::InterfaceBlock> &getInterfaceBlocks() const { return interfaceBlocks; }
+    const std::vector<sh::Varying> &getInputVaryings() const { return inVaryings; }
+    const std::vector<sh::Varying> &getOutputVaryings() const { return outVaryings; }
 
     ShHashFunction64 getHashFunction() const { return hashFunction; }
     NameMap &getNameMap() { return nameMap; }
@@ -125,8 +126,20 @@ class TCompiler : public TShHandleBase
     // Get the resources set by InitBuiltInSymbolTable
     const ShBuiltInResources &getResources() const;
 
-  protected:
+    int getGeometryShaderMaxVertices() const { return mGeometryMaxVertices; }
+    int getGeometryShaderInvocations() const { return mGeometryInvocations; }
+    TLayoutGeometryShaderEXT getGeometryShaderInputPrimitives() const
+    {
+        return mGeometryPrimitiveIn;
+    }
+    TLayoutGeometryShaderEXT getGeometryShaderOutputPrimitives() const
+    {
+        return mGeometryPrimitiveOut;
+    }
+
     sh::GLenum getShaderType() const { return shaderType; }
+
+  protected:
     // Initialize symbol-table with built-in symbols.
     bool InitBuiltInSymbolTable(const ShBuiltInResources &resources);
     // Compute the string representation of the built-in resources
@@ -179,8 +192,10 @@ class TCompiler : public TShHandleBase
     std::vector<sh::OutputVariable> outputVariables;
     std::vector<sh::Uniform> uniforms;
     std::vector<sh::ShaderVariable> expandedUniforms;
-    std::vector<sh::Varying> varyings;
+
     std::vector<sh::InterfaceBlock> interfaceBlocks;
+    std::vector<sh::Varying> inVaryings;
+    std::vector<sh::Varying> outVaryings;
 
   private:
     // Creates the function call DAG for further analysis, returning false if there is a recursion
@@ -248,6 +263,12 @@ class TCompiler : public TShHandleBase
 
     // GL_OVR_multiview num_views.
     int mNumViews;
+
+    // geometry shader parameters
+    int mGeometryMaxVertices;
+    int mGeometryInvocations;
+    TLayoutGeometryShaderEXT mGeometryPrimitiveIn;
+    TLayoutGeometryShaderEXT mGeometryPrimitiveOut;
 
     // name hashing.
     ShHashFunction64 hashFunction;
