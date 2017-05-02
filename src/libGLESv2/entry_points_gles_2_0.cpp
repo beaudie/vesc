02839d8115b2bfbdcb95726d5fe50c40cb2d7dc8
Jamie Madill <jmadill@chromium.org>
2017-05-02 15:41:42 -0400

    GLES2: Clean up entry points for auto-gen.
    This cleans up a few things:
     * refactors a few remaining missed EPs
     * removes unnecessary includes
     * rename one Context entry point
     * moves some ES2 EP validation to validationES2
    
    The last item makes for a significant diff, but this is a refactor
    change only, with no functionality change.
    
    BUG=angleproject:747
    
    Change-Id: I7860cc4b6260b6c22faa5f2885297333c0cdb4ed

--- src/libGLESv2/entry_points_gles_2_0.cpp
+++ src/libGLESv2/entry_points_gles_2_0.cpp
@@ -8,29 +8,10 @@
 
 #include "libGLESv2/entry_points_gles_2_0_autogen.h"
 
-#include "libGLESv2/global_state.h"
-
-#include "libANGLE/formatutils.h"
-#include "libANGLE/Buffer.h"
-#include "libANGLE/Compiler.h"
-#include "libANGLE/Context.h"
-#include "libANGLE/Error.h"
-#include "libANGLE/Framebuffer.h"
-#include "libANGLE/Renderbuffer.h"
-#include "libANGLE/Shader.h"
-#include "libANGLE/Program.h"
-#include "libANGLE/Texture.h"
-#include "libANGLE/VertexArray.h"
-#include "libANGLE/FramebufferAttachment.h"
-
-#include "libANGLE/validationES.h"
-#include "libANGLE/validationES2.h"
-#include "libANGLE/validationES3.h"
-#include "libANGLE/queryconversions.h"
-#include "libANGLE/queryutils.h"
-
 #include "common/debug.h"
-#include "common/utilities.h"
+#include "libANGLE/Context.h"
+#include "libANGLE/validationES2.h"
+#include "libGLESv2/global_state.h"
 
 namespace gl
 {
@@ -1117,9 +1098,8 @@ void GL_APIENTRY GetFramebufferAttachmentParameteriv(GLenum target, GLenum attac
     Context *context = GetValidGlobalContext();
     if (context)
     {
-        if (!context->skipValidation() &&
-            !ValidateGetFramebufferAttachmentParameteriv(context, target, attachment, pname,
-                                                         nullptr))
+        if (!context->skipValidation() && !ValidateGetFramebufferAttachmentParameteriv(
+                                              context, target, attachment, pname, params))
         {
             return;
         }
@@ -1151,8 +1131,7 @@ void GL_APIENTRY GetProgramiv(GLuint program, GLenum pname, GLint* params)
     Context *context = GetValidGlobalContext();
     if (context)
     {
-        if (!context->skipValidation() &&
-            !ValidateGetProgramiv(context, program, pname, nullptr))
+        if (!context->skipValidation() && !ValidateGetProgramiv(context, program, pname, params))
         {
             return;
         }
@@ -1175,7 +1154,7 @@ void GL_APIENTRY GetProgramInfoLog(GLuint program, GLsizei bufsize, GLsizei* len
             return;
         }
 
-        context->getInfoLog(program, bufsize, length, infolog);
+        context->getProgramInfoLog(program, bufsize, length, infolog);
     }
 }
 
@@ -2028,7 +2007,7 @@ void GL_APIENTRY Uniform1i(GLint location, GLint x)
     Context *context = GetValidGlobalContext();
     if (context)
     {
-        if (!context->skipValidation() && !ValidateUniform1iv(context, location, 1, &x))
+        if (!context->skipValidation() && !ValidateUniform1i(context, location, x))
         {
             return;
         }
