01fc5eafc163b547e1cbb3cd509130f02ea44863
Olli Etuaho <oetuaho@nvidia.com>
2017-05-08 18:22:22 +0300

    Share a single TOperator enum among all constructor AST nodes
    The code is a lot simpler when the type information is only carried
    in the TType of the node, instead of being partially duplicated in the
    enum value.
    
    BUG=angleproject:1490
    TEST=angle_unittests
    
    Change-Id: I956376225ec01e469c7afb7378fa48cc097c0cea

--- src/compiler/translator/util.h
+++ src/compiler/translator/util.h
@@ -44,7 +44,6 @@ TString ArrayString(const TType &type);
 TType GetShaderVariableBasicType(const sh::ShaderVariable &var);
 
 bool CanBeConstructed(TBasicType type);
-TOperator TypeToConstructorOperator(const TType &type);
 
 bool IsBuiltinOutputVariable(TQualifier qualifier);
 bool IsBuiltinFragmentInputVariable(TQualifier qualifier);
