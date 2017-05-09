bbd91a97026ba32d38d1d74cffc81b6880951c36
Jamie Madill <jmadill@chromium.org>
2017-05-09 15:39:53 -0400

    D3D11: Use TextureHelper11 everywhere.
    This consolidates all texture allocation into ResourceManager11.
    It removes a lot of error checking and resource management code.
    In a few places we're storing some redundant information, like in
    TextureStorage11, we might store the Format pointer in two places,
    or the Texture's size.
    
    BUG=angleproject:2034
    
    Change-Id: I9369e76925a67632c444c662e5667c5ed7875547

--- src/libANGLE/renderer/d3d/d3d11/Renderer11.h
+++ src/libANGLE/renderer/d3d/d3d11/Renderer11.h
@@ -389,10 +389,16 @@ class Renderer11 : public RendererD3D
 
     gl::Version getMaxSupportedESVersion() const override;
 
-    gl::ErrorOrResult<TextureHelper11> createStagingTexture(GLenum textureType,
+    gl::ErrorOrResult<TextureHelper11> createStagingTexture(ResourceType textureType,
                                                             const d3d11::Format &formatSet,
                                                             const gl::Extents &size,
                                                             StagingAccess readAndWriteAccess);
+
+    template <typename DescT, typename ResourceT>
+    gl::Error allocateResource(const DescT &desc, ResourceT *resourceOut)
+    {
+        return mResourceManager11.allocate(this, &desc, nullptr, resourceOut);
+    }
 
     template <typename DescT, typename InitDataT, typename ResourceT>
     gl::Error allocateResource(const DescT &desc, InitDataT *initData, ResourceT *resourceOut)
@@ -405,6 +411,24 @@ class Renderer11 : public RendererD3D
     {
         return mResourceManager11.allocate(this, nullptr, initData, resourceOut);
     }
+
+    template <typename DescT>
+    gl::Error allocateTexture(const DescT &desc,
+                              const d3d11::Format &format,
+                              TextureHelper11 *textureOut)
+    {
+        return allocateTexture(desc, format, nullptr, textureOut);
+    }
+
+    gl::Error allocateTexture(const D3D11_TEXTURE2D_DESC &desc,
+                              const d3d11::Format &format,
+                              const D3D11_SUBRESOURCE_DATA *initData,
+                              TextureHelper11 *textureOut);
+
+    gl::Error allocateTexture(const D3D11_TEXTURE3D_DESC &desc,
+                              const d3d11::Format &format,
+                              const D3D11_SUBRESOURCE_DATA *initData,
+                              TextureHelper11 *textureOut);
 
   protected:
     gl::Error clearTextures(gl::SamplerType samplerType, size_t rangeStart, size_t rangeEnd) override;
