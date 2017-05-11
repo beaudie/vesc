4627151c3d5aac4604711111d20f7e3bc6b3a336
Olli Etuaho <oetuaho@nvidia.com>
2017-05-11 19:14:35 +0300

    Initialize locals and globals
    Guarantee that local and global variables are initialized before they
    are used in GLSL output. In HLSL output all variables were already
    being initialized.
    
    Initializing globals is done by the DeferGlobalInitializers AST
    transformation, which makes sure that the global variable init
    proceeds in the correct order. This is important since some global
    variables may use other uninitialized globals in their initializers.
    
    There's a separate AST transform for initializing locals. In case
    we're dealing with ESSL 1.00 which lacks array constructors, the local
    variable init can only be run after some simplification of the AST, so
    that it is able to handle complex cases like:
    
    for (int i[2], j = i[0]; i[0] < 3; ++i[0]) {
    }
    
    In this kind of case the newly created array initialization code needs
    to be hoisted out of the loop init statement, and the code also needs
    to make sure that j's initializer is run after i is initialized.
    
    BUG=angleproject:1966
    TEST=WebGL conformance tests, angle_end2end_tests
    
    Change-Id: Ic06927f5b6cc9619bc82c647ee966605cd80bab2

--- src/compiler/translator/InitializeVariables.h
+++ src/compiler/translator/InitializeVariables.h
@@ -9,23 +9,31 @@
 
 #include <GLSLANG/ShaderLang.h>
 
+// TODO: Maybe reorg code so that CreateInitCode can live somewhere else than here. Nicer if
+// IntermNode.h wouldn't need to be included in this header.
+#include "compiler/translator/IntermNode.h"
+
 namespace sh
 {
-class TIntermBlock;
 class TSymbolTable;
 
 typedef std::vector<sh::ShaderVariable> InitVariableList;
+
+TIntermSequence *CreateInitCode(TIntermSymbol *initializedSymbol);
+
+// Initialize all uninitialized local variables, so that undefined behavior is avoided.
+void InitializeUninitializedLocals(TIntermBlock *root, int shaderVersion);
 
 // Currently this function is only capable of initializing variables of basic types,
 // array of basic types, or struct of basic types.
 // For now it is used for the following two scenarios:
 //   1. initializing gl_Position;
-//   2. initializing ESSL 3.00 shaders' output variables (which might be structs).
-// Specifically, it's not feasible to make it work for local variables because if their
-// types are structs, we can't look into TSymbolTable to find the TType data.
+//   2. initializing ESSL 3.00 shaders' output variables (which might be structs, but not arrays of
+//   structs).
 void InitializeVariables(TIntermBlock *root,
                          const InitVariableList &vars,
                          const TSymbolTable &symbolTable);
+
 }  // namespace sh
 
 #endif  // COMPILER_TRANSLATOR_INITIALIZEVARIABLES_H_
