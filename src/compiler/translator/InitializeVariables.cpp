4233ab0cd3e5e736c470082fa1d061d672168ba1
Olli Etuaho <oetuaho@nvidia.com>
2017-05-11 19:14:35 +0300

    Initialize locals and globals
    Guarantee that local and global variables are initialized before they
    are used in GLSL output. In HLSL output all variables were already
    being initialized.
    
    Initializing globals is done by the DeferGlobalInitializers AST
    transformation, which makes sure that the global variable init
    proceeds in the correct order. This is important since some global
    variables may use other uninitialized globals in their initializers.
    
    There's a separate AST transform for initializing locals. In case
    we're dealing with ESSL 1.00 which lacks array constructors, the local
    variable init can only be run after some simplification of the AST, so
    that it is able to handle complex cases like:
    
    for (int i[2], j = i[0]; i[0] < 3; ++i[0]) {
    }
    
    In this kind of case the uninitialized array initialization code needs
    to be hoisted out of the loop init statement, and the code also needs
    to make sure that j's initializer is run after i is initialized.
    
    BUG=angleproject:1966
    TEST=WebGL conformance tests, angle_end2end_tests
    
    Change-Id: Ic06927f5b6cc9619bc82c647ee966605cd80bab2

--- src/compiler/translator/InitializeVariables.cpp
+++ src/compiler/translator/InitializeVariables.cpp
@@ -19,7 +19,67 @@ namespace sh
 namespace
 {
 
-void InsertInitCode(TIntermSequence *sequence,
+TIntermSequence *CreateArrayZeroInitSequence(TIntermTyped *initializedNode);
+
+TIntermBinary *CreateZeroInitAssignment(TIntermTyped *initializedNode)
+{
+    TIntermTyped *zero = TIntermTyped::CreateZero(initializedNode->getType());
+    return new TIntermBinary(EOpAssign, initializedNode, zero);
+}
+
+TIntermSequence *CreateStructZeroInitSequence(TIntermTyped *initializedNode)
+{
+    ASSERT(initializedNode->getBasicType() == EbtStruct);
+    TStructure *structType        = initializedNode->getType().getStruct();
+    TIntermSequence *initSequence = new TIntermSequence();
+    for (int i = 0; i < structType->fields().size(); ++i)
+    {
+        TIntermBinary *element = new TIntermBinary(
+            EOpIndexDirectStruct, initializedNode->deepCopy(), TIntermTyped::CreateIndexNode(i));
+        if (element->isArray())
+        {
+            TIntermSequence *arrayInit = CreateArrayZeroInitSequence(element);
+            initSequence->insert(initSequence->end(), arrayInit->begin(), arrayInit->end());
+        }
+        else if (element->getType().isStructureContainingArrays())
+        {
+            TIntermSequence *structInit = CreateStructZeroInitSequence(element);
+            initSequence->insert(initSequence->end(), structInit->begin(), structInit->end());
+        }
+        else
+        {
+            initSequence->push_back(CreateZeroInitAssignment(element));
+        }
+    }
+    return initSequence;
+}
+
+TIntermSequence *CreateArrayZeroInitSequence(TIntermTyped *initializedNode)
+{
+    ASSERT(initializedNode->isArray());
+    // Assign the array elements one by one to keep the AST compatible with ESSL 1.00 which
+    // doesn't have array assignment.
+    // Note that it is important to have the array init in the right order to workaround
+    // http://crbug.com/709317
+    TIntermSequence *initSequence = new TIntermSequence();
+    for (unsigned int i = 0; i < initializedNode->getArraySize(); ++i)
+    {
+        TIntermBinary *element = new TIntermBinary(EOpIndexDirect, initializedNode->deepCopy(),
+                                                   TIntermTyped::CreateIndexNode(i));
+        if (element->getType().isStructureContainingArrays())
+        {
+            TIntermSequence *structInit = CreateStructZeroInitSequence(element);
+            initSequence->insert(initSequence->end(), structInit->begin(), structInit->end());
+        }
+        else
+        {
+            initSequence->push_back(CreateZeroInitAssignment(element));
+        }
+    }
+    return initSequence;
+}
+
+void InsertInitCode(TIntermSequence *mainBody,
                     const InitVariableList &variables,
                     const TSymbolTable &symbolTable)
 {
@@ -27,61 +87,106 @@ void InsertInitCode(TIntermSequence *sequence,
     {
         TString name = TString(var.name.c_str());
 
+        TIntermSymbol *initializedSymbol = nullptr;
         if (var.isArray())
         {
-            // Assign the array elements one by one to keep the AST compatible with ESSL 1.00 which
-            // doesn't have array assignment.
             size_t pos = name.find_last_of('[');
             if (pos != TString::npos)
             {
                 name = name.substr(0, pos);
             }
-            TType elementType = sh::GetShaderVariableBasicType(var);
-            TType arrayType   = elementType;
+            TType arrayType = sh::GetShaderVariableBasicType(var);
             arrayType.setArraySize(var.elementCount());
-
-            // Workaround for http://crbug.com/709317
-            //   This loop is reversed to initialize elements in increasing
-            // order [0 1 2 ...]. Otherwise, they're initialized in
-            // decreasing order [... 2 1 0], due to
-            // `sequence->insert(sequence->begin(), ...)` below.
-            for (unsigned int i = var.arraySize; i > 0; --i)
-            {
-                unsigned int index = i - 1;
-                TIntermSymbol *arraySymbol = new TIntermSymbol(0, name, arrayType);
-                TIntermBinary *element     = new TIntermBinary(EOpIndexDirect, arraySymbol,
-                                                           TIntermTyped::CreateIndexNode(index));
-
-                TIntermTyped *zero        = TIntermTyped::CreateZero(elementType);
-                TIntermBinary *assignment = new TIntermBinary(EOpAssign, element, zero);
-
-                sequence->insert(sequence->begin(), assignment);
-            }
+            initializedSymbol = new TIntermSymbol(0, name, arrayType);
         }
         else if (var.isStruct())
         {
             TVariable *structInfo = reinterpret_cast<TVariable *>(symbolTable.findGlobal(name));
             ASSERT(structInfo);
 
-            TIntermSymbol *symbol = new TIntermSymbol(0, name, structInfo->getType());
-            TIntermTyped *zero    = TIntermTyped::CreateZero(structInfo->getType());
-
-            TIntermBinary *assign = new TIntermBinary(EOpAssign, symbol, zero);
-            sequence->insert(sequence->begin(), assign);
+            initializedSymbol = new TIntermSymbol(0, name, structInfo->getType());
         }
         else
         {
-            TType type            = sh::GetShaderVariableBasicType(var);
-            TIntermSymbol *symbol = new TIntermSymbol(0, name, type);
-            TIntermTyped *zero    = TIntermTyped::CreateZero(type);
-
-            TIntermBinary *assign = new TIntermBinary(EOpAssign, symbol, zero);
-            sequence->insert(sequence->begin(), assign);
+            TType type        = sh::GetShaderVariableBasicType(var);
+            initializedSymbol = new TIntermSymbol(0, name, type);
         }
+        TIntermSequence *initCode = CreateInitCode(initializedSymbol);
+        mainBody->insert(mainBody->begin(), initCode->begin(), initCode->end());
     }
 }
 
+class InitializeLocalsTraverser : public TIntermTraverser
+{
+  public:
+    InitializeLocalsTraverser(int shaderVersion)
+        : TIntermTraverser(true, false, false), mShaderVersion(shaderVersion)
+    {
+    }
+
+  protected:
+    bool visitDeclaration(Visit visit, TIntermDeclaration *node) override
+    {
+        for (TIntermNode *declarator : *node->getSequence())
+        {
+            if (!mInGlobalScope && !declarator->getAsBinaryNode())
+            {
+                TIntermSymbol *symbol = declarator->getAsSymbolNode();
+                ASSERT(symbol);
+                if (symbol->getSymbol() == "")
+                {
+                    continue;
+                }
+                if ((symbol->isArray() || symbol->getType().isStructureContainingArrays()) &&
+                    mShaderVersion == 100)
+                {
+                    // Initialize the array one element at a time, since ESSL 1.00 does not support
+                    // assigning arrays.
+                    // SeparateDeclarations should have already been run, so we don't need to worry
+                    // about further declarators in this declaration depending on the effects of
+                    // this declarator.
+                    ASSERT(node->getSequence()->size() == 1);
+                    insertStatementsInParentBlock(TIntermSequence(), *CreateInitCode(symbol));
+                }
+                else
+                {
+                    TIntermBinary *init = new TIntermBinary(
+                        EOpInitialize, symbol, TIntermTyped::CreateZero(symbol->getType()));
+                    queueReplacementWithParent(node, symbol, init, OriginalNode::BECOMES_CHILD);
+                }
+            }
+        }
+        return false;
+    }
+
+  private:
+    int mShaderVersion;
+};
+
 }  // namespace anonymous
+
+TIntermSequence *CreateInitCode(TIntermSymbol *initializedSymbol)
+{
+    if (initializedSymbol->getType().isStructureContainingArrays())
+    {
+        return CreateStructZeroInitSequence(initializedSymbol);
+    }
+    if (initializedSymbol->isArray())
+    {
+        return CreateArrayZeroInitSequence(initializedSymbol);
+    }
+
+    TIntermSequence *initCode = new TIntermSequence();
+    initCode->push_back(CreateZeroInitAssignment(initializedSymbol));
+    return initCode;
+}
+
+void InitializeUninitializedLocals(TIntermBlock *root, int shaderVersion)
+{
+    InitializeLocalsTraverser traverser(shaderVersion);
+    root->traverse(&traverser);
+    traverser.updateTree();
+}
 
 void InitializeVariables(TIntermBlock *root,
                          const InitVariableList &vars,
