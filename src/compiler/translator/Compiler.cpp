b34f16e8eff90e995573a57feed25cd3ce6ffed6
Olli Etuaho <oetuaho@nvidia.com>
2017-05-19 14:18:55 +0300

    Initialize uninitialized globals in GLSL output
    Initializing globals is done by the DeferGlobalInitializers AST
    transformation, which makes sure that the global variable init
    proceeds in the correct order. This is important since some global
    variables may use other uninitialized globals in their initializers.
    
    The global variable init is only done in case the WebGL shader spec is
    being followed. This avoids adding global initializers twice in case
    the same shader goes through multiple translations, assuming only one
    of them is a WebGL shader translation.
    
    BUG=angleproject:1966
    TEST=angle_end2end_tests, WebGL conformance tests
    
    Change-Id: Iea701d3ae40edc906abd87303f2aa27fd23bf55a

--- src/compiler/translator/Compiler.cpp
+++ src/compiler/translator/Compiler.cpp
@@ -472,7 +472,7 @@ TIntermBlock *TCompiler::compileTreeImpl(const char *const shaderStrings[],
 
         if (success)
         {
-            DeferGlobalInitializers(root);
+            DeferGlobalInitializers(root, needInitializeGlobalsInAST());
         }
     }
 
