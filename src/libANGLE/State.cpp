103b6dfa4258aa86b93b19de3ec2d6889c637a88
Jamie Madill <jmadill@chromium.org>
2017-06-09 14:22:17 -0400

    Smart caching of VAO input layout.
    Don't recompute the cached attribute layout for a program if the
    vertex array info hasn't changed. We can use the Serial class to
    know when a vertex array has identical state.
    
    BUG=angleproject:1156
    
    Change-Id: Ia11f6ac268f63c3299f6d6d80c2866009cb8429c

--- src/libANGLE/State.cpp
+++ src/libANGLE/State.cpp
@@ -1069,6 +1069,8 @@ void State::setProgram(const Context *context, Program *newProgram)
         {
             newProgram->addRef();
         }
+        mDirtyBits.set(DIRTY_BIT_PROGRAM_EXECUTABLE);
+        mDirtyBits.set(DIRTY_BIT_PROGRAM_BINDING);
     }
 }
 
@@ -2112,9 +2114,6 @@ void State::syncDirtyObjects(const Context *context, const DirtyObjects &bitset)
                 ASSERT(mVertexArray);
                 mVertexArray->syncImplState(context);
                 break;
-            case DIRTY_OBJECT_PROGRAM:
-                // TODO(jmadill): implement this
-                break;
             default:
                 UNREACHABLE();
                 break;
@@ -2143,9 +2142,6 @@ void State::syncDirtyObject(const Context *context, GLenum target)
         case GL_VERTEX_ARRAY:
             localSet.set(DIRTY_OBJECT_VERTEX_ARRAY);
             break;
-        case GL_PROGRAM:
-            localSet.set(DIRTY_OBJECT_PROGRAM);
-            break;
     }
 
     syncDirtyObjects(context, localSet);
@@ -2168,9 +2164,18 @@ void State::setObjectDirty(GLenum target)
         case GL_VERTEX_ARRAY:
             mDirtyObjects.set(DIRTY_OBJECT_VERTEX_ARRAY);
             break;
-        case GL_PROGRAM:
-            mDirtyObjects.set(DIRTY_OBJECT_PROGRAM);
-            break;
+    }
+}
+
+void State::onProgramExecutableChange(Program *program)
+{
+    // OpenGL Spec:
+    // "If LinkProgram or ProgramBinary successfully re-links a program object
+    //  that was already in use as a result of a previous call to UseProgram, then the
+    //  generated executable code will be installed as part of the current rendering state."
+    if (program->isLinked() && mProgram == program)
+    {
+        mDirtyBits.set(DIRTY_BIT_PROGRAM_EXECUTABLE);
     }
 }
 
