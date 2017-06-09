7eb67a23a5e104d69834fe36e0b31ac76fa52e89
Jamie Madill <jmadill@chromium.org>
2017-06-08 20:09:02 -0400

    Proliferate gl::Context everywhere.
    BUG=angleproject:1156
    
    Change-Id: Ie4c32ad6bf6caaff0289901f30b5c6bafa2ce259

--- src/libGLESv2/entry_points_gles_2_0_ext.cpp
+++ src/libGLESv2/entry_points_gles_2_0_ext.cpp
@@ -1049,7 +1049,7 @@ ANGLE_EXPORT void GL_APIENTRY EGLImageTargetTexture2DOES(GLenum target, GLeglIma
         }
 
         Texture *texture = context->getTargetTexture(target);
-        Error error      = texture->setEGLImageTarget(target, imageObject);
+        Error error      = texture->setEGLImageTarget(context, target, imageObject);
         if (error.isError())
         {
             context->handleError(error);
@@ -1075,7 +1075,7 @@ ANGLE_EXPORT void GL_APIENTRY EGLImageTargetRenderbufferStorageOES(GLenum target
         }
 
         Renderbuffer *renderbuffer = context->getGLState().getCurrentRenderbuffer();
-        Error error                = renderbuffer->setStorageEGLImageTarget(imageObject);
+        Error error                = renderbuffer->setStorageEGLImageTarget(context, imageObject);
         if (error.isError())
         {
             context->handleError(error);
@@ -2506,7 +2506,7 @@ ANGLE_EXPORT void GL_APIENTRY TexParameterfvRobustANGLE(GLenum target,
         }
 
         Texture *texture = context->getTargetTexture(target);
-        SetTexParameterfv(texture, pname, params);
+        SetTexParameterfv(context, texture, pname, params);
     }
 }
 
@@ -2529,7 +2529,7 @@ ANGLE_EXPORT void GL_APIENTRY TexParameterivRobustANGLE(GLenum target,
         }
 
         Texture *texture = context->getTargetTexture(target);
-        SetTexParameteriv(texture, pname, params);
+        SetTexParameteriv(context, texture, pname, params);
     }
 }
 
