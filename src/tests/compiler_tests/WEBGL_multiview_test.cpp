eaa11e8077e453653f74ed17d4cc2d0c1537ddf2
Martin Radev <mradev@nvidia.com>
2017-07-05 17:11:06 +0300

    Populate gl_InstanceID attribute information explicitly
    While compiling ESSL1 shaders, with the compiler having both
    SH_INITIALIZE_BUILTINS_FOR_INSTANCED_MULTIVIEW and SH_VARIABLES
    set, variable collection terminates with an assertion failure. The reason
    behind this is that SH_INITIALIZE_BUILTINS_FOR_INSTANCED_MULTIVIEW
    adds gl_InstanceID to the AST to initialize the multiview builtins, but
    the variable collection pass cannot find gl_InstanceID information in
    the symbol table because the builtin is only available in ESSL 3.00 and
    greater.
    
    To address this the patch populates the gl_InstanceID attribute
    information explicitly in the variable collection pass instead of
    retrieving it from the symbol table.
    
    BUG=angleproject:2062
    TEST=angle_unittests
    
    Change-Id: I5ecb9967ebe6658e956d17a2637090f9b685ef33

--- src/tests/compiler_tests/WEBGL_multiview_test.cpp
+++ src/tests/compiler_tests/WEBGL_multiview_test.cpp
@@ -692,6 +692,30 @@ TEST_F(WEBGLMultiviewVertexShaderOutputCodeTest, ViewIDAndInstanceIDHaveCorrectV
     EXPECT_TRUE(foundInHLSLCode("InstanceID = (gl_InstanceID / 3)"));
 }
 
+// Test that gl_InstanceID is collected in an ESSL1 shader if the
+// SH_INITIALIZE_BUILTINS_FOR_INSTANCED_MULTIVIEW option is set.
+TEST_F(WEBGLMultiviewVertexShaderTest, InstaceIDCollectedESSL1)
+{
+    const std::string &shaderString =
+        "#extension GL_OVR_multiview2 : require\n"
+        "layout(num_views = 2) in;\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position.x = gl_ViewID_OVR == 0 ? 0. : 1.;\n"
+        "   gl_Position.yzw = vec3(0., 0., 1.);\n"
+        "}\n";
+    mExtraCompileOptions |= SH_INITIALIZE_BUILTINS_FOR_INSTANCED_MULTIVIEW;
+    mExtraCompileOptions |= SH_VARIABLES;
+    compileAssumeSuccess(shaderString);
+
+    const std::vector<Attribute> &attributes = getAttributes();
+    bool isGLInstanceIDFound                 = false;
+    for (size_t i = 0u; i < attributes.size() && !isGLInstanceIDFound; ++i)
+    {
+        isGLInstanceIDFound = (attributes[i].name == "gl_InstanceID");
+    }
+    EXPECT_TRUE(isGLInstanceIDFound);
+}
 // The test checks that the directive enabling GL_OVR_multiview is not outputted if the extension is
 // emulated.
 TEST_F(WEBGLMultiviewVertexShaderOutputCodeTest, StrippedOVRMultiviewDirective)
