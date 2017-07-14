c3cc25fe96440b07fe8b69b6cf1f59cfc5edf1f6
Olli Etuaho <oetuaho@nvidia.com>
2017-07-14 17:03:42 +0300

    Clamp point size
    BUG=chromium:740560
    
    Change-Id: Ic4a66c9ea16f5ae76beb3bb6577716d10c3b226e

---
+++ src/compiler/translator/ClampPointSize.cpp
@@ -0,0 +1,40 @@
+//
+// Copyright (c) 2017 The ANGLE Project Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+// ClampPointSize.cpp: Limit the value that is written to gl_PointSize.
+//
+
+#include "compiler/translator/ClampPointSize.h"
+
+#include "compiler/translator/IntermNode_util.h"
+//#include "compiler/translator/RunAtTheEndOfShader.h"
+#include "compiler/translator/SymbolTable.h"
+
+namespace sh
+{
+
+void ClampPointSize(TIntermBlock *root, float maxPointSize, const TSymbolTable &symbolTable)
+{
+    TIntermSymbol *pointSizeNode = ReferToBuiltInSymbol("gl_PointSize", symbolTable, 100);
+
+    TConstantUnion *maxPointSizeConstant = new TConstantUnion();
+    maxPointSizeConstant->setFConst(maxPointSize);
+    TIntermConstantUnion *maxPointSizeNode =
+        new TIntermConstantUnion(maxPointSizeConstant, TType(EbtFloat, EbpHigh, EvqConst));
+
+    // min(gl_PointSize, maxPointSize)
+    TIntermSequence *minArguments = new TIntermSequence();
+    minArguments->push_back(pointSizeNode->deepCopy());
+    minArguments->push_back(maxPointSizeNode);
+    TIntermAggregate *clampedPointSize =
+        CreateBuiltInFunctionCallNode("min", minArguments, symbolTable, 100);
+
+    // gl_PointSize = min(gl_PointSize, maxPointSize)
+    TIntermBinary *assignPointSize = new TIntermBinary(EOpAssign, pointSizeNode, clampedPointSize);
+
+    // RunAtTheEndOfShader(root, assignPointSize);
+}
+
+}  // namespace sh
