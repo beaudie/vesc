31bcb112e395af33847d2a465237c763f72e4105
Olli Etuaho <oetuaho@nvidia.com>
2017-08-09 16:19:00 +0300

    Rename VariableInfo files to CollectVariables
    Files that only have a single function should be named consistently
    with the function. Unnecessary include in CallDAG.h is removed.
    
    BUG=angleproject:2068
    TEST=angle_unittests
    
    Change-Id: I27d55a766b9eb66fcfd1e0a2341a2843bb9dc5bb

--- src/compiler/translator/CollectVariables.cpp
+++ src/compiler/translator/CollectVariables.cpp
@@ -4,7 +4,7 @@
 // found in the LICENSE file.
 //
 
-#include "compiler/translator/VariableInfo.h"
+#include "compiler/translator/CollectVariables.h"
 
 #include "angle_gl.h"
 #include "common/utilities.h"
@@ -540,7 +540,7 @@ InterfaceBlock CollectVariablesTraverser::recordInterfaceBlock(const TIntermSymb
     ASSERT(blockType);
 
     InterfaceBlock interfaceBlock;
-    interfaceBlock.name = blockType->name().c_str();
+    interfaceBlock.name       = blockType->name().c_str();
     interfaceBlock.mappedName = HashName(blockType->name().c_str(), mHashFunction).c_str();
     interfaceBlock.instanceName =
         (blockType->hasInstanceName() ? blockType->instanceName().c_str() : "");
@@ -568,7 +568,7 @@ Uniform CollectVariablesTraverser::recordUniform(const TIntermSymbol &variable) 
 {
     Uniform uniform;
     setCommonVariableProperties(variable.getType(), variable.getSymbol(), &uniform);
-    uniform.binding = variable.getType().getLayoutQualifier().binding;
+    uniform.binding  = variable.getType().getLayoutQualifier().binding;
     uniform.location = variable.getType().getLayoutQualifier().location;
     uniform.offset   = variable.getType().getLayoutQualifier().offset;
     return uniform;
