4bd986ce122bb5e38a42fe54857ed384701d3492
Martin Radev <mradev@nvidia.com>
2017-08-09 16:25:36 +0300

    Handle Clear* commands for layered framebuffers
    The patch adds support for clearing the layers of 2D array textures
    attached to a multi-view framebuffer. According to the ANGLE_multiview
    spec, the layers which are outside of the range
    [baseViewIndex; baseViewIndex + numViews) should remain unmodified.
    Because the native Clear* commands clear all of the layers, a workaround
    is implemented which creates a FBO, attaches a single layer from all
    multi-view attachments and clears the contents.
    
    BUG=angleproject:2062
    TEST=angle_end2end_tests
    
    Change-Id: Ibf711d02046233eed16bdd3f9c96fc38f82ed0a8

---
+++ src/libANGLE/renderer/gl/ClearMultiviewGL.h
@@ -0,0 +1,101 @@
+//
+// Copyright 2017 The ANGLE Project Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+// ClearMultiviewGL:
+//   A helper for clearing multiview side-by-side and layered framebuffers.
+//
+
+#ifndef LIBANGLE_RENDERER_GL_CLEARMULTIVIEWGL_H_
+#define LIBANGLE_RENDERER_GL_CLEARMULTIVIEWGL_H_
+
+#include "angle_gl.h"
+#include "libANGLE/Error.h"
+#include "libANGLE/angletypes.h"
+
+namespace gl
+{
+class FramebufferState;
+}  // namespace gl
+
+namespace rx
+{
+class FunctionsGL;
+class StateManagerGL;
+
+class ClearMultiviewGL : angle::NonCopyable
+{
+  public:
+    // Enum containing the different types of Clear* commands.
+    enum class ClearCommandType
+    {
+        Clear,
+        ClearBufferfv,
+        ClearBufferuiv,
+        ClearBufferiv,
+        ClearBufferfi
+    };
+
+  public:
+    ClearMultiviewGL(const FunctionsGL *functions, StateManagerGL *stateManager);
+    ~ClearMultiviewGL();
+
+    ClearMultiviewGL(const ClearMultiviewGL &rht) = delete;
+    ClearMultiviewGL &operator=(const ClearMultiviewGL &rht) = delete;
+    ClearMultiviewGL(ClearMultiviewGL &&rht)                 = delete;
+    ClearMultiviewGL &operator=(ClearMultiviewGL &&rht) = delete;
+
+    gl::Error clearLayeredFBO(const gl::FramebufferState &state,
+                              ClearCommandType clearCommandType,
+                              GLbitfield mask,
+                              GLenum buffer,
+                              GLint drawbuffer,
+                              const uint8_t *values,
+                              GLfloat depth,
+                              GLint stencil);
+    gl::Error clearSideBySideFBO(const gl::FramebufferState &state,
+                                 const gl::Rectangle &scissorBase,
+                                 ClearCommandType clearCommandType,
+                                 GLbitfield mask,
+                                 GLenum buffer,
+                                 GLint drawbuffer,
+                                 const uint8_t *values,
+                                 GLfloat depth,
+                                 GLint stencil);
+
+    gl::Error initializeResources();
+
+  private:
+    void attachTextures(const gl::FramebufferState &state, int layer);
+    void genericClear(ClearCommandType clearCommandType,
+                      GLbitfield mask,
+                      GLenum buffer,
+                      GLint drawbuffer,
+                      const uint8_t *values,
+                      GLfloat depth,
+                      GLint stencil);
+
+    enum DirtyBitType : uint32_t
+    {
+        DIRTY_BIT_COLOR_ATTACHMENT_0,
+        DIRTY_BIT_COLOR_ATTACHMENT_MAX =
+            DIRTY_BIT_COLOR_ATTACHMENT_0 + gl::IMPLEMENTATION_MAX_FRAMEBUFFER_ATTACHMENTS,
+        DIRTY_BIT_DEPTH_ATTACHMENT = DIRTY_BIT_COLOR_ATTACHMENT_MAX,
+        DIRTY_BIT_STENCIL_ATTACHMENT,
+        DIRTY_BIT_DEPTH_STENCIL_ATTACHMENT,
+        DIRTY_BIT_UNKNOWN,
+        DIRTY_BIT_MAX = DIRTY_BIT_UNKNOWN
+    };
+    typedef angle::BitSet<DIRTY_BIT_MAX> DirtyBits;
+    DirtyBits mDirtyAttachments;
+
+    const FunctionsGL *mFunctions;
+    StateManagerGL *mStateManager;
+    gl::DrawBufferMask mPrevDrawBufferMask;
+
+    GLuint mFramebuffer;
+};
+}  // namespace rx
+
+#endif  // LIBANGLE_RENDERER_GL_CLEARMULTIVIEWGL_H_
