32ab2c204a975840591c6525bcedea304bf0e888
Jamie Madill <jmadill@chromium.org>
2017-08-17 12:20:52 -0400

    D3D11: Lazy robust resource init.
    BUG=angleproject:2107
    
    Change-Id: I2bf3a69b1eae0d4feeb5b17daca23451f1037be8

--- src/libANGLE/Context.cpp
+++ src/libANGLE/Context.cpp
@@ -2849,6 +2849,8 @@ void Context::initWorkarounds()
 Error Context::prepareForDraw(GLenum drawMode)
 {
     syncRendererState();
+    ANGLE_TRY(mGLState.clearUnclearedActiveTextures(this));
+    ANGLE_TRY(mGLState.getDrawFramebuffer()->clearUnclearedDrawAttachments(this));
 
     InfoLog infoLog;
     Error err = mImplementation->triggerDrawCallProgramRecompilation(this, &infoLog,
@@ -2975,9 +2977,13 @@ void Context::copyTexImage2D(GLenum target,
 
     Rectangle sourceArea(x, y, width, height);
 
-    const Framebuffer *framebuffer = mGLState.getReadFramebuffer();
+    Framebuffer *framebuffer = mGLState.getReadFramebuffer();
     Texture *texture =
         getTargetTexture(IsCubeMapTextureTarget(target) ? GL_TEXTURE_CUBE_MAP : target);
+
+    // Ensure source FBO is initialized.
+    ANGLE_CONTEXT_TRY(framebuffer->clearUnclearedReadAttachment(this, GL_COLOR_BUFFER_BIT));
+
     handleError(texture->copyImage(this, target, level, sourceArea, internalformat, framebuffer));
 }
 
