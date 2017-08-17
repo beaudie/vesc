df455b2dd215ec1e41d0aa255901439ef6860f71
Martin Radev <mradev@nvidia.com>
2017-08-17 16:05:29 +0300

    D3D11: Handle multi-view Draw* calls
    Because the ANGLE_multiview extension uses instancing to multiply
    geometry for each view, Draw* calls with an active multiview program
    have to be handled in the follwing way:
    1) Convert non-instanced Draw calls to their instanced versions.
    2) Multiply the number of instances in an instanced Draw call by the
    number of views.
    The patch also applies the viewport offsets to the viewport and scissor
    rectangle and propagates the computed viewports and scissors to the
    D3D11 runtime.
    
    BUG=angleproject:2062
    TEST=angle_end2end_tests
    
    Change-Id: I8b4295c95c2cc0c1046c67e1fb1a782a46703292

--- src/libANGLE/renderer/d3d/d3d11/VertexArray11.cpp
+++ src/libANGLE/renderer/d3d/d3d11/VertexArray11.cpp
@@ -180,6 +180,8 @@ gl::Error VertexArray11::updateDirtyAndDynamicAttribs(const gl::Context *context
     const auto &activeLocations = program->getActiveAttribLocationsMask();
     const auto &attribs         = mData.getVertexAttributes();
     const auto &bindings        = mData.getVertexBindings();
+    const int divisorMultiplier =
+        (program != nullptr && program->usesMultiview()) ? program->getNumViews() : 1;
 
     if (mAttribsToTranslate.any())
     {
@@ -197,7 +199,7 @@ gl::Error VertexArray11::updateDirtyAndDynamicAttribs(const gl::Context *context
             translatedAttrib->attribute = &attribs[dirtyAttribIndex];
             translatedAttrib->binding   = &bindings[translatedAttrib->attribute->bindingIndex];
             translatedAttrib->currentValueType = currentValue.Type;
-            translatedAttrib->divisor          = translatedAttrib->binding->getDivisor();
+            translatedAttrib->divisor = translatedAttrib->binding->getDivisor() * divisorMultiplier;
 
             switch (mAttributeStorageTypes[dirtyAttribIndex])
             {
@@ -232,7 +234,7 @@ gl::Error VertexArray11::updateDirtyAndDynamicAttribs(const gl::Context *context
             dynamicAttrib->attribute        = &attribs[dynamicAttribIndex];
             dynamicAttrib->binding          = &bindings[dynamicAttrib->attribute->bindingIndex];
             dynamicAttrib->currentValueType = currentValue.Type;
-            dynamicAttrib->divisor          = dynamicAttrib->binding->getDivisor();
+            dynamicAttrib->divisor = dynamicAttrib->binding->getDivisor() * divisorMultiplier;
         }
 
         ANGLE_TRY(vertexDataManager->storeDynamicAttribs(&mTranslatedAttribs, activeDynamicAttribs,
