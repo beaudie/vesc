3624838928153c1210e6afc522751665ee62b490
Jamie Madill <jmadill@chromium.org>
2017-08-30 15:32:48 -0400

    D3D11: Re-enable updateVertexBuffer dirty bits.
    In some cases, when the app would call glBufferSubData to do a small
    data update in an existing vertex buffer, the sync code would not
    flush out the data to the native D3D11 buffer from the temporary
    staging buffer.
    
    Fix this problem by notifying the VertexArray11 class when buffer
    data is updated. Note that in the future we can improve this by
    using a different update notification for when the buffer data changes
    and when the underlying storage changes. For now take a very broad
    approach.
    
    BUG=angleproject:1156
    
    Change-Id: I2e0fabc97c1f1d5a14d609247e61c602e9a5a85f

--- src/libANGLE/renderer/d3d/d3d11/StateManager11.cpp
+++ src/libANGLE/renderer/d3d/d3d11/StateManager11.cpp
@@ -1922,10 +1922,10 @@ gl::Error StateManager11::applyVertexBuffer(const gl::Context *context,
 
     // Currently buffer data updates sometimes won't get flushed if we short-circuit.
     // TODO(jmadill): Re-enable once we fix updates.
-    //if (!mInputLayoutIsDirty)
-    //{
-    //    return gl::NoError();
-    //}
+    if (!mInputLayoutIsDirty)
+    {
+        return gl::NoError();
+    }
 
     GLsizei numIndicesPerInstance = 0;
     if (instances > 0)
