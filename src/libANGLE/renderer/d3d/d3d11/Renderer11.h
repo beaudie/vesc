1ed5fb8563a8d802c34a434c1c6a13d5d90b32e9
Geoff Lang <geofflang@chromium.org>
2017-09-06 15:32:03 -0400

    Don't create SRVs for multisampled depth stencil textures in FL10.0.
    Renderer11::createRenderTarget already fixed this issue but it also is
    exposed in SwapChain11::resetOffscreenDepthBuffer now that multisampled
    surfaces are supported in ANGLE.
    
    BUG=angleproject:2136
    
    Change-Id: I978666ebc1bb3db14ddf69954d7eb750391bf7a8

--- src/libANGLE/renderer/d3d/d3d11/Renderer11.h
+++ src/libANGLE/renderer/d3d/d3d11/Renderer11.h
@@ -49,6 +49,9 @@ struct Renderer11DeviceCaps
     bool supportsDXGI1_2;                // Support for DXGI 1.2
     bool supportsClearView;              // Support for ID3D11DeviceContext1::ClearView
     bool supportsConstantBufferOffsets;  // Support for Constant buffer offset
+    bool supportsMultisampledDepthStencilSRVs;  // D3D feature level 10.0 no longer allows creation
+                                                // of textures with both the bind SRV and DSV flags
+                                                // when multisampled. crbug.com/656989
     UINT B5G6R5support;    // Bitfield of D3D11_FORMAT_SUPPORT values for DXGI_FORMAT_B5G6R5_UNORM
     UINT B5G6R5maxSamples;  // Maximum number of samples supported by DXGI_FORMAT_B5G6R5_UNORM
     UINT B4G4R4A4support;  // Bitfield of D3D11_FORMAT_SUPPORT values for DXGI_FORMAT_B4G4R4A4_UNORM
