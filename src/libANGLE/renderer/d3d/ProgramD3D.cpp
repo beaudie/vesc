d6fabf3e2a7d82e5e7262722d4f882d4c97580c7
JiangYizhou <yizhou.jiang@intel.com>
2017-09-13 13:47:52 +0800

    Workaround Intel driver bug on D3D when renderering with no render target
    When rendering with no render target on D3D, two bugs are found lead
    to unexpected behavior on Intel driver. The rendering samples always
    passed neglecting discard of pixel shader.
    
    Two bugs are listed bellow,
    1. When framebuffer has no attachments, the cached output layout will
    be reset to null during recompilation, thus SV_TARGET isn't generated
    in pixel shader. Then rendering samples always passed neglecting
    discard of pixel shader on Intel driver. So we keep the cached output
    layout when framebuffer has no attachments to avoid shader recompilation.
    
    2. When framebuffer has no attachments, the rtvMasks were set to 0 in
    angle. If rtvMasks is 0 and rendertarget is not set, then rendering
    samples always passed neglecting discard of pixel shader on Intel driver
    as well. However, according to msdn https://msdn.microsoft.com/en-us/
    library/windows/desktop/ff476462(v=vs.85).aspx the default value of
    rtvMasks should be D3D11_COLOR_WRITE_ENABLE_ALL.
    So we set rtvMasks of render target0 to D3D11_COLOR_WRITE_ENABLE_ALL
    instead of 0 to workaround the driver bug.
    
    BUG=angleproject:2152
    
    TEST=FramebufferTest_ES31.RenderingLimitToDefaultFBOSizeWithNoAttachments/ES3_1_D3D11
    TEST=dEQP-GLES31.functional.fbo.no_attachments.*
    
    Change-Id: I9c2936a075563f2764887e9150192058b515ac2e

--- src/libANGLE/renderer/d3d/ProgramD3D.cpp
+++ src/libANGLE/renderer/d3d/ProgramD3D.cpp
@@ -2495,10 +2495,18 @@ void ProgramD3D::updateCachedInputLayout(Serial associatedSerial, const gl::Stat
 void ProgramD3D::updateCachedOutputLayout(const gl::Context *context,
                                           const gl::Framebuffer *framebuffer)
 {
-    mPixelShaderOutputLayoutCache.clear();
-
     FramebufferD3D *fboD3D   = GetImplAs<FramebufferD3D>(framebuffer);
     const auto &colorbuffers = fboD3D->getColorAttachmentsForRender(context);
+
+    // When framebuffer has no attachment, keep the cached output layout to avoid shader
+    // recompilation.
+    if (colorbuffers.size() == 0 &&
+        (framebuffer->getDefaultWidth() != 0 || framebuffer->getDefaultHeight() != 0))
+    {
+        return;
+    }
+
+    mPixelShaderOutputLayoutCache.clear();
 
     for (size_t colorAttachment = 0; colorAttachment < colorbuffers.size(); ++colorAttachment)
     {
