32f6dfa33f006468b472ab86522fa738e3490589
Olli Etuaho <oetuaho@nvidia.com>
2017-09-26 13:34:10 +0300

    Support arrays of arrays in the API
    The ShaderVariable class that is used as an interface between the
    compiler and the rest of the code gets arrays of arrays support.
    
    Array of array variables are passed from the compiler just like any
    other variables. However, when stored in Program state each innermost
    array constitutes a separate variable. This is done to make the
    implementation match the GLES specification for program interface
    query APIs.
    
    This will be tested more fully once support for parsing arrays of
    arrays lands in the compiler.
    
    TEST=angle_end2end_tests, angle_unittests
    BUG=angleproject:2125
    
    Change-Id: I0f7159000f039be92a87a52b3b68cd9a215a21cb

--- src/libANGLE/VaryingPacking.cpp
+++ src/libANGLE/VaryingPacking.cpp
@@ -48,7 +48,9 @@ bool VaryingPacking::packVarying(const PackedVarying &packedVarying)
     }
 
     // "Arrays of size N are assumed to take N times the size of the base type"
-    varyingRows *= (packedVarying.isArrayElement() ? 1 : varying.elementCount());
+    ASSERT(!varying.isArrayOfArrays());
+    const unsigned int elementCount = varying.isArray() ? varying.getOutermostArraySize() : 1u;
+    varyingRows *= (packedVarying.isArrayElement() ? 1 : elementCount);
 
     unsigned int maxVaryingVectors = static_cast<unsigned int>(mRegisterMap.size());
 
@@ -200,7 +202,9 @@ void VaryingPacking::insert(unsigned int registerRow,
     registerInfo.packedVarying  = &packedVarying;
     registerInfo.registerColumn = registerColumn;
 
-    for (unsigned int arrayElement = 0; arrayElement < varying.elementCount(); ++arrayElement)
+    ASSERT(!varying.isArrayOfArrays());
+    unsigned int arrayElementCount = (varying.isArray() ? varying.getOutermostArraySize() : 1u);
+    for (unsigned int arrayElement = 0; arrayElement < arrayElementCount; ++arrayElement)
     {
         if (packedVarying.isArrayElement() && arrayElement != packedVarying.arrayIndex)
         {
