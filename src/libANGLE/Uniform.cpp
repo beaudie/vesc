725a55c13340fd3d795db2db5d76b075918534d0
Olli Etuaho <oetuaho@nvidia.com>
2017-09-26 13:34:10 +0300

    Support arrays of arrays in the API
    The ShaderVariable class that is used as an interface between the
    compiler and the rest of the code gets arrays of arrays support.
    
    Array of array variables are passed from the compiler just like any
    other variables. However, when stored in Program state each innermost
    array constitutes a separate variable. This is done to make the
    implementation match the GLES specification for program interface
    query APIs.
    
    This will be tested more fully once support for parsing arrays of
    arrays lands in the compiler.
    
    TEST=angle_end2end_tests, angle_unittests
    BUG=angleproject:2125
    
    Change-Id: I0f7159000f039be92a87a52b3b68cd9a215a21cb

--- src/libANGLE/Uniform.cpp
+++ src/libANGLE/Uniform.cpp
@@ -61,7 +61,7 @@ LinkedUniform::LinkedUniform()
 LinkedUniform::LinkedUniform(GLenum typeIn,
                              GLenum precisionIn,
                              const std::string &nameIn,
-                             unsigned int arraySizeIn,
+                             const std::vector<unsigned int> &arraySizesIn,
                              const int bindingIn,
                              const int offsetIn,
                              const int locationIn,
@@ -72,7 +72,7 @@ LinkedUniform::LinkedUniform(GLenum typeIn,
     type      = typeIn;
     precision = precisionIn;
     name      = nameIn;
-    arraySize = arraySizeIn;
+    arraySizes = arraySizesIn;
     binding   = bindingIn;
     offset    = offsetIn;
     location  = locationIn;
@@ -152,7 +152,7 @@ BufferVariable::BufferVariable()
 BufferVariable::BufferVariable(GLenum typeIn,
                                GLenum precisionIn,
                                const std::string &nameIn,
-                               unsigned int arraySizeIn,
+                               const std::vector<unsigned int> &arraySizesIn,
                                const int bufferIndexIn,
                                const sh::BlockMemberInfo &blockInfoIn)
     : bufferIndex(bufferIndexIn), blockInfo(blockInfoIn), topLevelArraySize(-1)
@@ -160,7 +160,7 @@ BufferVariable::BufferVariable(GLenum typeIn,
     type      = typeIn;
     precision = precisionIn;
     name      = nameIn;
-    arraySize = arraySizeIn;
+    arraySizes = arraySizesIn;
 }
 
 BufferVariable::~BufferVariable()
