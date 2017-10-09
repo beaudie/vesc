e8c34f80204807f945e3b84e08a582ca4ca3884c
Olli Etuaho <oetuaho@nvidia.com>
2017-10-09 17:18:02 +0300

    Initialize uninitialized GLSL arrays in a for loop
    Previously, creating nodes for initializing each single array element
    could result in memory bloat during translation when dealing with
    large arrays. The resulting shader could also end up very long.
    Initialize most arrays using a simple for loop instead. The loop is
    compatible with ESSL 1.00 Appendix A limitations.
    
    An exception is made for fragment outputs, so that they are not
    indexed by non-constant values.
    
    On some platforms using the a loop to initialize variables can cause
    problems, so we also have a compiler flag for turning this behavior
    off. The flag was already added earlier for a staggered rollout of
    this functionality.
    
    BUG=chromium:735497
    TEST=angle_unittests, angle_end2end_tests, WebGL conformance tests
    
    Change-Id: Iec727821d8137db56b440ddbe007879b1b55f61f

--- src/compiler/translator/DeferGlobalInitializers.cpp
+++ src/compiler/translator/DeferGlobalInitializers.cpp
@@ -29,7 +29,9 @@ namespace
 
 void GetDeferredInitializers(TIntermDeclaration *declaration,
                              bool initializeUninitializedGlobals,
-                             TIntermSequence *deferredInitializersOut)
+                             bool canUseLoopsToInitialize,
+                             TIntermSequence *deferredInitializersOut,
+                             TSymbolTable *symbolTable)
 {
     // We iterate with an index instead of using an iterator since we're replacing the children of
     // declaration inside the loop.
@@ -94,7 +96,8 @@ void GetDeferredInitializers(TIntermDeclaration *declaration,
 
             if (symbolNode->getQualifier() == EvqGlobal && symbolNode->getSymbol() != "")
             {
-                TIntermSequence *initCode = CreateInitCode(symbolNode);
+                TIntermSequence *initCode =
+                    CreateInitCode(symbolNode, canUseLoopsToInitialize, symbolTable);
                 deferredInitializersOut->insert(deferredInitializersOut->end(), initCode->begin(),
                                                 initCode->end());
             }
@@ -131,6 +134,7 @@ void InsertInitCallToMain(TIntermBlock *root,
 
 void DeferGlobalInitializers(TIntermBlock *root,
                              bool initializeUninitializedGlobals,
+                             bool canUseLoopsToInitialize,
                              TSymbolTable *symbolTable)
 {
     TIntermSequence *deferredInitializers = new TIntermSequence();
@@ -143,7 +147,7 @@ void DeferGlobalInitializers(TIntermBlock *root,
         if (declaration)
         {
             GetDeferredInitializers(declaration, initializeUninitializedGlobals,
-                                    deferredInitializers);
+                                    canUseLoopsToInitialize, deferredInitializers, symbolTable);
         }
     }
 
