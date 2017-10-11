f6e43d91e6fd3a8f635402b317162e7ab76be42d
jchen10 <jie.a.chen@intel.com>
2017-10-11 14:42:32 +0800

    ES31: UniformBlock and AtomicCounterBuffer
    Implements the 2 new interfaces for program query.
    
    BUG=angleproject:1920
    TEST=angle_end2end_tests:ProgramInterfaceTest*
    
    Change-Id: Ia23ddfef5f5dd7e15628f4c259273e1c01c14d80

--- src/libANGLE/validationES31.cpp
+++ src/libANGLE/validationES31.cpp
@@ -259,13 +259,21 @@ bool ValidateProgramResourceIndex(const Program *programObject,
 
         case GL_UNIFORM:
             return (index < static_cast<GLuint>(programObject->getActiveUniformCount()));
+#if 0
+        case GL_UNIFORM_BLOCK:
+            return (index < static_cast<GLuint>(programObject->getActiveUniformBlockCount()));
+
+        case GL_ATOMIC_COUNTER_BUFFER:
+            return (index < static_cast<GLuint>(
+                                programObject->getState().getAtomicCounterBuffers().size()));
+#endif
 
         // TODO(jie.a.chen@intel.com): more interfaces.
         case GL_UNIFORM_BLOCK:
+        case GL_ATOMIC_COUNTER_BUFFER:
         case GL_TRANSFORM_FEEDBACK_VARYING:
         case GL_BUFFER_VARIABLE:
         case GL_SHADER_STORAGE_BLOCK:
-        case GL_ATOMIC_COUNTER_BUFFER:
             UNIMPLEMENTED();
             return false;
 
