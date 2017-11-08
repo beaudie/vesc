e47595d2012d38fe0521ec71a6c41352729827a9
Jeff Gilbert <jgilbert@mozilla.com>
2017-11-07 16:03:38 -0800

    Remove formatType arg from computeCompressedImageSize.
    BUG=angleproject:2230
    
    Change-Id: Ie291ba05efa89bc4ea52966e975d5a21431a461f

--- src/libANGLE/validationES3.cpp
+++ src/libANGLE/validationES3.cpp
@@ -1378,8 +1378,7 @@ bool ValidateCompressedTexImage3D(Context *context,
         return false;
     }
 
-    auto blockSizeOrErr =
-        formatInfo.computeCompressedImageSize(GL_UNSIGNED_BYTE, gl::Extents(width, height, depth));
+    auto blockSizeOrErr = formatInfo.computeCompressedImageSize(gl::Extents(width, height, depth));
     if (blockSizeOrErr.isError())
     {
         context->handleError(InvalidValue());
@@ -2007,8 +2006,7 @@ bool ValidateCompressedTexSubImage3D(Context *context,
         return false;
     }
 
-    auto blockSizeOrErr =
-        formatInfo.computeCompressedImageSize(GL_UNSIGNED_BYTE, gl::Extents(width, height, depth));
+    auto blockSizeOrErr = formatInfo.computeCompressedImageSize(gl::Extents(width, height, depth));
     if (blockSizeOrErr.isError())
     {
         context->handleError(blockSizeOrErr.getError());
