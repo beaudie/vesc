3cc1798f38f1fb2e7670816f22f0389675cb1780
Jamie Madill <jmadill@chromium.org>
2017-11-13 10:25:00 -0500

    D3D11: Cache element array buffer updates.
    This attempts to reduce the amount of redundant validation done
    between indexed draw calls. It keeps the cached info in the
    VertexArray11 class.
    
    It also includes a fix to a missing direct buffer invalidation in
    CopyBufferSubData which was turning up with the new caching.
    
    BUG=angleproject:2229
    
    Change-Id: I63121bea19a9c8198e1925ed6a1460838e8f8955

--- src/libANGLE/renderer/d3d/IndexDataManager.h
+++ src/libANGLE/renderer/d3d/IndexDataManager.h
@@ -105,6 +105,30 @@ bool UsePrimitiveRestartWorkaround(bool primitiveRestartFixedIndexEnabled,
                                    GLenum type,
                                    RendererClass rendererClass);
 
+enum class IndexStorageType
+{
+    // Dynamic indexes are re-streamed every frame. They come from a client data pointer or
+    // from buffers that are updated frequently.
+    Dynamic,
+
+    // Static indexes are translated from the original storage once, and re-used multiple times.
+    Static,
+
+    // Direct indexes are never transated and are used directly from the source buffer. They are
+    // the fastest available path.
+    Direct,
+
+    // Not a real storage type.
+    Invalid,
+};
+
+IndexStorageType ClassifyIndexStorage(const gl::State &glState,
+                                      const gl::Buffer *elementArrayBuffer,
+                                      GLenum elementType,
+                                      GLenum destElementType,
+                                      const void *indices,
+                                      bool *needsTranslation);
+
 }  // namespace rx
 
 #endif  // LIBANGLE_INDEXDATAMANAGER_H_
