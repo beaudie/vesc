7885908ed1071303f83a98ce4e8eee16a3de07e7
Jamie Madill <jmadill@chromium.org>
2017-11-13 10:24:59 -0500

    D3D11: Lazy evaluation for draw call vertex params.
    The vertex params are the 'start' vertex index and the number of
    vertices in the draw call. For indexed draws, they sometimes mean we
    need to compute the index range. In other cases, we don't. Defer
    computing the index range as long as possible so we don't end up
    computing the index range when we don't have to.
    
    BUG=angleproject:2229
    
    Change-Id: I8e125416ef6767787a14509b322efa8dcd8e4a34

--- src/libANGLE/renderer/d3d/d3d11/Renderer11.cpp
+++ src/libANGLE/renderer/d3d/d3d11/Renderer11.cpp
@@ -1514,8 +1514,8 @@ gl::Error Renderer11::drawArrays(const gl::Context *context,
         return gl::NoError();
     }
 
-    ANGLE_TRY(
-        mStateManager.applyVertexBuffer(context, mode, startVertex, count, instances, nullptr));
+    DrawCallVertexParams vertexParams(startVertex, count, instances);
+    ANGLE_TRY(mStateManager.applyVertexBuffer(context, mode, vertexParams, nullptr));
 
     if (glState.isTransformFeedbackActiveUnpaused())
     {
@@ -1658,7 +1658,8 @@ gl::Error Renderer11::drawElements(const gl::Context *context,
         ANGLE_TRY(mStateManager.applyIndexBuffer(context, indices, count, type, lazyIndexRange,
                                                  &indexInfo));
 
-        ANGLE_TRY(mStateManager.applyVertexBuffer(context, mode, 0, 0, 0, &indexInfo));
+        DrawCallVertexParams vertexParams(0, 0, instances);
+        ANGLE_TRY(mStateManager.applyVertexBuffer(context, mode, vertexParams, &indexInfo));
         if (adjustedInstanceCount > 0)
         {
             mDeviceContext->DrawIndexedInstanced(count, adjustedInstanceCount, 0, 0, 0);
@@ -1673,13 +1674,11 @@ gl::Error Renderer11::drawElements(const gl::Context *context,
     ANGLE_TRY(
         mStateManager.applyIndexBuffer(context, indices, count, type, lazyIndexRange, &indexInfo));
 
-    const gl::IndexRange &indexRange = lazyIndexRange.getIndexRange().value();
-    size_t vertexCount               = indexRange.vertexCount();
-    ANGLE_TRY(mStateManager.applyVertexBuffer(context, mode, static_cast<GLsizei>(indexRange.start),
-                                              static_cast<GLsizei>(vertexCount), instances,
-                                              &indexInfo));
+    DrawCallVertexParams vertexParams(lazyIndexRange, 0, instances);
 
-    int startVertex = static_cast<int>(indexRange.start);
+    ANGLE_TRY(mStateManager.applyVertexBuffer(context, mode, vertexParams, &indexInfo));
+
+    int startVertex = static_cast<int>(vertexParams.firstVertex());
     int baseVertex  = -startVertex;
 
     if (mode == GL_LINE_LOOP)
@@ -1730,7 +1729,7 @@ gl::Error Renderer11::drawElements(const gl::Context *context,
     // efficent code path. Instanced rendering of emulated pointsprites requires a loop to draw each
     // batch of points. An offset into the instanced data buffer is calculated and applied on each
     // iteration to ensure all instances are rendered correctly.
-    GLsizei elementsToRender = static_cast<GLsizei>(indexRange.vertexCount());
+    GLsizei elementsToRender = vertexParams.vertexCount();
 
     // Each instance being rendered requires the inputlayout cache to reapply buffers and offsets.
     for (GLsizei i = 0; i < instances; i++)
@@ -1761,7 +1760,8 @@ gl::Error Renderer11::drawArraysIndirect(const gl::Context *context,
 
     if (!DrawCallNeedsTranslation(context, mode))
     {
-        ANGLE_TRY(mStateManager.applyVertexBuffer(context, mode, 0, 0, 0, nullptr));
+        DrawCallVertexParams vertexParams(0, 0, 0);
+        ANGLE_TRY(mStateManager.applyVertexBuffer(context, mode, vertexParams, nullptr));
         ID3D11Buffer *buffer = nullptr;
         ANGLE_TRY_RESULT(storage->getBuffer(context, BUFFER_USAGE_INDIRECT), buffer);
         mDeviceContext->DrawInstancedIndirect(buffer, static_cast<unsigned int>(offset));
@@ -1777,7 +1777,8 @@ gl::Error Renderer11::drawArraysIndirect(const gl::Context *context,
     GLuint instances = args->instanceCount;
     GLuint first     = args->first;
 
-    ANGLE_TRY(mStateManager.applyVertexBuffer(context, mode, first, count, instances, nullptr));
+    DrawCallVertexParams vertexParams(first, count, instances);
+    ANGLE_TRY(mStateManager.applyVertexBuffer(context, mode, vertexParams, nullptr));
 
     if (mode == GL_LINE_LOOP)
     {
@@ -1816,7 +1817,8 @@ gl::Error Renderer11::drawElementsIndirect(const gl::Context *context,
     {
         ANGLE_TRY(mStateManager.applyIndexBuffer(context, nullptr, 0, type, gl::HasIndexRange(),
                                                  &indexInfo));
-        ANGLE_TRY(mStateManager.applyVertexBuffer(context, mode, 0, 0, 0, &indexInfo));
+        DrawCallVertexParams vertexParams(0, 0, 0);
+        ANGLE_TRY(mStateManager.applyVertexBuffer(context, mode, vertexParams, &indexInfo));
         ID3D11Buffer *buffer = nullptr;
         ANGLE_TRY_RESULT(storage->getBuffer(context, BUFFER_USAGE_INDIRECT), buffer);
         mDeviceContext->DrawIndexedInstancedIndirect(buffer, static_cast<unsigned int>(offset));
@@ -1842,13 +1844,12 @@ gl::Error Renderer11::drawElementsIndirect(const gl::Context *context,
     ANGLE_TRY(
         mStateManager.applyIndexBuffer(context, indices, count, type, lazyIndexRange, &indexInfo));
 
-    const gl::IndexRange &indexRange = lazyIndexRange.getIndexRange().value();
-    size_t vertexCount = indexRange.vertexCount();
-    ANGLE_TRY(mStateManager.applyVertexBuffer(
-        context, mode, static_cast<GLsizei>(indexRange.start) + baseVertex,
-        static_cast<GLsizei>(vertexCount), instances, &indexInfo));
+    DrawCallVertexParams vertexParams(lazyIndexRange, baseVertex, instances);
 
-    int baseVertexLocation = -static_cast<int>(indexRange.start);
+    ANGLE_TRY(mStateManager.applyVertexBuffer(context, mode, vertexParams, &indexInfo));
+
+    int baseVertexLocation = -static_cast<int>(vertexParams.firstVertex());
+
     if (mode == GL_LINE_LOOP)
     {
         return drawLineLoop(context, count, type, indices, baseVertexLocation, instances);
