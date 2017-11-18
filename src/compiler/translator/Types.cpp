d9c04173329ae3c93d9d32dabdd25a2719b760ca
Jamie Madill <jmadill@chromium.org>
2017-11-18 14:30:40 -0500

    TType: Store array sizes vector as a pointer.
    This makes TType a literal type, and thus is something that could be
    constexpr.
    
    Note: this patch is not functional, and will break execution. It's
    intended to be a proof-of-concept. The fully implemented patch will
    need to check for nullptr array sizes in more places.
    
    Bug: angleproject:1432
    Change-Id: Ib0f49497587095b74a0e4b9e827faf12791d14e5

--- src/compiler/translator/Types.cpp
+++ src/compiler/translator/Types.cpp
@@ -364,7 +364,7 @@ TString TType::getCompleteString() const
         stream << getQualifierString() << " ";
     if (precision != EbpUndefined)
         stream << getPrecisionString() << " ";
-    for (auto arraySizeIter = mArraySizes.rbegin(); arraySizeIter != mArraySizes.rend();
+    for (auto arraySizeIter = mArraySizes->rbegin(); arraySizeIter != mArraySizes->rend();
          ++arraySizeIter)
     {
         stream << "array[" << (*arraySizeIter) << "] of ";
@@ -530,7 +530,7 @@ const char *TType::buildMangledName() const
         mangledName += static_cast<char>('0' + getNominalSize());
     }
 
-    for (unsigned int arraySize : mArraySizes)
+    for (unsigned int arraySize : *mArraySizes)
     {
         char buf[20];
         snprintf(buf, sizeof(buf), "%d", arraySize);
@@ -558,7 +558,7 @@ size_t TType::getObjectSize() const
     if (totalSize == 0)
         return 0;
 
-    for (size_t arraySize : mArraySizes)
+    for (size_t arraySize : *mArraySizes)
     {
         if (arraySize > INT_MAX / totalSize)
             totalSize = INT_MAX;
@@ -583,7 +583,7 @@ int TType::getLocationCount() const
         return 0;
     }
 
-    for (unsigned int arraySize : mArraySizes)
+    for (unsigned int arraySize : *mArraySizes)
     {
         if (arraySize > static_cast<unsigned int>(std::numeric_limits<int>::max() / count))
         {
@@ -600,8 +600,12 @@ int TType::getLocationCount() const
 
 unsigned int TType::getArraySizeProduct() const
 {
+    if (!mArraySizes)
+        return 1u;
+
     unsigned int product = 1u;
-    for (unsigned int arraySize : mArraySizes)
+
+    for (unsigned int arraySize : *mArraySizes)
     {
         product *= arraySize;
     }
@@ -610,7 +614,10 @@ unsigned int TType::getArraySizeProduct() const
 
 bool TType::isUnsizedArray() const
 {
-    for (unsigned int arraySize : mArraySizes)
+    if (!mArraySizes)
+        return false;
+
+    for (unsigned int arraySize : *mArraySizes)
     {
         if (arraySize == 0u)
         {
@@ -632,13 +639,14 @@ bool TType::isElementTypeOf(const TType &arrayType) const
     {
         return false;
     }
-    if (arrayType.mArraySizes.size() != mArraySizes.size() + 1u)
+    // FIXME
+    if (arrayType.mArraySizes->size() != mArraySizes->size() + 1u)
     {
         return false;
     }
-    for (size_t i = 0; i < mArraySizes.size(); ++i)
+    for (size_t i = 0; i < mArraySizes->size(); ++i)
     {
-        if (mArraySizes[i] != arrayType.mArraySizes[i])
+        if ((*mArraySizes)[i] != arrayType.mArraySizes[0][i])
         {
             return false;
         }
@@ -648,9 +656,9 @@ bool TType::isElementTypeOf(const TType &arrayType) const
 
 void TType::sizeUnsizedArrays(const TVector<unsigned int> &arraySizes)
 {
-    for (size_t i = 0u; i < mArraySizes.size(); ++i)
+    for (size_t i = 0u; i < mArraySizes->size(); ++i)
     {
-        if (mArraySizes[i] == 0)
+        if ((*mArraySizes)[i] == 0)
         {
             if (i < arraySizes.size())
             {
@@ -668,8 +676,8 @@ void TType::sizeUnsizedArrays(const TVector<unsigned int> &arraySizes)
 void TType::sizeOutermostUnsizedArray(unsigned int arraySize)
 {
     ASSERT(isArray());
-    ASSERT(mArraySizes.back() == 0u);
-    mArraySizes.back() = arraySize;
+    ASSERT(mArraySizes->back() == 0u);
+    mArraySizes->back() = arraySize;
 }
 
 void TType::setBasicType(TBasicType t)
@@ -703,14 +711,17 @@ void TType::setSecondarySize(unsigned char ss)
 
 void TType::makeArray(unsigned int s)
 {
-    mArraySizes.push_back(s);
+    if (!mArraySizes)
+        mArraySizes = new TVector<unsigned int>();
+
+    mArraySizes->push_back(s);
     invalidateMangledName();
 }
 
 void TType::setArraySize(size_t arrayDimension, unsigned int s)
 {
-    ASSERT(arrayDimension < mArraySizes.size());
-    if (mArraySizes.at(arrayDimension) != s)
+    ASSERT(arrayDimension < mArraySizes->size());
+    if (mArraySizes->at(arrayDimension) != s)
     {
         mArraySizes[arrayDimension] = s;
         invalidateMangledName();
@@ -719,9 +730,9 @@ void TType::setArraySize(size_t arrayDimension, unsigned int s)
 
 void TType::toArrayElementType()
 {
-    if (mArraySizes.size() > 0)
+    if (mArraySizes->size() > 0)
     {
-        mArraySizes.pop_back();
+        mArraySizes->pop_back();
         invalidateMangledName();
     }
 }
