2747672a1a877aec7376eb839eddd09a100ae3ed
Lingfeng Yang <lfy@google.com>
2017-12-11 09:22:58 -0800

    wip: gles1
    + GLES1 headers
    + mat4 transforms in libANGLE with tests against GLM
    
    Log
    
    1. Running conformance tests
    2. No asserts fired in gles1_conform
    3. Reduced "Unimplemented" spam in conform test
    4. Add breaking change to GLES1 conformance gtest driver:
    Call the setup functions first.
    5. Flaky fog test on Intel Iris, btw - run it separately enough times, they pass
    6. Add emulated vs user buffer map.
    7. Passed Gets test
    8. Found something weird; needed to call exttestdriversetup every test.
    9. Now with 16-18 failures
    
    Current:
    
    complete shader code for texture env (decal / combine)
        - sample apps for those first
        - add multitextured version
    
    BUG=angleproject:2306
    
    Change-Id: Ibc7708ac394c66bb73a39a9901939c6bad7e46d9

---
+++ samples/gles1/SimpleTexture2D.cpp
@@ -0,0 +1,96 @@
+//
+// Copyright (c) 2014 The ANGLE Project Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+
+//            Based on Simple_Texture2D.c from
+// Book:      OpenGL(R) ES 2.0 Programming Guide
+// Authors:   Aaftab Munshi, Dan Ginsburg, Dave Shreiner
+// ISBN-10:   0321502795
+// ISBN-13:   9780321502797
+// Publisher: Addison-Wesley Professional
+// URLs:      http://safari.informit.com/9780321563835
+//            http://www.opengles-book.com
+
+#include "SampleApplication.h"
+#include "shader_utils.h"
+#include "texture_utils.h"
+
+#include <GLES/gl.h>
+#include <GLES/glext.h>
+
+class SimpleTexture2DSample : public SampleApplication
+{
+  public:
+    SimpleTexture2DSample()
+        : SampleApplication("SimpleTexture2D", 1280, 720,
+                            1 /* gles1 */)
+    {
+    }
+
+    virtual bool initialize()
+    {
+        // Load the texture
+        mTexture = CreateSimpleTexture2D();
+
+        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+        return true;
+    }
+
+    virtual void destroy()
+    {
+        glDeleteTextures(1, &mTexture);
+    }
+
+    virtual void draw()
+    {
+        GLfloat vertices[] =
+        {
+            -0.5f,  0.5f, 0.0f,  // Position 0
+             0.0f,  0.0f,        // TexCoord 0
+            -0.5f, -0.5f, 0.0f,  // Position 1
+             0.0f,  1.0f,        // TexCoord 1
+             0.5f, -0.5f, 0.0f,  // Position 2
+             1.0f,  1.0f,        // TexCoord 2
+             0.5f,  0.5f, 0.0f,  // Position 3
+             1.0f,  0.0f         // TexCoord 3
+        };
+        GLushort indices[] = { 0, 1, 2, 0, 2, 3 };
+
+        // Set the viewport
+        glViewport(0, 0, getWindow()->getWidth(), getWindow()->getHeight());
+
+        // Clear the color buffer
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glEnable(GL_TEXTURE_2D);
+        glEnableClientState(GL_VERTEX_ARRAY);
+        glEnableClientState(GL_TEXTURE_COORD_ARRAY);
+
+        // Load the vertex position
+        glVertexPointer(3, GL_FLOAT, 5 * sizeof(GLfloat), vertices);
+        // Load the texture coordinate
+        glTexCoordPointer(2, GL_FLOAT, 5 * sizeof(GLfloat), vertices + 3);
+
+        // Bind the texture
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, mTexture);
+
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, indices);
+
+        glDisableClientState(GL_VERTEX_ARRAY);
+        glDisableClientState(GL_TEXTURE_COORD_ARRAY);
+    }
+
+  private:
+    // Texture handle
+    GLuint mTexture;
+};
+
+int main(int argc, char **argv)
+{
+    SimpleTexture2DSample app;
+    return app.run();
+}
