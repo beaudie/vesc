9a3ab333f1914abd3d88cc842c0cd5de14b36b26
Lingfeng Yang <lfy@google.com>
2017-12-11 09:22:58 -0800

    wip: gles1
    + GLES1 headers
    + mat4 transforms in libANGLE with tests against GLM
    
    Log
    
    1. Running conformance tests
    2. No asserts fired in gles1_conform
    3. Reduced "Unimplemented" spam in conform test
    4. Add breaking change to GLES1 conformance gtest driver:
    Call the setup functions first.
    5. Flaky fog test on Intel Iris, btw - run it separately enough times, they pass
    6. Add emulated vs user buffer map.
    7. Passed Gets test
    8. Found something weird; needed to call exttestdriversetup every test.
    9. Now with 16-18 failures
    10. Passing multitexture / texture combiner tests (albeit flaky, standalone)
    11. Fix cube/2d enablement
    12. 14-16 failures, works on Windows
    13. Simple (non-sprite) point rasterization, 13 failures
    14. Point sprite rasterization, 12 failures
    15. Fixed an issue when using buffer objects, non-enabled client states
        didn't upload their data correctly
    
    Current: better sample
    
    - think about a multitextured combine demo at some point
    
    BUG=angleproject:2306
    
    Change-Id: Ibc7708ac394c66bb73a39a9901939c6bad7e46d9

---
+++ samples/gles1/Triangle.cpp
@@ -0,0 +1,142 @@
+//
+// Copyright (c) 2014 The ANGLE Project Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+
+//            Based on Hello_Triangle.c from
+// Book:      OpenGL(R) ES 2.0 Programming Guide
+// Authors:   Aaftab Munshi, Dan Ginsburg, Dave Shreiner
+// ISBN-10:   0321502795
+// ISBN-13:   9780321502797
+// Publisher: Addison-Wesley Professional
+// URLs:      http://safari.informit.com/9780321563835
+//            http://www.opengles-book.com
+
+#include "SampleApplication.h"
+#include "shader_utils.h"
+
+#include <GLES/gl.h>
+
+#include "math.h"
+#ifndef _WIN32
+#include <sys/time.h>
+#endif
+
+#include <algorithm>
+
+class GLES1HelloTriangleSample : public SampleApplication
+{
+  public:
+    GLES1HelloTriangleSample(EGLint displayType)
+        : SampleApplication("GLES1HelloTriangle", 1280, 720, 1, 0, displayType)
+    {
+    }
+
+    virtual bool initialize()
+    {
+        glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
+
+        return true;
+    }
+
+    virtual void destroy()
+    {
+
+    }
+
+    virtual void draw()
+    {
+        GLfloat vertices[] =
+        {
+             0.0f,  0.5f, 0.0f,
+            -0.5f, -0.5f, 0.0f,
+             0.5f, -0.5f, 0.0f,
+        };
+
+        GLfloat colors[] =
+        {
+             1.0f,  0.0f, 0.0f,
+             0.0f,  1.0f, 0.0f,
+             0.0f,  0.0f, 1.0f,
+        };
+
+        // Set the viewport
+        int winWidth = getWindow()->getWidth();
+        int winHeight = getWindow()->getHeight();
+
+        int viewportDim = std::min(winWidth, winHeight);
+        int xshift = 0;
+        int yshift = 0;
+
+        xshift = (winWidth > viewportDim) ? ((winWidth - viewportDim) / 2) : 0;
+        yshift = (winHeight > viewportDim) ? ((winHeight - viewportDim) / 2) : 0;
+
+        glViewport(xshift, yshift, viewportDim, viewportDim);
+
+        // Clear the color buffer
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glEnableClientState(GL_VERTEX_ARRAY);
+
+        if (mTime < 200.0f) {
+            glShadeModel(GL_SMOOTH);
+            glEnableClientState(GL_COLOR_ARRAY);
+            glColorPointer(3, GL_FLOAT, 3 * sizeof(GLfloat), colors);
+        } else if (mTime >= 200.0f && mTime < 400.0f) {
+            glShadeModel(GL_FLAT);
+        } else {
+            glShadeModel(GL_SMOOTH);
+            glDisableClientState(GL_COLOR_ARRAY);
+            glColor4f(0.1f, mTime / mLoopTime, 0.4f, 1.0f);
+        }
+
+        if (mTime >= mLoopTime) {
+            mTime = 0;
+        }
+
+        glVertexPointer(3, GL_FLOAT, 3 * sizeof(GLfloat), vertices);
+
+        glMatrixMode(GL_MODELVIEW);
+
+        glLoadIdentity();
+        glTranslatef(sinf(2 * 3.14159265358979 * mTime / mLoopTime), 0.0f, 0.0f);
+        glRotatef(360.0f * (mTime / mLoopTime), 0.0f, 0.0f, 1.0f);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+#ifndef _WIN32
+        struct timeval tv;
+        gettimeofday(&tv, NULL);
+        uint64_t currTimeUs =
+            tv.tv_sec * 1000 * 1000 + tv.tv_usec;
+
+        if (currTimeUs - mTimeUs > 16667) {
+            mTime++;
+            mTimeUs = currTimeUs;
+        }
+#else
+		if (GetTickCount() - mTimeUs > 4) {
+			mTime++;
+			mTimeUs = GetTickCount();
+		}
+#endif
+    }
+
+  private:
+    uint64_t mTimeUs = 0;
+    float mTime = 0.0f;
+    float mLoopTime = 500.0f;
+};
+
+int main(int argc, char **argv)
+{
+    EGLint displayType = EGL_PLATFORM_ANGLE_TYPE_DEFAULT_ANGLE;
+
+    if (argc > 1)
+    {
+        displayType = GetDisplayTypeFromArg(argv[1]);
+    }
+
+    GLES1HelloTriangleSample app(displayType);
+    return app.run();
+}
---
+++ samples/sample_util/obj_utils.cpp
@@ -0,0 +1,119 @@
+//
+// Copyright (c) 2018 The ANGLE Project Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+
+// Based on:
+// https://github.com/LFY/lel-engine/blob/master/lel/OBJData.cpp
+
+#include "obj_utils.h"
+
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+ObjModel::ObjModel(const std::string &objFileName) {
+
+    std::ifstream stream(objFileName);
+
+    if (!stream)
+    {
+        std::cerr << "error opening obj file " << objFileName << " for reading.\n";
+        return;
+    }
+
+    float x, y, z;
+    unsigned int p0, t0, n0, p1, t1, n1, p2, t2, n2;
+
+    std::string line;
+
+    while (std::getline(stream, line)) {
+        if (sscanf(&line[0], "v %f %f %f", &x, &y, &z) == 3) {
+            mPositionList.push_back({x, y, z});
+        } else if (sscanf(&line[0], "vn %f %f %f", &x, &y, &z) == 3) {
+            mNormalList.push_back({x, y, z});
+        } else if (sscanf(&line[0], "vt %f %f", &x, &y) == 2) {
+            mTexcoordList.push_back({x, 1.0f - y}); // OpenGL has flipped texcoords vs Blender
+        } else if (sscanf(&line[0], "f %u/%u/%u %u/%u/%u %u/%u/%u",
+                          &p0, &t0, &n0, &p1, &t1, &n1, &p2, &t2, &n2) == 9) {
+            mIndices.push_back({p0, t0, n0,
+                             p1, t1, n1,
+                             p2, t2, n2});
+        }
+    }
+
+    for (unsigned int i = 0; i < (unsigned int) mIndices.size(); i++) {
+        unsigned int vertA = mIndices[i][0] - 1;
+        unsigned int vertB = mIndices[i][3] - 1;
+        unsigned int vertC = mIndices[i][6] - 1;
+
+        unsigned int normA = mIndices[i][2] - 1;
+        unsigned int normB = mIndices[i][5] - 1;
+        unsigned int normC = mIndices[i][8] - 1;
+
+        unsigned int texA = mIndices[i][1] - 1;
+        unsigned int texB = mIndices[i][4] - 1;
+        unsigned int texC = mIndices[i][7] - 1;
+
+        VertexKey keyA = {vertA, normA, texA};
+        VertexKey keyB = {vertB, normB, texB};
+        VertexKey keyC = {vertC, normC, texC};
+
+        VertexAttributes &currDataA = vertexDataMap[keyA];
+        VertexAttributes &currDataB = vertexDataMap[keyB];
+        VertexAttributes &currDataC = vertexDataMap[keyC];
+
+        indexDataMap[keyA] = 0;
+        indexDataMap[keyB] = 0;
+        indexDataMap[keyC] = 0;
+
+        unsigned int posAttribBytes = sizeof(float) * 3;
+        unsigned int normAttribBytes = sizeof(float) * 3;
+        unsigned int texcoordAttribBytes = sizeof(float) * 2;
+
+        memcpy(&currDataA.pos, &mPositionList[vertA][0], posAttribBytes);
+        memcpy(&currDataB.pos, &mPositionList[vertB][0], posAttribBytes);
+        memcpy(&currDataC.pos, &mPositionList[vertC][0], posAttribBytes);
+
+        memcpy(&currDataA.norm, &mNormalList[normA][0], normAttribBytes);
+        memcpy(&currDataB.norm, &mNormalList[normB][0], normAttribBytes);
+        memcpy(&currDataC.norm, &mNormalList[normC][0], normAttribBytes);
+
+        memcpy(&currDataA.texcoord, &mTexcoordList[texA][0], texcoordAttribBytes);
+        memcpy(&currDataB.texcoord, &mTexcoordList[texB][0], texcoordAttribBytes);
+        memcpy(&currDataC.texcoord, &mTexcoordList[texC][0], texcoordAttribBytes);
+
+    }
+
+    unsigned int actualIndexMapIndex = 0;
+    for (auto it : vertexDataMap) {
+        indexDataMap[it.first] = actualIndexMapIndex;
+        actualIndexMapIndex++;
+        vertexData.push_back(it.second);
+    }
+
+    for (unsigned int i = 0; i < (unsigned int) mIndices.size(); i++) {
+        unsigned int vertA = mIndices[i][0] - 1;
+        unsigned int vertB = mIndices[i][3] - 1;
+        unsigned int vertC = mIndices[i][6] - 1;
+
+        unsigned int normA = mIndices[i][2] - 1;
+        unsigned int normB = mIndices[i][5] - 1;
+        unsigned int normC = mIndices[i][8] - 1;
+
+        unsigned int texA = mIndices[i][1] - 1;
+        unsigned int texB = mIndices[i][4] - 1;
+        unsigned int texC = mIndices[i][7] - 1;
+
+        VertexKey keyA = {vertA, normA, texA};
+        VertexKey keyB = {vertB, normB, texB};
+        VertexKey keyC = {vertC, normC, texC};
+
+        indexData.push_back(indexDataMap[keyA]);
+        indexData.push_back(indexDataMap[keyB]);
+        indexData.push_back(indexDataMap[keyC]);
+    }
+}
