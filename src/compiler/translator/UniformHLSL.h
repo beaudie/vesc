aaa2f979aaccdc6a00748cca6207f83b7563d8dd
Olli Etuaho <oetuaho@nvidia.com>
2017-12-21 12:58:29 +0200

    Store referenced interface blocks in a cleaner data structure
    The previous code was hard to read since the referenced interface
    blocks stored a different type of node depending on if the interface
    block was instanced or not.
    
    BUG=angleproject:2267
    TEST=angle_unittests
    
    Change-Id: Ie8fdb61a17280ca0875159702f819b884d08706b

--- src/compiler/translator/UniformHLSL.h
+++ src/compiler/translator/UniformHLSL.h
@@ -36,7 +36,7 @@ class UniformHLSL : angle::NonCopyable
     // Must be called after uniformsHeader
     void samplerMetadataUniforms(TInfoSinkBase &out, const char *reg);
 
-    TString uniformBlocksHeader(const ReferencedSymbols &referencedInterfaceBlocks);
+    TString uniformBlocksHeader(const ReferencedInterfaceBlocks &referencedInterfaceBlocks);
 
     // Used for direct index references
     static TString UniformBlockInstanceString(const TString &instanceName, unsigned int arrayIndex);
@@ -52,7 +52,7 @@ class UniformHLSL : angle::NonCopyable
 
   private:
     TString uniformBlockString(const TInterfaceBlock &interfaceBlock,
-                               const TString &instanceName,
+                               const TVariable *instanceVariable,
                                unsigned int registerIndex,
                                unsigned int arrayIndex);
     TString uniformBlockMembersString(const TInterfaceBlock &interfaceBlock,
