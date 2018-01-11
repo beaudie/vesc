1a24941b4b711d3a517c50b1ae289fe00f0bca81
Frank Henigman <fjhenigman@chromium.org>
2018-01-11 17:15:23 -0500

    Add SanityTest.PlatformAvailabilityTest.
    Add a new test to check some basic assumptions about platform availability.
    This is useful because other graphics tests are filtered by platform
    availability, so if all availability checks fail no tests run and no
    failures are recorded.
    
    BUG=angleproject:2286
    
    Change-Id: Iac1fedc590c2d91c8db91f9389f4d7f7be35a601

---
+++ src/tests/gl_tests/SanityTest.cpp
@@ -0,0 +1,42 @@
+//
+// Copyright 2018 The ANGLE Project Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+// Sanity:
+//   Check EGL initialization.
+//
+
+#include "gpu_info_util/SystemInfo.h"
+#include "test_utils/ANGLETest.h"
+
+using namespace angle;
+
+namespace
+{
+
+TEST(SanityTest, PlatformAvailabilityTest)
+{
+    SystemInfo info;
+    GetSystemInfo(&info);
+
+    if (IsLinux())
+    {
+        EXPECT_TRUE(IsPlatformAvailable(ES2_OPENGL()));
+        EXPECT_TRUE(IsPlatformAvailable(ES2_VULKAN()));
+
+        if (IsIntel(info.gpus[0].vendorId))
+        {
+            EXPECT_TRUE(IsPlatformAvailable(ES2_OPENGLES()));
+        }
+    }
+
+    if (IsWindows())
+    {
+        EXPECT_TRUE(IsPlatformAvailable(ES2_OPENGL()));
+        EXPECT_TRUE(IsPlatformAvailable(ES2_OPENGLES()));
+        EXPECT_TRUE(IsPlatformAvailable(ES2_VULKAN()));
+    }
+}
+
+}  // anonymous namespace
