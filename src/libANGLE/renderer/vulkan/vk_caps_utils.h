962eab21b65127f649729f72cd3d4bc9a8b730c4
Luc Ferron <lucferron@google.com>
2018-01-25 13:25:27 -0500

    Begin implementing Vulkan caps mappings
    Bug: angleproject:1577
    
    Change-Id: Ibed36dee9120e9182362bc9858cf513f798079cf

---
+++ src/libANGLE/renderer/vulkan/vk_caps_utils.h
@@ -0,0 +1,32 @@
+//
+// Copyright 2016 The ANGLE Project Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+// vk_utils:
+//    Helper functions for the Vulkan Caps.
+//
+
+#ifndef LIBANGLE_RENDERER_VULKAN_VK_CAPS_UTILS_H_
+#define LIBANGLE_RENDERER_VULKAN_VK_CAPS_UTILS_H_
+
+#include <limits>
+
+#include <vulkan/vulkan.h>
+#include "libANGLE/Caps.h"
+
+namespace rx
+{
+
+namespace vk
+{
+
+void GenerateCaps(gl::Caps *outCaps,
+                  gl::TextureCapsMap * /*outTextureCaps*/,
+                  gl::Extensions *outExtensions,
+                  gl::Limitations * /* outLimitations */,
+                  const VkPhysicalDeviceProperties *physicalDeviceProperties);
+}
+}
+
+#endif
