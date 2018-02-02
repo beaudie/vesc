253659bf55d16cacbd8324f0b796603756cebb7a
Frank Henigman <fjhenigman@chromium.org>
2018-02-02 01:21:12 -0500

    New test for vertex attribute arrangements.
    Test various ways of organizing attributes: in memory or buffer object,
    sequential or interleaved.
    Test non-indexed draw, indices in buffer, indices in memory.
    
    BUG=angleproject:1683
    
    Change-Id: I54a752679b31bbb3f573c922d59da69a18302daa

---
+++ src/tests/gl_tests/AttributeLayoutTest.cpp
@@ -0,0 +1,414 @@
+//
+// Copyright 2018 The ANGLE Project Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+// AttributeLayoutTest:
+//   Test various layouts of vertex attribute data:
+//   - in memory, in buffer object, or combination of both
+//   - float, integer, or combination of both
+//   - sequential or interleaved
+
+#include <array>
+#include <vector>
+
+#include "test_utils/ANGLETest.h"
+
+using namespace angle;
+
+namespace
+{
+
+// Test will draw these four triangles.
+// clang-format off
+const GLfloat triangleData[] = {
+    // xy       rgb
+    0,0,        1,1,0,
+    -1,+1,      1,1,0,
+    +1,+1,      1,1,0,
+
+    0,0,        0,1,0,
+    +1,+1,      0,1,0,
+    +1,-1,      0,1,0,
+
+    0,0,        0,1,1,
+    +1,-1,      0,1,1,
+    -1,-1,      0,1,1,
+
+    0,0,        1,0,1,
+    -1,-1,      1,0,1,
+    -1,+1,      1,0,1,
+};
+// clang-format on
+
+constexpr size_t kNumVertex = sizeof(triangleData) / sizeof(triangleData[0]) / 5;
+
+// A container for one or more vertex attributes.
+class Container
+{
+  public:
+    static constexpr size_t kSize = 1024;
+
+    virtual void init(void)                                              = 0;
+    virtual void putData(unsigned offset, const void *data, size_t size) = 0;
+    virtual const char *getAddress()                                     = 0;
+    virtual GLuint getBuffer()                                           = 0;
+};
+
+// Vertex attribute data in client memory.
+class Memory : public Container
+{
+  public:
+    void init(void) override { memset(mMemory, 0, kSize); }
+
+    void putData(unsigned offset, const void *data, size_t size) override
+    {
+        ASSERT(offset + size <= kSize);
+        memcpy(mMemory + offset, data, size);
+    }
+
+    virtual const char *getAddress() override { return mMemory; }
+
+    virtual GLuint getBuffer() override { return 0; }
+
+  protected:
+    char mMemory[kSize];
+};
+
+// Vertex attribute data in buffer object.
+class Buffer : public Container
+{
+  public:
+    Buffer() : mBuffer(0) {}
+
+    ~Buffer() { glDeleteBuffers(1, &mBuffer); }
+
+    void init(void) override
+    {
+        if (!mBuffer)
+        {
+            glGenBuffers(1, &mBuffer);
+            mZeroes.fill(0);
+        }
+        glBindBuffer(GL_ARRAY_BUFFER, mBuffer);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(mZeroes), mZeroes.data(), GL_STATIC_DRAW);
+    }
+
+    void putData(unsigned offset, const void *data, size_t size) override
+    {
+        ASSERT(offset + size <= kSize);
+        glBindBuffer(GL_ARRAY_BUFFER, mBuffer);
+        glBufferSubData(GL_ARRAY_BUFFER, offset, size, data);
+    }
+
+    virtual const char *getAddress() override { return nullptr; }
+
+    virtual GLuint getBuffer() override { return mBuffer; }
+
+  protected:
+    GLenum mBuffer;
+    static std::array<char, kSize> mZeroes;
+};
+std::array<char, Buffer::kSize> Buffer::mZeroes;
+
+// clang-format off
+template<class Type> struct GLType {};
+template<> struct GLType<GLbyte  > { static constexpr GLenum kGLType = GL_BYTE; };
+template<> struct GLType<GLubyte > { static constexpr GLenum kGLType = GL_UNSIGNED_BYTE; };
+template<> struct GLType<GLshort > { static constexpr GLenum kGLType = GL_SHORT; };
+template<> struct GLType<GLushort> { static constexpr GLenum kGLType = GL_UNSIGNED_SHORT; };
+template<> struct GLType<GLfloat > { static constexpr GLenum kGLType = GL_FLOAT; };
+// clang-format on
+
+// Encapsulates the data for one vertex attribute, where it lives, and how it is layed out.
+class Attrib
+{
+  public:
+    template <class T>
+    Attrib(Container &container, unsigned offset, unsigned stride, const T &data)
+        : mContainer(container),
+          mOffset(offset),
+          mStride(stride),
+          mData(reinterpret_cast<const char *>(data.data())),
+          mDimension(data.size() / kNumVertex),
+          mAttribSize(mDimension * sizeof(typename T::value_type)),
+          mGLType(GLType<typename T::value_type>::kGLType)
+    {
+    }
+
+    void initContainer(void) const { mContainer.init(); }
+
+    void fillContainer(void) const
+    {
+        unsigned offset = mOffset;
+        for (unsigned i = 0; i < kNumVertex; ++i)
+        {
+            mContainer.putData(offset, mData + i * mAttribSize, mAttribSize);
+            offset += mStride;
+        }
+    }
+
+    void enable(unsigned index) const
+    {
+        glBindBuffer(GL_ARRAY_BUFFER, mContainer.getBuffer());
+        glVertexAttribPointer(index, mDimension, mGLType, GL_FALSE, mStride,
+                              mContainer.getAddress() + mOffset);
+        glEnableVertexAttribArray(index);
+    }
+
+  protected:
+    Container &mContainer;
+    unsigned mOffset;
+    unsigned mStride;
+    const char *mData;
+    int mDimension;
+    size_t mAttribSize;
+    GLenum mGLType;
+};
+
+typedef std::vector<Attrib> TestCase;
+
+void PrepareTestCase(const TestCase &tc)
+{
+    for (const Attrib &a : tc)
+    {
+        a.initContainer();
+    }
+    for (const Attrib &a : tc)
+    {
+        a.fillContainer();
+    }
+    unsigned i = 0;
+    for (const Attrib &a : tc)
+    {
+        a.enable(i++);
+    }
+}
+
+template <class Type, size_t Dimension>
+using VertexData = std::array<Type, Dimension * kNumVertex>;
+
+class AttributeLayoutTest : public ANGLETest
+{
+  protected:
+    AttributeLayoutTest() : mProgram(0)
+    {
+        setWindowWidth(128);
+        setWindowHeight(128);
+        setConfigRedBits(8);
+        setConfigGreenBits(8);
+        setConfigBlueBits(8);
+        setConfigAlphaBits(8);
+    }
+
+    void PrepareVertexData(void);
+    void GetTestCases(void);
+
+    void SetUp() override
+    {
+        ANGLETest::SetUp();
+
+        glClearColor(0, 0, 0, 0);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glDisable(GL_DEPTH_TEST);
+
+        const std::string vertexSource =
+            "attribute mediump vec2 coord;\n"
+            "attribute mediump vec3 color;\n"
+            "varying mediump vec3 vcolor;\n"
+            "void main(void)\n"
+            "{\n"
+            "    gl_Position = vec4(coord, 0, 1);\n"
+            "    vcolor = color;\n"
+            "}\n";
+
+        const std::string fragmentSource =
+            "varying mediump vec3 vcolor;\n"
+            "void main(void)\n"
+            "{\n"
+            "    gl_FragColor = vec4(vcolor, 0);\n"
+            "}\n";
+
+        mProgram = CompileProgram(vertexSource, fragmentSource);
+        ASSERT_NE(0u, mProgram);
+        glUseProgram(mProgram);
+
+        glGenBuffers(1, &mIndexBuffer);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, mIndexBuffer);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(mIndices), mIndices, GL_STATIC_DRAW);
+
+        PrepareVertexData();
+        GetTestCases();
+    }
+
+    void TearDown() override
+    {
+        glDeleteProgram(mProgram);
+        glDeleteBuffers(1, &mIndexBuffer);
+        ANGLETest::TearDown();
+    }
+
+    virtual void Draw(void) = 0;
+
+    void Run(void)
+    {
+        glViewport(0, 0, getWindowWidth(), getWindowHeight());
+        glUseProgram(mProgram);
+
+        for (unsigned i = 0; i < mTestCases.size(); ++i)
+        {
+            PrepareTestCase(mTestCases[i]);
+
+            glClear(GL_COLOR_BUFFER_BIT);
+            Draw();
+
+            int w = getWindowWidth() / 4;
+            int h = getWindowHeight() / 4;
+            EXPECT_PIXEL_EQ(w * 1, h * 2, 255, 0, 255, 0) << " test case " << i;
+            EXPECT_PIXEL_EQ(w * 3, h * 2, 0, 255, 0, 0) << " test case " << i;
+            EXPECT_PIXEL_EQ(w * 2, h * 1, 0, 255, 255, 0) << " test case " << i;
+            EXPECT_PIXEL_EQ(w * 2, h * 3, 255, 255, 0, 0) << " test case " << i;
+        }
+    }
+
+    static const GLushort mIndices[kNumVertex];
+
+    GLuint mProgram;
+    GLuint mIndexBuffer;
+
+    std::vector<TestCase> mTestCases;
+
+    VertexData<GLfloat, 2> mCoord;
+    VertexData<GLfloat, 3> mColor;
+    VertexData<GLbyte, 3> mBColor;
+};
+const GLushort AttributeLayoutTest::mIndices[kNumVertex] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+
+void AttributeLayoutTest::PrepareVertexData(void)
+{
+    // Compiler complains about unused variable without these.
+    (void)GLType<GLbyte>::kGLType;
+    (void)GLType<GLubyte>::kGLType;
+    (void)GLType<GLshort>::kGLType;
+    (void)GLType<GLushort>::kGLType;
+    (void)GLType<GLfloat>::kGLType;
+
+    mCoord.fill(0);
+    mColor.fill(0);
+    mBColor.fill(0);
+
+    for (unsigned i = 0; i < kNumVertex; ++i)
+    {
+        GLfloat x = triangleData[i * 5 + 0];
+        GLfloat y = triangleData[i * 5 + 1];
+        GLfloat r = triangleData[i * 5 + 2];
+        GLfloat g = triangleData[i * 5 + 3];
+        GLfloat b = triangleData[i * 5 + 4];
+
+        mCoord[i * 2 + 0] = x;
+        mCoord[i * 2 + 1] = y;
+
+        mColor[i * 3 + 0] = r;
+        mColor[i * 3 + 1] = g;
+        mColor[i * 3 + 2] = b;
+
+        mBColor[i * 3 + 0] = r;
+        mBColor[i * 3 + 1] = g;
+        mBColor[i * 3 + 2] = b;
+    }
+}
+
+void AttributeLayoutTest::GetTestCases(void)
+{
+    static Memory M0, M1;
+    static Buffer B0, B1;
+
+    // two buffers
+    mTestCases.push_back({Attrib(B0, 0, 8, mCoord), Attrib(B1, 0, 12, mColor)});
+
+    // one buffer, sequential
+    mTestCases.push_back({Attrib(B0, 0, 8, mCoord), Attrib(B0, 96, 12, mColor)});
+
+    if (IsVulkan())
+    {
+        std::cout << "cases skipped on Vulkan: non-float vertex data and vertex data unsupported"
+                  << std::endl;
+        return;
+    }
+
+    // one buffer, interleaved - ANGLE bug 2310
+    mTestCases.push_back({Attrib(B0, 0, 20, mCoord), Attrib(B0, 8, 20, mColor)});
+
+    // two memory
+    mTestCases.push_back({Attrib(M0, 0, 8, mCoord), Attrib(M1, 0, 12, mColor)});
+
+    // one memory, sequential
+    mTestCases.push_back({Attrib(M0, 0, 8, mCoord), Attrib(M0, 96, 12, mColor)});
+
+    // one memory, interleaved
+    mTestCases.push_back({Attrib(M0, 0, 20, mCoord), Attrib(M0, 8, 20, mColor)});
+
+    // buffer and memory
+    mTestCases.push_back({Attrib(B0, 0, 8, mCoord), Attrib(M0, 0, 12, mColor)});
+
+    // float and integer
+    mTestCases.push_back({Attrib(B0, 0, 8, mCoord), Attrib(M0, 0, 12, mBColor)});
+}
+
+class AttributeLayoutNonIndexed : public AttributeLayoutTest
+{
+    void Draw(void) override
+    {
+        // TODO(fjhenigman): try another value for first vertex, besides 0
+        glDrawArrays(GL_TRIANGLES, 0, kNumVertex);
+    }
+};
+
+class AttributeLayoutMemoryIndexed : public AttributeLayoutTest
+{
+    void Draw(void) override
+    {
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+        glDrawElements(GL_TRIANGLES, kNumVertex, GL_UNSIGNED_SHORT, mIndices);
+    }
+};
+
+class AttributeLayoutBufferIndexed : public AttributeLayoutTest
+{
+    void Draw(void) override
+    {
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, mIndexBuffer);
+        glDrawElements(GL_TRIANGLES, kNumVertex, GL_UNSIGNED_SHORT, nullptr);
+    }
+};
+
+TEST_P(AttributeLayoutNonIndexed, Test)
+{
+    Run();
+}
+
+TEST_P(AttributeLayoutMemoryIndexed, Test)
+{
+    if (IsVulkan())
+    {
+        std::cout << "test skipped on Vulkan: indices in memory not supported" << std::endl;
+        return;
+    }
+    Run();
+}
+
+TEST_P(AttributeLayoutBufferIndexed, Test)
+{
+    Run();
+}
+
+#define PARAMS                                                                            \
+    ES2_VULKAN(), ES2_OPENGL(), ES2_D3D9(), ES2_D3D11(), ES2_D3D11_FL9_3(), ES3_OPENGL(), \
+        ES2_OPENGLES(), ES3_OPENGLES()
+
+ANGLE_INSTANTIATE_TEST(AttributeLayoutNonIndexed, PARAMS);
+ANGLE_INSTANTIATE_TEST(AttributeLayoutMemoryIndexed, PARAMS);
+ANGLE_INSTANTIATE_TEST(AttributeLayoutBufferIndexed, PARAMS);
+
+}  // anonymous namespace
