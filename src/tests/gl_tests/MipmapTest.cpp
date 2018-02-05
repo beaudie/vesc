cc1be8ec7f42faefbe138a43627624ac8e4b543f
Yunchao He <yunchao.he@intel.com>
2018-02-05 17:52:27 +0800

    Clean up end2end tests for Intel Linux platform.
    Removed tests can pass on Intel Linux Desktop (Intel KabyLake, Ubuntu 17.04)
    with system default mesa and latest upstream mesa.
    
    BUG=angleproject:2205
    
    Change-Id: Ie3f0f34b9708a7dab81f66d9ec83a469658deee7

--- src/tests/gl_tests/MipmapTest.cpp
+++ src/tests/gl_tests/MipmapTest.cpp
@@ -603,7 +603,7 @@ TEST_P(MipmapTest, GenerateMipmapFromRenderedImage)
 TEST_P(MipmapTest, RenderOntoLevelZeroAfterGenerateMipmap)
 {
     // TODO(geofflang): Figure out why this is broken on AMD OpenGL
-    if ((IsAMD() || IsIntel()) && getPlatformRenderer() == EGL_PLATFORM_ANGLE_TYPE_OPENGL_ANGLE)
+    if (IsAMD() && getPlatformRenderer() == EGL_PLATFORM_ANGLE_TYPE_OPENGL_ANGLE)
     {
         std::cout << "Test skipped on Intel/AMD OpenGL." << std::endl;
         return;
