a86085c31f415f78b324f5a3a194df0ab755a636
Luc Ferron <lucferron@chromium.org>
2018-02-07 17:02:08 -0500

    Vulkan: Texture caps map
    We are generating the caps map using various bits from the results of
    vkGetPhysicalDeviceFormatProperties. This contains all the information
    required to fill out "renderable", "texturable" and "filterable".
    
    Later we'll need to read from vkGetPhysicalDeviceImageFormatProperties as
    well to get the sampleCounts.
    
    The tests for now are not very meaningful since they use the same logic
    as the caps code to build the expected results, however as soon as we'll
    add the hard-coded list of mandatory texture caps, the test will be a good
    validator that we are not breaking anything.
    
    We only check the optimal set of flags since it is the most restrictive
    set.
    
    isTexturable = flags contain VK_FORMAT_FEATURE_SAMPLED_IMAGE_BIT
    isFilterable = flags contain VK_FORMAT_FEATURE_SAMPLED_IMAGE_FILTER_LINEAR_BIT
    isRenderable = flags contain VK_FORMAT_FEATURE_COLOR_ATTACHMENT_BIT OR
    			     VK_FORMAT_FEATURE_DEPTH_STENCIL_ATTACHMENT_BIT
    
    Bug: angleproject:2348
    
    Change-Id: I9da0712190d2678d7e377d6fcd4ca83d23eefd38

--- src/libANGLE/renderer/vulkan/vk_caps_utils.h
+++ src/libANGLE/renderer/vulkan/vk_caps_utils.h
@@ -11,6 +11,7 @@
 #define LIBANGLE_RENDERER_VULKAN_VK_CAPS_UTILS_H_
 
 #include <vulkan/vulkan.h>
+#include "libANGLE/Caps.h"
 
 namespace gl
 {
@@ -25,12 +26,15 @@ namespace rx
 
 namespace vk
 {
+class FormatTable;
 
 void GenerateCaps(const VkPhysicalDeviceProperties &physicalDeviceProperties,
+                  const gl::TextureCapsMap &textureCaps,
                   gl::Caps *outCaps,
-                  gl::TextureCapsMap * /*outTextureCaps*/,
                   gl::Extensions *outExtensions,
                   gl::Limitations * /* outLimitations */);
+
+gl::TextureCaps GenerateTextureFormatCaps(const VkFormatProperties &formatProperties);
 }  // namespace vk
 }  // namespace rx
 
