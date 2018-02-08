18adccec360606e1dc41b54947d3e6cd778f0947
Olli Etuaho <oetuaho@nvidia.com>
2018-02-08 14:46:44 +0200

    Statically allocate built-in variable symbols
    A script gen_builtin_symbols.py now generates code for initializing
    built-in variable symbols.
    
    The symbols are defined in a header file so they can be referenced
    from AST traversers as well.
    
    BUG=angleproject:2267
    TEST=angle_unittests
    
    Change-Id: Ida7f3aeb06d2bce0f737f1483b1bd5833aeddd2e

--- src/compiler/translator/SymbolTable.h
+++ src/compiler/translator/SymbolTable.h
@@ -250,6 +250,7 @@ class TSymbolTable : angle::NonCopyable
                               SymbolType symbolType);
 
     bool insert(ESymbolLevel level, TSymbol *symbol);
+    void insertBuiltIn(ESymbolLevel level, const TSymbol *symbol);
 
     TFunction *findUserDefinedFunction(const ImmutableString &name) const;
 
@@ -268,6 +269,10 @@ class TSymbolTable : angle::NonCopyable
                                     ShShaderSpec spec,
                                     const ShBuiltInResources &resources);
     void markBuiltInInitializationFinished();
+
+    void insertStaticBuiltInVariables(sh::GLenum shaderType,
+                                      ShShaderSpec spec,
+                                      const ShBuiltInResources &resources);
 
     std::vector<std::unique_ptr<TSymbolTableBuiltInLevel>> mBuiltInTable;
     std::vector<std::unique_ptr<TSymbolTableLevel>> mTable;
