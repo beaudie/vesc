174be996148d1946e94e37447d5394b03a3cd979
Corentin Wallez <cwallez@chromium.org>
2018-02-27 15:17:10 -0500

    Use packed enums for the texture types and targets, part 2
    This completes the refactor by using the packed enums in the gl:: layer
    and in the backends.
    
    The packed enum code generation is modified to support explicitly
    assigning values to the packed enums so that the TextureTarget cube map
    faces are in the correct order and easy to iterate over.
    
    BUG=angleproject:2169
    
    Change-Id: I5903235e684ccf382e92a8a1e10c5c85b4b16a04

--- src/libANGLE/queryutils.cpp
+++ src/libANGLE/queryutils.cpp
@@ -33,7 +33,7 @@ namespace
 
 template <typename ParamType>
 void QueryTexLevelParameterBase(const Texture *texture,
-                                GLenum target,
+                                TextureTarget target,
                                 GLint level,
                                 GLenum pname,
                                 ParamType *params)
@@ -848,8 +848,16 @@ void QueryFramebufferAttachmentParameteriv(const Context *context,
             break;
 
         case GL_FRAMEBUFFER_ATTACHMENT_TEXTURE_CUBE_MAP_FACE:
-            *params = attachmentObject->cubeMapFace();
-            break;
+        {
+            *params = GL_NONE;
+
+            TextureTarget face = attachmentObject->cubeMapFace();
+            if (face != TextureTarget::InvalidEnum)
+            {
+                *params = ToGLenum(attachmentObject->cubeMapFace());
+            }
+        }
+        break;
 
         case GL_FRAMEBUFFER_ATTACHMENT_RED_SIZE:
             *params = attachmentObject->getRedSize();
@@ -1099,7 +1107,7 @@ void QueryShaderiv(const Context *context, Shader *shader, GLenum pname, GLint *
 }
 
 void QueryTexLevelParameterfv(const Texture *texture,
-                              GLenum target,
+                              TextureTarget target,
                               GLint level,
                               GLenum pname,
                               GLfloat *params)
@@ -1108,7 +1116,7 @@ void QueryTexLevelParameterfv(const Texture *texture,
 }
 
 void QueryTexLevelParameteriv(const Texture *texture,
-                              GLenum target,
+                              TextureTarget target,
                               GLint level,
                               GLenum pname,
                               GLint *params)
