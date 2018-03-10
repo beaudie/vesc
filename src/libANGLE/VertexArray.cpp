39c82326fca82c9f8db693e812c7b643cdabaaff
Jamie Madill <jmadill@chromium.org>
2018-03-10 12:56:34 -0500

    Return gl::Error from VertexArray::syncState.
    Sync this method will be doing non-trivial updates in the future, we
    will need to be able to handle gl::Errors generated by the driver.
    
    Bug: angleproject:2389
    
    Change-Id: Ia52b95e6adb1eaaa4fe46a4861131bae783e68b4

--- src/libANGLE/VertexArray.cpp
+++ src/libANGLE/VertexArray.cpp
@@ -260,13 +260,14 @@ void VertexArray::setElementArrayBuffer(const Context *context, Buffer *buffer)
     mDirtyBits.set(DIRTY_BIT_ELEMENT_ARRAY_BUFFER);
 }
 
-void VertexArray::syncState(const Context *context, const DrawCallParams &drawCallParams)
+Error VertexArray::syncState(const Context *context, const DrawCallParams &drawCallParams)
 {
     // We call syncState even if we have no dirty bits, so the back end can do any draw call
     // related state updates. Some state updates might depends on drawCallParams changes rather
     // than dirty bits themselves.
-    mVertexArray->syncState(context, mDirtyBits, drawCallParams);
+    ANGLE_TRY(mVertexArray->syncState(context, mDirtyBits, drawCallParams));
     mDirtyBits.reset();
+    return NoError();
 }
 
 void VertexArray::onBindingChanged(bool bound)
