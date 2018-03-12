a623144758e749c670b2db65afc8726ebbfd4f53
Jamie Madill <jmadill@chromium.org>
2018-03-12 10:53:23 -0400

    Move Buffer Subject/Observer to front end.
    This makes BufferImpl into an Observer Subject. It also refactors
    the Vertex Array updates for the D3D11 backend use more of a dirty
    bit coding style.
    
    Bug: angleproject:2389
    
    Change-Id: Ibd7095c043451626fe27a742979a48faf1f1f135

--- src/libANGLE/renderer/d3d/IndexDataManager.cpp
+++ src/libANGLE/renderer/d3d/IndexDataManager.cpp
@@ -300,7 +300,6 @@ gl::Error IndexDataManager::getStreamingIndexBuffer(GLenum destinationIndexType,
 }
 
 gl::Error GetIndexTranslationDestType(const gl::Context *context,
-                                      GLenum srcType,
                                       const gl::DrawCallParams &drawCallParams,
                                       bool usePrimitiveRestartWorkaround,
                                       GLenum *destTypeOut)
@@ -309,16 +308,23 @@ gl::Error GetIndexTranslationDestType(const gl::Context *context,
     // see http://msdn.microsoft.com/en-us/library/windows/desktop/bb205124(v=vs.85).aspx
     if (usePrimitiveRestartWorkaround)
     {
+        // Conservatively assume we need to translate the indices for draw indirect.
+        if (drawCallParams.isDrawIndirect())
+        {
+            *destTypeOut = GL_UNSIGNED_INT;
+            return gl::NoError();
+        }
+
         ANGLE_TRY(drawCallParams.ensureIndexRangeResolved(context));
         const gl::IndexRange &indexRange = drawCallParams.getIndexRange();
-        if (indexRange.end == gl::GetPrimitiveRestartIndex(srcType))
+        if (indexRange.end == gl::GetPrimitiveRestartIndex(drawCallParams.type()))
         {
             *destTypeOut = GL_UNSIGNED_INT;
             return gl::NoError();
         }
     }
 
-    *destTypeOut = (srcType == GL_UNSIGNED_INT) ? GL_UNSIGNED_INT : GL_UNSIGNED_SHORT;
+    *destTypeOut = (drawCallParams.type() == GL_UNSIGNED_INT) ? GL_UNSIGNED_INT : GL_UNSIGNED_SHORT;
     return gl::NoError();
 }
 
