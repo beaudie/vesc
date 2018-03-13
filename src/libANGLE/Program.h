194eba56052f24388dcb354d03e7cb833d855951
James Darpinian <jdarpinian@chromium.org>
2018-03-12 17:26:57 -0700

    Check that transform feedback will not overflow its buffers.
    Also fix the check for uniform buffer size to use the actual buffer
    size instead of the size of the bound range.
    
    Bug: 820639
    Change-Id: Iaa2a617ee7ce5ce7cfabbf64bd1d6f8c82c46b65

--- src/libANGLE/Program.h
+++ src/libANGLE/Program.h
@@ -432,6 +432,9 @@ class ProgramState final : angle::NonCopyable
     GLenum mGeometryShaderOutputPrimitiveType;
     int mGeometryShaderInvocations;
     int mGeometryShaderMaxVertices;
+
+    // The size of the data written to each transform feedback buffer per vertex.
+    std::vector<GLsizei> mTransformFeedbackStrides;
 };
 
 class ProgramBindings final : angle::NonCopyable
@@ -692,6 +695,11 @@ class Program final : angle::NonCopyable, public LabeledObject
     ComponentTypeMask getAttributesTypeMask() const { return mState.mAttributesTypeMask; }
     AttributesMask getAttributesMask() const { return mState.mAttributesMask; }
 
+    const std::vector<GLsizei> &getTransformFeedbackStrides() const
+    {
+        return mState.mTransformFeedbackStrides;
+    }
+
   private:
     ~Program() override;
 
@@ -744,6 +752,7 @@ class Program final : angle::NonCopyable, public LabeledObject
                                        const Caps &caps) const;
     bool linkValidateGlobalNames(const Context *context, InfoLog &infoLog) const;
 
+    void updateTransformFeedbackStrides();
     void gatherTransformFeedbackVaryings(const ProgramMergedVaryings &varyings);
 
     ProgramMergedVaryings getMergedVaryings(const Context *context) const;
