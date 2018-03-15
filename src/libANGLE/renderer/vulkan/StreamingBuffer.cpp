0e4476bad69c540e385b0ad7ccf1f052b19620a7
Luc Ferron <lucferron@chromium.org>
2018-03-15 10:17:04 -0400

    Vulkan: Dynamic update of uniforms
    - This change enables us to update uniforms indefintely using
    VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER_DYNAMIC descriptor types.
    
    - Enables 219 new dEQP tests in the uniform_api namespace.
    
    - Creates a new white box test to validate new buffer allocation.
    
    bug:angleproject:2392
    
    Change-Id: I8146e6104a6b7727f63265a4671577d251a8fca8

--- src/libANGLE/renderer/vulkan/StreamingBuffer.cpp
+++ src/libANGLE/renderer/vulkan/StreamingBuffer.cpp
@@ -9,6 +9,7 @@
 //
 
 #include "StreamingBuffer.h"
+#include <iostream>
 
 #include "anglebase/numerics/safe_math.h"
 
@@ -17,28 +18,41 @@
 
 namespace rx
 {
-StreamingBuffer::StreamingBuffer(VkBufferUsageFlags usage, size_t minSize, size_t minAlignment)
+StreamingBuffer::StreamingBuffer(VkBufferUsageFlags usage, size_t minSize)
     : mUsage(usage),
       mMinSize(minSize),
       mNextWriteOffset(0),
       mLastFlushOffset(0),
       mSize(0),
-      mMinAlignment(minAlignment),
+      mMinAlignment(0),
       mMappedMemory(nullptr)
 {
 }
 
-StreamingBuffer::~StreamingBuffer()
+void StreamingBuffer::init(size_t minAlignment)
 {
+    ASSERT(!valid() && minAlignment > 0);
+    mMinAlignment = minAlignment;
 }
 
-gl::Error StreamingBuffer::allocate(ContextVk *context,
+StreamingBuffer::~StreamingBuffer()
+{
+    ASSERT(mMinAlignment == 0);
+}
+
+bool StreamingBuffer::valid()
+{
+    return mMinAlignment > 0;
+}
+
+vk::Error StreamingBuffer::allocate(ContextVk *context,
                                     size_t sizeInBytes,
                                     uint8_t **ptrOut,
                                     VkBuffer *handleOut,
                                     VkDeviceSize *offsetOut,
                                     bool *outNewBufferAllocated)
 {
+    ASSERT(valid());
     RendererVk *renderer = context->getRenderer();
 
     // TODO(fjhenigman): Update this when we have buffers that need to
@@ -92,18 +106,26 @@ gl::Error StreamingBuffer::allocate(ContextVk *context,
         }
     }
 
+    std::cout << "Size to allocate: " << sizeToAllocate << ", "
+              << "SizeInBytes: " << sizeInBytes << ", Alignment: " << mMinAlignment
+              << ", Next offset: " << mNextWriteOffset << ", Max Size: " << mSize << std::endl;
+
     ASSERT(mBuffer.valid());
-    *handleOut = mBuffer.getHandle();
+
+    if (handleOut != nullptr)
+    {
+        *handleOut = mBuffer.getHandle();
+    }
 
     ASSERT(mMappedMemory);
     *ptrOut    = mMappedMemory + mNextWriteOffset;
     *offsetOut = mNextWriteOffset;
     mNextWriteOffset += sizeToAllocate;
 
-    return gl::NoError();
+    return vk::NoError();
 }
 
-gl::Error StreamingBuffer::flush(ContextVk *context)
+vk::Error StreamingBuffer::flush(ContextVk *context)
 {
     if (mNextWriteOffset > mLastFlushOffset)
     {
@@ -117,13 +139,27 @@ gl::Error StreamingBuffer::flush(ContextVk *context)
 
         mLastFlushOffset = mNextWriteOffset;
     }
-    return gl::NoError();
+    return vk::NoError();
 }
 
 void StreamingBuffer::destroy(VkDevice device)
 {
+    mMinAlignment = 0;
     mBuffer.destroy(device);
     mMemory.destroy(device);
 }
 
+VkBuffer StreamingBuffer::getCurrentBufferHandle() const
+{
+    return mBuffer.getHandle();
+}
+
+void StreamingBuffer::setMinimumSize(size_t minSize)
+{
+    // This will really only have an effect next time we call allocate.
+    mMinSize = minSize;
+
+    // Forces a new allocation on the next allocate.
+    mSize = 0;
+}
 }  // namespace rx
