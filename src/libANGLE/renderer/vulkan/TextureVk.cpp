137cc81d6e9b11bd7eeb038ac3a404cfa2f56d7f
Jamie Madill <jmadill@chromium.org>
2018-03-29 13:34:30 -0400

    Texture: Pass ImageIndex to relevant methods.
    The Vulkan back-end will be using this helper struct, so maky it
    everywhere. This cleans up a lot of the code for D3D as well.
    
    Potentially in the future we could generate these in the entry
    points if we supported packing multiple arguments into one.
    
    Also changes a few parameter types to GLint for compatibility.
    
    Bug: angleproject:2318
    Change-Id: I57cea4a42483ab51859229517d783b58f206b8e7

--- src/libANGLE/renderer/vulkan/TextureVk.cpp
+++ src/libANGLE/renderer/vulkan/TextureVk.cpp
@@ -162,21 +162,14 @@ gl::Error TextureVk::onDestroy(const gl::Context *context)
     ContextVk *contextVk = vk::GetImpl(context);
     RendererVk *renderer = contextVk->getRenderer();
 
-    mImage.release(renderer->getCurrentQueueSerial(), renderer);
-
-    renderer->releaseResource(*this, &mImageView);
+    releaseImage(context, renderer);
     renderer->releaseResource(*this, &mSampler);
-
-    mStagingStorage.release(renderer);
-
-    onStateChange(context, angle::SubjectMessage::DEPENDENT_DIRTY_BITS);
 
     return gl::NoError();
 }
 
 gl::Error TextureVk::setImage(const gl::Context *context,
-                              gl::TextureTarget target,
-                              size_t level,
+                              const gl::ImageIndex &index,
                               GLenum internalFormat,
                               const gl::Extents &size,
                               GLenum format,
@@ -189,20 +182,18 @@ gl::Error TextureVk::setImage(const gl::Context *context,
     VkDevice device      = contextVk->getDevice();
 
     // TODO(jmadill): support multi-level textures.
-    ASSERT(level == 0);
+    ASSERT(index.mipIndex == 0);
+
+    // Convert internalFormat to sized internal format.
+    const gl::InternalFormat &formatInfo = gl::GetInternalFormatInfo(internalFormat, type);
 
     if (mImage.valid())
     {
-        const gl::ImageDesc &desc = mState.getImageDesc(target, level);
-
-        // TODO(jmadill): Consider comparing stored vk::Format.
-        if (desc.size != size ||
-            !gl::Format::SameSized(desc.format, gl::Format(internalFormat, type)))
+        const gl::ImageDesc &desc = mState.getImageDesc(index);
+        const vk::Format &vkFormat = renderer->getFormat(formatInfo.sizedInternalFormat);
+        if (desc.size != size || mImage.getFormat() != vkFormat)
         {
-            mImage.release(renderer->getCurrentQueueSerial(), renderer);
-            renderer->releaseResource(*this, &mImageView);
-
-            onStateChange(context, angle::SubjectMessage::DEPENDENT_DIRTY_BITS);
+            releaseImage(context, renderer);
         }
     }
 
@@ -213,7 +204,7 @@ gl::Error TextureVk::setImage(const gl::Context *context,
     }
 
     // TODO(jmadill): Cube map textures. http://anglebug.com/2318
-    if (target != gl::TextureTarget::_2D)
+    if (index.target != gl::TextureTarget::_2D)
     {
         UNIMPLEMENTED();
         return gl::InternalError();
@@ -252,8 +243,6 @@ gl::Error TextureVk::setImage(const gl::Context *context,
     // Handle initial data.
     if (pixels)
     {
-        // Convert internalFormat to sized internal format.
-        const gl::InternalFormat &formatInfo = gl::GetInternalFormatInfo(internalFormat, type);
         ANGLE_TRY(mStagingStorage.stageSubresourceUpdate(contextVk, size, formatInfo, unpack, type,
                                                          pixels));
     }
@@ -262,8 +251,7 @@ gl::Error TextureVk::setImage(const gl::Context *context,
 }
 
 gl::Error TextureVk::setSubImage(const gl::Context *context,
-                                 gl::TextureTarget target,
-                                 size_t level,
+                                 const gl::ImageIndex &index,
                                  const gl::Box &area,
                                  GLenum format,
                                  GLenum type,
@@ -279,8 +267,7 @@ gl::Error TextureVk::setSubImage(const gl::Context *context,
 }
 
 gl::Error TextureVk::setCompressedImage(const gl::Context *context,
-                                        gl::TextureTarget target,
-                                        size_t level,
+                                        const gl::ImageIndex &index,
                                         GLenum internalFormat,
                                         const gl::Extents &size,
                                         const gl::PixelUnpackState &unpack,
@@ -292,8 +279,7 @@ gl::Error TextureVk::setCompressedImage(const gl::Context *context,
 }
 
 gl::Error TextureVk::setCompressedSubImage(const gl::Context *context,
-                                           gl::TextureTarget target,
-                                           size_t level,
+                                           const gl::ImageIndex &index,
                                            const gl::Box &area,
                                            GLenum format,
                                            const gl::PixelUnpackState &unpack,
@@ -305,8 +291,7 @@ gl::Error TextureVk::setCompressedSubImage(const gl::Context *context,
 }
 
 gl::Error TextureVk::copyImage(const gl::Context *context,
-                               gl::TextureTarget target,
-                               size_t level,
+                               const gl::ImageIndex &index,
                                const gl::Rectangle &sourceArea,
                                GLenum internalFormat,
                                gl::Framebuffer *source)
@@ -316,8 +301,7 @@ gl::Error TextureVk::copyImage(const gl::Context *context,
 }
 
 gl::Error TextureVk::copySubImage(const gl::Context *context,
-                                  gl::TextureTarget target,
-                                  size_t level,
+                                  const gl::ImageIndex &index,
                                   const gl::Offset &destOffset,
                                   const gl::Rectangle &sourceArea,
                                   gl::Framebuffer *source)
@@ -488,6 +472,15 @@ const vk::Sampler &TextureVk::getSampler() const
 {
     ASSERT(mSampler.valid());
     return mSampler;
+}
+
+void TextureVk::releaseImage(const gl::Context *context, RendererVk *renderer)
+{
+    mImage.release(renderer->getCurrentQueueSerial(), renderer);
+    renderer->releaseResource(*this, &mImageView);
+    onStateChange(context, angle::SubjectMessage::DEPENDENT_DIRTY_BITS);
+
+    mStagingStorage.release(renderer);
 }
 
 }  // namespace rx
