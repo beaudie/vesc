c6798d5cf5d97e92a3484c4fdac3acbd6700be26
Luc Ferron <lucferron@chromium.org>
2018-04-04 11:41:44 -0400

    Vulkan: Implement color mask and depth mask bits
    These two features are heavily used in the
    functional.depth_stencil_clear.* dEQP tests.
    
    Enable a bunch of color/depth/stencil clear tests, however there is
    still 2 tests in particular that are giving me trouble. I will work
    on them separately in a subsequent change.
    
    Bug: angleproject:2443
    Bug: angleproject:2455
    
    Change-Id: Ic93420c7b525b424e9641f78265e264ddb163ab1

--- src/libANGLE/renderer/vulkan/vk_cache_utils.h
+++ src/libANGLE/renderer/vulkan/vk_cache_utils.h
@@ -301,6 +301,7 @@ class PipelineDesc final
     // Depth/stencil states.
     void updateDepthTestEnabled(const gl::DepthStencilState &depthStencilState);
     void updateDepthFunc(const gl::DepthStencilState &depthStencilState);
+    void updateDepthWriteEnabled(const gl::DepthStencilState &depthStencilState);
     void updateStencilTestEnabled(const gl::DepthStencilState &depthStencilState);
     void updateStencilFrontFuncs(GLint ref, const gl::DepthStencilState &depthStencilState);
     void updateStencilBackFuncs(GLint ref, const gl::DepthStencilState &depthStencilState);
@@ -308,6 +309,7 @@ class PipelineDesc final
     void updateStencilBackOps(const gl::DepthStencilState &depthStencilState);
     void updateStencilFrontWriteMask(const gl::DepthStencilState &depthStencilState);
     void updateStencilBackWriteMask(const gl::DepthStencilState &depthStencilState);
+    void updateColorWriteMask(const gl::BlendState &blendState);
 
   private:
     // TODO(jmadill): Handle Geometry/Compute shaders when necessary.
