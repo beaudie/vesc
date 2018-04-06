bb392022d3cefa2f4eadd3a9b2b4c0cc6c158153
Cody Northrop <cnorthrop@google.com>
2018-04-06 09:13:18 -0600

    Use EGL handle provided by Android loader
    Test: test-opengl-gl2_copyTexImage
    
    Change-Id: Id3e14a4628d4758abc657d4ff9d4df318937b3aa

--- src/libANGLE/renderer/gl/egl/android/DisplayAndroid.cpp
+++ src/libANGLE/renderer/gl/egl/android/DisplayAndroid.cpp
@@ -9,6 +9,7 @@
 #include <android/native_window.h>
 
 #include "common/debug.h"
+#include "libANGLE/AttributeMap.h"
 #include "libANGLE/Display.h"
 #include "libANGLE/Surface.h"
 #include "libANGLE/renderer/gl/renderergl_utils.h"
@@ -45,7 +46,7 @@ egl::Error DisplayAndroid::initialize(egl::Display *display)
 {
     FunctionsEGLDL *egl = new FunctionsEGLDL();
     mEGL = egl;
-    ANGLE_TRY(egl->initialize(display->getNativeDisplayId(), GetEGLPath()));
+    ANGLE_TRY(egl->initialize(display->getNativeDisplayId(), GetEGLPath(), display->getAttributeMap()));
 
     gl::Version eglVersion(mEGL->majorVersion, mEGL->minorVersion);
     ASSERT(eglVersion >= gl::Version(1, 4));
