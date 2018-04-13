b45dfefe38d3c4dfa2bb9b72da690c701a74aff0
Olli Etuaho <oetuaho@nvidia.com>
2018-04-13 14:11:46 +0300

    Fix Vulkan renderer string for NVIDIA
    The Vulkan physical device properties "deviceName" field does not
    include "NVIDIA" for all NVIDIA GPUs. Add NVIDIA to the renderer name
    in case the PCI vendor ID matches, so that Vulkan backend test
    failures can be suppressed on NVIDIA using IsNVIDIA().
    
    BUG=angleproject:2487
    
    Change-Id: I8e440499664e5ba19773f72104d11d076dae727d

--- src/libANGLE/renderer/vulkan/RendererVk.cpp
+++ src/libANGLE/renderer/vulkan/RendererVk.cpp
@@ -579,22 +579,15 @@ vk::ErrorOrResult<uint32_t> RendererVk::selectPresentQueueForSurface(VkSurfaceKH
 
 std::string RendererVk::getVendorString() const
 {
-    switch (mPhysicalDeviceProperties.vendorID)
+    const char *vendorString = GetVendorString(mPhysicalDeviceProperties.vendorID);
+    if (vendorString)
     {
-        case VENDOR_ID_AMD:
-            return "Advanced Micro Devices";
-        case VENDOR_ID_NVIDIA:
-            return "NVIDIA";
-        case VENDOR_ID_INTEL:
-            return "Intel";
-        default:
-        {
-            // TODO(jmadill): More vendor IDs.
-            std::stringstream strstr;
-            strstr << "Vendor ID: " << mPhysicalDeviceProperties.vendorID;
-            return strstr.str();
-        }
+        return vendorString;
     }
+    // TODO(jmadill): More vendor IDs.
+    std::stringstream strstr;
+    strstr << "Vendor ID: " << mPhysicalDeviceProperties.vendorID;
+    return strstr.str();
 }
 
 std::string RendererVk::getRendererDescription() const
@@ -608,7 +601,19 @@ std::string RendererVk::getRendererDescription() const
     strstr << VK_VERSION_MINOR(apiVersion) << ".";
     strstr << VK_VERSION_PATCH(apiVersion);
 
-    strstr << "(" << mPhysicalDeviceProperties.deviceName << ")";
+    strstr << "(";
+
+    // In the case of NVIDIA, deviceName does not necessarily contain "NVIDIA". Add "NVIDIA" so that
+    // Vulkan end2end tests can be selectively disabled on NVIDIA. TODO(jmadill): should not be
+    // needed after http://anglebug.com/1874 is fixed and end2end_tests use more sophisticated
+    // driver detection.
+    if (mPhysicalDeviceProperties.vendorID == VENDOR_ID_NVIDIA)
+    {
+        ASSERT(GetVendorString(mPhysicalDeviceProperties.vendorID));
+        strstr << GetVendorString(mPhysicalDeviceProperties.vendorID) << " ";
+    }
+
+    strstr << mPhysicalDeviceProperties.deviceName << ")";
 
     return strstr.str();
 }
