9d4c49a789086839e4f1430ab5246e946dd5655e
Jamie Madill <jmadill@chromium.org>
2018-04-19 16:55:00 -0400

    Fix WebGL compat feedback loop null deref.
    This regressed in "Optimize ValidateDrawAttribs: Part 2."
    
    Bug: chromium:834943
    Bug: angleproject:1391
    Change-Id: I217719d76b0524ed7900e18bcc4ca1280ec7b6ff

--- src/tests/gl_tests/WebGLCompatibilityTest.cpp
+++ src/tests/gl_tests/WebGLCompatibilityTest.cpp
@@ -4135,6 +4135,21 @@ TEST_P(WebGL2CompatibilityTest, BindAttribLocationLimitation)
     EXPECT_GL_ERROR(GL_INVALID_VALUE);
 }
 
+// Covers a bug in transform feedback loop detection.
+TEST_P(WebGL2CompatibilityTest, TransformFeedbackCheckNullDeref)
+{
+    constexpr char kVS[] = R"(attribute vec4 color; void main() { color.r; })";
+    constexpr char kFS[] = R"(void main(){})";
+    ANGLE_GL_PROGRAM(program, kVS, kFS);
+    glUseProgram(program);
+
+    GLBuffer buffer;
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glEnableVertexAttribArray(0);
+    glDrawArrays(GL_POINTS, 0, 1);
+    EXPECT_GL_NO_ERROR();
+}
+
 // Use this to select which configurations (e.g. which renderer, which GLES major version) these
 // tests should be run against.
 ANGLE_INSTANTIATE_TEST(WebGLCompatibilityTest,
