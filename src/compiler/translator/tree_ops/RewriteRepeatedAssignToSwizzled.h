5c519b2b494063c7e89b5c4e62b5d81fd30a30c3
Olli Etuaho <oetuaho@nvidia.com>
2018-05-02 17:57:39 +0300

    Rewrite repeated assignments to swizzled vectors on NVIDIA
    This works around a bug that reproduces on some NVIDIA OpenGL drivers
    prior to version 397.31.
    
    BUG=chromium:798117
    TEST=angle_end2end_tests
    
    Change-Id: Iafc6a9a64e56fa98b42117149fe6867040e932e5

---
+++ src/compiler/translator/tree_ops/RewriteRepeatedAssignToSwizzled.h
@@ -0,0 +1,25 @@
+//
+// Copyright (c) 2018 The ANGLE Project Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+// RewriteRepeatedAssignToSwizzled.h: Rewrite expressions that assign an
+// assignment to a swizzled vector, like:
+//     v.x = z = expression;
+// to:
+//     z = expression;
+//     v.x = z;
+
+#ifndef COMPILER_TRANSLATOR_TREEOPS_REWRITEREPEATEDASSIGNTOSWIZZLED_H_
+#define COMPILER_TRANSLATOR_TREEOPS_REWRITEREPEATEDASSIGNTOSWIZZLED_H_
+
+namespace sh
+{
+
+class TIntermBlock;
+
+void RewriteRepeatedAssignToSwizzled(TIntermBlock *root);
+
+}  // namespace sh
+
+#endif  // COMPILER_TRANSLATOR_TREEOPS_REWRITEREPEATEDASSIGNTOSWIZZLED_H_
