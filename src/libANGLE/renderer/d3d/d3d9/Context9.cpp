ee0871fd0d21b231b06233d2bcb292d34e41c663
Tobin Ehlis <tobine@google.com>
2018-05-03 11:10:44 -0600

    Debug: Add Systrace Markers
    Update ScopedPerfEventHelper class to add systrace markers by default.
    This change unifies ANGLE EVENT* macro system so that at the base level
    in LoggingAnnotator class, systrace markers will be added by default.
    
    Modify the base DebugLogger to use char* by default and move any
    conversions to wchar_t to the Windows specializations where wchar is
    used. This limits type conversions to only where they're needed.
    
    This change also includes some new TRACE_EVENT() calls in the VK
    backend which will result in systrace markers for those calls on the
    Android platform.
    
    The new build flag "angle_enable_trace" is added to enable the tracing
    calls.
    
    Bug: angleproject:2528
    Change-Id: Icefc197d4407e1cd31338710e37865abae6a0b15

--- src/libANGLE/renderer/d3d/d3d9/Context9.cpp
+++ src/libANGLE/renderer/d3d/d3d9/Context9.cpp
@@ -28,7 +28,7 @@ namespace rx
 {
 
 Context9::Context9(const gl::ContextState &state, Renderer9 *renderer)
-    : ContextD3D(state), mRenderer(renderer)
+    : ContextD3D(state), mRenderer(renderer), mMarker(nullptr)
 {
 }
 
@@ -227,25 +227,20 @@ std::string Context9::getRendererDescription() const
 
 void Context9::insertEventMarker(GLsizei length, const char *marker)
 {
-    auto optionalString = angle::WidenString(static_cast<size_t>(length), marker);
-    if (optionalString.valid())
-    {
-        mRenderer->getAnnotator()->setMarker(optionalString.value().data());
-    }
+    mRenderer->getAnnotator()->setMarker(marker);
 }
 
 void Context9::pushGroupMarker(GLsizei length, const char *marker)
 {
-    auto optionalString = angle::WidenString(static_cast<size_t>(length), marker);
-    if (optionalString.valid())
-    {
-        mRenderer->getAnnotator()->beginEvent(optionalString.value().data());
-    }
+    mMarker = marker;
+    mRenderer->getAnnotator()->beginEvent(mMarker, mMarker);
 }
 
 void Context9::popGroupMarker()
 {
-    mRenderer->getAnnotator()->endEvent();
+    ASSERT(mMarker);
+    mRenderer->getAnnotator()->endEvent(mMarker);
+    mMarker = nullptr;
 }
 
 void Context9::pushDebugGroup(GLenum source, GLuint id, GLsizei length, const char *message)
