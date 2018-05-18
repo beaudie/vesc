df7751aa6d92055444e8ec63c487f956924bb32a
Qin Jiajia <jiajia.qin@intel.com>
2018-05-18 13:29:09 +0800

    ES31: Support shader storage block in D3D11 compiler - Part1
    This patch is the first step to implement a basic skeleton to translate
    shader storage block to HLSL RWByteAddressBuffer.
    
    In GLSL each shader storage block is just one structured block and in API side
    it corresponds to a buffer range where stores the whole structure.
    RWStructuredBuffer is an array-like object and can have many structured
    elements. The structured element doesn't support unsized array and also have
    a small limitation on the element size. So we choose RWByteAddressBuffer as
    the counterpart of shader storage block in HLSL.
    
    Due to RWByteAddressBuffer does not support using an index to reference a
    specific location, we must use Load and Store to process the read/write
    operation of a buffer variable. Moreover, in the compiler tree, since we
    can't use variable name to get the resource value in RWByteAddressBuffer,
    we have to calculate the offset of buffer variable in a shader storage block,
    then call the corresponding wrapper function to get the right value.
    
    In this patch, we only process below situations:
        assign_to_ssbo := ssbo_access_chain = expr_no_ssbo;
        assign_from_ssbo := lvalue_no_ssbo = ssbo_access_chain;
    
    The translation is like below:
        // GLSL
        #version 310 es
        layout(local_size_x=8) in;
        layout(std140, binding = 0) buffer blockA {
            float f[8];
        } instanceA;
        layout(std140, binding = 1) buffer blockB {
            float f[8];
        };
        void main()
        {
            float data = instanceA.f[gl_LocalInvocationIndex];
            f[gl_LocalInvocationIndex] = data;
        }
    
        // HLSL
        RWByteAddressBuffer _instanceA: register(u0);
        RWByteAddressBuffer _blockB: register(u1);
        float float_Load(RWByteAddressBuffer buffer, uint loc)
        {
            float result = asfloat(buffer.Load(loc));
            return result;
        }
    
        void float_Store(RWByteAddressBuffer buffer, uint loc, float value)
        {
            buffer.Store(loc, asuint(value));
        }
    
        void gl_main()
        {
        float _data = float_Load(_instanceA, 0 + 16 * gl_LocalInvocationIndex);
        float_Store(_blockB, 0 + 16 * gl_LocalInvocationIndex, _data);
        }
    
    We will do below things in the following patches:
    1. Modify the intermediate tree to flatten all ssbo usages to:
        assign_to_ssbo := ssbo_access_chain = expr_no_ssbo;
        assign_from_ssbo := lvalue_no_ssbo = ssbo_access_chain;
    e.g.
        intanceA.a +=1;
      ->tmp = intanceA.a;
        intanceA.a = tmp + 1;
    
        while(++instanceA.a < 16) {
        }
      ->
        int PreIncrement(out int a)
        {
            a += 1;
            return a;
        }
        tmp = instanceA.a;
        while(PreIncrement(tmp) < 16) {
            instanceA.a = tmp
        }
    
    2. Add offset calculation for structure and array of arrays.
    TODOs have been marked in the corresponding places in this patch.
    
    3. Improve helper functions so that they can process all possible types.
    TODOs have been marked in the corresponding places in this patch.
    
    4. Process the swizzle situation.
    TODOs have been marked in the corresponding places in this patch.
    A possible method is to extend current helper functions like below:
        *_Load(RWByteAddressBuffer buffer, uint loc, bool isSwizzle, uint4 swizzleOffset)
    
    Bug: angleproject:1951
    Test: angle_end2end_tests
    
    Change-Id: I68ae68d5bb77d0d5627c8272627a7f689b8dc38b

---
+++ src/compiler/translator/ShaderStorageBlockFunctionHLSL.h
@@ -0,0 +1,53 @@
+//
+// Copyright 2018 The ANGLE Project Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+// ShaderStorageBlockFunctionHLSL: Wrapper functions for RWByteAddressBuffer Load/Store functions.
+//
+
+#ifndef COMPILER_TRANSLATOR_SHADERSTORAGEBLOCKFUNCTIONHLSL_H_
+#define COMPILER_TRANSLATOR_SHADERSTORAGEBLOCKFUNCTIONHLSL_H_
+
+#include <set>
+
+#include "compiler/translator/InfoSink.h"
+#include "compiler/translator/Types.h"
+
+namespace sh
+{
+
+enum class SSBOMethod
+{
+    LOAD,
+    STORE
+};
+
+class ShaderStorageBlockFunctionHLSL final : angle::NonCopyable
+{
+  public:
+    TString registerShaderStorageBlockFunction(const TType &type, SSBOMethod method);
+
+    void shaderStorageBlockFunctionHeader(TInfoSinkBase &out);
+
+  private:
+    struct ShaderStorageBlockFunction
+    {
+        bool operator<(const ShaderStorageBlockFunction &rhs) const;
+        TString functionName;
+        TString typeString;
+        SSBOMethod method;
+        TType type;
+    };
+
+    static void OutputSSBOLoadFunctionBody(TInfoSinkBase &out,
+                                           const ShaderStorageBlockFunction &imageFunction);
+    static void OutputSSBOStoreFunctionBody(TInfoSinkBase &out,
+                                            const ShaderStorageBlockFunction &imageFunction);
+    using ShaderStorageBlockFunctionSet = std::set<ShaderStorageBlockFunction>;
+    ShaderStorageBlockFunctionSet mRegisteredShaderStorageBlockFunctions;
+};
+
+}  // namespace sh
+
+#endif  // COMPILER_TRANSLATOR_SHADERSTORAGEBLOCKFUNCTIONHLSL_H_
