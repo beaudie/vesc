5a8eb5438c47c75ad545cde0c1755c3fbf06b3ad
Corentin Wallez <cwallez@chromium.org>
2018-05-25 14:52:46 -0400

    angle_end2end_tests run all Vulkan tests in a block.
    This modifies the main function of angle_end2end_tests to run all Vulkan
    tests and then all other tests buy modifying the gtest_filter command
    line programmatically.
    
    BUG=angleproject:2537
    
    Change-Id: I0a8ead1715ff9c51c1092b03d2b719d6d0e3e07e

--- src/tests/angle_end2end_tests_main.cpp
+++ src/tests/angle_end2end_tests_main.cpp
@@ -11,6 +11,23 @@ int main(int argc, char** argv)
 {
     testing::InitGoogleTest(&argc, argv);
     testing::AddGlobalTestEnvironment(new ANGLETestEnvironment());
-    int rt = RUN_ALL_TESTS();
-    return rt;
+
+    // If the user specified a filter, run exactly their filter.
+    if (::testing::GTEST_FLAG(filter) != "*")
+    {
+        return RUN_ALL_TESTS();
+    }
+
+    // Run all Vulkan tests first so they can reuse the same window
+    ::testing::GTEST_FLAG(filter) = "*/*VULKAN";
+    int rt1 = RUN_ALL_TESTS();
+
+    // Test window re-creation will happen here.
+
+    // Run all other tests as they can share the same window
+    ::testing::GTEST_FLAG(filter) = "-*/*VULKAN";
+    int rt2 = RUN_ALL_TESTS();
+
+    // rt is 0 on success and 1 on failure
+    return std::max(rt1, rt2);
 }
