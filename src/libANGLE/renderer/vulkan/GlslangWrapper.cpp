0a55dbd4b5b6edec0cb8ffa210250048cf70a2f9
Luc Ferron <lucferron@chromium.org>
2018-06-04 14:28:58 -0400

    Vulkan: Keep unused uniforms list to fix glslang issues
    We we're unable to cleanup the unused uniforms if we did not keep
    a list of them while parsing them in ProgramLinkResource. Now
    that we keep a history of them, we're able to clean them up and
    fix a few dEQP tests.
    
    Bug: angleproject:2582
    Bug: angleproject:2590
    
    Change-Id: Ic1f9151e356a3d05e83f1031cc7b187b370284e5

--- src/libANGLE/renderer/vulkan/GlslangWrapper.cpp
+++ src/libANGLE/renderer/vulkan/GlslangWrapper.cpp
@@ -41,7 +41,7 @@ void InsertLayoutSpecifierString(std::string *shaderString,
     searchStringBuilder << kLayoutMarkerBegin << variableName << kMarkerEnd;
     std::string searchString = searchStringBuilder.str();
 
-    if (layoutString != "")
+    if (!layoutString.empty())
     {
         angle::ReplaceSubstring(shaderString, searchString, "layout(" + layoutString + ")");
     }
@@ -190,10 +190,8 @@ gl::LinkResult GlslangWrapper::linkProgram(const gl::Context *glContext,
     // Assign textures to a descriptor set and binding.
     int textureCount     = 0;
     const auto &uniforms = programState.getUniforms();
-    for (unsigned int uniformIndex : programState.getSamplerUniformRange())
+    for (const gl::LinkedUniform &samplerUniform : uniforms)
     {
-        const gl::LinkedUniform &samplerUniform = uniforms[uniformIndex];
-
         std::string setBindingString = "set = 1, binding = " + Str(textureCount);
 
         ASSERT(samplerUniform.isActive(gl::ShaderType::Vertex) ||
@@ -219,6 +217,16 @@ gl::LinkResult GlslangWrapper::linkProgram(const gl::Context *glContext,
         }
 
         textureCount += samplerUniform.getBasicTypeElementCount();
+    }
+
+    for (const gl::LinkedUniform &linkedUniform : resources.unusedUniforms)
+    {
+        InsertLayoutSpecifierString(&vertexSource, linkedUniform.name, "");
+        InsertLayoutSpecifierString(&fragmentSource, linkedUniform.name, "");
+
+        std::string qualifierToUse = linkedUniform.isSampler() ? kUniformQualifier : "";
+        InsertQualifierSpecifierString(&vertexSource, linkedUniform.name, qualifierToUse);
+        InsertQualifierSpecifierString(&fragmentSource, linkedUniform.name, qualifierToUse);
     }
 
     std::array<const char *, 2> strings = {{vertexSource.c_str(), fragmentSource.c_str()}};
