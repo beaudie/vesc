109617fcff4a66d71211e64b1ba64a4069f71071
Jamie Madill <jmadill@chromium.org>
2018-07-11 12:00:34 -0400

    Vulkan: Store ProgramVk shaders in ShaderInfo.
    This encapsulates most of the logic for a Program into a helper class.
    Now we can store multiple instances of the Program's back-end to
    implement different shader behaviour at draw time.
    
    This will be useful for shader patching for OpenGL line segment raster.
    
    Bug: angleproject:2598
    Change-Id: I800a737088574e28f3a4ec23b91c0cb2647e4e12

--- src/libANGLE/renderer/vulkan/ContextVk.cpp
+++ src/libANGLE/renderer/vulkan/ContextVk.cpp
@@ -151,16 +151,15 @@ gl::Error ContextVk::initPipeline(const gl::DrawCallParams &drawCallParams)
     // Trigger draw call shader patching and fill out the pipeline desc.
     const vk::ShaderAndSerial *vertexShaderAndSerial   = nullptr;
     const vk::ShaderAndSerial *fragmentShaderAndSerial = nullptr;
+    const vk::PipelineLayout *pipelineLayout           = nullptr;
     ANGLE_TRY(programVk->initShaders(this, drawCallParams, &vertexShaderAndSerial,
-                                     &fragmentShaderAndSerial));
+                                     &fragmentShaderAndSerial, &pipelineLayout));
 
     mPipelineDesc->updateShaders(vertexShaderAndSerial->getSerial(),
                                  fragmentShaderAndSerial->getSerial());
 
-    const vk::PipelineLayout &pipelineLayout = programVk->getPipelineLayout();
-
     ANGLE_TRY(mRenderer->getPipeline(*vertexShaderAndSerial, *fragmentShaderAndSerial,
-                                     pipelineLayout, *mPipelineDesc, activeAttribLocationsMask,
+                                     *pipelineLayout, *mPipelineDesc, activeAttribLocationsMask,
                                      &mCurrentPipeline));
 
     return gl::NoError();
@@ -223,30 +222,9 @@ gl::Error ContextVk::setupDraw(const gl::Context *context,
     ASSERT(mCurrentPipeline && mCurrentPipeline->valid());
     mCurrentPipeline->updateSerial(queueSerial);
 
-    // TODO(jmadill): Can probably use more dirty bits here.
-    ANGLE_TRY(programVk->updateUniforms(this));
-    ANGLE_TRY(programVk->updateTexturesDescriptorSet(context));
-
     // Bind the graphics descriptor sets.
-    // TODO(jmadill): Handle multiple command buffers.
-    const auto &descriptorSets   = programVk->getDescriptorSets();
-    const gl::RangeUI &usedRange = programVk->getUsedDescriptorSetRange();
-    const vk::PipelineLayout &pipelineLayout = programVk->getPipelineLayout();
-    if (!usedRange.empty())
-    {
-        ASSERT(!descriptorSets.empty());
-        (*commandBufferOut)
-            ->bindDescriptorSets(VK_PIPELINE_BIND_POINT_GRAPHICS, pipelineLayout, usedRange.low(),
-                                 usedRange.length(), &descriptorSets[usedRange.low()],
-                                 programVk->getDynamicOffsetsCount(),
-                                 programVk->getDynamicOffsets());
-    }
-
-    (*commandBufferOut)
-        ->bindDescriptorSets(VK_PIPELINE_BIND_POINT_GRAPHICS, pipelineLayout,
-                             kDriverUniformsDescriptorSetIndex, 1, &mDriverUniformsDescriptorSet, 0,
-                             nullptr);
-
+    programVk->updateDescriptorSets(this, drawCallParams, mDriverUniformsDescriptorSet,
+                                    *commandBufferOut);
     return gl::NoError();
 }
 
