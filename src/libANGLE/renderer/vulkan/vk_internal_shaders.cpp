e3d6140914427140f68bb8e061ada4776416df22
Jamie Madill <jmadill@chromium.org>
2018-07-12 15:56:54 -0400

    Vulkan: Add a create shader helper.
    This will be useful for line segment rasterization.
    
    Bug: angleproject:2598
    Change-Id: I84912c976665ecb32903181fa820b8db88786a28

--- src/libANGLE/renderer/vulkan/vk_internal_shaders.cpp
+++ src/libANGLE/renderer/vulkan/vk_internal_shaders.cpp
@@ -43,18 +43,7 @@ angle::Result ShaderLibrary::getShader(vk::Context *context,
     }
 
     const priv::ShaderBlob &shaderCode = priv::GetInternalShaderBlob(shaderID);
-
-    // Create shader lazily. Access will need to be locked for multi-threading.
-    VkShaderModuleCreateInfo createInfo;
-    createInfo.sType    = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
-    createInfo.pNext    = nullptr;
-    createInfo.flags    = 0;
-    createInfo.codeSize = shaderCode.codeSize;
-    createInfo.pCode    = shaderCode.code;
-
-    ANGLE_TRY(shader.get().init(context, createInfo));
-    shader.updateSerial(context->getRenderer()->issueShaderSerial());
-    return angle::Result::Continue();
+    return InitShaderAndSerial(context, &shader, shaderCode.code, shaderCode.codeSize);
 }
 }  // namespace vk
 }  // namespace rx
