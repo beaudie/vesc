202c76f01c9397f570731aee1313e567c4a1f84d
Jamie Madill <jmadill@chromium.org>
2018-07-26 10:51:54 -0400

    D3D: Add streaming buffer initialize.
    This gives us better control over the error returned by the init
    routine. Also changes the streaming buffer in VertexDataManager to not
    be a unique_ptr.
    
    Bug: angleproject:2738
    Change-Id: I3193840dfb71c7574adbe65bc5f9227f4add1fd3

--- src/libANGLE/renderer/d3d/VertexDataManager.h
+++ src/libANGLE/renderer/d3d/VertexDataManager.h
@@ -132,17 +132,17 @@ class VertexDataManager : angle::NonCopyable
     gl::Error reserveSpaceForAttrib(const TranslatedAttribute &translatedAttrib,
                                     GLint start,
                                     size_t count,
-                                    GLsizei instances) const;
+                                    GLsizei instances);
 
     gl::Error storeDynamicAttrib(const gl::Context *context,
                                  TranslatedAttribute *translated,
                                  GLint start,
                                  size_t count,
-                                 GLsizei instances) const;
+                                 GLsizei instances);
 
     BufferFactoryD3D *const mFactory;
 
-    std::unique_ptr<StreamingVertexBufferInterface> mStreamingBuffer;
+    StreamingVertexBufferInterface mStreamingBuffer;
     std::vector<CurrentValueState> mCurrentValueCache;
     gl::AttributesMask mDynamicAttribsMaskCache;
 };
