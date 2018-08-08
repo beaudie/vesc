ce5704e030fcc8ecece49da9eb337d0d131243eb
jchen10 <jie.a.chen@intel.com>
2018-08-08 15:44:09 +0800

    Enable some tests on Intel Windows Vulkan
    The tests IndexBufferOffsetTest.UInt16Index and StateChangeTest.
    VertexBufferUpdatedAfterDraw can pass with the latest driver
    release 24.20.100.6194.
    
    Bug: angleproject:2663
    Bug: angleproject:2664
    
    Change-Id: I0324767f5c538879d865cf12304da4625d49bd81

--- src/tests/gl_tests/StateChangeTest.cpp
+++ src/tests/gl_tests/StateChangeTest.cpp
@@ -655,11 +655,6 @@ TEST_P(StateChangeRenderTest, GenerateMipmap)
 // Tests that D3D11 dirty bit updates don't forget about BufferSubData attrib updates.
 TEST_P(StateChangeTest, VertexBufferUpdatedAfterDraw)
 {
-    // TODO(jie.a.chen@intel.com): Re-enable the test once the driver fix is
-    // available in public release.
-    // http://anglebug.com/2664.
-    ANGLE_SKIP_TEST_IF(IsVulkan() && IsIntel());
-
     const std::string vs =
         "attribute vec2 position;\n"
         "attribute vec4 color;\n"
