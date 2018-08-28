d1c0db173173f5686cd947505548c325180e834e
Jamie Madill <jmadill@chromium.org>
2018-08-28 16:58:49 -0400

    Vulkan: Optimize ContextVk::updateDefaultAttributes.
    This function was getting called every time because the dirty bit mask
    included dirty bits for unused program attributes. Fix this by making
    sure we never have dirty bits set for inactive attributes.
    
    Bug: angleproject:2786
    Change-Id: Icf72dadd8a853d3a448abb303119b7b71f6c8193

--- src/libANGLE/Context.cpp
+++ src/libANGLE/Context.cpp
@@ -7743,6 +7743,7 @@ void StateCache::updateActiveAttribsMask(Context *context)
     {
         mCachedActiveBufferedAttribsMask = AttributesMask();
         mCachedActiveClientAttribsMask   = AttributesMask();
+        mCachedActiveDefaultAttribsMask  = AttributesMask();
         return;
     }
 
@@ -7759,6 +7760,7 @@ void StateCache::updateActiveAttribsMask(Context *context)
 
     mCachedActiveClientAttribsMask   = activeAttribs & clientAttribs;
     mCachedActiveBufferedAttribsMask = activeAttribs & ~clientAttribs;
+    mCachedActiveDefaultAttribsMask  = activeAttribs & ~enabledAttribs;
     mCachedHasAnyEnabledClientAttrib = (clientAttribs & enabledAttribs).any();
 }
 
