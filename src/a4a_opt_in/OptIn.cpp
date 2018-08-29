107a3f875a3289d8e1d4c3f014f8eb2424c795b2
Ian Elliott <ianelliott@google.com>
2018-08-29 17:04:15 -0600

    Add stubs for A4A opt-in library.
    Change-Id: I9ba8abf5fdac4a1bae24bc78ece62337d24e903b

---
+++ src/a4a_opt_in/OptIn.cpp
@@ -0,0 +1,28 @@
+//
+// Copyright (c) 2018 The ANGLE Project Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+
+// OptIn.cpp: Implementation of the code that helps the Android EGL loader
+// determine whether to use ANGLE or a native GLES driver.
+
+#include "OptIn.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+ANGLE_EXPORT bool ANGLEUseForApplication(const char *app_name,
+                                         // TODO: Add app_version and app_intent
+                                         const char *device_mfr,
+                                         const char *device_model,
+                                         ANGLEPreference dev_opt,
+                                         ANGLEPreference app_pref)
+{
+    return false;
+}
+
+#ifdef __cplusplus
+}  // extern "C"
+#endif
