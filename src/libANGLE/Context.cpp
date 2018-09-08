871841bb2b7d370e8ea022e67a0f77c116434055
Frank Henigman <fjhenigman@chromium.org>
2018-09-07 21:11:28 -0400

    test - ignore
    Change-Id: I9a57341f3bbced2dad7606cec68bdc4131c42be3

--- src/libANGLE/Context.cpp
+++ src/libANGLE/Context.cpp
@@ -2,7 +2,7 @@
 // Copyright (c) 2002-2014 The ANGLE Project Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-//
+//x
 
 // Context.cpp: Implements the gl::Context class, managing all GL state and performing
 // rendering operations. It is the GLES2 specific implementation of EGLContext.
