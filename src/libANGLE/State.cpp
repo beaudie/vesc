016fd575bfcf5885c0f5f6d5900982c812da44dd
Jamie Madill <jmadill@chromium.org>
2018-09-17 08:32:46 -0400

    Skip Texture::syncState when no dirty bits.
    We sometimes generate local dirty bits in TextureGL. To make sure the local
    dirty bits don't get skipped we use a Subject/Observer pattern between the
    TextureGL and gl::Texture. This allows us to skip syncState in the hot path.
    
    Also inlines a couple of other texture functions.
    
    Bug: angleproject:2763
    Change-Id: Ie1d8a5865deaf2a563a358c31ae28bef6b2458b1

--- src/libANGLE/State.cpp
+++ src/libANGLE/State.cpp
@@ -25,6 +25,7 @@
 #include "libANGLE/queryconversions.h"
 #include "libANGLE/queryutils.h"
 #include "libANGLE/renderer/ContextImpl.h"
+#include "libANGLE/renderer/TextureImpl.h"
 
 namespace gl
 {
@@ -2743,7 +2744,10 @@ Error State::syncProgramTextures(const Context *context)
         // TODO(jmadill): Use specific dirty bit for completeness change.
         if (texture->isSamplerComplete(context, sampler))
         {
-            ANGLE_TRY(texture->syncState(context));
+            if (texture->hasAnyDirtyBit())
+            {
+                ANGLE_TRY(texture->syncState(context));
+            }
             mActiveTexturesCache[textureUnitIndex] = texture;
         }
         else
@@ -2752,7 +2756,7 @@ Error State::syncProgramTextures(const Context *context)
         }
 
         // Bind the texture unconditionally, to recieve completeness change notifications.
-        mCompleteTextureBindings[textureUnitIndex].bind(texture->getSubject());
+        mCompleteTextureBindings[textureUnitIndex].bind(texture->getImplementation());
         newActiveTextures.set(textureUnitIndex);
 
         if (texture->initState() == InitState::MayNeedInit)
