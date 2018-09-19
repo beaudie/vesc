8a91c0232f2fbb836760aafc3436522489342603
Yizhou Jiang <yizhou.jiang@intel.com>
2018-09-19 15:23:29 +0800

    Enable ANGLE_texture_multisample in glsl in es 3.0
    Enable gsampler2dMS, texelFetch, textureSize in glslang in
    es 3.0 if ANGLE_texture_multisample is supported.
    
    BUG=angleproject:2275
    TEST=SamplerMultisampleEXTTest.TextureMultisampleEXTEnabled
    
    Change-Id: Ibfa367970db3ae790f3822e57eb50090843dc6db

--- src/libANGLE/Caps.h
+++ src/libANGLE/Caps.h
@@ -439,6 +439,9 @@ struct Extensions
     // GL_EXT_blend_func_extended
     bool blendFuncExtended;
     GLuint maxDualSourceDrawBuffers;
+
+    // GL_ANGLE_TEXTURE_MULTISAMPLE
+    bool textureMultisample;
 };
 
 struct ExtensionInfo
