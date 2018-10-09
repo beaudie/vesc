1c0c10fe5edfe18a9e97ddd57385ed8f7eebc9a8
Qin Jiajia <jiajia.qin@intel.com>
2018-10-09 13:59:56 +0800

    ES31: Add vector and matrix support in SSBO for D3D
    Bug: angleproject:1951
    
    Change-Id: I19d4218b28770464e4cb38027cce694e2a817b5b

--- src/compiler/translator/ShaderStorageBlockFunctionHLSL.cpp
+++ src/compiler/translator/ShaderStorageBlockFunctionHLSL.cpp
@@ -13,41 +13,83 @@
 namespace sh
 {
 
+namespace
+{
+
+unsigned int GetVectorStride(TLayoutBlockStorage storage)
+{
+    if (storage == EbsStd140)
+    {
+        // The base alignment and stride of arrays of scalars and vectors are rounded up a multiple
+        // of the base alignment of a vec4
+        return 16;
+    }
+    else
+    {
+        // TODO(jiajia.qin@intel.com): Calculate the stride for other layout storage.
+        // http://anglebug.com/1951
+        return 16;
+    }
+}
+
+}  // anonymous namespace
+
 // static
 void ShaderStorageBlockFunctionHLSL::OutputSSBOLoadFunctionBody(
     TInfoSinkBase &out,
     const ShaderStorageBlockFunction &ssboFunction)
 {
-    if (ssboFunction.type.isScalar())
+    const char *convertString;
+    switch (ssboFunction.type.getBasicType())
     {
-        TString convertString;
-        switch (ssboFunction.type.getBasicType())
-        {
-            case EbtFloat:
-                convertString = "asfloat(";
-                break;
-            case EbtInt:
-                convertString = "asint(";
-                break;
-            case EbtUInt:
-                convertString = "asuint(";
-                break;
-            case EbtBool:
-                convertString = "asint(";
-                break;
-            default:
-                UNREACHABLE();
-                break;
-        }
-
-        out << "    " << ssboFunction.typeString << " result = " << convertString
-            << "buffer.Load(loc));\n";
-        out << "    return result;\n";
-        return;
+        case EbtFloat:
+            convertString = "asfloat(";
+            break;
+        case EbtInt:
+            convertString = "asint(";
+            break;
+        case EbtUInt:
+            convertString = "asuint(";
+            break;
+        case EbtBool:
+            convertString = "asint(";
+            break;
+        default:
+            UNREACHABLE();
+            return;
     }
 
-    // TODO(jiajia.qin@intel.com): Process all possible return types.
-    out << "    return 1.0;\n";
+    out << "    " << ssboFunction.typeString << " result";
+    if (ssboFunction.type.isScalar())
+    {
+        out << " = " << convertString << "buffer.Load(loc));\n";
+    }
+    else if (ssboFunction.type.isVector())
+    {
+        out << " = " << convertString << "buffer.Load" << ssboFunction.type.getNominalSize()
+            << "(loc));\n";
+    }
+    else if (ssboFunction.type.isMatrix())
+    {
+        out << " = {";
+        unsigned int vectorStride =
+            GetVectorStride(ssboFunction.type.getLayoutQualifier().blockStorage);
+        for (int rowIndex = 0; rowIndex < ssboFunction.type.getRows(); rowIndex++)
+        {
+            out << "asfloat(buffer.Load" << ssboFunction.type.getCols() << "(loc +"
+                << rowIndex * vectorStride << ")), ";
+        }
+
+        out << "};\n";
+    }
+    else
+    {
+        // TODO(jiajia.qin@intel.com): Process all possible return types. http://anglebug.com/1951
+        out << ";\n";
+    }
+
+    out << "    return result;\n";
+    return;
 }
 
 // static
@@ -59,8 +101,25 @@ void ShaderStorageBlockFunctionHLSL::OutputSSBOStoreFunctionBody(
     {
         out << "    buffer.Store(loc, asuint(value));\n";
     }
-
-    // TODO(jiajia.qin@intel.com): Process all possible return types.
+    else if (ssboFunction.type.isVector())
+    {
+        out << "    buffer.Store" << ssboFunction.type.getNominalSize()
+            << "(loc, asuint(value));\n";
+    }
+    else if (ssboFunction.type.isMatrix())
+    {
+        unsigned int vectorStride =
+            GetVectorStride(ssboFunction.type.getLayoutQualifier().blockStorage);
+        for (int rowIndex = 0; rowIndex < ssboFunction.type.getRows(); rowIndex++)
+        {
+            out << "    buffer.Store" << ssboFunction.type.getCols() << "(loc +"
+                << rowIndex * vectorStride << ", asuint(value[" << rowIndex << "]));\n";
+        }
+    }
+    else
+    {
+        // TODO(jiajia.qin@intel.com): Process all possible return types. http://anglebug.com/1951
+    }
 }
 
 bool ShaderStorageBlockFunctionHLSL::ShaderStorageBlockFunction::operator<(
