8f7ec49369d2d8df259cd835c626c4f2b3e11a3d
Shahbaz Youssefi <syoussefi@chromium.org>
2018-10-11 10:43:47 -0400

    TEST: Vulkan: Implement GL_EXT_disjoint_timer_query
    - QueryVk::queryCounter() and relevant utils are implemented for the
      sake of Timestamp queries.
    - TimeElapsed queries are implemented using two Timestamp queries
    - A bug in glGetQueryObject* is fixed, where the function is expected to
      flush enough commands to ensure forward progress.
    
    Bug: 2885
    Change-Id: Id13882f3ad8dbc58542b6cc1134be0b62cebfb82

--- src/libANGLE/renderer/vulkan/vk_helpers.h
+++ src/libANGLE/renderer/vulkan/vk_helpers.h
@@ -176,8 +176,9 @@ class DynamicallyGrowingPool
 // DynamicQueryPool allocates indices out of QueryPool as needed.  Once a QueryPool is exhausted,
 // another is created.  The query pools live permanently, but are recycled as indices get freed.
 
-// This is an arbitrary default size for occlusion query pools.
+// These are arbitrary default sizes for query pools.
 constexpr uint32_t kDefaultOcclusionQueryPoolSize = 64;
+constexpr uint32_t kDefaultTimestampQueryPoolSize = 1;
 
 class QueryHelper;
 
