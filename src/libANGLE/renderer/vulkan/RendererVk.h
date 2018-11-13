9b8badc6b8d5f701b09f073ee2aec57555734bb1
Frank Henigman <fjhenigman@chromium.org>
2018-11-13 17:22:36 -0500

    Vulkan: support instanced draws.
    Enable instanced draws with the Vulkan backend.
    So far it only works when Vulkan has VK_EXT_vertex_attribute_divisor.
    
    BUG=angleproject:2672
    
    Change-Id: I9445ba64282fa00a6eaee207b15efa2c7a9abbd3

--- src/libANGLE/renderer/vulkan/RendererVk.h
+++ src/libANGLE/renderer/vulkan/RendererVk.h
@@ -255,10 +255,12 @@ class RendererVk : angle::NonCopyable
     VkDebugReportCallbackEXT mDebugReportCallback;
     VkPhysicalDevice mPhysicalDevice;
     VkPhysicalDeviceProperties mPhysicalDeviceProperties;
+    PFN_vkGetPhysicalDeviceProperties2 mGetPhysicalDeviceProperties2;
     VkPhysicalDeviceFeatures mPhysicalDeviceFeatures;
     std::vector<VkQueueFamilyProperties> mQueueFamilyProperties;
     VkQueue mQueue;
     uint32_t mCurrentQueueFamilyIndex;
+    uint32_t mMaxVertexAttribDivisor;
     VkDevice mDevice;
     vk::CommandPool mCommandPool;
     SerialFactory mQueueSerialFactory;
