85664f15f4966f866e2d9581fe1975e09c7390bd
Ian Elliott <ianelliott@google.com>
2018-12-06 14:38:59 -0700

    A4A opt-in/out: add TestRuleProcess test and use a test fixture
    The new TestRuleProcess test tests the full rule-processing paths.
    The test fixture reduces code duplication.
    
    Bug: angleproject:2993
    
    Change-Id: Ia07ea408fb771dfd1a21cc2fd4610e0e88f529ea

--- src/feature_support_util/feature_support_util_unittest.cpp
+++ src/feature_support_util/feature_support_util_unittest.cpp
@@ -13,8 +13,25 @@
 
 using namespace angle;
 
+class FeatureSupportUtilTest : public testing::Test
+{
+  protected:
+    FeatureSupportUtilTest()
+    {
+        mSystemInfo.machineManufacturer = "MfrFoo";
+        mSystemInfo.machineModelName    = "ModelX";
+        mSystemInfo.gpus.resize(1);
+        mSystemInfo.gpus[0].vendorId              = 123;
+        mSystemInfo.gpus[0].deviceId              = 234;
+        mSystemInfo.gpus[0].driverVendor          = "GPUVendorA";
+        mSystemInfo.gpus[0].detailedDriverVersion = {1, 2, 3, 4};
+    }
+
+    SystemInfo mSystemInfo;
+};
+
 // Test the ANGLEGetFeatureSupportUtilAPIVersion function
-TEST(FeatureSupportUtilTest, APIVersion)
+TEST_F(FeatureSupportUtilTest, APIVersion)
 {
     unsigned int versionToUse;
     unsigned int zero = 0;
@@ -37,38 +54,24 @@ TEST(FeatureSupportUtilTest, APIVersion)
 }
 
 // Test the ANGLEAddDeviceInfoToSystemInfo function
-TEST(FeatureSupportUtilTest, SystemInfo)
+TEST_F(FeatureSupportUtilTest, SystemInfo)
 {
-    // TODO(ianelliott): Replace this with a gtest "fixture", per review feedback.
-    SystemInfo systemInfo;
+    SystemInfo systemInfo = mSystemInfo;
     systemInfo.machineManufacturer = "BAD";
     systemInfo.machineModelName    = "BAD";
-    systemInfo.gpus.resize(1);
-    systemInfo.gpus[0].vendorId              = 123;
-    systemInfo.gpus[0].deviceId              = 234;
-    systemInfo.gpus[0].driverVendor          = "DriverVendorA";
-    systemInfo.gpus[0].detailedDriverVersion = {1, 2, 3, 4};
 
-    char mfr[]   = "Google";
-    char model[] = "Pixel1";
+    char mfr[]   = "MfrFoo";
+    char model[] = "ModelX";
 
     ANGLEAddDeviceInfoToSystemInfo(mfr, model, &systemInfo);
-    EXPECT_EQ("Google", systemInfo.machineManufacturer);
-    EXPECT_EQ("Pixel1", systemInfo.machineModelName);
+    EXPECT_EQ("MfrFoo", systemInfo.machineManufacturer);
+    EXPECT_EQ("ModelX", systemInfo.machineModelName);
 }
 
 // Test the ANGLEAndroidParseRulesString function
-TEST(FeatureSupportUtilTest, ParseRules)
+TEST_F(FeatureSupportUtilTest, ParseRules)
 {
-    // TODO(ianelliott): Replace this with a gtest "fixture", per review feedback.
-    SystemInfo systemInfo;
-    systemInfo.machineManufacturer = "Google";
-    systemInfo.machineModelName    = "Pixel1";
-    systemInfo.gpus.resize(1);
-    systemInfo.gpus[0].vendorId              = 123;
-    systemInfo.gpus[0].deviceId              = 234;
-    systemInfo.gpus[0].driverVendor          = "DriverVendorA";
-    systemInfo.gpus[0].detailedDriverVersion = {1, 2, 3, 4};
+    SystemInfo systemInfo = mSystemInfo;
 
     constexpr char rulesFileContents[] =
         "{\"Rules\":[{\"Rule\":\"Default Rule (i.e. use native driver)\", \"AppChoice\":true, "
@@ -77,5 +80,72 @@ TEST(FeatureSupportUtilTest, ParseRules)
     int rulesVersion        = 0;
     EXPECT_TRUE(ANGLEAndroidParseRulesString(rulesFileContents, &rulesHandle, &rulesVersion));
     EXPECT_NE(nullptr, rulesHandle);
+    ANGLEFreeRulesHandle(rulesHandle);
+}
+
+// Test the ANGLEAndroidParseRulesString and ANGLEShouldBeUsedForApplication functions
+TEST_F(FeatureSupportUtilTest, TestRuleProcessing)
+{
+    SystemInfo systemInfo = mSystemInfo;
+
+    constexpr char rulesFileContents[] =
+        "{\n"
+        "    \"Rules\":[\n"
+        "        {\n"
+        "            \"Rule\":\"Default Rule (i.e. do not use ANGLE)\",\n"
+        "            \"UseANGLE\":false\n"
+        "        },\n"
+        "        {\n"
+        "            \"Rule\":\"Supported application(s)\",\n"
+        "            \"UseANGLE\":true,\n"
+        "            \"Applications\":[\n"
+        "                {\n"
+        "                    \"AppName\":\"com.isvA.app1\"\n"
+        "                }\n"
+        "            ]\n"
+        "        },\n"
+        "        {\n"
+        "            \"Rule\":\"Exceptions for bad drivers(s)\",\n"
+        "            \"UseANGLE\":false,\n"
+        "            \"Applications\":[\n"
+        "                {\n"
+        "                    \"AppName\":\"com.isvA.app1\"\n"
+        "                }\n"
+        "            ],\n"
+        "            \"Devices\":[\n"
+        "                {\n"
+        "                    \"Manufacturer\":\"MfrFoo\",\n"
+        "                    \"Model\":\"ModelX\",\n"
+        "                    \"GPUs\":[\n"
+        "                        {\n"
+        "                            \"Vendor\" : \"GPUVendorA\",\n"
+        "                            \"DeviceId\" : 234,\n"
+        "                            \"VerMajor\" : 1, \"VerMinor\" : 2,\n"
+        "                            \"VerSubMinor\" : 3, \"VerPatch\" : 4}\n"
+        "                        }\n"
+        "                    ]\n"
+        "                }\n"
+        "            ]\n"
+        "        }\n"
+        "    ]\n"
+        "}\n";
+    RulesHandle rulesHandle = nullptr;
+    int rulesVersion        = 0;
+    EXPECT_TRUE(ANGLEAndroidParseRulesString(rulesFileContents, &rulesHandle, &rulesVersion));
+    EXPECT_NE(nullptr, rulesHandle);
+
+    // Test app1 with a SystemInfo that has an unsupported driver--should fail:
+    char app1[] = "com.isvA.app1";
+    EXPECT_FALSE(ANGLEShouldBeUsedForApplication(rulesHandle, rulesVersion, &systemInfo, app1));
+
+    // Test app1 with a SystemInfo that has a supported driver--should pass:
+    systemInfo.gpus[0].detailedDriverVersion = {1, 2, 3, 5};
+    EXPECT_TRUE(ANGLEShouldBeUsedForApplication(rulesHandle, rulesVersion, &systemInfo, app1));
+
+    // Test unsupported app2--should fail:
+    char app2[] = "com.isvB.app2";
+    EXPECT_FALSE(ANGLEShouldBeUsedForApplication(rulesHandle, rulesVersion, &systemInfo, app2));
+
+    // Free the rules data structures:
     ANGLEFreeRulesHandle(rulesHandle);
 }
