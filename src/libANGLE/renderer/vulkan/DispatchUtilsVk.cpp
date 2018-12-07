b0890537c604d926c6347883c74cde3899dc6b21
Shahbaz Youssefi <syoussefi@chromium.org>
2018-12-07 10:18:37 +0100

    TEST: debug convert vertex on android
    Change-Id: Ia64cb003c28871eced6a8ee1495f8584b8a407f4

--- src/libANGLE/renderer/vulkan/DispatchUtilsVk.cpp
+++ src/libANGLE/renderer/vulkan/DispatchUtilsVk.cpp
@@ -333,7 +333,7 @@ angle::Result DispatchUtilsVk::convertVertexBuffer(vk::Context *context,
     // Tell src we are going to read from it.
     src->onRead(dest, VK_ACCESS_SHADER_READ_BIT);
     // Tell dest it's being written to.
-    dest->onWrite(VK_ACCESS_SHADER_WRITE_BIT);
+    dest->onWrite(VK_ACCESS_MEMORY_WRITE_BIT);//VK_ACCESS_SHADER_WRITE_BIT);
 
     bool srcIsInt   = params.srcFormat->componentType == GL_INT;
     bool srcIsUint  = params.srcFormat->componentType == GL_UNSIGNED_INT;
