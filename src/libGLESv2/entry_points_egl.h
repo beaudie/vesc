d754f06b504f185b18e92a42a02184b4788b01c2
Jamie Madill <jmadill@chromium.org>
2018-12-14 11:00:24 -0500

    Load correct libGLESv2 on Linux and Mac.
    libEGL was implicitly loading libGLESv2 on startup. This is bad
    because on platforms like Linux and Mac we could sometimes use the
    incorrect rpath. This in turn meant we needed workarounds like using
    "_angle" extensions to our shared objects to get the correct loading
    behaviour.
    
    Fix this by loading libGLESv2 dynamically in libEGL. We build the
    loader automatically from egl.xml. The loader itself is lazily
    initialized on every EGL entry point call. This is necessary because
    on Linux, etc, there is no equivalent to Windows' DLLMain.
    
    We also use an EGL.h with different generation options so we have the
    proper function pointer types. A README is included for instructions
    on how to regenerate EGL.h.
    
    The entry point generation script is refactored into a helper class
    that is used in the loader generator. Also adds the libGLESv2 versions
    of the EGL entry points in the DEF file on Windows. This allows them to
    be imported properly in 32-bit configurations.
    
    Also fixes up some errors in ANGLE's entry point definitions.
    
    This CL will help us to run ANGLE tests against native drivers.
    
    Bug: angleproject:2871
    Change-Id: Id6ecf969308f17b1be4083538428c9c1a1836572

--- src/libGLESv2/entry_points_egl.h
+++ src/libGLESv2/entry_points_egl.h
@@ -12,124 +12,125 @@
 #include <EGL/egl.h>
 #include <export.h>
 
-namespace egl
-{
+extern "C" {
 
 // EGL 1.0
-ANGLE_EXPORT EGLBoolean EGLAPIENTRY ChooseConfig(EGLDisplay dpy,
-                                                 const EGLint *attrib_list,
-                                                 EGLConfig *configs,
-                                                 EGLint config_size,
-                                                 EGLint *num_config);
-ANGLE_EXPORT EGLBoolean EGLAPIENTRY CopyBuffers(EGLDisplay dpy,
-                                                EGLSurface surface,
-                                                EGLNativePixmapType target);
-ANGLE_EXPORT EGLContext EGLAPIENTRY CreateContext(EGLDisplay dpy,
-                                                  EGLConfig config,
-                                                  EGLContext share_context,
-                                                  const EGLint *attrib_list);
-ANGLE_EXPORT EGLSurface EGLAPIENTRY CreatePbufferSurface(EGLDisplay dpy,
-                                                         EGLConfig config,
-                                                         const EGLint *attrib_list);
-ANGLE_EXPORT EGLSurface EGLAPIENTRY CreatePixmapSurface(EGLDisplay dpy,
-                                                        EGLConfig config,
-                                                        EGLNativePixmapType pixmap,
-                                                        const EGLint *attrib_list);
-ANGLE_EXPORT EGLSurface EGLAPIENTRY CreateWindowSurface(EGLDisplay dpy,
-                                                        EGLConfig config,
-                                                        EGLNativeWindowType win,
-                                                        const EGLint *attrib_list);
-ANGLE_EXPORT EGLBoolean EGLAPIENTRY DestroyContext(EGLDisplay dpy, EGLContext ctx);
-ANGLE_EXPORT EGLBoolean EGLAPIENTRY DestroySurface(EGLDisplay dpy, EGLSurface surface);
-ANGLE_EXPORT EGLBoolean EGLAPIENTRY GetConfigAttrib(EGLDisplay dpy,
-                                                    EGLConfig config,
+ANGLE_EXPORT EGLBoolean EGLAPIENTRY EGLChooseConfig(EGLDisplay dpy,
+                                                    const EGLint *attrib_list,
+                                                    EGLConfig *configs,
+                                                    EGLint config_size,
+                                                    EGLint *num_config);
+ANGLE_EXPORT EGLBoolean EGLAPIENTRY EGLCopyBuffers(EGLDisplay dpy,
+                                                   EGLSurface surface,
+                                                   EGLNativePixmapType target);
+ANGLE_EXPORT EGLContext EGLAPIENTRY EGLCreateContext(EGLDisplay dpy,
+                                                     EGLConfig config,
+                                                     EGLContext share_context,
+                                                     const EGLint *attrib_list);
+ANGLE_EXPORT EGLSurface EGLAPIENTRY EGLCreatePbufferSurface(EGLDisplay dpy,
+                                                            EGLConfig config,
+                                                            const EGLint *attrib_list);
+ANGLE_EXPORT EGLSurface EGLAPIENTRY EGLCreatePixmapSurface(EGLDisplay dpy,
+                                                           EGLConfig config,
+                                                           EGLNativePixmapType pixmap,
+                                                           const EGLint *attrib_list);
+ANGLE_EXPORT EGLSurface EGLAPIENTRY EGLCreateWindowSurface(EGLDisplay dpy,
+                                                           EGLConfig config,
+                                                           EGLNativeWindowType win,
+                                                           const EGLint *attrib_list);
+ANGLE_EXPORT EGLBoolean EGLAPIENTRY EGLDestroyContext(EGLDisplay dpy, EGLContext ctx);
+ANGLE_EXPORT EGLBoolean EGLAPIENTRY EGLDestroySurface(EGLDisplay dpy, EGLSurface surface);
+ANGLE_EXPORT EGLBoolean EGLAPIENTRY EGLGetConfigAttrib(EGLDisplay dpy,
+                                                       EGLConfig config,
+                                                       EGLint attribute,
+                                                       EGLint *value);
+ANGLE_EXPORT EGLBoolean EGLAPIENTRY EGLGetConfigs(EGLDisplay dpy,
+                                                  EGLConfig *configs,
+                                                  EGLint config_size,
+                                                  EGLint *num_config);
+ANGLE_EXPORT EGLDisplay EGLAPIENTRY EGLGetCurrentDisplay(void);
+ANGLE_EXPORT EGLSurface EGLAPIENTRY EGLGetCurrentSurface(EGLint readdraw);
+ANGLE_EXPORT EGLDisplay EGLAPIENTRY EGLGetDisplay(EGLNativeDisplayType display_id);
+ANGLE_EXPORT EGLint EGLAPIENTRY EGLGetError(void);
+ANGLE_EXPORT __eglMustCastToProperFunctionPointerType EGLAPIENTRY
+EGLGetProcAddress(const char *procname);
+ANGLE_EXPORT EGLBoolean EGLAPIENTRY EGLInitialize(EGLDisplay dpy, EGLint *major, EGLint *minor);
+ANGLE_EXPORT EGLBoolean EGLAPIENTRY EGLMakeCurrent(EGLDisplay dpy,
+                                                   EGLSurface draw,
+                                                   EGLSurface read,
+                                                   EGLContext ctx);
+ANGLE_EXPORT EGLBoolean EGLAPIENTRY EGLQueryContext(EGLDisplay dpy,
+                                                    EGLContext ctx,
                                                     EGLint attribute,
                                                     EGLint *value);
-ANGLE_EXPORT EGLBoolean EGLAPIENTRY GetConfigs(EGLDisplay dpy,
-                                               EGLConfig *configs,
-                                               EGLint config_size,
-                                               EGLint *num_config);
-ANGLE_EXPORT EGLDisplay EGLAPIENTRY GetCurrentDisplay(void);
-ANGLE_EXPORT EGLSurface EGLAPIENTRY GetCurrentSurface(EGLint readdraw);
-ANGLE_EXPORT EGLDisplay EGLAPIENTRY GetDisplay(EGLNativeDisplayType display_id);
-ANGLE_EXPORT EGLint EGLAPIENTRY GetError(void);
-ANGLE_EXPORT __eglMustCastToProperFunctionPointerType EGLAPIENTRY
-GetProcAddress(const char *procname);
-ANGLE_EXPORT EGLBoolean EGLAPIENTRY Initialize(EGLDisplay dpy, EGLint *major, EGLint *minor);
-ANGLE_EXPORT EGLBoolean EGLAPIENTRY MakeCurrent(EGLDisplay dpy,
-                                                EGLSurface draw,
-                                                EGLSurface read,
-                                                EGLContext ctx);
-ANGLE_EXPORT EGLBoolean EGLAPIENTRY QueryContext(EGLDisplay dpy,
-                                                 EGLContext ctx,
-                                                 EGLint attribute,
-                                                 EGLint *value);
-ANGLE_EXPORT const char *EGLAPIENTRY QueryString(EGLDisplay dpy, EGLint name);
-ANGLE_EXPORT EGLBoolean EGLAPIENTRY QuerySurface(EGLDisplay dpy,
-                                                 EGLSurface surface,
-                                                 EGLint attribute,
-                                                 EGLint *value);
-ANGLE_EXPORT EGLBoolean EGLAPIENTRY SwapBuffers(EGLDisplay dpy, EGLSurface surface);
-ANGLE_EXPORT EGLBoolean EGLAPIENTRY Terminate(EGLDisplay dpy);
-ANGLE_EXPORT EGLBoolean EGLAPIENTRY WaitGL(void);
-ANGLE_EXPORT EGLBoolean EGLAPIENTRY WaitNative(EGLint engine);
+ANGLE_EXPORT const char *EGLAPIENTRY EGLQueryString(EGLDisplay dpy, EGLint name);
+ANGLE_EXPORT EGLBoolean EGLAPIENTRY EGLQuerySurface(EGLDisplay dpy,
+                                                    EGLSurface surface,
+                                                    EGLint attribute,
+                                                    EGLint *value);
+ANGLE_EXPORT EGLBoolean EGLAPIENTRY EGLSwapBuffers(EGLDisplay dpy, EGLSurface surface);
+ANGLE_EXPORT EGLBoolean EGLAPIENTRY EGLTerminate(EGLDisplay dpy);
+ANGLE_EXPORT EGLBoolean EGLAPIENTRY EGLWaitGL(void);
+ANGLE_EXPORT EGLBoolean EGLAPIENTRY EGLWaitNative(EGLint engine);
 
 // EGL 1.1
-ANGLE_EXPORT EGLBoolean EGLAPIENTRY BindTexImage(EGLDisplay dpy, EGLSurface surface, EGLint buffer);
-ANGLE_EXPORT EGLBoolean EGLAPIENTRY ReleaseTexImage(EGLDisplay dpy,
+ANGLE_EXPORT EGLBoolean EGLAPIENTRY EGLBindTexImage(EGLDisplay dpy,
                                                     EGLSurface surface,
                                                     EGLint buffer);
-ANGLE_EXPORT EGLBoolean EGLAPIENTRY SurfaceAttrib(EGLDisplay dpy,
-                                                  EGLSurface surface,
-                                                  EGLint attribute,
-                                                  EGLint value);
-ANGLE_EXPORT EGLBoolean EGLAPIENTRY SwapInterval(EGLDisplay dpy, EGLint interval);
+ANGLE_EXPORT EGLBoolean EGLAPIENTRY EGLReleaseTexImage(EGLDisplay dpy,
+                                                       EGLSurface surface,
+                                                       EGLint buffer);
+ANGLE_EXPORT EGLBoolean EGLAPIENTRY EGLSurfaceAttrib(EGLDisplay dpy,
+                                                     EGLSurface surface,
+                                                     EGLint attribute,
+                                                     EGLint value);
+ANGLE_EXPORT EGLBoolean EGLAPIENTRY EGLSwapInterval(EGLDisplay dpy, EGLint interval);
 
 // EGL 1.2
-ANGLE_EXPORT EGLBoolean EGLAPIENTRY BindAPI(EGLenum api);
-ANGLE_EXPORT EGLenum EGLAPIENTRY QueryAPI(void);
-ANGLE_EXPORT EGLSurface EGLAPIENTRY CreatePbufferFromClientBuffer(EGLDisplay dpy,
-                                                                  EGLenum buftype,
-                                                                  EGLClientBuffer buffer,
-                                                                  EGLConfig config,
-                                                                  const EGLint *attrib_list);
-ANGLE_EXPORT EGLBoolean EGLAPIENTRY ReleaseThread(void);
-ANGLE_EXPORT EGLBoolean EGLAPIENTRY WaitClient(void);
+ANGLE_EXPORT EGLBoolean EGLAPIENTRY EGLBindAPI(EGLenum api);
+ANGLE_EXPORT EGLenum EGLAPIENTRY EGLQueryAPI(void);
+ANGLE_EXPORT EGLSurface EGLAPIENTRY EGLCreatePbufferFromClientBuffer(EGLDisplay dpy,
+                                                                     EGLenum buftype,
+                                                                     EGLClientBuffer buffer,
+                                                                     EGLConfig config,
+                                                                     const EGLint *attrib_list);
+ANGLE_EXPORT EGLBoolean EGLAPIENTRY EGLReleaseThread(void);
+ANGLE_EXPORT EGLBoolean EGLAPIENTRY EGLWaitClient(void);
 
 // EGL 1.4
-ANGLE_EXPORT EGLContext EGLAPIENTRY GetCurrentContext(void);
+ANGLE_EXPORT EGLContext EGLAPIENTRY EGLGetCurrentContext(void);
 
 // EGL 1.5
-ANGLE_EXPORT EGLSync EGLAPIENTRY CreateSync(EGLDisplay dpy,
-                                            EGLenum type,
-                                            const EGLAttrib *attrib_list);
-ANGLE_EXPORT EGLBoolean EGLAPIENTRY DestroySync(EGLDisplay dpy, EGLSync sync);
-ANGLE_EXPORT EGLint EGLAPIENTRY ClientWaitSync(EGLDisplay dpy,
-                                               EGLSync sync,
-                                               EGLint flags,
-                                               EGLTime timeout);
-ANGLE_EXPORT EGLBoolean EGLAPIENTRY GetSyncAttrib(EGLDisplay dpy,
+ANGLE_EXPORT EGLSync EGLAPIENTRY EGLCreateSync(EGLDisplay dpy,
+                                               EGLenum type,
+                                               const EGLAttrib *attrib_list);
+ANGLE_EXPORT EGLBoolean EGLAPIENTRY EGLDestroySync(EGLDisplay dpy, EGLSync sync);
+ANGLE_EXPORT EGLint EGLAPIENTRY EGLClientWaitSync(EGLDisplay dpy,
                                                   EGLSync sync,
-                                                  EGLint attribute,
-                                                  EGLAttrib *value);
-ANGLE_EXPORT EGLImage EGLAPIENTRY CreateImage(EGLDisplay dpy,
-                                              EGLContext ctx,
-                                              EGLenum target,
-                                              EGLClientBuffer buffer,
-                                              const EGLAttrib *attrib_list);
-ANGLE_EXPORT EGLBoolean EGLAPIENTRY DestroyImage(EGLDisplay dpy, EGLImage image);
-ANGLE_EXPORT EGLDisplay EGLAPIENTRY GetPlatformDisplay(EGLenum platform,
-                                                       void *native_display,
-                                                       const EGLAttrib *attrib_list);
-ANGLE_EXPORT EGLSurface EGLAPIENTRY CreatePlatformWindowSurface(EGLDisplay dpy,
-                                                                EGLConfig config,
-                                                                void *native_window,
-                                                                const EGLAttrib *attrib_list);
-ANGLE_EXPORT EGLSurface EGLAPIENTRY CreatePlatformPixmapSurface(EGLDisplay dpy,
-                                                                EGLConfig config,
-                                                                void *native_pixmap,
-                                                                const EGLAttrib *attrib_list);
-ANGLE_EXPORT EGLBoolean EGLAPIENTRY WaitSync(EGLDisplay dpy, EGLSync sync, EGLint flags);
-}  // namespace egl
+                                                  EGLint flags,
+                                                  EGLTime timeout);
+ANGLE_EXPORT EGLBoolean EGLAPIENTRY EGLGetSyncAttrib(EGLDisplay dpy,
+                                                     EGLSync sync,
+                                                     EGLint attribute,
+                                                     EGLAttrib *value);
+ANGLE_EXPORT EGLImage EGLAPIENTRY EGLCreateImage(EGLDisplay dpy,
+                                                 EGLContext ctx,
+                                                 EGLenum target,
+                                                 EGLClientBuffer buffer,
+                                                 const EGLAttrib *attrib_list);
+ANGLE_EXPORT EGLBoolean EGLAPIENTRY EGLDestroyImage(EGLDisplay dpy, EGLImage image);
+ANGLE_EXPORT EGLDisplay EGLAPIENTRY EGLGetPlatformDisplay(EGLenum platform,
+                                                          void *native_display,
+                                                          const EGLAttrib *attrib_list);
+ANGLE_EXPORT EGLSurface EGLAPIENTRY EGLCreatePlatformWindowSurface(EGLDisplay dpy,
+                                                                   EGLConfig config,
+                                                                   void *native_window,
+                                                                   const EGLAttrib *attrib_list);
+ANGLE_EXPORT EGLSurface EGLAPIENTRY EGLCreatePlatformPixmapSurface(EGLDisplay dpy,
+                                                                   EGLConfig config,
+                                                                   void *native_pixmap,
+                                                                   const EGLAttrib *attrib_list);
+ANGLE_EXPORT EGLBoolean EGLAPIENTRY EGLWaitSync(EGLDisplay dpy, EGLSync sync, EGLint flags);
+}  // extern "C"
 
 #endif  // LIBGLESV2_ENTRYPOINTSEGL_H_
