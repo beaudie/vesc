68f33800dc990ceecb84d4ead371639e5340f711
Jamie Madill <jmadill@chromium.org>
2018-12-23 10:08:26 -0500

    Vulkan: Faster state transitions.
    Implements a transition table from Pipeline Cache entry to
    state change neighbouring Pipeline Cache entries. We use
    a 64-bit mask to do a quick scan over the pipeline desc.
    This ends up being a lot faster than doing a full hash
    and memcmp over the pipeline description.
    
    Note that there could be future optimizations to this design.
    We might keep a hash map of the pipeline transitions instead
    of a list. Or use a sorted list. This could speed up the search
    when there are many transitions for cache entries. Also we could
    skip the transition table and opt to do a full hash when there
    are more than a configurable number of dirty states. This might
    be a bit faster in some cases. Likely this will be something we
    can add performance tests for in the future.
    
    Documentation is also added in a README file for the Vulkan back
    end. This will be extended over time.
    
    Improves performance significantly on the VBO state change test.
    
    Bug: angleproject:3013
    Change-Id: I793f9e3efd8887acf00ad60e4ac2502a54c95dee

--- src/libANGLE/renderer/vulkan/vk_helpers.h
+++ src/libANGLE/renderer/vulkan/vk_helpers.h
@@ -634,7 +634,8 @@ class ShaderProgramHelper : angle::NonCopyable
                                       const PipelineLayout &pipelineLayout,
                                       const GraphicsPipelineDesc &pipelineDesc,
                                       const gl::AttributesMask &activeAttribLocationsMask,
-                                      PipelineAndSerial **pipelineOut)
+                                      const vk::GraphicsPipelineDesc **descPtrOut,
+                                      PipelineHelper **pipelineOut)
     {
         // Pull in a compatible RenderPass.
         vk::RenderPass *compatibleRenderPass = nullptr;
@@ -644,7 +645,7 @@ class ShaderProgramHelper : angle::NonCopyable
         return mGraphicsPipelines.getPipeline(
             context, pipelineCache, *compatibleRenderPass, pipelineLayout,
             activeAttribLocationsMask, mShaders[gl::ShaderType::Vertex].get().get(),
-            mShaders[gl::ShaderType::Fragment].get().get(), pipelineDesc, pipelineOut);
+            mShaders[gl::ShaderType::Fragment].get().get(), pipelineDesc, descPtrOut, pipelineOut);
     }
 
     angle::Result getComputePipeline(Context *context,
