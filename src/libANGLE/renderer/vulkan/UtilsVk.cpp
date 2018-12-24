d884640822bc39e3c43957d411db397346998597
Jamie Madill <jmadill@chromium.org>
2018-12-23 19:58:01 -0500

    Vulkan: Put viewport and scissor back in pipeline desc.
    Turns out this is much faster than using dynamic state. When we
    support multiple viewports it might be easier to use dynamic
    state since we won't need to make an overly large pipeline
    description. We could support both methods using a flag to
    indicate the viewport and/or scissor regions are invalid.
    Until then we can remove the pipeline and scissor dirty bits.
    
    Improves perf by about 15% in the Vulkan VBO state change test.
    
    Bug: angleproject:3013
    Change-Id: I4572250626a9a0f0ca3451b17e8f0de186416cae

--- src/libANGLE/renderer/vulkan/UtilsVk.cpp
+++ src/libANGLE/renderer/vulkan/UtilsVk.cpp
@@ -610,6 +610,18 @@ angle::Result UtilsVk::clearImage(ContextVk *contextVk,
     pipelineDesc.setColorWriteMask(params.colorMaskFlags, *params.alphaMask);
     pipelineDesc.setRenderPassDesc(*params.renderPassDesc);
 
+    const gl::Rectangle &renderArea = framebuffer->getFramebuffer()->getRenderPassRenderArea();
+    bool invertViewport             = contextVk->isViewportFlipEnabledForDrawFBO();
+
+    VkViewport viewport;
+    gl_vk::GetViewport(renderArea, 0.0f, 1.0f, invertViewport, params.renderAreaHeight, &viewport);
+    pipelineDesc.setViewport(viewport);
+
+    VkRect2D scissor;
+    const gl::State &glState = contextVk->getState();
+    gl_vk::GetScissor(glState, invertViewport, renderArea, &scissor);
+    pipelineDesc.setScissor(scissor);
+
     vk::ShaderLibrary &shaderLibrary                    = renderer->getShaderLibrary();
     vk::RefCounted<vk::ShaderAndSerial> *vertexShader   = nullptr;
     vk::RefCounted<vk::ShaderAndSerial> *fragmentShader = nullptr;
@@ -620,17 +632,6 @@ angle::Result UtilsVk::clearImage(ContextVk *contextVk,
                            &mImageClearProgram, &pipelineDesc, VK_NULL_HANDLE, &shaderParams,
                            sizeof(shaderParams), commandBuffer));
 
-    VkViewport viewport;
-    const gl::Rectangle &renderArea = framebuffer->getFramebuffer()->getRenderPassRenderArea();
-    bool invertViewport             = contextVk->isViewportFlipEnabledForDrawFBO();
-    gl_vk::GetViewport(renderArea, 0.0f, 1.0f, invertViewport, params.renderAreaHeight, &viewport);
-
-    VkRect2D scissor;
-    const gl::State &glState = contextVk->getState();
-    gl_vk::GetScissor(glState, invertViewport, renderArea, &scissor);
-
-    commandBuffer->setViewport(0, 1, &viewport);
-    commandBuffer->setScissor(0, 1, &scissor);
     commandBuffer->draw(6, 1, 0, 0);
 
     return angle::Result::Continue;
@@ -691,6 +692,13 @@ angle::Result UtilsVk::copyImage(vk::Context *context,
     renderArea.width  = params.srcExtents[0];
     renderArea.height = params.srcExtents[1];
 
+    VkViewport viewport;
+    gl_vk::GetViewport(renderArea, 0.0f, 1.0f, false, dest->getExtents().height, &viewport);
+    pipelineDesc.setViewport(viewport);
+
+    VkRect2D scissor = gl_vk::GetRect(renderArea);
+    pipelineDesc.setScissor(scissor);
+
     // Change source layout outside render pass
     vk::CommandBuffer *srcLayoutChange;
     ANGLE_TRY(src->recordCommands(context, &srcLayoutChange));
@@ -738,13 +746,6 @@ angle::Result UtilsVk::copyImage(vk::Context *context,
                            &mImageCopyPrograms[flags], &pipelineDesc, descriptorSet, &shaderParams,
                            sizeof(shaderParams), commandBuffer));
 
-    VkViewport viewport;
-    gl_vk::GetViewport(renderArea, 0.0f, 1.0f, false, dest->getExtents().height, &viewport);
-
-    VkRect2D scissor = gl_vk::GetRect(renderArea);
-
-    commandBuffer->setViewport(0, 1, &viewport);
-    commandBuffer->setScissor(0, 1, &scissor);
     commandBuffer->draw(6, 1, 0, 0);
 
     descriptorPoolBinding.reset();
