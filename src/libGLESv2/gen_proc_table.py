de853c09a43ce742261c2d2ecb8c8a0d8dd60861
Jamie Madill <jmadill@chromium.org>
2018-12-27 18:08:49 -0500

    Rename EGL exports from libGLESv2.
    This names the EGL exports the same as the EGL header. Instead of
    EGL_ChooseConfig, we get eglChooseConfig, etc. We also can remove
    libEGL_static from our build configuration since in a static build
    the EGL exports are already defined in libGLESv2.
    
    Actually removing libEGL_static is blocked on removing it from the
    build in Chromium.
    
    Bug: angleproject:2871
    Change-Id: Ic0f90fc79f2fe804f46029cc1317429abdf61595

--- src/libGLESv2/gen_proc_table.py
+++ src/libGLESv2/gen_proc_table.py
@@ -66,8 +66,6 @@ for description, functions in json_data.iteritems():
             # Special handling for EGL_ANGLE_explicit_context extension
             if support_egl_ANGLE_explicit_context:
                 all_functions[function + "ContextANGLE"] = "gl::" + function[2:] + "ContextANGLE"
-        elif function.startswith("egl"):
-            all_functions[function] = "EGL_" + function[3:]
         else:
             all_functions[function] = function
 
