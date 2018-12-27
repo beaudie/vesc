f32c8e81f65d8fa12281b0ec81978955195540e5
Jamie Madill <jmadill@chromium.org>
2018-12-27 15:37:25 -0500

    Vulkan: Make draw command buffer a dirty bit.
    This frees us from checking the FB every draw. Slightly reduces time
    spent in all draw methods. Only visible on the draw call overhead
    tests.
    
    Bug: angleproject:3014
    Change-Id: I89fd0d9e0822400a5c5a16acb5a9c400a0e71ab5

--- src/libANGLE/renderer/vulkan/ContextVk.cpp
+++ src/libANGLE/renderer/vulkan/ContextVk.cpp
@@ -71,6 +71,7 @@ ContextVk::ContextVk(const gl::State &state, gl::ErrorSet *errorSet, RendererVk 
     : ContextImpl(state, errorSet),
       vk::Context(renderer),
       mCurrentPipeline(nullptr),
+      mCommandBuffer(nullptr),
       mCurrentDrawMode(gl::PrimitiveMode::InvalidEnum),
       mVertexArray(nullptr),
       mDrawFramebuffer(nullptr),
@@ -201,8 +202,7 @@ angle::Result ContextVk::setupDraw(const gl::Context *context,
                                    GLsizei vertexOrIndexCount,
                                    gl::DrawElementsType indexTypeOrNone,
                                    const void *indices,
-                                   DirtyBits dirtyBitMask,
-                                   vk::CommandBuffer **commandBufferOut)
+                                   DirtyBits dirtyBitMask)
 {
     // Set any dirty bits that depend on draw call parameters or other objects.
     if (mode != mCurrentDrawMode)
@@ -220,11 +220,14 @@ angle::Result ContextVk::setupDraw(const gl::Context *context,
         mDirtyBits.set(DIRTY_BIT_VERTEX_BUFFERS);
     }
 
-    if (!mDrawFramebuffer->appendToStartedRenderPass(mRenderer->getCurrentQueueSerial(),
-                                                     commandBufferOut))
+    if (!mCommandBuffer)
     {
-        ANGLE_TRY(mDrawFramebuffer->startNewRenderPass(this, commandBufferOut));
-        mDirtyBits |= mNewCommandBufferDirtyBits;
+        if (!mDrawFramebuffer->appendToStartedRenderPass(mRenderer->getCurrentQueueSerial(),
+                                                         &mCommandBuffer))
+        {
+            ANGLE_TRY(mDrawFramebuffer->startNewRenderPass(this, &mCommandBuffer));
+            mDirtyBits |= mNewCommandBufferDirtyBits;
+        }
     }
 
     if (mProgram->dirtyUniforms())
@@ -241,7 +244,7 @@ angle::Result ContextVk::setupDraw(const gl::Context *context,
     // Flush any relevant dirty bits.
     for (size_t dirtyBit : dirtyBits)
     {
-        ANGLE_TRY((this->*mDirtyBitHandlers[dirtyBit])(context, *commandBufferOut));
+        ANGLE_TRY((this->*mDirtyBitHandlers[dirtyBit])(context, mCommandBuffer));
     }
 
     mDirtyBits &= ~dirtyBitMask;
@@ -253,8 +256,7 @@ angle::Result ContextVk::setupIndexedDraw(const gl::Context *context,
                                           gl::PrimitiveMode mode,
                                           GLsizei indexCount,
                                           gl::DrawElementsType indexType,
-                                          const void *indices,
-                                          vk::CommandBuffer **commandBufferOut)
+                                          const void *indices)
 {
     if (indexType != mCurrentDrawElementsType)
     {
@@ -283,8 +285,7 @@ angle::Result ContextVk::setupIndexedDraw(const gl::Context *context,
         }
     }
 
-    return setupDraw(context, mode, 0, indexCount, indexType, indices, mIndexedDirtyBitsMask,
-                     commandBufferOut);
+    return setupDraw(context, mode, 0, indexCount, indexType, indices, mIndexedDirtyBitsMask);
 }
 
 angle::Result ContextVk::setupLineLoopDraw(const gl::Context *context,
@@ -292,8 +293,7 @@ angle::Result ContextVk::setupLineLoopDraw(const gl::Context *context,
                                            GLint firstVertex,
                                            GLsizei vertexOrIndexCount,
                                            gl::DrawElementsType indexTypeOrInvalid,
-                                           const void *indices,
-                                           vk::CommandBuffer **commandBufferOut)
+                                           const void *indices)
 {
     ANGLE_TRY(mVertexArray->handleLineLoop(this, firstVertex, vertexOrIndexCount,
                                            indexTypeOrInvalid, indices));
@@ -302,7 +302,7 @@ angle::Result ContextVk::setupLineLoopDraw(const gl::Context *context,
                                    ? indexTypeOrInvalid
                                    : gl::DrawElementsType::UnsignedInt;
     return setupDraw(context, mode, firstVertex, vertexOrIndexCount, indexTypeOrInvalid, indices,
-                     mIndexedDirtyBitsMask, commandBufferOut);
+                     mIndexedDirtyBitsMask);
 }
 
 angle::Result ContextVk::handleDirtyDefaultAttribs(const gl::Context *context,
@@ -430,20 +430,19 @@ angle::Result ContextVk::drawArrays(const gl::Context *context,
                                     GLint first,
                                     GLsizei count)
 {
-    vk::CommandBuffer *commandBuffer = nullptr;
-    uint32_t clampedVertexCount      = gl::GetClampedVertexCount<uint32_t>(count);
+    uint32_t clampedVertexCount = gl::GetClampedVertexCount<uint32_t>(count);
 
     if (mode == gl::PrimitiveMode::LineLoop)
     {
         ANGLE_TRY(setupLineLoopDraw(context, mode, first, count, gl::DrawElementsType::InvalidEnum,
-                                    nullptr, &commandBuffer));
-        vk::LineLoopHelper::Draw(clampedVertexCount, commandBuffer);
+                                    nullptr));
+        vk::LineLoopHelper::Draw(clampedVertexCount, mCommandBuffer);
     }
     else
     {
         ANGLE_TRY(setupDraw(context, mode, first, count, gl::DrawElementsType::InvalidEnum, nullptr,
-                            mNonIndexedDirtyBitsMask, &commandBuffer));
-        commandBuffer->draw(clampedVertexCount, 1, first, 0);
+                            mNonIndexedDirtyBitsMask));
+        mCommandBuffer->draw(clampedVertexCount, 1, first, 0);
     }
 
     return angle::Result::Continue;
@@ -465,16 +464,15 @@ angle::Result ContextVk::drawElements(const gl::Context *context,
                                       gl::DrawElementsType type,
                                       const void *indices)
 {
-    vk::CommandBuffer *commandBuffer = nullptr;
     if (mode == gl::PrimitiveMode::LineLoop)
     {
-        ANGLE_TRY(setupLineLoopDraw(context, mode, 0, count, type, indices, &commandBuffer));
-        vk::LineLoopHelper::Draw(count, commandBuffer);
+        ANGLE_TRY(setupLineLoopDraw(context, mode, 0, count, type, indices));
+        vk::LineLoopHelper::Draw(count, mCommandBuffer);
     }
     else
     {
-        ANGLE_TRY(setupIndexedDraw(context, mode, count, type, indices, &commandBuffer));
-        commandBuffer->drawIndexed(count, 1, 0, 0, 0);
+        ANGLE_TRY(setupIndexedDraw(context, mode, count, type, indices));
+        mCommandBuffer->drawIndexed(count, 1, 0, 0, 0);
     }
 
     return angle::Result::Continue;
