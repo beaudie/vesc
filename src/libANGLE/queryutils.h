b94f03514577cc60e370b8b7728a21459cf2891a
Shahbaz Youssefi <syoussefi@chromium.org>
2019-01-14 17:02:52 -0500

    Implement EGL_KHR_fence_sync and EGL_KHR_wait_sync
    EGL_KHR_fence_sync introduces the EGLSync object and associated
    create/destroy/clientWait functions.  EGL_KHR_wait_sync adds the
    serverWait function on top of that.
    
    Bug: angleproject:2466
    Change-Id: Iebb239a85c4471ea18b3c3a8a83b793af555e31d

--- src/libANGLE/queryutils.h
+++ src/libANGLE/queryutils.h
@@ -232,6 +232,7 @@ namespace egl
 {
 struct Config;
 class Surface;
+class Sync;
 
 void QueryConfigAttrib(const Config *config, EGLint attribute, EGLint *value);
 
@@ -239,6 +240,7 @@ void QueryContextAttrib(const gl::Context *context, EGLint attribute, EGLint *va
 
 void QuerySurfaceAttrib(const Surface *surface, EGLint attribute, EGLint *value);
 void SetSurfaceAttrib(Surface *surface, EGLint attribute, EGLint value);
+Error GetSyncAttrib(Display *display, Sync *sync, EGLint attribute, EGLint *value);
 
 }  // namespace egl
 
