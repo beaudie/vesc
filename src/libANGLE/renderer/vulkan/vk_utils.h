1e1ca69008595628f5c0a8d0982f16a265d15f0f
Tobin Ehlis <tobine@google.com>
2019-01-14 12:35:07 -0700

    Vulkan: Adding custom pool allocator
    Copied pool allocator used by compiler to common and hooking it up as
    custom allocator for CommandPools.
    
    RendererVk now has a private poolAllocator and VkAllocationCallbacks
    struct. The allocation callbacks are initialized to static functions
    in RendererVk::initializeDevice() and then passed to CommandPool init()
    and destroy() functions.
    
    Using the pool allocator saves Command Pool/Buffer clean-up time which
    was showing us as a bottleneck is some cases.
    
    Bug: angleproject:2863
    Change-Id: I81aa8a7ec60397676fa722d6435029db27947ef4

--- src/libANGLE/renderer/vulkan/vk_utils.h
+++ src/libANGLE/renderer/vulkan/vk_utils.h
@@ -16,6 +16,7 @@
 
 #include "common/Optional.h"
 #include "common/PackedEnums.h"
+#include "common/PoolAlloc.h"
 #include "common/debug.h"
 #include "libANGLE/Error.h"
 #include "libANGLE/Observer.h"
@@ -283,14 +284,19 @@ class MemoryProperties final : angle::NonCopyable
     VkPhysicalDeviceMemoryProperties mMemoryProperties;
 };
 
+void InitPoolAllocationCallbacks(angle::PoolAllocator *poolAllocator,
+                                 VkAllocationCallbacks *allocationCallbacks);
+
 class CommandPool final : public WrappedObject<CommandPool, VkCommandPool>
 {
   public:
     CommandPool();
 
-    void destroy(VkDevice device);
+    void destroy(VkDevice device, const VkAllocationCallbacks *pAllocationCallbacks);
 
-    VkResult init(VkDevice device, const VkCommandPoolCreateInfo &createInfo);
+    VkResult init(VkDevice device,
+                  const VkCommandPoolCreateInfo &createInfo,
+                  const VkAllocationCallbacks *pAllocationCallbacks);
 };
 
 // Helper class that wraps a Vulkan command buffer.
@@ -301,7 +307,7 @@ class CommandBuffer : public WrappedObject<CommandBuffer, VkCommandBuffer>
 
     VkCommandBuffer releaseHandle();
 
-    // This is used for normal pool allocated command buffers. It reset the handle.
+    // This is used for normal pool allocated command buffers. It resets the handle.
     void destroy(VkDevice device);
 
     // This is used in conjunction with VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT.
@@ -776,6 +782,28 @@ class Scoped final : angle::NonCopyable
 
   private:
     VkDevice mDevice;
+    T mVar;
+};
+
+// Helper class to handle RAII patterns for initialization. Requires that T have a destroy method
+// that takes a VkDevice & VkAllocationCallbacks ptr and returns void.
+template <typename T>
+class ScopedCustomAllocation final : angle::NonCopyable
+{
+  public:
+    ScopedCustomAllocation(VkDevice device, VkAllocationCallbacks allocationCBs)
+        : mDevice(device), mAllocationCallbacks(allocationCBs)
+    {}
+    ~ScopedCustomAllocation() { mVar.destroy(mDevice, &mAllocationCallbacks); }
+
+    const T &get() const { return mVar; }
+    T &get() { return mVar; }
+
+    T &&release() { return std::move(mVar); }
+
+  private:
+    VkDevice mDevice;
+    VkAllocationCallbacks mAllocationCallbacks;
     T mVar;
 };
 
