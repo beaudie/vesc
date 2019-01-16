c8f8068ab00da75e1c491e680866e83732a3207f
Geoff Lang <geofflang@chromium.org>
2019-01-16 09:34:30 -0500

    Vulkan: Implement EGL Images for 2D and Renderbuffer sources.
    No support for non-zero mipmaps as sources yet.
    
    Supress dEQP tests due to aparent driver bugs with scissored clears on depth
    or stencil attachments.
    
    BUG=angleproject:2668
    
    Change-Id: Idaa5e70ce9b0c91232fbb989cbf4de1b9134aafb

--- src/libANGLE/renderer/vulkan/vk_helpers.cpp
+++ src/libANGLE/renderer/vulkan/vk_helpers.cpp
@@ -1957,7 +1957,7 @@ angle::Result ImageHelper::allocateStagingMemory(ContextVk *contextVk,
                                    newBufferAllocatedOut);
 }
 
-angle::Result ImageHelper::flushStagedUpdates(ContextVk *contextVk,
+angle::Result ImageHelper::flushStagedUpdates(Context *context,
                                               uint32_t levelCount,
                                               vk::CommandBuffer *commandBuffer)
 {
@@ -1966,9 +1966,9 @@ angle::Result ImageHelper::flushStagedUpdates(ContextVk *contextVk,
         return angle::Result::Continue;
     }
 
-    RendererVk *renderer = contextVk->getRenderer();
+    RendererVk *renderer = context->getRenderer();
 
-    ANGLE_TRY(mStagingBuffer.flush(contextVk));
+    ANGLE_TRY(mStagingBuffer.flush(context));
 
     std::vector<SubresourceUpdate> updatesToKeep;
 
@@ -2024,7 +2024,7 @@ angle::Result ImageHelper::flushStagedUpdates(ContextVk *contextVk,
 
     if (mSubresourceUpdates.empty())
     {
-        mStagingBuffer.releaseRetainedBuffers(contextVk->getRenderer());
+        mStagingBuffer.releaseRetainedBuffers(context->getRenderer());
     }
     else
     {
