4c16374562ee54d3d24f7fb5257294cd95527133
Michael Spang <spang@chromium.org>
2019-01-21 18:09:15 -0500

    Vulkan: Port renderer to Fuchsia
    Add DisplayVk and WindowSurfaceVk subclasses for Fuchsia to the vulkan
    renderer, as well as an implementation of OSWindow that renders
    fullscreen for the test suite.
    
    Fuchsia implements VkSurfaceKHR in a vulkan layer, so add the ability
    for DisplayVk subclasses to request this.
    
    Disallow use of the vulkan loader from third_party as Fuchsia uses a fork
    of the loader and has not sent those changes upstream yet.
    
    Invent an EGLNativeWindowType for Fuchsia consisting of just a size and
    in image pipe handle. The size is necessary because we cannot
    automatically determine the desired size from an image pipe.
    
    Image pipes can only be used once to create a VkSurfaceKHR. This means we
    have to recreate the pipe in tests that call eglCreateWindowSurface more
    than once with a single OSWindow, or the second call will fail. Add a
    resetNativeWindow() method to accomplish this.
    
    BUG=angleproject:2475
    TEST=angle_end2end_tests on Fuchsia
    
    Change-Id: I71a613a362dd1c8aada49a3c02ae461e064457bf

---
+++ util/fuchsia/ScenicWindow.cpp
@@ -0,0 +1,191 @@
+//
+// Copyright (c) 2019 The ANGLE Project Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+
+#include "util/fuchsia/ScenicWindow.h"
+
+#include <lib/async-loop/cpp/loop.h>
+#include <lib/fdio/util.h>
+#include <lib/fidl/cpp/interface_ptr.h>
+#include <lib/fidl/cpp/interface_request.h>
+#include <lib/zx/channel.h>
+#include <zircon/status.h>
+
+#include "common/debug.h"
+
+namespace
+{
+
+async::Loop *GetDefaultLoop()
+{
+    static async::Loop *defaultLoop = new async::Loop(&kAsyncLoopConfigAttachToThread);
+    return defaultLoop;
+}
+
+zx::channel ConnectToServiceRoot()
+{
+    zx::channel clientChannel;
+    zx::channel serverChannel;
+    zx_status_t result = zx::channel::create(0, &clientChannel, &serverChannel);
+    ASSERT(result == ZX_OK);
+    result = fdio_service_connect("/svc/.", serverChannel.release());
+    ASSERT(result == ZX_OK);
+    return clientChannel;
+}
+
+template <typename Interface>
+zx_status_t ConnectToService(zx_handle_t serviceRoot, fidl::InterfaceRequest<Interface> request)
+{
+    ASSERT(request.is_valid());
+    return fdio_service_connect_at(serviceRoot, Interface::Name_, request.TakeChannel().release());
+}
+
+template <typename Interface>
+fidl::InterfacePtr<Interface> ConnectToService(zx_handle_t serviceRoot)
+{
+    fidl::InterfacePtr<Interface> result;
+    ConnectToService(serviceRoot, result.NewRequest());
+    return result;
+}
+
+}  // namespace
+
+ScenicWindow::ScenicWindow()
+    : loop_(GetDefaultLoop()),
+      mServiceRoot(ConnectToServiceRoot()),
+      mScenic(ConnectToService<fuchsia::ui::scenic::Scenic>(mServiceRoot.get())),
+      mViewManager(ConnectToService<fuchsia::ui::viewsv1::ViewManager>(mServiceRoot.get())),
+      mPresenter(ConnectToService<fuchsia::ui::policy::Presenter>(mServiceRoot.get())),
+      mScenicSession(mScenic.get()),
+      mParent(&mScenicSession),
+      mShape(&mScenicSession),
+      mMaterial(&mScenicSession),
+      mViewListenerBinding(this),
+      mNativeWindow(std::make_unique<FuchsiaEGLWindow>())
+{}
+
+ScenicWindow::~ScenicWindow()
+{
+    destroy();
+}
+
+bool ScenicWindow::initialize(const std::string &name, size_t width, size_t height)
+{
+    destroy();
+
+    // Set up scenic resources.
+    zx::eventpair parentExportToken;
+    mParent.BindAsRequest(&parentExportToken);
+    mParent.SetEventMask(fuchsia::ui::gfx::kMetricsEventMask);
+    mParent.AddChild(mShape);
+    mShape.SetShape(scenic::Rectangle(&mScenicSession, width, height));
+    mShape.SetMaterial(mMaterial);
+
+    // Create view and present it.
+    zx::eventpair viewHolderToken;
+    zx::eventpair viewToken;
+    zx_status_t status = zx::eventpair::create(0 /* options */, &viewToken, &viewHolderToken);
+    ASSERT(status == ZX_OK);
+    mPresenter->Present2(std::move(viewHolderToken), nullptr);
+    mViewManager->CreateView2(mView.NewRequest(), std::move(viewToken),
+                              mViewListenerBinding.NewBinding(), std::move(parentExportToken),
+                              "ANGLE");
+    mView.set_error_handler(fit::bind_member(this, &ScenicWindow::OnScenicError));
+    mViewListenerBinding.set_error_handler(fit::bind_member(this, &ScenicWindow::OnScenicError));
+
+    // Set up FuchsiaEGLWindow.
+    mNativeWindow->magic           = FUCHSIA_EGL_WINDOW_MAGIC;
+    mNativeWindow->size            = sizeof(*mNativeWindow);
+    mNativeWindow->width           = width;
+    mNativeWindow->height          = height;
+
+    return true;
+}
+
+void ScenicWindow::destroy()
+{
+    setNativeWindowImagePipe(ZX_HANDLE_INVALID);
+}
+
+void ScenicWindow::resetNativeWindow()
+{
+    fuchsia::images::ImagePipePtr imagePipe;
+    uint32_t imagePipeId = mScenicSession.AllocResourceId();
+    mScenicSession.Enqueue(scenic::NewCreateImagePipeCmd(imagePipeId, imagePipe.NewRequest()));
+    mMaterial.SetTexture(imagePipeId);
+    mScenicSession.ReleaseResource(imagePipeId);
+    mScenicSession.Present(0, [](fuchsia::images::PresentationInfo info) {});
+
+    setNativeWindowImagePipe(imagePipe.Unbind().TakeChannel().release());
+}
+
+EGLNativeWindowType ScenicWindow::getNativeWindow() const
+{
+    return reinterpret_cast<EGLNativeWindowType>(mNativeWindow.get());
+}
+
+EGLNativeDisplayType ScenicWindow::getNativeDisplay() const
+{
+    return EGL_DEFAULT_DISPLAY;
+}
+
+void ScenicWindow::messageLoop()
+{
+    loop_->Run(zx::deadline_after({}), true /* once */);
+}
+
+void ScenicWindow::setMousePosition(int x, int y)
+{
+    UNIMPLEMENTED();
+}
+
+bool ScenicWindow::setPosition(int x, int y)
+{
+    UNIMPLEMENTED();
+    return false;
+}
+
+bool ScenicWindow::resize(int width, int height)
+{
+    mNativeWindow->width  = width;
+    mNativeWindow->height = height;
+
+    return true;
+}
+
+void ScenicWindow::setVisible(bool isVisible) {}
+
+void ScenicWindow::signalTestEvent() {}
+
+void ScenicWindow::OnPropertiesChanged(fuchsia::ui::viewsv1::ViewProperties properties,
+                                       OnPropertiesChangedCallback callback)
+{
+    UNIMPLEMENTED();
+}
+
+void ScenicWindow::OnScenicEvents(std::vector<fuchsia::ui::scenic::Event> events)
+{
+    UNIMPLEMENTED();
+}
+
+void ScenicWindow::OnScenicError(zx_status_t status)
+{
+    WARN() << "OnScenicError: " << zx_status_get_string(status);
+}
+
+void ScenicWindow::setNativeWindowImagePipe(zx_handle_t imagePipeHandle)
+{
+    if (mNativeWindow->imagePipeHandle != ZX_HANDLE_INVALID)
+    {
+        zx_handle_close(mNativeWindow->imagePipeHandle);
+    }
+    mNativeWindow->imagePipeHandle = imagePipeHandle;
+}
+
+// static
+OSWindow *OSWindow::New()
+{
+    return new ScenicWindow;
+}
