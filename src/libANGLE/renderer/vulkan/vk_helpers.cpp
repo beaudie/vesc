0ec92cb3c9177f6087bb2b50a7d78ded37269ae2
Shahbaz Youssefi <syoussefi@chromium.org>
2019-01-23 13:58:09 -0500

    Vulkan: implement swapchain resizing
    Bug: angleproject:2942
    Change-Id: Ic62a5a57b712ffa2b087f5fecde0dc8942194075

--- src/libANGLE/renderer/vulkan/vk_helpers.cpp
+++ src/libANGLE/renderer/vulkan/vk_helpers.cpp
@@ -1260,6 +1260,7 @@ void ImageHelper::init2DWeakReference(VkImage handle,
     mExtents    = extents;
     mFormat     = &format;
     mSamples    = samples;
+    mCurrentLayout = VK_IMAGE_LAYOUT_UNDEFINED;
     mLayerCount = 1;
     mLevelCount = 1;
 
