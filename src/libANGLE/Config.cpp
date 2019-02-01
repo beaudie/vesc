3692e185cdd15f99f7ecd443f59e959520f66b6f
Ian Elliott <ianelliott@google.com>
2019-02-01 15:43:59 -0700

    Implement EGL_ANDROID_recordable for Vulkan back-end.
    This initial implementation provides the extension, and always answers
    that the ANativeWindow is not recordable.
    
    BUG=angleproject:2511
    
    Change-Id: Id3c57351dd1029bff7adf7166f9c82eee6e634b3

--- src/libANGLE/Config.cpp
+++ src/libANGLE/Config.cpp
@@ -59,7 +59,8 @@ Config::Config()
       transparentGreenValue(0),
       transparentBlueValue(0),
       optimalOrientation(0),
-      colorComponentType(EGL_COLOR_COMPONENT_TYPE_FIXED_EXT)
+      colorComponentType(EGL_COLOR_COMPONENT_TYPE_FIXED_EXT),
+      recordable(EGL_FALSE)
 {}
 
 Config::~Config() {}
@@ -265,7 +266,7 @@ std::vector<const Config *> ConfigSet::filter(const AttributeMap &attributeMap) 
                     match = config.stencilSize >= attributeValue;
                     break;
                 case EGL_CONFIG_CAVEAT:
-                    match = config.configCaveat == (EGLenum)attributeValue;
+                    match = config.configCaveat == static_cast<EGLenum>(attributeValue);
                     break;
                 case EGL_CONFIG_ID:
                     match = config.configID == attributeValue;
@@ -274,7 +275,7 @@ std::vector<const Config *> ConfigSet::filter(const AttributeMap &attributeMap) 
                     match = config.level >= attributeValue;
                     break;
                 case EGL_NATIVE_RENDERABLE:
-                    match = config.nativeRenderable == (EGLBoolean)attributeValue;
+                    match = config.nativeRenderable == static_cast<EGLBoolean>(attributeValue);
                     break;
                 case EGL_NATIVE_VISUAL_TYPE:
                     match = config.nativeVisualType == attributeValue;
@@ -289,7 +290,7 @@ std::vector<const Config *> ConfigSet::filter(const AttributeMap &attributeMap) 
                     match = (config.surfaceType & attributeValue) == attributeValue;
                     break;
                 case EGL_TRANSPARENT_TYPE:
-                    match = config.transparentType == (EGLenum)attributeValue;
+                    match = config.transparentType == static_cast<EGLenum>(attributeValue);
                     break;
                 case EGL_TRANSPARENT_BLUE_VALUE:
                     match = config.transparentBlueValue == attributeValue;
@@ -301,10 +302,10 @@ std::vector<const Config *> ConfigSet::filter(const AttributeMap &attributeMap) 
                     match = config.transparentRedValue == attributeValue;
                     break;
                 case EGL_BIND_TO_TEXTURE_RGB:
-                    match = config.bindToTextureRGB == (EGLBoolean)attributeValue;
+                    match = config.bindToTextureRGB == static_cast<EGLBoolean>(attributeValue);
                     break;
                 case EGL_BIND_TO_TEXTURE_RGBA:
-                    match = config.bindToTextureRGBA == (EGLBoolean)attributeValue;
+                    match = config.bindToTextureRGBA == static_cast<EGLBoolean>(attributeValue);
                     break;
                 case EGL_MIN_SWAP_INTERVAL:
                     match = config.minSwapInterval == attributeValue;
@@ -319,7 +320,7 @@ std::vector<const Config *> ConfigSet::filter(const AttributeMap &attributeMap) 
                     match = config.alphaMaskSize >= attributeValue;
                     break;
                 case EGL_COLOR_BUFFER_TYPE:
-                    match = config.colorBufferType == (EGLenum)attributeValue;
+                    match = config.colorBufferType == static_cast<EGLenum>(attributeValue);
                     break;
                 case EGL_RENDERABLE_TYPE:
                     match = (config.renderableType & attributeValue) == attributeValue;
@@ -345,6 +346,9 @@ std::vector<const Config *> ConfigSet::filter(const AttributeMap &attributeMap) 
                     break;
                 case EGL_COLOR_COMPONENT_TYPE_EXT:
                     match = config.colorComponentType == static_cast<EGLenum>(attributeValue);
+                    break;
+                case EGL_RECORDABLE_ANDROID:
+                    match = config.recordable == static_cast<EGLBoolean>(attributeValue);
                     break;
                 default:
                     UNREACHABLE();
