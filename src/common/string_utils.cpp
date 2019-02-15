70ac6d4121c9fe02dd2e9732330bbbfc04311b3e
Preston Jackson <pjackson@google.com>
2019-02-15 15:44:24 -0700

    Fixing WidenString to correctly return Invalid on non-Windows systems.
    Adding a check for static_cast<size_t>(-1) result from mbstowcs() when
    conversion fails. Updated string_utils tests to include WidenString.
    
    Bug: angleproject:2532
    Change-Id: I501da564db1bd11a0d0024e3026c323230bddc35

--- src/common/string_utils.cpp
+++ src/common/string_utils.cpp
@@ -15,6 +15,8 @@
 #include <fstream>
 #include <sstream>
 
+#include <iostream>
+
 #include "common/platform.h"
 
 namespace angle
@@ -143,7 +145,7 @@ Optional<std::vector<wchar_t>> WidenString(size_t length, const char *cString)
 #if !defined(ANGLE_PLATFORM_WINDOWS)
     mbstate_t mbstate = {};
     size_t written    = mbsrtowcs(wcstring.data(), &cString, length + 1, &mbstate);
-    if (written == 0)
+    if (written == static_cast<std::size_t>(-1))
     {
         return Optional<std::vector<wchar_t>>::Invalid();
     }
