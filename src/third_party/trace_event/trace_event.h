adf9ef74954104dede7c373abd4fd59cfcf237af
Courtney Goeltzenleuchter <courtneygo@google.com>
2019-02-26 13:43:41 -0700

    Make compiler defer destroy until end of scope
    In release builds was seeing the trace events being very short.
    Apparently the compiler decided that since no one is accessing the
    trace object it was okay to destroy it as soon as it was done
    creating it.
    Looking online, some indicate that it is okay for the compiler to
    reorder things so long as the effect was the same, e.g. no side-effects.
    For whatever reason, compiler didn't think outputing trace markers
    had side effects so it was destroyed right away.
    Adding the static global variable and referencing it in the constructor
    and destructor appears to be sufficient to cause the compiler to
    wait to call the destructor until the end of scope - as intended by
    the code. So now trace markers look the same for both release and
    debug builds.
    
    Bug: angleproject:3176
    Test: ./external/chromium-trace/systrace.py -t 10 -o /tmp/maps.html -a com.google.android.apps.maps gfx sched freq idle am wm gfx view sync irq binder_driver
    Examine trace for markers and verify they are expected size.
    
    Change-Id: I593b8685c5e72a844bebec4b98e1373110a235ad

--- src/third_party/trace_event/trace_event.h
+++ src/third_party/trace_event/trace_event.h
@@ -462,20 +462,16 @@
 // Implementation detail: internal macro to create static category and add begin
 // event if the category is enabled. Also adds the end event when the scope
 // ends.
-#define INTERNAL_TRACE_EVENT_ADD_SCOPED(category, name, ...)                      \
-    do                                                                            \
-    {                                                                             \
-        INTERNAL_TRACE_EVENT_GET_CATEGORY_INFO(category);                         \
-        gl::TraceEvent::TraceEndOnScopeClose INTERNALTRACEEVENTUID(profileScope); \
-        if (*INTERNALTRACEEVENTUID(catstatic))                                    \
-        {                                                                         \
-            gl::TraceEvent::addTraceEvent(                                        \
-                TRACE_EVENT_PHASE_BEGIN, INTERNALTRACEEVENTUID(catstatic), name,  \
-                gl::TraceEvent::noEventId, TRACE_EVENT_FLAG_NONE, ##__VA_ARGS__); \
-            INTERNALTRACEEVENTUID(profileScope)                                   \
-                .initialize(INTERNALTRACEEVENTUID(catstatic), name);              \
-        }                                                                         \
-    } while (0)
+#define INTERNAL_TRACE_EVENT_ADD_SCOPED(category, name, ...)                                     \
+    INTERNAL_TRACE_EVENT_GET_CATEGORY_INFO(category);                                            \
+    gl::TraceEvent::TraceEndOnScopeClose INTERNALTRACEEVENTUID(profileScope);                    \
+    if (*INTERNALTRACEEVENTUID(catstatic))                                                       \
+    {                                                                                            \
+        gl::TraceEvent::addTraceEvent(TRACE_EVENT_PHASE_BEGIN, INTERNALTRACEEVENTUID(catstatic), \
+                                      name, gl::TraceEvent::noEventId, TRACE_EVENT_FLAG_NONE,    \
+                                      ##__VA_ARGS__);                                            \
+        INTERNALTRACEEVENTUID(profileScope).initialize(INTERNALTRACEEVENTUID(catstatic), name);  \
+    }
 
 // Implementation detail: internal macro to create static category and add
 // event if the category is enabled.
@@ -534,6 +530,7 @@ namespace TraceEvent
 // used.
 const int zeroNumArgs              = 0;
 const unsigned long long noEventId = 0;
+static int activeScopedTraces      = 0;
 
 // TraceID encapsulates an ID that can either be an integer or pointer. Pointers
 // are mangled with the Process ID so that they are unlikely to collide when the
@@ -710,9 +707,10 @@ class TraceEndOnScopeClose
 {
   public:
     // Note: members of m_data intentionally left uninitialized. See initialize.
-    TraceEndOnScopeClose() : m_pdata(0) {}
+    TraceEndOnScopeClose() : m_pdata(0) { activeScopedTraces++; }
     ~TraceEndOnScopeClose()
     {
+        activeScopedTraces--;
         if (m_pdata)
             addEventIfEnabled();
     }
