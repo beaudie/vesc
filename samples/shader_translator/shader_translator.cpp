98b6e3fd18a2e559f1c925271fff798b30314db1
Mingyu Hu <mihu@microsoft.com>
2019-03-12 14:27:40 -0700

    Bug: angleproject:3341
    GL_ANGLE_multiview has been renamed to GL_OVR_multiview2.
    
    changes include:
    1) GL_OVR_multiview to GL_OVR_multiview2 extension directive change
    2) Removal of all references to side by side. We no longer support multiple views in a single 2DTexture. Only 2DTextureArray's are supported
    3) WebGL 2 (ES3) is required for multiview
    
    Change-Id: Ie0c1d21d7610f8feebdb2e4d01c6947f57e69328

--- samples/shader_translator/shader_translator.cpp
+++ samples/shader_translator/shader_translator.cpp
@@ -219,7 +219,7 @@ int main(int argc, char *argv[])
                       case 'n': resources.NV_shader_framebuffer_fetch = 1; break;
                       case 'a': resources.ARM_shader_framebuffer_fetch = 1; break;
                       case 'm':
-                          resources.OVR_multiview = 1;
+                          resources.OVR_multiview2 = 1;
                           compileOptions |= SH_INITIALIZE_BUILTINS_FOR_INSTANCED_MULTIVIEW;
                           compileOptions |= SH_SELECT_VIEW_IN_NV_GLSL_VERTEX_SHADER;
                           break;
