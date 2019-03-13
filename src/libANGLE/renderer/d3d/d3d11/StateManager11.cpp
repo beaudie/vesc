7f9d62c4cff2840d654d81e94a09f58fce0f703e
Jonah Ryan-Davis <jonahr@google.com>
2019-03-13 12:56:49 -0400

    gl_VertexID is incorrect if offset argument to glDrawArrays is non-zero
    On D3D the vertex ID is always indexed from 0, unlike GL. The D3D
    backend had assumed the wrong behavior. This forwards the true offset to
    D3D by using the ConstantsBuffer.
    
    Bug: angle/3090
    Change-Id: Ia19e3490503c97541af14979b75af0c94c67ab6b

--- src/libANGLE/renderer/d3d/d3d11/StateManager11.cpp
+++ src/libANGLE/renderer/d3d/d3d11/StateManager11.cpp
@@ -489,6 +489,12 @@ void ShaderConstants11::onViewportChange(const gl::Rectangle &glViewport,
     mVertex.viewScale[1] = mPixel.viewScale[1];
 }
 
+void ShaderConstants11::onFirstVertexChange(GLint firstVertex)
+{
+    mVertex.firstVertex = static_cast<uint32_t>(firstVertex);
+    mShaderConstantsDirty.set(gl::ShaderType::Vertex);
+}
+
 void ShaderConstants11::onSamplerChange(gl::ShaderType shaderType,
                                         unsigned int samplerIndex,
                                         const gl::Texture &texture,
@@ -2169,6 +2175,8 @@ angle::Result StateManager11::updateState(const gl::Context *context,
     if (!mLastFirstVertex.valid() || mLastFirstVertex.value() != firstVertex)
     {
         mLastFirstVertex = firstVertex;
+        mShaderConstants.onFirstVertexChange(firstVertex);
+        mInternalDirtyBits.set(DIRTY_BIT_PROGRAM_UNIFORM_BUFFERS);
         invalidateInputLayout();
     }
 
