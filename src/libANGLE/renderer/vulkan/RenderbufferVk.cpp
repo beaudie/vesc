f84332963869d064a54d5336a0e675a1031a31cc
Shahbaz Youssefi <syoussefi@chromium.org>
2019-03-21 16:30:31 -0400

    Vulkan: Implement robust resource initialization
    If a texture or renderbuffer needs to be cleared for robust access or
    due to having emulated channels, it is immediately cleared.  The former
    relies on a front-end feature that optimizes robust access clears only
    to levels and layers that are not fully initialized through data upload.
    
    Bug: angleproject:2722
    Change-Id: Icdab856eb4ffe963f78569b6d80d9ff5cb27ff9b

--- src/libANGLE/renderer/vulkan/RenderbufferVk.cpp
+++ src/libANGLE/renderer/vulkan/RenderbufferVk.cpp
@@ -17,14 +17,6 @@
 
 namespace rx
 {
-
-namespace
-{
-constexpr VkClearDepthStencilValue kDefaultClearDepthStencilValue = {0.0f, 1};
-constexpr VkClearColorValue kBlackClearColorValue                 = {{0}};
-
-}  // anonymous namespace
-
 RenderbufferVk::RenderbufferVk(const gl::RenderbufferState &state)
     : RenderbufferImpl(state), mOwnsImage(false), mImage(nullptr)
 {}
@@ -92,18 +84,11 @@ angle::Result RenderbufferVk::setStorage(const gl::Context *context,
         ANGLE_TRY(mImage->initImageView(contextVk, gl::TextureType::_2D, aspect, gl::SwizzleState(),
                                         &mImageView, 0, 1));
 
-        // TODO(jmadill): Fold this into the RenderPass load/store ops. http://anglebug.com/2361
-        vk::CommandBuffer *commandBuffer = nullptr;
-        ANGLE_TRY(mImage->recordCommands(contextVk, &commandBuffer));
-
-        if (isDepthOrStencilFormat)
+        // Clear the renderbuffer if it has emulated channels.
+        if (vkFormat.hasEmulatedChannels())
         {
-            mImage->clearDepthStencil(aspect, aspect, kDefaultClearDepthStencilValue,
-                                      commandBuffer);
-        }
-        else
-        {
-            mImage->clearColor(kBlackClearColorValue, 0, 1, commandBuffer);
+            mImage->stageSubresourceEmulatedClear(gl::ImageIndex::Make2D(0),
+                                                  vkFormat.angleFormat());
         }
 
         mRenderTarget.init(mImage, &mImageView, 0, 0, nullptr);
@@ -172,7 +157,7 @@ angle::Result RenderbufferVk::getAttachmentRenderTarget(const gl::Context *conte
 angle::Result RenderbufferVk::initializeContents(const gl::Context *context,
                                                  const gl::ImageIndex &imageIndex)
 {
-    UNIMPLEMENTED();
+    mImage->stageSubresourceRobustClear(imageIndex, mImage->getFormat().angleFormat());
     return angle::Result::Continue;
 }
 
