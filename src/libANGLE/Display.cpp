00b453c0b7e2755c558d4792730ac94025046186
Geoff Lang <geofflang@chromium.org>
2019-03-27 14:08:44 -0400

    Vulkan: Pass the current context to egl Sync operations.
    The EGL_KHR_fence_sync spec says that if a flush is needed before waiting
    on the sync, it's done on the current context for the current thread.
    
    This helps simplify the multithreading design, we don't need to worry about
    flusing on a context that may no longer exist or is executing on a different
    thread.  It does allow infinite waits because the context with the fence is
    never flushed but the spec allows this.
    
    BUG=angleproject:2464
    
    Change-Id: I8bf2f93c408fee2dae95caa5bb9c76ba67687931

--- src/libANGLE/Display.cpp
+++ src/libANGLE/Display.cpp
@@ -896,7 +896,10 @@ Error Display::createContext(const Config *configuration,
     return NoError();
 }
 
-Error Display::createSync(EGLenum type, const AttributeMap &attribs, Sync **outSync)
+Error Display::createSync(const gl::Context *currentContext,
+                          EGLenum type,
+                          const AttributeMap &attribs,
+                          Sync **outSync)
 {
     ASSERT(isInitialized());
 
@@ -908,7 +911,7 @@ Error Display::createSync(EGLenum type, const AttributeMap &attribs, Sync **outS
     angle::UniqueObjectPointer<egl::Sync, Display> syncPtr(new Sync(mImplementation, type, attribs),
                                                            this);
 
-    ANGLE_TRY(syncPtr->initialize(this));
+    ANGLE_TRY(syncPtr->initialize(this, currentContext));
 
     Sync *sync = syncPtr.release();
 
@@ -1425,16 +1428,6 @@ EGLint Display::programCacheResize(EGLint limit, EGLenum mode)
             UNREACHABLE();
             return 0;
     }
-}
-
-Error Display::clientWaitSync(Sync *sync, EGLint flags, EGLTime timeout, EGLint *outResult)
-{
-    return sync->clientWait(this, flags, timeout, outResult);
-}
-
-Error Display::waitSync(Sync *sync, EGLint flags)
-{
-    return sync->serverWait(this, flags);
 }
 
 }  // namespace egl
