08596f53a1efa8d46994df81e05bc050b3dec928
Shahbaz Youssefi <syoussefi@chromium.org>
2019-03-29 00:32:45 -0400

    Vulkan: glClearBuffer* implementation
    Refactors FramebufferVk::clear such that specific render targets could
    be cleared, with clear values not necessarily set through glClearColor
    etc.
    
    FramebufferVk::clearWithRenderPassOp is modified so that loadOp and
    clear values are set after the render pass has been registered in the
    graph.  This allows multiple glClearBuffer calls to coalesce into the
    same render pass.
    
    glClearBuffer calls are then implemented simply as calls to the
    refactored clear function with the appropriate parameters.
    
    Bug: angleproject:3187
    Change-Id: I2fdfcbea5bf244f63ec981b91caca47f5ee3cd3a

--- src/tests/gl_tests/ClearTest.cpp
+++ src/tests/gl_tests/ClearTest.cpp
@@ -895,6 +895,9 @@ TEST_P(ClearTestES3, MixedSRGBClear)
 // flush or finish after ClearBufferfv or each draw.
 TEST_P(ClearTestES3, RepeatedClear)
 {
+    // ES3 shaders are not yet supported on Vulkan.
+    ANGLE_SKIP_TEST_IF(IsVulkan());
+
     constexpr char kVS[] =
         "#version 300 es\n"
         "in highp vec2 position;\n"
@@ -1270,7 +1273,7 @@ ANGLE_INSTANTIATE_TEST(ClearTest,
                        ES2_OPENGLES(),
                        ES3_OPENGLES(),
                        ES2_VULKAN());
-ANGLE_INSTANTIATE_TEST(ClearTestES3, ES3_D3D11(), ES3_OPENGL(), ES3_OPENGLES());
+ANGLE_INSTANTIATE_TEST(ClearTestES3, ES3_D3D11(), ES3_OPENGL(), ES3_OPENGLES(), ES3_VULKAN());
 ANGLE_INSTANTIATE_TEST(ScissoredClearTest, ES2_D3D11(), ES2_OPENGL(), ES2_VULKAN());
 ANGLE_INSTANTIATE_TEST(VulkanClearTest, ES2_VULKAN());
 
