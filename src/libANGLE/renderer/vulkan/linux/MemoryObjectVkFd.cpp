556ad8e42a66464703357f3888e3a501079bd363
Michael Spang <spang@chromium.org>
2019-04-03 17:17:57 -0400

    Vulkan: Implement glTexStorageMem2DEXT
    This implements support for creating textures that alias vulkan images
    allocated inside external memory.
    
    Bug: angleproject:3289
    
    Change-Id: Iad071f353a217793102ae737647c7cd572f7b0ad

--- src/libANGLE/renderer/vulkan/linux/MemoryObjectVkFd.cpp
+++ src/libANGLE/renderer/vulkan/linux/MemoryObjectVkFd.cpp
@@ -55,4 +55,53 @@ angle::Result MemoryObjectVkFd::importMemoryOpaqueFd(gl::Context *context, GLuin
     return angle::Result::Continue;
 }
 
+angle::Result MemoryObjectVkFd::initImage(const gl::Context *context,
+                                          gl::TextureType type,
+                                          size_t levels,
+                                          GLenum internalFormat,
+                                          const gl::Extents &size,
+                                          GLuint64 offset,
+                                          vk::ImageHelper *image)
+{
+    ContextVk *contextVk = vk::GetImpl(context);
+    RendererVk *renderer = contextVk->getRenderer();
+
+    const vk::Format &vkFormat = renderer->getFormat(internalFormat);
+
+    // TODO(spang): Check capabilities; this must == supported usage flags.
+    VkImageUsageFlags usage = VK_IMAGE_USAGE_TRANSFER_SRC_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT |
+                              VK_IMAGE_USAGE_SAMPLED_BIT | VK_IMAGE_USAGE_STORAGE_BIT |
+                              VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT |
+                              VK_IMAGE_USAGE_INPUT_ATTACHMENT_BIT;
+
+    VkExternalMemoryImageCreateInfo externalMemoryImageCreateInfo = {};
+    externalMemoryImageCreateInfo.sType       = VK_STRUCTURE_TYPE_EXTERNAL_MEMORY_IMAGE_CREATE_INFO;
+    externalMemoryImageCreateInfo.handleTypes = VK_EXTERNAL_MEMORY_HANDLE_TYPE_OPAQUE_FD_BIT;
+
+    ANGLE_TRY(image->initExternal(contextVk, type, size, vkFormat, 1, usage,
+                                  vk::ImageLayout::ExternalPreInitialized,
+                                  &externalMemoryImageCreateInfo, levels, 1));
+
+    VkMemoryRequirements externalMemoryRequirements;
+    vkGetImageMemoryRequirements(renderer->getDevice(), image->getImage().getHandle(),
+                                 &externalMemoryRequirements);
+
+    ASSERT(mFd != -1);
+    VkImportMemoryFdInfoKHR importMemoryFdInfo = {};
+    importMemoryFdInfo.sType                   = VK_STRUCTURE_TYPE_IMPORT_MEMORY_FD_INFO_KHR;
+    importMemoryFdInfo.handleType              = VK_EXTERNAL_MEMORY_HANDLE_TYPE_OPAQUE_FD_BIT;
+    importMemoryFdInfo.fd                      = dup(mFd);
+
+    // TODO(jmadill, spang): Memory sub-allocation. http://anglebug.com/2162
+    ASSERT(offset == 0);
+    ASSERT(externalMemoryRequirements.size == mSize);
+
+    VkMemoryPropertyFlags flags = 0;
+    ANGLE_TRY(image->initExternalMemory(contextVk, renderer->getMemoryProperties(),
+                                        externalMemoryRequirements, &importMemoryFdInfo,
+                                        VK_QUEUE_FAMILY_EXTERNAL, flags));
+
+    return angle::Result::Continue;
+}
+
 }  // namespace rx
