771fed564e3c6ac2a69b7bf08f7e27bfe604e94f
Michael Spang <spang@chromium.org>
2019-04-03 14:57:35 -0400

    Implement resource management for GL_EXT_memory_object
    This implements glCreateMemoryObjectsEXT, glDeleteMemoryObjectsEXT, and
    glIsMemoryObjectEXT. It's not possible to do anything useful with them
    yet.
    
    Bug: 3289
    
    Change-Id: I8882b657e9de564b5f97f8dea87838f67b1928f8

---
+++ src/libANGLE/MemoryObject.cpp
@@ -0,0 +1,26 @@
+//
+// Copyright 2019 The ANGLE Project Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+// MemoryObject.h: Implements the gl::MemoryObject class [EXT_external_objects]
+
+#include "libANGLE/MemoryObject.h"
+
+#include "common/angleutils.h"
+#include "libANGLE/renderer/GLImplFactory.h"
+#include "libANGLE/renderer/MemoryObjectImpl.h"
+
+namespace gl
+{
+
+MemoryObject::MemoryObject(rx::GLImplFactory *factory, GLuint id)
+    : RefCountObject(id), mMemoryObject(factory->createMemoryObject())
+{}
+
+MemoryObject::~MemoryObject()
+{
+    SafeDelete(mMemoryObject);
+}
+
+}  // namespace gl
