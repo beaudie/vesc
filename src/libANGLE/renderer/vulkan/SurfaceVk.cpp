dc9cec7ab7c0c8774a530eaff8126a5d98865e53
Geoff Lang <geofflang@chromium.org>
2019-04-09 09:55:14 -0400

    Vulkan: Store fences instead of serials in SurfaceVK::SwapHistory
    Previous swaps may not have been submitted by the same context as the current
    swap and we can't safely wait on serials submitted by other contexts.
    
    BUG=angleproject:2464
    
    Change-Id: Ibcb23171feae038fd45abdefb80c4e8a373744e2

--- src/libANGLE/renderer/vulkan/SurfaceVk.cpp
+++ src/libANGLE/renderer/vulkan/SurfaceVk.cpp
@@ -296,7 +296,7 @@ WindowSurfaceVk::SwapHistory::SwapHistory(SwapHistory &&other)
 
 WindowSurfaceVk::SwapHistory &WindowSurfaceVk::SwapHistory::operator=(SwapHistory &&other)
 {
-    std::swap(serial, other.serial);
+    std::swap(fence, other.fence);
     std::swap(semaphores, other.semaphores);
     std::swap(swapchain, other.swapchain);
     return *this;
@@ -312,11 +312,23 @@ void WindowSurfaceVk::SwapHistory::destroy(VkDevice device)
         swapchain = VK_NULL_HANDLE;
     }
 
+    fence.reset(device);
+
     for (vk::Semaphore &semaphore : semaphores)
     {
         semaphore.destroy(device);
     }
     semaphores.clear();
+}
+
+angle::Result WindowSurfaceVk::SwapHistory::waitFence(ContextVk *context)
+{
+    if (fence)
+    {
+        ANGLE_VK_TRY(context,
+                     fence->wait(context->getDevice(), std::numeric_limits<uint64_t>::max()));
+    }
+    return angle::Result::Continue;
 }
 
 WindowSurfaceVk::WindowSurfaceVk(const egl::SurfaceState &surfaceState,
@@ -524,7 +536,7 @@ angle::Result WindowSurfaceVk::recreateSwapchain(ContextVk *contextVk,
             {
                 // TODO: Can't finishToSerial on a serial from another context that this surface may
                 // have swapped with.  Need to use fences.
-                ANGLE_TRY(contextVk->finishToSerial(swap.serial));
+                ANGLE_TRY(swap.waitFence(contextVk));
                 vkDestroySwapchainKHR(contextVk->getDevice(), swap.swapchain, nullptr);
                 swap.swapchain = VK_NULL_HANDLE;
             }
@@ -765,10 +777,8 @@ angle::Result WindowSurfaceVk::present(ContextVk *contextVk,
     // Throttle the submissions to avoid getting too far ahead of the GPU.
     SwapHistory &swap = mSwapHistory[mCurrentSwapHistoryIndex];
     {
-        // TODO: Can't finishToSerial on a serial from another context that this surface may have
-        // swapped with.  Need to use fences.
         TRACE_EVENT0("gpu.angle", "WindowSurfaceVk::present: Throttle CPU");
-        ANGLE_TRY(contextVk->finishToSerial(swap.serial));
+        ANGLE_TRY(swap.waitFence(contextVk));
         swap.destroy(contextVk->getDevice());
     }
 
@@ -778,6 +788,9 @@ angle::Result WindowSurfaceVk::present(ContextVk *contextVk,
     ANGLE_TRY(image.image.recordCommands(contextVk, &swapCommands));
 
     image.image.changeLayout(VK_IMAGE_ASPECT_COLOR_BIT, vk::ImageLayout::Present, swapCommands);
+
+    // Grab the fence used for the last flush to know when the work to render this frame is done.
+    ANGLE_TRY(contextVk->getSubmitFence(&swap.fence));
 
     ANGLE_TRY(contextVk->flushImpl());
 
@@ -827,7 +840,6 @@ angle::Result WindowSurfaceVk::present(ContextVk *contextVk,
     }
 
     // Update the swap history for this presentation
-    swap.serial     = contextVk->getLastSubmittedQueueSerial();
     swap.semaphores = std::move(mFlushSemaphoreChain);
 
     ++mCurrentSwapHistoryIndex;
