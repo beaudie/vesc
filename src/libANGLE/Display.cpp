b6bbe4564afad0a600d09d301cba0255a7d38d36
Tim Van Patten <timvp@google.com>
2019-04-11 12:56:00 -0600

    Set EGL_OPENGL_ES2_BIT for returned configs
    The Khronos conformance tests only test configs that have the conformant
    bit set. ANGLE is currently targetting OpenGL ES 2.0 compliance, so this
    change is setting the EGL_OPENGL_ES2_BIT bit to indicate this.
    
    Bug: needToOpenABug
    Test: Try Bots
    Change-Id: Ie31f63c3ea3b7bddfceec80ebc28f079ffd363df

--- src/libANGLE/Display.cpp
+++ src/libANGLE/Display.cpp
@@ -539,13 +539,12 @@ Error Display::initialize()
         return EglNotInitialized();
     }
 
-    // OpenGL ES1 is implemented in the frontend, explicitly add ES1 support to all configs
     for (auto &config : mConfigSet)
     {
-        // TODO(geofflang): Enable the conformant bit once we pass enough tests
-        // config.second.conformant |= EGL_OPENGL_ES_BIT;
+        config.second.conformant |= EGL_OPENGL_ES2_BIT;
 
-        config.second.renderableType |= EGL_OPENGL_ES_BIT;
+        // OpenGL ES1 is implemented in the frontend, explicitly add ES1 support to all configs
+        config.second.renderableType |= EGL_OPENGL_ES2_BIT;
     }
 
     initDisplayExtensions();
