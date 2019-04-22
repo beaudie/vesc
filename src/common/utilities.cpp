0cd2c0335437fccad582b09216997ee35c82cb86
Xinghua Cao <xinghua.cao@intel.com>
2019-04-22 17:34:53 +0800

    Unset conflicting SRVs and UAVs
    D3D11 cannot allow the same (sub)-resource bound as both
    a SRV and an UAV at the same time. Unset conflicting SRVs
    and UVAs between render pipeline and compute pipeline.
    
    Bug: angleproject:3152
    TEST=angle_end2end_tests.ComputeShaderTest.*
    
    Change-Id: I9cb8b902edbf987166a57af314af6b21a6874998

--- src/common/utilities.cpp
+++ src/common/utilities.cpp
@@ -893,6 +893,22 @@ unsigned int ElementTypeSize(GLenum elementType)
     }
 }
 
+PipelineType GetPipelineType(ShaderType type)
+{
+    switch (type)
+    {
+        case ShaderType::Vertex:
+        case ShaderType::Fragment:
+        case ShaderType::Geometry:
+            return PipelineType::RenderPipeline;
+        case ShaderType::Compute:
+            return PipelineType::ComputePipeline;
+        default:
+            UNREACHABLE();
+            return PipelineType::RenderPipeline;
+    }
+}
+
 }  // namespace gl
 
 namespace egl
