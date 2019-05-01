1c36e3f827664109a6238dfca0ad47e6a0e82f33
Jamie Madill <jmadill@chromium.org>
2019-05-01 11:46:38 -0400

    Remove several underused platform configs.
    * Removes D3D11 Warp and Reference configs from tests.
    * Removes several permutations of OpenGL back-end specific configs.
    * Removes FL 9_3 since it is no longer supported.
    * Removes present path "Copy" since it's redundant with normal D3D11.
    
    Reduces number of configs from over 60 to 25.
    
    Bug: angleproject:3393
    Change-Id: Ia5a23de3c4865b17ee50673a4066757b901a4b5a

--- src/tests/gl_tests/UniformBufferTest.cpp
+++ src/tests/gl_tests/UniformBufferTest.cpp
@@ -1520,7 +1520,6 @@ TEST_P(UniformBufferTest, DependentBufferChange)
 // tests should be run against.
 ANGLE_INSTANTIATE_TEST(UniformBufferTest,
                        ES3_D3D11(),
-                       ES3_D3D11_FL11_1(),
                        ES3_OPENGL(),
                        ES3_OPENGLES());
 ANGLE_INSTANTIATE_TEST(UniformBufferTest31, ES31_D3D11(), ES31_OPENGL(), ES31_OPENGLES());
