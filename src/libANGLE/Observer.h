5f69a19db695063cf966c77709e508efb88319ef
Jamie Madill <jmadill@chromium.org>
2019-05-09 13:30:08 -0400

    Rename state change notification messages.
    This makes the style use CamelCase instead of ALL_CAPS. It also cleans
    up some of the naming.
    
    Bug: angleproject:3427
    Change-Id: I6dff4f6d335ecf1a27e48df65743b1490bd3025a

--- src/libANGLE/Observer.h
+++ src/libANGLE/Observer.h
@@ -33,12 +33,12 @@ using SubjectIndex = size_t;
 
 enum class SubjectMessage
 {
-    CONTENTS_CHANGED,
-    STORAGE_CHANGED,
-    BINDING_CHANGED,
-    DEPENDENT_DIRTY_BITS,
-    RESOURCE_MAPPED,
-    RESOURCE_UNMAPPED,
+    BindingChanged,
+    ContentsChanged,
+    DirtyBitsFlagged,
+    SubjectChanged,
+    SubjectMapped,
+    SubjectUnmapped,
 };
 
 // The observing class inherits from this interface class.
