e348d1ba1a123c86082bc1673468f0e9ebdbc61e
Tim Van Patten <timvp@google.com>
2019-05-14 17:07:27 -0600

    Vulkan: Update front face mode during syncState()
    When handling DIRTY_BIT_DRAW_FRAMEBUFFER_BINDING in syncState(), the
    call to update the front face was missing, so culling could be enabled
    for the wrong front faces.
    
    Bug: angleproject:3237
    Test: Verify 3D apps render (more) correctly
    Test: New SimpleStateChangeTest end2end tests
    Change-Id: I1d94a977bea9e48d90b5346861e5565d2371cadd

--- src/tests/gl_tests/StateChangeTest.cpp
+++ src/tests/gl_tests/StateChangeTest.cpp
@@ -16,6 +16,19 @@ using namespace angle;
 namespace
 {
 
+constexpr char kBasicVertexShader[] =
+    R"(attribute vec3 position;
+void main()
+{
+    gl_Position = vec4(position, 1);
+})";
+
+constexpr char kGreenFragmentShader[] =
+    R"(void main()
+{
+    gl_FragColor = vec4(0, 1, 0, 1);
+})";
+
 class StateChangeTest : public ANGLETest
 {
   protected:
@@ -3528,6 +3541,130 @@ TEST_P(SimpleStateChangeTest, DeleteTextureThenDraw)
     glDrawArrays(GL_TRIANGLES, 0, 3);
     ASSERT_GL_NO_ERROR();
     EXPECT_PIXEL_COLOR_EQ(0, 0, GLColor::black);
+}
+
+// Validates if culling rasterization states work. Simply draws a quad with
+// cull face enabled with cullface front and make sure the face have not been rendered.
+TEST_P(SimpleStateChangeTest, FboCullFaceFrontEnabledCCWState)
+{
+    // Render to an FBO
+    GLFramebuffer fbo;
+    GLTexture texture;
+
+    glBindFramebuffer(GL_FRAMEBUFFER, fbo);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, getWindowWidth(), getWindowHeight(), 0, GL_RGBA,
+                 GL_UNSIGNED_BYTE, nullptr);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texture, 0);
+    ASSERT_GLENUM_EQ(GL_FRAMEBUFFER_COMPLETE, glCheckFramebufferStatus(GL_FRAMEBUFFER));
+
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_FRONT);
+    glFrontFace(GL_CCW);
+
+    ANGLE_GL_PROGRAM(greenProgram, kBasicVertexShader, kGreenFragmentShader);
+    glUseProgram(greenProgram);
+    drawQuad(greenProgram.get(), std::string("position"), 0.0f, 1.0f, true);
+    ASSERT_GL_NO_ERROR();
+    EXPECT_PIXEL_COLOR_EQ(0, 0, GLColor::transparentBlack);
+}
+
+// Validates if culling rasterization states work. Simply draws a quad with
+// cull face enabled with cullface back and make sure the face has been rendered.
+TEST_P(SimpleStateChangeTest, FboCullFaceBackEnabledCCWState)
+{
+    // Render to an FBO
+    GLFramebuffer fbo;
+    GLTexture texture;
+
+    glBindFramebuffer(GL_FRAMEBUFFER, fbo);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, getWindowWidth(), getWindowHeight(), 0, GL_RGBA,
+                 GL_UNSIGNED_BYTE, nullptr);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texture, 0);
+    ASSERT_GLENUM_EQ(GL_FRAMEBUFFER_COMPLETE, glCheckFramebufferStatus(GL_FRAMEBUFFER));
+
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_BACK);
+    glFrontFace(GL_CCW);
+
+    ANGLE_GL_PROGRAM(greenProgram, kBasicVertexShader, kGreenFragmentShader);
+    glUseProgram(greenProgram);
+    drawQuad(greenProgram.get(), std::string("position"), 0.0f, 1.0f, true);
+    ASSERT_GL_NO_ERROR();
+    EXPECT_PIXEL_COLOR_EQ(0, 0, GLColor::green);
+}
+
+// Validates if culling rasterization states work. Clears the texture and then draws
+// a quad with cull face enabled with cullface front and make sure the face have not
+// been rendered.
+TEST_P(SimpleStateChangeTest, ClearFboCullFaceFrontEnabledCCWState)
+{
+    // Render to an FBO
+    GLFramebuffer fbo;
+    GLTexture texture;
+
+    glBindFramebuffer(GL_FRAMEBUFFER, fbo);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, getWindowWidth(), getWindowHeight(), 0, GL_RGBA,
+                 GL_UNSIGNED_BYTE, nullptr);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texture, 0);
+    ASSERT_GLENUM_EQ(GL_FRAMEBUFFER_COMPLETE, glCheckFramebufferStatus(GL_FRAMEBUFFER));
+
+    glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    ASSERT_GL_NO_ERROR();
+    EXPECT_PIXEL_COLOR_EQ(0, 0, GLColor::blue);
+
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_FRONT);
+    glFrontFace(GL_CCW);
+
+    ANGLE_GL_PROGRAM(greenProgram, kBasicVertexShader, kGreenFragmentShader);
+    glUseProgram(greenProgram);
+    drawQuad(greenProgram.get(), std::string("position"), 0.0f, 1.0f, true);
+    ASSERT_GL_NO_ERROR();
+    EXPECT_PIXEL_COLOR_EQ(0, 0, GLColor::blue);
+}
+
+// Validates if culling rasterization states work. Clears the texture and then draws
+// a quad with cull face enabled with cullface back and make sure the face has
+// been rendered.
+TEST_P(SimpleStateChangeTest, ClearFboCullBackEnabledCCWState)
+{
+    // Render to an FBO
+    GLFramebuffer fbo;
+    GLTexture texture;
+
+    glBindFramebuffer(GL_FRAMEBUFFER, fbo);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, getWindowWidth(), getWindowHeight(), 0, GL_RGBA,
+                 GL_UNSIGNED_BYTE, nullptr);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texture, 0);
+    ASSERT_GLENUM_EQ(GL_FRAMEBUFFER_COMPLETE, glCheckFramebufferStatus(GL_FRAMEBUFFER));
+
+    glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    ASSERT_GL_NO_ERROR();
+    EXPECT_PIXEL_COLOR_EQ(0, 0, GLColor::blue);
+
+    glEnable(GL_CULL_FACE);
+    glCullFace(GL_BACK);
+    glFrontFace(GL_CCW);
+
+    ANGLE_GL_PROGRAM(greenProgram, kBasicVertexShader, kGreenFragmentShader);
+    glUseProgram(greenProgram);
+    drawQuad(greenProgram.get(), std::string("position"), 0.0f, 1.0f, true);
+    ASSERT_GL_NO_ERROR();
+    EXPECT_PIXEL_COLOR_EQ(0, 0, GLColor::green);
 }
 }  // anonymous namespace
 
