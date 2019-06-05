a02b8797039504b03fb0386ebc59559590bbe02e
Jamie Madill <jmadill@chromium.org>
2019-06-05 12:30:37 -0400

    Vulkan: Implement a texture descriptor cache.
    The descriptor cache is stored in each program. It is indexed by a set
    of 32-bit serials. Each texture generates a unique serial for every
    combination of VkImage and VkSampler that the texture owns. The texture
    descriptor is refreshed every time a texture changes or is rebound.
    
    The descriptor cache is accessed via an unoredered map with the texture
    serial sets as the hash key. We also store the maximum active texture
    index in the cache key so we don't need to hash and memcmp on all 64
    active textures.
    
    This will currently fail if more than MAX_UINT serials are generated.
    But that number is high enough that it shouldn't be possible to hit
    in practice in a practical amount of time.
    
    Requires shifting the texture sync to ContextVk so we can get the new
    serial after the textures are updated. And to make sure to update the
    image layouts even if the descriptors are not dirty.
    
    Improves performance of the T-Rex demo. Also improves the score of the
    texture state change microbenchmark by about 40%.
    
    Bug: angleproject:3117
    Change-Id: Ieb9bec1e8c1a7619814afab767a1980b959a8241

--- src/libANGLE/renderer/vulkan/UtilsVk.cpp
+++ src/libANGLE/renderer/vulkan/UtilsVk.cpp
@@ -1256,9 +1256,10 @@ angle::Result UtilsVk::allocateDescriptorSet(ContextVk *contextVk,
                                              vk::RefCountedDescriptorPoolBinding *bindingOut,
                                              VkDescriptorSet *descriptorSetOut)
 {
+    bool newPoolAllocated;
     ANGLE_TRY(mDescriptorPools[function].allocateSets(
-        contextVk, mDescriptorSetLayouts[function][kSetIndex].get().ptr(), 1,
-        bindingOut, descriptorSetOut));
+        contextVk, mDescriptorSetLayouts[function][kSetIndex].get().ptr(), 1, bindingOut,
+        descriptorSetOut, &newPoolAllocated));
     bindingOut->get().updateSerial(contextVk->getCurrentQueueSerial());
     return angle::Result::Continue;
 }
