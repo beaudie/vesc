100ac0c6eb01f2579fc5e3786ccd93c0a574e3cd
Jiacheng Lu <lujc@google.com>
2019-06-07 15:54:00 -0600

    Vulkan: Use a persistant CommandPool
    Previously transient CommandPool is used for CommandBuffer allocation,
    it is created and destroyed per frame. However, profiling found that
    CommandPool destroy is very inefficient. So this commit removed the
    previous logic and use two preallocated resetable CommandPools (One for
    Primary and One for Secondary)
    
    Bug: angleproject:3508
    Change-Id: I8b36f2738b082811c3177935c61b10e01acb6947

---
+++ src/libANGLE/renderer/vulkan/CommandVk.cpp
@@ -0,0 +1,147 @@
+#include "libANGLE/renderer/vulkan/CommandVk.h"
+
+namespace rx
+{
+
+namespace vk
+{
+
+PersistantCommandBuffer::~PersistantCommandBuffer()
+{
+    ASSERT(!allocated());
+}
+
+bool PersistantCommandBuffer::allocated() const
+{
+    return priv::CommandBuffer::valid() && mPoolAllocateId != kNullPollAllocatedId &&
+           mOwnerPool != nullptr;
+}
+
+// Never destroy, it only calls the pool to collect this buffer
+void PersistantCommandBuffer::releaseHandle()
+{
+    if (mInflight)
+    {
+        ASSERT(mOwnerPool);
+        ASSERT(mOwnerPool->collect(std::move(*this)) == angle::Result::Continue);
+    }
+}
+
+PersistantCommandPool::PersistantCommandPool(VkCommandBufferLevel bufferLevel)
+    : mBufferLevel(bufferLevel), mVkDeviceCopy(VK_NULL_HANDLE)
+{}
+
+PersistantCommandPool::~PersistantCommandPool()
+{
+    ASSERT(mHandle == VK_NULL_HANDLE && mFreeBufferList.empty());
+}
+
+VkResult PersistantCommandPool::init(VkDevice device, uint32_t queueFamilyIndex)
+{
+    ASSERT(!valid());
+    mVkDeviceCopy = device;
+
+    // Initialize the command pool now that we know the queue family index.
+    VkCommandPoolCreateInfo commandPoolInfo = {};
+    commandPoolInfo.sType                   = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO;
+    commandPoolInfo.flags                   = VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT;
+    commandPoolInfo.queueFamilyIndex        = queueFamilyIndex;
+    VkResult res = vkCreateCommandPool(device, &commandPoolInfo, nullptr, &mHandle);
+    if (res != VK_SUCCESS)
+    {
+        return res;
+    }
+
+    allocateCommandBufferArray(getInitBufferNum());
+    return res;
+}
+
+void PersistantCommandPool::destroy(VkDevice device)
+{
+    if (valid())
+    {
+        ASSERT(mVkDeviceCopy == device);
+        for (CommandBufferArrayHandle &bufferArrayHandle : mAllocatedBufferArrays)
+        {
+            vkFreeCommandBuffers(device, this->getHandle(), bufferArrayHandle.size,
+                                 bufferArrayHandle.array);
+            delete[] bufferArrayHandle.array;
+            bufferArrayHandle.size  = 0;
+            bufferArrayHandle.array = nullptr;
+        }
+        mAllocatedBufferArrays.clear();
+        mAllocatedBufferCount = 0;
+
+        vkDestroyCommandPool(device, mHandle, nullptr);
+        mHandle = VK_NULL_HANDLE;
+    }
+    for (PersistantCommandBuffer &cmdBuf : mFreeBufferList)
+    {
+        cmdBuf.destroy(device);
+    }
+    mFreeBufferList.clear();
+}
+
+angle::Result PersistantCommandPool::alloc(PersistantCommandBuffer *bufferOutput)
+{
+    ASSERT(!mFreeBufferList.empty());
+
+    *bufferOutput = std::move(mFreeBufferList.front());
+    mFreeBufferList.pop_front();
+
+    bufferOutput->mInflight = true;
+    return angle::Result::Continue;
+}
+
+angle::Result PersistantCommandPool::collect(PersistantCommandBuffer &&buffer)
+{
+    ASSERT(buffer.mOwnerPool == this);
+    ASSERT(buffer.mInflight == true);
+    ASSERT(buffer.mPoolAllocateId != PersistantCommandBuffer::kNullPollAllocatedId);
+    ASSERT(buffer.mPoolAllocateId < (int32_t)mAllocatedBufferCount);
+    ASSERT(buffer.mBufferLevel == this->mBufferLevel);
+
+    buffer.mInflight = false;
+    // VK_COMMAND_BUFFER_RESET_RELEASE_RESOURCES_BIT NOT set, CommandBuffer
+    // still hold the memory resource
+    vkResetCommandBuffer(buffer.mHandle, 0);
+
+    mFreeBufferList.emplace_front(std::move(buffer));
+
+    return angle::Result::Continue;
+}
+
+void PersistantCommandPool::allocateCommandBufferArray(uint32_t count)
+{
+    VkCommandBuffer *pCmdBuffers = new VkCommandBuffer[count];
+    {
+        ASSERT(mVkDeviceCopy);
+        VkCommandBufferAllocateInfo commandBufferInfo = {};
+        commandBufferInfo.sType              = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+        commandBufferInfo.commandPool        = this->getHandle();
+        commandBufferInfo.level              = mBufferLevel;
+        commandBufferInfo.commandBufferCount = count;
+        ASSERT(vkAllocateCommandBuffers(mVkDeviceCopy, &commandBufferInfo, pCmdBuffers) ==
+               VK_SUCCESS);
+    }
+
+    mAllocatedBufferArrays.emplace_back(CommandBufferArrayHandle{
+        .array = pCmdBuffers,
+        .size  = count,
+    });
+
+    for (uint32_t i = 0; i < count; i++)
+    {
+        uint32_t id = mAllocatedBufferCount + i;
+        mFreeBufferList.emplace_back(
+            PersistantCommandBuffer(this, mBufferLevel, id, pCmdBuffers[i]));
+    }
+
+    mAllocatedBufferCount += count;
+    // If we allocate too many, it mostly like would be a bug
+    ASSERT(mAllocatedBufferCount < 100);
+}
+
+}  // namespace vk
+
+}  // namespace rx
---
+++ src/libANGLE/renderer/vulkan/CommandVk.h
@@ -0,0 +1,114 @@
+#ifndef LIBANGLE_RENDERER_VULKAN_COMMANDVK_H_
+#define LIBANGLE_RENDERER_VULKAN_COMMANDVK_H_
+
+#include <list>
+
+#include "libANGLE/renderer/vulkan/vk_wrapper.h"
+
+namespace rx
+{
+
+namespace vk
+{
+
+class PersistantCommandPool;
+
+class PersistantCommandBuffer final : public priv::CommandBuffer
+{
+    friend class PersistantCommandPool;
+
+  public:
+    PersistantCommandBuffer()
+        : priv::CommandBuffer(),
+          mInflight(false),
+          mPoolAllocateId(kNullPollAllocatedId),
+          mOwnerPool(nullptr)
+    {}
+    virtual ~PersistantCommandBuffer();
+
+    // Only works to initialize empty objects
+    PersistantCommandBuffer &operator=(PersistantCommandBuffer &&other)
+    {
+        ASSERT(!allocated());
+        std::swap(mHandle, other.mHandle);
+        std::swap(mInflight, other.mInflight);
+        std::swap(mPoolAllocateId, other.mPoolAllocateId);
+        std::swap(mBufferLevel, other.mBufferLevel);
+        std::swap(mOwnerPool, other.mOwnerPool);
+        return *this;
+    }
+    PersistantCommandBuffer(PersistantCommandBuffer &&other) { *this = std::move(other); }
+
+    bool allocated() const;
+
+    void releaseHandle();
+
+    // TODO: make base class's destroy() private
+
+  private:
+    PersistantCommandBuffer(PersistantCommandPool *pPool,
+                            VkCommandBufferLevel bufLevel,
+                            int32_t allocateId,
+                            const VkCommandBuffer &vkBufRef)
+        : priv::CommandBuffer(),
+          mInflight(false),
+          mPoolAllocateId(allocateId),
+          mBufferLevel(bufLevel),
+          mOwnerPool(pPool)
+    {
+        mHandle = vkBufRef;
+    }
+
+    bool mInflight;
+
+    static constexpr int kNullPollAllocatedId = -1;
+    int32_t mPoolAllocateId;
+
+    VkCommandBufferLevel mBufferLevel;
+    PersistantCommandPool *mOwnerPool;
+};
+
+class PersistantCommandPool final : WrappedObject<PersistantCommandPool, VkCommandPool>
+{
+  public:
+    PersistantCommandPool(VkCommandBufferLevel bufferLevel);
+    ~PersistantCommandPool();
+
+    inline uint32_t getInitBufferNum() const { return 10; }
+    // auto increate CommandBuffer number in a batch manner
+    inline uint32_t getBatchBufferNum() const { return 5; }
+
+    void destroy(VkDevice device);
+    VkResult init(VkDevice device, uint32_t queueFamilyIndex);
+
+    // TODO: reset
+
+    // XXX: There may be some approch to provide two types of
+    // CommandPool & CommandBuffer based on their level with template
+    // so that a compile time check is possible
+    angle::Result alloc(PersistantCommandBuffer *bufferOutput);
+    angle::Result collect(PersistantCommandBuffer &&buffer);
+
+  private:
+    struct CommandBufferArrayHandle
+    {
+        VkCommandBuffer *array;
+        uint32_t size;
+    };
+    void allocateBatch() { allocateCommandBufferArray(getBatchBufferNum()); }
+    void allocateCommandBufferArray(uint32_t count);
+
+    uint32_t mAllocatedBufferCount;
+    std::list<CommandBufferArrayHandle> mAllocatedBufferArrays;
+
+    std::list<PersistantCommandBuffer> mFreeBufferList;
+
+    VkCommandBufferLevel mBufferLevel;
+    // Assume VkDevice associated with pool wouldn't change (i.e. per PersistantPool per VkDevice)
+    VkDevice mVkDeviceCopy;
+};
+
+}  // namespace vk
+}  // namespace rx
+
+#endif
