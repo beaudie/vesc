267eda1a0bc9c02586e284e12715d7d4fe32ef34
Jiacheng Lu <lujc@google.com>
2019-06-07 15:54:00 -0600

    Vulkan: Use a persistent CommandPool
    Previously transient CommandPool is used for CommandBuffer allocation,
    it is created and destroyed per frame. However, profiling found that
    CommandPool destroy is very inefficient. So this commit removed the
    previous logic and use two preallocated resetable CommandPools (One for
    Primary and One for Secondary)
    
    Bug: angleproject:3508
    Change-Id: I8b36f2738b082811c3177935c61b10e01acb6947

--- src/libANGLE/renderer/vulkan/ContextVk.h
+++ src/libANGLE/renderer/vulkan/ContextVk.h
@@ -14,6 +14,7 @@
 
 #include "common/PackedEnums.h"
 #include "libANGLE/renderer/ContextImpl.h"
+#include "libANGLE/renderer/vulkan/PersistentCommandPool.h"
 #include "libANGLE/renderer/vulkan/RendererVk.h"
 #include "libANGLE/renderer/vulkan/vk_helpers.h"
 
@@ -392,7 +393,6 @@ class ContextVk : public ContextImpl, public vk::Context, public vk::CommandBuff
 
     angle::Result submitFrame(const VkSubmitInfo &submitInfo,
                               vk::PrimaryCommandBuffer &&commandBuffer);
-    void freeAllInFlightResources();
     angle::Result flushCommandGraph(vk::PrimaryCommandBuffer *commandBatch);
 
     angle::Result synchronizeCpuGpuTime();
@@ -488,6 +488,9 @@ class ContextVk : public ContextImpl, public vk::Context, public vk::CommandBuff
     vk::CommandPool mCommandPool;
     std::vector<vk::CommandPool> mCommandPoolFreeList;
 
+    // We use a Persistent CommandPool with pre-allocated buffers for primary CommandBuffer
+    vk::PersistentCommandPool mPrimaryCommandPool;
+
     Serial mLastCompletedQueueSerial;
     Serial mLastSubmittedQueueSerial;
     Serial mCurrentQueueSerial;
@@ -501,10 +504,15 @@ class ContextVk : public ContextImpl, public vk::Context, public vk::CommandBuff
 
         void destroy(VkDevice device);
 
+        vk::PrimaryCommandBuffer primaryCommands;
+        // commandPool is for secondary CommandBuffer allocation
         vk::CommandPool commandPool;
         vk::Shared<vk::Fence> fence;
         Serial serial;
     };
+
+    angle::Result releaseCommandPoolToBatch(CommandBatch *batch);
+    void recycleCommandPoolFromBatch(CommandBatch *batch);
 
     std::vector<CommandBatch> mInFlightCommands;
     std::vector<vk::GarbageObject> mGarbage;
