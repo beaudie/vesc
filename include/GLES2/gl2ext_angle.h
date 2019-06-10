d164c7705c8ae9132dc05687ffd5caf6710d1849
Michael Spang <spang@chromium.org>
2019-06-10 18:11:57 -0400

    RFC: Add external object extensions for Fuchsia to GL API
    Add extensions layerd on top of GL_EXT_memory_object and GL_EXT_semaphore
    to support GL & Vulkan interoperation on Fuchsia. They are analagous to
    the file descriptor versions, except not quite as opaque.
    
    They have not yet been documented or submitted to Khronos.
    
    Bug: angleproject:3492
    
    Change-Id: Ic4723ded9fff752c4186eaa6a7f07d9ac51a3364

--- include/GLES2/gl2ext_angle.h
+++ include/GLES2/gl2ext_angle.h
@@ -680,6 +680,34 @@ GL_APICALL void GL_APIENTRY glInvalidateTextureANGLE (GLenum target);
 #endif
 #endif /* GL_ANGLE_texture_external_update */
 
+#ifndef GL_ANGLE_memory_object_zircon_handle
+#define GL_ANGLE_memory_object_zircon_handle 1
+#define GL_HANDLE_TYPE_ZIRCON_VMO_ANGLE 0xA0C5 /* XXX - Temporary value */
+typedef void(GL_APIENTRYP PFNGLIMPORTMEMORYZIRCONHANDLEANGLEPROC)(GLuint memory,
+                                                             GLuint64 size,
+                                                             GLenum handleType,
+                                                             GLuint handle);
+#ifdef GL_GLEXT_PROTOTYPES
+GL_APICALL void GL_APIENTRY glImportMemoryZirconHandleANGLE(GLuint memory,
+                                                       GLuint64 size,
+                                                       GLenum handleType,
+                                                       GLuint handle);
+#endif
+#endif /* GL_ANGLE_memory_object_zircon_handle */
+
+#ifndef GL_ANGLE_semaphore_zircon_handle
+#define GL_ANGLE_semaphore_zircon_handle 1
+#define GL_HANDLE_TYPE_ZIRCON_EVENT_ANGLE 0xA0C6 /* XXX - Temporary value */
+typedef void(GL_APIENTRYP PFNGLIMPORTSEMAPHOREZIRCONHANDLEANGLEPROC)(GLuint semaphore,
+                                                             GLenum handleType,
+                                                             GLuint handle);
+#ifdef GL_GLEXT_PROTOTYPES
+GL_APICALL void GL_APIENTRY glImportSemaphoreZirconHandleANGLE(GLuint memory,
+                                                       GLenum handleType,
+                                                       GLuint handle);
+#endif
+#endif /* GL_ANGLE_semaphore_zircon_handle */
+
 // clang-format on
 
 #endif  // INCLUDE_GLES2_GL2EXT_ANGLE_H_
