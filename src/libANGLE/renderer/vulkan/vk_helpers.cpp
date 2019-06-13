59cdf00a3e351513b7de48860089ad6da995e398
Shahbaz Youssefi <syoussefi@chromium.org>
2019-06-13 14:32:20 -0400

    Vulkan: Reduce number of layout-transition-only graph nodes
    Bug: angleproject:3347
    Change-Id: I2fa58d6f9c73cda9a72e7543d4771a06efe6d644

--- src/libANGLE/renderer/vulkan/vk_helpers.cpp
+++ src/libANGLE/renderer/vulkan/vk_helpers.cpp
@@ -1790,6 +1790,10 @@ void ImageHelper::changeLayoutWithCommand(VkImageAspectFlags aspectMask,
                                           vk::CommandBuffer *commandBuffer)
 {
     ASSERT(commandBuffer != nullptr);
+    ASSERT(newLayout != ImageLayout::ColorAttachment ||
+           !mFormat->imageFormat().hasDepthOrStencilBits());
+    ASSERT(newLayout != ImageLayout::DepthStencilAttachment ||
+           mFormat->imageFormat().hasDepthOrStencilBits());
 
     if (!isLayoutChangeNecessary(newLayout))
     {
