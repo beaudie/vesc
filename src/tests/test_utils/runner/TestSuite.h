ece80538ba5a2a10fe26d0df138d7138fdccd22e
Jamie Madill <jmadill@chromium.org>
2019-06-16 22:16:35 -0400

    Add new test runner harness.
    Bug: angleproject:3162
    Change-Id: Idb15f113de8eb32db12bc93542de93b08d7c1447

---
+++ src/tests/test_utils/runner/TestSuite.h
@@ -0,0 +1,32 @@
+//
+// Copyright 2019 The ANGLE Project Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+// TestSuite:
+//   Basic implementation of a test harness in ANGLE.
+
+#include <memory>
+#include <string>
+
+namespace angle
+{
+
+class TestSuite
+{
+  public:
+    TestSuite(int *argc, char **argv);
+    ~TestSuite();
+
+  private:
+    void parseTestSuiteName(const char *executable);
+    bool parseTestSuiteFlag(const char *argument);
+
+    std::string mTestSuiteName;
+    std::string mFilterString;
+    const char *mResultsDirectory;
+    int mShardCount;
+    int mShardIndex;
+};
+
+}  // namespace angle
