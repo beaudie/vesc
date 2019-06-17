a3c7a4e5f4cf367c47d8aad94e61149048856ab1
Jamie Madill <jmadill@chromium.org>
2019-06-16 22:16:35 -0400

    Add new test runner harness.
    Bug: angleproject:3162
    Change-Id: Idb15f113de8eb32db12bc93542de93b08d7c1447

---
+++ src/tests/test_utils/runner/TestSuite.h
@@ -0,0 +1,99 @@
+//
+// Copyright 2019 The ANGLE Project Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+// TestSuite:
+//   Basic implementation of a test harness in ANGLE.
+
+#include <map>
+#include <memory>
+#include <string>
+
+#include "util/system_utils.h"
+
+namespace angle
+{
+struct TestIdentifier
+{
+    TestIdentifier();
+    TestIdentifier(const TestIdentifier &other);
+    ~TestIdentifier();
+
+    TestIdentifier &operator=(const TestIdentifier &other);
+
+    bool valid() const { return !testName.empty(); }
+
+    std::string testCaseName;
+    std::string testName;
+};
+
+inline bool operator<(const TestIdentifier &a, const TestIdentifier &b)
+{
+    return std::tie(a.testCaseName, a.testName) < std::tie(b.testCaseName, b.testName);
+}
+
+enum class TestResultType
+{
+    Crash,
+    Fail,
+    Skip,
+    Pass,
+    Timeout,
+    Unknown,
+};
+
+struct TestResult
+{
+    TestResultType type;
+    double elapsedTimeSeconds;
+};
+
+struct TestResults
+{
+    TestResults();
+    ~TestResults();
+
+    std::map<TestIdentifier, TestResult> results;
+    TestIdentifier currentTest;
+};
+
+struct FileLine
+{
+    const char *file;
+    int line;
+};
+
+class TestSuite
+{
+  public:
+    TestSuite(int *argc, char **argv);
+    ~TestSuite();
+
+    int run();
+    void onCrash();
+
+  private:
+    bool parseSingleArg(const char *argument);
+    bool runChildTestProcess(const std::vector<TestIdentifier> &testsInBatch);
+    int printUnexpectedFailuresAndReturnCount() const;
+
+    std::string mTestExcutableName;
+    std::string mTestSuiteName;
+    std::vector<TestIdentifier> mTestQueue;
+    std::string mFilterString;
+    std::string mFilterFile;
+    std::string mResultsDirectory;
+    std::string mResultsFile;
+    int mShardCount;
+    int mShardIndex;
+    angle::CrashCallback mCrashCallback;
+    TestResults mTestResults;
+    bool mBotMode;
+    int mBatchSize;
+    int mCurrentResultCount;
+    int mTotalResultCount;
+    std::map<TestIdentifier, FileLine> mTestFileLines;
+};
+
+}  // namespace angle
