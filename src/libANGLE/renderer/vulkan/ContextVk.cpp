802caf56632e26800c110f8a8c3bedb0c0c45f49
Courtney Goeltzenleuchter <courtneygo@google.com>
2019-06-19 12:11:13 -0600

    Vulkan: Fix dirty element array buffer updates.
    The issue occurs that binds the same buffer and calls glDrawElements
    with the same indices. The offset has been reset in the
    VertexArrayVk::syncState(), but it doesn't check the actual value
    in the ContextVk::setupIndexedDraw().
    
    Bug: angleproject:3362
    Change-Id: I0f7d2fc162bc8f1c36cb09ba689fd27b482b9035

--- src/libANGLE/renderer/vulkan/ContextVk.cpp
+++ src/libANGLE/renderer/vulkan/ContextVk.cpp
@@ -473,10 +473,13 @@ angle::Result ContextVk::setupIndexedDraw(const gl::Context *context,
                                           const void *indices,
                                           vk::CommandBuffer **commandBufferOut)
 {
+    ASSERT(mode != gl::PrimitiveMode::LineLoop);
+
     if (indexType != mCurrentDrawElementsType)
     {
         mDirtyBits.set(DIRTY_BIT_INDEX_BUFFER);
         mCurrentDrawElementsType = indexType;
+        mLastIndexBufferOffset   = reinterpret_cast<const void *>(angle::DirtyPointer);
     }
 
     const gl::Buffer *elementArrayBuffer = mVertexArray->getState().getElementArrayBuffer();
@@ -517,6 +520,7 @@ angle::Result ContextVk::setupLineLoopDraw(const gl::Context *context,
     ANGLE_TRY(mVertexArray->handleLineLoop(this, firstVertex, vertexOrIndexCount,
                                            indexTypeOrInvalid, indices, numIndicesOut));
     mDirtyBits.set(DIRTY_BIT_INDEX_BUFFER);
+    mLastIndexBufferOffset   = reinterpret_cast<const void *>(angle::DirtyPointer);
     mCurrentDrawElementsType = indexTypeOrInvalid != gl::DrawElementsType::InvalidEnum
                                    ? indexTypeOrInvalid
                                    : gl::DrawElementsType::UnsignedInt;
