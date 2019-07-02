e4dd23d3708f7d9edb39c6e74d1f908a42e9326e
James Dong <dongja@google.com>
2019-07-02 14:11:32 -0600

    Add bitmask for mismatched vertex attribs
    Adds a bitmask in StateCache that indicates when a vertex attribute is
    a different type than what is expected in the current program. Also
    refactors validation in WebGL for this case to use the bitmask.
    
    Preliminary step towards fixing this case in Vulkan backend.
    
    Bug: angleproject:3634
    Test: ./angle_end2end_tests --gtest_filter='WebGL2CompatibilityTest.VertexShaderAttributeTypeMismatch/*'
    Change-Id: I05637ad52c61443e4f951843d5934a4347cbb101

--- src/libANGLE/Context.cpp
+++ src/libANGLE/Context.cpp
@@ -8469,6 +8469,8 @@ void StateCache::updateActiveAttribsMask(Context *context)
     mCachedActiveBufferedAttribsMask = activeEnabled & ~clientAttribs;
     mCachedActiveDefaultAttribsMask  = activeAttribs & ~enabledAttribs;
     mCachedHasAnyEnabledClientAttrib = (clientAttribs & enabledAttribs).any();
+
+    updateMismatchedAttribsMask(glState);
 }
 
 void StateCache::updateVertexElementLimitsImpl(Context *context)
@@ -8520,6 +8522,34 @@ void StateCache::updateBasicDrawStatesError()
 void StateCache::updateBasicDrawElementsError()
 {
     mCachedBasicDrawElementsError = kInvalidPointer;
+}
+
+ANGLE_INLINE void StateCache::updateMismatchedAttribsMask(const State &glState)
+{
+    const VertexArray *vao = glState.getVertexArray();
+
+    unsigned long programAttribActiveBits = glState.getProgram()->getAttributesMask().to_ulong();
+    unsigned long currentValueActiveBits  = mCachedActiveDefaultAttribsMask.to_ulong();
+    unsigned long programAttribTypeBits = glState.getProgram()->getAttributesTypeMask().to_ulong();
+    unsigned long arrayAttribTypeBits   = vao->getAttributesTypeMask().to_ulong();
+    unsigned long currentValueTypeBits  = glState.getCurrentValuesTypeMask().to_ulong();
+
+    programAttribActiveBits |= programAttribActiveBits << kMaxComponentTypeMaskIndex;
+    currentValueActiveBits |= currentValueActiveBits << kMaxComponentTypeMaskIndex;
+
+    // Selects bits from arrayAttribTypeBits where the bit in currentValueActiveBits is 0,
+    // and the bits from currentValueTypeBits where the bit is 1.
+    unsigned long outputTypeBits =
+        arrayAttribTypeBits ^
+        ((arrayAttribTypeBits ^ currentValueTypeBits) & currentValueActiveBits);
+    unsigned long inputTypeBits = programAttribTypeBits;
+
+    unsigned long mismatchedTypeBits   = outputTypeBits ^ inputTypeBits;
+    unsigned long mismatchedAttribBits = mismatchedTypeBits;
+    mismatchedAttribBits |= mismatchedTypeBits >> kMaxComponentTypeMaskIndex;
+    mismatchedAttribBits &= programAttribActiveBits;
+
+    mCachedMismatchedAttribsMask = AttributesMask(mismatchedAttribBits);
 }
 
 intptr_t StateCache::getBasicDrawStatesErrorImpl(Context *context) const
@@ -8598,6 +8628,10 @@ void StateCache::onStencilStateChange(Context *context)
 void StateCache::onDefaultVertexAttributeChange(Context *context)
 {
     updateBasicDrawStatesError();
+
+    const State &glState = context->getState();
+
+    updateMismatchedAttribsMask(glState);
 }
 
 void StateCache::onActiveTextureChange(Context *context)
