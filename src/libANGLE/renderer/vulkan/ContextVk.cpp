6ba349abbc4d1a90a6a4a012cee8b4c56019a360
Tobin Ehlis <tobine@google.com>
2019-07-11 11:29:35 -0600

    Vulkan: Enable sampler objects
    This is the initial implementation to get GLES3.0 Sampler Objects
    working on the Vulkan backend.
    When samplers are dirty, a corresponding Vulkan sampler object will be
    created with associated state and textures are flagged as dirty anytime
    sampler bindings change.
    Then when handling textures dirty, any bound sampler objects are pulled
    from active texture units. When sampler objects are bound, their state
    is used instead of the built-in texture's sampler state.
    
    This change also adds a test that uses the same texture object with
    different sampler objects to test a corner case that dEQP misses.
    
    Bug: angleproject:3208
    Change-Id: I643d9c9d5cb92fecc02dad815a07bcf349534c70

--- src/libANGLE/renderer/vulkan/ContextVk.cpp
+++ src/libANGLE/renderer/vulkan/ContextVk.cpp
@@ -293,7 +293,7 @@ ContextVk::ContextVk(const gl::State &state, gl::ErrorSet *errorSet, RendererVk 
     mGraphicsDirtyBits = mNewGraphicsCommandBufferDirtyBits;
     mComputeDirtyBits  = mNewComputeCommandBufferDirtyBits;
 
-    mActiveTextures.fill(nullptr);
+    mActiveTextures.fill({nullptr, nullptr});
 
     mPipelineDirtyBitsMask.set();
     mPipelineDirtyBitsMask.reset(gl::State::DIRTY_BIT_TEXTURE_BINDINGS);
@@ -2431,6 +2431,7 @@ angle::Result ContextVk::updateActiveTextures(const gl::Context *context,
     for (size_t textureUnit : activeTextures)
     {
         gl::Texture *texture        = textures[textureUnit];
+        gl::Sampler *sampler        = mState.getSampler(textureUnit);
         gl::TextureType textureType = textureTypes[textureUnit];
 
         // Null textures represent incomplete textures.
@@ -2440,6 +2441,7 @@ angle::Result ContextVk::updateActiveTextures(const gl::Context *context,
         }
 
         TextureVk *textureVk = vk::GetImpl(texture);
+        SamplerVk *samplerVk = (sampler != nullptr) ? vk::GetImpl(sampler) : nullptr;
 
         vk::ImageHelper &image = textureVk->getImage();
 
@@ -2471,14 +2473,19 @@ angle::Result ContextVk::updateActiveTextures(const gl::Context *context,
 
         image.addReadDependency(recorder);
 
-        mActiveTextures[textureUnit] = textureVk;
-        mActiveTexturesDesc.update(textureUnit, textureVk->getSerial());
+        mActiveTextures[textureUnit].texture = textureVk;
+        mActiveTextures[textureUnit].sampler = samplerVk;
+        // Cache serials from sampler and texture, but re-use texture if no sampler bound
+        ASSERT(textureVk != nullptr);
+        mActiveTexturesDesc.update(
+            textureUnit, textureVk->getSerial(),
+            (samplerVk != nullptr) ? samplerVk->getSerial() : textureVk->getSerial());
     }
 
     return angle::Result::Continue;
 }
 
-const gl::ActiveTextureArray<TextureVk *> &ContextVk::getActiveTextures() const
+const gl::ActiveTextureArray<vk::TextureUnit> &ContextVk::getActiveTextures() const
 {
     return mActiveTextures;
 }
