7331604689612b6aad30e8a3f1c793dd5df00ad7
James Dong <dongja@google.com>
2019-07-24 11:33:49 -0600

    Vulkan: invalidate translation buffers for SSBOs
    Translation buffers weren't being marked dirty after running a compute
    shader in which they are bound as SSBOs.
    This change invalidates all SSBOs after a draw or compute call.
    
    Bug: angleproject:3739
    Change-Id: I66b56df7e619b55afc7e3da6b5613b6d050e06bb

--- src/libANGLE/Context.inl.h
+++ src/libANGLE/Context.inl.h
@@ -46,6 +46,16 @@ ANGLE_INLINE void MarkTransformFeedbackBufferUsage(const Context *context,
     }
 }
 
+ANGLE_INLINE void MarkShaderStorageBufferUsage(const Context *context)
+{
+    for (size_t index : context->getStateCache().getActiveShaderStorageBufferIndices())
+    {
+        const gl::OffsetBindingPointer<gl::Buffer> &buffer =
+            context->getState().getIndexedShaderStorageBuffer(index);
+        buffer.get()->onDataChanged();
+    }
+}
+
 // Return true if the draw is a no-op, else return false.
 //  A no-op draw occurs if the count of vertices is less than the minimum required to
 //  have a valid primitive for this mode (0 for points, 0-1 for lines, 0-2 for tris).
