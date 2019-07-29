d42d119dc066e86be1f06c5dcf8842ded1e4e66d
Tim Van Patten <timvp@google.com>
2019-07-29 16:04:56 -0600

    Enable ASTC 3D Textures
    Enable ASTC 3D textures.
    
    ETC are still unsupported, since dEQP fails if we indicate support.
    
    Bug: angleproject:3188
    Test: dEQP.KHR_GLES2/texture_3d_compressed_texture_*astc*
    Test: dEQP.KHR_GLES2/texture_3d_compressed_texture_negative_compressed_tex_image
    Change-Id: I8559ae42d19e63fdf96ea7a49f2d7e3d5098771c

--- src/libANGLE/validationES3.cpp
+++ src/libANGLE/validationES3.cpp
@@ -534,12 +534,6 @@ bool ValidateES3TexImageParametersBase(Context *context,
             return false;
         }
 
-        if (texType == TextureType::_3D)
-        {
-            context->validationError(GL_INVALID_OPERATION, kInvalidTextureTarget);
-            return false;
-        }
-
         // Disallow 3D-only compressed formats from being set on 2D textures
         if (actualFormatInfo.compressedBlockDepth > 1 && texType != TextureType::_2DArray)
         {
@@ -685,7 +679,7 @@ bool ValidateES3TexImage3DParameters(Context *context,
         return false;
     }
 
-    if (IsETC2EACFormat(format) && target != TextureTarget::_2DArray)
+    if (IsETC2EACFormat(internalformat) && target != TextureTarget::_2DArray)
     {
         // ES 3.1, Section 8.7, page 169.
         context->validationError(GL_INVALID_OPERATION, kInternalFormatRequiresTexture2DArray);
