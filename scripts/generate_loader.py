2f470313748e82506afa9088538f50e73cbbb41e
Tim Van Patten <timvp@google.com>
2019-07-29 11:34:21 -0600

    Make libEGL function pointers hidden in symbol table
    The libEGL_angle.so and libGLESv2_angle.so libraries are typically
    loaded into the same process each time, so we need to make sure there
    aren't any duplicated symbols which could violate ODR and lead to
    crashes or undefined behavior.   In this case, the libEGL_angle.so
    function pointer names were colliding with libGLESv2_angle.so function
    names, causing crashes at runtime.   The fix here is to mark the
    libEGL_angle.so function pointer symbols 'hidden', so they don't appear
    in the symbol table for other executables/libraries to see and can't be
    overridden by other symbols.
    
    Bug: angleproject:3751
    Test: Embed ANGLE within dEQP.apk and verify no crashes
    Change-Id: Ibb78369374a4a68b3489a7dd7775c52e29fa37e0

--- scripts/generate_loader.py
+++ scripts/generate_loader.py
@@ -97,7 +97,8 @@ def gen_libegl_loader():
     all_cmds = xml.all_cmd_names.get_all_commands()
 
     path = os.path.join("..", "src", "libEGL")
-    write_header(data_source_name, all_cmds, "egl", libegl_preamble, path, "LIBEGL", "", "EGL_")
+    write_header(data_source_name, all_cmds, "egl", libegl_preamble, path, "LIBEGL", "", "EGL_",
+                 "HIDDEN ")
     write_source(data_source_name, all_cmds, "egl", path, "", "EGL_")
 
 
@@ -223,6 +224,8 @@ def main():
 
 libegl_preamble = """#include <EGL/egl.h>
 #include <EGL/eglext.h>
+
+#define HIDDEN __attribute__((visibility("hidden")))
 """
 
 util_gles_preamble = """#if defined(GL_GLES_PROTOTYPES) && GL_GLES_PROTOTYPES
