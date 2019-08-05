4244daf4fd2e86c4b56511f03a76c98e11ceef28
Courtney Goeltzenleuchter <courtneygo@google.com>
2019-08-05 09:26:23 -0600

    Add support for sampling stencil texture
    Suspect a potential driver issue with integer sampling from stencil so
    skip dEQP-GLES31.functional.stencil_texturing.misc.compare_mode_effect
    in the expectations file. Will follow-up with more directed tests.
    Also skip dEQP-GLES31.functional.stencil_texturing.misc.base_level
    
    Blocking Bug: angleproject:3148
    
    Test:
    dEQP-GLES31.functional.stencil_texturing*
    
    Bug: angleproject:3575
    Change-Id: I9547406b44ec16629631c1bf50907e6a24f1a20e

--- src/libANGLE/renderer/vulkan/TextureVk.h
+++ src/libANGLE/renderer/vulkan/TextureVk.h
@@ -171,6 +171,19 @@ class TextureVk : public TextureImpl
     }
 
   private:
+    struct TextureVkViews final : angle::NonCopyable
+    {
+        TextureVkViews() {}
+        ~TextureVkViews() {}
+
+        void release(ContextVk *contextVk, Serial currentSerial);
+        vk::ImageView mDrawBaseLevelImageView;
+        vk::ImageView mReadBaseLevelImageView;
+        vk::ImageView mReadMipmapImageView;
+        vk::ImageView mFetchBaseLevelImageView;
+        vk::ImageView mFetchMipmapImageView;
+    };
+
     // Transform an image index from the frontend into one that can be used on the backing
     // ImageHelper, taking into account mipmap or cube face offsets
     gl::ImageIndex getNativeImageIndex(const gl::ImageIndex &inputImageIndex) const;
@@ -280,6 +293,13 @@ class TextureVk : public TextureImpl
                                  const vk::Format &format,
                                  uint32_t levelCount,
                                  uint32_t layerCount);
+    angle::Result initImageViewImpl(ContextVk *contextVk,
+                                    const vk::Format &format,
+                                    uint32_t levelCount,
+                                    uint32_t layerCount,
+                                    TextureVkViews *views,
+                                    VkImageAspectFlags aspectFlags,
+                                    gl::SwizzleState mappedSwizzle);
     angle::Result initCubeMapRenderTargets(ContextVk *contextVk);
 
     angle::Result ensureImageInitializedImpl(ContextVk *contextVk,
@@ -288,6 +308,10 @@ class TextureVk : public TextureImpl
                                              const vk::Format &format);
 
     void onStagingBufferChange() { onStateChange(angle::SubjectMessage::SubjectChanged); }
+
+    bool isStencilMode() const { return mState.getDepthStencilTextureMode() == GL_STENCIL_INDEX; }
+
+    const TextureVkViews *getTextureViews() const;
 
     bool mOwnsImage;
 
@@ -303,11 +327,8 @@ class TextureVk : public TextureImpl
 
     vk::ImageHelper *mImage;
 
-    vk::ImageView mDrawBaseLevelImageView;
-    vk::ImageView mReadBaseLevelImageView;
-    vk::ImageView mReadMipmapImageView;
-    vk::ImageView mFetchBaseLevelImageView;
-    vk::ImageView mFetchMipmapImageView;
+    TextureVkViews mDefaultViews;
+    TextureVkViews mStencilViews;
     std::vector<std::vector<vk::ImageView>> mLayerLevelDrawImageViews;
     vk::Sampler mSampler;
 
