240f2969a672495106cedfbd98b537586e707215
Tim Van Patten <timvp@google.com>
2019-08-08 13:03:34 -0600

    Support separate read and draw surfaces in eglMakeCurrent
    Update ANGLE's default framebuffer implementation to support reading
    and writing to different surfaces within the same framebuffer.
    
    Bug: angleproject:2620
    Test: EGLSurfaceTest[3]
    Change-Id: I4b1ea04ca87a751f80cf190bf3adec148fc4fce3

--- src/libANGLE/ResourceManager.h
+++ src/libANGLE/ResourceManager.h
@@ -278,7 +278,7 @@ class FramebufferManager
     Framebuffer *getFramebuffer(GLuint handle) const;
     void setDefaultFramebuffer(Framebuffer *framebuffer);
 
-    void invalidateFramebufferComplenessCache() const;
+    void invalidateFramebufferCompletenessCache() const;
 
     Framebuffer *checkFramebufferAllocation(rx::GLImplFactory *factory,
                                             const Caps &caps,
