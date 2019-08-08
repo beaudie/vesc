9e18e2ce55aec83e61e5a5790f1fa1503cf62225
Tobin Ehlis <tobine@google.com>
2019-08-08 13:02:46 -0600

    Use volk to load vk* func ptrs
    This change updates all ANGLE targets that use Vulkan to dyanmically
    link all of the VK entrypoints using the volk OSS library from
    https://github.com/zeux/volk.
    
    It's only two source files so baking them directly into ANGLE repo.
    Also it's used in both the tests and libANGLE trees so added to
    src/common/third_party/volk dir.
    
    Bug: angleproject:3740
    Change-Id: I84311fc3aa8d95d6fcf92443a281b8310c65a874

--- src/tests/test_utils/VulkanExternalHelper.cpp
+++ src/tests/test_utils/VulkanExternalHelper.cpp
@@ -8,7 +8,6 @@
 
 #include "test_utils/VulkanExternalHelper.h"
 
-#include <vulkan/vulkan.h>
 #include <vector>
 
 #include "common/bitset_utils.h"
@@ -142,7 +141,8 @@ VulkanExternalHelper::~VulkanExternalHelper()
 void VulkanExternalHelper::initialize()
 {
     ASSERT(mInstance == VK_NULL_HANDLE);
-
+    VkResult result = volkInitialize();
+    ASSERT(result == VK_SUCCESS);
     std::vector<VkExtensionProperties> instanceExtensionProperties =
         EnumerateInstanceExtensionProperties(nullptr);
 
@@ -185,9 +185,10 @@ void VulkanExternalHelper::initialize()
         /* .ppEnabledExtensionName = */ enabledInstanceExtensions.data(),
     };
 
-    VkResult result = vkCreateInstance(&instanceCreateInfo, nullptr, &mInstance);
+    result = vkCreateInstance(&instanceCreateInfo, nullptr, &mInstance);
     ASSERT(result == VK_SUCCESS);
     ASSERT(mInstance != VK_NULL_HANDLE);
+    volkLoadInstance(mInstance);
 
     std::vector<VkPhysicalDevice> physicalDevices = EnumeratePhysicalDevices(mInstance);
     ASSERT(physicalDevices.size() > 0);
