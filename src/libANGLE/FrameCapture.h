226f7e8bcc15d6ac2c7396075830cca78b64736b
Jiacheng Lu <lujc@google.com>
2019-08-09 15:45:56 -0600

    Support in memory frame capture replay
    Enable FrameCapture to dispatch its calls back to Context and replay
    them.
    
    Bug: angleproject:3785
    Change-Id: I580eff1fca3e39414a9b45bb296e0f3866ea33bc

--- src/libANGLE/FrameCapture.h
+++ src/libANGLE/FrameCapture.h
@@ -34,6 +34,42 @@ struct ParamCapture : angle::NonCopyable
     ParamCapture(ParamCapture &&other);
     ParamCapture &operator=(ParamCapture &&other);
 
+    template <typename T>
+    T getAs() const
+    {
+        return angle::AccessParamValue<T>(type, value);
+    }
+    template <typename T>
+    T getAsPointer() const
+    {
+        if (!data.empty())
+        {
+            ASSERT(data.size() == 1);
+            return reinterpret_cast<T>(const_cast<uint8_t *>(data[0].data()));
+        }
+
+        return getAs<T>();
+    }
+    template <typename T>
+    T getAsPointerPointer(std::vector<const uint8_t *> *pointersBufferOut) const
+    {
+        static_assert(sizeof(typename std::remove_pointer<T>::type) == sizeof(uint8_t *),
+                      "pointer size not match!");
+
+        if (!data.empty())
+        {
+            pointersBufferOut->clear();
+            pointersBufferOut->reserve(data.size());
+            for (const auto &data : data)
+            {
+                pointersBufferOut->emplace_back(data.data());
+            }
+            return reinterpret_cast<T>(const_cast<uint8_t **>(pointersBufferOut->data()));
+        }
+
+        return getAs<T>();
+    }
+
     std::string name;
     ParamType type;
     ParamValue value;
@@ -102,6 +138,7 @@ class FrameCapture final : angle::NonCopyable
     void captureCall(const gl::Context *context, CallCapture &&call);
     void onEndFrame();
     bool enabled() const;
+    void replay(gl::Context *context);
 
   private:
     // <CallName, ParamName>
@@ -126,6 +163,19 @@ class FrameCapture final : angle::NonCopyable
     gl::AttribArray<size_t> mClientArraySizes;
     std::map<Counter, int> mDataCounters;
     size_t mReadBufferSize;
+
+    struct ReplayParamBuffer
+    {
+        ReplayParamBuffer();
+        ~ReplayParamBuffer();
+
+        std::vector<const uint8_t *> pointersBuffer;
+        std::vector<uint8_t> readBuffer;
+    } mReplayParamBuffer;
+
+    static void ReplayCallCapture(gl::Context *context,
+                                  FrameCapture::ReplayParamBuffer *paramBuffer,
+                                  const CallCapture &call);
 };
 
 template <typename CaptureFuncT, typename... ArgsT>
