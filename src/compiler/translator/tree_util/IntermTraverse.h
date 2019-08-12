e923351e0552c8f1dc1c45a752c51f7c038582fd
Shahbaz Youssefi <syoussefi@chromium.org>
2019-08-12 16:46:57 -0400

    Vulkan: Support mixed column/row-major buffer fields
    Adds a comprehensive test for mixed column/row-major interface blocks,
    which flushes out various bugs in different OpenGL drivers too.
    
    Bug: angleproject:3443
    Change-Id: Ie88cca743373891bbb49d9f564f30407475e07fb

--- src/compiler/translator/tree_util/IntermTraverse.h
+++ src/compiler/translator/tree_util/IntermTraverse.h
@@ -129,10 +129,13 @@ class TIntermTraverser : angle::NonCopyable
     friend void TIntermConstantUnion::traverse(TIntermTraverser *);
     friend void TIntermFunctionPrototype::traverse(TIntermTraverser *);
 
-    TIntermNode *getParentNode() { return mPath.size() <= 1 ? nullptr : mPath[mPath.size() - 2u]; }
+    TIntermNode *getParentNode() const
+    {
+        return mPath.size() <= 1 ? nullptr : mPath[mPath.size() - 2u];
+    }
 
     // Return the nth ancestor of the node being traversed. getAncestorNode(0) == getParentNode()
-    TIntermNode *getAncestorNode(unsigned int n)
+    TIntermNode *getAncestorNode(unsigned int n) const
     {
         if (mPath.size() > n + 1u)
         {
@@ -142,6 +145,12 @@ class TIntermTraverser : angle::NonCopyable
     }
 
     const TIntermBlock *getParentBlock() const;
+
+    TIntermNode *getRootNode() const
+    {
+        ASSERT(!mPath.empty());
+        return mPath.front();
+    }
 
     void pushParentBlock(TIntermBlock *node);
     void incrementParentBlockPos();
