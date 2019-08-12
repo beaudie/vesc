b8b002eb31aa0e8b5ff9cc2ad5066454850c7999
Shahbaz Youssefi <syoussefi@chromium.org>
2019-08-12 16:46:57 -0400

    Vulkan: Support mixed column/row-major buffer fields
    Adds a comprehensive test for mixed column/row-major interface blocks,
    which flushes out various bugs in different OpenGL drivers too.
    
    Bug: angleproject:3443
    Change-Id: Ie88cca743373891bbb49d9f564f30407475e07fb

---
+++ src/compiler/translator/tree_ops/NameNamelessUniformBuffers.h
@@ -0,0 +1,26 @@
+//
+// Copyright 2019 The ANGLE Project Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+// NameNamelessUniformBuffers: Gives nameless uniform buffer variables internal names.
+//
+// For example:
+//   uniform UniformBuffer { int a; };
+//   x = a;
+// becomes:
+//   uniform UniformBuffer { int a; } s123;
+//   x = s123.a;
+//
+
+#ifndef COMPILER_TRANSLATOR_TREEOPS_NAMENAMELESSUNIFORMBUFFERS_H_
+#define COMPILER_TRANSLATOR_TREEOPS_NAMENAMELESSUNIFORMBUFFERS_H_
+
+namespace sh
+{
+class TIntermBlock;
+class TSymbolTable;
+void NameNamelessUniformBuffers(TIntermBlock *root, TSymbolTable *symbolTable);
+}  // namespace sh
+
+#endif  // COMPILER_TRANSLATOR_TREEOPS_NAMENAMELESSUNIFORMBUFFERS_H_
