ccd5181e4b5f15bd79625031cdbbffe4912f51cb
James Dong <dongja@google.com>
2019-08-14 10:39:34 -0600

    Vulkan: Improve cubemap emulation seam handling
    Changes seamful cubemap emulation to always compute the derivative,
    emulating the bias parameter by scaling the provided derivatives.
    This results in more accurate mipmap levels for seams within primitives.
    There are some artifacts as a result of how derivatives are calculated,
    but this matches the native driver.
    
    Bug: angleproject:3243
    Bug: angleproject:3732
    Change-Id: Icb976e2a7e14cb4210645571edc037d4e607bd0d

--- src/libANGLE/renderer/vulkan/ContextVk.cpp
+++ src/libANGLE/renderer/vulkan/ContextVk.cpp
@@ -239,7 +239,6 @@ ContextVk::ContextVk(const gl::State &state, gl::ErrorSet *errorSet, RendererVk 
       mFlipYForCurrentSurface(false),
       mIsAnyHostVisibleBufferWritten(false),
       mEmulateSeamfulCubeMapSampling(false),
-      mEmulateSeamfulCubeMapSamplingWithSubgroupOps(false),
       mLastCompletedQueueSerial(renderer->nextSerial()),
       mCurrentQueueSerial(renderer->nextSerial()),
       mPoolAllocator(kDefaultPoolAllocatorPageSize, 1),
@@ -443,8 +442,7 @@ angle::Result ContextVk::initialize()
         ANGLE_TRY(synchronizeCpuGpuTime());
     }
 
-    mEmulateSeamfulCubeMapSampling =
-        shouldEmulateSeamfulCubeMapSampling(&mEmulateSeamfulCubeMapSamplingWithSubgroupOps);
+    mEmulateSeamfulCubeMapSampling = shouldEmulateSeamfulCubeMapSampling();
 
     return angle::Result::Continue;
 }
@@ -2923,7 +2921,7 @@ vk::DescriptorSetLayoutDesc ContextVk::getDriverUniformsDescriptorSetDesc(
     return desc;
 }
 
-bool ContextVk::shouldEmulateSeamfulCubeMapSampling(bool *useSubgroupOpsOut) const
+bool ContextVk::shouldEmulateSeamfulCubeMapSampling() const
 {
     // Only allow seamful cube map sampling in non-webgl ES2.
     if (mState.getClientMajorVersion() != 2 || mState.isWebGL())
@@ -2935,15 +2933,6 @@ bool ContextVk::shouldEmulateSeamfulCubeMapSampling(bool *useSubgroupOpsOut) con
     {
         return false;
     }
-
-    // Use subgroup ops where available.
-    constexpr VkSubgroupFeatureFlags kSeamfulCubeMapSubgroupOperations =
-        VK_SUBGROUP_FEATURE_BASIC_BIT | VK_SUBGROUP_FEATURE_BALLOT_BIT |
-        VK_SUBGROUP_FEATURE_QUAD_BIT;
-    const VkSubgroupFeatureFlags deviceSupportedOperations =
-        mRenderer->getPhysicalDeviceSubgroupProperties().supportedOperations;
-    *useSubgroupOpsOut = (deviceSupportedOperations & kSeamfulCubeMapSubgroupOperations) ==
-                         kSeamfulCubeMapSubgroupOperations;
 
     return true;
 }
