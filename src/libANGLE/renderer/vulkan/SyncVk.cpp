2ffae70b06089d692c3c8163f8e92c1424544c65
Shahbaz Youssefi <syoussefi@chromium.org>
2019-08-23 14:53:26 -0400

    Vulkan: Introduce ContextScoped
    Scoped is changed to DeviceScoped and a ContextScoped class is added.
    The former destroys objects at the end of scope, and the latter releases
    them.
    
    Bug: angleproject:3757
    Change-Id: Ia0bbded53e94af9c8c72c77d07306d6724c6c409

--- src/libANGLE/renderer/vulkan/SyncVk.cpp
+++ src/libANGLE/renderer/vulkan/SyncVk.cpp
@@ -50,7 +50,7 @@ angle::Result FenceSyncVk::initialize(ContextVk *contextVk)
     eventCreateInfo.sType             = VK_STRUCTURE_TYPE_EVENT_CREATE_INFO;
     eventCreateInfo.flags             = 0;
 
-    vk::Scoped<vk::Event> event(device);
+    vk::DeviceScoped<vk::Event> event(device);
     ANGLE_VK_TRY(contextVk, event.get().init(device, eventCreateInfo));
 
     ANGLE_TRY(contextVk->getNextSubmitFence(&mFence));
