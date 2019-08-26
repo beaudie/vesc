f9cc59e56b4c06d3cce5c09bd34396bde261ed2b
Mingyu Hu <mihu@microsoft.com>
2019-08-26 15:59:48 -0700

    GL_EXT_multisampled_render_to_texture extension. Part 1.
    For textures that use this extension, a multisampled texture is implicitly created for the texture.
    Upon write or read, the multisampled texture is either return to be drawn to or resolved and
    returned as a single sampled texture.
    
    Adding new parameters for extension without adding any real code change. Since no new code paths were added, we expect all tests to pass as before.
    
    Bug: angleproject:980428
    Change-Id: I551b46a66f422eabd357fd021e00cf266a991efb

--- src/libANGLE/Context.cpp
+++ src/libANGLE/Context.cpp
@@ -5373,6 +5373,14 @@ void Context::renderbufferStorageMultisample(GLenum target,
         renderbuffer->setStorageMultisample(this, samples, convertedInternalFormat, width, height));
 }
 
+void Context::framebufferTexture2DMultisample(GLenum target,
+                                              GLenum attachment,
+                                              GLenum textarget,
+                                              GLuint texture,
+                                              GLint level,
+                                              GLsizei samples)
+{}
+
 void Context::getSynciv(GLsync sync, GLenum pname, GLsizei bufSize, GLsizei *length, GLint *values)
 {
     const Sync *syncObject = nullptr;
