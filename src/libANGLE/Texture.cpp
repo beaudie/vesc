af835c570988e990984fd3da37aab6b26bbdf0b8
Xinyi He <xinyi.he@arm.com>
2019-09-09 16:06:09 +0800

    Vulkan: Mipmap is unconditionally enabled in ANGLE(Clone)
    It clones from 1750627 for test run.
    
    Bug: angleproject:3737
    Change-Id: I3c3d454b0a8a17369741c6753516ff7ffd85f174

--- src/libANGLE/Texture.cpp
+++ src/libANGLE/Texture.cpp
@@ -496,6 +496,26 @@ TextureTarget TextureState::getBaseImageTarget() const
                                          : NonCubeTextureTypeToTarget(mType);
 }
 
+GLuint TextureState::getEnabledLevelCount() const
+{
+    GLuint levelCount      = 0;
+    const GLuint baseLevel = getEffectiveBaseLevel();
+    const GLuint maxLevel  = std::min(getEffectiveMaxLevel(), getMipmapMaxLevel());
+
+    for (size_t descIndex = baseLevel; descIndex < mImageDescs.size();)
+    {
+        if (!mImageDescs[descIndex].size.empty())
+        {
+            levelCount++;
+        }
+        descIndex = (mType == TextureType::CubeMap) ? descIndex + 6 : descIndex + 1;
+    }
+    // The original image already takes account into the levelCount.
+    levelCount = std::min(maxLevel - baseLevel + 1, levelCount);
+
+    return levelCount;
+}
+
 ImageDesc::ImageDesc()
     : ImageDesc(Extents(0, 0, 0), Format::Invalid(), 0, GL_TRUE, InitState::Initialized)
 {}
