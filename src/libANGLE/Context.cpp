d0622cd20955e4576c0c1cac7260b6c6e16e8ee1
Jamie Madill <jmadill@chromium.org>
2019-09-17 10:47:12 -0400

    GLES1: Correct 'active' attributes mask.
    The active attributes are those consumed by the Program. Previously
    we were returning those enabled as client arrays. But this excluded
    default attributes.
    
    Uncovered when changing how resource usage was tracked for Vulkan
    multithreading.
    
    Bug:2464
    Change-Id: I48996be9e4470bb1432e042f98046c95ea8adbfe

--- src/libANGLE/Context.cpp
+++ src/libANGLE/Context.cpp
@@ -9052,7 +9052,7 @@ void StateCache::updateActiveAttribsMask(Context *context)
         return;
     }
 
-    AttributesMask activeAttribs = isGLES1 ? glState.gles1().getVertexArraysAttributeMask()
+    AttributesMask activeAttribs = isGLES1 ? glState.gles1().getActiveAttributesMask()
                                            : glState.getProgram()->getActiveAttribLocationsMask();
 
     const VertexArray *vao = glState.getVertexArray();
