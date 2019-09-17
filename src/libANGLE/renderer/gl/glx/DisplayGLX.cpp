3ecc46d0a0c28c62d960d2114fc5db9d04365749
Jonah Ryan-Davis <jonahr@google.com>
2019-09-17 11:20:47 -0400

    GL: DisplayGLX::makeCurrent should handle drawSurface==0
    In this case the draw surface should be unbound, but ANGLE had been
    skipping this case (and handling it internally). This caused a crash on
    Linux Intel. Now makeCurrent is actually called on the driver with
    drawSurface 0.
    
    Bug: chromium:988440
    Change-Id: Ifae7a828455eb67e516a47515ec6c24cc36e3c2b

--- src/libANGLE/renderer/gl/glx/DisplayGLX.cpp
+++ src/libANGLE/renderer/gl/glx/DisplayGLX.cpp
@@ -380,17 +380,14 @@ egl::Error DisplayGLX::makeCurrent(egl::Surface *drawSurface,
                                    egl::Surface *readSurface,
                                    gl::Context *context)
 {
-    if (drawSurface)
+    glx::Drawable drawable = (drawSurface ? GetImplAs<SurfaceGLX>(drawSurface)->getDrawable() : 0);
+    if (drawable != mCurrentDrawable)
     {
-        glx::Drawable drawable = GetImplAs<SurfaceGLX>(drawSurface)->getDrawable();
-        if (drawable != mCurrentDrawable)
+        if (mGLX.makeCurrent(drawable, mContext) != True)
         {
-            if (mGLX.makeCurrent(drawable, mContext) != True)
-            {
-                return egl::EglContextLost() << "Failed to make the GLX context current";
-            }
-            mCurrentDrawable = drawable;
+            return egl::EglContextLost() << "Failed to make the GLX context current";
         }
+        mCurrentDrawable = drawable;
     }
 
     return DisplayGL::makeCurrent(drawSurface, readSurface, context);
