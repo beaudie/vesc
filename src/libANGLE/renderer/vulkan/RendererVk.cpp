87c3f15107530ecac057a784bddda03842b31450
Jamie Madill <jmadill@chromium.org>
2019-09-19 14:19:14 -0400

    Vulkan: Store current Serial in RendererVk.
    This gives a stronger ordering on serials than if they're acquired by
    the ContextVk. Part of the steps of implementing multithreaded GL on
    Vulkan.
    
    Implements a "globalFinish" method in RendererVk that is triggered on
    ContextVk destruction. This helped fixed some racy object deletion
    situations where the ContextVk could have queued work that uses deleted
    objects. Flush all the Contexts before destruction to avoid these
    hanging deleted objects.
    
    Bug: angleproject:2464
    Change-Id: I244e9bbf6cd47b272c7cbca45b0fb1eb46d626fc

--- src/libANGLE/renderer/vulkan/RendererVk.cpp
+++ src/libANGLE/renderer/vulkan/RendererVk.cpp
@@ -548,6 +548,8 @@ RendererVk::RendererVk()
       mCurrentQueueFamilyIndex(std::numeric_limits<uint32_t>::max()),
       mMaxVertexAttribDivisor(1),
       mDevice(VK_NULL_HANDLE),
+      mLastCompletedQueueSerial(mQueueSerialFactory.generate()),
+      mCurrentQueueSerial(mQueueSerialFactory.generate()),
       mDeviceLost(false),
       mPipelineCacheVkUpdateTimeout(kPipelineCacheVkUpdatePeriod),
       mPipelineCacheDirty(false),
@@ -606,7 +608,8 @@ void RendererVk::onDestroy(vk::Context *context)
 
 void RendererVk::notifyDeviceLost()
 {
-    mDeviceLost = true;
+    mLastCompletedQueueSerial = mLastSubmittedQueueSerial;
+    mDeviceLost               = true;
     mDisplay->notifyDeviceLost();
 }
 
@@ -1547,7 +1550,8 @@ bool RendererVk::hasBufferFormatFeatureBits(VkFormat format, const VkFormatFeatu
 
 angle::Result RendererVk::queueSubmit(vk::Context *context,
                                       const VkSubmitInfo &submitInfo,
-                                      const vk::Fence &fence)
+                                      const vk::Fence &fence,
+                                      Serial *serialOut)
 {
     {
         std::lock_guard<decltype(mQueueMutex)> lock(mQueueMutex);
@@ -1555,6 +1559,10 @@ angle::Result RendererVk::queueSubmit(vk::Context *context,
     }
 
     ANGLE_TRY(cleanupGarbage(context, false));
+
+    *serialOut                = mCurrentQueueSerial;
+    mLastSubmittedQueueSerial = mCurrentQueueSerial;
+    mCurrentQueueSerial       = mQueueSerialFactory.generate();
 
     return angle::Result::Continue;
 }
@@ -1581,11 +1589,6 @@ VkResult RendererVk::queuePresent(const VkPresentInfoKHR &presentInfo)
         ANGLE_TRACE_EVENT0("gpu.angle", "vkQueuePresentKHR");
         return vkQueuePresentKHR(mQueue, &presentInfo);
     }
-}
-
-Serial RendererVk::nextSerial()
-{
-    return mQueueSerialFactory.generate();
 }
 
 angle::Result RendererVk::newSharedFence(vk::Context *context,
@@ -1711,4 +1714,28 @@ uint64_t RendererVk::getMaxFenceWaitTimeNs() const
 #endif
 }
 
+void RendererVk::onCompletedSerial(Serial serial)
+{
+    if (serial > mLastCompletedQueueSerial)
+    {
+        mLastCompletedQueueSerial = serial;
+    }
+}
+
+angle::Result RendererVk::globalFinish()
+{
+    for (gl::Context *context : mDisplay->getContextSet())
+    {
+        ContextVk *contextVk = vk::GetImpl(context);
+        ANGLE_TRY(contextVk->flushImpl(nullptr));
+    }
+
+    for (gl::Context *context : mDisplay->getContextSet())
+    {
+        ContextVk *contextVk = vk::GetImpl(context);
+        ANGLE_TRY(contextVk->finishImpl());
+    }
+
+    return angle::Result::Continue;
+}
 }  // namespace rx
