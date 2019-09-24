501e2cbe128a26b49e47fde52cedc424eeac5b9f
Jamie Madill <jmadill@chromium.org>
2019-09-24 09:11:52 -0400

    Add more test_utils functions.
    Includes methods for creating temporary files, deleting files, and
    reading files into a string. Also renames GetPathSeparator to mention
    it's only used for environment variables. Includes a new virtual type
    angle::Process that will be used to implement cross-platform async
    Process launching for tests. Also includes a way to specify a custom
    crash handler callback.
    
    Bug: angleproject:3162
    Change-Id: I3e2c2e9837608884c98379fa0f78c9ffbe158d73

---
+++ util/test_utils.cpp
@@ -0,0 +1,41 @@
+//
+// Copyright 2019 The ANGLE Project Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+
+// system_utils: Defines common utility functions
+
+#include "util/test_utils.h"
+
+#include <fstream>
+
+namespace angle
+{
+bool CreateTemporaryFile(std::string *tempFileNameOut)
+{
+    std::string tempPath;
+
+    if (!GetTempDir(&tempPath))
+        return false;
+
+    return CreateTemporaryFileInDir(tempPath, tempFileNameOut);
+}
+
+bool ReadEntireFileToString(const std::string &filePath, std::string *contentsOut)
+{
+    std::ifstream stream(filePath.c_str());
+    if (!stream)
+    {
+        return false;
+    }
+
+    stream.seekg(0, std::ios::end);
+    contentsOut->reserve(static_cast<unsigned int>(stream.tellg()));
+    stream.seekg(0, std::ios::beg);
+
+    contentsOut->assign((std::istreambuf_iterator<char>(stream)), std::istreambuf_iterator<char>());
+
+    return true;
+}
+}  // namespace angle
