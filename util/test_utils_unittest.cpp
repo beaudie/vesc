f7f7856cf9f5bba5c82447eb5cac31095373cf5e
Jamie Madill <jmadill@chromium.org>
2019-09-25 18:29:28 -0400

    Rename util/system_utils to util/test_utils.
    This removes a GN naming conflict between util/system_utils and
    common/system_utils. This conflict was preventing us from adding
    unit tests to utils' version of system_utils. Since these functions are
    only useful to tests and samples rename them test_utils for simplicity.
    
    Also moves a couple functions (e.g. RunApp) from common/ to util/ since
    they are only used in test applications. This will reduce binary size
    of our distribution very slightly.
    
    Will enable further development of ANGLE's standalone testing harness.
    
    Bug: angleproject:3162
    Change-Id: I9e34fb69f96c5de6dc2453fce4148a0f285e15ed

---
+++ util/test_utils_unittest.cpp
@@ -0,0 +1,94 @@
+//
+// Copyright 2019 The ANGLE Project Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// test_utils_unittest.cpp: Unit tests for ANGLE's test utility functions
+
+#include "gtest/gtest.h"
+
+#include "common/system_utils.h"
+#include "util/test_utils.h"
+#include "util/test_utils_unittest_helper.h"
+
+using namespace angle;
+
+namespace
+{
+#if defined(ANGLE_PLATFORM_WINDOWS)
+constexpr char kRunAppHelperExecutable[] = "angle_unittests_helper.exe";
+#else
+constexpr char kRunAppHelperExecutable[] = "angle_unittests_helper";
+#endif
+
+// Transforms various line endings into C/Unix line endings:
+//
+// - A\nB -> A\nB
+// - A\rB -> A\nB
+// - A\r\nB -> A\nB
+std::string NormalizeNewLines(const std::string &str)
+{
+    std::string result;
+
+    for (size_t i = 0; i < str.size(); ++i)
+    {
+        if (str[i] == '\r')
+        {
+            if (i + 1 < str.size() && str[i + 1] == '\n')
+            {
+                ++i;
+            }
+            result += '\n';
+        }
+        else
+        {
+            result += str[i];
+        }
+    }
+
+    return result;
+}
+
+// Test running an external application and receiving its output
+TEST(TestUtils, RunApp)
+{
+#if defined(ANGLE_PLATFORM_ANDROID)
+    // TODO: android support. http://anglebug.com/3125
+    return;
+#endif
+
+#if defined(ANGLE_PLATFORM_FUCHSIA)
+    // TODO: fuchsia support. http://anglebug.com/3161
+    return;
+#endif
+
+    std::string executablePath = GetExecutableDirectory();
+    EXPECT_NE(executablePath, "");
+    executablePath += "/";
+    executablePath += kRunAppHelperExecutable;
+
+    std::vector<const char *> args = {executablePath.c_str(), kRunAppTestArg1, kRunAppTestArg2,
+                                      nullptr};
+
+    std::string stdoutOutput;
+    std::string stderrOutput;
+    int exitCode = EXIT_FAILURE;
+
+    // Test that the application can be executed.
+    bool ranApp = RunApp(args, &stdoutOutput, &stderrOutput, &exitCode);
+    EXPECT_TRUE(ranApp);
+    EXPECT_EQ(kRunAppTestStdout, NormalizeNewLines(stdoutOutput));
+    EXPECT_EQ(kRunAppTestStderr, NormalizeNewLines(stderrOutput));
+    EXPECT_EQ(EXIT_SUCCESS, exitCode);
+
+    // Test that environment variables reach the cild.
+    bool setEnvDone = SetEnvironmentVar(kRunAppTestEnvVarName, kRunAppTestEnvVarValue);
+    EXPECT_TRUE(setEnvDone);
+
+    ranApp = RunApp(args, &stdoutOutput, &stderrOutput, &exitCode);
+    EXPECT_TRUE(ranApp);
+    EXPECT_EQ("", stdoutOutput);
+    EXPECT_EQ(kRunAppTestEnvVarValue, NormalizeNewLines(stderrOutput));
+    EXPECT_EQ(EXIT_SUCCESS, exitCode);
+}
+}  // namespace
