89d54025c661d2c92b8b6309baea70b6a420e327
Jamie Madill <jmadill@chromium.org>
2019-09-25 18:29:28 -0400

    Rename util/system_utils to util/test_utils.
    This removes a GN naming conflict between util/system_utils and
    common/system_utils. This conflict was preventing us from adding
    unit tests to utils' version of system_utils. Since these functions are
    only useful to tests and samples rename them test_utils for simplicity.
    
    Also moves a couple functions (e.g. RunApp) from common/ to util/ since
    they are only used in test applications. This will reduce binary size
    of our distribution very slightly.
    
    Will enable further development of ANGLE's standalone testing harness.
    
    Bug: angleproject:3162
    Change-Id: I9e34fb69f96c5de6dc2453fce4148a0f285e15ed

--- src/common/system_utils.h
+++ src/common/system_utils.h
@@ -29,17 +29,6 @@ bool IsDirectory(const char *filename);
 // Get absolute time in seconds.  Use this function to get an absolute time with an unknown origin.
 double GetCurrentTime();
 
-// Run an application and get the output.  Gets a nullptr-terminated set of args to execute the
-// application with, and returns the stdout and stderr outputs as well as the exit code.
-//
-// Pass nullptr for stdoutOut/stderrOut if you don't need to capture. exitCodeOut is required.
-//
-// Returns false if it fails to actually execute the application.
-bool RunApp(const std::vector<const char *> &args,
-            std::string *stdoutOut,
-            std::string *stderrOut,
-            int *exitCodeOut);
-
 class Library : angle::NonCopyable
 {
   public:
