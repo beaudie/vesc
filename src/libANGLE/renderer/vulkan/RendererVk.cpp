da85029e7d87f11484150db87f16444694336b18
Tim Van Patten <timvp@google.com>
2019-10-04 10:13:23 -0600

    Vulkan: Prevent Creating ES 3.0 Context if Min. Caps. Not Met
    ANGLE should not allow creating an ES 3.0 context if the device's
    maxPerStageDescriptorUniformBuffers or maxVertexOutputComponents
    values are too low to support the necessary minimums for GLES 3.0.
    
    Bug: angleproject:3938
    Test: CQ
    Change-Id: I4fce841d1f364c1aef1a883fa5edb64a4b93f08e

--- src/libANGLE/renderer/vulkan/RendererVk.cpp
+++ src/libANGLE/renderer/vulkan/RendererVk.cpp
@@ -1235,6 +1235,21 @@ gl::Version RendererVk::getMaxSupportedESVersion() const
         maxVersion = std::max(maxVersion, gl::Version(2, 0));
     }
 
+    // Limit to GLES 2.0 if maxPerStageDescriptorUniformBuffers is too low.
+    // Table 6.31 MAX_VERTEX_UNIFORM_BLOCKS minimum value = 12
+    // Table 6.32 MAX_FRAGMENT_UNIFORM_BLOCKS minimum value = 12
+    if (mPhysicalDeviceProperties.limits.maxPerStageDescriptorUniformBuffers < 12)
+    {
+        maxVersion = std::max(maxVersion, gl::Version(2, 0));
+    }
+
+    // Limit to GLES 2.0 if maxVertexOutputComponents is too low.
+    // Table 6.31 MAX VERTEX OUTPUT COMPONENTS minimum value = 64
+    if (mPhysicalDeviceProperties.limits.maxVertexOutputComponents < 64)
+    {
+        maxVersion = std::max(maxVersion, gl::Version(2, 0));
+    }
+
     return maxVersion;
 }
 
