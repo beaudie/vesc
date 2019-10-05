d0b275128a1040824e130537fb8d3ddb31f20bb9
Jamie Madill <jmadill@chromium.org>
2019-10-05 10:07:45 -0400

    Vulkan: Add "ImageViewHelper".
    This allows views to track a different lifetime than vk::ImageHelper.
    This in turn will fix the race condition on ContextVk destruction when
    releasing ImageViews owned by TextureVk and RenderbufferVk. For now
    this is a refactoring change only.
    
    Bug: angleproject:####
    Change-Id: I9581975bd5d4913233bbed8439dd4a632cc78a2a

--- src/libANGLE/renderer/vulkan/TextureVk.h
+++ src/libANGLE/renderer/vulkan/TextureVk.h
@@ -158,15 +158,11 @@ class TextureVk : public TextureImpl
     // A special view for cube maps as a 2D array, used with shaders that do texelFetch() and for
     // seamful cube map emulation.
     const vk::ImageView &getFetchImageView() const;
-    angle::Result getLayerLevelDrawImageView(vk::Context *context,
-                                             size_t layer,
-                                             size_t level,
-                                             const vk::ImageView **imageViewOut);
-    angle::Result getLayerLevelStorageImageView(ContextVk *contextVk,
-                                                bool allLayers,
-                                                size_t singleLayer,
-                                                size_t level,
-                                                const vk::ImageView **imageViewOut);
+    angle::Result getStorageImageView(ContextVk *contextVk,
+                                      bool allLayers,
+                                      size_t level,
+                                      size_t singleLayer,
+                                      const vk::ImageView **imageViewOut);
     const vk::Sampler &getSampler() const;
 
     angle::Result ensureImageInitialized(ContextVk *contextVk);
@@ -291,7 +287,6 @@ class TextureVk : public TextureImpl
                             const gl::Extents &extents,
                             const uint32_t levelCount);
     void releaseImage(ContextVk *contextVk);
-    void releaseImageViews(ContextVk *contextVk);
     void releaseStagingBuffer(ContextVk *contextVk);
     uint32_t getLevelCount() const;
     angle::Result initImageViews(ContextVk *contextVk,
@@ -299,11 +294,11 @@ class TextureVk : public TextureImpl
                                  const bool sized,
                                  uint32_t levelCount,
                                  uint32_t layerCount);
+    angle::Result getLevelLayerImageView(vk::Context *context,
+                                         size_t level,
+                                         size_t layer,
+                                         const vk::ImageView **imageViewOut);
     angle::Result initLayerRenderTargets(ContextVk *contextVk, GLuint layerCount);
-    vk::ImageView *getLevelImageViewImpl(vk::ImageViewVector *imageViews, size_t level);
-    vk::ImageView *getLayerLevelImageViewImpl(vk::LayerLevelImageViewVector *imageViews,
-                                              size_t layer,
-                                              size_t level);
 
     angle::Result ensureImageInitializedImpl(ContextVk *contextVk,
                                              const gl::Extents &baseLevelExtents,
@@ -328,17 +323,7 @@ class TextureVk : public TextureImpl
 
     vk::ImageHelper *mImage;
 
-    // Read views.
-    vk::ImageView mReadImageView;
-    vk::ImageView mFetchImageView;
-    vk::ImageView mStencilReadImageView;
-
-    // Draw views.
-    vk::ImageView mDrawImageView;
-    vk::LayerLevelImageViewVector mLayerLevelDrawImageViews;
-
-    // Storage image views.
-    vk::ImageViewVector mLevelStorageImageViews;
+    vk::ImageViewHelper mImageViews;
 
     vk::Sampler mSampler;
     RenderTargetVk mRenderTarget;
