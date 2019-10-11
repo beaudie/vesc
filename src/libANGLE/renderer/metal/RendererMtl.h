75ad24563caf4125724ccca1eeed40c4a8d11200
Le Quyen <lehoangq@gmail.com>
2019-10-11 16:48:35 +0800

    Metal backend implementation pt 2
    This is without Metal specific shader translator implemented yet.
    
    Bug: angleproject:2634
    Change-Id: I95d589442251c9ba111bd05a2dc379a36739046c

--- src/libANGLE/renderer/metal/RendererMtl.h
+++ src/libANGLE/renderer/metal/RendererMtl.h
@@ -12,8 +12,12 @@
 #import <Metal/Metal.h>
 
 #include "common/PackedEnums.h"
-#include "libANGLE/Caps.h"
 #include "libANGLE/angletypes.h"
+#include "libANGLE/renderer/metal/StateCacheMtl.h"
+#include "libANGLE/renderer/metal/UtilsMtl.h"
+#include "libANGLE/renderer/metal/mtl_command_buffer.h"
+#include "libANGLE/renderer/metal/mtl_format_utils.h"
+#include "libANGLE/renderer/metal/mtl_utils.h"
 
 namespace egl
 {
@@ -36,12 +40,63 @@ class RendererMtl final : angle::NonCopyable
 
     std::string getVendorString() const;
     std::string getRendererDescription() const;
+    gl::Caps getNativeCaps() const;
+    const gl::TextureCapsMap &getNativeTextureCaps() const;
+    const gl::Extensions &getNativeExtensions() const;
     const gl::Limitations &getNativeLimitations() const;
 
-    id<MTLDevice> getMetalDevice() const { return nil; }
+    id<MTLDevice> getMetalDevice() const { return mMetalDevice; }
+
+    mtl::CommandQueue &cmdQueue() { return mCmdQueue; }
+    const mtl::FormatTable &getFormatTable() const { return mFormatTable; }
+    UtilsMtl &getUtils() { return mUtils; }
+    StateCacheMtl &getStateCache() { return mStateCache; }
+
+    id<MTLDepthStencilState> getDepthStencilState(const mtl::DepthStencilDesc &desc)
+    {
+        return mStateCache.getDepthStencilState(getMetalDevice(), desc);
+    }
+    id<MTLSamplerState> getSamplerState(const mtl::SamplerDesc &desc)
+    {
+        return mStateCache.getSamplerState(getMetalDevice(), desc);
+    }
+
+    mtl::TextureRef getNullTexture(const gl::Context *context, gl::TextureType type);
+
+    const mtl::Format &getPixelFormat(angle::FormatID angleFormatId) const
+    {
+        return mFormatTable.getPixelFormat(angleFormatId);
+    }
+
+    // See mtl::FormatTable::getVertexFormat()
+    const mtl::VertexFormat &getVertexFormat(angle::FormatID angleFormatId,
+                                             bool tightlyPacked) const
+    {
+        return mFormatTable.getVertexFormat(angleFormatId, tightlyPacked);
+    }
 
   private:
-    gl::Limitations mNativeLimitations;
+    void ensureCapsInitialized() const;
+    void initializeCaps() const;
+    void initializeExtensions() const;
+    void initializeTextureCaps() const;
+
+    mtl::AutoObjCPtr<id<MTLDevice>> mMetalDevice = nil;
+
+    mtl::CommandQueue mCmdQueue;
+
+    mtl::FormatTable mFormatTable;
+    StateCacheMtl mStateCache;
+    UtilsMtl mUtils;
+
+    static const size_t kNumTexturesType = static_cast<size_t>(gl::TextureType::EnumCount);
+    mtl::TextureRef mNullTextures[kNumTexturesType];
+
+    mutable bool mCapsInitialized;
+    mutable gl::TextureCapsMap mNativeTextureCaps;
+    mutable gl::Extensions mNativeExtensions;
+    mutable gl::Caps mNativeCaps;
+    mutable gl::Limitations mNativeLimitations;
 };
 }  // namespace rx
 
