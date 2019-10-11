bedcb200e37df3c943781738fae856de315fbc07
Le Quyen <lehoangq@gmail.com>
2019-10-11 16:48:35 +0800

    Metal backend implementation pt 2
    This is without Metal specific shader translator implemented yet.
    
    Bug: angleproject:2634
    Change-Id: I95d589442251c9ba111bd05a2dc379a36739046c

--- src/libANGLE/renderer/metal/RendererMtl.h
+++ src/libANGLE/renderer/metal/RendererMtl.h
@@ -12,8 +12,12 @@
 #import <Metal/Metal.h>
 
 #include "common/PackedEnums.h"
-#include "libANGLE/Caps.h"
 #include "libANGLE/angletypes.h"
+#include "libANGLE/renderer/metal/mtl_command_buffer.h"
+#include "libANGLE/renderer/metal/mtl_format_utils.h"
+#include "libANGLE/renderer/metal/mtl_render_utils.h"
+#include "libANGLE/renderer/metal/mtl_state_cache.h"
+#include "libANGLE/renderer/metal/mtl_utils.h"
 
 namespace egl
 {
@@ -24,6 +28,18 @@ namespace rx
 {
 
 class ContextMtl;
+
+struct LimitationsMtl : public gl::Limitations
+{
+    // BaseVertex/Instanced draw support:
+    bool hasBaseVertexInstancedDraw = true;
+    // Non-uniform compute shader dispatch support, i.e. Group size is not necessarily to be fixed:
+    bool hasNonUniformDispatch = true;
+    // Texture swizzle support:
+    bool hasTextureSwizzle = false;
+
+    bool allowSeparatedDepthStencilBuffers = false;
+};
 
 class RendererMtl final : angle::NonCopyable
 {
@@ -36,12 +52,64 @@ class RendererMtl final : angle::NonCopyable
 
     std::string getVendorString() const;
     std::string getRendererDescription() const;
-    const gl::Limitations &getNativeLimitations() const;
+    gl::Caps getNativeCaps() const;
+    const gl::TextureCapsMap &getNativeTextureCaps() const;
+    const gl::Extensions &getNativeExtensions() const;
+    const LimitationsMtl &getNativeLimitations() const { return mNativeLimitations; }
 
-    id<MTLDevice> getMetalDevice() const { return nil; }
+    id<MTLDevice> getMetalDevice() const { return mMetalDevice; }
+
+    mtl::CommandQueue &cmdQueue() { return mCmdQueue; }
+    const mtl::FormatTable &getFormatTable() const { return mFormatTable; }
+    mtl::RenderUtils &getUtils() { return mUtils; }
+    mtl::StateCache &getStateCache() { return mStateCache; }
+
+    id<MTLDepthStencilState> getDepthStencilState(const mtl::DepthStencilDesc &desc)
+    {
+        return mStateCache.getDepthStencilState(getMetalDevice(), desc);
+    }
+    id<MTLSamplerState> getSamplerState(const mtl::SamplerDesc &desc)
+    {
+        return mStateCache.getSamplerState(getMetalDevice(), desc);
+    }
+
+    const mtl::TextureRef &getNullTexture(const gl::Context *context, gl::TextureType type);
+
+    const mtl::Format &getPixelFormat(angle::FormatID angleFormatId) const
+    {
+        return mFormatTable.getPixelFormat(angleFormatId);
+    }
+
+    // See mtl::FormatTable::getVertexFormat()
+    const mtl::VertexFormat &getVertexFormat(angle::FormatID angleFormatId,
+                                             bool tightlyPacked) const
+    {
+        return mFormatTable.getVertexFormat(angleFormatId, tightlyPacked);
+    }
 
   private:
-    gl::Limitations mNativeLimitations;
+    void ensureCapsInitialized() const;
+    void initializeCaps() const;
+    void initializeExtensions() const;
+    void initializeTextureCaps() const;
+    void initializeLimitations();
+
+    mtl::AutoObjCPtr<id<MTLDevice>> mMetalDevice = nil;
+
+    mtl::CommandQueue mCmdQueue;
+
+    mtl::FormatTable mFormatTable;
+    mtl::StateCache mStateCache;
+    mtl::RenderUtils mUtils;
+
+    static const size_t kNumTexturesType = static_cast<size_t>(gl::TextureType::EnumCount);
+    mtl::TextureRef mNullTextures[kNumTexturesType];
+
+    mutable bool mCapsInitialized;
+    mutable gl::TextureCapsMap mNativeTextureCaps;
+    mutable gl::Extensions mNativeExtensions;
+    mutable gl::Caps mNativeCaps;
+    LimitationsMtl mNativeLimitations;
 };
 }  // namespace rx
 
