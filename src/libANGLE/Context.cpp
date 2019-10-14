057028d47ffcb23d85b614b71134a919693421c0
Yan <shaobo.yan@intel.com>
2019-10-14 14:13:59 +0800

    Implement SamplerVideoWEBGL for WEBGL_video_texture extension on desktop
    WEBGL_video_texture is an extension that will improve uploading video frame to WebGL performance.
    (https://www.khronos.org/registry/webgl/extensions/proposals/WEBGL_video_texture/)
    
    This extension introduced a new texture type TEXTURE_VIDEO_IMAGE_WEBGL and a new sampler type
    samplerVideoWEBGL to sample it.
    
    In chromium implementation, TEXTURE_VIDEO_IMAGE_WEBGL maps to different native texture type
    based on platform. On desktop, it maps to GL_TEXTURE2D(Currently supported). On Android, it
    should map to GL_TEXTURE_EXTERNAL(TODO). SamplerVideoWEBGL needs to be mapped to sampler2D or
    samplerExternalOES according to TEXTURE_VIDEO_IMAGE_WEBGL implementation.
    
    This patch implements samplerVideoWEBGL in Angle to support WEBGL_video_texture on desktop. In
    this case, samplerVideoWEBGL should map to sampler2D.
    
    Bug: 776222, angleproject:3889
    Change-Id: Idb0a5fcde37ca75ccc1181226b91f257212e7500

--- src/libANGLE/Context.cpp
+++ src/libANGLE/Context.cpp
@@ -441,6 +441,15 @@ void Context::initialize()
         mZeroTextures[TextureType::External].set(this, zeroTextureExternal);
     }
 
+    // This will destroy native 2D image. Ensure state manager aware this when binding
+    // this texture type.
+    if (mSupportedExtensions.webglVideoTexture)
+    {
+        Texture *zeroTextureVideoImage =
+            new Texture(mImplementation.get(), {0}, TextureType::VideoImage);
+        mZeroTextures[TextureType::VideoImage].set(this, zeroTextureVideoImage);
+    }
+
     mState.initializeZeroTextures(this, mZeroTextures);
 
     bindVertexArray({0});
@@ -8853,6 +8862,7 @@ void StateCache::updateValidBindTextureTypes(Context *context)
         {TextureType::External, exts.eglImageExternal || exts.eglStreamConsumerExternal},
         {TextureType::Rectangle, exts.textureRectangle},
         {TextureType::CubeMap, true},
+        {TextureType::VideoImage, exts.webglVideoTexture},
     }};
 }
 
