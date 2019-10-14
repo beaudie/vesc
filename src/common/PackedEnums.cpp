56b5f20619b575accb31393ddb168bf2ea7899ac
Yan <shaobo.yan@intel.com>
2019-10-14 14:13:59 +0800

    Implement SamplerVideoWEBGL for WEBGL_video_texture extension on desktop
    WEBGL_video_texture is an extension that will improve uploading video frame to WebGL performance.
    (https://www.khronos.org/registry/webgl/extensions/proposals/WEBGL_video_texture/)
    
    This extension introduced a new texture type TEXTURE_VIDEO_IMAGE_WEBGL and a new sampler type
    samplerVideoWEBGL to sample it.
    
    In chromium implementation, TEXTURE_VIDEO_IMAGE_WEBGL maps to different native texture type
    based on platform. On desktop, it maps to GL_TEXTURE2D(Currently supported). On Android, it
    should map to GL_TEXTURE_EXTERNAL(TODO). SamplerVideoWEBGL needs to be mapped to sampler2D or
    samplerExternalOES according to TEXTURE_VIDEO_IMAGE_WEBGL implementation.
    
    This patch implements samplerVideoWEBGL in Angle to support WEBGL_video_texture on desktop. In
    this case, samplerVideoWEBGL should map to sampler2D.
    
    Bug: 776222, angleproject:3889
    Change-Id: Idb0a5fcde37ca75ccc1181226b91f257212e7500

--- src/common/PackedEnums.cpp
+++ src/common/PackedEnums.cpp
@@ -38,6 +38,8 @@ TextureType TextureTargetToType(TextureTarget target)
             return TextureType::_2DMultisampleArray;
         case TextureTarget::_3D:
             return TextureType::_3D;
+        case TextureTarget::VideoTexture:
+            return TextureType::VideoTexture;
         case TextureTarget::InvalidEnum:
             return TextureType::InvalidEnum;
         default:
@@ -69,6 +71,8 @@ TextureTarget NonCubeTextureTypeToTarget(TextureType type)
             return TextureTarget::_2DMultisampleArray;
         case TextureType::_3D:
             return TextureTarget::_3D;
+        case TextureType::VideoTexture:
+            return TextureTarget::VideoTexture;
         default:
             UNREACHABLE();
             return TextureTarget::InvalidEnum;
@@ -151,6 +155,9 @@ TextureType SamplerTypeToTextureType(GLenum samplerType)
 
         case GL_SAMPLER_2D_RECT_ANGLE:
             return TextureType::Rectangle;
+
+        case GL_SAMPLER_VIDEO_IMAGE_WEBGL:
+            return TextureType::VideoTexture;
 
         default:
             UNREACHABLE();
