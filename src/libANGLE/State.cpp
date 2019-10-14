1c1dea1af630518b3e4f058cef81069de3207f9a
Yan <shaobo.yan@intel.com>
2019-10-14 14:13:59 +0800

    Implement SamplerVideoWEBGL for WEBGL_video_texture extension on desktop
    WEBGL_video_texture is an extension that will improve uploading video frame to WebGL performance.
    (https://www.khronos.org/registry/webgl/extensions/proposals/WEBGL_video_texture/)
    
    This extension introduced a new texture type TEXTURE_VIDEO_IMAGE_WEBGL and a new sampler type
    samplerVideoWEBGL to sample it.
    
    In chromium implementation, TEXTURE_VIDEO_IMAGE_WEBGL maps to different native texture type
    based on platform. On desktop, it maps to GL_TEXTURE2D(Currently supported). On Android, it
    should map to GL_TEXTURE_EXTERNAL(TODO). SamplerVideoWEBGL needs to be mapped to sampler2D or
    samplerExternalOES according to TEXTURE_VIDEO_IMAGE_WEBGL implementation.
    
    This patch implements samplerVideoWEBGL in Angle to support WEBGL_video_texture on desktop. In
    this case, samplerVideoWEBGL should map to sampler2D.
    
    Bug: 776222
    Change-Id: Idb0a5fcde37ca75ccc1181226b91f257212e7500

--- src/libANGLE/State.cpp
+++ src/libANGLE/State.cpp
@@ -398,6 +398,10 @@ void State::initialize(Context *context)
     {
         mSamplerTextures[TextureType::External].resize(caps.maxCombinedTextureImageUnits);
     }
+    if (nativeExtensions.webglVideoTexture)
+    {
+        mSamplerTextures[TextureType::VideoTexture].resize(caps.maxCombinedTextureImageUnits);
+    }
     mCompleteTextureBindings.reserve(caps.maxCombinedTextureImageUnits);
     for (uint32_t textureIndex = 0; textureIndex < caps.maxCombinedTextureImageUnits;
          ++textureIndex)
