50e69131f4e052ffb9e7c7d30b5aad56efe43399
Jonah Ryan-Davis <jonahr@google.com>
2019-10-25 10:53:40 -0600

    Vulkan support for MacOS (using SwiftShader)
    Created a new WindowSurface/Display for MacOS/Vulkan, along with some
    GN changes to get it working.
    
    Bug: 1015454
    Change-Id: I3f7a12f173795efe598856c702ce53b1e50831eb

---
+++ src/libANGLE/renderer/vulkan/macOS/DisplayVkMacOS.mm
@@ -0,0 +1,57 @@
+//
+// Copyright 2019 The ANGLE Project Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+// DisplayVkMacOS.mm:
+//    Implements methods from DisplayVkMacOS
+//
+
+#include "libANGLE/renderer/vulkan/macOS/DisplayVkMacOS.h"
+
+#include <vulkan/vulkan.h>
+
+#include "libANGLE/renderer/vulkan/macOS/WindowSurfaceVkMacOS.h"
+#include "libANGLE/renderer/vulkan/vk_caps_utils.h"
+
+#import <Cocoa/Cocoa.h>
+
+namespace rx
+{
+
+DisplayVkMacOS::DisplayVkMacOS(const egl::DisplayState &state) : DisplayVk(state) {}
+
+bool DisplayVkMacOS::isValidNativeWindow(EGLNativeWindowType window) const
+{
+    NSObject *layer = reinterpret_cast<NSObject *>(window);
+    return [layer isKindOfClass:[CALayer class]];
+}
+
+SurfaceImpl *DisplayVkMacOS::createWindowSurfaceVk(const egl::SurfaceState &state,
+                                                   EGLNativeWindowType window,
+                                                   EGLint width,
+                                                   EGLint height)
+{
+    ASSERT(isValidNativeWindow(window));
+    return new WindowSurfaceVkMacOS(state, window, width, height);
+}
+
+egl::ConfigSet DisplayVkMacOS::generateConfigs()
+{
+    constexpr GLenum kColorFormats[] = {GL_BGRA8_EXT, GL_BGRX8_ANGLEX};
+    return egl_vk::GenerateConfigs(kColorFormats, egl_vk::kConfigDepthStencilFormats, this);
+}
+
+bool DisplayVkMacOS::checkConfigSupport(egl::Config *config)
+{
+    // TODO(geofflang): Test for native support and modify the config accordingly.
+    // anglebug.com/2692
+    return true;
+}
+
+const char *DisplayVkMacOS::getWSIExtension() const
+{
+    return VK_EXT_METAL_SURFACE_EXTENSION_NAME;
+}
+
+}  // namespace rx
