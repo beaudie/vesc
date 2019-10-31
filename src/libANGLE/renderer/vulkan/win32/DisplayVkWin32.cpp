3e7f84e5bdab8219556789eddfe0961b0def8843
Courtney Goeltzenleuchter <courtneygo@google.com>
2019-10-31 17:18:29 -0600

    Vulkan: Add 565 and validate pixel formats: Windows
    Add support for 565 to Windows. Also includes code that verifies
    generated configs can actually be supported by Vulkan.
    Test:
    --use-angle=swiftshader --deqp-gl-config-name=rgb565d0s0ms0 --gtest_filter=dEQP.GLES3/functional_fragment_ops_interaction_basic_shader_80
    
    Bug: angleproject:4016
    Change-Id: Idcfd4dd368c4a30ef177aa89e1035d2f00570c99

--- src/libANGLE/renderer/vulkan/win32/DisplayVkWin32.cpp
+++ src/libANGLE/renderer/vulkan/win32/DisplayVkWin32.cpp
@@ -8,8 +8,11 @@
 //
 
 #include "libANGLE/renderer/vulkan/win32/DisplayVkWin32.h"
+#include "libANGLE/renderer/vulkan/DisplayVk.h"
+#include "libANGLE/renderer/vulkan/RendererVk.h"
 
 #include <vulkan/vulkan.h>
+#include <windows.h>
 
 #include "libANGLE/renderer/vulkan/vk_caps_utils.h"
 #include "libANGLE/renderer/vulkan/win32/WindowSurfaceVkWin32.h"
@@ -18,6 +21,23 @@ namespace rx
 {
 
 DisplayVkWin32::DisplayVkWin32(const egl::DisplayState &state) : DisplayVk(state) {}
+
+DisplayVkWin32::~DisplayVkWin32() {}
+
+void DisplayVkWin32::terminate()
+{
+    if (mWindowClass)
+    {
+        if (!UnregisterClassA(reinterpret_cast<const char *>(mWindowClass),
+                              GetModuleHandle(nullptr)))
+        {
+            WARN() << "Failed to unregister ANGLE window class: " << gl::FmtHex(mWindowClass);
+        }
+        mWindowClass = NULL;
+    }
+
+    DisplayVk::terminate();
+}
 
 bool DisplayVkWin32::isValidNativeWindow(EGLNativeWindowType window) const
 {
@@ -30,17 +50,105 @@ SurfaceImpl *DisplayVkWin32::createWindowSurfaceVk(const egl::SurfaceState &stat
     return new WindowSurfaceVkWin32(state, window);
 }
 
+egl::Error DisplayVkWin32::initialize(egl::Display *display)
+{
+    ANGLE_TRY(DisplayVk::initialize(display));
+
+    HINSTANCE hinstance = GetModuleHandle(nullptr);
+
+    std::ostringstream stream;
+    stream << "ANGLE DisplayWGL " << gl::FmtHex(display) << " Intermediate Window Class";
+
+    const LPSTR idcArrow  = MAKEINTRESOURCEA(32512);
+    std::string className = stream.str();
+    WNDCLASSA wndClass;
+    if (!GetClassInfoA(hinstance, className.c_str(), &wndClass))
+    {
+        WNDCLASSA intermediateClassDesc     = {};
+        intermediateClassDesc.style         = CS_OWNDC;
+        intermediateClassDesc.lpfnWndProc   = DefWindowProcA;
+        intermediateClassDesc.cbClsExtra    = 0;
+        intermediateClassDesc.cbWndExtra    = 0;
+        intermediateClassDesc.hInstance     = GetModuleHandle(nullptr);
+        intermediateClassDesc.hIcon         = nullptr;
+        intermediateClassDesc.hCursor       = LoadCursorA(nullptr, idcArrow);
+        intermediateClassDesc.hbrBackground = 0;
+        intermediateClassDesc.lpszMenuName  = nullptr;
+        intermediateClassDesc.lpszClassName = className.c_str();
+        mWindowClass                        = RegisterClassA(&intermediateClassDesc);
+        if (!mWindowClass)
+        {
+            return egl::EglNotInitialized()
+                   << "Failed to register intermediate OpenGL window class \"" << className.c_str()
+                   << "\":" << gl::FmtErr(HRESULT_CODE(GetLastError()));
+        }
+    }
+
+    HWND dummyWindow =
+        CreateWindowExA(0, reinterpret_cast<const char *>(mWindowClass), "ANGLE Dummy Window",
+                        WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+                        CW_USEDEFAULT, nullptr, nullptr, nullptr, nullptr);
+    if (!dummyWindow)
+    {
+        return egl::EglNotInitialized() << "Failed to create dummy OpenGL window.";
+    }
+
+    VkSurfaceKHR surfaceVk;
+    VkWin32SurfaceCreateInfoKHR info = {VK_STRUCTURE_TYPE_WIN32_SURFACE_CREATE_INFO_KHR, nullptr, 0,
+                                        GetModuleHandle(nullptr), dummyWindow};
+
+    VkInstance instance         = mRenderer->getInstance();
+    VkPhysicalDevice physDevice = mRenderer->getPhysicalDevice();
+
+    if (vkCreateWin32SurfaceKHR(instance, &info, nullptr, &surfaceVk) == VK_SUCCESS)
+    {
+        uint32_t surfaceFormatCount;
+
+        vkGetPhysicalDeviceSurfaceFormatsKHR(physDevice, surfaceVk, &surfaceFormatCount, nullptr);
+        mSurfaceFormats.resize(surfaceFormatCount);
+        vkGetPhysicalDeviceSurfaceFormatsKHR(physDevice, surfaceVk, &surfaceFormatCount,
+                                             mSurfaceFormats.data());
+        vkDestroySurfaceKHR(instance, surfaceVk, nullptr);
+    }
+    else
+    {
+        return egl::EglNotInitialized() << "vkCreateWin32SurfaceKHR failed";
+    }
+
+    DestroyWindow(dummyWindow);
+    return egl::NoError();
+}
+
 egl::ConfigSet DisplayVkWin32::generateConfigs()
 {
-    constexpr GLenum kColorFormats[] = {GL_BGRA8_EXT, GL_BGRX8_ANGLEX};
-    return egl_vk::GenerateConfigs(kColorFormats, egl_vk::kConfigDepthStencilFormats, this);
+    // If we've already computed the configs, don't need to do it again.
+    if (mConfigs.size() > 0)
+        return mConfigs;
+
+    constexpr GLenum kColorFormats[] = {GL_RGB565, GL_BGRA8_EXT, GL_BGRX8_ANGLEX};
+    mConfigs = egl_vk::GenerateConfigs(kColorFormats, egl_vk::kConfigDepthStencilFormats, this);
+
+    return mConfigs;
 }
 
 bool DisplayVkWin32::checkConfigSupport(egl::Config *config)
 {
-    // TODO(geofflang): Test for native support and modify the config accordingly.
-    // anglebug.com/2692
-    return true;
+    const vk::Format &formatVk = this->getRenderer()->getFormat(config->renderTargetFormat);
+    VkFormat nativeFormat      = formatVk.vkImageFormat;
+    if (mSurfaceFormats.size() == 1u && mSurfaceFormats[0].format == VK_FORMAT_UNDEFINED)
+    {
+        return true;
+    }
+
+    for (const VkSurfaceFormatKHR &surfaceFormat : mSurfaceFormats)
+    {
+        if (surfaceFormat.format == nativeFormat)
+        {
+            return true;
+        }
+    }
+
+    return false;
 }
 
 const char *DisplayVkWin32::getWSIExtension() const
