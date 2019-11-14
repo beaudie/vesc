ffca9f0da9fcf024abba92e47c1ae6c04848c91d
Courtney Goeltzenleuchter <courtneygo@google.com>
2019-11-14 09:04:24 -0700

    Vulkan: Ensure consistent limits for texture & framebuffer
    GLES 3.1 requires that the maximum framebuffer width & height be
    at least as large as the maximum texture size. Vulkan doesn't have
    that requirement and for SwiftShader the values are different
    (4096 for FB, 8192 for texture). Use the smaller of the two to
    keep things consistent.
    
    Test:
      angle_deqp_gles31_tests --use-angle=swiftshader --gtest_filter=dEQP-GLES31.functional.state_query.integer.max_framebuffer_*
    
    Bug: angleproject:4102
    Change-Id: Ie04a5272084c5f6a19ac06d952281b7df9fe9a06

--- src/libANGLE/renderer/vulkan/vk_caps_utils.cpp
+++ src/libANGLE/renderer/vulkan/vk_caps_utils.cpp
@@ -132,7 +132,8 @@ void RendererVk::ensureCapsInitialized() const
     // https://vulkan.lunarg.com/doc/view/1.0.30.0/linux/vkspec.chunked/ch31s02.html
     mNativeCaps.maxElementIndex       = std::numeric_limits<GLuint>::max() - 1;
     mNativeCaps.max3DTextureSize      = limitsVk.maxImageDimension3D;
-    mNativeCaps.max2DTextureSize      = limitsVk.maxImageDimension2D;
+    mNativeCaps.max2DTextureSize =
+        std::min(limitsVk.maxFramebufferWidth, limitsVk.maxImageDimension2D);
     mNativeCaps.maxArrayTextureLayers = limitsVk.maxImageArrayLayers;
     mNativeCaps.maxLODBias            = limitsVk.maxSamplerLodBias;
     mNativeCaps.maxCubeMapTextureSize = limitsVk.maxImageDimensionCube;
@@ -147,8 +148,10 @@ void RendererVk::ensureCapsInitialized() const
 
     mNativeCaps.maxDrawBuffers =
         std::min<uint32_t>(limitsVk.maxColorAttachments, limitsVk.maxFragmentOutputAttachments);
-    mNativeCaps.maxFramebufferWidth    = limitsVk.maxFramebufferWidth;
-    mNativeCaps.maxFramebufferHeight   = limitsVk.maxFramebufferHeight;
+    mNativeCaps.maxFramebufferWidth =
+        std::min(limitsVk.maxFramebufferWidth, limitsVk.maxImageDimension2D);
+    mNativeCaps.maxFramebufferHeight =
+        std::min(limitsVk.maxFramebufferHeight, limitsVk.maxImageDimension2D);
     mNativeCaps.maxColorAttachments    = limitsVk.maxColorAttachments;
     mNativeCaps.maxViewportWidth       = limitsVk.maxViewportDimensions[0];
     mNativeCaps.maxViewportHeight      = limitsVk.maxViewportDimensions[1];
