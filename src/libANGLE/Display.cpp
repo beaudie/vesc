2c3575dc0dd4ca36eccf3a6a3d9cacfc49f277b9
Jonah Ryan-Davis <jonahr@google.com>
2019-11-19 15:19:04 -0500

    Add ability to disable all ANGLE features
    Chrome has a --disable-gpu-driver-bug-workarounds flag that needs to be
    able to be forwarded to ANGLE
    
    Bug: 1016377
    Change-Id: Ied6c8656742e25c32d508b8bfe76a902d82bcf93

--- src/libANGLE/Display.cpp
+++ src/libANGLE/Display.cpp
@@ -565,6 +565,8 @@ void Display::setAttributes(rx::DisplayImpl *impl, const AttributeMap &attribMap
         reinterpret_cast<const char **>(mAttributeMap.get(EGL_FEATURE_OVERRIDES_DISABLED_ANGLE, 0));
     mState.featureOverridesEnabled  = EGLStringArrayToStringVector(featuresForceEnabled);
     mState.featureOverridesDisabled = EGLStringArrayToStringVector(featuresForceDisabled);
+    mState.featuresAllDisabled =
+        static_cast<bool>(mAttributeMap.get(EGL_FEATURE_ALL_DISABLED_ANGLE, 0));
 }
 
 Error Display::initialize()
@@ -610,7 +612,10 @@ Error Display::initialize()
         config.second.renderableType |= EGL_OPENGL_ES_BIT;
     }
 
-    initializeFrontendFeatures();
+    if (!mState.featuresAllDisabled)
+    {
+        initializeFrontendFeatures();
+    }
 
     mFeatures.clear();
     mFrontendFeatures.populateFeatureList(&mFeatures);
