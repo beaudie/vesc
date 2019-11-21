e105084851b3b5e49ed191fb484b1282dbdf07df
Jamie Madill <jmadill@chromium.org>
2019-11-21 11:17:33 -0500

    Posix: Fix async LaunchProcess.
    The prior method was only checking for a PID's existance. We should
    also check for zombie process states. This allows our child process
    launcher to correctly determine when a process is done on Linux and
    other Posix systems.
    
    Also starts the test timer on Posix LaunchProcess implementations.
    
    Also removes the need to end command line arg lists with nullptr.
    
    Also adds a regression test.
    
    Bug: angleproject:3162
    Change-Id: I5725a8a0e8c5151f2d3e06df0a36dd64c0e0ee69

--- util/test_utils_unittest.cpp
+++ util/test_utils_unittest.cpp
@@ -144,8 +144,7 @@ TEST(TestUtils, RunApp)
     executablePath += "/";
     executablePath += kRunAppHelperExecutable;
 
-    std::vector<const char *> args = {executablePath.c_str(), kRunAppTestArg1, kRunAppTestArg2,
-                                      nullptr};
+    std::vector<const char *> args = {executablePath.c_str(), kRunAppTestArg1, kRunAppTestArg2};
 
     // Test that the application can be executed.
     {
@@ -154,12 +153,13 @@ TEST(TestUtils, RunApp)
         EXPECT_TRUE(process->finish());
         EXPECT_TRUE(process->finished());
 
+        EXPECT_GT(process->getElapsedTimeSeconds(), 0.0);
         EXPECT_EQ(kRunAppTestStdout, NormalizeNewLines(process->getStdout()));
         EXPECT_EQ(kRunAppTestStderr, NormalizeNewLines(process->getStderr()));
         EXPECT_EQ(EXIT_SUCCESS, process->getExitCode());
     }
 
-    // Test that environment variables reach the cild.
+    // Test that environment variables reach the child.
     {
         bool setEnvDone = SetEnvironmentVar(kRunAppTestEnvVarName, kRunAppTestEnvVarValue);
         EXPECT_TRUE(setEnvDone);
@@ -168,8 +168,54 @@ TEST(TestUtils, RunApp)
         EXPECT_TRUE(process->started());
         EXPECT_TRUE(process->finish());
 
+        EXPECT_GT(process->getElapsedTimeSeconds(), 0.0);
         EXPECT_EQ("", process->getStdout());
         EXPECT_EQ(kRunAppTestEnvVarValue, NormalizeNewLines(process->getStderr()));
+        EXPECT_EQ(EXIT_SUCCESS, process->getExitCode());
+
+        // Unset environment var.
+        SetEnvironmentVar(kRunAppTestEnvVarName, "");
+    }
+}
+
+// Test running an external application and receiving its output asynchronously.
+TEST(TestUtils, RunAppAsync)
+{
+#if defined(ANGLE_PLATFORM_ANDROID)
+    // TODO: android support. http://anglebug.com/3125
+    return;
+#endif
+
+#if defined(ANGLE_PLATFORM_FUCHSIA)
+    // TODO: fuchsia support. http://anglebug.com/3161
+    return;
+#endif
+
+    std::string executablePath = GetExecutableDirectory();
+    EXPECT_NE(executablePath, "");
+    executablePath += "/";
+    executablePath += kRunAppHelperExecutable;
+
+    std::vector<const char *> args = {executablePath.c_str(), kRunAppTestArg1, kRunAppTestArg2};
+
+    // Test that the application can be executed.
+    {
+        ProcessHandle process(args, true, true);
+        EXPECT_TRUE(process->started());
+
+        constexpr double kTimeout = 3.0;
+
+        Timer timer;
+        timer.start();
+        while (!process->finished() && timer.getElapsedTime() < kTimeout)
+        {
+            Sleep(1);
+        }
+
+        EXPECT_TRUE(process->finished());
+        EXPECT_GT(process->getElapsedTimeSeconds(), 0.0);
+        EXPECT_EQ(kRunAppTestStdout, NormalizeNewLines(process->getStdout()));
+        EXPECT_EQ(kRunAppTestStderr, NormalizeNewLines(process->getStderr()));
         EXPECT_EQ(EXIT_SUCCESS, process->getExitCode());
     }
 }
