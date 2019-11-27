7ff9a75e143234ee451128758d093f062c65fff2
Courtney Goeltzenleuchter <courtneygo@google.com>
2019-11-27 09:21:45 -0700

    Add Compute Shared Memory Size Validation
    Add tracking of shared memory declarations in compute shaders.
    
    Test:
      angle_deqp_gles31_tests --gtest_filter=dEQP.GLES31/functional_debug_negative_coverage_callbacks_compute_exceed_shared_memory_size_limit
    
    Bug: 4173
    Change-Id: If2a86d467a82f73fa5b2ee0ced752701acfe1872

--- src/compiler/translator/CollectVariables.cpp
+++ src/compiler/translator/CollectVariables.cpp
@@ -100,7 +100,7 @@ ShaderVariable *FindVariableInInterfaceBlock(const ImmutableString &name,
 }
 
 // Traverses the intermediate tree to collect all attributes, uniforms, varyings, fragment outputs,
-// and interface blocks.
+// shared data and interface blocks.
 class CollectVariablesTraverser : public TIntermTraverser
 {
   public:
@@ -109,6 +109,7 @@ class CollectVariablesTraverser : public TIntermTraverser
                               std::vector<ShaderVariable> *uniforms,
                               std::vector<ShaderVariable> *inputVaryings,
                               std::vector<ShaderVariable> *outputVaryings,
+                              std::vector<ShaderVariable> *sharedVariables,
                               std::vector<InterfaceBlock> *uniformBlocks,
                               std::vector<InterfaceBlock> *shaderStorageBlocks,
                               std::vector<InterfaceBlock> *inBlocks,
@@ -159,6 +160,7 @@ class CollectVariablesTraverser : public TIntermTraverser
     std::vector<ShaderVariable> *mUniforms;
     std::vector<ShaderVariable> *mInputVaryings;
     std::vector<ShaderVariable> *mOutputVaryings;
+    std::vector<ShaderVariable> *mSharedVariables;
     std::vector<InterfaceBlock> *mUniformBlocks;
     std::vector<InterfaceBlock> *mShaderStorageBlocks;
     std::vector<InterfaceBlock> *mInBlocks;
@@ -208,6 +210,9 @@ class CollectVariablesTraverser : public TIntermTraverser
     bool mPrimitiveIDAdded;
     bool mLayerAdded;
 
+    // Shared memory variables
+    bool mSharedVariableAdded;
+
     ShHashFunction64 mHashFunction;
 
     GLenum mShaderType;
@@ -220,6 +225,7 @@ CollectVariablesTraverser::CollectVariablesTraverser(
     std::vector<sh::ShaderVariable> *uniforms,
     std::vector<sh::ShaderVariable> *inputVaryings,
     std::vector<sh::ShaderVariable> *outputVaryings,
+    std::vector<sh::ShaderVariable> *sharedVariables,
     std::vector<sh::InterfaceBlock> *uniformBlocks,
     std::vector<sh::InterfaceBlock> *shaderStorageBlocks,
     std::vector<sh::InterfaceBlock> *inBlocks,
@@ -233,6 +239,7 @@ CollectVariablesTraverser::CollectVariablesTraverser(
       mUniforms(uniforms),
       mInputVaryings(inputVaryings),
       mOutputVaryings(outputVaryings),
+      mSharedVariables(sharedVariables),
       mUniformBlocks(uniformBlocks),
       mShaderStorageBlocks(shaderStorageBlocks),
       mInBlocks(inBlocks),
@@ -265,6 +272,7 @@ CollectVariablesTraverser::CollectVariablesTraverser(
       mInvocationIDAdded(false),
       mPrimitiveIDAdded(false),
       mLayerAdded(false),
+      mSharedVariableAdded(false),
       mHashFunction(hashFunction),
       mShaderType(shaderType),
       mExtensionBehavior(extensionBehavior)
@@ -597,6 +605,13 @@ void CollectVariablesTraverser::visitSymbol(TIntermSymbol *symbol)
                     ASSERT(mShaderType == GL_VERTEX_SHADER &&
                            (IsExtensionEnabled(mExtensionBehavior, TExtension::OVR_multiview2) ||
                             IsExtensionEnabled(mExtensionBehavior, TExtension::OVR_multiview)));
+                }
+                break;
+            case EvqShared:
+                if (mShaderType == GL_COMPUTE_SHADER)
+                {
+                    recordBuiltInVaryingUsed(symbol->variable(), &mSharedVariableAdded,
+                                             mSharedVariables);
                 }
                 break;
             default:
@@ -984,6 +999,7 @@ void CollectVariables(TIntermBlock *root,
                       std::vector<ShaderVariable> *uniforms,
                       std::vector<ShaderVariable> *inputVaryings,
                       std::vector<ShaderVariable> *outputVaryings,
+                      std::vector<ShaderVariable> *sharedVariables,
                       std::vector<InterfaceBlock> *uniformBlocks,
                       std::vector<InterfaceBlock> *shaderStorageBlocks,
                       std::vector<InterfaceBlock> *inBlocks,
@@ -993,8 +1009,9 @@ void CollectVariables(TIntermBlock *root,
                       const TExtensionBehavior &extensionBehavior)
 {
     CollectVariablesTraverser collect(attributes, outputVariables, uniforms, inputVaryings,
-                                      outputVaryings, uniformBlocks, shaderStorageBlocks, inBlocks,
-                                      hashFunction, symbolTable, shaderType, extensionBehavior);
+                                      outputVaryings, sharedVariables, uniformBlocks,
+                                      shaderStorageBlocks, inBlocks, hashFunction, symbolTable,
+                                      shaderType, extensionBehavior);
     root->traverse(&collect);
 }
 
--- src/compiler/translator/ShaderLang.cpp
+++ src/compiler/translator/ShaderLang.cpp
@@ -671,4 +671,17 @@ int GetGeometryShaderMaxVertices(const ShHandle handle)
     return maxVertices;
 }
 
+unsigned int GetShaderSharedMemorySize(const ShHandle handle)
+{
+    ASSERT(handle);
+
+    TShHandleBase *base = static_cast<TShHandleBase *>(handle);
+    TCompiler *compiler = base->getAsCompiler();
+    ASSERT(compiler);
+
+    unsigned int sharedMemorySize = compiler->getSharedMemorySize();
+    ASSERT(sharedMemorySize >= 0);
+    return sharedMemorySize;
+}
+
 }  // namespace sh
