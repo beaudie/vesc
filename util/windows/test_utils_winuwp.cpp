8f135b41eb65a28c86dd19011485dbc968e5ebe4
Jamie Madill <jmadill@chromium.org>
2019-12-03 11:05:38 -0500

    WIP: Improve UWP build.
    Bug: None
    Change-Id: I49dddfcdc0118b11466fe171f949c28d101ac6a2

---
+++ util/windows/test_utils_winuwp.cpp
@@ -0,0 +1,26 @@
+//
+// Copyright 2019 The ANGLE Project Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+// test_utils_uwp.cpp: Implementation of test utility functions for WinUWP.
+
+#include "util/test_utils.h"
+
+namespace angle
+{
+void PrintStackBacktrace()
+{
+    // Not available on UWP
+}
+
+void InitCrashHandler(CrashCallback *callback)
+{
+    // Not available on UWP
+}
+
+void TerminateCrashHandler()
+{
+    // Not available on UWP
+}
+}  // namespace angle
