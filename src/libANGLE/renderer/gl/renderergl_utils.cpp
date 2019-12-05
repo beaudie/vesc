ac48fdbfc89a2872c998c500e216374f5da889ee
Jonah Ryan-Davis <jonahr@google.com>
2019-12-05 12:59:13 -0500

    Also use GL_RENDERER to determine vendorID
    ~10% or more Linux users* do not have AMD/ATI in the GL_VENDOR,
    only in GL_RENDERER. Our heuristics only checked GL_VENDOR, so
    this CL adds GL_RENDERER to the string being checked to find the
    vendor ID.
    (*According to https://feedback.wildfiregames.com/report/opengl/feature/GL_VENDOR)
    
    Bug: angleproject:4204
    Change-Id: Ie89080b4431fdf02a4ca09709973ea3f271a3e84

--- src/libANGLE/renderer/gl/renderergl_utils.cpp
+++ src/libANGLE/renderer/gl/renderergl_utils.cpp
@@ -38,11 +38,11 @@ namespace rx
 VendorID GetVendorID(const FunctionsGL *functions)
 {
     std::string nativeVendorString(reinterpret_cast<const char *>(functions->getString(GL_VENDOR)));
-    if (nativeVendorString.find("Intel") != std::string::npos)
-    {
-        return VENDOR_ID_INTEL;
-    }
-    else if (nativeVendorString.find("NVIDIA") != std::string::npos)
+    // Concatenate GL_RENDERER to the string being checked because some vendors put their names in
+    // GL_RENDERER
+    nativeVendorString +=
+        " " + std::string(reinterpret_cast<const char *>(functions->getString(GL_RENDERER)));
+    if (nativeVendorString.find("NVIDIA") != std::string::npos)
     {
         return VENDOR_ID_NVIDIA;
     }
@@ -54,6 +54,10 @@ VendorID GetVendorID(const FunctionsGL *functions)
     else if (nativeVendorString.find("Qualcomm") != std::string::npos)
     {
         return VENDOR_ID_QUALCOMM;
+    }
+    else if (nativeVendorString.find("Intel") != std::string::npos)
+    {
+        return VENDOR_ID_INTEL;
     }
     else
     {
@@ -1113,7 +1117,7 @@ void GenerateCaps(const FunctionsGL *functions,
                                     functions->hasGLExtension("GL_ARB_pixel_buffer_object") ||
                                     functions->hasGLExtension("GL_EXT_pixel_buffer_object") ||
                                     functions->hasGLESExtension("GL_NV_pixel_buffer_object");
-    extensions->glSync = nativegl::SupportsFenceSync(functions);
+    extensions->glSync    = nativegl::SupportsFenceSync(functions);
     extensions->mapBuffer = functions->isAtLeastGL(gl::Version(1, 5)) ||
                             functions->isAtLeastGLES(gl::Version(3, 0)) ||
                             functions->hasGLESExtension("GL_OES_mapbuffer");
