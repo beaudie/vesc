9259d4e3b351b5f9d74b863d1e37e9aa46f88da4
Eric Binet <ericbinet@google.com>
2020-01-06 14:59:44 -0500

    Fix colored indexed point tests not using color in vertex buffer
    In the previous version, the IndexedPointsTestUInt tests named
    VertexWithColorUnsignedIntOffset* were redundant with UnsignedIntOffset*.
    
    Bug: angleproject:3923
    Change-Id: I6b9b8f2bd78882bcac7c3b89e798291ee94eea70

--- src/tests/gl_tests/IndexedPointsTest.cpp
+++ src/tests/gl_tests/IndexedPointsTest.cpp
@@ -398,7 +398,7 @@ TEST_P(IndexedPointsTestUInt, VertexWithColorUnsignedIntOffset0)
     }
     // http://anglebug.com/4092
     ANGLE_SKIP_TEST_IF(isSwiftshader());
-    runTest(0, false);
+    runTest(0, true);
 }
 
 TEST_P(IndexedPointsTestUInt, VertexWithColorUnsignedIntOffset1)
@@ -409,7 +409,7 @@ TEST_P(IndexedPointsTestUInt, VertexWithColorUnsignedIntOffset1)
     }
     // http://anglebug.com/4092
     ANGLE_SKIP_TEST_IF(isSwiftshader());
-    runTest(1, false);
+    runTest(1, true);
 }
 
 TEST_P(IndexedPointsTestUInt, VertexWithColorUnsignedIntOffset2)
@@ -420,7 +420,7 @@ TEST_P(IndexedPointsTestUInt, VertexWithColorUnsignedIntOffset2)
     }
     // http://anglebug.com/4092
     ANGLE_SKIP_TEST_IF(isSwiftshader());
-    runTest(2, false);
+    runTest(2, true);
 }
 
 TEST_P(IndexedPointsTestUInt, VertexWithColorUnsignedIntOffset3)
@@ -431,7 +431,7 @@ TEST_P(IndexedPointsTestUInt, VertexWithColorUnsignedIntOffset3)
     }
     // http://anglebug.com/4092
     ANGLE_SKIP_TEST_IF(isSwiftshader());
-    runTest(3, false);
+    runTest(3, true);
 }
 
 // TODO(lucferron): Diagnose and fix the UByte tests below for Vulkan.
