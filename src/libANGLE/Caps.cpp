d1b1b028aa5260a2bc15720ba264a7f70cf2bfd4
Alexis Hetu <sugoi@google.com>
2020-01-14 10:35:01 -0500

    GL_EXT_float_blend support
    DetermineFloatBlendSupport was added to be able to set floatBlend to
    true if requirements are met. Essentially GL_EXT_float_blend complements
    GL_EXT_color_buffer_float by making any non-blendable formats blendable.
    
    Bug: angleproject:4291
    Change-Id: I40d6bb7a93b8847a63de2d25c6a8dc411d6d8fbb

--- src/libANGLE/Caps.cpp
+++ src/libANGLE/Caps.cpp
@@ -652,6 +652,18 @@ static bool DetermineColorBufferFloatSupport(const TextureCapsMap &textureCaps)
            GetFormatSupport(textureCaps, blendableFormats, true, false, true, true, true);
 }
 
+// Check for GL_EXT_float_blend
+static bool DetermineFloatBlendSupport(const TextureCapsMap &textureCaps)
+{
+    constexpr GLenum requiredFormats[] = {
+        GL_R32F,
+        GL_RG32F,
+        GL_RGBA32F,
+    };
+
+    return GetFormatSupport(textureCaps, requiredFormats, true, false, true, true, true);
+}
+
 // Check for GL_EXT_texture_norm16
 static bool DetermineTextureNorm16Support(const TextureCapsMap &textureCaps)
 {
@@ -758,6 +770,7 @@ void Extensions::setTextureExtensionSupport(const TextureCapsMap &textureCaps)
     colorBufferFloatRGB              = DetermineColorBufferFloatRGBSupport(textureCaps);
     colorBufferFloatRGBA             = DetermineColorBufferFloatRGBASupport(textureCaps);
     colorBufferFloat                 = DetermineColorBufferFloatSupport(textureCaps);
+    floatBlend                       = DetermineFloatBlendSupport(textureCaps);
     textureNorm16                    = DetermineTextureNorm16Support(textureCaps);
     textureCompressionBPTC           = DetermineBPTCTextureSupport(textureCaps);
     compressedTexturePVRTC           = DeterminePVRTCTextureSupport(textureCaps);
