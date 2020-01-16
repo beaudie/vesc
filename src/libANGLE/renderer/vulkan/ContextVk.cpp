14f76ff60cf69e7ab0b1a456a42d5c45ac56e023
Jamie Madill <jmadill@chromium.org>
2020-01-16 11:27:58 -0500

    Vulkan: Make ContextVk own ResourceUseList.
    This moves the resource use tracking functionality out of CommandGraph.
    Making the list a separate class helps the implementation avoid tricky
    circular include problems.
    
    Bug: angleproject:4029
    Change-Id: I3288fc685b21e949f12b0796109a2b7bb117c249

--- src/libANGLE/renderer/vulkan/ContextVk.cpp
+++ src/libANGLE/renderer/vulkan/ContextVk.cpp
@@ -642,7 +642,7 @@ void ContextVk::onDestroy(const gl::Context *context)
 
     mCommandQueue.destroy(device);
 
-    mCommandGraph.getResourceUseList().releaseResourceUses();
+    mResourceUseList.releaseResourceUses();
 
     mUtils.destroy(device);
 
@@ -803,8 +803,7 @@ angle::Result ContextVk::setupDraw(const gl::Context *context,
         mGraphicsDirtyBits |= mNewGraphicsCommandBufferDirtyBits;
 
         gl::Rectangle scissoredRenderArea = mDrawFramebuffer->getScissoredRenderArea(this);
-        if (!mDrawFramebuffer->appendToStartedRenderPass(&mCommandGraph.getResourceUseList(),
-                                                         scissoredRenderArea,
+        if (!mDrawFramebuffer->appendToStartedRenderPass(&mResourceUseList, scissoredRenderArea,
                                                          &mRenderPassCommandBuffer))
         {
             ANGLE_TRY(mDrawFramebuffer->startNewRenderPass(this, scissoredRenderArea,
@@ -3128,13 +3127,13 @@ angle::Result ContextVk::updateActiveTextures(const gl::Context *context,
         {
             samplerVk     = nullptr;
             samplerSerial = kZeroSerial;
-            textureVk->onSamplerGraphAccess(&mCommandGraph.getResourceUseList());
+            textureVk->onSamplerGraphAccess(&mResourceUseList);
         }
         else
         {
             samplerVk     = vk::GetImpl(sampler);
             samplerSerial = samplerVk->getSerial();
-            samplerVk->onSamplerGraphAccess(&mCommandGraph.getResourceUseList());
+            samplerVk->onSamplerGraphAccess(&mResourceUseList);
         }
 
         vk::ImageHelper &image = textureVk->getImage();
@@ -3160,7 +3159,7 @@ angle::Result ContextVk::updateActiveTextures(const gl::Context *context,
             image.changeLayout(aspectFlags, textureLayout, srcLayoutChange);
         }
 
-        textureVk->onImageViewGraphAccess(&mCommandGraph.getResourceUseList());
+        textureVk->onImageViewGraphAccess(&mResourceUseList);
         image.addReadDependency(this, recorder);
 
         mActiveTextures[textureUnit].texture = textureVk;
