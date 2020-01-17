cbe8a11642ee10e2a2d75190fa318e2bcd88bb25
Kenneth Russell <kbr@chromium.org>
2020-01-17 13:11:20 -0800

    Add driver bug workaround for rewriting row major matrices.
    Hook up previously written RewriteRowMajorMatrices transformation, and
    enable on all GPU types on macOS. It is needed at least for AMD and
    Intel GPUs.
    
    Add a new test which verifies the behavior of dynamically-indexed
    arrays of row-major matrices.
    
    Bug: angleproject:2273
    Change-Id: Id582f9cf6b9b1a59091aab1786539174f360b705

--- src/tests/gl_tests/GLSLTest.cpp
+++ src/tests/gl_tests/GLSLTest.cpp
@@ -7368,7 +7368,8 @@ TEST_P(GLSLTest_ES3, MixedRowAndColumnMajorMatrices_ReadSideEffectOrder)
     // http://anglebug.com/3837
     ANGLE_SKIP_TEST_IF(IsLinux() && IsIntel() && IsOpenGL());
 
-    // Fails on Mac on Intel and AMD: http://anglebug.com/3842
+    // IntermTraverser::insertStatementsInParentBlock that's used to move side effects does not
+    // respect the order of evaluation of logical expressions.  http://anglebug.com/3829.
     ANGLE_SKIP_TEST_IF(IsOSX() && IsOpenGL() && (IsIntel() || IsAMD()));
 
     constexpr char kFS[] = R"(#version 300 es
@@ -7429,7 +7430,8 @@ TEST_P(GLSLTest_ES3, MixedRowAndColumnMajorMatrices_ReadSideEffectShortCircuit)
     // Fails on Android: http://anglebug.com/3839
     ANGLE_SKIP_TEST_IF(IsAndroid() && IsOpenGL());
 
-    // Fails on Mac on Intel and AMD: http://anglebug.com/3842
+    // IntermTraverser::insertStatementsInParentBlock that's used to move side effects does not
+    // respect the order of evaluation of logical expressions.  http://anglebug.com/3829.
     ANGLE_SKIP_TEST_IF(IsOSX() && IsOpenGL() && (IsIntel() || IsAMD()));
 
     // Fails on Mac on Nvidia: http://anglebug.com/3843
