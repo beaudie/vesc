5ef6899f666ea5a34994671aab461e9adc208f20
Courtney Goeltzenleuchter <courtneygo@google.com>
2020-01-24 16:09:15 -0700

    Vulkan: Validate pixel formats before put in configs
    With the newly available AHardwareBuffer_isSupported call we can
    now determine if a specific pixel format is supported or not.
    Expanded the list to include all interesting formats and only
    generate configs for those that are supported by the platform.
    
    Bug: angleproject:4062
    Change-Id: If8be27cb18f1f911d38ba44a1679ce4f93a1aabb

--- src/common/android_util.cpp
+++ src/common/android_util.cpp
@@ -9,6 +9,7 @@
 #include "common/android_util.h"
 #include "common/debug.h"
 
+#include <android/hardware_buffer.h>
 #include <cstdint>
 
 // Taken from cutils/native_handle.h:
@@ -54,122 +55,6 @@ typedef struct ANativeWindowBuffer
     // architecture.
     void *reserved_proc[8 - (sizeof(uint64_t) / sizeof(void *))];
 } ANativeWindowBuffer_t;
-
-// Taken from android/hardware_buffer.h
-// https://android.googlesource.com/platform/frameworks/native/+/master/libs/nativewindow/include/android/hardware_buffer.h
-
-// AHARDWAREBUFFER_FORMAT_B8G8R8A8_UNORM AHARDWAREBUFFER_FORMAT_B4G4R4A4_UNORM,
-// AHARDWAREBUFFER_FORMAT_B5G5R5A1_UNORM formats were deprecated and re-added explicitly.
-
-// clang-format off
-/**
- * Buffer pixel formats.
- */
-enum {
-    /**
-     * Corresponding formats:
-     *   Vulkan: VK_FORMAT_R8G8B8A8_UNORM
-     *   OpenGL ES: GL_RGBA8
-     */
-    AHARDWAREBUFFER_FORMAT_R8G8B8A8_UNORM           = 1,
-
-    /**
-     * 32 bits per pixel, 8 bits per channel format where alpha values are
-     * ignored (always opaque).
-     * Corresponding formats:
-     *   Vulkan: VK_FORMAT_R8G8B8A8_UNORM
-     *   OpenGL ES: GL_RGB8
-     */
-    AHARDWAREBUFFER_FORMAT_R8G8B8X8_UNORM           = 2,
-
-    /**
-     * Corresponding formats:
-     *   Vulkan: VK_FORMAT_R8G8B8_UNORM
-     *   OpenGL ES: GL_RGB8
-     */
-    AHARDWAREBUFFER_FORMAT_R8G8B8_UNORM             = 3,
-
-    /**
-     * Corresponding formats:
-     *   Vulkan: VK_FORMAT_R5G6B5_UNORM_PACK16
-     *   OpenGL ES: GL_RGB565
-     */
-    AHARDWAREBUFFER_FORMAT_R5G6B5_UNORM             = 4,
-
-    AHARDWAREBUFFER_FORMAT_B8G8R8A8_UNORM           = 5,
-    AHARDWAREBUFFER_FORMAT_B5G5R5A1_UNORM           = 6,
-    AHARDWAREBUFFER_FORMAT_B4G4R4A4_UNORM           = 7,
-    /**
-     * Corresponding formats:
-     *   Vulkan: VK_FORMAT_R16G16B16A16_SFLOAT
-     *   OpenGL ES: GL_RGBA16F
-     */
-    AHARDWAREBUFFER_FORMAT_R16G16B16A16_FLOAT       = 0x16,
-
-    /**
-     * Corresponding formats:
-     *   Vulkan: VK_FORMAT_A2B10G10R10_UNORM_PACK32
-     *   OpenGL ES: GL_RGB10_A2
-     */
-    AHARDWAREBUFFER_FORMAT_R10G10B10A2_UNORM        = 0x2b,
-
-    /**
-     * An opaque binary blob format that must have height 1, with width equal to
-     * the buffer size in bytes.
-     */
-    AHARDWAREBUFFER_FORMAT_BLOB                     = 0x21,
-
-    /**
-     * Corresponding formats:
-     *   Vulkan: VK_FORMAT_D16_UNORM
-     *   OpenGL ES: GL_DEPTH_COMPONENT16
-     */
-    AHARDWAREBUFFER_FORMAT_D16_UNORM                = 0x30,
-
-    /**
-     * Corresponding formats:
-     *   Vulkan: VK_FORMAT_X8_D24_UNORM_PACK32
-     *   OpenGL ES: GL_DEPTH_COMPONENT24
-     */
-    AHARDWAREBUFFER_FORMAT_D24_UNORM                = 0x31,
-
-    /**
-     * Corresponding formats:
-     *   Vulkan: VK_FORMAT_D24_UNORM_S8_UINT
-     *   OpenGL ES: GL_DEPTH24_STENCIL8
-     */
-    AHARDWAREBUFFER_FORMAT_D24_UNORM_S8_UINT        = 0x32,
-
-    /**
-     * Corresponding formats:
-     *   Vulkan: VK_FORMAT_D32_SFLOAT
-     *   OpenGL ES: GL_DEPTH_COMPONENT32F
-     */
-    AHARDWAREBUFFER_FORMAT_D32_FLOAT                = 0x33,
-
-    /**
-     * Corresponding formats:
-     *   Vulkan: VK_FORMAT_D32_SFLOAT_S8_UINT
-     *   OpenGL ES: GL_DEPTH32F_STENCIL8
-     */
-    AHARDWAREBUFFER_FORMAT_D32_FLOAT_S8_UINT        = 0x34,
-
-    /**
-     * Corresponding formats:
-     *   Vulkan: VK_FORMAT_S8_UINT
-     *   OpenGL ES: GL_STENCIL_INDEX8
-     */
-    AHARDWAREBUFFER_FORMAT_S8_UINT                  = 0x35,
-
-    /**
-     * YUV 420 888 format.
-     * Must have an even width and height. Can be accessed in OpenGL
-     * shaders through an external sampler. Does not support mip-maps
-     * cube-maps or multi-layered textures.
-     */
-    AHARDWAREBUFFER_FORMAT_Y8Cb8Cr8_420             = 0x23,
-};
-// clang-format on
 
 namespace
 {
@@ -232,12 +117,6 @@ GLenum NativePixelFormatToGLInternalFormat(int pixelFormat)
             return GL_RGB8;
         case AHARDWAREBUFFER_FORMAT_R5G6B5_UNORM:
             return GL_RGB565;
-        case AHARDWAREBUFFER_FORMAT_B8G8R8A8_UNORM:
-            return GL_BGRA8_EXT;
-        case AHARDWAREBUFFER_FORMAT_B5G5R5A1_UNORM:
-            return GL_RGB5_A1;
-        case AHARDWAREBUFFER_FORMAT_B4G4R4A4_UNORM:
-            return GL_RGBA4;
         case AHARDWAREBUFFER_FORMAT_R16G16B16A16_FLOAT:
             return GL_RGBA16F;
         case AHARDWAREBUFFER_FORMAT_R10G10B10A2_UNORM:
@@ -276,6 +155,18 @@ EGLClientBuffer AHardwareBufferToClientBuffer(const AHardwareBuffer *hardwareBuf
 {
     return offsetPointer<EGLClientBuffer>(hardwareBuffer,
                                           kAHardwareBufferToANativeWindowBufferOffset);
+}
+
+int AHardwareBufferFormatIsSupported(uint32_t format)
+{
+    AHardwareBuffer_Desc desc = {};
+    desc.format               = format;
+    desc.width                = 1;
+    desc.height               = 1;
+    desc.layers               = 1;
+    desc.usage = AHARDWAREBUFFER_USAGE_GPU_SAMPLED_IMAGE | AHARDWAREBUFFER_USAGE_GPU_FRAMEBUFFER;
+
+    return AHardwareBuffer_isSupported(&desc);
 }
 
 }  // namespace android
