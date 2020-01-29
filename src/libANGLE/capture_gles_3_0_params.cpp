3b3e46065d23b3d684ec95b0ce779d2029c5b8d8
Cody Northrop <cnorthrop@google.com>
2020-01-29 16:21:05 -0700

    Capture/Replay: Implement Manhattan ES 3.0 support
    Populate the entrypoints required to get gfxBench
    Manhattan scene to capture and replay correctly from
    the beginning of the trace.
    
    Test: Captured and replayed Manhattan frames 0-500 on
    both Windows and Linux (Nvidia GPUs)
    
    Bug: angleproject:4091
    Change-Id: I5447a6835e55e944772d37219f2bd414606f0a70

--- src/libANGLE/capture_gles_3_0_params.cpp
+++ src/libANGLE/capture_gles_3_0_params.cpp
@@ -6,6 +6,7 @@
 // capture_gles3_params.cpp:
 //   Pointer parameter capture functions for the OpenGL ES 3.0 entry points.
 
+#include "libANGLE/capture_gles_2_0_autogen.h"
 #include "libANGLE/capture_gles_3_0_autogen.h"
 
 using namespace angle;
@@ -55,7 +56,17 @@ void CaptureCompressedTexImage3D_data(const State &glState,
                                       const void *data,
                                       ParamCapture *paramCapture)
 {
-    UNIMPLEMENTED();
+    if (glState.getTargetBuffer(gl::BufferBinding::PixelUnpack))
+    {
+        return;
+    }
+
+    if (!data)
+    {
+        return;
+    }
+
+    CaptureMemory(data, imageSize, paramCapture);
 }
 
 void CaptureCompressedTexSubImage3D_data(const State &glState,
@@ -73,7 +84,8 @@ void CaptureCompressedTexSubImage3D_data(const State &glState,
                                          const void *data,
                                          ParamCapture *paramCapture)
 {
-    UNIMPLEMENTED();
+    CaptureCompressedTexImage3D_data(glState, isCallValid, targetPacked, level, 0, width, height,
+                                     depth, 0, imageSize, data, paramCapture);
 }
 
 void CaptureDeleteQueries_idsPacked(const State &glState,
@@ -118,7 +130,7 @@ void CaptureDrawBuffers_bufs(const State &glState,
                              const GLenum *bufs,
                              ParamCapture *paramCapture)
 {
-    UNIMPLEMENTED();
+    CaptureMemory(bufs, sizeof(GLenum) * n, paramCapture);
 }
 
 void CaptureDrawElementsInstanced_indices(const State &glState,
@@ -130,7 +142,8 @@ void CaptureDrawElementsInstanced_indices(const State &glState,
                                           GLsizei instancecount,
                                           ParamCapture *paramCapture)
 {
-    UNIMPLEMENTED();
+    CaptureDrawElements_indices(glState, isCallValid, modePacked, count, typePacked, indices,
+                                paramCapture);
 }
 
 void CaptureDrawRangeElements_indices(const State &glState,
@@ -354,7 +367,8 @@ void CaptureGetQueryObjectuiv_params(const State &glState,
                                      GLuint *params,
                                      ParamCapture *paramCapture)
 {
-    UNIMPLEMENTED();
+    // This only returns one value
+    paramCapture->readBufferSizeBytes = sizeof(GLint);
 }
 
 void CaptureGetQueryiv_params(const State &glState,
@@ -473,7 +487,7 @@ void CaptureGetUniformBlockIndex_uniformBlockName(const State &glState,
                                                   const GLchar *uniformBlockName,
                                                   ParamCapture *paramCapture)
 {
-    UNIMPLEMENTED();
+    CaptureString(uniformBlockName, paramCapture);
 }
 
 void CaptureGetUniformIndices_uniformNames(const State &glState,
@@ -597,7 +611,33 @@ void CaptureTexImage3D_pixels(const State &glState,
                               const void *pixels,
                               ParamCapture *paramCapture)
 {
-    UNIMPLEMENTED();
+    if (glState.getTargetBuffer(gl::BufferBinding::PixelUnpack))
+    {
+        return;
+    }
+
+    if (!pixels)
+    {
+        return;
+    }
+
+    const gl::InternalFormat &internalFormatInfo = gl::GetInternalFormatInfo(format, type);
+    const gl::PixelUnpackState &unpack           = glState.getUnpackState();
+
+    GLuint srcRowPitch = 0;
+    (void)internalFormatInfo.computeRowPitch(type, width, unpack.alignment, unpack.rowLength,
+                                             &srcRowPitch);
+    GLuint srcDepthPitch = 0;
+    (void)internalFormatInfo.computeDepthPitch(height, unpack.imageHeight, srcRowPitch,
+                                               &srcDepthPitch);
+    GLuint srcSkipBytes = 0;
+    (void)internalFormatInfo.computeSkipBytes(type, srcRowPitch, srcDepthPitch, unpack,
+                                              true /* is3D */, &srcSkipBytes);
+
+    // Is this correct?
+    size_t captureSize = (srcRowPitch * height) * depth + srcSkipBytes;
+
+    CaptureMemory(pixels, captureSize, paramCapture);
 }
 
 void CaptureTexSubImage3D_pixels(const State &glState,
@@ -615,7 +655,8 @@ void CaptureTexSubImage3D_pixels(const State &glState,
                                  const void *pixels,
                                  ParamCapture *paramCapture)
 {
-    UNIMPLEMENTED();
+    CaptureTexImage3D_pixels(glState, isCallValid, targetPacked, level, 0, width, height, depth, 0,
+                             format, type, pixels, paramCapture);
 }
 
 void CaptureTransformFeedbackVaryings_varyings(const State &glState,
@@ -626,7 +667,14 @@ void CaptureTransformFeedbackVaryings_varyings(const State &glState,
                                                GLenum bufferMode,
                                                ParamCapture *paramCapture)
 {
-    UNIMPLEMENTED();
+    for (GLsizei index = 0; index < count; ++index)
+    {
+        size_t len = strlen(varyings[index]);
+        // includes the '\0' suffix
+        std::vector<uint8_t> data(len + 1, 0);
+        memcpy(data.data(), varyings[index], len);
+        paramCapture->data.emplace_back(std::move(data));
+    }
 }
 
 void CaptureUniform1uiv_value(const State &glState,
