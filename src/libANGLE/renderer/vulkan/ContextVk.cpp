5ff31358773c2e9b16fe60a09f95b9a4f6f91cf7
Tobin Ehlis <tobine@google.com>
2020-01-31 16:55:50 -0700

    Vulkan:Add FramebufferVk cache
    This adds a FramebufferVk cache. Cache signature is based on unique Serial
    values that are assigned to ImageHelper objects backing all color and DS
    rendertargets as well as level/layer values unique to the imageView.
    
    Update the Serials and cache signature at FramebufferVk::syncState() time.
    L0 cache is a currently active framebuffer.
    L1 cache retrieves previously created framebuffer from new cache.
    If neither of those hit, create new FramebufferVk and add to L1.
    
    Bug: angleproject:4322
    Change-Id: I3f585271798ddfb9e5f194020adca8cf8a6b19dd

--- src/libANGLE/renderer/vulkan/ContextVk.cpp
+++ src/libANGLE/renderer/vulkan/ContextVk.cpp
@@ -106,9 +106,6 @@ constexpr size_t kInFlightCommandsLimit = 100u;
 // Initially dumping the command graphs is disabled.
 constexpr bool kEnableCommandGraphDiagnostics = false;
 
-// Used as fallback serial for null sampler objects
-constexpr Serial kZeroSerial = Serial();
-
 void InitializeSubmitInfo(VkSubmitInfo *submitInfo,
                           const vk::PrimaryCommandBuffer &commandBuffer,
                           const std::vector<VkSemaphore> &waitSemaphores,
@@ -3007,6 +3004,7 @@ void ContextVk::onDrawFramebufferChange(FramebufferVk *framebufferVk)
                                                           framebufferVk->getSamples());
     }
     mGraphicsPipelineDesc->updateRenderPassDesc(&mGraphicsPipelineTransition, renderPassDesc);
+    mActiveFramebufferDesc = framebufferVk->getFramebufferDesc();
 }
 
 void ContextVk::invalidateCurrentTransformFeedbackBuffers()
@@ -3435,7 +3433,7 @@ angle::Result ContextVk::updateActiveTextures(const gl::Context *context)
         if (sampler == nullptr)
         {
             samplerVk     = nullptr;
-            samplerSerial = kZeroSerial;
+            samplerSerial = rx::kZeroSerial;
         }
         else
         {
