e20494b381867b792ca95c97241a510102850a60
Ian Elliott <ianelliott@google.com>
2020-02-12 17:27:31 -0700

    Vulkan: Implement Android pre-rotation
    As an Android GLES driver on top of Vulkan, ANGLE must pre-rotate
    rendering on behalf of the application.  This involves modifying the
    vertex shader to multiply gl_Position with a mat2 "rotation matrix".
    Not doing so means that SurfaceFlinger (SF) will perform a costly
    rotation blit before presenting every image.  Setting
    WindowSurfaceVk::mPreTransform to mCurrentTransform tells SF to not do
    the blit.
    
    When the surface is rotated 90 or 270 degrees, the width and height
    must be swapped for:
    
    - The swapchain images, and for any depth, stencil, and/or multisample
      attachments used with the swapchain image.
    
    - The viewport, scissor, and render area.
    
    Because the Metal back-end shares the TranslatorVulkan, it will define
    the same preRotation (mat2) DriverUniform that is used for Vulkan.
    
    Bug: angleproject:3502
    Change-Id: I968dbe8869ba0f50de18dd41f1195e847c06b545

--- src/libANGLE/renderer/metal/ContextMtl.mm
+++ src/libANGLE/renderer/metal/ContextMtl.mm
@@ -1435,8 +1435,7 @@ angle::Result ContextMtl::setupDraw(const gl::Context *context,
                 ANGLE_TRY(handleDirtyDepthBias(context));
                 break;
             case DIRTY_BIT_STENCIL_REF:
-                mRenderEncoder.setStencilRefVals(mStencilRefFront,
-                                                 mStencilRefBack);
+                mRenderEncoder.setStencilRefVals(mStencilRefFront, mStencilRefBack);
                 break;
             case DIRTY_BIT_BLEND_COLOR:
                 mRenderEncoder.setBlendColor(
@@ -1589,6 +1588,16 @@ angle::Result ContextMtl::handleDirtyDriverUniforms(const gl::Context *context)
     mDriverUniforms.depthRange[1] = depthRangeFar;
     mDriverUniforms.depthRange[2] = depthRangeDiff;
     mDriverUniforms.depthRange[3] = NeedToInvertDepthRange(depthRangeNear, depthRangeFar) ? -1 : 1;
+
+    // Fill in a mat2 identity matrix, plus padding
+    mDriverUniforms.preRotation[0] = 1.0f;
+    mDriverUniforms.preRotation[1] = 0.0f;
+    mDriverUniforms.preRotation[2] = 0.0f;
+    mDriverUniforms.preRotation[3] = 0.0f;
+    mDriverUniforms.preRotation[4] = 0.0f;
+    mDriverUniforms.preRotation[5] = 1.0f;
+    mDriverUniforms.preRotation[6] = 0.0f;
+    mDriverUniforms.preRotation[7] = 0.0f;
 
     ASSERT(mRenderEncoder.valid());
     mRenderEncoder.setFragmentData(mDriverUniforms, mtl::kDriverUniformsBindingIndex);
