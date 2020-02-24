5cc8f48a89eaa97ef87b0d8826168eb090573638
Alexey Knyazev <lexa.knyazev@gmail.com>
2020-02-24 19:43:32 +0400

    D3D11: Implement OES_draw_buffers_indexed
    Bug: angleproject:4394
    Change-Id: I284796e18be71de1b5bfb087d36f6a45be4c3f70

--- src/libANGLE/renderer/d3d/FramebufferD3D.h
+++ src/libANGLE/renderer/d3d/FramebufferD3D.h
@@ -102,6 +102,11 @@ class FramebufferD3D : public FramebufferImpl
 
     const gl::AttachmentList &getColorAttachmentsForRender(const gl::Context *context);
 
+    const gl::DrawBufferMask getLastColorAttachmentsForRenderMask()
+    {
+        return mColorAttachmentsForRenderMask;
+    }
+
     void destroy(const gl::Context *context) override;
 
   private:
@@ -131,6 +136,7 @@ class FramebufferD3D : public FramebufferImpl
     RendererD3D *mRenderer;
     Optional<gl::AttachmentList> mColorAttachmentsForRender;
     gl::DrawBufferMask mCurrentActiveProgramOutputs;
+    gl::DrawBufferMask mColorAttachmentsForRenderMask;
 
     gl::FramebufferAttachment mDummyAttachment;
 };
