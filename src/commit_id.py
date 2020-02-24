7c76d06a1049c04c481a5bf7e3856a66b252115b
Geoff Lang <geofflang@google.com>
2020-02-24 14:13:40 -0500

    Always use commit_id.py to generate commit.h
    commit_id_.py is capable of generating the default commit.h.
    This makes it so we always take a single path in gn.
    
    Remove the existing commit.h and generate it into the root
    generation folder (not the id subfolder) because Android blueprints
    can't handle generating into subfolders that don't exist.
    
    Make the <angle_dir> argument capable of taking a filename or
    directory name. This allows us to pass the .git/HEAD file which is
    a gn input.  Android blueprints require all paths used as input
    or output to a script are listed as inputs or outputs in the genrule.
    
    BUG=angleproject:2344
    
    Change-Id: Ifd9c8331f421586db6f2c6e17faf3242376e11d0

--- src/commit_id.py
+++ src/commit_id.py
@@ -11,8 +11,7 @@ import sys
 import os
 
 usage = """\
-Usage: commit_id.py check <angle_dir>                - check if git is present
-       commit_id.py gen <angle_dir> <file_to_write>  - generate commit.h"""
+Usage: commit_id.py gen <angle_dir> <file_to_write>  - generate commit.h"""
 
 
 def grab_output(command, cwd):
@@ -23,28 +22,26 @@ if len(sys.argv) < 3:
     sys.exit(usage)
 
 operation = sys.argv[1]
-cwd = sys.argv[2]
-
-if operation == 'check':
-    index_path = os.path.join(cwd, '.git', 'index')
-    if os.path.exists(index_path):
-        print("1")
-    else:
-        print("0")
-    sys.exit(0)
+if os.path.isdir(sys.argv[2]):
+    cwd = sys.argv[2]
+else:
+    cwd = os.path.dirname(sys.argv[2])
 
 if len(sys.argv) < 4 or operation != 'gen':
     sys.exit(usage)
 
 output_file = sys.argv[3]
 commit_id_size = 12
+commit_id = 'unknown hash'
+commit_date = 'unknown date'
 
-try:
-    commit_id = grab_output('git rev-parse --short=%d HEAD' % commit_id_size, cwd)
-    commit_date = grab_output('git show -s --format=%ci HEAD', cwd)
-except:
-    commit_id = 'invalid-hash'
-    commit_date = 'invalid-date'
+index_path = os.path.join(cwd, '.git', 'index')
+if os.path.exists(index_path):
+    try:
+        commit_id = grab_output('git rev-parse --short=%d HEAD' % commit_id_size, cwd)
+        commit_date = grab_output('git show -s --format=%ci HEAD', cwd)
+    except:
+        pass
 
 hfile = open(output_file, 'w')
 
