a34905134e9ef86cb1e1111edb3eeb7a8fa91fae
Tim Van Patten <timvp@google.com>
2020-02-24 17:38:10 -0700

    Vulkan: Support VS, FS, and CS in the same PPO
    This CL adds support for a Program Pipeline Object to have a VS, FS,
    and CS attached to the same PPO and then using that PPO for both draw
    and dispatch calls.
    
    Bug: angleproject:3570
    Test: KHR-GLES31.core.compute_shader.sso*
    Change-Id: Ife322fff1b5b3446e9dd28705d576b6d726883f3

--- src/libANGLE/State.cpp
+++ src/libANGLE/State.cpp
@@ -1700,8 +1700,15 @@ angle::Result State::setProgram(const Context *context, Program *newProgram)
             mProgram->release(context);
         }
 
-        mProgram    = newProgram;
-        mExecutable = nullptr;
+        mProgram = newProgram;
+        if (mProgramPipeline.get())
+        {
+            mExecutable = &mProgramPipeline->getExecutable();
+        }
+        else
+        {
+            mExecutable = nullptr;
+        }
 
         if (mProgram)
         {
