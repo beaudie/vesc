66865c5c35ad10ab99a5a20fdbb473fa342d81a3
Tim Van Patten <timvp@google.com>
2020-03-03 12:04:26 -0700

    Vulkan: Don't detach separable shaders in Program::detachShader()
    To support Program Pipeline Objects, we need to hold on to the shaders
    in separable programs, rather than detaching them during
    glDetachShader()/glCreateShaderProgramv(). This is necessary due to
    requiring the shader information for validating shader interfaces,
    varyings, etc.
    
    Instead, a new ShaderMap of bools will be stored in
    ProgramState::mAttachedShadersMarkedForDetach to track when a caller
    attempts to detach a shader from a separable Program. Later, when a new
    shader is attached, we will first validate that the old shader is marked
    to be detached, and if so, release it then.
    
    Bug: angleproject:3570
    Change-Id: I63fac2e5914c1c1a73f0b37863bac0f185ecb44c

--- src/libANGLE/Program.cpp
+++ src/libANGLE/Program.cpp
@@ -1080,6 +1080,7 @@ ImageBinding::~ImageBinding() = default;
 ProgramState::ProgramState()
     : mLabel(),
       mAttachedShaders{},
+      mAttachedShadersMarkedForDetach{},
       mLocationsUsedForXfbExtension(0),
       mTransformFeedbackBufferMode(GL_INTERLEAVED_ATTRIBS),
       mDefaultUniformRange(0, 0),
@@ -1289,7 +1290,8 @@ void Program::onDestroy(const Context *context)
         if (mState.mAttachedShaders[shaderType])
         {
             mState.mAttachedShaders[shaderType]->release(context);
-            mState.mAttachedShaders[shaderType] = nullptr;
+            mState.mAttachedShaders[shaderType]                = nullptr;
+            mState.mAttachedShadersMarkedForDetach[shaderType] = false;
         }
     }
 
@@ -1318,11 +1320,20 @@ const std::string &Program::getLabel() const
     return mState.mLabel;
 }
 
-void Program::attachShader(Shader *shader)
+void Program::attachShader(const Context *context, Shader *shader)
 {
     ASSERT(mLinkResolved);
     ShaderType shaderType = shader->getType();
     ASSERT(shaderType != ShaderType::InvalidEnum);
+
+    // Since detachShader doesn't actually detach anymore, we need to do that work when attaching a
+    // new shader to make sure we don't lose track of it and free the resources.
+    if (mState.mAttachedShaders[shaderType])
+    {
+        mState.mAttachedShaders[shaderType]->release(context);
+        mState.mAttachedShaders[shaderType]                = nullptr;
+        mState.mAttachedShadersMarkedForDetach[shaderType] = false;
+    }
 
     mState.mAttachedShaders[shaderType] = shader;
     mState.mAttachedShaders[shaderType]->addRef();
@@ -1335,6 +1346,16 @@ void Program::detachShader(const Context *context, Shader *shader)
     ASSERT(shaderType != ShaderType::InvalidEnum);
 
     ASSERT(mState.mAttachedShaders[shaderType] == shader);
+
+    if (isSeparable())
+    {
+        // Don't actually detach the shader since we still need it in case this
+        // Program is part of a Program Pipeline Object. Instead, leave a mark
+        // that indicates we intended to.
+        mState.mAttachedShadersMarkedForDetach[shaderType] = true;
+        return;
+    }
+
     shader->release(context);
     mState.mAttachedShaders[shaderType] = nullptr;
 }
