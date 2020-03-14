8c9ae2e3522c433163bbac6170c1a7aea5bd02de
Le Hoang Quyen <lehoangq@gmail.com>
2020-03-14 17:31:57 +0800

    Implement GL_APPLE_clip_distance
    - Built-in variable gl_ClipDistance has been added to compiler.
    - Desktop GL: gl_ClipDistance is supported since GL 3.0. Enable/Disable
    each gl_ClipDistances[i] works out of the box via glEnable().
    - Vulkan/Metal: Use uniform variable to control writing to each
    gl_ClipDistance. One bit flag controls one element in the gl_ClipDistance
    array. The writing to the disabled element in vertex shader will be
    ignored, and turned into zero assignment instead.
    - Direct3D/Mobile GL: Not implemented yet.
    - Added ClipDistanceTest to gl_tests and compiler unittests.
    - GL_APPLE_clip_distance is a subset of GL_EXT_clip_cull_distance, so
    GL_EXT_clip_cull_distance could be implemented in future if needed.
    
    Bug: angleproject:4452
    Change-Id: I571ac8b56826989808a680226a04bec4cf59988e

--- include/GLSLANG/ShaderLang.h
+++ include/GLSLANG/ShaderLang.h
@@ -26,7 +26,7 @@
 
 // Version number for shader translation API.
 // It is incremented every time the API changes.
-#define ANGLE_SH_VERSION 227
+#define ANGLE_SH_VERSION 228
 
 enum ShShaderSpec
 {
@@ -395,6 +395,7 @@ struct ShBuiltInResources
     int ANGLE_multi_draw;
     int ANGLE_base_vertex_base_instance;
     int WEBGL_video_texture;
+    int APPLE_clip_distance;
 
     // Set to 1 to enable replacing GL_EXT_draw_buffers #extension directives
     // with GL_NV_draw_buffers in ESSL output. This flag can be used to emulate
@@ -536,6 +537,9 @@ struct ShBuiltInResources
 
     // Subpixel bits used in rasterization.
     int SubPixelBits;
+
+    // APPLE_clip_distance/EXT_clip_cull_distance constant
+    int MaxClipDistances;
 };
 
 //
