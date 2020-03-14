adcf75b5a87788f97e9fd57efe169df4d6b18672
Le Hoang Quyen <lehoangq@gmail.com>
2020-03-14 17:31:57 +0800

    Implement GL_APPLE_clip_distance
    - Built-in variable gl_ClipDistance has been added to compiler.
    - Desktop GL: gl_ClipDistance is supported since GL 3.0. Enable/Disable
    each gl_ClipDistances[i] works out of the box via glEnable().
    - Vulkan/Metal: Use uniform variable to control writing to each
    gl_ClipDistance. One bit flag controls one element in the gl_ClipDistance
    array. The writing to the disabled element in vertex shader will be
    ignored, and turned into zero assignment instead.
    - Direct3D/Mobile GL: Not implemented yet.
    - Added ClipDistanceTest to gl_tests and compiler unittests.
    - GL_APPLE_clip_distance is a subset of GL_EXT_clip_cull_distance, so
    GL_EXT_clip_cull_distance could be implemented in future if needed.
    
    Bug: angleproject:4452
    Change-Id: I571ac8b56826989808a680226a04bec4cf59988e

--- src/compiler/translator/Compiler.cpp
+++ src/compiler/translator/Compiler.cpp
@@ -1075,6 +1075,7 @@ void TCompiler::setResourceString()
         << ":WEBGL_debug_shader_precision:" << mResources.WEBGL_debug_shader_precision
         << ":ANGLE_multi_draw:" << mResources.ANGLE_multi_draw
         << ":ANGLE_base_vertex_base_instance:" << mResources.ANGLE_base_vertex_base_instance
+        << ":APPLE_clip_distance:" << mResources.APPLE_clip_distance
         << ":MinProgramTextureGatherOffset:" << mResources.MinProgramTextureGatherOffset
         << ":MaxProgramTextureGatherOffset:" << mResources.MaxProgramTextureGatherOffset
         << ":MaxImageUnits:" << mResources.MaxImageUnits
@@ -1112,7 +1113,8 @@ void TCompiler::setResourceString()
         << ":MaxGeometryAtomicCounters:" << mResources.MaxGeometryAtomicCounters
         << ":MaxGeometryShaderStorageBlocks:" << mResources.MaxGeometryShaderStorageBlocks
         << ":MaxGeometryShaderInvocations:" << mResources.MaxGeometryShaderInvocations
-        << ":MaxGeometryImageUniforms:" << mResources.MaxGeometryImageUniforms;
+        << ":MaxGeometryImageUniforms:" << mResources.MaxGeometryImageUniforms
+        << ":MaxClipDistances" << mResources.MaxClipDistances;
     // clang-format on
 
     mBuiltInResourcesString = strstream.str();
