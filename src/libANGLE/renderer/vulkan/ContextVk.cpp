a7630722d5a4cf387e851b1d029dfa163352aee5
Tobin Ehlis <tobine@google.com>
2020-03-17 13:10:00 -0600

    WIP:Vulkan:Move work off main thread
    Initial POC to add worker thread for command submission.
    This is just a toy example to get started. Right now this threads
    at the explicit ContextVk::flush() boundary.
    
    Currently this ends a command buffer across thread boundaries, which
    is not ok. Will need to make this just a submission thread so that
    different threads are modifying the same cmd buffer from same command
    pool.
    Ideally we'll want to split up how we record commands and split groups
    of command recording into separate threads.
    
    Bug: angleproject:4324
    Change-Id: Idcedbb4a01f95692008d2b58c83558fd3e2339ae

--- src/libANGLE/renderer/vulkan/ContextVk.cpp
+++ src/libANGLE/renderer/vulkan/ContextVk.cpp
@@ -855,7 +855,10 @@ angle::Result ContextVk::startPrimaryCommandBuffer()
 
 angle::Result ContextVk::flush(const gl::Context *context)
 {
-    return flushImpl(nullptr);
+    mSubmitThread = std::thread(&ContextVk::flushImpl, this, nullptr);
+    // return flushImpl(nullptr);
+    // mSubmitThread.join();
+    return angle::Result::Continue;
 }
 
 angle::Result ContextVk::finish(const gl::Context *context)
@@ -2197,7 +2200,8 @@ angle::Result ContextVk::clearWithRenderPassOp(
 {
     // Validate cache variable is in sync.
     ASSERT(mDrawFramebuffer == vk::GetImpl(mState.getDrawFramebuffer()));
-
+    if (mSubmitThread.joinable())
+        mSubmitThread.join();
     // Start a new render pass if:
     //
     // - no render pass has started,
