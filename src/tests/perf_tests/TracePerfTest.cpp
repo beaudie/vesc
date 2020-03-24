0f8c73705e15a4f01d22810bb8caab4f9a1cfb66
Cody Northrop <cnorthrop@google.com>
2020-03-24 15:02:06 -0600

    Capture/Replay: Update CaptureReplay sample for compression
    * Move DecompressBinaryData to a shared helper
    * Start using it in CaptureReplay sample
    * Error out if decompress callback isn't set correctly
    
    Test: AngryBirds and CandyCrush captures replay on desktop
    Test: angle_perftests --gtest_filter="*Trace*"
    Bug: angleproject:4484
    Change-Id: I0432004fdb0cfb0fd61f9a66f792591c9aa40d9b

--- src/tests/perf_tests/TracePerfTest.cpp
+++ src/tests/perf_tests/TracePerfTest.cpp
@@ -13,6 +13,7 @@
 #include "tests/perf_tests/ANGLEPerfTest.h"
 #include "tests/perf_tests/DrawCallPerfParams.h"
 #include "util/egl_loader_autogen.h"
+#include "util/frame_capture_utils.h"
 
 #include "restricted_traces/trex_200/trex_200_capture_context1.h"
 
@@ -20,35 +21,12 @@
 #include <functional>
 #include <sstream>
 
-#define USE_SYSTEM_ZLIB
-#include "compression_utils_portable.h"
-
 using namespace angle;
 using namespace egl_platform;
 
 namespace
 {
 void FramebufferChangeCallback(void *userData, GLenum target, GLuint framebuffer);
-
-ANGLE_MAYBE_UNUSED uint8_t *DecompressBinaryData(const std::vector<uint8_t> &compressedData)
-{
-    uint32_t uncompressedSize =
-        zlib_internal::GetGzipUncompressedSize(compressedData.data(), compressedData.size());
-
-    std::unique_ptr<uint8_t[]> uncompressedData(new uint8_t[uncompressedSize]);
-    uLong destLen = uncompressedSize;
-    int zResult =
-        zlib_internal::GzipUncompressHelper(uncompressedData.get(), &destLen, compressedData.data(),
-                                            static_cast<uLong>(compressedData.size()));
-
-    if (zResult != Z_OK)
-    {
-        std::cerr << "Failure to decompressed binary data: " << zResult << "\n";
-        return nullptr;
-    }
-
-    return uncompressedData.release();
-}
 
 // TODO (anglebug.com/4496)
 // Temporarily limit the tests to a single trace to get the bots going
@@ -166,7 +144,7 @@ void TracePerfTest::initializeBenchmark()
     switch (params.testID)
     {
         case TracePerfTestID::TRex200:
-            trex_200::SetBinaryDataDecompressCallback(DecompressBinaryData);
+            trex_200::SetBinaryDataDecompressCallback(angle::DecompressBinaryData);
             TRACE_TEST_CASE(trex_200);
             break;
         default:
