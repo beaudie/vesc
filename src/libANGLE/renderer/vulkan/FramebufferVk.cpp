5d19d5905274d1591cd89096b455e24f8780e41f
Jamie Madill <jmadill@chromium.org>
2020-03-26 17:22:18 -0400

    Vulkan: Mask out Depth/Stencil RTs in feedback loops.
    This should enable some cases of limited support for feedback loops
    with depth/stencil buffers. For example with Manhattan and the Vulkan
    back-end.
    
    Increases the number of RenderPasses in Manhattan slightly. This will
    regress performance slightly until we can work out a better solution
    that is also conformant with the spec.
    
    Bug: angleproject:4517
    Change-Id: I2758e6b4c2a930474c09cdc0950f3b6c34541089

--- src/libANGLE/renderer/vulkan/FramebufferVk.cpp
+++ src/libANGLE/renderer/vulkan/FramebufferVk.cpp
@@ -114,7 +114,9 @@ FramebufferVk::FramebufferVk(RendererVk *renderer,
     : FramebufferImpl(state),
       mBackbuffer(backbuffer),
       mFramebuffer(nullptr),
-      mActiveColorComponents(0)
+      mActiveColorComponents(0),
+      mAllowDepthStencilFeedbackLoops(
+          !renderer->getFeatures().supportDepthStencilRenderingFeedbackLoops.enabled)
 {
     mReadPixelBuffer.init(renderer, VK_BUFFER_USAGE_TRANSFER_DST_BIT, kReadPixelsBufferAlignment,
                           kMinReadPixelsBufferSize, true);
@@ -228,6 +230,34 @@ angle::Result FramebufferVk::clearImpl(const gl::Context *context,
     ASSERT(!clearDepth || contextVk->getState().getDepthStencilState().depthMask);
     // The front-end should ensure we don't attempt to clear stencil if all bits are masked.
     ASSERT(!clearStencil || stencilMask != 0);
+
+    if ((clearDepth || clearStencil) && mState.hasDepthStencilFeedbackLoop())
+    {
+        // We only support full render area clears when we have a D/S feedback loop.
+        ANGLE_VK_CHECK(contextVk, scissoredRenderArea == getCompleteRenderArea(),
+                       VK_ERROR_INCOMPATIBLE_DRIVER);
+
+        RenderTargetVk *depthStencilRT = mRenderTargetCache.getDepthStencil(true);
+        vk::ImageHelper &image         = depthStencilRT->getImage();
+
+        vk::CommandBuffer *commandBuffer;
+        ANGLE_TRY(
+            contextVk->onImageWrite(image.getAspectFlags(), vk::ImageLayout::TransferDst, &image));
+        ANGLE_TRY(contextVk->endRenderPassAndGetCommandBuffer(&commandBuffer));
+
+        VkImageSubresourceRange range;
+        range.aspectMask     = image.getAspectFlags();
+        range.baseMipLevel   = depthStencilRT->getLevelIndex();
+        range.levelCount     = 1;
+        range.baseArrayLayer = depthStencilRT->getLayerIndex();
+        range.layerCount     = 1;
+
+        commandBuffer->clearDepthStencilImage(image.getImage(),
+                                              VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
+                                              clearDepthStencilValue, 1, &range);
+        clearDepth   = false;
+        clearStencil = false;
+    }
 
     // If there is nothing to clear, return right away (for example, if asked to clear depth, but
     // there is no depth attachment).
@@ -447,7 +477,7 @@ angle::Result FramebufferVk::readPixels(const gl::Context *context,
 
 RenderTargetVk *FramebufferVk::getDepthStencilRenderTarget() const
 {
-    return mRenderTargetCache.getDepthStencil(true);
+    return mRenderTargetCache.getDepthStencil(mAllowDepthStencilFeedbackLoops);
 }
 
 RenderTargetVk *FramebufferVk::getColorDrawRenderTarget(size_t colorIndex) const
@@ -983,7 +1013,8 @@ angle::Result FramebufferVk::invalidateImpl(ContextVk *contextVk,
 
 void FramebufferVk::updateDepthStencilAttachmentSerial(ContextVk *contextVk)
 {
-    RenderTargetVk *depthStencilRT = mRenderTargetCache.getDepthStencil(true);
+    RenderTargetVk *depthStencilRT =
+        mRenderTargetCache.getDepthStencil(mAllowDepthStencilFeedbackLoops);
 
     if (depthStencilRT != nullptr)
     {
@@ -1021,7 +1052,8 @@ angle::Result FramebufferVk::syncState(const gl::Context *context,
             case gl::Framebuffer::DIRTY_BIT_DEPTH_BUFFER_CONTENTS:
             case gl::Framebuffer::DIRTY_BIT_STENCIL_BUFFER_CONTENTS:
             {
-                RenderTargetVk *depthStencilRT = mRenderTargetCache.getDepthStencil(true);
+                RenderTargetVk *depthStencilRT =
+                    mRenderTargetCache.getDepthStencil(mAllowDepthStencilFeedbackLoops);
                 ASSERT(depthStencilRT != nullptr);
                 ANGLE_TRY(depthStencilRT->flushStagedUpdates(contextVk));
                 mCurrentFramebufferDesc.update(vk::kFramebufferDescDepthStencilIndex,
@@ -1141,7 +1173,8 @@ void FramebufferVk::updateRenderPassDesc()
         }
     }
 
-    RenderTargetVk *depthStencilRenderTarget = mRenderTargetCache.getDepthStencil(true);
+    RenderTargetVk *depthStencilRenderTarget =
+        mRenderTargetCache.getDepthStencil(mAllowDepthStencilFeedbackLoops);
     if (depthStencilRenderTarget)
     {
         mRenderPassDesc.packDepthStencilAttachment(
@@ -1200,7 +1233,8 @@ angle::Result FramebufferVk::getFramebuffer(ContextVk *contextVk, vk::Framebuffe
         attachmentsSize = colorRenderTarget->getExtents();
     }
 
-    RenderTargetVk *depthStencilRenderTarget = mRenderTargetCache.getDepthStencil(true);
+    RenderTargetVk *depthStencilRenderTarget =
+        mRenderTargetCache.getDepthStencil(mAllowDepthStencilFeedbackLoops);
     if (depthStencilRenderTarget)
     {
         const vk::ImageView *imageView = nullptr;
@@ -1324,7 +1358,8 @@ angle::Result FramebufferVk::startNewRenderPass(ContextVk *contextVk,
         attachmentClearValues.emplace_back(kUninitializedClearValue);
     }
 
-    RenderTargetVk *depthStencilRenderTarget = mRenderTargetCache.getDepthStencil(true);
+    RenderTargetVk *depthStencilRenderTarget =
+        mRenderTargetCache.getDepthStencil(mAllowDepthStencilFeedbackLoops);
     if (depthStencilRenderTarget)
     {
         ANGLE_TRY(depthStencilRenderTarget->onDepthStencilDraw(contextVk));
@@ -1402,7 +1437,7 @@ RenderTargetVk *FramebufferVk::getFirstRenderTarget() const
         }
     }
 
-    return mRenderTargetCache.getDepthStencil(true);
+    return mRenderTargetCache.getDepthStencil(mAllowDepthStencilFeedbackLoops);
 }
 
 GLint FramebufferVk::getSamples() const
