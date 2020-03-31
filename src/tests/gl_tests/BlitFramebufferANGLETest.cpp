a8bff51b4d1b96d5a2a301510b2e6e8573eb2947
jchen10 <jie.a.chen@intel.com>
2020-03-31 09:21:17 +0800

    Remove some Intel Linux GL tests suppression
    BlitFramebufferTest.MultisampleDepthClear
    ClearTest
    
    Bug: angleproject:3614
    Change-Id: Ic944f52453c9665344769186e65010315312dae2

--- src/tests/gl_tests/BlitFramebufferANGLETest.cpp
+++ src/tests/gl_tests/BlitFramebufferANGLETest.cpp
@@ -1220,9 +1220,6 @@ TEST_P(BlitFramebufferTest, MultisampleDepth)
 // Tests clearing a multisampled depth buffer.
 TEST_P(BlitFramebufferTest, MultisampleDepthClear)
 {
-    // clearDepth && !maskDepth fails on Intel Ubuntu 19.04 Mesa 19.0.2 GL. http://anglebug.com/3614
-    ANGLE_SKIP_TEST_IF(IsLinux() && IsIntel() && IsDesktopOpenGL());
-
     // http://anglebug.com/4092
     ANGLE_SKIP_TEST_IF(IsAndroid() && IsOpenGLES());
 
