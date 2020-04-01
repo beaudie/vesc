6ea6d4e3b393012cabcc99ac58e5f5c15d93e80c
Jonah Ryan-Davis <jonahr@google.com>
2020-04-01 16:53:31 -0400

    GL: Support GL_OES_texture_cube_map_array in shader translator
    GL_OES_texture_cube_map_array is core in 3.2. This CL adds support
    for this extension in the shader translator.
    Now passes dEQP.KHR_GLES31/core_texture_cube_map_array.*,
    dEQP.GLES31/functional_fbo_color_texcubearray_*, and
    dEQP.GLES31/functional_opaque_type_indexing*samplercubearray* with GL backend.
    
    Bug: angleproject:3584
    Change-Id: I1a8485c7d9d3fffb9b5109d292b35b0c56a3c665

--- src/compiler/translator/ExtensionBehavior.h
+++ src/compiler/translator/ExtensionBehavior.h
@@ -44,6 +44,8 @@ enum class TExtension
     ANGLE_base_vertex_base_instance,
     WEBGL_video_texture,
     APPLE_clip_distance,
+    OES_texture_cube_map_array,
+    EXT_texture_cube_map_array,
 };
 
 enum TBehavior
