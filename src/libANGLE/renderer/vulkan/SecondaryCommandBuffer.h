19cb5f4ef98ce744861275e10c84447be1e32d15
Tobin Ehlis <tobine@google.com>
2020-04-14 12:13:15 -0600

    Vulkan: Allow barriers to be prepended to cmd buffer
    Template the pipelineBarrier command in SecondaryCommandBuffer (SCB)
    class to allow barriers to be prepended to the start of the command
    buffer.
    Previously we would add these commands to the primary command buffer and
    then execute the SCB into the primary. This change allows us to skip the
    first step of adding pipelineBarrier command to the primary command
    buffer.
    
    Bug: b:153666475
    Change-Id: I2aa58060f161af219696f6b335cae1ce9b8893f6

--- src/libANGLE/renderer/vulkan/SecondaryCommandBuffer.h
+++ src/libANGLE/renderer/vulkan/SecondaryCommandBuffer.h
@@ -597,6 +597,7 @@ class SecondaryCommandBuffer final : angle::NonCopyable
                        VkPipelineStageFlags dstStageMask,
                        const VkMemoryBarrier *memoryBarrier);
 
+    template <bool prepend = false>
     void pipelineBarrier(VkPipelineStageFlags srcStageMask,
                          VkPipelineStageFlags dstStageMask,
                          VkDependencyFlags dependencyFlags,
@@ -1291,6 +1292,7 @@ ANGLE_INLINE void SecondaryCommandBuffer::memoryBarrier(VkPipelineStageFlags src
     paramStruct->memoryBarrier       = *memoryBarrier;
 }
 
+template <bool prepend>
 ANGLE_INLINE void SecondaryCommandBuffer::pipelineBarrier(
     VkPipelineStageFlags srcStageMask,
     VkPipelineStageFlags dstStageMask,
@@ -1306,7 +1308,7 @@ ANGLE_INLINE void SecondaryCommandBuffer::pipelineBarrier(
     size_t memBarrierSize              = memoryBarrierCount * sizeof(VkMemoryBarrier);
     size_t buffBarrierSize             = bufferMemoryBarrierCount * sizeof(VkBufferMemoryBarrier);
     size_t imgBarrierSize              = imageMemoryBarrierCount * sizeof(VkImageMemoryBarrier);
-    PipelineBarrierParams *paramStruct = initCommand<PipelineBarrierParams>(
+    PipelineBarrierParams *paramStruct = initCommand<PipelineBarrierParams, prepend>(
         CommandID::PipelineBarrier, memBarrierSize + buffBarrierSize + imgBarrierSize, &writePtr);
     paramStruct->srcStageMask             = srcStageMask;
     paramStruct->dstStageMask             = dstStageMask;
