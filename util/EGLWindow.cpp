8036a88f22cdef1e7c457929c9e099bdef2581f1
Jamie Madill <jmadill@chromium.org>
2020-05-01 13:38:16 -0400

    EGLWindow: Clean up some init attribute code.
    EGL_CONTEXT_OPENGL_DEBUG is only exposed as of EGL 1.5.
    We can also now properly test for no_error extension strings.
    Do not require backwards compatible context extensions.
    
    Fixes parts of using EGLWindow to load native EGL drivers.
    
    Bug: angleproject:4596
    Change-Id: I4de8f492f265d29f6603ed1b35b123e362699c54

--- util/EGLWindow.cpp
+++ util/EGLWindow.cpp
@@ -394,11 +394,11 @@ EGLContext EGLWindow::createContext(EGLContext share) const
         return EGL_NO_CONTEXT;
     }
 
-    bool hasBackwardsCompatibleContextExtension =
-        strstr(displayExtensions, "EGL_ANGLE_create_context_backwards_compatible") != nullptr;
-    if (!hasProgramCacheControlExtension)
+    bool hasKHRCreateContextNoError =
+        strstr(displayExtensions, "EGL_KHR_create_context_no_error") != nullptr;
+    if (mConfigParams.noError && !hasKHRCreateContextNoError)
     {
-        std::cerr << "EGL_ANGLE_create_context_backwards_compatible missing.\n";
+        std::cerr << "EGL_KHR_create_context_no_error missing.\n";
         return EGL_NO_CONTEXT;
     }
 
@@ -418,15 +418,18 @@ EGLContext EGLWindow::createContext(EGLContext share) const
         contextAttributes.push_back(EGL_CONTEXT_MINOR_VERSION_KHR);
         contextAttributes.push_back(mClientMinorVersion);
 
-        contextAttributes.push_back(EGL_CONTEXT_OPENGL_DEBUG);
-        contextAttributes.push_back(mConfigParams.debug ? EGL_TRUE : EGL_FALSE);
+        // EGL_CONTEXT_OPENGL_DEBUG is only valid as of EGL 1.5.
+        if (mEGLMinorVersion >= 5)
+        {
+            contextAttributes.push_back(EGL_CONTEXT_OPENGL_DEBUG);
+            contextAttributes.push_back(mConfigParams.debug ? EGL_TRUE : EGL_FALSE);
+        }
 
-        // TODO(jmadill): Check for the extension string.
-        // bool hasKHRCreateContextNoError = strstr(displayExtensions,
-        // "EGL_KHR_create_context_no_error") != nullptr;
-
-        contextAttributes.push_back(EGL_CONTEXT_OPENGL_NO_ERROR_KHR);
-        contextAttributes.push_back(mConfigParams.noError ? EGL_TRUE : EGL_FALSE);
+        if (hasKHRCreateContextNoError)
+        {
+            contextAttributes.push_back(EGL_CONTEXT_OPENGL_NO_ERROR_KHR);
+            contextAttributes.push_back(mConfigParams.noError ? EGL_TRUE : EGL_FALSE);
+        }
 
         if (mConfigParams.webGLCompatibility.valid())
         {
@@ -470,6 +473,8 @@ EGLContext EGLWindow::createContext(EGLContext share) const
                 mConfigParams.contextProgramCacheEnabled.value() ? EGL_TRUE : EGL_FALSE);
         }
 
+        bool hasBackwardsCompatibleContextExtension =
+            strstr(displayExtensions, "EGL_ANGLE_create_context_backwards_compatible") != nullptr;
         if (hasBackwardsCompatibleContextExtension)
         {
             // Always request the exact context version that the config wants
