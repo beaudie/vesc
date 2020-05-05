ab2ea14d88044c92b41427fbe0a434974c596f48
Tobin Ehlis <tobine@google.com>
2020-05-05 17:01:18 -0600

    Vulkan:Initial worker thread disabled by default
    Created new CommandProcessor class that can be run as a worker thread.
    Running CommandProcessor within RendererVk as a worker thread that
    takes a CommmandBufferHelper (CBH) ptr as the interface and processes
    that CBH into a primary command buffer.
    
    Main thread has a queue of CBH to draw from. After submitting a CBH to
    the worker, it pulls next CBH from the queue. Worker thread releases CBH
    back to the main thread queue when done.
    
    Synchronization goes two ways:
    1. Work submitted to worker thread is managaed with a mutex and
    condition variable based around the work queue.
    2. Available CBH ptrs for the main thread have a mutex and condition
    variable that manages the CBH queue.
    
    The worker thread is disabled by default, and, when enabled, it will
    currently behave and perform as the non-threaded code. This is because
    the kNumCommandBuffers const in ContextVk.h is set to 2. With only 2
    command buffers, they will be assigned to the inside and outside
    RenderPass command buffers respectively. Then, as soon as one is
    submitted, the main thread will stall waiting for it to be completed
    and put back into the queue mentioned in #2 above.
    
    The next step is to move command submission to the worker thread and
    update the number of command buffers so that processing/submission
    will occur in parallel with the main thread. Right now there is a
    race condition issue when attempting to run in parallel because the
    main thread updates and submits the same primary command buffers
    that are used in the worker thread, which is in violation of the
    Vulkan spec.
    
    The follow-on CL will fix this issue as the main thread will only
    touch SecondaryCommandBuffers and the worker thread will be the
    only thread touching the primary command buffers.
    
    Bug: b/154030730
    Change-Id: Ib0c518bbd7ca9a3a7e789f4e1f2f7131ddc0509e

--- src/libANGLE/renderer/vulkan/ContextVk.cpp
+++ src/libANGLE/renderer/vulkan/ContextVk.cpp
@@ -119,7 +119,6 @@ constexpr VkColorComponentFlags kAllColorChannelsMask =
 constexpr VkBufferUsageFlags kVertexBufferUsage = VK_BUFFER_USAGE_VERTEX_BUFFER_BIT;
 constexpr size_t kDefaultValueSize              = sizeof(gl::VertexAttribCurrentValueData::Values);
 constexpr size_t kDefaultBufferSize             = kDefaultValueSize * 16;
-constexpr size_t kDefaultPoolAllocatorPageSize  = 16 * 1024;
 constexpr size_t kDriverUniformsAllocatorPageSize = 4 * 1024;
 
 constexpr size_t kInFlightCommandsLimit = 100u;
@@ -639,7 +638,6 @@ ContextVk::ContextVk(const gl::State &state, gl::ErrorSet *errorSet, RendererVk 
       mIsAnyHostVisibleBufferWritten(false),
       mEmulateSeamfulCubeMapSampling(false),
       mUseOldRewriteStructSamplers(false),
-      mPoolAllocator(kDefaultPoolAllocatorPageSize, 1),
       mOutsideRenderPassCommands(nullptr),
       mRenderPassCommands(nullptr),
       mHasPrimaryCommands(false),
@@ -858,15 +856,19 @@ angle::Result ContextVk::initialize()
 
     mUseOldRewriteStructSamplers = shouldUseOldRewriteStructSamplers();
 
-    // Push a scope in the pool allocator so we can easily reinitialize on flush.
-    mPoolAllocator.push();
-    mOutsideRenderPassCommands = &mCommandBuffers[0];
-    mRenderPassCommands        = &mCommandBuffers[1];
+    // Prepare command buffer queue by:
+    //  1. Initializing each command buffer (as non-renderpass initially)
+    //  2. Put a pointer to each command buffer into queue
+    for (vk::CommandBufferHelper &commandBuffer : mCommandBuffers)
+    {
+        commandBuffer.initialize(false,
+                                 mRenderer->getFeatures().preferAggregateBarrierCalls.enabled);
+        recycleCommandBuffer(&commandBuffer);
+    }
+    // Now assign initial command buffers from queue
+    getNextAvailableCommandBuffer(&mOutsideRenderPassCommands, false);
+    getNextAvailableCommandBuffer(&mRenderPassCommands, true);
 
-    mOutsideRenderPassCommands->initialize(
-        &mPoolAllocator, false, mRenderer->getFeatures().preferAggregateBarrierCalls.enabled);
-    mRenderPassCommands->initialize(&mPoolAllocator, true,
-                                    mRenderer->getFeatures().preferAggregateBarrierCalls.enabled);
     ANGLE_TRY(startPrimaryCommandBuffer());
 
     if (mGpuEventsEnabled)
@@ -3725,13 +3727,15 @@ angle::Result ContextVk::flushImpl(const vk::Semaphore *signalSemaphore)
                                 TRACE_EVENT_PHASE_END, eventName));
     }
     ANGLE_TRY(flushOutsideRenderPassCommands());
-    ANGLE_VK_TRY(this, mPrimaryCommands.end());
 
-    // Free secondary command pool allocations and restart command buffers with the new page.
-    mPoolAllocator.pop();
-    mPoolAllocator.push();
-    mOutsideRenderPassCommands->reset();
-    mRenderPassCommands->reset();
+    if (mRenderer->getFeatures().enableCommandProcessingThread.enabled)
+    {
+        // Worker thread must complete adding any commands that were just flushed above to the
+        //  primary command buffer before we can End the primary command buffer below.
+        mRenderer->waitForWorkerThreadIdle();
+    }
+
+    ANGLE_VK_TRY(this, mPrimaryCommands.end());
 
     Serial serial = getCurrentQueueSerial();
     mResourceUseList.releaseResourceUsesAndUpdateSerials(serial);
@@ -4176,7 +4180,17 @@ angle::Result ContextVk::endRenderPass()
 
     mRenderPassCommands->pauseTransformFeedbackIfStarted();
 
-    ANGLE_TRY(mRenderPassCommands->flushToPrimary(this, &mPrimaryCommands));
+    if (mRenderer->getFeatures().enableCommandProcessingThread.enabled)
+    {
+        vk::CommandProcessorTask task = {this, &mPrimaryCommands, mRenderPassCommands};
+        queueCommandsToWorker(task);
+        getNextAvailableCommandBuffer(&mRenderPassCommands, true);
+    }
+    else
+    {
+        ANGLE_TRY(mRenderPassCommands->flushToPrimary(this, &mPrimaryCommands));
+    }
+
     mHasPrimaryCommands = true;
 
     if (mGpuEventsEnabled)
@@ -4195,6 +4209,30 @@ void ContextVk::onRenderPassImageWrite(VkImageAspectFlags aspectFlags,
                                        vk::ImageHelper *image)
 {
     mRenderPassCommands->imageWrite(&mResourceUseList, aspectFlags, imageLayout, image);
+}
+
+void ContextVk::getNextAvailableCommandBuffer(vk::CommandBufferHelper **commandBuffer,
+                                              bool hasRenderPass)
+{
+    ANGLE_TRACE_EVENT0("gpu.angle", "ContextVk::getNextAvailableCommandBuffer");
+    std::unique_lock<std::mutex> lock(mCommandBufferQueueMutex);
+    // Only wake if notified and command queue is not empty
+    mAvailableCommandBufferCondition.wait(lock,
+                                          [this] { return !mAvailableCommandBuffers.empty(); });
+    *commandBuffer = mAvailableCommandBuffers.front();
+    ASSERT((*commandBuffer)->empty());
+    mAvailableCommandBuffers.pop();
+    lock.unlock();
+    (*commandBuffer)->setHasRenderPass(hasRenderPass);
+}
+
+void ContextVk::recycleCommandBuffer(vk::CommandBufferHelper *commandBuffer)
+{
+    ANGLE_TRACE_EVENT0("gpu.angle", "RendererVk::waitForWorkerThreadIdle");
+    std::lock_guard<std::mutex> queueLock(mCommandBufferQueueMutex);
+    ASSERT(commandBuffer->empty());
+    mAvailableCommandBuffers.push(commandBuffer);
+    mAvailableCommandBufferCondition.notify_one();
 }
 
 angle::Result ContextVk::syncExternalMemory()
@@ -4279,7 +4317,16 @@ angle::Result ContextVk::flushOutsideRenderPassCommands()
 {
     if (!mOutsideRenderPassCommands->empty())
     {
-        ANGLE_TRY(mOutsideRenderPassCommands->flushToPrimary(this, &mPrimaryCommands));
+        if (mRenderer->getFeatures().enableCommandProcessingThread.enabled)
+        {
+            vk::CommandProcessorTask task = {this, &mPrimaryCommands, mOutsideRenderPassCommands};
+            queueCommandsToWorker(task);
+            getNextAvailableCommandBuffer(&mOutsideRenderPassCommands, false);
+        }
+        else
+        {
+            ANGLE_TRY(mOutsideRenderPassCommands->flushToPrimary(this, &mPrimaryCommands));
+        }
         mHasPrimaryCommands = true;
     }
     return angle::Result::Continue;
