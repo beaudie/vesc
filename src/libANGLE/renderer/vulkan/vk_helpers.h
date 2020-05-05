1cdbd63776730c8bed2c9f1a33d8037d742a6b81
Tobin Ehlis <tobine@google.com>
2020-05-05 17:01:18 -0600

    Vulkan:Initial worker thread disabled by default
    Created new CommandProcessor class that can be run as a worker thread.
    Running CommandProcessor within RendererVk as a worker thread that
    takes a CommmandBufferHelper (CBH) ptr as the interface and processes
    that CBH into a primary command buffer.
    
    Main thread has a queue of CBH to draw from. After submitting a CBH to
    the worker, it pulls next CBH from the queue. Worker thread releases CBH
    back to the main thread queue when done.
    
    Synchronization goes two ways:
    1. Work submitted to worker thread is managaed with a mutex and
    condition variable based around the work queue.
    2. Available CBH ptrs for the main thread have a mutex and condition
    variable that manages the CBH queue.
    
    The worker thread is disabled by default, and, when enabled, it will
    currently behave and perform as the non-threaded code. This is because
    the kNumCommandBuffers const in ContextVk.h is set to 2. With only 2
    command buffers, they will be assigned to the inside and outside
    RenderPass command buffers respectively. Then, as soon as one is
    submitted, the main thread will stall waiting for it to be completed
    and put back into the queue mentioned in #2 above.
    
    The next step is to move command submission to the worker thread and
    update the number of command buffers so that processing/submission
    will occur in parallel with the main thread. Right now there is a
    race condition issue when attempting to run in parallel because the
    main thread updates and submits the same primary command buffers
    that are used in the worker thread, which is in violation of the
    Vulkan spec.
    
    The follow-on CL will fix this issue as the main thread will only
    touch SecondaryCommandBuffers and the worker thread will be the
    only thread touching the primary command buffers.
    
    Bug: b/154030730
    Change-Id: Ib0c518bbd7ca9a3a7e789f4e1f2f7131ddc0509e

--- src/libANGLE/renderer/vulkan/vk_helpers.h
+++ src/libANGLE/renderer/vulkan/vk_helpers.h
@@ -828,9 +828,7 @@ struct CommandBufferHelper : angle::NonCopyable
     ~CommandBufferHelper();
 
     // General Functions (non-renderPass specific)
-    void initialize(angle::PoolAllocator *poolAllocator,
-                    bool canHaveRenderPass,
-                    bool mergeBarriers);
+    void initialize(bool isRenderPassCommandBuffer, bool mergeBarriers);
 
     void bufferRead(vk::ResourceUseList *resourceUseList,
                     VkAccessFlags readAccessType,
@@ -858,8 +856,9 @@ struct CommandBufferHelper : angle::NonCopyable
     void executeBarriers(vk::PrimaryCommandBuffer *primary);
 
     bool empty() const { return (!mCommandBuffer.empty() || mRenderPassStarted) ? false : true; }
-
+    void setHasRenderPass(bool hasRenderPass) { mIsRenderPassCommandBuffer = hasRenderPass; }
     void reset();
+    void releaseToContextQueue(ContextVk *contextVk);
 
     // RenderPass related functions
     bool started() const
@@ -932,6 +931,9 @@ struct CommandBufferHelper : angle::NonCopyable
 
   private:
     void addCommandDiagnostics(ContextVk *contextVk);
+    // Allocator used by this class. Using a pool allocator per CBH to avoid threading issues
+    //  that occur w/ shared allocator between multiple CBHs.
+    angle::PoolAllocator mAllocator;
 
     // General state (non-renderPass related)
     PipelineBarrierArray mPipelineBarriers;
@@ -954,6 +956,19 @@ struct CommandBufferHelper : angle::NonCopyable
 
     bool mIsRenderPassCommandBuffer;
     bool mMergeBarriers;
+};
+
+// CommandProcessorTask is used to queue a task to the worker thread when
+//  enableCommandProcessingThread feature is true.
+// The typical task includes pointers in all values and the worker thread will
+//  process the SecondaryCommandBuffer commands in cbh into the primaryCB.
+// There is a special task in which all of the pointers are null that will trigger
+//  the worker thread to exit, and is sent when the renderer instance shuts down.
+struct CommandProcessorTask
+{
+    ContextVk *contextVk;
+    vk::PrimaryCommandBuffer *primaryCB;
+    CommandBufferHelper *commandBuffer;
 };
 
 // Imagine an image going through a few layout transitions:
