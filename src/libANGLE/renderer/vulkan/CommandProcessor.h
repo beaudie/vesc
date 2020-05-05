ca79f68d85097a88662284a60c307adcb7f834e9
Tobin Ehlis <tobine@google.com>
2020-05-05 17:01:18 -0600

    Vulkan:Initial worker thread disabled by default
    Created new CommandProcessor class that can be run as a worker thread.
    Running CommandProcessor within RendererVk as a worker thread that
    takes a CommmandBufferHelper (CBH) ptr as the interface and processes
    that CBH into a primary command buffer.
    
    Main thread has a queue of CBH to draw from. After submitting a CBH to
    the worker, it pulls next CBH from the queue. Worker thread releases CBH
    back to the main thread queue when done.
    
    Synchronization goes two ways:
    1. Work submitted to worker thread is managaed with a mutex and
    condition variable based around the work queue.
    2. Available CBH ptrs for the main thread have a mutex and condition
    variable that manages the CBH queue.
    
    The worker thread is disabled by default, and, when enabled, it will
    currently behave and perform as the non-threaded code. This is because
    the kNumCommandBuffers const in ContextVk.h is set to 2. With only 2
    command buffers, they will be assigned to the inside and outside
    RenderPass command buffers respectively. Then, as soon as one is
    submitted, the main thread will stall waiting for it to be completed
    and put back into the queue mentioned in #2 above.
    
    The next step is to move command submission to the worker thread and
    update the number of command buffers so that processing/submission
    will occur in parallel with the main thread. Right now there is a
    race condition issue when attempting to run in parallel because the
    main thread updates and submits the same primary command buffers
    that are used in the worker thread, which is in violation of the
    Vulkan spec.
    
    The follow-on CL will fix this issue as the main thread will only
    touch SecondaryCommandBuffers and the worker thread will be the
    only thread touching the primary command buffers.
    
    Bug: b/154030730
    Change-Id: Ib0c518bbd7ca9a3a7e789f4e1f2f7131ddc0509e

---
+++ src/libANGLE/renderer/vulkan/CommandProcessor.h
@@ -0,0 +1,54 @@
+//
+// Copyright 2020 The ANGLE Project Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+// CommandProcessor.h:
+//    A class to process and submit Vulkan command buffers that can be
+//    used in an asynchronous worker thread.
+//
+
+#ifndef LIBANGLE_RENDERER_VULKAN_COMMAND_PROCESSOR_H_
+#define LIBANGLE_RENDERER_VULKAN_COMMAND_PROCESSOR_H_
+
+#include <condition_variable>
+#include <mutex>
+#include <queue>
+#include <thread>
+
+#include "libANGLE/renderer/vulkan/vk_headers.h"
+#include "libANGLE/renderer/vulkan/vk_helpers.h"
+
+namespace rx
+{
+
+class CommandProcessor : angle::NonCopyable
+{
+  public:
+    CommandProcessor();
+    ~CommandProcessor() = default;
+
+    // Main worker loop that should be launched in its own thread. The
+    //  loop waits for work to be submitted from a separate thread.
+    angle::Result processCommandProcessorTasks();
+    // Called asynchronously from workLoop() thread to queue work that is
+    //  then processed by the workLoop() thread
+    void queueCommands(vk::CommandProcessorTask commands);
+    // Used by separate thread to wait for worker thread to complete all
+    //  outstanding work.
+    void waitForWorkComplete();
+
+  private:
+    std::queue<vk::CommandProcessorTask> mCommandsQueue;
+    std::mutex mWorkerMutex;
+    // Signal worker thread when work is available
+    std::condition_variable mWorkAvailableCondition;
+    // Signal main thread when all work completed
+    std::condition_variable mWorkerIdleCondition;
+    // Track worker thread Idle state for assertion purposes
+    bool mWorkerThreadIdle;
+};
+
+}  // namespace rx
+
+#endif  // LIBANGLE_RENDERER_VULKAN_COMMAND_PROCESSOR_H_
