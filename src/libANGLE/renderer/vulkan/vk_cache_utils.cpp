a06cbd4d18441c215ee5a2dc495e9ce345ddca48
Courtney Goeltzenleuchter <courtneygo@google.com>
2020-05-12 09:45:21 -0600

    Add support for VK_KHR_sampler_ycbcr_conversion
    Angry Birds 2 video player (for ads) requires YUV conversion.
    
    Bug: b/155487768
    Change-Id: I9255450f81aa4daa2aace7205d4f6c3f225abcca

--- src/libANGLE/renderer/vulkan/vk_cache_utils.cpp
+++ src/libANGLE/renderer/vulkan/vk_cache_utils.cpp
@@ -13,6 +13,7 @@
 #include "common/aligned_memory.h"
 #include "libANGLE/BlobCache.h"
 #include "libANGLE/VertexAttribute.h"
+#include "libANGLE/renderer/vulkan/DisplayVk.h"
 #include "libANGLE/renderer/vulkan/FramebufferVk.h"
 #include "libANGLE/renderer/vulkan/ProgramVk.h"
 #include "libANGLE/renderer/vulkan/RendererVk.h"
@@ -1736,9 +1737,11 @@ SamplerDesc::SamplerDesc(const SamplerDesc &other) = default;
 
 SamplerDesc &SamplerDesc::operator=(const SamplerDesc &rhs) = default;
 
-SamplerDesc::SamplerDesc(const gl::SamplerState &samplerState, bool stencilMode)
+SamplerDesc::SamplerDesc(const gl::SamplerState &samplerState,
+                         bool stencilMode,
+                         uint64_t externalFormat)
 {
-    update(samplerState, stencilMode);
+    update(samplerState, stencilMode, externalFormat);
 }
 
 void SamplerDesc::reset()
@@ -1747,6 +1750,7 @@ void SamplerDesc::reset()
     mMaxAnisotropy  = 0.0f;
     mMinLod         = 0.0f;
     mMaxLod         = 0.0f;
+    mExternalFormat = 0;
     mMagFilter      = 0;
     mMinFilter      = 0;
     mMipmapMode     = 0;
@@ -1756,14 +1760,21 @@ void SamplerDesc::reset()
     mCompareEnabled = 0;
     mCompareOp      = 0;
     mReserved       = 0;
+    mPad            = 0;
 }
 
-void SamplerDesc::update(const gl::SamplerState &samplerState, bool stencilMode)
+// Need Vulkan version of SamplerState that contains the yuv conversion info
+void SamplerDesc::update(const gl::SamplerState &samplerState,
+                         bool stencilMode,
+                         uint64_t externalFormat)
 {
     mMipLodBias    = 0.0f;
     mMaxAnisotropy = samplerState.getMaxAnisotropy();
     mMinLod        = samplerState.getMinLod();
     mMaxLod        = samplerState.getMaxLod();
+
+    // GL has no notion of external format, this must be provided from metadata from the image
+    mExternalFormat = externalFormat;
 
     bool compareEnable    = samplerState.getCompareMode() == GL_COMPARE_REF_TO_TEXTURE;
     VkCompareOp compareOp = gl_vk::GetCompareOp(samplerState.getCompareFunc());
@@ -1834,6 +1845,24 @@ angle::Result SamplerDesc::init(ContextVk *contextVk, vk::Sampler *sampler) cons
         filteringInfo.samplerFilteringPrecisionMode =
             VK_SAMPLER_FILTERING_PRECISION_MODE_HIGH_GOOGLE;
         vk::AddToPNextChain(&createInfo, &filteringInfo);
+    }
+
+    VkSamplerYcbcrConversionInfo yuvConversionInfo = {};
+    if (mExternalFormat)
+    {
+        ASSERT((contextVk->getRenderer()->getFeatures().supportsYUVSamplerConversion.enabled));
+        yuvConversionInfo.sType = VK_STRUCTURE_TYPE_SAMPLER_YCBCR_CONVERSION_INFO;
+        yuvConversionInfo.pNext = nullptr;
+        ANGLE_TRY(contextVk->getRenderer()->getYuvConversionCache().getYuvConversion(
+            mExternalFormat, &yuvConversionInfo.conversion));
+        vk::AddToPNextChain(&createInfo, &yuvConversionInfo);
+
+        // Vulkan spec requires these settings:
+        createInfo.addressModeU            = VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE;
+        createInfo.addressModeV            = VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE;
+        createInfo.addressModeW            = VK_SAMPLER_ADDRESS_MODE_CLAMP_TO_EDGE;
+        createInfo.anisotropyEnable        = VK_FALSE;
+        createInfo.unnormalizedCoordinates = VK_FALSE;
     }
 
     ANGLE_VK_TRY(contextVk, sampler->init(contextVk->getDevice(), createInfo));
@@ -2157,6 +2186,72 @@ angle::Result PipelineLayoutCache::getPipelineLayout(
     auto insertedItem = mPayload.emplace(desc, vk::RefCountedPipelineLayout(std::move(newLayout)));
     vk::RefCountedPipelineLayout &insertedLayout = insertedItem.first->second;
     pipelineLayoutOut->set(&insertedLayout);
+
+    return angle::Result::Continue;
+}
+
+// YuvConversionCache implementation
+vk::SamplerYcbcrConversionCache::SamplerYcbcrConversionCache() = default;
+
+vk::SamplerYcbcrConversionCache::~SamplerYcbcrConversionCache()
+{
+    ASSERT(mPayload.empty());
+}
+
+void vk::SamplerYcbcrConversionCache::destroy(RendererVk *renderer)
+{
+    VkDevice device = renderer->getDevice();
+
+    for (auto &iter : mPayload)
+    {
+        vk::RefCountedSamplerYcbcrConversion &yuvSampler = iter.second;
+        ASSERT(!yuvSampler.isReferenced());
+        yuvSampler.get().destroy(device);
+
+        renderer->getActiveHandleCounts().onDeallocate(vk::HandleType::SamplerYcbcrConversion);
+    }
+
+    mPayload.clear();
+}
+
+angle::Result vk::SamplerYcbcrConversionCache::getYuvConversion(
+    uint64_t externalFormat,
+    VkSamplerYcbcrConversion *yuvConversion)
+{
+    auto iter = mPayload.find(externalFormat);
+    if (iter != mPayload.end())
+    {
+        *yuvConversion = iter->second.get().getHandle();
+        return angle::Result::Continue;
+    }
+
+    return angle::Result::Stop;
+}
+
+angle::Result vk::SamplerYcbcrConversionCache::createYuvConversion(
+    DisplayVk *displayVk,
+    uint64_t externalFormat,
+    VkSamplerYcbcrConversionCreateInfo *yuvConversionInfo)
+{
+    ASSERT(externalFormat != 0);
+
+    auto iter = mPayload.find(externalFormat);
+    if (iter != mPayload.end())
+    {
+        vk::RefCountedSamplerYcbcrConversion &refCountedYuvConversion = iter->second;
+        refCountedYuvConversion.addRef();
+        return angle::Result::Continue;
+    }
+
+    RendererVk *renderer = displayVk->getRenderer();
+    VkDevice device      = renderer->getDevice();
+
+    SamplerYcbcrConversion wrappedYuvConversion;
+    wrappedYuvConversion.init(device, *yuvConversionInfo);
+    mPayload.emplace(externalFormat,
+                     vk::RefCountedSamplerYcbcrConversion(std::move(wrappedYuvConversion)));
+
+    renderer->getActiveHandleCounts().onAllocate(vk::HandleType::SamplerYcbcrConversion);
 
     return angle::Result::Continue;
 }
