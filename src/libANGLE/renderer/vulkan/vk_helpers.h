507f37e0af2b50ba700376df02d451d36cc5a40f
Courtney Goeltzenleuchter <courtneygo@google.com>
2020-05-12 09:45:21 -0600

    Add support for VK_KHR_sampler_ycbcr_conversion
    Angry Birds 2 video player (for ads) requires YUV conversion.
    
    Bug: b/155487768
    Change-Id: I9255450f81aa4daa2aace7205d4f6c3f225abcca

--- src/libANGLE/renderer/vulkan/vk_helpers.h
+++ src/libANGLE/renderer/vulkan/vk_helpers.h
@@ -1376,6 +1376,11 @@ class ImageHelper final : public Resource, public angle::Subject
                                       GLuint *inputDepthPitch,
                                       GLuint *inputSkipBytes);
 
+    void setExternalFormat(uint64_t externalFormat) { mExternalFormat = externalFormat; }
+
+    uint64_t getExternalFormat() { return mExternalFormat; }
+    bool hasImmutableSampler() { return mExternalFormat != 0; }
+
   private:
     enum class UpdateSource
     {
@@ -1487,6 +1492,9 @@ class ImageHelper final : public Resource, public angle::Subject
     // For optimizing transition between different shader readonly layouts
     ImageLayout mLastNonShaderReadOnlyLayout;
     VkPipelineStageFlags mCurrentShaderReadStageMask;
+
+    // For imported images
+    uint64_t mExternalFormat;
 
     // Cached properties.
     uint32_t mBaseLevel;
