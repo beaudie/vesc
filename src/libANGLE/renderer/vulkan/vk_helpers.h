2bad72fd3e05d3ae09d8709820044170d34287ac
Michael Spang <spang@chromium.org>
2020-05-11 22:56:18 -0400

    Vulkan: Tighten up asserts for resource ownership
    We're not permitted to make accesses to a resource while it's owned by an
    external instance or API. Add some asserts to verify this.
    
    Exempt images on platforms that don't implement external memory barriers.
    
    Bug: angleproject:3289
    Change-Id: I1de929f6a412bfe5c1b798eaa1dc401bbceb5b7f

--- src/libANGLE/renderer/vulkan/vk_helpers.h
+++ src/libANGLE/renderer/vulkan/vk_helpers.h
@@ -772,6 +772,9 @@ class BufferHelper final : public Resource
                            uint32_t externalQueueFamilyIndex,
                            CommandBuffer *commandBuffer);
 
+    // Returns true if the image is owned by an external API or instance.
+    bool isReleasedToExternal() const;
+
     // Currently always returns false. Should be smarter about accumulation.
     bool canAccumulateRead(ContextVk *contextVk, VkAccessFlags readAccessType);
     bool canAccumulateWrite(ContextVk *contextVk, VkAccessFlags writeAccessType);
@@ -1293,6 +1296,9 @@ class ImageHelper final : public Resource, public angle::Subject
                            uint32_t externalQueueFamilyIndex,
                            ImageLayout desiredLayout,
                            CommandBuffer *commandBuffer);
+
+    // Returns true if the image is owned by an external API or instance.
+    bool isReleasedToExternal() const;
 
     // If the image is used externally to GL, its layout could be different from ANGLE's internal
     // state.  This function is used to inform ImageHelper of an external layout change.
