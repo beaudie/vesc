cb18eb8f9737c907ae11344a98f360b9642250d0
Tobin Ehlis <tobine@google.com>
2020-05-19 17:27:50 -0600

    Vulkan:Migrate timer sync functions to one-off cmd buffer
    ContextVk's getTimestamp() and synchronizeCpuGpuTime() were the two
    places that mCommandQueue was used outside of processing
    SecondaryCommandBuffers. Migrating those functions to use the one-
    off command buffer instead. This will allow for the removal of
    mCommandQueue from ContextVk as we migrate it to RendererVk
    instead for threading phase2 development.
    
    Bug: b/154030730
    Change-Id: Ic00ce56739dadcdc4b5a1b1839e0e18603bfe0eb

--- src/libANGLE/renderer/vulkan/vk_helpers.cpp
+++ src/libANGLE/renderer/vulkan/vk_helpers.cpp
@@ -2193,7 +2193,7 @@ angle::Result BufferHelper::initializeNonZeroMemory(Context *context, VkDeviceSi
 
     Serial serial;
     ANGLE_TRY(renderer->queueSubmitOneOff(context, std::move(commandBuffer),
-                                          egl::ContextPriority::Medium, &serial));
+                                          egl::ContextPriority::Medium, nullptr, &serial));
 
     stagingBuffer.collectGarbage(renderer, serial);
     mUse.updateSerialOneOff(serial);
@@ -2588,7 +2588,7 @@ angle::Result ImageHelper::initializeNonZeroMemory(Context *context, VkDeviceSiz
 
     Serial serial;
     ANGLE_TRY(renderer->queueSubmitOneOff(context, std::move(commandBuffer),
-                                          egl::ContextPriority::Medium, &serial));
+                                          egl::ContextPriority::Medium, nullptr, &serial));
 
     stagingBuffer.collectGarbage(renderer, serial);
     mUse.updateSerialOneOff(serial);
