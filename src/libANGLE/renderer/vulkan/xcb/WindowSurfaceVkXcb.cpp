6994725372fcb51529bff6469888c20560cae01b
Jonah Ryan-Davis <jonahr@google.com>
2020-05-19 14:03:42 -0400

    Add error-handling when getting Xcb window size
    We can't trust the reply from Xcb if an error has been flagged.
    This was causing a crash in SwANGLE.
    
    Bug: chromium:1080984
    Change-Id: Icaf58197869b563e4193efcf4e70646d519210cf

--- src/libANGLE/renderer/vulkan/xcb/WindowSurfaceVkXcb.cpp
+++ src/libANGLE/renderer/vulkan/xcb/WindowSurfaceVkXcb.cpp
@@ -39,7 +39,13 @@ angle::Result WindowSurfaceVkXcb::getCurrentWindowSize(vk::Context *context,
 {
     xcb_get_geometry_cookie_t cookie =
         xcb_get_geometry(mXcbConnection, static_cast<xcb_drawable_t>(mNativeWindowType));
-    xcb_get_geometry_reply_t *reply = xcb_get_geometry_reply(mXcbConnection, cookie, nullptr);
+    xcb_generic_error_t *error      = nullptr;
+    xcb_get_geometry_reply_t *reply = xcb_get_geometry_reply(mXcbConnection, cookie, &error);
+    if (error)
+    {
+        free(error);
+        return angle::Result::Stop;
+    }
     ASSERT(reply);
     *extentsOut = gl::Extents(reply->width, reply->height, 1);
     free(reply);
