82f465bc36775744b9b02fd74517141170f36a4d
Tobin Ehlis <tobine@google.com>
2020-05-20 17:27:34 -0600

    WIP:Vulkan:Working on phase2 threading code
    Working on enhancing worker thread to completely own primary command
    buffers. This will include not only processing SCBs from main thread
    into a primary, but also submitting those command buffers to the queue.
    
    Currently have a rough port of ContextVk's CommandQueue to a new
    interface in the RendererVk as CommandWorkQueue.
    
    Need to start testing and lots of clean-up to do.
    Also, need an over-arching error handling solution to deal with
    errors between worker thread and main thread.
    
    Bug: b/154030730
    Change-Id: Ieaacb0aaa108206713f71744f803d1c2d16286f6

--- src/libANGLE/renderer/vulkan/CommandProcessor.h
+++ src/libANGLE/renderer/vulkan/CommandProcessor.h
@@ -16,16 +16,66 @@
 #include <queue>
 #include <thread>
 
+#include "libANGLE/renderer/vulkan/PersistentCommandPool.h"
 #include "libANGLE/renderer/vulkan/vk_headers.h"
 #include "libANGLE/renderer/vulkan/vk_helpers.h"
 
 namespace rx
 {
+class RendererVk;
+
+class CommandWorkQueue final : angle::NonCopyable
+{
+  public:
+    CommandWorkQueue();
+    ~CommandWorkQueue();
+
+    angle::Result init(RendererVk *context);
+    void destroy(VkDevice device);
+    void handleDeviceLost(RendererVk *renderer);
+
+    bool hasInFlightCommands() const;
+
+    angle::Result allocatePrimaryCommandBuffer(VkDevice device,
+                                               vk::PrimaryCommandBuffer *commandBufferOut);
+    angle::Result releasePrimaryCommandBuffer(vk::PrimaryCommandBuffer &&commandBuffer);
+
+    void clearAllGarbage(RendererVk *renderer);
+
+    angle::Result finishToSerial(RendererVk *renderer, Serial serial);
+
+    angle::Result submitFrame(RendererVk *renderer,
+                              egl::ContextPriority priority,
+                              const VkSubmitInfo &submitInfo,
+                              const vk::Shared<vk::Fence> &sharedFence,
+                              vk::GarbageList *currentGarbage,
+                              vk::CommandPool *commandPool,
+                              vk::PrimaryCommandBuffer &&commandBuffer);
+
+    vk::Shared<vk::Fence> getLastSubmittedFence(const VkDevice device) const;
+
+    // Check to see which batches have finished completion (forward progress for
+    // mLastCompletedQueueSerial, for example for when the application busy waits on a query
+    // result). It would be nice if we didn't have to expose this for QueryVk::getResult.
+    angle::Result checkCompletedCommands(RendererVk *renderer);
+
+  private:
+    angle::Result releaseToCommandBatch(RendererVk *renderer,
+                                        vk::PrimaryCommandBuffer &&commandBuffer,
+                                        vk::CommandPool *commandPool,
+                                        vk::CommandBatch *batch);
+
+    vk::GarbageQueue mGarbageQueue;
+    std::vector<vk::CommandBatch> mInFlightCommands;
+
+    // Keeps a free list of reusable primary command buffers.
+    vk::PersistentCommandPool mPrimaryCommandPool;
+};
 
 class CommandProcessor : angle::NonCopyable
 {
   public:
-    CommandProcessor();
+    CommandProcessor(RendererVk *renderer);
     ~CommandProcessor() = default;
 
     // Main worker loop that should be launched in its own thread. The
@@ -37,6 +87,18 @@ class CommandProcessor : angle::NonCopyable
     // Used by separate thread to wait for worker thread to complete all
     //  outstanding work.
     void waitForWorkComplete();
+    void clearAllGarbage() { mCommandWorkQueue.clearAllGarbage(mRenderer); }
+    angle::Result checkCompletedCommands()
+    {
+        return mCommandWorkQueue.checkCompletedCommands(mRenderer);
+    }
+    angle::Result finishToSerial(Serial serial)
+    {
+        return mCommandWorkQueue.finishToSerial(mRenderer, serial);
+    }
+    vk::Shared<vk::Fence> getLastSubmittedFence() const;
+    bool hasInFlightCommandBuffers() { return mCommandWorkQueue.hasInFlightCommands(); }
+    void handleDeviceLost() { mCommandWorkQueue.handleDeviceLost(mRenderer); }
 
   private:
     std::queue<vk::CommandProcessorTask> mCommandsQueue;
@@ -47,6 +109,11 @@ class CommandProcessor : angle::NonCopyable
     std::condition_variable mWorkerIdleCondition;
     // Track worker thread Idle state for assertion purposes
     bool mWorkerThreadIdle;
+    CommandWorkQueue mCommandWorkQueue;
+    // Command pool to allocate processor thread primary command buffers from
+    vk::CommandPool mCommandPool;
+    vk::PrimaryCommandBuffer mPrimaryCommandBuffer;
+    RendererVk *mRenderer;
 };
 
 }  // namespace rx
