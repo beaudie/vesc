fc009713f236d70992385b310c0c9d79e4b7ad37
Tobin Ehlis <tobine@google.com>
2020-06-01 17:00:39 -0600

    WIP:Vulkan:Working on phase2 threading code
    Working on enhancing worker thread to completely own primary command
    buffers. This will include not only processing SCBs from main thread
    into a primary, but also submitting those command buffers to the queue.
    
    Currently have a rough port of ContextVk's CommandQueue to a new
    interface in the RendererVk as CommandWorkQueue.
    
    Need to start testing and lots of clean-up to do.
    Also, need an over-arching error handling solution to deal with
    errors between worker thread and main thread.
    
    Bug: b/154030730
    Change-Id: Ieaacb0aaa108206713f71744f803d1c2d16286f6

--- src/libANGLE/renderer/vulkan/RendererVk.cpp
+++ src/libANGLE/renderer/vulkan/RendererVk.cpp
@@ -440,6 +440,7 @@ RendererVk::RendererVk()
       mPipelineCacheVkUpdateTimeout(kPipelineCacheVkUpdatePeriod),
       mPipelineCacheDirty(false),
       mPipelineCacheInitialized(false),
+      mCommandProcessor(this),
       mGlslangInitialized(false)
 {
     VkFormatProperties invalid = {0, 0, kInvalidFormatFeatureFlags};
@@ -1726,7 +1727,7 @@ void RendererVk::initFeatures(DisplayVk *displayVk, const ExtensionNameList &dev
     ANGLE_FEATURE_CONDITION(&mFeatures, preferAggregateBarrierCalls, isNvidia || isAMD || isIntel);
 
     // Currently disabled by default: http://anglebug.com/4324
-    ANGLE_FEATURE_CONDITION(&mFeatures, enableCommandProcessingThread, false);
+    ANGLE_FEATURE_CONDITION(&mFeatures, enableCommandProcessingThread, true);
 
     angle::PlatformMethods *platform = ANGLEPlatformCurrent();
     platform->overrideFeaturesVk(platform, &mFeatures);
@@ -1982,6 +1983,29 @@ angle::Result RendererVk::queueSubmit(vk::Context *context,
     return angle::Result::Continue;
 }
 
+angle::Result RendererVk::commandProcessorThreadQueueSubmit(vk::Context *context,
+                                                            egl::ContextPriority priority,
+                                                            const VkSubmitInfo &submitInfo,
+                                                            const vk::Fence *fence,
+                                                            Serial *serialOut)
+{
+    ASSERT(getFeatures().enableCommandProcessingThread.enabled);
+
+    {
+        std::lock_guard<decltype(mQueueMutex)> lock(mQueueMutex);
+        VkFence handle = fence ? fence->getHandle() : VK_NULL_HANDLE;
+        ANGLE_VK_TRY(context, vkQueueSubmit(mQueues[priority], 1, &submitInfo, handle));
+    }
+
+    ANGLE_TRY(cleanupGarbage(false));
+
+    *serialOut                = mCurrentQueueSerial;
+    mLastSubmittedQueueSerial = mCurrentQueueSerial;
+    mCurrentQueueSerial       = mQueueSerialFactory.generate();
+
+    return angle::Result::Continue;
+}
+
 angle::Result RendererVk::queueSubmitOneOff(vk::Context *context,
                                             vk::PrimaryCommandBuffer &&primary,
                                             egl::ContextPriority priority,
@@ -2045,6 +2069,20 @@ VkResult RendererVk::queuePresent(egl::ContextPriority priority,
         //  present may have dependencies on that thread.
         mCommandProcessor.waitForWorkComplete();
     }
+
+    std::lock_guard<decltype(mQueueMutex)> lock(mQueueMutex);
+
+    {
+        ANGLE_TRACE_EVENT0("gpu.angle", "vkQueuePresentKHR");
+        return vkQueuePresentKHR(mQueues[priority], &presentInfo);
+    }
+}
+
+VkResult RendererVk::commandProcessorThreadQueuePresent(egl::ContextPriority priority,
+                                                        const VkPresentInfoKHR &presentInfo)
+{
+    ANGLE_TRACE_EVENT0("gpu.angle", "RendererVk::commandProcessorThreadQueuePresent");
+    ASSERT(getFeatures().enableCommandProcessingThread.enabled);
 
     std::lock_guard<decltype(mQueueMutex)> lock(mQueueMutex);
 
