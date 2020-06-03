4fe6c0218dd2b81f9fcd781652e5b9ded54d5a4d
Manh Nguyen <nguyenmh@google.com>
2020-06-03 18:24:17 -0400

    Fill in missing GL methods for MemorySizeTest
    Implements captures of glGetRenderbufferParameteriv,
    glGetBufferParameteriv, and glgetAttachedShader. Fix FrameCapture and
    CaptureReplay sample so that it has single-frame capture and replay
    capabilities.
    
    Bug: angleproject:4681
    Bug: angleproject:4682
    Change-Id: I12c25a3857a88f2f40b3c3e8624da1379a950339

--- samples/capture_replay/CaptureReplay.cpp
+++ samples/capture_replay/CaptureReplay.cpp
@@ -60,9 +60,11 @@ class CaptureReplaySample : public SampleApplication
     void draw() override
     {
         // Compute the current frame, looping from kReplayFrameStart to kReplayFrameEnd.
-        uint32_t frame =
-            kReplayFrameStart + (mCurrentFrame % (kReplayFrameEnd - kReplayFrameStart));
-
+        uint32_t frame = kReplayFrameStart;
+        if (kReplayFrameStart != kReplayFrameEnd)
+        {
+            frame = kReplayFrameStart + (mCurrentFrame % (kReplayFrameEnd - kReplayFrameStart));
+        }
         if (mPreviousFrame > frame)
         {
             ResetContextReplay();
