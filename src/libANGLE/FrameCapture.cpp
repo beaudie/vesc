4fc383cc144415d371f86c37e8bc294d4bbddb9c
Manh Nguyen <nguyenmh@google.com>
2020-06-03 10:45:34 -0400

    Fix ExceedMaxVertexAtomicCounters test crash with FrameCapture
    The AtomicCounterBufferTest31.ExceedMaxVertexAtomicCounters crash is
    because when program linking fails, as it should for this test, the
    code tries to get the info log by calling glGetProgramInfoLog. The
    FrameCapture capturing this call currently tries to make sure that the
    program is linked. However, glGetProgramInfoLog does not require the
    program to be linked to execute.
    
    Bug: angleproject:4679
    Change-Id: Ibbb87d6437152e9eb225e411100b1729e56e4c4a

--- src/libANGLE/FrameCapture.cpp
+++ src/libANGLE/FrameCapture.cpp
@@ -3891,10 +3891,9 @@ void CaptureStringLimit(const GLchar *str, uint32_t limit, ParamCapture *paramCa
     }
 }
 
-gl::Program *GetLinkedProgramForCapture(const gl::State &glState, gl::ShaderProgramID handle)
+gl::Program *GetProgramForCapture(const gl::State &glState, gl::ShaderProgramID handle)
 {
     gl::Program *program = glState.getShaderProgramManagerForCapture().getProgram(handle);
-    ASSERT(program->isLinked());
     return program;
 }
 
