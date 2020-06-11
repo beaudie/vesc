0463be2ee7b41a80691b5c041e70d7ca2d567f4f
Jonah Ryan-Davis <jonahr@google.com>
2020-06-11 11:30:14 -0400

    Add basic multithreading support to GLX
    The first step for true multithreading support is allowing multiple
    threads to use ANGLE as long as only one thread has a current context
    at a time.
    Added a test to MultithreadingTests to reproduce this behavior and
    implemented it in GLX.
    
    Bug: angleproject:4724
    Bug: angleproject:4725
    Bug: chromium:1087084
    Change-Id: I908d3f02f34a681f1c9d0919dd17aee1a489173c

--- src/tests/gl_tests/MultithreadingTest.cpp
+++ src/tests/gl_tests/MultithreadingTest.cpp
@@ -9,6 +9,7 @@
 
 #include "test_utils/gl_raii.h"
 #include "util/EGLWindow.h"
+#include "util/Timer.h"
 
 #include <mutex>
 #include <thread>
@@ -199,6 +200,66 @@ TEST_P(MultithreadingTest, MultiContextDraw)
         }
     };
     runMultithreadedGLTest(testBody, 4);
+}
+
+TEST_P(MultithreadingTest, MultiCreateContext)
+{
+    // Supported by WGL and GLX (https://anglebug.com/4725)
+    ANGLE_SKIP_TEST_IF(!IsWindows() && !IsLinux());
+
+    std::mutex mutex;
+
+    EGLWindow *window  = getEGLWindow();
+    EGLDisplay dpy     = window->getDisplay();
+    EGLContext ctx     = window->getContext();
+    EGLSurface surface = window->getSurface();
+
+    // Un-makeCurrent the test window's context
+    EXPECT_EGL_TRUE(eglMakeCurrent(dpy, EGL_NO_SURFACE, EGL_NO_SURFACE, EGL_NO_CONTEXT));
+    EXPECT_EGL_SUCCESS();
+
+    Timer timer;
+    timer.start();
+    constexpr double kTimeout     = 3.0;
+    constexpr size_t kThreadCount = 16;
+    uint32_t barrier              = 0;
+    std::vector<std::thread> threads(kThreadCount);
+    std::vector<EGLContext> contexts(kThreadCount);
+    for (size_t threadIdx = 0; threadIdx < kThreadCount; threadIdx++)
+    {
+        threads[threadIdx] = std::thread([&, threadIdx]() {
+            contexts[threadIdx] = EGL_NO_CONTEXT;
+            {
+                std::lock_guard<decltype(mutex)> lock(mutex);
+
+                contexts[threadIdx] = window->createContext(EGL_NO_CONTEXT);
+                EXPECT_NE(EGL_NO_CONTEXT, contexts[threadIdx]);
+
+                barrier++;
+            }
+
+            while (barrier < kThreadCount && timer.getElapsedTime() < kTimeout)
+            {
+            }
+
+            {
+                std::lock_guard<decltype(mutex)> lock(mutex);
+
+                EXPECT_TRUE(eglDestroyContext(dpy, contexts[threadIdx]));
+            }
+        });
+    }
+
+    for (std::thread &thread : threads)
+    {
+        thread.join();
+    }
+
+    EXPECT_LT(timer.getElapsedTime(), kTimeout);
+
+    // Re-make current the test window's context for teardown.
+    EXPECT_EGL_TRUE(eglMakeCurrent(dpy, surface, surface, ctx));
+    EXPECT_EGL_SUCCESS();
 }
 
 // TODO(geofflang): Test sharing a program between multiple shared contexts on multiple threads
