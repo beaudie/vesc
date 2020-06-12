0278f4cdf6530a9dd26ffe6035cee64eaef255d7
Mohan Maiya <m.maiya@samsung.com>
2020-06-12 08:17:47 -0700

    Optimize thread specific storage and retrieval with native ASM
    Android reserves specific TLS slots to store thread specific
    values. Given that the Context object gets queried on every
    call we leverage Android's ASM code to improve the speed of
    this operation.
    
    TLS_SLOT_OPENGL is an unused slot in Android and using that
    in combination with the ASM code,rather than using the
    pthread API, allows angle to store and retrieve thread specific
    context object much more efficiently.
    
    Bug: angleproject:4717
    Change-Id: I27a117fe82e62407e01c8c372918b866aaea9ee5

--- src/common/tls.h
+++ src/common/tls.h
@@ -9,6 +9,7 @@
 #ifndef COMMON_TLS_H_
 #define COMMON_TLS_H_
 
+#include "common/angleutils.h"
 #include "common/platform.h"
 
 #ifdef ANGLE_PLATFORM_WINDOWS
@@ -33,6 +34,84 @@ typedef pthread_key_t TLSIndex;
 #else
 #    error Unsupported platform.
 #endif
+
+#if defined(__aarch64__)
+#    define __get_tls()                                 \
+        ({                                              \
+            void **__val;                               \
+            __asm__("mrs %0, tpidr_el0" : "=r"(__val)); \
+            __val;                                      \
+        })
+#elif defined(__arm__)
+#    define __get_tls()                                          \
+        ({                                                       \
+            void **__val;                                        \
+            __asm__("mrc p15, 0, %0, c13, c0, 3" : "=r"(__val)); \
+            __val;                                               \
+        })
+#elif defined(__mips__)
+#    define __get_tls()                                                                      \
+        /* On mips32r1, this goes via a kernel illegal instruction trap that's optimized for \
+         * v1. */                                                                            \
+        ({                                                                                   \
+            register void **__val asm("v1");                                                 \
+            __asm__(                                                                         \
+                ".set    push\n"                                                             \
+                ".set    mips32r2\n"                                                         \
+                "rdhwr   %0,$29\n"                                                           \
+                ".set    pop\n"                                                              \
+                : "=r"(__val));                                                              \
+            __val;                                                                           \
+        })
+#elif defined(__i386__)
+#    define __get_tls()                               \
+        ({                                            \
+            void **__val;                             \
+            __asm__("movl %%gs:0, %0" : "=r"(__val)); \
+            __val;                                    \
+        })
+#elif defined(__x86_64__)
+#    define __get_tls()                              \
+        ({                                           \
+            void **__val;                            \
+            __asm__("mov %%fs:0, %0" : "=r"(__val)); \
+            __val;                                   \
+        })
+#else
+#    error unsupported architecture
+#endif
+
+//  - TLS_SLOT_OPENGL and TLS_SLOT_OPENGL_API: These two aren't used by bionic
+//    itself, but allow the graphics code to access TLS directly rather than
+//    using the pthread API.
+//
+// Choose the TLS_SLOT_OPENGL TLS slot with the value that matches value in the header file in
+// bionic(bionic_asm_tls.h)
+// #define TLS_SLOT_OPENGL           3
+constexpr TLSIndex kContextTlsSlot = 3;
+
+extern bool gUseFastContextTls;
+ANGLE_INLINE bool SetContextTLSFast(void *value)
+{
+    if (gUseFastContextTls == true)
+    {
+        __get_tls()[kContextTlsSlot] = value;
+        return true;
+    }
+    return false;
+}
+
+ANGLE_INLINE bool GetContextTLSFast(void **value)
+{
+    if (gUseFastContextTls == true)
+    {
+        *value = __get_tls()[kContextTlsSlot];
+        return true;
+    }
+    return false;
+}
+
+void SetPlatformType(bool platformTypeVulkan);
 
 // TODO(kbr): for POSIX platforms this will have to be changed to take
 // in a destructor function pointer, to allow the thread-local storage
