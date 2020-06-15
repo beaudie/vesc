a53aec99630223a8df3feb24599280c2c2c74e16
Manh Nguyen <nguyenmh@google.com>
2020-06-15 10:59:18 -0400

    Automate testing of capture_replay
    automation script will:
    1. compile all tests in angle_end2end with framecapture enabled
    2. run each test with frame capture
    3. compile CaptureReplayTest with cpp trace files
    4. Run CaptureReplayTest
    
    Bug: angleproject:4749
    Change-Id: Ieb21c6137653632d73beedea48712e46fc15a0f3

---
+++ src/tests/capture_replay_tests/CaptureReplayTest.h
@@ -0,0 +1,49 @@
+//
+// Copyright 2014 The ANGLE Project Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+
+#ifndef CAPTURE_REPLAY_TEST_H
+#define CAPTURE_REPLAY_TEST_H
+
+#include <stdint.h>
+#include <list>
+#include <memory>
+#include <string>
+
+#include "common/system_utils.h"
+#include "util/EGLPlatformParameters.h"
+#include "util/OSWindow.h"
+#include "util/egl_loader_autogen.h"
+
+class EGLWindow;
+
+namespace angle
+{
+class Library;
+}  // namespace angle
+
+class CaptureReplayTest
+{
+  public:
+    CaptureReplayTest(int width, int height, EGLint glesMajorVersion, EGLint glesMinorVersion);
+    ~CaptureReplayTest();
+    bool initialize();
+    void draw();
+    void swap();
+    int run();
+
+  private:
+    uint32_t mWidth;
+    uint32_t mHeight;
+    OSWindow *mOSWindow;
+    EGLWindow *mEGLWindow;
+
+    EGLPlatformParameters mPlatformParams;
+
+    // Handle to the entry point binding library.
+    std::unique_ptr<angle::Library> mEntryPointsLib;
+};
+
+#endif  // CAPTURE_REPLAY_TEST_H
