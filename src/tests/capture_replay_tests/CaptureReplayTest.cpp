3427109e812eeffe4978732fa71ce7017d11fec9
Manh Nguyen <nguyenmh@google.com>
2020-06-15 10:59:18 -0400

    Automate testing of capture_replay
    automation script will:
    1. compile all tests in angle_end2end with framecapture enabled
    2. run each test with frame capture
    3. compile CaptureReplayTest with cpp trace files
    4. Run CaptureReplayTest
    
    Test: Run the script run_tests_with_framecapture.py to run this test
    Bug: angleproject:4749
    Change-Id: Ieb21c6137653632d73beedea48712e46fc15a0f3

---
+++ src/tests/capture_replay_tests/CaptureReplayTest.cpp
@@ -0,0 +1,136 @@
+//
+// Copyright 2020 The ANGLE Project Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+
+#include "CaptureReplayTest.h"
+
+#include "util/EGLWindow.h"
+#include "util/gles_loader_autogen.h"
+
+#include <string.h>
+#include <functional>
+#include <iostream>
+#include <utility>
+#include "util/frame_capture_utils.h"
+
+// Build the right context header based on replay ID
+// This will expand to "angle_capture_context<#>.h"
+#include ANGLE_MACRO_STRINGIZE(ANGLE_CAPTURE_REPLAY_TEST_HEADER)
+
+// Assign the context numbered functions based on GN arg selecting replay ID
+std::function<void()> SetupContextReplay = reinterpret_cast<void (*)()>(
+    ANGLE_MACRO_CONCAT(SetupContext,
+                       ANGLE_MACRO_CONCAT(ANGLE_CAPTURE_REPLAY_TEST_CONTEXT_ID, Replay)));
+std::function<void(int)> ReplayContextFrame = reinterpret_cast<void (*)(int)>(
+    ANGLE_MACRO_CONCAT(ReplayContext,
+                       ANGLE_MACRO_CONCAT(ANGLE_CAPTURE_REPLAY_TEST_CONTEXT_ID, Frame)));
+std::function<void()> ResetContextReplay = reinterpret_cast<void (*)()>(
+    ANGLE_MACRO_CONCAT(ResetContext,
+                       ANGLE_MACRO_CONCAT(ANGLE_CAPTURE_REPLAY_TEST_CONTEXT_ID, Replay)));
+
+CaptureReplayTest::CaptureReplayTest(int width,
+                                     int height,
+                                     EGLint glesMajorVersion,
+                                     EGLint glesMinorVersion)
+    : mWidth(width), mHeight(height), mOSWindow(nullptr), mEGLWindow(nullptr)
+
+{
+    mPlatformParams.renderer   = EGL_PLATFORM_ANGLE_TYPE_DEFAULT_ANGLE;
+    mPlatformParams.deviceType = EGL_PLATFORM_ANGLE_DEVICE_TYPE_HARDWARE_ANGLE;
+
+    // Load EGL library so we can initialize the display.
+    mEntryPointsLib.reset(
+        angle::OpenSharedLibrary(ANGLE_EGL_LIBRARY_NAME, angle::SearchType::ApplicationDir));
+
+    mEGLWindow = EGLWindow::New(glesMajorVersion, glesMinorVersion);
+    mOSWindow  = OSWindow::New();
+}
+
+CaptureReplayTest::~CaptureReplayTest()
+{
+    EGLWindow::Delete(&mEGLWindow);
+    OSWindow::Delete(&mOSWindow);
+}
+
+bool CaptureReplayTest::initialize()
+{
+    // Set CWD to executable directory.
+    std::string exeDir = angle::GetExecutableDirectory();
+    if (!angle::SetCWD(exeDir.c_str()))
+        return false;
+    if (kIsBinaryDataCompressed)
+    {
+        SetBinaryDataDecompressCallback(angle::DecompressBinaryData);
+    }
+    SetBinaryDataDir(ANGLE_CAPTURE_REPLAY_TEST_DATA_DIR);
+    SetupContextReplay();
+
+    eglSwapInterval(mEGLWindow->getDisplay(), 1);
+    return true;
+}
+
+void CaptureReplayTest::draw()
+{
+    ReplayContextFrame(0);
+}
+
+void CaptureReplayTest::swap()
+{
+    mEGLWindow->swap();
+}
+
+int CaptureReplayTest::run()
+{
+    if (!mOSWindow->initialize("Capture Replay Test", mWidth, mHeight))
+    {
+        return -1;
+    }
+
+    mOSWindow->setVisible(true);
+
+    ConfigParameters configParams;
+    configParams.redBits     = 8;
+    configParams.greenBits   = 8;
+    configParams.blueBits    = 8;
+    configParams.alphaBits   = 8;
+    configParams.depthBits   = 24;
+    configParams.stencilBits = 8;
+
+    if (!mEGLWindow->initializeGL(mOSWindow, mEntryPointsLib.get(),
+        angle::GLESDriverType::AngleEGL, mPlatformParams, configParams))
+    {
+        return -1;
+    }
+
+    // Disable vsync
+    if (!mEGLWindow->setSwapInterval(0))
+    {
+        return -1;
+    }
+
+    angle::LoadGLES(eglGetProcAddress);
+
+    int result = 0;
+
+    if (!initialize())
+    {
+        result = -1;
+    }
+
+    draw();
+    swap();
+
+    mEGLWindow->destroyGL();
+    mOSWindow->destroy();
+
+    return result;
+}
+
+int main(int argc, char **argv)
+{
+    //TODO(nguyenmh): initialize app with arguments taken from cmdline
+    CaptureReplayTest app(128, 128, 2, 0);
+    return app.run();
+}
---
+++ src/tests/run_tests_with_framecapture.py
@@ -0,0 +1,129 @@
+#
+# Copyright 2020 The ANGLE Project Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+"""
+Script testing capture_replay with angle_end2end_tests
+"""
+
+#automation script will:
+#1. compile all tests in angle_end2end with framecapture enabled
+#2. run each test with frame capture
+#3. compile CaptureReplayTest with cpp trace files
+#4. Run CaptureReplayTest
+#5. Output the number of test successes and failures. A test succeeds if no error occurs during
+# its capture and replay, and the GL states at the end of two runs match
+
+# run this script with Python to test capture replay on angle_end2end tests
+# python path/to/run_tests_with_framecapture.py
+# command line arguments:
+# --compile_dir: specifies compile directory relative to angle folder. default is out\Debug
+
+import subprocess
+import os
+import argparse
+from sys import platform
+
+class Test():
+
+    def __init__(self, full_test_name, params):
+        self.full_test_name = full_test_name
+        self.params = params
+
+    def __str__(self):
+        return self.full_test_name + " Params: " + self.params
+
+    def Run(self, test_exe_path):
+        output = subprocess.check_output(
+            '"' + test_exe_path + '" --gtest_filter=' + self.full_test_name, shell=True)
+        print("Ran " + self.full_test_name + " with capture")
+        return output
+
+    def CompileReplay(self, compile_dir, replay_exec):
+        subprocess.check_output(
+            'gn gen --args="use_goma=true angle_with_capture_by_default=true ' +
+            'angle_build_capture_replay_test=true" ' +
+            compile_dir,
+            shell=True)
+        subprocess.check_output("autoninja " + replay_exec + " -C " + compile_dir, shell=True)
+        print("Compiled replay of " + self.full_test_name)
+
+    def RunReplay(self, compile_dir, replay_exec_with_ext):
+        subprocess.check_output('"' + compile_dir + '/' + replay_exec_with_ext + '"', shell=True)
+        print("Played replay of " + self.full_test_name)
+
+
+def SetCWDToAngleFolder():
+    angle_folder = "angle"
+    cwd = os.path.dirname(os.path.abspath(__file__))
+    cwd = cwd.split(angle_folder)[0] + angle_folder
+    os.chdir(cwd)
+    return cwd
+
+
+def main(COMPILE_DIR):
+    CWD = SetCWDToAngleFolder()
+    CAPTURE_OUT_DIR = "src/tests/capture_replay_tests"  # relative to CWD, angle folder
+    environment_vars = [("ANGLE_CAPTURE_FRAME_END", "0"),
+                        ("ANGLE_CAPTURE_OUT_DIR", CAPTURE_OUT_DIR)]
+    TEST_EXEC = "angle_end2end_tests"
+    REPLAY_EXEC = "capture_replay_test"
+    if platform == "win32":
+        TEST_EXEC_WITH_EXT = TEST_EXEC + ".exe"
+        REPLAY_EXEC_WITH_EXT = REPLAY_EXEC + ".exe"
+    else:
+        TEST_EXEC_WITH_EXT = TEST_EXEC
+        REPLAY_EXEC_WITH_EXT = REPLAY_EXEC
+    # generate gn files
+    subprocess.call(
+        'gn gen --args="use_goma=true angle_with_capture_by_default=true" ' + COMPILE_DIR,
+        shell=True)
+    print("Generated ninja files")
+    # compile angle_end2end
+    subprocess.call("autoninja " + TEST_EXEC + " -C " + COMPILE_DIR, shell=True)
+    print("Compiled all tests in angle_end2end")
+    # get a list of tests
+    output = subprocess.check_output(
+        '"' + COMPILE_DIR + '/' + TEST_EXEC_WITH_EXT + '" --gtest_list_tests',
+        shell=True).splitlines()
+
+    all_tests = []
+    last_testcase_name = ""
+    test_name_splitter = "# GetParam() ="
+    for line in output:
+        if test_name_splitter in line:
+            # must be a test name line
+            test_name_and_params = line.split(test_name_splitter)
+            all_tests.append(
+                Test(last_testcase_name + test_name_and_params[0].strip(),
+                     test_name_and_params[1].strip()))
+        else:
+            # might not be a test case name line but by the time we use it to concatenate
+            # with a test name, it will have been updated correctly to a test case
+            last_testcase_name = line
+    for environment_var in environment_vars:
+        os.environ[environment_var[0]] = environment_var[1]
+
+    replay_file_created_keyword = "Saved "
+    for test in all_tests:
+        print(test)
+        os.environ["ANGLE_CAPTURE_ENABLED"] = "1"
+        run_output = test.Run(COMPILE_DIR + "/" + TEST_EXEC_WITH_EXT)
+        if replay_file_created_keyword in run_output:
+            os.environ["ANGLE_CAPTURE_ENABLED"] = "0"
+            test.CompileReplay(COMPILE_DIR, REPLAY_EXEC)
+            test.RunReplay(COMPILE_DIR, REPLAY_EXEC_WITH_EXT)
+        else:
+            print("Skipping replay since capture doesn't produce any file")
+
+    for environment_var in environment_vars:
+        del os.environ[environment_var[0]]
+
+
+if __name__ == "__main__":
+    DEFAULT_COMPILE_DIR = "out/Debug"  # relative to CWD: angle folder
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--compile_dir', default=DEFAULT_COMPILE_DIR)
+    args = parser.parse_args()
+    main(args.compile_dir)
