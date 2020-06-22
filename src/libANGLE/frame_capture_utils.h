7eb46d97ecb597b748395f60e3585a9a1a693ff7
Manh Nguyen <nguyenmh@google.com>
2020-06-21 22:27:06 -0400

    Serialize framebuffers + compare contexts for CaptureReplayTests
    Adds to frame capture the ability to serialize a frame's pre-swap
    GL state and store it in the binary data file
    Adds to CaptureReplayTests the ability to compare its serialized GL
    state with the serialized state pulled from the binary data file
    Adds a serialization module that serializes framebuffers' GL states
    and the contents of their color attachments
    Adds checks to automation script so that it would skips tests that
    do not produce the expected trace files
    
    Bug: angleproject:4779
    Change-Id: I40a02e018073749e79f0ddbfd3d4065745548f46

---
+++ src/libANGLE/frame_capture_utils.h
@@ -0,0 +1,58 @@
+//
+// Copyright 2020 The ANGLE Project Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+// frame_capture_utils.h:
+//   ANGLE frame capture utils interface.
+//
+#ifndef FRAME_CAPTURE_UTILS_H_
+#define FRAME_CAPTURE_UTILS_H_
+
+#include <vector>
+
+namespace gl
+{
+class BinaryOutputStream;
+class Context;
+class Framebuffer;
+class FramebufferAttachment;
+class FramebufferState;
+class ImageIndex;
+}  // namespace gl
+
+typedef unsigned int GLenum;
+
+namespace angle
+{
+class MemoryBuffer;
+class ScratchBuffer;
+
+void SerializeContext(gl::BinaryOutputStream *bos, gl::Context *context);
+
+void SerializeFramebuffer(gl::Context *context,
+                          gl::BinaryOutputStream *bos,
+                          gl::Framebuffer *framebuffer);
+
+void SerializeFramebufferState(gl::Context *context,
+                               gl::BinaryOutputStream *bos,
+                               gl::Framebuffer *framebuffer,
+                               const gl::FramebufferState *framebufferState);
+
+void SerializeFramebufferAttachment(gl::Context *context,
+                                    GLenum mode,
+                                    gl::BinaryOutputStream *bos,
+                                    ScratchBuffer &scratchBuffer,
+                                    gl::Framebuffer *framebuffer,
+                                    const gl::FramebufferAttachment *framebufferAttachment);
+
+void SerializeImageIndex(gl::BinaryOutputStream *bos, const gl::ImageIndex &imageIndex);
+
+bool ReadPixelsFromAttachment(gl::Context *context,
+                              gl::Framebuffer *framebuffer,
+                              const gl::FramebufferAttachment *framebufferAttachment,
+                              ScratchBuffer &scratchBuffer,
+                              MemoryBuffer **pixels);
+
+}  // namespace angle
+#endif  // FRAME_CAPTURE_UTILS_H_
