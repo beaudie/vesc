07bf4853edd3df721a3693df30daaf27bef24260
shrekshao <shrekshao@google.com>
2020-06-25 11:49:12 -0700

    MultiDraw Refactor
    Refactor MultiDraw* from general Context to different ContextImpl.
    Move general multiDraw code to renderer_utils.cpp.
    
    Bug: angleproject:3402
    Change-Id: I85cb4b781afa2b3a8beb382a9c735910057f2ebe

--- src/libANGLE/renderer/d3d/d3d11/Context11.cpp
+++ src/libANGLE/renderer/d3d/d3d11/Context11.cpp
@@ -11,6 +11,7 @@
 
 #include "common/string_utils.h"
 #include "libANGLE/Context.h"
+#include "libANGLE/Context.inl.h"
 #include "libANGLE/MemoryProgramCache.h"
 #include "libANGLE/renderer/OverlayImpl.h"
 #include "libANGLE/renderer/d3d/CompilerD3D.h"
@@ -314,9 +315,9 @@ ANGLE_INLINE angle::Result Context11::drawElementsImpl(const gl::Context *contex
         GLint startVertex;
         ANGLE_TRY(ComputeStartVertex(GetImplAs<Context11>(context), indexRange, baseVertex,
                                      &startVertex));
-        ANGLE_TRY(mRenderer->getStateManager()->updateState(context, mode, startVertex, indexCount,
-                                                            indexType, indices, instanceCount,
-                                                            baseVertex, baseInstance));
+        ANGLE_TRY(mRenderer->getStateManager()->updateState(
+            context, mode, startVertex, indexCount, indexType, indices, instanceCount, baseVertex,
+            baseInstance));
         return mRenderer->drawElements(context, mode, startVertex, indexCount, indexType, indices,
                                        instanceCount, baseVertex, baseInstance);
     }
@@ -415,9 +416,9 @@ angle::Result Context11::drawArraysIndirect(const gl::Context *context,
         const gl::DrawArraysIndirectCommand *cmd = nullptr;
         ANGLE_TRY(ReadbackIndirectBuffer(context, indirect, &cmd));
 
-        ANGLE_TRY(mRenderer->getStateManager()->updateState(context, mode, cmd->first, cmd->count,
-                                                            gl::DrawElementsType::InvalidEnum,
-                                                            nullptr, cmd->instanceCount, 0, 0));
+        ANGLE_TRY(mRenderer->getStateManager()->updateState(
+            context, mode, cmd->first, cmd->count, gl::DrawElementsType::InvalidEnum, nullptr,
+            cmd->instanceCount, 0, 0));
         return mRenderer->drawArrays(context, mode, cmd->first, cmd->count, cmd->instanceCount,
                                      cmd->baseInstance);
     }
@@ -455,18 +456,88 @@ angle::Result Context11::drawElementsIndirect(const gl::Context *context,
         ANGLE_TRY(ComputeStartVertex(GetImplAs<Context11>(context), indexRange, cmd->baseVertex,
                                      &startVertex));
 
-        ANGLE_TRY(mRenderer->getStateManager()->updateState(context, mode, startVertex, cmd->count,
-                                                            type, indices, cmd->primCount,
-                                                            cmd->baseVertex, cmd->baseInstance));
+        ANGLE_TRY(mRenderer->getStateManager()->updateState(
+            context, mode, startVertex, cmd->count, type, indices, cmd->primCount, cmd->baseVertex,
+            cmd->baseInstance));
         return mRenderer->drawElements(context, mode, static_cast<GLint>(indexRange.start),
                                        cmd->count, type, indices, cmd->primCount, 0, 0);
     }
     else
     {
-        ANGLE_TRY(
-            mRenderer->getStateManager()->updateState(context, mode, 0, 0, type, nullptr, 0, 0, 0));
+        ANGLE_TRY(mRenderer->getStateManager()->updateState(context, mode, 0, 0, type, nullptr, 0,
+                                                            0, 0));
         return mRenderer->drawElementsIndirect(context, indirect);
     }
+}
+
+angle::Result Context11::multiDrawArrays(const gl::Context *context,
+                                         gl::PrimitiveMode mode,
+                                         const GLint *firsts,
+                                         const GLsizei *counts,
+                                         GLsizei drawcount)
+{
+    return rx::MultiDrawArraysGeneral(this, context, mode, firsts, counts, drawcount);
+}
+
+angle::Result Context11::multiDrawArraysInstanced(const gl::Context *context,
+                                                  gl::PrimitiveMode mode,
+                                                  const GLint *firsts,
+                                                  const GLsizei *counts,
+                                                  const GLsizei *instanceCounts,
+                                                  GLsizei drawcount)
+{
+    return rx::MultiDrawArraysInstancedGeneral(this, context, mode, firsts, counts, instanceCounts,
+                                               drawcount);
+}
+
+angle::Result Context11::multiDrawElements(const gl::Context *context,
+                                           gl::PrimitiveMode mode,
+                                           const GLsizei *counts,
+                                           gl::DrawElementsType type,
+                                           const GLvoid *const *indices,
+                                           GLsizei drawcount)
+{
+    return rx::MultiDrawElementsGeneral(this, context, mode, counts, type, indices, drawcount);
+}
+
+angle::Result Context11::multiDrawElementsInstanced(const gl::Context *context,
+                                                    gl::PrimitiveMode mode,
+                                                    const GLsizei *counts,
+                                                    gl::DrawElementsType type,
+                                                    const GLvoid *const *indices,
+                                                    const GLsizei *instanceCounts,
+                                                    GLsizei drawcount)
+{
+    return rx::MultiDrawElementsInstancedGeneral(this, context, mode, counts, type, indices,
+                                                 instanceCounts, drawcount);
+}
+
+angle::Result Context11::multiDrawArraysInstancedBaseInstance(const gl::Context *context,
+                                                              gl::PrimitiveMode mode,
+                                                              const GLint *firsts,
+                                                              const GLsizei *counts,
+                                                              const GLsizei *instanceCounts,
+                                                              const GLuint *baseInstances,
+                                                              GLsizei drawcount)
+{
+    return rx::MultiDrawArraysInstancedBaseInstanceGeneral(
+        this, context, mode, firsts, counts, instanceCounts, baseInstances, drawcount);
+}
+
+angle::Result Context11::multiDrawElementsInstancedBaseVertexBaseInstance(
+    const gl::Context *context,
+    gl::PrimitiveMode mode,
+    const GLsizei *counts,
+    gl::DrawElementsType type,
+    const GLvoid *const *indices,
+    const GLsizei *instanceCounts,
+    const GLint *baseVertices,
+    const GLuint *baseInstances,
+    GLsizei drawcount)
+{
+    return rx::MultiDrawElementsInstancedBaseVertexBaseInstanceGeneral(
+        this, context, mode, counts, type, indices, instanceCounts, baseVertices, baseInstances,
+        drawcount);
 }
 
 gl::GraphicsResetStatus Context11::getResetStatus()
