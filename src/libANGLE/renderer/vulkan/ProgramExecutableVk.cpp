d7a2eebe4055b6ea6d7f072763b0360f424f6bf3
Charlie Lao <cclao@google.com>
2020-06-26 14:26:50 -0700

    Vulkan: Let all shader stages share one buffer for default uniform
    Right now each shader stage has its own vk::DynamicBuffer for default
    uniform storage. This is less efficient than just share one buffer. This
    CL moves the storage from per shader stage into its ProgramVk object.
    
    Bug: b/159457348
    Change-Id: If47248ea23c4e48407d3b211583ae2b048d4d10f

--- src/libANGLE/renderer/vulkan/ProgramExecutableVk.cpp
+++ src/libANGLE/renderer/vulkan/ProgramExecutableVk.cpp
@@ -823,6 +823,7 @@ angle::Result ProgramExecutableVk::createPipelineLayout(const gl::Context *glCon
 void ProgramExecutableVk::updateDefaultUniformsDescriptorSet(
     const gl::ShaderType shaderType,
     gl::ShaderMap<DefaultUniformBlock> &defaultUniformBlocks,
+    vk::BufferHelper *defaultUniformBuffer,
     ContextVk *contextVk)
 {
     const std::string uniformBlockName = kDefaultUniformNames[shaderType];
@@ -838,9 +839,8 @@ void ProgramExecutableVk::updateDefaultUniformsDescriptorSet(
 
     if (!uniformBlock.uniformData.empty())
     {
-        vk::BufferHelper *bufferHelper = uniformBlock.storage.getCurrentBuffer();
-        bufferInfo.buffer              = bufferHelper->getBuffer().getHandle();
-        mDescriptorBuffersCache.emplace_back(bufferHelper);
+        bufferInfo.buffer = defaultUniformBuffer->getBuffer().getHandle();
+        mDescriptorBuffersCache.emplace_back(defaultUniformBuffer);
     }
     else
     {
@@ -1156,6 +1156,7 @@ angle::Result ProgramExecutableVk::updateShaderResourcesDescriptorSet(
 angle::Result ProgramExecutableVk::updateTransformFeedbackDescriptorSet(
     const gl::ProgramState &programState,
     gl::ShaderMap<DefaultUniformBlock> &defaultUniformBlocks,
+    vk::BufferHelper *defaultUniformBuffer,
     ContextVk *contextVk)
 {
     const gl::ProgramExecutable &executable = programState.getExecutable();
@@ -1166,7 +1167,8 @@ angle::Result ProgramExecutableVk::updateTransformFeedbackDescriptorSet(
     mDescriptorBuffersCache.clear();
     for (const gl::ShaderType shaderType : executable.getLinkedShaderStages())
     {
-        updateDefaultUniformsDescriptorSet(shaderType, defaultUniformBlocks, contextVk);
+        updateDefaultUniformsDescriptorSet(shaderType, defaultUniformBlocks, defaultUniformBuffer,
+                                           contextVk);
     }
 
     updateTransformFeedbackDescriptorSetImpl(programState, contextVk);
