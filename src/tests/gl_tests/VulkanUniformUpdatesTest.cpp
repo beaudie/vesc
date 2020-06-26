6cba8bbc29e163eabec659758a3ceb01da3d8f4e
Charlie Lao <cclao@google.com>
2020-06-26 14:26:50 -0700

    Vulkan: Let all shader stages share one buffer for default uniform
    Right now each shader stage has its own vk::DynamicBuffer for default
    uniform storage. This is less efficient than just share one buffer. This
    CL moves the storage from per shader stage into its ProgramVk object.
    
    Bug: b/159457348
    Change-Id: If47248ea23c4e48407d3b211583ae2b048d4d10f

--- src/tests/gl_tests/VulkanUniformUpdatesTest.cpp
+++ src/tests/gl_tests/VulkanUniformUpdatesTest.cpp
@@ -475,6 +475,89 @@ TEST_P(VulkanUniformUpdatesTest, TextureStagingBufferRecycling)
     }
 }
 
+// This test tries to create a situation that VS and FS's uniform data might get placed in
+// different buffers and verify uniforms not getting stale data.
+TEST_P(VulkanUniformUpdatesTest, UpdateAfterNewBufferIsAllocated)
+{
+    ASSERT_TRUE(IsVulkan());
+
+    constexpr char kPositionUniformVertexShader[] = R"(attribute vec2 position;
+uniform float uniformVS;
+varying vec4 outVS;
+void main()
+{
+    outVS = vec4(uniformVS, uniformVS, uniformVS, uniformVS);
+    gl_Position = vec4(position, 0, 1);
+})";
+
+    constexpr char kColorUniformFragmentShader[] = R"(precision mediump float;
+varying vec4 outVS;
+uniform float uniformFS;
+void main()
+{
+    if(outVS[0] > uniformFS)
+        gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+    else
+        gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);
+})";
+
+    ANGLE_GL_PROGRAM(program, kPositionUniformVertexShader, kColorUniformFragmentShader);
+    glUseProgram(program);
+
+    limitMaxSets(program);
+
+    rx::ProgramVk *programVk = hackProgram(program);
+
+    // Set a really small min size so that every uniform update actually allocates a new buffer.
+    programVk->setDefaultUniformBlocksMinSizeForTesting(128);
+
+    GLint uniformVSLocation = glGetUniformLocation(program, "uniformVS");
+    ASSERT_NE(uniformVSLocation, -1);
+    GLint uniformFSLocation = glGetUniformLocation(program, "uniformFS");
+    ASSERT_NE(uniformFSLocation, -1);
+
+    glUniform1f(uniformVSLocation, 10.0);
+    glUniform1f(uniformFSLocation, 11.0);
+    drawQuad(program, "position", 0.5f, 1.0f);
+    ASSERT_GL_NO_ERROR();
+
+    const GLsizei kHalfX  = getWindowWidth() / 2;
+    const GLsizei kHalfY  = getWindowHeight() / 2;
+    const GLsizei xoffset = kHalfX;
+    const GLsizei yoffset = kHalfY;
+    // 10.0f < 11.0f, should see green
+    EXPECT_PIXEL_RECT_EQ(xoffset, yoffset, kHalfX, kHalfY, GLColor::green);
+
+    // Now only update FS's uniform
+    for (int i = 0; i < 3; i++)
+    {
+        glUniform1f(uniformFSLocation, 1.0f + i / 10.0f);
+        drawQuad(program, "position", 0.5f, 1.0f);
+        ASSERT_GL_NO_ERROR();
+    }
+    // 10.0f > 9.0f, should see red
+    EXPECT_PIXEL_RECT_EQ(xoffset, yoffset, kHalfX, kHalfY, GLColor::red);
+
+    // 10.0f < 11.0f, should see green again
+    glUniform1f(uniformFSLocation, 11.0f);
+    drawQuad(program, "position", 0.5f, 1.0f);
+    ASSERT_GL_NO_ERROR();
+    EXPECT_PIXEL_RECT_EQ(xoffset, yoffset, kHalfX, kHalfY, GLColor::green);
+
+    // Now only update VS's uniform and flush the draw and readback and verify for every iteration.
+    // This will ensure the old buffers are finished and possibly recycled.
+    for (int i = 0; i < 100; i++)
+    {
+        // Make VS uniform value ping pong across FS uniform value
+        float vsUniformValue  = (i % 1) == 0 ? (11.0 + (i - 50)) : (11.0 - (i - 50));
+        GLColor expectedColor = vsUniformValue > 11.0f ? GLColor::red : GLColor::green;
+        glUniform1f(uniformVSLocation, vsUniformValue);
+        drawQuad(program, "position", 0.5f, 1.0f);
+        ASSERT_GL_NO_ERROR();
+        EXPECT_PIXEL_RECT_EQ(xoffset, yoffset, kHalfX, kHalfY, expectedColor);
+    }
+}
+
 ANGLE_INSTANTIATE_TEST(VulkanUniformUpdatesTest, ES2_VULKAN(), ES3_VULKAN());
 
 }  // anonymous namespace
