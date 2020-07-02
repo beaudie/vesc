db4c3292c9c4e19199e3f83862fd24cb2e5dbdff
Shahbaz Youssefi <syoussefi@chromium.org>
2020-07-02 10:15:17 -0400

    Vulkan: Generate mipmap in compute
    An initial implementation based on AMD's FFX SPD (Single Pass
    Downsampler).
    
    Appart from requiring STORAGE_IMAGE support for the image format, the
    following limitations are in place due to FFX SPD:
    
    - Image must be 2D or 2D array (including cube maps)
    - Image must be single-sampled
    
    The following _can_ be supported, but not yet implemented:
    
    - sRGB formats
    - Integer formats
    - depth/stencil formats
    
    Bug: angleproject:4551
    Change-Id: Ibc4d5cea701cca31e55e3d651540872bbd3b473f

--- src/libANGLE/renderer/vulkan/vk_helpers.h
+++ src/libANGLE/renderer/vulkan/vk_helpers.h
@@ -1169,6 +1169,7 @@ class ImageHelper final : public Resource, public angle::Subject
                      const VkImageSubresourceLayers &dstSubresources,
                      CommandBuffer *commandBuffer);
 
+    // Generate mipmap from level 0 into the rest of the levels with blit.
     angle::Result generateMipmapsWithBlit(ContextVk *contextVk, GLuint maxLevel);
 
     // Resolve this image into a destination image.  This image should be in the TransferSrc layout.
