77ef6d437ee55a3c0a6120bac32f00958524c588
Charlie Lao <cclao@google.com>
2020-07-06 14:53:52 -0700

    Vulkan: Add DescriptorSet cache for transform feedback
    This adds a transform feedback descriptor set cache that uses buffer
    unique IDs as the key to lookup. This should reduce the
    possibility to allocate a new descriptor set, thus reduce the
    possibility to deplete the descriptor set pool, which in turn helps
    improve the default uniform cache hits.
    
    Bug: b/159457348
    Change-Id: I2ff9b9e5c3609aa149931959410100249d0ba2fc

--- src/libANGLE/renderer/vulkan/vk_cache_utils.cpp
+++ src/libANGLE/renderer/vulkan/vk_cache_utils.cpp
@@ -1684,6 +1684,33 @@ bool TextureDescriptorDesc::operator==(const TextureDescriptorDesc &other) const
     return memcmp(mSerials.data(), other.mSerials.data(), sizeof(TexUnitSerials) * mMaxIndex) == 0;
 }
 
+// TransformFeedbackDesc implementation.
+TransformFeedbackDesc::TransformFeedbackDesc()
+{
+    reset();
+}
+
+TransformFeedbackDesc::~TransformFeedbackDesc()                                  = default;
+TransformFeedbackDesc::TransformFeedbackDesc(const TransformFeedbackDesc &other) = default;
+TransformFeedbackDesc &TransformFeedbackDesc::operator=(const TransformFeedbackDesc &other) =
+    default;
+
+size_t TransformFeedbackDesc::hash() const
+{
+    return angle::ComputeGenericHash(&mBufferObjectIDs, sizeof(UniqueObjectID) * kMaxKeyCount);
+}
+
+void TransformFeedbackDesc::reset()
+{
+    memset(&mBufferObjectIDs, 0, sizeof(UniqueObjectID) * kMaxKeyCount);
+}
+
+bool TransformFeedbackDesc::operator==(const TransformFeedbackDesc &other) const
+{
+    return memcmp(&mBufferObjectIDs, &other.mBufferObjectIDs,
+                  sizeof(UniqueObjectID) * kMaxKeyCount) == 0;
+}
+
 // FramebufferDesc implementation.
 
 FramebufferDesc::FramebufferDesc()
