8c7712cb442c4caf69e3e27ce5da154f6de43637
Charlie Lao <cclao@google.com>
2020-07-06 21:37:08 -0700

    Vulkan: Remove mDescriptorBuffersCache
    Per program mDescriptorBuffersCache makes sense when uniform buffer is
    per program. Since the uniform buffers now moved to ContextVk, this
    should also tracked by ContextVk.
    
    Bug: b/159457348
    Change-Id: I8839a0f7e99a6720f05b51e15417006128c280bb

--- src/libANGLE/renderer/vulkan/ProgramVk.cpp
+++ src/libANGLE/renderer/vulkan/ProgramVk.cpp
@@ -729,7 +729,6 @@ angle::Result ProgramVk::updateUniforms(ContextVk *contextVk)
         if (requiredSpace)
         {
             // Allocate space from dynamicBuffer
-            defaultUniformStorage.releaseInFlightBuffers(contextVk);
             bool bufferModified = false;
             ANGLE_TRY(defaultUniformStorage.allocate(contextVk, requiredSpace, &bufferData, nullptr,
                                                      &bufferOffset, &bufferModified));
@@ -769,8 +768,6 @@ angle::Result ProgramVk::updateUniforms(ContextVk *contextVk)
             vk::GetImpl(glState.getCurrentTransformFeedback());
         vk::TransformFeedbackDesc *xfbBufferDesc = &transformFeedbackVk->getTransformFeedbackDesc();
 
-        mExecutable.mDescriptorBuffersCache.clear();
-
         // We need to reinitialize the descriptor sets if we newly allocated buffers since we can't
         // modify the descriptor sets once initialized.
         bool newDescriptorSetAllocated;
@@ -790,8 +787,6 @@ angle::Result ProgramVk::updateUniforms(ContextVk *contextVk)
     }
     else if (!mExecutable.isDefaultUniformDescriptorSetValid(defaultUniformBuffer))
     {
-        mExecutable.mDescriptorBuffersCache.clear();
-
         // We need to reinitialize the descriptor sets if we newly allocated buffers since we can't
         // modify the descriptor sets once initialized.
         bool newDescriptorSetAllocated;
