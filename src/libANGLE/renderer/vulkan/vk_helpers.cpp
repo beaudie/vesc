aeaafc4b80a34439f0852d10cae701b4f20331d3
Charlie Lao <cclao@google.com>
2020-07-08 18:27:21 -0700

    Vulkan: Add driverUniform's buffer to mResourceUseList
    There is a bug with mDriverUniforms' dynamicBuffer that we never add it
    to context's mResourceUseList to let it hold onto it until GPU
    completes. This causes old buffer gets recycled prematurely and gets
    overwritten, result in rendering corruption. This CL adds current buffer
    to the mResourceUseList so that it will be waited properly before gets
    recycled.
    
    Bug: b/160777679
    Change-Id: I7707442e0f5ba408f5f28337422274e0c23b6bfb

--- src/libANGLE/renderer/vulkan/vk_helpers.cpp
+++ src/libANGLE/renderer/vulkan/vk_helpers.cpp
@@ -928,6 +928,7 @@ void DynamicBuffer::initWithFlags(RendererVk *renderer,
     mUsage               = usage;
     mHostVisible         = ((memoryPropertyFlags & VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT) != 0);
     mMemoryPropertyFlags = memoryPropertyFlags;
+    mDataDirty           = false;
 
     // Check that we haven't overriden the initial size of the buffer in setMinimumSizeForTesting.
     if (mInitialSize == 0)
@@ -1036,6 +1037,7 @@ angle::Result DynamicBuffer::allocate(ContextVk *contextVk,
     }
 
     ASSERT(mBuffer != nullptr);
+    mDataDirty = true;
 
     if (bufferOut != nullptr)
     {
@@ -1119,6 +1121,21 @@ void DynamicBuffer::release(RendererVk *renderer)
         mBuffer->release(renderer);
         SafeDelete(mBuffer);
     }
+}
+
+void DynamicBuffer::retainInFlightBuffers(ResourceUseList *resourceUseList)
+{
+    if (!mDataDirty)
+    {
+        return;
+    }
+
+    for (BufferHelper *toRetain : mInFlightBuffers)
+    {
+        toRetain->retain(resourceUseList);
+    }
+    mBuffer->retain(resourceUseList);
+    mDataDirty = false;
 }
 
 void DynamicBuffer::releaseInFlightBuffers(ContextVk *contextVk)
