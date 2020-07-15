bcc0150bacab6bcd92e30acb774f4817c8e53ac0
Charlie Lao <cclao@google.com>
2020-07-15 14:55:18 -0700

    Vulkan: Move mEmptyBuffer from program to ContextVk
    And merge that with TheNUllBuffer as well so that you only have one
    dummy buffer per context.
    
    Bug: b/161391337
    Change-Id: I75fddb5c48393876e745ff237e11d9c5672ae10e

--- src/libANGLE/renderer/vulkan/ContextVk.h
+++ src/libANGLE/renderer/vulkan/ContextVk.h
@@ -586,6 +586,8 @@ class ContextVk : public ContextImpl, public vk::Context
         return mWriteInfos[oldSize];
     }
 
+    vk::BufferHelper &getEmptyBuffer() { return mEmptyBuffer; }
+
   private:
     // Dirty bits.
     enum DirtyBitType : size_t
@@ -1060,6 +1062,13 @@ class ContextVk : public ContextImpl, public vk::Context
     };
 
     ShareGroupVk *mShareGroupVk;
+
+    // This is a special "empty" placeholder buffer for when a shader has no uniforms or doesn't
+    // use all slots in the atomic counter buffer array. It is necessary because we want to keep a
+    // compatible pipeline layout in all cases, and Vulkan does not tolerate having null handles in
+    // a descriptor set. This is also used for null vertex buffer, since Vulkan does not allow
+    // binding a null vertex buffer. We use it as a placeholder.
+    vk::BufferHelper mEmptyBuffer;
 
     std::vector<std::string> mCommandBufferDiagnostics;
 };
