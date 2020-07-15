438d5d512da4c49fb716912f19506b975e6ca636
Tim Van Patten <timvp@google.com>
2020-07-14 19:10:12 -0600

    Vulkan: Optimize MSAA using subpass resolve attachments
    If a user is performing a blit to resolve a multisample color buffer
    into a single attachment, ANGLE can use subpass resolve attachments to
    resolve directly into the destination buffer as part of the render pass.
    This allows the data to remain in tiler memory and reduce the extra
    bandwidth required to write the multisampled data back to perform the
    copy.
    
    While this is better than performing vkCmdResolveImage(), there is still
    another major part of optimizing MSAA using resolve attachments not
    implemented here: discarding the multisampled image rather than writing
    it to GMEM, which requires the user to invalidate the read FBO after the
    blit.
    
    This CL was verified with AGI to make sure there are no explicit blits
    to resolve the multiple sampled image.
    
    Bug: b/159903491
    Test: FramebufferTest_ES31.*Blit*
    Test: VulkanPerformanceCounterTest_ES31.MultisampleResolveWithBlit
    Change-Id: I320a26088d8f614a295e7feec275d71310391806

--- src/libANGLE/renderer/vulkan/ContextVk.h
+++ src/libANGLE/renderer/vulkan/ContextVk.h
@@ -550,6 +550,7 @@ class ContextVk : public ContextImpl, public vk::Context
                                      const vk::ClearValuesArray &clearValues,
                                      vk::CommandBuffer **commandBufferOut);
 
+    bool hasRenderPassCommandBuffer() const { return mRenderPassCommandBuffer != nullptr; }
     // Only returns true if we have a started RP and we've run setupDraw.
     bool hasStartedRenderPass() const
     {
@@ -572,6 +573,16 @@ class ContextVk : public ContextImpl, public vk::Context
     {
         ASSERT(hasStartedRenderPass());
         return *mRenderPassCommands;
+    }
+
+    // TODO(https://anglebug.com/4968): Support multiple open render passes.
+    vk::CommandBufferHelper *getRenderPassCommands() { return mRenderPassCommands; }
+
+    // TODO(https://anglebug.com/4968): Support multiple open render passes.
+    void restoreRenderPassCommandBuffer()
+    {
+        mRenderPassCommandBuffer = &mRenderPassCommands->getCommandBuffer();
+        ASSERT(hasStartedRenderPass());
     }
 
     egl::ContextPriority getContextPriority() const override { return mContextPriority; }
@@ -627,6 +638,7 @@ class ContextVk : public ContextImpl, public vk::Context
     vk::DynamicBuffer *getStagingBufferStorage() { return &mStagingBufferStorage; }
 
     const vk::PerfCounters &getPerfCounters() const { return mPerfCounters; }
+    vk::PerfCounters *getPerfCounters() { return &mPerfCounters; }
 
   private:
     // Dirty bits.
