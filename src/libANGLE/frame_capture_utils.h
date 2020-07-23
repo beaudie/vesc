d0f670bd71c825a45e45476f62ef2bab4db4fd8f
Manh Nguyen <nguyenmh@google.com>
2020-07-22 22:14:16 -0400

    Adds sampler serialization capability
    Adds the ability to serialize sampler objects.
    Adds serialization of sampler objects to serializeContext method so
    that capture replay regression testing now compares the states of
    samplers too.
    
    Bug: angleproject:4817
    Change-Id: Ic32d442eac9aa3afc5c9eea577522fe7054a755a

--- src/libANGLE/frame_capture_utils.h
+++ src/libANGLE/frame_capture_utils.h
@@ -11,11 +11,12 @@
 
 #include <vector>
 
+#include "common/Color.h"
+#include "libANGLE/BinaryStream.h"
 #include "libANGLE/Error.h"
 
 namespace gl
 {
-class BinaryOutputStream;
 class Buffer;
 class BufferState;
 class Context;
@@ -23,6 +24,8 @@ class Framebuffer;
 class FramebufferAttachment;
 class FramebufferState;
 class ImageIndex;
+class Sampler;
+class SamplerState;
 }  // namespace gl
 
 typedef unsigned int GLenum;
@@ -60,5 +63,19 @@ Result SerializeBuffer(const gl::Context *context,
 
 void SerializeBufferState(gl::BinaryOutputStream *bos, const gl::BufferState &bufferState);
 
+void SerializeSampler(gl::BinaryOutputStream *bos, gl::Sampler *sampler);
+
+void SerializeSamplerState(gl::BinaryOutputStream *bos, const gl::SamplerState &samplerState);
+
+void SerializeColorGeneric(gl::BinaryOutputStream *bos, const ColorGeneric &colorGeneric);
+
+template <typename T>
+void SerializeColor(gl::BinaryOutputStream *bos, const Color<T> &color)
+{
+    bos->writeInt(color.red);
+    bos->writeInt(color.green);
+    bos->writeInt(color.blue);
+    bos->writeInt(color.alpha);
+}
 }  // namespace angle
 #endif  // FRAME_CAPTURE_UTILS_H_
