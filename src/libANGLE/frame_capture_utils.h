2aedcb07c4b1f30ff2f7f8a9eaa4b80f67dee50d
Manh Nguyen <nguyenmh@google.com>
2020-07-23 11:12:46 -0400

    Serializes GL context states and reformats frame_capture_utils
    Adds the ability to serialize GL context states.
    Adds serialization of GL context states to serializeContext method so
    that capture replay regression testing now compares the GL context
    states.
    
    Reformats frame_capture_utils to make most serialization methods
    private, except for the ones called by other classes and template
    methods.
    
    Bug: angleproject:4817
    Change-Id: I2d53c88be3d503268bd6e2e5058b5fabe0644f67

--- src/libANGLE/frame_capture_utils.h
+++ src/libANGLE/frame_capture_utils.h
@@ -11,54 +11,20 @@
 
 #include <vector>
 
+#include "common/Color.h"
+#include "libANGLE/BinaryStream.h"
 #include "libANGLE/Error.h"
 
 namespace gl
 {
-class BinaryOutputStream;
-class Buffer;
-class BufferState;
-class Context;
-class Framebuffer;
-class FramebufferAttachment;
-class FramebufferState;
-class ImageIndex;
-}  // namespace gl
 
-typedef unsigned int GLenum;
+class Context;
+}  // namespace gl
 
 namespace angle
 {
-class MemoryBuffer;
-class ScratchBuffer;
 
 Result SerializeContext(gl::BinaryOutputStream *bos, const gl::Context *context);
-
-Result SerializeFramebuffer(const gl::Context *context,
-                            gl::BinaryOutputStream *bos,
-                            ScratchBuffer *scratchBuffer,
-                            gl::Framebuffer *framebuffer);
-
-Result SerializeFramebufferState(const gl::Context *context,
-                                 gl::BinaryOutputStream *bos,
-                                 ScratchBuffer *scratchBuffer,
-                                 gl::Framebuffer *framebuffer,
-                                 const gl::FramebufferState &framebufferState);
-
-Result SerializeFramebufferAttachment(const gl::Context *context,
-                                      gl::BinaryOutputStream *bos,
-                                      ScratchBuffer *scratchBuffer,
-                                      gl::Framebuffer *framebuffer,
-                                      const gl::FramebufferAttachment &framebufferAttachment);
-
-void SerializeImageIndex(gl::BinaryOutputStream *bos, const gl::ImageIndex &imageIndex);
-
-Result SerializeBuffer(const gl::Context *context,
-                       gl::BinaryOutputStream *bos,
-                       ScratchBuffer *scratchBuffer,
-                       gl::Buffer *buffer);
-
-void SerializeBufferState(gl::BinaryOutputStream *bos, const gl::BufferState &bufferState);
 
 }  // namespace angle
 #endif  // FRAME_CAPTURE_UTILS_H_
