cc90490fbfa031079f6ce3357e63285f59bb96b7
Jamie Madill <jmadill@chromium.org>
2020-07-23 19:00:34 -0400

    Add a custom trace tests loader.
    This custom loader will disambiguate the trace tests gl layer
    from the util GL/EGL loader.
    
    Bug: angleproject:4845
    Change-Id: I5e8340eb50f736d931302f71f15f556fd9e52081

--- src/tests/perf_tests/TracePerfTest.cpp
+++ src/tests/perf_tests/TracePerfTest.cpp
@@ -97,6 +97,24 @@ class TracePerfTest : public ANGLERenderTest, public ::testing::WithParamInterfa
     std::string mStartingDirectory;
 };
 
+class TracePerfTest;
+TracePerfTest *gCurrentTracePerfTest = nullptr;
+
+void BindFramebufferProc(GLenum target, GLuint framebuffer)
+{
+    glBindFramebuffer(target, framebuffer);
+    gCurrentTracePerfTest->onReplayFramebufferChange(target, framebuffer);
+}
+
+angle::GenericProc TraceLoadProc(const char *procName)
+{
+    if (strcmp(procName, "glBindFramebuffer") == 0)
+    {
+        return reinterpret_cast<angle::GenericProc>(BindFramebufferProc);
+    }
+    return gCurrentTracePerfTest->getGLWindow()->getProcAddress(procName);
+}
+
 TracePerfTest::TracePerfTest()
     : ANGLERenderTest("TracePerf", GetParam()), mStartFrame(0), mEndFrame(0)
 {
@@ -118,6 +136,8 @@ TracePerfTest::TracePerfTest()
 
     // We already swap in TracePerfTest::drawBenchmark, no need to swap again in the harness.
     disableTestHarnessSwap();
+
+    gCurrentTracePerfTest = this;
 }
 
 void TracePerfTest::initializeBenchmark()
@@ -132,6 +152,8 @@ void TracePerfTest::initializeBenchmark()
         std::string exeDir = angle::GetExecutableDirectory();
         angle::SetCWD(exeDir.c_str());
     }
+
+    trace_angle::LoadGLES(TraceLoadProc);
 
     const TraceInfo &traceInfo = GetTraceInfo(params.testID);
     mStartFrame                = traceInfo.startFrame;
