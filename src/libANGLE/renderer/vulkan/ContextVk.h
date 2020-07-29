b5064b5ecc568ef5f32f3fcbc2520af844272598
Tim Van Patten <timvp@google.com>
2020-07-29 12:01:49 -0600

    Vulkan: Don't always end the render pass when updating the scissor
    We don't always need to end the render pass when updating the scissor,
    since it will be ended later when necessary.
    
    This change is in preparation for optimizing resolving multisample
    images with glBlit, since the render pass needs to be updated before
    it's ended.
    
    Bug: angleproject:4753
    Test: CQ
    Change-Id: Ie657587ca9f4461dcc03f0f9c251ac2c17398f5b

--- src/libANGLE/renderer/vulkan/ContextVk.h
+++ src/libANGLE/renderer/vulkan/ContextVk.h
@@ -473,7 +473,14 @@ class ContextVk : public ContextImpl, public vk::Context
     TextureSerial generateTextureSerial();
     SamplerSerial generateSamplerSerial();
 
-    angle::Result updateScissor(const gl::State &glState);
+    angle::Result updateScissor(const gl::State &glState)
+    {
+        return updateScissorImpl(glState, false);
+    }
+    angle::Result updateScissorAndEndRenderPass(const gl::State &glState)
+    {
+        return updateScissorImpl(glState, true);
+    }
 
     bool emulateSeamfulCubeMapSampling() const { return mEmulateSeamfulCubeMapSampling; }
 
@@ -881,6 +888,8 @@ class ContextVk : public ContextImpl, public vk::Context
     T *allocDescriptorInfos(std::vector<T> *descriptorVector, size_t count);
     template <typename T, const T *VkWriteDescriptorSet::*pInfo>
     void growDesciptorCapacity(std::vector<T> *descriptorVector, size_t newSize);
+
+    angle::Result updateScissorImpl(const gl::State &glState, bool shouldEndRenderPass);
 
     std::array<DirtyBitHandler, DIRTY_BIT_MAX> mGraphicsDirtyBitHandlers;
     std::array<DirtyBitHandler, DIRTY_BIT_MAX> mComputeDirtyBitHandlers;
