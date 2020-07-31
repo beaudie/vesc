345a77de21b1dbb6ed5645b94c51600e5098011e
Manh Nguyen <nguyenmh@google.com>
2020-07-30 22:11:12 -0400

    Add vertex array serialization capability
    Serializes vertex arrays' states stored on CPU.
    
    Adds vertex array serialization to serializeContext method so that
    capture replay regresssion testing now compares the states of vertex
    array objects too.
    
    Bug: angleproject:4817
    Change-Id: Ia2897c056adff9bd433765186240ab07f78db232

--- src/libANGLE/frame_capture_utils.cpp
+++ src/libANGLE/frame_capture_utils.cpp
@@ -907,6 +907,64 @@ Result SerializeTexture(const gl::Context *context,
     ANGLE_TRY(SerializeTextureData(bos, context, texture, scratchBuffer));
     return Result::Continue;
 }
+void SerializeFormat(gl::BinaryOutputStream *bos, const angle::Format *format)
+{
+    bos->writeInt(format->glInternalFormat);
+}
+
+void SerializeVertexAttributeVector(gl::BinaryOutputStream *bos,
+                                    const std::vector<gl::VertexAttribute> &vertexAttributes)
+{
+    for (const gl::VertexAttribute &vertexAttribute : vertexAttributes)
+    {
+        bos->writeInt(vertexAttribute.enabled);
+        ASSERT(vertexAttribute.format);
+        SerializeFormat(bos, vertexAttribute.format);
+        bos->writeInt(vertexAttribute.relativeOffset);
+        bos->writeInt(vertexAttribute.vertexAttribArrayStride);
+        bos->writeInt(vertexAttribute.bindingIndex);
+    }
+}
+
+void SerializeVertexBindingsVector(gl::BinaryOutputStream *bos,
+                                   const std::vector<gl::VertexBinding> &vertexBindings)
+{
+    for (const gl::VertexBinding &vertexBinding : vertexBindings)
+    {
+        bos->writeInt(vertexBinding.getStride());
+        bos->writeInt(vertexBinding.getDivisor());
+        bos->writeInt(vertexBinding.getOffset());
+        bos->writeInt(vertexBinding.getBuffer().id().value);
+        bos->writeInt(vertexBinding.getBoundAttributesMask().to_ulong());
+    }
+}
+
+void SerializeVertexArrayState(gl::BinaryOutputStream *bos,
+                               const gl::VertexArrayState &vertexArrayState)
+{
+    bos->writeString(vertexArrayState.getLabel());
+    SerializeVertexAttributeVector(bos, vertexArrayState.getVertexAttributes());
+    if (vertexArrayState.getElementArrayBuffer())
+    {
+        bos->writeInt(vertexArrayState.getElementArrayBuffer()->id().value);
+    }
+    else
+    {
+        bos->writeInt(0);
+    }
+    SerializeVertexBindingsVector(bos, vertexArrayState.getVertexBindings());
+    bos->writeInt(vertexArrayState.getEnabledAttributesMask().to_ulong());
+    bos->writeInt(vertexArrayState.getVertexAttributesTypeMask().to_ulong());
+    bos->writeInt(vertexArrayState.getClientMemoryAttribsMask().to_ulong());
+    bos->writeInt(vertexArrayState.getNullPointerClientMemoryAttribsMask().to_ulong());
+}
+
+void SerializeVertexArray(gl::BinaryOutputStream *bos, gl::VertexArray *vertexArray)
+{
+    bos->writeInt(vertexArray->id().value);
+    SerializeVertexArrayState(bos, vertexArray->getState());
+    bos->writeInt(vertexArray->isBufferAccessValidationEnabled());
+}
 
 }  // namespace
 
@@ -961,6 +1019,12 @@ Result SerializeContext(gl::BinaryOutputStream *bos, const gl::Context *context)
     {
         gl::Texture *texturePtr = texture.second;
         ANGLE_TRY(SerializeTexture(context, bos, &scratchBuffer, texturePtr));
+    }
+    const gl::VertexArrayMap &vertexArrayMap = context->getVertexArraysForCapture();
+    for (auto &vertexArray : vertexArrayMap)
+    {
+        gl::VertexArray *vertexArrayPtr = vertexArray.second;
+        SerializeVertexArray(bos, vertexArrayPtr);
     }
 
     scratchBuffer.clear();
