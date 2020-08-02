f9b3e35c105713c98dd2c6b2f99012d8edc32bd8
Jamie Madill <jmadill@chromium.org>
2020-08-02 16:04:17 -0400

    Vulkan: Add Image serial.
    This is owned by the ImageHelper. We'll use it in RenderPass trakcing.
    
    Bug: angleproject:4911
    Change-Id: Ic71cef2b16eefb86c73c9c5e1e38ee7cd6a60adb

--- src/libANGLE/renderer/vulkan/vk_helpers.h
+++ src/libANGLE/renderer/vulkan/vk_helpers.h
@@ -1163,6 +1163,12 @@ class ImageHelper final : public Resource, public angle::Subject
     const Format &getFormat() const { return *mFormat; }
     GLint getSamples() const { return mSamples; }
 
+    ImageSerial getImageSerial() const
+    {
+        ASSERT(valid() && mImageSerial.valid());
+        return mImageSerial;
+    }
+
     void setCurrentImageLayout(ImageLayout newLayout) { mCurrentLayout = newLayout; }
     ImageLayout getCurrentImageLayout() const { return mCurrentLayout; }
     VkImageLayout getCurrentLayout() const;
@@ -1543,6 +1549,7 @@ class ImageHelper final : public Resource, public angle::Subject
     VkExtent3D mExtents;
     const Format *mFormat;
     GLint mSamples;
+    ImageSerial mImageSerial;
 
     // Current state.
     ImageLayout mCurrentLayout;
