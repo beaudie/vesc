dbf202640b705b62601877205495e82582a3b5fc
Geoff Lang <geofflang@chromium.org>
2020-08-06 20:55:05 -0400

    Add a Vulkan feature to compress float32 vertex formats.
    Bug: b/161716126
    Change-Id: I9a09656a72e8499faa4124adf876d7261c8341c9

--- src/libANGLE/renderer/vulkan/vk_cache_utils.cpp
+++ src/libANGLE/renderer/vulkan/vk_cache_utils.cpp
@@ -652,6 +652,7 @@ void GraphicsPipelineDesc::initDefaults()
         SetBitField(packedAttrib.stride, 0);
         SetBitField(packedAttrib.divisor, 0);
         SetBitField(packedAttrib.format, defaultFormat);
+        SetBitField(packedAttrib.compressed, 1);
         SetBitField(packedAttrib.offset, 0);
     }
 
@@ -848,7 +849,8 @@ angle::Result GraphicsPipelineDesc::initializePipeline(
         angle::FormatID formatID         = static_cast<angle::FormatID>(packedAttrib.format);
         const vk::Format &format         = contextVk->getRenderer()->getFormat(formatID);
         const angle::Format &angleFormat = format.intendedFormat();
-        VkFormat vkFormat                = format.vkBufferFormat;
+        VkFormat vkFormat =
+            packedAttrib.compressed ? format.vkCompressedBufferFormat : format.vkBufferFormat;
 
         gl::ComponentType attribType =
             GetVertexAttributeComponentType(angleFormat.isPureInt(), angleFormat.vertexAttribType);
@@ -1059,6 +1061,7 @@ void GraphicsPipelineDesc::updateVertexInput(GraphicsPipelineTransitionBits *tra
                                              GLuint stride,
                                              GLuint divisor,
                                              angle::FormatID format,
+                                             bool compressed,
                                              GLuint relativeOffset)
 {
     vk::PackedAttribDesc &packedAttrib = mVertexInputAttribs.attribs[attribIndex];
@@ -1072,6 +1075,7 @@ void GraphicsPipelineDesc::updateVertexInput(GraphicsPipelineTransitionBits *tra
     }
 
     SetBitField(packedAttrib.format, format);
+    SetBitField(packedAttrib.compressed, compressed);
     SetBitField(packedAttrib.offset, relativeOffset);
 
     constexpr size_t kAttribBits = kPackedAttribDescSize * kBitsPerByte;
