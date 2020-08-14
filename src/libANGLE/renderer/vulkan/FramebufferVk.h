bd3b124521fa1f31127ab9d8e46e941bf663da79
Ian Elliott <ianelliott@google.com>
2020-08-13 18:24:26 -0600

    Vulkan: do not end render pass when invalidating
    Initially, FramebufferVk::invalidateImpl() was very conservative and
    always ended a render pass (if the framebuffer is part of the current
    render pass).  This adversely affects PUBG Mobile, which invalidates
    the depth buffer every frame, causing the render pass to be split.
    
    Test: PUBG MOBILE on Android
    Test: angle_deqp_gles3_tests --gtest_filter=dEQP.GLES3/functional_fbo_invalidate_* --use-angle=vulkan
    Bug: b/163854287
    Change-Id: I343dee1db3ebaf039ff92557f9ef25b24bcdcc93

--- src/libANGLE/renderer/vulkan/FramebufferVk.h
+++ src/libANGLE/renderer/vulkan/FramebufferVk.h
@@ -117,6 +117,7 @@ class FramebufferVk : public FramebufferImpl
     angle::Result startNewRenderPass(ContextVk *contextVk,
                                      const gl::Rectangle &renderArea,
                                      vk::CommandBuffer **commandBufferOut);
+    angle::Result resumeRenderPass(ContextVk *contextVk);
 
     RenderTargetVk *getFirstRenderTarget() const;
     GLint getSamples() const;
