38d23fd78664119bec694a7d4a6c2597fced179c
Jamie Madill <jmadill@chromium.org>
2020-08-16 14:09:41 -0400

    Add FastIntegerMap class to improve serial tracking.
    A very basic data structure that makes a vector sized to the length of
    the maximum value integer key in the map. We should investigate the
    relative perf vs abseil's flat maps which will use far less storage.
    
    Bug: angleproject:4950
    Change-Id: I1c47cd9c54d6982fc4645b85a65219eb8a3f59f5

--- src/common/FastVector.h
+++ src/common/FastVector.h
@@ -505,6 +505,59 @@ class FastUnorderedSet final
   private:
     FastVector<T, N> mData;
 };
+
+template <typename Key, typename T, T Invalid>
+class FastIntegerMap final
+{
+  public:
+    FastIntegerMap() {}
+    ~FastIntegerMap() {}
+
+    void insert(Key key, T value)
+    {
+        ASSERT(!contains(key));
+        ensureCapacity(key);
+        ASSERT(mData.size() > key);
+        mData[key] = value;
+        mSize++;
+    }
+
+    bool contains(Key key) const
+    {
+        if (key >= mData.size())
+            return false;
+        return mData[key] != Invalid;
+    }
+
+    bool get(Key key, T *out) const
+    {
+        if (!contains(key))
+            return false;
+        *out = mData[key];
+        return true;
+    }
+
+    void clear()
+    {
+        mData.clear();
+        mSize = 0;
+    }
+
+    bool empty() const { return mSize == 0; }
+    size_t size() const { return mSize; }
+
+  private:
+    void ensureCapacity(Key key)
+    {
+        if (mData.size() <= key)
+        {
+            mData.resize(key * 2, Invalid);
+        }
+    }
+
+    std::vector<T> mData;
+    size_t mSize = 0;
+};
 }  // namespace angle
 
 #endif  // COMMON_FASTVECTOR_H_
