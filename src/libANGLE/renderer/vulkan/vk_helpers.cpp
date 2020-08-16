639368e4d3af24024dacea81e4cf3ab0e9777ff7
Jamie Madill <jmadill@chromium.org>
2020-08-16 14:09:41 -0400

    Add FastIntegerMap class to improve serial tracking.
    Bug: angleproject:4950
    Change-Id: I1c47cd9c54d6982fc4645b85a65219eb8a3f59f5

--- src/libANGLE/renderer/vulkan/vk_helpers.cpp
+++ src/libANGLE/renderer/vulkan/vk_helpers.cpp
@@ -569,13 +569,13 @@ void CommandBufferHelper::initialize(bool isRenderPassCommandBuffer, bool mergeB
 
 bool CommandBufferHelper::usesBuffer(const BufferHelper &buffer) const
 {
-    return mUsedBuffers.contains(buffer.getBufferSerial());
+    return mUsedBuffers.contains(buffer.getBufferSerial().getValue());
 }
 
 bool CommandBufferHelper::usesBufferForWrite(const BufferHelper &buffer) const
 {
     BufferAccess access;
-    if (!mUsedBuffers.get(buffer.getBufferSerial(), &access))
+    if (!mUsedBuffers.get(buffer.getBufferSerial().getValue(), &access))
     {
         return false;
     }
@@ -595,9 +595,9 @@ void CommandBufferHelper::bufferRead(ResourceUseList *resourceUseList,
     }
 
     ASSERT(!usesBufferForWrite(*buffer));
-    if (!mUsedBuffers.contains(buffer->getBufferSerial()))
+    if (!mUsedBuffers.contains(buffer->getBufferSerial().getValue()))
     {
-        mUsedBuffers.insert(buffer->getBufferSerial(), BufferAccess::Read);
+        mUsedBuffers.insert(buffer->getBufferSerial().getValue(), BufferAccess::Read);
     }
 }
 
@@ -621,7 +621,7 @@ void CommandBufferHelper::bufferWrite(ResourceUseList *resourceUseList,
     if (aliasingMode == AliasingMode::Disallowed)
     {
         ASSERT(!usesBuffer(*buffer));
-        mUsedBuffers.insert(buffer->getBufferSerial(), BufferAccess::Write);
+        mUsedBuffers.insert(buffer->getBufferSerial().getValue(), BufferAccess::Write);
     }
 }
 
