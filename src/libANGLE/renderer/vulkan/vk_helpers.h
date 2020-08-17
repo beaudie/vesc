308f4e48339bc7bbe064c8cecd967cc8ada543bd
Jamie Madill <jmadill@chromium.org>
2020-08-17 19:02:09 -0400

    Vulkan: Check aggregate barrier feature instead of caching.
    Bug: b/157508684
    Change-Id: I6c98cb017d44c1279439d815e77f6ad1a32133e2

--- src/libANGLE/renderer/vulkan/vk_helpers.h
+++ src/libANGLE/renderer/vulkan/vk_helpers.h
@@ -859,7 +859,7 @@ class CommandBufferHelper : angle::NonCopyable
     ~CommandBufferHelper();
 
     // General Functions (non-renderPass specific)
-    void initialize(bool isRenderPassCommandBuffer, bool mergeBarriers);
+    void initialize(bool isRenderPassCommandBuffer);
 
     void bufferRead(ResourceUseList *resourceUseList,
                     VkAccessFlags readAccessType,
@@ -885,7 +885,7 @@ class CommandBufferHelper : angle::NonCopyable
 
     angle::Result flushToPrimary(ContextVk *contextVk, PrimaryCommandBuffer *primary);
 
-    void executeBarriers(PrimaryCommandBuffer *primary);
+    void executeBarriers(ContextVk *contextVk, PrimaryCommandBuffer *primary);
 
     void setHasRenderPass(bool hasRenderPass) { mIsRenderPassCommandBuffer = hasRenderPass; }
     void reset();
@@ -1016,7 +1016,6 @@ class CommandBufferHelper : angle::NonCopyable
     bool mRebindTransformFeedbackBuffers;
 
     bool mIsRenderPassCommandBuffer;
-    bool mMergeBarriers;
 
     ResourceAccess mDepthStartAccess;
     ResourceAccess mStencilStartAccess;
