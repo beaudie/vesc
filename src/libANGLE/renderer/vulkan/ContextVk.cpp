d6b5dc7e3ce3b240b301358f0432fb535d8f6c5c
Jamie Madill <jmadill@chromium.org>
2020-08-25 18:01:29 -0400

    Vulkan: Fix optimizeRenderPassForPresent regression.
    gfxbench clears the depth buffer right before the swap. Even though the
    last draw call that presents the frame didn't ever read or write to
    depth, the tracking we added thought this depth write meant we had to
    keep the LOAD_OP as CLEAR. Instead we can refine our check to treat
    clears specially when enabling the depth-stencil read-only mode instead
    of changing how the tracking works for clears. This way the tracking
    can not affect other apps that don't use depth-stencil read-only loops.
    
    Also adds a regression test that counts the clears after a swap.
    
    Bug: angleproject:4959
    Bug: angleproject:4979
    Change-Id: I12ece6474019f7519a467f827110ad817f7d4df7

--- src/libANGLE/renderer/vulkan/ContextVk.cpp
+++ src/libANGLE/renderer/vulkan/ContextVk.cpp
@@ -3934,7 +3934,7 @@ angle::Result ContextVk::updateActiveTextures(const gl::Context *context)
 
             if (hasStartedRenderPass())
             {
-                if (mRenderPassCommands->getDepthStartAccess() == vk::ResourceAccess::Write)
+                if (mRenderPassCommands->hasDepthWriteOrClear())
                 {
                     ANGLE_TRY(flushCommandsAndEndRenderPass());
                 }
@@ -4615,7 +4615,7 @@ angle::Result ContextVk::flushCommandsAndEndRenderPass()
 
     addOverlayUsedBuffersCount(mRenderPassCommands);
 
-    mRenderPassCommands->endRenderPass();
+    mRenderPassCommands->endRenderPass(this);
 
     if (mRenderer->getFeatures().enableCommandProcessingThread.enabled)
     {
