47d943f0f76326dc9d97935b08325a83e0e87cb7
Charlie Lao <cclao@google.com>
2020-08-28 11:03:20 -0700

    Vulkan: Defer glFlush in certain cases
    Manhattan is calling glFlush in middle of a renderpass. This CL tracks
    the usage of sync objects and a few other things and defer the flush
    when we think it is good to do so. To reduce the chance that we might
    get into GPU starvation, the deferred flush are flushed when draw FBO
    binding changes.
    
    Bug: b/166475273
    Change-Id: I6baa3898d5efc456e2205c44e13c64f3d79d1464

--- include/platform/FeaturesVk.h
+++ include/platform/FeaturesVk.h
@@ -361,6 +361,17 @@ struct FeaturesVk : FeatureSetBase
         "preferred_large_heap_block_size_4M", FeatureCategory::VulkanWorkarounds,
         "Use 4 MB preferred large heap block size with AMD allocator", &members,
         "http://anglebug.com/4995"};
+
+    // Manhattan is calling glFlush in the middle of renderpass which breaks renderpass and hurts
+    // performance on tile based GPU. On tiled based GPU there is also advantage to hold off
+    // submission so that we we see a glClear call we can can go back to the renderpass that does
+    // the store and change it to DontCare. But defer glFlush call might hurt performance in some
+    // cases that we can't keep GPU busy enough. And for debugging purpose we may also want to
+    // disabled. This feature flag is here to allow easy turn on/off this feature as needed.
+    // https://issuetracker.google.com/issues/166475273
+    Feature enableDeferredFlush = {"enable_glFlush_deferral", FeatureCategory::VulkanWorkarounds,
+                                   "Allow glFlush to deferred when possible", &members,
+                                   "https://issuetracker.google.com/issues/166475273"};
 };
 
 inline FeaturesVk::FeaturesVk()  = default;
