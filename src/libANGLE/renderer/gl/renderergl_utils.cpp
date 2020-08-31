51914e27b63e6005d21ae8e8644e9d7deb2c244c
Jonah Ryan-Davis <jonahr@google.com>
2020-08-31 16:00:29 -0400

    Disable OES_get_program_binary on Linux/AMD
    There are flickering issues on Linux/AMD possibly related to
    ProgramBinary. We will disable support until we know more about
    this issue.
    
    Bug: angleproject:5007
    Change-Id: If459f60ae7f8a2cd4ea934fe3e54d890387d57db

--- src/libANGLE/renderer/gl/renderergl_utils.cpp
+++ src/libANGLE/renderer/gl/renderergl_utils.cpp
@@ -1796,12 +1796,15 @@ void InitializeFeatures(const FunctionsGL *functions, angle::FeaturesGL *feature
 void InitializeFrontendFeatures(const FunctionsGL *functions, angle::FrontendFeatures *features)
 {
     VendorID vendor = GetVendorID(functions);
+    bool isAMD      = IsAMD(vendor);
     bool isIntel    = IsIntel(vendor);
     bool isQualcomm = IsQualcomm(vendor);
 
     ANGLE_FEATURE_CONDITION(features, disableProgramCachingForTransformFeedback,
                             IsAndroid() && isQualcomm);
     ANGLE_FEATURE_CONDITION(features, syncFramebufferBindingsOnTexImage, IsWindows() && isIntel);
+
+    ANGLE_FEATURE_CONDITION(features, disableProgramBinary, isAMD && IsLinux());
 }
 
 }  // namespace nativegl_gl
