6d74d824dc4e2ad3193c2bc1faa69a837da54885
Trevor David Black <vantablack@google.com>
2020-09-07 22:09:22 +0000

    First pass at increasing inclusivity
    Link to the inclusivity rules
    https://source.android.com/setup/contribute/respectful-code
    
    Bug: b/162834212
    Bug: chromium:1097198
    Change-Id: Ied5a9e3879d72bff3f77ea6fcda9b82f30c32c2f

--- src/tests/gl_tests/TransformFeedbackTest.cpp
+++ src/tests/gl_tests/TransformFeedbackTest.cpp
@@ -2095,9 +2095,9 @@ void main() {
     constexpr char kFS[] =
         R"(#version 300 es
 precision mediump float;
-out vec4 dummy;
+out vec4 unused;
 void main() {
-  dummy = vec4(0.5);
+  unused = vec4(0.5);
 })";
 
     std::vector<std::string> tfVaryings;
