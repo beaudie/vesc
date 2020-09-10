de4ee1ad1a1b1f458876976c0d87ddfb3fe764a3
Mohan Maiya <m.maiya@samsung.com>
2020-09-10 09:19:15 -0700

    EGL: Add support for EGL_ANDROID_create_native_client_buffer
    This EGL extension will add support for creating EGLClientBuffer
    backed by an Android window buffer (struct ANativeWindowBuffer)
    which can be later used to create an EGLImage.
    
    Bug: angleproject:5018
    Tests: angle_end2end_tests --gtest_filter=ImageTest.SourceNativeClientBufferTarget*
    Change-Id: If78ed7b80ad09629b8c5f5b5a0eb07a548e82e6e

--- src/common/android_util.h
+++ src/common/android_util.h
@@ -10,6 +10,10 @@
 #define COMMON_ANDROIDUTIL_H_
 
 #include <EGL/egl.h>
+#include <EGL/eglext.h>
+#include <stdint.h>
+#include <utility>
+
 #include "angle_gl.h"
 
 struct ANativeWindowBuffer;
@@ -24,6 +28,28 @@ namespace android
 ANativeWindowBuffer *ClientBufferToANativeWindowBuffer(EGLClientBuffer clientBuffer);
 EGLClientBuffer AHardwareBufferToClientBuffer(const AHardwareBuffer *hardwareBuffer);
 
+using FormatAndType                           = std::pair<GLenum, GLenum>;
+constexpr uint32_t kSupportedFormatTypesCount = 3;
+inline const FormatAndType *GetSupportedFormatTypesForNativeClientBuffer()
+{
+    // TODO(http://anglebug.com/4062): add GL_RGB10_A2 and GL_RGBA16F formats once this
+    // issue is resolved
+    // We only support these 3 GL internal formats with these 3
+    // format types for creating native client buffers, for now.
+    static constexpr FormatAndType kSupportedFormatTypes[kSupportedFormatTypesCount] = {
+        {GL_RGB8, GL_UNSIGNED_BYTE},
+        {GL_RGBA8, GL_UNSIGNED_BYTE},
+        {GL_RGB565, GL_UNSIGNED_SHORT_5_6_5}};
+
+    return kSupportedFormatTypes;
+}
+
+EGLClientBuffer CreateEGLClientBufferFromAHardwareBuffer(int width,
+                                                         int height,
+                                                         int depth,
+                                                         int androidFormat,
+                                                         int usage);
+
 void GetANativeWindowBufferProperties(const ANativeWindowBuffer *buffer,
                                       int *width,
                                       int *height,
@@ -33,6 +59,8 @@ GLenum NativePixelFormatToGLInternalFormat(int pixelFormat);
 int GLInternalFormatToNativePixelFormat(GLenum internalFormat);
 
 AHardwareBuffer *ANativeWindowBufferToAHardwareBuffer(ANativeWindowBuffer *windowBuffer);
+
+uint64_t GetAHBUsage(int eglNativeBufferUsage);
 
 }  // namespace android
 }  // namespace angle
