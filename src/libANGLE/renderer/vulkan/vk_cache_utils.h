47fb445522a24fa3975323e1001a0a674736d06d
Tim Van Patten <timvp@google.com>
2020-09-11 21:21:55 -0600

    Vulkan: Allocate descriptor pools with layouts
    Descriptor set layouts and pools are very tightly coupled, since their
    binding types and counts must match to ensure the number of available
    descriptor sets within a pool remains accurate. To achieve this,
    descriptor pools will now be allocated when the descriptor set layouts
    are created. This allows the descriptor set handle to be stored within
    the DynamicDescriptorPool to ensure we only allocate a descriptor pool
    when necessary.
    
    Bug: angleproject:5033
    Test: CQ
    Change-Id: I4faf82c24f31052e57b656c968788bb0c131b619

--- src/libANGLE/renderer/vulkan/vk_cache_utils.h
+++ src/libANGLE/renderer/vulkan/vk_cache_utils.h
@@ -52,6 +52,7 @@ enum DescriptorSetIndex : uint32_t
 
 namespace vk
 {
+class DynamicDescriptorPool;
 class ImageHelper;
 enum class ImageLayout;
 
@@ -1226,11 +1227,17 @@ class DescriptorSetLayoutCache final : angle::NonCopyable
     void destroy(VkDevice device);
 
     angle::Result getDescriptorSetLayout(
-        vk::Context *context,
+        ContextVk *contextVk,
         const vk::DescriptorSetLayoutDesc &desc,
-        vk::BindingPointer<vk::DescriptorSetLayout> *descriptorSetLayoutOut);
+        vk::BindingPointer<vk::DescriptorSetLayout> *descriptorSetLayoutOut,
+        vk::DynamicDescriptorPool *descriptorPoolOut);
 
   private:
+    angle::Result getDescriptorPool(ContextVk *contextVk,
+                                    const vk::DescriptorSetLayoutBindingVector &bindingVector,
+                                    VkDescriptorSetLayout descriptorSetLayout,
+                                    vk::DynamicDescriptorPool *descriptorPoolOut);
+
     std::unordered_map<vk::DescriptorSetLayoutDesc, vk::RefCountedDescriptorSetLayout> mPayload;
 };
 
