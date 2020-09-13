966b123519b0e44daf7cfa8d27b793b1e6f9a1ca
Shahbaz Youssefi <syoussefi@chromium.org>
2020-09-13 04:57:45 -0400

    Test
    Bug: None
    Change-Id: I05027d2445e162c8a2f90acefa1c7183a635fea5

--- src/libANGLE/renderer/vulkan/vk_cache_utils.cpp
+++ src/libANGLE/renderer/vulkan/vk_cache_utils.cpp
@@ -1038,6 +1038,18 @@ angle::Result GraphicsPipelineDesc::initializePipeline(
                 state.blendEnable = VK_TRUE;
                 UnpackBlendAttachmentState(inputAndBlend.attachments[colorIndexGL], &state);
             }
+            else
+            {
+                const vk::Format &f =
+                    contextVk->getRenderer()->getFormat(mRenderPassDesc[colorIndexGL]);
+                fprintf(stderr,
+                        "Attachment %u: actualFormatID: %u NOT BLENDABLE but asked to blend.\n"
+                        "intendedFormatID: %u, internalFormat: 0x%04x vk image format: %u\n",
+                        colorIndexGL, actualFormatID, f.intendedFormatID, f.internalFormat,
+                        f.vkImageFormat);
+                state.blendEnable = VK_FALSE;
+                UnpackBlendAttachmentState(inputAndBlend.attachments[colorIndexGL], &state);
+            }
         }
         state.colorWriteMask =
             Int4Array_Get<VkColorComponentFlags>(inputAndBlend.colorWriteMaskBits, colorIndexGL);
