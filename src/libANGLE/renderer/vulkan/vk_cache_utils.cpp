8f4e3ada8acb1d134ff8c4a77cd0e0b54630da13
Tim Van Patten <timvp@google.com>
2020-09-15 09:56:47 -0600

    Vulkan: Check CQ tests bindEmptyForUnusedDescriptorSets
    Quick hack CL that asserts if the bindEmptyForUnusedDescriptorSets path
    is tested in CQ.
    
    Bug: angleproject:5033
    Change-Id: I2eaa878d5edb10f45397578301817d9648e97426

--- src/libANGLE/renderer/vulkan/vk_cache_utils.cpp
+++ src/libANGLE/renderer/vulkan/vk_cache_utils.cpp
@@ -2329,6 +2329,7 @@ angle::Result DescriptorSetLayoutCache::getDescriptorPool(
     if (renderer->getFeatures().bindEmptyForUnusedDescriptorSets.enabled &&
         descriptorPoolSizes.empty())
     {
+        ASSERT(false);  // TIMTIM - Does CQ test this path?
         // For this workaround, we have to create an empty descriptor set for each descriptor set
         // index, so make sure their pools are initialized.
         VkDescriptorPoolSize poolSize = {};
