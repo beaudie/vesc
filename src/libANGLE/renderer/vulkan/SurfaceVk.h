cd637f604b878ca790af2162f952e2d9ad7acb77
Ian Elliott <ianelliott@google.com>
2020-09-16 17:14:16 -0600

    Vulkan: Delay vkAcquireNextImageKHR till later
    Currently, ANGLE calls vkAcquireNextImageKHR() immediately after
    calling vkQueuePresentKHR(), which can cause the process to stall
    (even with multi-threading).  Delay it until it is absoluately needed.
    
    Test: angle_end2end_tests --gtest_filter=*EGLPreRotationSurfaceTest*/*
    Test: angle_white_box_tests --gtest_filter=VulkanPerformanceCounterTest.*Invalidate*/*
    Test: angle_deqp_egl_tests --gtest_filter=dEQP.EGL/functional_query_context_get_current_display_rgba8888_window* --use-angle=vulkan
    Test: angle_perftests --gtest_filter=GenerateMipmapBenchmark.Run/vulkan_webgl
    Bug: b/162082698
    Change-Id: I466df9237136dd59a9556faa8cf2dbad94e076fe

--- src/libANGLE/renderer/vulkan/SurfaceVk.h
+++ src/libANGLE/renderer/vulkan/SurfaceVk.h
@@ -193,6 +193,11 @@ class WindowSurfaceVk : public SurfaceVk
     void destroy(const egl::Display *display) override;
 
     egl::Error initialize(const egl::Display *display) override;
+    angle::Result getAttachmentRenderTarget(const gl::Context *context,
+                                            GLenum binding,
+                                            const gl::ImageIndex &imageIndex,
+                                            GLsizei samples,
+                                            FramebufferAttachmentRenderTarget **rtOut) override;
     FramebufferImpl *createDefaultFramebuffer(const gl::Context *context,
                                               const gl::FramebufferState &state) override;
     egl::Error swap(const gl::Context *context) override;
@@ -255,19 +260,27 @@ class WindowSurfaceVk : public SurfaceVk
     virtual angle::Result getCurrentWindowSize(vk::Context *context, gl::Extents *extentsOut) = 0;
 
     angle::Result initializeImpl(DisplayVk *displayVk);
-    angle::Result recreateSwapchain(ContextVk *contextVk,
-                                    const gl::Extents &extents,
-                                    uint32_t swapHistoryIndex);
+    angle::Result recreateSwapchain(ContextVk *contextVk, const gl::Extents &extents);
     angle::Result createSwapChain(vk::Context *context,
                                   const gl::Extents &extents,
                                   VkSwapchainKHR oldSwapchain);
-    angle::Result checkForOutOfDateSwapchain(ContextVk *contextVk,
-                                             uint32_t swapHistoryIndex,
-                                             bool presentOutOfDate);
+    angle::Result checkForOutOfDateSwapchain(ContextVk *contextVk, bool presentOutOfDate);
     angle::Result resizeSwapchainImages(vk::Context *context, uint32_t imageCount);
     void releaseSwapchainImages(ContextVk *contextVk);
     void destroySwapChainImages(DisplayVk *displayVk);
-    VkResult nextSwapchainImage(vk::Context *context);
+    // This method calls vkAcquireNextImageKHR() to acquire the next swapchain image.  It is called
+    // when the swapchain is initially created and when present() finds the swapchain out of date.
+    // Otherwise, it is called later by deferAcquiringOfNextSwapchainImage().
+    VkResult acquireNextSwapchainImage(vk::Context *context);
+    // This method is called when a swapchain image is presented.  It saves state necessary to call
+    // deferAcquiringOfNextSwapchainImage() later.
+    void deferAcquiringOfNextSwapchainImage(const gl::Context *context);
+    // Called later, when the next swapchain image must be acquired, this method will recreate the
+    // swapchain (if needed) and call the acquireNextSwapchainImage() method.
+    angle::Result doDeferredAcquireOfNextSwapchainImage(const gl::Context *context,
+                                                        bool presentOutOfDate);
+    // Returns true if should call doDeferredAcquireOfNextSwapchainImage()
+    bool needToAcquireNextSwapchainImage() const;
     angle::Result present(ContextVk *contextVk,
                           EGLint *rects,
                           EGLint n_rects,
@@ -323,6 +336,9 @@ class WindowSurfaceVk : public SurfaceVk
     vk::ImageViewHelper mColorImageMSViews;
     angle::ObserverBinding mColorImageMSBinding;
     vk::Framebuffer mFramebufferMS;
+
+    // True when acquiring the next image is deferred.
+    bool mAcquireDeferred;
 };
 
 }  // namespace rx
