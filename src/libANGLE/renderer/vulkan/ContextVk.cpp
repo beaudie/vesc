9bbb04ac32972947951181f287a6fe0d0bb8c1e9
Courtney Goeltzenleuchter <courtneygo@google.com>
2020-09-18 15:15:12 -0600

    WIP: Add stress test for QueueSerial handling
    During code review an issue was seen in how QueueSerials are assigned
    that could allow the serial the resources are given to be different than
    the actual serial used to submit the work.
    Added stress test to try and provoke the misbehavior.
    
    Bug: b/168744476
    Change-Id: I058035918a03dba6be6b7a70a7b91cc765b2c55f

--- src/libANGLE/renderer/vulkan/ContextVk.cpp
+++ src/libANGLE/renderer/vulkan/ContextVk.cpp
@@ -4159,7 +4159,7 @@ angle::Result ContextVk::flushImpl(const vk::Semaphore *signalSemaphore)
     }
 
     ANGLE_VK_TRY(this, mPrimaryCommands.end());
-    
+
     waitForSwapchainImageIfNecessary();
 
     VkSubmitInfo submitInfo = {};
