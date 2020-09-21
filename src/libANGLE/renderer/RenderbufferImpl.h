a45eb02e8d4fe044d71cbf6a33eddd03fd8d06df
Brandon Schade <b.schade@samsung.com>
2020-09-21 15:34:19 -0700

    Vulkan: Add support for EXT_copy_image
    Add support for GL_EXT_copy_image which allows
    image data transfer between image objects.
    This is implemented by using the vkCmdCopyImage
    API call.
    
    Bug: angleproject:3593
    Test: dEQP-GLES31.functional.copy_image.*
    Change-Id: I30a34a8711b5d2e5834064d7453e03d6ec0df478

--- src/libANGLE/renderer/RenderbufferImpl.h
+++ src/libANGLE/renderer/RenderbufferImpl.h
@@ -49,6 +49,34 @@ class RenderbufferImpl : public FramebufferAttachmentObjectImpl
     virtual angle::Result setStorageEGLImageTarget(const gl::Context *context,
                                                    egl::Image *image)       = 0;
 
+    virtual angle::Result copyImageSubData(const gl::Context *context,
+                                           const gl::Renderbuffer *srcBuffer,
+                                           GLint srcLevel,
+                                           GLint srcX,
+                                           GLint srcY,
+                                           GLint srcZ,
+                                           GLint dstLevel,
+                                           GLint dstX,
+                                           GLint dstY,
+                                           GLint dstZ,
+                                           GLsizei srcWidth,
+                                           GLsizei srcHeight,
+                                           GLsizei srcDepth);
+
+    virtual angle::Result copyImageSubData(const gl::Context *context,
+                                           const gl::Texture *srcTexture,
+                                           GLint srcLevel,
+                                           GLint srcX,
+                                           GLint srcY,
+                                           GLint srcZ,
+                                           GLint dstLevel,
+                                           GLint dstX,
+                                           GLint dstY,
+                                           GLint dstZ,
+                                           GLsizei srcWidth,
+                                           GLsizei srcHeight,
+                                           GLsizei srcDepth);
+
     virtual GLenum getColorReadFormat(const gl::Context *context);
     virtual GLenum getColorReadType(const gl::Context *context);
 
@@ -65,6 +93,42 @@ class RenderbufferImpl : public FramebufferAttachmentObjectImpl
   protected:
     const gl::RenderbufferState &mState;
 };
+
+inline angle::Result RenderbufferImpl::copyImageSubData(const gl::Context *context,
+                                                        const gl::Renderbuffer *srcBuffer,
+                                                        GLint srcLevel,
+                                                        GLint srcX,
+                                                        GLint srcY,
+                                                        GLint srcZ,
+                                                        GLint dstLevel,
+                                                        GLint dstX,
+                                                        GLint dstY,
+                                                        GLint dstZ,
+                                                        GLsizei srcWidth,
+                                                        GLsizei srcHeight,
+                                                        GLsizei srcDepth)
+{
+    UNREACHABLE();
+    return angle::Result::Stop;
+}
+
+inline angle::Result RenderbufferImpl::copyImageSubData(const gl::Context *context,
+                                                        const gl::Texture *srcTexture,
+                                                        GLint srcLevel,
+                                                        GLint srcX,
+                                                        GLint srcY,
+                                                        GLint srcZ,
+                                                        GLint dstLevel,
+                                                        GLint dstX,
+                                                        GLint dstY,
+                                                        GLint dstZ,
+                                                        GLsizei srcWidth,
+                                                        GLsizei srcHeight,
+                                                        GLsizei srcDepth)
+{
+    UNREACHABLE();
+    return angle::Result::Stop;
+}
 
 inline GLint RenderbufferImpl::getMemorySize() const
 {
