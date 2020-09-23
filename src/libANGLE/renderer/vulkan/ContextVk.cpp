ca62202b26523707cbf5ec65690fdb6c1edbdba6
Jonah Ryan-Davis <jonahr@google.com>
2020-09-23 15:12:56 -0400

    Pass GL_VERSION info through ANGLE's GL_RENDERER string
    Chrome needs ANGLE to pass through the underlying driver vendor and
    version, which cannot always be determined by the SystemInfo library.
    This is done by construction GL_RENDERER in the frontend through
    combining GL_VENDOR, GL_RENDERER, and GL_VERSION from the backends.
    
    Example changes:
    Metal - before:
    ANGLE(Metal Renderer: AMD Radeon Pro 560X)
    Metal - after:
    ANGLE (AMD, Metal Renderer: AMD Radeon Pro 560X, Version 10.15.6 (Build 19G2021))
    
    Mac GL - before:
    ANGLE (ATI Technologies Inc., AMD Radeon Pro 560X OpenGL Engine, OpenGL 4.1 core)
    Mac GL - after
    ANGLE (ATI Technologies Inc., AMD Radeon Pro 560X OpenGL Engine, OpenGL 4.1 ATI-3.8.24)
    
    Mac SwANGLE - before:
    ANGLE (Vulkan 1.1.0(SwiftShader Device (Subzero) (0x0000C0DE)))
    Mac SwANGLE - after
    ANGLE (Google, Vulkan 1.1.0 (SwiftShader Device (Subzero) (0x0000C0DE)), SwiftShader driver-5.0.0)
    
    Windows GL - before:
    ANGLE (NVIDIA Corporation, Quadro P1000/PCIe/SSE2, OpenGL 4.5 core)
    Windows GL - after:
    ANGLE (NVIDIA Corporation, Quadro P1000/PCIe/SSE2, OpenGL 4.5.0 NVIDIA 390.77)
    
    Windows D3D11 - before:
    ANGLE (NVIDIA Quadro P1000 Direct3D11 vs_5_0 ps_5_0)
    Windows D3D - after:
    ANGLE (NVIDIA, NVIDIA Quadro P1000 Direct3D11 vs_5_0 ps_5_0, 23.21.13.9077)
    
    Windows D3D9 - after:
    ANGLE (NVIDIA, NVIDIA Quadro P1000 Direct3D9Ex vs_3_0 ps_3_0, nvldumdx.dll-23.21.13.9077)
    
    Windows vulkan - before:
    ANGLE (Vulkan 1.0.65(NVIDIA Quadro P1000 (0x00001CB1)))
    Windows vulkan - after
    ANGLE (NVIDIA, Vulkan 1.0.65 (NVIDIA Quadro P1000 (0x00001CB1)), 390.308.0)
    
    Windows SwANGLE - before:
    ANGLE (Vulkan 1.1.0(SwiftShader Device (Subzero) (0x0000C0DE)))
    Windows SwANGLE - after:
    ANGLE (Google, Vulkan 1.1.0 (SwiftShader Device (Subzero) (0x0000C0DE)), SwiftShader driver-5.0.0)
    
    Linux Mesa LLVM - before:
    ANGLE (VMware, Inc., llvmpipe (LLVM 10.0.0, 256 bits), OpenGL 3.3 core)
    Linux Mesa LLVM - after:
    ANGLE (VMware, Inc., llvmpipe (LLVM 10.0.0, 256 bits), OpenGL 3.3 (Core Profile) Mesa 20.1.2)
    
    Linux GL - before:
    ANGLE (NVIDIA Corporation, Quadro P1000/PCIe/SSE2, OpenGL 4.5 core)
    Linux GL - after:
    ANGLE (NVIDIA Corporation, Quadro P1000/PCIe/SSE2, OpenGL 4.5.0 NVIDIA 440.100)
    
    Linux vulkan - before:
    ANGLE (Vulkan 1.1.119(NVIDIA Quadro P1000 (0x00001CB1)))
    Linux vulkan - after:
    ANGLE (NVIDIA, Vulkan 1.1.119 (NVIDIA Quadro P1000 (0x00001CB1)), 440.400.0)
    
    Linux SwANGLE - before:
    ANGLE (Vulkan 1.1.0(SwiftShader Device (Subzero) (0x0000C0DE)))
    Linux SwANGLE - after:
    ANGLE (Google, Vulkan 1.1.0 (SwiftShader Device (Subzero) (0x0000C0DE)), SwiftShader driver-5.0.0)
    
    Bug: chromium:1126526
    Bug: chromium:1131248
    Change-Id: Ia618ebcd7f3caaeb376b4b6a03446732efdaeecb

--- src/libANGLE/renderer/vulkan/ContextVk.cpp
+++ src/libANGLE/renderer/vulkan/ContextVk.cpp
@@ -2572,12 +2572,6 @@ gl::GraphicsResetStatus ContextVk::getResetStatus()
     return gl::GraphicsResetStatus::NoError;
 }
 
-std::string ContextVk::getVendorString() const
-{
-    UNIMPLEMENTED();
-    return std::string();
-}
-
 std::string ContextVk::getRendererDescription() const
 {
     return mRenderer->getRendererDescription();
