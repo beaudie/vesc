6ad7299e413c02a6415d60510ba5315e77fc8e3b
Charlie Lao <cclao@google.com>
2020-09-22 17:33:32 -0700

    Vulkan: Move mReadOnlyDepth out of FramebufferDesc
    The depth read only or not should not affect framebuffer and renderpass
    creation. This CL moves this out of FramebufferDesc into FramebufferVk.
    
    Bug: b/168953278
    Change-Id: I5bd05b262b7b3b0dc70f9fb8fc4a3db5e7082916

--- src/libANGLE/renderer/vulkan/vk_cache_utils.cpp
+++ src/libANGLE/renderer/vulkan/vk_cache_utils.cpp
@@ -2192,11 +2192,6 @@ void FramebufferDesc::updateDepthStencil(ImageViewSubresourceSerial serial)
     update(kFramebufferDescDepthStencilIndex, serial);
 }
 
-void FramebufferDesc::updateReadOnlyDepth(bool readOnlyDepth)
-{
-    mReadOnlyDepth = readOnlyDepth;
-}
-
 size_t FramebufferDesc::hash() const
 {
     return angle::ComputeGenericHash(&mSerials, sizeof(mSerials[0]) * mMaxIndex) ^
@@ -2205,15 +2200,14 @@ size_t FramebufferDesc::hash() const
 
 void FramebufferDesc::reset()
 {
-    mMaxIndex      = 0;
-    mReadOnlyDepth = false;
+    mMaxIndex = 0;
     mColorUnresolveAttachmentMask.reset();
     memset(&mSerials, 0, sizeof(mSerials));
 }
 
 bool FramebufferDesc::operator==(const FramebufferDesc &other) const
 {
-    if (mMaxIndex != other.mMaxIndex || mReadOnlyDepth != other.mReadOnlyDepth ||
+    if (mMaxIndex != other.mMaxIndex ||
         mColorUnresolveAttachmentMask != other.mColorUnresolveAttachmentMask)
     {
         return false;
