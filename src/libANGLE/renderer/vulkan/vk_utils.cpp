05ad421041ed1fac2cb31ac62faa47d8c750f72c
Courtney Goeltzenleuchter <courtneygo@google.com>
2020-09-23 08:41:18 -0600

    Vulkan: fix ASSERT issue in ResourceSerialFactory
    Refactor ResourceSerialFactory::issueSerial so that the ASSERT is valid
    in a threaded environment. Otherwise it's possible for another thread to
    increment mCurrentUniqueSerial in the middle of the ASSERT.
    
    Bug: b/168744561
    Change-Id: I7208e8be91b21c50ad4a260d02d69f5b7dab2efe

--- src/libANGLE/renderer/vulkan/vk_utils.cpp
+++ src/libANGLE/renderer/vulkan/vk_utils.cpp
@@ -764,8 +764,9 @@ ResourceSerialFactory::~ResourceSerialFactory() {}
 
 uint32_t ResourceSerialFactory::issueSerial()
 {
-    ASSERT(mCurrentUniqueSerial + 1 > mCurrentUniqueSerial);
-    return mCurrentUniqueSerial++;
+    uint32_t newSerial = ++mCurrentUniqueSerial;
+    ASSERT(newSerial > 0);
+    return newSerial;
 }
 
 #define ANGLE_DEFINE_GEN_VK_SERIAL(Type)                         \
