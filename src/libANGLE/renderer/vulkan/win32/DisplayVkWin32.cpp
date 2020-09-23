5e9ba0ff59e0d17c66205356e27a3b03f9cd0f0d
Jeff Vigil <j.vigil@samsung.com>
2020-09-23 12:41:02 -0700

    EGL: Add stencil8 to configs
    Add stencil8 option to Depth and Stencil of configs based on
    support of stencil8 format from GLES.
    Add stencil8 to configs
    Adjust dEQP-EGL expectations
    Unblocks dEQP-EGL.functional.*.*_no_depth_stencil tests
    Test: angle_deqp_egl_tests
    
    Bug: angleproject:3231
    Change-Id: I57d7085a71a5b0dc45803351c9116a1694668852

--- src/libANGLE/renderer/vulkan/win32/DisplayVkWin32.cpp
+++ src/libANGLE/renderer/vulkan/win32/DisplayVkWin32.cpp
@@ -134,9 +134,18 @@ egl::Error DisplayVkWin32::initialize(egl::Display *display)
 
 egl::ConfigSet DisplayVkWin32::generateConfigs()
 {
-    constexpr GLenum kColorFormats[] = {GL_RGB565, GL_BGRA8_EXT, GL_BGRX8_ANGLEX, GL_RGB10_A2_EXT,
-                                        GL_RGBA16F_EXT};
-    return egl_vk::GenerateConfigs(kColorFormats, egl_vk::kConfigDepthStencilFormats, this);
+    const std::vector<GLenum> kColorFormats = {GL_RGB565, GL_BGRA8_EXT, GL_BGRX8_ANGLEX,
+                                               GL_RGB10_A2_EXT, GL_RGBA16F_EXT};
+
+    angle::FixedVector<GLenum, 5> depthStencilFormats = {
+        GL_NONE, GL_DEPTH24_STENCIL8, GL_DEPTH_COMPONENT24, GL_DEPTH_COMPONENT16};
+
+    if (getCaps().stencil8)
+    {
+        depthStencilFormats.push_back(GL_STENCIL_INDEX8);
+    }
+    return egl_vk::GenerateConfigs(kColorFormats.data(), kColorFormats.size(),
+                                   depthStencilFormats.data(), depthStencilFormats.size(), this);
 }
 
 void DisplayVkWin32::checkConfigSupport(egl::Config *config)
