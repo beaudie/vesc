5337debddd6809bcc76393b61eb1a0bb995756be
Hyunchang Kim <hckim.kim@samsung.com>
2020-09-28 17:58:31 +0900

    Vulkan: Support OES_texture_cube_map_array extension
    Add support for GL_OES_texture_cube_map_array to the Vulkan backend
    if the underlying ICD supports creation of of cube array ImageViews
    
    Bug: angleproject:3584
    Test: dEQP-GLES31.functional.fbo.color.texcubearray.*
    Change-Id: I636cbf347d718abfc1600119b312578370b8e02e

--- src/libANGLE/renderer/vulkan/vk_caps_utils.cpp
+++ src/libANGLE/renderer/vulkan/vk_caps_utils.cpp
@@ -203,6 +203,10 @@ void RendererVk::ensureCapsInitialized() const
 
     mNativeExtensions.textureFilteringCHROMIUM = getFeatures().supportsFilteringPrecision.enabled;
 
+    // Only expose texture cubemap array if the physical device supports it.
+    mNativeExtensions.textureCubeMapArrayOES = (mPhysicalDeviceFeatures.imageCubeArray == VK_TRUE);
+    mNativeExtensions.textureCubeMapArrayEXT = (mPhysicalDeviceFeatures.imageCubeArray == VK_TRUE);
+
     mNativeExtensions.shadowSamplersEXT = true;
 
     // https://vulkan.lunarg.com/doc/view/1.0.30.0/linux/vkspec.chunked/ch31s02.html
