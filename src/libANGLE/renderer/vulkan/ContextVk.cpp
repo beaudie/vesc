f7a7a5b3d73ebb176075642488e4e66e6896bcb0
Courtney Goeltzenleuchter <courtneygo@google.com>
2020-09-30 16:49:54 -0600

    DEBUG: debug statements
    Bug: b/154030730
    Change-Id: Ibeaafa7c665184602a7d098e750373605c4e14cd

--- src/libANGLE/renderer/vulkan/ContextVk.cpp
+++ src/libANGLE/renderer/vulkan/ContextVk.cpp
@@ -392,6 +392,7 @@ angle::Result CommandQueue::checkCompletedCommands(vk::Context *context)
         }
         ANGLE_VK_TRY(context, result);
 
+        WARN() << "completed serial: " << batch.serial.getValue();
         renderer->onCompletedSerial(batch.serial);
 
         renderer->resetSharedFence(&batch.fence);
