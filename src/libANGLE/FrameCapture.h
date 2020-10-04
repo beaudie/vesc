caa542868827e7ccfde88d3e2b7d211d25b5da6a
Jamie Madill <jmadill@chromium.org>
2020-10-04 12:18:29 -0400

    Capture/Replay: Fix temp shader ID.
    We could mess up capturing Program #1 by using a temp shader ID of 1.
    Instead we can use a shader ID that isn't used in the application.
    Noticed when capturing from the T-Rex replay.
    
    Bug: angleproject:5134
    Change-Id: Ic6fefe9d8cdf327a5ebbb47539b2e11161a4b13c

--- src/libANGLE/FrameCapture.h
+++ src/libANGLE/FrameCapture.h
@@ -253,6 +253,9 @@ class ResourceTracker final : angle::NonCopyable
         mStartingBuffersMappedInitial[id] = mapped;
     }
 
+    void onShaderProgramAccess(gl::ShaderProgramID shaderProgramID);
+    uint32_t getMaxShaderPrograms() const { return mMaxShaderPrograms; }
+
   private:
     // Buffer regen calls will delete and gen a buffer
     BufferCalls mBufferRegenCalls;
@@ -276,6 +279,9 @@ class ResourceTracker final : angle::NonCopyable
     BufferMapStatusMap mStartingBuffersMappedInitial;
     // The status of buffer mapping throughout the trace, modified with each Map/Unmap call
     BufferMapStatusMap mStartingBuffersMappedCurrent;
+
+    // Maximum accessed shader program ID.
+    uint32_t mMaxShaderPrograms = 0;
 };
 
 // Used by the CPP replay to filter out unnecessary code.
