a81fe54c87d44ddaa4986ffbaa2aa2e2faa73605
Charlie Lao <cclao@google.com>
2020-10-06 14:55:48 -0700

    Vulkan: Introduce and use late fragment test ImageLayout when possible
    If early fragment test is never enabled during entire render pass, we
    can use the DepthStencilAttachmentLate layout that will only block
    VK_PIPELINE_STAGE_LATE_FRAGMENT_TESTS_BIT stage. Similarly this CL
    introduces and uses DepthStencilAttachmentLateReadOnly for read only
    depth stencil if early fragment test never enabled. This helps reduce
    the barrier and thus improves performance.
    
    Bug: b/169891513
    Change-Id: Ic4913b60b626086c55c21fb0fb757606ec71d994

--- src/libANGLE/renderer/vulkan/vk_helpers.cpp
+++ src/libANGLE/renderer/vulkan/vk_helpers.cpp
@@ -170,6 +170,21 @@ constexpr angle::PackedEnumMap<ImageLayout, ImageMemoryBarrierData> kImageMemory
         },
     },
     {
+        ImageLayout::DepthStencilAttachmentLateReadOnly,
+            ImageMemoryBarrierData{
+            "DepthStencilAttachmentLateReadOnly",
+            VK_IMAGE_LAYOUT_DEPTH_STENCIL_READ_ONLY_OPTIMAL,
+            VK_PIPELINE_STAGE_LATE_FRAGMENT_TESTS_BIT,
+            VK_PIPELINE_STAGE_LATE_FRAGMENT_TESTS_BIT,
+            // Transition to: all reads must happen after barrier.
+            VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_READ_BIT,
+            // Transition from: RAR and WAR don't need memory barrier.
+            0,
+            ResourceAccess::ReadOnly,
+            PipelineStage::LateFragmentTest,
+        },
+    },
+    {
         ImageLayout::DepthStencilAttachment,
         ImageMemoryBarrierData{
             "DepthStencilAttachment",
@@ -182,6 +197,21 @@ constexpr angle::PackedEnumMap<ImageLayout, ImageMemoryBarrierData> kImageMemory
             VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT,
             ResourceAccess::Write,
             PipelineStage::EarlyFragmentTest,
+        },
+    },
+    {
+        ImageLayout::DepthStencilAttachmentLate,
+        ImageMemoryBarrierData{
+            "DepthStencilAttachmentLate",
+            VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL,
+            VK_PIPELINE_STAGE_LATE_FRAGMENT_TESTS_BIT,
+            VK_PIPELINE_STAGE_LATE_FRAGMENT_TESTS_BIT,
+            // Transition to: all reads and writes must happen after barrier.
+            VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_READ_BIT | VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT,
+            // Transition from: all writes must finish before barrier.
+            VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT,
+            ResourceAccess::Write,
+            PipelineStage::LateFragmentTest,
         },
     },
     {
@@ -651,6 +681,7 @@ CommandBufferHelper::CommandBufferHelper()
       mTransformFeedbackCounterBuffers{},
       mValidTransformFeedbackBufferCount(0),
       mRebindTransformFeedbackBuffers(false),
+      mEarlyFragmentTestEverEnabled(false),
       mIsRenderPassCommandBuffer(false),
       mReadOnlyDepthStencilMode(false),
       mDepthAccess(ResourceAccess::Unused),
@@ -937,14 +968,17 @@ void CommandBufferHelper::finalizeDepthStencilImageLayout()
         }
         else
         {
-            imageLayout     = ImageLayout::DepthStencilAttachmentReadOnly;
+            imageLayout = mEarlyFragmentTestEverEnabled
+                              ? ImageLayout::DepthStencilAttachmentReadOnly
+                              : ImageLayout::DepthStencilAttachmentLateReadOnly;
             barrierRequired = mDepthStencilImage->isReadBarrierNecessary(imageLayout);
         }
     }
     else
     {
         // Write always requires a barrier
-        imageLayout     = ImageLayout::DepthStencilAttachment;
+        imageLayout = mEarlyFragmentTestEverEnabled ? ImageLayout::DepthStencilAttachment
+                                                    : ImageLayout::DepthStencilAttachmentLate;
         barrierRequired = true;
     }
 
@@ -1289,9 +1323,10 @@ void CommandBufferHelper::reset()
         mStencilCmdSizeDisabled            = kInfiniteCmdSize;
         mDepthStencilAttachmentIndex       = kAttachmentIndexInvalid;
         mRenderPassUsedImages.clear();
-        mDepthStencilImage        = nullptr;
-        mDepthStencilResolveImage = nullptr;
-        mReadOnlyDepthStencilMode = false;
+        mDepthStencilImage            = nullptr;
+        mDepthStencilResolveImage     = nullptr;
+        mReadOnlyDepthStencilMode     = false;
+        mEarlyFragmentTestEverEnabled = false;
     }
     // This state should never change for non-renderPass command buffer
     ASSERT(mRenderPassStarted == false);
