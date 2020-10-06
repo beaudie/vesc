961d34a87326e20e00d4dd7e4dde236b1af5ead7
Charlie Lao <cclao@google.com>
2020-10-06 13:34:53 -0700

    Vulkan: Fine-grained depth stencil layout transition barrier
    Gfxbench Manhattan uses depth attachment as texture in the same render
    pass, which creates a feedback loop. The layout we are using for this
    has kAllShadersPipelineStageFlags even though it only used by fragment
    shader. This creates an unnecessary TA/3D dependency that hurts
    performance on some mobile GPUs. This CL renames DepthStencilReadOnly to
    DepthStencilAllShadersReadOnly, adds DepthStencilFragmentShaderReadOnly
    and DepthStencilAttachmentReadOnly layouts so that if it is used by
    fragment shader, we only barrier against fragment stage. If not used by
    any shader, we don't block any shader stages.
    
    Bug: b/169891513
    Change-Id: Iede497e8af028861f9166a32d122f26dc7aa864a

--- src/libANGLE/renderer/vulkan/vk_helpers.cpp
+++ src/libANGLE/renderer/vulkan/vk_helpers.cpp
@@ -125,14 +125,44 @@ constexpr angle::PackedEnumMap<ImageLayout, ImageMemoryBarrierData> kImageMemory
         },
     },
     {
-        ImageLayout::DepthStencilReadOnly,
-        ImageMemoryBarrierData{
-            "DepthStencilReadOnly",
+        ImageLayout::DepthStencilFragmentShaderReadOnly,
+            ImageMemoryBarrierData{
+            "DepthStencilFragmentShaderReadOnly",
+            VK_IMAGE_LAYOUT_DEPTH_STENCIL_READ_ONLY_OPTIMAL,
+            VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT | VK_PIPELINE_STAGE_EARLY_FRAGMENT_TESTS_BIT,
+            VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT | VK_PIPELINE_STAGE_LATE_FRAGMENT_TESTS_BIT,
+            // Transition to: all reads must happen after barrier.
+            VK_ACCESS_SHADER_READ_BIT | VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_READ_BIT,
+            // Transition from: RAR and WAR don't need memory barrier.
+            0,
+            ResourceAccess::ReadOnly,
+            PipelineStage::EarlyFragmentTest,
+        },
+    },
+    {
+        ImageLayout::DepthStencilAllShadersReadOnly,
+            ImageMemoryBarrierData{
+            "DepthStencilAllShadersReadOnly",
             VK_IMAGE_LAYOUT_DEPTH_STENCIL_READ_ONLY_OPTIMAL,
             kAllShadersPipelineStageFlags | VK_PIPELINE_STAGE_EARLY_FRAGMENT_TESTS_BIT,
             kAllShadersPipelineStageFlags | VK_PIPELINE_STAGE_LATE_FRAGMENT_TESTS_BIT,
             // Transition to: all reads must happen after barrier.
             VK_ACCESS_SHADER_READ_BIT | VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_READ_BIT,
+            // Transition from: RAR and WAR don't need memory barrier.
+            0,
+            ResourceAccess::ReadOnly,
+            PipelineStage::VertexShader,
+        },
+    },
+    {
+        ImageLayout::DepthStencilAttachmentReadOnly,
+            ImageMemoryBarrierData{
+            "DepthStencilAttachmentReadOnly",
+            VK_IMAGE_LAYOUT_DEPTH_STENCIL_READ_ONLY_OPTIMAL,
+            VK_PIPELINE_STAGE_EARLY_FRAGMENT_TESTS_BIT,
+            VK_PIPELINE_STAGE_LATE_FRAGMENT_TESTS_BIT,
+            // Transition to: all reads must happen after barrier.
+            VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_READ_BIT,
             // Transition from: RAR and WAR don't need memory barrier.
             0,
             ResourceAccess::ReadOnly,
@@ -573,7 +603,8 @@ bool IsExternalQueueFamily(uint32_t queueFamilyIndex)
 
 bool IsShaderReadOnlyLayout(const ImageMemoryBarrierData &imageLayout)
 {
-    return imageLayout.layout == VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
+    return imageLayout.layout == VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL ||
+           imageLayout.layout == VK_IMAGE_LAYOUT_DEPTH_STENCIL_READ_ONLY_OPTIMAL;
 }
 }  // anonymous namespace
 
@@ -736,6 +767,22 @@ void CommandBufferHelper::imageRead(ResourceUseList *resourceUseList,
         {
             mRenderPassUsedImages.insert(image->getImageSerial().getValue());
         }
+        else if (image == mDepthStencilImage)
+        {
+            // If the image is also used as depth stencil attachment, this is called feedback loop.
+            // Without feedback loop, the depthstencil image layout is determined at
+            // finalizeDepthStencilImageLayout call, which get called by endRenderPass. But with
+            // feedback loop, we already picked the layout (because descriptor set needs it). To
+            // prevent finalizeDepthStencilImageLayout try to pick layout again which might be
+            // different from what the texture code already picked, we set mDepthStencilImage to
+            // null here.
+            mDepthStencilImage = nullptr;
+            mAttachmentOps.setLayouts(mDepthStencilAttachmentIndex, imageLayout, imageLayout);
+        }
+        else
+        {
+            ASSERT(image != mDepthStencilResolveImage);
+        }
     }
 }
 
@@ -897,7 +944,7 @@ void CommandBufferHelper::finalizeDepthStencilImageLayout()
 
     if (mReadOnlyDepthStencilMode)
     {
-        imageLayout     = ImageLayout::DepthStencilReadOnly;
+        imageLayout     = ImageLayout::DepthStencilAttachmentReadOnly;
         barrierRequired = mDepthStencilImage->isReadBarrierNecessary(imageLayout);
     }
     else
@@ -1032,6 +1079,21 @@ void CommandBufferHelper::endRenderPass(ContextVk *contextVk)
 
     // Ensure we don't write to a read-only RenderPass. (ReadOnly -> !Write)
     ASSERT(!mReadOnlyDepthStencilMode || mDepthAccess != ResourceAccess::Write);
+
+    // Fill out perf counters
+    PerfCounters &counters = contextVk->getPerfCounters();
+
+    counters.depthClears += dsOps.loadOp == VK_ATTACHMENT_LOAD_OP_CLEAR ? 1 : 0;
+    counters.depthLoads += dsOps.loadOp == VK_ATTACHMENT_LOAD_OP_LOAD ? 1 : 0;
+    counters.depthStores += dsOps.storeOp == VK_ATTACHMENT_STORE_OP_STORE ? 1 : 0;
+    counters.stencilClears += dsOps.stencilLoadOp == VK_ATTACHMENT_LOAD_OP_CLEAR ? 1 : 0;
+    counters.stencilLoads += dsOps.stencilLoadOp == VK_ATTACHMENT_LOAD_OP_LOAD ? 1 : 0;
+    counters.stencilStores += dsOps.stencilStoreOp == VK_ATTACHMENT_STORE_OP_STORE ? 1 : 0;
+    counters.readOnlyDepthStencilRenderPasses +=
+        kImageMemoryBarrierData[static_cast<ImageLayout>(dsOps.finalLayout)].type ==
+                ResourceAccess::ReadOnly
+            ? 1
+            : 0;
 }
 
 void CommandBufferHelper::beginTransformFeedback(size_t validBufferCount,
