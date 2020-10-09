6e2decd55c1c53743df11ac65cce44804e130d7a
Cody Northrop <cnorthrop@google.com>
2020-10-08 22:38:25 -0600

    Allow glBindImageTexture with no image units
    Detected when capturing LEGO Legacy.
    
    Test: TODO add a test
    Bug: ??
    Change-Id: I9aee2ee173436f5de5062758a70e0909f6fc9a7f

--- src/libANGLE/State.cpp
+++ src/libANGLE/State.cpp
@@ -3422,6 +3422,9 @@ void State::setImageUnit(const Context *context,
                          GLenum access,
                          GLenum format)
 {
+    if (mImageUnits.empty())
+        return;
+
     ImageUnit &imageUnit = mImageUnits[unit];
 
     if (texture)
