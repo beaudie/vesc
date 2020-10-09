d4dfb0f2e08ab38ca95c3c85cc5d4585b43c2030
Shahbaz Youssefi <syoussefi@chromium.org>
2020-10-09 11:03:29 -0400

    Vulkan: Use depthClamp to clear depth where available
    This will avoid breaking render pass if clearing depth in clearWithDraw.
    
    Bug: angleproject:4836
    Change-Id: I50242d1115efc91059923143f6ae5fd25fb3d36f

--- src/libANGLE/renderer/vulkan/FramebufferVk.cpp
+++ src/libANGLE/renderer/vulkan/FramebufferVk.cpp
@@ -2017,7 +2017,9 @@ angle::Result FramebufferVk::clearWithDraw(ContextVk *contextVk,
                                            const VkClearColorValue &clearColorValue,
                                            const VkClearDepthStencilValue &clearDepthStencilValue)
 {
-    if (clearDepth)
+    RendererVk *renderer = contextVk->getRenderer();
+
+    if (clearDepth && !renderer->getPhysicalDeviceFeatures().depthClamp)
     {
         VkClearValue clearValue;
         clearValue.depthStencil = clearDepthStencilValue;
@@ -2034,10 +2036,11 @@ angle::Result FramebufferVk::clearWithDraw(ContextVk *contextVk,
     UtilsVk::ClearFramebufferParameters params = {};
     params.clearArea                           = clearArea;
     params.colorClearValue                     = clearColorValue;
-    params.stencilClearValue = static_cast<uint8_t>(clearDepthStencilValue.stencil);
-    params.stencilMask       = stencilMask;
+    params.depthStencilClearValue              = clearDepthStencilValue;
+    params.stencilMask                         = stencilMask;
 
     params.clearColor   = true;
+    params.clearDepth   = clearDepth;
     params.clearStencil = clearStencil;
 
     const auto &colorRenderTargets = mRenderTargetCache.getColors();
@@ -2058,12 +2061,13 @@ angle::Result FramebufferVk::clearWithDraw(ContextVk *contextVk,
 
         ANGLE_TRY(contextVk->getUtils().clearFramebuffer(contextVk, this, params));
 
-        // Clear stencil only once!
+        // Clear depth/stencil only once!
+        params.clearDepth   = false;
         params.clearStencil = false;
     }
 
-    // If there was no color clear, clear stencil alone.
-    if (params.clearStencil)
+    // If there was no color clear, clear depth/stencil alone.
+    if (params.clearDepth || params.clearStencil)
     {
         params.clearColor = false;
         ANGLE_TRY(contextVk->getUtils().clearFramebuffer(contextVk, this, params));
