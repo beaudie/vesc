bd087d8370ceeeb165146bdac1955dbf16266719
Shahbaz Youssefi <syoussefi@chromium.org>
2020-10-09 15:27:28 -0400

    Vulkan: Don't break the render pass on scissored clears
    clearImmediatelyWithRenderPassOp is removed and the draw path is used
    for the scissor.  That path was added to avoid creating a large number
    of graphics pipelines due to the scissor state.  This is now done by
    using dynamic state for scissor in the draw path for clear.
    
    Running the following dEQP tests without and with dynamic state for
    scissor:
    
        dEQP-GLES3.functional.fragment_ops.depth_stencil.stencil_ops.*
    
    the number of graphics pipelines is reduced from 95392 to 16.
    
    Bug: angleproject:4617
    Bug: angleproject:4836
    Change-Id: Ib373d8cd23ca2b67e6fd26aa2a1103f281f7e473

--- src/libANGLE/renderer/vulkan/FramebufferVk.h
+++ src/libANGLE/renderer/vulkan/FramebufferVk.h
@@ -174,15 +174,6 @@ class FramebufferVk : public FramebufferImpl
                             const VkClearColorValue &clearColorValue,
                             const VkClearDepthStencilValue &clearDepthStencilValue);
 
-    angle::Result clearImmediatelyWithRenderPassOp(
-        ContextVk *contextVk,
-        const gl::Rectangle &clearArea,
-        gl::DrawBufferMask clearColorBuffers,
-        bool clearDepth,
-        bool clearStencil,
-        const VkClearColorValue &clearColorValue,
-        const VkClearDepthStencilValue &clearDepthStencilValue);
-
     angle::Result clearWithDraw(ContextVk *contextVk,
                                 const gl::Rectangle &clearArea,
                                 gl::DrawBufferMask clearColorBuffers,
