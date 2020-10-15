70de0a4a5d690143976453075d8cf83ba5e9ec6c
Courtney Goeltzenleuchter <courtneygo@google.com>
2020-10-15 15:23:30 -0600

    WIP: Vulkan: Add task ID for worker jobs
    Add task ID to make it easier to know what work is being done when.
    Also enables us to wait for specific taskIDs to be finished rather than
    waiting for the entire queue to empty.
    
    Change-Id: Ifd7d9decaa83b0eb85a5edecc14e8ebf65732a57

--- src/libANGLE/renderer/vulkan/CommandProcessor.h
+++ src/libANGLE/renderer/vulkan/CommandProcessor.h
@@ -87,6 +87,8 @@ class CommandProcessorTask
         *this = std::move(other);
     }
 
+    void setTaskSerial(Serial serial) { mTaskSerial = serial; }
+    Serial getTaskSerial() const { return mTaskSerial; }
     void setQueueSerial(Serial serial) { mSerial = serial; }
     Serial getQueueSerial() const { return mSerial; }
     vk::ResourceUseList &getResourceUseList() { return mResourceUseList; }
@@ -109,6 +111,7 @@ class CommandProcessorTask
 
   private:
     CustomTask mTask;
+    Serial mTaskSerial;
 
     // ProcessCommands
     ContextVk *mContextVk;
@@ -296,9 +299,11 @@ class CommandProcessor : public vk::Context
     TaskProcessor mTaskProcessor;
 
     AtomicSerialFactory mQueueSerialFactory;
+    AtomicSerialFactory mTaskSerialFactory;
     std::mutex mCommandProcessorQueueSerialMutex;
     Serial mCommandProcessorLastSubmittedSerial;
     Serial mCommandProcessorCurrentQueueSerial;
+    Serial mTaskSerial;
 
     mutable std::mutex mErrorMutex;
     std::queue<vk::Error> mErrors;
