32e244ac683b6b01693cf024eec730cc2b7ce4a8
Mohan Maiya <m.maiya@samsung.com>
2020-10-15 17:08:56 -0700

    Vulkan: Add OES_shader_multisample_interpolation support
    Support OES_shader_multisample_interpolation extension if
    (maxInterpolationOffset - (1 / pow(2, subPixelInterpolationOffsetBits))
    >= 0.5)
    
    Bug: angleproject:3589
    Tests: dEQP-GLES31.functional.shaders.multisample_interpolation.*
           dEQP-GLES31.functional.state_query.multisample_interpolation.*
    Change-Id: I42997f10be82e3be8b63c56833cbbf791bf4be9b

--- src/compiler/translator/ParseContext.cpp
+++ src/compiler/translator/ParseContext.cpp
@@ -534,6 +534,7 @@ bool TParseContext::checkCanBeLValue(const TSourceLoc &line, const char *op, TIn
         case EvqNoPerspectiveIn:
         case EvqSmoothIn:
         case EvqCentroidIn:
+        case EvqSampleIn:
             message = "can't modify an input";
             break;
         case EvqUniform:
@@ -5983,6 +5984,67 @@ void TParseContext::checkSingleTextureOffset(const TSourceLoc &line,
     }
 }
 
+bool TParseContext::isFragmentInQualifier(const TIntermTyped *arg0) const
+{
+    if (arg0->getType().getQualifier() != EvqVaryingIn &&
+        arg0->getType().getQualifier() != EvqFragmentIn &&
+        arg0->getType().getQualifier() != EvqCentroidIn &&
+        arg0->getType().getQualifier() != EvqSampleIn &&
+        arg0->getType().getQualifier() != EvqSmoothIn &&
+        arg0->getType().getQualifier() != EvqFlatIn &&
+        arg0->getType().getQualifier() != EvqNoPerspectiveIn)
+    {
+        return false;
+    }
+    return true;
+}
+
+void TParseContext::checkInterpolationFS(TIntermAggregate *functionCall)
+{
+    const TFunction *func = functionCall->getFunction();
+    if (BuiltInGroup::isInterpolationFS(func))
+    {
+        TIntermTyped *arg0 = nullptr;
+
+        if (functionCall->getAsAggregate())
+        {
+            const TIntermSequence *argp = functionCall->getSequence();
+            if (argp->size() > 0)
+                arg0 = (*argp)[0]->getAsTyped();
+        }
+        else
+        {
+            assert(functionCall->getAsUnaryNode());
+            arg0 = functionCall->getAsUnaryNode()->getOperand();
+        }
+
+        // Make sure the first argument is an interpolant, or an array element of an interpolant
+        if (!isFragmentInQualifier(arg0))
+        {
+            // It might still be an array element.
+            auto findLValueBase = [](TIntermTyped *node) {
+                do
+                {
+                    const TIntermBinary *binary = node->getAsBinaryNode();
+                    if (binary == nullptr)
+                        return node;
+                    TOperator op = binary->getOp();
+                    if (op != EOpIndexDirect && op != EOpIndexIndirect)
+                        return static_cast<TIntermTyped *>(nullptr);
+
+                    node = node->getAsBinaryNode()->getLeft();
+                } while (true);
+            };
+            const TIntermTyped *base = findLValueBase(arg0);
+
+            if (base == nullptr || (!isFragmentInQualifier(base)))
+                error(arg0->getLine(),
+                      "first argument must be an interpolant, or interpolant-array element",
+                      func->name());
+        }
+    }
+}
+
 void TParseContext::checkAtomicMemoryBuiltinFunctions(TIntermAggregate *functionCall)
 {
     const TFunction *func = functionCall->getFunction();
@@ -6248,6 +6310,7 @@ TIntermTyped *TParseContext::addNonConstructorFunctionCall(TFunctionLookup *fnCa
             callNode->setLine(loc);
             checkTextureOffset(callNode);
             checkTextureGather(callNode);
+            checkInterpolationFS(callNode);
             checkImageMemoryAccessForBuiltinFunctions(callNode);
             functionCallRValueLValueErrorCheck(fnCandidate, callNode);
             return callNode;
