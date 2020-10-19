de1410d27552a1e79ba2ccf8b2f240e683a5089b
Courtney Goeltzenleuchter <courtneygo@google.com>
2020-10-18 19:09:47 -0600

    Allow single threaded CommandProcessor
    In replacing the legacy CommandQueue code with the threading
    capable CommandProcessor it would be good to be able to run the
    CommandProcessor in a single-thread environment. This CL changes the
    meaning of the feature flags for the commandProcessor and
    asynchronousCommandProcessing so that enabling commandProcess only
    changes the code paths to use the command processor but work it still
    done as part of the submitting thread (e.g. ContextVk).
    Enabling asynchronousCommandProcessing will cause a separate worker
    thread to be spawned which will asynchronously process the commands.
    This allows us to switch to the CommandProcessor without threading and
    then enable threading once performance issues are resolved.
    
    Bug: b/161912801
    Bug: b/170329600
    Change-Id: I534862b109a7e7708108190b7c3e894071d4c2ed

--- src/libANGLE/renderer/vulkan/RendererVk.h
+++ src/libANGLE/renderer/vulkan/RendererVk.h
@@ -227,9 +227,9 @@ class RendererVk : angle::NonCopyable
         }
     }
 
-    vk::Shared<vk::Fence> getLastSubmittedFence() const
+    vk::Shared<vk::Fence> getLastSubmittedFence(const vk::Context *context) const
     {
-        return mCommandProcessor.getLastSubmittedFence();
+        return mCommandProcessor.getLastSubmittedFence(context);
     }
     void handleDeviceLost() { mCommandProcessor.handleDeviceLost(); }
 
@@ -250,7 +250,7 @@ class RendererVk : angle::NonCopyable
 
     ANGLE_INLINE Serial getCurrentQueueSerial()
     {
-        if (getFeatures().enableCommandProcessingThread.enabled)
+        if (getFeatures().commandProcessor.enabled)
         {
             return mCommandProcessor.getCurrentQueueSerial();
         }
@@ -259,7 +259,7 @@ class RendererVk : angle::NonCopyable
     }
     ANGLE_INLINE Serial getLastSubmittedQueueSerial()
     {
-        if (getFeatures().enableCommandProcessingThread.enabled)
+        if (getFeatures().commandProcessor.enabled)
         {
             return mCommandProcessor.getLastSubmittedSerial();
         }
@@ -273,6 +273,11 @@ class RendererVk : angle::NonCopyable
     }
 
     void onCompletedSerial(Serial serial);
+
+    VkResult getLastPresentResult(VkSwapchainKHR swapchain)
+    {
+        return mCommandProcessor.getLastPresentResult(swapchain);
+    }
 
     bool enableDebugUtils() const { return mEnableDebugUtils; }
 
@@ -289,6 +294,7 @@ class RendererVk : angle::NonCopyable
     vk::Error getAndClearPendingError() { return mCommandProcessor.getAndClearPendingError(); }
     void waitForCommandProcessorIdle(vk::Context *context)
     {
+        ASSERT(getFeatures().asynchronousCommandProcessing.enabled);
         mCommandProcessor.waitForWorkComplete(context);
     }
 
@@ -308,6 +314,8 @@ class RendererVk : angle::NonCopyable
 
     void outputVmaStatString();
 
+    angle::Result cleanupGarbage(bool block);
+
   private:
     angle::Result initializeDevice(DisplayVk *displayVk, uint32_t queueFamilyIndex);
     void ensureCapsInitialized() const;
@@ -326,8 +334,6 @@ class RendererVk : angle::NonCopyable
 
     template <VkFormatFeatureFlags VkFormatProperties::*features>
     bool hasFormatFeatureBits(VkFormat format, const VkFormatFeatureFlags featureBits) const;
-
-    angle::Result cleanupGarbage(bool block);
 
     egl::Display *mDisplay;
 
