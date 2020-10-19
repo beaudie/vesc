d0dea5346e3f7a2ea36d04485b4b73db6443de4d
Courtney Goeltzenleuchter <courtneygo@google.com>
2020-10-18 19:22:58 -0600

    WIP: debug prints
    Change-Id: Iff682263d22f0c519904b82faa02c27bca51b6e9

--- src/libANGLE/renderer/vulkan/CommandProcessor.cpp
+++ src/libANGLE/renderer/vulkan/CommandProcessor.cpp
@@ -564,6 +564,9 @@ void CommandProcessor::queueCommand(vk::Context *context, vk::CommandProcessorTa
 
     if (context->getRenderer()->getFeatures().asynchronousCommandProcessing.enabled)
     {
+        WARN() << "Queue: task id: " << task->getTaskSerial().getValue()
+               << ", cmd: " << task->getTaskCommand()
+               << ", queue serial: " << task->getQueueSerial().getValue();
         mTasks.emplace(std::move(*task));
         mWorkAvailableCondition.notify_one();
     }
@@ -655,6 +658,9 @@ angle::Result CommandProcessor::processTasksImpl(bool *exitThread)
 
 angle::Result CommandProcessor::processTask(vk::Context *context, vk::CommandProcessorTask *task)
 {
+    WARN() << "Process: task id: " << task->getTaskSerial().getValue()
+           << ", cmd: " << task->getTaskCommand()
+           << ", serial: " << task->getQueueSerial().getValue();
     switch (task->getTaskCommand())
     {
         case vk::CustomTask::Exit:
