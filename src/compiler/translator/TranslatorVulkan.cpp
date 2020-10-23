1a28fa409881373ad436aea62685a5b414f2f4ac
Mohan Maiya <m.maiya@samsung.com>
2020-10-23 14:41:34 -0700

    Vulkan: Support gl_SampleMask with non-const index
    This commit makes OES_sample_variables extension
    support gl_SampleMask or gl_SampleMaskIn with non
    constant index, using replacing gl_SampleMask
    with ANGLESampleMask and reassigning
    ANGLESampleMask to gl_SampleMask at the last
    
    Bug: angleproject:3588
    Tests: dEQP-GLES31.functional.shaders.sample_variables.sample_mask.
               discard_half_per_pixel.*
               discard_half_per_sample.*
    Change-Id: Ie668c3ccf4d40352574db57f5e43b2f17ef6d8b8

--- src/compiler/translator/TranslatorVulkan.cpp
+++ src/compiler/translator/TranslatorVulkan.cpp
@@ -31,6 +31,7 @@
 #include "compiler/translator/tree_util/FindMain.h"
 #include "compiler/translator/tree_util/IntermNode_util.h"
 #include "compiler/translator/tree_util/ReplaceClipDistanceVariable.h"
+#include "compiler/translator/tree_util/ReplaceSampleMaskVariables.h"
 #include "compiler/translator/tree_util/ReplaceVariable.h"
 #include "compiler/translator/tree_util/RunAtTheEndOfShader.h"
 #include "compiler/translator/util.h"
@@ -928,14 +929,21 @@ bool TranslatorVulkan::translateImpl(TIntermBlock *root,
     // if it's core profile shaders and they are used.
     if (getShaderType() == GL_FRAGMENT_SHADER)
     {
-        bool usesPointCoord = false;
-        bool usesFragCoord  = false;
+        bool usesPointCoord   = false;
+        bool usesFragCoord    = false;
+        bool usesSampleMaskIn = false;
 
         // Search for the gl_PointCoord usage, if its used, we need to flip the y coordinate.
         for (const ShaderVariable &inputVarying : mInputVaryings)
         {
             if (!inputVarying.isBuiltIn())
             {
+                continue;
+            }
+
+            if (inputVarying.name == "gl_SampleMaskIn")
+            {
+                usesSampleMaskIn = true;
                 continue;
             }
 
@@ -961,9 +969,10 @@ bool TranslatorVulkan::translateImpl(TIntermBlock *root,
             }
         }
 
-        bool hasGLFragColor = false;
-        bool hasGLFragData  = false;
-        bool usePreRotation = compileOptions & SH_ADD_PRE_ROTATION;
+        bool hasGLFragColor  = false;
+        bool hasGLFragData   = false;
+        bool usePreRotation  = compileOptions & SH_ADD_PRE_ROTATION;
+        bool hasGLSampleMask = false;
 
         for (const ShaderVariable &outputVar : mOutputVariables)
         {
@@ -977,6 +986,12 @@ bool TranslatorVulkan::translateImpl(TIntermBlock *root,
             {
                 ASSERT(!hasGLFragData);
                 hasGLFragData = true;
+                continue;
+            }
+            else if (outputVar.name == "gl_SampleMask")
+            {
+                ASSERT(!hasGLSampleMask);
+                hasGLSampleMask = true;
                 continue;
             }
         }
@@ -1011,6 +1026,17 @@ bool TranslatorVulkan::translateImpl(TIntermBlock *root,
             {
                 return false;
             }
+        }
+
+        if (usesSampleMaskIn &&
+            !ReplaceSampleMaskInToANGLESampleMaskIn(this, root, &getSymbolTable()))
+        {
+            return false;
+        }
+
+        if (hasGLSampleMask && !ReplaceSampleMaskToANGLESampleMask(this, root, &getSymbolTable()))
+        {
+            return false;
         }
 
         {
