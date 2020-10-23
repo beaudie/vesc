ba6313fb023035355ec506ef35b678e291006430
Mohan Maiya <m.maiya@samsung.com>
2020-10-23 14:41:34 -0700

    Vulkan: Support gl_SampleMask with non-const index
    Add support for access to gl_SampleMask or gl_SampleMaskIn
    with non-constant index. Replace gl_SampleMask with
    ANGLESampleMask and reassign ANGLESampleMask
    to gl_SampleMask in the end.
    
    Bug: angleproject:3588
    Tests: dEQP-GLES31.functional.shaders.sample_variables.sample_mask.
               discard_half_per_pixel.*
               discard_half_per_sample.*
    Change-Id: Ie668c3ccf4d40352574db57f5e43b2f17ef6d8b8

---
+++ src/compiler/translator/tree_util/ReplaceSampleMaskVariables.h
@@ -0,0 +1,37 @@
+//
+// Copyright 2020 The ANGLE Project Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+// ReplaceSampleMaskVariable.cpp: Find any references to gl_SampleMask and gl_SampleMaskIn, and
+// replace it with ANGLESampleMask or ANGLESampleMaskIn.
+//
+
+#ifndef COMPILER_TRANSLATOR_TREEUTIL_REPLACESAMPLEMASKVARIABLE_H_
+#define COMPILER_TRANSLATOR_TREEUTIL_REPLACESAMPLEMASKVARIABLE_H_
+
+#include "common/angleutils.h"
+
+namespace sh
+{
+
+class TCompiler;
+class TIntermBlock;
+class TSymbolTable;
+class TIntermTyped;
+
+// Replace every gl_SampleMask and gl_SampleMaskIn assignment with assignment to "ANGLESampleMask"
+// and "ANGLESampleMaskIn", then at the end of shader re-assign the values of this global variable
+// to gl_SampleMask and gl_SampleMaskIn. This to solve the problem which the non constant index is
+// used for the unsized array problem.
+ANGLE_NO_DISCARD bool ReplaceSampleMaskToANGLESampleMask(TCompiler *compiler,
+                                                         TIntermBlock *root,
+                                                         TSymbolTable *symbolTable);
+
+ANGLE_NO_DISCARD bool ReplaceSampleMaskInToANGLESampleMaskIn(TCompiler *compiler,
+                                                             TIntermBlock *root,
+                                                             TSymbolTable *symbolTable);
+
+}  // namespace sh
+
+#endif  // COMPILER_TRANSLATOR_TREEUTIL_REPLACESAMPLEMASKVARIABLE_H_
