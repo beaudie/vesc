cdf7c14f5436c2b3c55991e55c7576d55d94e505
Jamie Madill <jmadill@chromium.org>
2020-10-26 17:43:11 -0400

    Capture/Replay: Always base replays on frame "1".
    This removes the unnecessary frame counting from application replays.
    Helps keep the traces more consistent for most use cases.
    
    Bug: angleproject:5134
    Change-Id: Icd8654d509423edcc74cd0ef694795a787f5cc05

--- src/libANGLE/FrameCapture.h
+++ src/libANGLE/FrameCapture.h
@@ -316,6 +316,7 @@ class FrameCapture final : angle::NonCopyable
 
     bool isCapturing() const;
     void replay(gl::Context *context);
+    uint32_t getFrameCount() const;
 
     void trackBufferMapping(CallCapture *call,
                             gl::BufferID id,
@@ -356,8 +357,8 @@ class FrameCapture final : angle::NonCopyable
     bool mCompression;
     gl::AttribArray<int> mClientVertexArrayMap;
     uint32_t mFrameIndex;
-    uint32_t mFrameStart;
-    uint32_t mFrameEnd;
+    uint32_t mCaptureStartFrame;
+    uint32_t mCaptureEndFrame;
     bool mIsFirstFrame        = true;
     bool mWroteIndexFile      = false;
     EGLint mDrawSurfaceWidth  = 0;
