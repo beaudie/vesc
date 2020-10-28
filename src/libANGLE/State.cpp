a4a330f16c2a1f7b117c6c2875172f3989f37736
Ian Elliott <ianelliott@google.com>
2020-10-28 10:14:21 -0600

    Vulkan: App-compat for apps that disable validation & don't link
    As seen on Android, when PUBG MOBILE rebuilds all of its shaders, it:
    
    - Disables validation
    - Tries to link two shaders that can't be linked
    - Calls glUseProgram() for the un-linked program
    - Never uses the program
    
    Because validation was disabled, the appropriate error isn't produced
    and ANGLE ASSERT's in the post-validation code.  For
    application-compatibility, this case is recognized and avoided.
    
    Bug: b/168839960
    Change-Id: I521785c741d57d6c228bb60c81abe638d95e8d2d

--- src/libANGLE/State.cpp
+++ src/libANGLE/State.cpp
@@ -1763,6 +1763,14 @@ void State::setVertexBindingDivisor(GLuint bindingIndex, GLuint divisor)
 
 angle::Result State::setProgram(const Context *context, Program *newProgram)
 {
+    if (newProgram && !newProgram->isLinked())
+    {
+        // Protect against applications that disable validation and try to use a program that was
+        // not successfully linked.  https://issuetracker.google.com/issues/168839960
+        WARN() << "Attempted to use a program that was not successfully linked";
+        return angle::Result::Stop;
+    }
+
     if (mProgram != newProgram)
     {
         if (mProgram)
