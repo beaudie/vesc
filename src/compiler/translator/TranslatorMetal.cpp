c5a306d7123a0b0c2808cb91b04618303b6232ed
Charlie Lao <cclao@google.com>
2020-11-02 12:25:32 -0800

    Vulkan: Add shader support to use rotation specialized constant
    If use rotation specialized constant is enabled via compiler options,
    this CL will use rotation specialized constant to generate flipXY,
    rotation matrix and negFliXY.  This allows driver to able to optimize
    out to minimum instructions for rotation.
    
    Bug: b/171750979
    Change-Id: I9851ac999d4d35b9f230f796e5445bca0dcb1e77

--- src/compiler/translator/TranslatorMetal.cpp
+++ src/compiler/translator/TranslatorMetal.cpp
@@ -151,7 +151,7 @@ bool TranslatorMetal::translate(TIntermBlock *root,
 
     if (getShaderType() == GL_VERTEX_SHADER)
     {
-        auto negFlipY = getDriverUniformNegFlipYRef(driverUniforms);
+        auto negFlipY = GenerateNegFlipY(nullptr, driverUniforms);
 
         // Append gl_Position.y correction to main
         if (!AppendVertexShaderPositionYCorrectionToMain(this, root, &getSymbolTable(), negFlipY))
