4482e535ead198d57e1eb64d03d01a2569ccb481
Geoff Lang <geofflang@chromium.org>
2020-11-03 13:33:44 -0500

    GL: Implement EXT_YUV_target
    Add test coverage of YUV format sampling as RGB or directly as YUV and
    rendering as YUV using layout(yuv).
    
    Initializing YUV AHardwareBuffers requires Android API 29 so ANGLE must
    be compiled with:
    android32_ndk_api_level = 29
    android64_ndk_api_level = 29
    
    The following tests can still run with Android API 26 because they don't
    need to initialize the buffer:
    ImageTestES3.ClearYUVAHB
    ImageTestES3.YUVValidation
    
    Bug: angleproject:4852
    Bug: b/172649538
    Change-Id: I4fe9afb2a68fb827dc5a5732b213b5eb60d585ac

--- src/libANGLE/Texture.cpp
+++ src/libANGLE/Texture.cpp
@@ -1868,6 +1868,17 @@ const ColorGeneric &Texture::getBorderColor() const
     return mState.mSamplerState.getBorderColor();
 }
 
+GLint Texture::getRequiredTextureImageUnits(const Context *context) const
+{
+    // Only external texture types can return non-1.
+    if (mState.mType != TextureType::External)
+    {
+        return 1;
+    }
+
+    return mTexture->getRequiredExternalTextureImageUnits(context);
+}
+
 void Texture::setCrop(const Rectangle &rect)
 {
     mState.setCrop(rect);
