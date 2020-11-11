f594faa550a98f4d1bd30d78821c93473eeb701d
Shahbaz Youssefi <syoussefi@chromium.org>
2020-11-11 15:26:39 -0500

    Fix texture buffer width query
    If glTexBuffer is used, the width should be calculated based on the
    buffer size.  Since ES doesn't have immutable buffers,
    EXT_texture_buffer allows mutable buffers to be attached to the texture.
    This means that updates to the buffer must update the image description.
    This is done by having the texture observe the attached buffer.
    
    Bug: angleproject:3573
    Bug: angleproject:5334
    Test: dEQP-GLES31.functional.state_query.texture_level.texture_buffer.width_*
    Change-Id: I7d9e5344a3d4ea70da7ba525f13b5876481a70a3

--- src/libANGLE/Texture.h
+++ src/libANGLE/Texture.h
@@ -637,6 +637,8 @@ class Texture final : public RefCountObject<TextureID>,
     DirtyBits mDirtyBits;
     rx::TextureImpl *mTexture;
     angle::ObserverBinding mImplObserver;
+    // For EXT_texture_buffer, observes buffer changes.
+    angle::ObserverBinding mBufferObserver;
 
     std::string mLabel;
 
