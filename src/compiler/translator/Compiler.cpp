9eb3391a19af147cfad0078296c664e2d5e763c9
Charlie Lao <cclao@google.com>
2020-11-12 18:37:26 -0800

    Vulkan: Experimental: forcing highp to mediump in fragment shader.
    Handy code for debug/experiment that forces highp in fragment shader to
    mediump.
    
    Bug: b/173140243
    Change-Id: I1841b67862343df8a12bf302066d7eefd7709604

--- src/compiler/translator/Compiler.cpp
+++ src/compiler/translator/Compiler.cpp
@@ -41,6 +41,7 @@
 #include "compiler/translator/tree_ops/RemovePow.h"
 #include "compiler/translator/tree_ops/RemoveUnreferencedVariables.h"
 #include "compiler/translator/tree_ops/RewriteDoWhile.h"
+#include "compiler/translator/tree_ops/RewritePrecisionQualifier.h"
 #include "compiler/translator/tree_ops/RewriteRepeatedAssignToSwizzled.h"
 #include "compiler/translator/tree_ops/ScalarizeVecAndMatConstructorArgs.h"
 #include "compiler/translator/tree_ops/SeparateDeclarations.h"
@@ -785,6 +786,15 @@ bool TCompiler::checkAndSimplifyAST(TIntermBlock *root,
     {
         if (!ScalarizeVecAndMatConstructorArgs(this, root, mShaderType, highPrecisionSupported,
                                                &mSymbolTable))
+        {
+            return false;
+        }
+    }
+
+    if (mShaderType == GL_FRAGMENT_SHADER &&
+        (compileOptions & SH_FORCE_FRAGMENT_SHADER_DROP_HIGHP_TO_MEDIUMP))
+    {
+        if (!RewriteFragmentShaderPrecisionQualifiers(root, &mSymbolTable))
         {
             return false;
         }
