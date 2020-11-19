0b79d0f174213211b2bbace8c626c39c9d66446a
Tim Van Patten <timvp@google.com>
2020-11-18 20:21:06 -0700

    Vulkan: Only newSharedFence() when using mCommandQueue
    The implementation of mCommandProcessor.submitFrame() doesn't use the
    'sharedFence' parameter, so the ownership of the passed in fence is not
    passed along to anything else. This means the vk::Shared<vk::Fence> is
    still referenced when it's dtor'ed when RendererVk::submitFrame()
    completes, causing an ASSERT().
    
    This CL moves the call to newSharedFence() into the conditional block
    that calls CommandQueue::submitFrame(), which uses the parameter. Note
    that CommandProcessor::submitFrame() is *not* being updated to remove
    the 'sharedFence' parameter, since we specifically want the APIs to the
    two functions to match.
    
    Bug: b/172704839
    Change-Id: I52b16be12fd0ee88fc4e45e83b5c42572e6ca9eb

--- src/libANGLE/renderer/vulkan/RendererVk.cpp
+++ src/libANGLE/renderer/vulkan/RendererVk.cpp
@@ -2464,8 +2464,6 @@ angle::Result RendererVk::submitFrame(vk::Context *context,
     std::lock_guard<std::mutex> commandQueueLock(mCommandQueueMutex);
 
     vk::Shared<vk::Fence> submitFence;
-    ANGLE_TRY(newSharedFence(context, &submitFence));
-
     Serial submitQueueSerial;
 
     if (mFeatures.asyncCommandQueue.enabled)
@@ -2478,6 +2476,7 @@ angle::Result RendererVk::submitFrame(vk::Context *context,
     }
     else
     {
+        ANGLE_TRY(newSharedFence(context, &submitFence));
         submitQueueSerial = mCommandQueue.reserveSubmitSerial();
 
         ANGLE_TRY(mCommandQueue.submitFrame(
