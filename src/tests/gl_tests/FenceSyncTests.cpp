28f80aa63404479f10aa6257ade01c071a4b9b15
Tim Van Patten <timvp@google.com>
2020-11-20 13:07:53 -0700

    Vulkan: Ignore glFlush to reduce vkQueueSubmits in Asphalt 9
    Multithreaded apps can use the following pattern:
    
      glDrawElements()
      glFenceSync()
      glFlush()
      glWaitSync()
    
    This currently results in a vkQueueSubmit for every glFlush() to ensure
    that the work has landed in the command queue in the correct order.
    However, ANGLE can instead avoid the vkQueueSubmit during the glFlush()
    in this situation by instead flushing the ContextVk's commands and
    ending the render pass to ensure the commands are submitted in the
    correct order to the renderer. This improves performance for Asphalt 9
    by reducing frame times from 150-200msec to 35-55msec.
    
    Specifically, ANGLE will call flushCommandsAndEndRenderPass() when
    there is a sync object pending a flush or if the ContextVk is currently
    shared.
    
    Additionally, on all devices except Qualcomm, ANGLE can ignore all other
    glFlush() calls entirely and return immediately. For Qualcomm devices,
    ANGLE is still required to perform a full flush (resulting in a
    vkQueueSubmit), since ignoring the glFlush() reduces the Manhattan 3.0
    offscreen score by ~3%.
    
    Bug: angleproject:5306
    Change-Id: I9d747caf5bf306166be0fec630a78caf41208c27

--- src/tests/gl_tests/FenceSyncTests.cpp
+++ src/tests/gl_tests/FenceSyncTests.cpp
@@ -238,29 +238,23 @@ TEST_P(FenceSyncTest, BasicOperations)
     glWaitSync(sync, 0, GL_TIMEOUT_IGNORED);
     EXPECT_GL_NO_ERROR();
 
-    GLsizei length         = 0;
     GLint value            = 0;
     unsigned int loopCount = 0;
 
     glFlush();
 
     // Use 'loopCount' to make sure the test doesn't get stuck in an infinite loop
-    while (value != GL_SIGNALED && loopCount <= 1000000)
+    constexpr GLuint64 kTimeout = 1000000;  // 1msec
+    while (value != GL_CONDITION_SATISFIED && value != GL_ALREADY_SIGNALED && ++loopCount <= 100)
     {
-        loopCount++;
-
-        glGetSynciv(sync, GL_SYNC_STATUS, 1, &length, &value);
-        ASSERT_GL_NO_ERROR();
+        value = glClientWaitSync(sync, GL_SYNC_FLUSH_COMMANDS_BIT, kTimeout);
     }
-
-    ASSERT_GLENUM_EQ(GL_SIGNALED, value);
-
-    for (size_t i = 0; i < 20; i++)
-    {
-        glClear(GL_COLOR_BUFFER_BIT);
-        glClientWaitSync(sync, GL_SYNC_FLUSH_COMMANDS_BIT, GL_TIMEOUT_IGNORED);
-        EXPECT_GL_NO_ERROR();
-    }
+    EXPECT_GL_NO_ERROR();
+#if 0  // TIMTIM
+    ASSERT_TRUE(value == GL_CONDITION_SATISFIED || value == GL_ALREADY_SIGNALED);
+#else
+    EXPECT_EQ(GL_ALREADY_SIGNALED, value);
+#endif
 }
 
 // Test that multiple fences and draws can be issued
