6fa8597110f572f62abfc9cf1146c9c6d56a7c95
Charlie Lao <cclao@google.com>
2020-11-19 16:47:25 -0800

    Vulkan: Use specialized constant for halfRenderAreaWidth
    halfRenderArea is used by fragment shader to adjust gl_FragCoord for
    rotation and yflip compensation. This CL bakes halfRenderAreaWidth into
    shader via specialization constant, thus allow compiler to consolidate
    the calculation into one MAD instruction.
    
    Bug: b/173800146
    Change-Id: Id66301278e3389e2582369b695825e632bccecee

--- src/libANGLE/renderer/vulkan/ContextVk.cpp
+++ src/libANGLE/renderer/vulkan/ContextVk.cpp
@@ -1085,8 +1085,8 @@ angle::Result ContextVk::handleDirtyGraphicsPipeline(const gl::Context *context,
     {
         const vk::GraphicsPipelineDesc *descPtr;
 
-        // The desc's surface rotation specialization constant depends on both program's
-        // specConstUsageBits and drawable. We need to update it if program has changed.
+        // The desc's specialization constant depends on program's
+        // specConstUsageBits. We need to update it if program has changed.
         SpecConstUsageBits usageBits = getCurrentProgramSpecConstUsageBits();
         updateGraphicsPipelineDescWithSpecConstUsageBits(usageBits);
 
@@ -3174,6 +3174,19 @@ void ContextVk::updateGraphicsPipelineDescWithSpecConstUsageBits(SpecConstUsageB
         // program object will be retrieved.
         mGraphicsPipelineDesc->updateSurfaceRotation(&mGraphicsPipelineTransition, rotationAndFlip);
     }
+
+    // Always set specialization constant to 1x1 if it is not used so that pipeline program with
+    // only drawable size difference will be able to be reused.
+    if (!usageBits.test(sh::vk::SpecConstUsage::DrawableSize))
+    {
+        mGraphicsPipelineDesc->updateDrawableSize(&mGraphicsPipelineTransition, 1, 1);
+    }
+    else
+    {
+        mGraphicsPipelineDesc->updateDrawableSize(
+            &mGraphicsPipelineTransition, mDrawFramebuffer->getState().getDimensions().width,
+            mDrawFramebuffer->getState().getDimensions().height);
+    }
 }
 
 void ContextVk::updateSurfaceRotationDrawFramebuffer(const gl::State &glState)
@@ -3384,6 +3397,9 @@ void ContextVk::onDrawFramebufferRenderPassDescChange(FramebufferVk *framebuffer
     invalidateCurrentGraphicsPipeline();
     mGraphicsPipelineDesc->updateRenderPassDesc(&mGraphicsPipelineTransition,
                                                 framebufferVk->getRenderPassDesc());
+    mGraphicsPipelineDesc->updateDrawableSize(&mGraphicsPipelineTransition,
+                                              framebufferVk->getState().getDimensions().width,
+                                              framebufferVk->getState().getDimensions().height);
 }
 
 void ContextVk::invalidateCurrentTransformFeedbackBuffers()
