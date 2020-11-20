4b080606c0102cc48fd966c6b862b2da4321c88e
Mohan Maiya <m.maiya@samsung.com>
2020-11-19 17:13:59 -0800

    Vulkan: Support basic GL_EXT_tessellation_shader
    Shader translattor can recognize inputs of tessellation shaders.
    DIRTY_BIT_PATCH_VERTICES is added to support patchVertices.
    Implicit sized array supported for tessellation shader.
    Supported state query and transform feedback.
    Not enabled tessellation until it is completed.
    
    Test: dEQP-GLES31.functional.tessellation.*
    Bug: angleproject:3572
    Change-Id: Idcc5dcf30b284950d9cd548edf83af596e572ec4

--- src/compiler/translator/TranslatorVulkan.cpp
+++ src/compiler/translator/TranslatorVulkan.cpp
@@ -172,6 +172,8 @@ constexpr gl::ShaderMap<const char *> kDefaultUniformNames = {
     {gl::ShaderType::Geometry, vk::kDefaultUniformsNameGS},
     {gl::ShaderType::Fragment, vk::kDefaultUniformsNameFS},
     {gl::ShaderType::Compute, vk::kDefaultUniformsNameCS},
+    {gl::ShaderType::TessControl, vk::kDefaultUniformsNameTCS},
+    {gl::ShaderType::TessEvaluation, vk::kDefaultUniformsNameTES},
 };
 
 // Specialization constant names
@@ -993,12 +995,12 @@ bool TranslatorVulkan::translateImpl(TIntermBlock *root,
         {
             return false;
         }
-
         // Append depth range translation to main.
         if (!transformDepthBeforeCorrection(root, driverUniforms))
         {
             return false;
         }
+
         if (!AppendVertexShaderDepthCorrectionToMain(this, root, &getSymbolTable()))
         {
             return false;
@@ -1015,6 +1017,17 @@ bool TranslatorVulkan::translateImpl(TIntermBlock *root,
         WriteGeometryShaderLayoutQualifiers(
             sink, getGeometryShaderInputPrimitiveType(), getGeometryShaderInvocations(),
             getGeometryShaderOutputPrimitiveType(), getGeometryShaderMaxVertices());
+    }
+    else if (getShaderType() == GL_TESS_CONTROL_SHADER_EXT)
+    {
+        WriteTessControlShaderLayoutQualifiers(sink, getTessControlShaderOutputVertices());
+    }
+    else if (getShaderType() == GL_TESS_EVALUATION_SHADER_EXT)
+    {
+        WriteTessEvaluationShaderLayoutQualifiers(sink, getTessEvaluationShaderInputPrimitiveType(),
+                                                  getTessEvaluationShaderInputVertexSpacingType(),
+                                                  getTessEvaluationShaderInputOrderingType(),
+                                                  getTessEvaluationShaderInputPointType());
     }
     else
     {
