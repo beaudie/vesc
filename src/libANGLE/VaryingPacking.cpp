66ca2129406997d75e55541c6614065a3286a157
Jamie Madill <jmadill@chromium.org>
2020-11-27 11:08:41 -0800

    Vulkan: Support basic GL_EXT_tessellation_shader
    Shader translator can recognize inputs of tessellation shaders.
    DIRTY_BIT_PATCH_VERTICES is added to support patchVertices.
    Supported state query and transform feedback.
    Not enabled tessellation until it is completed.
    
    Based on work by Mohan Maiya (m.maiya@samsung.com).
    
    Test: dEQP-GLES31.functional.tessellation.*
    Bug: angleproject:3572
    Change-Id: I4cad2cca30adb754fd12c83027673906541f566a

--- src/libANGLE/VaryingPacking.cpp
+++ src/libANGLE/VaryingPacking.cpp
@@ -88,6 +88,25 @@ bool ComparePackedVarying(const PackedVarying &x, const PackedVarying &y)
     return gl::CompareShaderVar(*px, *py);
 }
 
+bool ShouldSkipPackedVarying(const sh::ShaderVariable &varying, PackMode packMode)
+{
+    // Don't pack gl_Position. Also don't count gl_PointSize for D3D9.
+    return varying.name == "gl_Position" ||
+           (varying.name == "gl_PointSize" && packMode == PackMode::ANGLE_NON_CONFORMANT_D3D9);
+}
+
+bool ShouldStripVaryingArrayDimension(ShaderType shaderType)
+{
+    switch (shaderType)
+    {
+        case ShaderType::Geometry:
+        case ShaderType::TessControl:
+        case ShaderType::TessEvaluation:
+            return true;
+        default:
+            return false;
+    }
+}
 }  // anonymous namespace
 
 // Implementation of VaryingInShaderRef
@@ -166,6 +185,35 @@ PackedVarying &PackedVarying::operator=(PackedVarying &&other)
     return *this;
 }
 
+unsigned int PackedVarying::getBasicTypeElementCount() const
+{
+    // "Geometry shader inputs, tessellation control shader inputs and outputs, and tessellation
+    // evaluation inputs all have an additional level of arrayness relative to other shader inputs
+    // and outputs. This outer array level is removed from the type before considering how many
+    // locations the type consumes."
+
+    if (backVarying.varying &&
+        (backVarying.stage == ShaderType::Geometry ||
+         backVarying.stage == ShaderType::TessEvaluation ||
+         backVarying.stage == ShaderType::TessControl) &&
+        backVarying.varying->isArray())
+    {
+        std::vector<unsigned int> arraySizes = backVarying.varying->arraySizes;
+        arraySizes.pop_back();
+        return arraySizes.empty() ? 1 : arraySizes.back();
+    }
+
+    if (frontVarying.varying && frontVarying.stage == ShaderType::TessControl &&
+        frontVarying.varying->isArray())
+    {
+        std::vector<unsigned int> arraySizes = frontVarying.varying->arraySizes;
+        arraySizes.pop_back();
+        return arraySizes.empty() ? 1 : arraySizes.back();
+    }
+
+    return varying().getBasicTypeElementCount();
+}
+
 // Implementation of VaryingPacking
 VaryingPacking::VaryingPacking() = default;
 
@@ -233,7 +281,7 @@ bool VaryingPacking::packVaryingIntoRegisterMap(const PackedVarying &packedVaryi
     // "Arrays of size N are assumed to take N times the size of the base type"
     // GLSL ES 3.10 section 4.3.6: Output variables cannot be arrays of arrays or arrays of
     // structures, so we may use getBasicTypeElementCount().
-    const unsigned int elementCount = varying.getBasicTypeElementCount();
+    const unsigned int elementCount = packedVarying.getBasicTypeElementCount();
     varyingRows *= (packedVarying.isTransformFeedbackArrayElement() ? 1 : elementCount);
 
     unsigned int maxVaryingVectors = static_cast<unsigned int>(mRegisterMap.size());
@@ -389,7 +437,7 @@ void VaryingPacking::insertVaryingIntoRegisterMap(unsigned int registerRow,
 
     // GLSL ES 3.10 section 4.3.6: Output variables cannot be arrays of arrays or arrays of
     // structures, so we may use getBasicTypeElementCount().
-    const unsigned int arrayElementCount = varying.getBasicTypeElementCount();
+    const unsigned int arrayElementCount = packedVarying.getBasicTypeElementCount();
     for (unsigned int arrayElement = 0; arrayElement < arrayElementCount; ++arrayElement)
     {
         if (packedVarying.isTransformFeedbackArrayElement() &&
@@ -569,11 +617,10 @@ void VaryingPacking::collectUserVarying(const sh::ShaderVariable &varying,
         const std::vector<unsigned int> &arraySizes = varying.arraySizes;
         size_t arrayDimensions                      = arraySizes.size();
 
-        // Geometry shader inputs have an extra level of array-ness that should be
-        // removed.
-        if (&varying == output && ref.backShaderStage == ShaderType::Geometry)
+        // Some shader inputs have an extra level of array-ness to remove.
+        if (&varying == output && ShouldStripVaryingArrayDimension(ref.backShaderStage) &&
+            arrayDimensions > 0)
         {
-            ASSERT(arrayDimensions > 0);
             --arrayDimensions;
         }
 
@@ -586,11 +633,17 @@ void VaryingPacking::collectUserVarying(const sh::ShaderVariable &varying,
             const GLuint effectiveArrayIndex = isArray ? arrayIndex : GL_INVALID_INDEX;
             for (GLuint fieldIndex = 0; fieldIndex < varying.fields.size(); ++fieldIndex)
             {
-                if (varying.fields[fieldIndex].isStruct())
+                const sh::ShaderVariable &fieldVarying = varying.fields[fieldIndex];
+                if (ShouldSkipPackedVarying(fieldVarying, mPackMode))
+                {
+                    continue;
+                }
+
+                if (fieldVarying.isStruct())
                 {
 
-                    for (GLuint nestedIndex = 0;
-                         nestedIndex < varying.fields[fieldIndex].fields.size(); nestedIndex++)
+                    for (GLuint nestedIndex = 0; nestedIndex < fieldVarying.fields.size();
+                         nestedIndex++)
                     {
                         collectUserVaryingField(ref, effectiveArrayIndex, fieldIndex, nestedIndex,
                                                 uniqueFullNames);
@@ -735,10 +788,7 @@ bool VaryingPacking::collectAndPackUserVaryings(gl::InfoLog &infoLog,
         {
             const sh::ShaderVariable *varying = output ? output : input;
 
-            // Don't count gl_Position. Also don't count gl_PointSize for D3D9.
-            if (varying->name != "gl_Position" &&
-                !(varying->name == "gl_PointSize" &&
-                  mPackMode == PackMode::ANGLE_NON_CONFORMANT_D3D9))
+            if (!ShouldSkipPackedVarying(*varying, mPackMode))
             {
                 collectUserVarying(*varying, ref, &uniqueFullNames);
                 continue;
