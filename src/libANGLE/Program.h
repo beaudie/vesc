9927edb7986e403f798e46a90c2c35e71058f962
Jamie Madill <jmadill@chromium.org>
2020-11-27 11:08:41 -0800

    Vulkan: Support basic GL_EXT_tessellation_shader
    Shader translator can recognize inputs of tessellation shaders.
    DIRTY_BIT_PATCH_VERTICES is added to support patchVertices.
    Supported state query and transform feedback.
    Not enabled tessellation until it is completed.
    
    Based on work by Mohan Maiya (m.maiya@samsung.com).
    
    Test: dEQP-GLES31.functional.tessellation.*
    Bug: angleproject:3572
    Change-Id: I4cad2cca30adb754fd12c83027673906541f566a

--- src/libANGLE/Program.h
+++ src/libANGLE/Program.h
@@ -430,6 +430,13 @@ class ProgramState final : angle::NonCopyable
     int mGeometryShaderInvocations;
     int mGeometryShaderMaxVertices;
 
+    // GL_EXT_tessellation_shader
+    int mTessControlShaderVertices;
+    GLenum mTessGenMode;
+    GLenum mTessGenSpacing;
+    GLenum mTessGenVertexOrder;
+    GLenum mTessGenPointMode;
+
     // GL_ANGLE_multi_draw
     int mDrawIDLocation;
 
@@ -758,6 +765,12 @@ class Program final : public LabeledObject, public angle::Subject
     GLint getGeometryShaderInvocations() const;
     GLint getGeometryShaderMaxVertices() const;
 
+    GLint getTessControlShaderVertices() const;
+    GLenum getTessGenMode() const;
+    GLenum getTessGenPointMode() const;
+    GLenum getTessGenSpacing() const;
+    GLenum getTessGenVertexOrder() const;
+
     const ProgramState &getState() const
     {
         ASSERT(!mLinkingState);
@@ -858,6 +871,7 @@ class Program final : public LabeledObject, public angle::Subject
                                        const sh::ShaderVariable &input,
                                        const sh::ShaderVariable &output,
                                        bool validateGeometryShaderInputs,
+                                       bool validateTessellationShaderInputs,
                                        bool isSeparable,
                                        gl::InfoLog &infoLog);
     static bool linkValidateShaderInterfaceMatching(
@@ -913,6 +927,7 @@ class Program final : public LabeledObject, public angle::Subject
                                                   const sh::ShaderVariable &inputVarying,
                                                   int shaderVersion,
                                                   bool validateGeometryShaderInputVarying,
+                                                  bool validateTessellationShaderInputs,
                                                   bool isSeparable,
                                                   std::string *mismatchedStructFieldName);
 
