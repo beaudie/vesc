85cc71b1c90d273e613d268378bde5e2a92a1878
Jamie Madill <jmadill@chromium.org>
2020-11-27 11:08:41 -0800

    Vulkan: Support basic GL_EXT_tessellation_shader
    Shader translator can recognize inputs of tessellation shaders.
    DIRTY_BIT_PATCH_VERTICES is added to support patchVertices.
    Supported state query and transform feedback.
    Not enabled tessellation until it is completed.
    
    Based on work by Mohan Maiya (m.maiya@samsung.com).
    
    Test: dEQP-GLES31.functional.tessellation.*
    Bug: angleproject:3572
    Change-Id: I4cad2cca30adb754fd12c83027673906541f566a

--- src/libANGLE/renderer/vulkan/RendererVk.cpp
+++ src/libANGLE/renderer/vulkan/RendererVk.cpp
@@ -1387,6 +1387,8 @@ angle::Result RendererVk::initializeDevice(DisplayVk *displayVk, uint32_t queueF
     enabledFeatures.features.sampleRateShading = mPhysicalDeviceFeatures.sampleRateShading;
     // Used to support depth clears through draw calls.
     enabledFeatures.features.depthClamp = mPhysicalDeviceFeatures.depthClamp;
+    // Used to support tessellation Shader:
+    enabledFeatures.features.tessellationShader = mPhysicalDeviceFeatures.tessellationShader;
 
     if (!vk::CommandBuffer::ExecutesInline())
     {
