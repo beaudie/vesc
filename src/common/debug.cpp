05313d2faa015220c9c1d25fe606885da23b54f8
Ian Elliott <ianelliott@google.com>
2020-11-30 15:18:32 -0700

    Vulkan: Use VK_EXT_device_memory_report extension
    This CL can enable and use the VK_EXT_device_memory_report extension.
    
    This is disabled by default.  To enable, link with the required
    extension, enable one or both of two features, AND build with the
    following GN arg:
    
      angle_enable_trace = true
    
    The two added  features are:
    
    - logMemoryReportStats provides per-swap statistics
    - logMemoryReportCallbacks provides per-callback logging
    
    If either or both of the features are enabled, if the
    VK_EXT_device_memory_report extension is available, it is enabled.
    
    Bug: b/173636655
    Change-Id: Ic5cf6c06efdb34f2313ef143853b3cc90f55faa5

--- src/common/debug.cpp
+++ src/common/debug.cpp
@@ -163,8 +163,8 @@ void ScopedPerfEventHelper::begin(const char *format, ...)
 LogMessage::LogMessage(const char *file, const char *function, int line, LogSeverity severity)
     : mFile(file), mFunction(function), mLine(line), mSeverity(severity)
 {
-    // EVENT() does not require additional function(line) info.
-    if (mSeverity != LOG_EVENT)
+    // INFO() and EVENT() do not require additional function(line) info.
+    if (mSeverity > LOG_INFO)
     {
         const char *slash = std::max(strrchr(mFile, '/'), strrchr(mFile, '\\'));
         mStream << (slash ? (slash + 1) : mFile) << ":" << mLine << " (" << mFunction << "): ";
