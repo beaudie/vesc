332635906e545a1a162306044ca16c3058d341f9
Shahbaz Youssefi <syoussefi@chromium.org>
2020-12-02 13:11:54 -0500

    Vulkan: Support PrimitivesGenerated query
    This query uses the Vulkan transform feedback extension.  In GL,
    GL_TRANSFORM_FEEDBACK_PRIMITIVES_WRITTEN and GL_PRIMITIVES_GENERATED
    queries can be independently begun/ended.  However, Vulkan requires that
    queries from pools of the same type can only be active one at a time.
    This forbids the two GL queries from being handled by two VK queries
    when they are simultaneously begun.
    
    This change makes these queries share their QueryHelper objects.  The
    Vulkan transform feedback queries unconditionally retrieve both results
    anyway, so this is just a matter of making sure the two GL queries are
    merged as one when they are simultaneously used.
    
    The change fixes a number of issues as collateral:
    
    - TransformFeedbackPrimitivesWritten queries when !emulated were not
      released
    - Stashed queries were never released
    - If no render pass is open when a query ends, then getResult(no_wait)
      ended up waiting
    
    Bug: angleproject:5404
    Change-Id: I8ce13ea76ffd31b3152ded7c713c6466d0315504

--- src/libANGLE/renderer/vulkan/vk_helpers.h
+++ src/libANGLE/renderer/vulkan/vk_helpers.h
@@ -421,6 +421,11 @@ class QueryResult final
 
     size_t getDataSize() const { return mIntsPerResult * sizeof(uint64_t); }
     uint64_t getResult() const { return mResults[0]; }
+    uint64_t getSecondaryResult() const
+    {
+        ASSERT(mIntsPerResult > 1);
+        return mResults[1];
+    }
     uint64_t *getPointerToResults() { return mResults.data(); }
 
   private:
@@ -464,6 +469,9 @@ class QueryHelper final : public Resource
     // All other timestamp accesses should be made on outsideRenderPassCommandBuffer
     void writeTimestamp(ContextVk *contextVk, CommandBuffer *outsideRenderPassCommandBuffer);
 
+    // Whether this query helper has generated any commands.
+    bool hasCommands() const;
+
     angle::Result getUint64ResultNonBlocking(ContextVk *contextVk,
                                              QueryResult *resultOut,
                                              bool *availableOut);
@@ -488,6 +496,8 @@ class QueryHelper final : public Resource
     size_t mQueryPoolIndex;
     uint32_t mQuery;
 };
+
+using RefCountedQueryHelper = RefCounted<QueryHelper>;
 
 // DynamicSemaphorePool allocates semaphores as needed.  It uses a std::vector
 // as a pool to allocate many semaphores at once.  The pools live permanently,
