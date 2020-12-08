d8f1213445c8fdbf942b15cae255edd38c5db8aa
Brandon Schade <b.schade@samsung.com>
2020-12-07 18:14:50 -0800

    Vulkan: Add support for EXT_blend_func_extended
    This implementation utilizes vulkan's dualSrcBlend
    feature. Expose this extension if the underlying vulkan
    backend allows the use of this feature.
    
    Test: angle_end2end_tests --gtest_filter=EXTBlendFuncExtendedDrawTest*
    Bug: angleproject:5074
    Change-Id: I7d2f611df89d65e5cac35158cb5f41a0ebd58aae

--- src/libANGLE/renderer/vulkan/RendererVk.cpp
+++ src/libANGLE/renderer/vulkan/RendererVk.cpp
@@ -1534,6 +1534,8 @@ angle::Result RendererVk::initializeDevice(DisplayVk *displayVk, uint32_t queueF
     enabledFeatures.features.shaderCullDistance = mPhysicalDeviceFeatures.shaderCullDistance;
     // Used to support tessellation Shader:
     enabledFeatures.features.tessellationShader = mPhysicalDeviceFeatures.tessellationShader;
+    // Used to support EXT_blend_func_extended
+    enabledFeatures.features.dualSrcBlend = mPhysicalDeviceFeatures.dualSrcBlend;
 
     if (!vk::CommandBuffer::ExecutesInline())
     {
