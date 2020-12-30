dcfff7374c2adbab9cda0b2d8458c8ba25762338
Jamie Madill <jmadill@chromium.org>
2020-12-30 17:17:17 -0500

    Simplify ProgramPipeline::getMergedVaryings().
    Varyings cannot have mismatched locations or names. Knowing this
    allows us to simplify this function considerably since we must
    match on both name and location.
    
    Bug: angleproject:5496
    Change-Id: Ibf54faeeb01d1940570b366ed153fff7c9135c52

--- src/libANGLE/Program.h
+++ src/libANGLE/Program.h
@@ -470,7 +470,16 @@ struct ProgramVaryingRef
 
 using ProgramMergedVaryings = std::vector<ProgramVaryingRef>;
 
-class Program final : public LabeledObject, public angle::Subject
+class HasAttachedShaders
+{
+  public:
+    virtual Shader *getAttachedShader(ShaderType shaderType) const = 0;
+
+  protected:
+    virtual ~HasAttachedShaders() {}
+};
+
+class Program final : public LabeledObject, public angle::Subject, public HasAttachedShaders
 {
   public:
     Program(rx::GLImplFactory *factory, ShaderProgramManager *manager, ShaderProgramID handle);
@@ -491,7 +500,7 @@ class Program final : public LabeledObject, public angle::Subject
     void detachShader(const Context *context, Shader *shader);
     int getAttachedShadersCount() const;
 
-    const Shader *getAttachedShader(ShaderType shaderType) const;
+    Shader *getAttachedShader(ShaderType shaderType) const override;
 
     void bindAttributeLocation(GLuint index, const char *name);
     void bindUniformLocation(UniformLocation location, const char *name);
@@ -928,7 +937,6 @@ class Program final : public LabeledObject, public angle::Subject
 
     void gatherTransformFeedbackVaryings(const ProgramMergedVaryings &varyings, ShaderType stage);
 
-    ProgramMergedVaryings getMergedVaryings() const;
     int getOutputLocationForLink(const sh::ShaderVariable &outputVariable) const;
     bool isOutputSecondaryForLink(const sh::ShaderVariable &outputVariable) const;
     bool linkOutputVariables(const Caps &caps,
