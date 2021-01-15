911332e5cb735965fd82bb89dec88fdcdbf50e8e
Amy Liu <amy.liu@arm.com>
2021-01-15 10:29:36 +0800

    Compression of the data from vkGetPipelineCacheData.
    The size of pipelineCacheData sometimes is greater than
    64k which cannot be saved because of the Android blob cache
    limitation (single cache data size should be < 64k).
    Implement the compression to store more cache data.
    
    Bug: angleproject:4722
    Change-Id: I435b086d70d0e6378f1141464ae2bafbe076f193

--- src/libANGLE/BlobCache.h
+++ src/libANGLE/BlobCache.h
@@ -140,6 +140,14 @@ class BlobCache final : angle::NonCopyable
 
     bool isCachingEnabled() const { return areBlobCacheFuncsSet() || maxSize() > 0; }
 
+    // In oder to store more cache in blob cache, compress cacheData to compressedData
+    // before being stored.
+    bool compressCache(angle::MemoryBuffer *cacheData, angle::MemoryBuffer *compressedData);
+
+    bool decompressCache(const uint8_t *compressedData,
+                         const size_t compressedSize,
+                         angle::MemoryBuffer *uncompressedData);
+
   private:
     // This internal cache is used only if the application is not providing caching callbacks
     using CacheEntry = std::pair<angle::MemoryBuffer, CacheSource>;
