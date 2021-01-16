2639192e85275723edbfc538658eeead17e62103
Cody Northrop <cnorthrop@google.com>
2021-01-15 17:42:39 -0700

    Tests: Add Aztec Ruins trace
    Test: angle_perftests --gtest_filter="*aztec_ruins*"
    Bug: b/160808198
    Bug: angleproject:5553
    Change-Id: I24e3ae44698c5347fa5b9412d6a3ab16fb4f5c08

--- src/tests/perf_tests/TracePerfTest.cpp
+++ src/tests/perf_tests/TracePerfTest.cpp
@@ -307,6 +307,11 @@ TracePerfTest::TracePerfTest()
         addExtensionPrerequisite("GL_KHR_texture_compression_astc_ldr");
     }
 
+    if (param.testID == RestrictedTraceID::aztec_ruins)
+    {
+        addExtensionPrerequisite("GL_KHR_texture_compression_astc_ldr");
+    }
+
     // We already swap in TracePerfTest::drawBenchmark, no need to swap again in the harness.
     disableTestHarnessSwap();
 
