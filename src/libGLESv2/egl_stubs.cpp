b858f281e1ea77ebdeb0887c51f7c7e86f268f4c
Doug Horn <doughorn@google.com>
2021-01-24 17:46:26 -0800

    Fix crash in multi-context D3D11 backend.
    The crash occured due to a lack of marking a context dirty on
    eglMakeCurrent(), resulting in a situation where a thread B would
    call eglMakeCurrent() but before issuing additional GL calls,
    thread A would issue a draw command. Prior to this change, context
    state would only be marked dirty on gl calls.
    
    Bug: b/177602915
    Test: dEQP-EGL.functional.sharing.gles2.multithread.*
    dEQP-EGL.functional.multithread.*
    dEQP-EGL.functional.render.multi_thread.*
    dEQP-EGL.functional.color_clears.multi_thread.*
    
    Change-Id: I765e0423002a373f94ea459b81b6e6f6942870d2

--- src/libGLESv2/egl_stubs.cpp
+++ src/libGLESv2/egl_stubs.cpp
@@ -277,7 +277,15 @@ EGLBoolean DestroyContext(Thread *thread, Display *display, gl::Context *context
 {
     ANGLE_EGL_TRY_RETURN(thread, display->prepareForCall(), "eglDestroyContext",
                          GetDisplayIfValid(display), EGL_FALSE);
-    bool contextWasCurrent = context == thread->getContext();
+    gl::Context *contextForThread = thread->getContext();
+    bool contextWasCurrent        = context == contextForThread;
+
+    // Display can't access the current global context, but does exhibit a context switch,
+    // so ensuring the current context is correct needs to happen here.
+    if (!contextWasCurrent)
+    {
+        SetContextCurrent(thread, context);
+    }
 
     ANGLE_EGL_TRY_RETURN(thread, display->destroyContext(thread, context), "eglDestroyContext",
                          GetContextIfValid(display, context), EGL_FALSE);
@@ -287,6 +295,10 @@ EGLBoolean DestroyContext(Thread *thread, Display *display, gl::Context *context
         ANGLE_EGL_TRY_RETURN(thread, display->makeCurrent(context, nullptr, nullptr, nullptr),
                              "eglDestroyContext", GetContextIfValid(display, context), EGL_FALSE);
         SetContextCurrent(thread, nullptr);
+    }
+    else
+    {
+        SetContextCurrent(thread, contextForThread);
     }
 
     thread->setSuccess();
@@ -481,7 +493,6 @@ EGLBoolean MakeCurrent(Thread *thread,
         ANGLE_EGL_TRY_RETURN(
             thread, display->makeCurrent(previousContext, drawSurface, readSurface, context),
             "eglMakeCurrent", GetContextIfValid(display, context), EGL_FALSE);
-
         SetContextCurrent(thread, context);
     }
 
