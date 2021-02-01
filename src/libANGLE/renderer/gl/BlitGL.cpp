0e98904fac6ff3168717577e32061f73aa1ef1ac
Geoff Lang <geofflang@chromium.org>
2021-02-01 13:21:16 -0500

    GL: Support VAOs without native VAOs.
    Share the default VAO state between all frontend VAO when
    there is no native VAO support. Forcefully sync state every
    time a new frontend VAO is bound.
    
    Bug: angleproject:5577, chromium:1167179
    Change-Id: Ieaedb5108ad28fc78e7e58b74495639c5246bb05

--- src/libANGLE/renderer/gl/BlitGL.cpp
+++ src/libANGLE/renderer/gl/BlitGL.cpp
@@ -365,7 +365,7 @@ angle::Result BlitGL::copySubImageToLUMAWorkaroundTexture(const gl::Context *con
     ANGLE_GL_TRY(context, mFunctions->uniform1i(blitProgram->multiplyAlphaLocation, 0));
     ANGLE_GL_TRY(context, mFunctions->uniform1i(blitProgram->unMultiplyAlphaLocation, 0));
 
-    mStateManager->bindVertexArray(mVAO, 0);
+    ANGLE_TRY(setVAOState(context));
     ANGLE_GL_TRY(context, mFunctions->drawArrays(GL_TRIANGLES, 0, 3));
 
     // Copy the swizzled texture to the destination texture
@@ -548,7 +548,7 @@ angle::Result BlitGL::blitColorBufferWithShader(const gl::Context *context,
 
     mStateManager->bindFramebuffer(GL_DRAW_FRAMEBUFFER, destFramebuffer);
 
-    mStateManager->bindVertexArray(mVAO, 0);
+    ANGLE_TRY(setVAOState(context));
     ANGLE_GL_TRY(context, mFunctions->drawArrays(GL_TRIANGLES, 0, 3));
 
     ANGLE_TRY(scopedState.exit(context));
@@ -663,7 +663,7 @@ angle::Result BlitGL::copySubTexture(const gl::Context *context,
                                                     unpackUnmultiplyAlpha));
     }
 
-    mStateManager->bindVertexArray(mVAO, 0);
+    ANGLE_TRY(setVAOState(context));
     ANGLE_GL_TRY(context, mFunctions->drawArrays(GL_TRIANGLES, 0, 3));
 
     *copySucceededOut = true;
@@ -1075,7 +1075,7 @@ angle::Result BlitGL::generateSRGBMipmap(const gl::Context *context,
     mStateManager->bindTexture(sourceType, source->getTextureID());
     ANGLE_TRY(source->setMinFilter(context, GL_NEAREST));
 
-    mStateManager->bindVertexArray(mVAO, 0);
+    ANGLE_TRY(setVAOState(context));
     ANGLE_GL_TRY(context, mFunctions->drawArrays(GL_TRIANGLES, 0, 3));
 
     // Generate mipmaps on the linear texture
@@ -1133,21 +1133,10 @@ angle::Result BlitGL::initializeResources(const gl::Context *context)
     ANGLE_GL_TRY(context, mFunctions->bufferData(GL_ARRAY_BUFFER, sizeof(float) * 6, vertexData,
                                                  GL_STATIC_DRAW));
 
-    ANGLE_GL_TRY(context, mFunctions->genVertexArrays(1, &mVAO));
-
-    mStateManager->bindVertexArray(mVAO, 0);
-    mStateManager->bindBuffer(gl::BufferBinding::Array, mVertexBuffer);
-
-    // Enable all attributes with the same buffer so that it doesn't matter what location the
-    // texcoord attribute is assigned
-    GLint maxAttributes = 0;
-    ANGLE_GL_TRY(context, mFunctions->getIntegerv(GL_MAX_VERTEX_ATTRIBS, &maxAttributes));
-
-    for (GLint i = 0; i < maxAttributes; i++)
+    if (!mFeatures.syncVertexArraysToDefault.enabled)
     {
-        ANGLE_GL_TRY(context, mFunctions->enableVertexAttribArray(i));
-        ANGLE_GL_TRY(context,
-                     mFunctions->vertexAttribPointer(i, 2, GL_FLOAT, GL_FALSE, 0, nullptr));
+        ANGLE_GL_TRY(context, mFunctions->genVertexArrays(1, &mVAO));
+        ANGLE_TRY(setVAOState(context));
     }
 
     constexpr GLenum potentialSRGBMipmapGenerationFormats[] = {
@@ -1227,6 +1216,49 @@ angle::Result BlitGL::setScratchTextureParameter(const gl::Context *context,
     return angle::Result::Continue;
 }
 
+angle::Result BlitGL::setVAOState(const gl::Context *context)
+{
+    if (mFeatures.syncVertexArraysToDefault.enabled)
+    {
+        ASSERT(mVAO == 0);
+        mStateManager->bindVertexArray(mVAO, mStateManager->getDefaultVAOState());
+        ANGLE_TRY(initializeVAOState(context));
+    }
+    else
+    {
+        mStateManager->bindVertexArray(mVAO, nullptr);
+    }
+
+    return angle::Result::Continue;
+}
+
+angle::Result BlitGL::initializeVAOState(const gl::Context *context)
+{
+    mStateManager->bindBuffer(gl::BufferBinding::Array, mVertexBuffer);
+
+    ANGLE_GL_TRY(context, mFunctions->enableVertexAttribArray(mTexcoordAttribLocation));
+    ANGLE_GL_TRY(context, mFunctions->vertexAttribPointer(mTexcoordAttribLocation, 2, GL_FLOAT,
+                                                          GL_FALSE, 0, nullptr));
+    if (mFeatures.syncVertexArraysToDefault.enabled)
+    {
+        VertexArrayStateGL *defaultVAOState = mStateManager->getDefaultVAOState();
+
+        VertexAttributeGL &attribute = defaultVAOState->attributes[mTexcoordAttribLocation];
+        attribute.enabled            = true;
+        attribute.format             = &angle::Format::Get(angle::FormatID::R32G32_FLOAT);
+        attribute.pointer            = nullptr;
+
+        VertexBindingGL &binding = defaultVAOState->bindings[mTexcoordAttribLocation];
+        binding.stride           = 8;
+        binding.offset           = 0;
+        binding.buffer           = mVertexBuffer;
+
+        mStateManager->setDefaultVAOStateDirty();
+    }
+
+    return angle::Result::Continue;
+}
+
 angle::Result BlitGL::getBlitProgram(const gl::Context *context,
                                      gl::TextureType sourceTextureType,
                                      GLenum sourceComponentType,
@@ -1242,6 +1274,7 @@ angle::Result BlitGL::getBlitProgram(const gl::Context *context,
 
         // Depending on what types need to be output by the shaders, different versions need to be
         // used.
+        const std::string texcoordAttribName = "a_texcoord";
         std::string version;
         std::string vsInputVariableQualifier;
         std::string vsOutputVariableQualifier;
@@ -1282,15 +1315,17 @@ angle::Result BlitGL::getBlitProgram(const gl::Context *context,
             // Compile the vertex shader
             std::ostringstream vsSourceStream;
             vsSourceStream << "#version " << version << "\n";
-            vsSourceStream << vsInputVariableQualifier << " vec2 a_texcoord;\n";
+            vsSourceStream << vsInputVariableQualifier << " vec2 " << texcoordAttribName << ";\n";
             vsSourceStream << "uniform vec2 u_scale;\n";
             vsSourceStream << "uniform vec2 u_offset;\n";
             vsSourceStream << vsOutputVariableQualifier << " vec2 v_texcoord;\n";
             vsSourceStream << "\n";
             vsSourceStream << "void main()\n";
             vsSourceStream << "{\n";
-            vsSourceStream << "    gl_Position = vec4((a_texcoord * 2.0) - 1.0, 0.0, 1.0);\n";
-            vsSourceStream << "    v_texcoord = a_texcoord * u_scale + u_offset;\n";
+            vsSourceStream << "    gl_Position = vec4((" << texcoordAttribName
+                           << " * 2.0) - 1.0, 0.0, 1.0);\n";
+            vsSourceStream << "    v_texcoord = " << texcoordAttribName
+                           << " * u_scale + u_offset;\n";
             vsSourceStream << "}\n";
 
             std::string vsSourceStr  = vsSourceStream.str();
@@ -1477,6 +1512,9 @@ angle::Result BlitGL::getBlitProgram(const gl::Context *context,
             ANGLE_GL_TRY(context, mFunctions->deleteShader(fs));
         }
 
+        ANGLE_GL_TRY(context,
+                     mFunctions->bindAttribLocation(result.program, mTexcoordAttribLocation,
+                                                    texcoordAttribName.c_str()));
         ANGLE_GL_TRY(context, mFunctions->linkProgram(result.program));
         ANGLE_TRY(CheckLinkStatus(context, mFunctions, result.program));
 
