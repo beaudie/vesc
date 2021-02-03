444b367c20d26c9b380d9d161d5052570bf00e1c
Cody Northrop <cnorthrop@google.com>
2021-02-03 14:59:16 -0700

    Tests: Add Shadow Fight 2 trace
    Test: angle_perftests --gtest_filter="*shadow_fight_2*"
    Bug: angleproject:5610
    Change-Id: I80c6c44e19d77124096ed5970f99fbb6157bf03b

--- src/tests/restricted_traces/restricted_traces_autogen.h
+++ src/tests/restricted_traces/restricted_traces_autogen.h
@@ -84,6 +84,7 @@ enum class RestrictedTraceID
     real_commando_secret_mission,
     real_gangster_crime,
     saint_seiya_awakening,
+    shadow_fight_2,
     subway_surfers,
     temple_run_300,
     trex_200,
