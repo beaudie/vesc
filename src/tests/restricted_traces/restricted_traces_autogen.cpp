061e48e980421e5eb0ce2823f9f4d6fe8fcd7bc3
Tim Van Patten <timvp@google.com>
2021-02-03 18:12:06 -0700

    Tests: Add Bus Simulator Indonesia trace
    Bug: angleproject:5612
    Test: angle_perftests --gtest_filter=TracePerfTest.Run/*bus_simulator_indonesia*
    Change-Id: I067795b237fc32406d63785e347324b423978768

--- src/tests/restricted_traces/restricted_traces_autogen.cpp
+++ src/tests/restricted_traces/restricted_traces_autogen.cpp
@@ -15,6 +15,7 @@
 #include "arena_of_valor/arena_of_valor_capture_context1.h"
 #include "asphalt_8/asphalt_8_capture_context2.h"
 #include "brawl_stars/brawl_stars_capture_context1.h"
+#include "bus_simulator_indonesia/bus_simulator_indonesia_capture_context1.h"
 #include "candy_crush_500/candy_crush_500_capture_context1.h"
 #include "clash_of_clans/clash_of_clans_capture_context1.h"
 #include "clash_royale/clash_royale_capture_context2.h"
@@ -79,6 +80,10 @@ constexpr angle::PackedEnumMap<RestrictedTraceID, TraceInfo> kTraceInfos = {
     {RestrictedTraceID::brawl_stars,
      {brawl_stars::kReplayFrameStart, brawl_stars::kReplayFrameEnd,
       brawl_stars::kReplayDrawSurfaceWidth, brawl_stars::kReplayDrawSurfaceHeight, "brawl_stars"}},
+    {RestrictedTraceID::bus_simulator_indonesia,
+     {bus_simulator_indonesia::kReplayFrameStart, bus_simulator_indonesia::kReplayFrameEnd,
+      bus_simulator_indonesia::kReplayDrawSurfaceWidth,
+      bus_simulator_indonesia::kReplayDrawSurfaceHeight, "bus_simulator_indonesia"}},
     {RestrictedTraceID::candy_crush_500,
      {candy_crush_500::kReplayFrameStart, candy_crush_500::kReplayFrameEnd,
       candy_crush_500::kReplayDrawSurfaceWidth, candy_crush_500::kReplayDrawSurfaceHeight,
@@ -264,6 +269,9 @@ void ReplayFrame(RestrictedTraceID traceID, uint32_t frameIndex)
         case RestrictedTraceID::brawl_stars:
             brawl_stars::ReplayContext1Frame(frameIndex);
             break;
+        case RestrictedTraceID::bus_simulator_indonesia:
+            bus_simulator_indonesia::ReplayContext1Frame(frameIndex);
+            break;
         case RestrictedTraceID::candy_crush_500:
             candy_crush_500::ReplayContext1Frame(frameIndex);
             break;
@@ -418,6 +426,9 @@ void ResetReplay(RestrictedTraceID traceID)
             break;
         case RestrictedTraceID::brawl_stars:
             brawl_stars::ResetContext1Replay();
+            break;
+        case RestrictedTraceID::bus_simulator_indonesia:
+            bus_simulator_indonesia::ResetContext1Replay();
             break;
         case RestrictedTraceID::candy_crush_500:
             candy_crush_500::ResetContext1Replay();
@@ -574,6 +585,9 @@ void SetupReplay(RestrictedTraceID traceID)
         case RestrictedTraceID::brawl_stars:
             brawl_stars::SetupContext1Replay();
             break;
+        case RestrictedTraceID::bus_simulator_indonesia:
+            bus_simulator_indonesia::SetupContext1Replay();
+            break;
         case RestrictedTraceID::candy_crush_500:
             candy_crush_500::SetupContext1Replay();
             break;
@@ -729,6 +743,9 @@ void SetBinaryDataDir(RestrictedTraceID traceID, const char *dataDir)
         case RestrictedTraceID::brawl_stars:
             brawl_stars::SetBinaryDataDir(dataDir);
             break;
+        case RestrictedTraceID::bus_simulator_indonesia:
+            bus_simulator_indonesia::SetBinaryDataDir(dataDir);
+            break;
         case RestrictedTraceID::candy_crush_500:
             candy_crush_500::SetBinaryDataDir(dataDir);
             break;
@@ -883,6 +900,9 @@ void SetBinaryDataDecompressCallback(RestrictedTraceID traceID, DecompressCallba
             break;
         case RestrictedTraceID::brawl_stars:
             brawl_stars::SetBinaryDataDecompressCallback(callback);
+            break;
+        case RestrictedTraceID::bus_simulator_indonesia:
+            bus_simulator_indonesia::SetBinaryDataDecompressCallback(callback);
             break;
         case RestrictedTraceID::candy_crush_500:
             candy_crush_500::SetBinaryDataDecompressCallback(callback);
