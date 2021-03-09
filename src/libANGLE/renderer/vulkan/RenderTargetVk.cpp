b3157e4222e25b0dc13b1c1cd93ffdfb30dd8bd9
Charlie Lao <cclao@google.com>
2021-03-08 19:08:14 -0800

    Vulkan: Defer color image layout changes at endRenderPass time
    Right now color render target's image's layout change are done at
    beginRenderPass time. The problem is that the layout also depends on
    whether texture is also been used for sampler or not. That information
    is not known when renderpass starts. We did some special treatment for
    depth stencil attachment so that its layout determination is deferred
    until endRenderPass time. This CL expands that same mechanism to color
    attachment as well. Right now the color attachment will still pick the
    same ImageLayout::ColorAttachment layout since the logic to detect it is
    also used for texture sampling is not there yet.
    
    Bug: b/175584609
    Change-Id: Id7486174d475f894461578b31d0d40fdd90e808a

--- src/libANGLE/renderer/vulkan/RenderTargetVk.cpp
+++ src/libANGLE/renderer/vulkan/RenderTargetVk.cpp
@@ -95,24 +95,25 @@ vk::ImageOrBufferViewSubresourceSerial RenderTargetVk::getResolveSubresourceSeri
     return getSubresourceSerialImpl(mResolveImageViews);
 }
 
-void RenderTargetVk::onColorDraw(ContextVk *contextVk, uint32_t framebufferLayerCount)
+void RenderTargetVk::onColorDraw(ContextVk *contextVk,
+                                 uint32_t framebufferLayerCount,
+                                 vk::PackedAttachmentIndex packedAttachmentIndex)
 {
     ASSERT(!mImage->getFormat().actualImageFormat().hasDepthOrStencilBits());
     ASSERT(framebufferLayerCount <= mLayerCount);
 
-    contextVk->onImageRenderPassWrite(mLevelIndexGL, mLayerIndex, framebufferLayerCount,
-                                      VK_IMAGE_ASPECT_COLOR_BIT, vk::ImageLayout::ColorAttachment,
-                                      mImage);
+    contextVk->onColorDraw(mImage, mResolveImage, packedAttachmentIndex);
+    retainImageViews(contextVk);
+
+    mImage->onWrite(mLevelIndexGL, 1, mLayerIndex, framebufferLayerCount,
+                    VK_IMAGE_ASPECT_COLOR_BIT);
     if (mResolveImage)
     {
         // Multisampled render to texture framebuffers cannot be layered.
         ASSERT(framebufferLayerCount == 1);
-
-        contextVk->onImageRenderPassWrite(mLevelIndexGL, mLayerIndex, framebufferLayerCount,
-                                          VK_IMAGE_ASPECT_COLOR_BIT,
-                                          vk::ImageLayout::ColorAttachment, mResolveImage);
+        mResolveImage->onWrite(mLevelIndexGL, 1, mLayerIndex, framebufferLayerCount,
+                               VK_IMAGE_ASPECT_COLOR_BIT);
     }
-    retainImageViews(contextVk);
 }
 
 void RenderTargetVk::onDepthStencilDraw(ContextVk *contextVk, uint32_t framebufferLayerCount)
