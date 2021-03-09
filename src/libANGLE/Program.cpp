257eaac3028e9a3bda78df52cf207ae27213de13
Mohan Maiya <m.maiya@samsung.com>
2021-03-09 08:09:55 -0800

    Call resolveLink() in Program::detachShader()
    Previously glDetachShader() would only call resolveLink during
    validation.
    
    Adding the resolveLink in Program::detachShader handles the case where
    glDetachShader() is called with EGL_CONTEXT_OPENGL_NO_ERROR_KHR to skip
    validation.
    
    Tests: angle_end2end_tests --gtest_filter=ContextNoErrorTest.DetachAfterLink*
    Bug: angleproject:2868
    Change-Id: I1e01cf8e6f8a382333226bdef037c46f4c62a119

--- src/libANGLE/Program.cpp
+++ src/libANGLE/Program.cpp
@@ -1376,7 +1376,7 @@ void Program::attachShader(const Context *context, Shader *shader)
 
 void Program::detachShader(const Context *context, Shader *shader)
 {
-    ASSERT(!mLinkingState);
+    resolveLink(context);
     ShaderType shaderType = shader->getType();
     ASSERT(shaderType != ShaderType::InvalidEnum);
 
