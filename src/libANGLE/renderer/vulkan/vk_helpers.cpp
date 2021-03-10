871cde2957428614dc0bad52db410c28bddc8f7f
Charlie Lao <cclao@google.com>
2021-03-10 11:57:38 -0800

    Vulkan: Track images used for both attachment and sampler
    This CL adds a new layout ImageLayout::ColorAttachmentAndShaderRead. We
    detect that the same ImageHelper object is used for both sampler and
    attachment and will pick this new layout instead of ColorAttachment.
    
    Bug: b/175584609
    Change-Id: I2089f59d4dc1ad4de7edab0e067797adcfa9b020

--- src/libANGLE/renderer/vulkan/vk_helpers.cpp
+++ src/libANGLE/renderer/vulkan/vk_helpers.cpp
@@ -145,6 +145,21 @@ constexpr angle::PackedEnumMap<ImageLayout, ImageMemoryBarrierData> kImageMemory
         },
     },
     {
+        ImageLayout::ColorAttachmentAndShaderRead,
+        ImageMemoryBarrierData{
+            "ColorAttachmentAndShaderRead",
+            VK_IMAGE_LAYOUT_GENERAL,
+            VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT | kAllShadersPipelineStageFlags,
+            VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT | kAllShadersPipelineStageFlags,
+            // Transition to: all reads and writes must happen after barrier.
+            VK_ACCESS_COLOR_ATTACHMENT_READ_BIT | VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT | VK_ACCESS_SHADER_READ_BIT,
+            // Transition from: all writes must finish before barrier.
+            VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT,
+            ResourceAccess::Write,
+            PipelineStage::VertexShader,
+        },
+    },
+    {
         ImageLayout::DepthStencilReadOnly,
         ImageMemoryBarrierData{
             "DepthStencilReadOnly",
@@ -1024,6 +1039,7 @@ void CommandBufferHelper::colorImagesDraw(ResourceUseList *resourceUseList,
     }
     ASSERT(mColorImages[packedAttachmentIndex] == nullptr);
     mColorImages[packedAttachmentIndex] = image;
+    image->setRenderPassUsageFlag(RenderPassUsage::RenderTargetAttachment);
 
     if (resolveImage)
     {
@@ -1034,20 +1050,8 @@ void CommandBufferHelper::colorImagesDraw(ResourceUseList *resourceUseList,
         }
         ASSERT(mColorResolveImages[packedAttachmentIndex] == nullptr);
         mColorResolveImages[packedAttachmentIndex] = resolveImage;
+        resolveImage->setRenderPassUsageFlag(RenderPassUsage::RenderTargetAttachment);
     }
-}
-
-bool CommandBufferHelper::usesImageInAttachments(const ImageHelper &image)
-{
-    ASSERT(mIsRenderPassCommandBuffer);
-    for (PackedAttachmentIndex index = kAttachmentIndexZero; index < mColorImagesCount; ++index)
-    {
-        if (mColorImages[index] == &image || mColorResolveImages[index] == &image)
-        {
-            return true;
-        }
-    }
-    return false;
 }
 
 void CommandBufferHelper::depthStencilImagesDraw(ResourceUseList *resourceUseList,
@@ -1225,6 +1229,13 @@ void CommandBufferHelper::updateColorImageLayout(Context *context,
                                                  VkImageAspectFlags aspectFlags,
                                                  ImageLayout imageLayout)
 {
+    if (image->usedByCurrentRenderPassAsAttachmentAndSampler())
+    {
+        // texture code already picked layout and inserted barrier
+        ASSERT(imageLayout == ImageLayout::ColorAttachmentAndShaderRead);
+        return;
+    }
+
     updateImageLayoutAndBarrier(context, image, aspectFlags, imageLayout);
 
     if (mImageToPresent == image)
@@ -1252,6 +1263,7 @@ void CommandBufferHelper::finalizeColorImageLayout(Context *context,
     mAttachmentOps.setLayouts(packedAttachmentIndex, imageLayout, imageLayout);
     updateColorImageLayout(context, mColorImages[packedAttachmentIndex], VK_IMAGE_ASPECT_COLOR_BIT,
                            imageLayout);
+    mColorImages[packedAttachmentIndex]->resetRenderPassUsageFlags();
 }
 
 void CommandBufferHelper::finalizeColorResolveImageLayout(
@@ -1266,6 +1278,7 @@ void CommandBufferHelper::finalizeColorResolveImageLayout(
     ImageLayout imageLayout = ImageLayout::ColorAttachment;
     updateColorImageLayout(context, mColorResolveImages[packedAttachmentIndex],
                            VK_IMAGE_ASPECT_COLOR_BIT, imageLayout);
+    mColorResolveImages[packedAttachmentIndex]->resetRenderPassUsageFlags();
 }
 
 void CommandBufferHelper::finalizeDepthStencilImageLayout(Context *context)
@@ -1362,17 +1375,20 @@ void CommandBufferHelper::onImageHelperRelease(Context *context, const ImageHelp
 {
     ASSERT(mIsRenderPassCommandBuffer);
 
-    for (PackedAttachmentIndex index = kAttachmentIndexZero; index < mColorImagesCount; ++index)
+    if (image->hasRenderPassUseFlag(RenderPassUsage::RenderTargetAttachment))
     {
-        if (mColorImages[index] == image)
+        for (PackedAttachmentIndex index = kAttachmentIndexZero; index < mColorImagesCount; ++index)
         {
-            finalizeColorImageLayout(context, index);
-            mColorImages[index] = nullptr;
-        }
-        if (mColorResolveImages[index] == image)
-        {
-            finalizeColorResolveImageLayout(context, index);
-            mColorResolveImages[index] = nullptr;
+            if (mColorImages[index] == image)
+            {
+                finalizeColorImageLayout(context, index);
+                mColorImages[index] = nullptr;
+            }
+            if (mColorResolveImages[index] == image)
+            {
+                finalizeColorResolveImageLayout(context, index);
+                mColorResolveImages[index] = nullptr;
+            }
         }
     }
 
@@ -3659,6 +3675,7 @@ void ImageHelper::resetCachedProperties()
     mLevelCount                  = 0;
     mExternalFormat              = 0;
     mCurrentSingleClearValue.reset();
+    mRenderPassUseFlags.reset();
 
     setEntireContentUndefined();
 }
@@ -4438,6 +4455,27 @@ gl::Extents ImageHelper::getRotatedLevelExtents2D(LevelIndex levelVk) const
 bool ImageHelper::isDepthOrStencil() const
 {
     return mFormat->actualImageFormat().hasDepthOrStencilBits();
+}
+
+void ImageHelper::setRenderPassUsageFlag(RenderPassUsage flag)
+{
+    mRenderPassUseFlags.set(flag);
+}
+
+void ImageHelper::resetRenderPassUsageFlags()
+{
+    mRenderPassUseFlags.reset();
+}
+
+bool ImageHelper::hasRenderPassUseFlag(RenderPassUsage flag) const
+{
+    return mRenderPassUseFlags.test(flag);
+}
+
+bool ImageHelper::usedByCurrentRenderPassAsAttachmentAndSampler() const
+{
+    return mRenderPassUseFlags[RenderPassUsage::RenderTargetAttachment] &&
+           mRenderPassUseFlags[RenderPassUsage::TextureSampler];
 }
 
 bool ImageHelper::isReadBarrierNecessary(ImageLayout newLayout) const
