a7a9d11793bf70ca805736fbd3f2a1cfc290ef7b
John Plate <jplate@google.com>
2021-03-11 11:30:39 +0000

    Fix CL_EVENT macro
    Bug: angleproject:5745
    Change-Id: Icbfab0d0bd4b1510b98d5ed746f4d3ddfa5ec36e

---
+++ src/libOpenCL/entry_points_utils.h
@@ -0,0 +1,25 @@
+//
+// Copyright 2021 The ANGLE Project Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+// entry_points_utils.h:
+//   These helpers are used in CL entry point routines.
+
+#ifndef LIBOPENCL_ENTRY_POINTS_UTILS_H_
+#define LIBOPENCL_ENTRY_POINTS_UTILS_H_
+
+#include "common/debug.h"
+
+#include <cinttypes>
+#include <cstdio>
+
+#if defined(ANGLE_TRACE_ENABLED)
+#    define CL_EVENT(entryPoint, ...)                                 \
+        std::printf("OpenCL entry point = " #entryPoint __VA_ARGS__); \
+        std::printf("\n")
+#else
+#    define CL_EVENT(entryPoint, ...) (void(0))
+#endif
+
+#endif  // LIBOPENCL_ENTRY_POINTS_UTILS_H_
