93c31673a6012c0d44ed4148b7287e164d2f9ad6
Tim Van Patten <timvp@google.com>
2021-03-17 17:43:03 -0600

    Reland "Save/Load missing members"
    This is a reland of b2e76cf58fc012b068aab4716a432bb8951e93e3
    
    Always call saveLinkedStateInfo() so we fill in the necessary values
    with valid data for monolithic programs also.
    
    Original change's description:
    > Save/Load missing members
    >
    > There are several class/struct members that are missing when a program
    > is serialized/deserialized. This leads to errors when attempting to link
    > programs that have been deserialized. For example, when drawing with a
    > PPO that contains programs which were created with glProgramBinary().
    >
    > This CL adds saving/loading the missing members.
    >
    > Bug: b/182409935
    > Change-Id: I637c6cd8c174acd6da8d51433893323a32e5d7c0
    > Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/2770683
    > Commit-Queue: Tim Van Patten <timvp@google.com>
    > Reviewed-by: Jamie Madill <jmadill@chromium.org>
    > Reviewed-by: Shahbaz Youssefi <syoussefi@chromium.org>
    > Reviewed-by: Cody Northrop <cnorthrop@google.com>
    
    Bug: b/182409935
    Bug: angleproject:5793
    Change-Id: I07ca8f5dfed8c0a9eac3a0defb1602d2ba021c5f

--- src/libANGLE/ProgramExecutable.cpp
+++ src/libANGLE/ProgramExecutable.cpp
@@ -10,7 +10,6 @@
 
 #include "libANGLE/Context.h"
 #include "libANGLE/Program.h"
-#include "libANGLE/ProgramPipeline.h"
 #include "libANGLE/Shader.h"
 
 namespace gl
@@ -414,6 +413,35 @@ void ProgramExecutable::load(gl::BinaryInputStream *stream)
             mGraphicsImageBindings.emplace_back(imageBinding);
         }
     }
+
+    for (ShaderType shaderType : mLinkedGraphicsShaderStages)
+    {
+        mLinkedOutputVaryings[shaderType].resize(stream->readInt<size_t>());
+        for (sh::ShaderVariable &variable : mLinkedOutputVaryings[shaderType])
+        {
+            LoadShaderVar(stream, &variable);
+        }
+        mLinkedInputVaryings[shaderType].resize(stream->readInt<size_t>());
+        for (sh::ShaderVariable &variable : mLinkedInputVaryings[shaderType])
+        {
+            LoadShaderVar(stream, &variable);
+        }
+        mLinkedShaderVersions[shaderType] = stream->readInt<int>();
+    }
+    for (ShaderType shaderType : mLinkedComputeShaderStages)
+    {
+        mLinkedOutputVaryings[shaderType].resize(stream->readInt<size_t>());
+        for (sh::ShaderVariable &variable : mLinkedOutputVaryings[shaderType])
+        {
+            LoadShaderVar(stream, &variable);
+        }
+        mLinkedInputVaryings[shaderType].resize(stream->readInt<size_t>());
+        for (sh::ShaderVariable &variable : mLinkedInputVaryings[shaderType])
+        {
+            LoadShaderVar(stream, &variable);
+        }
+        mLinkedShaderVersions[shaderType] = stream->readInt<int>();
+    }
 }
 
 void ProgramExecutable::save(gl::BinaryOutputStream *stream) const
@@ -466,8 +494,6 @@ void ProgramExecutable::save(gl::BinaryOutputStream *stream) const
     for (const LinkedUniform &uniform : getUniforms())
     {
         WriteShaderVar(stream, uniform);
-
-        // FIXME: referenced
 
         stream->writeInt(uniform.bufferIndex);
         WriteBlockMemberInfo(stream, uniform.blockInfo);
@@ -562,6 +588,35 @@ void ProgramExecutable::save(gl::BinaryOutputStream *stream) const
         {
             stream->writeInt(imageBinding.boundImageUnits[i]);
         }
+    }
+
+    for (ShaderType shaderType : mLinkedGraphicsShaderStages)
+    {
+        stream->writeInt(mLinkedOutputVaryings[shaderType].size());
+        for (const sh::ShaderVariable &shaderVariable : mLinkedOutputVaryings[shaderType])
+        {
+            WriteShaderVar(stream, shaderVariable);
+        }
+        stream->writeInt(mLinkedInputVaryings[shaderType].size());
+        for (const sh::ShaderVariable &shaderVariable : mLinkedInputVaryings[shaderType])
+        {
+            WriteShaderVar(stream, shaderVariable);
+        }
+        stream->writeInt(mLinkedShaderVersions[shaderType]);
+    }
+    for (ShaderType shaderType : mLinkedComputeShaderStages)
+    {
+        stream->writeInt(mLinkedOutputVaryings[shaderType].size());
+        for (const sh::ShaderVariable &shaderVariable : mLinkedOutputVaryings[shaderType])
+        {
+            WriteShaderVar(stream, shaderVariable);
+        }
+        stream->writeInt(mLinkedInputVaryings[shaderType].size());
+        for (const sh::ShaderVariable &shaderVariable : mLinkedInputVaryings[shaderType])
+        {
+            WriteShaderVar(stream, shaderVariable);
+        }
+        stream->writeInt(mLinkedShaderVersions[shaderType]);
     }
 }
 
