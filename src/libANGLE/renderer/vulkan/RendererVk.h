e949835eb5c474e5252c2cf6f3190d0355a60b13
Amy Liu <amy.liu@arm.com>
2021-03-18 16:17:25 +0800

    Compression of the data from vkGetPipelineCacheData.
    The size of pipelineCacheData sometimes is greater than
    64k which cannot be saved because of the Android blob cache
    limitation (single cache data size should be < 64k).
    Implement the compression to store more cache data.
    
    Re-land this patch before fixing the performance regression
    of big pipeline cache.
    
    Bug: angleproject:4722
    Change-Id: I4bc05a88334c3e7e9e945d1a0877429db1750422

--- src/libANGLE/renderer/vulkan/RendererVk.h
+++ src/libANGLE/renderer/vulkan/RendererVk.h
@@ -376,7 +376,6 @@ class RendererVk : angle::NonCopyable
     void queryDeviceExtensionFeatures(const vk::ExtensionNameList &deviceExtensionNames);
 
     void initFeatures(DisplayVk *display, const vk::ExtensionNameList &extensions);
-    void initPipelineCacheVkKey();
     angle::Result initPipelineCache(DisplayVk *display,
                                     vk::PipelineCache *pipelineCache,
                                     bool *success);
@@ -445,7 +444,6 @@ class RendererVk : angle::NonCopyable
     // a lock.
     std::mutex mPipelineCacheMutex;
     vk::PipelineCache mPipelineCache;
-    egl::BlobCache::Key mPipelineCacheVkBlobKey;
     uint32_t mPipelineCacheVkUpdateTimeout;
     bool mPipelineCacheDirty;
     bool mPipelineCacheInitialized;
