c67f753853130825ca34a04425054931ab7933fd
Tim Van Patten <timvp@google.com>
2021-03-19 20:10:42 -0600

    Detach separable shaders
    This effectively reverts the following CL:
    Vulkan: Don't detach separable shaders in Program::detachShader()
    https://chromium-review.googlesource.com/c/angle/angle/+/2084399
    
    Bug: angleproject:3570
    Bug: b/182409935
    Test: ProgramPipelineTest31.DetachAndModifyShader
    Change-Id: Id69f732d1d4f3eea70b2de8c008452878eaf1682

--- src/libANGLE/capture/frame_capture_utils.cpp
+++ src/libANGLE/capture/frame_capture_utils.cpp
@@ -765,10 +765,6 @@ void SerializeProgramState(gl::BinaryOutputStream *bos, const gl::ProgramState &
             bos->writeInt(0);
         }
     }
-    for (bool isAttached : programState.getAttachedShadersMarkedForDetach())
-    {
-        bos->writeBool(isAttached);
-    }
     bos->writeInt(programState.getLocationsUsedForXfbExtension());
     for (const std::string &transformFeedbackVaryingName :
          programState.getTransformFeedbackVaryingNames())
