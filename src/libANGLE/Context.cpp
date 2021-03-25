1635ed8534d106adc5903d40552220fe9760e3f6
Jeff Vigil <j.vigil@samsung.com>
2021-03-25 15:43:06 -0700

    EGL: GLES: Implement GL_EXT_protected_textures
    Implement EGL_EXT_protected_content Images
    Add protected member to Images and Textures
    Add error when creating objects if not supported or
    does't match native buffer
    When creating siblings pass protected state
    Add extension caps
    Add Validation
    Add GetTexParameter and SetTextparameter
    Add protected to Texture and state
    Expand tests for images and textures
    
    Test: angle_end2end_test --gtest_filter=EGLProtectedContentTest
    
    Bug: angleproject:3965
    Change-Id: I35a89b4e80bba6d9b6831c68e71630eef304dacb

--- src/libANGLE/Context.cpp
+++ src/libANGLE/Context.cpp
@@ -6,7 +6,6 @@
 
 // Context.cpp: Implements the gl::Context class, managing all GL state and performing
 // rendering operations. It is the GLES2 specific implementation of EGLContext.
-
 #include "libANGLE/Context.h"
 #include "libANGLE/Context.inl.h"
 
@@ -3557,6 +3556,12 @@ Extensions Context::generateSupportedExtensions() const
         // GL_KHR_texture_compression_astc_ldr and GL_KHR_texture_compression_astc_hdr
         ASSERT(supportedExtensions.textureCompressionASTCLDRKHR);
         ASSERT(supportedExtensions.textureCompressionASTCHDRKHR);
+    }
+
+    // GL_KHR_protected_textures
+    if (!mDisplay->getExtensions().protectedContentEXT)
+    {
+        supportedExtensions.protectedTexturesEXT = false;
     }
 
     // GL_ANGLE_get_tex_level_parameter is implemented in the frontend
