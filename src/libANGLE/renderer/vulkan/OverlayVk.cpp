091dffabe97ea57a19e4e6258236829941206a05
Jeff Vigil <j.vigil@samsung.com>
2021-03-25 15:40:58 -0700

    EGL: implement EGL_EXT_protected_content Context
    Add Validation check to Contexts and Surfaces
    Add Vulkan protected memory features and properties
    Add protected member to Surfaces and Contexts
    Implement hasProtectedContent in Vulkan
    Add QueueFamily helper, extent DeviceQueueMap
    Protected Swapchains always on for Android
    Add EGLProtectedContentTest
    
    Test: angle_end2end_test --gtest_filter=EGLProtectedContentTest
    
    Bug: angleproject:3965
    Change-Id: I9352b1e594f71bb4e89cee7137a468940d186b1b

--- src/libANGLE/renderer/vulkan/OverlayVk.cpp
+++ src/libANGLE/renderer/vulkan/OverlayVk.cpp
@@ -115,13 +115,14 @@ angle::Result OverlayVk::createFont(ContextVk *contextVk)
     bool useRobustInit = false;
 
     // Create the font image.
-    ANGLE_TRY(
-        mFontImage.init(contextVk, gl::TextureType::_2D,
-                        VkExtent3D{gl::overlay::kFontImageWidth, gl::overlay::kFontImageHeight, 1},
-                        renderer->getFormat(angle::FormatID::R8_UNORM), 1,
-                        VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT,
-                        gl::LevelIndex(0), 1, gl::overlay::kFontCount, useRobustInit));
-    ANGLE_TRY(mFontImage.initMemory(contextVk, renderer->getMemoryProperties(),
+    ANGLE_TRY(mFontImage.init(
+        contextVk, gl::TextureType::_2D,
+        VkExtent3D{gl::overlay::kFontImageWidth, gl::overlay::kFontImageHeight, 1},
+        renderer->getFormat(angle::FormatID::R8_UNORM), 1,
+        VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT, gl::LevelIndex(0), 1,
+        gl::overlay::kFontCount, useRobustInit, contextVk->hasProtectedContent()));
+    ANGLE_TRY(mFontImage.initMemory(contextVk, contextVk->hasProtectedContent(),
+                                    renderer->getMemoryProperties(),
                                     VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT));
     ANGLE_TRY(mFontImage.initImageView(contextVk, gl::TextureType::_2DArray,
                                        VK_IMAGE_ASPECT_COLOR_BIT, gl::SwizzleState(),
@@ -193,8 +194,10 @@ angle::Result OverlayVk::cullWidgets(ContextVk *contextVk)
     ANGLE_TRY(mCulledWidgets.init(contextVk, gl::TextureType::_2D, culledWidgetsExtent,
                                   renderer->getFormat(angle::FormatID::R32G32_UINT), 1,
                                   VK_IMAGE_USAGE_STORAGE_BIT | VK_IMAGE_USAGE_SAMPLED_BIT,
-                                  gl::LevelIndex(0), 1, 1, useRobustInit));
-    ANGLE_TRY(mCulledWidgets.initMemory(contextVk, renderer->getMemoryProperties(),
+                                  gl::LevelIndex(0), 1, 1, useRobustInit,
+                                  contextVk->hasProtectedContent()));
+    ANGLE_TRY(mCulledWidgets.initMemory(contextVk, contextVk->hasProtectedContent(),
+                                        renderer->getMemoryProperties(),
                                         VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT));
     ANGLE_TRY(mCulledWidgets.initImageView(contextVk, gl::TextureType::_2D,
                                            VK_IMAGE_ASPECT_COLOR_BIT, gl::SwizzleState(),
