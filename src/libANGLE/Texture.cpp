e05c9ac246429027a98b64a9a00d14e8e000de1d
Jeff Vigil <j.vigil@samsung.com>
2021-03-25 15:43:06 -0700

    EGL: GLES: Implement GL_EXT_protected_textures
    Implement EGL_EXT_protected_content Images
    Add protected member to Images and Textures
    Add error when creating objects if not supported or
    does't match native buffer
    When creating siblings pass protected state
    Add extension caps
    Add Validation
    Add GetTexParameter and SetTextparameter
    Add protected to Texture and state
    Expand tests for images and textures
    
    Test: angle_end2end_test --gtest_filter=EGLProtectedContentTest
    
    Bug: angleproject:3965
    Change-Id: I35a89b4e80bba6d9b6831c68e71630eef304dacb

--- src/libANGLE/Texture.cpp
+++ src/libANGLE/Texture.cpp
@@ -144,6 +144,7 @@ TextureState::TextureState(TextureType type)
       mImmutableFormat(false),
       mImmutableLevels(0),
       mUsage(GL_NONE),
+      mHasProtectedContent(false),
       mImageDescs((IMPLEMENTATION_MAX_TEXTURE_LEVELS + 1) * (type == TextureType::CubeMap ? 6 : 1)),
       mCropRect(0, 0, 0, 0),
       mGenerateMipmapHint(GL_FALSE),
@@ -1046,6 +1047,16 @@ GLenum Texture::getUsage() const
     return mState.mUsage;
 }
 
+void Texture::setProtectedContent(Context *context, bool hasProtectedContent)
+{
+    mState.mHasProtectedContent = hasProtectedContent;
+}
+
+bool Texture::hasProtectedContent() const
+{
+    return mState.mHasProtectedContent;
+}
+
 const TextureState &Texture::getTextureState() const
 {
     return mState;
@@ -1707,6 +1718,7 @@ angle::Result Texture::bindTexImageFromSurface(Context *context, egl::Surface *s
     Extents size(surface->getWidth(), surface->getHeight(), 1);
     ImageDesc desc(size, surface->getBindTexImageFormat(), InitState::Initialized);
     mState.setImageDesc(NonCubeTextureTypeToTarget(mState.mType), 0, desc);
+    mState.mHasProtectedContent = surface->hasProtectedContent();
     signalDirtyStorage(InitState::Initialized);
     return angle::Result::Continue;
 }
@@ -1720,6 +1732,7 @@ angle::Result Texture::releaseTexImageFromSurface(const Context *context)
     // Erase the image info for level 0
     ASSERT(mState.mType == TextureType::_2D || mState.mType == TextureType::Rectangle);
     mState.clearImageDesc(NonCubeTextureTypeToTarget(mState.mType), 0);
+    mState.mHasProtectedContent = false;
     signalDirtyStorage(InitState::Initialized);
     return angle::Result::Continue;
 }
@@ -1810,6 +1823,7 @@ angle::Result Texture::setEGLImageTarget(Context *context,
     mState.clearImageDescs();
     mState.setImageDesc(NonCubeTextureTypeToTarget(type), 0,
                         ImageDesc(size, imageTarget->getFormat(), initState));
+    mState.mHasProtectedContent = imageTarget->hasProtectedContent();
     signalDirtyStorage(initState);
 
     return angle::Result::Continue;
