f3f4dd6ff3af1f6770c1580171dd2d7684dd5b19
Jeff Vigil <j.vigil@samsung.com>
2021-03-25 15:40:58 -0700

    EGL: implement EGL_EXT_protected_content Context
    Add Validation check to Contexts and Surfaces
    Add Vulkan protected memory features and properties
    Add protected member to Surfaces and Contexts
    Implement hasProtectedContent in Vulkan
    Add QueueFamily helper, extent DeviceQueueMap
    Protected Swapchains always on for Android
    Add EGLProtectedContentTest
    
    Test: angle_end2end_test --gtest_filter=EGLProtectedContentTest
    
    Bug: angleproject:3965
    Change-Id: I9352b1e594f71bb4e89cee7137a468940d186b1b

--- src/libANGLE/Caps.h
+++ src/libANGLE/Caps.h
@@ -1264,6 +1264,9 @@ struct DisplayExtensions
 
     // EGL_EXT_buffer_age
     bool bufferAgeEXT = false;
+
+    // EGL_EXT_protected_content
+    bool protectedContentEXT = false;
 };
 
 struct DeviceExtensions
