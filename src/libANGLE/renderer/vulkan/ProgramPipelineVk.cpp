e667100eba555107c5ae84512ae9a2484e18d162
Mohan Maiya <m.maiya@samsung.com>
2021-03-25 18:54:50 -0700

    Assign XFB location first then assign varying location
    Varying location is determined based on XFB location. If the transform
    feedback stage is Vertex, there is no problem. But if the transform
    feedback stage is GS or TES, previous implementation can cause shader
    interface mismatch.
    
    Bug: angleproject:5557
    Test: KHR-GLES32.core.tessellation_shader.tessellation_control_to_tessellation_evaluation.gl_in
    Change-Id: I1ecc7913a178c9e674307c528d1bdf13aabcb665

--- src/libANGLE/renderer/vulkan/ProgramPipelineVk.cpp
+++ src/libANGLE/renderer/vulkan/ProgramPipelineVk.cpp
@@ -71,6 +71,36 @@ angle::Result ProgramPipelineVk::link(const gl::Context *glContext,
     // set/binding locations need to be re-assigned to their correct values.
     const gl::ShaderType linkedTransformFeedbackStage =
         glExecutable.getLinkedTransformFeedbackStage();
+
+    // This should be done before assigning varying location. Otherwise, We can encounter shader
+    // interface mismatching problem in case the transformFeedback stage is not Vertex stage.
+    for (const gl::ShaderType shaderType : glExecutable.getLinkedShaderStages())
+    {
+        gl::Program *glProgram =
+            const_cast<gl::Program *>(glPipeline->getShaderProgram(shaderType));
+        if (glProgram)
+        {
+            // The program interface info must survive across shaders, except
+            // for some program-specific values.
+            ProgramVk *programVk = vk::GetImpl(glProgram);
+            const GlslangProgramInterfaceInfo &programProgramInterfaceInfo =
+                programVk->getGlslangProgramInterfaceInfo();
+            glslangProgramInterfaceInfo.locationsUsedForXfbExtension =
+                programProgramInterfaceInfo.locationsUsedForXfbExtension;
+
+            const bool isTransformFeedbackStage =
+                shaderType == linkedTransformFeedbackStage &&
+                !glProgram->getState().getLinkedTransformFeedbackVaryings().empty();
+            if (options.supportsTransformFeedbackExtension &&
+                gl::ShaderTypeSupportsTransformFeedback(shaderType))
+            {
+                GlslangAssignTransformFeedbackLocations(
+                    shaderType, glProgram->getState(), isTransformFeedbackStage,
+                    &glslangProgramInterfaceInfo, &mExecutable.mVariableInfoMap);
+            }
+        }
+    }
+
     gl::ShaderType frontShaderType = gl::ShaderType::InvalidEnum;
     for (const gl::ShaderType shaderType : glExecutable.getLinkedShaderStages())
     {
