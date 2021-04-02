809e642169f5c7426fcf092f1d3554f600581a9a
Charlie Lao <cclao@google.com>
2021-04-02 13:59:17 -0700

    Vulkan: Add hacks to minimize GPU workload for CPU perf benchmark
    This CL try to modify the app traces such that all the GPU workload will
    be minimized. The goal is to make the frame time reflect the driver CPU
    performance, so that we can use it to compare ANGLE vs native GLES on
    the driver overhead.
    
    Bug: b/123456789
    Change-Id: Ie8b5b585e0ed55320fed7863da37cce419acc65f

--- src/tests/restricted_traces/angle_trace_gl.h
+++ src/tests/restricted_traces/angle_trace_gl.h
@@ -11,4 +11,49 @@
 #include "trace_egl_loader_autogen.h"
 #include "trace_gles_loader_autogen.h"
 
+// For CPU performance benchmark
+#if defined(glViewport)
+#    undef glViewport
+#endif
+#define glViewport(x, y, width, height) t_glViewport(x, y, 1, 1)
+
+#if defined(glDrawElements)
+#    undef glDrawElements
+#endif
+#define glDrawElements(mode, count, type, indices) t_glDrawElements(GL_POINTS, 1, type, indices)
+
+#if defined(glDrawElementsInstanced)
+#    undef glDrawElementsInstanced
+#endif
+#define glDrawElementsInstanced(mode, count, type, indices, instancecount) \
+    t_glDrawElementsInstanced(GL_POINTS, 1, type, indices, 1)
+
+#if defined(glDrawElementsBaseVertexOES)
+#    undef glDrawElementsBaseVertexOES
+#endif
+#define glDrawElementsBaseVertexOES(mode, count, type, indices, basevertex) \
+    t_glDrawElementsBaseVertexOES(GL_POINTS, 1, type, indices, basevertex)
+
+#if defined(glDrawRangeElements)
+#    undef glDrawRangeElements
+#endif
+#define glDrawRangeElements(mode, start, end, count, type, indices) \
+    t_glDrawRangeElements(GL_POINTS, start, end, 1, type, indices)
+
+#if defined(glDrawArrays)
+#    undef glDrawArrays
+#endif
+#define glDrawArrays(mode, first, count) t_glDrawArrays(GL_POINTS, first, 1)
+
+#if defined(glDrawArraysInstanced)
+#    undef glDrawArraysInstanced
+#endif
+#define glDrawArraysInstanced(mode, first, count, instancecount) \
+    t_glDrawArraysInstanced(GL_POINTS, first, 1, 1)
+
+#if defined(glDrawArraysIndirect)
+#    undef glDrawArraysIndirect
+#endif
+#define glDrawArraysIndirect(mode, indirect) t_glDrawArraysIndirect(GL_POINTS, indirect)
+
 #endif  // ANGLE_TRACE_GL_H_
