272ec7bbd815e20d276d5d094c673fc70a52cd29
Geoff Lang <geofflang@google.com>
2021-04-08 12:06:57 -0400

    Reland "Add a Vulkan feature to compress float32 vertex formats."
    This reverts commit 8ace36f8c15877264fb58af7b54baad635899dca.
    
    Original change's description:
    > Revert "Add a Vulkan feature to compress float32 vertex formats."
    >
    > Bug: b/167404532
    > Bug: b/161716126
    > Change-Id: I95157a006d5c1fd2d3c0c2c2be37fa0403c07f93
    > Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/2510011
    > Reviewed-by: Tim Van Patten <timvp@google.com>
    > Reviewed-by: Jamie Madill <jmadill@chromium.org>
    > Commit-Queue: Geoff Lang <geofflang@chromium.org>
    
    Bug: b/167404532
    Bug: b/161716126
    Change-Id: Ic6811fe3a7124e6eb1efe7c72a1a03a120217753

--- src/libANGLE/renderer/vulkan/RendererVk.cpp
+++ src/libANGLE/renderer/vulkan/RendererVk.cpp
@@ -2346,6 +2346,8 @@ void RendererVk::initFeatures(DisplayVk *displayVk,
     ANGLE_FEATURE_CONDITION(&mFeatures, forceNearestFiltering, false);
     ANGLE_FEATURE_CONDITION(&mFeatures, forceNearestMipFiltering, false);
 
+    ANGLE_FEATURE_CONDITION(&mFeatures, compressVertexData, false);
+
     ANGLE_FEATURE_CONDITION(
         &mFeatures, preferDrawClearOverVkCmdClearAttachments,
         IsPixel2(mPhysicalDeviceProperties.vendorID, mPhysicalDeviceProperties.deviceID));
