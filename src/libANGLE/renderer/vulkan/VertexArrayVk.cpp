f12ca2632b75840d2eec3cd9335e1ff01d182d2f
Geoff Lang <geofflang@google.com>
2021-04-08 12:06:57 -0400

    Reland "Add a Vulkan feature to compress float32 vertex formats."
    This reverts commit 8ace36f8c15877264fb58af7b54baad635899dca.
    
    Original change's description:
    > Revert "Add a Vulkan feature to compress float32 vertex formats."
    >
    > Bug: b/167404532
    > Bug: b/161716126
    > Change-Id: I95157a006d5c1fd2d3c0c2c2be37fa0403c07f93
    > Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/2510011
    > Reviewed-by: Tim Van Patten <timvp@google.com>
    > Reviewed-by: Jamie Madill <jmadill@chromium.org>
    > Commit-Queue: Geoff Lang <geofflang@chromium.org>
    
    Bug: b/167404532
    Bug: b/161716126
    Change-Id: Ic6811fe3a7124e6eb1efe7c72a1a03a120217753

--- src/libANGLE/renderer/vulkan/VertexArrayVk.cpp
+++ src/libANGLE/renderer/vulkan/VertexArrayVk.cpp
@@ -576,6 +576,14 @@ angle::Result VertexArrayVk::syncDirtyAttrib(ContextVk *contextVk,
             bool bindingIsAligned               = BindingIsAligned(
                 binding, intendedFormat, intendedFormat.channelCount, attrib.relativeOffset);
 
+            if (renderer->getFeatures().compressVertexData.enabled &&
+                gl::IsStaticBufferUsage(bufferGL->getUsage()) &&
+                vertexFormat.actualCompressedBufferFormatID != angle::FormatID::NONE &&
+                vertexFormat.actualBufferFormatID != vertexFormat.actualCompressedBufferFormatID)
+            {
+                compressed = true;
+            }
+
             if (vertexFormat.getVertexLoadRequiresConversion(compressed) || !bindingIsAligned)
             {
                 ConversionBuffer *conversion = bufferVk->getVertexConversionBuffer(
@@ -583,6 +591,13 @@ angle::Result VertexArrayVk::syncDirtyAttrib(ContextVk *contextVk,
                     binding.getOffset() + attrib.relativeOffset, !bindingIsAligned);
                 if (conversion->dirty)
                 {
+                    if (compressed)
+                    {
+                        INFO() << "Compressing vertex data in buffer " << bufferGL->id().value
+                               << " from " << vertexFormat.vkBufferFormat << " to "
+                               << vertexFormat.vkCompressedBufferFormat << ".";
+                    }
+
                     if (bindingIsAligned)
                     {
                         ANGLE_TRY(convertVertexBufferGPU(contextVk, bufferVk, binding, attribIndex,
