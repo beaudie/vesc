062a099aae370d36112fdb32568d45612daa121d
Lubosz Sarnecki <lubosz.sarnecki@collabora.com>
2021-04-09 16:29:36 +0200

    tests: Add DepthStencilTestES3.ReadPixelsDepth24.
    Add a test that creates a framebuffer formatted GL_DEPTH_COMPONENT24 and
    read pixels from it using glReadPixels.
    
    This test can only run on GLES3 as the GL_DEPTH_COMPONENT24 internal
    format is not available on GLES2.
    
    This test does not reproduce anglebug.com/5799.
    
    Test: angle_end2end_tests --gtest_filter=DepthStencilTestES3.ReadPixelsDepth24/ES3_Vulkan_SwiftShader
    Bug: angleproject:5799
    Change-Id: I4e898bf756498796df02ef41cc2b989df78f13df

--- src/tests/gl_tests/DepthStencilTest.cpp
+++ src/tests/gl_tests/DepthStencilTest.cpp
@@ -106,7 +106,12 @@ class DepthStencilTest : public ANGLETest
 };
 
 class DepthStencilTestES3 : public DepthStencilTest
-{};
+{
+  protected:
+    void compareDepth(uint32_t expected);
+    void clearAndCompareDepth(GLfloat depth, uint32_t expected);
+    void drawAndCompareDepth(GLProgram &program, GLfloat depth, uint32_t expected);
+};
 
 void DepthStencilTest::ensureColor(GLColor color)
 {
@@ -307,6 +312,83 @@ TEST_P(DepthStencilTestES3, ClearThenDraw)
     EXPECT_PIXEL_COLOR_EQ(kSize - 1, 0, GLColor::red);
     EXPECT_PIXEL_COLOR_EQ(0, kSize - 1, GLColor::red);
     EXPECT_PIXEL_COLOR_EQ(kSize - 1, kSize - 1, GLColor::red);
+}
+
+void DepthStencilTestES3::compareDepth(uint32_t expected)
+{
+    uint32_t pixel;
+    glReadPixels(0, 0, 1, 1, GL_DEPTH_COMPONENT, GL_UNSIGNED_INT, &pixel);
+
+    // Use a tolerance of 256 since Vulkan and OpenGL backends differ in the
+    // the unused byte when storing the 24bit depth value in
+    // a 32bit integer.
+    // In Vulkan the least significant byte is reused, in GL the most.
+    // Clearing the depth buffer with 0.5f on Mesa/Intel/Vulkan evaluates to
+    // 0x80000000, on GL to 0x80000080.
+    // Another reason for the high tolerance is that the scale differs slightly
+    // between GPU vendors. Where 0.5f evaluates to 0x80000100
+    // on Mesa/Intel/Vulkan and 0x80000000 on Mesa/AMD/Vulkan.
+    EXPECT_NEAR(pixel, expected, 256);
+}
+
+void DepthStencilTestES3::clearAndCompareDepth(GLfloat depth, uint32_t expected)
+{
+    glClearDepthf(depth);
+    glClear(GL_DEPTH_BUFFER_BIT);
+    compareDepth(expected);
+}
+
+void DepthStencilTestES3::drawAndCompareDepth(GLProgram &program,
+                                              GLfloat positionZ,
+                                              uint32_t expected)
+{
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_ALWAYS);
+    drawQuad(program, essl3_shaders::PositionAttrib(), positionZ, 1.0f);
+    glDisable(GL_DEPTH_TEST);
+    compareDepth(expected);
+}
+
+TEST_P(DepthStencilTestES3, ReadPixelsDepth24)
+{
+    ANGLE_SKIP_TEST_IF(getClientMajorVersion() < 3 && !IsGLExtensionEnabled("GL_OES_depth24") &&
+                       !IsGLExtensionEnabled("GL_NV_read_depth"));
+
+    // Create GL_DEPTH_COMPONENT24 texture
+    GLTexture depthTexture;
+    glBindTexture(GL_TEXTURE_2D, depthTexture);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT24, getWindowWidth(), getWindowHeight(), 0,
+                 GL_DEPTH_COMPONENT, GL_UNSIGNED_INT, nullptr);
+
+    // Set up framebuffer
+    GLFramebuffer depthFBO;
+    GLRenderbuffer depthRenderbuffer;
+
+    glBindFramebuffer(GL_FRAMEBUFFER, depthFBO);
+
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, depthTexture, 0);
+    glBindRenderbuffer(GL_RENDERBUFFER, depthRenderbuffer);
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT24, getWindowWidth(),
+                          getWindowHeight());
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER,
+                              depthRenderbuffer);
+
+    ASSERT_GL_FRAMEBUFFER_COMPLETE(GL_FRAMEBUFFER);
+
+    // Test clear
+    clearAndCompareDepth(0.0f, 0x0);
+    clearAndCompareDepth(0.125f, 0x20000000);
+    clearAndCompareDepth(0.5f, 0x80000000);
+    clearAndCompareDepth(1.0f, 0xffffffff);
+
+    // Test draw
+    ANGLE_GL_PROGRAM(depthTestProgram, essl3_shaders::vs::Simple(), essl3_shaders::fs::Green());
+    drawAndCompareDepth(depthTestProgram, 0.0f, 0x80000000);
+    drawAndCompareDepth(depthTestProgram, 0.125f, 0x8fffffff);
+    drawAndCompareDepth(depthTestProgram, 0.5f, 0xbfffffff);
+    drawAndCompareDepth(depthTestProgram, 1.0f, 0xffffffff);
+
+    ASSERT_GL_NO_ERROR();
 }
 
 ANGLE_INSTANTIATE_TEST_ES2_AND_ES3(DepthStencilTest);
