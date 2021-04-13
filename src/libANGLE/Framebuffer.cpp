b0e76609098d4e6662c8872a2ce104c15025ba39
Charlie Lao <cclao@google.com>
2021-04-13 14:52:31 -0700

    Vulkan: Add support for FBO with unequal sized attachments
    OpenGLES 3.0 allows FBO with unequal sized attachments. This CL removes
    assertion that all attachment must have equal size from vulkan backend,
    and uses common intersect area to create VkFramebuffer object.
    
    Bug: b/181800403
    Change-Id: Icbb12a26784b184ebd91740855672013f64b889d

--- src/libANGLE/Framebuffer.cpp
+++ src/libANGLE/Framebuffer.cpp
@@ -517,6 +517,34 @@ const FramebufferAttachment *FramebufferState::getDepthStencilAttachment() const
     return nullptr;
 }
 
+const Extents FramebufferState::getCommonIntersectExtent() const
+{
+    int32_t width  = INT_MAX;
+    int32_t height = INT_MAX;
+    for (const auto &attachment : mColorAttachments)
+    {
+        if (attachment.isAttached())
+        {
+            width  = std::min(width, attachment.getSize().width);
+            height = std::min(height, attachment.getSize().height);
+        }
+    }
+
+    if (mDepthAttachment.isAttached())
+    {
+        width  = std::min(width, mDepthAttachment.getSize().width);
+        height = std::min(height, mDepthAttachment.getSize().height);
+    }
+
+    if (mStencilAttachment.isAttached())
+    {
+        width  = std::min(width, mStencilAttachment.getSize().width);
+        height = std::min(height, mStencilAttachment.getSize().height);
+    }
+
+    return Extents(width, height, 0);
+}
+
 bool FramebufferState::attachmentsHaveSameDimensions() const
 {
     Optional<Extents> attachmentSize;
@@ -673,11 +701,12 @@ Box FramebufferState::getDimensions() const
 
 Extents FramebufferState::getExtents() const
 {
-    ASSERT(attachmentsHaveSameDimensions());
+    // OpenGLES3.0 (https://www.khronos.org/registry/OpenGL/specs/es/3.0/es_spec_3.0.pdf
+    // section 4.4.4.2) allows attachments have unequal size.
     const FramebufferAttachment *first = getFirstNonNullAttachment();
     if (first)
     {
-        return first->getSize();
+        return getCommonIntersectExtent();
     }
     return Extents(getDefaultWidth(), getDefaultHeight(), 0);
 }
