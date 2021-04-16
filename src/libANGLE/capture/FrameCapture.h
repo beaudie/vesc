28a18805a8751d6e77b4e354556dc123fa92e331
Tim Van Patten <timvp@google.com>
2021-04-16 12:28:50 -0600

    Capture/Replay: Multi-Context Support
    Add support for capturing and replaying multiple contexts.
    
    Specifically, this enables capturing and replaying Asphalt 9.
    
    Bug: angleproject:5878
    Change-Id: I5bc9b7ece5388ce405ba3f9e9dc3967e78662000

--- src/libANGLE/capture/FrameCapture.h
+++ src/libANGLE/capture/FrameCapture.h
@@ -499,6 +499,7 @@ class FrameCaptureShared final : angle::NonCopyable
 
     std::vector<CallCapture> mSetupCalls;
     std::vector<CallCapture> mFrameCalls;
+    uint32_t mLastContextId;
 
     // We save one large buffer of binary data for the whole CPP replay.
     // This simplifies a lot of file management.
@@ -520,6 +521,7 @@ class FrameCaptureShared final : angle::NonCopyable
     size_t mReadBufferSize;
     HasResourceTypeMap mHasResourceType;
     BufferDataMap mBufferDataMap;
+    gl::ContextID mPresentationContextID;
 
     ResourceTracker mResourceTracker;
 
@@ -536,7 +538,6 @@ class FrameCaptureShared final : angle::NonCopyable
     ProgramSourceMap mCachedProgramSources;
 
     // Cache a shadow copy of texture level data
-    TextureLevels mCachedTextureLevels;
     TextureLevelDataMap mCachedTextureLevelData;
 
     gl::ContextID mWindowSurfaceContextID;
@@ -768,6 +769,11 @@ template <>
 void WriteParamValueReplay<ParamType::TGLubyte>(std::ostream &os,
                                                 const CallCapture &call,
                                                 GLubyte value);
+
+template <>
+void WriteParamValueReplay<ParamType::TEGLContext>(std::ostream &os,
+                                                   const CallCapture &call,
+                                                   EGLContext value);
 
 // General fallback for any unspecific type.
 template <ParamType ParamT, typename T>
