8e404f3feb921035f83a4852cf2339a1243a667c
John Plate <jplate@google.com>
2021-04-21 21:43:46 +0100

    Generate empty CL object classes
    Bug: angleproject:5886
    Change-Id: I01566f40e85bd7f5531536fdc1df42965622a939

---
+++ src/libANGLE/CLBuffer.cpp
@@ -0,0 +1,14 @@
+//
+// Copyright 2021 The ANGLE Project Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+
+// CLBuffer.cpp: Implements the cl::Buffer class.
+
+#include "libANGLE/CLBuffer.h"
+
+namespace cl
+{
+// TODO
+}  // namespace cl
---
+++ src/libANGLE/CLPipe.cpp
@@ -0,0 +1,14 @@
+//
+// Copyright 2021 The ANGLE Project Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+
+// CLPipe.cpp: Implements the cl::Pipe class.
+
+#include "libANGLE/CLPipe.h"
+
+namespace cl
+{
+// TODO
+}  // namespace cl
