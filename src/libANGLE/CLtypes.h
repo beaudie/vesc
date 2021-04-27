d2cfc2525d97ef86ff727712243b6e507ad3334b
John Plate <jplate@google.com>
2021-04-27 19:31:31 +0100

    Add support for OpenCL ICD Loader
    Bug: angleproject:5908
    Change-Id: Idafc0d15b69f9a21f2ab5e48c4c34f0dc0e0ee96

--- src/libANGLE/CLtypes.h
+++ src/libANGLE/CLtypes.h
@@ -3,15 +3,14 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 //
-
 // CLtypes.h: Defines common types for the OpenCL support in ANGLE.
 
 #ifndef LIBANGLE_CLTYPES_H_
 #define LIBANGLE_CLTYPES_H_
 
-#include "angle_cl.h"
+#include "angle_cl_no_export.h"
 
-#include <type_traits>
+#include <memory>
 
 namespace cl
 {
@@ -21,6 +20,7 @@ class Device;
 class Event;
 class Kernel;
 class Memory;
+class Object;
 class Platform;
 class Program;
 class Sampler;
