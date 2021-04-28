706218b60f2b596f0b296031578b683f072b62e3
John Plate <jplate@google.com>
2021-04-28 18:26:00 +0100

    Add loader for CL pass-through back end
    Bug: angleproject:5904
    Change-Id: If4960f3150f6bbc85a30f0f6ac5c9e668e6ff756

--- src/libANGLE/renderer/cl/CLPlatformCL.cpp
+++ src/libANGLE/renderer/cl/CLPlatformCL.cpp
@@ -9,11 +9,233 @@
 
 #include "libANGLE/renderer/cl/CLPlatformCL.h"
 
+#include "libANGLE/CLPlatform.h"
+#include "libANGLE/Debug.h"
+
+#include "anglebase/no_destructor.h"
+#include "common/angle_version.h"
+
+extern "C" {
+#include "icd.h"
+}  // extern "C"
+
+#include <cstdlib>
+#include <unordered_set>
+#include <vector>
+
 namespace rx
 {
 
-CLPlatformCL::CLPlatformCL() {}
+namespace
+{
+using ExtensionSet = std::unordered_set<std::string>;
 
-CLPlatformCL::~CLPlatformCL() {}
+const ExtensionSet &GetSupportedExternsions()
+{
+    static angle::base::NoDestructor<ExtensionSet> sExtensions(
+        {"cl_khr_extended_versioning", "cl_khr_icd"});
+    return *sExtensions;
+}
+}  // namespace
+
+CLPlatformCL::~CLPlatformCL() = default;
+
+CLPlatformCL::ImplList CLPlatformCL::GetPlatforms(bool isIcd)
+{
+    // Initialize Khronos OpenCL ICD Loader
+    if (khrIcdVendors == nullptr)
+    {
+        // Store ANGLE OpenCL ICD library to make sure the ICD Loader will skip it
+        khrIcdVendors          = new KHRicdVendor{};
+        khrIcdVendors->library = khrIcdOsLibraryLoad(ANGLE_OPENCL_ICD_LIB_NAME);
+        if (khrIcdVendors->library != nullptr)
+        {
+            khrIcdInitialize();
+            khrIcdOsLibraryUnload(khrIcdVendors->library);
+            khrIcdVendors->library = nullptr;
+        }
+    }
+
+    // Try to create pass-through platform for each found ICD platform
+    ImplList implList;
+    for (auto *vendorIt = khrIcdVendors; vendorIt != nullptr; vendorIt = vendorIt->next)
+    {
+        if (vendorIt->platform != nullptr)
+        {
+            auto impl = Create(vendorIt->platform);
+            if (impl)
+            {
+                implList.emplace_back(std::move(impl));
+            }
+        }
+    }
+    return implList;
+}
+
+CLPlatformCL::CLPlatformCL(cl_platform_id platform,
+                           std::string &&profile,
+                           std::string &&versionString,
+                           cl_version version,
+                           std::string &&name,
+                           std::string &&extensions,
+                           ExtensionList &&extensionList,
+                           cl_ulong hostTimerRes)
+    : mPlatform(platform),
+      mProfile(std::move(profile)),
+      mVersionString(std::move(versionString)),
+      mName(std::move(name)),
+      mExtensions(std::move(extensions)),
+      mExtensionList(std::move(extensionList)),
+      mVersion(version),
+      mHostTimerRes(hostTimerRes)
+{}
+
+#define GET_INFO(info, size, param, size_ret) \
+    result = platform->getDispatch().clGetPlatformInfo(platform, info, size, param, size_ret)
+
+#define TRY_GET_INFO(info, size, param, size_ret)        \
+    do                                                   \
+    {                                                    \
+        GET_INFO(info, size, param, size_ret);           \
+        if (result != CL_SUCCESS)                        \
+        {                                                \
+            ERR() << "Failed to query CL platform info"; \
+            return std::unique_ptr<CLPlatformCL>();      \
+        }                                                \
+    } while (0)
+
+std::unique_ptr<CLPlatformCL> CLPlatformCL::Create(cl_platform_id platform)
+{
+    cl_int result    = 0;
+    size_t paramSize = 0u;
+    std::vector<std::string::value_type> param;
+
+    // Skip ANGLE CL implementation to prevent passthrough loop
+    TRY_GET_INFO(CL_PLATFORM_VENDOR, 0u, nullptr, &paramSize);
+    param.resize(paramSize, '\0');
+    TRY_GET_INFO(CL_PLATFORM_VENDOR, paramSize, param.data(), nullptr);
+    if (std::string(param.data()).compare(cl::Platform::GetVendor()) == 0)
+    {
+        ERR() << "Tried to create CL pass-through back end for ANGLE library";
+        return std::unique_ptr<CLPlatformCL>();
+    }
+
+    // Skip platform if it is not ICD compatible
+    TRY_GET_INFO(CL_PLATFORM_EXTENSIONS, 0u, nullptr, &paramSize);
+    param.resize(paramSize, '\0');
+    TRY_GET_INFO(CL_PLATFORM_EXTENSIONS, paramSize, param.data(), nullptr);
+    std::string extensions(param.data());
+    if (extensions.find("cl_khr_icd") == std::string::npos)
+    {
+        WARN() << "CL platform is not ICD compatible";
+        return std::unique_ptr<CLPlatformCL>();
+    }
+
+    // Filter out extensions which are not (yet) supported to be passed through
+    if (!extensions.empty())
+    {
+        auto &supported                 = GetSupportedExternsions();
+        std::string::size_type extStart = 0u;
+        do
+        {
+            const std::string::size_type spacePos = extensions.find(' ', extStart);
+            const bool foundSpace                 = spacePos != std::string::npos;
+            const std::string::size_type length =
+                (foundSpace ? spacePos : extensions.length()) - extStart;
+            if (supported.find(extensions.substr(extStart, length)) != supported.cend())
+            {
+                extStart = foundSpace && spacePos + 1u < extensions.length() ? spacePos + 1u
+                                                                             : std::string::npos;
+            }
+            else
+            {
+                extensions.erase(extStart, length + (foundSpace ? 1u : 0u));
+                if (extStart >= extensions.length())
+                {
+                    extStart = std::string::npos;
+                }
+            }
+        } while (extStart != std::string::npos);
+        while (!extensions.empty() && extensions.back() == ' ')
+        {
+            extensions.pop_back();
+        }
+    }
+
+    // Fetch common platform info
+    TRY_GET_INFO(CL_PLATFORM_PROFILE, 0u, nullptr, &paramSize);
+    param.resize(paramSize, '\0');
+    TRY_GET_INFO(CL_PLATFORM_PROFILE, paramSize, param.data(), nullptr);
+    std::string profile(param.data());
+
+    TRY_GET_INFO(CL_PLATFORM_VERSION, 0u, nullptr, &paramSize);
+    param.resize(paramSize, '\0');
+    TRY_GET_INFO(CL_PLATFORM_VERSION, paramSize, param.data(), nullptr);
+    std::string versionStr(param.data());
+    versionStr += " (ANGLE " ANGLE_VERSION_STRING ")";
+
+    const auto spacePos = versionStr.find(' ');
+    const auto dotPos   = versionStr.find('.');
+    if (spacePos == std::string::npos || dotPos == std::string::npos)
+    {
+        ERR() << "Failed to extract version from OpenCL version string: " << versionStr;
+        return std::unique_ptr<CLPlatformCL>();
+    }
+    const auto major = static_cast<cl_uint>(std::strtol(&versionStr[spacePos + 1u], nullptr, 10));
+    const auto minor = static_cast<cl_uint>(std::strtol(&versionStr[dotPos + 1u], nullptr, 10));
+    if (major == 0)
+    {
+        ERR() << "Failed to extract version from OpenCL version string: " << versionStr;
+        return std::unique_ptr<CLPlatformCL>();
+    }
+
+    cl_version version = 0u;
+    GET_INFO(CL_PLATFORM_NUMERIC_VERSION, sizeof(version), &version, nullptr);
+    if (result != CL_SUCCESS)
+    {
+        version = CL_MAKE_VERSION(major, minor, 0);
+    }
+    else if (CL_VERSION_MAJOR(version) != major || CL_VERSION_MINOR(version) != minor)
+    {
+        WARN() << "CL_PLATFORM_NUMERIC_VERSION = " << CL_VERSION_MAJOR(version) << '.'
+               << CL_VERSION_MINOR(version) << " does not match version string: " << versionStr;
+    }
+
+    TRY_GET_INFO(CL_PLATFORM_NAME, 0u, nullptr, &paramSize);
+    param.resize(paramSize, '\0');
+    TRY_GET_INFO(CL_PLATFORM_NAME, paramSize, param.data(), nullptr);
+    std::string name("ANGLE pass-through -> ");
+    name += param.data();
+
+    rx::CLPlatformImpl::ExtensionList extList;
+    GET_INFO(CL_PLATFORM_EXTENSIONS_WITH_VERSION, 0u, nullptr, &paramSize);
+    if (result == CL_SUCCESS)
+    {
+        extList.resize(paramSize);
+        TRY_GET_INFO(CL_PLATFORM_EXTENSIONS_WITH_VERSION, paramSize, extList.data(), nullptr);
+
+        // Filter out extensions which are not (yet) supported to be passed through
+        auto &supported = GetSupportedExternsions();
+        auto extIt      = extList.cbegin();
+        while (extIt != extList.cend())
+        {
+            if (supported.find(extIt->name) != supported.cend())
+            {
+                ++extIt;
+            }
+            else
+            {
+                extIt = extList.erase(extIt);
+            }
+        }
+    }
+
+    cl_ulong hostTimerRes = 0u;
+    GET_INFO(CL_PLATFORM_HOST_TIMER_RESOLUTION, sizeof(hostTimerRes), &hostTimerRes, nullptr);
+
+    return std::unique_ptr<CLPlatformCL>(
+        new CLPlatformCL(platform, std::move(profile), std::move(versionStr), version,
+                         std::move(name), std::move(extensions), std::move(extList), hostTimerRes));
+}
 
 }  // namespace rx
--- src/libANGLE/renderer/vulkan/CLPlatformVk.cpp
+++ src/libANGLE/renderer/vulkan/CLPlatformVk.cpp
@@ -9,11 +9,53 @@
 
 #include "libANGLE/renderer/vulkan/CLPlatformVk.h"
 
+#include "anglebase/no_destructor.h"
+#include "common/angle_version.h"
+
+#include <algorithm>
+
 namespace rx
 {
 
-CLPlatformVk::CLPlatformVk() {}
+namespace
+{
+std::string CreateExtensionString(const CLPlatformImpl::ExtensionList &extList)
+{
+    std::string extensions;
+    for (const auto &ext : extList)
+    {
+        extensions += ext.name;
+        extensions += ' ';
+    }
+    if (!extensions.empty())
+    {
+        extensions.pop_back();
+    }
+    return extensions;
+}
+}  // anonymous namespace
+
+CLPlatformVk::CLPlatformVk()
+    : mExtensionList({cl_name_version{CL_MAKE_VERSION(1, 0, 0), "cl_khr_icd"},
+                      cl_name_version{CL_MAKE_VERSION(1, 0, 0), "cl_khr_extended_versioning"}}),
+      mExtensions(CreateExtensionString(mExtensionList))
+{}
 
 CLPlatformVk::~CLPlatformVk() = default;
+
+CLPlatformVk::ImplList CLPlatformVk::GetPlatforms()
+{
+    ImplList implList;
+    implList.emplace_back(new CLPlatformVk);
+    return implList;
+}
+
+const std::string &CLPlatformVk::GetVersionString()
+{
+    static const angle::base::NoDestructor<const std::string> sVersion(
+        "OpenCL " + std::to_string(CL_VERSION_MAJOR(GetVersion())) + "." +
+        std::to_string(CL_VERSION_MINOR(GetVersion())) + " ANGLE " ANGLE_VERSION_STRING);
+    return *sVersion;
+}
 
 }  // namespace rx
