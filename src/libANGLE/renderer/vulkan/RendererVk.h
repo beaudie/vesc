ea418647d26fb8bdaaa2efb82234c90bfef38d71
Mohan Maiya <m.maiya@samsung.com>
2021-05-05 17:02:43 -0700

    Vulkan: Add support for EXT_texture_border_clamp
    Add support for GL_EXT_texture_border_clamp. This is implemented by
    using VK_EXT_custom_border_color.
    
    Bug: angleproject:3577
    Test: dEQP-GLES31.functional.texture.border_clamp*
    Change-Id: Ie9fa1eb5dd03b997b5ae182787641a53080a9e51

--- src/libANGLE/renderer/vulkan/RendererVk.h
+++ src/libANGLE/renderer/vulkan/RendererVk.h
@@ -449,6 +449,7 @@ class RendererVk : angle::NonCopyable
     VkPhysicalDeviceMultisampledRenderToSingleSampledFeaturesEXT
         mMultisampledRenderToSingleSampledFeatures;
     VkPhysicalDeviceDriverPropertiesKHR mDriverProperties;
+    VkPhysicalDeviceCustomBorderColorFeaturesEXT mCustomBorderColorFeatures;
     VkExternalFenceProperties mExternalFenceProperties;
     VkExternalSemaphoreProperties mExternalSemaphoreProperties;
     VkPhysicalDeviceSamplerYcbcrConversionFeatures mSamplerYcbcrConversionFeatures;
