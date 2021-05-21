655ca8fa4ab66b912f3a683a1e22ba159ddd8296
Geoff Lang <geofflang@google.com>
2021-05-21 15:07:44 -0400

    GL: Skip redundant flushes.
    Skip flushes when there is no work submitted since the last
    flush.
    
    Bug: chromium:1181068
    Change-Id: Ie41dc4b7f214f7d7d79176a8ab7eece2ba40d288

--- src/libANGLE/renderer/gl/RendererGL.cpp
+++ src/libANGLE/renderer/gl/RendererGL.cpp
@@ -39,6 +39,8 @@
 #include "libANGLE/renderer/gl/renderergl_utils.h"
 #include "libANGLE/renderer/renderer_utils.h"
 
+#include <iostream>
+
 namespace
 {
 
@@ -215,8 +217,15 @@ RendererGL::~RendererGL()
 
 angle::Result RendererGL::flush()
 {
+    if (!mWorkDoneSinceLastFlush && !mNeedsFlushBeforeDeleteTextures)
+    {
+        std::cerr << "RendererGL::flush: Skipping redundant flush." << std::endl;
+        return angle::Result::Continue;
+    }
+
     mFunctions->flush();
     mNeedsFlushBeforeDeleteTextures = false;
+    mWorkDoneSinceLastFlush         = false;
     return angle::Result::Continue;
 }
 
@@ -229,6 +238,7 @@ angle::Result RendererGL::finish()
 
     mFunctions->finish();
     mNeedsFlushBeforeDeleteTextures = false;
+    mWorkDoneSinceLastFlush         = false;
 
     if (mFeatures.finishDoesNotCauseQueriesToBeAvailable.enabled && mUseDebugOutput)
     {
@@ -335,23 +345,27 @@ angle::Result RendererGL::dispatchCompute(const gl::Context *context,
                                           GLuint numGroupsZ)
 {
     mFunctions->dispatchCompute(numGroupsX, numGroupsY, numGroupsZ);
+    mWorkDoneSinceLastFlush = true;
     return angle::Result::Continue;
 }
 
 angle::Result RendererGL::dispatchComputeIndirect(const gl::Context *context, GLintptr indirect)
 {
     mFunctions->dispatchComputeIndirect(indirect);
+    mWorkDoneSinceLastFlush = true;
     return angle::Result::Continue;
 }
 
 angle::Result RendererGL::memoryBarrier(GLbitfield barriers)
 {
     mFunctions->memoryBarrier(barriers);
+    mWorkDoneSinceLastFlush = true;
     return angle::Result::Continue;
 }
 angle::Result RendererGL::memoryBarrierByRegion(GLbitfield barriers)
 {
     mFunctions->memoryBarrierByRegion(barriers);
+    mWorkDoneSinceLastFlush = true;
     return angle::Result::Continue;
 }
 
@@ -429,6 +443,11 @@ void RendererGL::setMaxShaderCompilerThreads(GLuint count)
 void RendererGL::setNeedsFlushBeforeDeleteTextures()
 {
     mNeedsFlushBeforeDeleteTextures = true;
+}
+
+void RendererGL::markWorkSubmitted()
+{
+    mWorkDoneSinceLastFlush = true;
 }
 
 void RendererGL::flushIfNecessaryBeforeDeleteTextures()
