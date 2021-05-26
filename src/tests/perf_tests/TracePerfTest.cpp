57bc9f826061d9a3b9c1946e7a8ba6b758198e50
Cody Northrop <cnorthrop@google.com>
2021-05-26 15:07:54 -0600

    Tests: Add Genshin Impact trace
    Test: angle_perftests --gtest_filter="*genshin_impact*"
    Bug: b/181794064
    Change-Id: Ic0debb1e284d672b8ab179ac24cd711c9e5d7e9d

--- src/tests/perf_tests/TracePerfTest.cpp
+++ src/tests/perf_tests/TracePerfTest.cpp
@@ -966,6 +966,11 @@ TracePerfTest::TracePerfTest()
         }
     }
 
+    if (param.testID == RestrictedTraceID::genshin_impact)
+    {
+        addExtensionPrerequisite("GL_KHR_texture_compression_astc_ldr");
+    }
+
     // We already swap in TracePerfTest::drawBenchmark, no need to swap again in the harness.
     disableTestHarnessSwap();
 
