09da637bda4d742143da4ad4785ba3b6cc053ac3
Alexis Hetu <sugoi@google.com>
2021-05-26 12:30:10 -0400

    Mute MSAN false positive
    MSAN currently detects false positives on global/static
    thread_local variables which are created at library load
    time. This CL mutes the false positive in order to unblock
    running blink_web_tests with SwANGLE on MSAN bots.
    
    Bug: chromium:1211047
    Change-Id: I7cd103d470177d783df7f73ca6ceeedc0774b163

--- src/libGLESv2/global_state.cpp
+++ src/libGLESv2/global_state.cpp
@@ -87,7 +87,12 @@ void SetGlobalLastContext(gl::Context *context)
     g_LastContext = context;
 }
 
-Thread *GetCurrentThread()
+// This function causes an MSAN false positive, which is muted. See https://crbug.com/1211047
+#if defined(__clang__) && defined(MEMORY_SANITIZER)
+__attribute__((no_sanitize_memory))
+#endif
+Thread *
+GetCurrentThread()
 {
     Thread *current = gCurrentThread;
     return (current ? current : AllocateCurrentThread());
