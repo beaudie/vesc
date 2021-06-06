69c4dbac510a5d5c956087b6231daa5e050fa94e
Mohan Maiya <m.maiya@samsung.com>
2021-06-06 11:36:49 -0700

    D3D: Update format table with new YUV format
    Is currently mapped to angle::FormatID::NONE
    
    Bug: angleproject:5773
    Change-Id: I0cba77158ef06bf39fbb30702e0fa91a37f660a9

--- src/libANGLE/renderer/d3d/d3d11/texture_format_table_autogen.cpp
+++ src/libANGLE/renderer/d3d/d3d11/texture_format_table_autogen.cpp
@@ -1608,6 +1608,20 @@ const Format &Format::Get(GLenum internalFormat, const Renderer11DeviceCaps &dev
                                          Initialize4ComponentData<GLubyte, 0x00, 0x00, 0x00, 0xFF>);
             return info;
         }
+        case GL_G8_B8R8_2PLANE_420_UNORM_ANGLEX:
+        {
+            static constexpr Format info(GL_G8_B8R8_2PLANE_420_UNORM_ANGLEX,
+                                         angle::FormatID::NONE,
+                                         DXGI_FORMAT_UNKNOWN,
+                                         DXGI_FORMAT_UNKNOWN,
+                                         DXGI_FORMAT_UNKNOWN,
+                                         DXGI_FORMAT_UNKNOWN,
+                                         DXGI_FORMAT_UNKNOWN,
+                                         DXGI_FORMAT_UNKNOWN,
+                                         GL_NONE,
+                                         nullptr);
+            return info;
+        }
         case GL_LUMINANCE16F_EXT:
         {
             static constexpr Format info(GL_LUMINANCE16F_EXT,
