dd09b5fd28a17411fb5a05e0e8000a32f0c1d048
Mohan Maiya <m.maiya@samsung.com>
2021-06-08 13:11:54 -0700

    Add YUV format support to frontend
    1. Add new YUV format enums to angleutils.h
    2. Add G8_B8R8_2PLANE_420_UNORM_ANGLEX format to InternalFormatInfoMap
    
    Bug: angleproject:5773
    Change-Id: Iaad4043f803c2702a5d41d3b9dc3a00c5f6aed73

--- src/libANGLE/Caps.cpp
+++ src/libANGLE/Caps.cpp
@@ -1085,6 +1085,7 @@ const ExtensionInfoMap &GetExtensionInfoMap()
         map["GL_ANGLE_get_serialized_context_string"] = esOnlyExtension(&Extensions::getSerializedContextStringANGLE);
         map["GL_EXT_primitive_bounding_box"] = esOnlyExtension(&Extensions::primitiveBoundingBoxEXT);
         map["GL_ANGLE_relaxed_vertex_attribute_type"] = esOnlyExtension(&Extensions::relaxedVertexAttributeTypeANGLE);
+        map["GL_ANGLE_texture_external_yuv_sampling"] = enableableExtension(&Extensions::textureExternalYuvSamplingANGLE);
         // GLES1 extensions
         map["GL_OES_point_size_array"] = enableableExtension(&Extensions::pointSizeArrayOES);
         map["GL_OES_texture_cube_map"] = enableableExtension(&Extensions::textureCubeMapOES);
