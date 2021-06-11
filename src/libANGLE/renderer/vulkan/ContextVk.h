7a0d1c3677407d9c0026cf6636275c0f865ccd58
Cody Northrop <cnorthrop@google.com>
2021-06-10 21:27:01 -0600

    WIP: Vulkan: Add dynamic viewport/scissor support
    Based on http://crrev/c/1316888
    
    Bug: None
    Change-Id: Ib7415be1128f8fedae4a7ca72e067b2815201223

--- src/libANGLE/renderer/vulkan/ContextVk.h
+++ src/libANGLE/renderer/vulkan/ContextVk.h
@@ -365,6 +365,7 @@ class ContextVk : public ContextImpl, public vk::Context, public MultisampleText
     const VkClearValue &getClearColorValue() const;
     const VkClearValue &getClearDepthStencilValue() const;
     gl::BlendStateExt::ColorMaskStorage::Type getClearColorMasks() const;
+    const VkRect2D &getScissor() const { return mScissor; }
     angle::Result getIncompleteTexture(const gl::Context *context,
                                        gl::TextureType type,
                                        gl::SamplerFormat format,
@@ -651,6 +652,9 @@ class ContextVk : public ContextImpl, public vk::Context, public MultisampleText
         DIRTY_BIT_TRANSFORM_FEEDBACK_RESUME,
         DIRTY_BIT_DESCRIPTOR_SETS,
         DIRTY_BIT_FRAMEBUFFER_FETCH_BARRIER,
+        // Dynamic viewport/scissor
+        DIRTY_BIT_VIEWPORT,
+        DIRTY_BIT_SCISSOR,
         DIRTY_BIT_MAX,
     };
 
@@ -779,6 +783,8 @@ class ContextVk : public ContextImpl, public vk::Context, public MultisampleText
     {
         // Note: DIRTY_BIT_PIPELINE_BINDING will be automatically set if pipeline bind is necessary.
         mGraphicsDirtyBits.set(DIRTY_BIT_PIPELINE_DESC);
+        mGraphicsDirtyBits.set(DIRTY_BIT_VIEWPORT);
+        mGraphicsDirtyBits.set(DIRTY_BIT_SCISSOR);
     }
 
     ANGLE_INLINE void invalidateCurrentComputePipeline()
@@ -792,6 +798,7 @@ class ContextVk : public ContextImpl, public vk::Context, public MultisampleText
     angle::Result invalidateCurrentShaderResources();
     void invalidateGraphicsDriverUniforms();
     void invalidateDriverUniforms();
+    void invalidateCurrentPipeline();
 
     angle::Result handleNoopDrawEvent() override;
 
@@ -832,6 +839,10 @@ class ContextVk : public ContextImpl, public vk::Context, public MultisampleText
                                                              DirtyBits dirtyBitMask);
     angle::Result handleDirtyGraphicsDescriptorSets(DirtyBits::Iterator *dirtyBitsIterator,
                                                     DirtyBits dirtyBitMask);
+    angle::Result handleDirtyGraphicsViewport(DirtyBits::Iterator *dirtyBitsIterator,
+                                              DirtyBits dirtyBitMask);
+    angle::Result handleDirtyGraphicsScissor(DirtyBits::Iterator *dirtyBitsIterator,
+                                             DirtyBits dirtyBitMask);
 
     // Handlers for compute pipeline dirty bits.
     angle::Result handleDirtyComputeMemoryBarrier();
@@ -854,6 +865,9 @@ class ContextVk : public ContextImpl, public vk::Context, public MultisampleText
                                               VkPipelineBindPoint bindPoint,
                                               DriverUniformsDescriptorSet *driverUniforms);
     angle::Result handleDirtyDescriptorSetsImpl(vk::CommandBuffer *commandBuffer);
+    angle::Result handleDirtyViewportImpl(vk::CommandBuffer *commandBuffer);
+    angle::Result handleDirtyScissorImpl(vk::CommandBuffer *commandBuffer);
+
     angle::Result allocateDriverUniforms(size_t driverUniformsSize,
                                          DriverUniformsDescriptorSet *driverUniforms,
                                          uint8_t **ptrOut,
@@ -1132,6 +1146,10 @@ class ContextVk : public ContextImpl, public vk::Context, public MultisampleText
 
     // Record GL API calls for debuggers
     std::vector<std::string> mEventLog;
+
+    // Viewport and scissor are handled as dynamic state.
+    VkViewport mViewport;
+    VkRect2D mScissor;
 };
 
 ANGLE_INLINE angle::Result ContextVk::endRenderPassIfTransformFeedbackBuffer(
