b21207f3bbfc33dd98a85e7702946689d6db0fed
Geoff Lang <geofflang@chromium.org>
2021-06-17 12:50:39 -0400

    Vulkan: Prefer the local vulkan loader over the system one.
    Load the Vulkan loader ourselves and give vkGetInstanceProcAddr to
    volk. This allows us to always prefer loading from the current module
    directory instead of using the platform-specific ordering.
    
    Refactor angle::Library loading to use ModuleDir instead of
    ApplicationDir.
    
    Bug: chromium:1219969
    Change-Id: I4981f367c4cc34b80c6465534c0dcb19b2bafafb

---
+++ src/common/vulkan/vulkan_library.cpp
@@ -0,0 +1,56 @@
+//
+// Copyright 2021 The ANGLE Project Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+// vulkan_library.cpp:
+//    Helper functions for the loading Vulkan libraries.
+//
+
+#include "common/vulkan/vulkan_library.h"
+
+#include "common/system_utils.h"
+
+namespace angle
+{
+namespace vk
+{
+std::unique_ptr<angle::Library> OpenLibVulkan()
+{
+    constexpr const char *kLibVulkanNames[] = {
+#if defined(ANGLE_PLATFORM_WINDOWS)
+        "vulkan-1.dll",
+#elif defined(ANGLE_PLATFORM_APPLE)
+        "libvulkan.dylib",
+        "libvulkan.1.dylib",
+        "libMoltenVK.dylib"
+#else
+        "libvulkan.so.1",
+        "libvulkan.so",
+#endif
+    };
+
+    // Prefer local vulkan loaders if they exist
+    constexpr angle::SearchType kSearchTypes[] = {
+        angle::SearchType::ModuleDir,
+        angle::SearchType::SystemDir,
+        angle::SearchType::Default,
+    };
+
+    for (angle::SearchType searchType : kSearchTypes)
+    {
+        for (const char *libraryName : kLibVulkanNames)
+        {
+            std::unique_ptr<angle::Library> library(
+                angle::OpenSharedLibraryWithExtension(libraryName, searchType));
+            if (library)
+            {
+                return library;
+            }
+        }
+    }
+
+    return nullptr;
+}
+}  // namespace vk
+}  // namespace angle
