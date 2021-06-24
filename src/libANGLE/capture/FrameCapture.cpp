google/angle

commit cb4f8a79a4def3dd8fd9f941d6674939e1731860

Capture: Add a specialization for TGLeglImageOES
When writing out the parameter TGLeglImageOES an additional cast is
needed.

Bug: angleproject:6036
Change-Id: I7470a58a6ae17b0185d0c2e3fe1184b698f3efaf
Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/2944952
Reviewed-by: Cody Northrop <cnorthrop@google.com>
Reviewed-by: Jamie Madill <jmadill@chromium.org>
Commit-Queue: Gert Wollny <gert.wollny@collabora.com>

src/libANGLE/capture/FrameCapture.cpp
@@ -5575,4 +5575,13 @@ void WriteParamValueReplay<ParamType::TUniformBlockIndex>(std::ostream &os,
     os << "gUniformBlockIndexes[gShaderProgramMap[" << programID.value << "]][" << value.value
        << "]";
 }
+
+template <>
+void WriteParamValueReplay<ParamType::TGLeglImageOES>(std::ostream &os,
+                                                      const CallCapture &call,
+                                                      GLeglImageOES value)
+{
+    os << "reinterpret_cast<EGLImageKHR>(" << value << ")";
+}
+
 }  // namespace angle

src/libANGLE/capture/FrameCapture.h
@@ -664,6 +664,11 @@ void WriteParamValueReplay<ParamType::TGLsync>(std::ostream &os,
                                                const CallCapture &call,
                                                GLsync value);
 
+template <>
+void WriteParamValueReplay<ParamType::TGLeglImageOES>(std::ostream &os,
+                                                      const CallCapture &call,
+                                                      GLeglImageOES value);
+
 // General fallback for any unspecific type.
 template <ParamType ParamT, typename T>
 void WriteParamValueReplay(std::ostream &os, const CallCapture &call, T value)

beaudie/vesc

commit 48218230acef4ea4b52bd721d49f064fb39a23ae

Capture/Replay: Make sure EGLImageKHR is a parsable number
For some reason on win-trace-rel the pointer value us written
as a hex number but without the leading "0x", so the number gets
interpreted as octal, and compilation fails.

Fixes: cb4f8a79a4def3dd8fd9f941d6674939e1731860
    Capture: Add a specialization for TGLeglImageOES

Bug: angleproject:6036
Change-Id: I949ace542a0ad257b47fbe539893a56c1c32b0b3

src/libANGLE/capture/FrameCapture.cpp
@@ -5613,7 +5613,8 @@ void WriteParamValueReplay<ParamType::TGLeglImageOES>(std::ostream &os,
                                                       const CallCapture &call,
                                                       GLeglImageOES value)
 {
-    os << "reinterpret_cast<EGLImageKHR>(" << value << ")";
+    uint64_t pointerValue = reinterpret_cast<uint64_t>(value);
+    os << "reinterpret_cast<EGLImageKHR>(" << pointerValue << "ull)";
 }
 
 template <>