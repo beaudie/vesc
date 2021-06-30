940e960366060cb8ea561b83c6dca1ec57c9ac54
Cody Northrop <cnorthrop@google.com>
2021-06-29 19:19:49 -0600

    Tests: Add Toon Blast trace
    Test: angle_project --gtest_filter="*toon_blast*"
    Bug: b/192408760
    Bug: angleproject:6121
    Change-Id: Id301c48921a24164d4dccd7d53c1a4e51a0795ce

--- src/tests/restricted_traces/restricted_traces_autogen.cpp
+++ src/tests/restricted_traces/restricted_traces_autogen.cpp
@@ -99,6 +99,7 @@
 #include "talking_tom_hero_dash/talking_tom_hero_dash_capture_context1.h"
 #include "temple_run_2/temple_run_2_capture_context1.h"
 #include "temple_run_300/temple_run_300_capture_context1.h"
+#include "toon_blast/toon_blast_capture_context2.h"
 #include "trex_200/trex_200_capture_context1.h"
 #include "whatsapp/whatsapp_capture_context1.h"
 #include "world_of_tanks_blitz/world_of_tanks_blitz_capture_context1.h"
@@ -539,6 +540,10 @@ constexpr angle::PackedEnumMap<RestrictedTraceID, TraceInfo> kTraceInfos = {
       temple_run_300::kReplayContextClientMinorVersion, temple_run_300::kReplayFrameStart,
       temple_run_300::kReplayFrameEnd, temple_run_300::kReplayDrawSurfaceWidth,
       temple_run_300::kReplayDrawSurfaceHeight, "temple_run_300"}},
+    {RestrictedTraceID::toon_blast,
+     {toon_blast::kReplayContextClientMajorVersion, toon_blast::kReplayContextClientMinorVersion,
+      toon_blast::kReplayFrameStart, toon_blast::kReplayFrameEnd,
+      toon_blast::kReplayDrawSurfaceWidth, toon_blast::kReplayDrawSurfaceHeight, "toon_blast"}},
     {RestrictedTraceID::trex_200,
      {trex_200::kReplayContextClientMajorVersion, trex_200::kReplayContextClientMinorVersion,
       trex_200::kReplayFrameStart, trex_200::kReplayFrameEnd, trex_200::kReplayDrawSurfaceWidth,
