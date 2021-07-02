be8f17174968178e0e08360d7f9dee379c1ef8c0
Jamie Madill <jmadill@chromium.org>
2021-07-02 15:24:32 -0400

    Capture/Replay: Use flat map for uniform locations.
    Similarly to prior changes that change the resource map lookup, this
    change switches the uniform lookup from an unordered map to a plain
    pointer lookup. It internally uses an unordered_map of vectors, and
    caches each vector base pointer whenever a program updates uniform
    locations.
    
    Testing with Manhattan 3.0, this CL decreases the binary size in a
    Windows/Release build from 6.125MB to 4.413 MB. This is about a 28%
    decrease. This allows for longer traces and also reduced replay
    overhead.
    
    Bug: angleproject:5133
    Change-Id: I0957bc1075ec87ed9dca4a2525f27149c61cbe12

--- src/libANGLE/capture/FrameCapture.cpp
+++ src/libANGLE/capture/FrameCapture.cpp
@@ -1605,7 +1605,7 @@ void CaptureUpdateUniformLocations(const gl::Program *program, std::vector<CallC
         params.addParam(std::move(nameParam));
         params.addValueParam("location", ParamType::TGLint, location);
         params.addValueParam("count", ParamType::TGLint, static_cast<GLint>(count));
-        callsOut->emplace_back("UpdateUniformLocation", std::move(params));
+        callsOut->emplace_back("UpdateUniformLocation2", std::move(params));
     }
 }
 
@@ -1652,7 +1652,7 @@ void CaptureDeleteUniformLocations(gl::ShaderProgramID program, std::vector<Call
 {
     ParamBuffer params;
     params.addValueParam("program", ParamType::TShaderProgramID, program);
-    callsOut->emplace_back("DeleteUniformLocations", std::move(params));
+    callsOut->emplace_back("DeleteUniformLocations2", std::move(params));
 }
 
 void MaybeCaptureUpdateResourceIDs(std::vector<CallCapture> *callsOut)
@@ -6485,7 +6485,7 @@ void WriteParamValueReplay<ParamType::TUniformLocation>(std::ostream &os,
         return;
     }
 
-    os << "gUniformLocations[";
+    os << "gUniformLocations2[";
 
     // Find the program from the call parameters.
     gl::ShaderProgramID programID;
