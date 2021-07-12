f65ea198f4cef285c374be749726f2c5d8b66879
Gert Wollny <gert.wollny@collabora.com>
2021-07-12 14:58:01 +0200

    Capture/Replay: rework test runner to use expectations
    This CL changes the test runner to use a expectation
    file for all the tests including failing tests.
    
    The expectation file is also used to disable running certain
    tests. e.g. because they are known to crash or result in
    compilation failures, which then take down all the test
    in the corresponding bactch.
    
    The capture_replay_tests.py script will report failure if the
    test result doesn't meet the expectation. In order to keep
    the current behaviour to run subsets of tests in tact, the
    test filter is also applied to the expectation.
    
    This CL also adds the expectation for ES2_Vulkan_SwiftShader.
    
    Bug: angleproject:6166
    Change-Id: Ica05303e6ae3b7c6d2b6749c2c9c6593b659cc1a

--- src/tests/capture_replay_tests.py
+++ src/tests/capture_replay_tests.py
@@ -303,42 +303,73 @@ def GetTestsListForFilter(args, test_path, filter):
     return subprocess.check_output(cmd, text=True)
 
 
-def GetSkippedTestPatterns():
-    skipped_test_patterns = []
-    test_expectations_filename = "capture_replay_expectations.txt"
-    test_expectations_path = os.path.join(REPLAY_SAMPLE_FOLDER, test_expectations_filename)
-    with open(test_expectations_path, "rt") as f:
-        for line in f:
-            l = line.strip()
-            if l != "" and not l.startswith("#"):
-                skipped_test_patterns.append(l)
-    return skipped_test_patterns
+class TestExpectation():
+    # tests that must not be run as list
+    disabled_tests = []
+
+    # test expectations for tests that do not pass
+    non_pass_results = {}
+
+    # yapf: disable
+    # we want each pair on one line
+    result_map = { "FAIL" : "Fail",
+                   "TIMEOUT" : "Timeout",
+                   "CRASHED" : "Crashed",
+                   "COMPILE_FAILED" : "CompileFailed",
+                   "SKIP" : "Skipped",
+                   "PASS" : "Pass" }
+    # yapf: enable
+
+    def __init__(self):
+        expected_results_filename = "capture_replay_expectations.txt"
+        expected_results_path = os.path.join(REPLAY_SAMPLE_FOLDER, expected_results_filename)
+        with open(expected_results_path, "rt") as f:
+            for line in f:
+                l = line.strip()
+                if l != "" and not l.startswith("#"):
+                    self.ReadOneExpectation(l)
+
+    def ReadOneExpectation(self, line):
+        (testpattern, result) = line.split('=')
+        (test_info_string, test_name_string) = testpattern.split(':')
+        test_name = test_name_string.strip()
+        test_info = test_info_string.strip().split()
+        for backend in test_info[1:]:
+            test_name_qualified = test_name + '/' + backend
+            result_stripped = result.strip()
+            if result_stripped == 'DISABLED':
+                self.disabled_tests.append(test_name_qualified)
+            else:
+                self.non_pass_results[test_name_qualified] = self.result_map[result_stripped]
+
+    def TestIsDisabled(self, test_name):
+        return test_name in self.disabled_tests
+
+    def Filter(self, test_list):
+        result = {}
+        for t in test_list:
+            if t in self.non_pass_results.keys():
+                result[t] = self.non_pass_results[t]
+        return result
 
 
-def ParseTestNamesFromTestList(output):
-    def SkipTest(skipped_test_patterns, test):
-        for skipped_test_pattern in skipped_test_patterns:
-            if fnmatch.fnmatch(test, skipped_test_pattern):
-                return True
-        return False
-
+def ParseTestNamesFromTestList(output, test_expectation):
     output_lines = output.splitlines()
     tests = []
-    skipped_test_patterns = GetSkippedTestPatterns()
     seen_start_of_tests = False
-    skips = 0
+    disabled = 0
     for line in output_lines:
         l = line.strip()
         if l == "Tests list:":
             seen_start_of_tests = True
         elif not seen_start_of_tests:
             pass
-        elif not SkipTest(skipped_test_patterns, l):
+        elif not test_expectation.TestIsDisabled(l):
             tests.append(l)
         else:
-            skips += 1
+            disabled += 1
 
-    info('Found %s tests and %d skipped tests.' % (len(tests), skips))
+    info('Found %s tests and %d disabled tests.' % (len(tests), disabled))
     return tests
 
 
@@ -837,7 +868,9 @@ def main(args):
         # get a list of tests
         test_path = os.path.join(capture_build_dir, args.test_suite)
         test_list = GetTestsListForFilter(args, test_path, args.gtest_filter)
-        test_names = ParseTestNamesFromTestList(test_list)
+        test_expectation = TestExpectation()
+        test_names = ParseTestNamesFromTestList(test_list, test_expectation)
+        test_expectation_for_list = test_expectation.Filter(test_names)
         # objects created by manager can be shared by multiple processes. We use it to create
         # collections that are shared by multiple processes such as job queue or result list.
         manager = multiprocessing.Manager()
@@ -910,6 +943,9 @@ def main(args):
         # print out results
         logging.info("\n\n\n")
         logging.info("Results:")
+
+        test_results = {}
+
         for test_batch_result in result_list:
             debug(str(test_batch_result))
             passed_count += len(test_batch_result.passes)
@@ -921,14 +957,33 @@ def main(args):
 
             for failed_test in test_batch_result.fails:
                 failed_tests.append(failed_test)
+                test_results[failed_test] = "Fail"
+
             for timeout_test in test_batch_result.timeouts:
                 timed_out_tests.append(timeout_test)
+                test_results[timeout_test] = "Timeout"
+
             for crashed_test in test_batch_result.crashes:
                 crashed_tests.append(crashed_test)
+                test_results[crashed_test] = "Crashed"
+
             for compile_failed_test in test_batch_result.compile_fails:
                 compile_failed_tests.append(compile_failed_test)
+                test_results[compile_failed_test] = "CompileFailed"
+
             for skipped_test in test_batch_result.skips:
                 skipped_tests.append(skipped_test)
+                test_results[skipped_test] = "Skipped"
+
+        test_result = []
+        for test, result in sorted(test_results.items()):
+            test_result.append("{} {}\n".format(test, result))
+
+        expected_result = []
+        expected_result_map = sorted(test_expectation_for_list.items())
+        for test, result in expected_result_map:
+            if test in test_names:
+                expected_result.append("{} {}\n".format(test, result))
 
         logging.info("\n\n")
         logging.info("Elapsed time: %.2lf seconds" % (end_time - start_time))
@@ -937,31 +992,23 @@ def main(args):
             % (passed_count, failed_count, crashed_count, compile_failed_count, skipped_count,
                timedout_count))
 
-        retval = EXIT_SUCCESS
+        result_diff = difflib.unified_diff(
+            expected_result,
+            test_result,
+            fromfile="expected result",
+            tofile="obtained result",
+            n=0)
 
-        if len(failed_tests):
-            logging.info("Comparison Failed tests:")
-            for failed_test in sorted(failed_tests):
-                logging.info("  " + failed_test)
-            retval = EXIT_FAILURE
-        if len(crashed_tests):
-            logging.info("Crashed tests:")
-            for crashed_test in sorted(crashed_tests):
-                logging.info("  " + crashed_test)
-            retval = EXIT_FAILURE
-        if len(compile_failed_tests):
-            logging.info("Compile failed tests:")
-            for compile_failed_test in sorted(compile_failed_tests):
-                logging.info("  " + compile_failed_test)
-            retval = EXIT_FAILURE
-        if len(skipped_tests):
-            logging.info("Skipped tests:")
-            for skipped_test in sorted(skipped_tests):
-                logging.info("  " + skipped_test)
-        if len(timed_out_tests):
-            logging.info("Timeout tests:")
-            for timeout_test in sorted(timed_out_tests):
-                logging.info("  " + timeout_test)
+        diff_lines = 0
+        for line in result_diff:
+            if line is not None:
+                logging.info(line, end="")
+                diff_lines = diff_lines + 1
+
+        if diff_lines == 0:
+            retval = EXIT_SUCCESS
+        else:
+            logging.info("Failure: Obtained results differed from expectation")
             retval = EXIT_FAILURE
 
         # delete generated folders if --keep_temp_files flag is set to false
@@ -1035,6 +1082,7 @@ if __name__ == "__main__":
         default=DEFAULT_MAX_JOBS,
         type=int,
         help='Maximum number of test processes. Default is %d.' % DEFAULT_MAX_JOBS)
+
     # TODO(jmadill): Remove this argument. http://anglebug.com/6102
     parser.add_argument('--depot-tools-path', default=None, help='Path to depot tools')
     parser.add_argument('--xvfb', action='store_true', help='Run with xvfb.')
