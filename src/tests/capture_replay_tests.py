4eee1669d3b3271166351ece8c0ae06782336a9a
Gert Wollny <gert.wollny@collabora.com>
2021-07-13 14:14:11 +0200

    Capture/Replay: Add ES1_Vulkan expectation
    Bug: angleproject:6166
    Change-Id: Iaa76bb61b1c7e7b9f0b758417114b4b82c6bc5c8

--- src/tests/capture_replay_tests.py
+++ src/tests/capture_replay_tests.py
@@ -319,7 +319,8 @@ def GetExpectedResults():
     expected_results = {}
     # yapf: disable
     # we want each backend on one line
-    backends = ['ES1_Vulkan_AsyncQueue',
+    backends = ['ES1_Vulkan',
+                'ES1_Vulkan_AsyncQueue',
                 'ES1_Vulkan_SwiftShader',
                 'ES2_Vulkan',
                 'ES2_Vulkan_AsyncQueue',
