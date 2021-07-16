c42d9ce370f5156a8670cdab78fac92b35083a6d
Charlie Lao <cclao@google.com>
2021-07-16 14:26:20 -0700

    Vulkan: Use different strategy for buffer memory allocation
    This CL uses different memory allocation strategy based on the requested
    size. If the requested size exceeds 1M, we use dedicated memory
    allocation to avoid memory waste associated with the sub-allocator.
    Otherwise we uses VMA's sub-allocator pool.  This CL creates two sets of
    customized pool so that we uses different allocation strategy for each
    set of pool: the small pool uses buddy algorithm which favors speed over
    memory and large pool uses default algorithm that favors memory saving
    over speed. This CL also replaces vmaFindMemoryTypeIndexForBufferInfo
    with vmaFindMemoryTypeIndex to avoid create and destroy VkBuffer object
    just try to find memoryTypeIndex.
    
    Bug: b/195588159
    Change-Id: I2bddbfffd77ba2ce6b9389d83a31051c4b748c4d

--- src/libANGLE/renderer/vulkan/vk_utils.h
+++ src/libANGLE/renderer/vulkan/vk_utils.h
@@ -110,9 +110,12 @@ enum class TextureDimension
 // next highest values to meet native drivers are 16 bits or 32 bits.
 constexpr uint32_t kAttributeOffsetMaxBits = 15;
 
+constexpr uint32_t kInvalidMemoryTypeIndex = UINT32_MAX;
+
 namespace vk
 {
 struct Format;
+class BufferMemory;
 
 // A packed attachment index interface with vulkan API
 class PackedAttachmentIndex final
@@ -390,14 +393,18 @@ class BufferMemory : angle::NonCopyable
     bool isExternalBuffer() const { return mClientBuffer != nullptr; }
 
     uint8_t *getMappedMemory() const { return mMappedMemory; }
-    DeviceMemory *getExternalMemoryObject() { return &mExternalMemory; }
-    Allocation *getMemoryObject() { return &mAllocation; }
+    DeviceMemory *getDeviceMemoryObject() { return &mDeviceMemory; }
+    Allocation *getAllocationObject() { return &mAllocation; }
 
   private:
     angle::Result mapImpl(ContextVk *contextVk, VkDeviceSize size);
 
-    Allocation mAllocation;        // use mAllocation if isExternalBuffer() is false
-    DeviceMemory mExternalMemory;  // use mExternalMemory if isExternalBuffer() is true
+    // mAllocation and mDeviceMemory are mutally exclusive. Only one should be used at any time.
+    // mAllocation is used when it is allocated by BufferMemoryAllocator. mDeviceMemory is used when
+    // it is allocated by calling directly into Vulkan driver. External allocations is always
+    // allocated with vulkan call directly.
+    Allocation mAllocation;
+    DeviceMemory mDeviceMemory;
 
     void *mClientBuffer;
     uint8_t *mMappedMemory;
@@ -420,7 +427,7 @@ class StagingBuffer final : angle::NonCopyable
 
   private:
     Buffer mBuffer;
-    Allocation mAllocation;
+    BufferMemory mMemory;
     size_t mSize;
 };
 
