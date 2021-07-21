8413c69b3b4507165bcd99d504a23e6854abe5dd
Brandon Schade <b.schade@samsung.com>
2021-07-21 12:42:56 -0700

    Vulkan: Add support for EXT_primitive_bounding_box
    1. Added support for EXT_primitive_bounding_box extension
    2. Renamed shader variable gl_BoundingBoxEXT[] to ANGLEBoundingBox[]
    
    Bug: angleproject:5896
    Test: dEQP-GLES31.functional.primitive_bounding_box.*
    Change-Id: I15fa9af50c6fd8e86d225670ddd8eb39f6e65d35

--- src/libANGLE/validationESEXT.cpp
+++ src/libANGLE/validationESEXT.cpp
@@ -1514,8 +1514,7 @@ bool ValidatePrimitiveBoundingBoxEXT(const Context *context,
                                      GLfloat maxZ,
                                      GLfloat maxW)
 {
-    context->validationError(GL_INVALID_OPERATION, kExtensionNotEnabled);
-    return false;
+    return true;
 }
 
 // GL_EXT_separate_shader_objects
