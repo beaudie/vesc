08a7e8c7b09aced2a091141556bc6ff502f45e1c
Lubosz Sarnecki <lubosz.sarnecki@collabora.com>
2021-08-03 15:06:37 +0200

    FrameCapture: Don't capture default drawbuffers state.
    When capturing framebuffers during mid-execution-capture the
    drawbuffers state is now compared with the default one and skipped
    when equal.
    
    This prevents capturing the unavailable glDrawBuffers call when using
    OES_framebuffer_object on GLES 1 contexts.
    
    Bug: angleproject:6181
    Change-Id: Id2076223067dc5f89df88fe6a3a48b09fa9a3f5d

--- src/libANGLE/capture/FrameCapture.cpp
+++ src/libANGLE/capture/FrameCapture.cpp
@@ -3334,9 +3334,18 @@ void CaptureMidExecutionSetup(const gl::Context *context,
             CaptureFramebufferAttachment(context, setupCalls, replayState, *stencilAttachment);
         }
 
+        gl::FramebufferState defaultFrameBufferState(
+            context->getCaps(), framebuffer->getState().id(),
+            framebuffer->getState().getFramebufferSerial());
+        const std::vector<GLenum> &defaultDrawBufferStates =
+            defaultFrameBufferState.getDrawBufferStates();
         const std::vector<GLenum> &drawBufferStates = framebuffer->getDrawBufferStates();
-        cap(CaptureDrawBuffers(replayState, true, static_cast<GLsizei>(drawBufferStates.size()),
-                               drawBufferStates.data()));
+
+        if (drawBufferStates != defaultDrawBufferStates)
+        {
+            cap(CaptureDrawBuffers(replayState, true, static_cast<GLsizei>(drawBufferStates.size()),
+                                   drawBufferStates.data()));
+        }
     }
 
     // Capture framebuffer bindings.
