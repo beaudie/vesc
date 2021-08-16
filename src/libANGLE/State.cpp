9840970f1b4975fcf1ec737e025cc7e9957d2d10
Jamie Madill <jmadill@chromium.org>
2021-08-16 08:23:00 -0400

    Capture/Replay: MEC consistency improvements.
    Fixes a few very small bugs in MEC. The current program wasn't always
    captured correctly. Also texture bindings were not always left in the
    right state. Fixes this by tracking the currently bound GL state
    between the "shared" and "aux" MEC capture functions. Also updates the
    vertex array capture to detect when to use ES 2.0 semantics. ES 3.1
    is left unimplemented as it was before.
    
    Bug: angleproject:5133
    Change-Id: I5f03b8ffbdccec2664fda4698581b166a36fabdd

--- src/libANGLE/State.cpp
+++ src/libANGLE/State.cpp
@@ -3662,6 +3662,17 @@ State::ExtendedDirtyBits State::getAndResetExtendedDirtyBits() const
     return retVal;
 }
 
+void State::initializeForCapture(const Context *context)
+{
+    mCaps       = context->getCaps();
+    mExtensions = context->getExtensions();
+
+    // This little kludge gets around the frame capture "constness". It should be safe because
+    // nothing in the context is modified in a non-compatible way during capture.
+    Context *mutableContext = const_cast<Context *>(context);
+    initialize(mutableContext);
+}
+
 constexpr State::DirtyObjectHandler State::kDirtyObjectHandlers[DIRTY_OBJECT_MAX];
 
 }  // namespace gl
