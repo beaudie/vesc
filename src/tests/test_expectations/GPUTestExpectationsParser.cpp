d5f9ccd4b628658c4622b3018f8d476a7e145c64
Shahbaz Youssefi <syoussefi@chromium.org>
2021-08-23 17:25:15 -0400

    Vulkan: SPIR-V Gen: Drop dependency to glslang
    The SPIR-V gen path is now made default.  Compilation through glslang is
    still supported for debugging, and is enabled on the GLSL* end2end tests
    for smoke testing.  On release builds, glslang is not supported.
    
    Binary size saving:
    
    - 1.3MB on Linux (SPIR-V gen itself: 240KB)
    - 730KB on Android (SPIR-V gen itself: 140KB)
    
    Perf tests:
    
    - LinkProgramBenchmark.Run/vulkan_compile_single_thread
      * Through glslang:
          wall_time: 1310679.72, mean: 1289462.31, variation: 1.06%, truncated mean: 1287033.36, variation: 0.45%
      * Direct SPIR-V Gen:
          wall_time: 245797.35, mean: 244902.21, variation: 1.29%, truncated mean: 244495.91, variation: 0.89%
    
    - LinkProgramBenchmark.Run/vulkan_compile_multi_thread
      * Through glslang:
          wall_time: 4581189.74, mean: 4566440.34, variation: 0.34%, truncated mean: 4565894.83, variation: 0.25%
      * Direct SPIR-V Gen:
          wall_time: 1168123.91, mean: 1157034.08, variation: 0.75%, truncated mean: 1158164.10, variation: 0.38%
    
    Bug: angleproject:4889
    Bug: angleproject:6210
    Change-Id: I486342702977c8114e90073b97183aba115a8b2d

--- src/tests/test_expectations/GPUTestExpectationsParser.cpp
+++ src/tests/test_expectations/GPUTestExpectationsParser.cpp
@@ -82,8 +82,6 @@ enum Token
     kConfigPreRotation90,
     kConfigPreRotation180,
     kConfigPreRotation270,
-    // SPIR-V generation
-    kConfigSPIRVGen,
     // expectation
     kExpectationPass,
     kExpectationFail,
@@ -181,7 +179,6 @@ constexpr TokenInfo kTokenData[kNumberOfTokens] = {
     {"prerotation90", GPUTestConfig::kConditionPreRotation90},
     {"prerotation180", GPUTestConfig::kConditionPreRotation180},
     {"prerotation270", GPUTestConfig::kConditionPreRotation270},
-    {"spirvgen", GPUTestConfig::kConditionSPIRVGen},
     {"pass", GPUTestConfig::kConditionNone, GPUTestExpectationsParser::kGpuTestPass},
     {"fail", GPUTestConfig::kConditionNone, GPUTestExpectationsParser::kGpuTestFail},
     {"flaky", GPUTestConfig::kConditionNone, GPUTestExpectationsParser::kGpuTestFlaky},
@@ -504,7 +501,6 @@ bool GPUTestExpectationsParser::parseLine(const GPUTestConfig *config,
             case kConfigPreRotation90:
             case kConfigPreRotation180:
             case kConfigPreRotation270:
-            case kConfigSPIRVGen:
                 // MODIFIERS, check each condition and add accordingly.
                 if (stage != kLineParserConfigs && stage != kLineParserBugID)
                 {
