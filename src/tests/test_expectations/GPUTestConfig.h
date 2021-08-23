f652976cee54529c118523d69bab0904d1e51aea
Shahbaz Youssefi <syoussefi@chromium.org>
2021-08-23 17:25:15 -0400

    Vulkan: SPIR-V Gen: Drop dependency to glslang
    The SPIR-V gen path is now made default.  Compilation through glslang is
    still supported for debugging, and is enabled on the GLSL* end2end tests
    for smoke testing.  On release builds, glslang is not supported.
    
    Binary size saving:
    
    - 1.3MB on Linux (SPIR-V gen itself: 240KB)
    - 730KB on Android (SPIR-V gen itself: 140KB)
    
    Perf tests:
    
    - LinkProgramBenchmark.Run/vulkan_compile_single_thread
      * Through glslang:
          wall_time: 1310679.72, mean: 1289462.31, variation: 1.06%, truncated mean: 1287033.36, variation: 0.45%
      * Direct SPIR-V Gen:
          wall_time: 245797.35, mean: 244902.21, variation: 1.29%, truncated mean: 244495.91, variation: 0.89%
    
    - LinkProgramBenchmark.Run/vulkan_compile_multi_thread
      * Through glslang:
          wall_time: 4581189.74, mean: 4566440.34, variation: 0.34%, truncated mean: 4565894.83, variation: 0.25%
      * Direct SPIR-V Gen:
          wall_time: 1168123.91, mean: 1157034.08, variation: 0.75%, truncated mean: 1158164.10, variation: 0.38%
    
    Bug: angleproject:4889
    Bug: angleproject:6210
    Change-Id: I486342702977c8114e90073b97183aba115a8b2d

--- src/tests/test_expectations/GPUTestConfig.h
+++ src/tests/test_expectations/GPUTestConfig.h
@@ -71,8 +71,6 @@ struct GPUTestConfig
         kConditionPreRotation90,
         kConditionPreRotation180,
         kConditionPreRotation270,
-        // TODO: remove when no longer needed.  http://anglebug.com/6210
-        kConditionSPIRVGen,
 
         kNumberOfConditions,
     };
@@ -81,7 +79,7 @@ struct GPUTestConfig
 
     GPUTestConfig();
     GPUTestConfig(bool isSwiftShader);
-    GPUTestConfig(const API &api, uint32_t preRotation, bool enableDirectSPIRVGen);
+    GPUTestConfig(const API &api, uint32_t preRotation);
 
     const GPUTestConfig::ConditionArray &getConditions() const;
 
