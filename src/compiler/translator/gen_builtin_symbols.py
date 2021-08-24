6a024a3efd8fd1261a6b57913ebb771c7e8bdc92
Shahbaz Youssefi <syoussefi@chromium.org>
2021-08-24 12:13:23 -0400

    Translator: Stabilize autogenerated builtin function parameters
    When pruning the list of builtin function parameters, sorted() is used
    to stabilize the result.  This makes reviews of changes to the
    autogenerated output easier.
    
    Bug: angleproject:4889
    Change-Id: I97afc31de2a62b34a0a8183bb5a47eab9e62d9fc

--- src/compiler/translator/gen_builtin_symbols.py
+++ src/compiler/translator/gen_builtin_symbols.py
@@ -148,6 +148,7 @@ template_immutablestringtest_cpp = """// GENERATED FILE - DO NOT EDIT.
 namespace sh
 {{
 
+
 TEST(ImmutableStringTest, ScriptGeneratedHashesMatch)
 {{
 {script_generated_hash_tests}
@@ -1839,7 +1840,7 @@ def prune_parameters_arrays(parameter_declarations, function_declarations):
     for param_variable_name, param_declaration in sorted(
             parameter_declarations.items(), key=lambda item: -len(item[0])):
         replaced = False
-        for used in used_param_variable_names:
+        for used in sorted(used_param_variable_names):
             if used.startswith(param_variable_name):
                 parameter_variable_name_replacements[param_variable_name] = used
                 replaced = True
