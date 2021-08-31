8d09625bf5c16ee7a11a4ebf959ad9ff83a62ef8
Brandon Schade <b.schade@samsung.com>
2021-08-31 14:58:06 -0700

    Vulkan: Fix attribute access when attributes are mismatched
    If locations are specified to be noncontiguous or out of order when
    there is a mismatch between the attribute type specified by
    glVertexAttribPointer and the shader, the program's attribute type
    from ProgramExecutable's getProgramInputs() method would fail.
    This is due to using attribIndex which refers to the attribute's
    location rather than the position in the vector returned from
    getProgramInputs(). This change collects the program's attribute's
    type before iterating over the locations.
    
    Bug: angleproject:5762
    Test:
    VertexAttributeTestES3.DrawWithMismatchedComponentCountLocationSpecified
    Change-Id: Ie15f2095e618e12403d1524273d1add74b00cdbd

--- src/libANGLE/renderer/vulkan/vk_cache_utils.cpp
+++ src/libANGLE/renderer/vulkan/vk_cache_utils.cpp
@@ -1772,6 +1772,21 @@ angle::Result GraphicsPipelineDesc::initializePipeline(
     VkPipelineVertexInputDivisorStateCreateInfoEXT divisorState = {};
     divisorState.sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_DIVISOR_STATE_CREATE_INFO_EXT;
     divisorState.pVertexBindingDivisors = divisorDesc.data();
+
+    gl::AttribArray<GLenum> attributeTypes;
+    const gl::ProgramExecutable *programExecutable = contextVk->getState().getProgramExecutable();
+    if (programExecutable)
+    {
+        for (sh::ShaderVariable attribute : programExecutable->getProgramInputs())
+        {
+            if (attribute.isBuiltIn())
+            {
+                continue;
+            }
+            attributeTypes[attribute.location] = attribute.type;
+        }
+    }
+
     for (size_t attribIndexSizeT : activeAttribLocationsMask)
     {
         const uint32_t attribIndex = static_cast<uint32_t>(attribIndexSizeT);
@@ -1832,9 +1847,8 @@ angle::Result GraphicsPipelineDesc::initializePipeline(
                 vkFormat = convertedFormat.actualBufferVkFormat(packedAttrib.compressed);
             }
 
-            GLenum programAttributeType =
-                contextVk->getState().getProgramExecutable()->getProgramInputs()[attribIndex].type;
-            GLuint attribSize = gl::GetVertexFormatFromID(formatID).components;
+            GLenum programAttributeType = attributeTypes[attribIndex];
+            GLuint attribSize           = gl::GetVertexFormatFromID(formatID).components;
             GLuint shaderVarSize =
                 static_cast<GLuint>(gl::VariableColumnCount(programAttributeType));
 
