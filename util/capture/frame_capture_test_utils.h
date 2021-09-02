414c8291c8c3f1b3907f14ed731dab64c4e05311
Jamie Madill <jmadill@chromium.org>
2021-09-02 10:31:13 -0400

    Capture/Replay: Serialize trace metadata to a JSON file.
    This will allow us to remove more code auto-generation for the trace
    tests. The trace info now can be loaded directly from JSON instead of
    from the autogenerated information.
    
    Bug: angleproject:5133
    Change-Id: I04e22b9279b19282df274bc8defcd363d0449111

--- util/capture/frame_capture_test_utils.h
+++ util/capture/frame_capture_test_utils.h
@@ -7,8 +7,8 @@
 //   Helper functions for capture and replay of traces.
 //
 
-#ifndef UTIL_FRAME_CAPTURE_TEST_UTILS_H_
-#define UTIL_FRAME_CAPTURE_TEST_UTILS_H_
+#ifndef UTIL_CAPTURE_FRAME_CAPTURE_TEST_UTILS_H_
+#define UTIL_CAPTURE_FRAME_CAPTURE_TEST_UTILS_H_
 
 #include <iostream>
 #include <memory>
@@ -17,6 +17,7 @@
 #include <vector>
 
 #include "common/angleutils.h"
+#include "common/debug.h"
 #include "common/system_utils.h"
 
 #define USE_SYSTEM_ZLIB
@@ -129,6 +130,37 @@ class TraceLibrary
     std::unique_ptr<Library> mTraceLibrary;
 };
 
+static constexpr size_t kTraceInfoMaxNameLen = 32;
+
+struct TraceInfo
+{
+    char name[kTraceInfoMaxNameLen];
+    uint32_t contextClientMajorVersion;
+    uint32_t contextClientMinorVersion;
+    uint32_t frameStart;
+    uint32_t frameEnd;
+    uint32_t drawSurfaceWidth;
+    uint32_t drawSurfaceHeight;
+    uint32_t drawSurfaceColorSpace;
+    uint32_t displayPlatformType;
+    uint32_t displayDeviceType;
+    int configRedBits;
+    int configBlueBits;
+    int configGreenBits;
+    int configAlphaBits;
+    int configDepthBits;
+    int configStencilBits;
+    bool isBinaryDataCompressed;
+    bool areClientArraysEnabled;
+    bool isBindGeneratesResourcesEnabled;
+    bool isWebGLCompatibilityEnabled;
+    bool isRobustResourceInitEnabled;
+};
+
+bool LoadTraceNamesFromJSON(const std::string jsonFilePath, std::vector<std::string> *namesOut);
+bool LoadTraceInfoFromJSON(const std::string &traceName,
+                           const std::string &traceJsonPath,
+                           TraceInfo *traceInfoOut);
 }  // namespace angle
 
-#endif  // UTIL_FRAME_CAPTURE_TEST_UTILS_H_
+#endif  // UTIL_CAPTURE_FRAME_CAPTURE_TEST_UTILS_H_
