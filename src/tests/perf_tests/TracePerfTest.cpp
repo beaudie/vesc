f8ca75d4d7bc1159b27dc6e6b02f2fcca83f82ae
Lubosz Sarnecki <lubosz.sarnecki@collabora.com>
2021-09-09 15:29:13 +0200

    Tests: Add Nier Reincarnation trace.
    Requires EGL_KHR_gl_colorspace and GL_KHR_texture_compression_astc_ldr.
    
    Test: angle_perftests --gtest_filter="*nier_reincarnation*"
    Bug: angleproject:6366
    Change-Id: Ibb71520762cf9c0a17047324557e70215754bbbb

--- src/tests/perf_tests/TracePerfTest.cpp
+++ src/tests/perf_tests/TracePerfTest.cpp
@@ -1078,6 +1078,12 @@ TracePerfTest::TracePerfTest(const TracePerfParams &params)
         }
     }
 
+    if (traceNameIs("nier_reincarnation"))
+    {
+        addExtensionPrerequisiteEGL("EGL_KHR_gl_colorspace");
+        addExtensionPrerequisite("GL_KHR_texture_compression_astc_ldr");
+    }
+
     ASSERT(mParams.surfaceType == SurfaceType::Window || gEnableAllTraceTests);
     ASSERT(mParams.eglParameters.deviceType == EGL_PLATFORM_ANGLE_DEVICE_TYPE_HARDWARE_ANGLE ||
            gEnableAllTraceTests);
