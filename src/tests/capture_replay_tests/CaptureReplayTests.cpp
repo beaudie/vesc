google/angle

commit 2f3e4db838068584a0c15653cb07893683a55300

Capture/Replay: Serialize trace metadata to a JSON file.
This will allow us to remove more code auto-generation for the trace
tests. The trace info now can be loaded directly from JSON instead of
from the autogenerated information.

Bug: angleproject:5133
Change-Id: I04e22b9279b19282df274bc8defcd363d0449111
Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/3140218
Commit-Queue: Jamie Madill <jmadill@chromium.org>
Reviewed-by: Tim Van Patten <timvp@google.com>
Reviewed-by: Cody Northrop <cnorthrop@google.com>

src/tests/capture_replay_tests/CaptureReplayTests.cpp
@@ -24,13 +24,10 @@
 #include <string>
 #include <utility>
 
-#include "util/frame_capture_test_utils.h"
-
-// Build the right context header based on replay ID
-// This will expand to "angle_capture_context<#>.h"
-#include ANGLE_MACRO_STRINGIZE(ANGLE_CAPTURE_REPLAY_COMPOSITE_TESTS_HEADER)
+#include "util/capture/frame_capture_test_utils.h"
 
 constexpr char kResultTag[] = "*RESULT";
+constexpr char kTracePath[] = ANGLE_CAPTURE_REPLAY_TEST_NAMES_PATH;
 
 class CaptureReplayTests
 {
@@ -51,10 +48,10 @@ class CaptureReplayTests
         OSWindow::Delete(&mOSWindow);
     }
 
-    bool initializeTest(uint32_t testIndex, const TestTraceInfo &testTraceInfo)
+    bool initializeTest(const angle::TraceInfo &traceInfo)
     {
-        if (!mOSWindow->initialize(testTraceInfo.testName, testTraceInfo.replayDrawSurfaceWidth,
-                                   testTraceInfo.replayDrawSurfaceHeight))
+        if (!mOSWindow->initialize(traceInfo.name, traceInfo.drawSurfaceWidth,
+                                   traceInfo.drawSurfaceHeight))
         {
             return false;
         }
@@ -62,8 +59,8 @@ class CaptureReplayTests
         mOSWindow->disableErrorMessageDialog();
         mOSWindow->setVisible(true);
 
-        if (mEGLWindow && !mEGLWindow->isContextVersion(testTraceInfo.replayContextMajorVersion,
-                                                        testTraceInfo.replayContextMinorVersion))
+        if (mEGLWindow && !mEGLWindow->isContextVersion(traceInfo.contextClientMajorVersion,
+                                                        traceInfo.contextClientMinorVersion))
         {
             EGLWindow::Delete(&mEGLWindow);
             mEGLWindow = nullptr;
@@ -71,25 +68,25 @@ class CaptureReplayTests
 
         if (!mEGLWindow)
         {
-            mEGLWindow = EGLWindow::New(testTraceInfo.replayContextMajorVersion,
-                                        testTraceInfo.replayContextMinorVersion);
+            mEGLWindow = EGLWindow::New(traceInfo.contextClientMajorVersion,
+                                        traceInfo.contextClientMinorVersion);
         }
 
         ConfigParameters configParams;
-        configParams.redBits     = testTraceInfo.defaultFramebufferRedBits;
-        configParams.greenBits   = testTraceInfo.defaultFramebufferGreenBits;
-        configParams.blueBits    = testTraceInfo.defaultFramebufferBlueBits;
-        configParams.alphaBits   = testTraceInfo.defaultFramebufferAlphaBits;
-        configParams.depthBits   = testTraceInfo.defaultFramebufferDepthBits;
-        configParams.stencilBits = testTraceInfo.defaultFramebufferStencilBits;
+        configParams.redBits     = traceInfo.configRedBits;
+        configParams.greenBits   = traceInfo.configGreenBits;
+        configParams.blueBits    = traceInfo.configBlueBits;
+        configParams.alphaBits   = traceInfo.configAlphaBits;
+        configParams.depthBits   = traceInfo.configDepthBits;
+        configParams.stencilBits = traceInfo.configStencilBits;
 
-        configParams.clientArraysEnabled   = testTraceInfo.areClientArraysEnabled;
-        configParams.bindGeneratesResource = testTraceInfo.bindGeneratesResources;
-        configParams.webGLCompatibility    = testTraceInfo.webGLCompatibility;
-        configParams.robustResourceInit    = testTraceInfo.robustResourceInit;
+        configParams.clientArraysEnabled   = traceInfo.areClientArraysEnabled;
+        configParams.bindGeneratesResource = traceInfo.isBindGeneratesResourcesEnabled;
+        configParams.webGLCompatibility    = traceInfo.isWebGLCompatibilityEnabled;
+        configParams.robustResourceInit    = traceInfo.isRobustResourceInitEnabled;
 
-        mPlatformParams.renderer                 = testTraceInfo.replayPlatformType;
-        mPlatformParams.deviceType               = testTraceInfo.replayDeviceType;
+        mPlatformParams.renderer                 = traceInfo.displayPlatformType;
+        mPlatformParams.deviceType               = traceInfo.displayDeviceType;
         mPlatformParams.forceInitShaderVariables = EGL_TRUE;
 
         if (!mEGLWindow->initializeGL(mOSWindow, mEntryPointsLib.get(),
@@ -106,19 +103,15 @@ class CaptureReplayTests
             return false;
         }
 
-        mStartingDirectory = angle::GetCWD().value();
-
         // Load trace
-        mTraceLibrary.reset(new angle::TraceLibrary(testTraceInfo.testName.c_str()));
-
-        // Set CWD to executable directory.
-        std::string exeDir = angle::GetExecutableDirectory();
-        if (!angle::SetCWD(exeDir.c_str()))
+        mTraceLibrary.reset(new angle::TraceLibrary(traceInfo.name));
+        if (!mTraceLibrary->valid())
         {
-            cleanupTest();
+            std::cout << "Failed to load trace library: " << traceInfo.name << "\n";
             return false;
         }
-        if (testTraceInfo.isBinaryDataCompressed)
+
+        if (traceInfo.isBinaryDataCompressed)
         {
             mTraceLibrary->setBinaryDataDecompressCallback(angle::DecompressBinaryData);
         }
@@ -130,7 +123,6 @@ class CaptureReplayTests
 
     void cleanupTest()
     {
-        angle::SetCWD(mStartingDirectory.c_str());
         mTraceLibrary.reset(nullptr);
         mEGLWindow->destroyGL();
         mOSWindow->destroy();
@@ -138,15 +130,14 @@ class CaptureReplayTests
 
     void swap() { mEGLWindow->swap(); }
 
-    int runTest(uint32_t testIndex, const TestTraceInfo &testTraceInfo)
+    int runTest(const angle::TraceInfo &traceInfo)
     {
-        if (!initializeTest(testIndex, testTraceInfo))
+        if (!initializeTest(traceInfo))
         {
             return -1;
         }
 
-        for (uint32_t frame = testTraceInfo.replayFrameStart; frame <= testTraceInfo.replayFrameEnd;
-             frame++)
+        for (uint32_t frame = traceInfo.frameStart; frame <= traceInfo.frameEnd; frame++)
         {
             mTraceLibrary->replayFrame(frame);
 
@@ -164,12 +155,12 @@ class CaptureReplayTests
             if (!isEqual)
             {
                 std::ostringstream replayName;
-                replayName << testTraceInfo.testName << "_ContextReplayed" << frame << ".json";
+                replayName << traceInfo.name << "_ContextReplayed" << frame << ".json";
                 std::ofstream debugReplay(replayName.str());
                 debugReplay << (replayedSerializedState ? replayedSerializedState : "") << "\n";
 
                 std::ostringstream captureName;
-                captureName << testTraceInfo.testName << "_ContextCaptured" << frame << ".json";
+                captureName << traceInfo.name << "_ContextCaptured" << frame << ".json";
                 std::ofstream debugCapture(captureName.str());
 
                 debugCapture << (capturedSerializedState ? capturedSerializedState : "") << "\n";
@@ -184,11 +175,44 @@ class CaptureReplayTests
 
     int run()
     {
-        for (size_t i = 0; i < testTraceInfos.size(); i++)
+        std::string startingDirectory = angle::GetCWD().value();
+
+        // Set CWD to executable directory.
+        std::string exeDir = angle::GetExecutableDirectory();
+        if (!angle::SetCWD(exeDir.c_str()))
         {
-            int result = runTest(static_cast<uint32_t>(i), testTraceInfos[i]);
-            std::cout << kResultTag << " " << testTraceInfos[i].testName << " " << result << "\n";
+            std::cout << "Unable to SetCWD to trace directory: " << exeDir << "\n";
+            return 1;
         }
+
+        std::vector<std::string> traces;
+        if (!angle::LoadTraceNamesFromJSON(kTracePath, &traces))
+        {
+            std::cout << "Unable to load trace names from " << kTracePath << "\n";
+            return 1;
+        }
+
+        for (const std::string &trace : traces)
+        {
+            std::stringstream traceJsonPathStream;
+            traceJsonPathStream << ANGLE_CAPTURE_REPLAY_TEST_DATA_DIR << angle::GetPathSeparator()
+                                << trace << ".json";
+            std::string traceJsonPath = traceJsonPathStream.str();
+
+            int result                 = -1;
+            angle::TraceInfo traceInfo = {};
+            if (!angle::LoadTraceInfoFromJSON(trace, traceJsonPath, &traceInfo))
+            {
+                std::cout << "Unable to load trace data: " << traceJsonPath << "\n";
+            }
+            else
+            {
+                result = runTest(traceInfo);
+            }
+            std::cout << kResultTag << " " << trace << " " << result << "\n";
+        }
+
+        angle::SetCWD(startingDirectory.c_str());
         return 0;
     }
 
@@ -200,7 +224,6 @@ class CaptureReplayTests
         return !strcmp(replaySerializedContextState, capturedSerializedContextState);
     }
 
-    std::string mStartingDirectory;
     OSWindow *mOSWindow   = nullptr;
     EGLWindow *mEGLWindow = nullptr;
     EGLPlatformParameters mPlatformParams;

BUILD.gn
@@ -48,9 +48,6 @@ declare_args() {
 
   # Optional feature that forces dirty state whenever we use a new context regardless of thread.
   angle_force_context_check_every_call = false
-
-  # Indicate if the rapidJSON library is available to build with in third_party/.
-  angle_has_rapidjson = angle_has_build
 }
 
 if (angle_build_all) {

gni/angle.gni
@@ -81,6 +81,9 @@ declare_args() {
   } else {
     assert(false, "Unknown current CPU: $current_cpu")
   }
+
+  # Indicate if the rapidJSON library is available to build with in third_party/.
+  angle_has_rapidjson = angle_has_build
 }
 
 declare_args() {
@@ -540,8 +543,8 @@ template("angle_trace") {
                 "${_trace_prefix}.h",
               ] + invoker.sources
 
-    # TODO(http://anglebug.com/5878): Revert back to non-autogen'ed file names for the angledata.gz.
-    data = [ invoker.angledata ]
+    # TODO(http://anglebug.com/5133): Revert back to non-autogen'ed data.
+    data = invoker.data
     defines = [ "ANGLE_REPLAY_IMPLEMENTATION" ]
     suppressed_configs += [ "$angle_root:constructor_and_destructor_warnings" ]
 

samples/capture_replay/CaptureReplay.cpp
@@ -9,7 +9,7 @@
 
 #include <functional>
 
-#include "util/frame_capture_test_utils.h"
+#include "util/capture/frame_capture_test_utils.h"
 
 // Build the right context header based on replay ID
 // This will expand to "angle_capture_context<#>.h"

src/libANGLE/capture/FrameCapture.cpp
@@ -42,6 +42,7 @@
 #include "libANGLE/entry_points_utils.h"
 #include "libANGLE/queryconversions.h"
 #include "libANGLE/queryutils.h"
+#include "libANGLE/serializer/JsonSerializer.h"
 #include "third_party/ceval/ceval.h"
 
 #define USE_SYSTEM_ZLIB
@@ -5905,12 +5906,73 @@ void FrameCaptureShared::replay(gl::Context *context)
 void FrameCaptureShared::writeCppReplayIndexFiles(const gl::Context *context,
                                                   bool writeResetContextCall)
 {
-    const gl::ContextID contextId       = context->id();
-    const egl::Config *config           = context->getConfig();
-    const egl::AttributeMap &attributes = context->getDisplay()->getAttributeMap();
-    const egl::ShareGroup *shareGroup   = context->getShareGroup();
+    const gl::ContextID contextId           = context->id();
+    const egl::Config *config               = context->getConfig();
+    const egl::AttributeMap &displayAttribs = context->getDisplay()->getAttributeMap();
+    const egl::ShareGroup *shareGroup       = context->getShareGroup();
 
     unsigned frameCount = getFrameCount();
+
+    const SurfaceParams &surfaceParams = mDrawSurfaceParams.at(contextId);
+    const gl::State &glState           = context->getState();
+
+    // Serialize trace metadata into a JSON file. The JSON file will be named "trace_prefix.json".
+    //
+    // As of writing, it will have the format like so:
+    // {
+    //     "TraceMetadata":
+    //     {
+    //         "AreClientArraysEnabled" : 1, "CaptureRevision" : 16631, "ConfigAlphaBits" : 8,
+    //             "ConfigBlueBits" : 8, "ConfigDepthBits" : 24, "ConfigGreenBits" : 8,
+    // ... etc ...
+
+    JsonSerializer json;
+    json.startDocument("TraceMetadata");
+    json.addScalar("CaptureRevision", ANGLE_REVISION);
+    json.addScalar("ContextClientMajorVersion", context->getClientMajorVersion());
+    json.addScalar("ContextClientMinorVersion", context->getClientMinorVersion());
+    json.addScalar("DisplayPlatformType", displayAttribs.getAsInt(EGL_PLATFORM_ANGLE_TYPE_ANGLE));
+    json.addScalar("DisplayDeviceType",
+                   displayAttribs.getAsInt(EGL_PLATFORM_ANGLE_DEVICE_TYPE_ANGLE));
+    json.addScalar("FrameStart", 1);
+    json.addScalar("FrameEnd", frameCount);
+    json.addScalar("DrawSurfaceWidth", surfaceParams.extents.width);
+    json.addScalar("DrawSurfaceHeight", surfaceParams.extents.height);
+    json.addScalar("DrawSurfaceColorSpace", ToEGLenum(surfaceParams.colorSpace));
+    if (config)
+    {
+        json.addScalar("ConfigRedBits", config->redSize);
+        json.addScalar("ConfigGreenBits", config->greenSize);
+        json.addScalar("ConfigBlueBits", config->blueSize);
+        json.addScalar("ConfigAlphaBits", config->alphaSize);
+        json.addScalar("ConfigDepthBits", config->depthSize);
+        json.addScalar("ConfigStencilBits", config->stencilSize);
+    }
+    else
+    {
+        json.addScalar("ConfigRedBits", EGL_DONT_CARE);
+        json.addScalar("ConfigGreenBits", EGL_DONT_CARE);
+        json.addScalar("ConfigBlueBits", EGL_DONT_CARE);
+        json.addScalar("ConfigAlphaBits", EGL_DONT_CARE);
+        json.addScalar("ConfigDepthBits", EGL_DONT_CARE);
+        json.addScalar("ConfigStencilBits", EGL_DONT_CARE);
+    }
+    json.addBool("IsBinaryDataCompressed", mCompression);
+    json.addBool("AreClientArraysEnabled", glState.areClientArraysEnabled());
+    json.addBool("IsBindGeneratesResourcesEnabled", glState.isBindGeneratesResourceEnabled());
+    json.addBool("IsWebGLCompatibilityEnabled", glState.isWebGL());
+    json.addBool("IsRobustResourceInitEnabled", glState.isRobustResourceInitEnabled());
+    json.endDocument();
+
+    {
+        std::stringstream jsonFileNameStream;
+        jsonFileNameStream << mOutDirectory << FmtCapturePrefix(kNoContextId, mCaptureLabel)
+                           << ".json";
+        std::string jsonFileName = jsonFileNameStream.str();
+
+        SaveFileHelper saveData(jsonFileName);
+        saveData.write(reinterpret_cast<const uint8_t *>(json.data()), json.length());
+    }
 
     std::stringstream header;
     std::stringstream source;
@@ -5926,56 +5988,6 @@ void FrameCaptureShared::writeCppReplayIndexFiles(const gl::Context *context,
         header << "namespace " << mCaptureLabel << "\n";
         header << "{\n";
     }
-    header << "// Begin Trace Metadata\n";
-    header << "#define ANGLE_REPLAY_VERSION";
-    if (!mCaptureLabel.empty())
-    {
-        std::string captureLabelUpper = mCaptureLabel;
-        angle::ToUpper(&captureLabelUpper);
-        header << "_" << captureLabelUpper;
-    }
-    header << " " << ANGLE_REVISION << "\n";
-    header << "constexpr uint32_t kReplayContextClientMajorVersion = "
-           << context->getClientMajorVersion() << ";\n";
-    header << "constexpr uint32_t kReplayContextClientMinorVersion = "
-           << context->getClientMinorVersion() << ";\n";
-    header << "constexpr EGLint kReplayPlatformType = "
-           << attributes.getAsInt(EGL_PLATFORM_ANGLE_TYPE_ANGLE) << ";\n";
-    header << "constexpr EGLint kReplayDeviceType = "
-           << attributes.getAsInt(EGL_PLATFORM_ANGLE_DEVICE_TYPE_ANGLE) << ";\n";
-    header << "constexpr uint32_t kReplayFrameStart = 1;\n";
-    header << "constexpr uint32_t kReplayFrameEnd = " << frameCount << ";\n";
-    header << "constexpr EGLint kReplayDrawSurfaceWidth = "
-           << mDrawSurfaceParams.at(contextId).extents.width << ";\n";
-    header << "constexpr EGLint kReplayDrawSurfaceHeight = "
-           << mDrawSurfaceParams.at(contextId).extents.height << ";\n";
-    header << "constexpr EGLenum kReplayDrawSurfaceColorSpace = "
-           << mDrawSurfaceParams.at(contextId).colorSpace << ";\n";
-    header << "constexpr EGLint kDefaultFramebufferRedBits = "
-           << (config ? std::to_string(config->redSize) : "EGL_DONT_CARE") << ";\n";
-    header << "constexpr EGLint kDefaultFramebufferGreenBits = "
-           << (config ? std::to_string(config->greenSize) : "EGL_DONT_CARE") << ";\n";
-    header << "constexpr EGLint kDefaultFramebufferBlueBits = "
-           << (config ? std::to_string(config->blueSize) : "EGL_DONT_CARE") << ";\n";
-    header << "constexpr EGLint kDefaultFramebufferAlphaBits = "
-           << (config ? std::to_string(config->alphaSize) : "EGL_DONT_CARE") << ";\n";
-    header << "constexpr EGLint kDefaultFramebufferDepthBits = "
-           << (config ? std::to_string(config->depthSize) : "EGL_DONT_CARE") << ";\n";
-    header << "constexpr EGLint kDefaultFramebufferStencilBits = "
-           << (config ? std::to_string(config->stencilSize) : "EGL_DONT_CARE") << ";\n";
-    header << "constexpr bool kIsBinaryDataCompressed = " << (mCompression ? "true" : "false")
-           << ";\n";
-    header << "constexpr bool kAreClientArraysEnabled = "
-           << (context->getState().areClientArraysEnabled() ? "true" : "false") << ";\n";
-    header << "constexpr bool kbindGeneratesResources = "
-           << (context->getState().isBindGeneratesResourceEnabled() ? "true" : "false") << ";\n";
-    header << "constexpr bool kWebGLCompatibility = "
-           << (context->getState().getExtensions().webglCompatibility ? "true" : "false") << ";\n";
-    header << "constexpr bool kRobustResourceInit = "
-           << (context->getState().isRobustResourceInitEnabled() ? "true" : "false") << ";\n";
-
-    header << "// End Trace Metadata\n";
-    header << "\n";
     for (uint32_t frameIndex = 1; frameIndex <= frameCount; ++frameIndex)
     {
         header << "void " << FmtReplayFunction(contextId, frameIndex) << ";\n";

src/libANGLE/serializer/JsonSerializer.cpp
@@ -94,16 +94,22 @@ void JsonSerializer::addString(const std::string &name, const std::string &value
 void JsonSerializer::addVectorOfStrings(const std::string &name,
                                         const std::vector<std::string> &value)
 {
-    rapidjson::Value array(rapidjson::kArrayType);
-    array.SetArray();
+    rapidjson::Value arrayValue(rapidjson::kArrayType);
+    arrayValue.SetArray();
 
     for (const std::string &v : value)
     {
         rapidjson::Value str(v.c_str(), mAllocator);
-        array.PushBack(str, mAllocator);
+        arrayValue.PushBack(str, mAllocator);
     }
 
-    mGroupValueStack.top().insert(std::make_pair(name, std::move(array)));
+    mGroupValueStack.top().insert(std::make_pair(name, std::move(arrayValue)));
+}
+
+void JsonSerializer::addBool(const std::string &name, bool value)
+{
+    rapidjson::Value boolValue(value);
+    mGroupValueStack.top().insert(std::make_pair(name, std::move(boolValue)));
 }
 
 const char *JsonSerializer::data() const

src/libANGLE/serializer/JsonSerializer.h
@@ -105,6 +105,8 @@ class JsonSerializer : public angle::NonCopyable
 
     void addVectorOfStrings(const std::string &name, const std::vector<std::string> &value);
 
+    void addBool(const std::string &name, bool value);
+
   private:
     using SortedValueGroup = std::multimap<std::string, rapidjson::Value>;
 

src/tests/BUILD.gn
@@ -426,6 +426,7 @@ if (build_angle_perftests) {
       deps = [
         ":angle_perftests_shared",
         "$angle_root:angle_compression",
+        "$angle_root/util:angle_frame_capture_test_utils",
         "$angle_root/util:angle_png_utils",
         "restricted_traces:angle_restricted_traces",
       ]

beaudie/vesc

commit 486b0c5637090132c09202e28b07aa4273278bc7

Capture/Replay: Don't set CWD and pass path information instead
With that we avoid complication when evaluating the module path that
is used to load the test modules.

Fixes: 2f3e4db838068584a0c15653cb07893683a55300
  Capture/Replay: Serialize trace metadata to a JSON file.

Bug: angleproject:6385
Change-Id: If4e348237d9b0e99e404f49b28dddba1ac50ef92

src/libANGLE/capture/trace_fixture.cpp
@@ -28,7 +28,7 @@ void UpdateResourceMap2(GLuint *resourceMap, GLuint id, GLsizei readBufferOffset
 }
 
 DecompressCallback gDecompressCallback;
-const char *gBinaryDataDir = ".";
+std::string gBinaryDataDir = ".";
 
 void LoadBinaryData(const char *fileName)
 {
@@ -38,7 +38,8 @@ void LoadBinaryData(const char *fileName)
         delete[] gBinaryData;
     }
     char pathBuffer[1000] = {};
-    sprintf(pathBuffer, "%s/%s", gBinaryDataDir, fileName);
+
+    sprintf(pathBuffer, "%s/%s", gBinaryDataDir.c_str(), fileName);
     FILE *fp = fopen(pathBuffer, "rb");
     if (fp == 0)
     {

src/tests/capture_replay_tests/CaptureReplayTests.cpp
@@ -48,7 +48,7 @@ class CaptureReplayTests
         OSWindow::Delete(&mOSWindow);
     }
 
-    bool initializeTest(const angle::TraceInfo &traceInfo)
+    bool initializeTest(const std::string &execDir, const angle::TraceInfo &traceInfo)
     {
         if (!mOSWindow->initialize(traceInfo.name, traceInfo.drawSurfaceWidth,
                                    traceInfo.drawSurfaceHeight))
@@ -115,7 +115,12 @@ class CaptureReplayTests
         {
             mTraceLibrary->setBinaryDataDecompressCallback(angle::DecompressBinaryData);
         }
-        mTraceLibrary->setBinaryDataDir(ANGLE_CAPTURE_REPLAY_TEST_DATA_DIR);
+
+        std::stringstream binaryPathStream;
+        binaryPathStream << execDir << angle::GetPathSeparator()
+                         << ANGLE_CAPTURE_REPLAY_TEST_DATA_DIR;
+
+        mTraceLibrary->setBinaryDataDir(binaryPathStream.str().c_str());
 
         mTraceLibrary->setupReplay();
         return true;
@@ -130,9 +135,9 @@ class CaptureReplayTests
 
     void swap() { mEGLWindow->swap(); }
 
-    int runTest(const angle::TraceInfo &traceInfo)
+    int runTest(const std::string &exeDir, const angle::TraceInfo &traceInfo)
     {
-        if (!initializeTest(traceInfo))
+        if (!initializeTest(exeDir, traceInfo))
         {
             return -1;
         }
@@ -179,14 +184,13 @@ class CaptureReplayTests
 
         // Set CWD to executable directory.
         std::string exeDir = angle::GetExecutableDirectory();
-        if (!angle::SetCWD(exeDir.c_str()))
-        {
-            std::cout << "Unable to SetCWD to trace directory: " << exeDir << "\n";
-            return 1;
-        }
 
         std::vector<std::string> traces;
-        if (!angle::LoadTraceNamesFromJSON(kTracePath, &traces))
+
+        std::stringstream tracePathStream;
+        tracePathStream << exeDir << angle::GetPathSeparator() << kTracePath;
+
+        if (!angle::LoadTraceNamesFromJSON(tracePathStream.str(), &traces))
         {
             std::cout << "Unable to load trace names from " << kTracePath << "\n";
             return 1;
@@ -195,7 +199,8 @@ class CaptureReplayTests
         for (const std::string &trace : traces)
         {
             std::stringstream traceJsonPathStream;
-            traceJsonPathStream << ANGLE_CAPTURE_REPLAY_TEST_DATA_DIR << angle::GetPathSeparator()
+            traceJsonPathStream << exeDir << angle::GetPathSeparator()
+                                << ANGLE_CAPTURE_REPLAY_TEST_DATA_DIR << angle::GetPathSeparator()
                                 << trace << ".json";
             std::string traceJsonPath = traceJsonPathStream.str();
 
@@ -207,7 +212,7 @@ class CaptureReplayTests
             }
             else
             {
-                result = runTest(traceInfo);
+                result = runTest(exeDir, traceInfo);
             }
             std::cout << kResultTag << " " << trace << " " << result << "\n";
         }