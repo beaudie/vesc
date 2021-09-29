google/angle

commit 2d79918fdb0276cdb57488d72abd803250f962a3

Vulkan: Pass command pool around
Preparatory work for re-enabling support for Vulkan secondary command
buffers.  The command pool being passed around is not yet used.
Additionally, the work to allocate, initialize, reset and recycle
command buffers has been refactored to a helper class for better
abstraction.

The CommandBufferHelper class now holds a pointer to the pool it was
allocated from for clean up.  Each ContextVk has its own command pool,
where Vulkan secondary command buffers are allocated from.  The command
pool that CommandProcessor had was unused and is removed by this change.

Bug: angleproject:6100
Change-Id: I0d4fa6a76ef06e88779145ffe9c613518a8eb390
Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/3167213
Commit-Queue: Shahbaz Youssefi <syoussefi@chromium.org>
Reviewed-by: Jamie Madill <jmadill@chromium.org>
Reviewed-by: Tim Van Patten <timvp@google.com>

src/libANGLE/renderer/vulkan/CommandProcessor.cpp
@@ -288,7 +288,6 @@ CommandBatch &CommandBatch::operator=(CommandBatch &&other)
 void CommandBatch::destroy(VkDevice device)
 {
     primaryCommands.destroy(device);
-    commandPool.destroy(device);
     fence.reset(device);
     hasProtectedContent = false;
 }
@@ -420,7 +419,6 @@ angle::Result CommandProcessor::processTask(CommandProcessorTask *task)
                                                    mRenderer->getMaxFenceWaitTimeNs()));
             // Shutting down so cleanup
             mCommandQueue.destroy(this);
-            mCommandPool.destroy(mRenderer->getDevice());
             break;
         }
         case CustomTask::FlushAndQueueSubmit:
@@ -432,7 +430,8 @@ angle::Result CommandProcessor::processTask(CommandProcessorTask *task)
             ANGLE_TRY(mCommandQueue.submitFrame(
                 this, task->hasProtectedContent(), task->getPriority(), task->getWaitSemaphores(),
                 task->getWaitSemaphoreStageMasks(), task->getSemaphore(),
-                std::move(task->getGarbage()), &mCommandPool, task->getQueueSerial()));
+                std::move(task->getGarbage()), task->getCommandBuffer()->getCommandPool(),
+                task->getQueueSerial()));
 
             ASSERT(task->getGarbage().empty());
             break;
@@ -487,7 +486,7 @@ angle::Result CommandProcessor::processTask(CommandProcessorTask *task)
                                                                &commandBuffer));
             }
             ASSERT(task->getCommandBuffer()->empty());
-            mRenderer->recycleCommandBufferHelper(task->getCommandBuffer());
+            mRenderer->recycleCommandBufferHelper(mRenderer->getDevice(), task->getCommandBuffer());
             break;
         }
         case CustomTask::CheckCompletedCommands:
@@ -722,9 +721,8 @@ angle::Result CommandProcessor::flushOutsideRPCommands(Context *context,
     CommandProcessorTask task;
     task.initProcessCommands(hasProtectedContent, *outsideRPCommands, nullptr);
     queueCommand(std::move(task));
-    *outsideRPCommands = mRenderer->getCommandBufferHelper(false);
-
-    return angle::Result::Continue;
+    return mRenderer->getCommandBufferHelper(context, false, (*outsideRPCommands)->getCommandPool(),
+                                             outsideRPCommands);
 }
 
 angle::Result CommandProcessor::flushRenderPassCommands(Context *context,
@@ -738,9 +736,8 @@ angle::Result CommandProcessor::flushRenderPassCommands(Context *context,
     CommandProcessorTask task;
     task.initProcessCommands(hasProtectedContent, *renderPassCommands, &renderPass);
     queueCommand(std::move(task));
-    *renderPassCommands = mRenderer->getCommandBufferHelper(true);
-
-    return angle::Result::Continue;
+    return mRenderer->getCommandBufferHelper(context, true, (*renderPassCommands)->getCommandPool(),
+                                             renderPassCommands);
 }
 
 // CommandQueue implementation.
@@ -767,10 +764,10 @@ void CommandQueue::destroy(Context *context)
     mPrimaryCommands.destroy(renderer->getDevice());
     mPrimaryCommandPool.destroy(renderer->getDevice());
 
-    if (mProtectedCommandPool.valid())
+    if (mProtectedPrimaryCommandPool.valid())
     {
-        mProtectedCommands.destroy(renderer->getDevice());
-        mProtectedCommandPool.destroy(renderer->getDevice());
+        mProtectedPrimaryCommands.destroy(renderer->getDevice());
+        mProtectedPrimaryCommandPool.destroy(renderer->getDevice());
     }
 
     mFenceRecycler.destroy(context);
@@ -786,7 +783,7 @@ angle::Result CommandQueue::init(Context *context, const vk::DeviceQueueMap &que
 
     if (queueMap.isProtected())
     {
-        ANGLE_TRY(mProtectedCommandPool.init(context, true, queueMap.getIndex()));
+        ANGLE_TRY(mProtectedPrimaryCommandPool.init(context, true, queueMap.getIndex()));
     }
 
     return angle::Result::Continue;
@@ -824,7 +821,6 @@ angle::Result CommandQueue::retireFinishedCommands(Context *context, size_t fini
     ASSERT(finishedCount > 0);
 
     RendererVk *renderer = context->getRenderer();
-    VkDevice device      = renderer->getDevice();
 
     for (size_t commandIndex = 0; commandIndex < finishedCount; ++commandIndex)
     {
@@ -833,7 +829,6 @@ angle::Result CommandQueue::retireFinishedCommands(Context *context, size_t fini
         mLastCompletedQueueSerial = batch.serial;
         mFenceRecycler.resetSharedFence(&batch.fence);
         ANGLE_TRACE_EVENT0("gpu.angle", "command buffer recycling");
-        batch.commandPool.destroy(device);
         PersistentCommandPool &commandPool = getCommandPool(batch.hasProtectedContent);
         ANGLE_TRY(commandPool.collect(context, std::move(batch.primaryCommands)));
     }
@@ -870,36 +865,16 @@ angle::Result CommandQueue::retireFinishedCommands(Context *context, size_t fini
     return angle::Result::Continue;
 }
 
-angle::Result CommandQueue::releaseToCommandBatch(Context *context,
-                                                  bool hasProtectedContent,
-                                                  PrimaryCommandBuffer &&commandBuffer,
-                                                  CommandPool *commandPool,
-                                                  CommandBatch *batch)
+void CommandQueue::releaseToCommandBatch(bool hasProtectedContent,
+                                         PrimaryCommandBuffer &&commandBuffer,
+                                         CommandPool *commandPool,
+                                         CommandBatch *batch)
 {
     ANGLE_TRACE_EVENT0("gpu.angle", "CommandQueue::releaseToCommandBatch");
 
-    RendererVk *renderer = context->getRenderer();
-    VkDevice device      = renderer->getDevice();
-
-    batch->primaryCommands = std::move(commandBuffer);
-
-    if (commandPool->valid())
-    {
-        batch->commandPool = std::move(*commandPool);
-        // Recreate CommandPool
-        VkCommandPoolCreateInfo poolInfo = {};
-        poolInfo.sType                   = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO;
-        poolInfo.flags                   = VK_COMMAND_POOL_CREATE_TRANSIENT_BIT;
-        poolInfo.queueFamilyIndex        = mQueueMap.getIndex();
-        if (hasProtectedContent)
-        {
-            poolInfo.flags |= VK_COMMAND_POOL_CREATE_PROTECTED_BIT;
-        }
-        batch->hasProtectedContent = hasProtectedContent;
-        ANGLE_VK_TRY(context, commandPool->init(device, poolInfo));
-    }
-
-    return angle::Result::Continue;
+    batch->primaryCommands     = std::move(commandBuffer);
+    batch->commandPool         = commandPool;
+    batch->hasProtectedContent = hasProtectedContent;
 }
 
 void CommandQueue::clearAllGarbage(RendererVk *renderer)
@@ -931,7 +906,6 @@ void CommandQueue::handleDeviceLost(RendererVk *renderer)
         // by CommandPool::destroy
         batch.primaryCommands.destroy(device);
 
-        batch.commandPool.destroy(device);
         batch.fence.reset(device);
     }
     mInFlightCommands.clear();
@@ -1040,13 +1014,13 @@ angle::Result CommandQueue::submitFrame(
     // in the in-flight list.
     if (hasProtectedContent)
     {
-        ANGLE_TRY(releaseToCommandBatch(context, hasProtectedContent, std::move(mProtectedCommands),
-                                        commandPool, &batch));
+        releaseToCommandBatch(hasProtectedContent, std::move(mProtectedPrimaryCommands),
+                              commandPool, &batch);
     }
     else
     {
-        ANGLE_TRY(releaseToCommandBatch(context, hasProtectedContent, std::move(mPrimaryCommands),
-                                        commandPool, &batch));
+        releaseToCommandBatch(hasProtectedContent, std::move(mPrimaryCommands), commandPool,
+                              &batch);
     }
     mInFlightCommands.emplace_back(scopedBatch.release());
 
@@ -1139,8 +1113,7 @@ angle::Result CommandQueue::flushOutsideRPCommands(Context *context,
 {
     ANGLE_TRY(ensurePrimaryCommandBufferValid(context, hasProtectedContent));
     PrimaryCommandBuffer &commandBuffer = getCommandBuffer(hasProtectedContent);
-    return (*outsideRPCommands)
-        ->flushToPrimary(context->getRenderer()->getFeatures(), &commandBuffer, nullptr);
+    return (*outsideRPCommands)->flushToPrimary(context, &commandBuffer, nullptr);
 }
 
 angle::Result CommandQueue::flushRenderPassCommands(Context *context,
@@ -1150,8 +1123,7 @@ angle::Result CommandQueue::flushRenderPassCommands(Context *context,
 {
     ANGLE_TRY(ensurePrimaryCommandBufferValid(context, hasProtectedContent));
     PrimaryCommandBuffer &commandBuffer = getCommandBuffer(hasProtectedContent);
-    return (*renderPassCommands)
-        ->flushToPrimary(context->getRenderer()->getFeatures(), &commandBuffer, &renderPass);
+    return (*renderPassCommands)->flushToPrimary(context, &commandBuffer, &renderPass);
 }
 
 angle::Result CommandQueue::queueSubmitOneOff(Context *context,

src/libANGLE/renderer/vulkan/CommandProcessor.h
@@ -178,7 +178,7 @@ struct CommandBatch final : angle::NonCopyable
 
     PrimaryCommandBuffer primaryCommands;
     // commandPool is for secondary CommandBuffer allocation
-    CommandPool commandPool;
+    CommandPool *commandPool;
     Shared<Fence> fence;
     Serial serial;
     bool hasProtectedContent;
@@ -372,13 +372,13 @@ class CommandQueue final : public CommandQueueInterface
     {
         return mQueueMap.getDevicePriority(priority);
     }
+    uint32_t getDeviceQueueIndex() const { return mQueueMap.getIndex(); }
 
   private:
-    angle::Result releaseToCommandBatch(Context *context,
-                                        bool hasProtectedContent,
-                                        PrimaryCommandBuffer &&commandBuffer,
-                                        CommandPool *commandPool,
-                                        CommandBatch *batch);
+    void releaseToCommandBatch(bool hasProtectedContent,
+                               PrimaryCommandBuffer &&commandBuffer,
+                               CommandPool *commandPool,
+                               CommandBatch *batch);
     angle::Result retireFinishedCommands(Context *context, size_t finishedCount);
     angle::Result ensurePrimaryCommandBufferValid(Context *context, bool hasProtectedContent);
 
@@ -390,7 +390,7 @@ class CommandQueue final : public CommandQueueInterface
     {
         if (hasProtectedContent)
         {
-            return mProtectedCommands;
+            return mProtectedPrimaryCommands;
         }
         else
         {
@@ -402,7 +402,7 @@ class CommandQueue final : public CommandQueueInterface
     {
         if (hasProtectedContent)
         {
-            return mProtectedCommandPool;
+            return mProtectedPrimaryCommandPool;
         }
         else
         {
@@ -417,8 +417,8 @@ class CommandQueue final : public CommandQueueInterface
     // Keeps a free list of reusable primary command buffers.
     PrimaryCommandBuffer mPrimaryCommands;
     PersistentCommandPool mPrimaryCommandPool;
-    PrimaryCommandBuffer mProtectedCommands;
-    PersistentCommandPool mProtectedCommandPool;
+    PrimaryCommandBuffer mProtectedPrimaryCommands;
+    PersistentCommandPool mProtectedPrimaryCommandPool;
 
     // Queue serial management.
     AtomicSerialFactory mQueueSerialFactory;
@@ -460,7 +460,7 @@ class CommandProcessor : public Context, public CommandQueueInterface
                      unsigned int line) override;
 
     // CommandQueueInterface
-    angle::Result init(Context *context, const DeviceQueueMap &qeueMap) override;
+    angle::Result init(Context *context, const DeviceQueueMap &queueMap) override;
 
     void destroy(Context *context) override;
 
@@ -513,6 +513,7 @@ class CommandProcessor : public Context, public CommandQueueInterface
     {
         return mCommandQueue.getDriverPriority(priority);
     }
+    uint32_t getDeviceQueueIndex() const { return mCommandQueue.getDeviceQueueIndex(); }
 
   private:
     bool hasPendingError() const
@@ -548,8 +549,6 @@ class CommandProcessor : public Context, public CommandQueueInterface
     mutable std::condition_variable mWorkerIdleCondition;
     // Track worker thread Idle state for assertion purposes
     bool mWorkerThreadIdle;
-    // Command pool to allocate processor thread primary command buffers from
-    CommandPool mCommandPool;
     CommandQueue mCommandQueue;
 
     mutable std::mutex mQueueSerialMutex;

src/libANGLE/renderer/vulkan/ContextVk.cpp
@@ -627,8 +627,8 @@ void ContextVk::onDestroy(const gl::Context *context)
     }
 
     // Recycle current commands buffers.
-    mRenderer->recycleCommandBufferHelper(mOutsideRenderPassCommands);
-    mRenderer->recycleCommandBufferHelper(mRenderPassCommands);
+    mRenderer->recycleCommandBufferHelper(device, mOutsideRenderPassCommands);
+    mRenderer->recycleCommandBufferHelper(device, mRenderPassCommands);
     mOutsideRenderPassCommands = nullptr;
     mRenderPassCommands        = nullptr;
 
@@ -752,8 +752,9 @@ angle::Result ContextVk::initialize()
     mEmulateSeamfulCubeMapSampling = shouldEmulateSeamfulCubeMapSampling();
 
     // Assign initial command buffers from queue
-    mOutsideRenderPassCommands = mRenderer->getCommandBufferHelper(false);
-    mRenderPassCommands        = mRenderer->getCommandBufferHelper(true);
+    ANGLE_TRY(
+        mRenderer->getCommandBufferHelper(this, false, &mCommandPool, &mOutsideRenderPassCommands));
+    ANGLE_TRY(mRenderer->getCommandBufferHelper(this, true, &mCommandPool, &mRenderPassCommands));
 
     if (mGpuEventsEnabled)
     {
@@ -2529,8 +2530,8 @@ void ContextVk::clearAllGarbage()
 
 void ContextVk::handleDeviceLost()
 {
-    mOutsideRenderPassCommands->reset();
-    mRenderPassCommands->reset();
+    (void)mOutsideRenderPassCommands->reset(this);
+    (void)mRenderPassCommands->reset(this);
     mRenderer->handleDeviceLost();
     clearAllGarbage();
 
@@ -5570,12 +5571,10 @@ angle::Result ContextVk::beginNewRenderPass(
     // Next end any currently outstanding renderPass
     ANGLE_TRY(flushCommandsAndEndRenderPass());
 
-    mRenderPassCommands->beginRenderPass(
-        framebuffer, renderArea, renderPassDesc, renderPassAttachmentOps, colorAttachmentCount,
-        depthStencilAttachmentIndex, clearValues, commandBufferOut);
     mPerfCounters.renderPasses++;
-
-    return angle::Result::Continue;
+    return mRenderPassCommands->beginRenderPass(
+        this, framebuffer, renderArea, renderPassDesc, renderPassAttachmentOps,
+        colorAttachmentCount, depthStencilAttachmentIndex, clearValues, commandBufferOut);
 }
 
 angle::Result ContextVk::startRenderPass(gl::Rectangle renderArea,
@@ -5690,7 +5689,7 @@ angle::Result ContextVk::flushCommandsAndEndRenderPassImpl()
 
     pauseTransformFeedbackIfActiveUnpaused();
 
-    mRenderPassCommands->endRenderPass(this);
+    ANGLE_TRY(mRenderPassCommands->endRenderPass(this));
 
     if (vk::CommandBufferHelper::kEnableCommandStreamDiagnostics)
     {
@@ -6145,12 +6144,14 @@ angle::Result ContextVk::onResourceAccess(const vk::CommandBufferAccess &access)
 {
     ANGLE_TRY(flushCommandBuffersIfNecessary(access));
 
+    vk::CommandBuffer *commandBuffer = &mOutsideRenderPassCommands->getCommandBuffer();
+
     for (const vk::CommandBufferImageAccess &imageAccess : access.getReadImages())
     {
         ASSERT(!IsRenderPassStartedAndUsesImage(*mRenderPassCommands, *imageAccess.image));
 
         imageAccess.image->recordReadBarrier(this, imageAccess.aspectFlags, imageAccess.imageLayout,
-                                             &mOutsideRenderPassCommands->getCommandBuffer());
+                                             commandBuffer);
         imageAccess.image->retain(&mResourceUseList);
     }
 
@@ -6158,9 +6159,8 @@ angle::Result ContextVk::onResourceAccess(const vk::CommandBufferAccess &access)
     {
         ASSERT(!IsRenderPassStartedAndUsesImage(*mRenderPassCommands, *imageWrite.access.image));
 
-        imageWrite.access.image->recordWriteBarrier(
-            this, imageWrite.access.aspectFlags, imageWrite.access.imageLayout,
-            &mOutsideRenderPassCommands->getCommandBuffer());
+        imageWrite.access.image->recordWriteBarrier(this, imageWrite.access.aspectFlags,
+                                                    imageWrite.access.imageLayout, commandBuffer);
         imageWrite.access.image->retain(&mResourceUseList);
         imageWrite.access.image->onWrite(imageWrite.levelStart, imageWrite.levelCount,
                                          imageWrite.layerStart, imageWrite.layerCount,

src/libANGLE/renderer/vulkan/RendererVk.cpp
@@ -777,11 +777,7 @@ void RendererVk::onDestroy(vk::Context *context)
     mYuvConversionCache.destroy(this);
     mVkFormatDescriptorCountMap.clear();
 
-    for (vk::CommandBufferHelper *commandBufferHelper : mCommandBufferHelperFreeList)
-    {
-        SafeDelete(commandBufferHelper);
-    }
-    mCommandBufferHelperFreeList.clear();
+    mCommandBufferRecycler.onDestroy();
 
     mAllocator.destroy();
 
@@ -3231,34 +3227,22 @@ VkResult RendererVk::queuePresent(vk::Context *context,
     return result;
 }
 
-vk::CommandBufferHelper *RendererVk::getCommandBufferHelper(bool hasRenderPass)
+angle::Result RendererVk::getCommandBufferHelper(vk::Context *context,
+                                                 bool hasRenderPass,
+                                                 vk::CommandPool *commandPool,
+                                                 vk::CommandBufferHelper **commandBufferHelperOut)
 {
     ANGLE_TRACE_EVENT0("gpu.angle", "RendererVk::getCommandBufferHelper");
-    std::unique_lock<std::mutex> lock(mCommandBufferHelperFreeListMutex);
-
-    if (mCommandBufferHelperFreeList.empty())
-    {
-        vk::CommandBufferHelper *commandBuffer = new vk::CommandBufferHelper();
-        commandBuffer->initialize(hasRenderPass);
-        return commandBuffer;
-    }
-    else
-    {
-        vk::CommandBufferHelper *commandBuffer = mCommandBufferHelperFreeList.back();
-        mCommandBufferHelperFreeList.pop_back();
-        commandBuffer->setHasRenderPass(hasRenderPass);
-        return commandBuffer;
-    }
+    std::unique_lock<std::mutex> lock(mCommandBufferRecyclerMutex);
+    return mCommandBufferRecycler.getCommandBufferHelper(context, hasRenderPass, commandPool,
+                                                         commandBufferHelperOut);
 }
 
-void RendererVk::recycleCommandBufferHelper(vk::CommandBufferHelper *commandBuffer)
+void RendererVk::recycleCommandBufferHelper(VkDevice device, vk::CommandBufferHelper *commandBuffer)
 {
     ANGLE_TRACE_EVENT0("gpu.angle", "RendererVk::recycleCommandBufferHelper");
-    std::lock_guard<std::mutex> lock(mCommandBufferHelperFreeListMutex);
-
-    ASSERT(commandBuffer->empty());
-    commandBuffer->markOpen();
-    mCommandBufferHelperFreeList.push_back(commandBuffer);
+    std::lock_guard<std::mutex> lock(mCommandBufferRecyclerMutex);
+    mCommandBufferRecycler.recycleCommandBufferHelper(device, commandBuffer);
 }
 
 void RendererVk::logCacheStats() const

src/libANGLE/renderer/vulkan/RendererVk.h
@@ -237,6 +237,11 @@ class RendererVk : angle::NonCopyable
                                          bool hasProtectedContent,
                                          vk::PrimaryCommandBuffer *commandBufferOut);
 
+    void resetSecondaryCommandBuffer(vk::CommandBuffer &&commandBuffer)
+    {
+        mCommandBufferRecycler.resetCommandBufferHelper(std::move(commandBuffer));
+    }
+
     // Fire off a single command buffer immediately with default priority.
     // Command buffer must be allocated with getCommandBufferOneOff and is reclaimed.
     angle::Result queueSubmitOneOff(vk::Context *context,
@@ -383,8 +388,11 @@ class RendererVk : angle::NonCopyable
                           egl::ContextPriority priority,
                           const VkPresentInfoKHR &presentInfo);
 
-    vk::CommandBufferHelper *getCommandBufferHelper(bool hasRenderPass);
-    void recycleCommandBufferHelper(vk::CommandBufferHelper *commandBuffer);
+    angle::Result getCommandBufferHelper(vk::Context *context,
+                                         bool hasRenderPass,
+                                         vk::CommandPool *commandPool,
+                                         vk::CommandBufferHelper **commandBufferHelperOut);
+    void recycleCommandBufferHelper(VkDevice device, vk::CommandBufferHelper *commandBuffer);
 
     // Process GPU memory reports
     void processMemoryReportCallback(const VkDeviceMemoryReportCallbackDataEXT &callbackData)
@@ -535,8 +543,8 @@ class RendererVk : angle::NonCopyable
     vk::CommandQueue mCommandQueue;
 
     // Command buffer pool management.
-    std::mutex mCommandBufferHelperFreeListMutex;
-    std::vector<vk::CommandBufferHelper *> mCommandBufferHelperFreeList;
+    std::mutex mCommandBufferRecyclerMutex;
+    vk::CommandBufferRecycler mCommandBufferRecycler;
 
     // Async Command Queue
     vk::CommandProcessor mCommandProcessor;

src/libANGLE/renderer/vulkan/SecondaryCommandBuffer.h
@@ -709,7 +709,8 @@ class SecondaryCommandBuffer final : angle::NonCopyable
     void reset()
     {
         mCommands.clear();
-        initialize(mAllocator);
+        mCurrentWritePointer   = nullptr;
+        mCurrentBytesRemaining = 0;
     }
 
     // This will cause the SecondaryCommandBuffer to become invalid by clearing its allocator

beaudie/vesc

commit 429b84d990c1d89ddcb88cf1e98e925b0d9152a9

Vulkan: Fix crash in async queue
The change also fixes a regression introduced by
2d79918fdb0276cdb57488d72abd803250f962a3 which caused a segfault when
async queue is used.

Unfortunately, due to anglebug.com/6437, async queue tests are not run
on the bots at the moment.  This will be fixed in a follow up change.

Bug: angleproject:6436
Change-Id: I3c2f63549149fbd9c9ab61107e98e8015cdb1c77

src/libANGLE/renderer/vulkan/CommandProcessor.cpp
@@ -102,6 +102,7 @@ void CommandProcessorTask::initTask()
     mRenderPass                  = nullptr;
     mCommandBuffer               = nullptr;
     mSemaphore                   = nullptr;
+    mCommandPool                 = nullptr;
     mOneOffFence                 = nullptr;
     mPresentInfo                 = {};
     mPresentInfo.pResults        = nullptr;
@@ -210,6 +211,7 @@ void CommandProcessorTask::initFlushAndQueueSubmit(
     const Semaphore *semaphore,
     bool hasProtectedContent,
     egl::ContextPriority priority,
+    CommandPool *commandPool,
     GarbageList &&currentGarbage,
     Serial submitQueueSerial)
 {
@@ -217,6 +219,7 @@ void CommandProcessorTask::initFlushAndQueueSubmit(
     mWaitSemaphores          = waitSemaphores;
     mWaitSemaphoreStageMasks = waitSemaphoreStageMasks;
     mSemaphore               = semaphore;
+    mCommandPool             = commandPool;
     mGarbage                 = std::move(currentGarbage);
     mPriority                = priority;
     mHasProtectedContent     = hasProtectedContent;
@@ -251,6 +254,7 @@ CommandProcessorTask &CommandProcessorTask::operator=(CommandProcessorTask &&rhs
     std::swap(mWaitSemaphoreStageMasks, rhs.mWaitSemaphoreStageMasks);
     std::swap(mSemaphore, rhs.mSemaphore);
     std::swap(mOneOffFence, rhs.mOneOffFence);
+    std::swap(mCommandPool, rhs.mCommandPool);
     std::swap(mGarbage, rhs.mGarbage);
     std::swap(mSerial, rhs.mSerial);
     std::swap(mPriority, rhs.mPriority);
@@ -430,8 +434,7 @@ angle::Result CommandProcessor::processTask(CommandProcessorTask *task)
             ANGLE_TRY(mCommandQueue.submitFrame(
                 this, task->hasProtectedContent(), task->getPriority(), task->getWaitSemaphores(),
                 task->getWaitSemaphoreStageMasks(), task->getSemaphore(),
-                std::move(task->getGarbage()), task->getCommandBuffer()->getCommandPool(),
-                task->getQueueSerial()));
+                std::move(task->getGarbage()), task->getCommandPool(), task->getQueueSerial()));
 
             ASSERT(task->getGarbage().empty());
             break;
@@ -654,8 +657,8 @@ angle::Result CommandProcessor::submitFrame(
 
     CommandProcessorTask task;
     task.initFlushAndQueueSubmit(waitSemaphores, waitSemaphoreStageMasks, signalSemaphore,
-                                 hasProtectedContent, priority, std::move(currentGarbage),
-                                 submitQueueSerial);
+                                 hasProtectedContent, priority, commandPool,
+                                 std::move(currentGarbage), submitQueueSerial);
 
     queueCommand(std::move(task));
 

src/libANGLE/renderer/vulkan/CommandProcessor.h
@@ -96,6 +96,7 @@ class CommandProcessorTask
                                  const Semaphore *semaphore,
                                  bool hasProtectedContent,
                                  egl::ContextPriority priority,
+                                 CommandPool *commandPool,
                                  GarbageList &&currentGarbage,
                                  Serial submitQueueSerial);
 
@@ -129,6 +130,7 @@ class CommandProcessorTask
     const VkPresentInfoKHR &getPresentInfo() const { return mPresentInfo; }
     const RenderPass *getRenderPass() const { return mRenderPass; }
     CommandBufferHelper *getCommandBuffer() const { return mCommandBuffer; }
+    CommandPool *getCommandPool() const { return mCommandPool; }
 
   private:
     void copyPresentInfo(const VkPresentInfoKHR &other);
@@ -143,6 +145,7 @@ class CommandProcessorTask
     std::vector<VkSemaphore> mWaitSemaphores;
     std::vector<VkPipelineStageFlags> mWaitSemaphoreStageMasks;
     const Semaphore *mSemaphore;
+    CommandPool *mCommandPool;
     GarbageList mGarbage;
 
     // FinishToSerial & Flush command data