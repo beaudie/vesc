78dd5e20ddc73473ac6db94fc2919682bf0a6f95
Tim Van Patten <timvp@google.com>
2021-10-05 17:21:45 -0600

    Vulkan: Improve detection/handling of angle::FormatID::NONE
    Format::initialize() assigns angle::FormatID::NONE to
    mActualSampleOnlyImageFormatID for some entries. This can result in NONE
    erroneously being used to query format feature bits, leading to bugs.
    This CL adds ASSERT checks to detect when NONE is being used in places
    where a valid format ID is required, such as in
    RendererVk::getFormatFeatureBits().
    
    UNREACHABLE() is also added to GetFormatIDFromVkFormat() to detect when
    a format is missing from vk_format_map.json, but ANGLE is expected to
    return a valid VK format.
    
    FormatTable::initialize() is also updated to avoid performing invalid
    intialization when:
      mActualSampleOnlyImageFormatID == angle::FormatID::NONE
    
    Bug: angleproject:6277
    Change-Id: Ia83e9f998de315d4dfe58245a7bb2883ee104afb

--- src/libANGLE/renderer/vulkan/RendererVk.cpp
+++ src/libANGLE/renderer/vulkan/RendererVk.cpp
@@ -3047,6 +3047,7 @@ template <VkFormatFeatureFlags VkFormatProperties::*features>
 VkFormatFeatureFlags RendererVk::getFormatFeatureBits(angle::FormatID formatID,
                                                       const VkFormatFeatureFlags featureBits) const
 {
+    ASSERT(formatID != angle::FormatID::NONE);
     VkFormatProperties &deviceProperties = mFormatProperties[formatID];
 
     if (deviceProperties.bufferFeatures == kInvalidFormatFeatureFlags)
@@ -3060,6 +3061,7 @@ VkFormatFeatureFlags RendererVk::getFormatFeatureBits(angle::FormatID formatID,
         }
 
         VkFormat vkFormat = vk::GetVkFormatFromFormatID(formatID);
+        ASSERT(vkFormat != VK_FORMAT_UNDEFINED);
 
         // Otherwise query the format features and cache it.
         vkGetPhysicalDeviceFormatProperties(mPhysicalDevice, vkFormat, &deviceProperties);
