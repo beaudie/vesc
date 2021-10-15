962b639d90611a69c7bfc7cabb9c550031ab7315
Tim Van Patten <timvp@google.com>
2021-10-15 11:01:25 -0600

    Vuklan: Find formats with mActualSampleOnlyImageFormatID NONE
    Find any formats with:
      mActualSampleOnlyImageFormatID == angle::FormatID::NONE
    
    Bug: angleproject:6277
    Change-Id: If2eeba992d09cf826f355a2d662fbc4b5922c4b6

--- src/libANGLE/renderer/vulkan/vk_format_utils.cpp
+++ src/libANGLE/renderer/vulkan/vk_format_utils.cpp
@@ -248,17 +248,30 @@ void FormatTable::initialize(RendererVk *renderer,
         format.initialize(renderer, intendedAngleFormat);
         format.mIntendedFormatID = intendedFormatID;
 
+        if (!format.valid())
+        {
+            continue;
+        }
+
+        if (intendedAngleFormat.isBlock)
+        {
+            outCompressedTextureFormats->push_back(format.mIntendedGLFormat);
+        }
+
+        if (format.mActualSampleOnlyImageFormatID == angle::FormatID::NONE)
+        {
+            // No sample-able or render-able formats, so nothing left to do.
+            WARN() << "TIMTIM >> format.mActualSampleOnlyImageFormatID = "
+                   << ToUnderlying(format.mActualSampleOnlyImageFormatID);
+            UNREACHABLE();
+        }
+
         if (format.mActualRenderableImageFormatID == angle::FormatID::NONE)
         {
             // If renderable format was not set, it means there is no fallback format for
             // renderable. We populate this the same formatID as sampleOnly formatID so that
             // getActualFormatID() will be simpler.
             format.mActualRenderableImageFormatID = format.mActualSampleOnlyImageFormatID;
-        }
-
-        if (!format.valid())
-        {
-            continue;
         }
 
         gl::TextureCaps textureCaps;
@@ -286,11 +299,6 @@ void FormatTable::initialize(RendererVk *renderer,
                 format.mRenderableTextureLoadFunctions = GetLoadFunctionsMap(
                     format.mIntendedGLFormat, format.mActualRenderableImageFormatID);
             }
-        }
-
-        if (intendedAngleFormat.isBlock)
-        {
-            outCompressedTextureFormats->push_back(format.mIntendedGLFormat);
         }
     }
 }
