2dd932cfcca4829574d03cc60acc774615e2273d
Jamie Madill <jmadill@chromium.org>
2021-10-18 10:44:38 -0400

    Vulkan: Implement robust shader outputs.
    We pass the mask of missing shader outputs down to pipeline creation
    so we can use the color mask bits to block SwiftShader writing to
    unused outputs. This fixes the undefined behaviour present in
    Genshin Impact.
    
    It was easier to mask out the color attachments in initialize
    rather than set up the pipeline desc to mask out the attachments.
    This was because we manipulate the color mask in a fairly complex
    way before we initialize the pipeline desc.
    
    Bug: angleproject:6566
    Change-Id: Icb7ecccb37ae8e0d7d5fef8968f0dd7ef6fe6150

--- include/GLES2/gl2ext_angle.h
+++ include/GLES2/gl2ext_angle.h
@@ -567,6 +567,11 @@ GL_APICALL void GL_APIENTRY glImportSemaphoreZirconHandleANGLE(GLuint memory,
 #define GL_SERIALIZED_CONTEXT_STRING_ANGLE 0x96B0
 #endif /* GL_ANGLE_get_serialized_context_string */
 
+#ifndef GL_ANGLE_robust_fragment_shader_output
+#define GL_ANGLE_robust_fragment_shader_output
+#define GL_ROBUST_FRAGMENT_SHADER_OUTPUT_ANGLE 0x96B9
+#endif /* GL_ANGLE_robust_fragment_shader_output */
+
 // clang-format on
 
 #endif  // INCLUDE_GLES2_GL2EXT_ANGLE_H_
