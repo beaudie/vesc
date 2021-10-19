70876bc767672e31e60024d09fbd4db29eb7388c
Jamie Madill <jmadill@chromium.org>
2021-10-19 11:55:17 -0400

    Vulkan: Implement robust shader outputs.
    We pass the mask of missing shader outputs down to pipeline creation
    so we can use the color mask bits to block SwiftShader writing to
    unused outputs. This fixes the undefined behaviour present in
    Genshin Impact.
    
    It was easier to mask out the color attachments in initialize
    rather than set up the pipeline desc to mask out the attachments.
    This was because we manipulate the color mask in a fairly complex
    way before we initialize the pipeline desc.
    
    Bug: angleproject:6566
    Change-Id: Ie659fcd511cd286fa573fd25e3e6a0b9e123ebd6

--- src/libANGLE/renderer/vulkan/FramebufferVk.cpp
+++ src/libANGLE/renderer/vulkan/FramebufferVk.cpp
@@ -1939,8 +1939,12 @@ angle::Result FramebufferVk::syncState(const gl::Context *context,
     // Deactivate Framebuffer
     mFramebuffer = nullptr;
 
-    // Notify the ContextVk to update the pipeline desc.
-    return contextVk->onFramebufferChange(this, command);
+    if (binding == GL_DRAW_FRAMEBUFFER)
+    {
+        ANGLE_TRY(contextVk->onFramebufferChange(this, command));
+    }
+
+    return angle::Result::Continue;
 }
 
 void FramebufferVk::updateRenderPassDesc(ContextVk *contextVk)
