62f1be24a972baba8b3f85a77961d9276bcfd28c
YuxinHu <yuxinhu@google.com>
2021-10-27 01:25:11 +0000

    Add detachShader capture calls in MEC
    This change adds a capture call to detach shader
    object before deleting the shader object in function
    GenerateLinkedProgram(). This ensures that
    the Shader Object refcound is decremented to 0 before the
    glDeleteShader is called on the Shader Object,
    and the ShaderProgramID assigned to the Shader Object
    is released and can be reused for
    subsequent glCreateShader calls.
    
    The game World Cricket Championship2 detaches the shader object
    before deletes the shader object, with this change, we can follow
    the same behavior in GenerateLinkedProgram(), and the total number
    of ShaderProgramID we need will be consistent between
    SetupReplayContextShared() and the game uses.
    We are allocating memory slots for gUniformLocations2 array
    based on the total number of ShaderProgramID the game uses.
    Keeping the ShaderProgramID counts the same between
    trace replay and the game ensures we allocate the right amount
    of memory for the trace replay.
    
    Bug: angleproject:6621
    Bug: b/194508692
    Change-Id: Ib2810b486a6e9108db2a32f084d650a25f14bebd

--- src/libANGLE/capture/FrameCapture.cpp
+++ src/libANGLE/capture/FrameCapture.cpp
@@ -2363,6 +2363,10 @@ void GenerateLinkedProgram(const gl::Context *context,
     // Bump up our max program ID before creating temp shaders
     resourceTracker->onShaderProgramAccess(id);
 
+    // A map to store the tempShaderIDs of the shaders we attached below. We need this map to
+    // retrieve the tempShaderIDs we will pass to detachShader call
+    std::unordered_map<gl::ShaderType, gl::ShaderProgramID> tempShaderIDTracker;
+
     // Use max ID as a temporary shader ID.
     // Note this isn't the real ID of the shader, just a lookup into the gShaderProgram map.
     gl::ShaderProgramID tempShaderID = {resourceTracker->getMaxShaderPrograms()};
@@ -2370,6 +2374,7 @@ void GenerateLinkedProgram(const gl::Context *context,
     // Compile with last linked sources.
     for (gl::ShaderType shaderType : program->getExecutable().getLinkedShaderStages())
     {
+        tempShaderIDTracker[shaderType] = tempShaderID;
         const std::string &sourceString = linkedSources[shaderType];
         const char *sourcePointer       = sourceString.c_str();
 
@@ -2391,7 +2396,7 @@ void GenerateLinkedProgram(const gl::Context *context,
                 CaptureShaderSource(replayState, true, tempShaderID, 1, &sourcePointer, nullptr));
         Capture(setupCalls, CaptureCompileShader(replayState, true, tempShaderID));
         Capture(setupCalls, CaptureAttachShader(replayState, true, id, tempShaderID));
-        Capture(setupCalls, CaptureDeleteShader(replayState, true, tempShaderID));
+        tempShaderID.value += 1;
     }
 
     // Gather XFB varyings
@@ -2456,6 +2461,21 @@ void GenerateLinkedProgram(const gl::Context *context,
         GLuint blockBinding = program->getUniformBlockBinding(uniformBlockIndex);
         Capture(setupCalls, CaptureUniformBlockBinding(replayState, true, id, {uniformBlockIndex},
                                                        blockBinding));
+    }
+
+    // add DetachShader call if that's what the app does, so that the shader ID can be released and
+    // reused after glDeleteShader call
+    for (gl::ShaderType shaderType : program->getExecutable().getLinkedShaderStages())
+    {
+
+        gl::Shader *attachedShader = program->getAttachedShader(shaderType);
+        if (attachedShader == nullptr)
+        {
+            Capture(setupCalls,
+                    CaptureDetachShader(replayState, true, id, tempShaderIDTracker[shaderType]));
+        }
+        Capture(setupCalls,
+                CaptureDeleteShader(replayState, true, tempShaderIDTracker[shaderType]));
     }
 }
 
