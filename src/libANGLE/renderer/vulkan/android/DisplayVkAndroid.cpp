33fd285a44ec76f3c97a5f3278b9923ea1c6652c
Ian Elliott <ianelliott@google.com>
2021-10-29 17:26:17 -0600

    Trying to use non-existent extension in ...
    DisplayVkAndroid: :generateConfigs().  Turned off for now
    
    Bug: b/203826952
    Change-Id: Iadbeef02fd8288a0641e66a0a84ad686481a1073

--- src/libANGLE/renderer/vulkan/android/DisplayVkAndroid.cpp
+++ src/libANGLE/renderer/vulkan/android/DisplayVkAndroid.cpp
@@ -20,6 +20,14 @@
 #include "libANGLE/renderer/vulkan/android/WindowSurfaceVkAndroid.h"
 #include "libANGLE/renderer/vulkan/vk_caps_utils.h"
 
+#ifdef OLD_CODE
+#else  // OLD_CODE
+#    include <android/log.h>
+#    include <unistd.h>
+#    undef INFO
+#    define INFO(...) __android_log_print(ANDROID_LOG_INFO, "ANGLE", __VA_ARGS__)
+#endif  // OLD_CODE
+
 namespace rx
 {
 
@@ -50,11 +58,60 @@ SurfaceImpl *DisplayVkAndroid::createWindowSurfaceVk(const egl::SurfaceState &st
 
 egl::ConfigSet DisplayVkAndroid::generateConfigs()
 {
+#define OLD_CODE
+#ifdef OLD_CODE
     // The list of supported swapchain formats is available at:
     // https://cs.android.com/android/platform/superproject/+/master:frameworks/native/vulkan/libvulkan/swapchain.cpp;l=465-486?q=GetNativePixelFormat
     // TODO (Issue 4062): Add conditional support for GL_RGB10_A2 and GL_RGBA16F when the
     // Android Vulkan loader adds conditional support for them.
     const std::array<GLenum, 2> kColorFormats = {GL_RGBA8, GL_RGB565};
+#else  // OLD_CODE
+    INFO("%s(): GOT TO HERE 1", __FUNCTION__);
+    std::vector<GLenum> kColorFormats = {GL_RGBA8, GL_RGB565};
+    INFO("%s(): GOT TO HERE 1.1", __FUNCTION__);
+    // TODO(ianelliott): Define a Vulkan extension and enable it in RendererVk::initialize().  For
+    // now, call the old function with a VK_NULL_HANDLE for the VkSurfaceKHR handle.
+    INFO("%s(): GOT TO HERE 1.2", __FUNCTION__);
+    VkPhysicalDeviceSurfaceInfo2KHR surfaceInfo2 = {};
+    surfaceInfo2.sType   = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_SURFACE_INFO_2_KHR;
+    surfaceInfo2.surface = VK_NULL_HANDLE;
+    INFO("%s(): GOT TO HERE 1.3", __FUNCTION__);
+
+    uint32_t surfaceFormatCount     = 0;
+    VkPhysicalDevice physicalDevice = mRenderer->getPhysicalDevice();
+    INFO("%s(): GOT TO HERE 1.4", __FUNCTION__);
+    VkResult /*Hack*/ result = vkGetPhysicalDeviceSurfaceFormats2KHR(physicalDevice, &surfaceInfo2,
+                                                                     &surfaceFormatCount, nullptr);
+    INFO("%s(): GOT TO HERE 2", __FUNCTION__);
+    if (result != VK_SUCCESS)
+        return egl::ConfigSet();
+
+    std::vector<VkSurfaceFormat2KHR> surfaceFormats2(surfaceFormatCount);
+    for (VkSurfaceFormat2KHR &surfaceFormat2 : surfaceFormats2)
+    {
+        surfaceFormat2.sType = VK_STRUCTURE_TYPE_SURFACE_FORMAT_2_KHR;
+    }
+    /*Hack*/ result = vkGetPhysicalDeviceSurfaceFormats2KHR(
+        physicalDevice, &surfaceInfo2, &surfaceFormatCount, surfaceFormats2.data());
+    if (result != VK_SUCCESS)
+        return egl::ConfigSet();
+    INFO("%s(): GOT TO HERE 3", __FUNCTION__);
+
+    for (VkSurfaceFormat2KHR &surfaceFormat2 : surfaceFormats2)
+    {
+        INFO("%s(): GOT TO HERE 3.1", __FUNCTION__);
+        if (std::find(kColorFormats.begin(), kColorFormats.end(),
+                      surfaceFormat2.surfaceFormat.format) == kColorFormats.end())
+        {
+            INFO("%s(): GOT TO HERE 3.1.1", __FUNCTION__);
+            kColorFormats.push_back(surfaceFormat2.surfaceFormat.format);
+        }
+
+        // TODO: Look at surfaceFormat2.surfaceFormat.colorSpace too
+    }
+
+#endif  // OLD_CODE
+    INFO("%s(): GOT TO HERE 4", __FUNCTION__);
 
     std::vector<GLenum> depthStencilFormats(
         egl_vk::kConfigDepthStencilFormats,
