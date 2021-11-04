2035239472afc429e24159b07d4414b940e7bca0
Jamie Madill <jmadill@chromium.org>
2021-11-04 10:16:16 -0400

    EGL: Add early exit when hitting invalid attribute.
    According to the EGL spec, we stop checking attributes when we
    encounter an invalid enum. That means it's valid for an application to
    pass in a list of attributes without EGL_NONE as long as one of them
    is invalid. To handle this, we add lazy attribute validation to the
    AttributeMap class, that gets triggered in the validation calls.
    
    We only implement the early exit validation for the config attributes
    to fix an EGL test that would access out of bounds memory.
    
    Bug: angleproject:6660
    Change-Id: I264d0f98b4ddd9e74187846e9e668270a6fbaee1

--- src/libANGLE/validationEGL.cpp
+++ src/libANGLE/validationEGL.cpp
@@ -376,11 +376,13 @@ bool ValidateConfigAttributes(const ValidationContext *val,
                               const Display *display,
                               const AttributeMap &attributes)
 {
+    ANGLE_VALIDATION_TRY(attributes.validate(val, display, ValidateConfigAttribute));
+
     for (const auto &attrib : attributes)
     {
-        ANGLE_VALIDATION_TRY(ValidateConfigAttribute(val, display, attrib.first));
-        ANGLE_VALIDATION_TRY(
-            ValidateConfigAttributeValue(val, display, attrib.first, attrib.second));
+        EGLAttrib pname = attrib.first;
+        EGLAttrib value = attrib.second;
+        ANGLE_VALIDATION_TRY(ValidateConfigAttributeValue(val, display, pname, value));
     }
 
     return true;
@@ -528,6 +530,8 @@ bool ValidateGetPlatformDisplayCommon(const ValidationContext *val,
             val->setError(EGL_BAD_CONFIG, "Bad platform type.");
             return false;
     }
+
+    attribMap.skipValidate();
 
     if (platform == EGL_PLATFORM_ANGLE_ANGLE)
     {
@@ -1180,6 +1184,8 @@ bool ValidateCreateSyncBase(const ValidationContext *val,
 {
     ANGLE_VALIDATION_TRY(ValidateDisplay(val, display));
 
+    attribs.skipValidate();
+
     gl::Context *currentContext  = val->eglThread->getContext();
     egl::Display *currentDisplay = currentContext ? currentContext->getDisplay() : nullptr;
 
@@ -1605,6 +1611,8 @@ bool ValidateCreateContext(const ValidationContext *val,
             return false;
         }
     }
+
+    attributes.skipValidate();
 
     // Get the requested client version (default is 1) and check it is 2 or 3.
     EGLAttrib clientMajorVersion = 1;
@@ -2103,6 +2111,8 @@ bool ValidateCreateWindowSurface(const ValidationContext *val,
 
     const DisplayExtensions &displayExtensions = display->getExtensions();
 
+    attributes.skipValidate();
+
     for (const auto &attributeIter : attributes)
     {
         EGLAttrib attribute = attributeIter.first;
@@ -2276,6 +2286,8 @@ bool ValidateCreatePbufferSurface(const ValidationContext *val,
 
     const DisplayExtensions &displayExtensions = display->getExtensions();
 
+    attributes.skipValidate();
+
     for (const auto &attributeIter : attributes)
     {
         EGLAttrib attribute = attributeIter.first;
@@ -2420,6 +2432,8 @@ bool ValidateCreatePbufferFromClientBuffer(const ValidationContext *val,
     ANGLE_VALIDATION_TRY(ValidateConfig(val, display, config));
 
     const DisplayExtensions &displayExtensions = display->getExtensions();
+
+    attributes.skipValidate();
 
     switch (buftype)
     {
@@ -2727,11 +2741,12 @@ bool ValidateCreatePixmapSurface(const ValidationContext *val,
 
     const DisplayExtensions &displayExtensions = display->getExtensions();
 
-    for (AttributeMap::const_iterator attributeIter = attributes.begin();
-         attributeIter != attributes.end(); attributeIter++)
+    attributes.skipValidate();
+
+    for (const auto &attributePair : attributes)
     {
-        EGLAttrib attribute = attributeIter->first;
-        EGLAttrib value     = attributeIter->second;
+        EGLAttrib attribute = attributePair.first;
+        EGLAttrib value     = attributePair.second;
 
         switch (attribute)
         {
@@ -2924,6 +2939,8 @@ bool ValidateCreateImage(const ValidationContext *val,
 {
 
     ANGLE_VALIDATION_TRY(ValidateDisplay(val, display));
+
+    attributes.skipValidate();
 
     const DisplayExtensions &displayExtensions = display->getExtensions();
 
@@ -3799,6 +3816,8 @@ bool ValidateCreateStreamKHR(const ValidationContext *val,
         return false;
     }
 
+    attributes.skipValidate();
+
     for (const auto &attributeIter : attributes)
     {
         EGLAttrib attribute = attributeIter.first;
@@ -4079,6 +4098,9 @@ bool ValidateStreamConsumerGLTextureExternalAttribsNV(const ValidationContext *v
     {
         plane[i] = -1;
     }
+
+    attribs.skipValidate();
+
     for (const auto &attributeIter : attribs)
     {
         EGLAttrib attribute = attributeIter.first;
@@ -5230,6 +5252,8 @@ bool ValidateDebugMessageControlKHR(const ValidationContext *val,
         val->setError(EGL_BAD_ACCESS, "EGL_KHR_debug extension is not available.");
         return false;
     }
+
+    attribs.skipValidate();
 
     for (const auto &attrib : attribs)
     {
