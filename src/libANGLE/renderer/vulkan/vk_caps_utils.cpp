4e9a123343ae4bc311d44e9b8fdd6d3a693eaaf9
Cheryl Wei <cheryl.wei@arm.com>
2021-11-17 13:42:59 +0800

    Vulkan: Change numbers of bits defined for mediump and lowp integers
    Change 32-bit to 16-bit for mediump and lowp shader variable integers in
    ARM platform.
    
    Bug: angleproject:6645
    Change-Id: I30faa9efcfcd08c48da9f2d4f2afa699933a15ee

--- src/libANGLE/renderer/vulkan/vk_caps_utils.cpp
+++ src/libANGLE/renderer/vulkan/vk_caps_utils.cpp
@@ -615,11 +615,15 @@ void RendererVk::ensureCapsInitialized() const
 
     // Can't find documentation on the int precision in Vulkan.
     mNativeCaps.vertexHighpInt.setTwosComplementInt(32);
-    mNativeCaps.vertexMediumpInt.setTwosComplementInt(32);
-    mNativeCaps.vertexLowpInt.setTwosComplementInt(32);
+    mNativeCaps.vertexMediumpInt.setTwosComplementInt(
+        IsARM(mPhysicalDeviceProperties.vendorID) ? 16 : 32);
+    mNativeCaps.vertexLowpInt.setTwosComplementInt(IsARM(mPhysicalDeviceProperties.vendorID) ? 16
+                                                                                             : 32);
     mNativeCaps.fragmentHighpInt.setTwosComplementInt(32);
-    mNativeCaps.fragmentMediumpInt.setTwosComplementInt(32);
-    mNativeCaps.fragmentLowpInt.setTwosComplementInt(32);
+    mNativeCaps.fragmentMediumpInt.setTwosComplementInt(
+        IsARM(mPhysicalDeviceProperties.vendorID) ? 16 : 32);
+    mNativeCaps.fragmentLowpInt.setTwosComplementInt(
+        IsARM(mPhysicalDeviceProperties.vendorID) ? 16 : 32);
 
     // Compute shader limits.
     mNativeCaps.maxComputeWorkGroupCount[0] = LimitToInt(limitsVk.maxComputeWorkGroupCount[0]);
