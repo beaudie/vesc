97dee2f1e3043572246b2e77951d3211816c3e33
Gert Wollny <gert.wollny@collabora.com>
2021-11-29 21:01:03 +0100

    Capture/Replay: test and handle texture gen-on-bind
    Bug: angleproject:6425
    Change-Id: Ieb4c0a7a6ffdbf0690c07c10e001835a0dcbb6e1

--- src/libANGLE/capture/FrameCapture.cpp
+++ src/libANGLE/capture/FrameCapture.cpp
@@ -4068,6 +4068,13 @@ struct ParamValueTrait<gl::RenderbufferID>
     static const ParamType typeID     = ParamType::TRenderbufferID;
 };
 
+template <>
+struct ParamValueTrait<gl::TextureID>
+{
+    static constexpr const char *name = "texturePacked";
+    static const ParamType typeID     = ParamType::TTextureID;
+};
+
 }  // namespace
 
 ParamCapture::ParamCapture() : type(ParamType::TGLenum), enumGroup(gl::GLenumGroup::DefaultGroup) {}
@@ -5010,6 +5017,10 @@ void FrameCaptureShared::maybeCapturePreCallUpdates(
             }
             break;
         }
+
+        case EntryPoint::GLBindTexture:
+            maybeGenResourceOnBind<gl::TextureID>(call);
+            break;
 
         case EntryPoint::GLDeleteBuffers:
         {
