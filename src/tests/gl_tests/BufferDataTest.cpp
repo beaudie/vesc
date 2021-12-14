4506088059b00bfe585e52c1396db5b3edbc895c
Shrek Shao <shrekshao@google.com>
2021-12-14 12:55:39 -0800

    BufferSubDataTest.SmallIndexBufferUpdateAfterDraw testSetUp fix
    Failing on Linux FYI GPU TSAN Release, although already skipped.
    Changing SetUp/TearDown to testSetup/testTearDown to avoid assert
    failures before skipped.
    
    Bug: angleproject:6810
    Change-Id: Ibc746293fd0ad823e5b82c2d3db2862751b90f99

--- src/tests/gl_tests/BufferDataTest.cpp
+++ src/tests/gl_tests/BufferDataTest.cpp
@@ -205,11 +205,10 @@ TEST_P(BufferDataTest, RepeatedDrawDynamicBug)
 
 using BufferSubDataTestParams = angle::PlatformParameters;
 
-class BufferSubDataTest : public ANGLETestBase,
-                          public ::testing::TestWithParam<BufferSubDataTestParams>
+class BufferSubDataTest : public ANGLETestWithParam<BufferSubDataTestParams>
 {
   protected:
-    BufferSubDataTest() : ANGLETestBase(GetParam())
+    BufferSubDataTest()
     {
         setWindowWidth(16);
         setWindowHeight(16);
@@ -222,10 +221,8 @@ class BufferSubDataTest : public ANGLETestBase,
         mBuffer = 0;
     }
 
-    void SetUp() override
+    void testSetUp() override
     {
-        ANGLETestBase::ANGLETestSetUp();
-
         glGenBuffers(1, &mBuffer);
         ASSERT_NE(mBuffer, 0U);
 
@@ -238,11 +235,7 @@ class BufferSubDataTest : public ANGLETestBase,
         ASSERT_GL_NO_ERROR();
     }
 
-    void TearDown() override
-    {
-        glDeleteBuffers(1, &mBuffer);
-        ANGLETestBase::ANGLETestTearDown();
-    }
+    void testTearDown() override { glDeleteBuffers(1, &mBuffer); }
     GLuint mBuffer;
 };
 
