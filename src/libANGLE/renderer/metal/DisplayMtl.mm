18058d39cf873d275014fdeea9271b338b3a103e
Alexey Knyazev <lexa.knyazev@gmail.com>
2021-12-15 22:57:13 +0400

    Metal: Enable OES_fbo_render_mipmap
    Bug: angleproject:6813
    Change-Id: I643c114d7b37ca0532d2dda1964090a15e470842

--- src/libANGLE/renderer/metal/DisplayMtl.mm
+++ src/libANGLE/renderer/metal/DisplayMtl.mm
@@ -867,6 +867,7 @@ void DisplayMtl::initializeExtensions() const
     mNativeExtensions.drawBuffersEXT                = true;
     mNativeExtensions.drawBuffersIndexedEXT         = true;
     mNativeExtensions.drawBuffersIndexedOES         = true;
+    mNativeExtensions.fboRenderMipmapOES            = true;
     mNativeExtensions.fragDepthEXT                  = true;
     mNativeExtensions.framebufferBlitANGLE          = true;
     mNativeExtensions.framebufferBlitNV             = true;
