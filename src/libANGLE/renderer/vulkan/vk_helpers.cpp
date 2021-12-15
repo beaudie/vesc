41f438725f9c09d5037530038b4cbe22c504b005
Charlie Lao <cclao@google.com>
2021-12-14 16:05:24 -0800

    Vulkan: Use buffer memory barrier
    Bug: b/205337962
    Change-Id: I5a0a46d4790b973b888a8088a154aa17778677c7

--- src/libANGLE/renderer/vulkan/vk_helpers.cpp
+++ src/libANGLE/renderer/vulkan/vk_helpers.cpp
@@ -4200,6 +4200,24 @@ bool BufferHelper::isReleasedToExternal() const
 #endif
 }
 
+ANGLE_INLINE void BufferHelper::initBufferMemoryBarrierStruct(
+    VkPipelineStageFlags srcStageMask,
+    VkPipelineStageFlags dstStageMask,
+    VkFlags srcAccess,
+    VkFlags dstAccess,
+    VkBufferMemoryBarrier *bufferMemoryBarrier) const
+{
+    ASSERT(mSubAllocation.valid());
+    bufferMemoryBarrier->sType               = VK_STRUCTURE_TYPE_BUFFER_MEMORY_BARRIER;
+    bufferMemoryBarrier->srcAccessMask       = srcAccess;
+    bufferMemoryBarrier->dstAccessMask       = dstAccess;
+    bufferMemoryBarrier->srcQueueFamilyIndex = mCurrentQueueFamilyIndex;
+    bufferMemoryBarrier->dstQueueFamilyIndex = mCurrentQueueFamilyIndex;
+    bufferMemoryBarrier->buffer              = getBuffer().getHandle();
+    bufferMemoryBarrier->offset              = getOffset();
+    bufferMemoryBarrier->size                = getSize();
+}
+
 bool BufferHelper::recordReadBarrier(VkAccessFlags readAccessType,
                                      VkPipelineStageFlags readStage,
                                      PipelineBarrier *barrier)
@@ -4210,8 +4228,10 @@ bool BufferHelper::recordReadBarrier(VkAccessFlags readAccessType,
     if (mCurrentWriteAccess != 0 && (((mCurrentReadAccess & readAccessType) != readAccessType) ||
                                      ((mCurrentReadStages & readStage) != readStage)))
     {
-        barrier->mergeMemoryBarrier(mCurrentWriteStages, readStage, mCurrentWriteAccess,
-                                    readAccessType);
+        VkBufferMemoryBarrier bufferMemoryBarrier = {};
+        initBufferMemoryBarrierStruct(mCurrentWriteStages, readStage, mCurrentWriteAccess,
+                                      readAccessType, &bufferMemoryBarrier);
+        barrier->mergeBufferBarrier(mCurrentWriteStages, readStage, bufferMemoryBarrier);
         barrierModified = true;
     }
 
@@ -4232,8 +4252,11 @@ bool BufferHelper::recordWriteBarrier(VkAccessFlags writeAccessType,
            (mCurrentReadStages && mCurrentReadAccess));
     if (mCurrentReadAccess != 0 || mCurrentWriteAccess != 0)
     {
-        VkPipelineStageFlags srcStageMask = mCurrentWriteStages | mCurrentReadStages;
-        barrier->mergeMemoryBarrier(srcStageMask, writeStage, mCurrentWriteAccess, writeAccessType);
+        VkPipelineStageFlags srcStageMask         = mCurrentWriteStages | mCurrentReadStages;
+        VkBufferMemoryBarrier bufferMemoryBarrier = {};
+        initBufferMemoryBarrierStruct(srcStageMask, writeStage, mCurrentWriteAccess,
+                                      writeAccessType, &bufferMemoryBarrier);
+        barrier->mergeBufferBarrier(srcStageMask, writeStage, bufferMemoryBarrier);
         barrierModified = true;
     }
 
