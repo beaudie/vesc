f4bb23b19f12cb087dc355e2a52ff6b361fbde20
Charlie Lao <cclao@google.com>
2021-12-20 10:55:41 -0800

    Vulkan: Add mutex to protect mVirtualBlock
    mVirtualBlock could be accessed from multiple thread when
    asyncCommandQueue is enabled, where the free call could come from the
    submission thread. This CL adds a mVirtualBlockMutex in BufferBlock and
    always take the lock when mVirtualBlock is been accessed.
    
    This CL also adds ConditionalMutex class  for the general usage that a
    mutex is only used based on a boolean.
    
    Bug: angleproject:6840
    Change-Id: Ib647b4ff12ebfc08f2a70192d39f16e15a1fc5f5

--- src/common/angleutils.h
+++ src/common/angleutils.h
@@ -238,6 +238,32 @@ inline bool IsLittleEndian()
     return isLittleEndian;
 }
 
+// Helper class to use a mutex with the control of boolean.
+class ConditionalMutex final : angle::NonCopyable
+{
+  public:
+    ConditionalMutex() : mUseMutex(true) {}
+    void init(bool useMutex) { mUseMutex = useMutex; }
+    void lock()
+    {
+        if (mUseMutex)
+        {
+            mMutex.lock();
+        }
+    }
+    void unlock()
+    {
+        if (mUseMutex)
+        {
+            mMutex.unlock();
+        }
+    }
+
+  private:
+    std::mutex mMutex;
+    bool mUseMutex;
+};
+
 // snprintf is not defined with MSVC prior to to msvc14
 #if defined(_MSC_VER) && _MSC_VER < 1900
 #    define snprintf _snprintf
