5836a0fcd4263c55f7cb99d3ce1bf19dcf0083cd
Charlie Lao <cclao@google.com>
2021-12-23 09:55:27 -0800

    Vulkan: Use RendererScopedPtr for RAII an object
    Bug: b/208323792
    Change-Id: I643ea3aa0128f4b0dc761574faec9653d2c97e44

--- src/libANGLE/renderer/vulkan/vk_helpers.cpp
+++ src/libANGLE/renderer/vulkan/vk_helpers.cpp
@@ -5986,8 +5986,7 @@ angle::Result ImageHelper::stageSubresourceUpdateImpl(ContextVk *contextVk,
         }
     }
 
-    std::unique_ptr<RefCounted<BufferHelper>> stagingBuffer =
-        std::make_unique<RefCounted<BufferHelper>>();
+    RendererScopedPtr<RefCounted<BufferHelper>> stagingBuffer(contextVk->getRenderer());
     BufferHelper *currentBuffer = &stagingBuffer->get();
 
     ANGLE_TRY(currentBuffer->initStagingBuffer(contextVk, allocationSize));
@@ -6154,8 +6153,7 @@ angle::Result ImageHelper::reformatStagedBufferUpdates(ContextVk *contextVk,
                 uint8_t *srcData            = srcBuffer->getMappedMemory() + copy.bufferOffset;
 
                 // Allocate memory with dstFormat
-                std::unique_ptr<RefCounted<BufferHelper>> stagingBuffer =
-                    std::make_unique<RefCounted<BufferHelper>>();
+                RendererScopedPtr<RefCounted<BufferHelper>> stagingBuffer(renderer);
                 BufferHelper *dstBuffer = &stagingBuffer->get();
 
                 GLuint dstBufferSize = dstDataDepthPitch * copy.imageExtent.depth;
@@ -6374,8 +6372,7 @@ angle::Result ImageHelper::stageSubresourceUpdateAndGetData(ContextVk *contextVk
                                                             uint8_t **destData,
                                                             angle::FormatID formatID)
 {
-    std::unique_ptr<RefCounted<BufferHelper>> stagingBuffer =
-        std::make_unique<RefCounted<BufferHelper>>();
+    RendererScopedPtr<RefCounted<BufferHelper>> stagingBuffer(contextVk->getRenderer());
     BufferHelper *currentBuffer = &stagingBuffer->get();
 
     ANGLE_TRY(currentBuffer->initStagingBuffer(contextVk, allocationSize));
@@ -6443,8 +6440,7 @@ angle::Result ImageHelper::stageSubresourceUpdateFromFramebuffer(
     size_t outputRowPitch   = storageFormat.pixelBytes * clippedRectangle.width;
     size_t outputDepthPitch = outputRowPitch * clippedRectangle.height;
 
-    std::unique_ptr<RefCounted<BufferHelper>> stagingBuffer =
-        std::make_unique<RefCounted<BufferHelper>>();
+    RendererScopedPtr<RefCounted<BufferHelper>> stagingBuffer(renderer);
     BufferHelper *currentBuffer = &stagingBuffer->get();
 
     // The destination is only one layer deep.
@@ -6613,8 +6609,7 @@ angle::Result ImageHelper::stageRobustResourceClearWithFormat(ContextVk *context
         ANGLE_VK_CHECK_MATH(contextVk,
                             formatInfo.computeCompressedImageSize(glExtents, &totalSize));
 
-        std::unique_ptr<RefCounted<BufferHelper>> stagingBuffer =
-            std::make_unique<RefCounted<BufferHelper>>();
+        RendererScopedPtr<RefCounted<BufferHelper>> stagingBuffer(contextVk->getRenderer());
         BufferHelper *currentBuffer = &stagingBuffer->get();
 
         ANGLE_TRY(currentBuffer->initStagingBuffer(contextVk, totalSize));
