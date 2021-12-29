3606c6a9ff3371768effe4642d73b8a18fd08901
Tim Van Patten <timvp@google.com>
2021-12-28 20:37:33 -0700

    Decide GL_KHR_parallel_shader_compile in backends
    GL_KHR_parallel_shader_compile was previously being enabled
    unconditionally in the front end. However, some backends (Vulkan)
    perform worse with parallel shader compilation. This CL moves the
    decision of enabling GL_KHR_parallel_shader_compile to the backends. All
    backends except Vulkan will continue to have the extension enabled.
    Vulkan will disable the extension and always perform single-threaded
    shader compilation.
    
    Bug: angleproject:6748
    Change-Id: Ic8d3a183f397608f3002a05480deb976dfe44792

--- src/libANGLE/renderer/metal/DisplayMtl.mm
+++ src/libANGLE/renderer/metal/DisplayMtl.mm
@@ -477,6 +477,9 @@ void DisplayMtl::generateExtensions(egl::DisplayExtensions *outExtensions) const
 
     // EGL_ANGLE_metal_create_context_ownership_identity
     outExtensions->metalCreateContextOwnershipIdentityANGLE = true;
+
+    // GL_KHR_parallel_shader_compile
+    mNativeExtensions.parallelShaderCompileKHR = false;
 }
 
 void DisplayMtl::generateCaps(egl::Caps *outCaps) const {}
