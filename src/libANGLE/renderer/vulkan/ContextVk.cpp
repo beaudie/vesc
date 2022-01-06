1c552a492dfd2973d7fc556da3f55d6c91a61499
Amirali Abdolrashidi <abdolrashidi@google.com>
2022-01-06 10:38:25 -0800

    Vulkan: Invalidate depth/stencil unconditionally
    * Updated doDeferredAcquireNextImage(), so depth/stencil
    content is invalidated during swapImpl() regardless of
    whether render pass optimization is required.
    
    Bug: angleproject:6861
    Change-Id: I47fab18afe5b178bf55c2df3df712c40118aff1f

--- src/libANGLE/renderer/vulkan/ContextVk.cpp
+++ src/libANGLE/renderer/vulkan/ContextVk.cpp
@@ -3147,21 +3147,21 @@ void ContextVk::optimizeRenderPassForPresent(VkFramebuffer framebufferHandle)
         return;
     }
 
-    // EGL1.5 spec: The contents of ancillary buffers are always undefined after calling
-    // eglSwapBuffers
-    RenderTargetVk *depthStencilRenderTarget = mDrawFramebuffer->getDepthStencilRenderTarget();
-    if (depthStencilRenderTarget)
-    {
-        // Change depth/stencil attachment storeOp to DONT_CARE
-        const gl::DepthStencilState &dsState = mState.getDepthStencilState();
-        mRenderPassCommands->invalidateRenderPassStencilAttachment(
-            dsState, mRenderPassCommands->getRenderArea());
-        mRenderPassCommands->invalidateRenderPassDepthAttachment(
-            dsState, mRenderPassCommands->getRenderArea());
-        // Mark content as invalid so that we will not load them in next renderpass
-        depthStencilRenderTarget->invalidateEntireContent(this);
-        depthStencilRenderTarget->invalidateEntireStencilContent(this);
-    }
+    //    // EGL1.5 spec: The contents of ancillary buffers are always undefined after calling
+    //    // eglSwapBuffers
+    //    RenderTargetVk *depthStencilRenderTarget =
+    //    mDrawFramebuffer->getDepthStencilRenderTarget(); if (depthStencilRenderTarget)
+    //    {
+    //        // Change depth/stencil attachment storeOp to DONT_CARE
+    //        const gl::DepthStencilState &dsState = mState.getDepthStencilState();
+    //        mRenderPassCommands->invalidateRenderPassStencilAttachment(
+    //            dsState, mRenderPassCommands->getRenderArea());
+    //        mRenderPassCommands->invalidateRenderPassDepthAttachment(
+    //            dsState, mRenderPassCommands->getRenderArea());
+    //        // Mark content as invalid so that we will not load them in next renderpass
+    //        depthStencilRenderTarget->invalidateEntireContent(this);
+    //        depthStencilRenderTarget->invalidateEntireStencilContent(this);
+    //    }
 
     // Use finalLayout instead of extra barrier for layout change to present
     vk::ImageHelper &image = color0RenderTarget->getImageForWrite();
