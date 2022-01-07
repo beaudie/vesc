c84a732306c283c280b3ed01abcaddcc5f4be766
Mohan Maiya <m.maiya@samsung.com>
2022-01-07 07:02:53 -0800

    Account for EGL 1.5 version when dealing with robustness
    EGL 1.5 spec allows for EGL_CONTEXT_OPENGL_RESET_NOTIFICATION_STRATEGY
    and EGL_CONTEXT_OPENGL_ROBUST_ACCESS enums to be passed in to
    eglCreateContext. Update the validation layer and queries to account
    for the new enums.
    
    Bug: angleproject:4039
    Change-Id: I7088e0dca08cea2cfdcf1877b6d999c0e0336e5e

--- src/libANGLE/validationEGL.cpp
+++ src/libANGLE/validationEGL.cpp
@@ -1444,6 +1444,7 @@ bool ValidateCreateContextAttribute(const ValidationContext *val,
             return false;
 
         case EGL_CONTEXT_OPENGL_RESET_NOTIFICATION_STRATEGY_EXT:
+        case EGL_CONTEXT_OPENGL_RESET_NOTIFICATION_STRATEGY:
             if (!display->getExtensions().createContextRobustness)
             {
                 val->setError(EGL_BAD_ATTRIBUTE);
@@ -1676,6 +1677,7 @@ bool ValidateCreateContextAttributeValue(const ValidationContext *val,
             break;
 
         case EGL_CONTEXT_OPENGL_RESET_NOTIFICATION_STRATEGY_EXT:
+        case EGL_CONTEXT_OPENGL_RESET_NOTIFICATION_STRATEGY:
             if (value != EGL_LOSE_CONTEXT_ON_RESET_EXT && value != EGL_NO_RESET_NOTIFICATION_EXT)
             {
                 val->setError(EGL_BAD_ATTRIBUTE);
