e50f01c072cacf17fe3ef2a3a9c63ed4cba28943
Mohan Maiya <m.maiya@samsung.com>
2022-01-07 12:06:34 -0800

    Add missing implementation for glGetnUniformuiv()
    GL_KHR_robustness became core in GLES 3.2 and added
    glGetnUniformuiv() function in gles_3_2 entry points but this
    function was missing implementation in Context.cpp.
    
    Also fix validation for glGetnUniform*v and glFramebufferTexture
    
    Bug: angleproject:6593
    Tests: KHR-NoContext.es32.robustness.getnuniform
    Tests: dEQP-GLES31.functional.debug.negative_coverage.get_error.state.get_nuniform*iv
    Tests: dEQP-GLES31.functional.debug.negative_coverage.get_error.buffer.framebuffer_texture
    Change-Id: Ic26190e59d448d93ae9e2e0d2a7faff14c3776f8

--- src/libANGLE/validationES.cpp
+++ src/libANGLE/validationES.cpp
@@ -4412,12 +4412,12 @@ bool ValidateGetUniformBase(const Context *context,
     return true;
 }
 
-static bool ValidateSizedGetUniform(const Context *context,
-                                    angle::EntryPoint entryPoint,
-                                    ShaderProgramID program,
-                                    UniformLocation location,
-                                    GLsizei bufSize,
-                                    GLsizei *length)
+bool ValidateSizedGetUniform(const Context *context,
+                             angle::EntryPoint entryPoint,
+                             ShaderProgramID program,
+                             UniformLocation location,
+                             GLsizei bufSize,
+                             GLsizei *length)
 {
     if (length)
     {
@@ -4431,7 +4431,7 @@ static bool ValidateSizedGetUniform(const Context *context,
 
     if (bufSize < 0)
     {
-        context->validationError(entryPoint, GL_INVALID_VALUE, kNegativeBufferSize);
+        context->validationError(entryPoint, GL_INVALID_OPERATION, kNegativeBufferSize);
         return false;
     }
 
