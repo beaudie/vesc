d215595d159f30419fbb963dc8979beeda130e7d
Peng Huang <penghuang@chromium.org>
2022-01-11 13:09:55 -0500

    Wrap some global vulkan methods which depend on env variables
    ANGLE scoped sets some envion variables during initialization.
    Those env variables will change some vulkan global methods behavior.
    So to make sure those global methods have same behavior, this CL
    wraps those global methods, and sets those envion variables before
    call those vulkan methods.
    
    Bug: chromium:1264439
    Change-Id: Ie4e29884fcf5b67f4e53ddd3c90a9ade7b226a1f

--- src/libANGLE/renderer/vulkan/DeviceVk.cpp
+++ src/libANGLE/renderer/vulkan/DeviceVk.cpp
@@ -12,16 +12,115 @@
 #include <stdint.h>
 
 #include "common/debug.h"
+#include "common/vulkan/vulkan_icd.h"
 #include "libANGLE/Display.h"
 #include "libANGLE/renderer/vulkan/DisplayVk.h"
 #include "libANGLE/renderer/vulkan/RendererVk.h"
 
 namespace rx
 {
+namespace
+{
 
-DeviceVk::DeviceVk() = default;
+DeviceVk *gDevice = nullptr;
 
-DeviceVk::~DeviceVk() = default;
+class ScopedEnv : public angle::vk::ScopedVkLoaderEnvironment
+{
+  public:
+    ScopedEnv()
+        : angle::vk::ScopedVkLoaderEnvironment(gDevice->getRenderer()->getEnableValidationLayers(),
+                                               gDevice->getRenderer()->getEnabledICD())
+    {}
+};
+
+VKAPI_ATTR VkResult VKAPI_CALL WrappedCreateInstance(const VkInstanceCreateInfo *pCreateInfo,
+                                                     const VkAllocationCallbacks *pAllocator,
+                                                     VkInstance *pInstance)
+{
+    ScopedEnv scopedEnv;
+    return vkCreateInstance(pCreateInfo, pAllocator, pInstance);
+}
+
+VKAPI_ATTR VkResult VKAPI_CALL
+WrappedEnumerateInstanceExtensionProperties(const char *pLayerName,
+                                            uint32_t *pPropertyCount,
+                                            VkExtensionProperties *pProperties)
+{
+    ScopedEnv scopedEnv;
+    return vkEnumerateInstanceExtensionProperties(pLayerName, pPropertyCount, pProperties);
+}
+
+VKAPI_ATTR VkResult VKAPI_CALL
+WrappedEnumerateInstanceLayerProperties(uint32_t *pPropertyCount, VkLayerProperties *pProperties)
+{
+    ScopedEnv scopedEnv;
+    return vkEnumerateInstanceLayerProperties(pPropertyCount, pProperties);
+}
+
+VKAPI_ATTR VkResult VKAPI_CALL WrappedEnumerateInstanceVersion(uint32_t *pApiVersion)
+{
+    ScopedEnv scopedEnv;
+    return vkEnumerateInstanceVersion(pApiVersion);
+}
+
+VKAPI_ATTR PFN_vkVoidFunction VKAPI_CALL WrappedGetInstanceProcAddr(VkInstance instance,
+                                                                    const char *pName)
+{
+    if (!pName || pName[0] != 'v' || pName[1] != 'k')
+    {
+        return nullptr;
+    }
+
+    if (instance != VK_NULL_HANDLE)
+    {
+        return vkGetInstanceProcAddr(instance, pName);
+    }
+
+    if (strcmp(pName, "vkEnumerateInstanceVersion") == 0 && !vkGetInstanceProcAddr(instance, pName))
+    {
+        return nullptr;
+    }
+
+    struct NameAndFunction
+    {
+        const char *name;
+        PFN_vkVoidFunction pfn;
+    };
+    static const NameAndFunction kWrappedFunctions[] = {
+        {"vkCreateInstance", reinterpret_cast<PFN_vkVoidFunction>(WrappedCreateInstance)},
+        {"vkEnumerateInstanceExtensionProperties",
+         reinterpret_cast<PFN_vkVoidFunction>(WrappedEnumerateInstanceExtensionProperties)},
+        {"vkEnumerateInstanceLayerProperties",
+         reinterpret_cast<PFN_vkVoidFunction>(WrappedEnumerateInstanceLayerProperties)},
+        {"vkEnumerateInstanceVersion",
+         reinterpret_cast<PFN_vkVoidFunction>(WrappedEnumerateInstanceVersion)},
+        {"vkGetInstanceProcAddr", reinterpret_cast<PFN_vkVoidFunction>(WrappedGetInstanceProcAddr)},
+    };
+
+    for (const NameAndFunction &fun : kWrappedFunctions)
+    {
+        if (strcmp(pName, fun.name) == 0)
+        {
+            return fun.pfn;
+        }
+    }
+
+    return vkGetInstanceProcAddr(instance, pName);
+}
+
+}  // namespace
+
+DeviceVk::DeviceVk()
+{
+    ASSERT(!gDevice);
+    gDevice = this;
+}
+
+DeviceVk::~DeviceVk()
+{
+    ASSERT(gDevice == this);
+    gDevice = nullptr;
+}
 
 egl::Error DeviceVk::initialize()
 {
@@ -32,63 +131,65 @@ egl::Error DeviceVk::getAttribute(const egl::Display *display, EGLint attribute,
 {
     RendererVk *renderer =
         static_cast<rx::DisplayVk *>(display->getImplementation())->getRenderer();
+    ASSERT(mRenderer == nullptr || mRenderer == renderer);
+    mRenderer = renderer;
     switch (attribute)
     {
         case EGL_VULKAN_VERSION_ANGLE:
         {
-            auto version = static_cast<intptr_t>(renderer->getApiVersion());
+            auto version = static_cast<intptr_t>(mRenderer->getApiVersion());
             *outValue    = reinterpret_cast<void *>(version);
             return egl::NoError();
         }
         case EGL_VULKAN_INSTANCE_ANGLE:
         {
-            *outValue = renderer->getInstance();
+            *outValue = mRenderer->getInstance();
             return egl::NoError();
         }
         case EGL_VULKAN_DEVICE_ANGLE:
         {
-            *outValue = renderer->getDevice();
+            *outValue = mRenderer->getDevice();
             return egl::NoError();
         }
         case EGL_VULKAN_PHYSICAL_DEVICE_ANGLE:
         {
-            *outValue = renderer->getPhysicalDevice();
+            *outValue = mRenderer->getPhysicalDevice();
             return egl::NoError();
         }
         case EGL_VULKAN_QUEUE_ANGLE:
         {
             // egl::ContextPriority::Medium is the default context priority.
-            *outValue = renderer->getQueue(egl::ContextPriority::Medium);
+            *outValue = mRenderer->getQueue(egl::ContextPriority::Medium);
             return egl::NoError();
         }
         case EGL_VULKAN_QUEUE_FAMILIY_INDEX_ANGLE:
         {
-            intptr_t index = static_cast<intptr_t>(renderer->getQueueFamilyIndex());
+            intptr_t index = static_cast<intptr_t>(mRenderer->getQueueFamilyIndex());
             *outValue      = reinterpret_cast<void *>(index);
             return egl::NoError();
         }
         case EGL_VULKAN_DEVICE_EXTENSIONS_ANGLE:
         {
-            char **extensions = const_cast<char **>(renderer->getEnabledDeviceExtensions().data());
+            char **extensions = const_cast<char **>(mRenderer->getEnabledDeviceExtensions().data());
             *outValue         = reinterpret_cast<void *>(extensions);
             return egl::NoError();
         }
         case EGL_VULKAN_INSTANCE_EXTENSIONS_ANGLE:
         {
             char **extensions =
-                const_cast<char **>(renderer->getEnabledInstanceExtensions().data());
+                const_cast<char **>(mRenderer->getEnabledInstanceExtensions().data());
             *outValue = reinterpret_cast<void *>(extensions);
             return egl::NoError();
         }
         case EGL_VULKAN_FEATURES_ANGLE:
         {
-            const auto *features = &renderer->getEnabledFeatures();
+            const auto *features = &mRenderer->getEnabledFeatures();
             *outValue            = const_cast<void *>(reinterpret_cast<const void *>(features));
             return egl::NoError();
         }
         case EGL_VULKAN_GET_INSTANCE_PROC_ADDR:
         {
-            *outValue = reinterpret_cast<void *>(vkGetInstanceProcAddr);
+            *outValue = reinterpret_cast<void *>(WrappedGetInstanceProcAddr);
             return egl::NoError();
         }
         default:
