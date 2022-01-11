08c4af37d99dd319b13f4c25057f65cdfa0b1ea6
Peng Huang <penghuang@chromium.org>
2022-01-11 13:09:55 -0500

    Wrap some global vulkan methods which depend on env variables
    ANGLE scoped sets some envion variables during initialization.
    Those env variables will change some vulkan global methods behavior.
    So to make sure those global methods have same behavior, this CL
    wraps those global methods, and sets those envion variables before
    call those vulkan methods.
    
    Bug: chromium:1264439
    Change-Id: Ie4e29884fcf5b67f4e53ddd3c90a9ade7b226a1f

--- src/libANGLE/renderer/vulkan/DeviceVk.h
+++ src/libANGLE/renderer/vulkan/DeviceVk.h
@@ -12,8 +12,12 @@
 
 #include "libANGLE/renderer/DeviceImpl.h"
 
+#include "common/vulkan/vk_headers.h"
+
 namespace rx
 {
+
+class RendererVk;
 
 class DeviceVk : public DeviceImpl
 {
@@ -27,6 +31,25 @@ class DeviceVk : public DeviceImpl
                             void **outValue) override;
     EGLint getType() override;
     void generateExtensions(egl::DeviceExtensions *outExtensions) const override;
+    RendererVk *getRenderer() const { return mRenderer; }
+
+  private:
+    static VKAPI_ATTR VkResult VKAPI_CALL
+    WrappedCreateInstance(const VkInstanceCreateInfo *pCreateInfo,
+                          const VkAllocationCallbacks *pAllocator,
+                          VkInstance *pInstance);
+    static VKAPI_ATTR VkResult VKAPI_CALL
+    WrappedEnumerateInstanceExtensionProperties(const char *pLayerName,
+                                                uint32_t *pPropertyCount,
+                                                VkExtensionProperties *pProperties);
+    static VKAPI_ATTR VkResult VKAPI_CALL
+    WrappedEnumerateInstanceLayerProperties(uint32_t *pPropertyCount,
+                                            VkLayerProperties *pProperties);
+    static VKAPI_ATTR VkResult VKAPI_CALL WrappedEnumerateInstanceVersion(uint32_t *pApiVersion);
+    static VKAPI_ATTR PFN_vkVoidFunction VKAPI_CALL WrappedGetInstanceProcAddr(VkInstance instance,
+                                                                               const char *pName);
+
+    RendererVk *mRenderer = nullptr;
 };
 
 }  // namespace rx
