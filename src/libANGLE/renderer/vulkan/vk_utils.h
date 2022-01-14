1f2e87b001611e303618d39269f3de20a4ba5863
Tim Van Patten <timvp@google.com>
2022-01-13 20:00:24 -0700

    Vulkan: Use vkCmdClearAttachments for scissored clears
    Use vkCmdClearAttachments() for scissored clears rather than vkDrawCmd.
    
    Bug: angleproject:5194
    Test: VulkanPerformanceCounterTest.InceptionScissorClears/*
    Change-Id: Ib9aefccece9a40f95f6f901cdff73a14e3d12121

--- src/libANGLE/renderer/vulkan/vk_utils.h
+++ src/libANGLE/renderer/vulkan/vk_utils.h
@@ -1117,6 +1117,7 @@ struct PerfCounters
     uint32_t shaderBuffersDescriptorSetCacheMisses;
     uint32_t buffersGhosted;
     uint32_t vertexArraySyncStateCalls;
+    uint32_t vkCmdClearAttachments;
 };
 
 // A Vulkan image level index.
