e7af1d89ae417e78a7d66f779b748243a090459e
Tim Van Patten <timvp@google.com>
2022-01-13 20:00:24 -0700

    Vulkan: Use vkCmdClearAttachments for scissored clears
    Use vkCmdClearAttachments() for scissored clears rather than vkDrawCmd.
    
    Here are the results of ClearBenchmark.Run/vulkan_scissoredClear on
    Pixel 6:
    Before:
        [ RUN      ] ClearBenchmark.Run/vulkan_scissoredClear
        *RESULT Clear_vulkan.cpu_time: scissoredClear= 1473.1687851453 ns
        *RESULT Clear_vulkan.wall_time: scissoredClear= 1205.4494506141 ns
        *RESULT Clear_vulkan.gpu_time: scissoredClear= 893.3167845498 ns
        [       OK ] ClearBenchmark.Run/vulkan_scissoredClear (9160 ms)
    
    After:
        [ RUN      ] ClearBenchmark.Run/vulkan_scissoredClear
        Clear_vulkan.cpu_time: scissoredClear= 1327.6442658209 ns
        Clear_vulkan.wall_time: scissoredClear= 1114.3499904306 ns
        Clear_vulkan.gpu_time: scissoredClear= 822.3592952574 ns
        [       OK ] ClearBenchmark.Run/vulkan_scissoredClear (9281 ms)
    
    Bug: angleproject:5194
    Bug: b/166809097
    Test: VulkanPerformanceCounterTest.InceptionScissorClears/*
    Test: VulkanPerformanceCounterTest.Depth16Scissored/*
    Change-Id: Ib9aefccece9a40f95f6f901cdff73a14e3d12121

--- src/libANGLE/renderer/vulkan/vk_utils.h
+++ src/libANGLE/renderer/vulkan/vk_utils.h
@@ -1284,6 +1284,7 @@ struct PerfCounters
     uint32_t shaderBuffersDescriptorSetCacheMisses;
     uint32_t buffersGhosted;
     uint32_t vertexArraySyncStateCalls;
+    uint32_t vkCmdClearAttachments;
 };
 
 // A Vulkan image level index.
