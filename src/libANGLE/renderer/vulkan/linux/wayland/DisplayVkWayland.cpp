eb3e59edd1e6716e3b2d9a4b93b5f6d9ecfa8fc4
Antonio Caggiano <antonio.caggiano@collabora.com>
2022-01-17 11:32:41 +0100

    Vulkan: Wayland dmabuf extensions
    Implement EGL extensions image dmabuf import and import modifiers in
    Vulkan Wayland Display.
    
    Bug: angleproject:7065
    Change-Id: I91535db3581cbe51f0eddc429e2752641c568148
    Signed-off-by: Antonio Caggiano <antonio.caggiano@collabora.com>
    Signed-off-by: Constantine Shablya <constantine.shablya@collabora.com>

--- src/libANGLE/renderer/vulkan/linux/wayland/DisplayVkWayland.cpp
+++ src/libANGLE/renderer/vulkan/linux/wayland/DisplayVkWayland.cpp
@@ -11,13 +11,103 @@
 
 #include <wayland-client.h>
 
+#include "common/angleutils.h"
+#include "common/linux/dma_buf_utils.h"
 #include "common/system_utils.h"
 #include "libANGLE/Display.h"
+#include "libANGLE/renderer/vulkan/RendererVk.h"
 #include "libANGLE/renderer/vulkan/linux/wayland/WindowSurfaceVkWayland.h"
 #include "libANGLE/renderer/vulkan/vk_caps_utils.h"
 
 namespace rx
 {
+
+// Returns the list of DRM modifiers that a VkFormat supports
+std::vector<VkDrmFormatModifierPropertiesEXT> GetDrmModifiers(const DisplayVk *displayVk,
+                                                              VkFormat vkFormat)
+{
+    RendererVk *renderer = displayVk->getRenderer();
+
+    // Query list of drm format modifiers compatible with VkFormat.
+    VkDrmFormatModifierPropertiesListEXT formatModifierPropertiesList = {};
+    formatModifierPropertiesList.sType = VK_STRUCTURE_TYPE_DRM_FORMAT_MODIFIER_PROPERTIES_LIST_EXT;
+    formatModifierPropertiesList.drmFormatModifierCount = 0;
+
+    VkFormatProperties2 formatProperties = {};
+    formatProperties.sType               = VK_STRUCTURE_TYPE_FORMAT_PROPERTIES_2;
+    formatProperties.pNext               = &formatModifierPropertiesList;
+
+    vkGetPhysicalDeviceFormatProperties2(renderer->getPhysicalDevice(), vkFormat,
+                                         &formatProperties);
+
+    std::vector<VkDrmFormatModifierPropertiesEXT> formatModifierProperties(
+        formatModifierPropertiesList.drmFormatModifierCount);
+    formatModifierPropertiesList.pDrmFormatModifierProperties = formatModifierProperties.data();
+
+    vkGetPhysicalDeviceFormatProperties2(renderer->getPhysicalDevice(), vkFormat,
+                                         &formatProperties);
+
+    return formatModifierProperties;
+}
+
+// Returns true if that VkFormat has at least on format modifier in its properties
+bool SupportsDrmModifiers(const DisplayVk *displayVk, VkFormat vkFormat)
+{
+    RendererVk *renderer = displayVk->getRenderer();
+
+    // Query list of drm format modifiers compatible with VkFormat.
+    VkDrmFormatModifierPropertiesListEXT formatModifierPropertiesList = {};
+    formatModifierPropertiesList.sType = VK_STRUCTURE_TYPE_DRM_FORMAT_MODIFIER_PROPERTIES_LIST_EXT;
+    formatModifierPropertiesList.drmFormatModifierCount = 0;
+
+    VkFormatProperties2 formatProperties = {};
+    formatProperties.sType               = VK_STRUCTURE_TYPE_FORMAT_PROPERTIES_2;
+    formatProperties.pNext               = &formatModifierPropertiesList;
+
+    vkGetPhysicalDeviceFormatProperties2(renderer->getPhysicalDevice(), vkFormat,
+                                         &formatProperties);
+
+    // If there is at least one DRM format modifier, it is supported
+    return formatModifierPropertiesList.drmFormatModifierCount > 0;
+}
+
+// Returns a list of VkFormats supporting at least one DRM format modifier
+std::vector<VkFormat> GetVkFormatsWithDrmModifiers(const DisplayVk *displayVk)
+{
+    RendererVk *renderer = displayVk->getRenderer();
+
+    std::vector<VkFormat> vkFormats;
+    for (size_t formatIndex = 1; formatIndex < angle::kNumANGLEFormats; ++formatIndex)
+    {
+        const vk::Format &format = renderer->getFormat(angle::FormatID(formatIndex));
+        VkFormat vkFormat        = format.getActualImageVkFormat(rx::vk::ImageAccess::Renderable);
+
+        if (vkFormat != VK_FORMAT_UNDEFINED && SupportsDrmModifiers(displayVk, vkFormat))
+        {
+            vkFormats.push_back(vkFormat);
+        }
+    }
+
+    return vkFormats;
+}
+
+// Returns a list of supported DRM formats
+std::vector<EGLint> GetDrmFormats(const DisplayVk *displayVk)
+{
+    std::unordered_set<EGLint> drmFormatsSet;
+    for (VkFormat vkFormat : GetVkFormatsWithDrmModifiers(displayVk))
+    {
+        std::vector<EGLint> drmFormats = angle::VkFormatToDrmFourCCFormat(vkFormat);
+        for (EGLint drmFormat : drmFormats)
+        {
+            drmFormatsSet.insert(drmFormat);
+        }
+    }
+
+    std::vector<EGLint> drmFormats;
+    std::copy(std::begin(drmFormatsSet), std::end(drmFormatsSet), std::back_inserter(drmFormats));
+    return drmFormats;
+}
 
 DisplayVkWayland::DisplayVkWayland(const egl::DisplayState &state)
     : DisplayVkLinux(state), mWaylandDisplay(nullptr)
@@ -99,5 +189,87 @@ bool IsVulkanWaylandDisplayAvailable()
 DisplayImpl *CreateVulkanWaylandDisplay(const egl::DisplayState &state)
 {
     return new DisplayVkWayland(state);
+}
+
+bool DisplayVkWayland::supportsDmaBufFormat(EGLint format) const
+{
+    std::vector<EGLint> drmFormats = GetDrmFormats(this);
+    return std::find(std::begin(drmFormats), std::end(drmFormats), format) != std::end(drmFormats);
+}
+
+egl::Error DisplayVkWayland::queryDmaBufFormats(EGLint maxFormats,
+                                                EGLint *formats,
+                                                EGLint *numFormats)
+{
+    std::vector<EGLint> drmFormats = GetDrmFormats(this);
+
+    EGLint formatsSize = static_cast<EGLint>(drmFormats.size());
+    if (maxFormats == 0)
+    {
+        *numFormats = formatsSize;
+    }
+    else if (maxFormats > 0)
+    {
+        // Do not copy data beyond the limits of the vector
+        maxFormats = std::min(maxFormats, formatsSize);
+        std::memcpy(formats, drmFormats.data(), maxFormats * sizeof(EGLint));
+    }
+
+    return egl::NoError();
+}
+
+egl::Error DisplayVkWayland::queryDmaBufModifiers(EGLint format,
+                                                  EGLint maxModifiers,
+                                                  EGLuint64KHR *modifiers,
+                                                  EGLBoolean *externalOnly,
+                                                  EGLint *numModifiers)
+{
+    std::vector<VkFormat> vkFormats = angle::DrmFourCCFormatToVkFormats(format);
+
+    std::vector<EGLuint64KHR> drmModifiers;
+    for (size_t i = 0; i < vkFormats.size(); ++i)
+    {
+        VkFormat vkFmt = vkFormats[i];
+
+        std::vector<VkDrmFormatModifierPropertiesEXT> vkDrmMods = GetDrmModifiers(this, vkFmt);
+
+        std::vector<EGLuint64KHR> drmMods(vkDrmMods.size());
+        std::transform(std::begin(vkDrmMods), std::end(vkDrmMods), std::begin(drmMods),
+                       [](VkDrmFormatModifierPropertiesEXT props) {
+                           return static_cast<EGLuint64KHR>(props.drmFormatModifier);
+                       });
+        std::sort(std::begin(drmMods), std::end(drmMods));
+
+        if (i == 0)
+        {
+            // Just take the modifiers for the first format
+            drmModifiers = drmMods;
+        }
+        else
+        {
+            // Intersect the modifiers of all the other associated Vulkan formats
+            std::vector<EGLuint64KHR> prevMods = drmModifiers;
+            drmModifiers.clear();
+            std::set_intersection(std::begin(drmMods), std::end(drmMods), std::begin(prevMods),
+                                  std::end(prevMods), std::back_inserter(drmModifiers));
+        }
+    }
+
+    EGLint drmModifiersSize = static_cast<EGLint>(drmModifiers.size());
+
+    if (maxModifiers == 0)
+    {
+        *numModifiers = drmModifiersSize;
+    }
+    else if (maxModifiers > 0)
+    {
+        maxModifiers = std::min(maxModifiers, drmModifiersSize);
+        for (EGLint i = 0; i < maxModifiers; ++i)
+        {
+            modifiers[i] = drmModifiers[i];
+        }
+    }
+
+    return egl::NoError();
 }
 }  // namespace rx
