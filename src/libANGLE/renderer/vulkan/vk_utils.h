d3ae612f6f0ab7942ac107cd977e8802e83a69f0
Charlie Lao <cclao@google.com>
2022-01-19 18:22:56 -0800

    Vulkan: Use Vulkan API directly for BufferPool's buffer allocation
    With buffer allocation suballocated from the BufferPool, each
    BufferBlock in the BufferPool has much bigger size (usually 4M), there
    is no need to use VMA to do the allocation for BufferBlock. This CL
    skips VMA and calls into vulkan directly for BufferBlock's buffer
    allocation. The main benefit is that after this CL, BufferBlock will be
    using VkDeviceMemory instead of Allocator (which is VMA's object). Since
    external buffer object has to use VkDeviceMemory because VMA does not
    support external memory, switching to VkDeviceMemory allows us to
    converge the external memory code path and normal buffer code paths into
    one code path.
    
    Bug: b/205337962
    Change-Id: I7c183ab0fd7d9aceb6cf416b0214c300798bc010

--- src/libANGLE/renderer/vulkan/vk_utils.h
+++ src/libANGLE/renderer/vulkan/vk_utils.h
@@ -946,19 +946,20 @@ class BufferBlock final : angle::NonCopyable
     angle::Result init(ContextVk *contextVk,
                        Buffer &buffer,
                        vma::VirtualBlockCreateFlags flags,
-                       Allocation &allocation,
+                       DeviceMemory &deviceMemory,
                        VkMemoryPropertyFlags memoryPropertyFlags,
                        VkDeviceSize size);
     void initWithoutVirtualBlock(ContextVk *contextVk,
                                  Buffer &buffer,
-                                 Allocation &allocation,
+                                 DeviceMemory &deviceMemory,
                                  VkMemoryPropertyFlags memoryPropertyFlags,
                                  VkDeviceSize size);
 
     BufferBlock &operator=(BufferBlock &&other);
 
     Buffer *getBuffer();
-    const Allocation &getAllocation() const;
+    const DeviceMemory &getDeviceMemory() const;
+    DeviceMemory *getDeviceMemory() { return &mDeviceMemory; }
     BufferSerial getBufferSerial() const { return mSerial; }
 
     VkMemoryPropertyFlags getMemoryPropertyFlags() const;
@@ -972,8 +973,8 @@ class BufferBlock final : angle::NonCopyable
     bool isHostVisible() const;
     bool isCoherent() const;
     bool isMapped() const;
-    angle::Result map(ContextVk *contextVk);
-    void unmap(const Allocator &allocator);
+    VkResult map(const VkDevice device);
+    void unmap(const VkDevice device);
     uint8_t *getMappedMemory() const;
 
     // This should be called whenever this found to be empty. The total number of count of empty is
@@ -987,7 +988,7 @@ class BufferBlock final : angle::NonCopyable
     VirtualBlock mVirtualBlock;
 
     Buffer mBuffer;
-    Allocation mAllocation;
+    DeviceMemory mDeviceMemory;
     VkMemoryPropertyFlags mMemoryPropertyFlags;
     VkDeviceSize mSize;
     uint8_t *mMappedMemory;
@@ -1045,21 +1046,21 @@ class BufferSuballocation final : public WrappedObject<BufferSuballocation, VmaB
     VkResult init(VkDevice device, BufferBlock *block, VkDeviceSize offset, VkDeviceSize size);
     VkResult initWithEntireBuffer(ContextVk *contextVk,
                                   Buffer &buffer,
-                                  Allocation &allocation,
+                                  DeviceMemory &deviceMemory,
                                   VkMemoryPropertyFlags memoryPropertyFlags,
                                   VkDeviceSize size);
 
     BufferBlock *getBlock() const;
     const Buffer &getBuffer() const;
     VkDeviceSize getSize() const;
-    const Allocation &getAllocation() const;
+    const DeviceMemory &getDeviceMemory() const;
     VkMemoryMapFlags getMemoryPropertyFlags() const;
     bool isHostVisible() const;
     bool isCoherent() const;
     bool isMapped() const;
     uint8_t *getMappedMemory() const;
-    void flush(const Allocator &allocator) const;
-    void invalidate(const Allocator &allocator) const;
+    void flush(const VkDevice &device);
+    void invalidate(const VkDevice &device);
     VkDeviceSize getOffset() const;
 };
 
@@ -1069,9 +1070,9 @@ ANGLE_INLINE Buffer *BufferBlock::getBuffer()
     return &mBuffer;
 }
 
-ANGLE_INLINE const Allocation &BufferBlock::getAllocation() const
+ANGLE_INLINE const DeviceMemory &BufferBlock::getDeviceMemory() const
 {
-    return mAllocation;
+    return mDeviceMemory;
 }
 
 ANGLE_INLINE VkMemoryPropertyFlags BufferBlock::getMemoryPropertyFlags() const
@@ -1144,14 +1145,14 @@ ANGLE_INLINE VkResult BufferSuballocation::init(VkDevice device,
 ANGLE_INLINE VkResult
 BufferSuballocation::initWithEntireBuffer(ContextVk *contextVk,
                                           Buffer &buffer,
-                                          Allocation &allocation,
+                                          DeviceMemory &deviceMemory,
                                           VkMemoryPropertyFlags memoryPropertyFlags,
                                           VkDeviceSize size)
 {
     ASSERT(!valid());
 
     std::unique_ptr<BufferBlock> block = std::make_unique<BufferBlock>();
-    block->initWithoutVirtualBlock(contextVk, buffer, allocation, memoryPropertyFlags, size);
+    block->initWithoutVirtualBlock(contextVk, buffer, deviceMemory, memoryPropertyFlags, size);
 
     VmaBufferSuballocation vmaBufferSuballocation = new VmaBufferSuballocation_T;
     if (vmaBufferSuballocation == nullptr)
@@ -1184,9 +1185,9 @@ ANGLE_INLINE VkDeviceSize BufferSuballocation::getSize() const
     return mHandle->mSize;
 }
 
-ANGLE_INLINE const Allocation &BufferSuballocation::getAllocation() const
+ANGLE_INLINE const DeviceMemory &BufferSuballocation::getDeviceMemory() const
 {
-    return mHandle->mBufferBlock->getAllocation();
+    return *mHandle->mBufferBlock->getDeviceMemory();
 }
 
 ANGLE_INLINE VkMemoryMapFlags BufferSuballocation::getMemoryPropertyFlags() const
@@ -1210,13 +1211,29 @@ ANGLE_INLINE uint8_t *BufferSuballocation::getMappedMemory() const
 {
     return mHandle->mBufferBlock->getMappedMemory() + getOffset();
 }
-ANGLE_INLINE void BufferSuballocation::flush(const Allocator &allocator) const
+ANGLE_INLINE void BufferSuballocation::flush(const VkDevice &device)
 {
-    mHandle->mBufferBlock->getAllocation().flush(allocator, getOffset(), mHandle->mSize);
+    if (!isCoherent())
+    {
+        VkMappedMemoryRange mappedRange = {};
+        mappedRange.sType               = VK_STRUCTURE_TYPE_MAPPED_MEMORY_RANGE;
+        mappedRange.memory              = mHandle->mBufferBlock->getDeviceMemory()->getHandle();
+        mappedRange.offset              = getOffset();
+        mappedRange.size                = mHandle->mSize;
+        mHandle->mBufferBlock->getDeviceMemory()->flush(device, mappedRange);
+    }
 }
-ANGLE_INLINE void BufferSuballocation::invalidate(const Allocator &allocator) const
+ANGLE_INLINE void BufferSuballocation::invalidate(const VkDevice &device)
 {
-    mHandle->mBufferBlock->getAllocation().invalidate(allocator, getOffset(), mHandle->mSize);
+    if (!isCoherent())
+    {
+        VkMappedMemoryRange mappedRange = {};
+        mappedRange.sType               = VK_STRUCTURE_TYPE_MAPPED_MEMORY_RANGE;
+        mappedRange.memory              = mHandle->mBufferBlock->getDeviceMemory()->getHandle();
+        mappedRange.offset              = getOffset();
+        mappedRange.size                = mHandle->mSize;
+        mHandle->mBufferBlock->getDeviceMemory()->invalidate(device, mappedRange);
+    }
 }
 
 ANGLE_INLINE VkDeviceSize BufferSuballocation::getOffset() const
