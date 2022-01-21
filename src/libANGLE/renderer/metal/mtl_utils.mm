6aca4b0e6895d04bf60ad412bb17ea71639a0cfe
Alexey Knyazev <lexa.knyazev@gmail.com>
2022-01-21 14:59:47 +0400

    Add DXT1CompressedTextureTestWebGL2.InitializeTextureContents
    Bug: chromium:1289428
    Change-Id: Ieac6b99a4b19a34d0d87b7577882760ea395c605

--- src/libANGLE/renderer/metal/mtl_utils.mm
+++ src/libANGLE/renderer/metal/mtl_utils.mm
@@ -374,8 +374,15 @@ angle::Result InitializeTextureContents(const gl::Context *context,
     // Intiialize the content to black
     GLint layer, startDepth;
     GetSliceAndDepth(index, &layer, &startDepth);
-    if (texture->isCPUAccessible() && index.getType() != gl::TextureType::_2DMultisample &&
-        index.getType() != gl::TextureType::_2DMultisampleArray && !forceGPUInitialization)
+
+    // When emulated, opaque ETC2 formats may have uncompressed actual ANGLE format.
+    if (intendedInternalFormat.compressed && textureObjFormat.actualAngleFormat().isBlock)
+    {
+        return InitializeCompressedTextureContents(context, texture, textureObjFormat, index, layer,
+                                                   startDepth);
+    }
+    else if (texture->isCPUAccessible() && index.getType() != gl::TextureType::_2DMultisample &&
+             index.getType() != gl::TextureType::_2DMultisampleArray && !forceGPUInitialization)
     {
         const angle::Format &dstFormat = angle::Format::Get(textureObjFormat.actualFormatId);
         const size_t dstRowPitch       = dstFormat.pixelBytes * size.width;
@@ -417,11 +424,6 @@ angle::Result InitializeTextureContents(const gl::Context *context,
                                          conversionRow.data(), dstRowPitch);
             }
         }
-    }
-    else if (intendedInternalFormat.compressed)
-    {
-        return InitializeCompressedTextureContents(context, texture, textureObjFormat, index, layer,
-                                                   startDepth);
     }
     else
     {
