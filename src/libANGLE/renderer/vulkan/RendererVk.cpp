d83f3501e0ea6d592aa968aae35fd826fa6db144
Charlie Lao <cclao@google.com>
2022-01-24 11:50:27 -0800

    Vulkan: Use queue instead of vector for mSharedGarbageList
    The garbage collector code is iterating the mSharedGarbageList from
    begin to end and if the garbage object is completed, we destroy it and
    erase it from the vector. This is done one by one. The problem with that
    is each erase call end up moving the remaining vector to ahead by one,
    and this process is repeated for each completed element. This is O(N^2)
    problem and really magnifies when you have many garbage objects. Since
    we only add garbage when it is GPU pending, so most of these garbages
    are in sort of fifo order anyway. This CL changes the garbage list from
    vector to queue so that erase becomes pop and this becomes a O(N)
    problem. This reduces CPU overhead as measured with minimize-gpu-work
    for gardenscapes from 10ms to 2ms due to suballocation generates lots of
    garbages. But this CL is a general improvement for the garbage
    collection performance.
    
    Bug: b/215768827
    Change-Id: I002aadf75f958d8b79eb6281a2608597776e908d

--- src/libANGLE/renderer/vulkan/RendererVk.cpp
+++ src/libANGLE/renderer/vulkan/RendererVk.cpp
@@ -3383,27 +3383,30 @@ bool RendererVk::haveSameFormatFeatureBits(angle::FormatID formatID1,
            hasImageFormatFeatureBits(formatID2, fmt1OptimalFeatureBits);
 }
 
+// Destroy completed garbages in FIFO order until hitting pending garbage.
 angle::Result RendererVk::cleanupGarbage(Serial lastCompletedQueueSerial)
 {
+    vk::SharedGarbageList remainingGarbage;
     std::lock_guard<std::mutex> lock(mGarbageMutex);
-
-    for (auto garbageIter = mSharedGarbage.begin(); garbageIter != mSharedGarbage.end();)
+    while (!mSharedGarbage.empty())
     {
-        // Possibly 'counter' should be always zero when we add the object to garbage.
-        vk::SharedGarbage &garbage = *garbageIter;
-        if (garbage.destroyIfComplete(this, lastCompletedQueueSerial))
+        vk::SharedGarbage &garbage = mSharedGarbage.front();
+        if (!garbage.destroyIfComplete(this, lastCompletedQueueSerial))
         {
-            garbageIter = mSharedGarbage.erase(garbageIter);
+            remainingGarbage.push(std::move(garbage));
         }
-        else
-        {
-            garbageIter++;
-        }
+        mSharedGarbage.pop();
+    }
+
+    if (!remainingGarbage.empty())
+    {
+        mSharedGarbage = std::move(remainingGarbage);
     }
 
     return angle::Result::Continue;
 }
 
+// Destroy all completed garbages, skip the pending garbage from other contexts.
 void RendererVk::cleanupCompletedCommandsGarbage()
 {
     (void)cleanupGarbage(getLastCompletedQueueSerial());
