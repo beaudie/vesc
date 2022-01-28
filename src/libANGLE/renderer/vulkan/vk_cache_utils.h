c3ca10055b3b855e20dee0bf2c165276411ab0c9
Jamie Madill <jmadill@chromium.org>
2022-01-28 09:39:53 -0500

    Vulkan: Add shared descriptor set caches.
    This allows programs with the same sets of descriptors to
    share descriptor sets. Currently there is no cache eviction.
    
    This CL adds a new "Meta" class to manage the descriptor set
    caches. Each shared descriptor pool is unique to a descriptor
    set layout. The descriptor set cache is moved into the pool
    class. Now every instance of a descriptor pool in ANGLE has
    easy access to a descriptor set cache as well.
    
    Bug: angleproject:6776
    Change-Id: I06982e0349f5a87e4578e769fa356ce8e7ab49f0

--- src/libANGLE/renderer/vulkan/vk_cache_utils.h
+++ src/libANGLE/renderer/vulkan/vk_cache_utils.h
@@ -220,7 +220,7 @@ class alignas(4) RenderPassDesc final
     uint8_t mSamples;
     uint8_t mColorAttachmentRange;
 
-    // Multivew
+    // Multiview
     uint8_t mViewCount;
 
     // sRGB
@@ -784,6 +784,8 @@ class DescriptorSetLayoutDesc final
     void unpackBindings(DescriptorSetLayoutBindingVector *bindings,
                         std::vector<VkSampler> *immutableSamplers) const;
 
+    bool empty() const { return *this == DescriptorSetLayoutDesc(); }
+
   private:
     // There is a small risk of an issue if the sampler cache is evicted but not the descriptor
     // cache we would have an invalid handle here. Thus propose follow-up work:
@@ -1155,13 +1157,21 @@ class DescriptorSetDesc
 
     // Specific helpers for uniforms/xfb descriptors.
     static constexpr size_t kDefaultUniformBufferWordOffset = 0;
-    static constexpr size_t kXfbBufferSerialWordOffset      = 1;
-    static constexpr size_t kXfbBufferOffsetWordOffset      = 2;
-    static constexpr size_t kXfbWordStride                  = 2;
+    static constexpr size_t kDefaultUniformSizeOffset       = 1;
+    static constexpr size_t kXfbBufferSerialWordOffset =
+        kDefaultUniformSizeOffset + static_cast<size_t>(gl::ShaderType::EnumCount);
+    static constexpr size_t kXfbBufferOffsetWordOffset = kXfbBufferSerialWordOffset + 1;
+    static constexpr size_t kXfbWordStride             = 2;
 
     void updateDefaultUniformBuffer(BufferSerial bufferSerial)
     {
         setBufferSerial(kDefaultUniformBufferWordOffset, 1, 0, bufferSerial);
+    }
+
+    void updateDefaultUniformBufferSize(gl::ShaderType shaderType, VkDeviceSize dataSize)
+    {
+        setClamped64BitValue(kDefaultUniformSizeOffset, 1, static_cast<size_t>(shaderType),
+                             dataSize);
     }
 
     void updateTransformFeedbackBuffer(size_t xfbIndex,
@@ -1201,6 +1211,8 @@ class DescriptorSetDesc
         ASSERT(value <= static_cast<uint64_t>(std::numeric_limits<uint32_t>::max()));
         append32BitValue(static_cast<uint32_t>(value));
     }
+
+    bool empty() const { return mPayload.empty(); }
 
   private:
     void setBufferSerial(size_t wordOffset,
@@ -1508,6 +1520,7 @@ enum class VulkanCacheType
     UniformsAndXfbDescriptors,
     ShaderBuffersDescriptors,
     Framebuffer,
+    DescriptorMetaCache,
     EnumCount
 };
 
@@ -1520,15 +1533,21 @@ class CacheStats final : angle::NonCopyable
 
     ANGLE_INLINE void hit() { mHitCount++; }
     ANGLE_INLINE void miss() { mMissCount++; }
+    ANGLE_INLINE void incrementSize() { mSize++; }
+    ANGLE_INLINE void missAndIncrementSize()
+    {
+        mMissCount++;
+        mSize++;
+    }
     ANGLE_INLINE void accumulate(const CacheStats &stats)
     {
         mHitCount += stats.mHitCount;
         mMissCount += stats.mMissCount;
-        mSize = stats.mSize;
+        mSize += stats.mSize;
     }
 
-    uint64_t getHitCount() const { return mHitCount; }
-    uint64_t getMissCount() const { return mMissCount; }
+    uint32_t getHitCount() const { return mHitCount; }
+    uint32_t getMissCount() const { return mMissCount; }
 
     ANGLE_INLINE double getHitRatio() const
     {
@@ -1542,9 +1561,7 @@ class CacheStats final : angle::NonCopyable
         }
     }
 
-    ANGLE_INLINE void incrementSize() { ++mSize; }
-
-    ANGLE_INLINE uint64_t getSize() const { return mSize; }
+    ANGLE_INLINE uint32_t getSize() const { return mSize; }
 
     void reset()
     {
@@ -1553,10 +1570,16 @@ class CacheStats final : angle::NonCopyable
         mSize      = 0;
     }
 
+    void accumulateCacheStats(VulkanCacheType cacheType, const CacheStats &cacheStats)
+    {
+        mHitCount += cacheStats.getHitCount();
+        mMissCount += cacheStats.getMissCount();
+    }
+
   private:
-    uint64_t mHitCount;
-    uint64_t mMissCount;
-    uint64_t mSize;
+    uint32_t mHitCount;
+    uint32_t mMissCount;
+    uint32_t mSize;
 };
 
 template <VulkanCacheType CacheType>
@@ -1570,12 +1593,16 @@ class HasCacheStats : angle::NonCopyable
         mCacheStats.reset();
     }
 
+    void getCacheStats(CacheStats *accum) const { accum->accumulate(mCacheStats); }
+
   protected:
     HasCacheStats()          = default;
     virtual ~HasCacheStats() = default;
 
     CacheStats mCacheStats;
 };
+
+using VulkanCacheStats = angle::PackedEnumMap<VulkanCacheType, CacheStats>;
 
 // TODO(jmadill): Add cache trimming/eviction.
 class RenderPassCache final : angle::NonCopyable
@@ -1602,7 +1629,7 @@ class RenderPassCache final : angle::NonCopyable
             return angle::Result::Continue;
         }
 
-        mCompatibleRenderPassCacheStats.miss();
+        mCompatibleRenderPassCacheStats.missAndIncrementSize();
         return addRenderPass(contextVk, desc, renderPassOut);
     }
 
@@ -1667,7 +1694,7 @@ class GraphicsPipelineCache final : public HasCacheStats<VulkanCacheType::Graphi
             return angle::Result::Continue;
         }
 
-        mCacheStats.miss();
+        mCacheStats.missAndIncrementSize();
         return insertPipeline(contextVk, pipelineCacheVk, compatibleRenderPass, pipelineLayout,
                               activeAttribLocationsMask, programAttribsTypeMask, missingOutputsMask,
                               shaders, specConsts, desc, descPtrOut, pipelineOut);
@@ -1762,42 +1789,7 @@ class SamplerYcbcrConversionCache final
     SamplerYcbcrConversionMap mVkFormatPayload;
 };
 
-// DescriptorSet Cache
-class DriverUniformsDescriptorSetCache final
-    : public HasCacheStats<VulkanCacheType::DriverUniformsDescriptors>
-{
-  public:
-    DriverUniformsDescriptorSetCache() = default;
-    ~DriverUniformsDescriptorSetCache() override { ASSERT(mPayload.empty()); }
-
-    void destroy(RendererVk *rendererVk);
-
-    ANGLE_INLINE bool get(uint32_t serial, VkDescriptorSet *descriptorSet)
-    {
-        if (mPayload.get(serial, descriptorSet))
-        {
-            mCacheStats.hit();
-            return true;
-        }
-        mCacheStats.miss();
-        return false;
-    }
-
-    ANGLE_INLINE void insert(uint32_t serial, VkDescriptorSet descriptorSet)
-    {
-        mPayload.insert(serial, descriptorSet);
-    }
-
-    ANGLE_INLINE void clear() { mPayload.clear(); }
-
-    size_t getSize() const { return mPayload.size(); }
-
-  private:
-    static constexpr uint32_t kFlatMapSize = 16;
-    angle::FlatUnorderedMap<uint32_t, VkDescriptorSet, kFlatMapSize> mPayload;
-};
-
-// Templated Descriptors Cache
+// Descriptor Set Cache
 class DescriptorSetCache final : angle::NonCopyable
 {
   public:
@@ -1806,7 +1798,25 @@ class DescriptorSetCache final : angle::NonCopyable
 
     void destroy(RendererVk *rendererVk, VulkanCacheType cacheType);
 
-    ANGLE_INLINE bool get(const vk::DescriptorSetDesc &desc, VkDescriptorSet *descriptorSet)
+    DescriptorSetCache(DescriptorSetCache &&other) : DescriptorSetCache()
+    {
+        *this = std::move(other);
+    }
+
+    DescriptorSetCache &operator=(DescriptorSetCache &&other)
+    {
+        std::swap(mPayload, other.mPayload);
+        return *this;
+    }
+
+    void resetCache(CacheStats *accum)
+    {
+        accum->accumulate(mCacheStats);
+        mPayload.clear();
+    }
+
+    ANGLE_INLINE bool getDescriptorSet(const vk::DescriptorSetDesc &desc,
+                                       VkDescriptorSet *descriptorSet)
     {
         auto iter = mPayload.find(desc);
         if (iter != mPayload.end())
@@ -1819,14 +1829,15 @@ class DescriptorSetCache final : angle::NonCopyable
         return false;
     }
 
-    ANGLE_INLINE void insert(const vk::DescriptorSetDesc &desc, VkDescriptorSet descriptorSet)
+    ANGLE_INLINE void insertDescriptorSet(const vk::DescriptorSetDesc &desc,
+                                          VkDescriptorSet descriptorSet)
     {
         mPayload.emplace(desc, descriptorSet);
         mCacheStats.incrementSize();
     }
 
     template <typename Accumulator>
-    void accumulateCacheStats(VulkanCacheType cacheType, Accumulator *accumulator)
+    void accumulateCacheStats(VulkanCacheType cacheType, Accumulator *accumulator) const
     {
         accumulator->accumulateCacheStats(cacheType, mCacheStats);
     }
