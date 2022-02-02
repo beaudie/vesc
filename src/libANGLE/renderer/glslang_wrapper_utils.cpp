c8544f58f6da531466eb0816896744c4c0fd4076
Jamie Madill <jmadill@chromium.org>
2022-02-02 15:26:33 -0500

    Vulkan: Test bindings.
    Bug: None
    Change-Id: Ia66611b13bdd54130302f877a2ae5565b835a30f

--- src/libANGLE/renderer/glslang_wrapper_utils.cpp
+++ src/libANGLE/renderer/glslang_wrapper_utils.cpp
@@ -783,6 +783,8 @@ void AssignInterfaceBlockBindings(const GlslangSourceOptions &options,
                                   UniformBindingIndexMap *uniformBindingIndexMapOut,
                                   ShaderInterfaceVariableInfoMap *variableInfoMapOut)
 {
+    uint32_t initialBindingIndex = programInterfaceInfo->currentShaderResourceBindingIndex;
+
     for (const gl::InterfaceBlock &block : blocks)
     {
         if (!block.isArray || block.arrayElement == 0)
@@ -796,6 +798,16 @@ void AssignInterfaceBlockBindings(const GlslangSourceOptions &options,
                                          programInterfaceInfo->shaderResourceDescriptorSetIndex,
                                          uniformBindingIndexMapOut, variableInfoMapOut);
             }
+        }
+    }
+
+    uint32_t lastBindingIndex = programInterfaceInfo->currentShaderResourceBindingIndex;
+
+    for (const gl::InterfaceBlock &block : blocks)
+    {
+        if (!block.isArray || block.arrayElement == 0)
+        {
+            ASSERT(block.binding >= initialBindingIndex && block.binding < lastBindingIndex);
         }
     }
 }
