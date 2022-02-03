77aed382dd147d5b918f41f645a857a04ce0dbf1
Jamie Madill <jmadill@chromium.org>
2022-02-03 07:41:16 -0500

    Vulkan: Move shader resources descriptor set update.
    This is in preparation for adding shader serial information to the
    desc, to work around a bug with incorrect descriptor set cache
    hits. It could have some perf impact if we restart command buffers
    constantly while using UBOs/SSBOs.
    
    Bug: angleproject:6776
    Change-Id: I24f5cefd329a1c36f7f60cb2a520cfdf5f127051

--- src/libANGLE/renderer/vulkan/ContextVk.h
+++ src/libANGLE/renderer/vulkan/ContextVk.h
@@ -911,7 +911,8 @@ class ContextVk : public ContextImpl, public vk::Context, public MultisampleText
     angle::Result handleDirtyTexturesImpl(CommandBufferHelperT *commandBufferHelper,
                                           PipelineType pipelineType);
     template <typename CommandBufferHelperT>
-    angle::Result handleDirtyShaderResourcesImpl(CommandBufferHelperT *commandBufferHelper);
+    angle::Result handleDirtyShaderResourcesImpl(CommandBufferHelperT *commandBufferHelper,
+                                                 PipelineType pipelineType);
     void handleDirtyShaderBufferResourcesImpl(vk::CommandBufferHelperCommon *commandBufferHelper);
     template <typename CommandBufferT>
     void handleDirtyDriverUniformsBindingImpl(CommandBufferT *commandBuffer,
@@ -1012,6 +1013,8 @@ class ContextVk : public ContextImpl, public vk::Context, public MultisampleText
 
     SpecConstUsageBits getCurrentProgramSpecConstUsageBits() const;
     void updateGraphicsPipelineDescWithSpecConstUsageBits(SpecConstUsageBits usageBits);
+
+    void updateShaderResourcesDescriptorDesc(PipelineType pipelineType);
 
     ContextVkPerfCounters getAndResetObjectPerfCounters();
 
