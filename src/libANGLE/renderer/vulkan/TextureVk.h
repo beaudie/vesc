51cf97d1b7578537861ea85e01a95c0d59e17276
Jamie Madill <jmadill@chromium.org>
2022-02-17 13:30:32 -0500

    Vulkan: Add generic descriptors for DS cache.
    WIP
    
    Bug: angleproject:6776
    Change-Id: If1660a557585a75e9aa2560d6a38c56b62f555c8

--- src/libANGLE/renderer/vulkan/TextureVk.h
+++ src/libANGLE/renderer/vulkan/TextureVk.h
@@ -475,7 +475,9 @@ class TextureVk : public TextureImpl, public angle::ObserverInterface
     }
 
     angle::Result refreshImageViews(ContextVk *contextVk);
-    bool shouldDecodeSRGB(vk::Context *context, GLenum srgbDecode, bool texelFetchStaticUse) const;
+    bool shouldDecodeSRGB(vk::Context *contextVk,
+                          GLenum srgbDecode,
+                          bool texelFetchStaticUse) const;
     void initImageUsageFlags(ContextVk *contextVk, angle::FormatID actualFormatID);
     void handleImmutableSamplerTransition(const vk::ImageHelper *previousImage,
                                           const vk::ImageHelper *nextImage);
@@ -492,11 +494,12 @@ class TextureVk : public TextureImpl, public angle::ObserverInterface
 
     gl::TextureType mImageNativeType;
 
-    // The layer offset to apply when converting from a frontend texture layer to a texture layer in
-    // mImage. Used when this texture sources a cube map face or 3D texture layer from an EGL image.
+    // The layer offset to apply when converting from a front-end texture layer to a texture layer
+    // in mImage. Used when this texture sources a cube map face or 3D texture layer from an EGL
+    // image.
     uint32_t mImageLayerOffset;
 
-    // The level offset to apply when converting from a frontend texture level to texture level in
+    // The level offset to apply when converting from a front-end texture level to texture level in
     // mImage.
     uint32_t mImageLevelOffset;
 
