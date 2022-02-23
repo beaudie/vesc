2a7802bd01198645440faec72cc1db59eb9df45f
Charlie Lao <cclao@google.com>
2022-02-22 16:18:45 -0800

    Vulkan: Fix the data race for mUse from two threads
    A data race of mUse object between two threads can occur with the
    following sequences:
    1) You use the buffer in the context, it adds into mResourceUseList
    2) You release the buffer. Now it goes into mGarbageList, even though it
       is still in the mResourceUseList.
    
    Now this mUse object has references from two lists, the mGarabgeList and
    mResourceUseList, and they do not use the same mutex lock. This means
    the race could happen and corrupt the mUse data. The same thing could
    happen with ImageHelper object as well. This quick fix also grabs
    mGrabageMutex while processing mResourceUseList.
    
    This bug was exposed by
    MultithreadingTest.MultiContextClear/ES3_Vulkan_SwiftShader on
    linux-tsan-test
    
    Bug: angleproject:7045
    Change-Id: I264c970579aaa53373a61ff067fa0e21eb410ae6

--- src/libANGLE/renderer/vulkan/RendererVk.cpp
+++ src/libANGLE/renderer/vulkan/RendererVk.cpp
@@ -3763,11 +3763,21 @@ angle::Result RendererVk::submitFrame(vk::Context *context,
 
     waitSemaphores.clear();
     waitSemaphoreStageMasks.clear();
-    for (vk::ResourceUseList &it : resourceUseLists)
+
+    if (!resourceUseLists.empty())
     {
-        it.releaseResourceUsesAndUpdateSerials(*submitSerialOut);
+        // From the release call, we always put resources into mGarbageList, even though the
+        // resource maybe still in the context's mResourceUseList. This result in two lists have
+        // pointers to the mUse and data race between two threads processing the two lists. Here we
+        // always hold mGarbageMutex here while processing mResourceUseList so that data race will
+        // not occur.
+        std::lock_guard<std::mutex> lock(mGarbageMutex);
+        for (vk::ResourceUseList &it : resourceUseLists)
+        {
+            it.releaseResourceUsesAndUpdateSerials(*submitSerialOut);
+        }
+        resourceUseLists.clear();
     }
-    resourceUseLists.clear();
 
     return angle::Result::Continue;
 }
