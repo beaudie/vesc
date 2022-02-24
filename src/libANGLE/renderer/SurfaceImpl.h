347fa1358ed4f596dee73c47cebc98b5eed96246
Jeff Vigil <j.vigil@samsung.com>
2022-02-24 08:31:52 -0800

    EGL: Fix EGL_KHR_mutable_render_buffer
    Fixes for eglSurfaceAttrib error cases.
    Improve query for render buffer
    Add test case to EGLSurfaceTest
    
    Test: angle_end2end_test --gtest_filter=EGLSingleBufferTest
    
    Bug: angleproject:7134
    Change-Id: I4fa568c9530312003dc17111be212bf5b66d97fb

--- src/libANGLE/renderer/SurfaceImpl.h
+++ src/libANGLE/renderer/SurfaceImpl.h
@@ -122,6 +122,7 @@ class SurfaceImpl : public FramebufferAttachmentObjectImpl
     virtual EGLint origin() const;
 
     virtual egl::Error setRenderBuffer(EGLint renderBuffer);
+    virtual void getRenderBuffer(EGLenum *renderBufferOut) const;
 
   protected:
     const egl::SurfaceState &mState;
