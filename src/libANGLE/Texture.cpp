34e51d0096511a94ebf79183959a60ad7037199f
Yuxin Hu <yuxinhu@google.com>
2022-02-28 23:56:46 +0000

    Flush Texture Staged Updates When Destroying Context Share Group
    Bug: chromium:1299211
    Change-Id: I260de93c3a3099e023e31acbe017803e824459ad

--- src/libANGLE/Texture.cpp
+++ src/libANGLE/Texture.cpp
@@ -2129,7 +2129,7 @@ GLuint Texture::getNativeID() const
 
 angle::Result Texture::syncState(const Context *context, Command source)
 {
-    ASSERT(hasAnyDirtyBit() || source == Command::GenerateMipmap);
+    ASSERT(hasAnyDirtyBit() || source == Command::GenerateMipmap || source == Command::Other);
     ANGLE_TRY(mTexture->syncState(context, mDirtyBits, source));
     mDirtyBits.reset();
     mState.mInitState = InitState::Initialized;
