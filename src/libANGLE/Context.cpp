c9221abcd1fc58ca2d649a071703a3f407b50d24
Yuxin Hu <yuxinhu@google.com>
2022-02-28 23:56:46 +0000

    Flush Texture Staged Updates When Destroying Context Share Group
    Bug: chromium:1299211
    Change-Id: I260de93c3a3099e023e31acbe017803e824459ad

--- src/libANGLE/Context.cpp
+++ src/libANGLE/Context.cpp
@@ -743,7 +743,7 @@ egl::Error Context::onDestroy(const egl::Display *display)
     // are bound to them before the Programs are released()'ed.
     mState.mProgramPipelineManager->release(this);
     mState.mShaderProgramManager->release(this);
-    mState.mTextureManager->release(this);
+    // mState.mTextureManager->release(this);
     mState.mRenderbufferManager->release(this);
     mState.mSamplerManager->release(this);
     mState.mSyncManager->release(this);
@@ -755,6 +755,9 @@ egl::Error Context::onDestroy(const egl::Display *display)
     mMultiThreadPool.reset();
 
     mImplementation->onDestroy(this);
+    // Fix chromium:1299211. We need to access mTextureManager in ContextVk::onDestroy(),
+    // so we release the mTextureManager after the call on mImplementation->onDestroy().
+    mState.mTextureManager->release(this);
 
     // Backend requires implementation to be destroyed first to close down all the objects
     mState.mShareGroup->release(display);
