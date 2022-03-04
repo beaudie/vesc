c849f0e445adf771b551387fbd43a414205d20e0
Constantine Shablya <constantine.shablya@collabora.com>
2022-03-04 21:13:51 +0200

    Vulkan: GBM surface
    Add support for GBM platform window surface. Duplicate dmabuf image file
    descriptor when importing it in Vulkan. Present GBM surface after
    throttling submissions and flushing ANGLE context.
    
    Bug: angleproject:7217
    Change-Id: I45b33533983296dbad29d9c56c9dc2fab41b9178
    Signed-off-by: Constantine Shablya <constantine.shablya@collabora.com>
    Signed-off-by: Antonio Caggiano <antonio.caggiano@collabora.com>

---
+++ src/libANGLE/renderer/vulkan/linux/gbm/SurfaceVkGbm.h
@@ -0,0 +1,68 @@
+//
+// Copyright 2022 The ANGLE Project Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+// SurfaceVkGbm.h:
+//    Defines the class interface for SurfaceVkGbm, implementing WindowSurfaceVk.
+//
+
+#ifndef LIBANGLE_RENDERER_VULKAN_GBM_WINDOWSURFACEVKGBM_H_
+#define LIBANGLE_RENDERER_VULKAN_GBM_WINDOWSURFACEVKGBM_H_
+
+#include "libANGLE/renderer/vulkan/SurfaceVk.h"
+
+struct gbm_surface;
+
+namespace rx
+{
+
+class SurfaceVkGbm : public WindowSurfaceVk
+{
+  public:
+    SurfaceVkGbm(const egl::SurfaceState &surfaceState, EGLNativeWindowType window);
+    ~SurfaceVkGbm() override;
+
+    angle::Result getAttachmentRenderTarget(const gl::Context *context,
+                                            GLenum binding,
+                                            const gl::ImageIndex &imageIndex,
+                                            GLsizei samples,
+                                            FramebufferAttachmentRenderTarget **rtOut) override;
+
+    void setSwapInterval(EGLint interval) override;
+
+    egl::Error lockSurface(const egl::Display *display,
+                           EGLint usageHint,
+                           bool preservePixels,
+                           uint8_t **bufferPtrOut,
+                           EGLint *bufferPitchOut) override;
+    egl::Error unlockSurface(const egl::Display *display, bool preservePixels) override;
+
+  private:
+    angle::Result createSurfaceVk(vk::Context *context, gl::Extents *extentsOut) override;
+    angle::Result getCurrentWindowSize(vk::Context *context, gl::Extents *extentsOut) override;
+
+    angle::Result initializeImpl(DisplayVk *displayVk) override;
+    angle::Result createSwapChain(vk::Context *context);
+
+    angle::Result swapImpl(const gl::Context *context,
+                           const EGLint *rects,
+                           EGLint n_rects,
+                           const void *pNextChain) override;
+
+    angle::Result presentImpl(ContextVk *contextVk,
+                              vk::OutsideRenderPassCommandBuffer *commandBuffer,
+                              Serial *swapSerial,
+                              const EGLint *rects,
+                              EGLint n_rects,
+                              const void *pNextChain,
+                              bool *presentOutOfDate) override;
+
+    bool acquireNextImage();
+
+    gbm_surface *mGbmSurface;
+};
+
+}  // namespace rx
+
+#endif  // LIBANGLE_RENDERER_VULKAN_GBM_WINDOWSURFACEVKGBM_H_
