1a153addc318b9a01685b53e5b514a59530fe490
Charlie Lao <cclao@google.com>
2022-03-25 16:49:54 -0700

    Vulkan: Create buffer for vertex array if robust enabled
    Bug: chromium:1310038
    Change-Id: I3499ae600028149b1039082e5011232b3e4e5e80

--- src/libANGLE/renderer/vulkan/UtilsVk.cpp
+++ src/libANGLE/renderer/vulkan/UtilsVk.cpp
@@ -2529,7 +2529,7 @@ angle::Result UtilsVk::stencilBlitResolveNoShaderExport(ContextVk *contextVk,
 
     ANGLE_TRY(blitBuffer.get().initSuballocation(
         contextVk, contextVk->getRenderer()->getDeviceLocalMemoryTypeIndex(),
-        static_cast<size_t>(bufferSize), GetDefaultBufferAlignment(contextVk->getRenderer())));
+        static_cast<size_t>(bufferSize), contextVk->getRenderer()->getDefaultBufferAlignment()));
     blitBuffer.get().retainReadWrite(&contextVk->getResourceUseList());
 
     BlitResolveStencilNoExportShaderParams shaderParams;
