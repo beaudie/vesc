66b77d02a83abe201ba30ce863b334243482e836
Jamie Madill <jmadill@chromium.org>
2022-04-07 09:57:25 -0400

    Vulkan: Add shared descriptor set caches.
    This allows programs with the same sets of descriptors to
    share descriptor sets. Currently there is no cache eviction.
    
    This CL adds a new "Meta" class to manage the descriptor set
    caches. Each shared descriptor pool is unique to a descriptor
    set layout. The descriptor set cache is moved into the pool
    class. Now every instance of a descriptor pool in ANGLE has
    easy access to a descriptor set cache as well.
    
    Bug: angleproject:6776
    Change-Id: I06982e0349f5a87e4578e769fa356ce8e7ab49f0

--- src/libANGLE/renderer/vulkan/vk_cache_utils.cpp
+++ src/libANGLE/renderer/vulkan/vk_cache_utils.cpp
@@ -3831,7 +3831,7 @@ angle::Result RenderPassCache::getRenderPassWithOpsImpl(ContextVk *contextVk,
         outerIt            = emplaceResult.first;
     }
 
-    mRenderPassWithOpsCacheStats.miss();
+    mRenderPassWithOpsCacheStats.missAndIncrementSize();
     vk::RenderPassHelper newRenderPass;
     ANGLE_TRY(vk::InitializeRenderPassFromDesc(contextVk, desc, attachmentOps, &newRenderPass));
 
@@ -3961,7 +3961,7 @@ angle::Result DescriptorSetLayoutCache::getDescriptorSetLayout(
         return angle::Result::Continue;
     }
 
-    mCacheStats.miss();
+    mCacheStats.missAndIncrementSize();
     // We must unpack the descriptor set layout description.
     vk::DescriptorSetLayoutBindingVector bindingVector;
     std::vector<VkSampler> immutableSamplers;
@@ -4022,7 +4022,7 @@ angle::Result PipelineLayoutCache::getPipelineLayout(
         return angle::Result::Continue;
     }
 
-    mCacheStats.miss();
+    mCacheStats.missAndIncrementSize();
     // Note this does not handle gaps in descriptor set layouts gracefully.
     angle::FixedVector<VkDescriptorSetLayout, vk::kMaxDescriptorSetLayouts> setLayoutHandles;
     for (const vk::BindingPointer<vk::DescriptorSetLayout> &layoutPtr : descriptorSetLayouts)
@@ -4118,7 +4118,7 @@ angle::Result SamplerYcbcrConversionCache::getSamplerYcbcrConversion(
         return angle::Result::Continue;
     }
 
-    mCacheStats.miss();
+    mCacheStats.missAndIncrementSize();
 
     // Create the VkSamplerYcbcrConversion
     vk::SamplerYcbcrConversion wrappedSamplerYcbcrConversion;
@@ -4173,7 +4173,7 @@ angle::Result SamplerCache::getSampler(ContextVk *contextVk,
         return angle::Result::Continue;
     }
 
-    mCacheStats.miss();
+    mCacheStats.missAndIncrementSize();
     vk::SamplerHelper samplerHelper(contextVk);
     ANGLE_TRY(desc.init(contextVk, &samplerHelper.get()));
 
@@ -4185,12 +4185,5 @@ angle::Result SamplerCache::getSampler(ContextVk *contextVk,
     contextVk->getRenderer()->onAllocateHandle(vk::HandleType::Sampler);
 
     return angle::Result::Continue;
-}
-
-// DriverUniformsDescriptorSetCache implementation.
-void DriverUniformsDescriptorSetCache::destroy(RendererVk *rendererVk)
-{
-    accumulateCacheStats(rendererVk);
-    mPayload.clear();
 }
 }  // namespace rx
