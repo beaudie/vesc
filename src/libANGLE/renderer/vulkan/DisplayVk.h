53e15411bcd107014187b396132fb38417e6cf63
Jamie Madill <jmadill@chromium.org>
2022-04-07 09:57:25 -0400

    Vulkan: Add shared descriptor set caches.
    This allows programs with the same sets of descriptors to
    share descriptor sets. Currently there is no cache eviction.
    
    This CL adds a new "Meta" class to manage the descriptor set
    caches. Each shared descriptor pool is unique to a descriptor
    set layout. The descriptor set cache is moved into the pool
    class. Now every instance of a descriptor pool in ANGLE has
    easy access to a descriptor set cache as well.
    
    Bug: angleproject:6776
    Change-Id: I06982e0349f5a87e4578e769fa356ce8e7ab49f0

--- src/libANGLE/renderer/vulkan/DisplayVk.h
+++ src/libANGLE/renderer/vulkan/DisplayVk.h
@@ -35,6 +35,10 @@ class ShareGroupVk : public ShareGroupImpl
     PipelineLayoutCache &getPipelineLayoutCache() { return mPipelineLayoutCache; }
     DescriptorSetLayoutCache &getDescriptorSetLayoutCache() { return mDescriptorSetLayoutCache; }
     const ContextVkSet &getContexts() const { return mContexts; }
+    vk::MetaDescriptorPool &getMetaDescriptorPool(DescriptorSetIndex descriptorSetIndex)
+    {
+        return mMetaDescriptorPools[descriptorSetIndex];
+    }
 
     void releaseResourceUseLists(const Serial &submitSerial);
     void acquireResourceUseList(vk::ResourceUseList &&resourceUseList)
@@ -57,6 +61,9 @@ class ShareGroupVk : public ShareGroupImpl
 
     // DescriptorSetLayouts are also managed in a cache.
     DescriptorSetLayoutCache mDescriptorSetLayoutCache;
+
+    // Descriptor set caches
+    vk::DescriptorSetArray<vk::MetaDescriptorPool> mMetaDescriptorPools;
 
     // The list of contexts within the share group
     ContextVkSet mContexts;
