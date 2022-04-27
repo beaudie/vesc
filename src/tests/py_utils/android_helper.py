6690ccd7d01b2460f1381768c2f61bf83a1f2f72
Roman Lavrov <romanl@google.com>
2022-04-27 17:48:07 -0400

    angle_deqp_gles3_tests testing
    Bug: angleproject:7242
    Change-Id: Ie9c87dd57c7c7c4b0c337271e29445dad319fe30

--- src/tests/py_utils/android_helper.py
+++ src/tests/py_utils/android_helper.py
@@ -108,6 +108,28 @@ def _AddRestrictedTracesJson():
     _AdbShell('r=/sdcard/chromium_tests_root; tar -xf $r/t.tar -C $r/ && rm $r/t.tar')
 
 
+def _AddDeqpFiles():
+    _AdbShell('mkdir -p /sdcard/chromium_tests_root/')
+
+    def add(tar, fn):
+        assert (fn.startswith('../../'))
+        tar.add(fn, arcname=fn.replace('../../', ''))
+
+    with _TempLocalFile() as tempfile_path:
+        with tarfile.open(tempfile_path, 'w', format=tarfile.GNU_FORMAT) as tar:
+            for f in glob.glob('../../src/tests/deqp_support/*.txt', recursive=False):
+                add(tar, f)
+
+            tar.add('gen/vk_gl_cts_data')
+            add(
+                tar,
+                '../../third_party/VK-GL-CTS/src/external/openglcts/data/mustpass/gles/aosp_mustpass/main/gles3-master.txt'
+            )
+        _AdbRun(['push', tempfile_path, '/sdcard/chromium_tests_root/t.tar'])
+
+    _AdbShell('r=/sdcard/chromium_tests_root; tar -xf $r/t.tar -C $r/ && rm $r/t.tar')
+
+
 def PrepareTestSuite(suite_name):
     _GetAdbRoot()
 
@@ -126,6 +148,9 @@ def PrepareTestSuite(suite_name):
 
     if suite_name == 'angle_perftests':
         _AddRestrictedTracesJson()
+
+    if 'deqp' in suite_name:
+        _AddDeqpFiles()
 
 
 def PrepareRestrictedTraces(traces):
