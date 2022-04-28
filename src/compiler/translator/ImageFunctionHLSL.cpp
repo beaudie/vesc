f203939a5e5ee82496a538a44fec282926411054
Hailin Zhang <hailinzhang@google.com>
2022-04-28 14:44:54 -0700

    add GL_EXT_texture_buffer to d3d11 backend
    tested: deqp-gles31.exe --deqp-gl-context-type=egl --deqp-case=dEQP-GLES31.functional.texture.texture_buffer.* passed
    
    Bug: b/206367167
    Change-Id: I31a6f84bd701a737735a6bac2f4eef780c24a979

--- src/compiler/translator/ImageFunctionHLSL.cpp
+++ src/compiler/translator/ImageFunctionHLSL.cpp
@@ -71,6 +71,12 @@ void ImageFunctionHLSL::OutputImageFunctionArgumentList(
             case EbtUImage2DArray:
                 out << ", int3 p";
                 break;
+            case EbtUImageBuffer:
+            case EbtIImageBuffer:
+            case EbtImageBuffer:
+                out << ", int p";
+                break;
+
             default:
                 UNREACHABLE();
         }
@@ -83,18 +89,21 @@ void ImageFunctionHLSL::OutputImageFunctionArgumentList(
                 case EbtImage3D:
                 case EbtImageCube:
                 case EbtImage2DArray:
+                case EbtImageBuffer:
                     out << ", float4 data";
                     break;
                 case EbtIImage2D:
                 case EbtIImage3D:
                 case EbtIImageCube:
                 case EbtIImage2DArray:
+                case EbtIImageBuffer:
                     out << ", int4 data";
                     break;
                 case EbtUImage2D:
                 case EbtUImage3D:
                 case EbtUImageCube:
                 case EbtUImage2DArray:
+                case EbtUImageBuffer:
                     out << ", uint4 data";
                     break;
                 default:
@@ -150,6 +159,10 @@ void ImageFunctionHLSL::OutputImageLoadFunctionBody(
     {
         out << "    return " << imageReference << "[uint2(p.x, p.y)];\n";
     }
+    else if (IsImageBuffer(imageFunction.image))
+    {
+        out << "    return " << imageReference << "[uint(p.x)];\n";
+    }
     else
         UNREACHABLE();
 }
@@ -161,7 +174,8 @@ void ImageFunctionHLSL::OutputImageStoreFunctionBody(
     const ImmutableString &imageReference)
 {
     if (IsImage3D(imageFunction.image) || IsImage2DArray(imageFunction.image) ||
-        IsImage2D(imageFunction.image) || IsImageCube(imageFunction.image))
+        IsImage2D(imageFunction.image) || IsImageCube(imageFunction.image) ||
+        IsImageBuffer(imageFunction.image))
     {
         out << "    " << imageReference << "[p] = data;\n";
     }
@@ -255,6 +269,10 @@ const char *ImageFunctionHLSL::ImageFunction::getReturnType() const
             case EbtIImage2DArray:
             case EbtUImage2DArray:
                 return "int3";
+            case EbtImageBuffer:
+            case EbtIImageBuffer:
+            case EbtUImageBuffer:
+                return "int";
             default:
                 UNREACHABLE();
         }
@@ -263,16 +281,19 @@ const char *ImageFunctionHLSL::ImageFunction::getReturnType() const
     {
         switch (image)
         {
+            case EbtImageBuffer:
             case EbtImage2D:
             case EbtImage3D:
             case EbtImageCube:
             case EbtImage2DArray:
                 return "float4";
+            case EbtIImageBuffer:
             case EbtIImage2D:
             case EbtIImage3D:
             case EbtIImageCube:
             case EbtIImage2DArray:
                 return "int4";
+            case EbtUImageBuffer:
             case EbtUImage2D:
             case EbtUImage3D:
             case EbtUImageCube:
