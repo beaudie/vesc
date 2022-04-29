a1c19d31f70fa4a63c1c7fb3547bc5d3921b03e9
Mohan Maiya <m.maiya@samsung.com>
2022-04-29 14:40:42 -0700

    Vulkan: Account for enabled vertex attribute without client data
    The values loaded is undefined but should not cause program termination.
    
    Test: VertexAttributeTest.EnabledAttribWithoutAttribPointer*
    Bug: angleproject:7256
    Change-Id: Ifdae7bd52269a34be563bd4245164d26ba9d636a

--- src/tests/gl_tests/VertexAttributeTest.cpp
+++ src/tests/gl_tests/VertexAttributeTest.cpp
@@ -929,6 +929,41 @@ TEST_P(VertexAttributeTest, UnsignedPacked1010102ExtensionNormalized)
     };
 }
 
+TEST_P(VertexAttributeTest, EnabledAttribWithoutAttribPointer)
+{
+    constexpr char kVS[] =
+        R"(attribute mediump vec4 position;
+            attribute highp vec4 test;
+            varying mediump vec4 varColor;
+            void main()
+            {
+                gl_Position = position;
+                varColor = test;
+            })";
+
+    constexpr char kFS[] =
+        R"(varying mediump vec4 varColor;
+            void main()
+            {
+                gl_FragColor = varColor;
+            })";
+
+    ANGLE_GL_PROGRAM(program, kVS, kFS);
+    ASSERT_NE(0u, program);
+    GLint testAttribLocation = glGetAttribLocation(program, "test");
+    ASSERT_NE(-1, testAttribLocation);
+    glUseProgram(program);
+
+    glEnableVertexAttribArray(testAttribLocation);
+    // Do not call glVertexAttribPointer(...);
+
+    drawQuad(program, "position", 0.5f);
+
+    glDisableVertexAttribArray(testAttribLocation);
+
+    ASSERT_GL_NO_ERROR();
+}
+
 class VertexAttributeTestES3 : public VertexAttributeTest
 {
   protected:
