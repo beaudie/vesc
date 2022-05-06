dcc65bdc5e407dc5f527b706a4dab87a792d84f1
Yuxin Hu <yuxinhu@google.com>
2022-05-05 19:01:22 -0700

    Basis Universal Texture Experiment
    Adding ETC1 image decoder code to decompress
    the ETC1 data into RGBA data, so that we can
    compare the pixel values in the test
    
    Bug: angleproject:7250
    Change-Id: I0b8d6143473c38818407c42db3227d1f93e1c0a2

--- src/tests/gl_tests/KTXCompressedTextureTest.cpp
+++ src/tests/gl_tests/KTXCompressedTextureTest.cpp
@@ -7,6 +7,7 @@
 // KTXCompressedTextureTest.cpp: Tests of reading compressed texture stored in
 // .ktx formats
 
+#include "image_util/loadimage.h"
 #include "test_utils/ANGLETest.h"
 #include "test_utils/gl_raii.h"
 
@@ -36,23 +37,40 @@ TEST_P(KTXCompressedTextureTest, CompressedTexImageETC1)
     ANGLE_GL_PROGRAM(textureProgram, essl1_shaders::vs::Texture2D(),
                      essl1_shaders::fs::Texture2D());
     glUseProgram(textureProgram);
-
-    GLTexture texture;
-    glBindTexture(GL_TEXTURE_2D, texture);
+    GLTexture compressedETC1Texture;
+    glBindTexture(GL_TEXTURE_2D, compressedETC1Texture);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
     glCompressedTexImage2D(GL_TEXTURE_2D, 0, GL_ETC1_RGB8_OES, ktx_etc1_width, ktx_etc1_height, 0,
                            ktx_etc1_size, ktx_etc1_data);
-
     EXPECT_GL_NO_ERROR();
-
     GLint textureUniformLocation =
         glGetUniformLocation(textureProgram, essl1_shaders::Texture2DUniform());
     glUniform1i(textureUniformLocation, 0);
     drawQuad(textureProgram.get(), essl1_shaders::PositionAttrib(), 0.5f);
+    EXPECT_GL_NO_ERROR();
 
+    // Uncompress ETC1 texture data to RGBA texture data
+    const size_t decompressedPixelCount    = ktx_etc1_width * ktx_etc1_height;
+    const size_t decompressedBytesPerPixel = 4;
+    GLubyte decompressedTextureData[decompressedPixelCount * decompressedBytesPerPixel] = {0};
+    LoadETC1RGB8ToRGBA8(ktx_etc1_width, ktx_etc1_height, 1, ktx_etc1_data, ktx_etc1_width / 4 * 8,
+                        0, decompressedTextureData, decompressedBytesPerPixel * ktx_etc1_width, 0);
+
+    const size_t comparePixelX = ktx_etc1_width / 2;
+    const size_t comparePixelY = ktx_etc1_height / 2;
+    const size_t decompressedPixelIndex =
+        ktx_etc1_width * decompressedBytesPerPixel * (comparePixelY) +
+        comparePixelX * decompressedBytesPerPixel;
+
+    const GLColor expect(decompressedTextureData[decompressedPixelIndex],
+                         decompressedTextureData[decompressedPixelIndex + 1],
+                         decompressedTextureData[decompressedPixelIndex + 2],
+                         decompressedTextureData[decompressedPixelIndex + 3]);
+
+    EXPECT_PIXEL_COLOR_EQ(comparePixelX, comparePixelY, expect);
     EXPECT_GL_NO_ERROR();
 }
 
