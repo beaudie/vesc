5b7e3b6d9dcdc635ce13a999768848ce2409c1de
Gert Wollny <gert.wollny@collabora.com>
2022-05-10 13:06:59 +0200

    Capture/Replay: Add support for eglCreateImage/eglDestroyImage
    The capturing of these calls is straightforward.
    When replaying the test harness intercepts the calls to
    eglCreateImage and eglDestroyImage and forwards the calls the
    GLWindow implementation that takes care of handling the call.
    
    Bug: angleproject:4964
    Bug: angleproject:5822
    Bug: angleproject:6180
    Bug: angleproject:6286
    Bug: angleproject:6578
    Bug: angleproject:7111
    Change-Id: I385aa9648f93bf74706e9860e2aee5775eeba220

--- src/tests/capture_replay_tests/angle_trace_gl.h
+++ src/tests/capture_replay_tests/angle_trace_gl.h
@@ -1,7 +1,10 @@
 #ifndef ANGLE_TRACE_GL_H_
 #define ANGLE_TRACE_GL_H_
 
-#include "trace_fixture.h"
 #include "util/util_gl.h"
+
+// The ordering of these includes is important, because in trace_fixture.h the defines
+// for some EGL calls are re-defined to let the replay handle them in a specific way
+#include "trace_fixture.h"
 
 #endif  // ANGLE_TRACE_GL_H_
