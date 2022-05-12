f94bff4b21e34a73a0482632dcd3d35a372bab0b
Gert Wollny <gert.wollny@collabora.com>
2022-05-12 19:12:35 +0200

    retrace: Move API loader code for replay to a common location
    With that Capture/Replay will be able to make use of the same
    code.
    
    Bug: angleproject:4964
    Change-Id: Ie45407d89da69e6484c90bd87d8f2c32b965f314

--- src/tests/trace_utils/traces_export.h
+++ src/tests/trace_utils/traces_export.h
@@ -5,8 +5,8 @@
 //
 // restricted_traces_export: Export definitions for restricted traces.
 
-#ifndef ANGLE_RESTRICTED_TRACES_EXPORT_H_
-#define ANGLE_RESTRICTED_TRACES_EXPORT_H_
+#ifndef ANGLE_TRACES_EXPORT_H_
+#define ANGLE_TRACES_EXPORT_H_
 
 // See util/util_export.h for details on import/export labels.
 #if !defined(ANGLE_TRACE_EXPORT)
@@ -45,4 +45,4 @@ ANGLE_TRACE_LOADER_EXPORT void LoadEGL(LoadProc loadProc);
 ANGLE_TRACE_LOADER_EXPORT void LoadGLES(LoadProc loadProc);
 }  // namespace trace_angle
 
-#endif  // ANGLE_RESTRICTED_TRACES_EXPORT_H_
+#endif  // ANGLE_TRACES_EXPORT_H_
