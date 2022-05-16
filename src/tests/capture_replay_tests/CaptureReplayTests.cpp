c86fdb36eadf4a1905f295cbecc95976afd9ff43
Gert Wollny <gert.wollny@collabora.com>
2022-05-16 12:02:25 +0200

    Capture/Replay: Override eglCreateImage and eglDestroyImage
    Bug: angleproject:4964
    Change-Id: I1f67e0e32fa901249aa9ee134854afbe1ac29524

--- src/tests/capture_replay_tests/CaptureReplayTests.cpp
+++ src/tests/capture_replay_tests/CaptureReplayTests.cpp
@@ -32,8 +32,59 @@ static EGLWindow *gEGLWindow = nullptr;
 constexpr char kResultTag[] = "*RESULT";
 constexpr char kTracePath[] = ANGLE_CAPTURE_REPLAY_TEST_NAMES_PATH;
 
+extern "C" {
+static EGLImage KHRONOS_APIENTRY EGLCreateImage(EGLDisplay display,
+                                                EGLContext context,
+                                                EGLenum target,
+                                                EGLClientBuffer buffer,
+                                                const EGLAttrib *attrib_list)
+{
+
+    GLWindowContext ctx = reinterpret_cast<GLWindowContext>(context);
+    return gEGLWindow->createImage(ctx, target, buffer, attrib_list);
+}
+
+static EGLImage KHRONOS_APIENTRY EGLCreateImageKHR(EGLDisplay display,
+                                                   EGLContext context,
+                                                   EGLenum target,
+                                                   EGLClientBuffer buffer,
+                                                   const EGLint *attrib_list)
+{
+
+    GLWindowContext ctx = reinterpret_cast<GLWindowContext>(context);
+    return gEGLWindow->createImageKHR(ctx, target, buffer, attrib_list);
+}
+
+static EGLBoolean KHRONOS_APIENTRY EGLDestroyImage(EGLDisplay display, EGLImage image)
+{
+    return gEGLWindow->destroyImage(image);
+}
+
+static EGLBoolean KHRONOS_APIENTRY EGLDestroyImageKHR(EGLDisplay display, EGLImage image)
+{
+    return gEGLWindow->destroyImageKHR(image);
+}
+}
+
 angle::GenericProc KHRONOS_APIENTRY TraceLoadProc(const char *procName)
 {
+    // EGL
+    if (strcmp(procName, "eglCreateImage") == 0)
+    {
+        return reinterpret_cast<angle::GenericProc>(EGLCreateImage);
+    }
+    if (strcmp(procName, "eglCreateImageKHR") == 0)
+    {
+        return reinterpret_cast<angle::GenericProc>(EGLCreateImageKHR);
+    }
+    if (strcmp(procName, "eglDestroyImage") == 0)
+    {
+        return reinterpret_cast<angle::GenericProc>(EGLDestroyImage);
+    }
+    if (strcmp(procName, "eglDestroyImageKHR") == 0)
+    {
+        return reinterpret_cast<angle::GenericProc>(EGLDestroyImageKHR);
+    }
     assert(gEGLWindow);
     return gEGLWindow->getProcAddress(procName);
 }
