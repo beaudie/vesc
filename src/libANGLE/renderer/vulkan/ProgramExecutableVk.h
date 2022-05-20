be2a08c138dbe963b4c903dc21aa379d85cedff9
Shahbaz Youssefi <syoussefi@chromium.org>
2022-05-20 16:17:49 -0400

    Vulkan: Reduce pre-rotation spec const to bool
    The specialization constant now only dictates whether x and y should be
    swapped.  The complete 8 possible states of rotation and y-flip are
    achieved by using this swap in combination with a driver uniform for x
    and y flip.
    
    Swapping is still a specialization constant to avoid degrading
    performance of dFdx/dFdy which otherwise would need both to be evaluated
    instead of one.  On platforms which don't support pre-rotation, the
    specialization constant will never change and driver uniforms entirely
    govern y-flip.  On platforms that do support pre-rotation, only two
    variations of the pipeline are needed.
    
    Bug: angleproject:5906
    Change-Id: I73f84e89fa9349d2098fa5b21573aee57d93a30c

--- src/libANGLE/renderer/vulkan/ProgramExecutableVk.h
+++ src/libANGLE/renderer/vulkan/ProgramExecutableVk.h
@@ -53,11 +53,11 @@ struct ProgramTransformOptions final
 {
     uint8_t enableLineRasterEmulation : 1;
     uint8_t removeEarlyFragmentTestsOptimization : 1;
-    uint8_t surfaceRotation : 3;
+    uint8_t surfaceRotation : 1;
     uint8_t enableDepthCorrection : 1;
     uint8_t removeTransformFeedbackEmulation : 1;
-    uint8_t reserved : 1;  // must initialize to zero
-    static constexpr uint32_t kPermutationCount = 0x1 << 7;
+    uint8_t reserved : 3;  // must initialize to zero
+    static constexpr uint32_t kPermutationCount = 0x1 << 5;
 };
 static_assert(sizeof(ProgramTransformOptions) == 1, "Size check failed");
 static_assert(static_cast<int>(SurfaceRotation::EnumCount) <= 8, "Size check failed");
