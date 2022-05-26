e251cf08a0147234579917726eb17d50fc19635f
Yuxin Hu <yuxinhu@google.com>
2022-05-26 15:05:52 -0700

    Vulkan: Handling VK_EXT_load_store_op_none on ARM
    There is a bug on at least one ARM device when either
    depth or stencil load op is none. Add a temporary work
    around to avoid this case for ARM driver until
    angleproject:7370 is addressed. Also add a test to
    reproduce the deqp test failures with simiplied draw calls.
    
    Bug: angleproject:7370
    Bug: angleproject:7351
    Change-Id: I74ac49a75f85b1e006ed3b1d77f234f63d314ae0

--- src/libANGLE/renderer/vulkan/vk_helpers.cpp
+++ src/libANGLE/renderer/vulkan/vk_helpers.cpp
@@ -2036,6 +2036,21 @@ void RenderPassCommandBufferHelper::finalizeDepthStencilLoadStore(Context *conte
                                          mRenderPassDesc.hasStencilUnresolveAttachment(),
                                          &stencilLoadOp, &stencilStoreOp, &isStencilInvalidated);
 
+    const bool disableMixedDepthStencilLoadStoreNoneAndLoad =
+        context->getRenderer()->getFeatures().disallowMixedDepthStencilLoadOpNoneAndLoad.enabled;
+
+    if (disableMixedDepthStencilLoadStoreNoneAndLoad)
+    {
+        if (depthLoadOp == RenderPassLoadOp::None && stencilLoadOp != RenderPassLoadOp::None)
+        {
+            depthLoadOp = RenderPassLoadOp::Load;
+        }
+        if (depthLoadOp != RenderPassLoadOp::None && stencilLoadOp == RenderPassLoadOp::None)
+        {
+            stencilLoadOp = RenderPassLoadOp::Load;
+        }
+    }
+
     if (isDepthInvalidated)
     {
         dsOps.isInvalidated = true;
