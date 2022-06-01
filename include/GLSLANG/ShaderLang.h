0a6bfd7756e521f54c769586f499a24eca9fb83f
Amirali Abdolrashidi <abdolrashidi@google.com>
2022-06-01 10:54:03 -0700

    Change LineRasterEmulation into driver uniform
    * Changed Bresenham line raster emulation from a specialization
    constant into a driver uniform.
    * Added an option for SimpleOperationTest to run with enabled
    BasicGLLineRasterization and disabled BresenhamLineRasterization
    for Vulkan.
    
    Bug: angleproject:7366
    Change-Id: If17c8ce9b459ad801bae8e887e5674bd9a3ff2bf

--- include/GLSLANG/ShaderLang.h
+++ include/GLSLANG/ShaderLang.h
@@ -26,7 +26,7 @@
 
 // Version number for shader translation API.
 // It is incremented every time the API changes.
-#define ANGLE_SH_VERSION 277
+#define ANGLE_SH_VERSION 278
 
 enum ShShaderSpec
 {
@@ -831,21 +831,19 @@ namespace vk
 // Specialization constant ids
 enum class SpecializationConstantId : uint32_t
 {
-    LineRasterEmulation = 0,
-    SurfaceRotation     = 1,
-    Dither              = 2,
+    SurfaceRotation = 0,
+    Dither          = 1,
 
-    InvalidEnum = 3,
+    InvalidEnum = 2,
     EnumCount   = InvalidEnum,
 };
 
 enum class SpecConstUsage : uint32_t
 {
-    LineRasterEmulation = 0,
-    Rotation            = 1,
-    Dither              = 2,
+    Rotation = 0,
+    Dither   = 1,
 
-    InvalidEnum = 3,
+    InvalidEnum = 2,
     EnumCount   = InvalidEnum,
 };
 
@@ -875,7 +873,8 @@ extern const char kDriverUniformsVarName[];
 // - 5 bits for advanced blend equation
 // - 6 bits for sample count
 // - 8 bits for enabled clip planes
-// - 12 bits unused
+// - 1 bit for line raster emulation
+// - 11 bits unused
 constexpr uint32_t kDriverUniformsMiscSwapXYMask                  = 0x1;
 constexpr uint32_t kDriverUniformsMiscAdvancedBlendEquationOffset = 1;
 constexpr uint32_t kDriverUniformsMiscAdvancedBlendEquationMask   = 0x1F;
@@ -883,6 +882,8 @@ constexpr uint32_t kDriverUniformsMiscSampleCountOffset           = 6;
 constexpr uint32_t kDriverUniformsMiscSampleCountMask             = 0x3F;
 constexpr uint32_t kDriverUniformsMiscEnabledClipPlanesOffset     = 12;
 constexpr uint32_t kDriverUniformsMiscEnabledClipPlanesMask       = 0xFF;
+constexpr uint32_t kDriverUniformsMiscLineRasterEmulationOffset   = 20;
+constexpr uint32_t kDriverUniformsMiscLineRasterEmulationMask     = 0x1;
 
 // Interface block array name used for atomic counter emulation
 extern const char kAtomicCountersBlockName[];
