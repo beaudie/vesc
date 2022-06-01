5313583fc450d8286fbfd89535dce66d4b164814
Amirali Abdolrashidi <abdolrashidi@google.com>
2022-06-01 10:54:03 -0700

    Vulkan: Remove enableLineRasterEmulation
    * Removed the Bresenham line raster emulation specialization
    constant, along with related variables and functions.
    
    Bug: angleproject:7366
    Change-Id: If17c8ce9b459ad801bae8e887e5674bd9a3ff2bf

--- include/GLSLANG/ShaderLang.h
+++ include/GLSLANG/ShaderLang.h
@@ -26,7 +26,7 @@
 
 // Version number for shader translation API.
 // It is incremented every time the API changes.
-#define ANGLE_SH_VERSION 277
+#define ANGLE_SH_VERSION 278
 
 enum ShShaderSpec
 {
@@ -304,10 +304,7 @@ const ShCompileOptions SH_REMOVE_DYNAMIC_INDEXING_OF_SWIZZLED_VECTOR = UINT64_C(
 // This flag works around a slow fxc compile performance issue with dynamic uniform indexing.
 const ShCompileOptions SH_ALLOW_TRANSLATE_UNIFORM_BLOCK_TO_STRUCTUREDBUFFER = UINT64_C(1) << 46;
 
-// This flag indicates whether Bresenham line raster emulation code should be generated.  This
-// emulation is necessary if the backend uses a differnet algorithm to draw lines.  Currently only
-// implemented for the Vulkan backend.
-const ShCompileOptions SH_ADD_BRESENHAM_LINE_RASTER_EMULATION = UINT64_C(1) << 47;
+// Note: bit 47 is unused
 
 // This flag allows disabling ARB_texture_rectangle on a per-compile basis. This is necessary
 // for WebGL contexts becuase ARB_texture_rectangle may be necessary for the WebGL implementation
@@ -321,7 +318,7 @@ const ShCompileOptions SH_REWRITE_ROW_MAJOR_MATRICES = UINT64_C(1) << 49;
 // Drop any explicit precision qualifiers from shader.
 const ShCompileOptions SH_IGNORE_PRECISION_QUALIFIERS = UINT64_C(1) << 50;
 
-// Bit 51 is unused.
+// Note: bit 51 is unused
 
 // Note: bit 52 is unused
 
@@ -831,21 +828,19 @@ namespace vk
 // Specialization constant ids
 enum class SpecializationConstantId : uint32_t
 {
-    LineRasterEmulation = 0,
-    SurfaceRotation     = 1,
-    Dither              = 2,
+    SurfaceRotation = 0,
+    Dither          = 1,
 
-    InvalidEnum = 3,
+    InvalidEnum = 2,
     EnumCount   = InvalidEnum,
 };
 
 enum class SpecConstUsage : uint32_t
 {
-    LineRasterEmulation = 0,
-    Rotation            = 1,
-    Dither              = 2,
+    Rotation = 0,
+    Dither   = 1,
 
-    InvalidEnum = 3,
+    InvalidEnum = 2,
     EnumCount   = InvalidEnum,
 };
 
@@ -886,9 +881,6 @@ constexpr uint32_t kDriverUniformsMiscEnabledClipPlanesMask       = 0xFF;
 
 // Interface block array name used for atomic counter emulation
 extern const char kAtomicCountersBlockName[];
-
-// Line raster emulation varying
-extern const char kLineRasterEmulationPosition[];
 
 // Transform feedback emulation support
 extern const char kXfbEmulationGetOffsetsFunctionName[];
