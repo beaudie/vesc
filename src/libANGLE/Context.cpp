50d9d9f0844eceb53b8658bb1111dc9b292ec419
Faye Zhang <ffz@google.com>
2022-06-06 15:29:16 -0700

    Implement GL_EXT_base_instance
    * Auto-generated the code to lay the foundation for
    the following functions:
      * drawArraysInstancedBaseInstance()
      * drawElementsInstancedBaseInstance()
      * drawElementsInstancedBaseVertexBaseInstance()
    
    Bug: angleproject:6983
    Change-Id: I36167faf3ca98e42acf787dbf09ee7052e15e358

--- src/libANGLE/Context.cpp
+++ src/libANGLE/Context.cpp
@@ -6705,6 +6705,16 @@ void Context::multiDrawElements(PrimitiveMode mode,
         mImplementation->multiDrawElements(this, mode, counts, type, indices, drawcount));
 }
 
+void Context::drawElementsInstancedBaseInstance(GLenum mode,
+                                                GLsizei count,
+                                                GLenum type,
+                                                const void *indices,
+                                                GLsizei instancecount,
+                                                GLuint baseinstance)
+{
+    UNIMPLEMENTED();
+}
+
 void Context::multiDrawElementsInstanced(PrimitiveMode mode,
                                          const GLsizei *counts,
                                          DrawElementsType type,
@@ -6760,6 +6770,15 @@ void Context::drawArraysInstancedBaseInstance(PrimitiveMode mode,
     MarkTransformFeedbackBufferUsage(this, count, 1);
 }
 
+void Context::drawArraysInstancedBaseInstanceANGLE(PrimitiveMode mode,
+                                                   GLint first,
+                                                   GLsizei count,
+                                                   GLsizei instanceCount,
+                                                   GLuint baseInstance)
+{
+    drawArraysInstancedBaseInstance(mode, first, count, instanceCount, baseInstance);
+}
+
 void Context::drawElementsInstancedBaseVertexBaseInstance(PrimitiveMode mode,
                                                           GLsizei count,
                                                           DrawElementsType type,
@@ -6793,6 +6812,18 @@ void Context::drawElementsInstancedBaseVertexBaseInstance(PrimitiveMode mode,
 
     ANGLE_CONTEXT_TRY(mImplementation->drawElementsInstancedBaseVertexBaseInstance(
         this, mode, count, type, indices, instanceCount, baseVertex, baseInstance));
+}
+
+void Context::drawElementsInstancedBaseVertexBaseInstanceANGLE(PrimitiveMode mode,
+                                                               GLsizei count,
+                                                               DrawElementsType type,
+                                                               const GLvoid *indices,
+                                                               GLsizei instanceCount,
+                                                               GLint baseVertex,
+                                                               GLuint baseInstance)
+{
+    drawElementsInstancedBaseVertexBaseInstance(mode, count, type, indices, instanceCount,
+                                                baseVertex, baseInstance);
 }
 
 void Context::multiDrawArraysInstancedBaseInstance(PrimitiveMode mode,
