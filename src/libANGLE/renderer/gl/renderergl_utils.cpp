476d1c070c3e0db9c2d2248c8b3adcf49258635e
Geoff Lang <geofflang@google.com>
2022-06-07 13:27:04 -0400

    GL: Emulate texture storage luma formats in GLES3.
    Bug: chromium:983167
    Change-Id: I3978f347c28c3d9360aa9345d7f50270d5915183

--- src/libANGLE/renderer/gl/renderergl_utils.cpp
+++ src/libANGLE/renderer/gl/renderergl_utils.cpp
@@ -1374,6 +1374,7 @@ void GenerateCaps(const FunctionsGL *functions,
          functions->hasGLESExtension("GL_EXT_draw_buffers_indexed"));
     extensions->drawBuffersIndexedOES = extensions->drawBuffersIndexedEXT;
     extensions->textureStorageEXT     = functions->standard == STANDARD_GL_DESKTOP ||
+                                    functions->isAtLeastGLES(gl::Version(3, 0)) ||
                                     functions->hasGLESExtension("GL_EXT_texture_storage");
     extensions->textureFilterAnisotropicEXT =
         functions->hasGLExtension("GL_EXT_texture_filter_anisotropic") ||
