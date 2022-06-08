3ade00b3712ac19dba4e4a832bce85d95bbddebd
Xu Xing <xing.xu@intel.com>
2022-06-08 12:49:03 +0800

    Support timestamp behind flag enableTimestampQueries-t
    Bug: angleproject:7367
    Change-Id: I8805592aa30c836594ae990cc6076b49407cd33b

--- util/angle_features_autogen.h
+++ util/angle_features_autogen.h
@@ -104,6 +104,7 @@ enum class Feature
     EnablePrecisionQualifiers,
     EnablePreRotateSurfaces,
     EnableProgramBinaryForCapture,
+    EnableTimestampQueries,
     ExpandIntegerPowExpressions,
     ExplicitlyEnablePerSampleShading,
     ExposeNonConformantExtensionsAndVersions,
