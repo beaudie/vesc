00dbc7fbc438d42f30d7f1971cdbf3bc67308061
Jamie Madill <jmadill@chromium.org>
2022-06-15 11:00:21 -0400

    Android: Add python tests for android_helper.py.
    Bug: angleproject:7404
    Change-Id: Idabc435ee59ec58dcdda1b0fba09fa9faafdf932

---
+++ src/tests/py_utils/android_helper_tests.py
@@ -0,0 +1,9 @@
+# Copyright 2022 The ANGLE Project Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+# android_helper_tests: Self-tests for android_helper.py
+
+import android_helper
+
+android_helper.RunSmokeTest()
