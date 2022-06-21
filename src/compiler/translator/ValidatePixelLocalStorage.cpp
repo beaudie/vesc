c3c546351006308e34f5bf25324a265f05ce948a
Chris Dalton <chris@rive.app>
2022-06-21 11:20:45 -0600

    Implement GLSL additions for ANGLE_shader_pixel_local_storage
    Specs out, implements, and thoroughly tests the GLSL additions for
    ANGLE_shader_pixel_local_storage. Adds a simple transformation that
    rewrites PLS directly into shader images. Updates the existing PLS tests
    to use the newly built-in PLS features and ensures they continue
    passing.
    
    For now, applications call glBindImageTexture to configure their pixel
    local storage. The OpenGL ES API side of this extension will follow
    shortly.
    
    Bug: angleproject:7279
    Bug: angleproject:7484
    Change-Id: I141183069b5cbfcca01cbb77b5b36d3e5f834bf5

---
+++ src/compiler/translator/ValidatePixelLocalStorage.cpp
@@ -0,0 +1,118 @@
+//
+// Copyright 2002 The ANGLE Project Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+
+#include "compiler/translator/ValidatePixelLocalStorage.h"
+
+#include <GLES2/gl2.h>
+#include "compiler/translator/Diagnostics.h"
+#include "compiler/translator/Symbol.h"
+#include "compiler/translator/tree_util/IntermTraverse.h"
+
+namespace sh
+{
+
+namespace
+{
+
+class PLSErrorTraverser : public TLValueTrackingTraverser
+{
+  public:
+    PLSErrorTraverser(TSymbolTable *symbolTable, TDiagnostics *diagnostics)
+        : TLValueTrackingTraverser(true, false, true, symbolTable), mDiagnostics(diagnostics)
+    {}
+
+    bool visitFunctionDefinition(Visit visit, TIntermFunctionDefinition *function) override
+    {
+        mVisitingMain = visit == Visit::PreVisit && function->getFunction()->isMain();
+        return true;
+    }
+
+    bool visitBranch(Visit visit, TIntermBranch *branch) override
+    {
+        if (visit != Visit::PreVisit)
+        {
+            return true;
+        }
+
+        switch (branch->getFlowOp())
+        {
+            case EOpKill:
+                // Discard is not allowed when pixel local storage has been declared. When
+                // polyfilled with shader images, pixel local storage requires early_fragment_tests,
+                // which causes discard to interact differently with the depth and stencil tests.
+                //
+                // To ensure identical behavior across all backends (some of which may not have
+                // access to early_fragment_tests), we disallow discard if pixel local storage has
+                // been declared.
+                error(branch->getLine(), "illegal discard when pixel local storage is declared",
+                      "discard");
+                break;
+
+            case EOpReturn:
+                // Returning from main isn't allowed when pixel local storage has been declared.
+                // (ARB_fragment_shader_interlock isn't allowed after return from main.)
+                if (mVisitingMain)
+                {
+                    error(branch->getLine(),
+                          "illegal return from main when pixel local storage is declared",
+                          "return");
+                }
+                break;
+
+            default:
+                break;
+        }
+
+        return true;
+    }
+
+    void visitSymbol(TIntermSymbol *symbol) override
+    {
+        if (isLValueRequiredHere())
+        {
+            // When polyfilled with shader images, pixel local storage requires
+            // early_fragment_tests, which causes assignments to gl_FragDepth(EXT) and gl_SampleMask
+            // to be ignored.
+            //
+            // To ensure identical behavior across all backends, we disallow assignment to these
+            // values if pixel local storage has been declared.
+            if (symbol->getName().beginsWith("gl_FragDepth") ||  // gl_FragDepth, gl_FragDepthEXT,
+                symbol->getName().beginsWith("gl_SampleMask"))
+            {
+                error(symbol->getLine(),
+                      "value not assignable when pixel local storage is declared",
+                      symbol->getName().data());
+            }
+        }
+    }
+
+    bool hasErrors() const { return mHasErrors; }
+
+  private:
+    void error(const TSourceLoc &loc, const char *reason, const char *token)
+    {
+        mDiagnostics->error(loc, reason, token);
+        mHasErrors = true;
+    }
+
+    TDiagnostics *mDiagnostics;
+    angle::HashSet<int> mPLSBindings;
+    bool mHasErrors    = false;
+    bool mVisitingMain = false;
+};
+
+}  // namespace
+
+bool ValidatePixelLocalStorage(TIntermBlock *root,
+                               TSymbolTable *symbolTable,
+                               TDiagnostics *diagnostics)
+{
+    PLSErrorTraverser traverser(symbolTable, diagnostics);
+    root->traverse(&traverser);
+    return !traverser.hasErrors();
+}
+
+}  // namespace sh
