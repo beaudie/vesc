b65aef20b9b5f8b54f698834e4d112bdbb8a9084
Cody Northrop <cnorthrop@google.com>
2022-06-22 17:43:15 -0600

    Textures: Centralize texture format emulation check
    Refactor in order to use the check in multiple places.
    
    Bug: b/236478448
    Change-Id: I6df2e0d63c60fa3e8b2d65185fce164be0e7d961

--- src/tests/gl_tests/GetImageTest.cpp
+++ src/tests/gl_tests/GetImageTest.cpp
@@ -7,6 +7,7 @@
 //   Tests for the ANGLE_get_image extension.
 //
 
+#include "common/PackedEnums.h"
 #include "test_utils/ANGLETest.h"
 #include "test_utils/gl_raii.h"
 
@@ -370,7 +371,7 @@ TEST_P(GetImageTest, GetTexImageLuminance)
 
     constexpr GLColorR kMediumLuminance = GLColorR(kUNormHalf);
     std::vector<GLColorR> expectedData  = {kMediumLuminance, kMediumLuminance, kMediumLuminance,
-                                          kMediumLuminance};
+                                           kMediumLuminance};
 
     glViewport(0, 0, kSmallSize, kSmallSize);
 
@@ -717,7 +718,9 @@ bool IsFormatEmulated(GLenum target)
 {
     GLint readFormat;
     glGetTexParameteriv(target, GL_IMPLEMENTATION_COLOR_READ_FORMAT, &readFormat);
-    return readFormat == GL_RGBA || readFormat == GL_RG || readFormat == GL_RED;
+
+    // This helper only works for compressed formats
+    return gl::IsEmulatedCompressedFormat(readFormat);
 }
 
 // Basic GetCompressedTexImage.
