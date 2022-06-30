cfde0c323744e933f00359ebbbf8d123c18046c7
Bo Liu <boliu@google.com>
2022-06-30 16:47:52 -0400

    Make limit_max_texture_size_to_4096 webgl only
    Bug: chromium:1319451
    Change-Id: I7dedeb8d738c1070e650650e893d57984a9e7956

--- util/angle_features_autogen.h
+++ util/angle_features_autogen.h
@@ -151,7 +151,7 @@ enum class Feature
     LimitMaxColorTargetBitsForTesting,
     LimitMaxDrawBuffersForTesting,
     LimitMaxMSAASamplesTo4,
-    LimitMaxTextureSizeTo4096,
+    LimitWebglMaxTextureSizeTo4096,
     LogMemoryReportCallbacks,
     LogMemoryReportStats,
     LoseContextOnOutOfMemory,
