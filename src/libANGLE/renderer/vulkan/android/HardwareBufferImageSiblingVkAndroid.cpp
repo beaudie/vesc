c1c4bf6db180f038e27878c315f63afd81ee7fb6
Mohan Maiya <m.maiya@samsung.com>
2022-06-30 09:56:54 -0700

    Vulkan: min/mag filters follow chroma filter value
    The Vulkan spec states that for those formats lacking support for
    VK_FORMAT_FEATURE_SAMPLED_IMAGE_YCBCR_CONVERSION_SEPARATE_RECONSTRUCTION_FILTER_BIT
    the min and mag filter must be equal to the sampler YCbCr conversion's
    chroma filter. Having the min/mag filters follow the chroma filter
    leads to simpler and more efficient code.
    
    Also update getPreferredFilterForYUV function to return existing
    filter value when preferLinearFilterForYUV feature is disabled.
    
    Bug: angleproject:7382
    Bug: angleproject:7392
    Tests: Texture2DTestES3YUV.TexStorage2DYuvFilterModes*
    Tests: ImageTestES3.SourceYUVAHBTargetExternalYUVSampleLinearFiltering/ES3_Vulkan
    Change-Id: I550ef8feede1dc6c3a0d8e32f790113e90ef7a4b

--- src/libANGLE/renderer/vulkan/android/HardwareBufferImageSiblingVkAndroid.cpp
+++ src/libANGLE/renderer/vulkan/android/HardwareBufferImageSiblingVkAndroid.cpp
@@ -364,7 +364,7 @@ angle::Result HardwareBufferImageSiblingVkAndroid::initImpl(DisplayVk *displayVk
             renderer, bufferFormatProperties.externalFormat,
             bufferFormatProperties.suggestedYcbcrModel, bufferFormatProperties.suggestedYcbcrRange,
             bufferFormatProperties.suggestedXChromaOffset,
-            bufferFormatProperties.suggestedYChromaOffset, renderer->getPreferredFilterForYUV(),
+            bufferFormatProperties.suggestedYChromaOffset, kDefaultYCbCrChromaFilter,
             bufferFormatProperties.samplerYcbcrConversionComponents, angle::FormatID::NONE);
         mYUV = true;
     }
