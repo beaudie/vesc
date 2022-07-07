fa511e6bd33d87df739e1db55aa64f71e1ab868d
Lingfeng Yang <lfy@google.com>
2022-07-07 14:28:06 -0700

    Vulkan: __samplerExternal2DY2YEXT-aware TextureVk
    This CL adds the ability for TextureVk to return an ImageView that is
    created with a VkSamplerYcbcrConversion object that uses an identity
    conversion model.
    
    This allows direct sampling of YUV values without RGB conversion, which
    is needed for __samplerExternal2DY2YEXT.
    
    Bug: b/223456677
    Change-Id: Ie1d4e12375b7808a1f060747bc2d74baeda3fdea

--- src/libANGLE/renderer/vulkan/FramebufferVk.cpp
+++ src/libANGLE/renderer/vulkan/FramebufferVk.cpp
@@ -1343,7 +1343,7 @@ angle::Result FramebufferVk::blit(const gl::Context *context,
                 ANGLE_TRY(depthStencilImage->initLayerImageView(
                     contextVk, textureType, VK_IMAGE_ASPECT_DEPTH_BIT, gl::SwizzleState(),
                     &depthView.get(), levelIndex, 1, layerIndex, 1,
-                    gl::SrgbWriteControlMode::Default));
+                    gl::SrgbWriteControlMode::Default, false /* non-samplerExternal2DY2YEXT */));
             }
 
             if (blitStencilBuffer)
@@ -1351,7 +1351,7 @@ angle::Result FramebufferVk::blit(const gl::Context *context,
                 ANGLE_TRY(depthStencilImage->initLayerImageView(
                     contextVk, textureType, VK_IMAGE_ASPECT_STENCIL_BIT, gl::SwizzleState(),
                     &stencilView.get(), levelIndex, 1, layerIndex, 1,
-                    gl::SrgbWriteControlMode::Default));
+                    gl::SrgbWriteControlMode::Default, false /* non-samplerExternal2DY2YEXT */));
             }
 
             // If shader stencil export is not possible, defer stencil blit/stencil to another pass.
