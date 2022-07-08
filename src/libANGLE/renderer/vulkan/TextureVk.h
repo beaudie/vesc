849c6eef4bac7c590a5254b6bc60929a9e9a3e3e
Lingfeng Yang <lfy@google.com>
2022-07-07 17:03:27 -0700

    Vulkan: __samplerExternal2DY2YEXT-aware ProgramExecutableVk
    This CL incorporates __samplerExternal2DY2YEXT into the Vulkan objects
    involved in ProgramExecutableVk. Specifically, we need:
    
    1. TextureVk to create and return a VkSampler imbued with identity
       conversion model, so we can create an immutable sampler from it.
    2. Access to the identity conversion desc so that it can be traced in
       mImmutableSamplerIndexMap.
    
    Bug: b/223456677
    Change-Id: Ia302226a8aa4a97d4a42f79c79ce47af3abec37c

--- src/libANGLE/renderer/vulkan/TextureVk.h
+++ src/libANGLE/renderer/vulkan/TextureVk.h
@@ -239,10 +239,21 @@ class TextureVk : public TextureImpl, public angle::ObserverInterface
                                       const gl::ImageUnit &binding,
                                       const vk::ImageView **imageViewOut);
 
-    const vk::SamplerHelper &getSampler() const
+    const vk::SamplerHelper &getSampler(bool isSamplerExternalY2Y) const
     {
+        if (isSamplerExternalY2Y)
+        {
+            ASSERT(mSamplerExternal2DY2YEXTSampler.valid());
+            return mSamplerExternal2DY2YEXTSampler.get();
+        }
         ASSERT(mSampler.valid());
         return mSampler.get();
+    }
+
+    void resetSampler()
+    {
+        mSampler.reset();
+        mSamplerExternal2DY2YEXTSampler.reset();
     }
 
     // Normally, initialize the image with enabled mipmap level counts.
@@ -601,6 +612,9 @@ class TextureVk : public TextureImpl, public angle::ObserverInterface
     // |mSampler| contains the relevant Vulkan sampler states representing the OpenGL Texture
     // sampling states for the Texture.
     vk::SamplerBinding mSampler;
+    // |mSamplerExternal2DY2YEXTSampler| contains a version of mSampler that is meant for use with
+    // __samplerExternal2DY2YEXT (i.e., skipping conversion of YUV to RGB).
+    vk::SamplerBinding mSamplerExternal2DY2YEXTSampler;
 
     // The created vkImage usage flag.
     VkImageUsageFlags mImageUsageFlags;
