a482f1fa82b685ee4e9709b43d293f7a51b309f2
Lingfeng Yang <lfy@google.com>
2022-07-14 13:25:04 -0700

    glValidateProgramPipelines: Skip draw state error check
    Certain apps will call this API while having a surfaceless context,
    making it improperly fail.
    
    dEQP tests that previously relied on the full draw states check
    were actually relying on the part where the draw state check included a
    check that the pipeline object had programs/shaders. Relax the
    validation of the pipeline to only include that validation, and not also
    the framebuffer state / VBO state / etc.
    
    Bug: b/223456677
    Change-Id: I9211761934668aae8a20f07ac4f36b7f6c1281da

--- src/libANGLE/Context.cpp
+++ src/libANGLE/Context.cpp
@@ -9803,6 +9803,7 @@ StateCache::StateCache()
       mCachedInstancedVertexElementLimit(0),
       mCachedBasicDrawStatesError(kInvalidPointer),
       mCachedBasicDrawElementsError(kInvalidPointer),
+      mCachedProgramPipelineError(kInvalidPointer),
       mCachedTransformFeedbackActiveUnpaused(false),
       mCachedCanDraw(false)
 {
@@ -9906,6 +9907,11 @@ void StateCache::updateBasicDrawStatesError()
     mCachedBasicDrawStatesError = kInvalidPointer;
 }
 
+void StateCache::updateProgramPipelineError()
+{
+    mCachedProgramPipelineError = kInvalidPointer;
+}
+
 void StateCache::updateBasicDrawElementsError()
 {
     mCachedBasicDrawElementsError = kInvalidPointer;
@@ -9915,7 +9921,21 @@ intptr_t StateCache::getBasicDrawStatesErrorImpl(const Context *context) const
 {
     ASSERT(mCachedBasicDrawStatesError == kInvalidPointer);
     mCachedBasicDrawStatesError = reinterpret_cast<intptr_t>(ValidateDrawStates(context));
+    // Set mCachedProgramPipelineError to valid (nullptr) only if mCachedBasicDrawStatesError is
+    // valid.
+    if (mCachedBasicDrawStatesError == 0)
+    {
+        mCachedProgramPipelineError = mCachedBasicDrawStatesError;
+    }
     return mCachedBasicDrawStatesError;
+}
+
+intptr_t StateCache::getProgramPipelineErrorImpl(const Context *context) const
+{
+    ASSERT(mCachedProgramPipelineError == kInvalidPointer ||
+           (mCachedBasicDrawStatesError == 0 && mCachedProgramPipelineError == 0));
+    mCachedProgramPipelineError = reinterpret_cast<intptr_t>(ValidateProgramPipeline(context));
+    return mCachedProgramPipelineError;
 }
 
 intptr_t StateCache::getBasicDrawElementsErrorImpl(const Context *context) const
@@ -9938,6 +9958,7 @@ void StateCache::onProgramExecutableChange(Context *context)
     updateActiveAttribsMask(context);
     updateVertexElementLimits(context);
     updateBasicDrawStatesError();
+    updateProgramPipelineError();
     updateValidDrawModes(context);
     updateActiveShaderStorageBufferIndices(context);
     updateActiveImageUnitIndices(context);
