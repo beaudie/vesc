da46460385b7c1fc09a9842a504f1a72fec30744
Amirali Abdolrashidi <abdolrashidi@google.com>
2022-07-14 16:42:30 -0700

    Vulkan: Use push constants for driver uniforms
    * Updated the driver uniforms so they would be defined as push
      constants in SPIR-V. Their data would be updated via
      pushConstants() when handling the driver uniform dirty bits.
    
    * Updated TOutputVulkanGLSL to be able to generate the push
      constants as required.
    
    * When handling the driver uniform dirty bits, we no longer
      allocate a buffer.
    
    * Removed the driver uniform descriptor set from the pipeline
      layout.
    
    * Removed the binding-related functions and flags for the driver
      uniforms.
    
      * In invalidateGraphicsDescriptorSet(), DIRTY_BIT_DESCRIPTOR_SETS
      is used instead of DIRTY_BIT_DRIVER_UNIFORMS_BINDING. (Same for
      invalidateComputeDescriptorSet())
    
      * DIRTY_BIT_DRIVER_UNIFORMS_BINDING is replaced with
      DIRTY_BIT_DRIVER_UNIFORMS in other places.
    
    * Removed mDriverUniform and DriverUniformsDescriptorSet from
      ContextVk.
    
    * Added mSupportedVulkanShaderStageMask to RendererVk, which is used
      in creating the pipeline layout and updating the push constants.
    
    * Added a TODO note for driverUniformsDescriptorSetIndex.
    
    Bug: angleproject:6858
    Change-Id: I91037d378528962a816b12ff3f21249ee17b7652

--- src/libANGLE/renderer/vulkan/GlslangWrapperVk.cpp
+++ src/libANGLE/renderer/vulkan/GlslangWrapperVk.cpp
@@ -39,6 +39,9 @@ void GlslangWrapperVk::ResetGlslangProgramInterfaceInfo(
     glslangProgramInterfaceInfo->shaderResourceDescriptorSetIndex =
         ToUnderlying(DescriptorSetIndex::ShaderResource);
     glslangProgramInterfaceInfo->currentShaderResourceBindingIndex = 0;
+
+    // TODO: driverUniformsDescriptorSetIndex is no longer used in Vulkan. It can be removed
+    // altogether if Metal discontinues using it as well.
     glslangProgramInterfaceInfo->driverUniformsDescriptorSetIndex =
         ToUnderlying(DescriptorSetIndex::Internal);
 
