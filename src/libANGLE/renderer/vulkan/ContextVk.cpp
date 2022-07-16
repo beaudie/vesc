fbef7a92011931d597e10709ff75555f90bfaae5
Charlie Lao <cclao@google.com>
2022-07-15 18:17:01 -0700

    Debug: log DescriptorSet cache and pool info
    Bug: b/237686097
    Change-Id: Ib95d08961ef0da00f1c67fea6f86bd084f581330

--- src/libANGLE/renderer/vulkan/ContextVk.cpp
+++ src/libANGLE/renderer/vulkan/ContextVk.cpp
@@ -996,6 +996,8 @@ ContextVk::~ContextVk()
 
 void ContextVk::onDestroy(const gl::Context *context)
 {
+    mShareGroupVk->logDescriptorPool();
+
     // Remove context from the share group
     mShareGroupVk->removeContext(this);
 
