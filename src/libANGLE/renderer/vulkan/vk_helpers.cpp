74c6e72905b38e2926965dae52b26671a3fb5838
Charlie Lao <cclao@google.com>
2022-07-15 18:17:01 -0700

    Debug: log DescriptorSet cache and pool info
    Bug: b/237686097
    Change-Id: Ib95d08961ef0da00f1c67fea6f86bd084f581330

--- src/libANGLE/renderer/vulkan/vk_helpers.cpp
+++ src/libANGLE/renderer/vulkan/vk_helpers.cpp
@@ -3610,6 +3610,13 @@ void DynamicDescriptorPool::SetMaxSetsPerPoolMultiplierForTesting(uint32_t maxSe
     mMaxSetsPerPoolMultiplier = maxSetsPerPoolMultiplier;
 }
 
+void DynamicDescriptorPool::logDescriptorPool(std::ostringstream *out) const
+{
+    *out << "{" << mDescriptorPools.size() << "," << mDescriptorSetCache.getTotalCacheSize() << ", "
+         << mCacheStats.getHitRatio() << "}";
+    mCacheStats.resetHitAndMissCount();
+}
+
 // DynamicallyGrowingPool implementation
 template <typename Pool>
 DynamicallyGrowingPool<Pool>::DynamicallyGrowingPool()
@@ -10390,6 +10397,18 @@ angle::Result MetaDescriptorPool::bindCachedDescriptorPool(
     descriptorPoolOut->set(&descriptorPool);
 
     return angle::Result::Continue;
+}
+
+void MetaDescriptorPool::logDescriptorPool(std::ostringstream *out) const
+{
+    *out << "{";
+    for (const auto &iter : mPayload)
+    {
+        const RefCountedDescriptorPool &pool = iter.second;
+        pool.get().logDescriptorPool(out);
+        *out << ", ";
+    }
+    *out << "} ";
 }
 
 static_assert(static_cast<uint32_t>(PresentMode::ImmediateKHR) == VK_PRESENT_MODE_IMMEDIATE_KHR,
