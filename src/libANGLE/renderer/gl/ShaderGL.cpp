84d7ce0b00851489b6686286d40219b36020e208
Chris Dalton <chris@rive.app>
2022-07-16 19:58:32 -0600

    Make PLS coherent on desktop OpenGL
    Implements ANGLE_shader_pixel_local_storage_coherent using fragment
    shader synchronization extensions:
    
      NV_fragment_shader_interlock
      INTEL_fragment_shader_ordering
      ARB_fragment_shader_interlock
    
    With these extensions combined, we get coherency all 3 big desktop
    vendors: NVIDIA, Intel, and AMD.
    
    Bug: angleproject:7279
    Change-Id: Ie20b251fb772898e89994b799640f1f2806581eb

--- src/libANGLE/renderer/gl/ShaderGL.cpp
+++ src/libANGLE/renderer/gl/ShaderGL.cpp
@@ -370,6 +370,31 @@ std::shared_ptr<WaitableCompileEvent> ShaderGL::compile(const gl::Context *conte
         additionalOptions |= SH_REWRITE_ROW_MAJOR_MATRICES;
     }
 
+    // Prefer vendor-specific extensions first. The Coherency test doesn't always pass on Intel when
+    // we use the ARB extension.
+    if (features.supportsFragmentShaderInterlockNV.enabled)
+    {
+        // We advertise ANGLE_shader_pixel_local_storage_coherent based on the existence of this
+        // extension, so we better only use compatible backend GLSL versions.
+        ASSERT(compilerInstance->getShaderOutputType() >= SH_GLSL_430_CORE_OUTPUT);
+        additionalOptions |= SH_FRAGMENT_SYNCHRONIZATION_TYPE_NV;
+    }
+    else if (features.supportsFragmentShaderOrderingINTEL.enabled)
+    {
+        // We advertise ANGLE_shader_pixel_local_storage_coherent based on the existence of this
+        // extension, so we better only use compatible backend GLSL versions.
+        ASSERT(compilerInstance->getShaderOutputType() >= SH_GLSL_440_CORE_OUTPUT);
+        // Prioritize INTEL_fragment_shader_ordering last, since it doesn't have an end() delimiter.
+        additionalOptions |= SH_FRAGMENT_SYNCHRONIZATION_TYPE_INTEL;
+    }
+    else if (features.supportsFragmentShaderInterlockARB.enabled)
+    {
+        // We advertise ANGLE_shader_pixel_local_storage_coherent based on the existence of this
+        // extension, so we better only use compatible backend GLSL versions.
+        ASSERT(compilerInstance->getShaderOutputType() >= SH_GLSL_450_CORE_OUTPUT);
+        additionalOptions |= SH_FRAGMENT_SYNCHRONIZATION_TYPE_ARB;
+    }
+
     options |= additionalOptions;
 
     auto workerThreadPool = context->getShaderCompileThreadPool();
