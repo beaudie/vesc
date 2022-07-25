b1740a6c55adba824b4d646fd62891e23a5d972e
Amirali Abdolrashidi <abdolrashidi@google.com>
2022-07-25 14:22:28 -0700

    Making changes to remove the descriptor set
    Bug: None
    Change-Id: I2c704d1cd62c1f170d361842b1b49d72c4a53f68

--- src/compiler/translator/tree_util/DriverUniform.cpp
+++ src/compiler/translator/tree_util/DriverUniform.cpp
@@ -66,6 +66,7 @@ bool DriverUniform::addComputeDriverUniformsToShader(TIntermBlock *root, TSymbol
     // Define a driver uniform block "ANGLEUniformBlock" with instance name "ANGLEUniforms".
     TLayoutQualifier layoutQualifier = TLayoutQualifier::Create();
     layoutQualifier.blockStorage     = EbsStd140;
+    layoutQualifier.pushConstant     = true;
 
     mDriverUniforms = DeclareInterfaceBlock(root, symbolTable, driverFieldList, EvqUniform,
                                             layoutQualifier, TMemoryQualifier::Create(), 0,
