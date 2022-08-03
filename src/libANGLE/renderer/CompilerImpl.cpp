174d015e00d3150507d2ec4f42fb82c202027e16
Chris Dalton <chris@rive.app>
2022-08-03 15:43:29 -0600

    Make PLS coherent on desktop OpenGL
    Implements ANGLE_shader_pixel_local_storage_coherent using fragment
    shader synchronization extensions:
    
      NV_fragment_shader_interlock
      INTEL_fragment_shader_ordering
      ARB_fragment_shader_interlock
    
    With these extensions combined, we get coherency all 3 big desktop
    vendors: NVIDIA, Intel, and AMD.
    
    Bug: angleproject:7279
    Change-Id: Ie20b251fb772898e89994b799640f1f2806581eb

---
+++ src/libANGLE/renderer/CompilerImpl.cpp
@@ -0,0 +1,19 @@
+//
+// Copyright 2022 The ANGLE Project Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+
+// CompilerImpl.cpp: Implementation methods rx::CompilerImpl class.
+
+#include "libANGLE/renderer/CompilerImpl.h"
+
+namespace rx
+{
+
+CompilerBackendFeatures CompilerImpl::getBackendFeatures() const
+{
+    return CompilerBackendFeatures();
+}
+
+}  // namespace rx
