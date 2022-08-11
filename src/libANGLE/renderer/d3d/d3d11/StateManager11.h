1090fe580240f1dea9189d56b1ff3f50f4bdd7a4
Patrick To <patrto@microsoft.com>
2022-08-11 03:29:16 -0700

    Add Direct Composition offset to gl_FragCoord
    DComp usually gives us an offset at (0, 0), but this is not always the
    case. It is valid for DComp to give us an offset into a texture atlas,
    for example with some video overlays. If we do have an offset,
    gl_FragCoord must also be offset to point to the correct pixel in the
    surface.
    
    Bug: angleproject:1269749
    Change-Id: I3ca39860d176bcf9f1d520d781cfed2d1d1ad1dd

--- src/libANGLE/renderer/d3d/d3d11/StateManager11.h
+++ src/libANGLE/renderer/d3d/d3d11/StateManager11.h
@@ -42,6 +42,7 @@ class ShaderConstants11 : angle::NonCopyable
     void setMultiviewWriteToViewportIndex(GLfloat index);
     void onViewportChange(const gl::Rectangle &glViewport,
                           const D3D11_VIEWPORT &dxViewport,
+                          const gl::Offset &glOffset,
                           bool is9_3,
                           bool presentPathFast);
     bool onFirstVertexChange(GLint firstVertex);
@@ -106,6 +107,7 @@ class ShaderConstants11 : angle::NonCopyable
               viewCoords{.0f},
               depthFront{.0f},
               viewScale{.0f},
+              fragCoordOffset{.0f},
               multiviewWriteToViewportIndex{.0f},
               padding{.0f}
         {}
@@ -114,13 +116,14 @@ class ShaderConstants11 : angle::NonCopyable
         float viewCoords[4];
         float depthFront[4];
         float viewScale[2];
+        float fragCoordOffset[2];
         // multiviewWriteToViewportIndex is used to select either the side-by-side or layered
         // code-path in the GS. It's value, if set, is either 0.0f or 1.0f. The value is updated
         // whenever a multi-view draw framebuffer is made active.
         float multiviewWriteToViewportIndex;
 
         // Added here to manually pad the struct.
-        float padding;
+        float padding[3];
     };
     static_assert(sizeof(Pixel) % 16u == 0, "D3D11 constant buffers must be multiples of 16 bytes");
 
