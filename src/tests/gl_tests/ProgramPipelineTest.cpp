google/angle

commit 3a9f18f135fe82f77cd684d559f73f7030f22525

Refactor program pipeline handling.
In preparation for moving more code from gl::Program to
gl::ProgramExecutable so it can be shared with ProgramPipeline.

Bug: angleproject:6566
Change-Id: Icb7ecccb37ae8e0d7d5fef8968f0dd7ef6fe6150
Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/3226305
Reviewed-by: Tim Van Patten <timvp@google.com>
Reviewed-by: Cody Northrop <cnorthrop@google.com>
Commit-Queue: Jamie Madill <jmadill@chromium.org>

src/libANGLE/ProgramPipeline.cpp
@@ -124,7 +124,8 @@ void ProgramPipelineState::updateExecutableTextures()
     {
         const Program *program = getShaderProgram(shaderType);
         ASSERT(program);
-        mExecutable->setActiveTextureMask(program->getExecutable().getActiveSamplersMask());
+        mExecutable->setActiveTextureMask(mExecutable->getActiveSamplersMask() |
+                                          program->getExecutable().getActiveSamplersMask());
         mExecutable->setActiveImagesMask(mExecutable->getActiveImagesMask() |
                                          program->getExecutable().getActiveImagesMask());
         // Updates mActiveSamplerRefCounts, mActiveSamplerTypes, and mActiveSamplerFormats
@@ -197,15 +198,16 @@ void ProgramPipeline::activeShaderProgram(Program *shaderProgram)
     mState.activeShaderProgram(shaderProgram);
 }
 
-void ProgramPipeline::useProgramStages(const Context *context,
-                                       GLbitfield stages,
-                                       Program *shaderProgram)
+angle::Result ProgramPipeline::useProgramStages(const Context *context,
+                                                GLbitfield stages,
+                                                Program *shaderProgram)
 {
     mState.useProgramStages(context, stages, shaderProgram, &mProgramObserverBindings);
     updateLinkedShaderStages();
-    updateExecutable();
 
     mState.mIsLinked = false;
+
+    return link(context);
 }
 
 void ProgramPipeline::updateLinkedShaderStages()
@@ -432,8 +434,6 @@ void ProgramPipeline::updateHasBooleans()
 
 void ProgramPipeline::updateExecutable()
 {
-    mState.mExecutable->reset();
-
     // Vertex Shader ProgramExecutable properties
     updateExecutableAttributes();
     updateTransformFeedbackMembers();
@@ -471,6 +471,8 @@ angle::Result ProgramPipeline::link(const Context *context)
     ProgramVaryingPacking varyingPacking;
     LinkingVariables linkingVariables(mState);
 
+    mState.mExecutable->reset();
+
     if (mState.mExecutable->hasLinkedShaderStage(gl::ShaderType::Vertex))
     {
         InfoLog &infoLog = mState.mExecutable->getInfoLog();
@@ -486,6 +488,23 @@ angle::Result ProgramPipeline::link(const Context *context)
             return angle::Result::Stop;
         }
 
+        Program *fragmentShaderProgram = getShaderProgram(ShaderType::Fragment);
+        if (fragmentShaderProgram)
+        {
+            // We should also be validating image uniforms and SSBOs.
+            const int combinedImageUniforms             = 0;
+            const int combinedShaderStorageBlocks       = 0;
+            const ProgramExecutable &fragmentExecutable = fragmentShaderProgram->getExecutable();
+            if (!mState.mExecutable->linkValidateOutputVariables(
+                    context->getCaps(), context->getExtensions(), context->getClientVersion(),
+                    combinedImageUniforms, combinedShaderStorageBlocks,
+                    fragmentExecutable.getOutputVariables(),
+                    fragmentExecutable.getLinkedShaderVersion(ShaderType::Fragment),
+                    ProgramAliasedBindings(), ProgramAliasedBindings()))
+            {
+                return angle::Result::Continue;
+            }
+        }
         mergedVaryings = GetMergedVaryingsFromLinkingVariables(linkingVariables);
         // If separable program objects are in use, the set of attributes captured is taken
         // from the program object active on the last vertex processing stage.
@@ -524,7 +543,11 @@ angle::Result ProgramPipeline::link(const Context *context)
         ANGLE_TRY(getImplementation()->link(context, mergedVaryings, varyingPacking));
     }
 
+    mState.mExecutable->mActiveSamplerRefCounts.fill(0);
+    updateExecutable();
+
     mState.mIsLinked = true;
+    onStateChange(angle::SubjectMessage::SubjectChanged);
 
     return angle::Result::Continue;
 }
@@ -641,16 +664,16 @@ void ProgramPipeline::onSubjectStateChange(angle::SubjectIndex index, angle::Sub
     switch (message)
     {
         case angle::SubjectMessage::ProgramTextureOrImageBindingChanged:
-            mState.mIsLinked = false;
             mState.mExecutable->mActiveSamplerRefCounts.fill(0);
             mState.updateExecutableTextures();
             break;
+
         case angle::SubjectMessage::ProgramRelinked:
             mState.mIsLinked = false;
-            updateExecutable();
+            onStateChange(angle::SubjectMessage::ProgramRelinked);
             break;
         case angle::SubjectMessage::SamplerUniformsUpdated:
-            getExecutable().resetCachedValidateSamplersResult();
+            mState.mExecutable->resetCachedValidateSamplersResult();
             break;
         default:
             UNREACHABLE();

src/libANGLE/ProgramPipeline.h
@@ -38,12 +38,7 @@ class ProgramPipelineState final : angle::NonCopyable
 
     const std::string &getLabel() const;
 
-    const ProgramExecutable &getProgramExecutable() const
-    {
-        ASSERT(mExecutable);
-        return *mExecutable;
-    }
-    ProgramExecutable &getProgramExecutable()
+    ProgramExecutable &getExecutable() const
     {
         ASSERT(mExecutable);
         return *mExecutable;
@@ -91,7 +86,8 @@ class ProgramPipelineState final : angle::NonCopyable
 
 class ProgramPipeline final : public RefCountObject<ProgramPipelineID>,
                               public LabeledObject,
-                              public angle::ObserverInterface
+                              public angle::ObserverInterface,
+                              public angle::Subject
 {
   public:
     ProgramPipeline(rx::GLImplFactory *factory, ProgramPipelineID handle);
@@ -105,8 +101,7 @@ class ProgramPipeline final : public RefCountObject<ProgramPipelineID>,
     const ProgramPipelineState &getState() const { return mState; }
     ProgramPipelineState &getState() { return mState; }
 
-    const ProgramExecutable &getExecutable() const { return mState.getProgramExecutable(); }
-    ProgramExecutable &getExecutable() { return mState.getProgramExecutable(); }
+    ProgramExecutable &getExecutable() const { return mState.getExecutable(); }
 
     rx::ProgramPipelineImpl *getImplementation() const;
 
@@ -122,7 +117,9 @@ class ProgramPipeline final : public RefCountObject<ProgramPipelineID>,
         return program;
     }
 
-    void useProgramStages(const Context *context, GLbitfield stages, Program *shaderProgram);
+    angle::Result useProgramStages(const Context *context,
+                                   GLbitfield stages,
+                                   Program *shaderProgram);
 
     Program *getShaderProgram(ShaderType shaderType) const { return mState.mPrograms[shaderType]; }
 
@@ -131,6 +128,7 @@ class ProgramPipeline final : public RefCountObject<ProgramPipelineID>,
     bool linkVaryings(InfoLog &infoLog) const;
     void validate(const gl::Context *context);
     GLboolean isValid() const { return mState.isValid(); }
+    bool isLinked() const { return mState.mIsLinked; }
 
     // ObserverInterface implementation.
     void onSubjectStateChange(angle::SubjectIndex index, angle::SubjectMessage message) override;

src/libANGLE/validationES.cpp
@@ -461,19 +461,19 @@ bool ValidateFragmentShaderColorBufferMaskMatch(const Context *context)
     const Framebuffer *framebuffer = context->getState().getDrawFramebuffer();
 
     auto drawBufferMask     = framebuffer->getDrawBufferMask().to_ulong();
-    auto fragmentOutputMask = program->getActiveOutputVariables().to_ulong();
+    auto fragmentOutputMask = program->getExecutable().getActiveOutputVariablesMask().to_ulong();
 
     return drawBufferMask == (drawBufferMask & fragmentOutputMask);
 }
 
 bool ValidateFragmentShaderColorBufferTypeMatch(const Context *context)
 {
-    const Program *program         = context->getActiveLinkedProgram();
-    const Framebuffer *framebuffer = context->getState().getDrawFramebuffer();
+    const ProgramExecutable *executable = context->getState().getProgramExecutable();
+    const Framebuffer *framebuffer      = context->getState().getDrawFramebuffer();
 
-    return ValidateComponentTypeMasks(program->getDrawBufferTypeMask().to_ulong(),
+    return ValidateComponentTypeMasks(executable->getFragmentOutputsTypeMask().to_ulong(),
                                       framebuffer->getDrawBufferTypeMask().to_ulong(),
-                                      program->getActiveOutputVariables().to_ulong(),
+                                      executable->getActiveOutputVariablesMask().to_ulong(),
                                       framebuffer->getDrawBufferMask().to_ulong());
 }
 
@@ -4036,7 +4036,7 @@ const char *ValidateDrawStates(const Context *context)
                 return errorMsg;
             }
 
-            programIsYUVOutput = program->isYUVOutput();
+            programIsYUVOutput = executable->isYUVOutput();
         }
         else if (programPipeline)
         {
@@ -4052,10 +4052,7 @@ const char *ValidateDrawStates(const Context *context)
                 return errorMsg;
             }
 
-            bool goodResult = programPipeline->link(context) == angle::Result::Continue;
-
-            ASSERT(executable);
-            if (!goodResult)
+            if (!programPipeline->isLinked())
             {
                 return kProgramPipelineLinkFailed;
             }

src/libANGLE/Context.cpp
@@ -281,7 +281,8 @@ enum SubjectIndexes : angle::SubjectIndex
     kSamplerMaxSubjectIndex  = kSampler0SubjectIndex + IMPLEMENTATION_MAX_ACTIVE_TEXTURES,
     kVertexArraySubjectIndex = kSamplerMaxSubjectIndex,
     kReadFramebufferSubjectIndex,
-    kDrawFramebufferSubjectIndex
+    kDrawFramebufferSubjectIndex,
+    kProgramPipelineSubjectIndex,
 };
 
 bool IsClearBufferEnabled(const FramebufferState &fbState, GLenum buffer, GLint drawbuffer)
@@ -316,7 +317,6 @@ bool GetSaveAndRestoreState(const egl::AttributeMap &attribs)
 {
     return (attribs.get(EGL_EXTERNAL_CONTEXT_SAVE_STATE_ANGLE, EGL_FALSE) == EGL_TRUE);
 }
-
 }  // anonymous namespace
 
 #if defined(ANGLE_PLATFORM_APPLE)
@@ -400,6 +400,7 @@ Context::Context(egl::Display *display,
       mVertexArrayObserverBinding(this, kVertexArraySubjectIndex),
       mDrawFramebufferObserverBinding(this, kDrawFramebufferSubjectIndex),
       mReadFramebufferObserverBinding(this, kReadFramebufferSubjectIndex),
+      mProgramPipelineObserverBinding(this, kProgramPipelineSubjectIndex),
       mThreadPool(nullptr),
       mFrameCapture(new angle::FrameCapture),
       mRefCount(0),
@@ -1293,8 +1294,7 @@ void Context::useProgramStages(ProgramPipelineID pipeline,
                                                                        pipeline);
 
     ASSERT(programPipeline);
-    ANGLE_CONTEXT_TRY(mState.useProgramStages(this, programPipeline, stages, shaderProgram));
-    mStateCache.onProgramExecutableChange(this);
+    ANGLE_CONTEXT_TRY(programPipeline->useProgramStages(this, stages, shaderProgram));
 }
 
 void Context::bindTransformFeedback(GLenum target, TransformFeedbackID transformFeedbackHandle)
@@ -1310,7 +1310,12 @@ void Context::bindProgramPipeline(ProgramPipelineID pipelineHandle)
     ProgramPipeline *pipeline = mState.mProgramPipelineManager->checkProgramPipelineAllocation(
         mImplementation.get(), pipelineHandle);
     ANGLE_CONTEXT_TRY(mState.setProgramPipelineBinding(this, pipeline));
+    if (pipeline && pipeline->isLinked())
+    {
+        ANGLE_CONTEXT_TRY(mState.onProgramPipelineExecutableChange(this));
+    }
     mStateCache.onProgramExecutableChange(this);
+    mProgramPipelineObserverBinding.bind(pipeline);
 }
 
 void Context::beginQuery(QueryType target, QueryID query)
@@ -8917,6 +8922,22 @@ void Context::onSubjectStateChange(angle::SubjectIndex index, angle::SubjectMess
                     break;
                 case angle::SubjectMessage::SurfaceChanged:
                     mState.setDrawFramebufferBindingDirty();
+                    break;
+                default:
+                    UNREACHABLE();
+                    break;
+            }
+            break;
+
+        case kProgramPipelineSubjectIndex:
+            switch (message)
+            {
+                case angle::SubjectMessage::SubjectChanged:
+                    ANGLE_CONTEXT_TRY(mState.onProgramPipelineExecutableChange(this));
+                    mStateCache.onProgramExecutableChange(this);
+                    break;
+                case angle::SubjectMessage::ProgramRelinked:
+                    ANGLE_CONTEXT_TRY(mState.mProgramPipeline->link(this));
                     break;
                 default:
                     UNREACHABLE();

src/libANGLE/Context.h
@@ -775,6 +775,7 @@ class Context final : public egl::LabeledObject, angle::NonCopyable, public angl
     angle::ObserverBinding mVertexArrayObserverBinding;
     angle::ObserverBinding mDrawFramebufferObserverBinding;
     angle::ObserverBinding mReadFramebufferObserverBinding;
+    angle::ObserverBinding mProgramPipelineObserverBinding;
     std::vector<angle::ObserverBinding> mUniformBufferObserverBindings;
     std::vector<angle::ObserverBinding> mAtomicCounterBufferObserverBindings;
     std::vector<angle::ObserverBinding> mShaderStorageBufferObserverBindings;

src/libANGLE/Program.h
@@ -252,7 +252,6 @@ class ProgramState final : angle::NonCopyable
     {
         return mExecutable->getProgramInputs();
     }
-    DrawBufferMask getActiveOutputVariables() const { return mActiveOutputVariables; }
     const std::vector<sh::ShaderVariable> &getOutputVariables() const
     {
         return mExecutable->getOutputVariables();
@@ -352,12 +351,6 @@ class ProgramState final : angle::NonCopyable
 
     uint32_t getLocationsUsedForXfbExtension() const { return mLocationsUsedForXfbExtension; }
 
-    const std::vector<GLenum> &getOutputVariableTypes() const { return mOutputVariableTypes; }
-
-    ComponentTypeMask getDrawBufferTypeMask() const { return mDrawBufferTypeMask; }
-
-    bool isYUVOutput() const { return mYUVOutput; }
-
     bool hasBinaryRetrieveableHint() const { return mBinaryRetrieveableHint; }
 
     bool isSeparable() const { return mSeparable; }
@@ -393,16 +386,6 @@ class ProgramState final : angle::NonCopyable
     std::vector<VariableLocation> mUniformLocations;
     std::vector<BufferVariable> mBufferVariables;
     RangeUI mAtomicCounterUniformRange;
-
-    DrawBufferMask mActiveOutputVariables;
-
-    // Fragment output variable base types: FLOAT, INT, or UINT.  Ordered by location.
-    std::vector<GLenum> mOutputVariableTypes;
-    ComponentTypeMask mDrawBufferTypeMask;
-
-    // GL_EXT_YUV_target. YUV output shaders can only have one ouput and can only write to YUV
-    // framebuffers.
-    bool mYUVOutput;
 
     bool mBinaryRetrieveableHint;
     bool mSeparable;
@@ -479,7 +462,7 @@ class Program final : public LabeledObject, public angle::Subject
     void bindFragmentOutputIndex(GLuint index, const char *name);
 
     // KHR_parallel_shader_compile
-    // Try to link the program asynchrously. As a result, background threads may be launched to
+    // Try to link the program asynchronously. As a result, background threads may be launched to
     // execute the linking tasks concurrently.
     angle::Result link(const Context *context);
 
@@ -525,12 +508,6 @@ class Program final : public LabeledObject, public angle::Subject
 
     GLint getFragDataLocation(const std::string &name) const;
     size_t getOutputResourceCount() const;
-    const std::vector<GLenum> &getOutputVariableTypes() const;
-    DrawBufferMask getActiveOutputVariables() const
-    {
-        ASSERT(!mLinkingState);
-        return mState.mActiveOutputVariables;
-    }
 
     // EXT_blend_func_extended
     GLint getFragDataIndex(const std::string &name) const;
@@ -768,14 +745,6 @@ class Program final : public LabeledObject, public angle::Subject
 
     bool usesMultiview() const { return mState.usesMultiview(); }
 
-    ComponentTypeMask getDrawBufferTypeMask() const;
-
-    bool isYUVOutput() const
-    {
-        ASSERT(!mLinkingState);
-        return mState.isYUVOutput();
-    }
-
     const std::vector<GLsizei> &getTransformFeedbackStrides() const;
 
     // Program dirty bits.
@@ -843,14 +812,6 @@ class Program final : public LabeledObject, public angle::Subject
     bool linkAtomicCounterBuffers();
 
     void updateLinkedShaderStages();
-
-    int getOutputLocationForLink(const sh::ShaderVariable &outputVariable) const;
-    bool isOutputSecondaryForLink(const sh::ShaderVariable &outputVariable) const;
-    bool linkOutputVariables(const Caps &caps,
-                             const Extensions &extensions,
-                             const Version &version,
-                             GLuint combinedImageUniformsCount,
-                             GLuint combinedShaderStorageBlocksCount);
 
     void setUniformValuesFromBindingQualifiers();
     bool shouldIgnoreUniform(UniformLocation location) const;

beaudie/vesc

commit d8d637bb8adefa1ff3e8fda59d8ba618a842cc56

Optimize glUseProgramStages call
The commit 3a9f18f135fe82 caused a link to occur everytime
glUseProgramStages is called. This is redundant since the program
pipeline can be linked just before usage, thus allowing for multiple
stages to be bound before linking the executable.

Also early return if the same program is being bound to the same
stage of the pipeline.

Bug: angleproject:6566
Test: ProgramPipelineTest31.RepeatedCallToUseProgramStagesWithSamePrograms*Vulkan
Change-Id: Idbc2fcb4875bbd040e9ec847eb2a8f96f287173c

src/libANGLE/ProgramPipeline.cpp
@@ -88,18 +88,12 @@ void ProgramPipelineState::useProgramStage(const Context *context,
 
 void ProgramPipelineState::useProgramStages(
     const Context *context,
-    GLbitfield stages,
+    const gl::ShaderBitSet &shaderTypes,
     Program *shaderProgram,
     std::vector<angle::ObserverBinding> *programObserverBindings)
 {
-    for (size_t singleShaderBit : angle::BitSet16<16>(static_cast<uint16_t>(stages)))
+    for (ShaderType shaderType : shaderTypes)
     {
-        // Cast back to a bit after the iterator returns an index.
-        ShaderType shaderType = GetShaderTypeFromBitfield(angle::Bit<size_t>(singleShaderBit));
-        if (shaderType == ShaderType::InvalidEnum)
-        {
-            break;
-        }
         useProgramStage(context, shaderType, shaderProgram,
                         &programObserverBindings->at(static_cast<size_t>(shaderType)));
     }
@@ -208,12 +202,46 @@ angle::Result ProgramPipeline::useProgramStages(const Context *context,
                                                 GLbitfield stages,
                                                 Program *shaderProgram)
 {
-    mState.useProgramStages(context, stages, shaderProgram, &mProgramObserverBindings);
+    bool needToUpdatePipelineState = false;
+    gl::ShaderBitSet shaderTypes;
+    if (stages != GL_ALL_SHADER_BITS)
+    {
+        for (size_t singleShaderBit : angle::BitSet16<16>(static_cast<uint16_t>(stages)))
+        {
+            // Cast back to a bit after the iterator returns an index.
+            ShaderType shaderType = GetShaderTypeFromBitfield(angle::Bit<size_t>(singleShaderBit));
+            ASSERT(shaderType != ShaderType::InvalidEnum);
+            shaderTypes.set(shaderType);
+        }
+    }
+    else
+    {
+        shaderTypes.set();
+    }
+    ASSERT(shaderTypes.any());
+
+    for (ShaderType shaderType : shaderTypes)
+    {
+        if (mState.getShaderProgram(shaderType) != shaderProgram ||
+            (shaderProgram && shaderProgram->hasAnyDirtyBit()))
+        {
+            needToUpdatePipelineState = true;
+            break;
+        }
+    }
+
+    if (!needToUpdatePipelineState)
+    {
+        return angle::Result::Continue;
+    }
+
+    mState.useProgramStages(context, shaderTypes, shaderProgram, &mProgramObserverBindings);
     updateLinkedShaderStages();
 
     mState.mIsLinked = false;
+    onStateChange(angle::SubjectMessage::SubjectChanged);
 
-    return link(context);
+    return angle::Result::Continue;
 }
 
 void ProgramPipeline::updateLinkedShaderStages()

src/libANGLE/ProgramPipeline.h
@@ -46,7 +46,7 @@ class ProgramPipelineState final : angle::NonCopyable
 
     void activeShaderProgram(Program *shaderProgram);
     void useProgramStages(const Context *context,
-                          GLbitfield stages,
+                          const gl::ShaderBitSet &shaderTypes,
                           Program *shaderProgram,
                           std::vector<angle::ObserverBinding> *programObserverBindings);
 

src/libANGLE/validationES.cpp
@@ -4234,7 +4234,8 @@ const char *ValidateDrawStates(const Context *context)
                 return errorMsg;
             }
 
-            if (!programPipeline->isLinked())
+            bool linkResult = programPipeline->link(context) == angle::Result::Continue;
+            if (!linkResult)
             {
                 return kProgramPipelineLinkFailed;
             }

src/libANGLE/validationES3.cpp
@@ -3073,6 +3073,15 @@ bool ValidateBeginTransformFeedback(const Context *context,
         }
     }
 
+    // It is possible that a bound program pipeline hasn't been linked yet, do that before
+    // validating xfb state.
+    ProgramPipeline *programPipieline = context->getState().getProgramPipeline();
+    if (programPipieline && programPipieline->link(context) != angle::Result::Continue)
+    {
+        context->validationError(entryPoint, GL_INVALID_OPERATION, kProgramPipelineLinkFailed);
+        return false;
+    }
+
     const ProgramExecutable *programExecutable = context->getState().getProgramExecutable();
     if (!programExecutable)
     {

src/tests/gl_tests/ProgramPipelineTest.cpp
@@ -439,6 +439,48 @@ void main()
     glDeleteProgram(fragProg);
 }
 
+// Test glUseProgramStages with repeated calls to glUseProgramStages with the same programs.
+TEST_P(ProgramPipelineTest31, RepeatedCallToUseProgramStagesWithSamePrograms)
+{
+    ANGLE_SKIP_TEST_IF(!IsVulkan());
+
+    // Create two separable program objects from a
+    // single source string respectively (vertSrc and fragSrc)
+    const GLchar *vertString = essl31_shaders::vs::Simple();
+    const GLchar *fragString = R"(#version 310 es
+precision highp float;
+uniform float redColorIn;
+uniform float greenColorIn;
+out vec4 my_FragColor;
+void main()
+{
+    my_FragColor = vec4(redColorIn, greenColorIn, 0.0, 1.0);
+})";
+
+    bindProgramPipeline(vertString, fragString);
+
+    // Set the output color to red
+    GLint location = glGetUniformLocation(mFragProg, "redColorIn");
+    glActiveShaderProgram(mPipeline, mFragProg);
+    glUniform1f(location, 1.0);
+    location = glGetUniformLocation(mFragProg, "greenColorIn");
+    glActiveShaderProgram(mPipeline, mFragProg);
+    glUniform1f(location, 0.0);
+
+    // These following calls to glUseProgramStages should not cause a re-link.
+    glUseProgramStages(mPipeline, GL_VERTEX_SHADER_BIT, mVertProg);
+    EXPECT_GL_NO_ERROR();
+    glUseProgramStages(mPipeline, GL_FRAGMENT_SHADER_BIT, mFragProg);
+    EXPECT_GL_NO_ERROR();
+
+    drawQuadWithPPO("a_position", 0.5f, 1.0f);
+    ASSERT_GL_NO_ERROR();
+    EXPECT_PIXEL_COLOR_EQ(0, 0, GLColor::red);
+
+    glDeleteProgram(mVertProg);
+    glDeleteProgram(mFragProg);
+}
+
 // Test glUseProgramStages
 TEST_P(ProgramPipelineTest31, UseCreateShaderProgramv)
 {