3634d8e7cae010ed8392b381cdee569d07172933
Roman Lavrov <romanl@google.com>
2022-08-29 11:53:01 -0400

    Replace std::this_thread::get_id() use with a unique thread id.
    std::this_thread::get_id() can be reused. It's pthread_self()
    under the hood on Linux and Android and gets reused, for example
    when one thread terminates and another one starts it is likely to
    return the same value.
    
    Bug: angleproject:7602
    Change-Id: I83d818bc17ead5cce8bce7f7d88fc1c7c0fa860c

--- src/libANGLE/renderer/d3d/d3d11/DebugAnnotator11.cpp
+++ src/libANGLE/renderer/d3d/d3d11/DebugAnnotator11.cpp
@@ -67,7 +67,8 @@ bool DebugAnnotator11::getStatus()
 
 bool DebugAnnotator11::loggingEnabledForThisThread() const
 {
-    return mUserDefinedAnnotation != nullptr && std::this_thread::get_id() == mAnnotationThread;
+    return mUserDefinedAnnotation != nullptr &&
+           angle::GetCurrentThreadUniqueId() == mAnnotationThread;
 }
 
 void DebugAnnotator11::initialize(ID3D11DeviceContext *context)
@@ -81,7 +82,7 @@ void DebugAnnotator11::initialize(ID3D11DeviceContext *context)
     if (IsWindows10OrGreater())
 #endif
     {
-        mAnnotationThread = std::this_thread::get_id();
+        mAnnotationThread = angle::GetCurrentThreadUniqueId();
         mUserDefinedAnnotation.Attach(
             d3d11::DynamicCastComObject<ID3DUserDefinedAnnotation>(context));
     }
