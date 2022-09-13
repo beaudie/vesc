8c13e3468934d34ae5e17699c6c1affff02f13e7
Mohan Maiya <m.maiya@samsung.com>
2022-09-13 15:11:54 -0700

    Vulkan: Fix a cornercase bug when dynamically loading AHB APIs.
    The commit 122a1cc583f0 enabled dynamic loading of AHB APIs but did
    not handle the cornercase where some Android system services load
    "libnativewindow.so" with the RTLD_LOCAL flag. In such cases AHB
    APIs would not be resolvable through dlsym lookup. Explicitly dlopen
    "libnativewindow.so" in such cases.
    
    Bug: angleproject:7656
    Change-Id: Ie74140ad816b756e1afc325d370f26bcb09428ca

--- src/libANGLE/renderer/vulkan/android/AHBFunctions.cpp
+++ src/libANGLE/renderer/vulkan/android/AHBFunctions.cpp
@@ -20,16 +20,40 @@ void AssignFn(void *handle, const char *name, T &fn)
     fn = reinterpret_cast<T>(dlsym(handle, name));
 }
 
+constexpr char kNativeWindowLibraryName[] = "libnativewindow.so";
+constexpr char kAhbAcquireFunctionName[]  = "AHardwareBuffer_acquire";
+constexpr char kAhbDescribeFunctionName[] = "AHardwareBuffer_describe";
+constexpr char kAhbReleaseFunctionName[]  = "AHardwareBuffer_release";
+
 }  // namespace
 
 AHBFunctions::AHBFunctions()
 {
     void *handle = dlopen(nullptr, RTLD_NOW);
-    AssignFn(handle, "AHardwareBuffer_acquire", mAcquireFn);
-    AssignFn(handle, "AHardwareBuffer_describe", mDescribeFn);
-    AssignFn(handle, "AHardwareBuffer_release", mReleaseFn);
+    AssignFn(handle, kAhbAcquireFunctionName, mAcquireFn);
+    AssignFn(handle, kAhbDescribeFunctionName, mDescribeFn);
+    AssignFn(handle, kAhbReleaseFunctionName, mReleaseFn);
+
+    // Some services load "libnativewindow.so" with RTLD_LOCAL flag resulting in AHB function
+    // symbols being unresolvable through dlsym. Account for such cases and explicitly dlopen the
+    // library.
+    if (!valid())
+    {
+        mLibNativeWindowHandle = nullptr;
+        mLibNativeWindowHandle = dlopen(kNativeWindowLibraryName, RTLD_NOW);
+        ASSERT(mLibNativeWindowHandle);
+        AssignFn(mLibNativeWindowHandle, kAhbAcquireFunctionName, mAcquireFn);
+        AssignFn(mLibNativeWindowHandle, kAhbDescribeFunctionName, mDescribeFn);
+        AssignFn(mLibNativeWindowHandle, kAhbReleaseFunctionName, mReleaseFn);
+    }
 }
 
-AHBFunctions::~AHBFunctions() = default;
+AHBFunctions::~AHBFunctions()
+{
+    if (mLibNativeWindowHandle)
+    {
+        dlclose(mLibNativeWindowHandle);
+    }
+}
 
 }  // namespace rx
