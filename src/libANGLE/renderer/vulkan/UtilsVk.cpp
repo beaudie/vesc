9b422fe1191faff15fe726a0f2f6db164e882ff1
Amirali Abdolrashidi <abdolrashidi@google.com>
2022-09-15 17:31:35 -0700

    Vulkan: Use serial to check which FBO has open RP
    In preparation for the scenario where an imageless framebuffer is
    shared, checking whether the started render pass belongs to a certain
    framebuffer is tweaked to use a serial-based method instead of checking
    the framebuffer handle.
    
    * Added the last render pass serial to ContextVk, which increments
      every time a render pass begins. The serial is also stored in the
      FramebufferVk object invoking it.
    
      * Added the type RenderPassSerial for this purpose.
    
      * Serials are generated through a serial factory in ContextVk.
    
    * Updated hasStartedRenderPassWithSerial() to match the serials
      instead of the handles.
    
    * Removed the getFramebuffer() calls from FramebufferVk and UtilsVk
      that are now unused.
    
    Bug: angleproject:7553
    Change-Id: Id60dcbf7973558d35e55ff4af4c71e50c6853bba

--- src/libANGLE/renderer/vulkan/UtilsVk.cpp
+++ src/libANGLE/renderer/vulkan/UtilsVk.cpp
@@ -2050,9 +2050,10 @@ angle::Result UtilsVk::startRenderPass(ContextVk *contextVk,
                                               vk::ImageLayout::ColorAttachment,
                                               vk::ImageLayout::ColorAttachment);
 
-    ANGLE_TRY(contextVk->beginNewRenderPass(
-        framebuffer, renderArea, renderPassDesc, renderPassAttachmentOps,
-        vk::PackedAttachmentCount(1), vk::kAttachmentIndexInvalid, clearValues, commandBufferOut));
+    ANGLE_TRY(contextVk->beginNewRenderPass(framebuffer, renderArea, renderPassDesc,
+                                            renderPassAttachmentOps, vk::PackedAttachmentCount(1),
+                                            vk::kAttachmentIndexInvalid, clearValues,
+                                            commandBufferOut, nullptr));
 
     contextVk->addGarbage(&framebuffer);
 
@@ -2072,7 +2073,7 @@ angle::Result UtilsVk::clearFramebuffer(ContextVk *contextVk,
     // Start a new render pass if not already started
     ANGLE_TRY(framebuffer->getFramebuffer(contextVk, &currentFramebuffer, nullptr,
                                           SwapchainResolveMode::Disabled));
-    if (contextVk->hasStartedRenderPassWithFramebuffer(currentFramebuffer))
+    if (contextVk->hasStartedRenderPassWithSerial(framebuffer->getLastRenderPassSerial()))
     {
         vk::RenderPassCommandBufferHelper *renderPassCommands =
             &contextVk->getStartedRenderPassCommands();
