9ab33351f683c16826ae87e2f8ec6e072f60e164
Chris Dalton <chris@rive.app>
2022-10-02 17:08:35 -0600

    Implement an allow list for PLS
    In order to guarantee no data is lost while using the
    EXT_shader_pixel_local_storage extension, we need to restrict
    applications to a small subset of commands while pixel local storage is
    active. This CL implements the allow list for GL entrypoints using
    wildcard matching inside the code generator.
    
    Bug: angleproject:7279
    Change-Id: I5dd48bd93c10e8775f32be32a4fcf17855eb2f0e

---
+++ src/libANGLE/entry_points_utils.cpp
@@ -0,0 +1,24 @@
+//
+// Copyright 2022 The ANGLE Project Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+// entry_point_utils:
+//   These helpers are used in GL/GLES entry point routines.
+
+#include "libANGLE/entry_points_utils.h"
+
+#include "libANGLE/ErrorStrings.h"
+
+namespace gl
+{
+bool ValidatePixelLocalStorageInactive(const Context *context, angle::EntryPoint entryPoint)
+{
+    if (context->getState().getPixelLocalStorageActive())
+    {
+        context->validationError(entryPoint, GL_INVALID_OPERATION, err::kPLSActive);
+        return false;
+    }
+    return true;
+}
+}  // namespace gl
