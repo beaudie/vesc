8ee5671de6fca817431b3bbefda8854720774c14
Cody Northrop <cnorthrop@google.com>
2022-10-02 22:05:12 -0600

    Tests: Add Survivor.io trace
    Test: angle_perftests --gtest_filter="*survivor_io*"
    Bug: angleproject:7728
    Bug: angleproject:7733
    Change-Id: Ic337af781a209c3302bee748c5aed47f367993a8

--- src/tests/perf_tests/TracePerfTest.cpp
+++ src/tests/perf_tests/TracePerfTest.cpp
@@ -1312,6 +1312,14 @@ TracePerfTest::TracePerfTest(std::unique_ptr<const TracePerfParams> params)
         addExtensionPrerequisite("GL_EXT_texture_cube_map_array");
     }
 
+    if (traceNameIs("survivor_io"))
+    {
+        if (IsWindows() && IsNVIDIA() && mParams->isVulkan() && !mParams->isSwiftshader())
+        {
+            skipTest("http://anglebug.com/7733 Renders incorrectly on Nvidia Windows");
+        }
+    }
+
     // glDebugMessageControlKHR and glDebugMessageCallbackKHR crash on ARM GLES1.
     if (IsARM() && mParams->traceInfo.contextClientMajorVersion == 1)
     {
