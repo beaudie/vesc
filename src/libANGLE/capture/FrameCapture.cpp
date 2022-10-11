0b1c72099eb07da4a546e7bdb4eddb2142b83e10
Jamie Madill <jmadill@chromium.org>
2022-10-10 20:59:58 -0400

    Entry Points: Move enum helper to registry_xml.
    This will make it accessible to other generators.
    
    Bug: angleproject:7752
    Change-Id: I91bc9a4d6c919266ea329f66d271bf881d99d17a

--- src/libANGLE/capture/FrameCapture.cpp
+++ src/libANGLE/capture/FrameCapture.cpp
@@ -7678,7 +7678,7 @@ void FrameCaptureShared::replay(gl::Context *context)
     {
         INFO() << "frame index: " << mFrameIndex << " " << call.name();
 
-        if (call.entryPoint == EntryPoint::GLInvalid)
+        if (call.entryPoint == EntryPoint::Invalid)
         {
             if (call.customFunctionName == "UpdateClientArrayPointer")
             {
