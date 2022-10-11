60fe88a17c5054094a4ce778211fc804029a49e1
Shahbaz Youssefi <syoussefi@chromium.org>
2022-10-11 01:00:40 -0400

    Vulkan: Use VK_EXT_pipeline_protected_access
    This extension allows pipelines to be restricted to protected or
    unprotected command buffers only.
    
    Bug: angleproject:7714
    Change-Id: Id07be4c2ffd7ca19b8b4a2a869828bc11e89e467

--- src/libANGLE/renderer/vulkan/ContextVk.cpp
+++ src/libANGLE/renderer/vulkan/ContextVk.cpp
@@ -7897,6 +7897,10 @@ vk::ComputePipelineFlags ContextVk::getComputePipelineFlags() const
     {
         pipelineFlags.set(vk::ComputePipelineFlag::Robust);
     }
+    if (shouldRestrictPipelineToProtectedAccess())
+    {
+        pipelineFlags.set(vk::ComputePipelineFlag::Protected);
+    }
 
     return pipelineFlags;
 }
