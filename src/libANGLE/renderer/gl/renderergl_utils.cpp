515d4c7f8b6ec74ca3ab568457092331727a6bd9
Alexey Knyazev <lexa.knyazev@gmail.com>
2022-10-20 00:00:00 +0000

    GL: Adjust APPLE_clip_distance exposure
    Partial revert of
    I458cad29c10b9d9193c5233e24bac53361ba104e
    
    APPLE_clip_distance cannot be implemented on top of
    EXT_clip_cull_distance because the former is defined both
    for ES 2.0 and ES 3.0 while the latter is defined only for ES 3.0.
    
    Although some ES drivers allow gl_ClipDistance built-in in ESSL 1.00
    shaders, this behavior is not specified so ANGLE should not allow it.
    
    Added independent ESSL 3.0 gl_ClipDistance and gl_MaxClipDistances
    definitions.
    
    Bug: angleproject:4452
    Change-Id: Ib582ce0ac7ccb65f0200ef1d17eaab0c83b228cf

--- src/libANGLE/renderer/gl/renderergl_utils.cpp
+++ src/libANGLE/renderer/gl/renderergl_utils.cpp
@@ -1879,11 +1879,10 @@ void GenerateCaps(const FunctionsGL *functions,
                                     functions->isAtLeastGLES(gl::Version(3, 0)) ||
                                     functions->hasGLESExtension("GL_EXT_shadow_samplers");
 
-    // GL_APPLE_clip_distance
-    extensions->clipDistanceAPPLE = !features.disableClipCullDistance.enabled &&
-                                    (functions->isAtLeastGL(gl::Version(3, 0)) ||
-                                     functions->hasGLESExtension("GL_APPLE_clip_distance") ||
-                                     functions->hasGLESExtension("GL_EXT_clip_cull_distance"));
+    // GL_APPLE_clip_distance cannot be implemented on top of GL_EXT_clip_cull_distance,
+    // so require either native support or desktop GL.
+    extensions->clipDistanceAPPLE = functions->isAtLeastGL(gl::Version(3, 0)) ||
+                                    functions->hasGLESExtension("GL_APPLE_clip_distance");
     if (extensions->clipDistanceAPPLE)
     {
         caps->maxClipDistances = QuerySingleGLInt(functions, GL_MAX_CLIP_DISTANCES_EXT);
@@ -1897,6 +1896,7 @@ void GenerateCaps(const FunctionsGL *functions,
                                        functions->hasGLESExtension("GL_EXT_clip_cull_distance"));
     if (extensions->clipCullDistanceEXT)
     {
+        caps->maxClipDistances = QuerySingleGLInt(functions, GL_MAX_CLIP_DISTANCES_EXT);
         caps->maxCullDistances = QuerySingleGLInt(functions, GL_MAX_CULL_DISTANCES_EXT);
         caps->maxCombinedClipAndCullDistances =
             QuerySingleGLInt(functions, GL_MAX_COMBINED_CLIP_AND_CULL_DISTANCES_EXT);
