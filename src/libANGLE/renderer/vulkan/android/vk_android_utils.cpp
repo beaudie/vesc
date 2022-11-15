85622c7dc9cf28b54ce4b40f3eb52f0bde748b31
Amirali Abdolrashidi <abdolrashidi@google.com>
2022-11-14 16:07:19 -0800

    Vulkan: Add memory allocation/deallocation counter
    * Added a counter to the renderer object to keep track of the
    memory allocations and deallocations per allocation type.
    
      * The counters are updated with the functions onMemoryAlloc() and
      onMemoryDealloc(), which can be added next to the allocation or
      deallocation point.
    
      * Currently used for buffer memory and image memory.
    
    * Removed some of the redundant arguments from vk_helper and
      suballocation functions.
    
    Bug: b/242641395
    Change-Id: I58b38f619df7bef0ba5fa3373a8db5aed0ef142c

--- src/libANGLE/renderer/vulkan/android/vk_android_utils.cpp
+++ src/libANGLE/renderer/vulkan/android/vk_android_utils.cpp
@@ -22,6 +22,10 @@ namespace rx
 {
 namespace vk
 {
+
+// Used for memory allocation tracking.
+enum class MemoryAllocationType;
+
 angle::Result GetClientBufferMemoryRequirements(ContextVk *contextVk,
                                                 const AHardwareBuffer *hardwareBuffer,
                                                 VkMemoryRequirements &memRequirements)
@@ -64,6 +68,9 @@ angle::Result InitAndroidExternalMemory(ContextVk *contextVk,
                                         DeviceMemory *deviceMemoryOut)
 {
 #if defined(ANGLE_PLATFORM_ANDROID)
+    // ONLY FOR TESTING
+    ASSERT(false);
+
     const AHBFunctions &functions =
         GetImplAs<DisplayVkAndroid>(contextVk->getRenderer()->getDisplay())->getAHBFunctions();
     ASSERT(functions.valid());
@@ -80,9 +87,10 @@ angle::Result InitAndroidExternalMemory(ContextVk *contextVk,
     importHardwareBufferInfo.sType  = VK_STRUCTURE_TYPE_IMPORT_ANDROID_HARDWARE_BUFFER_INFO_ANDROID;
     importHardwareBufferInfo.buffer = hardwareBuffer;
 
-    ANGLE_TRY(AllocateBufferMemoryWithRequirements(
-        contextVk, memoryProperties, externalMemoryRequirements, &importHardwareBufferInfo, buffer,
-        memoryPropertyFlagsOut, deviceMemoryOut));
+    ANGLE_TRY(AllocateBufferMemoryWithRequirements(contextVk, MemoryAllocationType::BufferExternal,
+                                                   memoryProperties, externalMemoryRequirements,
+                                                   &importHardwareBufferInfo, buffer,
+                                                   memoryPropertyFlagsOut, deviceMemoryOut));
 
     functions.acquire(hardwareBuffer);
 
