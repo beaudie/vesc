3e4cadf0400247617302cee2aa055b665d882afe
Shahbaz Youssefi <syoussefi@chromium.org>
2022-11-16 21:47:04 -0500

    Vulkan: Move PipelineCacheAccess to namespace vk
    In preparation for a change that uses it in that namespace.
    
    Bug: angleproject:7369
    Change-Id: Icc75b8839d702fd3e6d3d00c1d8f81619cdd89cc

--- src/libANGLE/renderer/vulkan/RendererVk.cpp
+++ src/libANGLE/renderer/vulkan/RendererVk.cpp
@@ -4163,7 +4163,7 @@ angle::Result RendererVk::initPipelineCache(DisplayVk *display,
     return angle::Result::Continue;
 }
 
-angle::Result RendererVk::getPipelineCache(PipelineCacheAccess *pipelineCacheOut)
+angle::Result RendererVk::getPipelineCache(vk::PipelineCacheAccess *pipelineCacheOut)
 {
     DisplayVk *displayVk = vk::GetImpl(mDisplay);
 
@@ -4194,7 +4194,7 @@ angle::Result RendererVk::getPipelineCache(PipelineCacheAccess *pipelineCacheOut
 
 angle::Result RendererVk::mergeIntoPipelineCache(const vk::PipelineCache &pipelineCache)
 {
-    PipelineCacheAccess globalCache;
+    vk::PipelineCacheAccess globalCache;
     ANGLE_TRY(getPipelineCache(&globalCache));
 
     globalCache.merge(this, pipelineCache);
