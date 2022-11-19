05bbf7bb85959a08d14201483526ee1c0fcb393c
Jason Macnak <natsu@google.com>
2022-11-18 16:01:33 -0800

    Map AHARDWAREBUFFER_FORMAT_R8_UNORM to GL_R8
    Round 2. Round 1 (https://crrev.com/c/4034130) caused some Android
    build failures due to redefining AHARDWAREBUFFER_FORMAT_R8_UNORM.
    IIUC, Android platform SDK build will always have and use only the
    latest available <android/hardware_buffer.h> even though the target
    level (__ANDROID_API__) for supported devices might be set to an
    earlier level. With this, Round 2 tries handle the two different
    levels at play.
    
    Bug: b/259551816
    Test: cvd start --gpu_mode=guest_swiftshader
    Test: atest librenderengine_test
    Test: Presubmit on aosp/2310871 to check Android build
    Change-Id: I924027967c2de2f8a81fde64e0911b26e3f5927c

---
+++ src/common/android_platform.h
@@ -0,0 +1,47 @@
+//
+// Copyright 2022 The ANGLE Project Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+// android_platform.h: Android operating system specific includes and defines.
+//
+
+#ifndef COMMON_ANDROID_PLATFORM_H_
+#define COMMON_ANDROID_PLATFORM_H_
+
+#include "common/platform.h"
+
+#if defined(ANGLE_PLATFORM_ANDROID)
+
+#    if defined(__has_include) && __has_include(<android/ndk-version.h>)
+#        include <android/ndk-version.h>
+#    endif
+
+// Note: there are two kind of levels involved when building:
+//
+//   1. The level of the devices you are building *for*. This is
+//      ANGLE_AHARDWAREBUFFER_TARGET_SUPPORT and
+//      ANGLE_AHARDWAREBUFFER_TARGET_LOCK_PLANES_SUPPORT below.
+//
+//   2. The level of the repository you are building *from*. This is
+//      ANGLE_AHARDWAREBUFFER_BUILD_LEVEL below.
+
+#    if defined(__ANDROID_NDK__) && defined(__NDK_MAJOR__)
+#        define ANGLE_AHARDWAREBUFFER_BUILD_LEVEL __NDK_MAJOR__
+#    else
+// This is an Android platform build which will have the latest available.
+#        define ANGLE_AHARDWAREBUFFER_BUILD_LEVEL 10000
+#    endif
+
+#    if __ANDROID_API__ >= 26
+#        define ANGLE_AHARDWAREBUFFER_TARGET_SUPPORT
+#        include <android/hardware_buffer.h>
+#    endif  // __ANDROID_API__ >= 26
+
+#    if __ANDROID_API__ >= 29
+#        define ANGLE_AHARDWAREBUFFER_TARGET_LOCK_PLANES_SUPPORT
+#    endif
+
+#endif  // defined(ANGLE_PLATFORM_ANDROID)
+
+#endif /* COMMON_ANDROID_PLATFORM_H_ */
