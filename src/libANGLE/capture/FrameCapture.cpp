google/angle

commit b301b82235ca686d036a8d29380da22bb2060060

Capture/Replay: emit context setup for pre-MEC contexts only
When a context is made current for the first time it is added to
the shared context set. If this happens after the mid execution
capture has started, then the call to the context setup function
is created, but the according function implementations was not
emitted leading to compilation failure of the so created trace.

Since a context that was never current doesn't actually have any
setup that needs to be done when starting a MEC replay. Hence,
there is no need to emit the calls fot SetupReplayContextXX.

So track which context where actually in the shared context set
when MEC started, and only emit the according calls to the setup
functions.

Bug: angleproject:7805
Change-Id: I83f8714733ead5c0d71560013c360b5671f0822a
Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/4008199
Reviewed-by: Cody Northrop <cnorthrop@google.com>
Reviewed-by: Jamie Madill <jmadill@chromium.org>
Commit-Queue: Gert Wollny <gert.wollny@collabora.com>

src/libANGLE/capture/FrameCapture.cpp
@@ -7772,6 +7772,9 @@ void FrameCaptureShared::runMidExecutionCapture(gl::Context *mainContext)
         FrameCapture *frameCapture = shareContext->getFrameCapture();
         ASSERT(frameCapture->getSetupCalls().empty());
 
+        // Track that this context was created before MEC started
+        mCapturedContextSetups.insert(shareContext->id().value);
+
         if (shareContext->id() == mainContext->id())
         {
             CaptureMidExecutionSetup(shareContext, &frameCapture->getSetupCalls(),
@@ -8464,9 +8467,15 @@ void FrameCaptureShared::writeMainContextCppReplay(const gl::Context *context,
                 // MEC.
                 if (usesMidExecutionCapture())
                 {
-                    out << "    "
-                        << FmtSetupFunction(kNoPartId, shareContext->id(), FuncUsage::Call)
-                        << ";\n";
+                    // Only call SetupReplayContext for contexts that were current before MEC
+                    // started
+                    if (mCapturedContextSetups.find(context->id().value) !=
+                        mCapturedContextSetups.end())
+                    {
+                        out << "    "
+                            << FmtSetupFunction(kNoPartId, shareContext->id(), FuncUsage::Call)
+                            << ";\n";
+                    }
                 }
             }
 

src/libANGLE/capture/FrameCapture.h
@@ -776,6 +776,9 @@ class FrameCaptureShared final : angle::NonCopyable
     gl::ContextID mWindowSurfaceContextID;
 
     std::vector<CallCapture> mShareGroupSetupCalls;
+    // Track which Contexts were created and made current at least once before MEC,
+    // requiring setup for replay
+    std::unordered_set<GLuint> mCapturedContextSetups;
 };
 
 template <typename CaptureFuncT, typename... ArgsT>

beaudie/vesc

commit 3bf8881f259487676d11bacf469a8d0fdca66964

Capture/Replay: Fix collecting the initialized contexts
The main context initialization function will be emitted as
"Shared" and not based on the context number, so only add the
secondary contexts to the list of contexts that need additional
setup.

Fixes: b301b82235ca686d036a8d29380da22bb2060060
   Capture/Replay: emit context setup for pre-MEC contexts only

Bug: angleproject:7805
Change-Id: I4ac770a303ac93a448c9b46bf8c4cd58900ddd54

src/libANGLE/capture/FrameCapture.cpp
@@ -7772,9 +7772,6 @@ void FrameCaptureShared::runMidExecutionCapture(gl::Context *mainContext)
         FrameCapture *frameCapture = shareContext->getFrameCapture();
         ASSERT(frameCapture->getSetupCalls().empty());
 
-        // Track that this context was created before MEC started
-        mCapturedContextSetups.insert(shareContext->id().value);
-
         if (shareContext->id() == mainContext->id())
         {
             CaptureMidExecutionSetup(shareContext, &frameCapture->getSetupCalls(),
@@ -7786,6 +7783,9 @@ void FrameCaptureShared::runMidExecutionCapture(gl::Context *mainContext)
         }
         else
         {
+            // Track that this context was created as shared context before MEC started
+            mCapturedContextSetups.insert(shareContext->id().value);
+
             const gl::State &shareContextState = shareContext->getState();
             gl::State auxContextReplayState(
                 nullptr, nullptr, nullptr, nullptr, nullptr, shareContextState.getClientType(),