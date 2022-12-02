d891243b93d2aa3937ff9edbf45eef2f573e6695
Yuxin Hu <yuxinhu@google.com>
2022-12-01 16:51:20 -0800

    Make ANGLE program version only dependent on data that matters - part1
    This change introduces a new variable to track the
    version of ANGLE code that affects how shader
    program serialize/deserialize data. The new variable
    is Context::ANGLE_PROGRAM_VERSION.
    
    Add a presubmit check that if any Program* files
    changed, the ANGLE_PROGRAM_VERSION should also be
    incremented.
    
    Bug: angleproject:4981
    Change-Id: I2fb609416738d459d3289190c232c2d797ba58e3

---
+++ src/libANGLE/ANGLEShaderProgramVersion.cpp
@@ -0,0 +1,16 @@
+//
+// Copyright 2022 The ANGLE Project Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+// ANGLEShaderProgramVersion.cpp:
+//      Defines the values for the static const ANGLE_PROGRAM_VERSION.
+//      Whenever we touch files that affect program serialize/deserialize,
+//      we should increment the ANGLE_PROGRAM_VERSION number here as well.
+//
+#include "Context.h"
+
+namespace gl
+{
+const int Context::ANGLE_PROGRAM_VERSION = 1;
+}
