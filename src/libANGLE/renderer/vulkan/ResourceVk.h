bfebd178b60119cd7f823f3d149be2a3c895c689
Charlie Lao <cclao@google.com>
2022-12-08 17:02:12 -0800

    Vulkan: Better mUse tracking for DynamicallyGrowingPool<Pool>
    This is used only from DynamicallyGrowingPool<Pool>::onEntryFreed to set
    the its tracking QueueSerial. There is better way to do this now with
    per context serial. We can simply merge the QueryHelper's mUse into the
    pool instead of setting pool's use to the current queueSerial. The
    benefit of doing that is to possibly allow pool gets reuse/freed
    earlier (i.e., more accurate tracking). This CL switches it to more
    accurate tracking and removes mCurrentSerial from Context.
    
    This CL also removes unused DynamicSemaphorePool class.
    
    Bug: b/262054987
    Change-Id: Iac3e2495cc0e3623ba63e9da7f32ad6e9c223467

--- src/libANGLE/renderer/vulkan/ResourceVk.h
+++ src/libANGLE/renderer/vulkan/ResourceVk.h
@@ -111,6 +111,23 @@ class ResourceUse final
                    commandBufferQueueSerial.getSerial();
     }
 
+    // Merge other's serials into this object.
+    ANGLE_INLINE void merge(const ResourceUse &other)
+    {
+        if (mSerials.size() < other.mSerials.size())
+        {
+            mSerials.resize(other.mSerials.size());
+        }
+
+        for (SerialIndex i = 0; i < other.mSerials.size(); ++i)
+        {
+            if (mSerials[i] < other.mSerials[i])
+            {
+                mSerials[i] = other.mSerials[i];
+            }
+        }
+    }
+
   private:
     // The most recent time of use in a VkQueue.
     Serials mSerials;
@@ -157,6 +174,8 @@ class Resource : angle::NonCopyable
     }
 
     const ResourceUse &getResourceUse() const { return mUse; }
+
+    void mergeResourceUse(const ResourceUse &use) { mUse.merge(use); }
 
   protected:
     Resource();
