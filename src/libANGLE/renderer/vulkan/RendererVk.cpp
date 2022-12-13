3c541544031b22132e25869132a8c5c13e3e97a5
Shahbaz Youssefi <syoussefi@chromium.org>
2022-12-13 19:59:14 +0000

    Revert "Suppress VUID-vkCmdDraw-None-06887"
    This reverts commit 982755e0c0a864d56f0c7655a358e0c31d296ccf.
    
    Reason for revert: Spec was fixed to allow this, and VVL fix
    landed upstream
    
    Original change's description:
    > Suppress VUID-vkCmdDraw-None-06887
    >
    > * Suppressed the following VVLs:
    >   * VUID-vkCmdDraw-None-06887
    >   * VUID-vkCmdDrawIndexed-None-06887
    >
    > Bug: angleproject:7861
    > Change-Id: I15cdf934bd87b5d5219e69eebc9d339d4c180660
    > Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/4082149
    > Reviewed-by: Shahbaz Youssefi <syoussefi@chromium.org>
    > Auto-Submit: Amirali Abdolrashidi <abdolrashidi@google.com>
    > Commit-Queue: Shahbaz Youssefi <syoussefi@chromium.org>
    
    Bug: angleproject:7861
    Change-Id: Ide90680386858a73e6ba9534b20d47459a1bf1c7

--- src/libANGLE/renderer/vulkan/RendererVk.cpp
+++ src/libANGLE/renderer/vulkan/RendererVk.cpp
@@ -214,11 +214,14 @@ constexpr const char *kSkippedMessages[] = {
     "VUID-vkDestroySemaphore-semaphore-01137",
     // http://anglebug.com/7843
     "VUID-VkGraphicsPipelineCreateInfo-Vertex-07722",
+<<<<<<< PATCH SET (e67dbc Revert "Suppress VUID-vkCmdDraw-None-06887")
+=======
     // http://anglebug.com/7861
     "VUID-vkCmdDraw-None-06887",
     "VUID-vkCmdDrawIndexed-None-06887",
     // http://anglebug.com/7865
     "VUID-VkDescriptorImageInfo-imageView-06711",
+>>>>>>> BASE      (816505 GCC: Make some operators of QueueSerial non constexpr)
 };
 
 // Validation messages that should be ignored only when VK_EXT_primitive_topology_list_restart is
