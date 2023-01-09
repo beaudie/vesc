google/angle

commit b432c84c2b8cb6d20873e2b33b6684bee76db90d

Capture/Replay: Fix collecting the initialized contexts
The main context initialization function will be emitted as
"Shared" and not based on the context number, so only add the
secondary contexts to the list of contexts that need additional
setup.

Fixes: b301b82235ca686d036a8d29380da22bb2060060
   Capture/Replay: emit context setup for pre-MEC contexts only

Bug: angleproject:7805
Change-Id: I4ac770a303ac93a448c9b46bf8c4cd58900ddd54
Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/4068124
Commit-Queue: Gert Wollny <gert.wollny@collabora.com>
Reviewed-by: Cody Northrop <cnorthrop@google.com>
Reviewed-by: Jamie Madill <jmadill@chromium.org>

src/libANGLE/capture/FrameCapture.cpp
@@ -7772,9 +7772,6 @@ void FrameCaptureShared::runMidExecutionCapture(gl::Context *mainContext)
         FrameCapture *frameCapture = shareContext->getFrameCapture();
         ASSERT(frameCapture->getSetupCalls().empty());
 
-        // Track that this context was created before MEC started
-        mCapturedContextSetups.insert(shareContext->id().value);
-
         if (shareContext->id() == mainContext->id())
         {
             CaptureMidExecutionSetup(shareContext, &frameCapture->getSetupCalls(),
@@ -7786,6 +7783,9 @@ void FrameCaptureShared::runMidExecutionCapture(gl::Context *mainContext)
         }
         else
         {
+            // Track that this context was created as shared context before MEC started
+            mActiveSecondaryContexts.insert(shareContext->id().value);
+
             const gl::State &shareContextState = shareContext->getState();
             gl::State auxContextReplayState(
                 nullptr, nullptr, nullptr, nullptr, nullptr, shareContextState.getClientType(),
@@ -8467,10 +8467,10 @@ void FrameCaptureShared::writeMainContextCppReplay(const gl::Context *context,
                 // MEC.
                 if (usesMidExecutionCapture())
                 {
-                    // Only call SetupReplayContext for contexts that were current before MEC
-                    // started
-                    if (mCapturedContextSetups.find(context->id().value) !=
-                        mCapturedContextSetups.end())
+                    // Only call SetupReplayContext for secondary contexts that were current before
+                    // MEC started
+                    if (mActiveSecondaryContexts.find(context->id().value) !=
+                        mActiveSecondaryContexts.end())
                     {
                         out << "    "
                             << FmtSetupFunction(kNoPartId, shareContext->id(), FuncUsage::Call)

src/libANGLE/capture/FrameCapture.h
@@ -778,7 +778,7 @@ class FrameCaptureShared final : angle::NonCopyable
     std::vector<CallCapture> mShareGroupSetupCalls;
     // Track which Contexts were created and made current at least once before MEC,
     // requiring setup for replay
-    std::unordered_set<GLuint> mCapturedContextSetups;
+    std::unordered_set<GLuint> mActiveSecondaryContexts;
 };
 
 template <typename CaptureFuncT, typename... ArgsT>

beaudie/vesc

commit 31d96f19a5cc0902efb0b35566c03b3929003ce2

Capture/Replay: Test the correct context ID when emitting setup
Fixes: b432c84c2b8cb6d20873e2b33b6684bee76db90d (origin/chromium/5455)
    Capture/Replay: Fix collecting the initialized contexts

Bug: angleproject:7805
Bug: angleproject:7938
Change-Id: Ia321179bef8fd33c7968177ffcb4da78103a99fc

src/libANGLE/capture/FrameCapture.cpp
@@ -8604,7 +8604,7 @@ void FrameCaptureShared::writeMainContextCppReplay(const gl::Context *context,
                 {
                     // Only call SetupReplayContext for secondary contexts that were current before
                     // MEC started
-                    if (mActiveSecondaryContexts.find(context->id().value) !=
+                    if (mActiveSecondaryContexts.find(shareContext->id().value) !=
                         mActiveSecondaryContexts.end())
                     {
                         // TODO(http://anglebug.com/5878): Support capture/replay of