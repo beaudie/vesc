bfb330ed66a72a0cd592b7994e50fd11894495a4
Igor Nazarov <i.nazarov@samsung.com>
2023-01-17 17:42:46 +0200

    Revert "Reland: Frontend: separate lock in swap prep"
    This partially reverts commit:
        https://chromium.googlesource.com/angle/angle/+/926b43e751b7ec2445b32f2b3bbe78ca075fe7c9
    
    Except:
    - src\libANGLE\validationEGL.h
    - src\tests\egl_tests\EGLContextSharingTest.cpp
    - `syncPipelineCacheVk()` kept in the `WindowSurfaceVk::swapImpl()`.
    
    After the recent CL:
        https://chromium.googlesource.com/angle/angle/+/23ad4fa2be35aed303cbb8c2d632a04714354ef0
        Don't hold global surface lock during AcquireNextImage
    
    The `ANGLE_SCOPED_GLOBAL_SURFACE_LOCK() always used together with the
    `ANGLE_SCOPED_GLOBAL_LOCK()` making `g_SurfaceMutex` redundant.
    
    Calling `prepareSwap()` without `GlobalMutex` will not solve the problem
    entirely. `vkAcquireNextImageKHR()` may be also called from `glClear()`.
    In shared context scenarios it may block other thread, causing serious
    performance issue.
    
    Known application: WeSing (Karaoke).
    
    Problem will be solved by future commits:
    - "Implement "Global Mutex Unlock" functionality."
    - "Vulkan: Unlock mutexes during vkAcquireNextImageKHR() call."
    
    Bug: angleproject:6851
    Change-Id: I201ea5e4f96a05350dd7f822afab0b51fafeb104

--- src/libGLESv2/global_state.h
+++ src/libGLESv2/global_state.h
@@ -108,7 +108,6 @@ extern thread_local Thread *gCurrentThread;
 #endif
 
 angle::GlobalMutex &GetGlobalMutex();
-angle::GlobalMutex &GetGlobalSurfaceMutex();
 gl::Context *GetGlobalLastContext();
 void SetGlobalLastContext(gl::Context *context);
 Thread *GetCurrentThread();
@@ -127,14 +126,8 @@ class [[nodiscard]] ScopedSyncCurrentContextFromThread
 
 }  // namespace egl
 
-#define ANGLE_GLOBAL_SURFACE_LOCK_VAR_NAME globalSurfaceMutexLock
-#define ANGLE_SCOPED_GLOBAL_SURFACE_LOCK()                                  \
-    std::lock_guard<angle::GlobalMutex> ANGLE_GLOBAL_SURFACE_LOCK_VAR_NAME( \
-        egl::GetGlobalSurfaceMutex())
-
-#define ANGLE_GLOBAL_LOCK_VAR_NAME globalMutexLock
 #define ANGLE_SCOPED_GLOBAL_LOCK() \
-    std::lock_guard<angle::GlobalMutex> ANGLE_GLOBAL_LOCK_VAR_NAME(egl::GetGlobalMutex())
+    std::lock_guard<angle::GlobalMutex> globalMutexLock(egl::GetGlobalMutex())
 
 namespace gl
 {
