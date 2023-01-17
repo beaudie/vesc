6d6b4c38b442bb6df32044bb0663cbb6c4c1b913
Igor Nazarov <i.nazarov@samsung.com>
2023-01-17 17:44:06 +0200

    Implement "Global Mutex Unlock" functionality.
    This CL adds functionality to unlock `GlobalMutex` from backend side.
    Mutex unlock is required in some situations, when Vulkan API may return
    to EGL/GL API. Old solution for this problem used
    `std::recursive_mutex`, but it is not working in some cases.
    
    Additionally, mutex unlock may be used as optimization (unlock on Image
    Acquire, or Fence wait).
    
    CL adds new build option:
        angle_enable_global_mutex_unlock = false
    
    Bug: angleproject:6851
    Bug: angleproject:4354
    Change-Id: I57355fab89fce6572308309aeffe012251ab4979

--- src/libANGLE/Context.cpp
+++ src/libANGLE/Context.cpp
@@ -30,6 +30,7 @@
 #include "libANGLE/Display.h"
 #include "libANGLE/Fence.h"
 #include "libANGLE/FramebufferAttachment.h"
+#include "libANGLE/GlobalMutex.h"
 #include "libANGLE/MemoryObject.h"
 #include "libANGLE/PixelLocalStorage.h"
 #include "libANGLE/Program.h"
@@ -9551,6 +9552,13 @@ void Context::ensureSharedMutexActive(uint32_t activationDelayMicro)
         return;
     }
     ASSERT(mState.mSingleContextMutex != nullptr);
+
+    // Unlock GlobalMutex for two reasons:
+    // - avoid long bloking while waiting.
+    // - avoid infinite waiting. If this Context is in the middle of temporary unlock in other
+    //   thread, it will not be able to lock GlobalMutex again and therefore unlock the
+    //   "SingleContextMutex" later.
+    egl::ScopedGlobalMutexUnlock eglUnlock(egl::GlobalMutexUnlockType::IfOwned);
 
     // First, start using "SharedContextMutex"
     mState.useSharedContextMutex();
