6f23fdb44c60db71b3ff3d6cad57c1b2ba8cfa4f
Shahbaz Youssefi <syoussefi@chromium.org>
2023-01-19 10:54:54 -0500

    Vulkan: Disallow INPUT_ATTACHMENT usage on sampled image views
    Input attachments have similar requirements to draw attachments w.r.t
    swizzling.
    
    Bug: b/265647457
    Change-Id: I7ff007b2b2ae2d8615b4b339bc621835888eebde

--- src/libANGLE/renderer/vulkan/TextureVk.cpp
+++ src/libANGLE/renderer/vulkan/TextureVk.cpp
@@ -3236,10 +3236,12 @@ angle::Result TextureVk::initImageViews(ContextVk *contextVk, uint32_t levelCoun
     // Use this as a proxy for the SRGB override & skip decode settings.
     bool createExtraSRGBViews = mRequiresMutableStorage;
 
+    const VkImageUsageFlags kDisallowedSwizzledUsage =
+        VK_IMAGE_USAGE_STORAGE_BIT | VK_IMAGE_USAGE_INPUT_ATTACHMENT_BIT;
     ANGLE_TRY(getImageViews().initReadViews(contextVk, mState.getType(), *mImage, formatSwizzle,
                                             readSwizzle, baseLevelVk, levelCount, baseLayer,
                                             getImageViewLayerCount(), createExtraSRGBViews,
-                                            getImage().getUsage() & ~VK_IMAGE_USAGE_STORAGE_BIT));
+                                            getImage().getUsage() & ~kDisallowedSwizzledUsage));
 
     updateCachedImageViewSerials();
 
