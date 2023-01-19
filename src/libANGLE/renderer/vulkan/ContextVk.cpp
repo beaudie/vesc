5874e90629407ebde6388e07cda68f1ea46b6064
Igor Nazarov <i.nazarov@samsung.com>
2023-01-19 15:37:45 +0200

    Vulkan: Implement ExternalFence for use in SyncHelperNativeFence
    `ExternalFence` allows concurrent usage in `CommandQueue` and
    `SyncHelperNativeFence` classes eliminating need of additional
    `vkQueueSubmit()` call.
    
    Waiting in `CommandQueue` on `QueueSerial` or `ResourceUse` will ensure
    corresponding state of the native FD (because `CommandQueue` will wait
    on the same FD instead of some other fence).
    
    After this change there will be only single `vkQueueSubmit()` call
    from the `SyncHelperNativeFence::initializeWithFd()` method.
    
    This CL and the follow-up is sufficient to fix the bugs below.
    
    Bug: angleproject:8115
    Bug: angleproject:8117
    Test: angle_end2end_tests --gtest_filter=EGLSyncTest.AndroidNativeFence_ExternalFenceWaitVVLBug*
    Change-Id: Ic562ecc71a95203454a1dc438589a13bcf3bff7f

--- src/libANGLE/renderer/vulkan/ContextVk.cpp
+++ src/libANGLE/renderer/vulkan/ContextVk.cpp
@@ -3447,7 +3447,7 @@ void ContextVk::addOverlayUsedBuffersCount(vk::CommandBufferHelperCommon *comman
 }
 
 angle::Result ContextVk::submitCommands(const vk::Semaphore *signalSemaphore,
-                                        const vk::Fence *externalFence,
+                                        const vk::SharedExternalFence *externalFence,
                                         Submit submission)
 {
     if (vk::CommandBufferHelperCommon::kEnableCommandStreamDiagnostics)
@@ -7032,7 +7032,7 @@ angle::Result ContextVk::updateActiveImages(CommandBufferHelperT *commandBufferH
 }
 
 angle::Result ContextVk::flushImpl(const vk::Semaphore *signalSemaphore,
-                                   const vk::Fence *externalFence,
+                                   const vk::SharedExternalFence *externalFence,
                                    RenderPassClosureReason renderPassClosureReason)
 {
     bool someCommandsNeedFlush =
