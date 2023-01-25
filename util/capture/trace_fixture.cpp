google/angle

commit f7b5d5d1e0b6b4a9263c56a83e6cacf87904fdee

Capture/Replay: Remove inline variable declarations.
This makes parsing easier for the "simplified C" interpreter.
We introduce a resource ID buffer as a way to manage a list
of resource IDs to replace the inline resource lists.

Turns on the Among Us trace in the interpreter tests.

Bug: angleproject:7775
Change-Id: I1bb9c0e9b087965a18691bc99b2e9947610b9eaf
Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/4128719
Reviewed-by: Cody Northrop <cnorthrop@google.com>
Commit-Queue: Jamie Madill <jmadill@chromium.org>

util/capture/trace_fixture.cpp
@@ -168,6 +168,7 @@ void UpdateCurrentProgram(GLuint program)
 uint8_t *gBinaryData;
 uint8_t *gReadBuffer;
 uint8_t *gClientArrays[kMaxClientArrays];
+GLuint *gResourceIDBuffer;
 SyncResourceMap gSyncMap;
 ContextMap gContextMap;
 GLuint gShareContextId;
@@ -224,6 +225,7 @@ GLuint *AllocateZeroedUints(size_t count)
 void InitializeReplay3(const char *binaryDataFileName,
                        size_t maxClientArraySize,
                        size_t readBufferSize,
+                       size_t resourceIDBufferSize,
                        GLuint contextId,
                        uint32_t maxBuffer,
                        uint32_t maxContext,
@@ -249,7 +251,8 @@ void InitializeReplay3(const char *binaryDataFileName,
                       maxShaderProgram, maxSurface, maxTexture, maxTransformFeedback,
                       maxVertexArray);
 
-    gSyncMap2 = AllocateZeroedValues<GLsync>(maxSync);
+    gSyncMap2         = AllocateZeroedValues<GLsync>(maxSync);
+    gResourceIDBuffer = AllocateZeroedUints(resourceIDBufferSize);
 }
 
 void InitializeReplay2(const char *binaryDataFileName,
@@ -340,6 +343,7 @@ void FinishReplay()
         delete[] clientArray;
     }
     delete[] gReadBuffer;
+    delete[] gResourceIDBuffer;
 
     delete[] gBufferMap;
     delete[] gContextMap2;
@@ -385,6 +389,11 @@ void UpdateClientBufferDataWithOffset(GLuint bufferID,
 {
     uintptr_t dest = reinterpret_cast<uintptr_t>(gMappedBufferData[gBufferMap[bufferID]]) + offset;
     memcpy(reinterpret_cast<void *>(dest), source, size);
+}
+
+void UpdateResourceIDBuffer(int resourceIndex, GLuint id)
+{
+    gResourceIDBuffer[resourceIndex] = id;
 }
 
 void UpdateBufferID(GLuint id, GLsizei readBufferOffset)

util/capture/trace_fixture.h
@@ -84,6 +84,7 @@ void UpdateCurrentProgram(GLuint program);
 extern uint8_t *gBinaryData;
 extern uint8_t *gReadBuffer;
 extern uint8_t *gClientArrays[];
+extern GLuint *gResourceIDBuffer;
 
 extern GLuint *gBufferMap;
 extern GLuint *gFenceNVMap;
@@ -108,6 +109,7 @@ extern GLsync *gSyncMap2;
 void InitializeReplay3(const char *binaryDataFileName,
                        size_t maxClientArraySize,
                        size_t readBufferSize,
+                       size_t resourceIDBufferSize,
                        GLuint contextId,
                        uint32_t maxBuffer,
                        uint32_t maxContext,
@@ -171,6 +173,7 @@ void UpdateClientBufferDataWithOffset(GLuint bufferID,
                                       const void *source,
                                       GLsizei size,
                                       GLsizei offset);
+void UpdateResourceIDBuffer(int resourceIndex, GLuint id);
 void UpdateBufferID(GLuint id, GLsizei readBufferOffset);
 void UpdateFenceNVID(GLuint id, GLsizei readBufferOffset);
 void UpdateFramebufferID(GLuint id, GLsizei readBufferOffset);

util/capture/trace_interpreter_autogen.cpp
@@ -6245,6 +6245,12 @@ CallCapture ParseCallCapture(const Token &nameToken,
         ParamBuffer params = ParseParameters<decltype(UpdateRenderbufferID)>(paramTokens, shaders);
         return CallCapture("UpdateRenderbufferID", std::move(params));
     }
+    if (strcmp(nameToken, "UpdateResourceIDBuffer") == 0)
+    {
+        ParamBuffer params =
+            ParseParameters<decltype(UpdateResourceIDBuffer)>(paramTokens, shaders);
+        return CallCapture("UpdateResourceIDBuffer", std::move(params));
+    }
     if (strcmp(nameToken, "UpdateSamplerID") == 0)
     {
         ParamBuffer params = ParseParameters<decltype(UpdateSamplerID)>(paramTokens, shaders);
@@ -6357,14 +6363,14 @@ void DispatchCallCapture(Fn *fn, const Captures &cap)
           Arg<Fn, 15>(cap), Arg<Fn, 16>(cap), Arg<Fn, 17>(cap), Arg<Fn, 18>(cap), Arg<Fn, 19>(cap));
 }
 
-template <typename Fn, EnableIfNArgs<Fn, 21> = 0>
+template <typename Fn, EnableIfNArgs<Fn, 22> = 0>
 void DispatchCallCapture(Fn *fn, const Captures &cap)
 {
     (*fn)(Arg<Fn, 0>(cap), Arg<Fn, 1>(cap), Arg<Fn, 2>(cap), Arg<Fn, 3>(cap), Arg<Fn, 4>(cap),
           Arg<Fn, 5>(cap), Arg<Fn, 6>(cap), Arg<Fn, 7>(cap), Arg<Fn, 8>(cap), Arg<Fn, 9>(cap),
           Arg<Fn, 10>(cap), Arg<Fn, 11>(cap), Arg<Fn, 12>(cap), Arg<Fn, 13>(cap), Arg<Fn, 14>(cap),
           Arg<Fn, 15>(cap), Arg<Fn, 16>(cap), Arg<Fn, 17>(cap), Arg<Fn, 18>(cap), Arg<Fn, 19>(cap),
-          Arg<Fn, 20>(cap));
+          Arg<Fn, 20>(cap), Arg<Fn, 21>(cap));
 }
 
 void ReplayCustomFunctionCall(const CallCapture &call, const TraceFunctionMap &customFunctions)
@@ -6535,6 +6541,11 @@ void ReplayCustomFunctionCall(const CallCapture &call, const TraceFunctionMap &c
     if (call.customFunctionName == "UpdateRenderbufferID")
     {
         DispatchCallCapture(UpdateRenderbufferID, captures);
+        return;
+    }
+    if (call.customFunctionName == "UpdateResourceIDBuffer")
+    {
+        DispatchCallCapture(UpdateResourceIDBuffer, captures);
         return;
     }
     if (call.customFunctionName == "UpdateSamplerID")

infra/specs/test_suites.pyl
@@ -759,7 +759,7 @@
         'args': [
           '--log=debug',
           'interpret',
-          'trex_200',
+          'among_us:trex_200',
           '-L=2',
           '--show-test-stdout',
         ],

scripts/generate_entry_points.py
@@ -2494,8 +2494,7 @@ def format_replay_params(api, command_name, param_text_list, packed_enums, resou
             if cmd_no_suffix in packed_enums and param_name in packed_enums[cmd_no_suffix]:
                 packed_type = remove_id_suffix(packed_enums[cmd_no_suffix][param_name])
                 if packed_type == 'Sync':
-                    param_access = 'g%sMap[reinterpret_cast<uintptr_t>(%s)]' % (packed_type,
-                                                                                param_access)
+                    param_access = 'gSyncMap2[captures[%d].value.GLuintVal]' % i
                 elif packed_type in resource_id_types:
                     param_access = 'g%sMap[%s]' % (packed_type, param_access)
                 elif packed_type == 'UniformLocation':

src/common/frame_capture_utils.cpp
@@ -122,9 +122,9 @@ void ParamBuffer::addReturnValue(ParamCapture &&returnValue)
 
 const char *ParamBuffer::getNextParamName()
 {
-    static const char *kParamNames[] = {"p0",  "p1",  "p2",  "p3",  "p4",  "p5",  "p6",
-                                        "p7",  "p8",  "p9",  "p10", "p11", "p12", "p13",
-                                        "p14", "p15", "p16", "p17", "p18", "p19", "p20"};
+    static const char *kParamNames[] = {"p0",  "p1",  "p2",  "p3",  "p4",  "p5",  "p6",  "p7",
+                                        "p8",  "p9",  "p10", "p11", "p12", "p13", "p14", "p15",
+                                        "p16", "p17", "p18", "p19", "p20", "p21"};
     ASSERT(mParamCaptures.size() < ArraySize(kParamNames));
     return kParamNames[mParamCaptures.size()];
 }

src/libANGLE/capture/FrameCapture.h
@@ -752,6 +752,7 @@ class FrameCaptureShared final : angle::NonCopyable
     SurfaceParamsMap mDrawSurfaceParams;
     gl::AttribArray<size_t> mClientArraySizes;
     size_t mReadBufferSize;
+    size_t mResourceIDBufferSize;
     HasResourceTypeMap mHasResourceType;
     ResourceIDToSetupCallsMap mResourceIDToSetupCalls;
     BufferDataMap mBufferDataMap;

beaudie/vesc

commit 305d31dda38f272fbc547e86e7006f90cac1e3d7

Capture/Replay: Fix InitializeReplay3 and add InitializeReplay4
Fixes: f7b5d5d1e0b6b4a9263c56a83e6cacf87904fdee
  Capture/Replay: Remove inline variable declarations

In addition this needs a fix in the restricted trace
"csr2_drag_racing", i.e. "InitializeReplay3" must be replaced
with "InitializeReplay4"

Bug: angleproject:7973
Change-Id: Ic081c3da17e43eecea2bf7fe63ed5baef2fa98c5

util/capture/trace_fixture.cpp
@@ -222,10 +222,41 @@ GLuint *AllocateZeroedUints(size_t count)
     return AllocateZeroedValues<GLuint>(count);
 }
 
-void InitializeReplay3(const char *binaryDataFileName,
+void InitializeReplay4(const char *binaryDataFileName,
                        size_t maxClientArraySize,
                        size_t readBufferSize,
                        size_t resourceIDBufferSize,
+                       GLuint contextId,
+                       uint32_t maxBuffer,
+                       uint32_t maxContext,
+                       uint32_t maxFenceNV,
+                       uint32_t maxFramebuffer,
+                       uint32_t maxImage,
+                       uint32_t maxMemoryObject,
+                       uint32_t maxProgramPipeline,
+                       uint32_t maxQuery,
+                       uint32_t maxRenderbuffer,
+                       uint32_t maxSampler,
+                       uint32_t maxSemaphore,
+                       uint32_t maxShaderProgram,
+                       uint32_t maxSurface,
+                       uint32_t maxSync,
+                       uint32_t maxTexture,
+                       uint32_t maxTransformFeedback,
+                       uint32_t maxVertexArray)
+{
+    InitializeReplay3(binaryDataFileName, maxClientArraySize, readBufferSize, contextId, maxBuffer,
+                      maxContext, maxFenceNV, maxFramebuffer, maxImage, maxMemoryObject,
+                      maxProgramPipeline, maxQuery, maxRenderbuffer, maxSampler, maxSemaphore,
+                      maxShaderProgram, maxSurface, maxSync, maxTexture, maxTransformFeedback,
+                      maxVertexArray);
+
+    gResourceIDBuffer = AllocateZeroedUints(resourceIDBufferSize);
+}
+
+void InitializeReplay3(const char *binaryDataFileName,
+                       size_t maxClientArraySize,
+                       size_t readBufferSize,
                        GLuint contextId,
                        uint32_t maxBuffer,
                        uint32_t maxContext,
@@ -251,8 +282,7 @@ void InitializeReplay3(const char *binaryDataFileName,
                       maxShaderProgram, maxSurface, maxTexture, maxTransformFeedback,
                       maxVertexArray);
 
-    gSyncMap2         = AllocateZeroedValues<GLsync>(maxSync);
-    gResourceIDBuffer = AllocateZeroedUints(resourceIDBufferSize);
+    gSyncMap2 = AllocateZeroedValues<GLsync>(maxSync);
 }
 
 void InitializeReplay2(const char *binaryDataFileName,

util/capture/trace_fixture.h
@@ -107,10 +107,32 @@ extern EGLSurface *gSurfaceMap2;
 extern EGLContext *gContextMap2;
 extern GLsync *gSyncMap2;
 
-void InitializeReplay3(const char *binaryDataFileName,
+void InitializeReplay4(const char *binaryDataFileName,
                        size_t maxClientArraySize,
                        size_t readBufferSize,
                        size_t resourceIDBufferSize,
+                       GLuint contextId,
+                       uint32_t maxBuffer,
+                       uint32_t maxContext,
+                       uint32_t maxFenceNV,
+                       uint32_t maxFramebuffer,
+                       uint32_t maxImage,
+                       uint32_t maxMemoryObject,
+                       uint32_t maxProgramPipeline,
+                       uint32_t maxQuery,
+                       uint32_t maxRenderbuffer,
+                       uint32_t maxSampler,
+                       uint32_t maxSemaphore,
+                       uint32_t maxShaderProgram,
+                       uint32_t maxSurface,
+                       uint32_t maxSync,
+                       uint32_t maxTexture,
+                       uint32_t maxTransformFeedback,
+                       uint32_t maxVertexArray);
+
+void InitializeReplay3(const char *binaryDataFileName,
+                       size_t maxClientArraySize,
+                       size_t readBufferSize,
                        GLuint contextId,
                        uint32_t maxBuffer,
                        uint32_t maxContext,

util/capture/trace_interpreter_autogen.cpp
@@ -6146,6 +6146,11 @@ CallCapture ParseCallCapture(const Token &nameToken,
         ParamBuffer params = ParseParameters<decltype(InitializeReplay3)>(paramTokens, strings);
         return CallCapture("InitializeReplay3", std::move(params));
     }
+    if (strcmp(nameToken, "InitializeReplay4") == 0)
+    {
+        ParamBuffer params = ParseParameters<decltype(InitializeReplay4)>(paramTokens, strings);
+        return CallCapture("InitializeReplay4", std::move(params));
+    }
     if (strcmp(nameToken, "MapBufferOES") == 0)
     {
         ParamBuffer params = ParseParameters<decltype(MapBufferOES)>(paramTokens, strings);
@@ -6363,6 +6368,16 @@ void DispatchCallCapture(Fn *fn, const Captures &cap)
           Arg<Fn, 15>(cap), Arg<Fn, 16>(cap), Arg<Fn, 17>(cap), Arg<Fn, 18>(cap), Arg<Fn, 19>(cap));
 }
 
+template <typename Fn, EnableIfNArgs<Fn, 21> = 0>
+void DispatchCallCapture(Fn *fn, const Captures &cap)
+{
+    (*fn)(Arg<Fn, 0>(cap), Arg<Fn, 1>(cap), Arg<Fn, 2>(cap), Arg<Fn, 3>(cap), Arg<Fn, 4>(cap),
+          Arg<Fn, 5>(cap), Arg<Fn, 6>(cap), Arg<Fn, 7>(cap), Arg<Fn, 8>(cap), Arg<Fn, 9>(cap),
+          Arg<Fn, 10>(cap), Arg<Fn, 11>(cap), Arg<Fn, 12>(cap), Arg<Fn, 13>(cap), Arg<Fn, 14>(cap),
+          Arg<Fn, 15>(cap), Arg<Fn, 16>(cap), Arg<Fn, 17>(cap), Arg<Fn, 18>(cap), Arg<Fn, 19>(cap),
+          Arg<Fn, 20>(cap));
+}
+
 template <typename Fn, EnableIfNArgs<Fn, 22> = 0>
 void DispatchCallCapture(Fn *fn, const Captures &cap)
 {
@@ -6446,6 +6461,11 @@ void ReplayCustomFunctionCall(const CallCapture &call, const TraceFunctionMap &c
     if (call.customFunctionName == "InitializeReplay3")
     {
         DispatchCallCapture(InitializeReplay3, captures);
+        return;
+    }
+    if (call.customFunctionName == "InitializeReplay4")
+    {
+        DispatchCallCapture(InitializeReplay4, captures);
         return;
     }
     if (call.customFunctionName == "MapBufferOES")