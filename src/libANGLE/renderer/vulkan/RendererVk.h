9ee1cbf0abc47b49cce6a1f79fc4c3a0657e2446
Amirali Abdolrashidi <abdolrashidi@google.com>
2023-01-25 23:56:56 +0000

    Vulkan: Use VMA suballocation for images
    There is a maximum limit for device memory object allocation. On some
    platforms, there can be an error regarding too many object allocations
    when 4096 device memory handles have been allocated. Suballocation can
    help mitigate this issue. In this CL, some images will be allocated
    using VMA API calls, which use suballocation.
    
    * Added a new feature (useVmaForImageSuballocation).
    
      * Added VMA allocation for ImageHelper, which is used in initMemory().
    
        * Suballocation is used for VMA image allocation.
    
        * If enabled, mVmaAllocation will be initialized in the ImageHelper
          object (instead of mDeviceMemory).
    
      * It is currently used for all platforms.
    
    * Added test to make sure we can allocate at least 4096 images on
      supported platforms.
    
    * Minor change to the name of an arg in CreateBuffer() declaration.
    
    * Skipped the following test for capture on Windows:
      * FramebufferTestWithFormatFallback
        .R4G4B4A4_OutOfRangeStagedUpdateReformated
    
    Bug: b/218891184
    Change-Id: Ibf80c9c8c485b301da7d23b5ba4bcbb1a8e3194f

--- src/libANGLE/renderer/vulkan/RendererVk.h
+++ src/libANGLE/renderer/vulkan/RendererVk.h
@@ -136,6 +136,26 @@ enum class MemoryLogSeverity
     INFO,
     WARN,
 };
+
+class ImageMemorySuballocator : angle::NonCopyable
+{
+  public:
+    ImageMemorySuballocator();
+    ~ImageMemorySuballocator();
+
+    void destroy(RendererVk *renderer);
+
+    // Allocates memory for the image and binds it.
+    VkResult allocateAndBindMemory(RendererVk *renderer,
+                                   Image *image,
+                                   VkMemoryPropertyFlags requiredFlags,
+                                   VkMemoryPropertyFlags preferredFlags,
+                                   Allocation *allocationOut,
+                                   uint32_t *memoryTypeIndexOut,
+                                   VkDeviceMemory *deviceMemoryOut,
+                                   VkDeviceSize *offsetOut,
+                                   VkDeviceSize *sizeOut);
+};
 }  // namespace vk
 
 // Supports one semaphore from current surface, and one semaphore passed to
@@ -328,6 +348,7 @@ class RendererVk : angle::NonCopyable
     bool isVulkan11Device() const;
 
     const vk::Allocator &getAllocator() const { return mAllocator; }
+    vk::ImageMemorySuballocator &getImageMemorySuballocator() { return mImageMemorySuballocator; }
 
     angle::Result selectPresentQueueForSurface(DisplayVk *displayVk,
                                                VkSurfaceKHR surface,
@@ -437,6 +458,28 @@ class RendererVk : angle::NonCopyable
         {
             std::vector<vk::GarbageObject> sharedGarbage;
             CollectGarbage(&sharedGarbage, garbageIn...);
+            if (!sharedGarbage.empty())
+            {
+                collectGarbage(use, std::move(sharedGarbage));
+            }
+        }
+    }
+
+    void collectAllocationGarbage(const vk::ResourceUse &use, vk::Allocation &allocationGarbageIn)
+    {
+        if (!allocationGarbageIn.valid())
+        {
+            return;
+        }
+
+        if (hasResourceUseFinished(use))
+        {
+            allocationGarbageIn.destroy(getAllocator());
+        }
+        else
+        {
+            std::vector<vk::GarbageObject> sharedGarbage;
+            CollectGarbage(&sharedGarbage, &allocationGarbageIn);
             if (!sharedGarbage.empty())
             {
                 collectGarbage(use, std::move(sharedGarbage));
@@ -970,6 +1013,10 @@ class RendererVk : angle::NonCopyable
     mutable angle::FormatMap<VkFormatProperties> mFormatProperties;
 
     vk::Allocator mAllocator;
+
+    // Used to allocate memory for images using VMA, utilizing suballocation.
+    vk::ImageMemorySuballocator mImageMemorySuballocator;
+
     vk::MemoryProperties mMemoryProperties;
     VkDeviceSize mPreferredLargeHeapBlockSize;
 
