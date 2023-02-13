e85cf4a5fbfd5faa278e22dbc928bf06986c668e
Igor Nazarov <i.nazarov@samsung.com>
2023-02-13 21:30:38 +0200

    Vulkan: Rename "RendererVk::waitFor*ToBeSubmitted()" methods.
    Following "RendererVk" methods are a little bit confusing:
    - submitCommands()
    - hasResourceUseSubmitted()
    - hasQueueSerialSubmitted()
    - waitForResourceUseToBeSubmitted()
    - waitForQueueSerialToBeSubmitted()
    
    Because after "RendererVk::submitCommands(..., submitQueueSerial)" call
    "hasQueueSerialSubmitted(submitQueueSerial)" will always return "true".
    And it is not clear why need to call "waitForResourceUseToBeSubmitted()"
    method, if it already "Submitted". It is even more: it is technically
    illegal to call "waitFor*ToBeSubmitted()" if "has*Submitted" is "false".
    
    This refactoring suggests adding "ToDevice" to the methods names:
    - waitForResourceUseToBeSubmittedToDevice()
    - waitForQueueSerialToBeSubmittedToDevice()
    
    So that:
    - "Submitted"         - will mean to the RendererVk (and maybe Device)
    - "SubmittedToDevice" - definitely submitted to the Device.
    
    Bug: b/267348918
    Change-Id: I12323be3ddc0cbcff4667e52a37089b187b63fe8

--- src/libANGLE/renderer/vulkan/RendererVk.h
+++ src/libANGLE/renderer/vulkan/RendererVk.h
@@ -559,12 +559,14 @@ class RendererVk : angle::NonCopyable
         }
     }
 
-    angle::Result waitForResourceUseToBeSubmitted(vk::Context *context, const vk::ResourceUse &use)
+    angle::Result waitForResourceUseToBeSubmittedToDevice(vk::Context *context,
+                                                          const vk::ResourceUse &use)
     {
         // This is only needed for async submission code path. For immediate submission, it is a nop
         // since everything is submitted immediately.
         if (isAsyncCommandQueueEnabled())
         {
+            ASSERT(mCommandProcessor.hasResourceUseEnqueued(use));
             return mCommandProcessor.waitForResourceUseToBeSubmitted(context, use);
         }
         // This ResourceUse must have been submitted.
@@ -572,17 +574,18 @@ class RendererVk : angle::NonCopyable
         return angle::Result::Continue;
     }
 
-    angle::Result waitForQueueSerialToBeSubmitted(vk::Context *context,
-                                                  const QueueSerial &queueSerial)
+    angle::Result waitForQueueSerialToBeSubmittedToDevice(vk::Context *context,
+                                                          const QueueSerial &queueSerial)
     {
         // This is only needed for async submission code path. For immediate submission, it is a nop
         // since everything is submitted immediately.
         if (isAsyncCommandQueueEnabled())
         {
+            ASSERT(mCommandProcessor.hasQueueSerialEnqueued(queueSerial));
             return mCommandProcessor.waitForQueueSerialToBeSubmitted(context, queueSerial);
         }
         // This queueSerial must have been submitted.
-        ASSERT(mCommandQueue.hasResourceUseSubmitted(vk::ResourceUse(queueSerial)));
+        ASSERT(mCommandQueue.hasQueueSerialSubmitted(queueSerial));
         return angle::Result::Continue;
     }
 
@@ -621,7 +624,7 @@ class RendererVk : angle::NonCopyable
                                  egl::ContextPriority contextPriority,
                                  const vk::Semaphore *signalSemaphore,
                                  vk::SecondaryCommandPools *commandPools,
-                                 const QueueSerial &submitSerialOut);
+                                 const QueueSerial &submitQueueSerial);
 
     void handleDeviceLost();
     angle::Result finishResourceUse(vk::Context *context, const vk::ResourceUse &use);
