fa0cf7b06bfbceb9fe873e86c11aa6735eea41ac
Igor Nazarov <i.nazarov@samsung.com>
2023-02-20 17:11:44 +0200

    Vulkan: Add *_swapchain_acquire_image_wait_stage_top_of_pipe
    This is the alternative fix of the Swapchain Acquire Image semaphore
    wait stage flags bug.
    
    Instead of waiting for all stages where Image may be first used, this
    fix uses "VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT". Results are:
    - Relaxed pipeline barrier when transitioning from
      VK_IMAGE_LAYOUT_PRESENT_SRC_KHR. May improve execution time on Device.
    - Added latency from Swapchain Acquire Image semaphore signal operations
      to commands execution on Device. Hard to tell the effect, commands
      finish on Device may wait longer. May be slightly compensated by the
      relaxed barrier.
    
    How exactly this change will affect performance may depend from the
    Device and the Application.
    
    Bug: angleproject:8030
    Change-Id: I3879a7d0bbf19ca0c7441d6e7d6149e431b9d6f7

--- src/libANGLE/renderer/vulkan/vk_helpers.h
+++ src/libANGLE/renderer/vulkan/vk_helpers.h
@@ -43,10 +43,18 @@ constexpr VkImageCreateFlags kVkImageCreateFlagsNone = 0;
 
 constexpr VkFilter kDefaultYCbCrChromaFilter = VK_FILTER_NEAREST;
 
+#if ANGLE_ENABLE_VULKAN_SWAPCHAIN_ACQUIRE_IMAGE_WAIT_STAGE_TOP_OF_PIPE
+// Experimental. Using TOP OF PIPE as source mask, may relax pipeline barrier when transition from
+// VK_IMAGE_LAYOUT_PRESENT_SRC_KHR by increasing latency from Swapchain Acquire Image Semaphore
+// signal operation and commands execution on Device.
+constexpr VkPipelineStageFlags kSwapchainAcquireImageWaitStageFlags =
+    VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT;
+#else
 constexpr VkPipelineStageFlags kSwapchainAcquireImageWaitStageFlags =
     VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT |          // First use is a blit command.
     VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT |  // First use is a draw command.
     VK_PIPELINE_STAGE_TRANSFER_BIT;                  // First use is a clear without scissor.
+#endif
 
 using StagingBufferOffsetArray = std::array<VkDeviceSize, 2>;
 
