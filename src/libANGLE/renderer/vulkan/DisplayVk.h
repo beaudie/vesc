da499f74a5cf5d97ecdf364e560d811a18293698
Igor Nazarov <i.nazarov@samsung.com>
2023-02-24 15:41:00 +0200

    Vulkan: Enforce ContextPriority in ShareGroup and with EGLImage
    This CL enforces single Context Priority for all Contexts in a Share
    Group. This is necessary until Vulkan Semaphores will be used to
    automatically synchronize Resource access between Contexts.
    
    Contexts Priority updated when new Contexts is added to the Share Group.
    New Priority will be the highest among all ever existed Contexts
    (except if Priority is locked).
    
    When Contexts Priority changes, all flushed commands are submitted to
    the old VkQueue and semaphore is inserted into the new VkQueue.
    Currently opened RenderPasses and commands will not be flushed.
    
    When EGLImage is used in a Context, all Contexts in that Share Group
    locked (forever) to the Default Priority (Medium). This is done to
    simplify the implementation and because of the current limitations
    (lack of mutex protection across Context Share Groups).
    
    Note: the EGL_CONTEXT_PRIORITY_LEVEL_IMG will report initial priority.
    
    Bug: angleproject:8039
    Change-Id: Ia6a2f0084d39168a58fd7ec33edc90ece9cead05

--- src/libANGLE/renderer/vulkan/DisplayVk.h
+++ src/libANGLE/renderer/vulkan/DisplayVk.h
@@ -103,6 +103,13 @@ class ShareGroupVk : public ShareGroupImpl
     void addContext(ContextVk *contextVk);
     void removeContext(ContextVk *contextVk);
 
+    // Temporary workaround until VkSemaphore(s) will be used between different priorities.
+    angle::Result unifyContextsPriority(ContextVk *contextVk,
+                                        egl::ContextPriority newContextPriority,
+                                        egl::ContextPriority *newContextPriorityOut);
+    // Temporary workaround until VkSemaphore(s) will be used between different priorities.
+    angle::Result lockDefaultContextsPriority(ContextVk *contextVk);
+
     UpdateDescriptorSetsBuilder *getUpdateDescriptorSetsBuilder()
     {
         return &mUpdateDescriptorSetsBuilder;
@@ -116,6 +123,8 @@ class ShareGroupVk : public ShareGroupImpl
     void waitForCurrentMonolithicPipelineCreationTask();
 
   private:
+    angle::Result updateContextsPriority(ContextVk *contextVk, egl::ContextPriority newPriority);
+
     // VkFramebuffer caches
     FramebufferCache mFramebufferCache;
 
@@ -132,6 +141,10 @@ class ShareGroupVk : public ShareGroupImpl
 
     // The list of contexts within the share group
     ContextVkSet mContexts;
+
+    // Priority of all Contexts in the mContexts
+    egl::ContextPriority mContextsPriority;
+    bool mIsContextsPriorityLocked;
 
     // Storage for vkUpdateDescriptorSets
     UpdateDescriptorSetsBuilder mUpdateDescriptorSetsBuilder;
