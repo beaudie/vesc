cfe126df578434801718a4541c0ae559452ddf93
Alexey Knyazev <lexa.knyazev@gmail.com>
2023-03-01 00:00:00 +0000

    Metal: Implement ANGLE_stencil_texturing
    Used texture views to sample stencil values
    from combined depth/stencil textures.
    
    Texture2DDepthStencilTestES3.TexSampleModes* tests
    were split into swizzled and non-swizzled variants
    to suppress only swizzled cases on some platforms.
    
    Added a new avoidStencilTextureSwizzle workaround to
    skip creating swizzled texture views of stencil-only
    textures on platforms that fail to sample from them.
    
    Fixed: angleproject:8051
    Change-Id: I0b1148f8d30fc6459239efcdaeee6c0364633cc8

--- src/libANGLE/renderer/metal/DisplayMtl.mm
+++ src/libANGLE/renderer/metal/DisplayMtl.mm
@@ -934,6 +934,7 @@ void DisplayMtl::initializeExtensions() const
     mNativeExtensions.framebufferBlitNV             = true;
     mNativeExtensions.framebufferMultisampleANGLE   = true;
     mNativeExtensions.polygonOffsetClampEXT         = true;
+    mNativeExtensions.stencilTexturingANGLE         = true;
     mNativeExtensions.copyTextureCHROMIUM           = true;
     mNativeExtensions.copyCompressedTextureCHROMIUM = false;
 
@@ -1209,6 +1210,8 @@ void DisplayMtl::initializeFeatures()
 
     ANGLE_FEATURE_CONDITION((&mFeatures), hasTextureSwizzle,
                             isMetal2_2 && supportsEitherGPUFamily(3, 2) && !isSimulator);
+
+    ANGLE_FEATURE_CONDITION((&mFeatures), avoidStencilTextureSwizzle, isIntel());
 
     // http://crbug.com/1136673
     // Fence sync is flaky on Nvidia
