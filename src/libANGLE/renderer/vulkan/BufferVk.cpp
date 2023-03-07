33a34a75fd02bf57a103402ffa75b69810a5813a
Charlie Lao <cclao@google.com>
2023-03-07 15:06:10 -0800

    Vulkan: Optimize glBufferData call to improve storage reuse
    If app calls glBufferData with certain size, then calls it again with
    size 0, and then call it again with same old size again, we should try
    to reuse the existing storage. When size is zero, with the existing
    logic, we never free the storage. When glBufferData is called third time
    with the same size as the first glBufferData call, we expect to reuse
    the existing storage. But because of the storage reuse logic is
    comparing buffer's new size to the old size (which is 0), we missed the
    opportunity to reuse the existing storage. This CL update the reuse
    logic so that it checks the new size against storage's size and if the
    size is big enough, then we reuse instead of reallocate.
    
    Bug: b/271915956
    Change-Id: I6f91e3e85b104eca215b28e7d0bea413ecc4401c

--- src/libANGLE/renderer/vulkan/BufferVk.cpp
+++ src/libANGLE/renderer/vulkan/BufferVk.cpp
@@ -368,8 +368,7 @@ angle::Result BufferVk::setDataWithUsageFlags(const gl::Context *context,
 
         return angle::Result::Continue;
     }
-    return setDataWithMemoryType(context, target, data, size, memoryPropertyFlags,
-                                 persistentMapRequired, usage);
+    return setDataWithMemoryType(context, target, data, size, memoryPropertyFlags, usage);
 }
 
 angle::Result BufferVk::setData(const gl::Context *context,
@@ -382,7 +381,7 @@ angle::Result BufferVk::setData(const gl::Context *context,
     // Assume host visible/coherent memory available.
     VkMemoryPropertyFlags memoryPropertyFlags =
         GetPreferredMemoryType(contextVk->getRenderer(), target, usage);
-    return setDataWithMemoryType(context, target, data, size, memoryPropertyFlags, false, usage);
+    return setDataWithMemoryType(context, target, data, size, memoryPropertyFlags, usage);
 }
 
 angle::Result BufferVk::setDataWithMemoryType(const gl::Context *context,
@@ -390,12 +389,11 @@ angle::Result BufferVk::setDataWithMemoryType(const gl::Context *context,
                                               const void *data,
                                               size_t size,
                                               VkMemoryPropertyFlags memoryPropertyFlags,
-                                              bool persistentMapRequired,
                                               gl::BufferUsage usage)
 {
     ContextVk *contextVk = vk::GetImpl(context);
+    BufferUpdateType updateType;
 
-    mIsDynamicUsage = IsUsageDynamic(usage);
     // Reset the flag since the buffer contents are being reinitialized. If the caller passed in
     // data to fill the buffer, the flag will be updated when the data is copied to the buffer.
     mHasValidData = false;
@@ -406,30 +404,47 @@ angle::Result BufferVk::setDataWithMemoryType(const gl::Context *context,
         return angle::Result::Continue;
     }
 
-    const bool bufferSizeChanged = size != static_cast<size_t>(mState.getSize());
-    const bool inUseAndRespecifiedWithoutData =
-        (data == nullptr && isCurrentlyInUse(contextVk->getRenderer()));
-
-    // The entire buffer is being respecified, possibly with null data.
-    // Release and init a new mBuffer with requested size.
-    if (bufferSizeChanged || inUseAndRespecifiedWithoutData)
+    if (!mBuffer.valid())
     {
-        // Release and re-create the memory and buffer.
-        release(contextVk);
-
+        mIsDynamicUsage      = IsUsageDynamic(usage);
         mMemoryPropertyFlags = memoryPropertyFlags;
         ANGLE_TRY(GetMemoryTypeIndex(contextVk, size, memoryPropertyFlags, &mMemoryTypeIndex));
-
         ANGLE_TRY(acquireBufferHelper(contextVk, size));
+        updateType = BufferUpdateType::StorageRedefined;
+    }
+    else
+    {
+        const bool bufferSizeChanged     = size > mBuffer.getSize();
+        const bool bufferUsageChanged    = mIsDynamicUsage != IsUsageDynamic(usage);
+        const bool memoryPropertyChanged = mMemoryPropertyFlags != memoryPropertyFlags;
+        const bool inUseAndRespecifiedWithoutData =
+            (data == nullptr && isCurrentlyInUse(contextVk->getRenderer()));
+
+        mIsDynamicUsage      = IsUsageDynamic(usage);
+        mMemoryPropertyFlags = memoryPropertyFlags;
+
+        // The entire buffer is being respecified, possibly with null data.
+        // Release and init a new mBuffer with requested size.
+        if (bufferUsageChanged || bufferSizeChanged || memoryPropertyChanged ||
+            inUseAndRespecifiedWithoutData)
+        {
+            // Release and re-create the memory and buffer.
+            release(contextVk);
+            ANGLE_TRY(GetMemoryTypeIndex(contextVk, size, memoryPropertyFlags, &mMemoryTypeIndex));
+            ANGLE_TRY(acquireBufferHelper(contextVk, size));
+            updateType = BufferUpdateType::StorageRedefined;
+        }
+        else
+        {
+            updateType = BufferUpdateType::ContentsUpdate;
+        }
     }
 
     if (data)
     {
         // Treat full-buffer updates as SubData calls.
-        BufferUpdateType updateType = bufferSizeChanged ? BufferUpdateType::StorageRedefined
-                                                        : BufferUpdateType::ContentsUpdate;
-
-        ANGLE_TRY(setDataImpl(contextVk, static_cast<const uint8_t *>(data), size, 0, updateType));
+        ANGLE_TRY(
+            setDataImpl(contextVk, size, static_cast<const uint8_t *>(data), size, 0, updateType));
     }
 
     return angle::Result::Continue;
@@ -444,7 +459,8 @@ angle::Result BufferVk::setSubData(const gl::Context *context,
     ASSERT(mBuffer.valid());
 
     ContextVk *contextVk = vk::GetImpl(context);
-    ANGLE_TRY(setDataImpl(contextVk, static_cast<const uint8_t *>(data), size, offset,
+    ANGLE_TRY(setDataImpl(contextVk, static_cast<size_t>(mState.getSize()),
+                          static_cast<const uint8_t *>(data), size, offset,
                           BufferUpdateType::ContentsUpdate));
 
     return angle::Result::Continue;
@@ -883,23 +899,25 @@ angle::Result BufferVk::stagedUpdate(ContextVk *contextVk,
 }
 
 angle::Result BufferVk::acquireAndUpdate(ContextVk *contextVk,
+                                         size_t bufferSize,
                                          const uint8_t *data,
                                          size_t updateSize,
-                                         size_t offset,
+                                         size_t updateOffset,
                                          BufferUpdateType updateType)
 {
     // We shouldn't get here if this is external memory
     ASSERT(!isExternalBuffer());
     // If StorageRedefined, we can not use mState.getSize() to allocate a new buffer.
     ASSERT(updateType != BufferUpdateType::StorageRedefined);
+    ASSERT(mBuffer.valid());
+    ASSERT(mBuffer.getSize() >= bufferSize);
 
     // Here we acquire a new BufferHelper and directUpdate() the new buffer.
     // If the subData size was less than the buffer's size we additionally enqueue
     // a GPU copy of the remaining regions from the old mBuffer to the new one.
     vk::BufferHelper src;
-    size_t bufferSize              = static_cast<size_t>(mState.getSize());
-    size_t offsetAfterSubdata      = (offset + updateSize);
-    bool updateRegionBeforeSubData = mHasValidData && (offset > 0);
+    size_t offsetAfterSubdata      = (updateOffset + updateSize);
+    bool updateRegionBeforeSubData = mHasValidData && (updateOffset > 0);
     bool updateRegionAfterSubData  = mHasValidData && (offsetAfterSubdata < bufferSize);
 
     uint8_t *srcMapPtrBeforeSubData = nullptr;
@@ -927,7 +945,7 @@ angle::Result BufferVk::acquireAndUpdate(ContextVk *contextVk,
     }
 
     ANGLE_TRY(acquireBufferHelper(contextVk, bufferSize));
-    ANGLE_TRY(updateBuffer(contextVk, data, updateSize, offset));
+    ANGLE_TRY(updateBuffer(contextVk, data, updateSize, updateOffset));
 
     constexpr int kMaxCopyRegions = 2;
     angle::FixedVector<VkBufferCopy, kMaxCopyRegions> copyRegions;
@@ -937,11 +955,11 @@ angle::Result BufferVk::acquireAndUpdate(ContextVk *contextVk,
         if (srcMapPtrBeforeSubData)
         {
             ASSERT(mBuffer.isHostVisible());
-            ANGLE_TRY(directUpdate(contextVk, srcMapPtrBeforeSubData, offset, 0));
+            ANGLE_TRY(directUpdate(contextVk, srcMapPtrBeforeSubData, updateOffset, 0));
         }
         else
         {
-            copyRegions.push_back({src.getOffset(), mBuffer.getOffset(), offset});
+            copyRegions.push_back({src.getOffset(), mBuffer.getOffset(), updateOffset});
         }
     }
 
@@ -975,9 +993,10 @@ angle::Result BufferVk::acquireAndUpdate(ContextVk *contextVk,
 }
 
 angle::Result BufferVk::setDataImpl(ContextVk *contextVk,
+                                    size_t bufferSize,
                                     const uint8_t *data,
-                                    size_t size,
-                                    size_t offset,
+                                    size_t updateSize,
+                                    size_t updateOffset,
                                     BufferUpdateType updateType)
 {
     // if the buffer is currently in use
@@ -1003,13 +1022,11 @@ angle::Result BufferVk::setDataImpl(ContextVk *contextVk,
         const bool canAcquireAndUpdate =
             !isExternalBuffer() && updateType != BufferUpdateType::StorageRedefined;
         if (canAcquireAndUpdate &&
-            (!mHasValidData ||
-             ShouldAvoidRenderPassBreakOnUpdate(contextVk, mBuffer,
-                                                static_cast<size_t>(mState.getSize())) ||
-             ShouldAllocateNewMemoryForUpdate(contextVk, size,
-                                              static_cast<size_t>(mState.getSize()))))
+            (!mHasValidData || ShouldAvoidRenderPassBreakOnUpdate(contextVk, mBuffer, bufferSize) ||
+             ShouldAllocateNewMemoryForUpdate(contextVk, updateSize, bufferSize)))
         {
-            ANGLE_TRY(acquireAndUpdate(contextVk, data, size, offset, updateType));
+            ANGLE_TRY(acquireAndUpdate(contextVk, bufferSize, data, updateSize, updateOffset,
+                                       updateType));
         }
         else
         {
@@ -1019,12 +1036,12 @@ angle::Result BufferVk::setDataImpl(ContextVk *contextVk,
                                       "Breaking the render pass on small upload to large buffer");
             }
 
-            ANGLE_TRY(stagedUpdate(contextVk, data, size, offset));
+            ANGLE_TRY(stagedUpdate(contextVk, data, updateSize, updateOffset));
         }
     }
     else
     {
-        ANGLE_TRY(updateBuffer(contextVk, data, size, offset));
+        ANGLE_TRY(updateBuffer(contextVk, data, updateSize, updateOffset));
     }
 
     // Update conversions
