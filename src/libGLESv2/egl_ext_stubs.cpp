cd4a8a3de6b0ba465b5464a7dfb4b06964296b31
Mark Lobodzinski <mark@lunarg.com>
2023-03-10 11:56:00 -0700

    Don't hold global surface lock during AcquireNextImage
    On Android, the global surface lock must be released before calls
    to vkAcquireNextImageKHR, though GetSurfaceIfValid must remain
    inside the surface lock.
    
    Bug: b/268091451
    Change-Id: Ia9c96de52a367b4537a42cee83e2372322fbdfbc

--- src/libGLESv2/egl_ext_stubs.cpp
+++ src/libGLESv2/egl_ext_stubs.cpp
@@ -640,13 +640,13 @@ EGLBoolean SwapBuffersWithDamageKHR(Thread *thread,
 
 EGLBoolean PrepareSwapBuffersANGLE(EGLDisplay dpy, EGLSurface surface)
 {
-    ANGLE_SCOPED_GLOBAL_SURFACE_LOCK();
-
-    egl::Display *dpyPacked = PackParam<egl::Display *>(dpy);
-    SurfaceID surfacePacked = PackParam<SurfaceID>(surface);
-    Thread *thread          = egl::GetCurrentThread();
-    Surface *surfacePtr     = nullptr;
+    egl::Display *dpyPacked        = PackParam<egl::Display *>(dpy);
+    SurfaceID surfacePacked        = PackParam<SurfaceID>(surface);
+    Thread *thread                 = egl::GetCurrentThread();
+    Surface *surfacePtr            = nullptr;
+    const egl::Surface *eglSurface = GetSurfaceIfValid(dpyPacked, surfacePacked);
     {
+        ANGLE_SCOPED_GLOBAL_SURFACE_LOCK();
         ANGLE_SCOPED_GLOBAL_LOCK();
 
         EGL_EVENT(PrepareSwapBuffersANGLE, "dpy = 0x%016" PRIxPTR ", surface = 0x%016" PRIxPTR "",
@@ -661,7 +661,7 @@ EGLBoolean PrepareSwapBuffersANGLE(EGLDisplay dpy, EGLSurface surface)
         surfacePtr = dpyPacked->getSurface(surfacePacked);
     }
     ANGLE_EGL_TRY_RETURN(thread, surfacePtr->prepareSwap(thread->getContext()), "prepareSwap",
-                         GetSurfaceIfValid(dpyPacked, surfacePacked), EGL_FALSE);
+                         eglSurface, EGL_FALSE);
 
     thread->setSuccess();
     return EGL_TRUE;
