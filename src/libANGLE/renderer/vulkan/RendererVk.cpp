aca790ad26cba6d63a483deb2fc3046e9a3a9b8c
Igor Nazarov <i.nazarov@samsung.com>
2023-03-14 14:48:30 +0200

    Vulkan: Fix freeing Secondary Command Buffers from wrong thread.
    Problem:
    - Secondary Command Buffers are freed in the "CommandQueue" class.
    - This may happen from any Context thread that calls
      "checkCompletedCommands()" or "finish<*>()" methods.
    - As the result, one Command Buffer may be freed from one thread, while
      other Command Buffer from the same "VkCommandPool" is
      allocated/reset/recorded in the other thread.
    
    Vulkan spec demands external "VkCommandPool" synchronization for any
    modifications (begin/end/reset/free/cmd) on its "VkCommandBuffer"s.
    
    Fix:
    - Added new "rx::vk::SecondaryCommandPool" class that replaces the
      "rx::vk::CommandPool" wrapper.
    - This class has "collect()" method for storing "VkCommandBuffer"s.
      Collected buffers are freed from the correct thread on the next
      "allocate()" call.
    
    This CL only fixes the problem, keeping Secondary Command Buffer memory
    management as is (allocate/free single buffer without reuse).
    In the future CLs this behavior may be changed (reuse buffers,
    reset/free entire pools).
    
    Bug: angleproject:6100
    Change-Id: If938416c4df4fe55f0cfb418b6759721ac53098b

--- src/libANGLE/renderer/vulkan/RendererVk.cpp
+++ src/libANGLE/renderer/vulkan/RendererVk.cpp
@@ -5229,7 +5229,7 @@ void RendererVk::queuePresent(vk::Context *context,
 template <typename CommandBufferHelperT, typename RecyclerT>
 angle::Result RendererVk::getCommandBufferImpl(
     vk::Context *context,
-    vk::CommandPool *commandPool,
+    vk::SecondaryCommandPool *commandPool,
     vk::SecondaryCommandMemoryAllocator *commandsAllocator,
     RecyclerT *recycler,
     CommandBufferHelperT **commandBufferHelperOut)
@@ -5240,7 +5240,7 @@ angle::Result RendererVk::getCommandBufferImpl(
 
 angle::Result RendererVk::getOutsideRenderPassCommandBufferHelper(
     vk::Context *context,
-    vk::CommandPool *commandPool,
+    vk::SecondaryCommandPool *commandPool,
     vk::SecondaryCommandMemoryAllocator *commandsAllocator,
     vk::OutsideRenderPassCommandBufferHelper **commandBufferHelperOut)
 {
@@ -5251,7 +5251,7 @@ angle::Result RendererVk::getOutsideRenderPassCommandBufferHelper(
 
 angle::Result RendererVk::getRenderPassCommandBufferHelper(
     vk::Context *context,
-    vk::CommandPool *commandPool,
+    vk::SecondaryCommandPool *commandPool,
     vk::SecondaryCommandMemoryAllocator *commandsAllocator,
     vk::RenderPassCommandBufferHelper **commandBufferHelperOut)
 {
@@ -5261,19 +5261,17 @@ angle::Result RendererVk::getRenderPassCommandBufferHelper(
 }
 
 void RendererVk::recycleOutsideRenderPassCommandBufferHelper(
-    VkDevice device,
     vk::OutsideRenderPassCommandBufferHelper **commandBuffer)
 {
     ANGLE_TRACE_EVENT0("gpu.angle", "RendererVk::recycleOutsideRenderPassCommandBufferHelper");
-    mOutsideRenderPassCommandBufferRecycler.recycleCommandBufferHelper(device, commandBuffer);
+    mOutsideRenderPassCommandBufferRecycler.recycleCommandBufferHelper(commandBuffer);
 }
 
 void RendererVk::recycleRenderPassCommandBufferHelper(
-    VkDevice device,
     vk::RenderPassCommandBufferHelper **commandBuffer)
 {
     ANGLE_TRACE_EVENT0("gpu.angle", "RendererVk::recycleRenderPassCommandBufferHelper");
-    mRenderPassCommandBufferRecycler.recycleCommandBufferHelper(device, commandBuffer);
+    mRenderPassCommandBufferRecycler.recycleCommandBufferHelper(commandBuffer);
 }
 
 void RendererVk::logCacheStats() const
