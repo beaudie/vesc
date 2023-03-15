google/angle

commit b194c21ad30145ca15153a93425a37a8211df373

Vulkan: Enforce ContextPriority in ShareGroup and with EGLImage
This CL enforces single Context Priority for all Contexts in a Share
Group. This is necessary until Vulkan Semaphores will be used to
automatically synchronize Resource access between Contexts.

Contexts Priority updated when new Contexts is added to the Share Group.
New Priority will be the highest among all ever existed Contexts
(except if Priority is locked).

When Contexts Priority changes, all flushed commands are submitted to
the old VkQueue and semaphore is inserted into the new VkQueue.
Currently opened RenderPasses and commands will not be flushed.

When EGLImage is used in a Context, all Contexts in that Share Group
locked (forever) to the Default Priority (Medium). This is done to
simplify the implementation and because of the current limitations
(lack of mutex protection across Context Share Groups).

Notes:
- the EGL_CONTEXT_PRIORITY_LEVEL_IMG will report initial priority.
- below tests fail on G996B without this CL.

Bug: angleproject:8039
Test: angle_end2end_tests --gtest_filter=MultithreadingTestES3.RenderThenSampleDifferentContextPriority*
Test: angle_end2end_tests --gtest_filter=MultithreadingTestES3.RenderThenSampleInNewContextWithDifferentPriority*
Test: angle_end2end_tests --gtest_filter=MultithreadingTestES3.RenderThenSampleDifferentContextPriorityUsingEGLImage*
Change-Id: Ia6a2f0084d39168a58fd7ec33edc90ece9cead05
Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/4289750
Reviewed-by: Charlie Lao <cclao@google.com>
Reviewed-by: Shahbaz Youssefi <syoussefi@chromium.org>
Commit-Queue: Igor Nazarov <i.nazarov@samsung.com>

src/libANGLE/renderer/vulkan/RendererVk.cpp
@@ -4683,6 +4683,28 @@ angle::Result RendererVk::queueSubmitOneOff(vk::Context *context,
     return angle::Result::Continue;
 }
 
+angle::Result RendererVk::queueSubmitWaitSemaphore(vk::Context *context,
+                                                   egl::ContextPriority priority,
+                                                   const vk::Semaphore *waitSemaphore,
+                                                   VkPipelineStageFlags waitSemaphoreStageMasks,
+                                                   QueueSerial submitQueueSerial)
+{
+    if (isAsyncCommandQueueEnabled())
+    {
+        ANGLE_TRY(mCommandProcessor.enqueueSubmitOneOffCommands(
+            context, vk::ProtectionType::Unprotected, priority, VK_NULL_HANDLE, waitSemaphore,
+            waitSemaphoreStageMasks, nullptr, vk::SubmitPolicy::AllowDeferred, submitQueueSerial));
+    }
+    else
+    {
+        ANGLE_TRY(mCommandQueue.queueSubmitOneOff(
+            context, vk::ProtectionType::Unprotected, priority, VK_NULL_HANDLE, waitSemaphore,
+            waitSemaphoreStageMasks, nullptr, vk::SubmitPolicy::AllowDeferred, submitQueueSerial));
+    }
+
+    return angle::Result::Continue;
+}
+
 template <VkFormatFeatureFlags VkFormatProperties::*features>
 VkFormatFeatureFlags RendererVk::getFormatFeatureBits(angle::FormatID formatID,
                                                       const VkFormatFeatureFlags featureBits) const
@@ -5010,6 +5032,50 @@ angle::Result RendererVk::submitCommands(vk::Context *context,
     }
 
     ANGLE_TRY(mCommandQueue.postSubmitCheck(context));
+
+    return angle::Result::Continue;
+}
+
+angle::Result RendererVk::submitPriorityDependency(vk::Context *context,
+                                                   vk::ProtectionTypes protectionTypes,
+                                                   egl::ContextPriority srcContextPriority,
+                                                   egl::ContextPriority dstContextPriority,
+                                                   SerialIndex index)
+{
+    vk::DeviceScoped<vk::Semaphore> semaphore(mDevice);
+    ANGLE_VK_TRY(context, semaphore.get().init(mDevice));
+
+    // First, submit already flushed commands / wait semaphores into the source Priority VkQueue.
+    // Commands that are in the Secondary Command Buffers will be flushed into the new VkQueue.
+
+    // Submit commands and attach Signal Semaphore.
+    ASSERT(protectionTypes.any());
+    while (protectionTypes.any())
+    {
+        vk::ProtectionType protectionType = protectionTypes.first();
+        protectionTypes.reset(protectionType);
+
+        QueueSerial queueSerial(index, generateQueueSerial(index));
+        // Submit semaphore only if this is the last submission (all into the same VkQueue).
+        const vk::Semaphore *signalSemaphore = nullptr;
+        if (protectionTypes.none())
+        {
+            signalSemaphore = &semaphore.get();
+        }
+        ANGLE_TRY(submitCommands(context, protectionType, srcContextPriority, signalSemaphore,
+                                 queueSerial));
+    }
+
+    // Submit only Wait Semaphore into the destination Priority (VkQueue).
+    QueueSerial queueSerial(index, generateQueueSerial(index));
+    angle::Result result =
+        queueSubmitWaitSemaphore(context, dstContextPriority, &semaphore.get(),
+                                 VK_PIPELINE_STAGE_ALL_COMMANDS_BIT, queueSerial);
+
+    vk::ResourceUse semaphoreUse(queueSerial);
+    collectGarbage(semaphoreUse, &semaphore.get());
+    // Handle result later to be able to collect semaphore.
+    ANGLE_TRY(result);
 
     return angle::Result::Continue;
 }

src/libANGLE/renderer/vulkan/RendererVk.h
@@ -306,6 +306,12 @@ class RendererVk : angle::NonCopyable
                                     vk::SubmitPolicy submitPolicy,
                                     QueueSerial *queueSerialOut);
 
+    angle::Result queueSubmitWaitSemaphore(vk::Context *context,
+                                           egl::ContextPriority priority,
+                                           const vk::Semaphore *waitSemaphore,
+                                           VkPipelineStageFlags waitSemaphoreStageMasks,
+                                           QueueSerial submitQueueSerial);
+
     template <typename... ArgsT>
     void collectGarbage(const vk::ResourceUse &use, ArgsT... garbageIn)
     {
@@ -484,6 +490,12 @@ class RendererVk : angle::NonCopyable
                                  egl::ContextPriority contextPriority,
                                  const vk::Semaphore *signalSemaphore,
                                  const QueueSerial &submitSerialOut);
+
+    angle::Result submitPriorityDependency(vk::Context *context,
+                                           vk::ProtectionTypes protectionTypes,
+                                           egl::ContextPriority srcContextPriority,
+                                           egl::ContextPriority dstContextPriority,
+                                           SerialIndex index);
 
     void handleDeviceLost();
     angle::Result finishResourceUse(vk::Context *context, const vk::ResourceUse &use);

src/libANGLE/renderer/vulkan/ContextVk.cpp
@@ -887,7 +887,8 @@ ContextVk::ContextVk(const gl::State &state, gl::ErrorSet *errorSet, RendererVk 
       mHasWaitSemaphoresPendingSubmission(false),
       mGpuClockSync{std::numeric_limits<double>::max(), std::numeric_limits<double>::max()},
       mGpuEventTimestampOrigin(0),
-      mContextPriority(renderer->getDriverPriority(GetContextPriority(state))),
+      mInitialContextPriority(renderer->getDriverPriority(GetContextPriority(state))),
+      mContextPriority(mInitialContextPriority),
       mProtectionType(vk::ConvertProtectionBoolToType(state.hasProtectedContent())),
       mShareGroupVk(vk::GetImpl(state.getShareGroup()))
 {
@@ -1254,6 +1255,8 @@ angle::Result ContextVk::getIncompleteTexture(const gl::Context *context,
 angle::Result ContextVk::initialize()
 {
     ANGLE_TRACE_EVENT0("gpu.angle", "ContextVk::initialize");
+
+    ANGLE_TRY(mShareGroupVk->unifyContextsPriority(this));
 
     ANGLE_TRY(mQueryPools[gl::QueryType::AnySamples].init(this, VK_QUERY_TYPE_OCCLUSION,
                                                           vk::kDefaultOcclusionQueryPoolSize));

beaudie/vesc

commit ae7459b4d86ab3f6397ae11ed941c6b50198b1ef

Vulkan: Hot fix crash when using Invalid VkSemaphore Object.
Problem started after the commit:
    b194c21ad30145ca15153a93425a37a8211df373
    Vulkan: Enforce ContextPriority in ShareGroup and with EGLImage

Crashes "com.sec.android.app.launcher" (S906B) with the VVL error:

    VUID-VkSubmitInfo-pWaitSemaphores-parameter(ERROR / SPEC): msgNum: -1328048864 -
    Validation Error: [ VUID-VkSubmitInfo-pWaitSemaphores-parameter ]
    Object 0: handle = 0xb4000078c1aab430, type = VK_OBJECT_TYPE_INSTANCE;
    | MessageID = 0xb0d79520 | Invalid VkSemaphore Object 0xb400007831740b98.
    The Vulkan spec states: If waitSemaphoreCount is not 0, pWaitSemaphores
    must be a valid pointer to an array of waitSemaphoreCount valid VkSemaphore handles
    (https://www.khronos.org/registry/vulkan/specs/1.3-extensions/html/vkspec.html#VUID-VkSubmitInfo-pWaitSemaphores-parameter)

Happens only with enabled "asyncCommandQueue" feature:
- "RendererVk::submitPriorityDependency()" creates "Vk::Semaphore"
  object on the stack.
- Then submits this Semaphore with "queueSubmitWaitSemaphore()" method.
- "vk::Semaphore" passed to the "CommandProcessor"s
  "enqueueSubmitOneOffCommands()" method where stored in the
  "CommandProcessorTask".
- Stack "vk::Semaphore" object garbage collected by the "RendererVk"
  and then destroyed.
- "CommandProcessorTask" now points to the invalid memory.

May be the cause of the "chromium:1424783" issue.

This CL changes usage of "vk::Semaphore" and "vk::Fence" pointers in
the "CommandProcessor.h" classes to Vulkan Handles. This will make API
consistent, fix this problem, and avoid similar problems in the future.

Bug: angleproject:8039
Bug: chromium:1424783
Change-Id: I8818288b9783b5c5a7970bf82ec721452ae57471

src/libANGLE/renderer/vulkan/CommandProcessor.cpp
@@ -231,9 +231,9 @@ void CommandProcessorTask::initTask()
     mRenderPassCommandBuffer        = nullptr;
     mRenderPass                     = nullptr;
     mSemaphore                      = VK_NULL_HANDLE;
-    mOneOffWaitSemaphore            = nullptr;
+    mOneOffWaitSemaphore            = VK_NULL_HANDLE;
     mOneOffWaitSemaphoreStageMask   = 0;
-    mOneOffFence                    = nullptr;
+    mOneOffFence                    = VK_NULL_HANDLE;
     mPresentInfo                    = {};
     mPresentInfo.pResults           = nullptr;
     mPresentInfo.pSwapchains        = nullptr;
@@ -242,7 +242,7 @@ void CommandProcessorTask::initTask()
     mPresentInfo.pWaitSemaphores    = nullptr;
     mPresentFence                   = VK_NULL_HANDLE;
     mSwapchainStatus                = nullptr;
-    mOneOffCommandBufferVk          = VK_NULL_HANDLE;
+    mOneOffCommandBuffer            = VK_NULL_HANDLE;
     mPriority                       = egl::ContextPriority::Medium;
     mProtectionType                 = ProtectionType::InvalidEnum;
 }
@@ -389,7 +389,7 @@ void CommandProcessorTask::initPresent(egl::ContextPriority priority,
 }
 
 void CommandProcessorTask::initFlushAndQueueSubmit(
-    const VkSemaphore semaphore,
+    VkSemaphore semaphore,
     ProtectionType protectionType,
     egl::ContextPriority priority,
     SecondaryCommandBufferList &&commandBuffersToReset,
@@ -406,13 +406,13 @@ void CommandProcessorTask::initFlushAndQueueSubmit(
 void CommandProcessorTask::initOneOffQueueSubmit(VkCommandBuffer commandBufferHandle,
                                                  ProtectionType protectionType,
                                                  egl::ContextPriority priority,
-                                                 const Semaphore *waitSemaphore,
+                                                 VkSemaphore waitSemaphore,
                                                  VkPipelineStageFlags waitSemaphoreStageMask,
-                                                 const Fence *fence,
+                                                 VkFence fence,
                                                  const QueueSerial &submitQueueSerial)
 {
     mTask                         = CustomTask::OneOffQueueSubmit;
-    mOneOffCommandBufferVk        = commandBufferHandle;
+    mOneOffCommandBuffer          = commandBufferHandle;
     mOneOffWaitSemaphore          = waitSemaphore;
     mOneOffWaitSemaphoreStageMask = waitSemaphoreStageMask;
     mOneOffFence                  = fence;
@@ -442,7 +442,7 @@ CommandProcessorTask &CommandProcessorTask::operator=(CommandProcessorTask &&rhs
     std::swap(mSubmitQueueSerial, rhs.mSubmitQueueSerial);
     std::swap(mPriority, rhs.mPriority);
     std::swap(mProtectionType, rhs.mProtectionType);
-    std::swap(mOneOffCommandBufferVk, rhs.mOneOffCommandBufferVk);
+    std::swap(mOneOffCommandBuffer, rhs.mOneOffCommandBuffer);
 
     copyPresentInfo(rhs.mPresentInfo);
     std::swap(mSwapchainStatus, rhs.mSwapchainStatus);
@@ -679,7 +679,7 @@ angle::Result CommandProcessor::processTask(CommandProcessorTask *task)
 
             ANGLE_TRY(mCommandQueue->queueSubmitOneOff(
                 this, task->getProtectionType(), task->getPriority(),
-                task->getOneOffCommandBufferVk(), task->getOneOffWaitSemaphore(),
+                task->getOneOffCommandBuffer(), task->getOneOffWaitSemaphore(),
                 task->getOneOffWaitSemaphoreStageMask(), task->getOneOffFence(),
                 SubmitPolicy::EnsureSubmitted, task->getSubmitQueueSerial()));
             mNeedCommandsAndGarbageCleanup = true;
@@ -825,7 +825,7 @@ angle::Result CommandProcessor::enqueueSubmitCommands(
     Context *context,
     ProtectionType protectionType,
     egl::ContextPriority priority,
-    const VkSemaphore signalSemaphore,
+    VkSemaphore signalSemaphore,
     SecondaryCommandBufferList &&commandBuffersToReset,
     const QueueSerial &submitQueueSerial)
 {
@@ -847,9 +847,9 @@ angle::Result CommandProcessor::enqueueSubmitOneOffCommands(
     ProtectionType protectionType,
     egl::ContextPriority contextPriority,
     VkCommandBuffer commandBufferHandle,
-    const Semaphore *waitSemaphore,
+    VkSemaphore waitSemaphore,
     VkPipelineStageFlags waitSemaphoreStageMask,
-    const Fence *fence,
+    VkFence fence,
     SubmitPolicy submitPolicy,
     const QueueSerial &submitQueueSerial)
 {
@@ -1308,7 +1308,7 @@ angle::Result CommandQueue::flushRenderPassCommands(
 angle::Result CommandQueue::submitCommands(Context *context,
                                            ProtectionType protectionType,
                                            egl::ContextPriority priority,
-                                           const VkSemaphore signalSemaphore,
+                                           VkSemaphore signalSemaphore,
                                            SecondaryCommandBufferList &&commandBuffersToReset,
                                            const QueueSerial &submitQueueSerial)
 {
@@ -1341,7 +1341,7 @@ angle::Result CommandQueue::submitCommands(Context *context,
                                   signalSemaphore != VK_NULL_HANDLE || !waitSemaphores.empty();
     VkSubmitInfo submitInfo                   = {};
     VkProtectedSubmitInfo protectedSubmitInfo = {};
-    const Fence *fence                        = nullptr;
+    VkFence fence                             = VK_NULL_HANDLE;
 
     if (needsQueueSubmit)
     {
@@ -1362,7 +1362,7 @@ angle::Result CommandQueue::submitCommands(Context *context,
         }
 
         ANGLE_VK_TRY(context, batch.fence.init(context->getDevice(), &mFenceRecycler));
-        fence = &batch.fence.get();
+        fence = batch.fence.get().getHandle();
 
         ++mPerfCounters.vkQueueSubmitCallsTotal;
         ++mPerfCounters.vkQueueSubmitCallsPerFrame;
@@ -1383,9 +1383,9 @@ angle::Result CommandQueue::queueSubmitOneOff(Context *context,
                                               ProtectionType protectionType,
                                               egl::ContextPriority contextPriority,
                                               VkCommandBuffer commandBufferHandle,
-                                              const Semaphore *waitSemaphore,
+                                              VkSemaphore waitSemaphore,
                                               VkPipelineStageFlags waitSemaphoreStageMask,
-                                              const Fence *fence,
+                                              VkFence fence,
                                               SubmitPolicy submitPolicy,
                                               const QueueSerial &submitQueueSerial)
 {
@@ -1397,10 +1397,10 @@ angle::Result CommandQueue::queueSubmitOneOff(Context *context,
 
     // If caller passed in a fence, use it. Otherwise create an internal fence. That way if we can
     // go through normal waitForQueueSerial code path to wait for it to finish.
-    if (fence == nullptr)
+    if (fence == VK_NULL_HANDLE)
     {
         ANGLE_VK_TRY(context, batch.fence.init(context->getDevice(), &mFenceRecycler));
-        fence = &batch.fence.get();
+        fence = batch.fence.get().getHandle();
     }
 
     VkSubmitInfo submitInfo = {};
@@ -1423,10 +1423,10 @@ angle::Result CommandQueue::queueSubmitOneOff(Context *context,
         submitInfo.pCommandBuffers    = &commandBufferHandle;
     }
 
-    if (waitSemaphore != nullptr)
+    if (waitSemaphore != VK_NULL_HANDLE)
     {
         submitInfo.waitSemaphoreCount = 1;
-        submitInfo.pWaitSemaphores    = waitSemaphore->ptr();
+        submitInfo.pWaitSemaphores    = &waitSemaphore;
         submitInfo.pWaitDstStageMask  = &waitSemaphoreStageMask;
     }
 
@@ -1442,7 +1442,7 @@ angle::Result CommandQueue::queueSubmit(Context *context,
                                         std::unique_lock<std::mutex> &&dequeueLock,
                                         egl::ContextPriority contextPriority,
                                         const VkSubmitInfo &submitInfo,
-                                        const Fence *fence,
+                                        VkFence fence,
                                         DeviceScoped<CommandBatch> &commandBatch,
                                         const QueueSerial &submitQueueSerial)
 {
@@ -1466,9 +1466,8 @@ angle::Result CommandQueue::queueSubmit(Context *context,
 
     if (submitInfo.sType == VK_STRUCTURE_TYPE_SUBMIT_INFO)
     {
-        VkQueue queue       = getQueue(contextPriority);
-        VkFence fenceHandle = fence ? fence->getHandle() : VK_NULL_HANDLE;
-        ANGLE_VK_TRY(context, vkQueueSubmit(queue, 1, &submitInfo, fenceHandle));
+        VkQueue queue = getQueue(contextPriority);
+        ANGLE_VK_TRY(context, vkQueueSubmit(queue, 1, &submitInfo, fence));
     }
 
     mInFlightCommands.push(commandBatch.release());

src/libANGLE/renderer/vulkan/CommandProcessor.h
@@ -139,7 +139,7 @@ class CommandProcessorTask
                      const VkPresentInfoKHR &presentInfo,
                      SwapchainStatus *swapchainStatus);
 
-    void initFlushAndQueueSubmit(const VkSemaphore semaphore,
+    void initFlushAndQueueSubmit(VkSemaphore semaphore,
                                  ProtectionType protectionType,
                                  egl::ContextPriority priority,
                                  SecondaryCommandBufferList &&commandBuffersToReset,
@@ -148,9 +148,9 @@ class CommandProcessorTask
     void initOneOffQueueSubmit(VkCommandBuffer commandBufferHandle,
                                ProtectionType protectionType,
                                egl::ContextPriority priority,
-                               const Semaphore *waitSemaphore,
+                               VkSemaphore waitSemaphore,
                                VkPipelineStageFlags waitSemaphoreStageMask,
-                               const Fence *fence,
+                               VkFence fence,
                                const QueueSerial &submitQueueSerial);
 
     CommandProcessorTask &operator=(CommandProcessorTask &&rhs);
@@ -167,14 +167,17 @@ class CommandProcessorTask
     {
         return mWaitSemaphoreStageMasks;
     }
-    VkSemaphore getSemaphore() { return mSemaphore; }
+    VkSemaphore getSemaphore() const { return mSemaphore; }
     SecondaryCommandBufferList &getCommandBuffersToReset() { return mCommandBuffersToReset; }
     egl::ContextPriority getPriority() const { return mPriority; }
     ProtectionType getProtectionType() const { return mProtectionType; }
-    VkCommandBuffer getOneOffCommandBufferVk() const { return mOneOffCommandBufferVk; }
-    const Semaphore *getOneOffWaitSemaphore() { return mOneOffWaitSemaphore; }
-    VkPipelineStageFlags getOneOffWaitSemaphoreStageMask() { return mOneOffWaitSemaphoreStageMask; }
-    const Fence *getOneOffFence() { return mOneOffFence; }
+    VkCommandBuffer getOneOffCommandBuffer() const { return mOneOffCommandBuffer; }
+    VkSemaphore getOneOffWaitSemaphore() const { return mOneOffWaitSemaphore; }
+    VkPipelineStageFlags getOneOffWaitSemaphoreStageMask() const
+    {
+        return mOneOffWaitSemaphoreStageMask;
+    }
+    VkFence getOneOffFence() const { return mOneOffFence; }
     const VkPresentInfoKHR &getPresentInfo() const { return mPresentInfo; }
     SwapchainStatus *getSwapchainStatus() const { return mSwapchainStatus; }
     const RenderPass *getRenderPass() const { return mRenderPass; }
@@ -227,10 +230,10 @@ class CommandProcessorTask
     SwapchainStatus *mSwapchainStatus;
 
     // Used by OneOffQueueSubmit
-    VkCommandBuffer mOneOffCommandBufferVk;
-    const Semaphore *mOneOffWaitSemaphore;
+    VkCommandBuffer mOneOffCommandBuffer;
+    VkSemaphore mOneOffWaitSemaphore;
     VkPipelineStageFlags mOneOffWaitSemaphoreStageMask;
-    const Fence *mOneOffFence;
+    VkFence mOneOffFence;
 
     // Flush, Present & QueueWaitIdle data
     egl::ContextPriority mPriority;
@@ -380,7 +383,7 @@ class CommandQueue : angle::NonCopyable
     angle::Result submitCommands(Context *context,
                                  ProtectionType protectionType,
                                  egl::ContextPriority priority,
-                                 const VkSemaphore signalSemaphore,
+                                 VkSemaphore signalSemaphore,
                                  SecondaryCommandBufferList &&commandBuffersToReset,
                                  const QueueSerial &submitQueueSerial);
 
@@ -388,9 +391,9 @@ class CommandQueue : angle::NonCopyable
                                     ProtectionType protectionType,
                                     egl::ContextPriority contextPriority,
                                     VkCommandBuffer commandBufferHandle,
-                                    const Semaphore *waitSemaphore,
+                                    VkSemaphore waitSemaphore,
                                     VkPipelineStageFlags waitSemaphoreStageMask,
-                                    const Fence *fence,
+                                    VkFence fence,
                                     SubmitPolicy submitPolicy,
                                     const QueueSerial &submitQueueSerial);
 
@@ -463,7 +466,7 @@ class CommandQueue : angle::NonCopyable
                               std::unique_lock<std::mutex> &&dequeueLock,
                               egl::ContextPriority contextPriority,
                               const VkSubmitInfo &submitInfo,
-                              const Fence *fence,
+                              VkFence fence,
                               DeviceScoped<CommandBatch> &commandBatch,
                               const QueueSerial &submitQueueSerial);
 
@@ -544,7 +547,7 @@ class CommandProcessor : public Context
     angle::Result enqueueSubmitCommands(Context *context,
                                         ProtectionType protectionType,
                                         egl::ContextPriority priority,
-                                        const VkSemaphore signalSemaphore,
+                                        VkSemaphore signalSemaphore,
                                         SecondaryCommandBufferList &&commandBuffersToReset,
                                         const QueueSerial &submitQueueSerial);
 
@@ -554,9 +557,9 @@ class CommandProcessor : public Context
                                               ProtectionType protectionType,
                                               egl::ContextPriority contextPriority,
                                               VkCommandBuffer commandBufferHandle,
-                                              const Semaphore *waitSemaphore,
+                                              VkSemaphore waitSemaphore,
                                               VkPipelineStageFlags waitSemaphoreStageMask,
-                                              const Fence *fence,
+                                              VkFence fence,
                                               SubmitPolicy submitPolicy,
                                               const QueueSerial &submitQueueSerial);
     void enqueuePresent(egl::ContextPriority contextPriority,

src/libANGLE/renderer/vulkan/RendererVk.cpp
@@ -4658,17 +4658,20 @@ angle::Result RendererVk::queueSubmitOneOff(vk::Context *context,
     ANGLE_TRY(allocateScopedQueueSerialIndex(&index));
     QueueSerial submitQueueSerial(index.get(), generateQueueSerial(index.get()));
 
+    const VkSemaphore waitVkSemaphore = waitSemaphore ? waitSemaphore->getHandle() : VK_NULL_HANDLE;
+    const VkFence vkFence             = fence ? fence->getHandle() : VK_NULL_HANDLE;
+
     if (isAsyncCommandQueueEnabled())
     {
         ANGLE_TRY(mCommandProcessor.enqueueSubmitOneOffCommands(
-            context, protectionType, priority, primary.getHandle(), waitSemaphore,
-            waitSemaphoreStageMasks, fence, submitPolicy, submitQueueSerial));
+            context, protectionType, priority, primary.getHandle(), waitVkSemaphore,
+            waitSemaphoreStageMasks, vkFence, submitPolicy, submitQueueSerial));
     }
     else
     {
         ANGLE_TRY(mCommandQueue.queueSubmitOneOff(
-            context, protectionType, priority, primary.getHandle(), waitSemaphore,
-            waitSemaphoreStageMasks, fence, submitPolicy, submitQueueSerial));
+            context, protectionType, priority, primary.getHandle(), waitVkSemaphore,
+            waitSemaphoreStageMasks, vkFence, submitPolicy, submitQueueSerial));
     }
 
     *queueSerialOut = submitQueueSerial;
@@ -4685,21 +4688,23 @@ angle::Result RendererVk::queueSubmitOneOff(vk::Context *context,
 
 angle::Result RendererVk::queueSubmitWaitSemaphore(vk::Context *context,
                                                    egl::ContextPriority priority,
-                                                   const vk::Semaphore *waitSemaphore,
+                                                   const vk::Semaphore &waitSemaphore,
                                                    VkPipelineStageFlags waitSemaphoreStageMasks,
                                                    QueueSerial submitQueueSerial)
 {
     if (isAsyncCommandQueueEnabled())
     {
         ANGLE_TRY(mCommandProcessor.enqueueSubmitOneOffCommands(
-            context, vk::ProtectionType::Unprotected, priority, VK_NULL_HANDLE, waitSemaphore,
-            waitSemaphoreStageMasks, nullptr, vk::SubmitPolicy::AllowDeferred, submitQueueSerial));
+            context, vk::ProtectionType::Unprotected, priority, VK_NULL_HANDLE,
+            waitSemaphore.getHandle(), waitSemaphoreStageMasks, nullptr,
+            vk::SubmitPolicy::AllowDeferred, submitQueueSerial));
     }
     else
     {
         ANGLE_TRY(mCommandQueue.queueSubmitOneOff(
-            context, vk::ProtectionType::Unprotected, priority, VK_NULL_HANDLE, waitSemaphore,
-            waitSemaphoreStageMasks, nullptr, vk::SubmitPolicy::AllowDeferred, submitQueueSerial));
+            context, vk::ProtectionType::Unprotected, priority, VK_NULL_HANDLE,
+            waitSemaphore.getHandle(), waitSemaphoreStageMasks, nullptr,
+            vk::SubmitPolicy::AllowDeferred, submitQueueSerial));
     }
 
     return angle::Result::Continue;
@@ -5071,7 +5076,7 @@ angle::Result RendererVk::submitPriorityDependency(vk::Context *context,
     // Submit only Wait Semaphore into the destination Priority (VkQueue).
     QueueSerial queueSerial(index, generateQueueSerial(index));
     semaphore.get().setQueueSerial(queueSerial);
-    ANGLE_TRY(queueSubmitWaitSemaphore(context, dstContextPriority, &semaphore.get().get(),
+    ANGLE_TRY(queueSubmitWaitSemaphore(context, dstContextPriority, semaphore.get().get(),
                                        VK_PIPELINE_STAGE_ALL_COMMANDS_BIT, queueSerial));
 
     return angle::Result::Continue;

src/libANGLE/renderer/vulkan/RendererVk.h
@@ -308,7 +308,7 @@ class RendererVk : angle::NonCopyable
 
     angle::Result queueSubmitWaitSemaphore(vk::Context *context,
                                            egl::ContextPriority priority,
-                                           const vk::Semaphore *waitSemaphore,
+                                           const vk::Semaphore &waitSemaphore,
                                            VkPipelineStageFlags waitSemaphoreStageMasks,
                                            QueueSerial submitQueueSerial);
 