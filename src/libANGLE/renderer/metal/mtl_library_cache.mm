9c6059eae5939a9ef157e9a1ff81167579265225
Scott Violet <sky@chromium.org>
2023-03-21 13:08:27 -0700

    WIP: adds features to compile metal binaries, save to cache, and read from cache
    Bug: chromium:123456
    Change-Id: I745db15a7fbaa3787ba1a751655e3d27688e6ffc

--- src/libANGLE/renderer/metal/mtl_library_cache.mm
+++ src/libANGLE/renderer/metal/mtl_library_cache.mm
@@ -9,9 +9,19 @@
 
 #include "libANGLE/renderer/metal/mtl_library_cache.h"
 
+#include <stdio.h>
+
+#include <limits>
+
+#include "common/MemoryBuffer.h"
+#include "common/angleutils.h"
 #include "common/hash_utils.h"
 #include "common/mathutil.h"
+#include "common/string_utils.h"
+#include "common/system_utils.h"
 #include "libANGLE/renderer/metal/ContextMtl.h"
+#include "libANGLE/renderer/metal/mtl_context_device.h"
+#include "libANGLE/renderer/metal/process.h"
 
 namespace rx
 {
@@ -38,6 +48,114 @@ AutoObjCPtr<id<MTLLibrary>> LibraryCache::get(const std::string &source,
     }
 }
 
+namespace
+{
+
+// Reads a metallib file at the specified path.
+angle::MemoryBuffer ReadMetallibFromFile(const std::string &path)
+{
+    // TODO: optimize this to avoid the unnecessary strings.
+    std::string metallib;
+    if (!angle::ReadFileToString(path, &metallib))
+    {
+        FATAL() << "Failed reading back metallib";
+    }
+
+    angle::MemoryBuffer buffer;
+    if (!buffer.resize(metallib.size()))
+    {
+        FATAL() << "Failed to resize metallib buffer";
+    }
+    memcpy(buffer.data(), metallib.data(), metallib.size());
+    return buffer;
+}
+
+// Generates a key for the BlobCache based on the specified params.
+egl::BlobCacheKey GenerateBlobCacheKeyForShaderLibrary(
+    const std::string &source,
+    const std::map<std::string, std::string> &macros,
+    bool enableFastMath)
+{
+    angle::base::SecureHashAlgorithm sha1;
+    sha1.Update(source.c_str(), source.size());
+    const size_t macro_count = macros.size();
+    sha1.Update(&macro_count, sizeof(size_t));
+    for (const auto &macro : macros)
+    {
+        sha1.Update(macro.first.c_str(), macro.first.size());
+        sha1.Update(macro.second.c_str(), macro.second.size());
+    }
+    sha1.Update(&enableFastMath, sizeof(bool));
+    sha1.Final();
+    return sha1.DigestAsArray();
+}
+
+std::string BlobCacheKeyToString(const egl::BlobCacheKey &key)
+{
+  const char kUpCaseHexDigits[]   = "0123456789ABCDEF";
+    std::basic_ostringstream<char> stream;
+    for (size_t i = 0; i < key.size(); ++i)
+    {
+      int value = (int)key[i];
+      stream << kUpCaseHexDigits[value / 16];
+      stream << kUpCaseHexDigits[value % 16];
+        stream << "-";
+    }
+    return stream.str();
+}
+
+void AppendMetallibToFile(const egl::BlobCacheKey &cache_key, const angle::MemoryBuffer &buffer)
+{
+    static FILE* file = nullptr;
+    if (!file)
+    {
+        file = fopen("/tmp/shaders", "w");
+        if (!file)
+        {
+            FATAL() << "Unable to open file to save shaders to";
+        }
+    }
+    static int writeCount = 0;
+    if (buffer.size() > std::numeric_limits<uint32_t>::max() ||
+        cache_key.size() > std::numeric_limits<uint32_t>::max()) {
+      FATAL() << "Shader key and/or buffer size is too large";
+    }
+    printf("writing shader key %s\n", BlobCacheKeyToString(cache_key).c_str());
+    const uint32_t key_length   = static_cast<uint32_t>(cache_key.size());
+    const uint32_t value_length = static_cast<uint32_t>(buffer.size());
+    if (fwrite(&key_length, sizeof(key_length), 1, file) != 1 ||
+        fwrite(cache_key.data(), sizeof(egl::BlobCacheKey::value_type), cache_key.size(), file) !=
+            cache_key.size() ||
+        fwrite(&value_length, sizeof(value_length), 1, file) != 1 ||
+        fwrite(buffer.data(), sizeof(char), buffer.size(), file) != buffer.size())
+    {
+        FATAL() << "Failed writing shader to file";
+    }
+    // Flush as we won't have a change to close the file.
+    fflush(file);
+    printf("Wrote another metallib %d\n", ++writeCount);
+}
+
+// Returns a new MTLLibrary from the specified data.
+AutoObjCPtr<id<MTLLibrary>> NewMetalLibraryFromMetallib(ContextMtl *context,
+                                                        const uint8_t *data,
+                                                        size_t size)
+{
+    ANGLE_MTL_OBJC_SCOPE
+    {
+        // DISPATCH_DATA_DESTRUCTOR_DEFAULT makes a copy. I'm assuming we want that
+        // as the life of the BlobCache is not necessarily the same as that of the
+        // metallibrary.
+        auto mtl_data = dispatch_data_create(data, size, dispatch_get_main_queue(),
+                                             DISPATCH_DATA_DESTRUCTOR_DEFAULT);
+
+        NSError *nsError = nil;
+        return context->getMetalDevice().newLibraryWithData(mtl_data, &nsError);
+    }
+}
+
+}  // namespace
+
 AutoObjCPtr<id<MTLLibrary>> LibraryCache::getOrCompileShaderLibrary(
     ContextMtl *context,
     const std::string &source,
@@ -58,13 +176,62 @@ AutoObjCPtr<id<MTLLibrary>> LibraryCache::getOrCompileShaderLibrary(
     // Lock this cache entry while compiling the shader. This causes other threads calling this
     // function to wait and not duplicate the compilation.
     std::lock_guard<std::mutex> entryLockGuard(entry.lock);
-
-    if (!entry.library)
+    if (entry.library)
     {
-        entry.library = CreateShaderLibrary(context->getMetalDevice(), source, macros,
-                                            enableFastMath, errorOut);
+        return entry.library;
     }
 
+    if (features.compileMetalShaders.enabled)
+    {
+        if (features.enableParallelMtlLibraryCompilation.enabled)
+        {
+            // When enableParallelMtlLibraryCompilation is enabled, compilation happens in the
+            // background. Chrome's ProgramCache only saves to disk when called at certain points,
+            // which are not present when compiling in the background.
+            FATAL() << "EnableParallelMtlLibraryCompilation is not compatible with "
+                       "compileMetalShdaders";
+        }
+        std::string metallib_filename = CompileShaderLibraryToFile(source, macros, enableFastMath);
+        angle::MemoryBuffer memory_buffer = ReadMetallibFromFile(metallib_filename);
+        entry.library =
+            NewMetalLibraryFromMetallib(context, memory_buffer.data(), memory_buffer.size());
+        auto cache_key = GenerateBlobCacheKeyForShaderLibrary(source, macros, enableFastMath);
+        if (features.generateMetalShadersFile.enabled)
+        {
+            printf("XXX adding to shader cache %s\n", BlobCacheKeyToString(cache_key).c_str());
+            AppendMetallibToFile(cache_key, memory_buffer);
+        }
+        else
+        {
+            printf("XXX adding to cache %s\n", BlobCacheKeyToString(cache_key).c_str());
+            context->getDisplay()->getBlobCache()->put(cache_key, std::move(memory_buffer));
+            printf("XXX added\n");
+        }
+        return entry.library;
+    }
+
+    if (features.loadMetalShadersFromBlobCache.enabled)
+    {
+        WARN() << "Trying to load metallib from BlobCache";
+        auto cache_key = GenerateBlobCacheKeyForShaderLibrary(source, macros, enableFastMath);
+        egl::BlobCache::Value value;
+        size_t buffer_size;
+        angle::ScratchBuffer scratch_buffer;
+        if (context->getDisplay()->getBlobCache()->get(&scratch_buffer, cache_key, &value,
+                                                       &buffer_size))
+        {
+            entry.library = NewMetalLibraryFromMetallib(context, value.data(), value.size());
+            if (entry.library)
+            {
+                WARN() << "Found metallib in BlobCache";
+                printf("XXX found key in cache %s\n", BlobCacheKeyToString(cache_key).c_str());
+                return entry.library;
+            }
+        }
+    }
+
+    entry.library =
+        CreateShaderLibrary(context->getMetalDevice(), source, macros, enableFastMath, errorOut);
     return entry.library;
 }
 
