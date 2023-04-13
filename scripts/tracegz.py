0a76dca00492c38d5ef81a8b912d7dacccf95257
Roman Lavrov <romanl@google.com>
2023-04-13 15:16:53 -0400

    Trace Interpreter: gzip cpp and run gold tests.
    New test step:
    angle_restricted_trace_gold_interpreted_tests
    similar to existing
    angle_restricted_trace_gold_tests
    
    but runs a subset of newest traces that have the right format.
    Currently there is 9 of them and the filter is hard-coded in
    test_suites.pyl.
    
    Trace cpp files are concatenated and gzipped
    as a build step producing gen/tracegz_{trace}.gz
    and these gz files are then distributed as part of the build
    
    --trace-interpreter flag changes from boolean to string to support:
    --trace-interpreter=gz:
    * this uses the gz file above
    * gold tests added in this CL use this option
    
    --trace-interpreter=c:
    * using uncompress c/cpp file
    * existing angle_trace_interpreter_tests runs a retrace
      saving .c files and uses this option
    
    Bug: b/276742336
    Change-Id: I69544f25bda873af191978195d02ffbdd34363c0

---
+++ scripts/tracegz.py
@@ -0,0 +1,19 @@
+import sys
+import os
+import gzip
+import json
+
+trace_path = os.path.join('..', '..', 'src', 'tests', 'restricted_traces', sys.argv[1])
+
+jpath = os.path.join(trace_path, sys.argv[1] + '.json')
+with open(jpath, 'rb') as f:
+    jcont = f.read()
+
+j = json.loads(jcont)
+
+with open('gen/tracegz_' + sys.argv[1] + '.gz', 'wb') as f:
+    with gzip.GzipFile(fileobj=f, mode='wb', compresslevel=9, mtime=0) as fgzip:
+        for fn in j['TraceFiles']:
+            if fn.endswith('.cpp'):
+                with open(os.path.join(trace_path, fn), 'rb') as fcpp:
+                    fgzip.write(fcpp.read())
