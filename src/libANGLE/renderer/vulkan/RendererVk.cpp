9864352397d9e921aeb4c8e78768eb5fdaeb6bcb
angle-autoroll <angle-autoroll@skia-public.iam.gserviceaccount.com>
2023-04-19 12:34:56 +0000

    Roll vulkan-deps from c8e5e8c4c60b to 9403c35f9956 (31 revisions)
    https://chromium.googlesource.com/vulkan-deps.git/+log/c8e5e8c4c60b..9403c35f9956
    
    Suppress new VUIDs:
        "VUID-vkCmdDraw-None-02859",
        "VUID-vkCmdDrawIndexed-None-02859"
    
    Changed dependencies:
    * spirv-tools: https://chromium.googlesource.com/external/github.com/KhronosGroup/SPIRV-Tools.git/+log/f449fb4ad9..25ad5e19f1
    * vulkan-loader: https://chromium.googlesource.com/external/github.com/KhronosGroup/Vulkan-Loader.git/+log/8c8619df2d..f035e57c17
    * vulkan-tools: https://chromium.googlesource.com/external/github.com/KhronosGroup/Vulkan-Tools.git/+log/4e17c284d6..db7475424c
    * vulkan-validation-layers: https://chromium.googlesource.com/external/github.com/KhronosGroup/Vulkan-ValidationLayers.git/+log/7b50c36e7a..50c2f9e71a
    
    If this roll has caused a breakage, revert this CL and stop the roller
    using the controls here:
    https://autoroll.skia.org/r/vulkan-deps-angle-autoroll
    Please CC angle-team@google.com,jonahr@google.com on the revert to
    ensure that a human
    is aware of the problem.
    
    To file a bug in ANGLE:
    https://bugs.chromium.org/p/angleproject/issues/entry
    
    To report a problem with the AutoRoller itself, please file a bug:
    https://bugs.chromium.org/p/skia/issues/entry?template=Autoroller+Bug
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+doc/main/autoroll/README.md
    
    Bug: angleproject:8119
    Change-Id: I8cb9bd3b593f24af130d9239c35fb119ed6bb41b

--- src/libANGLE/renderer/vulkan/RendererVk.cpp
+++ src/libANGLE/renderer/vulkan/RendererVk.cpp
@@ -233,6 +233,8 @@ constexpr const char *kSkippedMessages[] = {
     // http://anglebug.com/8119
     "VUID-VkGraphicsPipelineCreateInfo-Input-07905",
     "UNASSIGNED-CoreValidation-Shader-VertexInputMismatch",
+    "VUID-vkCmdDraw-None-02859",
+    "VUID-vkCmdDrawIndexed-None-02859",
 };
 
 // Validation messages that should be ignored only when VK_EXT_primitive_topology_list_restart is
