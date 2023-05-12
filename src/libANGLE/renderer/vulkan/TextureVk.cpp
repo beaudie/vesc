0dfaec24fc03124b3063aa8fa833541e900e862f
Jason Macnak <natsu@google.com>
2023-05-12 08:38:19 -0700

    Vulkan: Move texture QFOTs to syncState()
    ... so that QFOTs are not actually scheduled until first use.
    SurfaceFlinger optimistically creates EGL images and textures for
    AHBs in case it does need them in the future. However, the images
    and textures may go unused. Prior to this change, ANGLE would
    create pending QFOT barriers while importing AHBs into EGL images
    and GL textures. However, SurfaceFlinger may not be doing any
    "real work" (other than repeatedly creating and destroying EGL
    images and GL textures) which would result in the command buffers
    containing the QFOTs being flushed. This can result in a large
    build up of unreleased memory (as the VkDeviceMemory would still
    be kept alive by the reference from the unflushed QFOT command
    buffer) and lead to the low memory killer nuking processes.
    
    Bug: b/282075554
    Test: cts -m CtsOpenGLTestCases
              -t android.opengl.cts.GLSurfaceViewTest
    Test: adb shell dumpsys SurfaceFlinger
    Test: angle_end2end_tests
              --gtest_filter=ImageTestES3.AHBImportReleaseStress/ES3_Vulkan
    Change-Id: I7776abb2c6f834e96aa3926c26e77c53352ee561

--- src/libANGLE/renderer/vulkan/TextureVk.cpp
+++ src/libANGLE/renderer/vulkan/TextureVk.cpp
@@ -1520,35 +1520,6 @@ angle::Result TextureVk::setEGLImageTarget(const gl::Context *context,
 
     ANGLE_TRY(initImageViews(contextVk, getImageViewLevelCount()));
 
-    // Transfer the image to this queue if needed
-    uint32_t rendererQueueFamilyIndex = renderer->getQueueFamilyIndex();
-    if (mImage->isQueueChangeNeccesary(rendererQueueFamilyIndex))
-    {
-        vk::ImageLayout newLayout = vk::ImageLayout::AllGraphicsShadersWrite;
-        if (mImage->getUsage() & VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT)
-        {
-            newLayout = vk::ImageLayout::ColorWrite;
-        }
-        else if (mImage->getUsage() & VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT)
-        {
-            newLayout = vk::ImageLayout::DepthWriteStencilWrite;
-        }
-        else if (mImage->getUsage() &
-                 (VK_IMAGE_USAGE_SAMPLED_BIT | VK_IMAGE_USAGE_INPUT_ATTACHMENT_BIT))
-        {
-            newLayout = vk::ImageLayout::AllGraphicsShadersReadOnly;
-        }
-
-        vk::OutsideRenderPassCommandBuffer *commandBuffer;
-        vk::CommandBufferAccess access;
-        access.onExternalAcquireRelease(mImage);
-        ANGLE_TRY(contextVk->getOutsideRenderPassCommandBuffer(access, &commandBuffer));
-        mImage->changeLayoutAndQueue(contextVk, mImage->getAspectFlags(), newLayout,
-                                     rendererQueueFamilyIndex, commandBuffer);
-
-        ANGLE_TRY(contextVk->onEGLImageQueueChange());
-    }
-
     return angle::Result::Continue;
 }
 
@@ -2862,6 +2833,34 @@ angle::Result TextureVk::syncState(const gl::Context *context,
         mBufferViews.init(renderer, offset, size);
         mDescriptorSetCacheManager.releaseKeys(contextVk);
         return angle::Result::Continue;
+    }
+
+    // Transfer the image to this queue if needed.
+    const uint32_t rendererQueueFamilyIndex = renderer->getQueueFamilyIndex();
+    if (mImage->valid() && mImage->isQueueChangeNeccesary(rendererQueueFamilyIndex))
+    {
+        vk::ImageLayout newLayout = vk::ImageLayout::AllGraphicsShadersWrite;
+        if (mImage->getUsage() & VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT)
+        {
+            newLayout = vk::ImageLayout::ColorWrite;
+        }
+        else if (mImage->getUsage() & VK_IMAGE_USAGE_DEPTH_STENCIL_ATTACHMENT_BIT)
+        {
+            newLayout = vk::ImageLayout::DepthWriteStencilWrite;
+        }
+        else if (mImage->getUsage() &
+                 (VK_IMAGE_USAGE_SAMPLED_BIT | VK_IMAGE_USAGE_INPUT_ATTACHMENT_BIT))
+        {
+            newLayout = vk::ImageLayout::AllGraphicsShadersReadOnly;
+        }
+
+        vk::OutsideRenderPassCommandBuffer *commandBuffer;
+        vk::CommandBufferAccess access;
+        access.onExternalAcquireRelease(mImage);
+        ANGLE_TRY(contextVk->getOutsideRenderPassCommandBuffer(access, &commandBuffer));
+        mImage->changeLayoutAndQueue(contextVk, mImage->getAspectFlags(), newLayout,
+                                     rendererQueueFamilyIndex, commandBuffer);
+        ANGLE_TRY(contextVk->onEGLImageQueueChange());
     }
 
     ANGLE_TRY(respecifyImageStorageIfNecessary(contextVk, source));
