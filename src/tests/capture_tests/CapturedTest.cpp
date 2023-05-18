6eae954334cac1e44d2284c651074687aba40f4f
Roman Lavrov <romanl@google.com>
2023-05-18 13:52:29 -0400

    Capture/Replay: Initial setup for angle_capture_tests
    Adds a basic test (CapturedTest) with a few frames. This test gets
    captured by capture_tests.py into a temporary directory and the
    resulting files are diff'ed with the files under expected/
    
    A diff fails the test. When capture changes, the workflow would be to
    run the command indicated by the error message in the test which will
    overwrite the files with new ones so that they can be added to the CL.
    
    Bug: b/286067106
    Change-Id: I7d5f6eed088d84f9e3eb8a72b24b1d92515fff38

---
+++ src/tests/capture_tests/CapturedTest.cpp
@@ -0,0 +1,112 @@
+//
+// Copyright 2023 The ANGLE Project Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+
+#include "test_utils/ANGLETest.h"
+
+#include "test_utils/gl_raii.h"
+#include "util/random_utils.h"
+#include "util/shader_utils.h"
+#include "util/test_utils.h"
+
+using namespace angle;
+
+namespace
+{
+class CapturedTest : public ANGLETest<>
+{
+  protected:
+    CapturedTest()
+    {
+        setWindowWidth(128);
+        setWindowHeight(128);
+        setConfigRedBits(8);
+        setConfigGreenBits(8);
+        setConfigBlueBits(8);
+        setConfigAlphaBits(8);
+        setConfigDepthBits(24);
+        setConfigStencilBits(8);
+    }
+
+    void testSetUp() override
+    {
+        // Captured as the first frame
+
+        mFBOs.resize(2, 0);
+        glGenFramebuffers(2, mFBOs.data());
+
+        ASSERT_GL_NO_ERROR();
+    }
+
+    void testTearDown() override
+    {
+        // Captured as the last frame
+
+        if (!mFBOs.empty())
+        {
+            glDeleteFramebuffers(static_cast<GLsizei>(mFBOs.size()), mFBOs.data());
+        }
+    }
+
+    void frame2();
+    void frame3();
+
+    std::vector<GLuint> mFBOs;
+};
+
+void CapturedTest::frame2()
+{
+    glClearColor(0.25f, 0.5f, 0.5f, 0.5f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    EXPECT_PIXEL_NEAR(0, 0, 64, 128, 128, 128, 1.0);
+}
+
+void CapturedTest::frame3()
+{
+    constexpr char kVS[] = R"(precision highp float;
+attribute vec3 attr1;
+void main(void) {
+   gl_Position = vec4(attr1, 1.0);
+})";
+
+    constexpr char kFS[] = R"(precision highp float;
+void main(void) {
+   gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
+})";
+
+    GLBuffer emptyBuffer;
+    glBindBuffer(GL_ARRAY_BUFFER, emptyBuffer);
+
+    ANGLE_GL_PROGRAM(program, kVS, kFS);
+    glBindAttribLocation(program, 0, "attr1");
+    glLinkProgram(program);
+    ASSERT_TRUE(CheckLinkStatusAndReturnProgram(program, true));
+    glUseProgram(program);
+
+    // Use non-existing attribute 1.
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 3, GL_UNSIGNED_BYTE, false, 1, 0);
+
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+    EXPECT_GL_NO_ERROR();
+}
+
+// Test captured by capture_tests.py
+TEST_P(CapturedTest, MultiFrame)
+{
+    // Swap before the first frame so that setup gets its own frame
+    swapBuffers();
+    frame2();
+
+    swapBuffers();
+    frame3();
+
+    // Test teardown gets its own frame without a swap here
+}
+
+GTEST_ALLOW_UNINSTANTIATED_PARAMETERIZED_TEST(CapturedTest);
+ANGLE_INSTANTIATE_TEST_ES3(CapturedTest);
+
+}  // anonymous namespace
