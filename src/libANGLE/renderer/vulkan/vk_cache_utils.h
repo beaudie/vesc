6a51ca0c5fcdd4caddeea6fba42d49186e61e800
Charlie Lao <cclao@google.com>
2023-05-19 12:32:47 -0700

    Debug: Debug
    Bug: b/282194402
    Change-Id: I1767bf5b515834994255740047c8b03dd7fbea7e

--- src/libANGLE/renderer/vulkan/vk_cache_utils.h
+++ src/libANGLE/renderer/vulkan/vk_cache_utils.h
@@ -1640,6 +1640,12 @@ class DescriptorSetDesc
         return (mDescriptorInfos == other.mDescriptorInfos);
     }
 
+    size_t size() const { return mDescriptorInfos.size(); }
+    const DescriptorInfoDesc &getInfoDesc(uint32_t infoDescIndex) const
+    {
+        return mDescriptorInfos[infoDescIndex];
+    }
+
     void updateInfoDesc(uint32_t infoDescIndex, const DescriptorInfoDesc &infoDesc)
     {
         mDescriptorInfos[infoDescIndex] = infoDesc;
@@ -1718,7 +1724,7 @@ class DescriptorSetDescBuilder final
                               TransformFeedbackVk *transformFeedbackVk);
 
     // Specific helpers for shader resource descriptors.
-    void updateShaderBuffers(gl::ShaderType shaderType,
+    bool updateShaderBuffers(gl::ShaderType shaderType,
                              ShaderVariableType variableType,
                              const ShaderInterfaceVariableInfoMap &variableInfoMap,
                              const gl::BufferVector &buffers,
