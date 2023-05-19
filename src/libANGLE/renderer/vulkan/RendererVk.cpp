4cd306457469f4bf4f728c0089ca27782402dea4
Amirali Abdolrashidi <abdolrashidi@google.com>
2023-05-19 12:46:43 -0700

    Temp: Remove device-local flag
    Bug: None
    Change-Id: If45bb2e8c43fd306c7a38ed1b58817776dee0015

--- src/libANGLE/renderer/vulkan/RendererVk.cpp
+++ src/libANGLE/renderer/vulkan/RendererVk.cpp
@@ -5598,9 +5598,8 @@ VkResult ImageMemorySuballocator::allocateAndBindMemory(Context *context,
     // Allocate and bind memory for the image. Try allocating on the device first. If unsuccessful,
     // it is possible to retry allocation.
     VkResult result = vma::AllocateAndBindMemoryForImage(
-        allocator.getHandle(), &image->mHandle, requiredFlags | VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
-        preferredFlags, allocateDedicatedMemory, &allocationOut->mHandle, memoryTypeIndexOut,
-        sizeOut);
+        allocator.getHandle(), &image->mHandle, requiredFlags, preferredFlags,
+        allocateDedicatedMemory, &allocationOut->mHandle, memoryTypeIndexOut, sizeOut);
 
     if (result != VK_SUCCESS)
     {
