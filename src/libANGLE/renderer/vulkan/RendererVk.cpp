19e79684311f8899e218f67191da43a1aad2f7dd
Amirali Abdolrashidi <abdolrashidi@google.com>
2023-05-19 11:51:04 -0700

    Vulkan: Free the garbage memory before realloc
    * Updated the allocation logic so we will try cleaning the garbage
      from the renderer before retrying the allocation.
    
    Bug: b/280304441
    Change-Id: Ia5e605e180833b44af8c77550ab1b0b8ba21724e

--- src/libANGLE/renderer/vulkan/RendererVk.cpp
+++ src/libANGLE/renderer/vulkan/RendererVk.cpp
@@ -5595,10 +5595,21 @@ VkResult ImageMemorySuballocator::allocateAndBindMemory(Context *context,
     bool allocateDedicatedMemory =
         memoryRequirements.size >= kImageSizeThresholdForDedicatedMemoryAllocation;
 
-    // Allocate and bind memory for the image.
+    // Allocate and bind memory for the image. Try allocating on the device first. If unsuccessful,
+    // it is possible to retry allocation after cleaning the garbage.
     VkResult result = vma::AllocateAndBindMemoryForImage(
-        allocator.getHandle(), &image->mHandle, requiredFlags, preferredFlags,
-        allocateDedicatedMemory, &allocationOut->mHandle, memoryTypeIndexOut, sizeOut);
+        allocator.getHandle(), &image->mHandle, requiredFlags | VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
+        preferredFlags, allocateDedicatedMemory, &allocationOut->mHandle, memoryTypeIndexOut,
+        sizeOut);
+
+    if (result != VK_SUCCESS)
+    {
+        renderer->cleanupGarbage();
+        result = vma::AllocateAndBindMemoryForImage(
+            allocator.getHandle(), &image->mHandle, requiredFlags, preferredFlags,
+            allocateDedicatedMemory, &allocationOut->mHandle, memoryTypeIndexOut, sizeOut);
+    }
+
     if (result != VK_SUCCESS)
     {
         // Record the failed memory allocation.
