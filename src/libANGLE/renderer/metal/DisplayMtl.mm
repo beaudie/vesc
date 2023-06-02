99a659dec460b278323d81cde37b53d911b77186
Maksim Sisov <msisov@igalia.com>
2023-06-02 13:34:46 +0300

    metal: fix render to texture support
    Bug: angleproject:3107, chromium:1424910
    Change-Id: Ib55a2caf558d4a4d3a34edc84fdfe2408cc8a80d

--- src/libANGLE/renderer/metal/DisplayMtl.mm
+++ src/libANGLE/renderer/metal/DisplayMtl.mm
@@ -981,16 +981,14 @@ void DisplayMtl::initializeExtensions() const
     // TODO(anglebug.com/6395): figure out why WebGL drawing buffer
     // creation fails on macOS when the Metal backend advertises the
     // EXT_multisampled_render_to_texture extension.
-    // TODO(anglebug.com/3107): Metal doesn't implement render to texture
-    // correctly. A texture (if used as a color attachment for a framebuffer)
-    // is always created with sample count == 1, which results in creation of a
-    // render pipeline with the same value. Moreover, if there is a more
-    // sophisticated case and a framebuffer also has a stencil/depth attachment,
-    // it will result in creation of a render pipeline with those attachment's
-    // sample count, but the texture that was used as a color attachment, will
-    // still remain with sample count 1. That results in Metal validation error
-    // if enabled.
-    mNativeExtensions.multisampledRenderToTextureEXT = false;
+    // #if !defined(ANGLE_PLATFORM_MACOS)
+    // EXT_multisampled_render_to_texture
+    if (mFeatures.allowMultisampleStoreAndResolve.enabled &&
+        mFeatures.hasDepthAutoResolve.enabled && mFeatures.hasStencilAutoResolve.enabled)
+    {
+        mNativeExtensions.multisampledRenderToTextureEXT = true;
+    }
+    // #endif
 
     // Enable EXT_blend_minmax
     mNativeExtensions.blendMinmaxEXT = true;
@@ -1242,7 +1240,7 @@ void DisplayMtl::initializeFeatures()
     ANGLE_FEATURE_CONDITION((&mFeatures), hasDepthAutoResolve, supportsEitherGPUFamily(3, 2));
     ANGLE_FEATURE_CONDITION((&mFeatures), hasStencilAutoResolve, supportsEitherGPUFamily(5, 2));
     ANGLE_FEATURE_CONDITION((&mFeatures), allowMultisampleStoreAndResolve,
-                            supportsEitherGPUFamily(3, 1));
+                            supportsEitherGPUFamily(4, 0));
 
     ANGLE_FEATURE_CONDITION((&mFeatures), allowRuntimeSamplerCompareMode,
                             supportsEitherGPUFamily(3, 1));
