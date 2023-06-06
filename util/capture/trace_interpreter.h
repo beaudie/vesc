eb4f258c5c783a4f43c8066a5d8eca3a060bb8ae
Roman Lavrov <romanl@google.com>
2023-06-06 15:36:36 -0400

    Add trace_interface: functions and callbacks for traces
    Defines the interface between the test suite
    (or an other TraceLibrary class user) and trace libraries.
    
    TraceFunctions defines entry points for calls suite->trace, such as
    SetupReplay() or SetBinaryDataDir().
    
    TraceCallbacks defines entry points for calls trace->suite, for example
    for loading .angledata.gz files.
    
    These are set up via the exported SetupEntryPoints() call. Functions
    like SetupReplay etc no longer need to be exported from the trace
    library.
    
    TraceInfo (parsed representation of the trace json) is moved to
    trace_interface as is. This is convenient for further changes to the
    fixture that will allow to easily move some of the captured parameters
    to json.
    
    This also moves Decompress functionality (and memory ownership) to test
    suite entirely, which avoids Decompress/Delete callbacks - the trace
    just calls LoadBinaryData via TraceCallbacks and TraceLibrary releases
    the memory either on FinishReplay or in its destructor.
    This should also take care of the memory leak described in
    https://crrev.com/c/3858185
    
    Bug: b/286072760
    Change-Id: Ibc6f6f64156ad805b1917c8fc41a3b0d2c0d6375

--- util/capture/trace_interpreter.h
+++ util/capture/trace_interpreter.h
@@ -287,10 +287,10 @@ ParamBuffer ParseParameters(const Token *paramTokens, const TraceStringMap &stri
 }  // namespace angle
 
 extern "C" {
-ANGLE_REPLAY_EXPORT void SetupReplay();
-ANGLE_REPLAY_EXPORT void ReplayFrame(uint32_t frameIndex);
-ANGLE_REPLAY_EXPORT void ResetReplay();
-ANGLE_REPLAY_EXPORT const char *GetSerializedContextState(uint32_t frameIndex);
+void SetupReplay();
+void ReplayFrame(uint32_t frameIndex);
+void ResetReplay();
+const char *GetSerializedContextState(uint32_t frameIndex);
 }  // extern "C"
 
 #endif  // ANGLE_TRACE_INTERPRETER_H_
