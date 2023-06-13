9ffd7002f3a3ae6eff790c83b29a1c74db1a7cda
Mike Schuchardt <mikes@lunarg.com>
2023-06-13 15:11:17 -0700

    Android: Add Galaxy A34, A54 and Oppo Find X6
    Adds device detection for use in expectation files
    
    Bug: b/285047631
    Bug: b/285047394
    Bug: b/285630872
    Change-Id: I30361d7bc1a0a08441c8b5afe64d4b5f22ae4ddb

--- src/tests/test_expectations/GPUTestExpectationsParser.cpp
+++ src/tests/test_expectations/GPUTestExpectationsParser.cpp
@@ -78,7 +78,10 @@ enum Token
     kConfigPixel4,
     kConfigPixel6,
     kConfigGalaxyA23,
+    kConfigGalaxyA34,
+    kConfigGalaxyA54,
     kConfigGalaxyS23,
+    kConfigFindX6,
     // GPU devices
     kConfigNVIDIAQuadroP400,
     kConfigNVIDIAGTX1660,
@@ -187,7 +190,10 @@ constexpr TokenInfo kTokenData[kNumberOfTokens] = {
     {"pixel4orxl", GPUTestConfig::kConditionPixel4OrXL},
     {"pixel6", GPUTestConfig::kConditionPixel6},
     {"galaxya23", GPUTestConfig::kConditionGalaxyA23},
+    {"galaxya34", GPUTestConfig::kConditionGalaxyA34},
+    {"galaxya54", GPUTestConfig::kConditionGalaxyA54},
     {"galaxys23", GPUTestConfig::kConditionGalaxyS23},
+    {"findx6", GPUTestConfig::kConditionFindX6},
     {"quadrop400", GPUTestConfig::kConditionNVIDIAQuadroP400},
     {"gtx1660", GPUTestConfig::kConditionNVIDIAGTX1660},
     {"prerotation", GPUTestConfig::kConditionPreRotation},
@@ -509,7 +515,10 @@ bool GPUTestExpectationsParser::parseLine(const GPUTestConfig *config,
             case kConfigPixel4:
             case kConfigPixel6:
             case kConfigGalaxyA23:
+            case kConfigGalaxyA34:
+            case kConfigGalaxyA54:
             case kConfigGalaxyS23:
+            case kConfigFindX6:
             case kConfigNVIDIAQuadroP400:
             case kConfigNVIDIAGTX1660:
             case kConfigPreRotation:
