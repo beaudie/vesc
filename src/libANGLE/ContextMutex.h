064bfa74ba561aa2eb5257feac9394b7e9326a11
Igor Nazarov <i.nazarov@samsung.com>
2023-06-20 20:23:36 +0300

    Remove TypedContexMutex<> class
    This CL removes `TypedContexMutex<>` class and so the ability to change
    mutex type when merging mutexes. This is a trade off for performance.
    
    Mutex type is defined in "SharedContextMutex.h":
    - ContextMutexType = angle::FastMutex1
    
    Performance test results for 1000'000 `glGetError()` calls on S906B
    with locked CPU frequencies:
    
            Before this CL:
        TypedContextMutex<angle::FastMutex1>  12.984 ms
        TypedContextMutex<std::mutex>         28.312 ms
            After this CL:
        ContextMutex (angle::FastMutex1)      12.303 ms
    
    Result is 5.2% better performance (12.303 vs 12.984).
    
    Bug: chromium:1454978
    Bug: chromium:1454981
    Change-Id: I4fedce56671d255e8007dc7993022d04e2fdbe02

--- src/libANGLE/ContextMutex.h
+++ src/libANGLE/ContextMutex.h
@@ -10,6 +10,7 @@
 
 #include <atomic>
 
+#include "common/FastMutex.h"
 #include "common/debug.h"
 
 namespace gl
@@ -25,12 +26,13 @@ constexpr bool kIsContextMutexEnabled = true;
 constexpr bool kIsContextMutexEnabled = false;
 #endif
 
-class ContextMutex : angle::NonCopyable
+using ContextMutexType = angle::FastMutex1;
+
+class ContextMutex final : angle::NonCopyable
 {
   public:
-    explicit ContextMutex(uint32_t priority);
     explicit ContextMutex(ContextMutex *root = nullptr);
-    virtual ~ContextMutex();
+    ~ContextMutex();
 
     // Merges mutexes so they work as one.
     // At the end, only single "root" mutex will be locked.
@@ -53,24 +55,13 @@ class ContextMutex : angle::NonCopyable
     ANGLE_INLINE void releaseAndUnlock() { release(UnlockBehaviour::kUnlock); }
     ANGLE_INLINE bool isReferenced() const { return mRefCount > 0; }
 
-    ANGLE_INLINE uint32_t getPriority() const { return mPriority; }
+    bool try_lock();
+    void lock();
+    void unlock();
 
-    ANGLE_INLINE bool try_lock() { return getRoot()->doTryLock(); }
-    ANGLE_INLINE void lock() { getRoot()->doLock(); }
-    ANGLE_INLINE void unlock()
-    {
-        ContextMutex *const root = getRoot();
-        // "root" is currently locked so "root->getRoot()" will return stable result.
-        ASSERT(root == root->getRoot());
-        root->doUnlock();
-    }
-
-    virtual bool doTryLock() = 0;
-    virtual void doLock()    = 0;
-    virtual void doUnlock()  = 0;
-
-  protected:
-    virtual uint32_t getLockLevel() const = 0;
+    bool doTryLock();
+    void doLock();
+    void doUnlock();
 
   private:
     enum class UnlockBehaviour
@@ -140,11 +131,15 @@ class ContextMutex : angle::NonCopyable
     // - minumum number of mutexes to reach mOldRoots size of N => 2^(N+1).
 
     std::atomic<ContextMutex *> mRoot;
+    ContextMutexType mMutex;
+    // Used when ASSERT() and/or recursion are/is enabled.
+    std::atomic<angle::ThreadId> mOwnerThreadId;
+    // Used only when recursion is enabled.
+    uint32_t mLockLevel;
     size_t mRefCount;
 
     std::set<ContextMutex *> mLeaves;
     std::vector<ContextMutex *> mOldRoots;
-    const uint32_t mPriority;
     uint32_t mRank;
 };
 
@@ -211,35 +206,6 @@ class [[nodiscard]] ScopedContextMutexLock final
 
   private:
     ContextMutex *mMutex = nullptr;
-};
-
-template <class Mutex>
-class TypedContextMutex final : public ContextMutex
-{
-  public:
-    explicit TypedContextMutex(uint32_t priority);
-    explicit TypedContextMutex(ContextMutex *root = nullptr);
-    ~TypedContextMutex() override;
-
-    // Note: most of the code here may be refactored to the base class, or most of the state
-    // variables moved to this implementation. On practice, such refactoring results in worse
-    // runtime performance.
-
-    // ContextMutex
-    bool doTryLock() override;
-    void doLock() override;
-    void doUnlock() override;
-
-  protected:
-    uint32_t getLockLevel() const override;
-
-  private:
-    Mutex mMutex;
-
-    // Used when ASSERT() and/or recursion are/is enabled.
-    std::atomic<angle::ThreadId> mOwnerThreadId;
-    // Used only when recursion is enabled.
-    uint32_t mLockLevel;
 };
 
 }  // namespace egl
