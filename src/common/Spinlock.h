821d77e0b218589e2479b21b8eeb3a161fdf7995
Igor Nazarov <i.nazarov@samsung.com>
2023-06-26 19:13:31 +0300

    Use yield() in angle::Spinlock instead of ANGLE_SMT_PAUSE()
    `ANGLE_SMT_PAUSE()` does not work as expected and causes threads that
    doing useful work starve for CPU cycles.
    
    Problem discovered in ANGLE end2end SwiftShader "win-x86-test", after
    replacing `std::mutex` with `angle::Spinlock` to protect Shared
    Contexts. Overall test execution time increased by 57%, and some tests
    were failing randomly because of the TIMEOUT error.
    
    Test commands:
        angle_end2end_tests --test-launcher-bot-mode --cfi-diag=0 --gtest_filter=*Vulkan_SwiftShader*
    
    Replacing `ANGLE_SMT_PAUSE()` by `std::this_thread::yield()` fixed the
    problem. Total test execution time increased only by 1.8% which in the
    margin of error.
    
    Tests using: `angle::Spinlock` + `ANGLE_SMT_PAUSE()`
        521.289272 seconds - https://ci.chromium.org/ui/p/angle/builders/try/win-x86-test/13549/overview
        533.989903 seconds - https://ci.chromium.org/ui/p/angle/builders/try/win-x86-test/13555/overview (1 FAILED)
        556.545179 seconds - https://ci.chromium.org/ui/p/angle/builders/try/win-x86-test/13558/overview (2 FAILED)
        517.187921 seconds - https://ci.chromium.org/ui/p/angle/builders/try/win-x86-test/13560/overview (1 FAILED)
    
    Tests using: `angle::Spinlock` + `std::this_thread::yield()`
        356.070396 seconds - https://ci.chromium.org/ui/p/angle/builders/try/win-x86-test/13586/overview
        341.888956 seconds - https://ci.chromium.org/ui/p/angle/builders/try/win-x86-test/13587/overview
        338.896357 seconds - https://ci.chromium.org/ui/p/angle/builders/try/win-x86-test/13589/overview
    
    Tests using: `std::mutex
        354.122618 seconds - https://ci.chromium.org/ui/p/angle/builders/try/win-x86-test/13591/overview
        327.921385 seconds - https://ci.chromium.org/ui/p/angle/builders/try/win-x86-test/13593/overview
        326.909106 seconds - https://ci.chromium.org/ui/p/angle/builders/try/win-x86-test/13594/overview
        325.102827 seconds - https://ci.chromium.org/ui/p/angle/builders/try/win-x86-test/13638/overview
        345.235797 seconds - https://ci.chromium.org/ui/p/angle/builders/try/win-x86-test/13639/overview
        358.007632 seconds - https://ci.chromium.org/ui/p/angle/builders/try/win-x86-test/13640/overview
    
    Recently added `__yield()` for ARM also performs worse than
    `std::this_thread::yield()`. In fact, `__yield()` performance is similar
    to simple busy wait.
    
    The above was discovered in simple test application, that
    runs N worker threads, each spins 1000'000'000 empty cycles starting at
    the same time. Additional test thread spins `yield()`, `__yield()`,
    `sleep()`, empty loop, or no loop at all. Resulting time to finish all
    worker threads on S906B (Android):
    
    Num. workers    Test thread operation               Avg. time (10 runs)
    8               empty loop                          5.805 sec
    8               __yield()                           6.285 sec
    8               std::this_thread::yield()           4.946 sec
    8               std::this_thread::sleep(1 micro)    4.573 sec
    8               N/A (no thread)                     4.421 sec
    
    4               empty loop                          4.652 sec
    4               __yield()                           4.765 sec
    4               std::this_thread::yield()           4.750 sec
    4               std::this_thread::sleep(1 micro)    3.507 sec
    4               N/A (no thread)                     3.510 sec
    
    Observations:
    - `__yield()` performs similar to "busy wait" regardless of the number
      of worker threads.
    - `std::this_thread::yield()` works same as `__yield()`/"busy wait" if
      worker thread count is low, but will not affect other threads much if
      their number is high.
    - `std::this_thread::sleep(1 micro)` almost did not affect other threads
      regardless of their number.
    
    Bug: angleproject:6015
    Bug: angleproject:6067
    Bug: angleproject:8226
    Change-Id: Iab60980f185ee9757a3436f8462be152426d5f8b

--- src/common/Spinlock.h
+++ src/common/Spinlock.h
@@ -12,26 +12,6 @@
 
 #include "common/angleutils.h"
 
-#ifdef _MSC_VER
-#    include <intrin.h>  // for _mm_pause() and __yield()
-#endif
-
-#if defined(__ARM_ARCH_7__) || defined(__aarch64__)
-#    if defined(__GNUC__) || defined(__clang__)
-#        include <arm_acle.h>  // for __yield()
-#    endif
-#endif
-
-#if defined(_MSC_VER) && (defined(_M_IX86) || defined(_M_X64))
-#    define ANGLE_SMT_PAUSE() _mm_pause()
-#elif defined(__GNUC__) && (defined(__i386__) || defined(__x86_64__))
-#    define ANGLE_SMT_PAUSE() __asm__ __volatile__("pause;")
-#elif defined(_M_ARM) || defined(_M_ARM64) || defined(__ARM_ARCH_7__) || defined(__aarch64__)
-#    define ANGLE_SMT_PAUSE() __yield()
-#else
-#    define ANGLE_SMT_PAUSE() static_cast<void>(0)
-#endif
-
 namespace angle
 {
 
@@ -64,8 +44,7 @@ ANGLE_INLINE void Spinlock::lock() noexcept
         // Relaxed wait to prevent unnecessary cache misses.
         while (mLock.load(std::memory_order_relaxed) != 0)
         {
-            // Optimization for simultaneous multithreading.
-            ANGLE_SMT_PAUSE();
+            std::this_thread::yield();
         }
     }
 }
