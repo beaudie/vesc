16990a3fe13493b6f7eda92a3db845162fc04846
Steven Noonan <steven@uplinklabs.net>
2023-06-26 12:07:52 -0700

    centralize basic OS/platform detection functions
    We had multiple different places that defined these, and with varying
    naming schemes. Centralize them to be defined in platform_helpers.h,
    which automatically gets included at the tail end of platform.h.
    
    Also renaming IsApple(uint32_t) to IsAppleGPU(uint32_t) to avoid
    ambiguous meaning: "IsApple" should mean "is Apple-vended OS" while
    "IsAppleGPU" should mean "is Apple GPU vendor ID".
    
    Change-Id: If4e3fc5ac1b5b8ad416663950a1b2ee912ccad99

---
+++ src/common/platform_helpers.h
@@ -0,0 +1,97 @@
+//
+// Copyright 2023 The ANGLE Project Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+
+// platform_helpers.h: common platform detection helper functions
+
+#ifndef COMMON_PLATFORM_HELPERS_H_
+#define COMMON_PLATFORM_HELPERS_H_
+
+namespace angle
+{
+
+inline constexpr bool IsAndroid()
+{
+#if defined(ANGLE_PLATFORM_ANDROID)
+    return true;
+#else
+    return false;
+#endif
+}
+
+inline constexpr bool IsApple()
+{
+    // This means any Apple-vended OS (including iOS, macOS, etc)
+#if defined(ANGLE_PLATFORM_APPLE)
+    return true;
+#else
+    return false;
+#endif
+}
+
+inline constexpr bool IsChromeOS()
+{
+#if defined(ANGLE_PLATFORM_CHROMEOS)
+    return true;
+#else
+    return false;
+#endif
+}
+
+inline constexpr bool IsFuchsia()
+{
+#if defined(ANGLE_PLATFORM_FUCHSIA)
+    return true;
+#else
+    return false;
+#endif
+}
+
+inline constexpr bool IsIOS()
+{
+#if ANGLE_PLATFORM_IOS_FAMILY
+    return true;
+#else
+    return false;
+#endif
+}
+
+inline constexpr bool IsLinux()
+{
+#if defined(ANGLE_PLATFORM_LINUX)
+    return true;
+#else
+    return false;
+#endif
+}
+
+inline constexpr bool IsMac()
+{
+#if defined(ANGLE_PLATFORM_MACOS)
+    return true;
+#else
+    return false;
+#endif
+}
+
+inline constexpr bool IsWindows()
+{
+#if defined(ANGLE_PLATFORM_WINDOWS)
+    return true;
+#else
+    return false;
+#endif
+}
+
+// Windows version helpers
+bool IsWindowsXP();
+bool IsWindowsVista();
+bool IsWindows7();
+bool IsWindows8();
+bool IsWindows10();
+
+}  // namespace angle
+
+#endif  // COMMON_PLATFORM_HELPERS_H_
