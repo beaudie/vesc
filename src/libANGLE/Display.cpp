ad9e835c452534456c86b11f1a160607914a2adb
Igor Nazarov <i.nazarov@samsung.com>
2023-06-27 18:08:27 +0300

    Rename SharedContexMutex into ContexMutex
    Follow up after:
        Replace (Single/Shared)ContextMutex classed with ContextMutex
    
    Renamed build option:
        angle_enable_shared_context_mutex -> angle_enable_context_mutex
    
    Renamed because there is no more SharedContexMutex class and
    ContextMutex is now used for both Shared and not Shared Contexts.
    
    Bug: angleproject:8226
    Change-Id: I68eea84aa59441d9c5b19870910b2bb499311e08

--- src/libANGLE/Display.cpp
+++ src/libANGLE/Display.cpp
@@ -1107,7 +1107,7 @@ Error Display::initialize()
     mSingleThreadPool = angle::WorkerThreadPool::Create(1, ANGLEPlatformCurrent());
     mMultiThreadPool  = angle::WorkerThreadPool::Create(0, ANGLEPlatformCurrent());
 
-    if (kIsSharedContextMutexEnabled)
+    if (kIsContextMutexEnabled)
     {
         ASSERT(mManagersMutex == nullptr);
         mManagersMutex = new ContextMutex();
@@ -1579,7 +1579,7 @@ Error Display::createContext(const Config *configuration,
 
     ScopedContextMutexLock mutexLock;
     ContextMutex *sharedContextMutex = nullptr;
-    if (kIsSharedContextMutexEnabled)
+    if (kIsContextMutexEnabled)
     {
         ASSERT(mManagersMutex != nullptr);
         if (shareContext != nullptr)
