b282d29f86f5f03f1e298f32a78b1301c8893468
Shahbaz Youssefi <syoussefi@chromium.org>
2023-07-06 13:34:28 -0400

    Make the glPatchParameteri entry point lockless
    This entry points only sets context-local state and thus doesn't require
    locking.
    
    Bug: angleproject:8224
    Change-Id: I17975a97aa7f68c3ddf2ef78069b8f519fdc4c1a

--- src/libANGLE/State.h
+++ src/libANGLE/State.h
@@ -1320,7 +1320,6 @@ class State : angle::NonCopyable
     PixelUnpackState &getUnpackState() { return mLocalState.getUnpackState(); }
     GLenum getCoverageModulation() const { return mLocalState.getCoverageModulation(); }
     bool getFramebufferSRGB() const { return mLocalState.getFramebufferSRGB(); }
-    void setPatchVertices(GLuint value) { mLocalState.setPatchVertices(value); }
     GLuint getPatchVertices() const { return mLocalState.getPatchVertices(); }
     float getLineWidth() const { return mLocalState.getLineWidth(); }
     unsigned int getActiveSampler() const { return mLocalState.getActiveSampler(); }
