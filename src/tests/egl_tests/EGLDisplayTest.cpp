240d55e7b91172699b23f349a69341d626ee8a23
Igor Nazarov <i.nazarov@samsung.com>
2023-07-24 13:00:32 +0300

    Perform Display terminate(InternalCleanup) from makeCurrent()
    Current state:
    - Call to `eglTerminate()` is canceled if there are Contexts current.
    - All not current Contexts are invalidated in `eglTerminate()` call.
    - Full Display termination will happen only when last Context is
      actually destroyed in `eglDestroyContext()` or last active thread
      terminates (Android only).
    
    Problem:
    - Context is not marked invalid after it is unmade from current when
      `eglTerminate()` was already called.
    - If `eglDestroyContext()` was called while context was current, it
      will be destroyed when unmade from current - in this case actual
      Display termination will NOT happen.
    
    After this change:
    - Context immediately invalidated after it is unmade from current when
      `eglTerminate()` was already called.
    - Full Display termination will happen after the last Context is unmade
      from current (all Contexts are invalid).
    
    Bug: angleproject:6798
    Test: angle_end2end_tests --gtest_filter=EGLDisplayTest.ContextLeakAfterTerminate*
    Change-Id: Idcce94b041649db58d3d879858fba99109347baf

--- src/tests/egl_tests/EGLDisplayTest.cpp
+++ src/tests/egl_tests/EGLDisplayTest.cpp
@@ -15,12 +15,62 @@ class EGLDisplayTest : public ANGLETest<>
 {};
 
 // Tests that an EGLDisplay can be re-initialized.
-TEST_P(EGLDisplayTest, InitalizeTerminateInitalize)
+TEST_P(EGLDisplayTest, InitializeTerminateInitialize)
 {
     EGLDisplay display = eglGetDisplay(EGL_DEFAULT_DISPLAY);
     EXPECT_EGL_TRUE(eglInitialize(display, nullptr, nullptr) != EGL_FALSE);
     EXPECT_EGL_TRUE(eglTerminate(display) != EGL_FALSE);
     EXPECT_EGL_TRUE(eglInitialize(display, nullptr, nullptr) != EGL_FALSE);
+}
+
+// Tests current Context leaking when call eglTerminate() while it is current.
+TEST_P(EGLDisplayTest, ContextLeakAfterTerminate)
+{
+    EGLDisplay display = eglGetDisplay(EGL_DEFAULT_DISPLAY);
+    EXPECT_EGL_TRUE(eglInitialize(display, nullptr, nullptr) != EGL_FALSE);
+
+    const EGLint configAttribs[] = {EGL_RED_SIZE,
+                                    8,
+                                    EGL_GREEN_SIZE,
+                                    8,
+                                    EGL_BLUE_SIZE,
+                                    8,
+                                    EGL_ALPHA_SIZE,
+                                    8,
+                                    EGL_RENDERABLE_TYPE,
+                                    EGL_OPENGL_ES2_BIT,
+                                    EGL_SURFACE_TYPE,
+                                    EGL_PBUFFER_BIT,
+                                    EGL_NONE};
+    EGLConfig config             = EGL_NO_CONFIG_KHR;
+    EGLint count                 = 0;
+    EXPECT_EGL_TRUE(eglChooseConfig(display, configAttribs, &config, 1, &count));
+    EXPECT_EGL_TRUE(count > 0);
+
+    const EGLint contextAttribs[] = {EGL_CONTEXT_MAJOR_VERSION, 2, EGL_NONE};
+    EGLContext context            = eglCreateContext(display, config, nullptr, contextAttribs);
+    EXPECT_NE(context, EGL_NO_CONTEXT);
+
+    const EGLint surfaceAttribs[] = {EGL_WIDTH, 64, EGL_HEIGHT, 64, EGL_NONE};
+    EGLSurface surface            = eglCreatePbufferSurface(display, config, surfaceAttribs);
+    EXPECT_NE(surface, EGL_NO_SURFACE);
+
+    // Make "context" current.
+    EXPECT_EGL_TRUE(eglMakeCurrent(display, surface, surface, context));
+
+    // Terminate display while "context" is current.
+    EXPECT_EGL_TRUE(eglTerminate(display) != EGL_FALSE);
+
+    // Unmake "context" from current and allow Display to actually terminate.
+    (void)eglMakeCurrent(display, EGL_NO_SURFACE, EGL_NO_SURFACE, EGL_NO_CONTEXT);
+
+    // Get EGLDisplay again.
+    display = eglGetDisplay(EGL_DEFAULT_DISPLAY);
+
+    // Check if Display was actually terminated.
+    EGLint val;
+    EXPECT_EGL_FALSE(eglQueryContext(display, context, EGL_CONTEXT_CLIENT_TYPE, &val));
+    EXPECT_EQ(eglGetError(), EGL_NOT_INITIALIZED);
 }
 
 ANGLE_INSTANTIATE_TEST(EGLDisplayTest,
