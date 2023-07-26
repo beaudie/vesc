e3a78c9f43dc60d1c5794e8627cd6d48f0eb2896
Yuxin Hu <yuxinhu@google.com>
2023-07-26 10:29:13 -0700

    Add the missing GraphicsPipelineDesc legacy dither bit update
    When the app calls glEnable(GL_DITHER) or glDisable(GL_DITHER),
    we need to update the legacy dither bit of the renderpass that
    belongs to ContextVk::mGraphicsPipelineDesc. If not, there is a
    change that the graphics pipeline will be created with a
    renderpass that has outdated legacy dither bit. This results
    the dither being applied to the render results incorrectly:
    e.g. the app calls glDisable(GL_DITHER),
    but the render results have dithering applied.
    
    Bug: b/286921997
    Bug: b/292282210
    Bug: b/293349058
    Bug: b/284462263
    Change-Id: Ie24b95898526c9021be6e3cb7620e4050f9faaaf

--- src/libANGLE/renderer/vulkan/ContextVk.cpp
+++ src/libANGLE/renderer/vulkan/ContextVk.cpp
@@ -5100,6 +5100,14 @@ void ContextVk::updateDither()
 
             onRenderPassFinished(RenderPassClosureReason::LegacyDithering);
         }
+
+        // update GraphicsPipelineDesc renderpass legacy dithering bit
+        if (isDitherEnabled() != mGraphicsPipelineDesc->isLegacyDitherEnabled())
+        {
+            mGraphicsPipelineDesc->updateRenderPassDesc(&mGraphicsPipelineTransition,
+                                                        framebufferVk->getRenderPassDesc());
+            invalidateCurrentGraphicsPipeline();
+        }
     }
 
     if (!getFeatures().emulateDithering.enabled)
