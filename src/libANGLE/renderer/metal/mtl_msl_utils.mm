65073ab408f4dca68f03567ac968576359aeac30
Charlie Lao <cclao@google.com>
2023-08-03 12:10:05 -0700

    Move name and mappedName out of LinkedUniform struct
    These are std::string objects. This CL moves these two data out of
    LinkedUniform and into ProgramExecutable class, side by side with
    mUniforms. With these two data moved out, LinkedUniform is now a simple
    struct with basic data types, and I can memcpy entire vector of
    uniforms.
    
    Bug: b/275102061
    Change-Id: I9ae13e7daca85f8e5f3662a4718f190bebb5f5d2

--- src/libANGLE/renderer/metal/mtl_msl_utils.mm
+++ src/libANGLE/renderer/metal/mtl_msl_utils.mm
@@ -490,12 +490,13 @@ angle::Result MTLGetMSL(const gl::Context *glContext,
     for (uint32_t textureIndex = 0; textureIndex < samplerBindings.size(); ++textureIndex)
     {
         const gl::SamplerBinding &samplerBinding = samplerBindings[textureIndex];
-        uint32_t uniformIndex = programState.getUniformIndexFromSamplerIndex(textureIndex);
-        const gl::LinkedUniform &samplerUniform = uniforms[uniformIndex];
-        bool isSamplerInStruct        = samplerUniform.name.find('.') != std::string::npos;
-        std::string mappedSamplerName = isSamplerInStruct
-                                            ? MSLGetMappedSamplerName(samplerUniform.name)
-                                            : MSLGetMappedSamplerName(samplerUniform.mappedName);
+        uint32_t uniformIndex          = programState.getUniformIndexFromSamplerIndex(textureIndex);
+        const std::string &uniformName = programState.getUniformNames()[uniformIndex];
+        const std::string &uniformMappedName = programState.getUniformMappedNames()[uniformIndex];
+        bool isSamplerInStruct               = uniformName.find('.') != std::string::npos;
+        std::string mappedSamplerName        = isSamplerInStruct
+                                                   ? MSLGetMappedSamplerName(uniformName)
+                                                   : MSLGetMappedSamplerName(uniformMappedName);
         // These need to be prefixed later seperately
         if (isSamplerInStruct)
             structSamplers.insert(mappedSamplerName);
