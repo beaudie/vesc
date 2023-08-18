cc915133b06045fcfdbb0dd45cbb3fbb8e22e7da
Yuxin Hu <yuxinhu@google.com>
2023-08-18 14:41:03 -0700

    Disable screen read capability on non-win32 platform
    This is a follow-up change of
    https://chromium-review.googlesource.com/c/angle/angle/+/4794979.
    
    Instead of using #ifdef to enable
    CAPABILITY_READ_SCREEN_PIXEL on NativeWindow
    and NativeWindowFactory based on platforms,
    this change instantiates NativeWindow
    and NativeWindowFactory with subclasses
    on win and posix (non-win) platforms, and we
    can pass different capabilities to the
    subclasses on different platforms.
    
    Bug: b/296487949
    Change-Id: I9a48d4d242184f2aa9c3fedacc2a2f3d8e06a7d9

---
+++ src/tests/deqp_support/posix/tcuANGLENativeWindowFactoryPosix.cpp
@@ -0,0 +1,64 @@
+/*-------------------------------------------------------------------------
+ * drawElements Quality Program Tester Core
+ * ----------------------------------------
+ *
+ * Copyright 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include "tcuANGLENativeWindowFactoryPosix.h"
+#include "tcuTexture.hpp"
+
+namespace tcu
+{
+
+constexpr eglu::NativeWindow::Capability kNonWin32WindowCapabilities =
+    static_cast<eglu::NativeWindow::Capability>(
+        eglu::NativeWindow::CAPABILITY_CREATE_SURFACE_LEGACY |
+        eglu::NativeWindow::CAPABILITY_GET_SURFACE_SIZE |
+        eglu::NativeWindow::CAPABILITY_GET_SCREEN_SIZE |
+        eglu::NativeWindow::CAPABILITY_SET_SURFACE_SIZE |
+        eglu::NativeWindow::CAPABILITY_CHANGE_VISIBILITY |
+        eglu::NativeWindow::CAPABILITY_CREATE_SURFACE_PLATFORM_EXTENSION);
+
+NativeWindowFactory *NativeWindowFactory::New(EventState *eventState, uint32_t preRotation)
+{
+    return new NativeWindowFactoryNonWin32(eventState, preRotation, kNonWin32WindowCapabilities);
+}
+
+NativeWindowFactoryNonWin32::NativeWindowFactoryNonWin32(
+    EventState *eventState,
+    uint32_t preRotation,
+    const eglu::NativeWindow::Capability windowCapability)
+    : NativeWindowFactory(eventState, preRotation, windowCapability)
+{}
+
+NativeWindow *NativeWindow::New(ANGLENativeDisplay *nativeDisplay,
+                                const eglu::WindowParams &params,
+                                EventState *eventState,
+                                uint32_t preRotation)
+{
+    return new NativeWindowNonWin32(nativeDisplay, params, eventState, preRotation,
+                                    kNonWin32WindowCapabilities);
+}
+
+NativeWindowNonWin32::NativeWindowNonWin32(ANGLENativeDisplay *nativeDisplay,
+                                           const eglu::WindowParams &params,
+                                           EventState *eventState,
+                                           uint32_t preRotation,
+                                           const eglu::NativeWindow::Capability windowCapability)
+    : NativeWindow(nativeDisplay, params, eventState, preRotation, windowCapability)
+{}
+}  // namespace tcu
