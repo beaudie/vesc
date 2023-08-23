6f045f2a3ea48f86241091c7ed6c93b739673bd6
Solti <solti@google.com>
2023-08-23 21:17:52 +0000

    AddTextureAhbTestCase
    Test: local test
    
    Bug: b/298033755
    Change-Id: I9cd34ee13ccf8680ffa854751a7572fe3313fc60

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+autoninja -C out/Android angle_end2end_tests || exit 1
+
+# out/Android/angle_end2end_tests --gtest_filter=*ImageTestES3.RGBXAHBUploadDataColorspace_Clone/ES3_Vulkan --verbose
+
+# using mali
+out/Android/angle_end2end_tests --gtest_filter=*ImageTestES3.RGBXAHBUploadDataColorspace_Clone/ES3_OpenGLES --verbose
+# out/Android/angle_end2end_tests --gtest_filter=*ImageTestES3.RGBXAHBUploadDataColorspace/* --verbose
