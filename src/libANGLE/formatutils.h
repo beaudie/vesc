fef3da7073e7545dc0050529852c6c8487f09f08
Charlie Lao <cclao@google.com>
2023-08-28 15:41:18 -0700

    Add GL_RGBX8_SRGB_ANGLEX and GL_BGRX8_SRGB_ANGLEX formats
    If app creates AHB with sRGBX format, and then create a EGLImage,
    Image::initialize() will retrieve the sized format from AHB, which is
    GL_RGBX_ANGLE, and then try to convert to sRGB color space. The bug here
    is that GetNonLinearFormat will pick GL_SRGB8 for all these formats
    :GL_RGB8, GL_BGRX8_ANGLEX, GL_RGBX8_ANGLE. This means that different
    AHBS with these three different format will end up createing a tetxure
    with same format which sRGB. I think the reason somone did it this way
    initially simply because there is no sRGB corresponding format available
    for BGRX8 and RGBX8. This CL adds sRGB internal version of these two
    formats, thus make it straight forward to pick the correct sRGB format.
    
    The other change here is for AHBs, right now we are always force
    mRequiredImageAccess to be Renderable. This actually workedaround the
    bug that mentioned earlier that we are picking the wrong format. Because
    of forced renderable, we end up with SRGB fallback to SRGBA. But
    external images should not fallback, we should use its existing format
    as is since it can be accessed via AHB interface directly.
    
    Bug: b/298037344
    Change-Id: I6119c4015cc5bf0effdf0530cb756b6c4656c38f

--- src/libANGLE/formatutils.h
+++ src/libANGLE/formatutils.h
@@ -66,9 +66,11 @@ ANGLE_INLINE GLenum GetNonLinearFormat(const GLenum format)
         case GL_RGBA8:
             return GL_SRGB8_ALPHA8;
         case GL_RGB8:
-        case GL_BGRX8_ANGLEX:
-        case GL_RGBX8_ANGLE:
             return GL_SRGB8;
+        case GL_BGRX8_ANGLEX:
+            return GL_BGRX8_SRGB_ANGLEX;
+        case GL_RGBX8_ANGLE:
+            return GL_RGBX8_SRGB_ANGLEX;
         case GL_RGBA16F:
             return GL_RGBA16F;
         default:
@@ -495,7 +497,6 @@ ANGLE_INLINE bool IsBGRAFormat(const GLenum internalFormat)
         case GL_BGR5_A1_ANGLEX:
         case GL_BGRA8_SRGB_ANGLEX:
         case GL_BGRX8_ANGLEX:
-        case GL_RGBX8_ANGLE:
         case GL_BGR565_ANGLEX:
         case GL_BGR10_A2_ANGLEX:
             return true;
