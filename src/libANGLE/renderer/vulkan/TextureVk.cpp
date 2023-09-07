26273142e4cb9dd00dd406547f4cfc4a0e158616
Amirali Abdolrashidi <abdolrashidi@google.com>
2023-09-07 14:44:24 -0700

    Vulkan: Flush pending image garbage more often
    * Added a counter to the renderer object to keep track of the pending
      image garbage.
      * mPendingImageGarbageSizeInBytes
    
    * Once it surpasses a limit (mPendingImageGarbageSizeLimit), it will
      flush the context so the pending garbages can be freed.
    
      * It is determined in the same function as the pending suballocation
        garbage limit (now renamed to calculatePendingGarbageSizeLimits()).
      * Currently the limit is based on the available heap sizes. It will
        use a fraction of the largest memory heap size.
        * The portion is currently kGarbageSizeLimitCoefficient = 0.2f.
    
    * This context flushing occurs after an image is released in the
      following function: TextureVk::releaseImage()
    
    * Unskipped the test "TextureDataInLoopManyTimes", which was failing
      on Android devices.
    
    Bug: b/280304441
    Change-Id: Ibcced1d118ea8a1f347028b62d29cfbd9e38e8c0

--- src/libANGLE/renderer/vulkan/TextureVk.cpp
+++ src/libANGLE/renderer/vulkan/TextureVk.cpp
@@ -3606,6 +3606,9 @@ void TextureVk::releaseImage(ContextVk *contextVk)
         }
     }
 
+    // Check the size of pending image garbage size, and flush the context if needed.
+    ASSERT(contextVk->checkAndFlushImageGarbage() == angle::Result::Continue);
+
     onStateChange(angle::SubjectMessage::SubjectChanged);
     mRedefinedLevels.reset();
 }
--- src/libANGLE/renderer/vulkan/vk_helpers.cpp
+++ src/libANGLE/renderer/vulkan/vk_helpers.cpp
@@ -5791,7 +5791,8 @@ void ImageHelper::releaseImage(RendererVk *renderer)
     }
 
     renderer->collectAllocationGarbage(mUse, mVmaAllocation);
-    renderer->collectGarbage(mUse, &mImage, &mDeviceMemory);
+    renderer->collectImageGarbage(mUse, mImage, mAllocationSize);
+    renderer->collectGarbage(mUse, &mDeviceMemory);
     mUse.reset();
     mImageSerial = kInvalidImageSerial;
     setEntireContentUndefined();
