0d2ed8fa8247dfee07347c4cfde9dbda4036a017
Mohan Maiya <m.maiya@samsung.com>
2023-09-20 20:35:09 -0700

    Frontend doesn't noop empty scissors
    Bug: angleproject:8348
    Tests: EGLSurfaceTest*OutOfBoundsScissor*
    Change-Id: I138d54896633d592f27b6eff36be8924d462c627

--- src/libANGLE/renderer/vulkan/FramebufferVk.cpp
+++ src/libANGLE/renderer/vulkan/FramebufferVk.cpp
@@ -456,7 +456,10 @@ angle::Result FramebufferVk::clearImpl(const gl::Context *context,
     ContextVk *contextVk = vk::GetImpl(context);
 
     const gl::Rectangle scissoredRenderArea = getRotatedScissoredRenderArea(contextVk);
-    ASSERT(scissoredRenderArea.width != 0 && scissoredRenderArea.height != 0);
+    if (scissoredRenderArea.width == 0 || scissoredRenderArea.height == 0)
+    {
+        return angle::Result::Continue;
+    }
 
     // This function assumes that only enabled attachments are asked to be cleared.
     ASSERT((clearColorBuffers & mState.getEnabledDrawBuffers()) == clearColorBuffers);
