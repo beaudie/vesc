637fe72fe942a912b75772ab877829567b5fb84d
Roman Lavrov <romanl@google.com>
2023-09-29 14:15:37 -0400

    Replace mActiveTextures.fill(nullptr) with memset
    std::array::fill yields unoptimized, unrolled loop with 8 byte
    increments.
    Surprisingly high (>2%) effect on the instructions counter in my tests
    (first frame of real_racing3 trace)
    The number of samples hitting this spot in profiling is also
    signficantly reduced.
    Impact on power harder to judge due to noise but does seem to be a bit
    better.
    
    Added a FillWithNullptr utility to check that nullptr is 0 and used it
    in other places where fill(nullptr) was used in ContextVk. It's not used
    elsewhere in vulkan.
    
    Bug: b/302708437
    Change-Id: If7fab66d858bc10ca356418d2ab26232bb9a9ce7

--- src/common/utilities.h
+++ src/common/utilities.h
@@ -308,6 +308,14 @@ bool IsDrawEntryPoint(EntryPoint entryPoint);
 bool IsDispatchEntryPoint(EntryPoint entryPoint);
 bool IsClearEntryPoint(EntryPoint entryPoint);
 bool IsQueryEntryPoint(EntryPoint entryPoint);
+
+template <typename T>
+void FillWithNullptr(T *array)
+{
+    static_assert(nullptr == 0);
+    // std::array::fill yields unoptimized, unrolled loop with 8 byte increments
+    memset(array->data(), 0, sizeof(*array));
+}
 }  // namespace angle
 
 void writeFile(const char *path, const void *data, size_t size);
