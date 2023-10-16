a452d692acc3bad6a9a868c505433a3757e6f8c2
Austin Annestrand <a.annestrand@samsung.com>
2023-10-16 12:43:43 -0500

    OpenCL: Add scaffolding to renderer/vulkan code
    Start off ANCLE (OpenCL on ANGLE) with backend scaffolding code with
    placeholder API error returns until they are later implemented.
    
    Bug: angleproject:8377
    Change-Id: I223d3482cce097ccb298e121fc03ec416e8958fd

--- src/libANGLE/renderer/vulkan/CLContextVk.cpp
+++ src/libANGLE/renderer/vulkan/CLContextVk.cpp
@@ -14,4 +14,122 @@ CLContextVk::CLContextVk(const cl::Context &context) : CLContextImpl(context) {}
 
 CLContextVk::~CLContextVk() = default;
 
+cl::DevicePtrs CLContextVk::getDevices(cl_int &errorCode) const
+{
+    UNIMPLEMENTED();
+    errorCode = CL_OUT_OF_RESOURCES;  // TODO(annestrand) Placeholder error for now
+    return cl::DevicePtrs{};
+}
+
+CLCommandQueueImpl::Ptr CLContextVk::createCommandQueue(const cl::CommandQueue &commandQueue,
+                                                        cl_int &errorCode)
+{
+    UNIMPLEMENTED();
+    errorCode = CL_OUT_OF_RESOURCES;  // TODO(annestrand) Placeholder error for now
+    return CLCommandQueueImpl::Ptr{};
+}
+
+CLMemoryImpl::Ptr CLContextVk::createBuffer(const cl::Buffer &buffer,
+                                            size_t size,
+                                            void *hostPtr,
+                                            cl_int &errorCode)
+{
+    UNIMPLEMENTED();
+    errorCode = CL_OUT_OF_RESOURCES;  // TODO(annestrand) Placeholder error for now
+    return CLMemoryImpl::Ptr{};
+}
+
+CLMemoryImpl::Ptr CLContextVk::createImage(const cl::Image &image,
+                                           cl::MemFlags flags,
+                                           const cl_image_format &format,
+                                           const cl::ImageDescriptor &desc,
+                                           void *hostPtr,
+                                           cl_int &errorCode)
+{
+    UNIMPLEMENTED();
+    errorCode = CL_OUT_OF_RESOURCES;  // TODO(annestrand) Placeholder error for now
+    return CLMemoryImpl::Ptr{};
+}
+
+cl_int CLContextVk::getSupportedImageFormats(cl::MemFlags flags,
+                                             cl::MemObjectType imageType,
+                                             cl_uint numEntries,
+                                             cl_image_format *imageFormats,
+                                             cl_uint *numImageFormats)
+{
+    UNIMPLEMENTED();
+    return CL_OUT_OF_RESOURCES;  // TODO(annestrand) Placeholder error for now
+}
+
+CLSamplerImpl::Ptr CLContextVk::createSampler(const cl::Sampler &sampler, cl_int &errorCode)
+{
+    UNIMPLEMENTED();
+    errorCode = CL_OUT_OF_RESOURCES;  // TODO(annestrand) Placeholder error for now
+    return CLSamplerImpl::Ptr{};
+}
+
+CLProgramImpl::Ptr CLContextVk::createProgramWithSource(const cl::Program &program,
+                                                        const std::string &source,
+                                                        cl_int &errorCode)
+{
+    UNIMPLEMENTED();
+    errorCode = CL_OUT_OF_RESOURCES;  // TODO(annestrand) Placeholder error for now
+    return CLProgramImpl::Ptr{};
+}
+
+CLProgramImpl::Ptr CLContextVk::createProgramWithIL(const cl::Program &program,
+                                                    const void *il,
+                                                    size_t length,
+                                                    cl_int &errorCode)
+{
+    // We currently do not support creating programs from IL
+    errorCode = CL_INVALID_VALUE;
+    return CLProgramImpl::Ptr{};
+}
+
+CLProgramImpl::Ptr CLContextVk::createProgramWithBinary(const cl::Program &program,
+                                                        const size_t *lengths,
+                                                        const unsigned char **binaries,
+                                                        cl_int *binaryStatus,
+                                                        cl_int &errorCode)
+{
+    UNIMPLEMENTED();
+    errorCode = CL_OUT_OF_RESOURCES;  // TODO(annestrand) Placeholder error for now
+    return CLProgramImpl::Ptr{};
+}
+
+CLProgramImpl::Ptr CLContextVk::createProgramWithBuiltInKernels(const cl::Program &program,
+                                                                const char *kernel_names,
+                                                                cl_int &errorCode)
+{
+    // We currently do not support builtin kernels
+    errorCode = CL_INVALID_VALUE;
+    return CLProgramImpl::Ptr{};
+}
+
+CLProgramImpl::Ptr CLContextVk::linkProgram(const cl::Program &program,
+                                            const cl::DevicePtrs &devices,
+                                            const char *options,
+                                            const cl::ProgramPtrs &inputPrograms,
+                                            cl::Program *notify,
+                                            cl_int &errorCode)
+{
+    UNIMPLEMENTED();
+    errorCode = CL_OUT_OF_RESOURCES;  // TODO(annestrand) Placeholder error for now
+    return CLProgramImpl::Ptr{};
+}
+
+CLEventImpl::Ptr CLContextVk::createUserEvent(const cl::Event &event, cl_int &errorCode)
+{
+    UNIMPLEMENTED();
+    errorCode = CL_OUT_OF_RESOURCES;  // TODO(annestrand) Placeholder error for now
+    return CLEventImpl::Ptr{};
+}
+
+cl_int CLContextVk::waitForEvents(const cl::EventPtrs &events)
+{
+    UNIMPLEMENTED();
+    return CL_OUT_OF_RESOURCES;  // TODO(annestrand) Placeholder error for now
+}
+
 }  // namespace rx
--- src/libANGLE/renderer/vulkan/CLDeviceVk.cpp
+++ src/libANGLE/renderer/vulkan/CLDeviceVk.cpp
@@ -18,32 +18,38 @@ CLDeviceVk::~CLDeviceVk() = default;
 
 CLDeviceImpl::Info CLDeviceVk::createInfo(cl::DeviceType type) const
 {
+    UNIMPLEMENTED();
     Info info(type);
     return info;
 }
 
 cl_int CLDeviceVk::getInfoUInt(cl::DeviceInfo name, cl_uint *value) const
 {
+    UNIMPLEMENTED();
     return CL_INVALID_VALUE;
 }
 
 cl_int CLDeviceVk::getInfoULong(cl::DeviceInfo name, cl_ulong *value) const
 {
+    UNIMPLEMENTED();
     return CL_INVALID_VALUE;
 }
 
 cl_int CLDeviceVk::getInfoSizeT(cl::DeviceInfo name, size_t *value) const
 {
+    UNIMPLEMENTED();
     return CL_INVALID_VALUE;
 }
 
 cl_int CLDeviceVk::getInfoStringLength(cl::DeviceInfo name, size_t *value) const
 {
+    UNIMPLEMENTED();
     return CL_INVALID_VALUE;
 }
 
 cl_int CLDeviceVk::getInfoString(cl::DeviceInfo name, size_t size, char *value) const
 {
+    UNIMPLEMENTED();
     return CL_INVALID_VALUE;
 }
 
@@ -52,6 +58,7 @@ cl_int CLDeviceVk::createSubDevices(const cl_device_partition_property *properti
                                     CreateFuncs &subDevices,
                                     cl_uint *numDevicesRet)
 {
+    UNIMPLEMENTED();
     return CL_INVALID_VALUE;
 }
 
