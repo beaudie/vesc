add32e8d538425f46a78a845be70ea038c5fcec3
Geoff Lang <geofflang@chromium.org>
2023-10-17 11:39:10 -0400

    M119: Move invalid uniform protection to the frontend.
    The frontend potentialy indexes into mUniformLocations with invalid
    uniform locations while validation is disabled too. Move the validation
    from the Metal backend to the frontend.
    
    Bug: chromium:1484878
    Change-Id: I92bc43aa28cfa26d601bb28f318860375f618608
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/4947652
    Reviewed-by: Shahbaz Youssefi <syoussefi@chromium.org>
    Commit-Queue: Shahbaz Youssefi <syoussefi@chromium.org>
    (cherry picked from commit e076d6cfd0e1d6948623bb344c5a38753b0c2b2e)

--- src/libANGLE/ProgramExecutable.cpp
+++ src/libANGLE/ProgramExecutable.cpp
@@ -354,7 +354,7 @@ template <typename T>
 GLuint GetResourceMaxNameSize(const T &resource, GLint max)
 {
     if (resource.isArray())
-    {
+    {`
         return std::max(max, clampCast<GLint>((resource.name + "[0]").size()));
     }
     else
@@ -2442,7 +2442,7 @@ GLuint ProgramExecutable::getUniformIndex(const std::string &name) const
 
 bool ProgramExecutable::shouldIgnoreUniform(UniformLocation location) const
 {
-    if (location.value == -1)
+    if (location.value < 0 || static_cast<size_t>(location.value) >= mUniformLocations.size())
     {
         return true;
     }
