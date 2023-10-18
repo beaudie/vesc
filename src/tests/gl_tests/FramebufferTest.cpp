0747ec59b164e62223df9e2b6866290990af871f
Yuxin Hu <yuxinhu@google.com>
2023-10-17 17:47:20 -0700

    Test To Investigate glcNearestEdgeTests with default egl config
    Bug: None
    Change-Id: I1e94087aac8c803bcc5db3515ed56291349daf28

--- src/tests/gl_tests/FramebufferTest.cpp
+++ src/tests/gl_tests/FramebufferTest.cpp
@@ -5936,6 +5936,320 @@ TEST_P(FramebufferExtensionsTest, ColorBufferFloatRgba)
     test("GL_CHROMIUM_color_buffer_float_rgba", GL_RGBA32F_EXT, true);
 }
 
+class DefaultFramebufferTest : public ANGLETest<>
+{
+  protected:
+    DefaultFramebufferTest()
+        : mDisplay(EGL_NO_DISPLAY),
+          mWindowSurface(EGL_NO_SURFACE),
+          mPbufferSurface(EGL_NO_SURFACE),
+          mContext(EGL_NO_CONTEXT),
+          mOSWindow(nullptr)
+    {}
+
+    void testSetUp() override
+    {
+        mOSWindow = OSWindow::New();
+        mOSWindow->initialize("DefaultFramebufferTest", 4, 4);
+    }
+
+    void tearDownContextAndSurface()
+    {
+        if (mDisplay == EGL_NO_DISPLAY)
+        {
+            return;
+        }
+
+        eglMakeCurrent(mDisplay, EGL_NO_SURFACE, EGL_NO_SURFACE, EGL_NO_CONTEXT);
+
+        if (mWindowSurface != EGL_NO_SURFACE)
+        {
+            eglDestroySurface(mDisplay, mWindowSurface);
+            mWindowSurface = EGL_NO_SURFACE;
+        }
+
+        if (mPbufferSurface != EGL_NO_SURFACE)
+        {
+            eglDestroySurface(mDisplay, mPbufferSurface);
+            mPbufferSurface = EGL_NO_SURFACE;
+        }
+
+        if (mContext != EGL_NO_CONTEXT)
+        {
+            eglDestroyContext(mDisplay, mContext);
+            mContext = EGL_NO_CONTEXT;
+        }
+    }
+
+    // Release any resources created in the test body
+    void testTearDown() override
+    {
+        tearDownContextAndSurface();
+
+        if (mDisplay != EGL_NO_DISPLAY)
+        {
+            eglTerminate(mDisplay);
+            mDisplay = EGL_NO_DISPLAY;
+        }
+
+        mOSWindow->destroy();
+        OSWindow::Delete(&mOSWindow);
+
+        ASSERT_TRUE(mWindowSurface == EGL_NO_SURFACE && mContext == EGL_NO_CONTEXT);
+    }
+
+    void initializeDisplay()
+    {
+        GLenum platformType = GetParam().getRenderer();
+        GLenum deviceType   = GetParam().getDeviceType();
+
+        std::vector<EGLint> displayAttributes;
+        displayAttributes.push_back(EGL_PLATFORM_ANGLE_TYPE_ANGLE);
+        displayAttributes.push_back(platformType);
+        displayAttributes.push_back(EGL_PLATFORM_ANGLE_MAX_VERSION_MAJOR_ANGLE);
+        displayAttributes.push_back(EGL_DONT_CARE);
+        displayAttributes.push_back(EGL_PLATFORM_ANGLE_MAX_VERSION_MINOR_ANGLE);
+        displayAttributes.push_back(EGL_DONT_CARE);
+        displayAttributes.push_back(EGL_PLATFORM_ANGLE_DEVICE_TYPE_ANGLE);
+        displayAttributes.push_back(deviceType);
+        displayAttributes.push_back(EGL_NONE);
+
+        mDisplay = eglGetPlatformDisplayEXT(EGL_PLATFORM_ANGLE_ANGLE,
+                                            reinterpret_cast<void *>(mOSWindow->getNativeDisplay()),
+                                            displayAttributes.data());
+        ASSERT_TRUE(mDisplay != EGL_NO_DISPLAY);
+
+        EGLint majorVersion, minorVersion;
+        ASSERT_TRUE(eglInitialize(mDisplay, &majorVersion, &minorVersion) == EGL_TRUE);
+
+        eglBindAPI(EGL_OPENGL_ES_API);
+        ASSERT_EGL_SUCCESS();
+    }
+
+    void initializeContext()
+    {
+        ASSERT_TRUE(mContext == EGL_NO_CONTEXT);
+
+        EGLint contextAttibutes[] = {EGL_CONTEXT_CLIENT_VERSION, GetParam().majorVersion, EGL_NONE};
+
+        if (!IsEGLDisplayExtensionEnabled(mDisplay, "EGL_ANGLE_context_virtualization"))
+        {
+            contextAttibutes[2] = EGL_NONE;
+        }
+
+        mContext = eglCreateContext(mDisplay, mConfig, nullptr, contextAttibutes);
+        ASSERT_EGL_SUCCESS();
+        ASSERT_TRUE(mContext != EGL_NO_CONTEXT);
+    }
+
+    void initializeWindowSurfaceWithAttribs(EGLConfig config,
+                                            const std::vector<EGLint> &additionalAttributes,
+                                            EGLenum expectedResult)
+    {
+        ASSERT_TRUE(mWindowSurface == EGL_NO_SURFACE);
+
+        EGLint surfaceType = EGL_NONE;
+        eglGetConfigAttrib(mDisplay, mConfig, EGL_SURFACE_TYPE, &surfaceType);
+
+        if (surfaceType & EGL_WINDOW_BIT)
+        {
+            std::vector<EGLint> windowAttributes = additionalAttributes;
+            windowAttributes.push_back(EGL_NONE);
+
+            // Create first window surface
+            mWindowSurface = eglCreateWindowSurface(mDisplay, mConfig, mOSWindow->getNativeWindow(),
+                                                    windowAttributes.data());
+        }
+
+        ASSERT_EGLENUM_EQ(eglGetError(), expectedResult);
+    }
+
+    void initializeSurfaceWithAttribs(EGLConfig config,
+                                      const std::vector<EGLint> &additionalAttributes)
+    {
+        mConfig = config;
+
+        EGLint surfaceType = EGL_NONE;
+        eglGetConfigAttrib(mDisplay, mConfig, EGL_SURFACE_TYPE, &surfaceType);
+
+        if (surfaceType & EGL_WINDOW_BIT)
+        {
+            initializeWindowSurfaceWithAttribs(config, additionalAttributes, EGL_SUCCESS);
+        }
+
+        if (surfaceType & EGL_PBUFFER_BIT)
+        {
+            ASSERT_TRUE(mPbufferSurface == EGL_NO_SURFACE);
+
+            std::vector<EGLint> pbufferAttributes = additionalAttributes;
+
+            // Give pbuffer non-zero dimensions.
+            pbufferAttributes.push_back(EGL_WIDTH);
+            pbufferAttributes.push_back(64);
+            pbufferAttributes.push_back(EGL_HEIGHT);
+            pbufferAttributes.push_back(64);
+            pbufferAttributes.push_back(EGL_NONE);
+
+            mPbufferSurface = eglCreatePbufferSurface(mDisplay, mConfig, pbufferAttributes.data());
+            ASSERT_EGL_SUCCESS();
+        }
+    }
+
+    void initializeSurface(EGLConfig config)
+    {
+        std::vector<EGLint> additionalAttributes;
+        initializeSurfaceWithAttribs(config, additionalAttributes);
+    }
+
+    void makeCurrent() { eglMakeCurrent(mDisplay, mWindowSurface, mWindowSurface, mContext); }
+
+    EGLConfig chooseDefaultWindowSurfaceConfig() const
+    {
+        const EGLint configAttributes[] = {
+            EGL_RED_SIZE,     EGL_DONT_CARE,  EGL_GREEN_SIZE,     EGL_DONT_CARE,  EGL_BLUE_SIZE,
+            EGL_DONT_CARE,    EGL_ALPHA_SIZE, EGL_DONT_CARE,      EGL_DEPTH_SIZE, EGL_DONT_CARE,
+            EGL_STENCIL_SIZE, EGL_DONT_CARE,  EGL_SAMPLE_BUFFERS, EGL_DONT_CARE,  EGL_SURFACE_TYPE,
+            EGL_WINDOW_BIT,   EGL_NONE};
+
+        EGLint configCount;
+        EGLConfig config;
+        if (eglChooseConfig(mDisplay, configAttributes, &config, 1, &configCount) != EGL_TRUE)
+            return nullptr;
+        if (configCount != 1)
+            return nullptr;
+        return config;
+    }
+
+    void initializeWindowSurfaceWithDefaultConfig()
+    {
+        EGLConfig defaultConfig = chooseDefaultWindowSurfaceConfig();
+        ASSERT_NE(defaultConfig, nullptr);
+        initializeSurface(defaultConfig);
+    }
+
+  private:
+    EGLDisplay mDisplay;
+    EGLSurface mWindowSurface;
+    EGLSurface mPbufferSurface;
+    EGLContext mContext;
+    EGLConfig mConfig;
+    OSWindow *mOSWindow;
+};
+
+// glReadPixel from default FBO with GL_IMPLEMENTATION_COLOR_READ_FORMAT and
+// GL_IMPLEMENTATION_COLOR_READ_TYPE should work
+TEST_P(DefaultFramebufferTest, ReadFromDefaultFBO)
+{
+    initializeDisplay();
+    initializeWindowSurfaceWithDefaultConfig();
+    initializeContext();
+    makeCurrent();
+
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+    constexpr char kVS1[] = R"(#version 300 es
+in highp vec2 a_position;
+in highp vec2 a_texcoord;
+out highp vec2 texcoord;
+void main()
+{
+    gl_Position = vec4(a_position, 0.0, 1.0);
+    texcoord = a_texcoord;
+})";
+
+    constexpr char kFS1[] = R"(#version 300 es
+precision highp float;
+in highp vec2 texcoord;
+out highp vec4 fragColor;
+uniform highp sampler2D texSampler;
+
+void main()
+{
+    fragColor = texture(texSampler, texcoord);
+})";
+
+    ANGLE_GL_PROGRAM(program, kVS1, kFS1);
+    glUseProgram(program);
+
+    const std::vector<float> positions = {-1.0f, -1.0f, -1.0f, 1.0f, 1.0f, -1.0f, 1.0f, 1.0f};
+    GLBuffer vertexBuffer;
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer.get());
+    glBufferData(GL_ARRAY_BUFFER, sizeof(positions[0]) * positions.size(), positions.data(),
+                 GL_STATIC_DRAW);
+    GLint vertexPosLocation = glGetAttribLocation(program, "a_position");
+    ASSERT_NE(vertexPosLocation, -1);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer.get());
+    glEnableVertexAttribArray(vertexPosLocation);
+    glVertexAttribPointer(vertexPosLocation, 2, GL_FLOAT, GL_FALSE, 0, 0);
+
+    const std::vector<float> texcoords = {0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f, 1.0f};
+    GLBuffer texcoordBuffer;
+    glBindBuffer(GL_ARRAY_BUFFER, texcoordBuffer.get());
+    glBufferData(GL_ARRAY_BUFFER, sizeof(texcoords[0]) * texcoords.size(), texcoords.data(),
+                 GL_STATIC_DRAW);
+    GLint texCoordLocation = glGetAttribLocation(program, "a_texcoord");
+    ASSERT_NE(texCoordLocation, -1);
+    glBindBuffer(GL_ARRAY_BUFFER, texcoordBuffer.get());
+    glEnableVertexAttribArray(texCoordLocation);
+    glVertexAttribPointer(texCoordLocation, 2, GL_FLOAT, GL_FALSE, 0, 0);
+
+    const std::vector<uint16_t> quadIndices = {0, 1, 2, 2, 1, 3};
+    GLBuffer indexBuffer;
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer.get());
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(quadIndices[0]) * quadIndices.size(),
+                 quadIndices.data(), GL_STATIC_DRAW);
+    ASSERT_GL_NO_ERROR();
+
+    // Create Texture
+    GLTexture texture;
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, 0);
+
+    std::vector<uint8_t> texData;
+
+    const size_t width  = 4;
+    const size_t height = 4;
+
+    texData.resize(width * height * 4);
+
+    for (size_t i = 0; i < width * height; ++i)
+    {
+        texData.push_back(125);
+        texData.push_back(125);
+        texData.push_back(125);
+        texData.push_back(125);
+    }
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE,
+                 texData.data());
+
+    GLint uniformTextureSamplerLocation = glGetUniformLocation(program, "texSampler");
+    glUniform1i(uniformTextureSamplerLocation, 0);
+
+    ASSERT_GL_NO_ERROR();
+
+    glDisable(GL_DITHER);
+
+    glDrawElements(GL_TRIANGLES, quadIndices.size(), GL_UNSIGNED_BYTE, 0);
+
+    std::vector<uint8_t> renderResult;
+    renderResult.resize(width * height * 4);
+
+    GLint readFormat;
+    glGetIntegerv(GL_IMPLEMENTATION_COLOR_READ_FORMAT, &readFormat);
+
+    GLint readType;
+    glGetIntegerv(GL_IMPLEMENTATION_COLOR_READ_TYPE, &readType);
+
+    glReadPixels(0, 0, width, height, readFormat, readType, renderResult.data());
+
+    ASSERT_GL_NO_ERROR();
+}
+
 ANGLE_INSTANTIATE_TEST_ES2_AND(AddMockTextureNoRenderTargetTest,
                                ES2_D3D9().enable(Feature::AddMockTextureNoRenderTarget),
                                ES2_D3D11().enable(Feature::AddMockTextureNoRenderTarget),
@@ -5961,3 +6275,5 @@ ANGLE_INSTANTIATE_TEST_ES31_AND(
     ES31_VULKAN().disable(Feature::SupportsImagelessFramebuffer),
     ES31_VULKAN().enable(Feature::ForceDelayedDeviceCreationForTesting));
 ANGLE_INSTANTIATE_TEST_ES3(FramebufferTestWithFormatFallback);
+GTEST_ALLOW_UNINSTANTIATED_PARAMETERIZED_TEST(DefaultFramebufferTest);
+ANGLE_INSTANTIATE_TEST(DefaultFramebufferTest, WithNoFixture(ES3_VULKAN()));
