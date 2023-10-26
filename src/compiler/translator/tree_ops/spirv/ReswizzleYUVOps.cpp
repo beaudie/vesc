c41efa895c45d5d2202be0ee2e8c0cc24e943319
Chris Forbes <chrisforbes@google.com>
2023-10-27 11:57:59 +1300

    Apply swizzle to outputs marked as layout(yuv)
    Change-Id: Ic33d5708b44aa79bd5ff128bd9172557eacf6ae2

--- src/compiler/translator/tree_ops/spirv/ReswizzleYUVOps.cpp
+++ src/compiler/translator/tree_ops/spirv/ReswizzleYUVOps.cpp
@@ -14,6 +14,7 @@
 #include "compiler/translator/SymbolTable.h"
 #include "compiler/translator/tree_util/IntermNode_util.h"
 #include "compiler/translator/tree_util/IntermTraverse.h"
+#include "compiler/translator/tree_util/RunAtTheEndOfShader.h"
 
 namespace sh
 {
@@ -28,8 +29,12 @@ class ReswizzleYUVOpsTraverser : public TIntermTraverser
     {}
 
     bool visitAggregate(Visit visit, TIntermAggregate *node) override;
+    bool visitDeclaration(Visit visit, TIntermDeclaration *node) override;
+
+    bool adjustOutput(TCompiler *compiler, TIntermBlock *root);
 
   private:
+    TIntermSymbol *mYuvOutput = nullptr;
 };
 
 bool ReswizzleYUVOpsTraverser::visitAggregate(Visit visit, TIntermAggregate *node)
@@ -68,12 +73,70 @@ bool ReswizzleYUVOpsTraverser::visitAggregate(Visit visit, TIntermAggregate *nod
 
     return true;
 }
+
+bool ReswizzleYUVOpsTraverser::visitDeclaration(Visit visit, TIntermDeclaration *node)
+{
+    const TIntermSequence &sequence = *node->getSequence();
+    ASSERT(sequence.size() == 1);
+
+    TIntermSymbol *symbol = sequence.front()->getAsSymbolNode();
+    if (symbol == nullptr)
+    {
+        return true;
+    }
+
+    if (symbol->getQualifier() != EvqFragmentOut)
+    {
+        return true;
+    }
+
+    if (!symbol->getType().getLayoutQualifier().yuv)
+    {
+        return true;
+    }
+
+    mYuvOutput = symbol;
+    return true;
+}
+
+bool ReswizzleYUVOpsTraverser::adjustOutput(TCompiler *compiler, TIntermBlock *root)
+{
+    if (mYuvOutput == nullptr)
+    {
+        // we never saw an output with layout(yuv) so there's nothing to do.
+        return true;
+    }
+
+    // TODO: consider rewriting the existing usages rather than just emitting
+    // an assignment, to be cleaner.
+
+    TIntermBlock *block = new TIntermBlock;
+
+    // output = output.brga
+    TVector<int> swizzle = { 2, 0, 1, 3 };
+    const int size = mYuvOutput->getType().getNominalSize();
+    if (size < 4)
+    {
+        swizzle.resize(size);
+    }
+
+    TIntermTyped *assignment = new TIntermBinary(EOpAssign, mYuvOutput->deepCopy(),
+            new TIntermSwizzle(mYuvOutput->deepCopy(), swizzle));
+    block->appendStatement(assignment);
+
+    return RunAtTheEndOfShader(compiler, root, block, mSymbolTable);
+}
+
 }  // anonymous namespace
 
 bool ReswizzleYUVOps(TCompiler *compiler, TIntermBlock *root, TSymbolTable *symbolTable)
 {
     ReswizzleYUVOpsTraverser traverser(symbolTable);
     root->traverse(&traverser);
-    return traverser.updateTree(compiler, root);
+    if (!traverser.updateTree(compiler, root))
+    {
+        return false;
+    }
+    return traverser.adjustOutput(compiler, root);
 }
 }  // namespace sh
