b16535f729c46ecf1e7a7e5b42be9606ce24cbb4
Sergey Kataev <sergeyka@chromium.org>
2023-11-28 17:38:28 -0800

    Flush active queries before Begin
    Bug: angleproject:8415
    Change-Id: I97dfd904d65933a66f25cd168c50fe150ef9c765

--- src/libANGLE/renderer/d3d/d3d11/Query11.cpp
+++ src/libANGLE/renderer/d3d/d3d11/Query11.cpp
@@ -69,6 +69,7 @@ Query11::~Query11()
 
 angle::Result Query11::begin(const gl::Context *context)
 {
+    ANGLE_TRY(flush(context11, true));
     mResultSum = 0;
     mRenderer->getStateManager()->onBeginQuery(this);
     return resume(GetImplAs<Context11>(context));
