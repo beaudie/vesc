ebb7d7ed51cc61b555b4584c4dcbbb79491b24f2
Sergey Kataev <sergeyka@chromium.org>
2023-11-28 18:03:40 -0800

    Cast
    Change-Id: Ia2d7c4428ad1dc2c3a0479c72283f84e97292c08

--- src/libANGLE/renderer/d3d/d3d11/Query11.cpp
+++ src/libANGLE/renderer/d3d/d3d11/Query11.cpp
@@ -69,7 +69,7 @@ Query11::~Query11()
 
 angle::Result Query11::begin(const gl::Context *context)
 {
-    ANGLE_TRY(flush(context11, true));
+    ANGLE_TRY(flush(GetImplAs<Context11>(context), true));
     mResultSum = 0;
     mRenderer->getStateManager()->onBeginQuery(this);
     return resume(GetImplAs<Context11>(context));
