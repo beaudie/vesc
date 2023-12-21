8b51b4f86c8236fcf62093a746c8591ab91b08ed
Shahbaz Youssefi <syoussefi@chromium.org>
2023-12-21 15:57:39 -0500

    Vulkan: Invalidate pipeline with FBO draw buffer change
    Enabling/disabling draw buffers can affect the graphics pipeline without
    changing the render pass description.  In that case, the graphics
    pipeline was not being invalidated.
    
    Bug: angleproject:8463
    Change-Id: I6848472dcbb3d3ce4c34d95be28c8ec3fc50dcd7

--- src/libANGLE/renderer/vulkan/ContextVk.cpp
+++ src/libANGLE/renderer/vulkan/ContextVk.cpp
@@ -4764,6 +4764,10 @@ void ContextVk::updateColorMasks()
                                                  framebufferVk->getEmulatedAlphaAttachmentMask(),
                                                  framebufferVk->getState().getEnabledDrawBuffers());
 
+    // This function may be called outside of ContextVk::syncState, and so invalidates the graphics
+    // pipeline.
+    invalidateCurrentGraphicsPipeline();
+
     onColorAccessChange();
 }
 
@@ -4795,6 +4799,10 @@ void ContextVk::updateBlendFuncsAndEquations()
 
     mGraphicsPipelineDesc->updateBlendEquations(&mGraphicsPipelineTransition, blendStateExt,
                                                 mCachedDrawFramebufferColorAttachmentMask);
+
+    // This function may be called outside of ContextVk::syncState, and so invalidates the graphics
+    // pipeline.
+    invalidateCurrentGraphicsPipeline();
 }
 
 void ContextVk::updateSampleMaskWithRasterizationSamples(const uint32_t rasterizationSamples)
