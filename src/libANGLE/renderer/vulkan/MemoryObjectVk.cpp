f55a45bb65a6cbdac144048f4980a30b10154fd7
Shahbaz Youssefi <syoussefi@chromium.org>
2024-01-12 16:18:49 -0500

    Debug
    Bug: None
    Change-Id: Icba400b35d055b7013011dad6ba5315545570b65

--- src/libANGLE/renderer/vulkan/MemoryObjectVk.cpp
+++ src/libANGLE/renderer/vulkan/MemoryObjectVk.cpp
@@ -242,6 +242,7 @@ angle::Result MemoryObjectVk::createImage(ContextVk *contextVk,
 
     // TODO(jmadill, spang): Memory sub-allocation. http://anglebug.com/2162
     ASSERT(offset == 0);
+    WARN() << externalMemoryRequirements.size << " vs " << mSize;
     ASSERT(externalMemoryRequirements.size == mSize);
 
     VkMemoryPropertyFlags flags = hasProtectedContent ? VK_MEMORY_PROPERTY_PROTECTED_BIT : 0;
