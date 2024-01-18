bdfa28f4793c503082b5dfb4fa2e6f5092f39d67
Alexey Knyazev <lexa.knyazev@gmail.com>
2024-01-18 00:00:00 +0000

    Metal: Skip array index clamp for constant values
    Do not wrap constant array indices with ANGLE_int_clamp
    because their values are validated during GLSL parsing.
    
    Fixed: angleproject:8488
    Change-Id: I3ad183475e1847cb7db1a07df3598c7d39767d7e

--- src/compiler/translator/msl/EmitMetal.cpp
+++ src/compiler/translator/msl/EmitMetal.cpp
@@ -1753,6 +1753,16 @@ bool GenMetalTraverser::visitBinary(Visit, TIntermBinary *binaryNode)
             TType leftType = leftNode.getType();
             groupedTraverse(leftNode);
             mOut << "[";
+            const TConstantUnion *constIndex = rightNode.getConstantValue();
+            if (!leftType.isUnsizedArray() && constIndex != nullptr &&
+                constIndex->getType() == EbtInt && constIndex->getIConst() >= 0 &&
+                constIndex->getIConst() < static_cast<int>(leftType.isArray()
+                                                               ? leftType.getOutermostArraySize()
+                                                               : leftType.getNominalSize()))
+            {
+                emitSingleConstant(constIndex);
+            }
+            else
             {
                 mOut << "ANGLE_int_clamp(";
                 groupedTraverse(rightNode);
