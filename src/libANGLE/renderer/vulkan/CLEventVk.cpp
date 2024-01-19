fb074914dae91e0b5477cd770aa075bf3539ae96
Austin Annestrand <a.annestrand@samsung.com>
2024-01-19 15:10:09 -0600

    OpenCL/Vulkan: Add initial CL Event creation
    Introduce initial support for CL Event (and user event) creation
    as well as implement initial event status query routine.
    
    Bug: angleproject:8574
    Change-Id: I97eb57a720610e00a04e512328f0b3b4287bf021

--- src/libANGLE/renderer/vulkan/CLEventVk.cpp
+++ src/libANGLE/renderer/vulkan/CLEventVk.cpp
@@ -6,15 +6,53 @@
 // CLEventVk.cpp: Implements the class methods for CLEventVk.
 
 #include "libANGLE/renderer/vulkan/CLEventVk.h"
+#include "libANGLE/renderer/vulkan/CLCommandQueueVk.h"
+#include "libANGLE/renderer/vulkan/CLContextVk.h"
+#include "libANGLE/renderer/vulkan/RendererVk.h"
 
+#include "libANGLE/CLCommandQueue.h"
+#include "libANGLE/CLContext.h"
+#include "libANGLE/CLEvent.h"
 #include "libANGLE/cl_utils.h"
 
 namespace rx
 {
 
-CLEventVk::CLEventVk(const cl::Event &event) : CLEventImpl(event) {}
+CLEventVk::CLEventVk(const cl::Event &event)
+    : CLEventImpl(event),
+      mStatus(CL_QUEUED),
+      mCommandQueueVk(nullptr),
+      mContextVk(&mEvent.getContext().getImpl<CLContextVk>())
+{
+    if (event.getCommandType() == CL_COMMAND_USER)
+    {
+        // User commands do not have command queue association.
+        // Set to submitted here rather than queued.
+        mStatus = CL_SUBMITTED;
+    }
+    else
+    {
+        mCommandQueueVk = &mEvent.getCommandQueue()->getImpl<CLCommandQueueVk>();
+    }
+}
 
 CLEventVk::~CLEventVk() = default;
+
+angle::Result CLEventVk::init(bool completed)
+{
+    // User Events are handled by runtime and should not be associated with
+    // VkQueue
+    assert(mEvent.getCommandType() != CL_COMMAND_USER);
+    if (completed)
+    {
+        mStatus = CL_COMPLETE;
+    }
+    else
+    {
+        mCommandQueueVk->onEventInit(this);
+    }
+    return angle::Result::Continue;
+}
 
 angle::Result CLEventVk::getCommandExecutionStatus(cl_int &executionStatus)
 {
@@ -24,8 +62,16 @@ angle::Result CLEventVk::getCommandExecutionStatus(cl_int &executionStatus)
 
 angle::Result CLEventVk::setUserEventStatus(cl_int executionStatus)
 {
-    UNIMPLEMENTED();
-    ANGLE_CL_RETURN_ERROR(CL_OUT_OF_RESOURCES);
+    if (executionStatus > CL_COMPLETE || executionStatus < CL_SUCCESS)
+    {
+        ANGLE_CL_RETURN_ERROR(CL_INVALID_VALUE);
+    }
+    if (mStatus == CL_COMPLETE)
+    {
+        ANGLE_CL_RETURN_ERROR(CL_INVALID_OPERATION);
+    }
+    mStatus = CL_COMPLETE;
+    return angle::Result::Continue;
 }
 
 angle::Result CLEventVk::setCallback(cl::Event &event, cl_int commandExecCallbackType)
@@ -38,6 +84,18 @@ angle::Result CLEventVk::getProfilingInfo(cl::ProfilingInfo name,
                                           size_t valueSize,
                                           void *value,
                                           size_t *valueSizeRet)
+{
+    UNIMPLEMENTED();
+    ANGLE_CL_RETURN_ERROR(CL_OUT_OF_RESOURCES);
+}
+
+angle::Result CLEventVk::clientWait(bool flushCommands, uint64_t timeout, VkResult *outResult)
+{
+    UNIMPLEMENTED();
+    ANGLE_CL_RETURN_ERROR(CL_OUT_OF_RESOURCES);
+}
+
+angle::Result CLEventVk::serverWait()
 {
     UNIMPLEMENTED();
     ANGLE_CL_RETURN_ERROR(CL_OUT_OF_RESOURCES);
