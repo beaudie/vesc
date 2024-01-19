5f64bfd9c36cb59bc4065c0137938f5d663ead46
Austin Annestrand <a.annestrand@samsung.com>
2024-01-19 14:50:11 -0600

    OpenCL/Vulkan: Add initial CL Buffer routines
    Enabling CL buffer creation and initial routines
    for the Vulkan Backend.
    
    Bug: angleproject:8573
    Change-Id: I55674efde6cab2cb45a336cf9211d19d49166f82

--- src/libANGLE/renderer/vulkan/CLMemoryVk.cpp
+++ src/libANGLE/renderer/vulkan/CLMemoryVk.cpp
@@ -6,20 +6,203 @@
 // CLMemoryVk.cpp: Implements the class methods for CLMemoryVk.
 
 #include "libANGLE/renderer/vulkan/CLMemoryVk.h"
+#include "libANGLE/renderer/vulkan/CLContextVk.h"
 
+#include "libANGLE/CLBuffer.h"
+#include "libANGLE/CLContext.h"
+#include "libANGLE/CLMemory.h"
 #include "libANGLE/cl_utils.h"
 
 namespace rx
 {
 
-CLMemoryVk::CLMemoryVk(const cl::Memory &memory) : CLMemoryImpl(memory) {}
+CLMemoryVk::CLMemoryVk(const cl::Memory &memory)
+    : CLMemoryImpl(memory),
+      mContext(&memory.getContext().getImpl<CLContextVk>()),
+      mRenderer(mContext->getRenderer()),
+      mMapPtr(nullptr),
+      mMapCount(0),
+      mParent(nullptr)
+{}
 
-CLMemoryVk::~CLMemoryVk() = default;
+CLMemoryVk::~CLMemoryVk()
+{
+    mContext->mData->mMemories.erase(mMemory.getNative());
+}
 
 angle::Result CLMemoryVk::createSubBuffer(const cl::Buffer &buffer,
                                           cl::MemFlags flags,
                                           size_t size,
                                           CLMemoryImpl::Ptr *subBufferOut)
+{
+    UNIMPLEMENTED();
+    ANGLE_CL_RETURN_ERROR(CL_OUT_OF_RESOURCES);
+}
+
+angle::Result CLMemoryVk::getMapPtr(uint8_t **mapPtrOut)
+{
+    if (mMapPtr == nullptr && mapPtrOut != nullptr)
+    {
+        ANGLE_TRY(map());
+        *mapPtrOut = mMapPtr;
+    }
+    return angle::Result::Continue;
+}
+
+VkBufferUsageFlags CLMemoryVk::getVkUsageFlags()
+{
+    VkBufferUsageFlags usageFlags =
+        VK_BUFFER_USAGE_TRANSFER_SRC_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT;
+
+    if (mMemory.getFlags().isSet(CL_MEM_WRITE_ONLY))
+    {
+        usageFlags |= VK_BUFFER_USAGE_STORAGE_BUFFER_BIT;
+    }
+    else if (mMemory.getFlags().isSet(CL_MEM_READ_ONLY))
+    {
+        usageFlags |= VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT;
+    }
+    else
+    {
+        usageFlags |= VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT | VK_BUFFER_USAGE_STORAGE_BUFFER_BIT;
+    }
+
+    return usageFlags;
+}
+
+VkMemoryPropertyFlags CLMemoryVk::getVkMemPropertyFlags()
+{
+    // TODO: http://anglebug.com/8588
+    VkMemoryPropertyFlags propFlags = VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT;
+
+    if (mMemory.getFlags().isSet(CL_MEM_USE_HOST_PTR | CL_MEM_ALLOC_HOST_PTR |
+                                 CL_MEM_COPY_HOST_PTR))
+    {
+        propFlags |= VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT;
+    }
+
+    return propFlags;
+}
+
+angle::Result CLMemoryVk::copyTo(void *dst, size_t srcOffset, size_t size)
+{
+    ANGLE_TRY(map());
+    void *src = reinterpret_cast<void *>(mMapPtr + srcOffset);
+    std::memcpy(dst, src, size);
+    ANGLE_TRY(unmap());
+    return angle::Result::Continue;
+}
+
+angle::Result CLMemoryVk::copyTo(CLMemoryVk *dst, size_t srcOffset, size_t dstOffset, size_t size)
+{
+    ANGLE_TRY(map());
+    ANGLE_TRY(dst->map());
+    uint8_t *ptr = nullptr;
+    ANGLE_TRY(dst->getMapPtr(&ptr));
+    ANGLE_TRY(copyTo(reinterpret_cast<void *>(ptr + dstOffset), srcOffset, size));
+    ANGLE_TRY(unmap());
+    ANGLE_TRY(dst->unmap());
+    return angle::Result::Continue;
+}
+
+angle::Result CLMemoryVk::copyFrom(const void *src, size_t srcOffset, size_t size)
+{
+    ANGLE_TRY(map());
+    void *dst = reinterpret_cast<void *>(reinterpret_cast<uintptr_t>(mMapPtr) + srcOffset);
+    std::memcpy(dst, src, size);
+    ANGLE_TRY(unmap());
+    return angle::Result::Continue;
+}
+
+CLBufferVk::CLBufferVk(const cl::Buffer &buffer) : CLMemoryVk(buffer)
+{
+    if (buffer.isSubBuffer())
+    {
+        mParent = &buffer.getParent()->getImpl<CLBufferVk>();
+    }
+    mDefaultBCI = {
+        .sType                 = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO,
+        .pNext                 = nullptr,
+        .flags                 = 0x0,
+        .size                  = 0u,
+        .usage                 = getVkUsageFlags(),
+        .sharingMode           = VK_SHARING_MODE_EXCLUSIVE,
+        .queueFamilyIndexCount = 0,
+        .pQueueFamilyIndices   = nullptr,
+    };
+}
+
+CLBufferVk::~CLBufferVk()
+{
+    ANGLE_CL_IMPL_TRY(unmap());
+    mBuffer.destroy(mRenderer);
+}
+
+angle::Result CLBufferVk::createSubBuffer(const cl::Buffer &buffer,
+                                          cl::MemFlags flags,
+                                          size_t size,
+                                          CLMemoryImpl::Ptr *subBufferOut)
+{
+    UNIMPLEMENTED();
+    ANGLE_CL_RETURN_ERROR(CL_OUT_OF_RESOURCES);
+}
+
+angle::Result CLBufferVk::create(size_t size)
+{
+    if (!isSubBuffer())
+    {
+        VkBufferCreateInfo createInfo  = mDefaultBCI;
+        createInfo.size                = size;
+        VkMemoryPropertyFlags memFlags = getVkMemPropertyFlags();
+        if (IsError(mBuffer.init(mContext, createInfo, memFlags)))
+        {
+            ANGLE_CL_RETURN_ERROR(CL_OUT_OF_RESOURCES);
+        }
+        if (mMemory.getFlags().isSet(CL_MEM_USE_HOST_PTR | CL_MEM_COPY_HOST_PTR))
+        {
+            ASSERT(mMemory.getHostPtr());
+            ANGLE_CL_IMPL_TRY_ERROR(
+                setDataImpl(static_cast<uint8_t *>(mMemory.getHostPtr()), size, 0),
+                CL_OUT_OF_RESOURCES);
+        }
+    }
+    return angle::Result::Continue;
+}
+
+angle::Result CLBufferVk::map()
+{
+    if (mParent != nullptr)
+    {
+        ANGLE_TRY(mParent->getMapPtr(&mMapPtr));
+        if (mMapPtr != nullptr)
+        {
+            mMapPtr += mBuffer.getOffset();
+            return angle::Result::Continue;
+        }
+        ANGLE_CL_RETURN_ERROR(CL_OUT_OF_RESOURCES);
+    }
+
+    if (!IsError(mBuffer.map(mContext, &mMapPtr)))
+    {
+        return angle::Result::Continue;
+    }
+
+    ANGLE_CL_RETURN_ERROR(CL_OUT_OF_RESOURCES);
+}
+
+angle::Result CLBufferVk::unmap()
+{
+    if (mParent != nullptr)
+    {
+        ANGLE_CL_IMPL_TRY_ERROR(mParent->unmap(), CL_OUT_OF_RESOURCES);
+        return angle::Result::Continue;
+    }
+    mBuffer.unmap(mRenderer);
+    mMapPtr = nullptr;
+    return angle::Result::Continue;
+}
+
+angle::Result CLBufferVk::setDataImpl(const uint8_t *data, size_t size, size_t offset)
 {
     UNIMPLEMENTED();
     ANGLE_CL_RETURN_ERROR(CL_OUT_OF_RESOURCES);
