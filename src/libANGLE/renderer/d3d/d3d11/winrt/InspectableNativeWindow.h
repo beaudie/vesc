4b67e65837b587e204f94b337a4b6ca8d87259f2
Matthew Leibowitz <mattleibow@live.com>
2024-01-22 23:07:02 +0200

    Add support for WinUI3/Windows App SDK
    Adds the necessary build args and code changes to
    support building ANGLE for WinUI 3 (aka Windows
    App SDK).
    
    To reduce the set of changes, and because it is
    functionally the same, a Windows App SDK build is
    basically a UWP build, but with an extra define so
    that the code can make a few changes to the
    namespaces and includes used.
    
    The main changes to the code are:
    - ICoreWindow is no longer used
    - Dispatcher has a few changes in naming and args
    - ISwapChainPanel is now in a different namesapce
    
    Bug: angleproject:8490
    Change-Id: Ibb298e3e86e8298dac12c2019eac7996a8185c51

--- src/libANGLE/renderer/d3d/d3d11/winrt/InspectableNativeWindow.h
+++ src/libANGLE/renderer/d3d/d3d11/winrt/InspectableNativeWindow.h
@@ -19,8 +19,16 @@
 
 #include <windows.applicationmodel.core.h>
 #undef GetCurrentTime
-#include <windows.ui.xaml.h>
-#include <windows.ui.xaml.media.dxinterop.h>
+#if defined(ANGLE_ENABLE_WINDOWS_APP_SDK)
+#    include <microsoft.ui.dispatching.h>
+#    include <microsoft.ui.xaml.h>
+#    include <microsoft.ui.xaml.media.dxinterop.h>
+using ISwapChainPanel = ABI::Microsoft::UI::Xaml::Controls::ISwapChainPanel;
+#else
+#    include <windows.ui.xaml.h>
+#    include <windows.ui.xaml.media.dxinterop.h>
+using ISwapChainPanel = ABI::Windows::UI::Xaml::Controls::ISwapChainPanel;
+#endif
 #include <wrl.h>
 #include <wrl/wrappers/corewrappers.h>
 
@@ -120,9 +128,8 @@ class InspectableNativeWindow
 
 bool IsCoreWindow(EGLNativeWindowType window,
                   ComPtr<ABI::Windows::UI::Core::ICoreWindow> *coreWindow = nullptr);
-bool IsSwapChainPanel(
-    EGLNativeWindowType window,
-    ComPtr<ABI::Windows::UI::Xaml::Controls::ISwapChainPanel> *swapChainPanel = nullptr);
+bool IsSwapChainPanel(EGLNativeWindowType window,
+                      ComPtr<ISwapChainPanel> *swapChainPanel = nullptr);
 bool IsEGLConfiguredPropertySet(
     EGLNativeWindowType window,
     ABI::Windows::Foundation::Collections::IPropertySet **propertySet = nullptr,
