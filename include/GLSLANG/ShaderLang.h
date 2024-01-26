5cbaa57ce11684cad6c127025540c967f7d9785e
Shahbaz Youssefi <syoussefi@chromium.org>
2024-01-26 14:37:42 -0500

    Vulkan: Workaround Nvidia driver bug with OpSelect
    Bug: angleproject:8503
    Change-Id: I4bd2580056df3d970a1316d0dbeaaad7590aa947

--- include/GLSLANG/ShaderLang.h
+++ include/GLSLANG/ShaderLang.h
@@ -26,7 +26,7 @@
 
 // Version number for shader translation API.
 // It is incremented every time the API changes.
-#define ANGLE_SH_VERSION 347
+#define ANGLE_SH_VERSION 348
 
 enum ShShaderSpec
 {
@@ -437,6 +437,9 @@ struct ShCompileOptions
 
     // Pre-transform explicit cubemap derivatives for Apple GPUs.
     uint64_t preTransformTextureCubeGradDerivatives : 1;
+
+    // Workaround for a driver bug with the use of the OpSelect SPIR-V instruction.
+    uint64_t avoidOpSelectWithMismatchingRelaxedPrecision : 1;
 
     ShCompileOptionsMetal metal;
     ShPixelLocalStorageOptions pls;
