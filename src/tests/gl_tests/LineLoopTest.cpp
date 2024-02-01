4cfd7387ef14a8f374cfe461fc6fadcd9aeb5f2c
Shahbaz Youssefi <syoussefi@chromium.org>
2024-02-01 14:40:15 -0500

    Add a line loop test
    Bug: chromium:1443177
    Change-Id: I96d0690cde3e02dfa8bb3095861b3e41e1ad92b4

--- src/tests/gl_tests/LineLoopTest.cpp
+++ src/tests/gl_tests/LineLoopTest.cpp
@@ -655,6 +655,87 @@ TEST_P(LineLoopIndirectTest, UseAsUBOThenUpdateThenUShortIndexIndirectBuffer)
     runTest(GL_UNSIGNED_SHORT, reinterpret_cast<const void *>(indices), sizeof(indices), 1, true);
 }
 
+// Regression test for a user bug
+TEST_P(LineLoopTest, Bug)
+{
+    constexpr char kVS[] = R"(
+attribute float vertexId;
+uniform float vertexCount;
+uniform float time;
+uniform vec2 resolution;
+
+varying vec4 v_color;
+
+void main() {
+  float phi = vertexId / vertexCount * 3.14159265*2.0;
+  
+  float x = cos(phi);
+  float y = sin(phi);
+  float z = 0.0;
+  
+  float theta = sin(cos(phi)*3.1415192 + time);//(x+time * 0.3) * 3.141592*2.0;
+  
+  theta *= 10.0;
+  
+  y *= cos(theta);
+  z = sin(theta) * sin(phi);
+  
+  vec4 pos = vec4(x, y, z, 1.0);
+    
+  vec3 eye = vec3(sin(time*0.3), 0, cos(time*0.3));
+  vec3 right = vec3(-eye.z, 0, eye.x);
+  vec3 look = -eye;
+
+  mat4 L = mat4(vec4(right, 0),
+                vec4(0, 1, 0, 0),
+                vec4(look, 0),
+                vec4(0, 0, 0, 1));
+  
+  gl_Position = (L * pos) * vec4((resolution.y / resolution.x) * 0.5, 0.5, 0.5, 1);
+  v_color = vec4(sin(theta) * 0.5 + 0.5,
+                 cos(phi) * 0.5 + 0.5,
+                 sin(time) * 0.5 + 0.5,
+                 1.0);
+})";
+
+    constexpr char kFS[] = R"(precision mediump float;
+varying vec4 v_color;
+void main() {
+    gl_FragColor = v_color;
+})";
+
+    ANGLE_GL_PROGRAM(program, kVS, kFS);
+    glUseProgram(program);
+
+    const int timeLoc        = glGetUniformLocation(program, "time");
+    const int vertexCountLoc = glGetUniformLocation(program, "vertexCount");
+    const int resolutionLoc  = glGetUniformLocation(program, "resolution");
+
+    glUniform1f(timeLoc, 123.345);
+    glUniform1f(vertexCountLoc, 2048);
+    glUniform2f(resolutionLoc, getWindowWidth(), getWindowHeight());
+
+    std::vector<float> vertexId(2048);
+    for (size_t i = 0; i < vertexId.size(); ++i)
+        vertexId[i] = (float)i;
+
+    GLBuffer buffer;
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, 2048 * sizeof(float), vertexId.data(), GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 1, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(0);
+
+    glClearColor(0, 0, 0, 1);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
+    glLineWidth(1);
+
+    glDrawArrays(GL_LINE_LOOP, 0, 2048);
+    ASSERT_GL_NO_ERROR();
+}
+
 ANGLE_INSTANTIATE_TEST_ES2(LineLoopTest);
 ANGLE_INSTANTIATE_TEST_ES3(LineLoopTestES3);
 
