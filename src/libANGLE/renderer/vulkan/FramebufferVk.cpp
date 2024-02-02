94dc3942b1b7b1e089d48411f5f784370b34cc09
Charlie Lao <cclao@google.com>
2024-02-02 14:13:33 -0800

    Debug: Add support for RefCountedEvent
    Bug: b/317411251
    Change-Id: Idaf5a7200b85f901eae5d376543f189d21522022

--- src/libANGLE/renderer/vulkan/FramebufferVk.cpp
+++ src/libANGLE/renderer/vulkan/FramebufferVk.cpp
@@ -1097,6 +1097,7 @@ angle::Result FramebufferVk::blitWithCommand(ContextVk *contextVk,
     commandBuffer->blitImage(srcImage->getImage(), VK_IMAGE_LAYOUT_TRANSFER_SRC_OPTIMAL,
                              dstImage->getImage(), VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, 1, &blit,
                              gl_vk::GetFilter(filter));
+    contextVk->trackImagesWithOutsideRenderPassEvent(srcImage, dstImage);
 
     return angle::Result::Continue;
 }
@@ -1729,6 +1730,7 @@ angle::Result FramebufferVk::resolveColorWithCommand(ContextVk *contextVk,
 
         perfCounters.resolveImageCommands++;
     }
+    contextVk->trackImagesWithOutsideRenderPassEvent(srcImage, nullptr);
 
     return angle::Result::Continue;
 }
