b6429e2bc655426b16451c2cf0fb0826743e636a
Charlie Lao <cclao@google.com>
2024-02-14 12:48:49 -0800

    Experiment: Skip image barrier in certain cases
    Bug: b/308455694
    Change-Id: Ib212335f4b0b9d8052209d9e577ad94cbeef6c48

--- src/libANGLE/renderer/vulkan/vk_helpers.cpp
+++ src/libANGLE/renderer/vulkan/vk_helpers.cpp
@@ -6729,7 +6729,7 @@ void ImageHelper::barrierImpl(Context *context,
     // Release the ANI semaphore to caller to add to the command submission.
     *acquireNextImageSemaphoreOut = mAcquireNextImageSemaphore.release();
 
-    if (mCurrentLayout == ImageLayout::SharedPresent)
+    if (mCurrentLayout == ImageLayout::SharedPresent || mCurrentLayout == newLayout)
     {
         const ImageMemoryBarrierData &transition = kImageMemoryBarrierData[mCurrentLayout];
 
