7c037f02bd4b3929b9932d7049de5c4230aabdcb
Amirali Abdolrashidi <abdolrashidi@google.com>
2024-02-15 11:27:39 -0800

    Vulkan: Add per-level image update tracker
    * Add a per-level image update tracker to ImageHelper.
    
      * It tracks the updates scheduled for different parts of the image.
        Within each level, it also tracks different layers, currently up
        to 64.
        * kMaxParallelSubresourceUpload; moved to vk_helper header.
    
    * Modified ImageHelper::recordWriteBarrier().
    
      * Added isWriteBarrierNecessary().
    
      * Now it checks the added updates for the image. It will no longer
        issue a barrier if the image is in the same layout and there is
        no write to a part of the image to which was previously written.
    
      * Currently, a barrier would still be issued for layouts that allow
        both reading and writing to the image (including self-copy):
        * TransferSrcDst (used in CopyImageSubData)
        * ComputeShaderWrite (used in compute-based mipmap generation)
    
    * Improves gpu_time for TextureUploadETC2TranscodingBenchmark perf test
    
    Bug: b/308455694
    Change-Id: I5cef296d991ce6ec02792edc3ffc5cc4994831e1

--- src/libANGLE/renderer/vulkan/vk_helpers.h
+++ src/libANGLE/renderer/vulkan/vk_helpers.h
@@ -49,6 +49,13 @@ constexpr VkPipelineStageFlags kSwapchainAcquireImageWaitStageFlags =
     VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT |  // First use is a draw command.
     VK_PIPELINE_STAGE_TRANSFER_BIT;                  // First use is a clear without scissor.
 
+// For each level, upload layers that don't conflict in parallel.  The layer is hashed to
+// `layer % kMaxParallelSubresourceUpload` and used to track whether that subresource is currently
+// in transfer.  If so, a barrier is inserted.  If mLayerCount > kMaxParallelSubresourceUpload,
+// there will be a few unnecessary barriers.
+constexpr uint32_t kMaxParallelSubresourceUpload = 64;
+using SubresourceLayerMask                       = std::bitset<kMaxParallelSubresourceUpload>;
+
 using StagingBufferOffsetArray = std::array<VkDeviceSize, 2>;
 
 // A dynamic buffer is conceptually an infinitely long buffer. Each time you write to the buffer,
@@ -2318,6 +2325,10 @@ class ImageHelper final : public Resource, public angle::Subject
     void recordWriteBarrier(Context *context,
                             VkImageAspectFlags aspectMask,
                             ImageLayout newLayout,
+                            gl::LevelIndex levelStart,
+                            uint32_t levelCount,
+                            uint32_t layerStart,
+                            uint32_t layerCount,
                             OutsideRenderPassCommandBufferHelper *commands);
 
     void recordWriteBarrierOneOff(Context *context,
@@ -2331,6 +2342,11 @@ class ImageHelper final : public Resource, public angle::Subject
 
     // This function can be used to prevent issuing redundant layout transition commands.
     bool isReadBarrierNecessary(ImageLayout newLayout) const;
+    bool isWriteBarrierNecessary(ImageLayout newLayout,
+                                 gl::LevelIndex levelStart,
+                                 uint32_t levelCount,
+                                 uint32_t layerStart,
+                                 uint32_t layerCount) const;
 
     void recordReadBarrier(Context *context,
                            VkImageAspectFlags aspectMask,
@@ -2649,6 +2665,19 @@ class ImageHelper final : public Resource, public angle::Subject
                      CommandBufferT *commandBuffer,
                      VkSemaphore *acquireNextImageSemaphoreOut);
 
+    void setSubresourcesWrittenSinceBarrier(gl::LevelIndex levelStart,
+                                            uint32_t levelCount,
+                                            uint32_t layerStart,
+                                            uint32_t layerCount);
+
+    void resetSubresourcesWrittenSinceBarrier()
+    {
+        for (auto &bitArray : mSubresourcesWrittenSinceBarrier)
+        {
+            bitArray.reset();
+        }
+    }
+
     // If the image has emulated channels, we clear them once so as not to leave garbage on those
     // channels.
     VkColorComponentFlags getEmulatedChannelsMask() const;
@@ -2907,6 +2936,10 @@ class ImageHelper final : public Resource, public angle::Subject
     // Only used for swapChain images. This is set when an image is acquired and is waited on
     // by the next submission (which uses this image), at which point it is released.
     Semaphore mAcquireNextImageSemaphore;
+
+    // Used to track subresource updates per level. This can help parallelize updates performed to
+    // different levels or layers of the image. See comment on kMaxParallelSubresourceUpload.
+    gl::TexLevelArray<SubresourceLayerMask> mSubresourcesWrittenSinceBarrier;
 };
 
 ANGLE_INLINE bool RenderPassCommandBufferHelper::usesImage(const ImageHelper &image) const
