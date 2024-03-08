a45d4f90d65dda4eeda0228f2cef527a4218cccd
Gowtham Tammana <g.tammana@samsung.com>
2024-03-07 20:01:34 -0600

    CL/VK: Fix get mapped memory
    Bug: angleproject:8641
    Change-Id: I3ad18fac7724750c9e11760c2d039f04ccd5d424
    Signed-off-by: Gowtham Tammana <g.tammana@samsung.com>

--- src/libANGLE/renderer/vulkan/CLCommandQueueVk.cpp
+++ src/libANGLE/renderer/vulkan/CLCommandQueueVk.cpp
@@ -291,8 +291,7 @@ angle::Result CLCommandQueueVk::enqueueCopyBufferRect(const cl::Buffer &srcBuffe
     auto srcBufferVk    = &srcBuffer.getImpl<CLBufferVk>();
     auto dstBufferVk    = &dstBuffer.getImpl<CLBufferVk>();
     uint8_t *mapPointer = nullptr;
-    ANGLE_TRY(srcBufferVk->getMapPtr(&mapPointer));
-    ASSERT(mapPointer);
+    ANGLE_TRY(srcBufferVk->map(mapPointer));
     ANGLE_TRY(dstBufferVk->setRect(static_cast<const void *>(mapPointer), srcRect, dstRect));
 
     ANGLE_TRY(createEvent(eventCreateFunc));
@@ -420,8 +419,8 @@ angle::Result CLCommandQueueVk::enqueueUnmapMemObject(const cl::Memory &memory,
                                                       CLEventImpl::CreateFunc *eventCreateFunc)
 {
     ANGLE_TRY(enqueueWaitForEvents(waitEvents));
-    ANGLE_TRY(createEvent(eventCreateFunc, waitEvents));
-    ANGLE_TRY(memory.getImpl<CLMemoryVk>().unmap());
+    memory.getImpl<CLMemoryVk>().unmap();
+    ANGLE_TRY(createEvent(eventCreateFunc));
 
     return angle::Result::Continue;
 }
