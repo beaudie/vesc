41ad63e8d70aa030bf8bbc52f2cf460e9216126d
Gowtham Tammana <g.tammana@samsung.com>
2024-03-07 20:01:34 -0600

    CL/VK: Fix get mapped memory
    Bug: angleproject:8641
    Change-Id: I3ad18fac7724750c9e11760c2d039f04ccd5d424
    Signed-off-by: Gowtham Tammana <g.tammana@samsung.com>

--- src/libANGLE/renderer/vulkan/CLMemoryVk.cpp
+++ src/libANGLE/renderer/vulkan/CLMemoryVk.cpp
@@ -21,8 +21,7 @@ CLMemoryVk::CLMemoryVk(const cl::Memory &memory)
     : CLMemoryImpl(memory),
       mContext(&memory.getContext().getImpl<CLContextVk>()),
       mRenderer(mContext->getRenderer()),
-      mMapPtr(nullptr),
-      mMapCount(0),
+      mMappedMemory(nullptr),
       mParent(nullptr)
 {}
 
@@ -38,16 +37,6 @@ angle::Result CLMemoryVk::createSubBuffer(const cl::Buffer &buffer,
 {
     UNIMPLEMENTED();
     ANGLE_CL_RETURN_ERROR(CL_OUT_OF_RESOURCES);
-}
-
-angle::Result CLMemoryVk::getMapPtr(uint8_t **mapPtrOut)
-{
-    if (mMapPtr == nullptr && mapPtrOut != nullptr)
-    {
-        ANGLE_TRY(map());
-        *mapPtrOut = mMapPtr;
-    }
-    return angle::Result::Continue;
 }
 
 VkBufferUsageFlags CLMemoryVk::getVkUsageFlags()
@@ -87,31 +76,28 @@ VkMemoryPropertyFlags CLMemoryVk::getVkMemPropertyFlags()
 
 angle::Result CLMemoryVk::copyTo(void *dst, size_t srcOffset, size_t size)
 {
-    ANGLE_TRY(map());
-    void *src = reinterpret_cast<void *>(mMapPtr + srcOffset);
+    uint8_t *src = nullptr;
+    ANGLE_TRY(map(src, srcOffset));
     std::memcpy(dst, src, size);
-    ANGLE_TRY(unmap());
+    unmap();
     return angle::Result::Continue;
 }
 
 angle::Result CLMemoryVk::copyTo(CLMemoryVk *dst, size_t srcOffset, size_t dstOffset, size_t size)
 {
-    ANGLE_TRY(map());
-    ANGLE_TRY(dst->map());
-    uint8_t *ptr = nullptr;
-    ANGLE_TRY(dst->getMapPtr(&ptr));
-    ANGLE_TRY(copyTo(reinterpret_cast<void *>(ptr + dstOffset), srcOffset, size));
-    ANGLE_TRY(unmap());
-    ANGLE_TRY(dst->unmap());
+    uint8_t *dstPtr = nullptr;
+    ANGLE_TRY(dst->map(dstPtr, dstOffset));
+    ANGLE_TRY(copyTo(dstPtr, srcOffset, size));
+    dst->unmap();
     return angle::Result::Continue;
 }
 
 angle::Result CLMemoryVk::copyFrom(const void *src, size_t srcOffset, size_t size)
 {
-    ANGLE_TRY(map());
-    void *dst = reinterpret_cast<void *>(reinterpret_cast<uintptr_t>(mMapPtr) + srcOffset);
+    uint8_t *dst = nullptr;
+    ANGLE_TRY(map(dst, srcOffset));
     std::memcpy(dst, src, size);
-    ANGLE_TRY(unmap());
+    unmap();
     return angle::Result::Continue;
 }
 
@@ -130,7 +116,10 @@ CLBufferVk::CLBufferVk(const cl::Buffer &buffer) : CLMemoryVk(buffer)
 
 CLBufferVk::~CLBufferVk()
 {
-    ANGLE_CL_IMPL_TRY(unmap());
+    if (isMapped())
+    {
+        unmap();
+    }
     mBuffer.destroy(mRenderer);
 }
 
@@ -164,36 +153,37 @@ angle::Result CLBufferVk::create(void *hostPtr)
     return angle::Result::Continue;
 }
 
-angle::Result CLBufferVk::map()
+angle::Result CLBufferVk::mapImpl()
 {
-    if (mParent != nullptr)
-    {
-        ANGLE_TRY(mParent->getMapPtr(&mMapPtr));
-        if (mMapPtr != nullptr)
-        {
-            mMapPtr += mBuffer.getOffset();
-            return angle::Result::Continue;
-        }
-        ANGLE_CL_RETURN_ERROR(CL_OUT_OF_RESOURCES);
-    }
+    ASSERT(!isMapped());
 
-    if (!IsError(mBuffer.map(mContext, &mMapPtr)))
+    if (isSubBuffer())
     {
+        ANGLE_TRY(mParent->map(mMappedMemory, static_cast<size_t>(mBuffer.getOffset())));
         return angle::Result::Continue;
     }
 
-    ANGLE_CL_RETURN_ERROR(CL_OUT_OF_RESOURCES);
+    ANGLE_TRY(mBuffer.map(mContext, &mMappedMemory));
+    return angle::Result::Continue;
 }
 
-angle::Result CLBufferVk::unmap()
+void CLBufferVk::unmapImpl()
 {
-    if (mParent != nullptr)
+    if (isSubBuffer())
     {
-        ANGLE_CL_IMPL_TRY_ERROR(mParent->unmap(), CL_OUT_OF_RESOURCES);
-        return angle::Result::Continue;
+        mMappedMemory = nullptr;
     }
     mBuffer.unmap(mRenderer);
-    mMapPtr = nullptr;
+    mMappedMemory = nullptr;
+}
+
+angle::Result CLMemoryVk::map(uint8_t *&ptrOut, size_t offset)
+{
+    if (!isMapped())
+    {
+        ANGLE_TRY(mapImpl());
+    }
+    ptrOut = mMappedMemory + offset;
     return angle::Result::Continue;
 }
 
@@ -205,7 +195,7 @@ angle::Result CLBufferVk::setRect(const void *data,
     ASSERT(srcRect.mSize == rect.mSize);
 
     uint8_t *mapPtr = nullptr;
-    ANGLE_TRY(getMapPtr(&mapPtr));
+    ANGLE_TRY(map(mapPtr));
     const uint8_t *srcData = reinterpret_cast<const uint8_t *>(data);
     for (int slice = 0; slice < rect.mSize.depth; slice++)
     {
@@ -229,7 +219,7 @@ angle::Result CLBufferVk::getRect(const cl::BufferRect &srcRect,
     ASSERT(srcRect.mSize == outRect.mSize);
 
     uint8_t *mapPtr = nullptr;
-    ANGLE_TRY(getMapPtr(&mapPtr));
+    ANGLE_TRY(map(mapPtr));
     uint8_t *dstData = reinterpret_cast<uint8_t *>(outData);
     for (int slice = 0; slice < srcRect.mSize.depth; slice++)
     {
@@ -263,8 +253,7 @@ angle::Result CLBufferVk::setDataImpl(const uint8_t *data, size_t size, size_t o
     }
 
     uint8_t *mapPointer = nullptr;
-    ANGLE_TRY(mBuffer.mapWithOffset(mContext, &mapPointer, offset));
-    ASSERT(mapPointer != nullptr);
+    ANGLE_TRY(map(mapPointer));
 
     std::memcpy(mapPointer, data, size);
     mBuffer.unmap(mRenderer);
