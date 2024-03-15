f3d04f2793b4c04c1fb80df8f8892cd882283633
Kimmo Kinnunen <kkinnunen@apple.com>
2024-03-15 15:39:39 +0200

    Make separated anonymous in/out structs work better
    Fixes failing case for other backends than GLSL output:
    VS:
    out struct { .. } a, b;
    
    FS:
    in struct { ..  } a, b
    
    Make the ANGLE VS-FS interface matching work by using the anonymous name
    as the variable structOrBlock name.
    
    Bug: angleproject:8590
    Change-Id: If8107387c02b5520134857fb210a27a8f92a2db0

--- src/tests/gl_tests/GLSLTest.cpp
+++ src/tests/gl_tests/GLSLTest.cpp
@@ -8810,6 +8810,205 @@ void main()
     EXPECT_PIXEL_COLOR_EQ(0, 0, GLColor::green);
 }
 
+// Test that a varying anonymous struct that is defined as a part of the declaration is handled
+// correctly.
+TEST_P(GLSLTest_ES3, VaryingAnonymousStructWithInlineDefinition)
+{
+    // TODO(anglebug.com/5491): see base case.
+    ANGLE_SKIP_TEST_IF(IsIOS() && IsOpenGLES());
+    constexpr char kVS[] = R"(#version 300 es
+in vec4 inputAttribute;
+
+flat out struct
+{
+    int field;
+} v_s;
+
+void main()
+{
+    v_s.field = 1;
+    gl_Position = inputAttribute;
+})";
+
+    constexpr char kFS[] = R"(#version 300 es
+
+precision highp float;
+out vec4 my_FragColor;
+
+flat in struct
+{
+    int field;
+} v_s;
+
+void main()
+{
+    bool success = (v_s.field == 1);
+    my_FragColor = vec4(1, 0, 0, 1);
+    if (success)
+    {
+        my_FragColor = vec4(0, 1, 0, 1);
+    }
+})";
+
+    ANGLE_GL_PROGRAM(program, kVS, kFS);
+    drawQuad(program.get(), "inputAttribute", 0.5f);
+    EXPECT_PIXEL_COLOR_EQ(0, 0, GLColor::green);
+}
+
+// Test that a varying anonymous structs that are defined as a part of the declaration is handled
+// correctly.
+TEST_P(GLSLTest_ES3, VaryingAnonymousStructWithInlineDefinition2)
+{
+    // TODO(anglebug.com/8613): Does not work due to SeparateDeclarations.
+    ANGLE_SKIP_TEST_IF(IsOpenGLES() || IsOpenGL());
+
+    // TODO(anglebug.com/5491): see base case.
+    ANGLE_SKIP_TEST_IF(IsIOS() && IsOpenGLES());
+    constexpr char kVS[] = R"(#version 300 es
+in vec4 inputAttribute;
+flat out struct
+{
+    int field;
+} v_s0, v_s1;
+void main()
+{
+    v_s0.field = 1;
+    v_s1.field = 2;
+    gl_Position = inputAttribute;
+})";
+
+    constexpr char kFS[] = R"(#version 300 es
+precision highp float;
+out vec4 my_FragColor;
+flat in struct
+{
+    int field;
+} v_s0, v_s1;
+void main()
+{
+    bool success = (v_s0.field == 1 && v_s1.field == 2);
+    my_FragColor = vec4(1, 0, 0, 1);
+    if (success)
+    {
+        my_FragColor = vec4(0, 1, 0, 1);
+    }
+})";
+
+    ANGLE_GL_PROGRAM(program, kVS, kFS);
+    drawQuad(program.get(), "inputAttribute", 0.5f);
+    EXPECT_PIXEL_COLOR_EQ(0, 0, GLColor::green);
+}
+
+// Test that a varying anonymous structs that are defined as a part of the declaration is handled
+// in a specific way. Highlights ambiguity of ES "Chapter 9. Shader Interface Matching":
+//  "When linking shaders, the type of declared vertex outputs and fragment inputs with the same
+//  name must match"
+TEST_P(GLSLTest_ES3, VaryingAnonymousStructWithInlineDefinition3)
+{
+    // TODO(anglebug.com/8613): Does not work due to SeparateDeclarations.
+    ANGLE_SKIP_TEST_IF(IsOpenGLES() || IsOpenGL());
+
+    // TODO(anglebug.com/5491): see base case.
+    ANGLE_SKIP_TEST_IF(IsIOS() && IsOpenGLES());
+    constexpr char kVS[] = R"(#version 300 es
+in vec4 inputAttribute;
+flat out struct
+{
+    int field;
+} v_s0;
+flat out struct
+{
+    int field;
+} v_s1;
+flat out struct
+{
+    int field;
+} v_s2, v_s3;
+
+void main()
+{
+    v_s0.field = 1;
+    v_s1.field = 2;
+    v_s2.field = 3;
+    v_s3.field = 4;
+    gl_Position = inputAttribute;
+})";
+
+    constexpr char kFS[] = R"(#version 300 es
+precision highp float;
+out vec4 my_FragColor;
+flat in struct
+{
+    int field;
+} v_s0, v_s1, v_s2, v_s3;
+void main()
+{
+    bool success = v_s0.field == 1 && v_s1.field == 2 && v_s2.field == 3 && v_s3.field == 4;
+    my_FragColor = vec4(1, 0, 0, 1);
+    if (success)
+    {
+        my_FragColor = vec4(0, 1, 0, 1);
+    }
+})";
+
+    ANGLE_GL_PROGRAM(program, kVS, kFS);
+    drawQuad(program.get(), "inputAttribute", 0.5f);
+    EXPECT_PIXEL_COLOR_EQ(0, 0, GLColor::green);
+}
+
+// Test that a varying anonymous structs can be compared for equality.
+TEST_P(GLSLTest_ES3, VaryingAnonymousStructEquality)
+{
+    // TODO(anglebug.com/8613): Does not work due to SeparateDeclarations.
+    ANGLE_SKIP_TEST_IF(IsOpenGLES() || IsOpenGL());
+
+    constexpr char kVS[] = R"(#version 300 es
+in vec4 inputAttribute;
+flat out struct
+{
+    int field;
+} v_s0;
+flat out struct
+{
+    int field;
+} v_s1;
+flat out struct
+{
+    int field;
+} v_s2, v_s3;
+
+void main()
+{
+    v_s0.field = 1;
+    v_s1.field = 2;
+    v_s2.field = 3;
+    v_s3.field = 4;
+    gl_Position = inputAttribute;
+})";
+
+    constexpr char kFS[] = R"(#version 300 es
+precision highp float;
+out vec4 my_FragColor;
+flat in struct
+{
+    int field;
+} v_s0, v_s1, v_s2, v_s3;
+void main()
+{
+    bool success = v_s0 != v_s1 && v_s0 != v_s2 && v_s0 != v_s3 && v_s1 != v_s2 && v_s1 != v_s3 && v_s2 != v_s3;
+    success = success && v_s0.field == 1 && v_s1.field == 2 && v_s2.field == 3 && v_s3.field == 4;
+    my_FragColor = vec4(1, 0, 0, 1);
+    if (success)
+    {
+        my_FragColor = vec4(0, 1, 0, 1);
+    }
+})";
+
+    ANGLE_GL_PROGRAM(program, kVS, kFS);
+    drawQuad(program.get(), "inputAttribute", 0.5f);
+    EXPECT_PIXEL_COLOR_EQ(0, 0, GLColor::green);
+}
+
 // Test mismatched precision in varying is handled correctly.
 TEST_P(GLSLTest_ES3, MismatchPrecisionFloat)
 {
