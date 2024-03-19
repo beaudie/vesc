3d26b22c8948bf5569ad43d2d124371927e6c36e
Roman Lavrov <romanl@google.com>
2024-03-19 17:42:18 -0400

    win-trace log build time
    Bug: None
    Change-Id: I1504dc40f46133518848b850983e7787927a20c6

--- src/tests/capture_replay_tests.py
+++ src/tests/capture_replay_tests.py
@@ -218,7 +218,10 @@ class ChildProcessesManager():
         cmd = [sys.executable, self._autoninja_path, '-C', build_dir, target]
         with self._ninja_lock:
             self._logger.info(' '.join(cmd))
-            return self.RunSubprocess(cmd, pipe_stdout=pipe_stdout)
+            start_time = time.time()
+            result = self.RunSubprocess(cmd, pipe_stdout=pipe_stdout)
+            self._logger.info('Build finished in %.1fs', time.time() - start_time)
+            return result
 
 
 def GetTestsListForFilter(args, test_path, filter, logger):
@@ -497,7 +500,7 @@ class TestBatch():
                               output, tests))
             return False
         returncode, output = child_processes_manager.RunAutoNinja(replay_build_dir, REPLAY_BINARY,
-                                                                  True)
+                                                                  False)
         if returncode != 0:
             self.logger.warning('Ninja failure output: %s' % output)
             self.results.append(
@@ -520,8 +523,10 @@ class TestBatch():
         for test in tests:
             self.UnlinkContextStateJsonFilesIfPresent(replay_build_dir, test.GetLabel())
 
+        start_time = time.time()
         returncode, output = child_processes_manager.RunSubprocess(
             run_cmd, env, timeout=SUBPROCESS_TIMEOUT)
+        self.logger.info('Replay finished in %.1fs', time.time() - start_time)
         if returncode == -1:
             cmd = replay_exe_path
             self.results.append(
