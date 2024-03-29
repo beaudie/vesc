8bd49c852950e39b00be2c8e6d3050962de99ebb
Mohan Maiya <m.maiya@samsung.com>
2024-03-28 17:06:03 -0700

    ProgramExecutable manages post-link tasks
    Post-link tasks are now owned and managed by ProgramExecutable.
    This provides the backends the option to either not wait
    or defer for as long as possible the wait for post-link tasks.
    
    Bug: angleproject:8297
    Change-Id: I5dbed6a070efe851fc1376699e9eccd3a21bd03f

--- src/libANGLE/ProgramExecutable.cpp
+++ src/libANGLE/ProgramExecutable.cpp
@@ -735,6 +735,8 @@ ProgramExecutable::ProgramExecutable(rx::GLImplFactory *factory, InfoLog *infoLo
 
 ProgramExecutable::~ProgramExecutable()
 {
+    ASSERT(mPostLinkSubTasks.empty());
+    ASSERT(mPostLinkSubTaskWaitableEvents.empty());
     ASSERT(mImplementation == nullptr);
 }
 
@@ -820,6 +822,9 @@ void ProgramExecutable::reset()
     mSamplerBindings.clear();
     mSamplerBoundTextureUnits.clear();
     mImageBindings.clear();
+
+    mPostLinkSubTasks.clear();
+    mPostLinkSubTaskWaitableEvents.clear();
 }
 
 void ProgramExecutable::load(gl::BinaryInputStream *stream)
@@ -3150,6 +3155,16 @@ void ProgramExecutable::setBaseInstanceUniform(GLuint baseInstance)
     mCachedBaseInstance   = baseInstance;
     GLint baseInstanceInt = baseInstance;
     mImplementation->setUniform1iv(mPod.baseInstanceLocation, 1, &baseInstanceInt);
+}
+
+void ProgramExecutable::waitForPostLinkTasks(const Context *context)
+{
+    if (mPostLinkSubTasks.empty())
+    {
+        return;
+    }
+
+    mImplementation->waitForPostLinkTasks(context);
 }
 
 void InstallExecutable(const Context *context,
