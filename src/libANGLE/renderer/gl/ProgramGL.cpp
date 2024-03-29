b65ace7e541ff1107849738147f10c6ff2b4c1de
Mohan Maiya <m.maiya@samsung.com>
2024-03-28 17:06:03 -0700

    ProgramExecutable manages post-link tasks
    Post-link tasks are now owned and managed by ProgramExecutable.
    This provides the backends the option to either not wait
    or defer for as long as possible the wait for post-link tasks.
    
    Bug: angleproject:8297
    Change-Id: I5dbed6a070efe851fc1376699e9eccd3a21bd03f

--- src/libANGLE/renderer/gl/ProgramGL.cpp
+++ src/libANGLE/renderer/gl/ProgramGL.cpp
@@ -107,10 +107,16 @@ class ProgramGL::LinkTaskGL final : public LinkTask
     {}
     ~LinkTaskGL() override = default;
 
-    std::vector<std::shared_ptr<LinkSubTask>> link(const gl::ProgramLinkedResources &resources,
-                                                   const gl::ProgramMergedVaryings &mergedVaryings,
-                                                   bool *canSubTasksRunPostLinkOut) override
+    void link(const gl::ProgramLinkedResources &resources,
+              const gl::ProgramMergedVaryings &mergedVaryings,
+              std::vector<std::shared_ptr<LinkSubTask>> *linkSubTasksOut,
+              std::vector<std::shared_ptr<LinkSubTask>> *postLinkSubTasksOut) override
     {
+        ASSERT(linkSubTasksOut);
+        ASSERT(postLinkSubTasksOut);
+        linkSubTasksOut->clear();
+        postLinkSubTasksOut->clear();
+
         mProgram->linkJobImpl(mExtensions);
 
         // If there is no native parallel compile, do the post-link right away.
@@ -121,7 +127,7 @@ class ProgramGL::LinkTaskGL final : public LinkTask
 
         // See comment on mResources
         mResources = &resources;
-        return {};
+        return;
     }
 
     angle::Result getResult(const gl::Context *context, gl::InfoLog &infoLog) override
