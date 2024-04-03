93fb5a4fbd2032edb63b1f763d8f77b55f98e10b
Austin Annestrand <a.annestrand@samsung.com>
2024-04-03 11:45:05 -0500

    CL/VK: Update missing reflection ops & DS creation
    Adding CLProgramVk routine to allocate DS.
    Also adding reflection parsing support for
    WGS IDs and PushConstantGlobalSize.
    
    Additionally, we now check VK implementation
    if non-semantic reflection data (in SPIR-V) is
    supported. If not, we strip that data from binary.
    
    Bug: angleproject:8631
    Change-Id: Ife02867c7c30b919abf663865adc92858e1bff8d

--- src/libANGLE/renderer/vulkan/CLDeviceVk.h
+++ src/libANGLE/renderer/vulkan/CLDeviceVk.h
@@ -38,6 +38,14 @@ class CLDeviceVk : public CLDeviceImpl
                                    CreateFuncs &subDevices,
                                    cl_uint *numDevicesRet) override;
 
+    bool isVkDeviceExtensionEnabled(const char *extension) const
+    {
+        const vk::ExtensionNameList &enabledDeviceExtensions =
+            mRenderer->getEnabledDeviceExtensions();
+        return std::find(enabledDeviceExtensions.begin(), enabledDeviceExtensions.end(),
+                         extension) != enabledDeviceExtensions.end();
+    }
+
     // Returns runtime-selected LWS value
     cl::WorkgroupSize selectWorkGroupSize(const cl::NDRange &ndrange) const;
 
