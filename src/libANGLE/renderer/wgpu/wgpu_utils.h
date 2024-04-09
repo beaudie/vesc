fe3d02d7adc771d2d9ccf3a2c332904f2ab4100c
Liza Burakova <liza@chromium.org>
2024-04-09 17:10:20 -0400

    Begin initializing caps for webgpu
    Bug: angleproject:8457
    Change-Id: Idde0673297ec675a13bd3e4fd4d220ec47392153

--- src/libANGLE/renderer/wgpu/wgpu_utils.h
+++ src/libANGLE/renderer/wgpu/wgpu_utils.h
@@ -10,6 +10,7 @@
 #include <dawn/webgpu_cpp.h>
 #include <stdint.h>
 
+#include "libANGLE/Caps.h"
 #include "libANGLE/Error.h"
 #include "libANGLE/angletypes.h"
 
@@ -26,6 +27,7 @@ enum class RenderPassClosureReason
     InvalidEnum,
     EnumCount = InvalidEnum,
 };
+void EnsureCapsInitialized(const wgpu::Device &device, gl::Caps *nativeCaps);
 
 }  // namespace webgpu
 
