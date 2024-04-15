a04185c4586cd02c6146597c269d43a85bc2babd
Liza Burakova <liza@chromium.org>
2024-04-15 16:51:20 -0400

    Adding basic readPixels
    Bug: angleproject:8457
    Change-Id: I349ed8a0ae3d8d0e187c658f3402c4f8cac23eb8

--- src/libANGLE/renderer/wgpu/wgpu_helpers.h
+++ src/libANGLE/renderer/wgpu/wgpu_helpers.h
@@ -70,6 +70,23 @@ class ImageHelper
                                      const gl::ImageIndex &index,
                                      const uint8_t *pixels);
 
+    static angle::Result getReadPixelsParams(rx::ContextWgpu *contextWgpu,
+                                             const gl::PixelPackState &packState,
+                                             gl::Buffer *packBuffer,
+                                             GLenum format,
+                                             GLenum type,
+                                             const gl::Rectangle &area,
+                                             const gl::Rectangle &clippedArea,
+                                             rx::PackPixelsParams *paramsOut,
+                                             GLuint *skipBytesOut);
+
+    angle::Result readPixels(rx::ContextWgpu *contextWgpu,
+                             size_t allocationSize,
+                             const gl::Rectangle &area,
+                             const rx::PackPixelsParams &packPixelsParams,
+                             const angle::Format &aspectFormat,
+                             void *pixels);
+
     LevelIndex toWgpuLevel(gl::LevelIndex levelIndexGl) const;
     gl::LevelIndex toGlLevel(LevelIndex levelIndexWgpu) const;
     wgpu::Texture &getTexture() { return mTexture; }
@@ -120,6 +137,7 @@ class BufferHelper : public angle::NonCopyable
     angle::Result unmap();
 
     uint8_t *getMapWritePointer(size_t offset, size_t size) const;
+    const uint8_t *getMapReadPointer(size_t offset, size_t size) const;
 
     const std::optional<BufferMapState> &getMappedState() const;
 
