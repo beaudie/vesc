6b6bd90e0f1607e4c0545b1e0b0ad71c0eac87d8
Shahbaz Youssefi <syoussefi@chromium.org>
2024-04-16 13:27:19 -0400

    Manual roll VK-GL-CTS from 393ca590ec21 to b529676d0523 (9 revisions)
    https://chromium.googlesource.com/external/github.com/KhronosGroup/VK-GL-CTS.git/+log/393ca590ec21..b529676d0523
    
    2024-03-29 yuxinhu@google.com Move external/openglcts/data into a new
    subdirectory
    2024-03-29 lorenzo@khronosgroup.org Limit Vulkan validation layers
    compilation to Linux and Android
    2024-03-29 lorenzo@khronosgroup.org Merge vk-gl-cts/opengl-cts-4.6.4
    into vk-gl-cts/main
    2024-03-29 lorenzo@khronosgroup.org Merge vk-gl-cts/opengl-es-cts-3.2.10
    into vk-gl-cts/main
    2024-03-29 gleese@broadcom.com Remove deIsInf, deIsNaN and variants
    2024-03-29 gleese@broadcom.com Stop generating (some?) empty test groups
    2024-03-29 lionel.g.landwerlin@intel.com Fix protected memory
    allocations with unprotected buffers
    2024-03-29 yuxinhu@google.com Add a new Android Activity that exports
    ES32 Test Run Params
    2024-03-29 yuxinhu@google.com Add scripts to generate Android bp for
    compiling both dEQP and KHR GLES tests
    
    If this roll has caused a breakage, revert this CL and stop the roller
    using the controls here:
    https://autoroll.skia.org/r/vk-gl-cts-angle-autoroll
    Please CC angle-team@google.com,syoussefi@google.com on the revert to
    ensure that a human
    is aware of the problem.
    
    To file a bug in ANGLE:
    https://bugs.chromium.org/p/angleproject/issues/entry
    
    To report a problem with the AutoRoller itself, please file a bug:
    https://issues.skia.org/issues/new?component=1389291&template=1850622
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+doc/main/autoroll/README.md
    
    Bug: None
    Change-Id: I5e07217cca190ec243f1b700348cde0384dbf052

--- scripts/gen_vk_gl_cts_build.py
+++ scripts/gen_vk_gl_cts_build.py
@@ -19,9 +19,12 @@ def initDataDirectories(dataDirectories):
     dataDirectories.append(os.path.join("data", "gles3"))
     dataDirectories.append(os.path.join("data", "gles31"))
     dataDirectories.append(os.path.join("external", "graphicsfuzz", "data", "gles3"))
-    dataDirectories.append(os.path.join("external", "openglcts", "data", "gles3"))
-    dataDirectories.append(os.path.join("external", "openglcts", "data", "gles31"))
-    dataDirectories.append(os.path.join("external", "openglcts", "data", "gles32"))
+    dataDirectories.append(
+        os.path.join("external", "openglcts", "data", "gl_cts", "data", "gles3"))
+    dataDirectories.append(
+        os.path.join("external", "openglcts", "data", "gl_cts", "data", "gles31"))
+    dataDirectories.append(
+        os.path.join("external", "openglcts", "data", "gl_cts", "data", "gles32"))
 
 
 def initPathReplacements(pathReplacements):
