c64f92528225a9eddf53ee49cf4c4d2aa25552fe
Mohan Maiya <m.maiya@samsung.com>
2024-04-23 17:08:58 -0500

    Vulkan: Optimize DescriptorSetLayout with no bindings
    There is no need to unpack bindings of DescriptorSetLayoutDesc and call
    into vkCreateDescriptorSetLayout when there are no bindings. Insert a
    placeholder vk::DescriptorSetLayout into DescriptorSetLayoutCache. This
    reduces the number of Vulkan handles created and can be reused across
    multiple pipelines that have DescriptorSetLayoutDesc with no bindings.
    
    Bug: angleproject:2462
    Change-Id: Ifc0d199697a95f3a5c037fd85520220a512d7949

--- src/libANGLE/renderer/vulkan/vk_cache_utils.cpp
+++ src/libANGLE/renderer/vulkan/vk_cache_utils.cpp
@@ -7468,6 +7468,18 @@ angle::Result DescriptorSetLayoutCache::getDescriptorSetLayout(
         return angle::Result::Continue;
     }
 
+    // Descriptor set layout handle is allowed to be VK_NULL_HANDLE iff -
+    // 1. VK_EXT_graphics_pipeline_library is supported
+    // 2. Pre-rasterization and fragment subset pipeline compilations are decoupled
+    if (context->getFeatures().decoupledPrerastFragmentPipelineCompilation.enabled && desc.empty())
+    {
+        auto insertedItem = mPayload.emplace(desc, vk::DescriptorSetLayout());
+        vk::RefCountedDescriptorSetLayout &insertedLayout = insertedItem.first->second;
+        descriptorSetLayoutOut->set(&insertedLayout);
+
+        return angle::Result::Continue;
+    }
+
     mCacheStats.missAndIncrementSize();
     // We must unpack the descriptor set layout description.
     vk::DescriptorSetLayoutBindingVector bindingVector;
@@ -7538,10 +7550,9 @@ angle::Result PipelineLayoutCache::getPipelineLayout(
         if (layoutPtr.valid())
         {
             VkDescriptorSetLayout setLayout = layoutPtr.get().getHandle();
-            if (setLayout != VK_NULL_HANDLE)
-            {
-                setLayoutHandles.push_back(setLayout);
-            }
+            ASSERT(setLayout != VK_NULL_HANDLE ||
+                   context->getFeatures().decoupledPrerastFragmentPipelineCompilation.enabled);
+            setLayoutHandles.push_back(setLayout);
         }
     }
 
