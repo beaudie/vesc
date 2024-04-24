ab960a0d1ae929d43c6fa68b095d3614643c4645
Rafay Khurram <r.khurram@samsung.com>
2024-04-24 02:11:42 -0500

    CL/Vulkan: Add skeleton for CLSamplerVk
    * It is setup to be a wrapper for the SamplerHelper interface
    
    Bug: angleproject:42266936
    Change-Id: Iac7e80c4d5262687d98a8188a60a24a9be190dc2

--- src/libANGLE/renderer/vulkan/vk_cache_utils.h
+++ src/libANGLE/renderer/vulkan/vk_cache_utils.h
@@ -2072,7 +2072,7 @@ CreateSharedFramebufferCacheKey(const FramebufferDesc &desc)
 class SamplerHelper final : angle::NonCopyable
 {
   public:
-    SamplerHelper(ContextVk *contextVk);
+    SamplerHelper(vk::Context *context);
     ~SamplerHelper();
 
     explicit SamplerHelper(SamplerHelper &&samplerHelper);
