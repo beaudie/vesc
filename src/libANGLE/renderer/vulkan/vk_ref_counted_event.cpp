17957fcdd3be44ec00be0b85354385057f29a7dd
Charlie Lao <cclao@google.com>
2024-04-25 10:35:02 -0700

    Vulkan: Add RefCountedEvent class
    Bug: b/336844257
    Change-Id: Iae5c4d2553a80f0f74cd6065d72a9c592c79f075

---
+++ src/libANGLE/renderer/vulkan/vk_ref_counted_event.cpp
@@ -0,0 +1,187 @@
+//
+// Copyright 2024 The ANGLE Project Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+//
+// RefCountedEvent:
+//    Manages reference count of VkEvent and its associated functions.
+//
+
+#include "libANGLE/renderer/vulkan/vk_ref_counted_event.h"
+#include "libANGLE/renderer/vulkan/vk_helpers.h"
+#include "libANGLE/renderer/vulkan/vk_renderer.h"
+
+namespace rx
+{
+namespace vk
+{
+
+void ReleaseRefcountedEvent(VkDevice device, RefCountedEventAndLayoutHandle atomicRefCountedEvent)
+{
+    const bool isLastReference = atomicRefCountedEvent->getAndReleaseRef() == 1;
+    if (isLastReference)
+    {
+        atomicRefCountedEvent->get().event.destroy(device);
+        SafeDelete(atomicRefCountedEvent);
+    }
+}
+
+void RefCountedEvent::init(VkDevice device, ImageLayout layout)
+{
+    ASSERT(mHandle == nullptr);
+    ASSERT(layout != ImageLayout::Undefined);
+
+    mHandle                      = new AtomicRefCounted<EventAndLayout>;
+    VkEventCreateInfo createInfo = {};
+    createInfo.sType             = VK_STRUCTURE_TYPE_EVENT_CREATE_INFO;
+    createInfo.flags             = 0;
+    mHandle->get().event.init(device, createInfo);
+    mHandle->addRef();
+    mHandle->get().imageLayout = layout;
+}
+
+// RefCountedEventGarbageObjects implementation
+void RefCountedEventGarbageObjects::add(RefCountedEvent *event)
+{
+    mGarbageObjects.emplace_back(GetGarbage(event));
+}
+
+void RefCountedEventGarbageObjects::addDuplicate(const RefCountedEvent &event)
+{
+    RefCountedEvent localEventCopy = event;
+    mGarbageObjects.emplace_back(GetGarbage(&localEventCopy));
+    ASSERT(!localEventCopy.valid());
+}
+
+void RefCountedEventGarbageObjects::add(std::vector<RefCountedEvent> *events)
+{
+    while (!events->empty())
+    {
+        mGarbageObjects.emplace_back(GetGarbage(&events->back()));
+        events->pop_back();
+    }
+}
+
+// EventBarrier implementation.
+bool EventBarrier::hasEvent(const VkEvent &event) const
+{
+    for (const VkEvent &existingEvent : mEvents)
+    {
+        if (existingEvent == event)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+void EventBarrier::addDiagnosticsString(std::ostringstream &out) const
+{
+    if (mMemoryBarrierSrcAccess != 0 || mMemoryBarrierDstAccess != 0)
+    {
+        out << "Src: 0x" << std::hex << mMemoryBarrierSrcAccess << " &rarr; Dst: 0x" << std::hex
+            << mMemoryBarrierDstAccess << std::endl;
+    }
+}
+
+void EventBarrier::execute(PrimaryCommandBuffer *primary)
+{
+    if (isEmpty())
+    {
+        return;
+    }
+
+    // Issue vkCmdWaitEvents call
+    VkMemoryBarrier memoryBarrier = {};
+    uint32_t memoryBarrierCount   = 0;
+    if (mMemoryBarrierDstAccess != 0)
+    {
+        memoryBarrier.sType         = VK_STRUCTURE_TYPE_MEMORY_BARRIER;
+        memoryBarrier.srcAccessMask = mMemoryBarrierSrcAccess;
+        memoryBarrier.dstAccessMask = mMemoryBarrierDstAccess;
+        memoryBarrierCount++;
+    }
+
+    primary->waitEvents(static_cast<uint32_t>(mEvents.size()), mEvents.data(), mSrcStageMask,
+                        mDstStageMask, memoryBarrierCount, &memoryBarrier, 0, nullptr,
+                        static_cast<uint32_t>(mImageMemoryBarriers.size()),
+                        mImageMemoryBarriers.data());
+
+    reset();
+}
+
+// EventBarrierArray implementation.
+void EventBarrierArray::addMemoryEvent(Context *context,
+                                       const RefCountedEvent &waitEvent,
+                                       VkPipelineStageFlags dstStageMask,
+                                       VkAccessFlags dstAccess)
+{
+    ASSERT(waitEvent.valid());
+
+    for (EventBarrier &barrier : mBarriers)
+    {
+        // If the event is already in the waiting list, just add the new stageMask to the
+        // dstStageMask. Otherwise we will end up with two waitEVent call that wait for the same
+        // VkEvent but for different dstStage and confuses VVL.
+        if (barrier.hasEvent(waitEvent.getEvent().getHandle()))
+        {
+            barrier.addAdditionalStageAccess(dstStageMask, dstAccess);
+            return;
+        }
+    }
+
+    // VkCmdWaitEvent must uses the same stageMask as VkCmdSetEvent due to
+    // VUID-vkCmdWaitEvents-srcStageMask-01158 requirements.
+    VkAccessFlags accessMask;
+    VkPipelineStageFlags stageFlags = GetRefCountedEventStageMask(context, waitEvent, &accessMask);
+    mBarriers.emplace_back(stageFlags, stageFlags, accessMask, accessMask,
+                           waitEvent.getEvent().getHandle());
+}
+
+void EventBarrierArray::addImageEvent(Context *context,
+                                      const RefCountedEvent &waitEvent,
+                                      VkPipelineStageFlags dstStageMask,
+                                      const VkImageMemoryBarrier &imageMemoryBarrier)
+{
+    ASSERT(waitEvent.valid());
+    VkAccessFlags accessMask;
+    VkPipelineStageFlags srcStageFlags =
+        GetRefCountedEventStageMask(context, waitEvent, &accessMask);
+
+    mBarriers.emplace_back();
+    EventBarrier &barrier = mBarriers.back();
+    // VkCmdWaitEvent must uses the same stageMask as VkCmdSetEvent due to
+    // VUID-vkCmdWaitEvents-srcStageMask-01158 requirements.
+    barrier.mSrcStageMask = srcStageFlags;
+    // If there is an event, we use the waitEvent to do layout change.
+    barrier.mEvents.emplace_back(waitEvent.getEvent().getHandle());
+    barrier.mDstStageMask = dstStageMask;
+    barrier.mImageMemoryBarriers.emplace_back(imageMemoryBarrier);
+}
+
+void EventBarrierArray::execute(Renderer *renderer, PrimaryCommandBuffer *primary)
+{
+    // make a local copy for faster access
+    if (mBarriers.empty())
+    {
+        return;
+    }
+
+    for (EventBarrier &barrier : mBarriers)
+    {
+        barrier.execute(primary);
+    }
+    mBarriers.clear();
+}
+
+void EventBarrierArray::addDiagnosticsString(std::ostringstream &out) const
+{
+    out << "Event Barrier: ";
+    for (const EventBarrier &barrier : mBarriers)
+    {
+        barrier.addDiagnosticsString(out);
+    }
+    out << "\\l";
+}
+}  // namespace vk
+}  // namespace rx
