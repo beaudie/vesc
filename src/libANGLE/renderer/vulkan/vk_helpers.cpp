fdf92c768967bf75a0e6ddc64c0d2dd01185f05a
Charlie Lao <cclao@google.com>
2024-05-01 11:47:34 -0700

    Vulkan: Minor cleanup
    Bug: b/336844257
    Change-Id: I8d93c6dd814a666debf9990d151cad79c45469f1

--- src/libANGLE/renderer/vulkan/vk_helpers.cpp
+++ src/libANGLE/renderer/vulkan/vk_helpers.cpp
@@ -1642,7 +1642,7 @@ void CommandBufferHelperCommon::bufferWrite(ContextVk *contextVk,
     buffer->setWriteQueueSerial(mQueueSerial);
 
     VkPipelineStageFlagBits stageBits = kPipelineStageFlagBitMap[writeStage];
-    buffer->recordWriteBarrier(writeAccessType, stageBits, &mPipelineBarriers, writeStage);
+    buffer->recordWriteBarrier(writeAccessType, stageBits, writeStage, &mPipelineBarriers);
 
     // Make sure host-visible buffer writes result in a barrier inserted at the end of the frame to
     // make the results visible to the host.  The buffer may be mapped by the application in the
@@ -1671,7 +1671,7 @@ void CommandBufferHelperCommon::bufferReadImpl(VkAccessFlags readAccessType,
                                                BufferHelper *buffer)
 {
     VkPipelineStageFlagBits stageBits = kPipelineStageFlagBitMap[readStage];
-    buffer->recordReadBarrier(readAccessType, stageBits, &mPipelineBarriers, readStage);
+    buffer->recordReadBarrier(readAccessType, stageBits, readStage, &mPipelineBarriers);
     ASSERT(!usesBufferForWrite(*buffer));
 }
 
@@ -5498,8 +5498,8 @@ bool BufferHelper::isReleasedToExternal() const
 
 void BufferHelper::recordReadBarrier(VkAccessFlags readAccessType,
                                      VkPipelineStageFlags readStage,
-                                     PipelineBarrierArray *barriers,
-                                     PipelineStage stageIndex)
+                                     PipelineStage stageIndex,
+                                     PipelineBarrierArray *barriers)
 {
     // If there was a prior write and we are making a read that is either a new access type or from
     // a new stage, we need a barrier
@@ -5517,8 +5517,8 @@ void BufferHelper::recordReadBarrier(VkAccessFlags readAccessType,
 
 void BufferHelper::recordWriteBarrier(VkAccessFlags writeAccessType,
                                       VkPipelineStageFlags writeStage,
-                                      PipelineBarrierArray *barriers,
-                                      PipelineStage stageIndex)
+                                      PipelineStage stageIndex,
+                                      PipelineBarrierArray *barriers)
 {
     // We don't need to check mCurrentReadStages here since if it is not zero, mCurrentReadAccess
     // must not be zero as well. stage is finer grain than accessType.
