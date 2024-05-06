53d73a4759794de7c6c376cecdeda2ed6b335f09
Charlie Lao <cclao@google.com>
2024-05-06 12:36:20 -0700

    Vulkan: Move RefCountedEvent GC and recycler to ShareGroupVk (2/3)
    One of the problem we had with RefCountedEvents is CPU overhead comes
    with it, and some part of the CPU overhead is due to atomic reference
    counting. The RefCountedEvents are only used by ImageHelper and
    ImageHelpers are per share group, so they are already protected by front
    end context share lock. The only reason we needs atomic here is due to
    garbage cleanup, which runs in separate thread and will decrement the
    refCount. The idea is to move that garbage list from RendererVk to
    ShareGroupVk so that access of RefCountedEvents are all protected
    already, thus we can remove the use of atomic. The down side with this
    approach is that a share group will hold onto its event garbage and not
    available for other context to reuse. But VkEvents are expected to be
    very light weighted objects, so that should be acceptable.
    
    This is the second CL in the series. In this CL, we added
    RefCountedEventGarbageRecycler to the ShareGroupVk which is responsible
    to garbage collect and recycle RefCountedEvent. Since most of
    ImageHelper code have only access to Context argument, for convenience
    we also stored the RefCountedEventGarbageRecycler pointer in the
    vk::Context for easy access. vk::Context argument is also passed to
    RefCounteEvent::init and release function so that it has access to the
    recycler. The garbage collection happens when RefCountedEvent is needed.
    The per renderer recycler is still kept to hold the RefCounteEvents that
    gets released from ShareGroupVk or when it is released without access to
    context information.
    
    Bug: b/336844257
    Change-Id: I36fe5d1c8dacdbe35bb2d380f94a32b9b72bbaa5

--- src/libANGLE/renderer/vulkan/vk_helpers.cpp
+++ src/libANGLE/renderer/vulkan/vk_helpers.cpp
@@ -1755,12 +1755,12 @@ template void CommandBufferHelperCommon::flushSetEventsImpl<VulkanSecondaryComma
     Context *context,
     VulkanSecondaryCommandBuffer *commandBuffer);
 
-void CommandBufferHelperCommon::collectRefCountedEventsGarbage(Renderer *renderer)
+void CommandBufferHelperCommon::collectRefCountedEventsGarbage(
+    RefCountedEventGarbageRecycler *garbageRecycler)
 {
     if (!mRefCountedEventCollector.empty())
     {
-        renderer->collectRefCountedEventsGarbage(mQueueSerial,
-                                                 std::move(mRefCountedEventCollector));
+        garbageRecycler->collectGarbage(mQueueSerial, std::move(mRefCountedEventCollector));
     }
 }
 
@@ -2724,7 +2724,7 @@ void RenderPassCommandBufferHelper::executeSetEvents(Context *context,
                           GetImageLayoutDstStageMask(context, layoutData));
         // Note that these events are already added to the garbage collector before command buffer
         // leaves ContextVk, so we just need to release the event after use.
-        refCountedEvent.release(context->getRenderer());
+        refCountedEvent.release(context);
     }
     mRefCountedEvents.mask.reset();
 }
@@ -7155,7 +7155,7 @@ void ImageHelper::barrierImpl(Context *context,
     {
         // For now we always use pipelineBarrier for singlebuffer mode. We could use event here in
         // future.
-        mCurrentEvent.release(context->getRenderer());
+        mCurrentEvent.release(context);
 
         const ImageMemoryBarrierData &transition = kImageMemoryBarrierData[mCurrentLayout];
         VkMemoryBarrier memoryBarrier            = {};
@@ -7212,7 +7212,7 @@ void ImageHelper::barrierImpl(Context *context,
         }
         commandBuffer->imageBarrier(srcStageMask, dstStageMask, imageMemoryBarrier);
         // We use pipelineBarrier here, no needs to wait for events any more.
-        mCurrentEvent.release(context->getRenderer());
+        mCurrentEvent.release(context);
     }
 
     mCurrentLayout           = newLayout;
@@ -7406,7 +7406,7 @@ void ImageHelper::updateLayoutAndBarrier(Context *context,
 
             // Release it. No need to garbage collect since we did not use the event here. ALl
             // previous use of event should garbage tracked already.
-            mCurrentEvent.release(context->getRenderer());
+            mCurrentEvent.release(context);
         }
         mBarrierQueueSerial = queueSerial;
     }
@@ -7521,7 +7521,7 @@ void ImageHelper::updateLayoutAndBarrier(Context *context,
                 mLastNonShaderReadOnlyLayout = ImageLayout::Undefined;
                 if (mLastNonShaderReadOnlyEvent.valid())
                 {
-                    mLastNonShaderReadOnlyEvent.release(context->getRenderer());
+                    mLastNonShaderReadOnlyEvent.release(context);
                 }
             }
 
@@ -7530,7 +7530,7 @@ void ImageHelper::updateLayoutAndBarrier(Context *context,
             const bool isShaderReadOnly = IsShaderReadOnlyLayout(transitionTo);
             if (isShaderReadOnly)
             {
-                mLastNonShaderReadOnlyEvent.release(context->getRenderer());
+                mLastNonShaderReadOnlyEvent.release(context);
                 mLastNonShaderReadOnlyLayout = mCurrentLayout;
                 mCurrentShaderReadStageMask  = dstStageMask;
             }
@@ -7549,7 +7549,7 @@ void ImageHelper::updateLayoutAndBarrier(Context *context,
             {
                 pipelineBarriers->mergeImageBarrier(transitionTo.barrierIndex, srcStageMask,
                                                     dstStageMask, imageMemoryBarrier);
-                mCurrentEvent.release(context->getRenderer());
+                mCurrentEvent.release(context);
             }
 
             mBarrierQueueSerial = queueSerial;
@@ -7570,7 +7570,7 @@ void ImageHelper::setCurrentRefCountedEvent(Context *context, ImageLayoutEventMa
     ASSERT(context->getRenderer()->getFeatures().useVkEventForImageBarrier.enabled);
 
     // If there is already an event, release it first.
-    mCurrentEvent.release(context->getRenderer());
+    mCurrentEvent.release(context);
 
     // Create the event if we have not yet so. Otherwise just use the already created event. This
     // means all images used in the same render pass that has the same layout will be tracked by the
