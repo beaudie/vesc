c4fe41dbfa5d673c469e947e31c0c478c4ae5487
Charlie Lao <cclao@google.com>
2024-05-06 11:03:19 -0700

    Vulkan: Move RefCountedEvent GC and recycler to ShareGroupVk (1/3)
    One of the problem we had with RefCountedEvents is CPU overhead comes
    with it, and some part of the CPU overhead is due to atomic reference
    counting. The RefCountedEvents are only used by ImageHelper and
    ImageHelpers are per share group, so they are already protected by front
    end context share lock. The only reason we needs atomic here is due to
    garbage cleanup, which runs in separate thread and will decrement the
    refCount. The idea is to move that garbage list from RendererVk to
    ShareGroupVk so that access of RefCountedEvents are all protected
    already, thus we can remove the use of atomic. The down side with this
    approach is that a share group will hold onto its event garbage and not
    available for other context to reuse. But VkEvents are expected to be
    very light weighted objects, so that should be acceptable (If not, we
    can add some limit to the number of events it can hold in the garbage
    list).
    
    This is the first CL in the series. Before this CL, the RefCounteEvents
    are garbage collected at flushToPrimrary time, at which time we have
    lost ContextVk information. In order for us to do garbage collect to
    ShareGroupVk, we need to move the garbage collection process early,
    before command buffers leaving ContextVk's visibility. For
    OutsideRenderPassCommands, this is easy to do, we just call
    flushSetEvents before we call mRenderer->flushRenderPassCommands. For
    RenderPassCommands, that flushSetEvents call will simply make another
    copy of RefCountedEvents and add to the garbage list and the actual
    VkCmdSetEvents are defered at the executeSetEvents call that get called
    from flushToPrimrary time.
    
    Bug: b/336844257
    Change-Id: I1948cd8240ff61d407931083b7584a54b1dc6b0d

--- src/libANGLE/renderer/vulkan/vk_helpers.h
+++ src/libANGLE/renderer/vulkan/vk_helpers.h
@@ -1351,6 +1351,10 @@ class CommandBufferHelperCommon : angle::NonCopyable
 
     RefCountedEventCollector *getRefCountedEventCollector() { return &mRefCountedEventCollector; }
 
+    // Clean up event garbage. Note that ImageHelper object may still holding reference count to it,
+    // so the event itself will not gets destroyed until the last refCount goes away.
+    void collectRefCountedEventsGarbage(Renderer *renderer);
+
     const QueueSerial &getQueueSerial() const { return mQueueSerial; }
 
     void setAcquireNextImageSemaphore(VkSemaphore semaphore)
@@ -1523,7 +1527,7 @@ class OutsideRenderPassCommandBufferHelper final : public CommandBufferHelperCom
     void trackImagesWithEvent(Context *context, ImageHelper *srcImage, ImageHelper *dstImage);
     void trackImagesWithEvent(Context *context, const ImageHelperPtr *images, size_t count);
 
-    // Issues VkCmdSetEvent calls.
+    // Issues SetEvent calls to the command buffer.
     void flushSetEvents(Context *context) { flushSetEventsImpl(context, &mCommandBuffer); }
 
     angle::Result flushToPrimary(Context *context, CommandsState *commandsState);
@@ -1874,6 +1878,8 @@ class RenderPassCommandBufferHelper final : public CommandBufferHelperCommon
     void updateDepthStencilReadOnlyMode(RenderPassUsageFlags dsUsageFlags,
                                         VkImageAspectFlags dsAspectFlags);
 
+    void flushSetEvents(Context *context);
+
   private:
     uint32_t getSubpassCommandBufferCount() const { return mCurrentSubpassCommandBufferIndex + 1; }
 
@@ -1911,6 +1917,7 @@ class RenderPassCommandBufferHelper final : public CommandBufferHelperCommon
     void finalizeFragmentShadingRateImageLayout(Context *context);
 
     void trackImagesWithEvent(Context *context, const ImageHelperPtr *images, size_t count);
+    void executeSetEvents(Context *context, PrimaryCommandBuffer *primary);
 
     // When using Vulkan secondary command buffers, each subpass must be recorded in a separate
     // command buffer.  Currently ANGLE produces render passes with at most 2 subpasses.
