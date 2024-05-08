e69bb304e138518b9fcb6adaf9cb2f6bafb2b617
Geoff Lang <geofflang@chromium.org>
2024-05-08 12:31:37 -0400

    WebGPU: Add Window and Pbuffer surfaces.
    Bug: angleproject:0
    Change-Id: I58465bdf895b4bab63ec3c74e326c6be4827bf60

--- src/libANGLE/renderer/wgpu/RenderTargetWgpu.h
+++ src/libANGLE/renderer/wgpu/RenderTargetWgpu.h
@@ -28,24 +28,15 @@ class RenderTargetWgpu final : public FramebufferAttachmentRenderTarget
     // Used in std::vector initialization.
     RenderTargetWgpu(RenderTargetWgpu &&other);
 
-    void set(webgpu::ImageHelper *image,
-             const wgpu::TextureView &texture,
-             const webgpu::LevelIndex level,
-             uint32_t layer,
-             const wgpu::TextureFormat &format);
-    void setTexture(const wgpu::TextureView &texture);
+    void set(webgpu::ImageHelper *imageHelper, const wgpu::TextureView &textureView);
     void reset();
 
-    wgpu::TextureView getTexture() { return mTexture; }
-    webgpu::ImageHelper *getImage() { return mImage; }
+    webgpu::ImageHelper *getImage() const { return mImageHelper; }
+    wgpu::TextureView getTextureView() const { return mTextureView; }
 
   private:
-    webgpu::ImageHelper *mImage;
-    // TODO(liza): move TextureView into ImageHelper.
-    wgpu::TextureView mTexture;
-    webgpu::LevelIndex mLevelIndex{0};
-    uint32_t mLayerIndex               = 0;
-    const wgpu::TextureFormat *mFormat = nullptr;
+    webgpu::ImageHelper *mImageHelper = nullptr;
+    wgpu::TextureView mTextureView;
 };
 }  // namespace rx
 
