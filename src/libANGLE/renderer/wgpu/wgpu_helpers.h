e735c0135bf020f63eb143402fb8a3ef75090f24
Liza Burakova <liza@chromium.org>
2024-05-08 12:19:23 -0400

    Adding basic readPixels.
    This change adds to methods in ImageHelper to read texture data.
    It also implements FramebufferWgpu::readPixels to get the parameters
    and read the texture data from an ImageHelper that's stored in
    mRenderTargetCache.
    
    Bug: angleproject:8653
    Change-Id: I349ed8a0ae3d8d0e187c658f3402c4f8cac23eb8

--- src/libANGLE/renderer/wgpu/wgpu_helpers.h
+++ src/libANGLE/renderer/wgpu/wgpu_helpers.h
@@ -65,6 +65,21 @@ class ImageHelper
 
     void resetImage();
 
+    static angle::Result getReadPixelsParams(rx::ContextWgpu *contextWgpu,
+                                             const gl::PixelPackState &packState,
+                                             gl::Buffer *packBuffer,
+                                             GLenum format,
+                                             GLenum type,
+                                             const gl::Rectangle &area,
+                                             const gl::Rectangle &clippedArea,
+                                             rx::PackPixelsParams *paramsOut,
+                                             GLuint *skipBytesOut);
+
+    angle::Result readPixels(rx::ContextWgpu *contextWgpu,
+                             const gl::Rectangle &area,
+                             const rx::PackPixelsParams &packPixelsParams,
+                             const angle::Format &aspectFormat,
+                             void *pixels);
     LevelIndex toWgpuLevel(gl::LevelIndex levelIndexGl) const;
     gl::LevelIndex toGlLevel(LevelIndex levelIndexWgpu) const;
     bool isTextureLevelInAllocatedImage(gl::LevelIndex textureLevel);
@@ -121,6 +136,7 @@ class BufferHelper : public angle::NonCopyable
     angle::Result unmap();
 
     uint8_t *getMapWritePointer(size_t offset, size_t size) const;
+    const uint8_t *getMapReadPointer(size_t offset, size_t size) const;
 
     const std::optional<BufferMapState> &getMappedState() const;
 
