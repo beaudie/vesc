7783ee78ae9cfb2f71a17085d2872f129d0cb53c
Charlie Lao <cclao@google.com>
2024-05-09 11:21:38 -0700

    Vulkan: Use QueueSerial to track RefCountedEvent garbage
    RefCountedEvent garbage are only used on one command buffer, so it is a
    single QueueSerial is enough to track it.
    
    Bug: b/336844257
    Change-Id: Icafeee722fe19f41dd863a2e6aca6ddd981d28a4

--- src/libANGLE/renderer/vulkan/vk_ref_counted_event.h
+++ src/libANGLE/renderer/vulkan/vk_ref_counted_event.h
@@ -155,20 +155,20 @@ class RefCountedEventsGarbage final
 
     RefCountedEventsGarbage(const QueueSerial &queueSerial,
                             RefCountedEventCollector &&refCountedEvents)
-        : mLifetime(queueSerial), mRefCountedEvents(std::move(refCountedEvents))
+        : mQueueSerial(queueSerial), mRefCountedEvents(std::move(refCountedEvents))
     {
         ASSERT(refCountedEvents.empty());
         ASSERT(!mRefCountedEvents.empty());
     }
 
     RefCountedEventsGarbage(RefCountedEventsGarbage &&other)
-        : mLifetime(other.mLifetime), mRefCountedEvents(std::move(other.mRefCountedEvents))
+        : mQueueSerial(other.mQueueSerial), mRefCountedEvents(std::move(other.mRefCountedEvents))
     {}
 
     RefCountedEventsGarbage &operator=(RefCountedEventsGarbage &&other)
     {
         ASSERT(mRefCountedEvents.empty());
-        mLifetime         = other.mLifetime;
+        mQueueSerial      = other.mQueueSerial;
         mRefCountedEvents = std::move(other.mRefCountedEvents);
         return *this;
     }
@@ -210,7 +210,7 @@ class RefCountedEventsGarbage final
 
   private:
     friend class RefCountedEventGarbageRecycler;
-    ResourceUse mLifetime;
+    QueueSerial mQueueSerial;
     RefCountedEventCollector mRefCountedEvents;
 };
 
