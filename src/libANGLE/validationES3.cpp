829f5bac3145b960568e1c7f0e92d7aef6785429
Amirali Abdolrashidi <abdolrashidi@google.com>
2024-05-21 13:07:44 -0700

    Implement KHR_robustness
    * Added implementation for the validation layers for the
      added functions, and updated some of the existing ones.
    
      * The core functions require GLES 3.2.
    
      * The KHR functions have been limited to GLES 2.0+.
        * KHR_robustness requires GLES 2.0 and removes support
          for GLES 1.1 for logistical reasons.
    
      * Some functions require GLES 3.0 with this extension, which become
        core in GLES 3.2.
    
        * glGetnUniformuivKHR()
    
        * The following functions have special cases when context is lost:
          * glGetSynciv() with pname = GL_SYNC_STATUS
          * glGetQueryObjectuiv() with pname = GL_QUERY_RESULT_AVAILABLE
          * (GL_CONTEXT_LOST was added in the KHR extension and is absent
            in GL_EXT_robustness)
    
    * Enabled robustnessKHR on several platforms.
    
    * Added unit tests similar to ContextLostTest.BasicUsage for the new
      cases.
      * Added ContextLostTestES32 for the core usage in GLES 3.2.
    
    Bug: b/42262244
    Change-Id: Id1425c39d9a1a66ae1a90048b673cb1ae391c0ef

--- src/libANGLE/validationES3.cpp
+++ src/libANGLE/validationES3.cpp
@@ -3779,7 +3779,12 @@ bool ValidateGetSynciv(const Context *context,
     {
         ANGLE_VALIDATION_ERROR(GL_CONTEXT_LOST, kContextLost);
 
-        if (pname == GL_SYNC_STATUS)
+        // According to the KHR_robustness spec, if ES 3.0 or later is not supported, there is no
+        // special behavior for GL_SYNC_STATUS.
+        bool isSyncStatusSpecialCase =
+            (context->getClientVersion() >= ES_3_2) ||
+            (context->getClientMajorVersion() >= 3 && context->getExtensions().robustnessKHR);
+        if (pname == GL_SYNC_STATUS && isSyncStatusSpecialCase)
         {
             // Generate an error but still return true, the context still needs to return a
             // value in this case.
