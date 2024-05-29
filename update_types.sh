f0a649bdef4a206cfc5cd183a167079b40682a3b
Solti <solti@google.com>
2024-05-29 19:27:02 +0000

    remove de* types
    The de* types were removed by the change:
        https://chromium.googlesource.com/external/github.com/KhronosGroup/VK-GL-CTS.git/+/1a09796462f70cfe2a6117f9bc6af6e3ef74e6ff
    
    This CL updates ANGLE accordingly.  The change is generated by:
        http://gpaste/4859746406629376?raw
    Reference:
        https://github.com/KhronosGroup/VK-GL-CTS/blob/aefd5c09cacdb5a3320da717af6ec646d96c2379/format_all.sh#L17-L41
    
    Bug: b/343202259
    Change-Id: Idc374fa0873e9ddaafe4c9f861995952f2f5e7e1

---
+++ update_types.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+git ls-files | grep -E '\.(c|cpp|h|hpp|java|json|py)$' \
+             | xargs -P 8 -d '\n' sed -b -i 's|\<deInt8\>|int8_t|g;
+                                      s|\<deUint8\>|uint8_t|g;
+                                      s|\<deInt16\>|int16_t|g;
+                                      s|\<deUint16\>|uint16_t|g;
+                                      s|\<deInt32\>|int32_t|g;
+                                      s|\<deUint32\>|uint32_t|g;
+                                      s|\<deInt64\>|int64_t|g;
+                                      s|\<deUint64\>|uint64_t|g;
+                                      s|\<deIntptr\>|intptr_t|g;
+                                      s|\<deUintptr\>|uintptr_t|g;
+                                      s|#include "int32_t\.h"|#include "deInt32.h"|;
+                                      s|#include <int32_t\.h>|#include <deInt32.h>|;
+                                      s|\<deBool\>|bool|g;
+                                      s|\<DE_TRUE\>|true|g;
+                                      s|\<DE_FALSE\>|false|g;
+                                      s|::\<deGetFalse()|false|g;
+                                      s|::\<deGetTrue()|true|g;
+                                      s|\<deGetFalse()|false|g;
+                                      s|\<deGetTrue()|true|g;
+                                      s|\<DE_OFFSET_OF\>|offsetof|g'
