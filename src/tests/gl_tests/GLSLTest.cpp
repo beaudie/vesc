27296f7cdcdebe343daff32a1c27c2c950c69904
Amirali Abdolrashidi <abdolrashidi@google.com>
2024-05-31 15:10:37 -0700

    Implement OES_tessellation_shader
      Based on the specs, there are only some minor differences between
    OES and EXT.
      * EXT requires EXT_gpu_shader5 and EXT_shader_io_blocks.
      * OES requires either the EXT or the OES version of these extensions.
        * For both, the EXT versions can be used.
    
    * Added validation for glPatchParameteriOES().
    
    * Added the extension support for GLSL.
    
    * Added a test to make sure using this extension works instead of EXT.
      * Turned the repeated test code into a function:
        * testTessellationTextureBufferAccess
    
    * Skipped the tests that fail on various platforms.
      * It seems that these tests were being skipped before enabling
        this extension ("Not supported").
    
    Bug: b/344030760
    Bug: angleproject:345306326
    Bug: angleproject:345304850
    Bug: angleproject:345312771
    Change-Id: I905da0132bf6525cb453dcaa613e4deb3155c4dd

--- src/tests/gl_tests/GLSLTest.cpp
+++ src/tests/gl_tests/GLSLTest.cpp
@@ -696,6 +696,113 @@ void main(void)
 
         glUnmapBuffer(GL_SHADER_STORAGE_BUFFER);
     }
+
+    void testTessellationTextureBufferAccess(const APIExtensionVersion usedExtension)
+    {
+        ASSERT(usedExtension == APIExtensionVersion::EXT ||
+               usedExtension == APIExtensionVersion::OES);
+
+        // Vertex shader
+        constexpr char kVS[] = R"(#version 310 es
+precision highp float;
+in vec4 inputAttribute;
+
+void main()
+{
+    gl_Position = inputAttribute;
+})";
+
+        // Tessellation shaders
+        constexpr char kGLSLVersion[] = R"(#version 310 es
+)";
+        constexpr char kTessEXT[]     = R"(#extension GL_EXT_tessellation_shader : require
+)";
+        constexpr char kTessOES[]     = R"(#extension GL_OES_tessellation_shader : require
+)";
+        constexpr char kTexBufEXT[]   = R"(#extension GL_EXT_texture_buffer : require
+)";
+        constexpr char kTexBufOES[]   = R"(#extension GL_OES_texture_buffer : require
+)";
+
+        std::string tcs;
+        std::string tes;
+
+        tcs.append(kGLSLVersion);
+        tes.append(kGLSLVersion);
+
+        if (usedExtension == APIExtensionVersion::EXT)
+        {
+            tcs.append(kTessEXT);
+            tes.append(kTessEXT);
+            tes.append(kTexBufEXT);
+        }
+        else
+        {
+            tcs.append(kTessOES);
+            tes.append(kTessOES);
+            tes.append(kTexBufOES);
+        }
+
+        constexpr char kTCSBody[] = R"(precision mediump float;
+layout(vertices = 2) out;
+
+void main()
+{
+    gl_out[gl_InvocationID].gl_Position = gl_in[gl_InvocationID].gl_Position;
+    gl_TessLevelInner[0] = 1.0;
+    gl_TessLevelInner[1] = 1.0;
+    gl_TessLevelOuter[0] = 1.0;
+    gl_TessLevelOuter[1] = 1.0;
+    gl_TessLevelOuter[2] = 1.0;
+    gl_TessLevelOuter[3] = 1.0;
+})";
+        tcs.append(kTCSBody);
+
+        constexpr char kTESBody[] = R"(precision mediump float;
+layout (isolines, point_mode) in;
+
+uniform highp samplerBuffer tex;
+
+out vec4 tex_color;
+
+void main()
+{
+    tex_color = texelFetch(tex, 0);
+    gl_Position = gl_in[0].gl_Position;
+})";
+        tes.append(kTESBody);
+
+        // Fragment shader
+        constexpr char kFS[] = R"(#version 310 es
+precision mediump float;
+layout(location = 0) out mediump vec4 color;
+
+in vec4 tex_color;
+
+void main()
+{
+    color = tex_color;
+})";
+
+        constexpr GLint kBufferSize = 32;
+        GLubyte texData[]           = {0u, 255u, 0u, 255u};
+
+        GLTexture texture;
+        glBindTexture(GL_TEXTURE_BUFFER, texture);
+
+        GLBuffer buffer;
+        glBindBuffer(GL_TEXTURE_BUFFER, buffer);
+        glBufferData(GL_TEXTURE_BUFFER, kBufferSize, texData, GL_STATIC_DRAW);
+        glTexBufferEXT(GL_TEXTURE_BUFFER, GL_RGBA8, buffer);
+        ASSERT_GL_NO_ERROR();
+
+        glClearColor(1.0, 0, 0, 1.0);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        ANGLE_GL_PROGRAM_WITH_TESS(program, kVS, tcs.c_str(), tes.c_str(), kFS);
+        drawPatches(program, "inputAttribute", 0.5f, 1.0f, GL_FALSE);
+        ASSERT_GL_NO_ERROR();
+    }
 };
 
 // Tests the "init output variables" ANGLE shader translator option.
@@ -7516,83 +7623,21 @@ TEST_P(GLSLTest_ES31, VaryingIOBlockDeclaredAsInAndOut)
     ASSERT_GL_NO_ERROR();
 }
 
-// Test that texture buffers can be accessed in a tessellation stage
+// Test that texture buffers can be accessed in a tessellation stage (using EXT)
 // Triggers a bug in the Vulkan backend: http://anglebug.com/7135
-TEST_P(GLSLTest_ES31, TessellationTextureBufferAccess)
+TEST_P(GLSLTest_ES31, TessellationTextureBufferAccessEXT)
 {
     ANGLE_SKIP_TEST_IF(!IsGLExtensionEnabled("GL_EXT_tessellation_shader"));
     ANGLE_SKIP_TEST_IF(!IsGLExtensionEnabled("GL_EXT_texture_buffer"));
+    testTessellationTextureBufferAccess(APIExtensionVersion::EXT);
+}
 
-    constexpr char kVS[] = R"(#version 310 es
-    precision highp float;
-    in vec4 inputAttribute;
-
-    void main()
-    {
-        gl_Position = inputAttribute;
-    })";
-
-    constexpr char kTCS[] = R"(#version 310 es
-    #extension GL_EXT_tessellation_shader : require
-    precision mediump float;
-    layout(vertices = 2) out;
-
-    void main()
-    {
-        gl_out[gl_InvocationID].gl_Position = gl_in[gl_InvocationID].gl_Position;
-        gl_TessLevelInner[0] = 1.0;
-        gl_TessLevelInner[1] = 1.0;
-        gl_TessLevelOuter[0] = 1.0;
-        gl_TessLevelOuter[1] = 1.0;
-        gl_TessLevelOuter[2] = 1.0;
-        gl_TessLevelOuter[3] = 1.0;
-    })";
-
-    constexpr char kTES[] = R"(#version 310 es
-    #extension GL_EXT_tessellation_shader : require
-    #extension GL_OES_texture_buffer : require
-    precision mediump float;
-    layout (isolines, point_mode) in;
-
-    uniform highp samplerBuffer tex;
-
-    out vec4 tex_color;
-
-    void main()
-    {
-        tex_color = texelFetch(tex, 0);
-        gl_Position = gl_in[0].gl_Position;
-    })";
-
-    constexpr char kFS[] = R"(#version 310 es
-    precision mediump float;
-    layout(location = 0) out mediump vec4 color;
-
-    in vec4 tex_color;
-
-    void main()
-    {
-        color = tex_color;
-    })";
-
-    constexpr GLint kBufferSize = 32;
-    GLubyte texData[]           = {0u, 255u, 0u, 255u};
-
-    GLTexture texture;
-    glBindTexture(GL_TEXTURE_BUFFER, texture);
-
-    GLBuffer buffer;
-    glBindBuffer(GL_TEXTURE_BUFFER, buffer);
-    glBufferData(GL_TEXTURE_BUFFER, kBufferSize, texData, GL_STATIC_DRAW);
-    glTexBufferEXT(GL_TEXTURE_BUFFER, GL_RGBA8, buffer);
-    ASSERT_GL_NO_ERROR();
-
-    glClearColor(1.0, 0, 0, 1.0);
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    ANGLE_GL_PROGRAM_WITH_TESS(program, kVS, kTCS, kTES, kFS);
-    drawPatches(program, "inputAttribute", 0.5f, 1.0f, GL_FALSE);
-    ASSERT_GL_NO_ERROR();
+// Test that texture buffers can be accessed in a tessellation stage (using OES)
+TEST_P(GLSLTest_ES31, TessellationTextureBufferAccessOES)
+{
+    ANGLE_SKIP_TEST_IF(!IsGLExtensionEnabled("GL_OES_tessellation_shader"));
+    ANGLE_SKIP_TEST_IF(!IsGLExtensionEnabled("GL_OES_texture_buffer"));
+    testTessellationTextureBufferAccess(APIExtensionVersion::OES);
 }
 
 // Test that a varying struct that's not declared in the fragment shader links successfully.
