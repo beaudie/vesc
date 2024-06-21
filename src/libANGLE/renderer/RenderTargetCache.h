db8c999714f0637e9a9280cd6b0cdb5f20317838
Amirali Abdolrashidi <abdolrashidi@google.com>
2024-06-21 14:48:06 -0700

    Unify updating cached color/read render targets
      Currently in updateColorRenderTarget(), there are two calls to update
    cached render target if the read RT is the same as the color RT, which
    means getting the same attachment RT twice.
    
    * Added updateCachedColorAndReadRenderTarget() to RenderTargetCache.
      * It will only get the attachment render target once, even if the
        read RT is the same.
    
    Bug: angleproject:348704287
    Change-Id: I9ff9fb2a4edbd4420feece0ee6edd40f1f7efdca

--- src/libANGLE/renderer/RenderTargetCache.h
+++ src/libANGLE/renderer/RenderTargetCache.h
@@ -52,6 +52,9 @@ class RenderTargetCache final : angle::NonCopyable
     angle::Result updateCachedRenderTarget(const gl::Context *context,
                                            const gl::FramebufferAttachment *attachment,
                                            RenderTargetT **cachedRenderTarget);
+    angle::Result updateCachedColorAndReadRenderTarget(const gl::Context *context,
+                                                       const gl::FramebufferState &state,
+                                                       size_t colorIndex);
 
     RenderTargetT *mReadRenderTarget                         = nullptr;
     gl::AttachmentArray<RenderTargetT *> mColorRenderTargets = {};
@@ -130,15 +133,7 @@ angle::Result RenderTargetCache<RenderTargetT>::updateColorRenderTarget(
     const gl::FramebufferState &state,
     size_t colorIndex)
 {
-    // If the color render target we're updating is also the read buffer, make sure we update the
-    // read render target also so it's not stale.
-    if (state.getReadBufferState() != GL_NONE && state.getReadIndex() == colorIndex)
-    {
-        ANGLE_TRY(updateReadColorRenderTarget(context, state));
-    }
-
-    return updateCachedRenderTarget(context, state.getColorAttachment(colorIndex),
-                                    &mColorRenderTargets[colorIndex]);
+    return updateCachedColorAndReadRenderTarget(context, state, colorIndex);
 }
 
 template <typename RenderTargetT>
@@ -164,6 +159,33 @@ angle::Result RenderTargetCache<RenderTargetT>::updateCachedRenderTarget(
                                               &newRenderTarget));
     }
     *cachedRenderTarget = newRenderTarget;
+    return angle::Result::Continue;
+}
+
+template <typename RenderTargetT>
+angle::Result RenderTargetCache<RenderTargetT>::updateCachedColorAndReadRenderTarget(
+    const gl::Context *context,
+    const gl::FramebufferState &state,
+    size_t colorIndex)
+{
+    RenderTargetT *newRenderTarget = nullptr;
+
+    const gl::FramebufferAttachment *attachment = state.getColorAttachment(colorIndex);
+    if (attachment)
+    {
+        ASSERT(attachment->isAttached());
+        ANGLE_TRY(attachment->getRenderTarget(context, attachment->getRenderToTextureSamples(),
+                                              &newRenderTarget));
+    }
+    mColorRenderTargets[colorIndex] = newRenderTarget;
+
+    // If the color render target we're updating is also the read buffer, make sure we update the
+    // read render target also so it's not stale.
+    if (state.getReadBufferState() != GL_NONE && state.getReadIndex() == colorIndex)
+    {
+        mReadRenderTarget = newRenderTarget;
+    }
+
     return angle::Result::Continue;
 }
 
