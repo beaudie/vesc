bbd4d13ac988463f3e7fd7a5e2f38a7614aa9f12
Amirali Abdolrashidi <abdolrashidi@google.com>
2024-06-21 18:22:32 -0700

    Vulkan: Implement EXT_clear_texture
    * Added new functions to TextureVk to clear the image.
      * clearImage()
      * clearSubImage()
        * Both implemented via clearSubImageImpl(), with the former a
          special case of the latter.
        * ImageHelper::stagePartialClear() is called to add the update.
    
    * Added new update type: ClearPartial
      * For single-sampled textures, a compute shader is used to generate
        a buffer with a preset clear value.
        * (ClearTexture.comp used in UtilsVk::clearTexture())
        * The buffer data is then transferred to the image.
    
      * For multisample textures, an attachment clear op is used through
        a render pass.
        * (UtilsVk::clearTextureMS())
        * Uses GetVkClearValueFromBytes()
          * It takes the data and the format info to determine its
            corresponding VkClearValue, color or D/S.
    
          * TODO: Method to use only one data pointer?
            * Currently DS ordering looks incorrect after
              pixelReadFunction().
    
    * Enabled the feature "supportsClearTexture" on Vulkan.
      * TODO: Some devices show failures on stencil-only tests. Therefore
        this extension should be disabled for those devices.
    
    * Added new unit tests in ClearTextureEXTTest for various formats and
      pixel sizes.
    
    * Added related multisample tests in FramebufferTest.cpp.
    
    Bug: angleproject:42266869
    Change-Id: I89c631d68a4ed63d9991abe1783333255ade20dd

--- src/libANGLE/Context.cpp
+++ src/libANGLE/Context.cpp
@@ -9986,7 +9986,7 @@ void Context::clearTexImage(TextureID texturePacked,
     // bound.
     if (texture->hasAnyDirtyBit())
     {
-        ANGLE_CONTEXT_TRY(texture->syncState(this, Command::Other));
+        ANGLE_CONTEXT_TRY(texture->syncState(this, Command::ClearTexture));
     }
 
     ANGLE_CONTEXT_TRY(
@@ -10011,7 +10011,7 @@ void Context::clearTexSubImage(TextureID texturePacked,
     // bound.
     if (texture->hasAnyDirtyBit())
     {
-        ANGLE_CONTEXT_TRY(texture->syncState(this, Command::Other));
+        ANGLE_CONTEXT_TRY(texture->syncState(this, Command::ClearTexture));
     }
 
     Box area(xoffset, yoffset, zoffset, width, height, depth);
