01336663a63dc86b6244aedcb6c335664705363d
Marcin Radomski <dextero@google.com>
2024-07-03 15:39:08 +0000

    generate_android_bp: emit flag for "frameworkless" builds
    Make roll_aosp.sh run gn with and without angle_android_frameworkless
    flag enabled. Use them to generate Android.bp files with module
    definitions that adjust required properties based on the value of a new
    angle_without_android_framework config flag.
    
    See ag/28402708 for a sample result of generated Android.bp.
    
    Bug: b/353262025
    Test: python3 -m unittest scripts/generate_android_bp.py
    Test: treehugger
    Test: Build the entire CL chain with the new Soong config flag enabled
    Test: run the system, attach debugger to eglGetDisplay(), see
    Test: DisplayVkOffscreen getting instantiated
    Change-Id: Ib76ef483ab5ad7d5e1c5ccdafb8f1c4b907ea0de

--- scripts/roll_aosp.sh
+++ scripts/roll_aosp.sh
@@ -90,6 +90,15 @@ function generate_Android_bp_file() {
 
         gn gen ${GN_OUTPUT_DIRECTORY} --args="${gn_args[*]}"
         gn desc ${GN_OUTPUT_DIRECTORY} --format=json "*" > ${GN_OUTPUT_DIRECTORY}/desc.$abi.json
+
+        gn_args_frameworkless=("${gn_args[@]}")
+        gn_args_frameworkless+=(
+            "angle_android_frameworkless = true"
+            "angle_vulkan_display_mode = \"offscreen\""
+        )
+
+        gn gen ${GN_OUTPUT_DIRECTORY} --args="${gn_args_frameworkless[*]}"
+        gn desc ${GN_OUTPUT_DIRECTORY} --format=json "*" > ${GN_OUTPUT_DIRECTORY}/desc.$abi.frameworkless.json
     done
 
     python3 scripts/generate_android_bp.py \
@@ -97,6 +106,10 @@ function generate_Android_bp_file() {
         --gn_json_arm64=${GN_OUTPUT_DIRECTORY}/desc.arm64.json \
         --gn_json_x86=${GN_OUTPUT_DIRECTORY}/desc.x86.json \
         --gn_json_x64=${GN_OUTPUT_DIRECTORY}/desc.x64.json \
+        --gn_json_arm_frameworkless=${GN_OUTPUT_DIRECTORY}/desc.arm.frameworkless.json \
+        --gn_json_arm64_frameworkless=${GN_OUTPUT_DIRECTORY}/desc.arm64.frameworkless.json \
+        --gn_json_x86_frameworkless=${GN_OUTPUT_DIRECTORY}/desc.x86.frameworkless.json \
+        --gn_json_x64_frameworkless=${GN_OUTPUT_DIRECTORY}/desc.x64.frameworkless.json \
         --output=Android.bp
 }
 
