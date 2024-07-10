f9064bc8f52593de6b08a3c3378cc4c2d1c15efb
Shahbaz Youssefi <syoussefi@chromium.org>
2024-07-10 12:47:42 -0400

    Reland "Vulkan: Use VK_KHR_dynamic_rendering[_local_read]"
    This is a reland of commit c379ff48043a47e444c388c45270db40d3172d50
    
    Original change's description:
    > Vulkan: Use VK_KHR_dynamic_rendering[_local_read]
    >
    > Bug: angleproject:42267038
    > Change-Id: I1f4eb0f309992a9c1c287a69520dadf5eff23b26
    > Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/5637155
    > Commit-Queue: Shahbaz Youssefi <syoussefi@chromium.org>
    > Reviewed-by: Amirali Abdolrashidi <abdolrashidi@google.com>
    > Reviewed-by: Charlie Lao <cclao@google.com>
    
    Bug: angleproject:42267038
    Change-Id: I083e6963b5421386695e49a9872edbb2016c9763

--- src/libANGLE/renderer/vulkan/vk_helpers.h
+++ src/libANGLE/renderer/vulkan/vk_helpers.h
@@ -101,6 +101,8 @@ enum class ImageLayout
 
     // Color (Write):
     ColorWrite,
+    // Used only with dynamic rendering, because it needs a different VkImageLayout
+    ColorWriteAndInput,
     MSRTTEmulationColorUnresolveAndResolve,
 
     // Depth (Write), Stencil (Write)
@@ -1386,9 +1388,6 @@ class CommandBufferHelperCommon : angle::NonCopyable
         mAcquireNextImageSemaphore.setHandle(semaphore);
     }
 
-    // Dumping the command stream is disabled by default.
-    static constexpr bool kEnableCommandStreamDiagnostics = false;
-
   protected:
     CommandBufferHelperCommon();
     ~CommandBufferHelperCommon();
@@ -1608,14 +1607,16 @@ class RenderPassFramebuffer : angle::NonCopyable
 
     void reset();
 
-    void setFramebuffer(Framebuffer &&initialFramebuffer,
+    void setFramebuffer(Context *context,
+                        Framebuffer &&initialFramebuffer,
                         FramebufferAttachmentsVector<VkImageView> &&imageViews,
                         uint32_t width,
                         uint32_t height,
                         uint32_t layers,
                         ImagelessFramebuffer imagelessFramebuffer)
     {
-        ASSERT(initialFramebuffer.valid());
+        // Framebuffers are mutually exclusive with dynamic rendering.
+        ASSERT(initialFramebuffer.valid() != context->getFeatures().preferDynamicRendering.enabled);
         mInitialFramebuffer = std::move(initialFramebuffer);
         mImageViews         = std::move(imageViews);
         mWidth              = width;
@@ -1627,6 +1628,7 @@ class RenderPassFramebuffer : angle::NonCopyable
     bool isImageless() { return mIsImageless; }
     const Framebuffer &getFramebuffer() const { return mInitialFramebuffer; }
     bool needsNewFramebufferWithResolveAttachments() const { return !mInitialFramebuffer.valid(); }
+    uint32_t getLayers() const { return mLayers; }
 
     // Helpers to determine if a resolve attachment already exists
     bool hasColorResolveAttachment(size_t colorIndexGL)
@@ -1659,6 +1661,12 @@ class RenderPassFramebuffer : angle::NonCopyable
 
     // Prepare for rendering using the initial imageless framebuffer.
     void packResolveViewsForRenderPassBegin(VkRenderPassAttachmentBeginInfo *beginInfoOut);
+
+    // For use with dynamic rendering.
+    const FramebufferAttachmentsVector<VkImageView> &getUnpackedImageViews() const
+    {
+        return mImageViews;
+    }
 
     // Packs views in a contiguous list.
     //
@@ -1912,6 +1920,9 @@ class RenderPassCommandBufferHelper final : public CommandBufferHelperCommon
 
     void collectRefCountedEventsGarbage(RefCountedEventsGarbageRecycler *garbageRecycler);
 
+    void updatePerfCountersForDynamicRenderingInstance(Context *context,
+                                                       angle::VulkanPerfCounters *countersOut);
+
     RenderPassSource getSource() const { return mSource; }
 
   private:
@@ -1998,6 +2009,7 @@ class RenderPassCommandBufferHelper final : public CommandBufferHelperCommon
     // final layout of the renderpass to transition it to the presentable layout
     RenderPassSource mSource;
     ImageHelper *mImageOptimizeForPresent;
+    ImageLayout mImageOptimizeForPresentOriginalLayout;
 
     friend class CommandBufferHelperCommon;
 };
