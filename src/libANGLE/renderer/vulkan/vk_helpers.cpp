966c44b62190b594b8f1ec73d6bafb2d3fb5865b
Shahbaz Youssefi <syoussefi@chromium.org>
2024-07-10 12:43:34 -0400

    Vulkan: Rework swap-time barrier logic
    Avoids unnecessary transitions when overlay is enabled
    
    Bug: angleproject:42267038
    Change-Id: I0534911c0142c5e94cf3be112283fb98fcde0f6c

--- src/libANGLE/renderer/vulkan/vk_helpers.cpp
+++ src/libANGLE/renderer/vulkan/vk_helpers.cpp
@@ -2360,9 +2360,12 @@ void RenderPassCommandBufferHelper::finalizeColorImageLayout(
         mAttachmentOps.setLayouts(packedAttachmentIndex, imageLayout, imageLayout);
     }
 
-    if (mImageOptimizeForPresent == image &&
-        context->getRenderer()->getFeatures().supportsPresentation.enabled)
+    if (mImageOptimizeForPresent == image)
     {
+        ASSERT(mSource == RenderPassSource::DefaultFramebuffer);
+
+        ASSERT(context->getFeatures().supportsPresentation.enabled);
+
         ASSERT(packedAttachmentIndex == kAttachmentIndexZero);
         // Use finalLayout instead of extra barrier for layout change to present
         mImageOptimizeForPresent->setCurrentImageLayout(ImageLayout::Present);
