98185787f50ed0f4481bd55969ee999309b6e6dd
Yuxin Hu <yuxinhu@google.com>
2024-08-01 14:15:48 -0700

    Add an option to output baked angle git hash to angle_commit.h
    We can leverage the ${rolling_to} hash value that is
    available in skia auto roller and use it as the
    ANGLE_COMMIT_HASH for Android Builds.
    
    Skia roller script can set ${rolling_to} hash to an
    environment variable ANGLE_UPSTREAM_HASH. In commit_id.py,
    check that if environment variable ANGLE_UPSTREAM_HASH
    is available, output the environment var into angle_commit.h.
    
    Bug: b/348044346
    Bug: skia:356444755
    Change-Id: I5232e4026d8b02d3ba603af0018060edb487566e

--- src/commit_id.py
+++ src/commit_id.py
@@ -113,7 +113,9 @@ commit_id = 'unknown hash'
 commit_date = 'unknown date'
 commit_position = '0'
 
-if git_dir_exists:
+commit_id = os.environ.get('ANGLE_UPSTREAM_HASH', 'unknown hash')
+
+if git_dir_exists and commit_id == 'unknown hash':
     try:
         commit_id = grab_output('git rev-parse --short=%d HEAD' % commit_id_size, cwd) or commit_id
         commit_date = grab_output('git show -s --format=%ci HEAD', cwd) or commit_date
