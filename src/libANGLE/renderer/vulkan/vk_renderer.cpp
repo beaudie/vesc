b16c4f54c94d444a3fcc4d60105b0d1bba566585
Charlie Lao <cclao@google.com>
2024-08-05 16:39:51 -0700

    Debug: Simulate Qualcomm GPU's lack of R8G8B8A8_USCALED support
    Bug: None
    Change-Id: I1a888aa1f246f9105141fd7022c004309eae0e40

--- src/libANGLE/renderer/vulkan/vk_renderer.cpp
+++ src/libANGLE/renderer/vulkan/vk_renderer.cpp
@@ -5685,6 +5685,18 @@ VkFormatFeatureFlags Renderer::getFormatFeatureBits(angle::FormatID formatID,
             {
                 deviceProperties.*features |= VK_FORMAT_FEATURE_SAMPLED_IMAGE_FILTER_LINEAR_BIT;
             }
+            if (formatID == angle::FormatID::R8G8B8A8_USCALED)
+            {
+                // for black_desert_mobile
+                deviceProperties.*features &= ~VK_FORMAT_FEATURE_VERTEX_BUFFER_BIT;
+                // deviceProperties.*features |= VK_FORMAT_FEATURE_VERTEX_BUFFER_BIT;
+            }
+            if (formatID == angle::FormatID::R16G16_SSCALED)
+            {
+                // for gangstar_vegas
+                deviceProperties.*features &= ~VK_FORMAT_FEATURE_VERTEX_BUFFER_BIT;
+                // deviceProperties.*features |= VK_FORMAT_FEATURE_VERTEX_BUFFER_BIT;
+            }
         }
     }
 
