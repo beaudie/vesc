154f7ab2376ed3502849734e829af65456a24ac5
angle-autoroll <angle-autoroll@skia-public.iam.gserviceaccount.com>
2024-08-15 10:01:05 +0000

    Roll vulkan-deps from f084a55f96da to 91058f2297c9 (4 revisions)
    Suppress new VUIDs:
    VUID-vkCmdDraw-Input-07939
    
    https://chromium.googlesource.com/vulkan-deps.git/+log/f084a55f96da..91058f2297c9
    
    Also rolling transitive DEPS:
      https://chromium.googlesource.com/external/github.com/KhronosGroup/glslang/+log/3a08cd8de1f88d114c18670366f29226db453a9e..0dc6711e5a178e4d5643437af688c6b48f829f5c
      https://chromium.googlesource.com/external/github.com/KhronosGroup/SPIRV-Headers/+log/f013f08e4455bcc1f0eed8e3dd5e2009682656d9..1b75a4ae0b4289014b4c369301dc925c366f78a6
      https://chromium.googlesource.com/external/github.com/KhronosGroup/Vulkan-ValidationLayers/+log/8d902c7f67edd9e54b0fa5f7d2c34f9abfeacba8..4eb3585f32ec194efe2d9cd88a2b2943ff41f922
    
    If this roll has caused a breakage, revert this CL and stop the roller
    using the controls here:
    https://autoroll.skia.org/r/vulkan-deps-angle-autoroll
    Please CC angle-team@google.com,ynovikov@google.com on the revert to
    ensure that a human
    is aware of the problem.
    
    To file a bug in ANGLE:
    https://bugs.chromium.org/p/angleproject/issues/entry
    
    To report a problem with the AutoRoller itself, please file a bug:
    https://issues.skia.org/issues/new?component=1389291&template=1850622
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+doc/main/autoroll/README.md
    
    Bug: chromium:359904720
    Change-Id: Idf82bf9a210b953785fc2387e5c64ef157ec36e6

--- src/libANGLE/renderer/vulkan/vk_renderer.cpp
+++ src/libANGLE/renderer/vulkan/vk_renderer.cpp
@@ -269,6 +269,8 @@ constexpr const char *kSkippedMessages[] = {
     "VUID-vkCmdDraw-None-06550",
     // https://anglebug.com/345304850
     "WARNING-Shader-OutputNotConsumed",
+    // https://crbug.com/359904720
+    "VUID-vkCmdDraw-Input-07939",
 };
 
 // Validation messages that should be ignored only when VK_EXT_primitive_topology_list_restart is
