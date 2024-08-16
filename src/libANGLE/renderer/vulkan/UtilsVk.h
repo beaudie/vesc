91a3fcab7ef6ee641721062c4067228033b2bb27
Charlie Lao <cclao@google.com>
2024-08-16 14:56:37 -0700

    Vulkan: Make VertexConversionBuffer a class
    And wrap the cache key (i.e, formatID/stride/offset) into a CacheKey
    struct so that we can easily add more data members. This CL also changes
    ConversionBuffer from struct to class to have better encapsulation. No
    functional changes is expected here.
    
    Bug: b/357622380
    Change-Id: Ieecf5c922b95a940137c8e54657ef3f458c55fc9

--- src/libANGLE/renderer/vulkan/UtilsVk.h
+++ src/libANGLE/renderer/vulkan/UtilsVk.h
@@ -28,6 +28,7 @@
 #ifndef LIBANGLE_RENDERER_VULKAN_UTILSVK_H_
 #define LIBANGLE_RENDERER_VULKAN_UTILSVK_H_
 
+#include "libANGLE/renderer/vulkan/BufferVk.h"
 #include "libANGLE/renderer/vulkan/vk_cache_utils.h"
 #include "libANGLE/renderer/vulkan/vk_helpers.h"
 #include "libANGLE/renderer/vulkan/vk_internal_shaders_autogen.h"
@@ -781,8 +782,8 @@ class LineLoopHelper final : angle::NonCopyable
     }
 
   private:
-    vk::BufferHelper mDynamicIndexBuffer;
-    vk::BufferHelper mDynamicIndirectBuffer;
+    ConversionBuffer mDynamicIndexBuffer;
+    ConversionBuffer mDynamicIndirectBuffer;
 };
 }  // namespace rx
 
