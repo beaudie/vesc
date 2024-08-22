060a71e8509e3c998e80c2f9e4b570412e86d479
Amirali Abdolrashidi <abdolrashidi@google.com>
2024-08-22 15:29:00 -0700

    Test: Use Vulkan API for full clears
    * For full clears from EXT_clear_texture, the following APIs
      are used:
      * vkCmdClearColorImage()
      * vkCmdClearDepthStencilImage()
    
    * TODO: Errors on some platforms regarding full clear of 3D
      textures (such as tests Clear3DWhole and Clear3DZeroDims)
    * TODO: Instead of staging the update, go through the clear
      path (not a deferred update).
    
    Bug: None
    Change-Id: I12ef3002dee190d7f8f43204f7d3f76e05d0b54f

--- src/libANGLE/renderer/vulkan/vk_helpers.h
+++ src/libANGLE/renderer/vulkan/vk_helpers.h
@@ -2459,6 +2459,7 @@ class ImageHelper final : public Resource, public angle::Subject
 
     angle::Result stagePartialClear(ContextVk *contextVk,
                                     const gl::Box &clearArea,
+                                    const bool isFullClear,
                                     gl::TextureType textureType,
                                     uint32_t levelIndex,
                                     uint32_t layerIndex,
