44d4249f67df8ef2362b589df68a58700c802bb9
Yuxin Hu <yuxinhu@google.com>
2024-09-02 16:23:44 -0700

    Add new functions defined in VK_KHR_Synchronization2 extension
    Bug: b/356985874
    Change-Id: I4f8b432aee9b11debcb8abe8b3089f88ad7a069b

--- src/libANGLE/renderer/vulkan/vk_renderer.cpp
+++ src/libANGLE/renderer/vulkan/vk_renderer.cpp
@@ -3558,6 +3558,10 @@ void Renderer::initDeviceExtensionEntryPoints()
         InitExternalMemoryHardwareBufferANDROIDFunctions(mDevice);
     }
 #    endif
+    if (mFeatures.supportsSynchronization2.enabled)
+    {
+        InitSynchronization2Functions(mDevice);
+    }
     // Extensions promoted to Vulkan 1.2
     {
         if (mFeatures.supportsHostQueryReset.enabled)
