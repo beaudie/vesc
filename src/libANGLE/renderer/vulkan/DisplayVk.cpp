07ef6a6908ab4ae93ca7cbd40a719adf13483ea6
Igor Nazarov <i.nazarov@samsung.com>
2024-09-04 20:04:10 +0300

    Vulkan: Add dual slots in CompressAndStorePipelineCacheVk()
    Change fixes following problem:
       Currently, each call to `CompressAndStorePipelineCacheVk()` stores
       chunks in order, starting from 0. This overrides previously stored
       chunks. In case of app termination (kill) in the middle of this
       process, the entire cache data will be corrupted, since it will
       partially contain chunks from the new and old caches.
    
    Solution:
    
    In order to fix this problem, this change introduces `slotIndex` into
    the `chunkCacheHash` calculation. Slot index is managed by
    `GlobalOps::getNextPipelineBlobCacheSlotIndex()` abstract method, which
    will alternate between 0 and 1 in `DisplayVk` implementation (when
    "useDualPipelineBlobCacheSlots" feature is enabled) and always
    0 in `CLPlatformVk` implementation.
    
    Additionally, chunk storing order is reversed: last chunk is stored
    first and the first (0 chunk) - last. This is done because 0 chunk is
    the first that is loaded in `GetAndDecompressPipelineCacheVk()` and used
    as indication that there is data in the cache. Writing it last, ensures
    that other chunks will be also available.
    
    In case of `DisplayVk`, each call to `CompressAndStorePipelineCacheVk()`
    will use slot index opposed to the slot that is stored in the cache,
    avoiding damaging existing data. After writing all chunks for a brief
    moment there may be 2 instances of the data. However, data for the
    previous slot will be immediately erased (by writing 1/0-sized blobs)
    starting from the 0 chunk.
    
    To determine if it is possible to write  0-sized blobs, added
    `GlobalOps::isBlobCacheSupportsZeroSizedValues()` abstract
    method. It is always `true` for `CLPlatformVk`, or depends on
    "hasBlobCacheWithZeroSizedValuesSupport" feature for `DisplayVk`.
    
    The `GetAndDecompressPipelineCacheVk()` function will iterate over each
    available slot index checking only 0 chunk until data is found.
    
    Note: this solution requires 2X pipeline cache size space in the blob
    cache to work as expected, otherwise it will exacerbate other problem:
       When blob cache is full, but still allows to store the current
       pipeline cache data, storing next chunk may trigger eviction of
       already stored items. Depending on the blob cache implementation,
       eviction process may choose to evict chunks from the current pipeline
       cache data. As the result: blob cache will not contain all chunks.
    
    The above problem will be addressed in the follow up CL.
    
    Bug: angleproject:4722
    Change-Id: I2920bc3d89263280cdfe0466446fca26415e2b25

--- src/libANGLE/renderer/vulkan/DisplayVk.cpp
+++ src/libANGLE/renderer/vulkan/DisplayVk.cpp
@@ -154,7 +154,8 @@ DisplayVk::DisplayVk(const egl::DisplayState &state)
     : DisplayImpl(state),
       vk::Context(new vk::Renderer()),
       mScratchBuffer(1000u),
-      mSupportedColorspaceFormatsMap{}
+      mSupportedColorspaceFormatsMap{},
+      mCurrentPipelineBlobCacheSlotIndex(0)
 {}
 
 DisplayVk::~DisplayVk()
@@ -660,6 +661,24 @@ void DisplayVk::populateFeatureList(angle::FeatureList *features)
 }
 
 // vk::GlobalOps
+uint8_t DisplayVk::getNextPipelineBlobCacheSlotIndex(uint8_t *previousSlotIndexOut)
+{
+    if (previousSlotIndexOut != nullptr)
+    {
+        *previousSlotIndexOut = mCurrentPipelineBlobCacheSlotIndex;
+    }
+    if (getFeatures().useDualPipelineBlobCacheSlots.enabled)
+    {
+        mCurrentPipelineBlobCacheSlotIndex = 1 - mCurrentPipelineBlobCacheSlotIndex;
+    }
+    return mCurrentPipelineBlobCacheSlotIndex;
+}
+
+bool DisplayVk::isBlobCacheSupportsZeroSizedValues() const
+{
+    return getFeatures().hasBlobCacheWithZeroSizedValuesSupport.enabled;
+}
+
 void DisplayVk::putBlob(const angle::BlobCacheKey &key, const angle::MemoryBuffer &value)
 {
     getBlobCache()->putApplication(key, value);
