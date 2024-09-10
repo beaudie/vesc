79995ba69768cae3a6195912714985839aa4cbb7
Gowtham Tammana <g.tammana@samsung.com>
2024-09-10 10:28:37 -0500

    CL: Drop image_descriptor and format from image create
    The image descriptor and format are part of the state and as such remove
    them passing down through the create()
    
    Bug: angleproject:365694588
    Change-Id: I6e5c4595c0093f15df65f43ed607643cb7c2ae14
    Signed-off-by: Gowtham Tammana <g.tammana@samsung.com>

--- src/libANGLE/CLMemory.cpp
+++ src/libANGLE/CLMemory.cpp
@@ -201,27 +201,6 @@ Memory::Memory(const Image &image,
       mImpl(nullptr),
       mSize(0u),
       mMapCount(0u)
-{
-    ANGLE_CL_IMPL_TRY(context.getImpl().createImage(image, flags, format, desc, hostPtr, &mImpl));
-    switch (image.getDescriptor().type)
-    {
-        case MemObjectType::Image1D_Array:
-            mSize = image.getSliceSize() * image.getDescriptor().arraySize;
-            break;
-        case MemObjectType::Image2D:
-            mSize = image.getSliceSize();
-            break;
-        case MemObjectType::Image2D_Array:
-            mSize = image.getSliceSize() * image.getDescriptor().arraySize;
-            break;
-        case MemObjectType::Image3D:
-            mSize = image.getSliceSize() * image.getDescriptor().depth;
-            break;
-        default:
-            // 1D, 1D-buffer and buffer
-            mSize = image.getRowSize();
-            break;
-    }
-}
+{}
 
 }  // namespace cl
