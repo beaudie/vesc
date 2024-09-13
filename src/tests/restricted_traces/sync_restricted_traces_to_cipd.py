28f67a307cff16a394ed36bfff029e93aac08d99
Mark Lobodzinski <mark@lunarg.com>
2024-09-13 10:49:08 -0600

    Add pre-upload Required Extensions check to trace sync script
    Add a check to make sure that a new ANGLE trace has been run through
    the retracer with the 'get_min_reqs` argument before uploading to CIPD.
    
    Bug: b/42266279
    Change-Id: Id6f847faf22a42062ab189ea9bc7fb9c3506e66b

--- src/tests/restricted_traces/sync_restricted_traces_to_cipd.py
+++ src/tests/restricted_traces/sync_restricted_traces_to_cipd.py
@@ -87,9 +87,15 @@ def check_trace_before_upload(trace):
             jtrace = json.load(f)
         additional_files = set([trace_json, trace + '.angledata.gz'])
         extra_files = set(files) - set(jtrace['TraceFiles']) - additional_files
+        required_extensions = 'RequiredExtensions' in jtrace
         if extra_files:
             logging.error('Unexpected files, not listed in %s.json [TraceFiles]:\n%s', trace,
                           '\n'.join(extra_files))
+        if not required_extensions:
+            logging.error(
+                '"RequiredExtensions" missing from %s.json.  Please run retrace_restricted_traces.py with the "get_min_reqs" argument\n',
+                trace)
+        if extra_files or not required_extensions:
             sys.exit(1)
 
 
