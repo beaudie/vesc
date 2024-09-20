01a3f2be60a775061ae3c4f31fbbfd49521091f0
Geoff Lang <geofflang@chromium.org>
2024-09-20 12:00:17 -0400

    WebGPU: Sync index buffers, add indexed draw calls
    Bug: angleproject:368600230
    Change-Id: Ibf1c4921635b525b7650e70ce7dc939e2bad2ebb

--- src/libANGLE/renderer/wgpu/wgpu_utils.cpp
+++ src/libANGLE/renderer/wgpu/wgpu_utils.cpp
@@ -581,5 +581,20 @@ wgpu::StencilOperation getStencilOp(const GLenum glStencilOp)
             return wgpu::StencilOperation::Keep;
     }
 }
+
+uint32_t GetFirstIndexForDrawCall(gl::DrawElementsType indexType, const void *indices)
+{
+    const size_t indexSize =
+        (indexType == gl::DrawElementsType::UnsignedInt) ? sizeof(GLuint) : sizeof(GLushort);
+    const uintptr_t indexBufferByteOffset = reinterpret_cast<uintptr_t>(indices);
+    if (indexBufferByteOffset % indexSize != 0)
+    {
+        // WebGPU only allows offsetting index buffers by multiples of the index size
+        UNIMPLEMENTED();
+    }
+
+    return static_cast<uint32_t>(indexBufferByteOffset / indexSize);
+}
+
 }  // namespace gl_wgpu
 }  // namespace rx
