b16ccc5a9cf1fb5d69da9732513bc0d269e9f569
Charlie Lao <cclao@google.com>
2024-10-11 14:21:19 -0700

    Vulkan: Switch DescriptorPoolPointer to use SharedPtr
    To make the reference counting easier to maintain, SharedPtr class is
    added to automatically tracking the reference counting and object
    creation/destruction. Right now we have BindingPointer class doing
    similar things except it does not automatically manage the object
    create/destroy, which makes it less robust as well as redundant code to
    manage object life cycle. The other problem with BindingPointer is that
    it does not work with normal assign/copy operator which made it hard to
    read and use.  SharedPtr uses exact same API semantics as
    std::shared_ptr, which makes the reference counting very easy to use.
    The main difference of SharedPtr and std::shared_ptr is that it does not
    use any atomic or lock since it assumes user only uses it under thread
    safe environment, which ANGLE's backend is.
    
    This CL also changes mDescriptorPools to mDynamicDescriptorPools to make
    it clear that it is dynamic descriptor pool not the descriptor pool.
    
    This is also preparation CL for the next CL where we will use SharedPtr
    to manage DescriptorSetHelper life cycle, which otherwise a bit
    complicated to manually manage.
    
    Bug: angleproject:372268711
    Change-Id: I1033d9bf259bbc075a9b374d8a28e1f67d889873

--- src/libANGLE/renderer/vulkan/vk_helpers.cpp
+++ src/libANGLE/renderer/vulkan/vk_helpers.cpp
@@ -4261,6 +4261,7 @@ DynamicDescriptorPool::DynamicDescriptorPool()
 DynamicDescriptorPool::~DynamicDescriptorPool()
 {
     ASSERT(mDescriptorSetCache.empty());
+    ASSERT(mDescriptorPools.empty());
 }
 
 DynamicDescriptorPool::DynamicDescriptorPool(DynamicDescriptorPool &&other)
@@ -12636,9 +12637,9 @@ void MetaDescriptorPool::destroy(Renderer *renderer)
 {
     for (auto &iter : mPayload)
     {
-        RefCountedDescriptorPool &refCountedPool = iter.second;
-        ASSERT(!refCountedPool.isReferenced());
-        refCountedPool.get().destroy(renderer);
+        DynamicDescriptorPoolPointer &pool = iter.second;
+        ASSERT(pool.unique());
+        pool->destroy(renderer);
     }
 
     mPayload.clear();
@@ -12649,13 +12650,12 @@ angle::Result MetaDescriptorPool::bindCachedDescriptorPool(
     const DescriptorSetLayoutDesc &descriptorSetLayoutDesc,
     uint32_t descriptorCountMultiplier,
     DescriptorSetLayoutCache *descriptorSetLayoutCache,
-    DescriptorPoolPointer *descriptorPoolOut)
+    DynamicDescriptorPoolPointer *dynamicDescriptorPoolOut)
 {
     auto cacheIter = mPayload.find(descriptorSetLayoutDesc);
     if (cacheIter != mPayload.end())
     {
-        RefCountedDescriptorPool &descriptorPool = cacheIter->second;
-        descriptorPoolOut->set(&descriptorPool);
+        *dynamicDescriptorPoolOut = cacheIter->second;
         return angle::Result::Continue;
     }
 
@@ -12667,10 +12667,10 @@ angle::Result MetaDescriptorPool::bindCachedDescriptorPool(
     ANGLE_TRY(InitDynamicDescriptorPool(context, descriptorSetLayoutDesc, descriptorSetLayout.get(),
                                         descriptorCountMultiplier, &newDescriptorPool));
 
-    auto insertIter = mPayload.emplace(descriptorSetLayoutDesc, std::move(newDescriptorPool));
-
-    RefCountedDescriptorPool &descriptorPool = insertIter.first->second;
-    descriptorPoolOut->set(&descriptorPool);
+    ASSERT(newDescriptorPool.valid());
+    DynamicDescriptorPoolPointer newDynamicDescriptorPoolPtr(std::move(newDescriptorPool));
+    mPayload.emplace(descriptorSetLayoutDesc, newDynamicDescriptorPoolPtr);
+    *dynamicDescriptorPoolOut = std::move(newDynamicDescriptorPoolPtr);
 
     return angle::Result::Continue;
 }
