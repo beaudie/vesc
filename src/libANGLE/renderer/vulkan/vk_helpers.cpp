ce6406cfe34f807505a4fbf4daac12e1f6727856
Charlie Lao <cclao@google.com>
2024-10-15 13:05:28 -0700

    Vulkan: Add feature flag to enable descriptorSet cache
    So that we can disable it to compare the performance difference.
    
    Bug: angleproject:372268711
    Change-Id: I02da254e5d58815741080634a2dd005617aa7432

--- src/libANGLE/renderer/vulkan/vk_helpers.cpp
+++ src/libANGLE/renderer/vulkan/vk_helpers.cpp
@@ -4484,6 +4484,7 @@ angle::Result DynamicDescriptorPool::getOrAllocateDescriptorSet(
     DescriptorSetPointer *descriptorSetOut,
     SharedDescriptorSetCacheKey *newSharedCacheKeyOut)
 {
+    ASSERT(context->getFeatures().descriptorSetCache.enabled);
     // First scan the descriptorSet cache.
     if (mDescriptorSetCache.getDescriptorSet(desc, descriptorSetOut))
     {
@@ -4548,6 +4549,7 @@ angle::Result DynamicDescriptorPool::allocateNewPool(Context *context)
 void DynamicDescriptorPool::releaseCachedDescriptorSet(Renderer *renderer,
                                                        const DescriptorSetDesc &desc)
 {
+    ASSERT(renderer->getFeatures().descriptorSetCache.enabled);
     DescriptorSetPointer descriptorSet;
     if (mDescriptorSetCache.getDescriptorSet(desc, &descriptorSet))
     {
