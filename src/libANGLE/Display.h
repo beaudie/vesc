42e9e027f4884625b9418c87bea3b694e4212399
Yuxin Hu <yuxinhu@google.com>
2024-10-15 18:27:00 -0700

    Use EGL sync global lock for all EGL*sync entrypoint calls
    To free EGL sync operations from waiting for other EGL calls
    to finish, we use a separate global lock for EGL*sync entrypoints.
    Below angle::SimpleMutex are added to protect resources that may
    have race condition due to being accessed by EGL*sync calls
    and non EGL*sync calls at the same time:
    
    1. Display::mContextMapMutex that protects Display::mState.contextMap
    
    2. static angle::base::NoDestructor<angle::SimpleMutex>
    anglePlatformDisplayMapMutex that protects static
    angle::base::NoDestructor<ANGLEPlatformDisplayMap> displays
    
    3. static angle::base::NoDestructor<angle::SimpleMutex>
    devicePlatformDisplayMapMutex that protects static
    angle::base::NoDestructor<DevicePlatformDisplayMap> displays
    
    EGL_Terminate() entry point takes both global lock and
    global egl sync lock. This is to protect Display::mSyncMap,
    Display::mSyncPools, and Display::mSyncHandleAllocator being
    get cleared by thread 1 calling eglTerminate, while they are
    still accessed by thread 2 through a call such as eglCreateSync.
    So that we won't have thread 2 finish validating the sync object with
    syncID exists in Display::mSyncMap, but then find the
    mSyncMap.find(syncID) returns a nullptr due to the mSyncMap
    is cleared by thread 1.
    
    When ANGLE_CAPTURE_ENABLED is enabled, fall back to global lock,
    as the CaptureEGLCallToFrameCapture() touches many resources
    (e.g. mMaxAccessedResourceIDs, mFrameCalls)
    that could lead to race conditions without a global lock.
    
    Bug: b/362604439
    Change-Id: Ic0d54a4cd66743bcd0f48f41f247dd223cff2f5e

--- src/libANGLE/Display.h
+++ src/libANGLE/Display.h
@@ -82,6 +82,7 @@ struct DisplayState final : private angle::NonCopyable
 
     EGLLabelKHR label;
     ContextMap contextMap;
+    mutable angle::SimpleMutex contextMapMutex;
     SurfaceMap surfaceMap;
     angle::FeatureOverrides featureOverrides;
     EGLNativeDisplayType displayId;
@@ -354,7 +355,9 @@ class Display final : public LabeledObject,
 
     Error restoreLostDevice();
     Error releaseContext(gl::Context *context, Thread *thread);
-    Error releaseContextImpl(gl::Context *context, ContextMap *contexts);
+    Error releaseContextImpl(gl::Context *context,
+                             ContextMap *contexts,
+                             std::unique_lock<angle::SimpleMutex> &&contextMapLock);
 
     void initDisplayExtensions();
     void initVendorString();
