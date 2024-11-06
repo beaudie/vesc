1dc022f67a51e113018cf6b26d0a0be285ed86e3
Ho Cheung <hocheung@chromium.org>
2024-11-06 16:08:28 +0800

    [code health] Remove underscores from test names in ANGLE (1/N)
    In the GoogleTest framework, underscores are not
    allowed. [1] ANGLE has underscores in many test
    names, and those should be changed.
    
    At the same time, add a comment to a test case
    according to the requirements of the Presubmit
    check.
    
    ````
    ***************
    ANGLE requires a comment describing what a test does.
    ***************
    ````
    
    [1]
    https://github.com/google/googletest/blob/main/docs/faq.md#why-should-test-suite-names-and-test-names-not-contain-underscore
    
    Bug: angleproject:377543127
    Change-Id: I90e634bd985ca518eca43e2c919a3a8bca91a55b

--- src/tests/compiler_tests/UnrollFlatten_test.cpp
+++ src/tests/compiler_tests/UnrollFlatten_test.cpp
@@ -201,14 +201,14 @@ TEST_F(UnrollFlattenTest, GradientInDiscont)
     expect(expectations, ArraySize(expectations));
 }
 
-class UnrollFlattenTest_ES3 : public UnrollFlattenTest
+class UnrollFlattenWithES3Test : public UnrollFlattenTest
 {
   public:
-    UnrollFlattenTest_ES3() : UnrollFlattenTest(SH_GLES3_SPEC) {}
+    UnrollFlattenWithES3Test() : UnrollFlattenTest(SH_GLES3_SPEC) {}
 };
 
 // Check that we correctly detect the ES3 builtin "texture" function as a gradient operation.
-TEST_F(UnrollFlattenTest_ES3, TextureBuiltin)
+TEST_F(UnrollFlattenWithES3Test, TextureBuiltin)
 {
     const std::string &shaderString =
         "#version 300 es\n"
